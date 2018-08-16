606e3988366c8e203e7c512cfa385316b00bc021
co9olguy <nathan@xanadu.ai>
2018-08-16 14:46:26 -0400

    fix typos

--- openqml/plugins/strawberryfields.py
+++ openqml/plugins/strawberryfields.py
@@ -75,7 +75,7 @@ class Gate(GateSpec):
 class Observable(Gate):
     """Implements hermitian observables.
 
-    We assume that all the observables in the circuit are consequtive, and commute.
+    We assume that all the observables in the circuit are consecutive, and commute.
     Since we are only interested in the expectation values, there is no need to project the state after the measurement.
     See :ref:`measurements`.
     """
