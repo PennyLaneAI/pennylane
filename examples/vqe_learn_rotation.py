69081feadaa709506cf885e60573a6de23510cad
mariaschuld <mariaschuld@gmail.com>
2018-08-29 14:13:32 +0200

    started coding new vqe examples

---
+++ examples/vqe_learn_rotation.py
@@ -0,0 +1,61 @@
+"""Variational quantum eigensolver example.
+
+In this demo we use a variational circuit as an ansatz for
+a VQE, and optimize the circuit to lower the energy expectation
+of a user-defined Hamiltonian.
+"""
+
+import openqml as qm
+from openqml import numpy as np
+
+dev1 = qm.device('default.qubit', wires=2)
+
+
+def ansatz(x,y,z):
+    qm.Rot(x, y, z, [0])
+    qm.CNOT([0, 1])
+
+
+@qm.qfunc(dev1)
+def circuit_Z(x, y, z):
+    """QNode"""
+    ansatz(x, y, z)
+    qm.expectation.PauliZ(1)
+
+
+@qm.qfunc(dev1)
+def circuit_Y(x, y, z):
+    """QNode"""
+    ansatz(x, y, z)
+    qm.expectation.PauliY(1)
+
+
+@qm.qfunc(dev1)
+def circuit_X(x, y, z):  # Todo: argument of circuit
+    """QNode"""
+    ansatz(x, y, z)
+    qm.expectation.PauliX(1)
+
+
+def cost(weights, batch):  # Todo: remove batch
+    """Cost (error) function to be minimized."""
+
+    expZ = circuit_Z(weights)
+    expX = circuit_X(weights)
+    expY = circuit_Y(weights)
+
+    return 0.1*expX + 0.5*expY - 0.3*expZ
+
+
+# initialize x with random value
+x0 = np.random.randn(3)
+o = qm.Optimizer(cost, x0)
+
+# train the circuit
+o.train(max_steps=100)
+
+# print the results
+print('Initial rotation angles:', x0)
+print('Optimized rotation angles:', o.weights)
+
+# Does not learn!!!!!!!!!!????????
