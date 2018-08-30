ee52daa752ea1b7c14a891d5fd963db9553ad855
mariaschuld <mariaschuld@gmail.com>
2018-08-30 10:00:31 +0200

    Improving second vqe example.

--- examples/vqe_learn_hamiltonian.py
+++ examples/vqe_learn_hamiltonian.py
@@ -1,72 +1,65 @@
 """Variational quantum eigensolver example.
-<<<<<<< HEAD
 
-=======
->>>>>>> d22739a2f4b1cb3e09f1cc7910801d7fd4fbfbe8
 In this demo we use a fixed quantum circuit
 and optimize the (classical) Hamiltonian parameters
 to lower the energy expectation. """
 
 import openqml as qm
 from openqml import numpy as np
-<<<<<<< HEAD
-=======
-import autograd
->>>>>>> d22739a2f4b1cb3e09f1cc7910801d7fd4fbfbe8
-
-dev1 = qm.device('default.qubit', wires=2)
 
 
 def ansatz():
-<<<<<<< HEAD
-=======
-    qm.QubitStateVector(np.array([1,1,0,1])/np.sqrt(3), wires=[0, 1])
->>>>>>> d22739a2f4b1cb3e09f1cc7910801d7fd4fbfbe8
+    """ Ansatz of the variational circuit."""
+    initial_state = np.array([1, 1, 0, 1])/np.sqrt(3)
+    qm.QubitStateVector(initial_state, wires=[0, 1])
+
     qm.Rot(0.4, 0.3, 1.3, [0])
     qm.CNOT([0, 1])
 
 
+dev1 = qm.device('default.qubit', wires=2)
+
+
 @qm.qfunc(dev1)
-def circuit_Z():
-    """QNode"""
+def circuit_X():
+    """Circuit measuring the X operator"""
     ansatz()
-    qm.expectation.PauliZ(1)
+    qm.expectation.PauliX(1)
 
 
 @qm.qfunc(dev1)
 def circuit_Y():
-    """QNode"""
+    """Circuit measuring the Y operator"""
     ansatz()
     qm.expectation.PauliY(1)
 
 
 @qm.qfunc(dev1)
-def circuit_X():
-    """QNode"""
+def circuit_Z():
+    """Circuit measuring the Z operator"""
     ansatz()
-    qm.expectation.PauliX(1)
+    qm.expectation.PauliZ(1)
 
 
 def cost(weights, batch):
     """Cost (error) function to be minimized."""
 
-    expZ = circuit_Z()
     expX = circuit_X()
     expY = circuit_Y()
+    expZ = circuit_Z()
 
     return weights[0]*expX + weights[1]*expY - weights[2]*expZ
 
 
 # initialize x with random value
-x0 = np.random.randn(3)
-
-o = qm.Optimizer(cost, x0)
+weights0 = np.random.randn(3)
 
 # train the device
+o = qm.Optimizer(cost, weights0)
 o.train(max_steps=100)
 
-print('Initial rotation angles:', x0)
-o.train(max_steps=100)
-o.weights
+print('Initial hamiltonian coefficients:', weights0)
+print('Trained hamiltonian coefficients:', o.weights)
+
 
 
