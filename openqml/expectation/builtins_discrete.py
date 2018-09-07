fbc3429f1cf5b7077aa6a8af6cffe65d791dc557
co9olguy <nathan@xanadu.ai>
2018-09-07 16:34:23 -0400

    fix typos

--- openqml/expectation/builtins_discrete.py
+++ openqml/expectation/builtins_discrete.py
@@ -11,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""This module contains a beamsplitter operation"""
+"""This module contains operations for the default qubit observables"""
+
 from .expectation import Expectation
 
 
