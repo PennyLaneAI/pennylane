43c27b7d6459b61e197c0ef8b61dbeb954177173
Christian Gogolin <git@cgogolin.de>
2018-09-13 16:44:18 +0200

    return numpy array from execute() fixes in the example

--- openqml-pq/examples/qubit_optimization.py
+++ openqml-pq/examples/qubit_optimization.py
@@ -24,23 +24,24 @@ def circuit(x, y, z):
     qm.RY(y, [0])
     qm.RX(x, [0])
     qm.CNOT([0, 1])
-    return qm.expectation.PauliZ(1)
+    return qm.expectation.PauliZ(wires=1)
 
+circuit = qm.QNode(circuit, dev1)
 
-def cost(x, batched):
+def cost(xyz, batched):
     """Cost (error) function to be minimized."""
-    return np.abs(circuit(x)-1)
+    return np.abs(circuit(*xyz)-1)
 
 
 # initialize x with random value
-x0 = np.random.randn(3)
-o = qm.Optimizer(cost, x0, optimizer=args.optimizer)
+xyz0 = np.random.randn(3)
+o = qm.Optimizer(cost, xyz0, optimizer=args.optimizer)
 
 # train the circuit
 c = o.train(max_steps=100)
 
 # print the results
-print('Initial rotation angles:', x0)
+print('Initial rotation angles:', xyz0)
 print('Optimized rotation angles:', o.weights)
 print('Circuit output at rotation angles:', circuit(*o.weights))
 print('Circuit gradient at rotation angles:', qm.grad(circuit, o.weights)[0])
