296608a5a1a706dafb2d0f5929d5cc8a6e275161
Josh Izaac <josh146@gmail.com>
2018-09-18 15:11:39 +0800

    added tests and bugfixes

--- openqml-sf/openqml_sf/simulator.py
+++ openqml-sf/openqml_sf/simulator.py
@@ -37,6 +37,9 @@ class StrawberryFieldsSimulator(Device):
     version = __version__
     author = 'Josh Izaac'
 
+    short_name = 'strawberryfields'
+    _operator_map = None
+
     def __init__(self, wires, *, shots=0, hbar=2):
         self.wires = wires
         self.hbar = hbar
@@ -63,10 +66,10 @@ class StrawberryFieldsSimulator(Device):
             params (Sequence): sequence of gate parameters.
         """
         gate = self._operator_map[gate_name](*params)
-        if isinstance(wires, int):
+        if isinstance(wires, int): #pragma: no cover
             gate | self.q[wires] #pylint: disable=pointless-statement
         else:
-            gate | [self.q[i] for i in wires] #pyling: disable=pointless-statement
+            gate | [self.q[i] for i in wires] #pylint: disable=pointless-statement
 
     @abc.abstractmethod
     def pre_execute_expectations(self):
---
+++ openqml-sf/tests/test_fock.py
@@ -0,0 +1,232 @@
+# Copyright 2018 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Unit tests for the Fock plugin.
+"""
+import inspect
+import unittest
+import logging as log
+log.getLogger()
+
+import strawberryfields as sf
+
+import openqml as qm
+from openqml import numpy as np
+
+from defaults import openqml_sf as qmsf, BaseTest
+
+
+class FockTests(BaseTest):
+    """Test the Fock simulator."""
+
+    def test_load_fock_device(self):
+        """Test that the fock plugin loads correctly"""
+        log.info('test_load_fock_device')
+
+        dev = qm.device('strawberryfields.fock', wires=2, cutoff_dim=5)
+        self.assertEqual(dev.wires, 2)
+        self.assertEqual(dev.cutoff, 5)
+        self.assertEqual(dev.hbar, 2)
+        self.assertEqual(dev.shots, 0)
+        self.assertEqual(dev.short_name, 'strawberryfields.fock')
+
+    def test_fock_args(self):
+        """Test that the fock plugin requires correct arguments"""
+        log.info('test_fock_args')
+
+        with self.assertRaisesRegex(TypeError, "missing 1 required positional argument: 'wires'"):
+            dev = qm.device('strawberryfields.fock')
+        with self.assertRaisesRegex(TypeError, "missing 1 required keyword-only argument: 'cutoff_dim'"):
+            dev = qm.device('strawberryfields.fock', wires=1)
+
+    def test_unsupported_gates(self):
+        """Test error is raised with unsupported gates"""
+        log.info('test_unsupported_ops')
+
+        dev = qm.device('strawberryfields.fock', wires=2, cutoff_dim=2)
+        gates = set(dev._operator_map.keys())
+        all_gates = {m[0] for m in inspect.getmembers(qm.ops, inspect.isclass)}
+
+        for g in all_gates - gates:
+            op = getattr(qm.ops, g)
+
+            if op.n_wires == 0:
+                wires = [0]
+            else:
+                wires = list(range(op.n_wires))
+
+            @qm.qfunc(dev)
+            def circuit(*x):
+                op(*x, wires=wires)
+                return qm.expectation.Fock(0)
+
+            with self.assertRaisesRegex(qm.DeviceError,
+                "Gate {} not supported on device strawberryfields.fock".format(g)):
+                x = np.random.random([op.n_params])
+                circuit(*x)
+
+    def test_unsupported_observables(self):
+        """Test error is raised with unsupported observables"""
+        log.info('test_unsupported_observables')
+
+        dev = qm.device('strawberryfields.fock', wires=2, cutoff_dim=2)
+        obs = set(dev._observable_map.keys())
+        all_obs = {m[0] for m in inspect.getmembers(qm.expectation, inspect.isclass)}
+
+        for g in all_obs - obs:
+            op = getattr(qm.expectation, g)
+
+            if op.n_wires == 0:
+                wires = [0]
+            else:
+                wires = list(range(op.n_wires))
+
+            @qm.qfunc(dev)
+            def circuit(*x):
+                return op(*x, wires=wires)
+
+            with self.assertRaisesRegex(qm.DeviceError,
+                "Observable {} not supported on device strawberryfields.fock".format(g)):
+                x = np.random.random([op.n_params])
+                circuit(*x)
+
+    def test_fock_circuit(self):
+        """Test that the fock plugin provides correct result for simple circuit"""
+        log.info('test_fock_circuit')
+
+        dev = qm.device('strawberryfields.fock', wires=1, cutoff_dim=10)
+
+        @qm.qfunc(dev)
+        def circuit(x):
+            qm.Displacement(x, 0, wires=0)
+            return qm.expectation.Fock(0)
+
+        self.assertAlmostEqual(circuit(1), 1, delta=self.tol)
+
+    def test_nonzero_shots(self):
+        """Test that the fock plugin provides correct result for high shot number"""
+        log.info('test_fock_circuit')
+
+        dev = qm.device('strawberryfields.fock', wires=1, cutoff_dim=10, shots=100)
+
+        @qm.qfunc(dev)
+        def circuit(x):
+            qm.Displacement(x, 0, wires=0)
+            return qm.expectation.Fock(0)
+
+        self.assertAlmostEqual(circuit(1), 1, delta=self.tol)
+
+    def test_supported_fock_gates(self):
+        """Test that all supported gates work correctly"""
+        log.info('test_supported_fock_gates')
+        cutoff_dim = 10
+        a = 0.312
+        b = 0.123
+
+        dev = qm.device('strawberryfields.fock', wires=2, cutoff_dim=cutoff_dim)
+
+        gates = list(dev._operator_map.items())
+        for g, sfop in gates:
+            log.info('\tTesting gate {}...'.format(g))
+            self.assertTrue(dev.supported(g))
+
+            op = getattr(qm.ops, g)
+
+            if op.n_wires == 0:
+                wires = [0]
+            else:
+                wires = list(range(op.n_wires))
+
+            @qm.qfunc(dev)
+            def circuit(*x):
+                qm.TwoModeSqueezing(0.1, 0, wires=[0, 1])
+                op(*x, wires=wires)
+                return qm.expectation.Fock(0), qm.expectation.Fock(1)
+
+            # compare to reference SF engine
+            def SF_reference(*x):
+                """SF reference circuit"""
+                eng, q = sf.Engine(2)
+                with eng:
+                    sf.ops.S2gate(0.1) | q
+                    sfop(*x) | [q[i] for i in wires]
+
+                state = eng.run('fock', cutoff_dim=cutoff_dim)
+                return state.mean_photon(0), state.mean_photon(1)
+
+            if g == 'GaussianState':
+                r = np.array([0, 0])
+                V = np.array([[0.5, 0], [0, 2]])
+                self.assertAllEqual(circuit(V, r), SF_reference(V, r))
+            elif g == 'FockDensityMatrix':
+                psi = np.random.random([10]) + np.random.random([10])*1j
+                psi /= np.linalg.norm(psi)
+                dm = np.outer(psi, psi.conj())
+                self.assertAllEqual(circuit(dm), SF_reference(dm))
+            elif g == 'FockStateVector':
+                psi = np.random.random([10]) + np.random.random([10])*1j
+                psi /= np.linalg.norm(psi)
+                self.assertAllEqual(circuit(psi), SF_reference(psi))
+            elif op.n_params == 1:
+                self.assertAllEqual(circuit(a), SF_reference(a))
+            elif op.n_params == 2:
+                self.assertAllEqual(circuit(a, b), SF_reference(a, b))
+
+    def test_supported_fock_observables(self):
+        """Test that all supported observables work correctly"""
+        log.info('test_supported_fock_observables')
+        cutoff_dim = 10
+        a = 0.312
+
+        dev = qm.device('strawberryfields.fock', wires=2, cutoff_dim=cutoff_dim)
+
+        observables = list(dev._observable_map.items())
+        for g, sfop in observables:
+            log.info('\tTesting observable {}...'.format(g))
+            self.assertTrue(dev.supported(g))
+
+            op = getattr(qm.expectation, g)
+            wires = list(range(op.n_wires))
+
+            @qm.qfunc(dev)
+            def circuit(*x):
+                qm.Displacement(0.1, 0, wires=0)
+                qm.TwoModeSqueezing(0.1, 0, wires=[0, 1])
+                return op(*x, wires=wires)
+
+            # compare to reference SF engine
+            def SF_reference(*x):
+                """SF reference circuit"""
+                eng, q = sf.Engine(2)
+                with eng:
+                    sf.ops.Xgate(0.2) | q[0]
+                    sf.ops.S2gate(0.1) | q
+
+                state = eng.run('fock', cutoff_dim=cutoff_dim)
+                return sfop(state, wires, x)[0]
+
+            if op.n_params == 0:
+                self.assertAllEqual(circuit(), SF_reference())
+            elif op.n_params == 1:
+                self.assertAllEqual(circuit(a), SF_reference(a))
+
+
+if __name__ == '__main__':
+    # run the tests in this file
+    suite = unittest.TestSuite()
+    for t in (PluginTest,):
+        ttt = unittest.TestLoader().loadTestsFromTestCase(t)
+        suite.addTests(ttt)
+
+    unittest.TextTestRunner().run(suite)
