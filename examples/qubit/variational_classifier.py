b784c94864068e4a6eaad16ed1ac7ba571a1c812
mariaschuld <mariaschuld@gmail.com>
2018-09-20 08:50:44 +0200

    rename vqe example, start classifier

--- examples/qubit/variational_classifier.py
+++ examples/qubit/variational_classifier.py
@@ -5,7 +5,9 @@ the "circuit-centric classifier" of Ref XXX.
 """
 
 import openqml as qm
-from openqml import numpy as np
+from openqml import numpy as onp
+import numpy as np
+from openqml._optimize import GradientDescentOptimizer
 
 
 def square_loss(labels, predictions):
@@ -25,14 +27,19 @@ def square_loss(labels, predictions):
     return loss
 
 
+def regularizer(weights):
+    """Regularizer penalty on weights"""
+    return onp.abs(weights.flatten()*2)
+
+
 def layer(W):
     """ Single layer of the quantum neural net
     with CNOT range 1."""
 
-    qm.Rotation(W[0, 0], W[0, 1], W[0, 2], [0])
-    qm.Rotation(W[1, 0], W[1, 1], W[1, 2], [1])
-    qm.Rotation(W[2, 0], W[2, 1], W[2, 2], [2])
-    qm.Rotation(W[3, 0], W[3, 1], W[3, 2], [2])
+    qm.Rot(W[0, 0], W[0, 1], W[0, 2], [0])
+    qm.Rot(W[1, 0], W[1, 1], W[1, 2], [1])
+    qm.Rot(W[2, 0], W[2, 1], W[2, 2], [2])
+    qm.Rot(W[3, 0], W[3, 1], W[3, 2], [3])
 
     qm.CNOT([0, 1])
     qm.CNOT([1, 2])
@@ -42,11 +49,12 @@ def layer(W):
 
 def make_random_layer(num_modes):
     """ Randomly initialised layer."""
-    W = np.random.randn(num_modes, num_modes)
+    W = onp.random.randn(num_modes, num_modes)
     return W
 
 
-dev = qm.device('default.qubit', wires=2)
+dev = qm.device('default.qubit', wires=4)
+
 
 @qm.qfunc(dev)
 def quantum_neural_net(weights, x):
@@ -55,43 +63,45 @@ def quantum_neural_net(weights, x):
     # We cheat and initialise the quantum state to
     # encode the input in its amplitudes
 
-    initial_state = x/np.sqrt(sum(x**2))
-    qm.QubitStateVector(initial_state, wires=[0, 1, 2, 3])
+    qm.QubitStateVector(x, wires=[0, 1, 2, 3])
 
     for W in weights:
         layer(W)
 
-    qm.expectation.PauliZ(0)
+    return qm.expectation.PauliZ(0)
 
 
 def cost(weights, features, labels):
     """Cost (error) function to be minimized."""
 
     # Compute prediction for each input in data batch
-    predictions = np.zeros((len(features), ))
+    predictions = onp.zeros((len(features), ))
     for idx, x in enumerate(features):
+        x = x / onp.sqrt(np.sum(x ** 2))
         predictions[idx] = quantum_neural_net(weights, x)
 
-    return square_loss(labels, predictions)
+    return square_loss(labels, predictions) + regularizer(weights)
 
 
 # load Iris data
 data = np.loadtxt("iris.txt")
 
 # initialize weights
+num_qubits = 4
 num_layers = 2
-weights0 = [make_random_layer(num_layers) for _ in range(num_layers)]
-o = qm.Optimizer(cost, weights0)
+weights0 = [make_random_layer(num_qubits) for _ in range(num_layers)]
+o = GradientDescentOptimizer(0.1)
 
 # train the circuit: HOW TO FEED IN DATA?
 batch_size = 3
-steps = 10
-for steps in range(steps):
-    batch_index = np.random.integer(batch_size)
-    batch = data[batch_index]
-    o.step(X=batch[:, 1:], y=batch[:, 0])
-    print(o.cost())
 
-# print the results
-print('Initial rotation angles:', weights0)
-print('Optimized rotation angles:', o.weights)
+weights = weights0
+for steps in range(11):
+    batch_index = np.random.randint(0, len(data), (batch_size, ))
+    batch = data[batch_index]
+    X = batch[:, 1:]
+    y = batch[:, 0]
+    
+    weights = o.step(lambda w: cost(w, X, y), weights)
+    print(cost(weights, X, y))
+
