2441056a0680d03cb778e2d407632af43b2262b0
Ville Bergholm <ville.bergholm@iki.fi>
2018-10-04 00:17:24 +0300

    openqml-sf/examples/photon_redirection.py fixed.

--- openqml-sf/examples/photon_redirection.py
+++ openqml-sf/examples/photon_redirection.py
@@ -5,9 +5,10 @@ In this demo we optimize an optical quantum circuit to redirect a photon from mo
 import openqml as qm
 from openqml import numpy as np
 
-dev = qm.device('strawberryfields.fock', wires=2, cutoff_dim=5)
-#dev = qm.device('strawberryfields.gaussian', wires=2)
+np.set_printoptions(precision=5)
 
+# construct a device to run the circuit on
+dev = qm.device('strawberryfields.fock', wires=2, cutoff_dim=5)
 
 def circuit(theta):
     """Here we define the quantum circuit.
@@ -21,22 +22,13 @@ def circuit(theta):
     qm.Beamsplitter(theta, 0, wires=[0, 1])
     return qm.expectation.PhotonNumber(wires=1)
 
+# a quantum node represents a combination of a circuit and a device that evaluates it
 q = qm.QNode(circuit, dev)
 
-# initialize theta with random value
-theta0 = np.array([np.pi * 0.2]) # np.random.randn(1)
-
+# initialize theta with a random value
+theta0 = np.random.randn(1)
 print('Initial beamsplitter angle: {} * pi'.format(theta0/np.pi))
-print('initial val: ', q(*theta0))
-print(q.grad_method_for_par)
-
-grad_F = q.gradient(theta0, method='F')
-print('initial grad_F: ', grad_F)
-grad_A = q.gradient(theta0)
-print('initial grad_A: ', grad_A)
-
-import sys
-sys.exit() # TODO remove when the gradients are fixed so that they agree
+print('initial circuit output: ', q(*theta0))
 
 def cost(theta):
     """Cost (error) function to be minimized.
@@ -54,5 +46,5 @@ c = o.train(max_steps=100)
 # print the results
 print('Initial beamsplitter angle: {} * pi'.format(theta0/np.pi))
 print('Optimized beamsplitter angle: {} * pi'.format(o.weights/np.pi))
-print('Circuit output at optimized angle:', q(o.weights))
-print('Circuit gradient at optimized angle:', qm.grad(q, o.weights))
+print('Circuit output at optimized angle: {:.5g}'.format(q(o.weights)))
+print('Circuit gradient at optimized angle: {:.5g}'.format(qm.grad(q, o.weights)))
