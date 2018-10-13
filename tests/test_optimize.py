7fb87afee3d717c70230c365195b92d427b3cfb3
co9olguy <nathan@xanadu.ai>
2018-10-13 15:55:16 -0400

    added sanity check tests for wrapped autograd gradients of purely classical functions

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Unit tests for the :mod:`openqml` :class:`QNode` class.
+Unit tests for the :mod:`openqml` :class:`GradientDescentOptimizer` subclasses.
 """
 
 import unittest
