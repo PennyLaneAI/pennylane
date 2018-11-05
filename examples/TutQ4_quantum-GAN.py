4b3f55a32e22b16a9f693c63aa69dcc2d806c890
mariaschuld <mariaschuld@gmail.com>
2018-11-05 10:42:27 +0200

    Ported Nathan's QGAN solution to Tut files.

--- examples/TutQ4_quantum-GAN.py
+++ examples/TutQ4_quantum-GAN.py
@@ -24,11 +24,11 @@ def true(phi, theta, omega):
     qml.Rot(phi, theta, omega, wires=0)
 
 
-def routine(w):
-    """Base routine used for generator and discriminator.
+def generator(w):
+    """Circuit that serves as a generator.
 
-        Args:
-            w (array[float]): variables of the circuit
+    Args:
+        w (array[float]): variables of the circuit
     """
     qml.RX(w[0], wires=0)
     qml.RX(w[1], wires=1)
@@ -42,116 +42,129 @@ def routine(w):
     qml.RZ(w[8], wires=0)
 
 
-def generator(w):
-    """Circuit that serves as a generator.
-
-    Args:
-        w (array[float]): variables of the circuit
-    """
-    routine(w)
-
-
 def discriminator(w):
     """Circuit that serves as a discriminator. Same as generator.
 
     Args:
         w (array[float]): variables of the circuit
     """
-    routine(w)
+    qml.RX(w[0], wires=0)
+    qml.RX(w[1], wires=2)
+    qml.RY(w[2], wires=0)
+    qml.RY(w[3], wires=2)
+    qml.RZ(w[4], wires=0)
+    qml.RZ(w[5], wires=2)
+    qml.CNOT(wires=[1, 2])
+    qml.RX(w[6], wires=2)
+    qml.RY(w[7], wires=2)
+    qml.RZ(w[8], wires=2)
 
 
 @qml.qnode(dev)
 def true_disc_circuit(phi, theta, omega, disc_weights):
-    """Circuit that serves as a discriminator. Same as generator.
+    """Feed discriminator with true samples.
 
     Args:
-        w (array[float]): variables of the circuit
+        phi (float): variable for true circuit
+        theta (float): variable for true circuit
+        omega (float): variable for true circuit
+        disc_weights (array[float]): array of discriminator variables
     """
     true(phi, theta, omega)
-    discriminator(disc_weights)
+    discriminator(disc_vars)
     return qml.expval.PauliZ(2)
 
 
 @qml.qnode(dev)
-def gen_disc_circuit(gen_weights, disc_weights):
-    generator(gen_weights)
-    discriminator(disc_weights)
+def gen_disc_circuit(gen_vars, disc_vars):
+    """Feed discriminator with samples produced by the generator.
+
+    Args:
+        gen_vars (array[float]): array of generator variables
+        disc_vars (array[float]): array of discriminator variables
+    """
+    generator(gen_vars)
+    discriminator(disc_vars)
     return qml.expval.PauliZ(2)
 
 
-def prob_real_true(disc_weights):
+def prob_real_true(disc_vars):
     """Probability that the discriminator guesses correctly.
 
     Args:
-        disc_weights: variables of the discriminator
+        disc_vars: variables of the discriminator
     """
-    true_disc_output = true_disc_circuit(phi, theta, omega, disc_weights)
+    true_disc_output = true_disc_circuit(phi, theta, omega, disc_vars)
     # convert to probability
     prob_real_true = (true_disc_output + 1) / 2
     return prob_real_true
 
 
-def prob_fake_true(gen_weights, disc_weights):
+def prob_fake_true(gen_vars, disc_vars):
     """Probability that the discriminator guesses wrong.
 
     Args:
-        gen_weights: variables of the generator
-        disc_weights: variables of the discriminator
+        gen_vars: variables of the generator
+        disc_vars: variables of the discriminator
     """
-    fake_disc_output = gen_disc_circuit(gen_weights, disc_weights)
+    fake_disc_output = gen_disc_circuit(gen_vars, disc_vars)
     # convert to probability
     prob_fake_true = (fake_disc_output + 1) / 2
-    return prob_fake_true  # want to minimize this prob
+    return prob_fake_true  # we want to minimize this prob
 
 
-def disc_cost(disc_weights):
+def disc_cost(disc_vars):
     """Cost for the discriminator is to classify a fake data sample as true.
 
     Args:
         disc_weights: variables of the discriminator
     """
-    cost = prob_fake_true(gen_weights, disc_weights) - prob_real_true(disc_weights)
+
+    cost = prob_fake_true(gen_vars, disc_vars) - prob_real_true(disc_vars)
     return cost
 
 
-def gen_cost(gen_weights):
+def gen_cost(gen_vars):
     """Cost for the generator is the opposite as the discriminator cost
     in the adversarial game.
 
     Args:
         gen_weights: variables of the generator
     """
-    return -prob_fake_true(gen_weights, disc_weights)
+    return -prob_fake_true(gen_vars, disc_vars)
+
 
 # Fix the angles with which the true data is generated
 phi = np.pi / 6
 theta = np.pi / 2
 omega = np.pi / 7
 # Initialize the other variables
-gen_weights = np.array([0] + [np.pi] + [0] * 7)
-disc_weights = np.random.normal(size=[9])
+eps = 1e-2
+gen_vars = np.array([0] + [np.pi] + [0] * 7) + eps * np.random.normal(size=[9])
+disc_vars = np.random.normal(size=[9])
 
 opt = qml.GradientDescentOptimizer(0.1)
 
 print("Training the discriminator.")
-for it in range(10):
-    disc_weights = opt.step(disc_cost, disc_weights)
-    cost = disc_cost(disc_weights)
-    print("Step {}: cost = {}".format(it+1, cost))
+for it in range(50):
+    disc_vars = opt.step(disc_cost, disc_vars)
+    cost = disc_cost(disc_vars)
+    if it % 5 == 0:
+        print("Step {}: cost = {}".format(it, cost))
 
-print("Probability of the discriminator to be correct: ", prob_real_true(disc_weights))
-print("Probability of the discriminator to be wrong: ", prob_fake_true(gen_weights, disc_weights))
+print("Probability of the discriminator to be correct: ", prob_real_true(disc_vars))
+print("Probability of the discriminator to be wrong: ", prob_fake_true(gen_vars, disc_vars))
 
 print("Training the generator.")
 # train generator
 for it in range(200):
-    gen_weights = opt.step(gen_cost, gen_weights)
-    cost = -gen_cost(gen_weights)
+    gen_vars = opt.step(gen_cost, gen_vars)
+    cost = -gen_cost(gen_vars)
     if it % 5 == 0:
-        print("Step {}: cost = {}".format(it+1, cost))
+        print("Step {}: cost = {}".format(it, cost))
 
-print("Probability of the discriminator to be correct: ", prob_real_true(disc_weights))
-print("Probability of the discriminator to be wrong: ", prob_fake_true(gen_weights, disc_weights))
+print("Probability of the discriminator to be correct: ", prob_real_true(disc_vars))
+print("Probability of the discriminator to be wrong: ", prob_fake_true(gen_vars, disc_vars))
 
 # should be close to zero at joint optimum
-print("Cost of the discriminator: ", disc_cost(disc_weights))
+print("Cost of the discriminator: ", disc_cost(disc_vars))
