9f50404530435e5e73db5bad5232f0b5e4410354
Josh Izaac <josh146@gmail.com>
2018-11-13 00:30:38 +0800

    support python 3.7 in setup.py

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ classifiers = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3 :: Only',
     "Topic :: Scientific/Engineering :: Physics"
 ]
