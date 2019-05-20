fc8ffa7c7ed36bf13f20c56141e732ea2797e317
Aroosa <ijaz.aroosa@gmail.com>
2019-05-20 14:08:54 -0400

    making final checks and incorporating changes Josh suggested

---
+++ examples/Q5_isingmodel-PyTorch.py
@@ -0,0 +1,95 @@
+# Qubit Optimization for 3 qubit Ising model using  Pennylane default qubit ecosystem.
+
+# Ising models are used in Quantum Annealing to solve Quadratic Unconstrained Binary
+
+# Optimization problems with non-convex  cost functions. This example demonstrates how
+
+# Gradient descent optimizers can get stuck in local minima when using non-convex cost functions.
+
+# This example  uses and compares pennylane gradient descent optimizer with the optimizers
+
+# of PyTorch and and TensorFlow for this quantum system: see Q5_isingmodel.ipynb
+
+import torch
+from torch.autograd import Variable
+import pennylane as qml
+from pennylane import numpy as np
+
+dev3 = qml.device("default.qubit", wires=3)
+
+
+@qml.qnode(dev3, interface="torch")
+def circuit3(p1, p2):
+    #   lets say first spin is always up (+1 eigenstate of pauli-z operator)
+    #   then we can optimize the rotation angles for the other two spins
+    #   so that the energy in minimized for the given couplings
+
+    #   WE USE THE GENERAL Rot(phi,theta,omega,wires) single qubit operation
+    qml.Rot(p1[0], p1[1], p1[2], wires=1)
+    qml.Rot(p2[0], p2[1], p2[2], wires=2)
+    return qml.expval.PauliZ(0), qml.expval.PauliZ(1), qml.expval.PauliZ(2)
+
+
+def cost(var1, var2):
+    # 	let coupling matrix be J=[1, -1]
+    #       circuit3 function returns a numpy array of pauliz exp values
+    spins = circuit3(var1, var2)
+    # 	the expectation value of pauliZ is plus 1 for spin up and -1 for spin down
+    energy = -(1 * spins[0] * spins[1]) - (-1 * spins[1] * spins[2])
+    return energy
+
+
+# TEST RUN: test it for [1,-1,-1] spin configuration. Total energy for this ising model should be
+
+# H = -1(J1*s1*s2 + J2*s2*s3) = -1 (1*1*-1  +  -1*-1*-1) = 2
+
+test1 = torch.tensor([0, np.pi, 0])
+test2 = torch.tensor([0, np.pi, 0])
+
+cost_check = cost(test1, test2)
+print(cost_check)
+
+# Random initialization in PyTorch
+
+p1 = Variable((np.pi * torch.rand(3, dtype=torch.float64)), requires_grad=True)
+p2 = Variable((np.pi * torch.rand(3, dtype=torch.float64)), requires_grad=True)
+var_init = [p1, p2]
+cost_init = cost(p1, p2)
+print(cost_init)
+
+# Now we use PyTorch optimizer to reduce this cost
+
+opt = torch.optim.Adam(var_init, lr=0.1)
+
+steps = 100
+
+
+def closure():
+    opt.zero_grad()
+    loss = cost(p1, p2)
+    loss.backward()
+    return loss
+
+
+var_pt = [var_init]
+cost_pt = [cost_init]
+
+for i in range(steps):
+    opt.step(closure)
+    if (i + 1) % 5 == 0:
+        p1n, p2n = opt.param_groups[0]["params"]
+        costn = cost(p1n, p2n)
+        var_pt.append([p1n, p2n])
+        cost_pt.append(costn)
+        print("Energy after step {:5d}: {: .7f} | Angles: {}".format(i + 1, costn, [p1n, p2n]))
+
+
+p1_final, p2_final = opt.param_groups[0]["params"]
+
+# the minimum energy is -2  for the spin configuration [1,1,-1] which corresponds to
+# (phi,theta,omega = (0,0,0) for spin2 and (0,pi,0) for spin3
+# We might not always see this value due to the non-convex cost function
+
+print(p1_final, p2_final)
+
+print(cost(p1_final, p2_final))
---
+++ examples/Q5_isingmodel-TF.py
@@ -0,0 +1,95 @@
+# Qubit Optimization for 3 qubit Ising model using  Pennylane default qubit ecosystem.
+
+# Ising models are used in Quantum Annealing to solve Quadratic Unconstrained Binary
+
+# Optimization problems with non-convex  cost functions. This example demonstrates how
+
+# Gradient descent optimizers can get stuck in local minima when using non-convex cost functions.
+
+# This example  uses and compares pennylane gradient descent optimizer with the optimizers
+
+# of PyTorch and and TensorFlow for this quantum system: see Q5_isingmodel.ipynb
+
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+
+# Pennylane interfaces with the eager execution of TensorFlow
+tf.enable_eager_execution()
+
+# You can check if the eager execution is working by using the following command
+# print(tf.executing_eagerly())
+
+import pennylane as qml
+from pennylane import numpy as np
+
+dev2 = qml.device("default.qubit", wires=3)
+
+
+@qml.qnode(dev2, interface="tfe")
+def circuit2(p1, p2):
+    #       lets say first spin is always up (+1 eigenstate of pauli-z operator)
+    #       then we can optimize the rotation angles for the other two spins
+    #       so that the energy in minimized for the given couplings
+
+    #       WE USE THE GENERAL Rot(phi,theta,omega,wires) single qubit operation
+    qml.Rot(p1[0], p1[1], p1[2], wires=1)
+    qml.Rot(p2[0], p2[1], p2[2], wires=2)
+    return qml.expval.PauliZ(0), qml.expval.PauliZ(1), qml.expval.PauliZ(2)
+
+
+def cost(var):
+    # 	let coupling matrix be J=[1, -1]
+    #       circuit2 function returns a numpy array of pauliz exp values
+    spins = circuit2(var[0], var[1])
+    # 	the expectation value of pauliZ is plus 1 for spin up and -1 for spin down
+    energy = -(1 * spins[0] * spins[1]) - (-1 * spins[1] * spins[2])
+    return energy
+
+
+# TEST RUN: test it for [1,-1,-1] spin configuration. Total energy for this ising model should be
+
+# H = -1(J1*s1*s2 + J2*s2*s3) = -1 (1*1*-1  +  -1*-1*-1) = 2
+
+test1 = tfe.Variable([0, np.pi, 0], dtype=tf.float64)
+test2 = tfe.Variable([0, np.pi, 0], dtype=tf.float64)
+cost_check = cost([test1, test2])
+print(cost_check)
+
+# Random initialization in TensorFlow
+
+t1 = tfe.Variable(tf.random_uniform([3], 0, np.pi, dtype=tf.float64))
+t2 = tfe.Variable(tf.random_uniform([3], 0, np.pi, dtype=tf.float64))
+var_init = [t1, t2]
+cost_init = cost(var_init)
+print(cost_init)
+
+# optimize using TensorFlow optimizer
+
+opt = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+
+steps = 100
+
+var = var_init
+var_tf = [var]
+cost_tf = [cost_init]
+
+for i in range(steps):
+    with tf.GradientTape() as tape:
+        loss = cost([t1, t2])
+        grads = tape.gradient(loss, [t1, t2])
+
+    opt.apply_gradients(zip(grads, [t1, t2]), global_step=tf.train.get_or_create_global_step())
+    if (i + 1) % 5 == 0:
+        var_tf.append([t1, t2])
+        cost_tf.append(cost([t1, t2]))
+        print(
+            "Energy after step {:5d}: {: .7f} | Angles: {}".format(i + 1, cost([t1, t2]), [t1, t2])
+        )
+
+# the minimum energy is -2  for the spin configuration [1,1,-1] which corresponds to
+# (phi,theta,omega = (0,0,0) for spin2 and (0,pi,0) for spin3
+# We might not always see this value due to the non-convex cost function
+
+print(t1, t2)
+
+print(cost([t1, t2]))
