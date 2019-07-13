672a0a8bad29f238fef2249792083983fe2dd98a
Simon Cross <hodgestar@gmail.com>
2019-07-13 16:41:57 +0200

    Remove non-working shebang.

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/usr/bin/env python3
 import sys
 import os
 from setuptools import setup
