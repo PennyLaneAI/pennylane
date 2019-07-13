2b4d3d980de9e34713eefd74129f57f01e9eb118
Simon Cross <hodgestar@gmail.com>
2019-07-13 16:42:14 +0200

    Remove unused imports.

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-import os
 from setuptools import setup
 # from sphinx.setup_command import BuildDoc
 
