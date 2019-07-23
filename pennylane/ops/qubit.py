e1e7dce39faf173c3ae65af67de9e48d923403ec
johannesjmeyer <jj.meyer@outlook.com>
2019-07-23 20:44:26 +0200

    Finetune the presentation in the docs

--- pennylane/ops/qubit.py
+++ pennylane/ops/qubit.py
@@ -345,8 +345,8 @@ class Rot(Operation):
 
     * Number of wires: 1
     * Number of parameters: 3
-    * Gradient recipe: :math:`\frac{d}{d\phi}f(R(\phi, \theta, \omega)) = \frac{1}{2}\left[f(R(\phi+\pi/2, \theta, \omega)) - f(R(\phi-\pi/2, \theta, \omega))\right]`.
-      where :math:`f` is an expectation value depending on :math:`R_x(\phi)`.
+    * Gradient recipe: :math:`\frac{d}{d\phi}f(R(\phi, \theta, \omega)) = \frac{1}{2}\left[f(R(\phi+\pi/2, \theta, \omega)) - f(R(\phi-\pi/2, \theta, \omega))\right]`
+      where :math:`f` is an expectation value depending on :math:`R(\phi, \theta, \omega)`.
       This gradient recipe applies for each angle argument :math:`\{\phi, \theta, \omega\}`.
 
     Args:
@@ -377,7 +377,8 @@ class CRX(Operation):
 
     * Number of wires: 2
     * Number of parameters: 1
-    * Gradient recipe: :math:`\frac{d}{d\phi}CR_x(\phi) = \frac{1}{2}\left[CR_x(\phi+\pi/2) - CR_x(\phi-\pi/2)\right]`
+    * Gradient recipe: :math:`\frac{d}{d\phi}f(CR_x(\phi)) = \frac{1}{2}\left[f(CR_x(\phi+\pi/2)) - f(CR_x(\phi-\pi/2))\right]`
+      where :math:`f` is an expectation value depending on :math:`CR_x(\phi)`.
 
     Args:
         phi (float): rotation angle :math:`\phi`
@@ -406,7 +407,8 @@ class CRY(Operation):
 
     * Number of wires: 2
     * Number of parameters: 1
-    * Gradient recipe: :math:`\frac{d}{d\phi}CR_y(\phi) = \frac{1}{2}\left[CR_y(\phi+\pi/2) - CR_y(\phi-\pi/2)\right]`
+    * Gradient recipe: :math:`\frac{d}{d\phi}f(CR_y(\phi)) = \frac{1}{2}\left[f(CR_y(\phi+\pi/2)) - f(CR_y(\phi-\pi/2))\right]`
+      where :math:`f` is an expectation value depending on :math:`CR_y(\phi)`.
 
     Args:
         phi (float): rotation angle :math:`\phi`
@@ -435,7 +437,8 @@ class CRZ(Operation):
 
     * Number of wires: 2
     * Number of parameters: 1
-    * Gradient recipe: :math:`\frac{d}{d\phi}CR_z(\phi) = \frac{1}{2}\left[CR_z(\phi+\pi/2) - CR_z(\phi-\pi/2)\right]`
+    * Gradient recipe: :math:`\frac{d}{d\phi}f(CR_z(\phi)) = \frac{1}{2}\left[f(CR_z(\phi+\pi/2)) - f(CR_z(\phi-\pi/2))\right]`
+      where :math:`f` is an expectation value depending on :math:`CR_z(\phi)`.
 
     Args:
         phi (float): rotation angle :math:`\phi`
@@ -464,7 +467,9 @@ class CRot(Operation):
 
     * Number of wires: 2
     * Number of parameters: 3
-    * Gradient recipe: :math:`\frac{d}{d\phi}CR(\phi) = \frac{1}{2}\left[CR(\phi+\pi/2) - CR(\phi-\pi/2)\right]`. This gradient recipe applies for each angle argument :math:`\{\phi, \theta, \omega\}`.
+    * Gradient recipe: :math:`\frac{d}{d\phi}f(CR(\phi, \theta, \omega)) = \frac{1}{2}\left[f(CR(\phi+\pi/2, \theta, \omega)) - f(CR(\phi-\pi/2, \theta, \omega))\right]`
+      where :math:`f` is an expectation value depending on :math:`CR(\phi, \theta, \omega)`.
+      This gradient recipe applies for each angle argument :math:`\{\phi, \theta, \omega\}`.
 
     Args:
         phi (float): rotation angle :math:`\phi`
