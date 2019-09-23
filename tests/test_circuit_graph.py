eff4cee81321c36ff369ea59840fc99c3b9516aa
Zeid <zeid@xanadu.ai>
2019-09-23 15:58:02 -0400

    Add tests for new CircuitGraph methods

--- tests/test_circuit_graph.py
+++ tests/test_circuit_graph.py
@@ -16,12 +16,46 @@ Unit tests for the :mod:`pennylane.circuit_graph` module.
 """
 # pylint: disable=no-self-use,too-many-arguments,protected-access
 
+from unittest.mock import MagicMock
+
 import numpy as np
 
 import pennylane as qml
 
 from pennylane.operation import Expectation
 from pennylane.circuit_graph import CircuitGraph
+
+import pytest
+
+
+@pytest.fixture()
+def queue():
+    """A fixture of a complex example of operations that depend on previous operations."""
+    return [
+        qml.RX(0.43, wires=0, do_queue=False),
+        qml.RY(0.35, wires=1, do_queue=False),
+        qml.RZ(0.35, wires=2, do_queue=False),
+        qml.CNOT(wires=[0, 1], do_queue=False),
+        qml.Hadamard(wires=2, do_queue=False),
+        qml.CNOT(wires=[2, 0], do_queue=False),
+        qml.PauliX(wires=1, do_queue=False),
+    ]
+
+
+@pytest.fixture()
+def obs():
+    """A fixture of observables to go after the queue fixture."""
+    return [
+        qml.expval(qml.PauliX(wires=0, do_queue=False)),
+        qml.expval(qml.Hermitian(np.identity(4), wires=[1, 2], do_queue=False)),
+    ]
+
+
+@pytest.fixture()
+def circuit(queue, obs):
+    """A fixture of a circuit generated based on the queue and obs fixtures above."""
+    circuit = CircuitGraph(queue, obs)
+    return circuit
 
 
 class TestCircuitGraph:
@@ -39,24 +73,9 @@ class TestCircuitGraph:
         assert len(res) == 2
         assert not res.edges()
 
-    def test_dependence(self):
+    def test_dependence(self, queue, obs):
         """Test a more complex example containing operations
         that do depend on the result of previous operations"""
-
-        queue = [
-            qml.RX(0.43, wires=0, do_queue=False),
-            qml.RY(0.35, wires=1, do_queue=False),
-            qml.RZ(0.35, wires=2, do_queue=False),
-            qml.CNOT(wires=[0, 1], do_queue=False),
-            qml.Hadamard(wires=2, do_queue=False),
-            qml.CNOT(wires=[2, 0], do_queue=False),
-            qml.PauliX(wires=1, do_queue=False),
-        ]
-
-        obs = [
-            qml.expval(qml.PauliX(wires=0, do_queue=False)),
-            qml.expval(qml.Hermitian(np.identity(4), wires=[1, 2], do_queue=False)),
-        ]
 
         circuit = CircuitGraph(queue, obs)
         res = circuit.graph
@@ -122,3 +141,115 @@ class TestCircuitGraph:
             (5, 8),
             (6, 8),
         ]
+
+    def test_ancestors_and_descendants_example(self, queue, obs):
+        """
+        Test that the `ancestors` and `descendants` methods return the expected result.
+        """
+        circuit = CircuitGraph(queue, obs)
+
+        ancestors = circuit.ancestors([6])
+        assert len(ancestors) == 3
+        for o_idx in (0, 1, 3):
+            assert queue[o_idx] in circuit.get_ops(ancestors)
+
+        descendants = circuit.get_ops(circuit.descendants([6]))
+        assert descendants == [obs[1]]
+
+    def test_get_nodes_example(self, queue, obs):
+        """
+        Given a sample circuit, test that the `get_nodes` method returns the expected result.
+        """
+        circuit = CircuitGraph(queue, obs)
+
+        o_idxs = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        nodes = circuit.get_nodes(o_idxs)
+
+        assert nodes[0]["op"] == queue[0]
+        assert nodes[1]["op"] == queue[1]
+        assert nodes[2]["op"] == queue[2]
+        assert nodes[3]["op"] == queue[3]
+        assert nodes[4]["op"] == queue[4]
+        assert nodes[5]["op"] == queue[5]
+        assert nodes[6]["op"] == queue[6]
+        assert nodes[7]["op"] == obs[0]
+        assert nodes[8]["op"] == obs[1]
+
+        assert nodes[0]["idx"] == 0
+        assert nodes[1]["idx"] == 1
+        assert nodes[2]["idx"] == 2
+        assert nodes[3]["idx"] == 3
+        assert nodes[4]["idx"] == 4
+        assert nodes[5]["idx"] == 5
+        assert nodes[6]["idx"] == 6
+        assert nodes[7]["idx"] == 7
+        assert nodes[8]["idx"] == 8
+
+        assert nodes[0]["name"] == "RX"
+        assert nodes[1]["name"] == "RY"
+        assert nodes[2]["name"] == "RZ"
+        assert nodes[3]["name"] == "CNOT"
+        assert nodes[4]["name"] == "Hadamard"
+        assert nodes[5]["name"] == "CNOT"
+        assert nodes[6]["name"] == "PauliX"
+        assert nodes[7]["name"] == "PauliX"
+        assert nodes[8]["name"] == "Hermitian"
+
+        assert nodes[0]["return_type"] is None
+        assert nodes[1]["return_type"] is None
+        assert nodes[2]["return_type"] is None
+        assert nodes[3]["return_type"] is None
+        assert nodes[4]["return_type"] is None
+        assert nodes[5]["return_type"] is None
+        assert nodes[6]["return_type"] is None
+        assert nodes[7]["return_type"] == Expectation
+        assert nodes[8]["return_type"] == Expectation
+
+    def test_get_nodes(self, circuit, monkeypatch):
+        """Test that `get_nodes` fetches the correct nodes from the graph"""
+        mock_graph = MagicMock()
+        monkeypatch.setattr(circuit, "_graph", mock_graph)
+
+        o_idxs = list(range(10))
+        result = circuit.get_nodes(o_idxs)
+        assert result == [mock_graph.nodes[i] for i in o_idxs]
+
+    def test_get_ops(self, circuit, monkeypatch):
+        """Test that `get_ops` fetches the correct ops based on given nodes"""
+        mock_nodes = MagicMock()
+        mock_ops = MagicMock()
+        mock_get_nodes = MagicMock()
+        mock_get_nodes.return_value = [mock_nodes.a, mock_nodes.b]
+        monkeypatch.setattr(circuit, "get_nodes", mock_get_nodes)
+
+        result = circuit.get_ops(mock_ops)
+        mock_get_nodes.assert_called_once_with(mock_ops)
+        assert result == [mock_nodes.a["op"], mock_nodes.b["op"]]
+
+    def test_update_node(self, circuit, monkeypatch):
+        """Test that `nx.set_node_attributes` is called correctly when updating a node using
+        `update_node` and a given node and op"""
+        mock_command = MagicMock()
+        mock_nx = MagicMock()
+
+        monkeypatch.setattr("pennylane.circuit_graph.nx", mock_nx)
+        monkeypatch.setattr("pennylane.circuit_graph.Command", mock_command)
+
+        node = MagicMock()
+        op = MagicMock()
+
+        circuit.update_node(node, op)
+
+        mock_command.assert_called_once_with(
+            name=op.name, op=op, return_type=op.return_type, idx=node["idx"]
+        )
+        mock_nx.set_node_attributes.assert_called_once_with(
+            circuit._graph,
+            {
+                node["idx"]: {
+                    **mock_command(
+                        name=op.name, op=op, return_type=op.return_type, idx=node["idx"]
+                    )._asdict()
+                }
+            },
+        )
