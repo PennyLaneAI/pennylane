e2d1264115818295ab3ff93259d6119c697a2514
johannesjmeyer <jj.meyer@outlook.com>
2019-09-30 12:17:54 +0200

    Test that corrent gates are called in BasisStatePrep

---
+++ tests/test_templates_state_preparations.py
@@ -0,0 +1,51 @@
+# Copyright 2018 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Unit tests for the :mod:`pennylane.template.state_preparations` module.
+"""
+# pylint: disable=protected-access,cell-var-from-loop
+import pytest
+import numpy as np
+from unittest.mock import patch, MagicMock
+
+import pennylane as qml
+from pennylane.templates.state_preparations import BasisStatePreparation
+
+class TestBasisStatePreparation:
+    """Tests the template BasisStatePreparation."""
+
+    @pytest.mark.parametrize("basis_state,wires,target_wires", [
+        ([0], [0], []),
+        ([0], [1], []),
+        ([1], [0], [0]),
+        ([1], [1], [1]),
+        ([0, 1], [0, 1], [1]),
+        ([1, 0], [1, 4], [1]),
+        ([1, 1], [0, 2], [0, 2]),
+        ([1, 0], [4, 5], [4]),
+        ([0, 0, 1, 0], [1, 2, 3, 4], [3]),
+        ([1, 1, 1, 0], [1, 2, 6, 8], [1, 2, 6]),
+        ([1, 0, 1, 1], [1, 2, 6, 8], [1, 6, 8]),
+    ])
+    def test_correct_pl_gates(self, tol, basis_state, wires, target_wires):
+        """Tests that the template BasisStatePreparation calls the correct
+        PennyLane gates on the correct wires."""
+
+        with patch("pennylane.PauliX") as mock:
+            BasisStatePreparation(basis_state, wires)
+
+            called_wires = [args[0] for args, kwargs in mock.call_args_list]
+
+            assert len(target_wires) == len(called_wires)
+            assert np.array_equal(called_wires, target_wires)
