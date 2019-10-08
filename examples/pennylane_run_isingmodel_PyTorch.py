cdf37086b12e99317701e5904ae373b523b12e39
Aroosa <44415402+AroosaIjaz@users.noreply.github.com>
2019-10-08 19:56:04 +0200

    Update examples/pennylane_run_isingmodel_PyTorch.py
    Co-Authored-By: Josh Izaac <josh146@gmail.com>

--- examples/pennylane_run_isingmodel_PyTorch.py
+++ examples/pennylane_run_isingmodel_PyTorch.py
@@ -55,7 +55,7 @@ def cost(var1, var2):
 ###############################################################################
 # Sanity check
 # ^^^^^^^^^^^^^
-# Let’s test the functions above using :math:`[s_1, s_2, s_3] = [1, -1, -1]` spin
+# Let's test the functions above using :math:`[s_1, s_2, s_3] = [1, -1, -1]` spin
 # configuration and the given coupling matrix. The total energy for this Ising model
 # should be:
 #
