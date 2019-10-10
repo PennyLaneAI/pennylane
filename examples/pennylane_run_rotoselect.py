ff9a42077ab102eb636b9795a0667a5066b2d578
Angus Lowe <anguslowe@live.ca>
2019-10-10 10:33:51 -0400

    Apply suggestion by Aroosa
    Co-Authored-By: Aroosa <44415402+AroosaIjaz@users.noreply.github.com>

--- examples/pennylane_run_rotoselect.py
+++ examples/pennylane_run_rotoselect.py
@@ -8,7 +8,7 @@ Quantum circuit structure learning
 This example shows how to learn a good selection of rotation
 gates so as to minimize a cost
 function using the Rotoselect algorithm of `Ostaszewski et al.
-(2019) <https://arxiv.org/abs/1905.09692>`__. We apply the algorithm to minimize a Hamiltonian for a 
+(2019) <https://arxiv.org/abs/1905.09692>`__. We apply this algorithm to minimize a Hamiltonian for a 
 variational quantum eigensolver (VQE) problem, 
 and improve upon an initial circuit structure ansatz.
 """
