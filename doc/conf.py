78f9492396a25bd3f6644b95b941b95670754752
Josh Izaac <josh146@gmail.com>
2019-10-19 16:42:23 -0400

    fix docs building

--- doc/conf.py
+++ doc/conf.py
@@ -371,11 +371,3 @@ autodoc_member_order = 'bysource'
 
 # inheritance_diagram graphviz attributes
 inheritance_node_attrs = dict(color='lightskyblue1', style='filled')
-
-from custom_directives import IncludeDirective, GalleryItemDirective, CustomGalleryItemDirective
-
-def setup(app):
-    app.add_directive('includenodoc', IncludeDirective)
-    app.add_directive('galleryitem', GalleryItemDirective)
-    app.add_directive('customgalleryitem', CustomGalleryItemDirective)
-    app.add_stylesheet('xanadu_gallery.css')
