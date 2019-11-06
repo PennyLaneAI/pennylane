0a0dd00f6361f0e475c3d3e15ed5377e9f539274
antalszava <antalszava@gmail.com>
2019-11-06 20:45:59 +0100

    Update pennylane/templates/layers.py
    Co-Authored-By: Josh Izaac <josh146@gmail.com>

--- pennylane/templates/layers.py
+++ pennylane/templates/layers.py
@@ -354,7 +354,7 @@ def Interferometer(theta, phi, varphi, wires, mesh='rectangular', beamsplitter='
           beamsplitter is implemented via PennyLane's ``Beamsplitter`` operation.
 
     Raises:
-        QuantumFunctionError: if `beamsplitter` or `mesh` is an instance of
+        QuantumFunctionError: if ``beamsplitter`` or ``mesh`` is an instance of
             :class:`~pennylane.variable.Variable`
     """
     if isinstance(beamsplitter, Variable):
