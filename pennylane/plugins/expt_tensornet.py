0ae2662305b20d9fb20666afa9b9239dfb5972f6
antalszava <antalszava@gmail.com>
2019-11-06 19:23:56 -0500

    Adding variance to tensornetwork simulator (Hermitian case needs checking)

--- pennylane/plugins/expt_tensornet.py
+++ pennylane/plugins/expt_tensornet.py
@@ -204,18 +204,78 @@ class TensorNetwork(Device):
             op_node, self._state_node, output_edge_order=self._free_edges
         )
 
-    def expval(self, observable, wires, par):
+    @staticmethod
+    def check_inputs(observable, wires, par):
+        """Helper function for checking if the observables and their parameters
+        are in a list.
+
+        Args:
+          observable (str or list[str]): name of the observable(s)
+          wires (int or list[int]): measured subsystems for each observable
+          par (Sequence[np.complex128]): parameter for each observable
+
+        Returns:
+          list[np.complex128]: the observables as tensornetwork Nodes
+        """
+        return [observable], [wires], [par] if not isinstance(observable, list)\
+            else observable, wires, par
+
+    def get_compatible_matrices(self, observable, wires, par):
+        """Helper function for getting and reshaping the matrices for observables.
+
+        Args:
+          observable (str or list[str]): name of the observable(s)
+          wires (list[int] or list[list[int]]): measured subsystems for each observable
+          par (Sequence[np.complex128]): parameter for each observable
+
+        Returns:
+          list[np.array[np.complex128]]: the observables as tensornetwork Nodes
+          list[list[int]]: measured subsystems for each observable
+        """
+
         if not isinstance(observable, list):
-            observable = [observable]
-            wires = [wires]
-            par = [par]
+            observable, wires, par = [observable], [wires], [par]
+
         matrices = []
+
         for o, p, w in zip(observable, par, wires):
             A = self._get_operator_matrix(o, p)
             num_mult_idxs = len(w)
             matrices.append(np.reshape(A, [2] * num_mult_idxs * 2))
-        nodes = [self._add_node(A, w, name=o) for A, w, o in zip(matrices, wires, observable)]
+        return matrices, wires
+
+    def create_nodes_from_matrices(self, matrices: list, wires: list, observable_names: list):
+        """Helper function for creating tensornetwork nodes based on matrices.
+
+        Args:
+          matrices (list[np.array[np.complex128]]): matrices of the observable
+          wires (Sequence[Sequence[int]]): measured subsystems for each observable
+          observable_names (Sequence[str]): name of the operation/observable
+
+        Returns:
+          list[tn.Node]: the observables as tensornetwork Nodes
+        """
+        return [self._add_node(A, w, name=o) for A, w, o in zip(matrices, wires, observable_names)]
+
+    def expval(self, observable, wires, par):
+
+        matrices, wires = self.get_compatible_matrices(observable, wires, par)
+        nodes = self.create_nodes_from_matrices(matrices, wires, observable)
+
         return self.ev(nodes, wires)
+
+    def var(self, observable, wires, par):
+
+        matrices, wires = self.get_compatible_matrices(observable, wires, par)
+        square_of_matrices = [A*A for A in matrices]
+
+        obs_nodes = self.create_nodes_from_matrices(matrices, wires, observable)
+        obs_nodes_for_squares = self.create_nodes_from_matrices(square_of_matrices, wires, observable)
+
+        first = self.ev(obs_nodes_for_squares, wires)
+        second = self.ev(obs_nodes, wires)**2
+
+        return first - second
 
     def _get_operator_matrix(self, operation, par):
         """Get the operator matrix for a given operation or observable.
@@ -236,7 +296,7 @@ class TensorNetwork(Device):
 
          Args:
             obs_nodes (Sequence[tn.Node]): the observables as tensornetwork Nodes
-            wires (Sequence[Sequence[[int]]): measured subsystems for each observable
+            wires (Sequence[Sequence[int]]): measured subsystems for each observable
          Returns:
             float: expectation value :math:`\expect{A} = \bra{\psi}A\ket{\psi}`
         """
