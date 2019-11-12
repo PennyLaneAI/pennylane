4c1adf10735bbcb167469277029013b3267d3287
johannesjmeyer <jj.meyer@outlook.com>
2019-11-12 12:11:20 +0100

    Add qubit_state_vector decomposition test

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -794,3 +794,19 @@ class TestDecomposition:
 
         assert rec.queue[2].name == "PauliX"
         assert rec.queue[2].wires == [3]
+
+    def test_qubit_state_vector_decomposition(self):
+        """Test the decomposition of QubitStateVector is equivalent
+        to the MottonenStatePreparation template"""
+        state = np.array([1/2, 1j/np.sqrt(2), 0, -1/2])
+
+        with qml.utils.OperationRecorder() as rec1:
+            qml.QubitStateVector.decomposition(state, wires=[0, 1])
+
+        with qml.utils.OperationRecorder() as rec2:
+            qml.templates.state_preparations.MottonenStatePreparation(state, wires=[0, 1])
+
+        for op1, op2 in zip(rec1.queue, rec2.queue):
+            assert op1.name == op2.name
+            assert op1.wires == op2.wires
+            assert op1.params == op2.params
