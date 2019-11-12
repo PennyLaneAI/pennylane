46fe3793d67086cc24e608109d6d176d926415b5
mariaschuld <mariaschuld@gmail.com>
2019-11-12 07:54:02 +0200

    increase coverage of init module to 100%

--- tests/test_init.py
+++ tests/test_init.py
@@ -19,192 +19,146 @@ import pytest
 import numpy as np
 import pennylane as qml
 
-@pytest.fixture(scope="module",
-                params=[2, 3])
-def n_wires(request):
-    """Number of qubits or modes."""
-    return request.param
+# Fix inputs to create fixture parameters
+n_wires = 3
+repeat = 2
+n_rots = 10
+n_if = n_wires * (n_wires - 1) / 2
+wrs = (repeat, n_wires)
+intf = (repeat, n_if)
 
-@pytest.fixture(scope="module",
-                params=[2, 3])
-def repeat(request):
-    """Number of layers."""
-    return request.param
+#######################################
+# Signatures
 
-@pytest.fixture(scope="module",
-                params=[2, 10])
-def n_rots(request):
-    """Number of rotations in random layer."""
-    return request.param
+base = {'n_wires': n_wires}
+rpt = {'n_layers': repeat, 'n_wires': n_wires}
+rpt_nrml = {'n_layers': repeat, 'n_wires': n_wires, 'mean': 0, 'std': 1}
+rpt_uni = {'n_layers': repeat, 'n_wires': n_wires, 'low': 0, 'high': 1}
+base_nrml = {'n_wires': n_wires, 'mean': 0, 'std': 1}
+base_uni = {'n_wires': n_wires, 'low': 0, 'high': 1}
+rnd_rpt_nrml = {'n_layers': repeat, 'n_wires': n_wires, 'n_rots': n_rots, 'mean': 0, 'std': 1}
+rnd_rpt_uni = {'n_layers': repeat, 'n_wires': n_wires, 'n_rots': n_rots, 'low': 0, 'high': 1}
 
+#######################################
+# Functions and their signatures
 
-@pytest.fixture(scope="module")
-def n_if(n_wires):
-    """Number of beamsplitters in an interferometer."""
-    return n_wires * (n_wires - 1) // 2
-
-
-a = (repeat, n_wires)
-b = (repeat, n_if)
-
-############
-# Functions
-# RPT - have a 'repeat' keyword argument
-
-RPT_NRML = [qml.init.random_layers_normal,
-            qml.init.strong_ent_layers_normal,
-            qml.init.cvqnn_layers_theta_normal,
-            qml.init.cvqnn_layers_phi_normal,
-            qml.init.cvqnn_layers_varphi_normal,
-            qml.init.cvqnn_layers_r_normal,
-            qml.init.cvqnn_layers_phi_r_normal,
-            qml.init.cvqnn_layers_a_normal,
-            qml.init.cvqnn_layers_phi_a_normal,
-            qml.init.cvqnn_layers_kappa_normal,
-            ]
-RPT_UNIF = [qml.init.random_layers_uniform,
-            qml.init.strong_ent_layers_uniform,
-            qml.init.cvqnn_layers_theta_uniform,
-            qml.init.cvqnn_layers_phi_uniform,
-            qml.init.cvqnn_layers_varphi_uniform,
-            qml.init.cvqnn_layers_r_uniform,
-            qml.init.cvqnn_layers_phi_r_uniform,
-            qml.init.cvqnn_layers_a_uniform,
-            qml.init.cvqnn_layers_phi_a_uniform,
-            qml.init.cvqnn_layers_kappa_uniform,
-            ]
-RPT_ALL = [qml.init.cvqnn_layers_all]
-NRML = [qml.init.interferometer_theta_normal,
-        qml.init.interferometer_phi_normal,
-        qml.init.interferometer_varphi_normal]
-UNIF = [qml.init.interferometer_theta_uniform,
-        qml.init.interferometer_phi_uniform,
-        qml.init.interferometer_varphi_uniform]
-ALL = [qml.init.interferometer_all]
+# Functions returning a single parameter array
+INIT = [(qml.init.random_layers_normal, rnd_rpt_nrml),
+        (qml.init.strong_ent_layers_normal, rpt_nrml),
+        (qml.init.cvqnn_layers_theta_normal, rpt_nrml),
+        (qml.init.cvqnn_layers_phi_normal, rpt_nrml),
+        (qml.init.cvqnn_layers_varphi_normal, rpt_nrml),
+        (qml.init.cvqnn_layers_r_normal, rpt_nrml),
+        (qml.init.cvqnn_layers_phi_r_normal, rpt_nrml),
+        (qml.init.cvqnn_layers_a_normal, rpt_nrml),
+        (qml.init.cvqnn_layers_phi_a_normal, rpt_nrml),
+        (qml.init.cvqnn_layers_kappa_normal, rpt_nrml),
+        (qml.init.interferometer_theta_normal, base_nrml),
+        (qml.init.interferometer_phi_normal, base_nrml),
+        (qml.init.interferometer_varphi_normal, base_nrml),
+        (qml.init.random_layers_uniform, rnd_rpt_uni),
+        (qml.init.strong_ent_layers_uniform, rpt_uni),
+        (qml.init.cvqnn_layers_theta_uniform, rpt_uni),
+        (qml.init.cvqnn_layers_phi_uniform, rpt_uni),
+        (qml.init.cvqnn_layers_varphi_uniform, rpt_uni),
+        (qml.init.cvqnn_layers_r_uniform, rpt_uni),
+        (qml.init.cvqnn_layers_phi_r_uniform, rpt_uni),
+        (qml.init.cvqnn_layers_a_uniform, rpt_uni),
+        (qml.init.cvqnn_layers_phi_a_uniform, rpt_uni),
+        (qml.init.cvqnn_layers_kappa_uniform, rpt_uni),
+        (qml.init.interferometer_theta_uniform, base_uni),
+        (qml.init.interferometer_phi_uniform, base_uni),
+        (qml.init.interferometer_varphi_uniform, base_uni)
+        ]
+# Functions returning a list of parameter arrays
+ALL = [(qml.init.cvqnn_layers_all, rpt),
+       (qml.init.interferometer_all, base)]
 
 ###########
 # Shapes
 
-RPT_SHAPES = [(repeat, n_rots), (repeat, n_wires, 3), b, b, a, a, a, a, a, a]
-SHAPES = [b, b, a]
-RPT_ALL_SHAPES = [[b, b, a, a, a, b, b, a, a, a, a]]
-ALL_SHAPES = [[b, b, a]]
+SHP = [(repeat, n_rots), (repeat, n_wires, 3), intf, intf, wrs, wrs,
+       wrs, wrs, wrs, wrs, (n_if,), (n_if,), (n_wires,),
+       (repeat, n_rots), (repeat, n_wires, 3), intf, intf, wrs, wrs,
+       wrs, wrs, wrs, wrs, (n_if,), (n_if,), (n_wires,),
+       ]
+ALL_SHP = [[intf, intf, wrs, wrs, wrs, intf, intf, wrs, wrs, wrs, wrs],
+           [(n_if,), (n_if,), (n_wires,)]]
 
 ###############
 # Combinations
 
-RPT_UNIF_SHP = list(zip(RPT_UNIF, RPT_SHAPES))
-RPT_NRML_SHP = list(zip(RPT_NRML, RPT_SHAPES))
-UNIF_SHP = list(zip(RPT_UNIF, SHAPES))
-NRML_SHP = list(zip(NRML, SHAPES))
-RPT_ALL_SHP = list(zip(RPT_ALL, RPT_ALL_SHAPES))
-ALL_SHP = list(zip(ALL, ALL_SHAPES))
-RPT_SHP = RPT_UNIF_SHP + RPT_NRML_SHP
-SHP = UNIF_SHP + NRML_SHP
-RPT_COMBINED = RPT_UNIF + RPT_NRML + RPT_ALL
-COMBINED = UNIF + NRML + ALL
+SHAPES = [i + (s,) for i, s in zip(INIT, SHP)]
+ALL_SHAPES = [i + (s,) for i, s in zip(ALL, ALL_SHP)]
+
+##################
 
 
 class TestInitRepeated:
-    """Tests the initialization functions from the ``init`` module for
-    functions that have a ``repeat`` keyword argument."""
+    """Tests the initialization functions from the ``init`` module."""
 
-    @pytest.mark.parametrize("init, shp", RPT_SHP)
-    def test_rpt_shape(self, init, shp, n_wires, repeat):
-        """Confirm that initialization functions with keyword ``repeat``
+    @pytest.mark.parametrize("init, sgntr, shp", SHAPES)
+    def test_shape(self, init, sgntr, shp, seed):
+        """Confirm that initialization functions
          return an array with the correct shape."""
-        p = init(n_wires=n_wires, n_layers=repeat, seed=0)
+        s = sgntr.copy()
+        s['seed'] = seed
+        p = init(**s)
         assert p.shape == shp
 
-    @pytest.mark.parametrize("init, shp", RPT_ALL_SHP)
-    def test_all_rpt_shape(self, init, shp, n_wires, repeat):
+    @pytest.mark.parametrize("init, sgntr, shp", ALL_SHAPES)
+    def test_all_shape(self, init, sgntr, shp, seed):
         """Confirm that ``all`` initialization functions
-        which have a ``repeat`` argument
          return an array with the correct shape."""
-        p = init(n_wires=n_wires, n_layers=repeat, seed=0)
+        s = sgntr.copy()
+        s['seed'] = seed
+        p = init(**s)
         shapes = [p_.shape for p_ in p]
         assert shapes == shp
 
-    @pytest.mark.parametrize("init", RPT_COMBINED)
-    def test_rpt_same_output_for_same_seed(self, init, n_wires, repeat, seed, tol):
-        """Confirm that initialization function returns a deterministic output
+    @pytest.mark.parametrize("init, sgntr", INIT)
+    def test_same_output_for_same_seed(self, init, sgntr, seed, tol):
+        """Confirm that initialization functions return a deterministic output
         for a fixed seed."""
-        p1 = init(n_layers=repeat, n_wires=n_wires, seed=seed)
-        p2 = init(n_layers=repeat, n_wires=n_wires, seed=seed)
+        s = sgntr.copy()
+        s['seed'] = seed
+        p1 = init(**s)
+        p2 = init(**s)
         assert np.allclose(p1, p2, atol=tol)
 
-    @pytest.mark.parametrize("init", RPT_COMBINED)
-    def test_rpt_diff_output_for_diff_seed(self, init, n_wires, repeat, seed, tol):
+    @pytest.mark.parametrize("init, sgntr", INIT)
+    def test_diff_output_for_diff_seed(self, init, sgntr, seed, tol):
         """Confirm that initialization function returns a different output for
         different seeds."""
-        p1 = init(n_layers=repeat, n_wires=n_wires, seed=seed)
-        p2 = init(n_layers=repeat, n_wires=n_wires, seed=seed+1)
+        s = sgntr.copy()
+        s['seed'] = seed
+        p1 = init(**s)
+        s['seed'] = seed + 1
+        p2 = init(**s)
         assert not np.allclose(p1, p2, atol=tol)
 
-    @pytest.mark.parametrize("init", RPT_UNIF)
-    def test_rpt_interval_uniform(self, init, seed, n_wires, tol):
-        """Test that uniformly sampled initialization functions lie
-        in correct interval."""
-        p = init(n_layers=repeat, n_wires=n_wires, low=1, high=1, seed=seed)
+    @pytest.mark.parametrize("init, sgntr", INIT)
+    def test_interval(self, init, sgntr, seed, tol):
+        """Test that sampled parameters lie in correct interval."""
+        s = sgntr.copy()
+        s['seed'] = seed
+
+        # Case A: Uniformly distributed parameters
+        if 'low' in sgntr.keys() and 'high' in s.keys():
+            s['low'] = 1
+            s['high'] = 1
+            p = init(**s)
+            p_mean = np.mean(p)
+            assert np.isclose(p_mean, 1, atol=tol)
+
+        # Case B: Normally distributed parameters
+        if 'mean' in sgntr.keys() and 'std' in s.keys():
+            s['mean'] = 1
+            s['std'] = 0
+
+        p = init(**s)
         p_mean = np.mean(p)
         assert np.isclose(p_mean, 1, atol=tol)
 
-    @pytest.mark.parametrize("init", RPT_NRML)
-    def test_rpt_interval_normal(self, init, seed, n_wires, tol):
-        """Test that normal samples of initialization functions lie
-        in correct interval."""
-        p = init(n_layers=repeat, n_wires=n_wires, mean=1, std=0, seed=seed)
-        p_mean = np.mean(p)
-        assert np.isclose(p_mean, 1, atol=tol)
 
 
-class TestInitNotRepeated:
-    """Tests the initialization functions from the ``init`` module for
-    functions that have no ``repeat`` keyword argument."""
-
-    @pytest.mark.parametrize("init, shp", SHP)
-    def test_shape(self, init, shp, n_wires):
-        """Confirm that initialization functions without ``repeat``
-         returns an array with the correct shape."""
-        p = init(n_wires=n_wires, seed=0)
-        assert p.shape == shp
-
-    @pytest.mark.parametrize("init, shp", ALL_SHP)
-    def test_all_shape(self, init, shp, n_wires):
-        """Confirm that ``all`` initialization functions
-         return an array with the correct shape."""
-        p = init(n_wires=n_wires, seed=0)
-        shapes = [p_.shape for p_ in p]
-        assert shapes == shp
-
-    @pytest.mark.parametrize("init", COMBINED)
-    def test_same_output_for_same_seed(self, init, n_wires, seed, tol):
-        """Confirm that initialization function returns a deterministic output
-        for a fixed seed."""
-        p1 = init(n_wires=n_wires, seed=seed)
-        p2 = init(n_wires=n_wires, seed=seed)
-        assert np.allclose(p1, p2, atol=tol)
-
-    @pytest.mark.parametrize("init", COMBINED)
-    def test_diff_output_for_diff_seed(self, init, n_wires, seed, tol):
-        """Confirm that initialization function returns a different output for
-        different seeds."""
-        p1 = init(n_wires=n_wires, seed=seed)
-        p2 = init(n_wires=n_wires, seed=seed + 1)
-        assert not np.allclose(p1, p2, atol=tol)
-
-    @pytest.mark.parametrize("init", UNIF)
-    def test_interval_uniform(self, init, seed, n_wires, tol):
-        """Test that uniformly sampled initialization functions lie
-        in correct interval."""
-        p = init(n_wires=n_wires, low=1, high=1, seed=seed)
-        p_mean = np.mean(p)
-        assert np.isclose(p_mean, 1, atol=tol)
-
-    @pytest.mark.parametrize("init", NRML)
-    def test_interval_normal(self, init, seed, n_wires, tol):
-        """Test that normal samples of initialization functions lie
-        in correct interval."""
-        p = init(n_wires=n_wires, mean=1, std=0, seed=seed)
-        p_mean = np.mean(p)
-        assert np.isclose(p_mean, 1, atol=tol)
