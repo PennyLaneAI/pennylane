81d64a0b259f6da1bff98249fb765a8fa1d643fb
antalszava <antalszava@gmail.com>
2019-11-13 14:38:55 -0500

    Remove whitespace in line 145 __init__.py

--- pennylane/__init__.py
+++ pennylane/__init__.py
@@ -142,7 +142,7 @@ def load(quantum_circuit_object, format: str):
     into PennyLane templates.
 
     .. note::
-    
+
         For more details on which operations are supported
         please consult the corresponding plugin documentation:
         https://pennylane.ai/plugins.html
