d2da1f5debdeba7d988efa6b73333c0124566346
Josh Izaac <josh146@gmail.com>
2019-11-15 18:33:04 -0500

    tf interface fix

--- pennylane/interfaces/tf.py
+++ pennylane/interfaces/tf.py
@@ -80,10 +80,10 @@ def TFQNode(qnode):
             # scalar result, cast to NumPy scalar
             res = np.array(res)
 
-        def grad(grad_output, **kwargs):
+        def grad(grad_output, **tfkwargs):
             """Returns the vector-Jacobian product"""
             # evaluate the Jacobian matrix of the QNode
-            variables = kwargs.get('variables', None)
+            variables = tfkwargs.get('variables', None)
             jacobian = qnode.jacobian(args, **kwargs)
 
             grad_output_np = grad_output.numpy()
