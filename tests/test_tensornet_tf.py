88cde3906ad6b79b8acc7ed9f66cee4336623e85
Josh Izaac <josh146@gmail.com>
2019-11-16 14:49:22 -0500

    Apply suggestions from code review
    Co-Authored-By: Nathan Killoran <co9olguy@users.noreply.github.com>

--- tests/test_tensornet_tf.py
+++ tests/test_tensornet_tf.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Xanadu Quantum Technologies Inc.
+# Copyright 2019 Xanadu Quantum Technologies Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Unit tests and integration tests for the :mod:`pennylane.plugin.Tensornet` device.
+Unit tests and integration tests for the :mod:`pennylane.plugin.Tensornet.tf` device.
 """
 import cmath
 # pylint: disable=protected-access,cell-var-from-loop
