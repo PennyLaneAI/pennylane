96977e9095981af47f2a1f10ebafca3bef3932ae
antalszava <antalszava@gmail.com>
2019-12-03 10:05:05 -0500

    Update pennylane/ops/qubit.py
    Co-Authored-By: Josh Izaac <josh146@gmail.com>

--- pennylane/ops/qubit.py
+++ pennylane/ops/qubit.py
@@ -449,7 +449,7 @@ class CRX(Operation):
             \end{bmatrix}.
         \end{align}
 
-    .. note:: The subscripts of the operations in the formula refer to the wires they act on, e.g. 1 corresponds to the first element in `wires` that is the **control qubit**.
+    .. note:: The subscripts of the operations in the formula refer to the wires they act on, e.g., 1 corresponds to the first element in ``wires`` that is the **control qubit**.
 
 
     **Details:**
