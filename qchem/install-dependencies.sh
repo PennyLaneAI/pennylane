3ae2f49e00afd45750927a1aacd41b790b6f0ce1
Josh Izaac <josh146@gmail.com>
2019-12-23 18:16:51 -0500

    make dependencies executable

--- qchem/install-dependencies.sh
+++ qchem/install-dependencies.sh
@@ -1,20 +1,21 @@
+# install miniconda
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
 source "$HOME/miniconda/etc/profile.d/conda.sh"
 hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
-Useful for debugging any issues with conda
+# Useful for debugging any issues with conda
 conda info -a
 create a virtual env
-conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION
+conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION pytest pytest-cov
 conda activate test-environment
+# Install psi4 and openbabel
 conda install -c conda-forge openbabel
 conda install psi4 psi4-rt python=$TRAVIS_PYTHON_VERSION -c psi4
-install PennyLane and Qchem
+# install PennyLane and Qchem
 pip install -r requirements.txt
-pip install wheel pytest pytest-cov --upgrade
-python3 setup.py bdist_wheel
+python setup.py bdist_wheel
 pip install dist/PennyLane*.whl
-cd qchem && python3 setup.py bdist_wheel && cd ../
+cd qchem && python setup.py bdist_wheel && cd ../
 pip install qchem/dist/PennyLane_Qchem*.whl
