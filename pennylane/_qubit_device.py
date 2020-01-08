35f25681f579902a2e772a2f56cdcdb71ba64aff
antalszava <antalszava@gmail.com>
2020-01-08 13:55:11 -0500

    Fix comment section

--- pennylane/_qubit_device.py
+++ pennylane/_qubit_device.py
@@ -1,4 +1,4 @@
-#       i Copyright 2018-2020 Xanadu Quantum Technologies Inc.
+# Copyright 2018-2020 Xanadu Quantum Technologies Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
