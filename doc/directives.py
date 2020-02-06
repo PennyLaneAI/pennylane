66dc1284d4877fa1e2d2f57f3538eb283f01d2b7
Josh Izaac <josh146@gmail.com>
2020-02-06 11:59:00 -0500

    remove sphinx gallery

--- doc/directives.py
+++ doc/directives.py
@@ -19,7 +19,6 @@ import os
 from docutils.parsers.rst import Directive, directives
 from docutils.statemachine import StringList
 from docutils import nodes
-import sphinx_gallery
 
 
 USAGE_DETAILS_TEMPLATE = """
@@ -56,79 +55,6 @@ class UsageDetails(Directive):
         node = nodes.section()
         self.state.nested_parse(string_list, self.content_offset, node)
         return [node]
-
-
-class GalleryItemDirective(Directive):
-    """
-    Create a sphinx gallery thumbnail for insertion anywhere in docs.
-    Optionally, you can specify the custom figure and intro/tooltip for the
-    thumbnail.
-    Example usage:
-    .. galleryitem:: intermediate/char_rnn_generation_tutorial.py
-        :figure: _static/img/char_rnn_generation.png
-        :intro: Put your custom intro here.
-        :size: put image size here
-    If figure is specified, a thumbnail will be made out of it and stored in
-    _static/thumbs. Therefore, consider _static/thumbs as a 'built' directory.
-    """
-
-    required_arguments = 1
-    optional_arguments = 1
-    final_argument_whitespace = True
-    option_spec = {'figure': directives.unchanged,
-                   'intro': directives.unchanged}
-    has_content = False
-    add_index = False
-
-    def run(self):
-        args = self.arguments
-        fname = args[-1]
-
-        env = self.state.document.settings.env
-        fname, abs_fname = env.relfn2path(fname)
-        basename = os.path.basename(fname)
-        dirname = os.path.dirname(fname)
-
-        try:
-            if 'intro' in self.options:
-                intro = self.options['intro'][:195] + '...'
-            else:
-                _, blocks = sphinx_gallery.gen_rst.split_code_and_text_blocks(abs_fname)
-                intro, _ = sphinx_gallery.gen_rst.extract_intro_and_title(abs_fname, blocks[0][1])
-
-            thumbnail_rst = sphinx_gallery.backreferences._thumbnail_div(
-                dirname, basename, intro)
-
-            if 'figure' in self.options:
-                rel_figname, figname = env.relfn2path(self.options['figure'])
-                save_figname = os.path.join('_static/thumbs/',
-                                            os.path.basename(figname))
-
-                try:
-                    os.makedirs('_static/thumbs')
-                except OSError:
-                    pass
-
-                x, y = (400, 280)
-                if 'size' in self.options:
-                    x, y = self.options['size'].split(" ")
-
-                sphinx_gallery.gen_rst.scale_image(figname, save_figname,
-                                                   x, y)
-                # replace figure in rst with simple regex
-                thumbnail_rst = re.sub(r'..\sfigure::\s.*\.png',
-                                       '.. figure:: /{}'.format(save_figname),
-                                       thumbnail_rst)
-
-            thumbnail = StringList(thumbnail_rst.split('\n'))
-            thumb = nodes.paragraph()
-            self.state.nested_parse(thumbnail, self.content_offset, thumb)
-
-            return [thumb]
-        except FileNotFoundError as e:
-            print(e)
-            return []
-
 
 
 GALLERY_TEMPLATE = """
