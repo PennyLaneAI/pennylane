c5e936ecbee9aa49d6f398201036ad12e6325a36
antalszava <antalszava@gmail.com>
2020-03-10 09:29:36 -0400

    Update pennylane/qnodes/decorator.py
    Co-Authored-By: Josh Izaac <josh146@gmail.com>

--- pennylane/qnodes/decorator.py
+++ pennylane/qnodes/decorator.py
@@ -181,8 +181,8 @@ def qnode(device, *, interface="autograd", mutable=True, diff_method="best", **k
             * ``None``: a non-differentiable QNode is returned.
 
     Keyword Args:
-        h (float): Step size for the finite difference method. Default is `1e-7` for analytic devices, or
-            `0.3` for non-analytic devices (those that estimate expectation values with a finite number of shots).
+        h (float): Step size for the finite difference method. Default is ``1e-7`` for analytic devices, or
+            ``0.3`` for non-analytic devices (those that estimate expectation values with a finite number of shots).
    """
 
     @lru_cache()
