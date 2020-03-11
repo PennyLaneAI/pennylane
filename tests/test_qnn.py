a2b2ae550410f3df115567bea2e59307c08efb87
trbromley <brotho02@gmail.com>
2020-03-11 13:16:47 -0400

    Update test

--- tests/test_qnn.py
+++ tests/test_qnn.py
@@ -38,10 +38,9 @@ def get_circuit(n_qubits, output_dim):
         """A circuit that embeds data using the AngleEmbedding and then performs a variety of
         operations. The output is a PauliZ measurement on the first output_dim qubits."""
         qml.templates.AngleEmbedding(x, wires=list(range(n_qubits)))
-
-        qml.templates.StronglyEntanglingLayers(w1, wires=list(range(n_qubits)))
-        # print(w2.shape, n_qubits, output_dim)
-        # qml.templates.StronglyEntanglingLayers(w2, wires=list(range(n_qubits)))
+        if n_qubits > 1:
+            qml.templates.StronglyEntanglingLayers(w1, wires=list(range(n_qubits)))
+            qml.templates.StronglyEntanglingLayers(w2, wires=list(range(n_qubits)))
         qml.RX(w3, wires=0)
         qml.RX(w4, wires=0)
         qml.Rot(*w5, wires=0)
