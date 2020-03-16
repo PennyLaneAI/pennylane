b313ef29abecb97b00520d2151461b670dd5f6df
johannesjmeyer <jj.meyer@outlook.com>
2020-03-16 17:03:37 +0100

    Add multiple contexts test

--- tests/test_queuing_context.py
+++ tests/test_queuing_context.py
@@ -22,7 +22,7 @@ from pennylane import QueuingContext
 
 @pytest.fixture(scope="function")
 def mock_queuing_context(monkeypatch):
-    """A mock instance of the abstract Device class with non-empty operations"""
+    """A mock instance of the abstract QueuingContext class."""
     with monkeypatch.context() as m:
         m.setattr(QueuingContext, '__abstractmethods__', frozenset())
         m.setattr(QueuingContext, '_append_operator', lambda self, operator: self.queue.append(operator))
@@ -31,6 +31,20 @@ def mock_queuing_context(monkeypatch):
         context.queue = []
 
         yield context
+
+@pytest.fixture(scope="function")
+def three_mock_queuing_contexts(monkeypatch):
+    """A list of three mock instances of the abstract QueuingContext class."""
+    with monkeypatch.context() as m:
+        m.setattr(QueuingContext, '__abstractmethods__', frozenset())
+        m.setattr(QueuingContext, '_append_operator', lambda self, operator: self.queue.append(operator))
+        m.setattr(QueuingContext, '_remove_operator', lambda self, operator: self.queue.remove(operator))
+
+        contexts = [QueuingContext() for _ in range(3)]
+        for context in contexts:
+            context.queue = []
+
+        yield contexts
 
 
 class TestQueuingContext:
@@ -45,6 +59,22 @@ class TestQueuingContext:
         with mock_queuing_context:
             assert len(QueuingContext._active_contexts) == 1
             assert mock_queuing_context in QueuingContext._active_contexts
+
+        assert not QueuingContext._active_contexts
+
+    def test_multiple_context_activation(self, three_mock_queuing_contexts):
+        """Test that multiple QueuingContexts are properly activated and deactivated."""
+
+        # Assert that the list of active contexts is empty
+        assert not QueuingContext._active_contexts
+
+        with three_mock_queuing_contexts[0]:
+            with three_mock_queuing_contexts[1]:
+                with three_mock_queuing_contexts[2]:
+                    assert len(QueuingContext._active_contexts) == 3
+                    assert three_mock_queuing_contexts[0] in QueuingContext._active_contexts
+                    assert three_mock_queuing_contexts[1] in QueuingContext._active_contexts
+                    assert three_mock_queuing_contexts[2] in QueuingContext._active_contexts
 
         assert not QueuingContext._active_contexts
 
@@ -70,9 +100,8 @@ class TestQueuingContext:
         assert len(mock_queuing_context.queue) == 1
         assert op in mock_queuing_context.queue
 
-
     def test_remove_operator(self, mock_queuing_context):
-        """Test that append_operator removes the operator to the queue."""
+        """Test that remove_operator removes the operator from the queue."""
 
         op = qml.PauliZ(0)
         assert not mock_queuing_context.queue
@@ -86,3 +115,53 @@ class TestQueuingContext:
             QueuingContext.remove_operator(op)
 
         assert not mock_queuing_context.queue
+
+    def test_append_operator_multiple_queues(self, three_mock_queuing_contexts):
+        """Test that append_operator appends the operator to multiple queues."""
+
+        op = qml.PauliZ(0)
+        assert not three_mock_queuing_contexts[0].queue
+        assert not three_mock_queuing_contexts[1].queue
+        assert not three_mock_queuing_contexts[2].queue
+
+        with three_mock_queuing_contexts[0]:
+            with three_mock_queuing_contexts[1]:
+                with three_mock_queuing_contexts[2]:
+                    QueuingContext.append_operator(op)
+
+        assert len(three_mock_queuing_contexts[0].queue) == 1
+        assert op in three_mock_queuing_contexts[0].queue
+
+        assert len(three_mock_queuing_contexts[1].queue) == 1
+        assert op in three_mock_queuing_contexts[1].queue
+
+        assert len(three_mock_queuing_contexts[1].queue) == 1
+        assert op in three_mock_queuing_contexts[1].queue
+
+    def test_remove_operator_multiple_queues(self, three_mock_queuing_contexts):
+        """Test that remove_operator removes the operator from the queue."""
+
+        op = qml.PauliZ(0)
+        assert not three_mock_queuing_contexts[0].queue
+        assert not three_mock_queuing_contexts[1].queue
+        assert not three_mock_queuing_contexts[2].queue
+
+        with three_mock_queuing_contexts[0]:
+            with three_mock_queuing_contexts[1]:
+                with three_mock_queuing_contexts[2]:
+                    QueuingContext.append_operator(op)
+
+                    assert len(three_mock_queuing_contexts[0].queue) == 1
+                    assert op in three_mock_queuing_contexts[0].queue
+
+                    assert len(three_mock_queuing_contexts[1].queue) == 1
+                    assert op in three_mock_queuing_contexts[1].queue
+
+                    assert len(three_mock_queuing_contexts[2].queue) == 1
+                    assert op in three_mock_queuing_contexts[2].queue
+
+                    QueuingContext.remove_operator(op)
+
+        assert not three_mock_queuing_contexts[0].queue
+        assert not three_mock_queuing_contexts[1].queue
+        assert not three_mock_queuing_contexts[2].queue
