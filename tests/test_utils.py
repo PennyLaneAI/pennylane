2d6cf986c5dd3c53e39ef0d0387ee4596aa46639
johannesjmeyer <jj.meyer@outlook.com>
2020-03-30 17:18:03 +0200

    Add expand_vector exception tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -322,17 +322,20 @@ class TestExpand:
     VECTOR1 = np.array([1, -1])
     ONES = np.array([1, 1])
 
-    @pytest.mark.parametrize("original_wires,expanded_wires,expected", [
-        ([0], 3, np.kron(np.kron(VECTOR1, ONES), ONES)),
-        ([1], 3, np.kron(np.kron(ONES, VECTOR1), ONES)),
-        ([2], 3, np.kron(np.kron(ONES, ONES), VECTOR1)),
-        ([0], [0, 4, 7], np.kron(np.kron(VECTOR1, ONES), ONES)),
-        ([4], [0, 4, 7], np.kron(np.kron(ONES, VECTOR1), ONES)),
-        ([7], [0, 4, 7], np.kron(np.kron(ONES, ONES), VECTOR1)),
-        ([0], [0, 4, 7], np.kron(np.kron(VECTOR1, ONES), ONES)),
-        ([4], [4, 0, 7], np.kron(np.kron(VECTOR1, ONES), ONES)),
-        ([7], [7, 4, 0], np.kron(np.kron(VECTOR1, ONES), ONES)),
-    ])
+    @pytest.mark.parametrize(
+        "original_wires,expanded_wires,expected",
+        [
+            ([0], 3, np.kron(np.kron(VECTOR1, ONES), ONES)),
+            ([1], 3, np.kron(np.kron(ONES, VECTOR1), ONES)),
+            ([2], 3, np.kron(np.kron(ONES, ONES), VECTOR1)),
+            ([0], [0, 4, 7], np.kron(np.kron(VECTOR1, ONES), ONES)),
+            ([4], [0, 4, 7], np.kron(np.kron(ONES, VECTOR1), ONES)),
+            ([7], [0, 4, 7], np.kron(np.kron(ONES, ONES), VECTOR1)),
+            ([0], [0, 4, 7], np.kron(np.kron(VECTOR1, ONES), ONES)),
+            ([4], [4, 0, 7], np.kron(np.kron(VECTOR1, ONES), ONES)),
+            ([7], [7, 4, 0], np.kron(np.kron(VECTOR1, ONES), ONES)),
+        ],
+    )
     def test_expand_vector_single_wire(self, original_wires, expanded_wires, expected, tol):
         """Test that expand_vector works with a single-wire vector."""
 
@@ -343,21 +346,37 @@ class TestExpand:
     VECTOR2 = np.array([1, 2, 3, 4])
     ONES = np.array([1, 1])
 
-    @pytest.mark.parametrize("original_wires,expanded_wires,expected", [
-        ([0, 1], 3, np.kron(VECTOR2, ONES)),
-        ([1, 2], 3, np.kron(ONES, VECTOR2)),
-        ([0, 2], 3, np.array([1, 2, 1, 2, 3, 4, 3, 4])),
-        ([0, 5], [0, 5, 9], np.kron(VECTOR2, ONES)),
-        ([5, 9], [0, 5, 9], np.kron(ONES, VECTOR2)),
-        ([0, 9], [0, 5, 9], np.array([1, 2, 1, 2, 3, 4, 3, 4])),
-        ([9, 0], [0, 5, 9], np.array([1, 3, 1, 3, 2, 4, 2, 4])),
-    ])
+    @pytest.mark.parametrize(
+        "original_wires,expanded_wires,expected",
+        [
+            ([0, 1], 3, np.kron(VECTOR2, ONES)),
+            ([1, 2], 3, np.kron(ONES, VECTOR2)),
+            ([0, 2], 3, np.array([1, 2, 1, 2, 3, 4, 3, 4])),
+            ([0, 5], [0, 5, 9], np.kron(VECTOR2, ONES)),
+            ([5, 9], [0, 5, 9], np.kron(ONES, VECTOR2)),
+            ([0, 9], [0, 5, 9], np.array([1, 2, 1, 2, 3, 4, 3, 4])),
+            ([9, 0], [0, 5, 9], np.array([1, 3, 1, 3, 2, 4, 2, 4])),
+        ],
+    )
     def test_expand_vector_two_wires(self, original_wires, expanded_wires, expected, tol):
         """Test that expand_vector works with a single-wire vector."""
 
         res = pu.expand_vector(TestExpand.VECTOR2, original_wires, expanded_wires)
 
         assert np.allclose(res, expected, atol=tol, rtol=0)
+
+    def test_expand_vector_invalid_wires(self):
+        """Test exception raised if unphysical subsystems provided."""
+        with pytest.raises(
+            ValueError, match="Invalid target subsystems provided in 'original_wires' argument"
+        ):
+            pu.expand_vector(TestExpand.VECTOR2, [-1, 5], 4)
+
+    def test_expand_vector_invalid_vector(self):
+        """Test exception raised if incorrect sized vector provided."""
+        with pytest.raises(ValueError, match="Vector parameter must be of length"):
+            pu.expand_vector(TestExpand.VECTOR1, [0, 1], 4)
+
 
 class TestOperationRecorder:
     """Test the OperationRecorder class."""
