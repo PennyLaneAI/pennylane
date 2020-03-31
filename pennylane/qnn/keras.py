e0c35485c59a10dfdd19069d6e8462b4e8570f34
trbromley <brotho02@gmail.com>
2020-03-31 15:12:58 -0400

    Clarify docstring

--- pennylane/qnn/keras.py
+++ pennylane/qnn/keras.py
@@ -300,6 +300,6 @@ class KerasLayer(Layer):
     @property
     def input_arg(self):
         """Name of the argument to be used as the input to the Keras
-        `Layer <https://www.tensorflow.org/api_docs/python/tf/keras/layers/Layer>`__. Defaults to
+        `Layer <https://www.tensorflow.org/api_docs/python/tf/keras/layers/Layer>`__. Set to
         ``"inputs"``."""
         return self._input_arg
