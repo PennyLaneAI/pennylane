59894ad347240dd678b656b27dad65dc1acbaee3
trbromley <brotho02@gmail.com>
2020-04-15 15:38:41 -0400

    Remove common fixtures from test_keras

--- tests/qnn/test_keras.py
+++ tests/qnn/test_keras.py
@@ -23,40 +23,6 @@ from pennylane.qnn.keras import KerasLayer
 tf = pytest.importorskip("tensorflow", minversion="2")
 
 
-@pytest.fixture
-def get_circuit(n_qubits, output_dim, interface):
-    """Fixture for getting a sample quantum circuit with a controllable qubit number and output
-    dimension. Returns both the circuit and the shape of the weights."""
-
-    dev = qml.device("default.qubit", wires=n_qubits)
-    weight_shapes = {
-        "w1": (3, n_qubits, 3),
-        "w2": (1,),
-        "w3": 1,
-        "w4": [3],
-        "w5": (2, n_qubits, 3),
-        "w6": 3,
-        "w7": 0,
-    }
-
-    @qml.qnode(dev, interface=interface)
-    def circuit(inputs, w1, w2, w3, w4, w5, w6, w7):
-        """A circuit that embeds data using the AngleEmbedding and then performs a variety of
-        operations. The output is a PauliZ measurement on the first output_dim qubits. One set of
-        parameters, w5, are specified as non-trainable."""
-        qml.templates.AngleEmbedding(inputs, wires=list(range(n_qubits)))
-        qml.templates.StronglyEntanglingLayers(w1, wires=list(range(n_qubits)))
-        qml.RX(w2[0], wires=0 % n_qubits)
-        qml.RX(w3, wires=1 % n_qubits)
-        qml.Rot(*w4, wires=2 % n_qubits)
-        qml.templates.StronglyEntanglingLayers(w5, wires=list(range(n_qubits)))
-        qml.Rot(*w6, wires=3 % n_qubits)
-        qml.RX(w7, wires=4 % n_qubits)
-        return [qml.expval(qml.PauliZ(i)) for i in range(output_dim)]
-
-    return circuit, weight_shapes
-
-
 @pytest.mark.usefixtures("get_circuit")
 @pytest.fixture
 def model(get_circuit, n_qubits, output_dim):
