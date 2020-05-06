9f22b030f39b6b96b07bb4471d73e77c2bee4c33
johannesjmeyer <jj.meyer@outlook.com>
2020-05-06 11:42:27 +0200

    Document tuple_to_word

--- pennylane/templates/subroutines/arbitrary_unitary.py
+++ pennylane/templates/subroutines/arbitrary_unitary.py
@@ -22,6 +22,17 @@ _PAULIS = ["I", "X", "Y", "Z"]
 
 
 def _tuple_to_word(index_tuple):
+    """Convert an integer tuple to the corresponding Pauli word.
+
+    The Pauli operators are converted as ``0 -> I``, ``1 -> X``,
+    ``2 -> Y``, ``3 -> Z``.
+
+    Args:
+        index_tuple (Tuple[int]): An integer tuple describing the Pauli word
+
+    Returns:
+        str: The corresponding Pauli word
+    """
     return "".join([_PAULIS[i] for i in index_tuple])
 
 
