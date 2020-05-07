cb97aab0c9c090d2cd882155333ebc1137abc34c
trbromley <brotho02@gmail.com>
2020-05-07 15:07:45 -0400

    Add fix

--- pennylane/interfaces/tf.py
+++ pennylane/interfaces/tf.py
@@ -16,13 +16,12 @@ This module contains the :func:`to_tf` function to convert Numpy-interfacing qua
 compatible quantum nodes.
 """
 # pylint: disable=redefined-outer-name
+import numbers
+from collections import Iterable
 from functools import partial
 
 import numpy as np
 import tensorflow as tf
-
-from pennylane.utils import unflatten
-
 
 if tf.__version__[0] == "1":
     import tensorflow.contrib.eager as tfe  # pylint: disable=unused-import,ungrouped-imports
@@ -30,6 +29,40 @@ if tf.__version__[0] == "1":
     Variable = tfe.Variable
 else:
     from tensorflow import Variable  # pylint: disable=unused-import,ungrouped-imports
+
+
+def unflatten_tf(flat, model):
+    """Restores an arbitrary nested structure to a flattened TF tensor.
+
+    See also :func:`_unflatten`.
+
+    Args:
+        flat (tf.Tensor): 1D tensor of items
+        model (array, Iterable, Number): model nested structure
+
+    Returns:
+        Union[tf.Tensor, list], array: first elements of flat arranged into the nested
+        structure of model, unused elements of flat
+
+    Raises:
+        TypeError: if ``model`` contains an object of unsupported type
+    """
+    if isinstance(model, (numbers.Number, str)):
+        return flat[0], flat[1:]
+
+    if isinstance(model, (tf.Tensor, tf.Variable)):
+        idx = tf.size(model)
+        res = tf.reshape(flat[:idx], model.shape)
+        return res, flat[idx:]
+
+    if isinstance(model, Iterable):
+        res = []
+        for x in model:
+            val, flat = unflatten_tf(flat, x)
+            res.append(val)
+        return res, flat
+
+    raise TypeError("Unsupported type in the model: {}".format(type(model)))
 
 
 def to_tf(qnode, dtype=None):
@@ -99,30 +132,17 @@ def to_tf(qnode, dtype=None):
             """Returns the vector-Jacobian product"""
             # evaluate the Jacobian matrix of the QNode
             variables = tfkwargs.get("variables", None)
-
             jacobian = qnode.jacobian(args, kwargs)
-            grad_output_np = grad_output.numpy()
-
-            # perform the vector-Jacobian product
-            if not grad_output_np.shape:
-                temp = grad_output_np * jacobian
-            else:
-                temp = grad_output_np.T @ jacobian
-
-            # restore the nested structure of the input args
-            grad_input = unflatten(temp.flat, args)
-
-            if isinstance(grad_input, list):
-                grad_input = [tf.convert_to_tensor(i, dtype=dtype) for i in grad_input]
-            elif isinstance(grad_input, tuple):
-                grad_input = tuple(tf.convert_to_tensor(i, dtype=dtype) for i in grad_input)
-            else:
-                grad_input = tf.convert_to_tensor(grad_input, dtype=dtype)
+            jacobian = tf.constant(jacobian, dtype=dtype)
+            grad_output_row = tf.transpose(tf.reshape(grad_output, [-1, 1]))
+            grad_input = tf.matmul(grad_output_row, jacobian)
+            grad_input = tf.reshape(grad_input, [-1])
+            grad_input_unflattened = unflatten_tf(grad_input, input_)[0]
 
             if variables is not None:
-                return grad_input, variables
+                return grad_input_unflattened, variables
 
-            return grad_input
+            return grad_input_unflattened
 
         return tf.convert_to_tensor(res, dtype=dtype), grad
 
