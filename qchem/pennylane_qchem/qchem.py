b06e31b51aabb0f2e9556eb0438ddd3f4c9f199a
agran2018 <nanoalain@gmail.com>
2020-05-08 19:04:07 -0400

    Remove trailing spaces

--- qchem/pennylane_qchem/qchem.py
+++ qchem/pennylane_qchem/qchem.py
@@ -648,17 +648,17 @@ def hf_state(n_electrons, m_spin_orbitals):
 
     The many-particle wave functon in the HF approximation simplifies to a Slater determinant:
 
-    .. math: 
+    .. math:
         \Phi({\bf x}_1, {\bf x}_2, \dots ,{\bf x}_N) = \left|\begin{array}{cccc}
         \varphi_1({\bf x}_1) & \varphi_2({\bf x}_1) & \cdots & \varphi_N({\bf x}_1) \\
         \varphi_1({\bf x}_2) & \varphi_2({\bf x}_2) & \cdots & \varphi_N({\bf x}_2) \\
         \vdots               & \vdots               & \ddots & \vdots            \\
         \varphi_1({\bf x}_N) & \varphi_2({\bf x}_N) & \cdots & \varphi_N({\bf x}_N),
 
-	where :math:`\varphi_i` denotes the single-particle wave function of the :math:`ith` HF 
+	where :math:`\varphi_i` denotes the single-particle wave function of the :math:`ith` HF
 	orbital. In Fock space, this determinant is represented by an occupation-number vector:
 
-    .. math: 
+    .. math:
         \vert {\bf n} \rangle = \vert n_1, n_2, \dots, n_M \rangle,
 
         n_i = \left\lbrace \begin{array}{ll} 1 & i \leq N_e \\ 0 & i > N_e \end{array} \right..
@@ -668,18 +668,18 @@ def hf_state(n_electrons, m_spin_orbitals):
     >>> init_state = hf_state(2, 6)
     >>> print(init_state)
     [1 1 0 0 0 0]
-    
-    Args: 
-        n_electrons (int): number of electrons 
+
+    Args:
+        n_electrons (int): number of electrons
         m_spin_orbitals (int): number of spin-orbitals
 
     Returns:
         array: NumPy array containing the vector :math:`{\bf n}`
-    """	
+    """
 
-	hf_state_on = [1 if i < n_electrons else 0 for i in range(m_spin_orbitals)]
+    hf_state_on = [1 if i < n_electrons else 0 for i in range(m_spin_orbitals)]
 
-	return np.array(hf_state_on)
+    return np.array(hf_state_on)
 
 
 __all__ = [
