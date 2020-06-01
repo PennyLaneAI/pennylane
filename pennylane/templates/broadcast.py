6dadb209fa693cbb895652c9128038e6fe81e4cc
mariaschuld <mariaschuld@gmail.com>
2020-06-01 14:16:21 +0200

    black

--- pennylane/templates/broadcast.py
+++ pennylane/templates/broadcast.py
@@ -49,7 +49,7 @@ def wires_pyramid(wires):
     """Wire sequence for the pyramid pattern."""
     sequence = []
     for layer in range(len(wires) // 2):
-        block = wires[layer: len(wires) - layer]
+        block = wires[layer : len(wires) - layer]
         sequence += [block.subset([i, i + 1]) for i in range(0, len(block) - 1, 2)]
     return sequence
 
--- pennylane/templates/subroutines/interferometer.py
+++ pennylane/templates/subroutines/interferometer.py
@@ -154,9 +154,7 @@ def Interferometer(theta, phi, varphi, wires, mesh="rectangular", beamsplitter="
                         Rotation(phi[n], wires=[w1])
                         Beamsplitter(theta[n], 0, wires=[w1, w2])
                     else:
-                        Beamsplitter(
-                            theta[n], phi[n], wires=[w1, w2]
-                        )
+                        Beamsplitter(theta[n], phi[n], wires=[w1, w2])
                     n += 1
 
     elif mesh == "triangular":
@@ -165,16 +163,10 @@ def Interferometer(theta, phi, varphi, wires, mesh="rectangular", beamsplitter="
         for l in range(2 * M - 3):
             for k in range(abs(l + 1 - (M - 1)), M - 1, 2):
                 if beamsplitter == "clements":
-                    Rotation(
-                        phi[n], wires=wires[k]
-                    )
-                    Beamsplitter(
-                        theta[n], 0, wires=[wires[k], wires[k + 1]]
-                    )
+                    Rotation(phi[n], wires=wires[k])
+                    Beamsplitter(theta[n], 0, wires=[wires[k], wires[k + 1]])
                 else:
-                    Beamsplitter(
-                        theta[n], phi[n], wires=[wires[k], wires[k + 1]]
-                    )
+                    Beamsplitter(theta[n], phi[n], wires=[wires[k], wires[k + 1]])
                 n += 1
 
     # apply the final local phase shifts to all modes
