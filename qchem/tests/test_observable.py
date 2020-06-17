cb588923fce8f73767e16e27c8a1ce8edf68d2cc
agran2018 <nanoalain@gmail.com>
2020-06-17 18:15:19 -0400

    Format files with the unit tests

--- qchem/tests/test_observable.py
+++ qchem/tests/test_observable.py
@@ -11,45 +11,76 @@ from openfermion.ops._qubit_operator import QubitOperator
 @pytest.mark.parametrize(
     ("me_table", "init_term", "mapping", "terms_exp"),
     [
-        (np.array([[0., 0., 0.5], [1., 1., -0.5], [1., 0., 0.0]]),
-         1/2,
-         "JORDAN_wigner",
-         {(): (0.5+0j), ((0, 'Z'),): (-0.25+0j), ((1, 'Z'),): (0.25+0j)}),
-
-        (np.array([[0., 0., 0.5], [1., 1., -0.5], [1., 0., 0.0]]),
-         0,
-         "JORDAN_wigner",
-         {((0, 'Z'),): (-0.25+0j), ((1, 'Z'),): (0.25+0j)}),
-
-        (np.array([[0., 0., 0.2], [1., 1., -0.5], [1., 0., 0.0]]),
-         1/2,
-         "bravyi_KITAEV",
-         {(): (0.35+0j), ((0, 'Z'),): (-0.1+0j), ((0, 'Z'), (1, 'Z')): (0.25+0j)}),
-
-        (np.array([[0., 0., 0., 0.,  0.25], [0., 1., 1., 0., -0.25], [ 0., 2., 2., 0., 0.25],
-                   [0., 3., 3., 0., -0.25], [1., 0., 0., 1., -0.25], [ 2., 0., 0., 2., 0.25]]),
-         1/2,
-         "JORDAN_wigner",
-         {(): (0.4375+0j),
-          ((1, 'Z'),): (0.125+0j),
-          ((0, 'Z'), (1, 'Z')): (-0.125+0j),
-          ((2, 'Z'),): (-0.125+0j),
-          ((0, 'Z'), (2, 'Z')): (0.125+0j),
-          ((0, 'Z'),): (0.0625+0j),
-          ((3, 'Z'),): (0.0625+0j),
-          ((0, 'Z'), (3, 'Z')): (-0.0625+0j)}),
-
-        (np.array([[0., 0., 0., 0.,  0.5], [0.,  1., 1., 0., -0.25], [ 0., 2., 2., 0.,  0.5],
-                   [0., 3., 3., 0., -0.25], [1., 0., 0., 1., -0.25], [ 2., 0., 0., 2., -0.25]]),
-         1/4,
-         "bravyi_KITAEV",
-         {(): (0.125+0j),
-          ((0, 'Z'), (1, 'Z')): (0.125+0j),
-          ((1, 'Z'),): (-0.125+0j), ((2, 'Z'),): (-0.0625+0j),
-          ((0, 'Z'), (2, 'Z')): (0.0625+0j),
-          ((1, 'Z'), (2, 'Z'), (3, 'Z')): (0.0625+0j),
-          ((0, 'Z'), (1, 'Z'), (2, 'Z'), (3, 'Z')): (-0.0625+0j),
-          ((0, 'Z'),): (0.125+0j)})
+        (
+            np.array([[0.0, 0.0, 0.5], [1.0, 1.0, -0.5], [1.0, 0.0, 0.0]]),
+            1 / 2,
+            "JORDAN_wigner",
+            {(): (0.5 + 0j), ((0, "Z"),): (-0.25 + 0j), ((1, "Z"),): (0.25 + 0j)},
+        ),
+        (
+            np.array([[0.0, 0.0, 0.5], [1.0, 1.0, -0.5], [1.0, 0.0, 0.0]]),
+            0,
+            "JORDAN_wigner",
+            {((0, "Z"),): (-0.25 + 0j), ((1, "Z"),): (0.25 + 0j)},
+        ),
+        (
+            np.array([[0.0, 0.0, 0.2], [1.0, 1.0, -0.5], [1.0, 0.0, 0.0]]),
+            1 / 2,
+            "bravyi_KITAEV",
+            {
+                (): (0.35 + 0j),
+                ((0, "Z"),): (-0.1 + 0j),
+                ((0, "Z"), (1, "Z")): (0.25 + 0j),
+            },
+        ),
+        (
+            np.array(
+                [
+                    [0.0, 0.0, 0.0, 0.0, 0.25],
+                    [0.0, 1.0, 1.0, 0.0, -0.25],
+                    [0.0, 2.0, 2.0, 0.0, 0.25],
+                    [0.0, 3.0, 3.0, 0.0, -0.25],
+                    [1.0, 0.0, 0.0, 1.0, -0.25],
+                    [2.0, 0.0, 0.0, 2.0, 0.25],
+                ]
+            ),
+            1 / 2,
+            "JORDAN_wigner",
+            {
+                (): (0.4375 + 0j),
+                ((1, "Z"),): (0.125 + 0j),
+                ((0, "Z"), (1, "Z")): (-0.125 + 0j),
+                ((2, "Z"),): (-0.125 + 0j),
+                ((0, "Z"), (2, "Z")): (0.125 + 0j),
+                ((0, "Z"),): (0.0625 + 0j),
+                ((3, "Z"),): (0.0625 + 0j),
+                ((0, "Z"), (3, "Z")): (-0.0625 + 0j),
+            },
+        ),
+        (
+            np.array(
+                [
+                    [0.0, 0.0, 0.0, 0.0, 0.5],
+                    [0.0, 1.0, 1.0, 0.0, -0.25],
+                    [0.0, 2.0, 2.0, 0.0, 0.5],
+                    [0.0, 3.0, 3.0, 0.0, -0.25],
+                    [1.0, 0.0, 0.0, 1.0, -0.25],
+                    [2.0, 0.0, 0.0, 2.0, -0.25],
+                ]
+            ),
+            1 / 4,
+            "bravyi_KITAEV",
+            {
+                (): (0.125 + 0j),
+                ((0, "Z"), (1, "Z")): (0.125 + 0j),
+                ((1, "Z"),): (-0.125 + 0j),
+                ((2, "Z"),): (-0.0625 + 0j),
+                ((0, "Z"), (2, "Z")): (0.0625 + 0j),
+                ((1, "Z"), (2, "Z"), (3, "Z")): (0.0625 + 0j),
+                ((0, "Z"), (1, "Z"), (2, "Z"), (3, "Z")): (-0.0625 + 0j),
+                ((0, "Z"),): (0.125 + 0j),
+            },
+        ),
     ],
 )
 def test_observable(me_table, init_term, mapping, terms_exp, monkeypatch):
@@ -68,17 +99,17 @@ def test_observable(me_table, init_term, mapping, terms_exp, monkeypatch):
     assert qchem._qubit_operators_equivalent(qubit_op, res_obs)
 
 
-@pytest.mark.parametrize("me_table",
+@pytest.mark.parametrize(
+    "me_table",
     [
-        np.array([[0., 0., 1., 0.5], [1., 1., -0.5]]),
-        np.array([[0., 0., 1., 0.5], [1., -0.5]]),
-        np.array([[0., 0., 1., 2., 0.5], [1., -0.5]]),
-        np.array([[0., 0., 1., 2., 3., 0.5], [1., 0., -0.5]]),
+        np.array([[0.0, 0.0, 1.0, 0.5], [1.0, 1.0, -0.5]]),
+        np.array([[0.0, 0.0, 1.0, 0.5], [1.0, -0.5]]),
+        np.array([[0.0, 0.0, 1.0, 2.0, 0.5], [1.0, -0.5]]),
+        np.array([[0.0, 0.0, 1.0, 2.0, 3.0, 0.5], [1.0, 0.0, -0.5]]),
     ],
 )
 def test_exceptions_observable(
-    me_table,
-    message_match="expected entries of 'me_table' to be of shape"
+    me_table, message_match="expected entries of 'me_table' to be of shape"
 ):
     """Test that the 'observable' function throws an exception if the
     array containing the matrix elements has illegal shapes."""
@@ -91,7 +122,7 @@ def test_mapping_observable(message_match="transformation is not available"):
     """Test that the 'observable' function throws an exception if the
     fermionic-to-qubit mapping is not properly defined."""
 
-    me_table = np.array([[0., 0., 0.5], [1., 1., -0.5]])
+    me_table = np.array([[0.0, 0.0, 0.5], [1.0, 1.0, -0.5]])
 
     with pytest.raises(TypeError, match=message_match):
         qchem.obs.observable(me_table, mapping="no_valid_transformation")
