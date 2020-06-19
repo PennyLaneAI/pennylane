fda1da72470d5bb55d70efbfeac6baf4dca24cfd
agran2018 <nanoalain@gmail.com>
2020-06-19 12:36:39 -0400

    Add function to test the built Sz observable

--- qchem/tests/test_sz_obs.py
+++ qchem/tests/test_sz_obs.py
@@ -43,3 +43,49 @@ def test_get_spinZ_matrix_elements(
     )
 
     assert np.allclose(sz_me_res, sz_me_exp, **tol)
+
+
+terms_lih_jw = {
+    ((0, 'Z'),): (-0.25+0j),
+    ((1, 'Z'),): (0.25+0j),
+    ((2, 'Z'),): (-0.25+0j),
+    ((3, 'Z'),): (0.25+0j),
+}
+
+terms_lih_anion_bk = {
+    ((0, 'Z'),): (-0.25+0j),
+    ((0, 'Z'), (1, 'Z')): (0.25+0j),
+    ((2, 'Z'),): (-0.25+0j),
+    ((1, 'Z'), (2, 'Z'), (3, 'Z')): (0.25+0j),
+    ((4, 'Z'),): (-0.25+0j),
+    ((4, 'Z'), (5, 'Z')): (0.25+0j),
+}
+
+
+@pytest.mark.parametrize(
+    ("mol_name", "n_act_elect", "n_act_orb", "mapping", "terms_exp"),
+    [
+        ("lih", 2, 2, "JORDAN_wigner", terms_lih_jw),
+        ("lih_anion", 3, 3, "bravyi_KITAEV", terms_lih_anion_bk),
+    ],
+)
+def test_build_sz_observable(
+    mol_name, n_act_elect, n_act_orb, mapping, terms_exp, monkeypatch
+):
+    r"""Tests the correctness of the built total-spin projection observable :math:`\hat{S}_z`.
+
+    The parametrized inputs are `.terms` attribute of the total spin `QubitOperator.
+    The equality checking is implemented in the `qchem` module itself as it could be
+    something useful to the users as well.
+    """
+
+    sz_me_table = qchem.get_spinZ_matrix_elements(
+        mol_name, ref_dir, n_active_electrons=n_act_elect, n_active_orbitals=n_act_orb
+    )
+
+    sz_obs = qchem.observable(sz_me_table, mapping=mapping)
+
+    sz_qubit_op = QubitOperator()
+    monkeypatch.setattr(sz_qubit_op, "terms", terms_exp)
+
+    assert qchem._qubit_operators_equivalent(sz_qubit_op, sz_obs)
