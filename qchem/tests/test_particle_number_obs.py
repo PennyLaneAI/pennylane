b1a2109bba9b336afb94ce2901ddd1c68c1c2c81
agran2018 <nanoalain@gmail.com>
2020-06-21 18:39:51 -0400

    Add unit test 'test_build_particle_number_observable'

--- qchem/tests/test_particle_number_obs.py
+++ qchem/tests/test_particle_number_obs.py
@@ -69,3 +69,70 @@ def test_get_particle_number_table(
 
     assert np.allclose(pn_table_res, pn_table_exp, **tol)
     assert pn_docc_res == pn_docc_exp
+
+
+terms_h20_jw = {
+    (): (7+0j),
+    ((0, 'Z'),): (-0.5+0j),
+    ((1, 'Z'),): (-0.5+0j),
+    ((2, 'Z'),): (-0.5+0j),
+    ((3, 'Z'),): (-0.5+0j),
+    ((4, 'Z'),): (-0.5+0j),
+    ((5, 'Z'),): (-0.5+0j), 
+    ((6, 'Z'),): (-0.5+0j),
+    ((7, 'Z'),): (-0.5+0j),
+    ((8, 'Z'),): (-0.5+0j),
+    ((9, 'Z'),): (-0.5+0j),
+    ((10, 'Z'),): (-0.5+0j),
+    ((11, 'Z'),): (-0.5+0j),
+    ((12, 'Z'),): (-0.5+0j),
+    ((13, 'Z'),): (-0.5+0j)
+}
+
+terms_lih_anion_bk = {
+    (): (7+0j),
+    ((0, 'Z'),): (-0.5+0j),
+    ((0, 'Z'), (1, 'Z')): (-0.5+0j),
+    ((2, 'Z'),): (-0.5+0j),
+    ((1, 'Z'), (2, 'Z'), (3, 'Z')): (-0.5+0j),
+    ((4, 'Z'),): (-0.5+0j),
+    ((4, 'Z'), (5, 'Z')): (-0.5+0j),
+    ((6, 'Z'),): (-0.5+0j),
+    ((3, 'Z'), (5, 'Z'), (6, 'Z'), (7, 'Z')): (-0.5+0j),
+    ((8, 'Z'),): (-0.5+0j),
+    ((8, 'Z'), (9, 'Z')): (-0.5+0j)
+}
+
+
+@pytest.mark.parametrize(
+    ("mol_name", "n_act_elect", "n_act_orb", "mapping", "terms_exp"),
+    [
+        ("h2o_psi4" , None, None, "JORDAN_wigner", terms_h20_jw),
+        ("lih_anion", 3   , None, "bravyi_KITAEV", terms_lih_anion_bk),
+    ],
+)
+def test_build_particle_number_observable(
+    mol_name,
+    n_act_elect,
+    n_act_orb,
+    mapping,
+    terms_exp,
+    monkeypatch
+):
+    r"""Tests the correctness of the built particle number observable :math:`\hat{N}`.
+
+    The parametrized inputs are `.terms` attribute of the particle number `QubitOperator.
+    The equality checking is implemented in the `qchem` module itself as it could be
+    something useful to the users as well.
+    """
+
+    pn_table, pn_docc = qchem.get_particle_number_table(
+        mol_name, ref_dir, n_active_electrons=n_act_elect, n_active_orbitals=n_act_orb
+    )
+
+    particle_number_obs = qchem.observable(pn_table, init_term=pn_docc, mapping=mapping)
+
+    particle_number_qubit_op = QubitOperator()
+    monkeypatch.setattr(particle_number_qubit_op, "terms", terms_exp)
+
+    assert qchem._qubit_operators_equivalent(particle_number_qubit_op, particle_number_obs)    
