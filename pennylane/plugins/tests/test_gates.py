5957deb72405bfdd3c968fc5bbbdac9afb2d8a20
Maria Schuld <mariaschuld@gmail.com>
2020-06-29 06:51:30 +0200

    fix imports

--- pennylane/plugins/tests/test_gates.py
+++ pennylane/plugins/tests/test_gates.py
@@ -62,7 +62,7 @@ cry = lambda theta: np.array(
     [
         [1, 0, 0, 0],
         [0, 1, 0, 0],
-        [0, 0, cos(theta / 2), -math.sin(theta / 2)],
+        [0, 0, cos(theta / 2), -sin(theta / 2)],
         [0, 0, sin(theta / 2), cos(theta / 2)],
     ]
 )
