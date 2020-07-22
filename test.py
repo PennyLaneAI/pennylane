a0ae21728e49402c13ab31f6f7c0bb5c3d2c3406
Jack Ceroni <jackceroni@gmail.com>
2020-07-22 13:21:10 -0400

    edit

--- test.py
+++
@@ -1,5 +0,0 @@
-import pennylane as qml
-
-hamiltonian = qml.Hamiltonian([1, 1, 1, -1], [qml.PauliZ(0), qml.PauliZ(0) @ qml.PauliZ(1), qml.PauliX(0) @ qml.PauliX(1), qml.PauliX(0) @ qml.PauliX(1)])
-print(hamiltonian.is_diagonal)
-
