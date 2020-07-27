5e25f208e1b5fb47e41d86d9fe2d71e0fc59ae21
Jack Ceroni <jackceroni@gmail.com>
2020-07-27 13:39:53 -0400

    edit

--- test.py
+++
@@ -1,7 +0,0 @@
-import pennylane as qml
-import numpy as np
-
-H = qml.Hamiltonian([2, 1], [qml.Hermitian(np.array([[0, 1], [1, 0]]), 0) @ qml.Hermitian(np.array([[0, 1], [1, 0]]), 1), qml.PauliX(1)])
-#H = qml.Hamiltonian([1, 1], [qml.PauliX(0), qml.PauliX(1)])
-
-print(H.decompose())
