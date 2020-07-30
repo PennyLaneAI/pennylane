485677c5aa9186ddd8b5fca96c952b6c9ba7f4b1
Jack Ceroni <jackceroni@gmail.com>
2020-07-30 15:19:19 -0400

    [QAOA] Adds Built-In Mixer Hamiltonians (#712)
    * qaoa
    
    * mixer
    
    * stuff
    
    * changes
    
    * qaoa
    
    * qaoa
    
    * qaoa
    
    * black and pylint
    
    * changes
    
    * edit
    
    * qaoa
    
    * qaoa changes
    
    * edits
    
    * black
    
    * small changes
    
    * changes
    
    * change
    
    * Apply suggestions from code review
    
    Co-authored-by: ixfoduap <40441298+ixfoduap@users.noreply.github.com>
    
    * qaoa
    
    * qaoa
    
    * edit
    
    * changes
    
    * Update pennylane/qaoa/qaoa.py
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * Update pennylane/qaoa/qaoa.py
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * yes
    
    * yes
    
    * edit
    
    * changes
    
    * edit
    
    * small edit
    
    * changes
    
    * edit
    
    * edit
    
    * edit
    
    * changes
    
    * wires
    
    * Update pennylane/qaoa/mixers.py
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * Update pennylane/qaoa/mixers.py
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * edit
    
    * changes
    
    * edit
    
    * edit
    
    * re-run tests
    
    * small edit
    
    Co-authored-by: ixfoduap <40441298+ixfoduap@users.noreply.github.com>
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    Co-authored-by: Josh Izaac <josh146@gmail.com>

---
+++ tests/test_qaoa.py
@@ -0,0 +1,136 @@
+# Copyright 2018-2020 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Unit tests for the :mod:`pennylane.qaoa` submodule.
+"""
+import pytest
+import numpy as np
+import pennylane as qml
+from pennylane import qaoa
+from networkx import Graph
+
+
+#####################################################
+
+graph = Graph()
+graph.add_nodes_from([0, 1, 2])
+graph.add_edges_from([(0, 1), (1, 2)])
+
+non_consecutive_graph = Graph([(0, 4), (3, 4), (2, 1), (2, 0)])
+
+
+class TestMixerHamiltonians:
+    """Tests that the mixer Hamiltonians are being generated correctly"""
+
+    def test_x_mixer_output(self):
+        """Tests that the output of the Pauli-X mixer is correct"""
+
+        wires = range(4)
+        mixer_hamiltonian = qaoa.x_mixer(wires)
+
+        mixer_coeffs = mixer_hamiltonian.coeffs
+        mixer_ops = [i.name for i in mixer_hamiltonian.ops]
+        mixer_wires = [i.wires[0] for i in mixer_hamiltonian.ops]
+
+        assert mixer_coeffs == [1, 1, 1, 1]
+        assert mixer_ops == ["PauliX", "PauliX", "PauliX", "PauliX"]
+        assert mixer_wires == [0, 1, 2, 3]
+
+    def test_xy_mixer_type_error(self):
+        """Tests that the XY mixer throws the correct error"""
+
+        graph = [(0, 1), (1, 2)]
+
+        with pytest.raises(ValueError, match=r"Input graph must be a nx.Graph object, got list"):
+            output = qaoa.xy_mixer(graph)
+
+    @pytest.mark.parametrize(
+        ("graph", "target_hamiltonian"),
+        [
+            (
+                Graph([(0, 1), (1, 2), (2, 3)]),
+                qml.Hamiltonian(
+                    [0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+                    [
+                        qml.PauliX(0) @ qml.PauliX(1),
+                        qml.PauliY(0) @ qml.PauliY(1),
+                        qml.PauliX(1) @ qml.PauliX(2),
+                        qml.PauliY(1) @ qml.PauliY(2),
+                        qml.PauliX(2) @ qml.PauliX(3),
+                        qml.PauliY(2) @ qml.PauliY(3),
+                    ],
+                ),
+            ),
+            (
+                Graph((np.array([0, 1]), np.array([1, 2]), np.array([2, 0]))),
+                qml.Hamiltonian(
+                    [0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+                    [
+                        qml.PauliX(0) @ qml.PauliX(1),
+                        qml.PauliY(0) @ qml.PauliY(1),
+                        qml.PauliX(0) @ qml.PauliX(2),
+                        qml.PauliY(0) @ qml.PauliY(2),
+                        qml.PauliX(1) @ qml.PauliX(2),
+                        qml.PauliY(1) @ qml.PauliY(2),
+                    ],
+                ),
+            ),
+            (
+                graph,
+                qml.Hamiltonian(
+                    [0.5, 0.5, 0.5, 0.5],
+                    [
+                        qml.PauliX(0) @ qml.PauliX(1),
+                        qml.PauliY(0) @ qml.PauliY(1),
+                        qml.PauliX(1) @ qml.PauliX(2),
+                        qml.PauliY(1) @ qml.PauliY(2),
+                    ],
+                ),
+            ),
+            (
+                non_consecutive_graph,
+                qml.Hamiltonian(
+                    [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+                    [
+                        qml.PauliX(0) @ qml.PauliX(4),
+                        qml.PauliY(0) @ qml.PauliY(4),
+                        qml.PauliX(0) @ qml.PauliX(2),
+                        qml.PauliY(0) @ qml.PauliY(2),
+                        qml.PauliX(4) @ qml.PauliX(3),
+                        qml.PauliY(4) @ qml.PauliY(3),
+                        qml.PauliX(2) @ qml.PauliX(1),
+                        qml.PauliY(2) @ qml.PauliY(1),
+                    ],
+                ),
+            ),
+        ],
+    )
+    def test_xy_mixer_output(self, graph, target_hamiltonian):
+        """Tests that the output of the XY mixer is correct"""
+
+        print(graph.edges)
+
+        mixer_hamiltonian = qaoa.xy_mixer(graph)
+
+        mixer_coeffs = mixer_hamiltonian.coeffs
+        mixer_ops = [i.name for i in mixer_hamiltonian.ops]
+        mixer_wires = [i.wires for i in mixer_hamiltonian.ops]
+
+        target_coeffs = target_hamiltonian.coeffs
+        target_ops = [i.name for i in target_hamiltonian.ops]
+        target_wires = [i.wires for i in target_hamiltonian.ops]
+
+        assert mixer_coeffs == target_coeffs
+        assert mixer_ops == target_ops
+        assert mixer_wires == target_wires
