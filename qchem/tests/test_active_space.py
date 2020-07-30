574d35ed42440f0e7f2ab6d263df9534d35f2569
agran2018 <nanoalain@gmail.com>
2020-07-30 18:02:56 -0400

    Polish the refactored function 'active_space'

--- qchem/tests/test_active_space.py
+++ qchem/tests/test_active_space.py
@@ -4,11 +4,13 @@ import pytest
 
 from pennylane import qchem
 
+from openfermion.hamiltonians import MolecularData
+
 ref_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_ref_files")
 
 
 @pytest.mark.parametrize(
-    ("mol_name", "n_act_electrons", "n_act_orbitals", "docc_indices_ref", "act_indices_ref"),
+    ("mol_name", "n_act_electrons", "n_act_orbitals", "core_ref", "active_ref"),
     [
         ("lih", None, None, [], list(range(6))),
         ("lih", 4, None, [], list(range(6))),
@@ -20,17 +22,27 @@ ref_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_ref_fi
     ],
 )
 def test_active_spaces(
-    mol_name, n_act_electrons, n_act_orbitals, docc_indices_ref, act_indices_ref
+    mol_name, n_act_electrons, n_act_orbitals, core_ref, active_ref
 ):
 
     r"""Test the correctness of the generated active spaces"""
 
-    docc_indices, active_indices = qchem.active_space(
-        mol_name, ref_dir, n_act_electrons, n_act_orbitals
+    molecule = MolecularData(filename=os.path.join(ref_dir, mol_name))
+
+    core, active = qchem.active_space(
+        molecule.n_electrons,
+        molecule.n_orbitals,
+        mult=molecule.multiplicity,
+        nact_els=n_act_electrons,
+        nact_orbs=n_act_orbitals
     )
 
-    assert docc_indices == docc_indices_ref
-    assert active_indices == act_indices_ref
+    # docc_indices, active_indices = qchem.active_space(
+    #     mol_name, ref_dir, n_act_electrons, n_act_orbitals
+    # )
+
+    assert core == core_ref
+    assert active == active_ref
 
 
 @pytest.mark.parametrize(
@@ -39,11 +51,11 @@ def test_active_spaces(
         ("lih", 6, 5, "greater than the total number of electrons"),
         ("lih", 1, 5, "should be even"),
         ("lih", -1, 5, "has to be greater than 0."),
-        ("lih", 2, 6, "can not be greater than the number " "of molecular orbitals"),
+        ("lih", 2, 6, "greater than the total number of orbitals"),
         ("lih", 2, 1, "there are no virtual orbitals"),
         ("lih_anion", 2, 5, "should be odd"),
         ("lih_anion", 3, -2, "has to be greater than 0."),
-        ("lih_anion", 3, 6, "can not be greater than the number " "of molecular orbitals"),
+        ("lih_anion", 3, 6, "greater than the total number of orbitals"),
         ("lih_anion", 3, 2, "there are no virtual orbitals"),
         ("lih_anion_2", 1, 2, "greater than or equal to"),
     ],
@@ -52,5 +64,12 @@ def test_inconsistent_active_spaces(mol_name, n_act_electrons, n_act_orbitals, m
 
     r"""Test that an error is raised if an inconsistent active space is generated"""
 
+    molecule = MolecularData(filename=os.path.join(ref_dir, mol_name))
+
+    # with pytest.raises(ValueError, match=message_match):
+    #     qchem.active_space(mol_name, ref_dir, n_act_electrons, n_act_orbitals)
+
     with pytest.raises(ValueError, match=message_match):
-        qchem.active_space(mol_name, ref_dir, n_act_electrons, n_act_orbitals)
+        qchem.active_space(molecule.n_electrons, molecule.n_orbitals, mult=molecule.multiplicity,
+            nact_els=n_act_electrons, nact_orbs=n_act_orbitals
+    )
