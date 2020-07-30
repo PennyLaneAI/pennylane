3f9a401dc8610ac246255960c6fb9189400c1ca2
Jack Ceroni <jackceroni@gmail.com>
2020-07-30 15:00:36 -0400

    small edit

--- pennylane/qaoa/__init__.py
+++ pennylane/qaoa/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 r"""
-This module contains functionality to construct QAOA workflows in PennyLane
+This module contains functionality to construct QAOA workflows in PennyLane.
 """
 
 from .mixers import *
