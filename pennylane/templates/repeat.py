ef8af38f6579df5a0709cdd8c7570f24af9567d5
Jack Ceroni <jackceroni@gmail.com>
2020-08-04 16:32:10 -0400

    changes

--- pennylane/templates/repeat.py
+++ pennylane/templates/repeat.py
@@ -15,6 +15,9 @@ r"""
 Contains the ``repeat`` template constructor.
 """
 # pylint: disable-msg=too-many-branches,too-many-arguments,protected-access
+import inspect
+import types
+
 from collections.abc import Iterable
 
 from pennylane.templates.decorator import template
@@ -30,29 +33,21 @@ def repeat(unitary, wires, depth, parameters=None, kwargs=None):
     r"""Repeatedly applies a series of quantum gates or templates.
 
     Args:
-        unitary (function):
-        wires (list): The wires on which each gate/template act
-        depth (int): The number of times the unitaries are repeated
-        parameters (list): A list of parameters that are passed into parametrized elements of ``unitaries``.
-            This argument has a shape (depth, N,), where N is the number of parametrized elements in ``unitaries``
+        unitary (function): A function that applies the quantum gates/templates being repeated.
+        This function must have a signature of the form ``(parameters, wires, **kwargs)`` or ``(wires, **kwargs)``.
+        wires (list): The wires on which ``unitary`` acts
+        depth (int): The number of times the unitaries are repeatedly applied
+        parameters (list): A list of parameters that are passed into ``unitary``
         kwargs (dict): A dictionary of auxilliary parameters for ``unitaries``
 
     Raises:
         ValueError: if inputs do not have the correct format
 
-    .. note::
-
-            Any template passed into ``qml.repeat`` must have a signature of the form:
-
-            .. code-block:: none
-
-                    template(params, wires, **kwargs)
-
     .. UsageDetails::
 
         **Repeating Gates**
 
-        To repeat a collection of gates, the type of gate as well as the wire(s) on which
+        To repeat a collection of gates, a function applying each of the gates, as well as the wire(s) on which
         it acts must be specified. For example:
 
         .. code-block:: python
@@ -61,9 +56,14 @@ def repeat(unitary, wires, depth, parameters=None, kwargs=None):
 
             dev = qml.device('default.qubit', wires=2)
 
+            def unitary(wires, **kwargs):
+                qml.Hadamard(wires=wires[0])
+                qml.CNOT(wires=wires)
+                qml.PauliX(wires=wires[1])
+
             @qml.qnode(dev)
             def circuit():
-                qml.repeat([qml.Hadamard, qml.CNOT, qml.PauliX], [0, [0, 1], 0], 3)
+                qml.repeat(unitary, [0, 1], 3)
                 return [qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1))]
 
             circuit()
@@ -75,14 +75,12 @@ def repeat(unitary, wires, depth, parameters=None, kwargs=None):
              0: ──H──╭C──X──H──╭C──X──H──╭C──X──┤ ⟨Z⟩
              1: ─────╰X────────╰X────────╰X─────┤ ⟨Z⟩
 
-        **Parametrized Gates and Templates**
+        **Parameters and Keyword Arguments**
 
-        The ``qml.repeat`` function can accept and pass varaitional parameters
-        to gates as well as PennyLane templates, with the ``parameters`` argument.
-        The first and second dimensions of ``parameters``, respectively,
-        are the number of repetations and the number of parametrized templates/operations.
-        After this, each element is a list of parameters passed into the corresponding
-        parametrized template/operation.
+        The ``qml.repeat`` function can accept and pass varaitional parameters and keyword arguments
+        to ``unitary``, with the ``parameters`` argument.
+        The :math:`i`-th element of ``parameters`` is the list of parameters passed into the template
+        at the :math:`i`-th layer.
 
         For example:
 
@@ -92,15 +90,19 @@ def repeat(unitary, wires, depth, parameters=None, kwargs=None):
 
             dev = qml.device("default.qubit", wires=3)
 
+            def unitary(parameters, wires, **kwargs):
+                qml.templates.AngleEmbedding(parameters[0], wires)
+                qml.RY(parameters[1], wires=wires[0])
+
+                if kwargs['var'] == True:
+                    qml.Hadamard(wires=wires[1])
+
             @qml.qnode(dev)
             def circuit():
                 qml.repeat(
-                    [qml.templates.AngleEmbedding, qml.RY, qml.Hadamard],
-                    [[0, 1, 2], 0, 1], 2,
-                    parameters=[
-                        [ [0.5, 0.5, 0.5], 0.3 ],
-                        [ [0.5, 0.5, 0.5], 0.3 ]
-                    ]
+                    unitary, [0, 1, 2], 2,
+                    [[0.5, 0.5, 0.5], 0.3], [[0.5, 0.5, 0.5], 0.3]],
+                    {'var' : True}
                 )
                 return [qml.expval(qml.PauliZ(i)) for i in range(3)]
 
@@ -121,11 +123,6 @@ def repeat(unitary, wires, depth, parameters=None, kwargs=None):
 
     wires = [Wires(w) for w in wires]
 
-    if not isinstance(unitaries, list):
-        raise ValueError(
-            "'unitaries' must be of type list, got {}".format(type(unitaries).__name__)
-        )
-
     if not isinstance(depth, int):
         raise ValueError("'depth' must be of type int, got {}".format(type(depth).__name__))
 
@@ -137,57 +134,45 @@ def repeat(unitary, wires, depth, parameters=None, kwargs=None):
     )
 
     if kwargs is None:
-        kwargs = {}
+        kwargs = [{} for i in range(0, depth)]
 
-    check_type(
-        kwargs, [dict], msg="'kwargs' must be a dictionary; got {}".format(type(kwargs)),
-    )
+    if not isinstance(kwargs, list):
+        raise ValueError("'kwargs' must be a list; got {}".format(type(kwargs)))
 
-    if len(wires) != len(unitaries):
-        raise ValueError(
-            "Expected wires to be length {}, got length {}".format(len(unitaries), len(wires))
+    if len(kwargs) != depth:
+        raise ValueError("Expected length of 'kwargs' to be {}, got {}".format(depth, len(kwargs)))
+
+    for i in kwargs:
+        check_type(
+            i, [dict], msg="Elements of 'kwargs' must be dictionaries; got {}".format(type(i)),
         )
 
-    # Checks if dimensions of parameters are correct
+    check_type(
+        unitary, [types.FunctionType], msg="'unitary' must be a function; got {}".format(type(unitary)),
+    )
+
+    if len(inspect.signature(unitary).parameters) not in [2, 3]:
+        raise ValueError("Signature of 'unitary' must be of the form (parameters, wires, **kwargs)")
+
+    if parameters is None and inspect.signature(unitary) == 3:
+        raise ValueError("Expected 'parameters', got None")
+
+    if parameters is not None and inspect.signature(unitary) == 2:
+        raise ValueError("Got 'parameters' with no 'parameters' argument in 'unitary'")
+
+
+    ##############
+
 
     if parameters is not None:
         shape = get_shape(parameters)
 
-        # Gets the expected dimensions of the parameter list
-        s = 0
-        for u in unitaries:
-            if type(u).__name__ == "function":
-                s += 1
-            elif u.num_params > 0:
-                s += 1
+        if int(shape[0]) != depth:
+            raise ValueError("Expected first dimension of 'parameters' to be {}; got {}".format(depth, int(shape[0])))
 
-        if shape[0] != depth or shape[1] != s:
-            raise ValueError(
-                "Shape of parameters must be {} got {}".format((depth, s), (shape[0], shape[1]))
-            )
+        for i, param in enumerate(parameters):
+            unitary(param, wires, **kwargs[i])
 
-        # Checks if elements are lists
-
-        for i in parameters:
-            for j in i:
-                if not isinstance(j, list):
-                    raise ValueError(
-                        "Elements of 'parameters[i]' must be of type list, got {}".format(
-                            type(j).__name__
-                        )
-                    )
-
-    ##############
-
-    wires = [w.tolist() for w in wires]  # TODO: Delete once operator takes Wires objects
-    for d in range(0, depth):
-        c = 0
-        for i, u in enumerate(unitaries):
-            if type(u).__name__ == "function":
-                u(*parameters[d][c], wires=wires[i], **kwargs)
-                c += 1
-            elif u.num_params > 0:
-                u(*parameters[d][c], wires=wires[i], **kwargs)
-                c += 1
-            else:
-                u(wires=wires[i], **kwargs)
+    else:
+        for i in range(0, depth):
+            unitary(wires, **kwargs[i])
