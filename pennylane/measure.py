aa70b9957a90165cd01811f9b4b4785890ca2b1a
trbromley <brotho02@gmail.com>
2020-08-05 08:28:00 -0400

    Tidy up import

--- pennylane/measure.py
+++ pennylane/measure.py
@@ -18,7 +18,8 @@ outcomes from quantum observables - expectation values, variances of expectation
 and measurement samples.
 """
 import pennylane as qml
-from pennylane.operation import Observable, Sample, Variance, Expectation, Probability, Tensor
+from pennylane.operation import (AllWires, Expectation, Observable,
+                                 Probability, Sample, State, Tensor, Variance)
 from pennylane.qnodes import QuantumFunctionError
 
 
@@ -199,9 +200,6 @@ def probs(wires):
     qml.QueuingContext.append(op)
 
     return op
-
-
-from pennylane.operation import AllWires, State
 
 
 def state():
