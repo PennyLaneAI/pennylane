eb98d84902a67dcd25c82f0867512231a078d256
trbromley <brotho02@gmail.com>
2020-08-07 16:36:30 -0400

    Update test

--- tests/circuit_drawer/test_representation_resolver.py
+++ tests/circuit_drawer/test_representation_resolver.py
@@ -696,6 +696,7 @@ class TestRepresentationResolver:
                 "Sample[H0 @ H1]",
             ),
             (qml.probs([0]), 0, "Probs"),
+            (qml.state(), 0, "State"),
         ],
     )
     def test_output_representation_ascii(self, ascii_representation_resolver, obs, wire, target):
