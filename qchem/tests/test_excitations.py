79a0f1597f2b49736d613fcd4972714ea103058c
agran2018 <nanoalain@gmail.com>
2020-08-10 17:35:46 -0400

    Add suggestions from code review

--- qchem/tests/test_excitations.py
+++ qchem/tests/test_excitations.py
@@ -6,15 +6,7 @@ from pennylane import qchem
 
 
 @pytest.mark.parametrize(
-    (
-        "n_electrons",
-        "n_spin_orbitals",
-        "delta_sz",
-        "n_singles",
-        "n_doubles",
-        "singles_exp",
-        "doubles_exp",
-    ),
+    ("electrons", "orbitals", "delta_sz", "n_singles", "n_doubles", "singles_exp", "doubles_exp",),
     [
         (1, 5, 0, 2, 0, [[0, 2], [0, 4]], []),
         (1, 5, 1, 0, 0, [], []),
@@ -40,12 +32,10 @@ from pennylane import qchem
         (3, 4, 2, 0, 0, [], []),
     ],
 )
-def test_excitations(
-    n_electrons, n_spin_orbitals, delta_sz, n_singles, n_doubles, singles_exp, doubles_exp
-):
+def test_excitations(electrons, orbitals, delta_sz, n_singles, n_doubles, singles_exp, doubles_exp):
     r"""Test the correctness of the generated configurations"""
 
-    singles, doubles = qchem.excitations(n_electrons, n_spin_orbitals, delta_sz)
+    singles, doubles = qchem.excitations(electrons, orbitals, delta_sz)
 
     assert len(singles) == len(singles_exp)
     assert len(doubles) == len(doubles_exp)
@@ -54,7 +44,7 @@ def test_excitations(
 
 
 @pytest.mark.parametrize(
-    ("n_electrons", "n_spin_orbitals", "delta_sz", "message_match"),
+    ("electrons", "orbitals", "delta_sz", "message_match"),
     [
         (0, 4, 0, "number of active electrons has to be greater than 0"),
         (3, 2, 0, "has to be greater than the number of active electrons"),
@@ -62,8 +52,8 @@ def test_excitations(
         (2, 4, 1.5, "Expected values for 'delta_sz'"),
     ],
 )
-def test_inconsistent_excitations(n_electrons, n_spin_orbitals, delta_sz, message_match):
+def test_inconsistent_excitations(electrons, orbitals, delta_sz, message_match):
     r"""Test that an error is raised if a set of inconsistent arguments is input"""
 
     with pytest.raises(ValueError, match=message_match):
-        qchem.excitations(n_electrons, n_spin_orbitals, delta_sz)
+        qchem.excitations(electrons, orbitals, delta_sz)
