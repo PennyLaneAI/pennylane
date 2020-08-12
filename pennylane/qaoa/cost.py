8758e554e4d8105b3e89ee669c68c99899272fa6
Jack Ceroni <jackceroni@gmail.com>
2020-08-12 09:49:54 -0400

    changes

--- pennylane/qaoa/cost.py
+++ pennylane/qaoa/cost.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 r"""
-Methods for generating QAOA cost and mixer Hamiltonians corresponding to
+Methods for generating QAOA cost Hamiltonians corresponding to
 different optimization problems.
 """
 
