aba06fe3c5c3d0dc120ac9d31d80d994aa57562e
Maria Schuld <mariaschuld@gmail.com>
2020-08-27 16:14:40 +0200

    add test for reversible diff

--- pennylane/devices/tests/test_properties.py
+++ pennylane/devices/tests/test_properties.py
@@ -14,6 +14,7 @@
 """Tests that a device has the right attributes, arguments and methods."""
 # pylint: disable=no-self-use
 import pytest
+import numpy as np
 import pennylane as qml
 from pennylane._device import DeviceError
 
@@ -122,19 +123,19 @@ class TestCapabilities:
             assert hasattr(circuit, "jacobian")
 
     def test_reversible_diff(self, device_kwargs):
-        """Test that the device computes the jacobian."""
+        """Test that the device supports reversible differentiation."""
         device_kwargs["wires"] = 1
         dev = qml.device(**device_kwargs)
         cap = dev.capabilities()
-        provides_jacobian = cap.get("provides_jacobian", False)
+        rev_diff = cap.get("supports_reversible_diff", False)
 
-        if not provides_jacobian:
-            pytest.skip("Device does not provide jacobian.")
+        if not rev_diff:
+            pytest.skip("Device does not support reversible differentiation.")
 
         else:
 
-            @qml.qnode(dev)
+            @qml.qnode(dev, diff_method="reversible")
             def circuit():
                 return qml.expval(qml.Identity(wires=0))
 
-            assert hasattr(circuit, "jacobian")
+            assert isinstance(circuit(), (float, np.array))
