45118150fcbd60e2e220b4f21350239a10d73353
Josh Izaac <josh146@gmail.com>
2020-09-03 23:50:08 +0930

    more

--- pennylane/beta/tapes/qubit_param_shift.py
+++ pennylane/beta/tapes/qubit_param_shift.py
@@ -18,6 +18,7 @@ Provides analytic differentiation for all one-parameter gates where the generato
 only has two unique eigenvalues; this includes one-parameter single-qubit gates.
 """
 # pylint: disable=attribute-defined-outside-init
+import copy
 import numpy as np
 
 import pennylane as qml
@@ -39,10 +40,17 @@ class QubitParamShiftTape(QuantumTape):
 
         self.var_idx = None
         self.var_mask = [m.return_type is qml.operation.Variance for m in self.measurements]
+        self._original_obs = self._obs.copy()
 
         if any(self.var_mask):
             self.analytic_pd = self._parameter_shift_var
             self.var_idx = np.where(self.var_mask)[0]
+
+        # # expand out the tape, if any operations are not supported by the parameter shift rule
+        # if {"CRX", "CRZ", "CRY", "CRot"} & {op.name for op in self.operations}:
+        #     stop = set(device.operations) - {"CRX", "CRZ", "CRY", "CRot"}
+        #     self = self.expand(depth=2, stop_at=stop)
+
 
     def _grad_method(self, idx, use_graph=True):
         op = self._par_info[idx]["op"]
@@ -72,14 +80,9 @@ class QubitParamShiftTape(QuantumTape):
         return "A"
 
     def jacobian(self, device, params=None, **options):
-        if any(self.var_mask):
-            self._evA = None
-            self._original_obs = self._obs.copy()
-
+        self._evA = None
         ret = super().jacobian(device, params, **options)
-
-        if any(self.var_mask):
-            self._evA = None
+        self._evA = None
 
         return ret
 
@@ -99,13 +102,18 @@ class QubitParamShiftTape(QuantumTape):
             array[float]: 1-dimensional array of length determined by the tape output
                 measurement statistics
         """
-        s = options.get("shift", np.pi/2)
+        op = self._par_info[idx[0]]["op"]
+        p_idx = self._par_info[idx[0]]["p_idx"]
+
+        s = np.pi/2 if op.grad_recipe[p_idx] is None else op.grad_recipe[p_idx]
+        s = options.get("shift", s)
 
         shift = np.zeros_like(params)
         shift[idx] = s
 
         shift_forward = np.array(self.execute_device(params + shift, device))
         shift_backward = np.array(self.execute_device(params - shift, device))
+
         return (shift_forward - shift_backward) / (2 * np.sin(s))
 
     def _parameter_shift_var(self, idx, device, params, **options):
@@ -133,15 +141,19 @@ class QubitParamShiftTape(QuantumTape):
         pdA = self._parameter_shift(idx, device, params, **options)
 
         # For involutory observables (A^2 = I) and thus we have d<A^2>/dp = 0
+        # Currently, the only non-involutory observable we have in PL is Hermitian
         involutory = [i for i in self.var_idx if self._obs[i][1].name != "Hermitian"]
 
         # non involutory observables we must compute d<A^2>/dp
         non_involutory = set(self.var_idx) - set(involutory)
+        original = []
 
         for i in non_involutory:
             # need to calculate d<A^2>/dp; replace the involutory observables
             # in the queue with <A^2>.
+            original[:0] = [self._obs[i]]
             obs = self._obs[i][1]
+
             w = obs.wires
             A = obs.matrix
 
@@ -156,10 +168,19 @@ class QubitParamShiftTape(QuantumTape):
         if non_involutory:
             # calculate the analytic derivatives of the <A^2> observables
             pdA2 = self._parameter_shift(idx, device, params, **options)
-            pdA2[np.array(involutory)] = 0
+
+            if involutory:
+                pdA2[np.array(involutory)] = 0
 
         # restore the original observables
         self._obs = self._original_obs
+
+        for i in non_involutory:
+            self._obs[i] = original.pop()
+
+        for i in self.var_idx:
+            for obs in self._obs[i]:
+                obs.return_type = qml.operation.Variance
 
         # return d(var(A))/dp = d<A^2>/dp -2 * <A> * d<A>/dp for the variances,
         # d<A>/dp for plain expectations
