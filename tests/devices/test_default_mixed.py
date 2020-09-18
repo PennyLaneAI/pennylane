b0a2b48250bc365dc30315e97fe6cdffcd95ce7f
ixfoduap <juanmiguel@xanadu.ai>
2020-09-18 17:30:18 -0400

    Adds tests for apply_channel

--- tests/devices/test_default_mixed.py
+++ tests/devices/test_default_mixed.py
@@ -19,32 +19,33 @@ import pytest
 import pennylane as qml
 from pennylane.ops import PauliZ, CZ, PauliX, Hadamard, CNOT, AmplitudeDamping, DepolarizingChannel
 from pennylane.devices.default_mixed import DefaultMixed
+from pennylane.wires import Wires
 import numpy as np
 
 INV_SQRT2 = 1 / np.sqrt(2)
 
 # Operations used for testing
 diags = [PauliZ, CZ]
-gates = [PauliX, Hadamard, CNOT]
 channels = [AmplitudeDamping, DepolarizingChannel]
-
+gates = [PauliX, Hadamard, CNOT]
+non_diags = [PauliX, Hadamard, CNOT, AmplitudeDamping, DepolarizingChannel]
 all_ops = [PauliZ, CZ, PauliX, Hadamard, CNOT, AmplitudeDamping, DepolarizingChannel]
 
 
 # functions for creating different states used in testing
 def basis_state(index, nr_wires):
-    rho = np.zeros((2 ** nr_wires, 2 ** nr_wires))
+    rho = np.zeros((2 ** nr_wires, 2 ** nr_wires), dtype=np.complex128)
     rho[index, index] = 1
     return rho
 
 
 def hadamard_state(nr_wires):
     """Equal superposition state (Hadamard on all qubits)"""
-    return np.ones((2 ** nr_wires, 2 ** nr_wires))/(2 ** nr_wires)
+    return np.ones((2 ** nr_wires, 2 ** nr_wires), dtype=np.complex128)/(2 ** nr_wires)
 
 
 def max_mixed_state(nr_wires):
-    return np.eye(2 ** nr_wires)/(2 ** nr_wires)
+    return np.eye(2 ** nr_wires, dtype=np.complex128)/(2 ** nr_wires)
 
 
 def root_state(nr_wires):
@@ -104,7 +105,7 @@ class TestState:
             # set channel parameter to 0.5
             dev.apply([op(0.5, wires=0)])
         else:
-            dev.apply([op(0)])
+            dev.apply([op(wires=0)])
         current_state = np.reshape(dev._state, (2 ** nr_wires, 2 ** nr_wires))
         assert np.allclose(dev.state, current_state)
 
@@ -221,6 +222,93 @@ class TestKrausOps:
 
 class TestApplyChannel:
     """Unit tests for the method `_apply_channel()`"""
+
+    x_apply_channel_init = [
+        [1, PauliX, basis_state(1, 1)],
+        [2, Hadamard, np.array([[0.5+0.j, 0.5+0.j],
+        [0.5+0.j, 0.5+0.j]])],
+        [2, CNOT, basis_state(0, 2)],
+        [1, AmplitudeDamping(0.5, wires=0),
+         basis_state(0, 1)],
+        [1, DepolarizingChannel(0.5, wires=0),
+         np.array([[2/3 + 0.j, 0. + 0.j],
+                [0. + 0.j, 1/3 + 0.j]])]
+    ]
+
+    @pytest.mark.parametrize("x", x_apply_channel_init)
+    def test_channel_init(self, x):
+        """Tests that channels are correctly applied to the default initial state"""
+        nr_wires = x[0]
+        op = x[1]
+        target_state = np.reshape(x[2], [2] * 2 * nr_wires)
+        dev = qml.device('default.mixed', wires=nr_wires)
+        kraus = dev._get_kraus(op)
+        if op == CNOT:
+            dev._apply_channel(kraus, wires=Wires([0, 1]))
+        else:
+            kraus = dev._get_kraus(op)
+            dev._apply_channel(kraus, wires=Wires(0))
+
+        assert np.allclose(dev._state, target_state)
+
+    x_apply_channel_mixed = [
+        [1, PauliX, max_mixed_state(1)],
+        [2, Hadamard, max_mixed_state(2)],
+        [2, CNOT, max_mixed_state(2)],
+        [1, AmplitudeDamping(0.5, wires=0),
+         np.array([[0.75 + 0.j, 0. + 0.j], [0. + 0.j, 0.25 + 0.j]])],
+        [1, DepolarizingChannel(0.5, wires=0),
+         np.array([[0.5 + 0.j, 0. + 0.j], [0. + 0.j, 0.5 + 0.j]])]
+    ]
+
+    @pytest.mark.parametrize("x", x_apply_channel_mixed)
+    def test_channel_mixed(self, x):
+        """Tests that channels are correctly applied to the maximally mixed state"""
+        nr_wires = x[0]
+        op = x[1]
+        target_state = np.reshape(x[2], [2] * 2 * nr_wires)
+        dev = qml.device('default.mixed', wires=nr_wires)
+        max_mixed = np.reshape(max_mixed_state(nr_wires), [2] * 2 * nr_wires)
+        dev._state = max_mixed
+        kraus = dev._get_kraus(op)
+        if op == CNOT:
+            dev._apply_channel(kraus, wires=Wires([0, 1]))
+        else:
+            kraus = dev._get_kraus(op)
+            dev._apply_channel(kraus, wires=Wires(0))
+
+        assert np.allclose(dev._state, target_state)
+
+    x_apply_channel_root = [
+        [1, PauliX, np.array([[0.5+0.0j, -0.5+0.0j], [-0.5-0.0j,  0.5+0.0j]])],
+        [1, Hadamard, np.array([[0.0+0.0j, 0.0+0.0j], [0.0+0.0j,  1.0+0.0j]])],
+        [2, CNOT, np.array([[0.25+0.0j,  0.0-0.25j, 0.0 + 0.25j, -0.25],
+         [0.0 + 0.25j,  0.25 + 0.0j, -0.25 + 0.0j,  0.0 - 0.25j],
+         [0.0 - 0.25j, - 0.25 + 0.0j, 0.25 + 0.0j,  0.0 + 0.25j],
+         [-0.25 + 0.0j, 0.0 + 0.25j, 0.0 - 0.25j,  0.25 + 0.0j]])],
+        [1, AmplitudeDamping(0.5, wires=0), np.array([[0.75 + 0.0j, -0.35355339-0.0j],
+         [-0.35355339 + 0.0j,  0.25 + 0.0j]])],
+        [1, DepolarizingChannel(0.5, wires=0), np.array([[0.5 + 0.0j, -1 / 6 + 0.0j],
+         [-1 / 6 + 0.0j, 0.5 + 0.0j]])]
+    ]
+
+    @pytest.mark.parametrize("x", x_apply_channel_root)
+    def test_channel_root(self, x):
+        """Tests that channels are correctly applied to root state"""
+        nr_wires = x[0]
+        op = x[1]
+        target_state = np.reshape(x[2], [2] * 2 * nr_wires)
+        dev = qml.device('default.mixed', wires=nr_wires)
+        root = np.reshape(root_state(nr_wires), [2] * 2 * nr_wires)
+        dev._state = root
+        kraus = dev._get_kraus(op)
+        if op == CNOT:
+            dev._apply_channel(kraus, wires=Wires([0, 1]))
+        else:
+            kraus = dev._get_kraus(op)
+            dev._apply_channel(kraus, wires=Wires(0))
+
+        assert np.allclose(dev._state, target_state)
 
 
 class TestApplyDiagonal:
