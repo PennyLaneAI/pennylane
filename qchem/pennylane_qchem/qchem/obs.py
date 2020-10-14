abf52645fa1747dda4dcd01884c454ee838cc73c
agran2018 <nanoalain@gmail.com>
2020-10-14 12:29:07 -0400

    Modify the 'observable' function to build PL observables from a list of FermionOperators

--- qchem/pennylane_qchem/qchem/obs.py
+++ qchem/pennylane_qchem/qchem/obs.py
@@ -211,19 +211,17 @@ def spin2(electrons, orbitals, mapping="jordan_wigner", wires=None):
     return observable([table], init_term=3 / 4 * electrons, mapping=mapping, wires=wires)
 
 
-def observable(matrix_elements, init_term=0, mapping="jordan_wigner", wires=None):
+def observable(fermion_ops, init_term=0, mapping="jordan_wigner", wires=None):
 
-    r"""Builds the many-body observable whose expectation value can be
+    r"""Builds the Fermion many-body observable whose expectation value can be
     measured in PennyLane.
 
-    This function can be used to build second-quantized operators in the basis
-    of single-particle states (e.g., HF states) and to transform them into
-    PennyLane observables. In general, the many-body observable :math:`\hat{O}` can combine
-    one-particle and two-particle operators, as in the case for electronic Hamiltonians:
+    The second-quantized operator of the Fermion many-body system can combine one-particle
+    and two-particle operators as in the case of electronic Hamiltonians :math:`\hat{H}`:
 
     .. math::
 
-        \hat{O} = \sum_{\alpha, \beta} \langle \alpha \vert \hat{t}^{(1)} +
+        \hat{H} = \sum_{\alpha, \beta} \langle \alpha \vert \hat{t}^{(1)} +
         \cdots + \hat{t}^{(n)} \vert \beta \rangle ~ \hat{c}_\alpha^\dagger \hat{c}_\beta
         + \frac{1}{2} \sum_{\alpha, \beta, \gamma, \delta}
         \langle \alpha, \beta \vert \hat{v}^{(1)} + \cdots + \hat{v}^{(n)}
@@ -231,41 +229,44 @@ def observable(matrix_elements, init_term=0, mapping="jordan_wigner", wires=None
         \hat{c}_\gamma \hat{c}_\delta
 
     In the latter equations the indices :math:`\alpha, \beta, \gamma, \delta` run over the
-    basis of single-particle states. The operators :math:`\hat{c}^\dagger` and :math:`\hat{c}`
-    are the particle creation and annihilation operators, respectively.
+    basis of single-particle states. If an `active space <https://en.wikipedia.org/wiki/
+    Complete_active_space>`_ (see :func:`~.active_space`) is defined,
+    :math:`\alpha, \beta, \gamma, \delta` run over the subset of active orbitals.
+    The operators :math:`\hat{c}^\dagger` and
+    :math:`\hat{c}` are the particle creation and annihilation operators, respectively.
     :math:`\langle \alpha \vert \hat{t} \vert \beta \rangle` denotes the matrix element of
     the single-particle operator :math:`\hat{t}` entering the observable. For example,
     in electronic structure calculations, this is the case for: the kinetic energy operator,
-    the nuclei Coulomb potential, or any other external fields included in the model Hamiltonian.
+    the nuclei Coulomb potential, or any other external fields included in the Hamiltonian.
     On the other hand, :math:`\langle \alpha, \beta \vert \hat{v} \vert \gamma, \delta \rangle`
     denotes the matrix element of the two-particle operator :math:`\hat{v}`, for example, the
     Coulomb interaction between the electrons.
 
-    If an `active space <https://en.wikipedia.org/wiki/Complete_active_space>`_
-    (see :func:`~.active_space`) is defined, the observable is expanded over the truncated
-    basis of active orbitals. The contribution of core orbitals can be passed to the
-    function using the keyword argument ``init_term``.
+    - The observable is built by adding the operators 
+      :math:`\sum_{\alpha, \beta} t_{\alpha\beta}^{(i)}
+      \hat{c}_\alpha^\dagger \hat{c}_\beta` and
+      :math:`\frac{1}{2} \sum_{\alpha, \beta, \gamma, \delta}
+      v_{\alpha\beta\gamma\delta}^{(i)}
+      \hat{c}_\alpha^\dagger \hat{c}_\beta^\dagger \hat{c}_\gamma \hat{c}_\delta`.
 
-    The function uses tools of `OpenFermion <https://github.com/quantumlib/OpenFermion>`_
-    to build the second-quantized operator and map it to basis of Pauli matrices via the
-    Jordan-Wigner or Bravyi-Kitaev transformation. Finally, the qubit operator is
-    converted to a a PennyLane observable by the function :func:`~.convert_observable`.
+    - All second-quantized one-particle and two-particle operators contributing to the
+      many-body observable must be represented using the `FermionOperator
+      <https://github.com/quantumlib/OpenFermion/blob/master/docs/
+      tutorials/intro_to_openfermion.ipynb>`_ data structure as implemented in OpenFermion.
+
+    - The function uses tools of `OpenFermion <https://github.com/quantumlib/OpenFermion>`_
+      to map the resulting fermionic Hamiltonian to the basis of Pauli matrices via the
+      Jordan-Wigner or Bravyi-Kitaev transformation. Finally, the qubit operator is converted
+      to a PennyLane observable by the function :func:`~.convert_observable`.
 
     Args:
-        matrix_elements (list(array[float])): list containing 2D numpy arrays with the matrix
-            elements of the operators :math:`\hat{t}` and :math:`\hat{v}`.
-            For single-particle operators the :math:`i`-th array in the list will have shape
-            ``(matrix_elements[i].shape[0], 3)`` with each row containing the indices
-            :math:`\alpha`, :math:`\beta` and the matrix element
-            :math:`\langle \alpha \vert \hat{t}^{(i)}\vert \beta \rangle`.
-            For two-particle operators the :math:`j`-th array in the list
-            will have shape ``(matrix_elements[j].shape[0], 5)`` with each row containing
-            the indices :math:`\alpha`, :math:`\beta`, :math:`\gamma`, :math:`\delta` and
-            the matrix element
-            :math:`\langle \alpha, \beta \vert \hat{v}^{(j)}\vert \gamma, \delta \rangle`.
-        init_term (float): the contribution of core orbitals, if any, or other quantity
-            required to initialize the many-body observable.
-        mapping (str): specifies the fermion-to-qubit mapping. Input values can
+        fermion_ops (list[FermionOperator]): list containing the FermionOperator data structures
+            representing the one-particle and/or two-particle operators entering the many-body
+            observable
+        init_term (float): Any quantity required to initialize the many-body observable. For
+            example, this can be used to pass the nuclear-nuclear repulsion energy :math:`V_{nn}`
+            which is typically included in the electronic Hamiltonian of molecules.
+        mapping (str): Specifies the fermion-to-qubit mapping. Input values can
             be ``'jordan_wigner'`` or ``'bravyi_kitaev'``.
         wires (Wires, list, tuple, dict): Custom wire mapping used to convert the qubit operator
             to an observable measurable in a PennyLane ansatz.
@@ -279,14 +280,16 @@ def observable(matrix_elements, init_term=0, mapping="jordan_wigner", wires=None
 
     **Example**
 
-    >>> t = np.array([[0., 0., 0.5], [1.0, 1.0, -0.5], [1.0, 0., 0.]])
-    >>> v = np.array([[ 0., 0., 0., 0., 0.25], [ 0., 1., 1., 0., -0.25], [ 1., 0., 0., 1., -0.5]])
-    >>> matrix_elements = [t, v]
-    >>> print(observable(matrix_elements, init_term=1/4, mapping="bravyi_kitaev"))
-    (0.15625) [I0]
-    + (-0.15625) [Z0]
-    + (0.34375) [Z0 Z1]
-    + (-0.09375) [Z1]
+    >>> t = FermionOperator("0^ 0", 0.5) + FermionOperator("1^ 1", 0.25) 
+    >>> v = FermionOperator("1^ 0^ 0 1", -0.15) + FermionOperator("2^ 0^ 2 0", 0.3)
+    >>> fermion_ops = [t, v]
+    >>> print(observable(fermion_ops, mapping="jordan_wigner"))
+    (0.2625) [I0]
+    + (-0.1375) [Z0]
+    + (-0.0875) [Z1]
+    + (-0.0375) [Z0 Z1]
+    + (0.075) [Z2]
+    + (-0.075) [Z0 Z2]
     """
 
     if mapping.strip().lower() not in ("jordan_wigner", "bravyi_kitaev"):
@@ -296,34 +299,16 @@ def observable(matrix_elements, init_term=0, mapping="jordan_wigner", wires=None
         )
 
     # Initialize the FermionOperator
-    mb_obs = FermionOperator() + FermionOperator("") * init_term
-
-    for table in matrix_elements:
-
-        if len(table.shape) != 2:
-            raise ValueError(
-                "Expected dimension for arrays in 'matrix_elements' is 2; got {}".format(
-                    table.shape
+    mb_obs = FermionOperator("") * init_term
+    for ops in fermion_ops:
+        if not isinstance(ops, FermionOperator):
+            raise TypeError(
+                "Elements in the lists are expected to be of type 'FermionOperator'; got {}".format(
+                    type(ops)
                 )
             )
 
-        if table.shape[1] not in (3, 5):
-            raise ValueError(
-                "Expected entries of matrix element tables to be of shape (3,) or (5,); got {}".format(
-                    table.shape[1]
-                )
-            )
-
-        if table.shape[1] == 5:
-            # two-particle operator
-            for i in table:
-                mb_obs += FermionOperator(
-                    ((int(i[0]), 1), (int(i[1]), 1), (int(i[2]), 0), (int(i[3]), 0)), i[4] / 2
-                )
-        else:
-            # single-particle operator
-            for i in table:
-                mb_obs += FermionOperator(((int(i[0]), 1), (int(i[1]), 0)), i[2])
+        mb_obs += ops
 
     # Map the fermionic operator to a qubit operator
     if mapping.strip().lower() == "bravyi_kitaev":
