81a48dadbf89d583086475369ff85ad58858537c
trbromley <brotho02@gmail.com>
2020-10-15 15:34:08 -0400

    Update test docstring

--- tests/tape/tapes/test_caching.py
+++ tests/tape/tapes/test_caching.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for caching executions of the quantum tape and QNode."""
+"""Tests for caching executions of the quantum device."""
 import numpy as np
 import pytest
 
