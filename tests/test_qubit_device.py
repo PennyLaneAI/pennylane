d1296c31e9f55574da2c884f33cae423b16953b8
Maria Schuld <mariaschuld@gmail.com>
2020-10-19 07:41:40 +0200

    Update tests/test_qubit_device.py
    Co-authored-by: Nathan Killoran <co9olguy@users.noreply.github.com>

--- tests/test_qubit_device.py
+++ tests/test_qubit_device.py
@@ -760,7 +760,7 @@ class TestBatchExecution:
 
         dev = mock_qubit_device_with_apply(wires=2)
 
-        tapes = [qml.tape.QuantumTape()]*n_tapes
+        tapes = [qml.tape.QuantumTape()] * n_tapes
         res = dev.batch_execute(tapes)
 
         assert len(res) == n_tapes
