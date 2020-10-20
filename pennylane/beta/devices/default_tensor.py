939b80f0deda9193841cc752d5c13fb84c3e1d59
Josh Izaac <josh146@gmail.com>
2020-10-20 15:50:56 +0800

    Bump development version to 0.13 (#857)

--- pennylane/beta/devices/default_tensor.py
+++ pennylane/beta/devices/default_tensor.py
@@ -99,8 +99,8 @@ class DefaultTensor(Device):
     # pylint: disable=attribute-defined-outside-init
     name = "PennyLane TensorNetwork simulator plugin"
     short_name = "default.tensor"
-    pennylane_requires = "0.12"
-    version = "0.12.0"
+    pennylane_requires = "0.13"
+    version = "0.13.0"
     author = "Xanadu Inc."
 
     _operation_map = {
