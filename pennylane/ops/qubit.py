67bc319fd42ac5f98d4c97103feeb3dacad6dd5d
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2020-10-21 10:44:04 -0400

    Fixed eigvals to work for all-identity case, and updated tests.

--- pennylane/ops/qubit.py
+++ pennylane/ops/qubit.py
@@ -925,6 +925,10 @@ class PauliRot(Operation):
 
     @classmethod
     def _eigvals(cls, theta, pauli_word):
+        # Identity must be treated specially because its eigenvalues are all the same
+        if pauli_word == "I" * len(pauli_word):
+            return np.exp(-1j * theta / 2) * np.ones(2 ** len(pauli_word))
+
         return MultiRZ._eigvals(theta, len(pauli_word))
 
     @staticmethod
