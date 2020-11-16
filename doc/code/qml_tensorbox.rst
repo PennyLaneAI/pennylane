a1cf69bc8c33712f53c530f613413360bd6edd2b
Josh Izaac <josh146@gmail.com>
2020-11-16 14:31:28 +0800

    more merging of master

--- doc/code/qml_tensorbox.rst
+++
@@ -1,9 +0,0 @@
-qml.tensorbox
-=============
-
-.. currentmodule:: pennylane.tensorbox
-
-.. automodapi:: pennylane.tensorbox
-    :no-heading:
-    :include-all-objects:
-    :no-inheritance-diagram:
--- pennylane/tape/__init__.py
+++ pennylane/tape/__init__.py
@@ -24,7 +24,6 @@ import pennylane as qml
 
 from . import measure
 from .circuit_graph import TapeCircuitGraph
-from .functions import UnifiedTensor
 from .queuing import AnnotatedQueue, Queue, QueuingContext
 from .measure import MeasurementProcess, state, density_matrix
 from .qnode import QNode, qnode
