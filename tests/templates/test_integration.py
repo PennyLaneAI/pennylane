219a8f40fe2faef47dcf7bebf2f3adb17947109e
soranjh <40344468+soranjh@users.noreply.github.com>
2020-11-18 11:06:01 -0500

    Add hardware-efficient particle-conserving ansatz U2 (#876)
    * Add the ParticleConservingU2 template

--- tests/templates/test_integration.py
+++ tests/templates/test_integration.py
@@ -148,6 +148,10 @@ QUBIT_DIFFABLE_NONDIFFABLE = [(qml.templates.AmplitudeEmbedding,
                                {'wires': [0, 1, 2, 3], 's_wires': [[0, 1, 2], [1, 2, 3]],
                                 'd_wires': [[[0, 1], [2, 3]]], 'init_state':np.array([1, 1, 0, 0])},
                                4),
+                              (qml.templates.ParticleConservingU2,
+                               {'weights': np.array([[0.35172862, 0.60808317, 1.44397231]])},
+                               {'wires': [0, 1], 'init_state': np.array([1, 0])},
+                               2),
                               (qml.templates.ParticleConservingU1,
                                {'weights': np.array([[[ 0.17586701, -0.20382066]]])},
                                {'wires': [0, 1], 'init_state':np.array([1, 0])},
@@ -188,7 +192,7 @@ CV_DIFFABLE_NONDIFFABLE = [(qml.templates.DisplacementEmbedding,
 # before they are called in a quantum function.
 # These templates will be skipped in tests of that nature.
 
-NO_OP_BEFORE = ["AmplitudeEmbedding", "UCCSD", "ParticleConservingU1"]
+NO_OP_BEFORE = ["AmplitudeEmbedding", "UCCSD",  "ParticleConservingU2", "ParticleConservingU1"]
 
 # Each entry to QUBIT_INIT and CV_INIT adds a template with specified inputs to the
 # integration tests ``TestIntegrationInitFunctions``
