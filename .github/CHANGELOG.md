69473e97fd887a53300c2b9479a87ddacbfb4a05
Tom Bromley <49409390+trbromley@users.noreply.github.com>
2020-11-18 09:46:45 -0500

    Add decomposition for CRot gate (#908)
    * Add decomposition
    
    * Add test
    
    * Update decomposition
    
    * Update changelog
    
    * Add more parameter options
    
    * Support in non tape mode
    
    * Update test to accommodate tape mode
    
    * Add comment

--- .github/CHANGELOG.md
+++ .github/CHANGELOG.md
@@ -50,6 +50,10 @@
 
 <h3>Improvements</h3>
 
+* The CRot gate now has a ``decomposition`` method, which breaks the gate down into rotations
+  and CNOT gates. This allows ``CRot`` to be used on devices that do not natively support it.
+  [(#908)](https://github.com/PennyLaneAI/pennylane/pull/908) 
+
 * QNodes in tape mode now support returning observables on the same wire if the observables are
   qubit-wise commuting Pauli words. Qubit-wise commuting observables can be evaluated with a
   single device run because they are diagonal in the same basis, or can be equivalently
--- tests/ops/test_qubit_ops.py
+++ tests/ops/test_qubit_ops.py
@@ -498,6 +498,27 @@ class TestOperations:
         decomposed_matrix = np.linalg.multi_dot(mats)
         assert np.allclose(decomposed_matrix, op.matrix, atol=tol, rtol=0)
 
+    @pytest.mark.parametrize("phi, theta, omega", [[0.5, 0.6, 0.7], [0.1, -0.4, 0.7], [-10, 5, -1]])
+    @pytest.mark.parametrize("tapemode", [True, False])
+    def test_CRot_decomposition(self, tol, phi, theta, omega, tapemode, monkeypatch):
+        """Tests that the decomposition of the CRot gate is correct"""
+        op = qml.CRot(phi, theta, omega, wires=[0, 1])
+
+        with monkeypatch.context() as m:
+            m.setattr(qml, "tape_mode_active", lambda: tapemode)
+            res = op.decomposition(phi, theta, omega, op.wires)
+
+        mats = []
+        for i in reversed(res):
+            if len(i.wires) == 1:
+                mats.append(np.kron(np.eye(2), i.matrix))
+            else:
+                mats.append(i.matrix)
+
+        decomposed_matrix = np.linalg.multi_dot(mats)
+
+        assert np.allclose(decomposed_matrix, op.matrix, atol=tol, rtol=0)
+
     def test_phase_shift(self, tol):
         """Test phase shift is correct"""
 
