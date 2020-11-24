e34d0bb248b2c12c51a33fda2e5e32a46f0f31f9
antalszava <antalszava@gmail.com>
2020-11-24 15:39:28 -0500

    Formatting

--- qchem/pennylane_qchem/qchem/structure.py
+++ qchem/pennylane_qchem/qchem/structure.py
@@ -584,7 +584,9 @@ def _terms_to_qubit_operator(coeffs, ops, wires=None):
     all_wires = Wires.all_wires([op.wires for op in ops], sort=True)
 
     if wires is not None:
-        qubit_indexed_wires = _process_wires(wires,)
+        qubit_indexed_wires = _process_wires(
+            wires,
+        )
         if not set(all_wires).issubset(set(qubit_indexed_wires)):
             raise ValueError("Supplied `wires` does not cover all wires defined in `ops`.")
     else:
@@ -601,7 +603,7 @@ def _terms_to_qubit_operator(coeffs, ops, wires=None):
             )
 
         # Pauli axis names, note s[-1] expects only 'Pauli{X,Y,Z}'
-        pauli_names = [s[-1] if s !="Identity" else s for s in op.name]
+        pauli_names = [s[-1] if s != "Identity" else s for s in op.name]
 
         all_identity = all(obs.name == "Identity" for obs in op.obs)
         if (op.name == ["Identity"] and len(op.wires) == 1) or all_identity:
@@ -610,7 +612,8 @@ def _terms_to_qubit_operator(coeffs, ops, wires=None):
             term_str = " ".join(
                 [
                     "{}{}".format(pauli, qubit_indexed_wires.index(wire))
-                    for pauli, wire in zip(pauli_names, op.wires) if pauli != "Identity"
+                    for pauli, wire in zip(pauli_names, op.wires)
+                    if pauli != "Identity"
                 ]
             )
 
