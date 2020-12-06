84e3f9336c29861aee0be538ccec465f757c97fd
Chase Roberts <chase@xanadu.ai>
2020-12-06 10:22:39 -0600

    remove test skip

--- tests/devices/test_default_qubit_jax.py
+++ tests/devices/test_default_qubit_jax.py
@@ -336,8 +336,6 @@ class TestHighLevelIntegration:
 
     def test_qnode_collection_integration(self):
         """Test that a PassthruQNode default.qubit.jax works with QNodeCollections."""
-        if qml.tape_mode_active():
-            pytest.skip("TODO(chase): Figure out why map doesn't set the interface")
         dev = qml.device("default.qubit.jax", wires=2)
 
         def ansatz(weights, **kwargs):
@@ -347,8 +345,10 @@ class TestHighLevelIntegration:
 
         obs_list = [qml.PauliX(0) @ qml.PauliY(1), qml.PauliZ(0), qml.PauliZ(0) @ qml.PauliZ(1)]
         qnodes = qml.map(ansatz, obs_list, dev, interface="jax")
-
-        assert qnodes.interface == "jax", f"Tape mode: {qml.tape_mode_active()}"
+        if qml.tape_mode_active():
+            pass # TODO(chase): Figure out why map doesn't set the interface in tape mode.
+        else:
+            assert qnodes.interface == "jax", f"Tape mode: {qml.tape_mode_active()}"
 
         weights = jnp.array([0.1, 0.2])
 
