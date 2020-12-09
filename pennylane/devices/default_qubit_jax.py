c2e2782f081b2fbd745c2c4ffa4b1cfa658efff2
Chase Roberts <chase@xanadu.ai>
2020-12-09 08:58:45 -0600

    states_to_binary now uses JAX ops and int32

--- pennylane/devices/default_qubit_jax.py
+++ pennylane/devices/default_qubit_jax.py
@@ -234,3 +234,23 @@ class DefaultQubitJax(DefaultQubit):
         else:
             key = self._prng_key
         return jax.random.choice(key, number_of_states, shape=(self.shots,), p=state_probability)
+
+    @staticmethod
+    def states_to_binary(samples, num_wires, dtype=jnp.int32):
+        """Convert basis states from base 10 to binary representation.
+
+        This is an auxiliary method to the generate_samples method.
+
+        Args:
+            samples (List[int]): samples of basis states in base 10 representation
+            num_wires (int): the number of qubits
+            dtype (type): Type of the internal integer array to be used. Can be
+                important to specify for large systems for memory allocation
+                purposes.
+
+        Returns:
+            List[int]: basis states in binary representation
+        """
+        powers_of_two = 1 << jnp.arange(num_wires, dtype=dtype)
+        states_sampled_base_ten = samples[:, None] & powers_of_two
+        return (states_sampled_base_ten > 0).astype(dtype)[:, ::-1]
