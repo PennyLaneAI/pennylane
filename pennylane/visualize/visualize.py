cb402ce3c0c20e6d9427d404a1482acde7bb34ac
Jack Ceroni <jackceroni@gmail.com>
2020-12-17 20:02:41 -0500

    changes

--- pennylane/visualize/visualize.py
+++ pennylane/visualize/visualize.py
@@ -39,24 +39,17 @@ class Visualize:
     When a PennyLane optimization loop is wrapped in the Visualize context manager, data from each optimization step
     is recorded and outputted as text or a graph.
 
-    Args:
-        steps (int): The number of steps for which the circuit is being optimized.
-
     Keyword Args:
         step_size (int): The number of steps taken between each data recording instance.
         cost_fn (func): The cost function that is being optimized.
 
     """
 
-    def __init__(self, steps, step_size=1, cost_fn=None):
-
-        if not isinstance(steps, int):
-            raise ValueError("'steps' must be of type int, got {}".format(type(steps)))
+    def __init__(self, step_size=1, cost_fn=None):
 
         if not isinstance(step_size, int):
             raise ValueError("'step_size' must be of type int, got {}".format(type(step_size)))
 
-        self.steps = steps
         self.step_size = step_size
         self.cost_fn = cost_fn
 
@@ -64,8 +57,10 @@ class Visualize:
         self._x_log = []
         self._y_log = []
         self._param_log = []
+        self._time_log = []
 
         self._complete = False
+        self._graph_bool = False
 
     @property
     def cost_data(self):
@@ -89,13 +84,28 @@ class Visualize:
 
         return (self._x_log, self._param_log)
 
+    @property
+    def time_data(self):
+        """
+        Returns a tuple of the recorded optimization steps, and the corresponding variational parameters.
+
+        Returns:
+            tuple: a tuple containing lists of optimization steps and execution times for each optimization step.
+        """
+
+        return (self._x_log, self._time_log)
+
     def __enter__(self):
         print("Beginning Optimization")
         print("--------------------------")
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+
         print("Optimization Complete")
+
+        if self._graph_bool:
+            plt.show()
 
     def update(self, params=None):
         """
@@ -160,8 +170,11 @@ class Visualize:
             if params is not None:
                 self._param_log.append(params)
                 if self.cost_fn is not None:
+                    start = time.time()
                     val = self.cost_fn(params)
+                    end = time.time()
                     self._y_log.append(val)
+                    self._time_log.append(start - end)
 
     def text(self, step=True, cost=False, params=False):
         """
@@ -171,6 +184,12 @@ class Visualize:
             step (bool): Choice of whether the number of steps are outputted or not.
             cost (bool): Choice of whether the cost function values are outputted or not.
             params (bool): Choice of whether the parameter values are outputted or not.
+
+        .. Warning::
+
+            Usage of ``viz.text()`` and dynamic ``viz.update()`` simultaneously within Jupyter leads to a strange
+            flickering of the output window. In this situation,
+            it is recommended that the text output built into the graph is used instead of ``viz.text()``.
 
         .. UsageDetails::
 
@@ -252,16 +271,29 @@ class Visualize:
         if self._step_log % self.step_size == 0:
 
             if step:
-                print("Optimization Step {} / {}".format(self._step_log, self.steps))
+                print("Optimization Step {}".format(self._step_log))
             if cost:
                 print("Cost: {}".format(self._y_log[len(self._y_log) - 1]))
             if params:
                 print("Parameters: {}".format(self._param_log[len(self._param_log) - 1]))
             print("--------------------------")
 
-    def graph(self):
+    def graph(self, color=None, xlim=None, ylim=None):
         """
         Returns a graph of the value of the cost function after each optimization step.
+
+        Keyword args:
+            color (str): The color of the graph. See the `Matplotlib documentation
+                         <https://matplotlib.org/tutorials/colors/colors.html>`_ for more information on
+                         the formats of color that can be passed as an argument.
+            xlim (tuple): The minimum and maximum values on the x-axis.
+            ylim (tuple): The minimum and maximum values on the y-axis.
+
+        .. Warning::
+
+            Usage of ``viz.text()`` and dynamic ``viz.update()`` simultaneously within Jupyter leads to a strange
+            flickering of the output window. In this situation,
+            it is recommended that the text output built into the graph is used instead of ``viz.text()``.
 
         .. UsageDetails::
 
@@ -303,49 +335,77 @@ class Visualize:
                         viz.update(params=params)
                         viz.graph()
 
-                Execution of this code block will result an a graph window, which will dynamically plot the value
-                of the cost function after each optimization step.
+            Execution of this code block will result an a graph window, which will dynamically plot the value
+            of the cost function after each optimization step.
 
-                In addition, the ``graph()`` method can also be called outside the optimization loop (after the
-                optimization has finished), to produce a static graph showing the values of the cost function for
-                each optimization step:
+            In addition, the ``graph()`` method can also be called outside the optimization loop (after the
+            optimization has finished), to produce a static graph showing the values of the cost function for
+            each optimization step:
 
-                .. code-block:: python3
+            .. code-block:: python3
 
-                optimizer = qml.GradientDescentOptimizer()
-                steps = 5
-                params = np.array([1.])
+            optimizer = qml.GradientDescentOptimizer()
+            steps = 5
+            params = np.array([1.])
 
-                with Visualize(steps, cost_fn=cost) as viz:
-                    for i in range(steps):
-                        params = optimizer.step(cost, params)
-                        viz.update(params=params)
-                    viz.graph()
+            with Visualize(steps, cost_fn=cost) as viz:
+                for i in range(steps):
+                    params = optimizer.step(cost, params)
+                    viz.update(params=params)
+                 viz.graph()
         """
+
+        if color is None:
+            color = "#1D9598"
+
+        self._graph_bool = True
+
+        if not isinstance(color, str):
+            raise ValueError("'color' must be of type str, got {}".format(type(color)))
 
         if _j_nb():
 
             plt.clf()
 
+            if xlim is not None:
+                mn, mx = xlim
+                plt.xlim(mn, mx)
+
+            if ylim is not None:
+                mn, mx = ylim
+                plt.ylim(mn, mx)
+
             plt.ylabel("Cost")
             plt.xlabel("Steps")
 
-            plt.plot(self._x_log, self._y_log, color="#1D9598")
-            plt.scatter(self._x_log, self._y_log, color="#1D9598")
+            plt.plot(self._x_log, self._y_log, color=color)
+            plt.scatter(self._x_log, self._y_log, color=color)
+
+            plt.gcf().text(0.13, 1.02, "Step {}".format(len(self._x_log)), fontsize=12)
+            plt.gcf().text(0.13, 0.95, "Cost = {}".format(self._y_log[len(self._y_log)-1]), fontsize=12)
 
             display.display(plt.gcf())
             display.clear_output(wait=True)
-            time.sleep(0.05)
-            if self.steps == self._step_log:
-                plt.show()
 
         else:
+
+            plt.clf()
+
+            if xlim is not None:
+                mn, mx = xlim
+                plt.xlim(mn, mx)
+
+            if ylim is not None:
+                mn, mx = ylim
+                plt.ylim(mn, mx)
 
             plt.ylabel("Cost")
             plt.xlabel("Steps")
 
-            plt.plot(self._x_log, self._y_log, color="#1D9598")
-            plt.scatter(self._x_log, self._y_log, color="#1D9598")
+            plt.plot(self._x_log, self._y_log, color=color)
+            plt.scatter(self._x_log, self._y_log, color=color)
+
+            plt.gcf().text(0.13, 0.94, "Step {}".format(len(self._x_log)), fontsize=11)
+            plt.gcf().text(0.13, 0.9, "Cost = {}".format(self._y_log[len(self._y_log) - 1]), fontsize=11)
+
             plt.pause(0.025)
-            if self.steps == self._step_log:
-                plt.show()
