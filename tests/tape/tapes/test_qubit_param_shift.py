a730d68340bc746f5464b67b936456fe15890418
Steven Oud <soud@protonmail.com>
2020-12-21 12:54:39 +0100

    Remove unused import

--- tests/tape/tapes/test_qubit_param_shift.py
+++ tests/tape/tapes/test_qubit_param_shift.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Unit tests for the qubit parameter-shift QubitParamShiftTape"""
-import itertools
 import pytest
 import numpy as np
 
