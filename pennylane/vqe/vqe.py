dfa37d7cd85ea06099bed4765d1b732a3cfc2f43
Christina Lee <chrissie.c.l@gmail.com>
2020-12-29 14:04:36 -0600

    change list comprehension

--- pennylane/vqe/vqe.py
+++ pennylane/vqe/vqe.py
@@ -160,7 +160,7 @@ class Hamiltonian:
     def __str__(self):
         terms = []
 
-        paired_coeff_op = [(coeff, op) for coeff, op in zip(self.coeffs, self.ops)]
+        paired_coeff_op = list(zip(h.coeffs, h.ops))
         paired_coeff_op.sort(key=lambda pair: (pair[1].num_wires, pair[0]))
 
         for coeff, op in paired_coeff_op:
