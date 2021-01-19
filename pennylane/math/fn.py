a289d25f3e7d11b38563f8d3b3340a3ddfba878f
Chase Roberts <chase@xanadu.ai>
2021-01-18 17:59:28 -0800

    Example

--- pennylane/math/fn.py
+++ pennylane/math/fn.py
@@ -522,7 +522,16 @@ def squeeze(tensor):
         tensor (tensor_like): A tensor-like object.
 
     Returns:
-        The input array, but with all or a subset of the dimensions of length 1 removed. This is always a itself or a view into a. Note that if all axes are squeezed, the result is a 0d array and not a scalar.
+        The input array, but with all or a subset of the dimensions of length 1 removed. 
+        This is always a itself or a view into a. Note that if all axes are squeezed, 
+        the result is a 0d array and not a scalar.
+
+    **Example**
+
+    >>> x = torch.ones((2, 1, 3, 4, 1))
+    >>> y = squeeze(x)
+    >>> y.shape
+    (2, 3, 4)
     """
     return TensorBox(tensor).squeeze(wrap_output=False)
 
