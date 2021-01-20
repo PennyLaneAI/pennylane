18eaaf9e4a57f3abccc47bd1b57f04d6e6e86982
trbromley <brotho02@gmail.com>
2021-01-20 15:42:00 -0500

    First attempt

--- pennylane/tape/tapes/rewind.py
+++ pennylane/tape/tapes/rewind.py
@@ -18,6 +18,7 @@ TODO
 import pennylane as qml
 from pennylane.devices import DefaultMixed
 from .jacobian_tape import JacobianTape
+import numpy as np
 
 
 class RewindTape(JacobianTape):
@@ -52,15 +53,51 @@ class RewindTape(JacobianTape):
         elif method == "numeric":
             raise ValueError("RewindTape does not support numeric differentiation")
 
-        if not device.capabilities().get("returns_state") or isinstance(device, DefaultMixed):
+        if not device.capabilities().get("returns_state") or isinstance(device, DefaultMixed) \
+            or not hasattr(device, "_apply_operation"):
             # TODO: consider renaming returns_state to, e.g., uses_statevector
             # TODO: consider adding a capability for mixed/pure state
+            # TODO: consider adding capability for apply_operation
             raise qml.QuantumFunctionError("The rewind gradient method is only supported on statevector-based devices")
 
         # Perform the forward pass
         # TODO: Could we use lower-level like device.apply, since we just need the state?
         self.execute(device, params=params)
-        phi = device.state
+        phi = device._state  # TODO: Do we need dev._state or dev.state?
+
+        lambdas = [device._apply_operation(phi, obs) for obs in self.observables]
+
+        jac = np.zeros((len(lambdas), len(self.trainable_params)))
+
+        for i, op in enumerate(reversed(self.operations)):
+            adj = op.inv()
+            phi = device._apply_operation(phi, adj)
+
+            # TODO: Only use a matrix when necessary
+            d_op_matrix = operator_derivative(op)
+            mu = device._apply_unitary(phi, d_op_matrix, op.wires)
+
+            jac_column = np.array([2 * np.real(dot_product(lambda_, mu)) for lambda_ in lambdas])
+            jac[:, i] = jac_column
+
+            lambdas = [device._apply_operation(lambda_, adj) for lambda_ in lambdas]
+
+        return np.flip(jac, axis=1)
+
+
+def dot_product(a, b):
+    """TODO"""
+    return np.sum(a.conj() * b)
+
+
+def operator_derivative(operator):
+    """TODO: do this better"""
+    generator, prefactor = operator.generator
+    param = operator.parameters[0]
+
+    return (- prefactor * np.sin(param * prefactor) * np.eye(2)
+                        - 1j * prefactor * np.cos(param * prefactor) * generator.matrix).T.conj()
+
 
 
 
