4b0e83e0f4694393278ddab185c527fea82e9e26
agran2018 <nanoalain@gmail.com>
2021-02-19 18:02:55 -0500

    Modify 'test_second_derivative.py'

--- qchem/tests/test_second_derivative.py
+++ qchem/tests/test_second_derivative.py
@@ -44,6 +44,25 @@ coeffs_diag = np.array(
     ]
 )
 
+delta_off_diag = 0.008
+coeffs_off_diag = np.array(
+    [
+        -0.04445503691300096,
+        0.08566509919512479,
+        0.08566509919536767,
+        0.28996324336433565,
+        0.2899632433644571,
+        0.05413053432861373,
+        -0.05413053432861373,
+        -0.05413053432861373,
+        0.05413053432861373,
+        -0.08536187184617826,
+        -0.03123133751738237,
+        -0.03123133751738237,
+        -0.08536187184617826,
+        -1.1678041893026774,
+    ]
+)
 
 x = np.array([-0.0211, -0.002, 0.0, 0.8345, 0.4519, 0.0, 1.4769, -0.273, 0.0])
 
@@ -55,7 +74,12 @@ def H(x):
 
 
 @pytest.mark.parametrize(
-    ("i", "j", "delta", "coeffs", "ops"), [(0, 0, delta_diag, coeffs_diag, ops),],
+    ("i", "j", "delta", "coeffs", "ops"),
+    [
+        (0, 0, delta_diag, coeffs_diag, ops),
+        (0, 3, delta_off_diag, coeffs_off_diag, ops),
+        (0, 2, delta_diag, [], []),
+    ],
 )
 def test_deriv2(i, j, delta, coeffs, ops, tol):
     r"""Tests the correctness of the second-order nuclear derivative of the electronic Hamiltonian
@@ -68,28 +92,28 @@ def test_deriv2(i, j, delta, coeffs, ops, tol):
 
     exp_obs = qml.vqe.Hamiltonian(coeffs, ops)
 
-    # assert exp_obs.compare(deriv2)
     assert np.allclose(deriv2.coeffs, exp_obs.coeffs, **tol)
-    # assert np.allclose(deriv2._obs_data(), exp_obs._obs_data(), **tol)
+    assert all(isinstance(o1, o2.__class__) for o1, o2 in zip(deriv2.ops, exp_obs.ops))
+    assert all(o1.wires == o2.wires for o1, o2 in zip(deriv2.ops, exp_obs.ops))
 
 
-# def test_integration(tol):
-#     r"""Tests integration with PennyLane."""
+def test_integration(tol):
+    r"""Tests integration with PennyLane."""
 
-#     dev = qml.device("default.qubit", wires=4)
+    dev = qml.device("default.qubit", wires=4)
 
-#     def circuit(param, wires):
-#         qml.BasisState(np.array([1, 1, 0, 0]), wires=[0, 1, 2, 3])
-#         qml.RY(param, wires=2)
-#         qml.CNOT(wires=[2, 3])
-#         qml.CNOT(wires=[2, 0])
-#         qml.CNOT(wires=[3, 1])
+    def circuit(param, wires):
+        qml.BasisState(np.array([1, 1, 0, 0]), wires=[0, 1, 2, 3])
+        qml.RY(param, wires=2)
+        qml.CNOT(wires=[2, 3])
+        qml.CNOT(wires=[2, 0])
+        qml.CNOT(wires=[3, 1])
 
-#     deriv2 = qchem.second_derivative(H, x, i=0, j=3)
+    deriv2 = qchem.second_derivative(H, x, i=0, j=0)
 
-#     shutil.rmtree("pyscf")
+    shutil.rmtree("pyscf")
 
-#     param = 6.07230111451844
-#     exp_res = 0.026747015308817568
+    param = 6.0723
+    exp_res = 0.632611572251488
 
-#     assert np.allclose(qml.ExpvalCost(circuit, deriv2, dev)(param), exp_res, **tol)
+    assert np.allclose(qml.ExpvalCost(circuit, deriv2, dev)(param), exp_res, **tol)
