b4e69b49205240d893ccc2dfc352e03137d227fa
agran2018 <nanoalain@gmail.com>
2021-02-23 16:30:35 -0500

    Add more tests to 'test_second_derivative.py'

--- qchem/tests/test_second_derivative.py
+++ qchem/tests/test_second_derivative.py
@@ -7,6 +7,49 @@ from pennylane import qchem
 import shutil
 
 
+delta_diag = 0.00529
+x_diag = np.array([-0.0211, -0.002, 0.0, 0.8345, 0.4519, 0.0, 1.4769, -0.273, 0.0])
+coeffs_diag = np.array(
+    [
+        0.3477265623545358,
+        -0.030523411304217995,
+        -0.030523411304495738,
+        -0.17712002790405676,
+        -0.177120027903779,
+        -0.03147619925860665,
+        0.03147619925860665,
+        0.03147619925860665,
+        -0.03147619925860665,
+        0.04561772636250726,
+        0.014141527103779092,
+        0.014141527103779092,
+        0.04561772636250726,
+        0.6647462687400493,
+    ]
+)
+
+
+delta_off_diag = 0.08
+x_off_diag = np.array([-0.028, -0.001, 0.0, 0.79, 0.43, 0.0, 1.5, -0.3, 0.0])
+coeffs_off_diag = np.array(
+    [
+        -0.144934802574563,
+        -0.01818706265356182,
+        -0.018187062653564248,
+        -0.17206776782849864,
+        -0.17206776782850228,
+        0.0009507863667557693,
+        -0.0009507863667557693,
+        -0.0009507863667557693,
+        0.0009507863667557693,
+        0.003285934907832377,
+        0.004236721274589892,
+        0.004236721274589892,
+        0.003285934907832377,
+        0.03200398467630317,
+    ]
+)
+
 ops = [
     qml.Identity(wires=[0]),
     qml.PauliZ(wires=[0]),
@@ -24,67 +67,24 @@ ops = [
     qml.PauliZ(wires=[2]) @ qml.PauliZ(wires=[3]),
 ]
 
-delta_diag = 0.005291772
-coeffs_diag = np.array(
-    [
-        0.34772577929044984,
-        -0.030524310020001295,
-        -0.030524310019168627,
-        -0.17711904124472824,
-        -0.17711904124389558,
-        -0.031476241863026465,
-        0.031476241863026465,
-        0.031476241863026465,
-        -0.031476241863026465,
-        0.0456184398783165,
-        0.014142198015151252,
-        0.014142198015151252,
-        0.0456184398783165,
-        0.6647460642390949,
-    ]
-)
-
-delta_off_diag = 0.008
-coeffs_off_diag = np.array(
-    [
-        -0.04445503691300096,
-        0.08566509919512479,
-        0.08566509919536767,
-        0.28996324336433565,
-        0.2899632433644571,
-        0.05413053432861373,
-        -0.05413053432861373,
-        -0.05413053432861373,
-        0.05413053432861373,
-        -0.08536187184617826,
-        -0.03123133751738237,
-        -0.03123133751738237,
-        -0.08536187184617826,
-        -1.1678041893026774,
-    ]
-)
-
-x = np.array([-0.0211, -0.002, 0.0, 0.8345, 0.4519, 0.0, 1.4769, -0.273, 0.0])
-
-
-def H(x):
-    return qml.qchem.molecular_hamiltonian(
-        ["H", "O", "H"], x, active_electrons=2, active_orbitals=2
-    )[0]
-
 
 @pytest.mark.parametrize(
-    ("i", "j", "delta", "coeffs", "ops"),
+    ("i", "j", "x", "delta", "coeffs", "ops"),
     [
-        (0, 0, delta_diag, coeffs_diag, ops),
-        (0, 3, delta_off_diag, coeffs_off_diag, ops),
-        (0, 2, delta_diag, [], []),
+        (0, 0, x_diag, delta_diag, coeffs_diag, ops),
+        (1, 3, x_off_diag, delta_off_diag, coeffs_off_diag, ops),
+        (0, 2, x_off_diag, delta_off_diag, [], []),
     ],
 )
-def test_deriv2(i, j, delta, coeffs, ops, tol):
-    r"""Tests the correctness of the second-order nuclear derivative of the electronic Hamiltonian
-    computed by the ``'second_derivative'`` function. 
+def test_deriv2(i, j, x, delta, coeffs, ops, tol):
+    r"""Tests the ``second_derivative`` function computing the second-order derivative
+    of the electronic Hamiltonian ``H(x)`` at the nuclear coordinates ``x``.
     """
+
+    def H(x):
+        return qml.qchem.molecular_hamiltonian(
+            ["H", "O", "H"], x, active_electrons=2, active_orbitals=2
+        )[0]
 
     deriv2 = qchem.second_derivative(H, x, i, j, delta=delta)
 
@@ -97,23 +97,11 @@ def test_deriv2(i, j, delta, coeffs, ops, tol):
     assert all(o1.wires == o2.wires for o1, o2 in zip(deriv2.ops, exp_obs.ops))
 
 
-def test_integration(tol):
-    r"""Tests integration with PennyLane."""
+def test_not_callable_h():
+    r"""Test that an error is raised if the input Hamiltonian 'H' is not a callable
+    object"""
 
-    dev = qml.device("default.qubit", wires=4)
-
-    def circuit(param, wires):
-        qml.BasisState(np.array([1, 1, 0, 0]), wires=[0, 1, 2, 3])
-        qml.RY(param, wires=2)
-        qml.CNOT(wires=[2, 3])
-        qml.CNOT(wires=[2, 0])
-        qml.CNOT(wires=[3, 1])
-
-    deriv2 = qchem.second_derivative(H, x, i=0, j=0)
-
-    shutil.rmtree("pyscf")
-
-    param = 6.0723
-    exp_res = 0.632611572251488
-
-    assert np.allclose(qml.ExpvalCost(circuit, deriv2, dev)(param), exp_res, **tol)
+    with pytest.raises(
+        TypeError, match="should be a callable function to build the electronic Hamiltonian"
+    ):
+        qchem.second_derivative(ops, x_diag, 0, 0)
