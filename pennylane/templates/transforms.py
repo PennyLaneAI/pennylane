7536c21e8a83c245659a93d438e78a0903d34c7b
Chase Roberts <chase@xanadu.ai>
2021-02-25 08:04:42 -0800

    Update pennylane/templates/transforms.py
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- pennylane/templates/transforms.py
+++ pennylane/templates/transforms.py
@@ -5,9 +5,10 @@ from pennylane.tape.tapes import QuantumTape
 def adjoint(fn):
     """Create a function that applies the adjoint of the provided operation or template.
 
-    Example:
-            `adjoint(fn)(args)` will apply all of the operations executed during fn(args),
-            but in reverse and with each operation adjointed.
+    **Example**
+     
+     ``adjoint(fn)(args)`` will apply all of the operations executed during ``fn(args)``,
+     but in reverse and with each operation adjointed.
     """
 
     @wraps(fn)
