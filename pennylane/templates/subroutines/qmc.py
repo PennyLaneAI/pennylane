08ae2c17c0ca4ad50d281db372e5e8f45aabb280
trbromley <brotho02@gmail.com>
2021-03-08 15:45:43 -0500

    Add to docstrings

--- pennylane/templates/subroutines/qmc.py
+++ pennylane/templates/subroutines/qmc.py
@@ -22,14 +22,14 @@ from pennylane.templates.decorator import template
 
 
 def probs_to_unitary(probs):
-    """Calculates the unitary matrix corresponding to an input probability distribution.
+    r"""Calculates the unitary matrix corresponding to an input probability distribution.
 
-    For a given distribution :math:`p_{i}`, this function returns the unitary :math:`U` that
-    transforms the :math:`|0\rangle` state as
+    For a given distribution :math:`p_{i}`, this function returns the unitary :math:`\mathcal{A}`
+    that transforms the :math:`|0\rangle` state as
 
     .. math::
 
-        U |0\rangle = \sum_{i} \sqrt{p_{i}} |i\rangle,
+        \mathcal{A} |0\rangle = \sum_{i} \sqrt{p_{i}} |i\rangle,
 
     so that measuring the resulting state in the computational basis will give the state
     :math:`|i\rangle` with probability :math:`p_{i}`. Note that the returned unitary matrix is
@@ -73,16 +73,43 @@ def probs_to_unitary(probs):
 
     return unitary
 
+
 def func_to_unitary(xs, func):
-    """
+    r"""Calculates the unitary that encodes a function onto an ancilla qubit register.
+
+    Consider the set of :math:`M` points :math:`X = \{x_{0}, x_{1}, \ldots, x_{M - 1}\}` and the
+    corresponding :math:`d`-dimensional discrete space :math:`X^{d}`. Also, consider the function
+    :math:`f: X^{d} \rightarrow \mathbb{R}` such that :math:`0 \leq f(x) \leq 1` for all
+    :math:`x \in X`.
+
+    This function returns a unitary :math:`\mathcal{R}` that performs the transformation:
+
+    .. math::
+
+        \mathcal{R} |0\rangle \otimes |i_{d - 1}i_{d - 2}\ldots i_{0}\rangle
+         = \left(\sqrt{1 - f(x_{i_{0}}, \ldots, x_{i_{d - 2}}, x_{i_{d - 1}})}|0\rangle +
+        \sqrt{f(x_{i_{0}}, \ldots, x_{i_{d - 2}}, x_{i_{d - 1}})} |1\rangle\right)
+        \otimes |i_{d - 1}i_{d - 2}\ldots i_{0}\rangle,
+
+    where :math:`i_{j} \in \{0, 1, \ldots , M - 1\}` for all :math:`j`. For a given input state
+    :math:`|0\rangle \otimes |i_{d - 1}i_{d - 2}\ldots i_{0}\rangle`, this unitary encodes the
+    amplitude :math:`\sqrt{f(x_{i_{0}}, \ldots, x_{i_{d - 2}}, x_{i_{d - 1}})}` onto the
+    :math:`|1\rangle` state of the ancilla qubit. Hence, measuring the ancilla qubit will result
+    in the :math:`|1\rangle` state with probability
+    :math:`f(x_{i_{0}}, \ldots, x_{i_{d - 2}}, x_{i_{d - 1}})`.
+
+    More generally, one can consider a :math:`d`-dimensional discrete product space
+    :math:`X_{0} \times X_{1} \times \ldots \times X_{d - 1}` with each :math:`X_{i}` composed of
+    an independent set of :math:`x`-values of varying length.
 
     Args:
-        xs (list[array]): A list of arrays
-        func (callable): A random variable with dimension equal to ``len(xs)``. This function must
-            evaluate within :math:`[0, 1]` for the range of input values specified by ``xs``.
+        xs (list[array]): a list of arrays containing the values for each :math:`X_{i}`
+        func (callable): A random variable that can be called with :math:`d` arguments.
+            It must evaluate within :math:`[0, 1]` for the range of input values specified by
+            ``xs``.
 
     Returns:
-        array: unitary
+        array: the :math:`\mathcal{R}` unitary
 
     Raises:
         ValueError: if func is not bounded with :math:`[0, 1]` for the range of input values
