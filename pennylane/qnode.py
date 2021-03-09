4f146757ea1283d9697d23214a89e231e3812d8a
Maria Schuld <mariaschuld@gmail.com>
2021-03-09 09:38:38 +0200

    Update pennylane/qnode.py
    Co-authored-by: Theodor <theodor@xanadu.ai>

--- pennylane/qnode.py
+++ pennylane/qnode.py
@@ -313,7 +313,8 @@ class QNode:
                 f"{backprop_interface} interface."
             )
 
-        if (getattr(device, "shots", 0) is None) and backprop_devices is not None:
+        if device.shots is None and backprop_devices is not None:
+
             # device is analytic and has child devices that support backpropagation natively
 
             if interface in backprop_devices:
