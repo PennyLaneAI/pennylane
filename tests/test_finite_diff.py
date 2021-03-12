e19508062bb68c4cf297555e6ef0805d5d894d0a
agran2018 <nanoalain@gmail.com>
2021-03-12 11:37:30 -0500

    Add unit test 'test_exceptions'

--- tests/test_finite_diff.py
+++ tests/test_finite_diff.py
@@ -4,50 +4,90 @@ import pytest
 import pennylane as qml
 
 
+# x1 = 0.5
+# grad1 = 0.8775789053009353
 
-x1 = 0.5
-grad1 = 0.8775789053009353
-
-x2 = np.array([0.5, 0.25])
-grad2 = np.array([np.array([0.87757891, 0.        ]), np.array([0.        , 0.96890838])])
-
-
-@pytest.mark.parametrize(
-    ("argnum", "idx", "delta", "x", "exp_grad"),
-    [
-        (0, None, 0.01, x1, grad1),
-        (0, None, 0.01, x2, grad2),
-    ],
-)
-def test_first_finit_diff(argnum, idx, delta, x, exp_grad):
-    r"""Tests the correctness of the first-order finite difference function."""
-
-    def f(x):
-        return np.sin(x)
-
-    grad = qml.finite_diff(f, N=1, argnum=argnum, idx=idx, delta=delta)(x)
-
-    print("computed", grad, type(grad))
-    print("expected", exp_grad, type(exp_grad))
-
-    assert np.allclose(grad, exp_grad)
-
-
-# def test_not_callable_func():
-#     r"""Test that an error is raised if the function to be differentiated
-#     is not a callable object"""
-
-#     with pytest.raises(TypeError, match="'F' should be a callable function"):
-#         qml.finite_diff(f(x1), x1, 0)
+# x2 = np.array([0.5, 0.25])
+# grad2 = np.array([np.array([0.87757891, 0.        ]), np.array([0.        , 0.96890838])])
 
 
 # @pytest.mark.parametrize(
-#     ("x", "i"),
-#     [(x2, None), (x2, 4)],
+#     ("argnum", "idx", "delta", "x", "exp_grad"),
+#     [
+#         # (0, None, 0.01, x1, grad1),
+#         (0, None, 0.01, x2, grad2),
+#     ],
 # )
-# def test_exceptions(x, i):
-#     r"""Test that an error is raised if the index 'i' of the variable we are differentiating
-#     with respect to is not given or is out of bounds for multivariable functions"""
+# def test_first_finit_diff(argnum, idx, delta, x, exp_grad):
+#     r"""Tests the correctness of the first-order finite difference function."""
 
-#     with pytest.raises(ValueError, match="'i' must be an integer between"):
-#         qml.finite_diff(f, x2, i)
+#     def f(x):
+#         return np.sin(x)
+
+#     grad = qml.finite_diff(f, N=1, argnum=argnum, idx=idx, delta=delta)(x)
+#     for i, _grad in enumerate(grad):
+#         grad[i] = np.array(_grad, dtype=float)
+
+#     print("computed", grad, type(grad))
+#     print("expected", exp_grad, type(exp_grad))
+
+#     assert np.allclose(grad, exp_grad)
+
+
+def f(x):
+    return np.sin(x)
+
+
+@pytest.mark.parametrize(
+    ("N", "delta", "func", "msg_match"),
+    [
+        (3, 0.01, f, "finite-difference approximations are supported up to second-order"),
+        (1, 0.0, f, "The value of the step size 'delta' has to be greater than 0"),
+        (0, 0.01, f(0.5), "'f' should be a callable function"),
+    ],
+)
+def test_exceptions_finite_diff(N, delta, func, msg_match):
+    r"""Test exceptions of the 'finite_diff' function"""
+
+    if N != 0:
+        with pytest.raises(ValueError, match=msg_match):
+            qml.finite_diff(f, N=N, delta=delta)
+    elif N == 0:
+        with pytest.raises(TypeError, match=msg_match):
+            qml.finite_diff(f(0.5), N=N, delta=delta)
+
+
+@pytest.mark.parametrize(
+    ("which", "argnum", "idx", "msg_match"),
+    [
+        ("both", 2, None, "The value of 'argnum' has to be between 0 and"),
+        ("both", 1, [0, 1], "is not an array, 'idx' should be set to 'None'"),
+        ("both", 0, [0, 2], "Indices in 'idx' can not be greater than"),
+        (
+            "second",
+            0,
+            [0, 2, 3],
+            "The number of indices given in 'idx' can not be greater than two",
+        ),
+    ],
+)
+def test_exceptions(which, argnum, idx, msg_match):
+    r"""Test exceptions raised by the internal functions
+    '_fd_first_order_centered' and '_fd_second_order_centered'"""
+
+    def f(x, y):
+        return np.sin(x) + 1 / y
+
+    x = np.array([0.5, -0.25])
+    y = 0.3
+
+    if which == "both":
+        with pytest.raises(ValueError, match=msg_match):
+            qml.finite_diff(f, argnum=argnum, idx=idx)(x, y)
+
+        with pytest.raises(ValueError, match=msg_match):
+            qml.finite_diff(f, N=2, argnum=argnum, idx=idx)(x, y)
+
+    if which == "second":
+        with pytest.raises(ValueError, match=msg_match):
+            qml.finite_diff(f, N=2, argnum=argnum, idx=idx)(x, y)
