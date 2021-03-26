9ca98aab1a5ca3fef9c57212c0796360b83cb462
albi3ro <chrissie.c.l@gmail.com>
2021-03-26 16:24:08 -0500

    move interface specific tests out of test_qnode.py

--- tests/interfaces/test_qnode_tf.py
+++ tests/interfaces/test_qnode_tf.py
@@ -34,6 +34,27 @@ from pennylane.tape import JacobianTape
 class TestQNode:
     """Tests the tensorflow interface used with a QNode."""
 
+    def test_import_error(self, mocker):
+        """Test that an exception is caught on import error"""
+        tf = pytest.importorskip("tensorflow", minversion="2.1")
+        mock = mocker.patch("pennylane.interfaces.tf.TFInterface.apply")
+        mock.side_effect = ImportError()
+
+        def func(x, y):
+            qml.RX(x, wires=0)
+            qml.RY(y, wires=1)
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.PauliZ(0))
+
+        dev = qml.device("default.qubit", wires=2)
+        qn = QNode(func, dev, interface="tf", diff_method="parameter-shift")
+
+        with pytest.raises(
+            qml.QuantumFunctionError,
+            match="TensorFlow not found. Please install the latest version of TensorFlow to enable the 'tf' interface",
+        ):
+            qn(0.1, 0.1)
+
     def test_execution_no_interface(self, dev_name, diff_method):
         """Test execution works without an interface, and that trainable parameters
         are correctly inferred within a gradient tape."""
@@ -137,6 +158,34 @@ class TestQNode:
         grad2 = tape.gradient(res2, a)
         assert np.allclose(res1, res2, atol=tol, rtol=0)
         assert np.allclose(grad1, grad2, atol=tol, rtol=0)
+
+    def test_drawing(self):
+        """Test circuit drawing when using the TF interface"""
+        tf = pytest.importorskip("tensorflow", minversion="2.1")
+
+        x = tf.Variable(0.1, dtype=tf.float64)
+        y = tf.Variable([0.2, 0.3], dtype=tf.float64)
+        z = tf.Variable(0.4, dtype=tf.float64)
+
+        dev = qml.device("default.qubit", wires=2)
+
+        @qnode(dev, interface="tf")
+        def circuit(p1, p2=y, **kwargs):
+            qml.RX(p1, wires=0)
+            qml.RY(p2[0] * p2[1], wires=1)
+            qml.RX(kwargs["p3"], wires=0)
+            qml.CNOT(wires=[0, 1])
+            return qml.state()
+
+        circuit(p1=x, p3=z)
+
+        result = circuit.draw()
+        expected = """\
+ 0: ──RX(0.1)───RX(0.4)──╭C──╭┤ State 
+ 1: ──RY(0.06)───────────╰X──╰┤ State 
+"""
+
+        assert result == expected
 
     def test_jacobian(self, dev_name, diff_method, mocker, tol):
         """Test jacobian calculation"""
