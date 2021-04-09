5645fd23970490dd25169018f2de1e9b4ec9c8e6
Maria Schuld <mariaschuld@gmail.com>
2021-04-09 09:02:07 +0200

    [Templates] Rewrite layer templates as operations (#1163)
    * converted CVQNN
    
    * backup
    
    * rewrite particle_conserving
    
    * add tests for simplified and stronglyent
    
    * add particle1 tests
    
    * finished all tests - 2 external bugs remaining
    
    * update changelog
    
    * black
    
    * delete old tests that are now failing
    
    * implement some review comments
    
    * reverse changes to interferometer
    
    * improve codecov
    
    * Update tests/templates/test_layers/test_basic_entangler.py
    
    Co-authored-by: Chase Roberts <chase@xanadu.ai>
    
    * Update tests/templates/test_layers/test_basic_entangler.py
    
    Co-authored-by: Chase Roberts <chase@xanadu.ai>
    
    * implement review comments
    
    * delete old integration tests
    
    Co-authored-by: Chase Roberts <chase@xanadu.ai>

--- pennylane/templates/layers/strongly_entangling.py
+++ pennylane/templates/layers/strongly_entangling.py
@@ -12,75 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 r"""
-Contains the ``StronglyEntanglingLayers`` template.
+Contains the StronglyEntanglingLayers template.
 """
 # pylint: disable-msg=too-many-branches,too-many-arguments,protected-access
 import pennylane as qml
-from pennylane.templates.decorator import template
-from pennylane.ops import CNOT, Rot
-from pennylane.templates import broadcast
-from pennylane.wires import Wires
+from pennylane.operation import Operation, AnyWires
 
 
-def _preprocess(weights, wires, ranges):
-    """Validate and pre-process inputs as follows:
-
-    * Check the shape of the weights tensor.
-    * If ranges is None, define a default.
-
-    Args:
-        weights (tensor_like): trainable parameters of the template
-        wires (Wires): wires that template acts on
-        ranges (Sequence[int]): range for each subsequent layer
-
-    Returns:
-        int, list[int]: number of times that the ansatz is repeated and preprocessed ranges
-    """
-    shape = qml.math.shape(weights)
-    repeat = shape[0]
-
-    if len(shape) != 3:
-        raise ValueError(f"Weights tensor must be 3-dimensional; got shape {shape}")
-
-    if shape[1] != len(wires):
-        raise ValueError(
-            f"Weights tensor must have second dimension of length {len(wires)}; got {shape[1]}"
-        )
-
-    if shape[2] != 3:
-        raise ValueError(f"Weights tensor must have third dimension of length 3; got {shape[2]}")
-
-    if len(wires) > 1:
-        if ranges is None:
-            # tile ranges with iterations of range(1, n_wires)
-            ranges = [(l % (len(wires) - 1)) + 1 for l in range(repeat)]
-    else:
-        ranges = [0] * repeat
-
-    return repeat, ranges
-
-
-def strongly_entangling_layer(weights, wires, r, imprimitive):
-    r"""A layer applying rotations on each qubit followed by cascades of 2-qubit entangling gates.
-
-    Args:
-        weights (tensor_like): weight tensor of shape ``(len(wires), 3)``
-        wires (Wires): wires that the template acts on
-        r (int): range of the imprimitive gates of this layer, defaults to 1
-        imprimitive (pennylane.ops.Operation): two-qubit gate to use, defaults to :class:`~pennylane.ops.CNOT`
-    """
-
-    broadcast(unitary=Rot, pattern="single", wires=wires, parameters=weights)
-
-    n_wires = len(wires)
-    if n_wires > 1:
-        for i in range(n_wires):
-            act_on = wires.subset([i, i + r], periodic_boundary=True)
-            imprimitive(wires=act_on)
-
-
-@template
-def StronglyEntanglingLayers(weights, wires, ranges=None, imprimitive=CNOT):
+class StronglyEntanglingLayers(Operation):
     r"""Layers consisting of single qubit rotations and entanglers, inspired by the circuit-centric classifier design
     `arXiv:1804.00633 <https://arxiv.org/abs/1804.00633>`_.
 
@@ -100,23 +39,95 @@ def StronglyEntanglingLayers(weights, wires, ranges=None, imprimitive=CNOT):
         :width: 60%
         :target: javascript:void(0);
 
+    .. note::
+        The two-qubit gate used as the imprimitive or entangler must not depend on parameters.
+
     Args:
 
         weights (tensor_like): weight tensor of shape ``(L, M, 3)``
-        wires (Iterable or Wires): Wires that the template acts on. Accepts an iterable of numbers or strings, or
-            a Wires object.
+        wires (Iterable): wires that the template acts on
         ranges (Sequence[int]): sequence determining the range hyperparameter for each subsequent layer; if None
                                 using :math:`r=l \mod M` for the :math:`l`th layer and :math:`M` wires.
         imprimitive (pennylane.ops.Operation): two-qubit gate to use, defaults to :class:`~pennylane.ops.CNOT`
 
-    Raises:
-        ValueError: if inputs do not have the correct format
+    .. UsageDetails::
+
+        **Parameter shape**
+
+        The expected shape for the weight tensor can be computed with the static method
+        :meth:`~.SimplifiedTwoDesign.shape` and used when creating randomly
+        initialised weight tensors:
+
+        .. code-block:: python
+
+            shape = StronglyEntanglingLayers.shape(n_layers=2, n_wires=2)
+            weights = np.random.random(size=shape)
+
     """
-    wires = Wires(wires)
-    repeat, ranges = _preprocess(weights, wires, ranges)
+    num_params = 1
+    num_wires = AnyWires
+    par_domain = "A"
 
-    for l in range(repeat):
+    def __init__(self, weights, wires, ranges=None, imprimitive=None, do_queue=True):
 
-        strongly_entangling_layer(
-            weights=weights[l], wires=wires, r=ranges[l], imprimitive=imprimitive
-        )
+        shape = qml.math.shape(weights)
+        self.n_layers = shape[0]
+
+        if len(shape) != 3:
+            raise ValueError(f"Weights tensor must be 3-dimensional; got shape {shape}")
+
+        if shape[1] != len(wires):
+            raise ValueError(
+                f"Weights tensor must have second dimension of length {len(wires)}; got {shape[1]}"
+            )
+
+        if shape[2] != 3:
+            raise ValueError(
+                f"Weights tensor must have third dimension of length 3; got {shape[2]}"
+            )
+
+        if ranges is None:
+            if len(wires) > 1:
+                # tile ranges with iterations of range(1, n_wires)
+                self.ranges = [(l % (len(wires) - 1)) + 1 for l in range(self.n_layers)]
+            else:
+                self.ranges = [0] * self.n_layers
+
+        self.imprimitive = imprimitive or qml.CNOT
+
+        super().__init__(weights, wires=wires, do_queue=do_queue)
+
+    def expand(self):
+
+        with qml.tape.QuantumTape() as tape:
+
+            for l in range(self.n_layers):
+
+                for i in range(len(self.wires)):
+                    qml.Rot(
+                        self.parameters[0][l, i, 0],
+                        self.parameters[0][l, i, 1],
+                        self.parameters[0][l, i, 2],
+                        wires=self.wires[i],
+                    )
+
+                if len(self.wires) > 1:
+                    for i in range(len(self.wires)):
+                        act_on = self.wires.subset([i, i + self.ranges[l]], periodic_boundary=True)
+                        self.imprimitive(wires=act_on)
+
+        return tape
+
+    @staticmethod
+    def shape(n_layers, n_wires):
+        r"""Returns the expected shape of the weights tensor.
+
+        Args:
+            n_layers (int): number of layers
+            n_wires (int): number of wires
+
+        Returns:
+            tuple[int]: shape
+        """
+
+        return n_layers, n_wires, 3
