b50a5fb038a752e634c3a3f27c7eb213397dd7a6
Antal Szava <antalszava@gmail.com>
2021-04-14 18:06:30 -0400

    remove wire tests that are in the device test suite

--- tests/devices/test_default_qubit.py
+++ tests/devices/test_default_qubit.py
@@ -1730,21 +1730,6 @@ class TestWiresIntegration:
 
         return circuit
 
-    def make_circuit_expval(self, wires):
-        """Factory for a qnode returning expvals using arbitrary wire labels."""
-        dev = qml.device("default.qubit", wires=wires)
-        n_wires = len(wires)
-
-        @qml.qnode(dev, diff_method="parameter-shift")
-        def circuit():
-            qml.RX(0.5, wires=wires[0 % n_wires])
-            qml.RY(2., wires=wires[1 % n_wires])
-            if n_wires > 1:
-                qml.CNOT(wires=[wires[0], wires[1]])
-            return [qml.expval(qml.PauliZ(wires=w)) for w in wires]
-
-        return circuit
-
     @pytest.mark.parametrize("wires1, wires2", [(['a', 'c', 'd'], [2, 3, 0]),
                                                 ([-1, -2, -3], ['q1', 'ancilla', 2]),
                                                 (['a', 'c'], [3, 0]),
@@ -1756,20 +1741,6 @@ class TestWiresIntegration:
 
         circuit1 = self.make_circuit_probs(wires1)
         circuit2 = self.make_circuit_probs(wires2)
-
-        assert np.allclose(circuit1(), circuit2(), tol)
-
-    @pytest.mark.parametrize("wires1, wires2", [(['a', 'c', 'd'], [2, 3, 0]),
-                                                ([-1, -2, -3], ['q1', 'ancilla', 2]),
-                                                (['a', 'c'], [3, 0]),
-                                                ([-1, -2], ['ancilla', 2]),
-                                                (['a'], ['nothing']),
-                                                ])
-    def test_wires_expval(self, wires1, wires2, tol):
-        """Test that the expectation of a circuit is independent from the wire labels used."""
-
-        circuit1 = self.make_circuit_expval(wires1)
-        circuit2 = self.make_circuit_expval(wires2)
 
         assert np.allclose(circuit1(), circuit2(), tol)
 
