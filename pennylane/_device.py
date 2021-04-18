8153ef5ac5a9661117fe1658e34d73a19e8074b3
Antal Szava <antalszava@gmail.com>
2021-04-17 22:22:56 -0400

    formatting

--- pennylane/_device.py
+++ pennylane/_device.py
@@ -303,7 +303,9 @@ class Device(abc.ABC):
         Returns:
             bool: whether or not shots are partitioned
         """
-        return self._shot_vector is not None and (len(self._shot_vector) > 1 or self._shot_vector[0].copies > 1)
+        return self._shot_vector is not None and (
+            len(self._shot_vector) > 1 or self._shot_vector[0].copies > 1
+        )
 
     def define_wire_map(self, wires):
         """Create the map from user-provided wire labels to the wire labels used by the device.
