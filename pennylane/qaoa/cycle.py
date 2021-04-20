e1352276760e4ba59637d7e9853be958d8dc77a5
anthayes92 <34694788+anthayes92@users.noreply.github.com>
2021-04-20 12:35:13 +0100

    Update pennylane/qaoa/cycle.py
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>

--- pennylane/qaoa/cycle.py
+++ pennylane/qaoa/cycle.py
@@ -105,7 +105,7 @@ def edge_weight(graph: nx.DiGraph) -> qml.Hamiltonian:
 
         \sum_{(i, j) \in E} Z_{ij}\log c_{ij}.
 
-    where :math:`Z_{ij}` is a qubit Pauli-Z matrix acting upon the qubit specified by the pair
+    where :math:`Z_{ij}` is a qubit Pauli-Z matrix acting upon the wire specified by the pair
     :math:`(i, j)`.
 
     Args:
