19450f08ecf442aa7605b40f12c4936f0f27fa53
Diego <diego_guala@hotmail.com>
2021-04-30 17:00:33 -0500

    Add superposition test

--- tests/templates/test_subroutines/test_quantum_multiplier.py
+++ tests/templates/test_subroutines/test_quantum_multiplier.py
@@ -66,7 +66,52 @@ class TestQuantumMultiplier:
 
         assert int(result_string,2) == int(a,2) * int(b,2)
     
-    # def test_superposition(self,a,b)
-    #     """Tests whether QuantumMultiplier produces correct products of superposition states"""
+    @pytest.mark.parametrize(
+        "a_state,b_state",
+        [
+            ([1/np.sqrt(2), 1/np.sqrt(2)], [1/np.sqrt(2), 1/np.sqrt(2)]),
+            ([1/np.sqrt(4), 1/np.sqrt(4), 1/np.sqrt(4), 1/np.sqrt(4)], [1/np.sqrt(4), 1/np.sqrt(4), 1/np.sqrt(4), 1/np.sqrt(4)]),
+            ([1/np.sqrt(2), 1/np.sqrt(2), 0, 0], [1/np.sqrt(3), np.sqrt(2/3), 0, 0]),
+            ([1j/np.sqrt(2), 1/np.sqrt(2), 0, 0], [1/np.sqrt(4), 1/np.sqrt(4), 1/np.sqrt(2), 0]),
+            ([-1j/np.sqrt(2), 1j/np.sqrt(2), 0, 0], [-1j/np.sqrt(3), np.sqrt(2/3), 0, 0]),
+        ],
+    )
+    def test_superposition(self, a_state, b_state):
+        """Tests whether QuantumMultiplier produces correct products of superposition states"""
+        a_input = np.array(a_state)
+        b_input = np.array(b_state)
+        n_a_wires = int(np.log2(len(a_state)))
+        n_b_wires = int(np.log2(len(b_state)))
+        multiplicand_wires=list(range(n_a_wires))
+        multiplier_wires=list(range(n_a_wires,n_a_wires+n_b_wires))
+        accumulator_wires = list(range(multiplier_wires[-1]+1,len(multiplicand_wires)+multiplier_wires[-1]+1))
+        carry_wires = list(range(accumulator_wires[-1]+1,len(multiplier_wires)+len(multiplicand_wires)+accumulator_wires[-1]+1))
+        work_wire = carry_wires[-1]+1
+        ancilla_wire = work_wire+1
+        n_wires = ancilla_wire+1
+        result_wires = carry_wires[::-1]+accumulator_wires
+        result_wires = result_wires[-(len(multiplier_wires)+len(multiplicand_wires)):]
+        dev = qml.device("default.qubit", wires=n_wires)
+        @qml.qnode(dev)
+        def circuit():
+            qml.templates.MottonenStatePreparation(np.kron(a_input,b_input), wires=multiplicand_wires + multiplier_wires)
+            qml.templates.QuantumMultiplier(multiplicand_wires=multiplicand_wires, multiplier_wires=multiplier_wires, carry_wires=carry_wires, accumulator_wires=accumulator_wires,work_wire=work_wire)
+            return qml.state()
 
+        state = circuit()
+        #state post processing
+        state_value = np.argwhere(state!=0) #the decimal location of the nonzero states
+        probs = [np.abs(state[int(i)])**2 for i in state_value] #the probability of these nonzero states
+        binary_format_string = "0" + str(dev.num_wires) + "b"
+        state_binary = [format(int(state_value), binary_format_string) for state_value in state_value] #the binary value of these nonzero states
+        result_strings = [""]*len(probs) #the binary value of the result
+        for j in range(len(probs)):
+            for bit in result_wires:
+                result_strings[j] = result_strings[j] + state_binary[j][bit]
+        result_value = [int(i,2) for i in result_strings]
+
+
+        expval = np.inner(np.array(result_value),np.array(probs))
+        classexpval = np.inner(np.array([np.abs(i)**2 for i in a_state]),np.array(range(len(a_state))))+ np.inner(np.array([np.abs(i)**2 for i in b_state]),np.array(range(len(b_state))))
+        assert np.isclose(expval, classexpval)
 
