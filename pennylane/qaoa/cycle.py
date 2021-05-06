a8a24da01fd2703682247f4c07ab2984c867aef9
anthayes92 <34694788+anthayes92@users.noreply.github.com>
2021-05-06 13:50:42 +0100

    add private helper functions for squaring Hamiltonian terms and colle… (#1213)
    * start qaoa cycles module, add mapping between edges and wires, add tests
    
    * update formatting and imports
    
    * update qaoa documentation, change cycles.py to cycle.py
    
    * add private helper functions for squaring Hamiltonian terms and collecting duplicate terms, add unit tests
    
    * Apply black to tests
    
    * update WIP entry in CHANGELOG.md
    
    * remove _collect_duplicates()
    
    * Update pennylane/qaoa/cycle.py
    
    Co-authored-by: trbromley <brotho02@gmail.com>
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>

--- pennylane/qaoa/cycle.py
+++ pennylane/qaoa/cycle.py
@@ -14,8 +14,8 @@
 r"""
 Functionality for finding the maximum weighted cycle of directed graphs.
 """
-
-from typing import Dict, Tuple
+import itertools
+from typing import Dict, Tuple, Iterable, List
 import networkx as nx
 import numpy as np
 import pennylane as qml
@@ -272,3 +272,37 @@ def loss_hamiltonian(graph: nx.Graph) -> qml.Hamiltonian:
         ops.append(qml.PauliZ(wires=edges_to_qubits[edge]))
 
     return qml.Hamiltonian(coeffs, ops)
+
+
+def _square_hamiltonian_terms(
+    coeffs: Iterable[float], ops: Iterable[qml.operation.Observable]
+) -> Tuple[List[float], List[qml.operation.Observable]]:
+    """Calculates the coefficients and observables that compose the squared Hamiltonian.
+
+    Args:
+        coeffs (Iterable[float]): coeffients of the input Hamiltonian
+        ops (Iterable[qml.operation.Observable]): observables of the input Hamiltonian
+
+    Returns:
+        Tuple[List[float], List[qml.operation.Observable]]: The list of coefficients and list of observables
+        of the squared Hamiltonian.
+    """
+    squared_coeffs, squared_ops = [], []
+    pairs = [(coeff, op) for coeff, op in zip(coeffs, ops)]
+    products = itertools.product(pairs, repeat=2)
+
+    for (coeff1, op1), (coeff2, op2) in products:
+        squared_coeffs.append(coeff1 * coeff2)
+
+        if isinstance(op1, qml.Identity):
+            squared_ops.append(op2)
+        elif isinstance(op2, qml.Identity):
+            squared_ops.append(op1)
+        elif op1.wires == op2.wires and type(op1) == type(op2):
+            squared_ops.append(qml.Identity(0))
+        elif op2.wires[0] < op1.wires[0]:
+            squared_ops.append(op2 @ op1)
+        else:
+            squared_ops.append(op1 @ op2)
+
+    return squared_coeffs, squared_ops
