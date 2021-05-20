f21405a982d87b39da743ac560579b157b868829
Maria Schuld <mariaschuld@gmail.com>
2021-05-20 08:45:42 +0200

    fix typo

--- pennylane/templates/layers/strongly_entangling.py
+++ pennylane/templates/layers/strongly_entangling.py
@@ -93,7 +93,7 @@ class StronglyEntanglingLayers(Operation):
             else:
                 self.ranges = [0] * self.n_layers
 
-        self.imprimitive = imprimitive or qml.CNOTbenchma
+        self.imprimitive = imprimitive or qml.CNOT
 
         super().__init__(weights, wires=wires, do_queue=do_queue)
 
