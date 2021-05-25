dbd248288b52d2945d8daacc383a35ea99df827b
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2021-05-25 15:30:19 -0400

    Run black.

--- pennylane/fourier/visualize.py
+++ pennylane/fourier/visualize.py
@@ -653,7 +653,7 @@ def radial_box(coeffs, n_inputs, ax, show_freqs=True, colour_dict=None, show_fli
     if ax.size != 2:
         raise ValueError("Matplotlib axis should consist of two subplots.")
 
-    if ax[0].name != 'polar' or ax[1].name != 'polar':
+    if ax[0].name != "polar" or ax[1].name != "polar":
         raise ValueError("Matplotlib axes for radial_box must be polar.")
 
     if colour_dict is None:
