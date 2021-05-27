f1a127a4866cbb8b51815914f9f1abb31c04b0d6
PCesteban <estebandpc@outlook.com>
2021-05-27 05:55:54 -0500

    Suggested change in docstring fixed

--- pennylane/devices/default_qubit_torch.py
+++ pennylane/devices/default_qubit_torch.py
@@ -120,8 +120,7 @@ class DefaultQubitTorch(DefaultQubit):
 
     Args:
          wires (int, Iterable): Number of subsystems represented by the device,
-            or iterable that contains unique labels for the subsystems as numbers (i.e., ``[-1, 0, 2]``)
-            or strings (``['ancilla', 'q1', 'q2']``). Default 1 if not specified.
+            or iterable that contains unique labels for the subsystems. Default 1 if not specified.
         shots (None, int): How many times the circuit should be evaluated (or sampled) to estimate
             the expectation values. Defaults to ``None`` if not specified, which means
             that the device returns analytical results.
