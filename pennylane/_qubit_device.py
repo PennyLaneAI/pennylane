e3e8131b8aa96909960052b5eb073d20aa3aafea
albi3ro <chrissie.c.l@gmail.com>
2021-05-31 17:50:48 -0500

    adjoint bug reverted

--- pennylane/_qubit_device.py
+++ pennylane/_qubit_device.py
@@ -885,12 +885,16 @@ class QubitDevice(Device):
         trainable_param_number = len(tape.trainable_params) - 1
         for op in expanded_ops:
 
-            op_inv = op.adjoint()
-            ket = self._apply_operation(ket, op_inv)
+            if (op.grad_method is not None) and (param_number in tape.trainable_params):
+                d_op_matrix = operation_derivative(op)
+
+            op.inv()
+            # I'd like to  use op.adjoint() here
+            # then we don't have to re-invert the operation at the end
+            ket = self._apply_operation(ket, op)
 
             if op.grad_method is not None:
                 if param_number in tape.trainable_params:
-                    d_op_matrix = operation_derivative(op)
 
                     ket_temp = self._apply_unitary(ket, d_op_matrix, op.wires)
 
@@ -900,7 +904,8 @@ class QubitDevice(Device):
                     trainable_param_number -= 1
                 param_number -= 1
 
-            bras = [self._apply_operation(bra_, op_inv) for bra_ in bras]
+            bras = [self._apply_operation(bra_, op) for bra_ in bras]
+            op.inv()
 
         if return_obs:
             return expectation, jac
