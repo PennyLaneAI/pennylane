81efd9f99fb86c3e4678c91ee4da0e032b46e36c
Maria Schuld <mariaschuld@gmail.com>
2021-06-02 16:54:20 +0200

    Update tests/fourier/test_spectrum.py

--- tests/fourier/test_spectrum.py
+++ tests/fourier/test_spectrum.py
@@ -160,7 +160,7 @@ class TestInterfaces:
             assert v1 == v2
 
     def test_integration_torch(self):
-        """Test that the spectra of a circuit with lots of edge cases is calculated correctly
+        """Test that the spectra of a circuit is calculated correctly
         in the torch interface."""
 
         torch = pytest.importorskip("torch")
