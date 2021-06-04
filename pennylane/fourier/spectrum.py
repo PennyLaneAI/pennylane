6bc15176a9b8c79ab72585280e7d95d51ffb9be1
Maria Schuld <mariaschuld@gmail.com>
2021-06-04 18:15:30 +0200

    Update pennylane/fourier/spectrum.py
    Co-authored-by: Olivia Di Matteo <2068515+glassnotes@users.noreply.github.com>

--- pennylane/fourier/spectrum.py
+++ pennylane/fourier/spectrum.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Contains a QNode transform that computes the frequency spectrum of a quantum
+"""Contains a transform that computes the frequency spectrum of a quantum
 circuit."""
 from itertools import product
 from functools import wraps
