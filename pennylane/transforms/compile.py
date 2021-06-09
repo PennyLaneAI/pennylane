6852c98330246c603818a620fe158bec002cf86d
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2021-06-09 11:29:44 -0400

    Allow for expansion of built-in templates.

--- pennylane/transforms/compile.py
+++ pennylane/transforms/compile.py
@@ -13,6 +13,14 @@
 # limitations under the License.
 """Code for a compilation transform."""
 
+from inspect import getmembers, isclass
+import pennylane.templates as templates
+
+# Get a list of the names of existing templates in PennyLane
+template_list = [cls[0] for cls in getmembers(templates, isclass)]
+
+from pennylane.tape import get_active_tape
+
 from pennylane.transforms import invisible, qfunc_transform, cancel_inverses, cnot_to_cz
 
 default_pipeline = [cancel_inverses]
@@ -57,14 +65,24 @@ def compile(tape, pipeline=default_pipeline, basis_set=None, num_passes=1):
     0: ──H──╭Z──┤ ⟨Z⟩
     1: ──H──╰C──┤
     """
-    if basis_set is not None:
-        expanded_tape = tape.expand(depth=5, stop_at=lambda obj: obj.name in basis_set)
-    else:
-        expanded_tape = tape
 
+    # Expand the tape; this is done to unroll any templates that may be present,
+    # as well as to decompose us over a specified basis set
+    # First, though, we have to stop whatever tape may be recording so that we
+    # don't queue anything as a result of the expansion process
+    current_tape = get_active_tape()
+    with current_tape.stop_recording():
+        if basis_set is not None:
+            expanded_tape = tape.expand(depth=5, stop_at=lambda obj: obj.name in basis_set)
+        else:
+            # Expand only the templates
+            expanded_tape = tape.expand(stop_at=lambda obj: obj.name not in template_list)
+
+    # Apply the compilation transforms
     for _ in range(num_passes):
         for transform in pipeline:
             expanded_tape = invisible(transform.tape_fn)(expanded_tape)
 
+    # Queue the operations on the optimized tape
     for op in expanded_tape.operations + expanded_tape.measurements:
         op.queue()
