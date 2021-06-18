fd7ae4c755636eaa4f8c7e4f0b7cac8a986df697
Romain <rmoyard@gmail.com>
2021-06-18 18:01:22 +0200

    Update pennylane/templates/state_preparations/mottonen.py
    Co-authored-by: antalszava <antalszava@gmail.com>

--- pennylane/templates/state_preparations/mottonen.py
+++ pennylane/templates/state_preparations/mottonen.py
@@ -203,6 +203,7 @@ def _get_alpha_y(a, n, k):
     with np.errstate(divide="ignore", invalid="ignore"):
         division = numerator / denominator
 
+    # Cast the numerator and denominator to ensure compatibility with interfaces
     division = qml.math.cast(division, np.float64)
     denominator = qml.math.cast(denominator, np.float64)
 
