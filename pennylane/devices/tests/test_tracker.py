11ed97b8fb003863dd9a6122ef1e2b5a3f34dffb
Christina Lee <chrissie.c.l@gmail.com>
2021-07-14 16:56:47 -0700

    suppress codefactor complaints

--- pennylane/devices/tests/test_tracker.py
+++ pennylane/devices/tests/test_tracker.py
@@ -11,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""Tests that the device uses a tracker properly if it is supported."""
+# pylint: disable=no-self-use,no-member
 import pytest
 
 import pennylane as qml
