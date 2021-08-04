a0d468cc1ee2c7ed1fe1731c503334e9f85be088
antalszava <antalszava@gmail.com>
2021-08-04 19:36:51 +0200

    Update tests/test_optimize.py
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -922,7 +922,6 @@ class TestOverOpts:
         original_ev = ev
 
         (x, y, ev), cost = opt.step_and_cost(cost, x, y, ev)
-        assert (
-            x == 0
-        )  # check that the argument to RX doesn't change, as the X rotation doesn't influence <Z>
+        # check that the argument to RX doesn't change, as the X rotation doesn't influence <Z>
+        assert (x == 0) 
         assert ev == original_ev
