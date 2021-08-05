6b44284ba5c0efe7d00f9fdb40a5a016266a0ce1
Josh Izaac <josh146@gmail.com>
2021-08-05 14:26:05 +0800

    add comment

--- pennylane/math/utils.py
+++ pennylane/math/utils.py
@@ -56,8 +56,17 @@ def allclose(a, b, rtol=1e-05, atol=1e-08, **kwargs):
     """Wrapper around np.allclose, allowing tensors ``a`` and ``b``
     to differ in type"""
     try:
+        # Some frameworks may provide their own allclose implementation.
+        # Try and use it if available.
         res = np.allclose(a, b, rtol=rtol, atol=atol, **kwargs)
     except (TypeError, AttributeError):
+        # Otherwise, convert the input to NumPy arrays.
+        #
+        # TODO: replace this with a bespoke, framework agnostic
+        # low-level implementation to avoid the NumPy conversion:
+        #
+        #    np.abs(a - b) <= atol + rtol * np.abs(b)
+        #
         t1 = ar.to_numpy(a)
         t2 = ar.to_numpy(b)
         res = np.allclose(t1, t2, rtol=rtol, atol=atol, **kwargs)
