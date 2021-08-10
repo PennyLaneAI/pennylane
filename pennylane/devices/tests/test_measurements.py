d4435a6db97253f990193bf128788cd8e6343b85
trbromley <brotho02@gmail.com>
2021-08-10 15:30:37 -0400

    Fix CI failures

--- pennylane/devices/tests/test_measurements.py
+++ pennylane/devices/tests/test_measurements.py
@@ -444,7 +444,7 @@ class TestTensorExpval:
 
         h_row = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
         h_col = np.array([15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
-        h_data = np.array([-1,  1,  1, -1, -1,  1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1])
+        h_data = np.array([-1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1])
         h = coo_matrix((h_data, (h_row, h_col)), shape=(16, 16))  # XXYY
 
         @qml.qnode(dev, diff_method="parameter-shift")
