734fba2c70560ab6dc9d8b21f076fb61568b44e9
Ankit Khandelwal <ankit27.kh@gmail.com>
2021-08-20 17:41:35 +0530

    Comment added

--- pennylane/qnode.py
+++ pennylane/qnode.py
@@ -188,6 +188,8 @@ class QNode:
         self._tape, self.interface, self.device, tape_diff_options = self.get_tape(
             device, interface, diff_method
         )
+        
+        # Update diff_method to reflect which method has been selected
         if self.diff_method == "best":
             if tape_diff_options["method"] == "device":
                 self.diff_method = "device"
