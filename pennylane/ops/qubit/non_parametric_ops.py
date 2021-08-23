c44ce360d2ffdfbdf402cf4a48755776127ef5c9
charmerDark <vishnuajith@gmail.com>
2021-08-23 22:30:31 +0530

    Update pennylane/ops/qubit/non_parametric_ops.py
    Co-authored-by: antalszava <antalszava@gmail.com>

--- pennylane/ops/qubit/non_parametric_ops.py
+++ pennylane/ops/qubit/non_parametric_ops.py
@@ -692,7 +692,7 @@ class SISWAP(Operation):
             CNOT(wires=[wires[0], wires[1]]),
             SX(wires=0),
             qml.RZ(7 * np.pi / 4, wires=wires[0]),
-            SX(wires=0),
+            SX(wires=wires[0]),
             qml.RZ(np.pi / 2, wires=wires[0]),
             SX(wires=wires[1]),
             qml.RZ(7 * np.pi / 4, wires=wires[1]),
