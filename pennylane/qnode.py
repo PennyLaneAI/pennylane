7594a3ea40c1820999256992cb377f016eb0e508
Ankit Khandelwal <ankit27.kh@gmail.com>
2021-08-24 08:13:52 +0530

    Update qnode.py

--- pennylane/qnode.py
+++ pennylane/qnode.py
@@ -106,7 +106,7 @@ class QNode:
             * ``"finite-diff"``: Uses numerical finite-differences for all quantum operation
               arguments.
 
-            * ``None``: Qnode can not be differentiated.
+            * ``None``: Qnode can not be differentiated. Works the same as `interface=None`.
 
         mutable (bool): If True, the underlying quantum circuit is re-constructed with
             every evaluation. This is the recommended approach, as it allows the underlying
