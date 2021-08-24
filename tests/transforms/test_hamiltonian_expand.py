a1f88d19054ec27b2dd918d40e5980db39758a36
Maria Schuld <mariaschuld@gmail.com>
2021-08-24 11:51:55 +0200

    fix another test

--- tests/transforms/test_hamiltonian_expand.py
+++ tests/transforms/test_hamiltonian_expand.py
@@ -139,7 +139,7 @@ class TestHamiltonianExpval:
         with pytest.raises(ValueError, match=r"Passed tape must end in"):
             tapes, fn = qml.transforms.hamiltonian_expand(tape)
 
-    def test_hamiltonian_dif_autograd(self):
+    def test_hamiltonian_dif_autograd(self, tol):
         """Tests that the hamiltonian_expand tape transform is differentiable with the Autograd interface"""
 
         H = qml.Hamiltonian(
@@ -153,9 +153,9 @@ class TestHamiltonianExpval:
             np.array(0.4),
             np.array(-0.5),
             np.array(0.7),
-            np.array(0.4),
-            np.array(-0.5),
-            np.array(0.7),
+            np.array(-0.2),
+            np.array(0.5),
+            np.array(1.0),
         ]
         output = 0.42294409781940356
         output2 = [
@@ -165,9 +165,9 @@ class TestHamiltonianExpval:
             -1.94289029e-09,
             3.50307411e-01,
             -3.41123470e-01,
-            0.0,  # these three are the Hamiltonian parameters
             0.0,
-            0.0,
+            -0.43657,
+            0.64123,
         ]
 
         with qml.tape.JacobianTape() as tape:
@@ -192,7 +192,7 @@ class TestHamiltonianExpval:
         assert np.isclose(cost(var), output)
         grad = qml.grad(cost)(var)
         for g, o in zip(grad, output2):
-            assert np.allclose(g, o)
+            assert np.allclose(g, o, atol=tol)
 
     def test_hamiltonian_dif_tensorflow(self):
         """Tests that the hamiltonian_expand tape transform is differentiable with the Tensorflow interface"""
