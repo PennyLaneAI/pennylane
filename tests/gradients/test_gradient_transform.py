1548b5d12577ba77822b42a1d7511a32b1915fc2
Josh Izaac <josh146@gmail.com>
2021-08-26 19:35:51 +0800

    update

--- tests/gradients/test_gradient_transform.py
+++ tests/gradients/test_gradient_transform.py
@@ -207,6 +207,62 @@ class TestGradientTransformIntegration:
         assert np.allclose(res, expected, atol=tol, rtol=0)
         assert spy.call_args[0][0].operations[0].name == "RX"
 
+    def test_permutated_arguments(self, tol):
+        """Test that a gradient transform acts on QNodes
+        correctly when the QNode arguments are permuted"""
+        dev = qml.device("default.qubit", wires=2)
+
+        @qml.qnode(dev)
+        def circuit(weights):
+            qml.RX(weights[1], wires=[0])
+            qml.RY(weights[0], wires=[1])
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.PauliZ(0)), qml.var(qml.PauliX(1))
+
+        w = np.array([-0.654, 0.543], requires_grad=True)
+        res = qml.gradients.param_shift(circuit)(w)
+
+        expected = qml.jacobian(circuit)(w)
+        assert np.allclose(res, expected, atol=tol, rtol=0)
+
+    def test_classical_processing_arguments(self, tol):
+        """Test that a gradient transform acts on QNodes
+        correctly when the QNode arguments are classical processed"""
+        dev = qml.device("default.qubit", wires=2)
+
+        @qml.qnode(dev)
+        def circuit(weights):
+            qml.RX(weights[0] ** 2, wires=[0])
+            qml.RY(weights[1], wires=[1])
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.PauliZ(0))
+
+        w = np.array([0.543, -0.654], requires_grad=True)
+        res = qml.gradients.param_shift(circuit)(w)
+
+        x, y = w
+        expected = [-2 * x * np.sin(x ** 2), 0]
+        assert np.allclose(res, expected, atol=tol, rtol=0)
+
+    def test_advanced_classical_processing_arguments(self, tol):
+        """Test that a gradient transform acts on QNodes
+        correctly when the QNode arguments are classical processed,
+        and the input weights and the output weights have weird shape."""
+        dev = qml.device("default.qubit", wires=2)
+
+        @qml.qnode(dev)
+        def circuit(weights):
+            qml.RX(weights[0, 0] ** 2, wires=[0])
+            qml.RY(weights[0, 1], wires=[1])
+            qml.CNOT(wires=[0, 1])
+            return qml.probs(wires=[0, 1])
+
+        w = np.array([[0.543, -0.654], [0.0, 0.0]], requires_grad=True)
+        res = qml.gradients.param_shift(circuit)(w)
+
+        expected = qml.jacobian(circuit)(w)
+        assert np.allclose(res, expected, atol=tol, rtol=0)
+
     def test_differentiation(self, tol):
         """Test that a gradient transform remains differentiable"""
         dev = qml.device("default.qubit", wires=2)
