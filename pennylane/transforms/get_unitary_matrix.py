b0beacbf6b2c4a16a832730b086cdc827574b3c8
ingstra <ingstra@student.chalmers.se>
2021-08-27 20:48:39 +0200

    only works when there is only one gate in the quantum function

--- pennylane/transforms/get_unitary_matrix.py
+++ pennylane/transforms/get_unitary_matrix.py
@@ -13,7 +13,7 @@ def get_unitary_matrix(fn, wire_order):
     n_wires = len(wire_order)
     # print(wires)
 
-    mat = np.eye(2 ** n_wires)
+    matrix = np.eye(2 ** n_wires)
 
     @wraps(fn)
     def wrapper(*args, **kwargs):
@@ -33,24 +33,22 @@ def get_unitary_matrix(fn, wire_order):
             tape = fn(*args, **kwargs)
         #### NOTE took the above from qml.adjoint. Appropriate here?
 
-        mat = np.eye(2 ** n_wires)
+        matrix = np.eye(2 ** n_wires)
 
         for op in tape.operations:
 
-            # ASSUME ADJACENT WIRES
-            # print("in main1")
-
-            tmpmat = tmp_func(op, wire_order, mat, np.eye(2 ** n_wires))
-            # print("tmpmat",tmpmat)
-            mat = np.dot(tmpmat, mat)
-            # print("in main2")
-            # print("MAT in main \n", mat)
-        return mat
+            tmpmat = _get_op_matrix(op, wire_order, matrix, np.eye(2 ** n_wires))
+            print("tmpmat", tmpmat)
+            print("MAT in main1 \n", matrix)
+            matrix = np.dot(tmpmat, matrix)
+            print("in main2")
+            print("MAT in main \n", matrix)
+        return matrix
 
     return wrapper
 
 
-def tmp_func(op, wire_order, mat, SWAP):
+def _get_op_matrix(op, wire_order, matrix, SWAP):
     # check if the operator wires match the wire order
     s = SequenceMatcher(lambda x: x == " ", op.wires.tolist(), wire_order.tolist())
 
@@ -63,22 +61,23 @@ def tmp_func(op, wire_order, mat, SWAP):
 
     if match[2] == len(op.wires):
 
+        # match[1] is the index of the first match in wire_order
         I_left = np.eye(2 ** match[1])
         I_right = np.eye(2 ** (n_wires - len(op.wires) - match[1]))
-        # print("MAT before op\n", mat)
+        # print("MAT before op\n", matrix)
 
-        mat = np.dot(np.kron(I_left, np.kron(op.matrix, I_right)), mat)
+        # matrix representation of all operators so far
+        matrix = np.dot(np.kron(I_left, np.kron(op.matrix, I_right)), matrix)
         # print("SWAP",SWAP,"\n")
-        # print("MAT after op before swap\n", mat)
-        mat = SWAP @ mat
-        # print("MAT after swap\n", mat)
-        return mat
+        print("MAT after op before swap\n", matrix)
+        # Swap back if wires were swapped
+        matrix = SWAP @ matrix
+        print("MAT after swap\n", matrix)
+        return matrix
 
     # if there are non-adjacent wires
     else:
         print(match, match[2])
-        matches = s.get_matching_blocks()
-        print(wire_order, op.wires, "\n", matches)
 
         wire_order_indices = wire_order.toarray().searchsorted(op.wires.toarray())
         for idx, position in enumerate(wire_order_indices[1:]):
@@ -108,13 +107,13 @@ def tmp_func(op, wire_order, mat, SWAP):
             # SWAP1 = swap(3, targets=[2,0]).full().real
             SWAP = np.matmul(SWAP, SWAP1)
             # SWAP =  np.kron(np.eye(2), qml.SWAP.matrix)
-            mat = SWAP @ mat
+            matrix = SWAP @ matrix
             wire_order = wire_order.tolist()
             wire_order[swaps[0]], wire_order[swaps[1]] = wire_order[swaps[1]], wire_order[swaps[0]]
             print("order", wire_order)
             wire_order = Wires(wire_order)
-            # print("mat in swap\n", mat)
-            # print("SWAP in swap\n", SWAP)
+            # print("mat in swap\n", matrix)
+            print("SWAP in swap\n", SWAP)
 
         print("recursion")
-        return tmp_func(op, wire_order, mat, SWAP)
+        return _get_op_matrix(op, wire_order, matrix, SWAP)
