40d9e27532a0726b3cfaf09c4d0ccb76743ee2a7
ingstra <ingstra@student.chalmers.se>
2021-08-27 17:26:15 +0200

    works for adjacent wires

--- pennylane/transforms/get_unitary_matrix.py
+++ pennylane/transforms/get_unitary_matrix.py
@@ -4,19 +4,20 @@ from functools import wraps, reduce
 from itertools import repeat
 from pennylane.tape import QuantumTape, get_active_tape
 import numpy as np
+from difflib import SequenceMatcher
 
 
-def get_unitary_matrix(fn, wires):
+def get_unitary_matrix(fn, wire_order):
     # wires or wire_order?
-    wires = Wires(wires)
-    n_wires = len(wires)
+    wire_order = Wires(wire_order)
+    n_wires = len(wire_order)
     # print(wires)
 
     mat = np.eye(2 ** n_wires)
 
     @wraps(fn)
     def wrapper(*args, **kwargs):
-            ######### NOTE BELOW
+        ######### NOTE BELOW
         active_tape = get_active_tape()
 
         if active_tape is not None:
@@ -36,11 +37,22 @@ def get_unitary_matrix(fn, wires):
 
         for op in tape.operations:
 
-            # Single qubit gates
-            if len(op.wires) == 1:
-                wire_pos = wires.index(op.wires)
-                I_left = np.eye(2 ** wire_pos)
-                I_right = np.eye(2 ** (n_wires - wire_pos - 1))
+            # ASSUME ADJACENT WIRES
+
+            # check if the operator wires match the wire order
+            s = SequenceMatcher(lambda x: x == " ", op.wires.tolist(), wire_order.tolist())
+
+            match = s.find_longest_match()
+            print(match, match[2])
+            print(wire_order, op.wires)
+
+            # if all operator wires are adjacent
+            # match [2] is the length of the matching sequence
+            if match[2] == len(op.wires):
+
+                I_left = np.eye(2 ** match[1])
+                I_right = np.eye(2 ** (n_wires - len(op.wires) - match[1]))
+
                 mat = np.dot(np.kron(I_left, np.kron(op.matrix, I_right)), mat)
 
         return mat
