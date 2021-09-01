22173acc7f0f25494c0940fc8d6e2eb59d6be893
soranjh <soranjh@yahoo.com>
2021-09-01 17:44:40 -0400

    add test for generate_params

---
+++ tests/hf/test_integrals.py
@@ -0,0 +1,40 @@
+# Copyright 2018-2021 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Unit tests for functions needed to computing integrals over basis functions.
+"""
+
+import pytest
+import numpy as np
+from pennylane import numpy as pnp
+from pennylane.hf.integrals import generate_params
+
+
+@pytest.mark.parametrize(
+    ("alpha", "coeff", "r"),
+    [
+        (
+            pnp.array([3.42525091, 0.62391373, 0.1688554], requires_grad=True),
+            pnp.array([0.15432897, 0.53532814, 0.44463454], requires_grad=True),
+            pnp.array([0.0, 0.0, 0.0], requires_grad=False),
+        ),
+    ],
+)
+def test_generate_params(alpha, coeff, r):
+    r"""Test that test_generate_params returns correct basis set parameters."""
+    params = [alpha, coeff, r]
+    args = [p for p in [alpha, coeff, r] if p.requires_grad]
+    basis_params = generate_params(params, args)
+
+    assert np.allclose(basis_params, (alpha, coeff, r))
