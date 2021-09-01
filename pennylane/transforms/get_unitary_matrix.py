bbac1e604e1b1d228ca2c1a2e191d79a07ac6b40
ingstra <ingstra@student.chalmers.se>
2021-09-01 20:56:50 +0200

    added licence/copyright

--- pennylane/transforms/get_unitary_matrix.py
+++ pennylane/transforms/get_unitary_matrix.py
@@ -1,3 +1,17 @@
+# Copyright 2018-2021 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from difflib import SequenceMatcher
 from functools import wraps, reduce
 
@@ -84,7 +98,6 @@ def _get_op_matrix(op, wire_order, SWAP_list):
             if m[0] != m[1]:
                 swaps = np.sort([m[0], m[1]])
 
-                # print("swap qubit index %d with qubit index %d" % (swaps[0], swaps[1]))
                 # construct SWAP operator to swap wires m[0] and m[1]
                 reshapedI = np.reshape(np.eye(2 ** n_wires), [2] * n_wires * 2)
 
