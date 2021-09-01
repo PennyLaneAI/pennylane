4586411c6236a19e7a6da15adeb69f8381640a91
Josh Izaac <josh146@gmail.com>
2021-09-02 02:28:09 +0800

    more

--- tests/interfaces/test_batch_autograd.py
+++ tests/interfaces/test_batch_autograd.py
@@ -48,24 +48,6 @@ class TestAutogradExecuteUnitTests:
         for args in spy.call_args_list:
             assert args[1]["shift"] == np.pi / 4
 
-    def test_unknown_gradient_fn_error(self):
-        """Test that an error is raised if an unknown gradient function
-        is passed"""
-        a = np.array([0.1, 0.2], requires_grad=True)
-
-        dev = qml.device("default.qubit", wires=1)
-
-        def cost(a, device):
-            with qml.tape.JacobianTape() as tape:
-                qml.RY(a[0], wires=0)
-                qml.RX(a[1], wires=0)
-                qml.expval(qml.PauliZ(0))
-
-            return execute([tape], device, gradient_fn=lambda x: x)[0]
-
-        with pytest.raises(ValueError, match="Unknown gradient function"):
-            res = qml.jacobian(cost)(a, device=dev)
-
     def test_incorrect_mode(self):
         """Test that an error is raised if an gradient transform
         is used with mode=forward"""
