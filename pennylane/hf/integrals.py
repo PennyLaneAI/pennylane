1223ad0f43684166a2fab78650e76acadce9b3dd
soranjh <40344468+soranjh@users.noreply.github.com>
2021-09-10 13:48:11 -0400

    Add overlap integral for HF solver (#1621)
    * add functions to compute overlap integral

--- pennylane/hf/integrals.py
+++ pennylane/hf/integrals.py
@@ -14,8 +14,8 @@
 """
 This module contains the functions needed for computing integrals over basis functions.
 """
+# pylint: disable= unbalanced-tuple-unpacking, too-many-arguments
 import autograd.numpy as anp
-import numpy as np
 from scipy.special import factorial2 as fac2
 
 
@@ -104,3 +104,204 @@ def contracted_norm(l, alpha, a):
     ).sum()
     n = 1 / anp.sqrt(c * s)
     return n
+
+
+def _generate_params(params, args):
+    """Generate basis set parameters. The default values are used for the non-differentiable
+    parameters and the user-defined values are used for the differentiable ones.
+
+    Args:
+        params (list(array[float])): default values of the basis set parameters
+        args (list(array[float])): initial values of the differentiable basis set parameters
+
+    Returns:
+        list(array[float]): basis set parameters
+    """
+    basis_params = []
+    c = 0
+    for p in params:
+        if p.requires_grad:
+            basis_params.append(args[c])
+            c += 1
+        else:
+            basis_params.append(p)
+    return basis_params
+
+
+def expansion(la, lb, ra, rb, alpha, beta, t):
+    r"""Compute Hermite Gaussian expansion coefficients recursively for two Gaussian functions.
+
+    An overlap distribution, which defines the product of two Gaussians, can be written as a Hermite
+    expansion as [`Helgaker (1995) p798 <https://www.worldscientific.com/doi/abs/10.1142/9789812832115_0001>`_]
+
+    .. math::
+
+        \Omega_{ij} = \sum_{t=0}^{i+j} E_t^{ij} \Lambda_t,
+
+    where :math:`\Lambda` is a Hermite polynomial of degree :math:`t`, :math:`E` denotes the expansion
+    coefficients, :math:`\Omega_{ij} = G_i G_j`, and :math:`G` is a Gaussian function. The overlap
+    integral between two Gaussian functions can be simply computed by integrating over the overlap
+    distribution which requires obtaining the expansion coefficients. This can be done recursively
+    as [`Helgaker (1995) p799 <https://www.worldscientific.com/doi/abs/10.1142/9789812832115_0001>`_]
+
+    .. math::
+
+        E_t^{i+1,j} = \frac{1}{2p} E_{t-1}^{ij} - \frac{qr}{\alpha} E_{t}^{ij} + (t+1) E_{t+1}^{ij},
+
+    and
+
+    .. math::
+
+        E_t^{i,j+1} = \frac{1}{2p} E_{t-1}^{ij} + \frac{qr}{\beta} E_{t}^{ij} + (t+1) E_{t+1}^{ij},
+
+    where :math:`p = \alpha + \beta` and :math:`q = \alpha \beta / (\alpha + \beta)` are computed
+    from the Gaussian exponents :math:`\alpha, \beta` and the position :math:`r` is computed as
+    :math:`r = r_\alpha - r_\beta`. The starting coefficient is
+
+    .. math::
+
+        E_0^{00} = e^{-qr^2},
+
+    and :math:`E_t^{ij} = 0` is :math:`t < 0` or :math:`t > (i+j)`.
+
+    Args:
+        la (integer): angular momentum component for the first Gaussian function
+        lb (integer): angular momentum component for the second Gaussian function
+        ra (float): position component of the the first Gaussian function
+        rb (float): position component of the the second Gaussian function
+        alpha (array[float]): exponent of the first Gaussian function
+        beta (array[float]): exponent of the second Gaussian function
+        t (integer): number of nodes in the Hermite Gaussian
+
+    Returns:
+        array[float]: expansion coefficients for each Gaussian combination
+
+    **Example**
+
+    >>> la, lb = 0, 0
+    >>> ra, rb = 0.0, 0.0
+    >>> alpha = np.array([3.42525091])
+    >>> beta =  np.array([3.42525091])
+    >>> t = 0
+    >>> c = expansion(la, lb, ra, rb, alpha, beta, t)
+    >>> c
+    array([1.])
+    """
+    p = alpha + beta
+    q = alpha * beta / p
+    r = ra - rb
+
+    if la == lb == t == 0:
+        return anp.exp(-q * r ** 2)
+
+    if t < 0 or t > (la + lb):
+        return 0.0
+
+    if lb == 0:
+        return (
+            (1 / (2 * p)) * expansion(la - 1, lb, ra, rb, alpha, beta, t - 1)
+            - (q * r / alpha) * expansion(la - 1, lb, ra, rb, alpha, beta, t)
+            + (t + 1) * expansion(la - 1, lb, ra, rb, alpha, beta, t + 1)
+        )
+
+    return (
+        (1 / (2 * p)) * expansion(la, lb - 1, ra, rb, alpha, beta, t - 1)
+        + (q * r / beta) * expansion(la, lb - 1, ra, rb, alpha, beta, t)
+        + (t + 1) * expansion(la, lb - 1, ra, rb, alpha, beta, t + 1)
+    )
+
+
+def gaussian_overlap(la, lb, ra, rb, alpha, beta):
+    r"""Compute overlap integral for two primitive Gaussian functions.
+
+    The overlap integral between two Gaussian functions denoted by :math:`a` and :math:`b` can be
+    computed as [`Helgaker (1995) p803 <https://www.worldscientific.com/doi/abs/10.1142/9789812832115_0001>`_]:
+
+    .. math::
+
+        S_{ab} = E^{ij} E^{kl} E^{mn} \left (\frac{\pi}{p}  \right )^{3/2},
+
+    where :math:`E` is a coefficient that can be computed recursively, :math:`i-n` are the angular
+    momentum quantum numbers corresponding to different Cartesian components and :math:`p` is
+    computed from the exponents of the two Gaussian functions as :math:`p = \alpha + \beta`.
+
+    Args:
+        la (integer): angular momentum for the first Gaussian function
+        lb (integer): angular momentum for the second Gaussian function
+        ra (float): position vector of the the first Gaussian function
+        rb (float): position vector of the the second Gaussian function
+        alpha (array[float]): exponent of the first Gaussian function
+        beta (array[float]): exponent of the second Gaussian function
+
+    Returns:
+        array[float]: overlap integral between primitive Gaussian functions
+
+    **Example**
+
+    >>> la, lb = (0, 0, 0), (0, 0, 0)
+    >>> ra, rb = np.array(([0., 0., 0.]), np.array(([0., 0., 0.])
+    >>> alpha = np.array([np.pi/2])
+    >>> beta = np.array([np.pi/2])
+    >>> o = gaussian_overlap(la, lb, ra, rb, alpha, beta)
+    >>> o
+    array([1.])
+    """
+    p = alpha + beta
+    s = 1.0
+    for i in range(3):
+        s = s * anp.sqrt(anp.pi / p) * expansion(la[i], lb[i], ra[i], rb[i], alpha, beta, 0)
+    return s
+
+
+def generate_overlap(basis_a, basis_b):
+    r"""Return a function that computes the overlap integral for two contracted Gaussian orbitals.
+
+    Args:
+        basis_a (BasisFunction): first basis function
+        basis_b (BasisFunction): second basis function
+
+    Returns:
+        function: function that computes the overlap integral
+
+    **Example**
+
+    >>> symbols  = ['H', 'H']
+    >>> geometry = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]], requires_grad = False)
+    >>> mol = Molecule(symbols, geometry)
+    >>> args = []
+    >>> generate_overlap(mol.basis_set[0], mol.basis_set[0])(*args)
+    1.0
+    """
+
+    def overlap_integral(*args):
+        r"""Normalize and compute the overlap integral for two contracted Gaussian functions.
+
+        Args:
+            args (array[float]): initial values of the differentiable parameters
+
+        Returns:
+            array[float]: the overlap integral between two contracted Gaussian orbitals
+        """
+
+        args_a = [i[0] for i in args]
+        args_b = [i[1] for i in args]
+
+        alpha, ca, ra = _generate_params(basis_a.params, args_a)
+        beta, cb, rb = _generate_params(basis_b.params, args_b)
+
+        ca = ca * primitive_norm(basis_a.l, alpha)
+        cb = cb * primitive_norm(basis_b.l, beta)
+
+        na = contracted_norm(basis_a.l, alpha, ca)
+        nb = contracted_norm(basis_b.l, beta, cb)
+
+        return (
+            na
+            * nb
+            * (
+                (ca[:, anp.newaxis] * cb)
+                * gaussian_overlap(basis_a.l, basis_b.l, ra, rb, alpha[:, anp.newaxis], beta)
+            ).sum()
+        )
+
+    return overlap_integral
--- tests/hf/test_integrals.py
+++ tests/hf/test_integrals.py
@@ -14,9 +14,19 @@
 """
 Unit tests for functions needed to computing integrals over basis functions.
 """
+import autograd
 import numpy as np
 import pytest
-from pennylane.hf.integrals import contracted_norm, primitive_norm
+from pennylane import numpy as pnp
+from pennylane.hf.integrals import (
+    contracted_norm,
+    expansion,
+    gaussian_overlap,
+    generate_overlap,
+    _generate_params,
+    primitive_norm,
+)
+from pennylane.hf.molecule import Molecule
 
 
 @pytest.mark.parametrize(
@@ -47,3 +57,220 @@ def test_gaussian_norm(l, alpha, n):
 def test_contraction_norm(l, alpha, a, n):
     r"""Test that the computed normalization constant of a contracted Gaussian function is correct."""
     assert np.allclose(contracted_norm(l, alpha, a), n)
+
+
+@pytest.mark.parametrize(
+    ("alpha", "coeff", "r"),
+    [
+        (
+            pnp.array([3.42525091, 0.62391373, 0.1688554], requires_grad=True),
+            pnp.array([0.15432897, 0.53532814, 0.44463454], requires_grad=True),
+            pnp.array([0.0, 0.0, 0.0], requires_grad=False),
+        ),
+        (
+            pnp.array([3.42525091, 0.62391373, 0.1688554], requires_grad=False),
+            pnp.array([0.15432897, 0.53532814, 0.44463454], requires_grad=False),
+            pnp.array([0.0, 0.0, 0.0], requires_grad=True),
+        ),
+    ],
+)
+def test_generate_params(alpha, coeff, r):
+    r"""Test that test_generate_params returns correct basis set parameters."""
+    params = [alpha, coeff, r]
+    args = [p for p in [alpha, coeff, r] if p.requires_grad]
+    basis_params = _generate_params(params, args)
+
+    assert np.allclose(basis_params, (alpha, coeff, r))
+
+
+@pytest.mark.parametrize(
+    ("la", "lb", "ra", "rb", "alpha", "beta", "t", "c"),
+    [
+        (
+            0,
+            0,
+            pnp.array([1.2]),
+            pnp.array([1.2]),
+            pnp.array([3.42525091]),
+            pnp.array([3.42525091]),
+            0,
+            pnp.array([1.0]),
+        ),
+        (
+            1,
+            0,
+            pnp.array([0.0]),
+            pnp.array([0.0]),
+            pnp.array([3.42525091]),
+            pnp.array([3.42525091]),
+            0,
+            pnp.array([0.0]),
+        ),
+        (
+            1,
+            1,
+            pnp.array([0.0]),
+            pnp.array([10.0]),
+            pnp.array([3.42525091]),
+            pnp.array([3.42525091]),
+            0,
+            pnp.array([0.0]),
+        ),
+    ],
+)
+def test_expansion(la, lb, ra, rb, alpha, beta, t, c):
+    r"""Test that expansion function returns correct value."""
+    assert np.allclose(expansion(la, lb, ra, rb, alpha, beta, t), c)
+    assert np.allclose(expansion(la, lb, ra, rb, alpha, beta, -1), pnp.array([0.0]))
+    assert np.allclose(expansion(0, 1, ra, rb, alpha, beta, 2), pnp.array([0.0]))
+
+
+@pytest.mark.parametrize(
+    ("la", "lb", "ra", "rb", "alpha", "beta", "o"),
+    [
+        # two normalized s orbitals
+        (
+            (0, 0, 0),
+            (0, 0, 0),
+            pnp.array([0.0, 0.0, 0.0]),
+            pnp.array([0.0, 0.0, 0.0]),
+            pnp.array([pnp.pi / 2]),
+            pnp.array([pnp.pi / 2]),
+            pnp.array([1.0]),
+        ),
+        (
+            (0, 0, 0),
+            (0, 0, 0),
+            pnp.array([0.0, 0.0, 0.0]),
+            pnp.array([20.0, 0.0, 0.0]),
+            pnp.array([3.42525091]),
+            pnp.array([3.42525091]),
+            pnp.array([0.0]),
+        ),
+        (
+            (1, 0, 0),
+            (0, 0, 1),
+            pnp.array([0.0, 0.0, 0.0]),
+            pnp.array([0.0, 0.0, 0.0]),
+            pnp.array([6.46480325]),
+            pnp.array([6.46480325]),
+            pnp.array([0.0]),
+        ),
+    ],
+)
+def test_gaussian_overlap(la, lb, ra, rb, alpha, beta, o):
+    r"""Test that gaussian overlap function returns a correct value."""
+    assert np.allclose(gaussian_overlap(la, lb, ra, rb, alpha, beta), o)
+
+
+@pytest.mark.parametrize(
+    ("symbols", "geometry", "alpha", "coef", "r", "o_ref"),
+    [
+        (
+            ["H", "H"],
+            pnp.array([[0.0, 0.0, 0.0], [0.0, 0.0, 20.0]], requires_grad=False),
+            pnp.array(
+                [[3.42525091, 0.62391373, 0.1688554], [3.42525091, 0.62391373, 0.1688554]],
+                requires_grad=False,
+            ),
+            pnp.array(
+                [[0.15432897, 0.53532814, 0.44463454], [0.15432897, 0.53532814, 0.44463454]],
+                requires_grad=True,
+            ),
+            pnp.array([[0.0, 0.0, 0.0], [0.0, 0.0, 20.0]], requires_grad=True),
+            pnp.array([0.0]),
+        ),
+        (
+            ["H", "H"],
+            pnp.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], requires_grad=False),
+            pnp.array(
+                [[3.42525091, 0.62391373, 0.1688554], [3.42525091, 0.62391373, 0.1688554]],
+                requires_grad=False,
+            ),
+            pnp.array(
+                [[0.15432897, 0.53532814, 0.44463454], [0.15432897, 0.53532814, 0.44463454]],
+                requires_grad=False,
+            ),
+            pnp.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], requires_grad=True),
+            pnp.array([1.0]),
+        ),
+        (
+            ["H", "H"],
+            pnp.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], requires_grad=True),
+            pnp.array(
+                [[3.42525091, 0.62391373, 0.1688554], [3.42525091, 0.62391373, 0.1688554]],
+                requires_grad=True,
+            ),
+            pnp.array(
+                [[0.15432897, 0.53532814, 0.44463454], [0.15432897, 0.53532814, 0.44463454]],
+                requires_grad=True,
+            ),
+            pnp.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], requires_grad=True),
+            pnp.array([1.0]),
+        ),
+    ],
+)
+def test_generate_overlap(symbols, geometry, alpha, coef, r, o_ref):
+    r"""Test that generate_overlap function returns a correct value for the overlap integral."""
+    mol = Molecule(symbols, geometry)
+    basis_a = mol.basis_set[0]
+    basis_b = mol.basis_set[1]
+    args = [p for p in [alpha, coef, r] if p.requires_grad]
+
+    o = generate_overlap(basis_a, basis_b)(*args)
+    assert np.allclose(o, o_ref)
+
+
+@pytest.mark.parametrize(
+    ("symbols", "geometry", "alpha", "coeff"),
+    [
+        (
+            ["H", "H"],
+            pnp.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]], requires_grad=False),
+            pnp.array(
+                [[3.42525091, 0.62391373, 0.1688554], [3.42525091, 0.62391373, 0.1688554]],
+                requires_grad=True,
+            ),
+            pnp.array(
+                [[0.15432897, 0.53532814, 0.44463454], [0.15432897, 0.53532814, 0.44463454]],
+                requires_grad=True,
+            ),
+        ),
+    ],
+)
+def test_gradient(symbols, geometry, alpha, coeff):
+    r"""Test that the overlap gradient computed with respect to the basis parameters is correct."""
+    mol = Molecule(symbols, geometry, alpha=alpha, coeff=coeff)
+    basis_a = mol.basis_set[0]
+    basis_b = mol.basis_set[1]
+    args = [mol.alpha, mol.coeff]
+
+    g_alpha = autograd.grad(generate_overlap(basis_a, basis_b), argnum=0)(*args)
+    g_coeff = autograd.grad(generate_overlap(basis_a, basis_b), argnum=1)(*args)
+
+    # compute overlap gradients with respect to alpha and coeff using finite diff
+    delta = 0.0001
+    g_ref_alpha = np.zeros(6).reshape(alpha.shape)
+    g_ref_coeff = np.zeros(6).reshape(coeff.shape)
+
+    for i in range(len(alpha)):
+        for j in range(len(alpha[0])):
+
+            alpha_minus = alpha.copy()
+            alpha_plus = alpha.copy()
+            alpha_minus[i][j] = alpha_minus[i][j] - delta
+            alpha_plus[i][j] = alpha_plus[i][j] + delta
+            o_minus = generate_overlap(basis_a, basis_b)(*[alpha_minus, coeff])
+            o_plus = generate_overlap(basis_a, basis_b)(*[alpha_plus, coeff])
+            g_ref_alpha[i][j] = (o_plus - o_minus) / (2 * delta)
+
+            coeff_minus = coeff.copy()
+            coeff_plus = coeff.copy()
+            coeff_minus[i][j] = coeff_minus[i][j] - delta
+            coeff_plus[i][j] = coeff_plus[i][j] + delta
+            o_minus = generate_overlap(basis_a, basis_b)(*[alpha, coeff_minus])
+            o_plus = generate_overlap(basis_a, basis_b)(*[alpha, coeff_plus])
+            g_ref_coeff[i][j] = (o_plus - o_minus) / (2 * delta)
+
+    assert np.allclose(g_alpha, g_ref_alpha)
+    assert np.allclose(g_coeff, g_ref_coeff)
