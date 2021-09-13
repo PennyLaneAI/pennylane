58cce92c9fc27066602a2fa655fce177e99dfd02
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2021-09-13 12:45:46 -0400

    Cast to 1j to handle square root of negative values.

--- pennylane/transforms/decompositions/utils.py
+++ pennylane/transforms/decompositions/utils.py
@@ -99,11 +99,11 @@ def _su2su2_to_tensor_products(U):
 
     # From the definition of A \otimes B, C1 C4^\dag = a1^2 I, so we can extract a1
     C14 = math.dot(C1, math.conj(math.T(C4)))
-    a1 = math.sqrt(C14[0, 0])
+    a1 = math.sqrt(math.cast_like(C14[0, 0], 1j))
 
     # Similarly, -C2 C3^\dag = a2^2 I, so we can extract a2
     C23 = math.dot(C2, math.conj(math.T(C3)))
-    a2 = math.sqrt(-C23[0, 0])
+    a2 = math.sqrt(-math.cast_like(C23[0, 0], 1j))
 
     # This gets us a1, a2 up to a sign. To resolve the sign, ensure that
     # C1 C2^dag = a1 a2* I
