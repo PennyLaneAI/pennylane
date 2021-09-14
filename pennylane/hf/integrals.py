459f21deda9767b014b5ff809712fd0c308df138
soranjh <soranjh@yahoo.com>
2021-09-14 15:01:38 -0400

    modify hermite_coulomb function

--- pennylane/hf/integrals.py
+++ pennylane/hf/integrals.py
@@ -331,7 +331,7 @@ def _boys(n, t):
     \gamma(m, t) = \int_{0}^{t} x^{m-1} e^{-x} dx.
 
     Args:
-        n (float): rank of the Boys function
+        n (float): order of the Boys function
         t (float): exponent of the Boys function
 
     Returns:
@@ -348,7 +348,9 @@ def _hermite_coulomb(t, u, v, n, p, dr):
     """Evaluate Hermite integral needed to compute the nuclear attraction and electron repulsion
     integrals.
 
-    These integrals are computed recursively starting from the Boys function as
+    These integrals are computed recursively starting from the Boys function
+    [`Helgaker (1995) p817 <https://www.worldscientific.com/doi/abs/10.1142/9789812832115_0001>`_]:
+
 
     .. math::
 
@@ -357,45 +359,57 @@ def _hermite_coulomb(t, u, v, n, p, dr):
     where :math:`F_n` is the Boys function, :math:`p` is computed from the exponents of the two
     Gaussian functions as :math:`p = \alpha + \beta`, and :math:`R_{CP}` is the distance between the
     center of the composite Gaussian centered at :math:`P` and the electrostatic potential at
-    :math:`C`.
+    :math:`C`. The following recursive equations are used to compute the evaluate the higher order
+    Hermite integrals
+
+    .. math::
+
+    R_{t+1, u, v}^n = t R_{t-1, u, v}^{n+1} + x R_{t, u, v}^{n+1}
+
+    R_{t, u+1, v}^n = u R_{t, u-1, v}^{n+1} + y R_{t, u, v}^{n+1}
+
+    R_{t, u, v+1}^n = v R_{t, u, v-1}^{n+1} + z R_{t, u, v}^{n+1}
+
+    where :math:`x`, :math:`y` and :math:`z` are the Cartesian components of :math:`R_{CP}`.
 
     Args:
         t (integer): order of Hermite derivative in x
         u (integer): order of Hermite derivative in y
         v (float): order of Hermite derivative in z
-        n (integer): rank of the Boys function
+        n (integer): order of the Boys function
         p (float): sum of the Gaussian exponents
-        dr (array[float]): distance between the center of the composite Gaussian and nucleus
+        dr (array[float]): distance between the center of the composite Gaussian and the nucleus
 
     Returns:
         array[float]: value of the Hermite integral
 
     """
-
     x, y, z = dr[0], dr[1], dr[2]
-
     T = p * (dr ** 2).sum(axis=0)
+    r = 0
 
-    val = 0
     if t == u == v == 0:
         f = []
         for term in T.flatten():
             f.append(_boys(n, term))
-        val = val + ((-2 * p) ** n) * anp.array(f).reshape(T.shape)
-    elif t == u == 0:
-        if v > 1:
-            val = val + (v - 1) * _hermite_coulomb(t, u, v - 2, n + 1, p, dr)
-        val = val + z * _hermite_coulomb(t, u, v - 1, n + 1, p, dr)
-    elif t == 0:
-        if u > 1:
-            val = val + (u - 1) * _hermite_coulomb(t, u - 2, v, n + 1, p, dr)
-        val = val + y * _hermite_coulomb(t, u - 1, v, n + 1, p, dr)
-    else:
-        if t > 1:
-            val = val + (t - 1) * _hermite_coulomb(t - 2, u, v, n + 1, p, dr)
-        val = val + x * _hermite_coulomb(t - 1, u, v, n + 1, p, dr)
+        return ((-2 * p) ** n) * anp.array(f).reshape(T.shape)
 
-    return val
+    if t == u == 0:
+        if v > 1:
+            r = r + (v - 1) * _hermite_coulomb(t, u, v - 2, n + 1, p, dr)
+        r = r + z * _hermite_coulomb(t, u, v - 1, n + 1, p, dr)
+        return r
+
+    if t == 0:
+        if u > 1:
+            r = r + (u - 1) * _hermite_coulomb(t, u - 2, v, n + 1, p, dr)
+        r = r + y * _hermite_coulomb(t, u - 1, v, n + 1, p, dr)
+        return r
+
+    if t > 1:
+        r = r + (t - 1) * _hermite_coulomb(t - 2, u, v, n + 1, p, dr)
+    r = r + x * _hermite_coulomb(t - 1, u, v, n + 1, p, dr)
+    return r
 
 
 def nuclear_attraction(la, lb, ra, rb, alpha, beta, r):
