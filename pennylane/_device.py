2b7b6e5ea46cd8c34efdd00d621ac4ae13752996
Antal Szava <antalszava@gmail.com>
2021-09-15 22:07:06 +0200

    comment

--- pennylane/_device.py
+++ pennylane/_device.py
@@ -408,6 +408,7 @@ class Device(abc.ABC):
 
         results = []
         if self._shot_vector is not None:
+            # The following warning assumes that QubitDevice.execute is stand-alone
             warnings.warn(
                 "Specifying a list of shots is only supported for"
                 "QubitDevice based devices. Falling back to executions using all shots in the shot list."
