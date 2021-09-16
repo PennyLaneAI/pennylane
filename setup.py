83e99569b8920c095ecc7aa140a628795e67f970
trbromley <brotho02@gmail.com>
2021-09-16 16:46:05 -0400

    Add setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ requirements = [
     "semantic_version==2.6",
     "autoray",
     "cachetools",
-    "pennylane-lightning @ https://github.com/PennyLaneAI/pennylane-lightning.git#egg=pennylane-lightning",
+    "pennylane-lightning @ git+https://github.com/PennyLaneAI/pennylane-lightning.git",
     "pybind11",
 ]
 
