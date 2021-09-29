17145060a309900f2305d11a4e5df9b5d20fc7c3
antalszava <antalszava@gmail.com>
2021-09-29 16:45:10 -0400

    Add test for QNode switching to CUDA usage for the torch device

--- tests/tape/test_qnode.py
+++ tests/tape/test_qnode.py
@@ -1187,6 +1187,38 @@ class TestIntegration:
 
         assert np.allclose(res, expected, atol=tol, rtol=0)
 
+    def test_switched_to_cuda(self):
+        """Test that if any tensor passed to operators is on the GPU then CUDA
+        is set internally as a device option for 'default.qubit.torch'."""
+        torch = pytest.importorskip("torch")
+        if not torch.cuda.is_available():
+            pytest.skip("Cuda device not available")
+        else:
+            n_qubits = 3
+            n_layers = 1
+
+            dev = qml.device("default.qubit", wires=n_qubits)
+
+            @qml.qnode(dev)
+            def circuit(inputs, weights):
+                qml.templates.AngleEmbedding(inputs, wires=range(n_qubits))
+                qml.templates.BasicEntanglerLayers(weights, wires=range(n_qubits))
+                return [qml.expval(qml.PauliZ(wires=i)) for i in range(n_qubits)]
+
+            weight_shapes = {"weights": (n_layers, n_qubits)}
+
+            qlayer = qml.qnn.TorchLayer(circuit, weight_shapes)
+
+            x = torch.rand((5, n_qubits), dtype=torch.float64).to(torch.device("cuda"))
+            res = qlayer(x)
+            assert circuit.device.short_name == 'default.qubit.torch'
+            assert circuit.device._torch_device == 'cuda'
+            assert res.is_cuda
+
+            loss = torch.sum(res).squeeze()
+            loss.backward()
+            assert loss.is_cuda
+
 
 class TestMutability:
     """Test for QNode immutability"""
