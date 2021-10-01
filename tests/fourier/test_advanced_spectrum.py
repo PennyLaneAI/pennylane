b0b49713ff5f8707fbfe1560714b46097a82830d
dwierichs <davidwierichs@gmail.com>
2021-10-01 15:08:38 +0200

    add another decomposition test making sure unnecessary decomp is not performed

--- tests/fourier/test_advanced_spectrum.py
+++ tests/fourier/test_advanced_spectrum.py
@@ -284,19 +284,26 @@ class TestCircuits:
             advanced_spectrum(circuit)(1.5)
 
     def test_multi_parameter_expansion(self):
-        """Test that a multi-parameter gate is decomposed correctly."""
+        """Test that a multi-parameter gate is decomposed correctly.
+        And that single-parameter gates are not decomposed."""
         dev = qml.device("default.qubit", wires=3)
+
+        class _CRX(qml.CRX):
+            @staticmethod
+            def decomposition(theta, wires):
+                raise NotImplementedError()
 
         @qml.qnode(dev)
         def circuit(x, z1, y, z2):
             qml.RX(x, wires=0)
             qml.Rot(z1, y, z2, wires=1)
+            _CRX(x, wires=[0, 2])
             return qml.expval(qml.PauliZ(wires=0))
 
         res = advanced_spectrum(circuit)(1.5, -2.1, 0.2, -0.418)
         assert res == OrderedDict(
             [
-                ("x", {(): [-1.0, 0.0, 1.0]}),
+                ("x", {(): [-2.0, -1.5, -1.0, -0.5, 0.0, 0.5, 1.0, 1.5, 2.0]}),
                 ("z1", {(): [-1.0, 0.0, 1.0]}),
                 ("y", {(): [-1.0, 0.0, 1.0]}),
                 ("z2", {(): [-1.0, 0.0, 1.0]}),
