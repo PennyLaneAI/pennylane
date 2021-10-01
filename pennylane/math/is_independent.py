c1a733d2f50809e78efa03b2abfe4de66332d594
David Wierichs <davidwierichs@gmail.com>
2021-10-01 16:07:52 +0200

    Update pennylane/math/is_independent.py
    Co-authored-by: Olivia Di Matteo <2068515+glassnotes@users.noreply.github.com>

--- pennylane/math/is_independent.py
+++ pennylane/math/is_independent.py
@@ -369,7 +369,7 @@ def is_independent(
 
     if interface == "torch":
         warnings.warn(
-            "The function is_independent only is available numerically for the PyTorch interface."
+            "The function is_independent is only available numerically for the PyTorch interface."
             " Make sure that sampling positions and evaluating the function at these positions"
             " is a sufficient test, or change the interface."
         )
