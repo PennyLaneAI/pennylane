d775ed71aa31916e1f81ca0094e22e1905fc950d
David Wierichs <davidwierichs@gmail.com>
2021-10-01 16:08:38 +0200

    Update tests/math/test_is_independent.py
    Co-authored-by: Olivia Di Matteo <2068515+glassnotes@users.noreply.github.com>

--- tests/math/test_is_independent.py
+++ tests/math/test_is_independent.py
@@ -293,7 +293,7 @@ if have_jax:
         @pytest.mark.xfail
         @pytest.mark.parametrize("func, args", zip(overlooked_lambdas, args_overlooked_lambdas))
         def test_overlooked_dependence(self, func, args):
-            """Test that particular functions that dependent on the input
+            """Test that particular functions that are dependent on the input
             are overlooked."""
             assert not is_independent(func, self.interface, args)
 
