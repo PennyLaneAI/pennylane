294cadb97f4639bc322da83afda36be7da056761
antalszava <antalszava@gmail.com>
2021-10-12 19:13:18 -0400

    Update strongly_entangling.py (#1731)

--- pennylane/templates/layers/strongly_entangling.py
+++ pennylane/templates/layers/strongly_entangling.py
@@ -55,7 +55,7 @@ class StronglyEntanglingLayers(Operation):
         **Parameter shape**
 
         The expected shape for the weight tensor can be computed with the static method
-        :meth:`~.SimplifiedTwoDesign.shape` and used when creating randomly
+        :meth:`~.StronglyEntanglingLayers.shape` and used when creating randomly
         initialised weight tensors:
 
         .. code-block:: python
