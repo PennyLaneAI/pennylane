9d952a7d185353730f72e2bc176ea422530c97c8
Josh Izaac <josh146@gmail.com>
2021-10-15 01:25:25 +0800

    Apply suggestions from code review

--- pennylane/qnn/keras.py
+++ pennylane/qnn/keras.py
@@ -215,14 +215,13 @@ class KerasLayer(Layer):
         self.qnode = qnode
 
         dtype = tf.float32 if tf.keras.backend.floatx() == tf.float32 else tf.float64
-        self.qnode.interface = "tf"
 
         try:
             # TODO: remove once the beta QNode is default
             if self.qnode.diff_method != "backprop" or self.qnode.diff_method_change:
                 self.qnode.to_tf(dtype=dtype)
         except AttributeError:
-            pass
+            self.qnode.interface = "tf"
 
         # Allows output_dim to be specified as an int or as a tuple, e.g, 5, (5,), (5, 2), [5, 2]
         # Note: Single digit values will be considered an int and multiple as a tuple, e.g [5,] or (5,)
