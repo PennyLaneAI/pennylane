f73ad6963f1f6c57c93033fbbaefd0dc4e218ff9
dwierichs <davidwierichs@gmail.com>
2021-10-17 11:59:49 +0200

    remove old addition

--- pennylane/transforms/batch_transform.py
+++ pennylane/transforms/batch_transform.py
@@ -379,36 +379,3 @@ class batch_transform:
             processing_fn = lambda x: x
 
         return tapes, processing_fn
-
-    def _set_expand_fn(self, lkwargs):
-        """Logic to set the ``expand_fn`` based on keyword arguments
-        lkwargs."""
-        # pylint: disable=method-hidden
-        pass
-
-    def set_expand_fn(self, expand_fn_setting_fn):
-        """Register a custom logic to set the ``expand_fn``
-        for the batch transform.
-
-        **Example**
-
-        .. code-block:: python
-
-            def my_transform(tape, *targs, **tkwargs):
-                ...
-                return tapes, processing_fn
-
-            @my_transform.set_expand_fn
-            def my_expand_fn_decision(self, lkwargs):
-                if lkwargs.pop("expand_RX", False):
-                    self.expand_fn = expand_RX_fn
-                self.expand_fn = None
-
-        The custom ``expand_fn`` logic must have arguments
-        ``self`` (the batch transform object), and ``lkwargs`` (the logic
-        keyword arguments).
-
-        It should set ``self.expand_fn`` to a callable object that
-        expands/transforms a tape, or to ``None``.
-        """
-        self._set_expand_fn = types.MethodType(expand_fn_setting_fn, self)
