defd696544e1c1aa91a934e37a16d591a2cfcb50
glassnotes <dimatteo.olivia@protonmail.com>
2021-10-20 14:43:16 -0400

    Use a class.

---
+++ pennylane/ops/qubit/attributes.py
@@ -0,0 +1,108 @@
+# Copyright 2018-2021 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This file contains a number of attributes that may be held by operators,
+and lists all operators satisfying those criteria.
+"""
+import pennylane as qml
+import inspect
+
+
+class Attribute(set):
+    r"""Class to represent a set of operators with a certain attribute.
+
+    **Example**
+
+    Suppose we would like to store a list of which qubit operations are
+    Pauli operators. We can create a new Attribute, ``is_pauli``, like so,
+    listing which operations satisfy this property.
+
+    >>> is_pauli = Attribute(["PauliX", "PauliZ"])
+
+    We can check either a string or an Operation for inclusion in this set:
+
+    >>> qml.PauliX(0) in is_pauli
+    True
+    >>> "Hadamard" in is_pauli
+    False
+
+    We can also dynamically add operators to the sets at runtime. This is useful
+    for adding custom operations to the attributes such as ``is_composable_rotation``
+    and ``is_self_inverse`` that are used in compilation transforms.
+
+    >>> is_pauli.add("PauliY")
+    >>> is_pauli
+    ["PauliX", "PauliY", "PauliZ"]
+    """
+
+    def __contains__(self, obj):
+        if isinstance(obj, str):
+            return super().__contains__(obj)
+
+        if isinstance(obj, qml.operation.Operation):
+            return super().__contains__(obj.name)
+
+        if issubclass(obj, qml.operation.Operation):
+            return super().__contains__(obj.__name__)
+
+        return False
+
+
+is_composable_rotation = Attribute(
+    [
+        "RX",
+        "RY",
+        "RZ",
+        "PhaseShift",
+        "CRX",
+        "CRY",
+        "CRZ",
+        "ControlledPhaseShift",
+        "Rot",
+    ]
+)
+"""Operators for which composing multiple copies of the operation results in an
+addition (or alternative accumulation) of parameters.
+
+For example, ``qml.RZ`` is a composable rotation. Applying ``qml.RZ(0.1,
+wires=0)`` followed by ``qml.RZ(0.2, wires=0)`` is equivalent to performing
+a single rotation ``qml.RZ(0.3, wires=0)``.
+"""
+
+
+is_self_inverse = Attribute(
+    ["Hadamard", "PauliX", "PauliY", "PauliZ", "CNOT", "CZ", "CY", "SWAP", "Toffoli"]
+)
+"""Operations that are their own inverses."""
+
+is_symmetric_over_all_wires = Attribute(
+    [
+        "CZ",
+        "SWAP",
+    ]
+)
+"""Operations that are the same if you exchange the order of wires.
+
+For example, ``qml.CZ(wires=[0, 1])`` has the same effect as ``qml.CZ(wires=[1,
+0])`` due to symmetry of the operation.
+"""
+
+is_symmetric_over_control_wires = Attribute(["Toffoli"])
+"""Controlled operations that are the same if you exchange the order of all but
+the last (target) wire.
+
+For example, ``qml.Toffoli(wires=[0, 1, 2])`` has the same effect as
+``qml.Toffoli(wires=[1, 0, 2])``, but neither are the same as
+``qml.Toffoli(wires=[0, 2, 1])``.
+"""
--- pennylane/transforms/optimization/cancel_inverses.py
+++ pennylane/transforms/optimization/cancel_inverses.py
@@ -17,7 +17,7 @@ from pennylane import apply
 from pennylane.wires import Wires
 from pennylane.transforms import qfunc_transform
 
-from .attributes import (
+from pennylane.ops.qubit.attributes import (
     is_self_inverse,
     is_symmetric_over_all_wires,
     is_symmetric_over_control_wires,
@@ -101,7 +101,7 @@ def cancel_inverses(tape):
 
         # Case 1
         are_self_inverses = (
-            current_gate.name in is_self_inverse
+            current_gate in is_self_inverse
         ) and current_gate.name == next_gate.name
 
         # Cases 2 and 3
@@ -127,11 +127,11 @@ def cancel_inverses(tape):
                 else:
                     # If the wires are in a different order, gates that are "symmetric"
                     # over all wires (e.g., CZ), can be cancelled.
-                    if current_gate.name in is_symmetric_over_all_wires:
+                    if current_gate in is_symmetric_over_all_wires:
                         list_copy.pop(next_gate_idx + 1)
                     # For other gates, as long as the control wires are the same, we can still
                     # cancel (e.g., the Toffoli gate).
-                    elif current_gate.name in is_symmetric_over_control_wires:
+                    elif current_gate in is_symmetric_over_control_wires:
                         if (
                             len(Wires.shared_wires([current_gate.wires[:-1], next_gate.wires[:-1]]))
                             == len(current_gate.wires) - 1
