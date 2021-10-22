03945f5e0452e179c30378393ddf812b97236391
Robert Lang <robert.lang@mail.utoronto.ca>
2021-10-22 00:14:22 -0400

    change to subroutines/__init__.py

--- pennylane/templates/subroutines/__init__.py
+++ pennylane/templates/subroutines/__init__.py
@@ -28,5 +28,5 @@ from .qmc import QuantumMonteCarlo
 from .all_singles_doubles import AllSinglesDoubles
 from .grover import GroverOperator
 from .qft import QFT
-
-# from .commuting_evolution import CommutingEvolution
+from .kupccgsd import kUpCCGSD
+#from .commuting_evolution import CommutingEvolution
