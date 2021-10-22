1d00f00522278828d478a5a03edcabbc59171998
antalszava <antalszava@gmail.com>
2021-10-22 15:37:56 +0200

    Update pennylane/_grad.py
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- pennylane/_grad.py
+++ pennylane/_grad.py
@@ -191,7 +191,7 @@ def jacobian(func, argnum=None):
                     "Starting with PennyLane v0.20.0, when using Autograd. inputs "
                     "have to explicitly specify requires_grad=True (or the "
                     "argnum argument must be passed) in order for trainable parameters to be "
-                    "identified.to be treated as trainable.",
+                    "identified.",
                     UserWarning,
                 )
                 trainable = True
