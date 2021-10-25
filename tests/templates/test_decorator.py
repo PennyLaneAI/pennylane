a263d8b278e94ff921b36b4c14e2aa3d2ce79908
Antal Szava <antalszava@gmail.com>
2021-10-25 19:08:11 -0400

    docstring

--- tests/templates/test_decorator.py
+++ tests/templates/test_decorator.py
@@ -105,8 +105,8 @@ class TestDecorator:
             my_template([0, 1])
 
     def test_deprecated_decorator_no_warn_if_not_called(self, recwarn):
-        """Test that simply decorating function with the template decorator
-        does not raise a warning."""
+        """Test that decorating a function with the template decorator does not
+        raise a warning if the function is not being called."""
         @template
         def my_template(wires):
             decorated_dummy_template(wires)
