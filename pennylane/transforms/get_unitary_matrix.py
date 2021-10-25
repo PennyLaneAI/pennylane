19773d0dbf04078c8d1ce7683b2352f792cfdb6d
Josh Izaac <josh146@gmail.com>
2021-10-25 21:58:44 +0800

    merge master

--- pennylane/transforms/get_unitary_matrix.py
+++ pennylane/transforms/get_unitary_matrix.py
@@ -43,19 +43,16 @@ def get_unitary_matrix(circuit, wire_order=None):
             qml.RX(theta, wires=1)
             qml.PauliZ(wires=0)
 
-
     We can use ``get_unitary_matrix`` to generate a new function
     that returns the unitary matrix corresponding to the function ``circuit``:
 
-
     >>> get_matrix = get_unitary_matrix(circuit)
-    >>> theta = np.pi/4
+    >>> theta = np.pi / 4
     >>> get_matrix(theta)
     array([[ 0.92387953+0.j,  0.+0.j ,  0.-0.38268343j,  0.+0.j],
        [ 0.+0.j,  -0.92387953+0.j,  0.+0.j,  0. +0.38268343j],
        [ 0. -0.38268343j,  0.+0.j,  0.92387953+0.j,  0.+0.j],
        [ 0.+0.j,  0.+0.38268343j,  0.+0.j,  -0.92387953+0.j]])
-
 
     Note that since ``wire_order`` was not specified, the default order ``[1, 0]``
     for ``circuit`` was used, and the unitary matrix corresponds to the operation
@@ -69,9 +66,28 @@ def get_unitary_matrix(circuit, wire_order=None):
     ``wire_order=["a", 0, "b", 1]`` you obtain the :math:`16\times 16` matrix for
     the operation :math:`I\otimes Z\otimes I\otimes  R_X(\theta)`.
 
-    This unitary matrix can also be used in differentiable calculations:
+    This unitary matrix can also be used in differentiable calculations.
+    For example, consider the following cost function:
 
-    >>> c_grad = qml.grad(lambda theta: qml.math.real(qml.math.trace(get_unitary_matrix(circuit)(theta))))
+    ```python
+    def circuit(theta):
+        qml.RX(theta, wires=1)
+        qml.PauliZ(wires=0)
+        qml.CNOT(wires=[0, 1])
+
+    def cost(theta):
+        matrix = get_unitary_matrix(circuit)(theta)
+        return np.real(np.trace(matrix))
+    ```
+
+    Since this cost function returns a real scalar as a function of ``theta``,
+    we can differentiate it:
+
+    >>> theta = np.array(0.3, requires_grad=True)
+    >>> cost(theta)
+    1.9775421558720845
+    >>> qml.grad(cost)(theta)
+    -0.14943813247359922
     """
 
     wires = wire_order
@@ -84,9 +100,14 @@ def get_unitary_matrix(circuit, wire_order=None):
             circuit.construct(args, kwargs)
             tape = circuit.qtape
 
+            # if no wire ordering is specified, take wire list from the device
+            wire_order = circuit.device.wires if wires is None else Wires(wires)
+
         elif isinstance(circuit, qml.tape.QuantumTape):
             # user passed a tape
             tape = circuit
+            # if no wire ordering is specified, take wire list from tape
+            wire_order = tape.wires if wires is None else Wires(wires)
 
         elif callable(circuit):
             # user passed something that is callable but not a tape or qnode.
@@ -95,15 +116,15 @@ def get_unitary_matrix(circuit, wire_order=None):
             if len(tape.operations) == 0:
                 raise ValueError("Function contains no quantum operation")
 
+            # if no wire ordering is specified, take wire list from tape
+            wire_order = tape.wires if wires is None else Wires(wires)
+
         else:
             raise ValueError("Input is not a tape, QNode, or quantum function")
 
         # get interface of parameters to be used to construct the output matrix in same framework
         params = tape.get_parameters(trainable_only=False)
         interface = qml.math._multi_dispatch(params)  # pylint: disable=protected-access
-
-        # if no wire ordering is specified, take wire list from tape
-        wire_order = tape.wires if wires is None else Wires(wires)
 
         n_wires = len(wire_order)
 
