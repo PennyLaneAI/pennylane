db68148162657febcf603b59099603cf5ccfac6a
trbromley <brotho02@gmail.com>
2021-10-26 12:15:42 -0400

    Add

--- pennylane/transforms/mitigate.py
+++ pennylane/transforms/mitigate.py
@@ -12,47 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """TODO"""
-from typing import Any, Dict, Sequence, Optional
+from functools import wraps
+from typing import Any, Dict, Sequence, Optional, Tuple
 from pennylane.transforms import batch_transform, single_tape_transform
 from pennylane.tape import QuantumTape
 from pennylane import apply
+from pennylane.math import mean
+from pennylane import QubitStateVector, BasisState
+from pennylane.operation import Operation
+from pennylane.measure import MeasurementProcess
 
 
 @batch_transform
-def mitigate_with_zne(tape: QuantumTape, scale_factors: Sequence[float], folding: callable, extrapolate: callable, folding_kwargs: Optional[Dict[str, Any]]=None, extrapolate_kwargs: Optional[Dict[str, Any]]=None) -> float:
+def mitigate_with_zne(tape: QuantumTape, scale_factors: Sequence[float], folding: callable, extrapolate: callable, folding_kwargs: Optional[Dict[str, Any]]=None, extrapolate_kwargs: Optional[Dict[str, Any]]=None, reps_per_factor=1) -> float:
     folding_kwargs = folding_kwargs or {}
     extrapolate_kwargs = extrapolate_kwargs or {}
 
-    tape_without_measurements = _remove_measurements(tape)
+    tape_no_meas = _remove_measurements(tape)
 
-    tapes = [folding(tape_without_measurements, s, **folding_kwargs) for s in scale_factors]
+    tapes = [[folding(tape_no_meas, s, **folding_kwargs) for _ in range(reps_per_factor)] for s in scale_factors]
+    tapes = [tape_ for tapes_ in tapes for tape_ in tapes_]
     tapes = [_add_measurements(t, tape.measurements) for t in tapes]
 
     def processing_fn(results):
+        results = [results[i:i + reps_per_factor] for i in range(0, len(results), reps_per_factor)]
+        results = mean(results, axis=1)
+
         return extrapolate(scale_factors, results, **extrapolate_kwargs)
 
     return tapes, processing_fn
-
-
-def _remove_measurements(tape):
-    """Removes the measurements of a given tape
-
-    Args:
-        tape (QuantumTape): input quantum tape which may include measurements
-
-    Returns:
-        QuantumTape: the input tape with the measurements removed
-    """
-    with QuantumTape() as new_tape:
-        for op in tape.operations:
-            apply(op)
-    return new_tape
-
-
-@single_tape_transform
-def _add_measurements(tape, measurements):
-    """TODO"""
-    for op in tape.operations:
-        apply(op)
-    for m in measurements:
-        apply(m)
