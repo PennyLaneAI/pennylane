68516878394c4138137975fceb2f0c3af38443b2
trbromley <brotho02@gmail.com>
2021-10-27 19:15:10 -0400

    Update docstring

--- pennylane/transforms/mitigate.py
+++ pennylane/transforms/mitigate.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""TODO"""
+"""Provides transforms for mitigating quantum circuits."""
 from typing import Any, Dict, Optional, Sequence
 
 from pennylane.math import mean
