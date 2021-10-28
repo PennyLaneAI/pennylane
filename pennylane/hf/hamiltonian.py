Refine HF solver to compute correct gradients (#1780)
* perturb the diagonal elements of overlap matrix

pennylane/hf/hamiltonian.py
@@ -141,7 +141,7 @@ def generate_electron_integrals(mol, core=None, active=None):
     return electron_integrals
 
 
-def generate_fermionic_hamiltonian(mol, cutoff=1.0e-12):
+def generate_fermionic_hamiltonian(mol, cutoff=1.0e-12, core=None, active=None):
     r"""Return a function that computes the fermionic hamiltonian.
 
     Args:
@@ -171,7 +171,7 @@ def generate_fermionic_hamiltonian(mol, cutoff=1.0e-12):
         Returns:
             tuple(array[float], list[list[int]]): the Hamiltonian coefficients and operators
         """
-        core_constant, one, two = generate_electron_integrals(mol)(*args)
+        core_constant, one, two = generate_electron_integrals(mol, core, active)(*args)
 
         core_constant = anp.array([core_constant])
 
@@ -204,7 +204,7 @@ def generate_fermionic_hamiltonian(mol, cutoff=1.0e-12):
     return fermionic_hamiltonian
 
 
-def generate_hamiltonian(mol, cutoff=1.0e-12):
+def generate_hamiltonian(mol, cutoff=1.0e-12, core=None, active=None):
     r"""Return a function that computes the qubit hamiltonian.
 
     Args:
@@ -240,12 +240,15 @@ def generate_hamiltonian(mol, cutoff=1.0e-12):
         Returns:
             Hamiltonian: the qubit Hamiltonian
         """
-        h_ferm = generate_fermionic_hamiltonian(mol, cutoff)(*args)
+        h_ferm = generate_fermionic_hamiltonian(mol, cutoff, core, active)(*args)
+
+        ops = []
 
         for n, t in enumerate(h_ferm[1]):
 
             if len(t) == 0:
-                h = qml.Hamiltonian([h_ferm[0][n]], [qml.Identity(0)])
+                coeffs = np.array([h_ferm[0][n]])
+                ops = ops + [qml.Identity(0)]
 
             elif len(t) == 2:
                 op = _generate_qubit_operator(t)
@@ -260,7 +263,8 @@ def generate_hamiltonian(mol, cutoff=1.0e-12):
                             for o_ in o:
                                 k = k @ _return_pauli(o_[1])(o_[0])
                             op[1][i] = k
-                    h = h + qml.Hamiltonian(np.array(op[0]) * h_ferm[0][n], op[1])
+                    coeffs = np.concatenate([coeffs, np.array(op[0]) * h_ferm[0][n]])
+                    ops = ops + op[1]
 
             elif len(t) == 4:
                 op = _generate_qubit_operator(t)
@@ -275,7 +279,10 @@ def generate_hamiltonian(mol, cutoff=1.0e-12):
                             for o_ in o:
                                 k = k @ _return_pauli(o_[1])(o_[0])
                             op[1][i] = k
-                    h = h + qml.Hamiltonian(np.array(op[0]) * h_ferm[0][n], op[1])
+                    coeffs = np.concatenate([coeffs, np.array(op[0]) * h_ferm[0][n]])
+                    ops = ops + op[1]
+
+        h = qml.Hamiltonian(coeffs, ops, simplify=True)
 
         return h
 

tests/hf/test_hartree_fock.py
@@ -100,6 +100,12 @@ def test_hf_energy(symbols, geometry, charge, e_ref):
             # HF gradient computed with pyscf using rhf.nuc_grad_method().kernel()
             np.array([[0.0, 0.0, 0.3650435], [0.0, 0.0, -0.3650435]]),
         ),
+        (
+            ["H", "Li"],
+            np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 2.0]], requires_grad=True),
+            # HF gradient computed with pyscf using rhf.nuc_grad_method().kernel()
+            np.array([[0.0, 0.0, 0.21034957], [0.0, 0.0, -0.21034957]]),
+        ),
     ],
 )
 def test_hf_energy_gradient(symbols, geometry, g_ref):
@@ -107,7 +113,7 @@ def test_hf_energy_gradient(symbols, geometry, g_ref):
     correct."""
     mol = Molecule(symbols, geometry)
     args = [mol.coordinates]
-    g = autograd.grad(hf_energy(mol), argnum=0)(*args)
+    g = autograd.grad(hf_energy(mol))(*args)
 
     assert np.allclose(g, g_ref)
 