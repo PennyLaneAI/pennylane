25b0e8ddd8d4b12d67a6cffe61ba367822dcb62d
trbromley <brotho02@gmail.com>
2021-10-27 20:02:52 -0400

    Add to tests

--- tests/transforms/test_mitigate.py
+++ tests/transforms/test_mitigate.py
@@ -14,3 +14,99 @@
 """
 Tests for mitigation transforms.
 """
+import pennylane as qml
+import numpy as np
+from pennylane.tape import QuantumTape
+from pennylane.transforms import mitigate_with_zne
+
+with QuantumTape() as tape:
+    qml.BasisState([1], wires=0)
+    qml.RX(0.9, wires=0)
+    qml.RY(0.4, wires=1)
+    qml.CNOT(wires=[0, 1])
+    qml.RY(0.5, wires=0)
+    qml.RX(0.6, wires=1)
+    qml.expval(qml.PauliZ(0) @ qml.PauliZ(1))
+
+
+with QuantumTape() as tape_base:
+    qml.RX(0.9, wires=0)
+    qml.RY(0.4, wires=1)
+    qml.CNOT(wires=[0, 1])
+    qml.RY(0.5, wires=0)
+    qml.RX(0.6, wires=1)
+
+
+def same_tape(tape1, tape2):
+    """Raises an error if tapes are not identical"""
+    assert all(o1.name == o2.name for o1, o2 in zip(tape1.operations, tape2.operations))
+    assert all(o1.wires == o2.wires for o1, o2 in zip(tape1.operations, tape2.operations))
+    assert all(
+        np.allclose(o1.parameters, o2.parameters)
+        for o1, o2 in zip(tape1.operations, tape2.operations)
+    )
+    assert len(tape1.measurements) == len(tape2.measurements)
+    assert all(m1.return_type == m2.return_type for m1, m2 in zip(tape1.measurements, tape2.measurements))
+    assert all(o1.name == o2.name for o1, o2 in zip(tape1.observables, tape2.observables))
+    assert all(o1.wires == o2.wires for o1, o2 in zip(tape1.observables, tape2.observables))
+
+
+class TestMitigateWithZNE:
+    """Tests for the mitigate_with_zne function"""
+
+    folding = lambda *args, **kwargs: tape_base
+    extrapolate = lambda *args, **kwargs: 3.141
+
+    def test_folding_call(self, mocker):
+        """Tests that arguments are passed to the folding function as expected"""
+        spy = mocker.spy(self, "folding")
+        scale_factors = [1, 2, -4]
+        folding_kwargs = {"Hello": "goodbye"}
+
+        mitigate_with_zne(tape, scale_factors, self.folding, self.extrapolate, folding_kwargs)
+
+        args = spy.call_args_list
+
+        for i in range(3):
+            same_tape(args[i][0][0], tape_base)
+        assert [args[i][0][1] for i in range(3)] == scale_factors
+        assert all(args[i][1] == folding_kwargs for i in range(3))
+
+    def test_extrapolate_call(self, mocker):
+        """Tests that arguments are passed to the extrapolate function as expected"""
+        spy = mocker.spy(self, "extrapolate")
+        scale_factors = [1, 2, -4]
+        random_results = [0.1, 0.2, 0.3]
+        extrapolate_kwargs = {"Hello": "goodbye"}
+
+        tapes, fn = mitigate_with_zne(tape, scale_factors, self.folding, self.extrapolate, extrapolate_kwargs=extrapolate_kwargs)
+        res = fn(random_results)
+        assert res == 3.141
+
+        args = spy.call_args
+        assert args[0][0] == scale_factors
+        assert np.allclose(args[0][1], random_results)
+
+        assert args[1] == extrapolate_kwargs
+
+        for t in tapes:
+            same_tape(t, tape)
+
+    def test_reps_per_factor_not_1(self, mocker):
+        """Tests if mitigation proceeds as expected when reps_per_factor is not 1 (default)"""
+        scale_factors = [1, 2, -4]
+        spy_fold = mocker.spy(self, "folding")
+        spy_extrapolate = mocker.spy(self, "extrapolate")
+        tapes, fn = mitigate_with_zne(tape, scale_factors, self.folding, self.extrapolate, reps_per_factor=2)
+        random_results = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+
+        args = spy_fold.call_args_list
+        for i in range(6):
+            same_tape(args[i][0][0], tape_base)
+        assert [args[i][0][1] for i in range(6)] == [1, 1, 2, 2, -4, -4]
+
+        fn(random_results)
+
+        args = spy_extrapolate.call_args
+        assert args[0][0] == scale_factors
+        assert np.allclose(args[0][1], np.mean(np.reshape(random_results, (3, 2)), axis=1))
