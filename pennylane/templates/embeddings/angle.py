aaa72b53b5956c6420b1a6a6040ebc5c8515021b
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2021-10-29 12:15:27 +0200

    no numpy

--- pennylane/templates/embeddings/angle.py
+++ pennylane/templates/embeddings/angle.py
@@ -18,7 +18,6 @@ Contains the ``AngleEmbedding`` template.
 import pennylane as qml
 from pennylane.ops import RX, RY, RZ
 from pennylane.operation import Operation, AnyWires
-import numpy as np
 
 ROT = {"X": RX, "Y": RY, "Z": RZ}
 
