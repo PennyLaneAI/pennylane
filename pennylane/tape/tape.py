a42120dc044aa309d07e0788c56b998ab2df3f02
dwierichs <davidwierichs@gmail.com>
2021-11-02 10:52:56 +0100

    sorted `get_parameters`

--- pennylane/tape/tape.py
+++ pennylane/tape/tape.py
@@ -803,7 +803,7 @@ class QuantumTape(AnnotatedQueue):
         [0.432, 0.543, 0.133]
         """
         params = []
-        iterator = self.trainable_params if trainable_only else self._par_info
+        iterator = sorted(self.trainable_params) if trainable_only else self._par_info
 
         for p_idx in iterator:
             op = self._par_info[p_idx]["op"]
