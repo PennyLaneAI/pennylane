f80f362fa3ab1aeb4ebfe4a8ad766773267932d1
antalszava <antalszava@gmail.com>
2021-11-05 07:41:36 -0400

    `qml.math.is_independent` no deprecation warnings with `qml.grad`/`qml.jacobian` (#1862)
    * is_independent no deprecation warning
    
    * is_independent no deprecation warning tests
    
    * docstring
    
    * formatting
    
    * mark arrayboxes as trainable still; adjust test case (no autograd warning)
    
    * define requires_grad=True for random args only with Autograd (not JAX) to avoid errors with JAX tracers

--- pennylane/math/is_independent.py
+++ pennylane/math/is_independent.py
@@ -23,6 +23,7 @@ a function is independent of its arguments for the interfaces
 import warnings
 
 import numpy as np
+from pennylane import numpy as pnp
 
 from autograd.tracer import isbox, new_box, trace_stack
 from autograd.core import VJPNode
@@ -199,6 +200,10 @@ def _get_random_args(args, interface, num, seed, bounds):
             tuple(np.random.random(np.shape(arg)) * width + bounds[0] for arg in args)
             for _ in range(num)
         ]
+        if interface == "autograd":
+
+            # Mark the arguments as trainable with Autograd
+            rnd_args = pnp.array(rnd_args, requires_grad=True)
 
     return rnd_args
 
@@ -328,8 +333,8 @@ def is_independent(
 
     .. code-block:: pycon
 
-        >>> x = np.array([0.2, 9.1, -3.2])
-        >>> weights = np.array([1.1, -0.7, 1.8])
+        >>> x = np.array([0.2, 9.1, -3.2], requires_grad=True)
+        >>> weights = np.array([1.1, -0.7, 1.8], requires_grad=True)
         >>> qml.math.is_independent(lin, "autograd", (x,), {"weights": weights})
         False
 
