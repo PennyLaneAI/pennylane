3604ee6a9635ed88e7f820a62dfc8e82b90b94ed
Diego <diego_guala@hotmail.com>
2021-11-09 16:42:33 -0600

    Compute_indices_MPS allow user-defined wire labels

--- pennylane/templates/tensornetworks/mps.py
+++ pennylane/templates/tensornetworks/mps.py
@@ -2,12 +2,13 @@ import pennylane as qml
 import pennylane.numpy as np
 from pennylane.operation import Operation, AnyWires
 
-def compute_indices_MPS(n_wires, loc):
+
+def compute_indices_MPS(wires, loc):
     """
     Generate a list of wire indices that quantum gates acts on
     Args:
         loc (int): local wire number of a single quantum gate
-        n_wires (int): number of input qubits
+        wires (Iterable): the wires on which MPS acts
     Returns:
         layers (array[int]): arrays of indices of each layer
     """
@@ -15,43 +16,53 @@ def compute_indices_MPS(n_wires, loc):
     # assert n_wires%2 == 0, "n_wires should be an even integer"
     # assert loc%2 == 0, "loc should be an even integer"
     # These two conditions might be obsolete .. e.g. n_wires = 9 and loc = 1
-    assert loc <= n_wires, "loc should be smaller or equal than num_wires"
-    
-    return np.array([list(range(j,j+loc)) for j in range(0,n_wires-loc//2,loc//2)])
+    assert loc <= len(wires), "loc should be smaller or equal than len(wires)"
+
+    return np.array([[wires[idx] for idx in range(j, j+loc)] for j in range(0, len(wires) - loc // 2, loc // 2)])
+
 
 class MPS_from_function(Operation):
-    
+
     num_params = 1
     num_wires = AnyWires
     par_domain = "A"
-    
-    def __init__(self, wires, loc, block, n_params_block, weights=None, ranges=None, imprimitive=None, do_queue=True, id=None):
-    
-    
+
+    def __init__(
+        self,
+        wires,
+        loc,
+        block,
+        n_params_block,
+        weights=None,
+        do_queue=True,
+        id=None,
+    ):
         n_wires = len(wires)
-        #shape_params_block = allowed_templates[block] # Number of parameter per block
-        #print("shape=", shape_params_block)
-        self.n_blocks = int(n_wires/(loc/2)-1)
+        # shape_params_block = allowed_templates[block] # Number of parameter per block
+        # print("shape=", shape_params_block)
+        self.n_blocks = int(n_wires / (loc / 2) - 1)
         self.block = block
 
-        if weights is None: 
+        if weights is None:
             self.weights = np.random.rand(n_params_block, int(self.n_blocks))
-        else: 
+        else:
             self.weights = weights
 
-        self.ind_gates = compute_indices_MPS(n_wires, loc)
-                
+        self.ind_gates = compute_indices_MPS(wires, loc)
+        # compute_indices_MPS returns a list of integers that are wires. 
+        # Instead, I want it to return a list of whatever is used to label the wires
+
         # TO DO: raise error if params_block does not match with the input block
         # TO DO: raise error if the number of wires does not match with the MPS structure
         super().__init__(weights, wires=wires, do_queue=do_queue, id=id)
-        
+
     def expand(self):
 
         with qml.tape.QuantumTape() as tape:
             for idx, w in enumerate(self.ind_gates):
-                self.block(weights=self.weights[...,idx], wires=w.tolist())
+                self.block(weights=self.weights[idx], wires=w.tolist())
                 # In this ordering, remember we need to measure the last qubit
-    
+
         return tape
 
     @staticmethod
@@ -65,11 +76,11 @@ class MPS_from_function(Operation):
         Returns:
             tuple[int]: shape
         """
-        
-        assert n_wires%2 == 0, "n_wires should be an even integer"
-        assert loc%2 == 0, "loc should be an even integer"
+
+        assert n_wires % 2 == 0, "n_wires should be an even integer"
+        assert loc % 2 == 0, "loc should be an even integer"
         assert loc <= n_wires, "loc should be smaller or equal than num_wires"
 
-        n_blocks = int(n_wires/(loc/2)-1)
+        n_blocks = int(n_wires / (loc / 2) - 1)
 
         return n_blocks, n_params_block
