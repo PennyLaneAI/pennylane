Fix shape of returned Hessian
Previously all the elements of the Hessian would be returned in a 1D array
(assuming scalar-valued function, +1D for vector valued functions), a
hold-over from computing gradients.
Now the proper dimensions are returned, with extrenous ones being
stripped (for example for single variable, scalar-valued functions).

pennylane/gradients/param_shift_hessian.py
@@ -15,14 +15,13 @@
 This module contains functions for computing the parameter-shift hessian
 of a qubit-based quantum tape.
 """
+from itertools import product
 import numpy as np
 
 import pennylane as qml
 
 from .gradient_transform import gradient_transform
 from .parameter_shift import _gradient_analysis
-
-from itertools import product
 
 def generate_multishifted_tapes(tape, idx, shifts):
     r"""Generate a list of tapes where the corresponding trainable parameter
@@ -56,7 +55,7 @@ def generate_multishifted_tapes(tape, idx, shifts):
 
 
 @gradient_transform
-def param_shift_hessian(tape, f0=None):
+def param_shift_hessian(tape):
     """Generate parameter-shift tapes and postprocessing method
     to directly compute the hessian."""
 
@@ -70,7 +69,7 @@ def param_shift_hessian(tape, f0=None):
     gradient_tapes = []
     gradient_coeffs = []
     shapes = []
-    unshifted_coeffs = []
+    dim = len(tape.trainable_params)
 
     if not tape.trainable_params:
         return gradient_tapes, lambda _: np.zeros([tape.output_dim, len(tape.trainable_params)])
@@ -86,62 +85,33 @@ def param_shift_hessian(tape, f0=None):
     # for now assume all operations support the 2-term parameter shift rule
     for idx in product(range(len(tape.trainable_params)), repeat=2):
 
-        if shifts[0] == 0 and multipliers[0] == 1:
-            # Gradient recipe includes a term with zero shift.
-
-            if not unshifted_coeffs and f0 is None:
-                # Ensure that the unshifted tape is appended
-                # to the gradient tapes, if not already.
-                gradient_tapes.append(tape)
-
-            # Store the unshifted coefficient. We know that
-            # it will always be the first coefficient due to processing.
-            unshifted_coeffs.append(coeffs[0])
-            coeffs, multipliers, shifts = recipe[:, 1:]
-
         # generate the gradient tapes
         gradient_coeffs.append(coeffs)
         g_tapes = generate_multishifted_tapes(tape, idx, shifts)
 
         gradient_tapes.extend(g_tapes)
-        shapes.append(len(g_tapes))
+        shapes.append((idx, len(g_tapes)))
 
     def processing_fn(results):
         grads = []
-        start = 1 if unshifted_coeffs and f0 is None else 0
-        r0 = f0 or results[0]
+        start = 0
 
-        for i, s in enumerate(shapes):
-
-            if s == 0:
-                # parameter has zero gradient
-                g = qml.math.zeros_like(results[0])
-                grads.append(g)
-                continue
-
+        for i, (idx, s) in enumerate(shapes):
             res = results[start : start + s]
             start = start + s
 
             # compute the linear combination of results and coefficients
-            res = qml.math.stack(res)
+            res = qml.math.squeeze(qml.math.stack(res))
             g = qml.math.tensordot(res, qml.math.convert_like(gradient_coeffs[i], res), [[0], [0]])
 
-            if unshifted_coeffs:
-                # add on the unshifted term
-                g = g + unshifted_coeffs[i] * r0
+            for i, elem in enumerate(qml.math.atleast_1d(g)):
+                if i < len(grads):
+                    grads[i][idx[0], idx[1]] = elem
+                else:
+                    hessian = qml.math.empty((dim,dim))
+                    hessian[idx[0], idx[1]] = elem
+                    grads.append(hessian)
 
-            grads.append(g)
-
-        # The following is for backwards compatibility; currently,
-        # the device stacks multiple measurement arrays, even if not the same
-        # size, resulting in a ragged array.
-        # In the future, we might want to change this so that only tuples
-        # of arrays are returned.
-        for i, g in enumerate(grads):
-            g = qml.math.convert_like(g, res[0])
-            if hasattr(g, "dtype") and g.dtype is np.dtype("object"):
-                grads[i] = qml.math.hstack(g)
-
-        return qml.math.T(qml.math.stack(grads))
+        return qml.math.squeeze(qml.math.array(grads))
 
     return gradient_tapes, processing_fn

tests/gradients/test_param_shift_hessian.py
@@ -39,7 +39,7 @@ class TestParameterShiftHessian:
         jacobian = qml.jacobian(qml.grad(circuit))(x)
         hessian = qml.gradients.param_shift_hessian(circuit)(x)
 
-        print(jacobian, '=?', hessian)
+        print('\n', jacobian, '=?', hessian)
 
         assert np.allclose(jacobian, hessian)
 
@@ -59,7 +59,7 @@ class TestParameterShiftHessian:
         jacobian = qml.jacobian(qml.jacobian(circuit))(x)
         hessian = qml.gradients.param_shift_hessian(circuit)(x)
 
-        print(jacobian, '=?', hessian)
+        print('\n', jacobian, '\n\t=?\n', hessian)
 
         assert np.allclose(jacobian, hessian)
 
@@ -80,7 +80,7 @@ class TestParameterShiftHessian:
         jacobian = qml.jacobian(qml.jacobian(circuit))(x)
         hessian = qml.gradients.param_shift_hessian(circuit)(x)
 
-        print(jacobian, '=?', hessian)
+        print('\n', jacobian, '\n\t=?\n', hessian)
 
         assert np.allclose(jacobian, hessian)
 
@@ -101,7 +101,7 @@ class TestParameterShiftHessian:
         jacobian = qml.jacobian(qml.jacobian(circuit))(x)
         hessian = qml.gradients.param_shift_hessian(circuit)(x)
 
-        print(jacobian, '=?', hessian)
+        print('\n', jacobian, '\n\t=?\n', hessian)
 
         assert np.allclose(jacobian, hessian)
 
@@ -125,6 +125,6 @@ class TestParameterShiftHessian:
             qml.jacobian(qml.jacobian(circuit))(x)
             jacobian_qruns = tracker.totals['executions'] - hessian_qruns
 
-            print(hessian_qruns, '<?', jacobian_qruns)
+            print('\n', hessian_qruns, '<?', jacobian_qruns)
 
             assert hessian_qruns < jacobian_qruns