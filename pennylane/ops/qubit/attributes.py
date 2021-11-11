bdfc30e893b657fd572432bb1dc5f52a2bbfebbd
Maria Schuld <mariaschuld@gmail.com>
2021-11-11 16:56:50 +0100

    try to delete invisible whitespace

--- pennylane/ops/qubit/attributes.py
+++ pennylane/ops/qubit/attributes.py
@@ -174,8 +174,8 @@ diagonal_in_z_basis = Attribute(
         "CZ",
         "DiagonalQubitUnitary",
         "RZ",
-        "ControlledPhaseShift",
         "PhaseShift",
+        "ControlledPhaseShift",
         "MultiRZ",
         "CRZ",
     ]
