Fix hessian for arbitrary QNode output dimensions
Previously, only QNode output dimensions of up to 1 were considered
(scalars or vectors).

pennylane/gradients/param_shift_hessian.py
@@ -135,7 +135,7 @@ def param_shift_hessian(tape):
     gradient_tapes = []
     gradient_coeffs = []
     shapes = []
-    dim = len(tape.trainable_params)
+    h_dim = len(tape.trainable_params)
 
     if not tape.trainable_params:
         return gradient_tapes, lambda _: np.zeros([tape.output_dim, len(tape.trainable_params)])
@@ -171,10 +171,15 @@ def param_shift_hessian(tape):
         shapes.append((idx, len(g_tapes)))
 
     def processing_fn(results):
-        grads = []
+        # The first results dimension is the number of terms/tapes in the parameter-shift
+        # rule, the remaining ones are the QNode output dimensions.
+        out_dim = np.shape(results)[1:]
+        # The desired shape is: (QNode output dimensions, # gate args, # gate args)
+        grads = np.empty(out_dim + (h_dim, h_dim))
+        # Keep track of tape results already consumed.
         start = 0
 
-        for i, (idx, s) in enumerate(shapes):
+        for i, (h_idx, s) in enumerate(shapes):
             res = results[start : start + s]
             start = start + s
 
@@ -182,13 +187,8 @@ def param_shift_hessian(tape):
             res = qml.math.squeeze(qml.math.stack(res))
             g = qml.math.tensordot(res, qml.math.convert_like(gradient_coeffs[i], res), [[0], [0]])
 
-            for j, elem in enumerate(qml.math.atleast_1d(g)):
-                if j < len(grads):
-                    grads[j][idx[0], idx[1]] = elem
-                else:
-                    hessian = qml.math.empty((dim,dim))
-                    hessian[idx[0], idx[1]] = elem
-                    grads.append(hessian)
+            for out_idx, elem in qml.math.ndenumerate(qml.math.reshape(g, out_dim)):
+                grads[out_idx][h_idx] = elem
 
         return qml.math.squeeze(qml.math.array(grads))
 

tests/gradients/test_param_shift_hessian.py
@@ -173,6 +173,26 @@ class TestParameterShiftHessian:
 
         assert np.allclose(jacobian, hessian)
 
+    def test_2term_shift_rules8(self):
+        """Test that the correct hessian is calculated higher dimensional outputs"""
+        dev = qml.device("default.qubit", wires=2)
+
+        @qml.beta.qnode(dev, diff_method="parameter-shift", max_diff=2)
+        def circuit(x):
+            qml.RX(x[0], wires=0)
+            qml.RY(x[1], wires=0)
+            qml.CNOT(wires=[0, 1])
+            return qml.probs(wires=0), qml.probs(wires=1)
+
+        x = np.ones([2], requires_grad=True)
+
+        jacobian = qml.jacobian(qml.jacobian(circuit))(x)
+        hessian = qml.gradients.param_shift_hessian(circuit)(x)
+
+        print('\n', jacobian, '\n\t=?\n', hessian)
+
+        assert np.allclose(jacobian, hessian)
+
     def test_less_quantum_invocations(self):
         """Test that the hessian invokes less hardware executions than double differentiation"""
 
@@ -188,11 +208,12 @@ class TestParameterShiftHessian:
 
         with qml.Tracker(dev) as tracker:
 
-            qml.gradients.param_shift_hessian(circuit)(x)
+            hessian = qml.gradients.param_shift_hessian(circuit)(x)
             hessian_qruns = tracker.totals['executions']
             qml.jacobian(qml.jacobian(circuit))(x)
             jacobian_qruns = tracker.totals['executions'] - hessian_qruns
 
+            print('\n', hessian)
             print('\n', hessian_qruns, '<?', jacobian_qruns)
 
             assert hessian_qruns < jacobian_qruns