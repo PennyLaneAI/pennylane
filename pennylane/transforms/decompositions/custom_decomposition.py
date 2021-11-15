31741ae7650f3ba00611d3665d204921df1a980f
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2021-11-15 16:31:24 -0500

    Update to consider multiple cases.

--- pennylane/transforms/decompositions/custom_decomposition.py
+++ pennylane/transforms/decompositions/custom_decomposition.py
@@ -28,7 +28,7 @@ def custom_decomposition(decomp_dict, dev=None):
     """A context manager to enable custom decompositions to be applied to Operations.
 
     Args:
-        decomp_dict (Dict[.Operator, Callable]): a dictionary detailing the
+        decomp_dict (Dict[Union(str, .Operator), Callable]): a dictionary detailing the
             pairings of operations and their custom decompositions. The functions
             must have the signature fn(*params, wires).
         dev (Device): an optional device on which to apply the decomposition.
@@ -70,45 +70,61 @@ def custom_decomposition(decomp_dict, dev=None):
         with custom_decomposition(decomp_dict, dev):
             print(qml.draw(circuit, expansion_strategy="device")(0.5))
     """
+    try:
+        with contextlib.ExitStack() as stack:
+            for obj, fn in decomp_dict.items():
+                # We enter a new context each decomposition the user passes
+                stack.enter_context(_custom_decomposition(obj, fn, dev=dev))
 
-    for obj, fn in decomp_dict.items():
+            stack = stack.pop_all()
 
-        # We might be providing a decomposition for an Operation that is allowed
-        # by a given device, so we need to trick the device into thinking it doesn't
-        # support it so it will apply our decomposition.
-        obj_supported_by_device = False
+        yield
 
-        if isinstance(obj, str):
-            obj = getattr(qml, obj)
+    finally:
+        stack.close()
 
-        original_decomp_method = obj.expand
 
-        # This is the method that will override the current .decomposition method of
-        # the given operation; it has the same signature as .decomposition and takes
-        # only self, but then passes its own parameters and wires to the custom
-        # decomposition function.
-        def instance_method(self):
-            with NonQueuingTape() as tape:
-                fn(*self.parameters, self.wires)
+@contextlib.contextmanager
+def _custom_decomposition(obj, fn, dev=None):
+    # We might be providing a decomposition for an Operation that is allowed
+    # by a given device, so we need to trick the device into thinking it doesn't
+    # support it so it will apply our decomposition.
+    obj_supported_by_device = False
 
-            return tape
+    # Covers the case where the user passes a string to indicate the Operator
+    if isinstance(obj, str):
+        obj = getattr(qml, obj)
 
-        if isinstance(obj, qml.operation.Operation):
-            # Provided object is an instance, not a class.
-            # Need to convert `fn` into an instance method.
-            instance_method = types.MethodType(instance_method, obj)
+    original_decomp_method = obj.expand
 
-        try:
-            obj.expand = instance_method
+    # This is the method that will override the current .decomposition method of
+    # the given operation; it has the same signature as .decomposition and takes
+    # only self, but then passes its own parameters and wires to the custom
+    # decomposition function.
+    def instance_method(self):
+        with NonQueuingTape() as tape:
+            fn(*self.parameters, self.wires)
 
-            if dev is not None and obj.__name__ in dev.operations:
-                obj_supported_by_device = True
-                dev.operations.remove(obj.__name__)
+        return tape
 
-            yield
+    if isinstance(obj, qml.operation.Operation):
+        # Provided object is an instance, not a class.
+        # Need to convert `fn` into an instance method.
+        instance_method = types.MethodType(instance_method, obj)
 
-        finally:
-            obj.expand = original_decomp_method
+    try:
+        # Actually set the new decomp function; note that we set
+        # expand rather than `decomposition` proper.
+        obj.expand = instance_method
 
-            if dev is not None and obj_supported_by_device:
-                dev.operations.add(obj.__name__)
+        if dev is not None and obj.__name__ in dev.operations:
+            obj_supported_by_device = True
+            dev.operations.remove(obj.__name__)
+
+        yield
+
+    finally:
+        obj.expand = original_decomp_method
+
+        if dev is not None and obj_supported_by_device:
+            dev.operations.add(obj.__name__)
