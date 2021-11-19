5c7abedff926f92deb72feaff8cd22b0b4f8f594
antalszava <antalszava@gmail.com>
2021-11-19 12:09:23 -0500

    Update pennylane/templates/embeddings/displacement.py

--- pennylane/templates/embeddings/displacement.py
+++ pennylane/templates/embeddings/displacement.py
@@ -45,7 +45,7 @@ class DisplacementEmbedding(Operation):
 
         Depending on the ``method`` argument, the feature vector will be encoded in the phase or the amplitude.
         The argument ``c`` will define the value of the other quantity.
-        The default values are :math:`0.1` for ``c`` and amplitude for ``method``.
+        The default values are :math:`0.1` for ``c`` and ``'amplitude'`` for ``method``.
 
         .. code-block:: python
 
