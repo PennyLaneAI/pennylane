3b753688ecb28115974de6f1b1023e7810f1d931
antalszava <antalszava@gmail.com>
2021-11-24 20:44:30 -0500

    Fix parametric ops GPU compatibility (`v0.19.1`) (#1927)
    * torch coerce change, qml.math for Hermitian
    
    * test changes
    
    * readd torch_device kwarg
    
    * format
    
    * use torch device arg and add cuda_was_set branch
    
    * polish
    
    * TestApply cover with torch_device; update the def for OrbitalRotation to be compatible with CUDA
    
    * Add notes to tests using execute; CRX + CRY convert_like; Integration tests passing
    
    * passthru test
    
    * more tests
    
    * format
    
    * coerce refine + test
    
    * tidy
    
    * branch off for Torch
    
    * def self._torch_device_requested and extend check in execute
    
    * test and further adjustments
    
    * format
    
    * changelog
    
    * Mark the kernel test not converging as xfail (#1918)
    
    * mark the kernel test not converging as xfail
    
    * format
    
    * skipif
    
    * skipif marker
    
    * format
    
    * changelog
    
    * lint
    
    * todo no longer relevant
    
    * PauliRot identity matrix consider Torch
    
    * format
    
    * paulirot identity matrix test
    
    * add gpu marker
    
    * more torch_device usage
    
    * more fixes with finite diff; update tests
    
    * qml.math.allclose
    
    * Move the state to device conversion from apply_operation to the execute method because it logically belongs there better
    
    * Move the state to device conversion within if statement, add comment; change a test to avoid raising a warning
    
    * format
    
    * error message; gpu marker on the coercion test in qml.math; error matching
    
    * format
    
    * warning
    
    * warning test
    
    * no print
    
    * format

--- doc/releases/changelog-0.19.1.md
+++ doc/releases/changelog-0.19.1.md
@@ -4,6 +4,11 @@
 
 <h3>Bug fixes</h3>
 
+* Fixes several bugs when using parametric operations with the
+  `default.qubit.tensor` device on GPU. The device takes the `torch_device`
+  argument once again to allow running non-parametric QNodes on the GPU.
+  [(#1927)](https://github.com/PennyLaneAI/pennylane/pull/1927)
+
 * Fixes a bug where using JAX's jit function on certain QNodes that contain
   the `qml.QubitStateVector` operation raised an error with earlier JAX
   versions (e.g., `jax==0.2.10` and `jaxlib==0.1.64`).
--- tests/gpu/test_torch.py
+++ tests/gpu/test_torch.py
@@ -15,6 +15,8 @@ import pytest
 import pennylane as qml
 from pennylane import numpy as np
 
+pytestmark = pytest.mark.gpu
+
 torch = pytest.importorskip("torch")
 
 
@@ -122,6 +124,27 @@ class TestTorchDevice:
         res.backward()
         assert x.grad.is_cuda
 
+    @pytest.mark.parametrize("init_device, par_device", [("cpu", "cuda"), ("cuda", "cpu")])
+    def test_different_devices_creation_and_parameters_warn(self, init_device, par_device):
+        """Test that a warning is raised if the Torch device specified on
+        PennyLane device creation differs from the Torch device of gate
+        parameters.
+        """
+        dev = qml.device("default.qubit.torch", wires=1, torch_device=init_device)
+
+        p = torch.tensor(0.543, dtype=torch.float64, device=par_device)
+
+        @qml.qnode(dev, interface="torch")
+        def circuit(x):
+            qml.RX(x, wires=0)
+            return qml.expval(qml.PauliY(0))
+
+        with pytest.warns(
+            UserWarning,
+            match=f"Torch device {init_device} specified upon PennyLane device creation does not match",
+        ):
+            circuit(p)
+
 
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="no cuda support")
 class TestqnnTorchLayer:
@@ -178,3 +201,21 @@ class TestqnnTorchLayer:
         loss = torch.sum(res).squeeze()
         loss.backward()
         assert loss.is_cuda
+
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="no cuda support")
+class TestParametricOps:
+    """Test that certain parametric operations perform computations correctly
+    with Torch devices internally."""
+
+    @pytest.mark.parametrize("theta", np.linspace(0, 2 * np.pi, 7))
+    def test_pauli_rot_identity(self, theta):
+        """Test that the PauliRot operation returns the correct matrix when
+        providing a gate parameter on the GPU and only specifying the identity
+        operation."""
+        x = torch.tensor(theta, device="cuda")
+        mat = qml.PauliRot(x, "I", wires=[0]).matrix
+
+        val = np.cos(-theta / 2) + 1j * np.sin(-theta / 2)
+        exp = torch.tensor(np.diag([val, val]), device="cuda")
+        assert torch.allclose(mat, exp)
