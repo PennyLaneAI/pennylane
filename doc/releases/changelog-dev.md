0ef592c0471fb24017eaaea796207e7172738c71
albi3ro <chrissie.c.l@gmail.com>
2021-11-30 11:32:45 +0000

    fix code factor complaints

--- doc/releases/changelog-dev.md
+++ doc/releases/changelog-dev.md
@@ -396,6 +396,8 @@
 
 <h3>Bug fixes</h3>
 
+* Fixes several code factor complaints.
+
 * Fixes a bug where the device test suite wasn't testing certain operations.
   [(#1943)](https://github.com/PennyLaneAI/pennylane/pull/1943)
 
--- pennylane/templates/subroutines/commuting_evolution.py
+++ pennylane/templates/subroutines/commuting_evolution.py
@@ -108,9 +108,10 @@ class CommutingEvolution(Operation):
     grad_method = None
 
     def __init__(self, hamiltonian, time, frequencies=None, shifts=None, do_queue=True, id=None):
+        # pylint: disable=import-outside-toplevel
         from pennylane.gradients.general_shift_rules import (
             get_shift_rule,
-        )  # pylint: disable=import-outside-toplevel
+        )
 
         if not isinstance(hamiltonian, qml.Hamiltonian):
             raise TypeError(
