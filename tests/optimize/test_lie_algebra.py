3a7055eeec34fa24b9b443c706ace1b1df057b57
rooler <roel.wier@gmail.com>
2021-12-01 16:47:03 -0500

    lint error

--- tests/optimize/test_lie_algebra.py
+++ tests/optimize/test_lie_algebra.py
@@ -348,7 +348,7 @@ def test_docstring_example_exact():
     opt = LieAlgebraOptimizer(circuit=quant_fun, stepsize=0.1, exact=True)
 
     for step in range(6):
-        circuit, cost = opt.step_and_cost()
+        _, cost = opt.step_and_cost()
         print(f"Step {step} - cost {cost}")
     assert np.isclose(cost, -2.23, atol=1e-2)
 
@@ -371,5 +371,5 @@ def test_example_shots():
     opt = LieAlgebraOptimizer(circuit=quant_fun, stepsize=0.1, exact=False)
 
     for step in range(3):
-        circuit, cost = opt.step_and_cost()
+        _, cost = opt.step_and_cost()
         print(f"Step {step} - cost {cost}")
