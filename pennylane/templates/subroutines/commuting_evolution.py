abcf2fa11b567fca8f38d797e876dbf3866bb39f
Josh Izaac <josh146@gmail.com>
2021-12-02 15:37:55 +0800

    Adds utility functions for the general parameter-shift rule (#1932)
    * Initial commit for CommutingEvolution and get_shift_rule along with tests
    
    * Small doc changes and codefactor changes
    
    * change to subroutines/__init__.py
    
    * template doc
    
    * fix circular import error
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * Update pennylane/circuit_drawer/representation_resolver.py
    
    * Changes to CommutingEvolution to work with differentiable Hamiltonian, small docstring typo fix.
    
    * Changes to CommutingEvolution to work with differentiable Hamiltonian
    
    * CommutingEvolution docstring typo fix
    
    * Added  keyword to CommutingEvolution to allow users to specify custom shifts for its parameter shift rule.
    
    * Changed keyword argument name to param_shift
    
    * Populated test_commuting_evolution with some tests.
    
    * Added tests for commuting_evolution, added param_shifts keyword argument to commuting_evolution
    
    * Fix to adjoint test for CommutingEvolution
    
    * Formatting changes, fix to commuting_evolution docstring
    
    * Fix get_shift_rule docstring example
    
    * fix incorrect master merge
    
    * Formatting change
    
    * Clarification in CommutingEvolution docstring
    
    * Small fixes and changelog
    
    * Added warning for near singular matrix inversion in get_shift_rule
    
    * Added test of near singular matrix warning
    
    * Changes to get_shift_rule
    
    * small fix
    
    * Avoided unnecessary equidistant shift check for default shifts
    
    * small fix for CommutingEvolution, switched to use beta.qnode in test_commuting_evolution
    
    * Added template fig
    
    * get_shift_rule uses np.linalg.solve and small changes
    
    * merge conflict
    
    * removed beta qnode
    
    * changelog
    
    * more
    
    * more
    
    * more
    
    * more
    
    * fix
    
    * fix
    
    * update
    
    * update
    
    * update
    
    * fix test
    
    * more tests
    
    * black
    
    * Updates the 'Gradients and training' quickstart guide to provide information on gradient transforms
    
    * more
    
    * fix
    
    * fix
    
    * fix
    
    * Apply suggestions from code review
    
    Co-authored-by: David Wierichs <davidwierichs@gmail.com>
    Co-authored-by: Maria Schuld <mariaschuld@gmail.com>
    
    * linting
    
    * suggested changes
    
    * fix maths
    
    * update changelog
    
    * fix rendering
    
    * Update pennylane/gradients/general_shift_rules.py
    
    Co-authored-by: David Wierichs <davidwierichs@gmail.com>
    
    * fix
    
    Co-authored-by: Robert Lang <robert.lang@mail.utoronto.ca>
    Co-authored-by: David Wierichs <davidwierichs@gmail.com>
    Co-authored-by: Maria Schuld <mariaschuld@gmail.com>

--- pennylane/templates/subroutines/commuting_evolution.py
+++ pennylane/templates/subroutines/commuting_evolution.py
@@ -108,11 +108,9 @@ class CommutingEvolution(Operation):
     grad_method = None
 
     def __init__(self, hamiltonian, time, frequencies=None, shifts=None, do_queue=True, id=None):
-        from pennylane.gradients.general_shift_rules import get_shift_rule
-
         # pylint: disable=import-outside-toplevel
         from pennylane.gradients.general_shift_rules import (
-            get_shift_rule,
+            generate_shift_rule,
         )
 
         if not isinstance(hamiltonian, qml.Hamiltonian):
@@ -121,9 +119,9 @@ class CommutingEvolution(Operation):
 
         trainable_hamiltonian = qml.math.requires_grad(hamiltonian.coeffs)
         if frequencies is not None and not trainable_hamiltonian:
-            self.grad_recipe = (get_shift_rule(frequencies, shifts)[0],) + (None,) * len(
-                hamiltonian.data
-            )
+            c, s = generate_shift_rule(frequencies, shifts)
+            recipe = qml.math.stack([c, qml.math.ones_like(c), s]).T
+            self.grad_recipe = (recipe,) + (None,) * len(hamiltonian.data)
             self.grad_method = "A"
 
         self.hamiltonian = hamiltonian
