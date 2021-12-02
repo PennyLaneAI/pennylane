faf0a00673a7ef4e3aea422fd69dab19f9d6b38d
Josh Izaac <josh146@gmail.com>
2021-12-02 15:09:26 +0800

    fix

--- pennylane/templates/subroutines/commuting_evolution.py
+++ pennylane/templates/subroutines/commuting_evolution.py
@@ -108,8 +108,6 @@ class CommutingEvolution(Operation):
     grad_method = None
 
     def __init__(self, hamiltonian, time, frequencies=None, shifts=None, do_queue=True, id=None):
-        from pennylane.gradients.general_shift_rules import get_shift_rule
-
         # pylint: disable=import-outside-toplevel
         from pennylane.gradients.general_shift_rules import (
             generate_shift_rule,
