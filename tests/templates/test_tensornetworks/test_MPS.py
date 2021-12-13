76135f9a86876914015259521667a1da26bee9bf
Diego <diego_guala@hotmail.com>
2021-12-13 17:02:30 -0600

    Edit Tests

--- tests/templates/test_tensornetworks/test_MPS.py
+++ tests/templates/test_tensornetworks/test_MPS.py
@@ -14,32 +14,62 @@
 r"""
 Tests for the MPS template.
 """
+import math
 import pytest
 import random
 import numpy as np
 import pennylane as qml
 from pennylane.templates.tensornetworks.mps import *
 
-"""
-Unit tests for the MPS template.
-"""
 
 class TestIndicesMPS: 
     """Test function that computes MPS indices"""
-    
-    def test_exception_wrong_dim(self):
-        """Verifies that exception is raised if the number of wires is incorrect."""
-        
-        n_wires = random.randrange(1,20,2)
-        loc = 2
-        with pytest.raises(AssertionError, match="number of wires must be an even integer; got {}".format(n_wires)):
-            compute_indices_MPS(range(n_wires), loc)
 
-        n_wires = random.randrange(2,20,2)
+    # @pytest.mark.parametrize(
+    #     ("n_wires", "loc", "msg_match"),
+    #     [
+    #         (2, 5, "loc must be an even integer; got 5"),
+    #         (4, 6, "loc must be smaller than or equal to the number of wires; got loc = 6 and number of wires = 4"),
+    #         (5, 0, "number of wires in each block must be larger than or equal to 2; got loc=0"),
+    #     ],
+    # )
+    def test_exception_loc_uneven(self):
+        """Verifies that an exception is raised if loc is not even."""
+
+        n_wires = random.randrange(2,20)
         loc = random.randrange(1,n_wires,2)
-        with pytest.raises(AssertionError, match="loc must be an even integer; got {}".format(loc)):
+        with pytest.raises(ValueError, match=f'loc must be an even integer; got {loc}'):
             compute_indices_MPS(range(n_wires), loc)
 
+    def test_exception_loc_large(self):
+        """Verifies that an exception is raised when loc is too large."""
+
+        n_wires = random.randrange(2,20)
+        loc = random.randrange(np.ceil(n_wires/2)*2,n_wires+20,2) #an even number between n_wires and n_wires + 20
+        with pytest.raises(ValueError, match=f'loc must be smaller than or equal to the number of wires; got loc = {loc} and number of wires = {n_wires}'):
+            compute_indices_MPS(range(n_wires), loc)
+
+    def test_exception_loc_small(self):
+        """Verifies that an exception is raised when loc is less than 2."""
+
+        n_wires = 2
+        loc = 0
+        with pytest.raises(ValueError, match=f'number of wires in each block must be larger than or equal to 2; got loc = {loc}'):
+            compute_indices_MPS(range(n_wires), loc)
+
+    @pytest.mark.parametrize(
+        ("n_wires", "loc"),
+        [
+            (5,4),
+            (9,4),
+            (7,6),
+        ],
+    )
+    def test_warning_many_wires(self, n_wires, loc):
+        """Verifies that a warning is raised if n_wires doesn't correspond to loc."""
+
+        with pytest.warns(Warning, match=f"The number of wires should be a multiple of {int(loc/2)}; got {n_wires}"):
+            compute_indices_MPS(range(n_wires), loc)
 
     @pytest.mark.parametrize(
         ("wires", "loc", "expected_indices"),
@@ -49,46 +79,62 @@ class TestIndicesMPS:
         ],
     )
     def test_indices_output(self, wires, loc, expected_indices):
-
+        """Verifies the indices are correct for both integer and string wire labels."""
         indices = compute_indices_MPS(wires, loc)
         for i in range(len(expected_indices)):
             assert all(indices[i] == expected_indices[i])
 
-class TestInputs:
-    """Test inputs and pre-processing (ensure the correct exceptions are thrown for the inputs)"""
+class TestTemplateInputs:
+    """Test template inputs and pre-processing (ensure the correct exceptions are thrown for the inputs)"""
 
     @pytest.mark.parametrize(
         ("block", "n_params_block", "wires", "loc", "msg_match"),
         [
-            (None, None, [1,2,3,4], 5, "loc must be smaller than or equal to the number of wires; got loc = 5 and number of wires = 4"),
-            (None, None, [1,2], 2, "number of wires must be greater than or equal to 3; got 2"),
-            (None, None, [1,2,3,4], 1, "number of wires in each block must be larger than or equal to 2; got loc=1"),
+            (None, None, [1,2,3,4], 7, "loc must be an even integer; got 7"),
+            (None, None, [1,2,3,4], 6, "loc must be smaller than or equal to the number of wires; got loc = 6 and number of wires = 4"),
+            (None, None, [1,2,3,4], 0, "number of wires in each block must be larger than or equal to 2; got loc = 0"),
         ],
     )
-    def test_exception_wrong_dim(self, block, n_params_block, wires, loc, msg_match):
+    def test_exception_wrong_input(self, block, n_params_block, wires, loc, msg_match):
         """Verifies that an exception is raised if the number of wires or loc is incorrect."""
         with pytest.raises(ValueError, match=msg_match):
             MPS(wires, loc, block, n_params_block)
 
+    def test_warning_many_wires(self):
+        """Verifies that a warning is raised if n_wires doesn't correspond to loc."""
+
+        loc = 4
+        wires = [1,2,3,4,5]
+        n_wires = len(wires)
+        n_params_block = 1
+        with pytest.warns(Warning, match=f"The number of wires should be a multiple of {int(loc/2)}; got {n_wires}"):
+            MPS(wires, loc, block = None, n_params_block=n_params_block)
+
     def circuit1(weights, wires):
-            qml.RZ(weights[0], wires = wires[0])
-            qml.RZ(weights[1], wires = wires[1])
+        qml.RZ(weights[0], wires = wires[0])
+        qml.RZ(weights[1], wires = wires[1])
+    def circuit2(block_weights,block_wires):
+        qml.CNOT(wires=[block_wires[0],block_wires[1]])
+        SELweights = np.array([[[block_weights[0],block_weights[1],block_weights[2]],
+        [block_weights[3],block_weights[4],block_weights[5]]]])
+        qml.StronglyEntanglingLayers(SELweights,wires=block_wires)
+
     @pytest.mark.parametrize(
-        ("block", "n_params_block", "wires", "loc", "weights", "msg_match"),
+        ("block", "n_params_block", "wires", "loc", "block_weights", "msg_match"),
         [
-            (circuit1, 2, [1,2,3,4], 2,  np.random.rand(3,3), "Weights tensor must have first dimension of length 2; got 3"),
-            (circuit1, 2, [1,2,3,4], 2,  np.random.rand(2,4), "Weights tensor must have last dimension of length 3; got 4"),
+            (circuit1, 2, [1,2,3,4], 2,  np.random.rand(3,3), "Weights tensor must have last dimension of length 2; got 3"),
+            (circuit1, 2, [1,2,3,4], 2,  np.random.rand(4,2), "Weights tensor must have first dimension of length 3; got 4"),
         ],
     )
-    def test_exception_wrong_weight_shape(self, block, n_params_block, wires, loc, weights, msg_match):
+    def test_exception_wrong_weight_shape(self, block, n_params_block, wires, loc, block_weights, msg_match):
         """Verifies that an exception is raised if the weights shape is incorrect."""
         with pytest.raises(ValueError, match=msg_match):
-            MPS(wires, loc, block, n_params_block, weights)
+            MPS(wires, loc, block, n_params_block, block_weights)
 
     # @pytest.mark.parametrize(
     #     ("weights"),
     #     [
-    #         ([1,2],
+    #         ([1,2],),
     #         (circuit1, 2, [1,2,3,4], 2,  np.random.rand(2,4), "Weights tensor must have last dimension of length 3; got 4"),
     #     ],
     # )
