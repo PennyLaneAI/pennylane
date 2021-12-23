4f7642ef3576b1b6fc21ff61f88de2d818a2d690
Maria Schuld <mariaschuld@gmail.com>
2021-12-23 12:22:44 +0200

    mottonnen fixed

--- pennylane/templates/state_preparations/mottonen.py
+++ pennylane/templates/state_preparations/mottonen.py
@@ -93,6 +93,53 @@ def _compute_theta(alpha):
     return theta / 2 ** k
 
 
+def _apply_uniform_rotation_dagger(gate, alpha, control_wires, target_wire, op_list):
+    r"""Applies a uniformly-controlled rotation to the target qubit.
+
+    A uniformly-controlled rotation is a sequence of multi-controlled
+    rotations, each of which is conditioned on the control qubits being in a different state.
+    For example, a uniformly-controlled rotation with two control qubits describes a sequence of
+    four multi-controlled rotations, each applying the rotation only if the control qubits
+    are in states :math:`|00\rangle`, :math:`|01\rangle`, :math:`|10\rangle`, and :math:`|11\rangle`, respectively.
+
+    To implement a uniformly-controlled rotation using single qubit rotations and CNOT gates,
+    a decomposition based on Gray codes is used. For this purpose, the multi-controlled rotation
+    angles alpha have to be converted into a set of non-controlled rotation angles theta.
+
+    For more details, see `Möttönen and Vartiainen (2005), Fig 7a<https://arxiv.org/pdf/quant-ph/0504100.pdf>`_.
+
+    Args:
+        gate (.Operation): gate to be applied, needs to have exactly one parameter
+        alpha (tensor_like): angles to decompose the uniformly-controlled rotation into multi-controlled rotations
+        control_wires (array[int]): wires that act as control
+        target_wire (int): wire that acts as target
+        op_list (list[~.Operation]): list of operations to append to
+    """
+
+    theta = _compute_theta(alpha)
+
+    gray_code_rank = len(control_wires)
+
+    if gray_code_rank == 0:
+        if qml.math.all(theta[..., 0] != 0.0):
+            op_list.append(gate(theta[..., 0], wires=[target_wire]))
+        return op_list
+
+    code = gray_code(gray_code_rank)
+    num_selections = len(code)
+
+    control_indices = [
+        int(np.log2(int(code[i], 2) ^ int(code[(i + 1) % num_selections], 2)))
+        for i in range(num_selections)
+    ]
+
+    for i, control_index in enumerate(control_indices):
+        if qml.math.all(theta[..., i] != 0.0):
+            op_list.append(gate(theta[..., i], wires=[target_wire]))
+        op_list.append(qml.CNOT(wires=[control_wires[control_index], target_wire]))
+    return op_list
+
+
 def _get_alpha_z(omega, n, k):
     r"""Computes the rotation angles required to implement the uniformly-controlled Z rotation
     applied to the :math:`k`th qubit.
@@ -291,7 +338,10 @@ class MottonenStatePreparation(Operation):
 
         >>> state_vector = torch.tensor([0.5, 0.5, 0.5, 0.5])
         >>> qml.MottonenStatePreparation.compute_decomposition(state_vector, wires=["a", "b"])
-        XXX
+        [RY(array(1.57079633), wires=['a']),
+        RY(array(1.57079633), wires=['b']),
+        CNOT(wires=['a', 'b']),
+        CNOT(wires=['a', 'b'])]
         """
 
         a = qml.math.abs(state_vector)
@@ -307,55 +357,15 @@ class MottonenStatePreparation(Operation):
             alpha_y_k = _get_alpha_y(a, len(wires_reverse), k)
             control = wires_reverse[k:]
             target = wires_reverse[k - 1]
-            theta = _compute_theta(alpha_y_k)
-            gray_code_rank = len(control)
-
-            if gray_code_rank == 0:
-                if qml.math.all(theta[..., 0] != 0.0):
-                    op_list.append(qml.RY(theta[..., 0], wires=[target]))
-                return op_list
-
-            code = gray_code(gray_code_rank)
-            num_selections = len(code)
-
-            control_indices = [
-                int(np.log2(int(code[i], 2) ^ int(code[(i + 1) % num_selections], 2)))
-                for i in range(num_selections)
-            ]
-
-            for i, control_index in enumerate(control_indices):
-                if qml.math.all(theta[..., i] != 0.0):
-                    op_list.append(qml.RY(theta[..., i], wires=[target]))
-                op_list.append(qml.CNOT(wires=[control[control_index], target]))
+            _apply_uniform_rotation_dagger(qml.RY, alpha_y_k, control, target, op_list)
 
         # If necessary, apply inverse z rotation cascade to prepare correct phases of amplitudes
         if not qml.math.allclose(omega, 0):
             for k in range(len(wires_reverse), 0, -1):
-
                 alpha_z_k = _get_alpha_z(omega, len(wires_reverse), k)
                 control = wires_reverse[k:]
                 target = wires_reverse[k - 1]
                 if len(alpha_z_k) > 0:
-                    theta = _compute_theta(alpha_z_k)
-
-                    gray_code_rank = len(control)
-
-                    if gray_code_rank == 0:
-                        if qml.math.all(theta[..., 0] != 0.0):
-                            op_list.append(qml.RZ(theta[..., 0], wires=[target]))
-                        return op_list
-
-                    code = gray_code(gray_code_rank)
-                    num_selections = len(code)
-
-                    control_indices = [
-                        int(np.log2(int(code[i], 2) ^ int(code[(i + 1) % num_selections], 2)))
-                        for i in range(num_selections)
-                    ]
-
-                    for i, control_index in enumerate(control_indices):
-                        if qml.math.all(theta[..., i] != 0.0):
-                            op_list.append(qml.RZ(theta[..., i], wires=[target]))
-                        op_list.append(qml.CNOT(wires=[control[control_index], target]))
+                    _apply_uniform_rotation_dagger(qml.RZ, alpha_z_k, control, target, op_list)
 
         return op_list
