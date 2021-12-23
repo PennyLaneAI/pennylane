bfeafdf08febc6e38a4281c0b1dd98cc4ccff656
dwierichs <davidwierichs@gmail.com>
2021-12-23 01:46:20 +0100

    add accidentally deleted print

--- tests/transforms/test_batch_transform.py
+++ tests/transforms/test_batch_transform.py
@@ -467,6 +467,7 @@ class TestBatchTransform:
             wrapper = self.default_qnode_wrapper(qnode, targs, tkwargs)
             assert targs == (a,)
             assert tkwargs == {}
+            print("custom wrapper called")
             return wrapper
 
         @my_transform(a)
