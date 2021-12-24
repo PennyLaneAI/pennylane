f1f536a0e32299ad08e74225675e5cfbaa893743
Maria Schuld <mariaschuld@gmail.com>
2021-12-24 17:28:05 +0200

    restore lines

--- pennylane/utils.py
+++ pennylane/utils.py
@@ -467,6 +467,9 @@ def get_generator(op, return_matrix=False):
     if return_matrix:
         obs = obs.matrix()
 
+        if not isinstance(obs, np.ndarray):
+            obs = obs.toarray()
+            
         if op.inverse:
             obs = qml.math.conj(qml.math.T(obs))
 
