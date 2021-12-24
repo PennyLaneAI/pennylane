2179e1f26c69bb2d2b05070da6aac289642bcbcb
Josh Izaac <josh146@gmail.com>
2021-12-24 17:51:03 +0800

    more

--- pennylane/operation.py
+++ pennylane/operation.py
@@ -1202,6 +1202,8 @@ class Operation(Operator):
         with tape:
             self.decomposition()
 
+        print("inside expand:", tape.operations)
+
         if not self.data:
             # original operation has no trainable parameters
             tape.trainable_params = {}
@@ -1209,7 +1211,6 @@ class Operation(Operator):
         if self.inverse:
             tape.inv()
 
-        print("here", tape.operations)
         return tape
 
     def inv(self):
