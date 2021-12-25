00703154d6b074b626be4219981fc69faba0f08d
jackaraz <jackaraz@gmail.com>
2021-12-25 10:03:01 -0500

    batching ability for non-trainable inputs only following issue #2037
    This function creates multiple circuit executions for batched input examples
    and executes all batch inputs with the same trainable variables. The main
    difference between the proposed version in the issue and this commit is the
    input `argnum` this indicates the location of the given input hence gives the
    ability to work across platforms.

---
+++ pennylane/transforms/batch_input.py
@@ -0,0 +1,83 @@
+from typing import Union, Sequence
+
+import pennylane as qml
+from pennylane import numpy as np
+from .batch_transform import batch_transform
+
+
+@batch_transform
+def batch_inputs(
+        tape: qml.tape.JacobianTape, argnum: Union[Sequence[int], int] = 0,
+) -> Sequence[Sequence[qml.tape.JacobianTape], Callable]:
+    """
+    In a classical ML application one needs to batch the non-trainable inputs of the network.
+    This function executes the same analogue for a quantum circuit where separate circuit
+    executions are created for each input examples  which will be executed with the same
+    trainable inputs. The batch dimension assummed to be the first rank of the
+    non trainable tensor object where for a rank 1 feature space the shape needs to be (Nt, x)
+    where `x` indicates the dimension of the features and `Nt` being the number of examples
+    within a batch.
+
+    * Example *
+
+    .. code-block:: python
+
+            dev = qml.device("default.qubit", wires = 2, shots=None)
+            @batch_input(argnum=0)
+            @qml.qnode(dev, diff_method="parameter-shift", interface="tf")
+            def circuit(inputs, weights):
+                qml.AngleEmbedding(inputs, wires = range(2), rotation="Y")
+                qml.RY(weights[0], wires=0)
+                qml.RY(weights[1], wires=1)
+                return qml.expval(qml.PauliZ(1))
+
+            >>> x = np.random.uniform(0,1,(10,2))
+            >>> x.requires_grad = False
+            >>> w = np.random.uniform(0,1,2)
+            >>> circuit(x, w)
+            <tf.Tensor: shape=(10,), dtype=float64, numpy=
+            array([0.17926078, 0.7480163 , 0.47816999, 0.50381628, 0.349178  ,
+                   0.17511444, 0.03769436, 0.19180259, 0.75867188, 0.55335748])>
+
+
+    Parameters
+    ----------
+    tape : qml.tape.JacobianTape
+        Record of the inputs to be executed
+    argnum : Union[Sequence[int], int]
+        One or more argument numbers indicating the location of the batched inputs. As
+        default first argument is assumed to be the only batched input.
+
+    Returns
+    -------
+    Sequence[Sequence[qml.tape.JacobianTape], Callable]
+        list of tapes arranged according to unbatched inputs and a callable function
+        to batch the results.
+    """
+    argnum = tuple(argnum) if isinstance(argnum, (list, tuple)) else (int(argnum),)
+
+    parameters = tape.get_parameters(trainable_only = False)
+
+    non_trainable, trainable = [], []
+    for idx in range(len(parameters)):
+        if idx in argnum:
+            non_trainable.append(parameters[idx])
+        else:
+            trainable.append(parameters[idx])
+
+    assert len(
+        np.unique([qml.math.shape(x)[0] for x in non_trainable])
+    ) == 1, "Batch dimension for all non-trainable inputs must be the same."
+
+    outputs = []
+    for inputs in zip(*non_trainable):
+        outputs += [list(inputs) + trainable]
+
+    # Construct new output tape with unstacked inputs
+    output_tapes = []
+    for params in outputs:
+        new_tape = tape.copy(copy_operations = True)
+        new_tape.set_parameters(params, trainable_only = False)
+        output_tapes.append(new_tape)
+
+    return output_tapes, lambda x: qml.math.squeeze(qml.math.stack(x))
