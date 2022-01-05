4fb6e86e6419ef520616e228e8d834eede47399a
Maria Schuld <mariaschuld@gmail.com>
2022-01-05 16:19:16 +0200

    one more test fixed

--- tests/circuit_graph/test_qasm.py
+++ tests/circuit_graph/test_qasm.py
@@ -558,7 +558,7 @@ class TestQNodeQasmIntegrationTests:
 
         # construct the qnode circuit
         qnode(state=np.array([1, -1, -1, 1]) / np.sqrt(4))
-        res = qnode.qtape.to_openqasm()
+        res = qnode.qtape.to_openqasm(precision=11)
 
         expected = dedent(
             """\
@@ -566,12 +566,12 @@ class TestQNodeQasmIntegrationTests:
             include "qelib1.inc";
             qreg q[2];
             creg c[2];
-            ry(1.5707963267948968) q[0];
-            ry(1.5707963267948968) q[1];
+            ry(1.5707963268) q[0];
+            ry(1.5707963268) q[1];
             cx q[0],q[1];
             cx q[0],q[1];
             cx q[0],q[1];
-            rz(3.141592653589793) q[1];
+            rz(3.1415926536) q[1];
             cx q[0],q[1];
             measure q[0] -> c[0];
             measure q[1] -> c[1];
