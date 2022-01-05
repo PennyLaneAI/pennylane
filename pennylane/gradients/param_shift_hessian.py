d6a46abed1e9b3d182d7eb34152711c51f7a2d4b
dime10 <dime10@users.noreply.github.com>
2022-01-05 04:53:15 -0500

    Review: optimize 0 derivative optimization

--- pennylane/gradients/param_shift_hessian.py
+++ pennylane/gradients/param_shift_hessian.py
@@ -108,17 +108,22 @@ def compute_hessian_tapes(tape, diff_methods, f0=None):
         [[0.25, 1], [1, 1], [-np.pi / 2, -np.pi / 2]],
     ]
 
-    # for now assume all operations support the 2-term parameter shift rule
     for i in range(h_dim):
+        # optimization: skip partial derivatives that are zero
+        if diff_methods[i] == "0":
+            # We may fill in coefficients and shapes for the entire row.
+            hessian_coeffs.extend([[]] * h_dim)
+            shapes.extend(((i, j), 0) for j in range(h_dim))
+            continue
 
-        # Fill in the coefficients and shapes for the entries in the lower triangular part
+        # Prefill Hessian in the lower triangular part for optimization below
         hessian_coeffs.extend([[]] * i)
         shapes.extend(((i, j), 0) for j in range(i))
 
         # optimization: only generate tapes for upper triangular matrix (j >= i)
         for j in range(i, h_dim):
             # optimization: skip partial derivatives that are zero
-            if diff_methods[i] == "0" or diff_methods[j] == "0":
+            if diff_methods[j] == "0":
                 hessian_coeffs.append([])
                 shapes.append(((i, j), 0))
                 continue
