e3dd24c9c945a0c18796cb32b2301b0eef775b3a
Antal Szava <antalszava@gmail.com>
2022-01-11 16:54:30 -0500

    remove outdated test

--- tests/interfaces/test_batch_jax.py
+++ tests/interfaces/test_batch_jax.py
@@ -55,27 +55,6 @@ class TestJaxExecuteUnitTests:
         for args in spy.call_args_list:
             assert args[1]["shift"] == np.pi / 4
 
-    @pytest.mark.skip(reason="just because")
-    def test_unknown_gradient_fn_error(self):
-        """Test that an error is raised if an unknown gradient function
-        is passed"""
-        a = jnp.array([0.1, 0.2])
-
-        dev = qml.device("default.qubit", wires=1)
-
-        def cost(a, device):
-            with qml.tape.JacobianTape() as tape:
-                qml.RY(a[0], wires=0)
-                qml.RX(a[1], wires=0)
-                qml.expval(qml.PauliZ(0))
-
-            return execute([tape], device, gradient_fn=lambda x: x, interface="jax",)[
-                0
-            ][0]
-
-        with pytest.raises(ValueError, match="Unknown gradient function"):
-            res = jax.grad(cost)(a, device=dev)
-
     def test_incorrect_mode(self):
         """Test that an error is raised if an gradient transform
         is used with mode=forward"""
