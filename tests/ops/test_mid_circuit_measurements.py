e0d581c897d2279136956cab9b11515799f4b199
Samuel Banning <samcbanning@gmail.com>
2022-01-13 15:46:35 -0500

    fixed tests

--- tests/ops/test_mid_circuit_measurements.py
+++ tests/ops/test_mid_circuit_measurements.py
@@ -51,7 +51,7 @@ class TestMidCircuitMeasurements:
         assert np.linalg.norm(normal_probs - teleported_probs) < 0.01
 
 
-    def test_runtime_op(self):
+    def test_simple_runtime_op(self):
         dev = qml.device("default.qubit", wires=2)
 
         @qml.qnode(dev)
@@ -65,7 +65,7 @@ class TestMidCircuitMeasurements:
 
         value = runtime_circuit()
 
-    def test_runtime_op(self):
+    def test_complex_runtime_op(self):
 
         dev = qml.device("default.qubit", wires=4)
 
@@ -79,19 +79,12 @@ class TestMidCircuitMeasurements:
             m1 = qml.Measure(1)
             m2 = qml.Measure(2)
 
-            @qml.apply_to_outcome
-            def fun_1(x, y):
-                return x * y
+            out1 = m0 * m1
 
-            out1 = fun_1(m0, m1)
+            out2 = qml.apply_to_outcome(lambda x, y, z: np.sin(x) + y + z)(out1, m1, m2)
 
-            rot = qml.apply_to_outcome(lambda x, y, z: np.sin(x) + y + z)(out1, m1, m2)
-
-            qml.RuntimeOp(qml.RZ, rot, wires=1)
+            qml.RuntimeOp(qml.RZ, out2, wires=1)
 
             return qml.probs(wires=1)
 
         value = runtime_circuit()
-
-        def test_runtime_op(self):
-            dev = qml.device("default.qubit", wires=2)
