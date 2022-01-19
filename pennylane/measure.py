13a4975fe0911a5c5a0615aa8ed10c88110ed1a9
cvjjm <60615188+cvjjm@users.noreply.github.com>
2022-01-19 16:35:22 +0100

    removed left over print statement

--- pennylane/measure.py
+++ pennylane/measure.py
@@ -185,7 +185,6 @@ class MeasurementProcess:
             self.obs.diagonalizing_gates()
             MeasurementProcess(self.return_type, wires=self.obs.wires, eigvals=self.obs.eigvals)
 
-        print(tape)
         return tape
 
     def queue(self, context=qml.QueuingContext):
