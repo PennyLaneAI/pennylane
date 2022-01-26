7a112c7172f539d80e382f73a2e23007c5148061
Samuel Banning <samcbanning@gmail.com>
2022-01-26 11:14:47 -0500

    progress

--- tests/workflows/qaoa.py
+++ tests/workflows/qaoa.py
@@ -4,59 +4,58 @@ from pennylane import numpy as np
 from matplotlib import pyplot as plt
 import networkx as nx
 
+def main(bucket_info=None, device_arn=None, display=False):
 
-edges = [(0, 1), (1, 2), (2, 0), (2, 3)]
-graph = nx.Graph(edges)
-
-# nx.draw(graph, with_labels=True)
-# plt.show()
-
-cost_h, mixer_h = qaoa.min_vertex_cover(graph, constrained=False)
-
-print("Cost Hamiltonian", cost_h)
-print("Mixer Hamiltonian", mixer_h)
-
-def qaoa_layer(gamma, alpha):
-    qaoa.cost_layer(gamma, cost_h)
-    qaoa.mixer_layer(alpha, mixer_h)
-
-wires = range(4)
-depth = 2
-
-def circuit(params, **kwargs):
-    for w in wires:
-        qml.Hadamard(wires=w)
-    qml.layer(qaoa_layer, depth, params[0], params[1])
-
-dev = qml.device("default.qubit", wires=wires)
-
-@qml.qnode(dev)
-def cost_function(params):
-    circuit(params)
-    return qml.expval(cost_h)
-
-optimizer = qml.GradientDescentOptimizer()
-steps = 70
-params = np.array([[0.5, 0.5], [0.5, 0.5]], requires_grad=True)
-
-for i in range(steps):
-    params = optimizer.step(cost_function, params)
-
-print("Optimal Parameters")
-print(params)
+    edges = [(0, 1), (1, 2), (2, 0), (2, 3)]
+    graph = nx.Graph(edges)
 
 
-@qml.qnode(dev)
-def probability_circuit(gamma, alpha):
-    circuit([gamma, alpha])
-    return qml.probs(wires=wires)
+    cost_h, mixer_h = qaoa.min_vertex_cover(graph, constrained=False)
+
+    print("Cost Hamiltonian", cost_h)
+    print("Mixer Hamiltonian", mixer_h)
+
+    def qaoa_layer(gamma, alpha):
+        qaoa.cost_layer(gamma, cost_h)
+        qaoa.mixer_layer(alpha, mixer_h)
+
+    wires = range(4)
+    depth = 2
+
+    def circuit(params, **kwargs):
+        for w in wires:
+            qml.Hadamard(wires=w)
+        qml.layer(qaoa_layer, depth, params[0], params[1])
+
+    dev = qml.device("default.qubit", wires=wires)
+
+    @qml.qnode(dev)
+    def cost_function(params):
+        circuit(params)
+        return qml.expval(cost_h)
+
+    optimizer = qml.GradientDescentOptimizer()
+    steps = 70
+    params = np.array([[0.5, 0.5], [0.5, 0.5]], requires_grad=True)
+
+    for i in range(steps):
+        params = optimizer.step(cost_function, params)
+
+    print("Optimal Parameters")
+    print(params)
 
 
-probs = probability_circuit(params[0], params[1])
+    @qml.qnode(dev)
+    def probability_circuit(gamma, alpha):
+        circuit([gamma, alpha])
+        return qml.probs(wires=wires)
 
-plt.style.use("seaborn")
-plt.bar(range(2 ** len(wires)), probs)
-plt.show()
+
+    probs = probability_circuit(params[0], params[1])
+
+    plt.style.use("seaborn")
+    plt.bar(range(2 ** len(wires)), probs)
+    plt.show()
 
 if __name__ == "__main__":
     my_bucket = "amazon-braket-Bucket-Name"
