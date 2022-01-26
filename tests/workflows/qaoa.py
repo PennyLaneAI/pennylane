f72ea3e35c42a620b0caff41fe9ff59613b7f01e
Samuel Banning <samcbanning@gmail.com>
2022-01-26 10:28:33 -0500

    progress

--- tests/workflows/qaoa.py
+++ tests/workflows/qaoa.py
@@ -1,1 +1,59 @@
- 
+import pennylane as qml
+from pennylane import qaoa
+from pennylane import numpy as np
+from matplotlib import pyplot as plt
+import networkx as nx
+
+
+edges = [(0, 1), (1, 2), (2, 0), (2, 3)]
+graph = nx.Graph(edges)
+
+# nx.draw(graph, with_labels=True)
+# plt.show()
+
+cost_h, mixer_h = qaoa.min_vertex_cover(graph, constrained=False)
+
+print("Cost Hamiltonian", cost_h)
+print("Mixer Hamiltonian", mixer_h)
+
+def qaoa_layer(gamma, alpha):
+    qaoa.cost_layer(gamma, cost_h)
+    qaoa.mixer_layer(alpha, mixer_h)
+
+wires = range(4)
+depth = 2
+
+def circuit(params, **kwargs):
+    for w in wires:
+        qml.Hadamard(wires=w)
+    qml.layer(qaoa_layer, depth, params[0], params[1])
+
+dev = qml.device("qulacs.simulator", wires=wires)
+
+@qml.qnode(dev)
+def cost_function(params):
+    circuit(params)
+    return qml.expval(cost_h)
+
+optimizer = qml.GradientDescentOptimizer()
+steps = 70
+params = np.array([[0.5, 0.5], [0.5, 0.5]], requires_grad=True)
+
+for i in range(steps):
+    params = optimizer.step(cost_function, params)
+
+print("Optimal Parameters")
+print(params)
+
+
+@qml.qnode(dev)
+def probability_circuit(gamma, alpha):
+    circuit([gamma, alpha])
+    return qml.probs(wires=wires)
+
+
+probs = probability_circuit(params[0], params[1])
+
+plt.style.use("seaborn")
+plt.bar(range(2 ** len(wires)), probs)
+plt.show()
