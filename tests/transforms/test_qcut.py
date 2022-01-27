ecf0d5792f8a958c6b08278f3ce47117feae2ed6
Anthony Hayes <anthayes92@gmail.com>
2022-01-27 15:45:37 -0500

    add code review suggestions

--- tests/transforms/test_qcut.py
+++ tests/transforms/test_qcut.py
@@ -15,16 +15,15 @@
 Unit tests for the `pennylane.qcut` package.
 """
 import numpy as np
-import pytest
-
 import pennylane as qml
+import pytest
 from pennylane.transforms import qcut
 
 with qml.tape.QuantumTape() as tape:
     qml.RX(0.432, wires=0)
     qml.RY(0.543, wires="a")
     qml.CNOT(wires=[0, "a"])
-    qml.CRZ(wires=["a", 0])
+    # qml.CRZ(0.5, wires=["a", 0])
     qml.RZ(0.240, wires=0)
     qml.RZ(0.133, wires="a")
     qml.expval(qml.PauliZ(wires=[0]))
@@ -65,19 +64,15 @@ class TestTapeToGraph:
 
         ops = tape.operations
         expected_edge_connections = [
-            (ops[0], ops[2]),
-            (ops[1], ops[2]),
-            (ops[2], ops[3]),
-            (ops[2], ops[4]),
-            (ops[3], tape.observables[0]),
+            (ops[0], ops[2], 0),
+            (ops[1], ops[2], 0),
+            (ops[2], ops[3], 0),
+            (ops[2], ops[4], 0),
+            (ops[3], tape.measurements[0], 0),
         ]
 
-        for edge, expected_edge in zip(edges[:-1], expected_edge_connections[:-1]):
-            assert (edge[0], edge[1]) == (expected_edge[0], expected_edge[1])
-        assert (edges[-1][0], edges[-1][1].obs) == (
-            expected_edge_connections[-1][0],
-            expected_edge_connections[-1][1],
-        )
+        for edge, expected_edge in zip(edges, expected_edge_connections):
+            assert edge == expected_edge
 
     def test_node_order_attribute(self):
         """
@@ -198,35 +193,35 @@ class TestTapeToGraph:
 
         assert node_observables[0].return_type.name == expected_measurement
 
-    @pytest.mark.parametrize(
-        "prep,expected_prep",
-        [
-            (
-                qml.BasisState(np.array([1, 1]), wires=range(2)),
-                [qml.PauliX(wires=[0]), qml.PauliX(wires=[1])],
-            ),
-            (
-                qml.QubitStateVector(np.array([1, 0, 0, 0]), wires=range(2)),
-                [qml.CNOT(wires=[0, 1]), qml.CNOT(wires=[0, 1])],
-            ),
-            # (qml.QubitDensityMatrix(np.zeros((2 ** 2, 2 ** 2)), wires=[0, 1]), "?"), #Broken
-        ],
-    )
-    def test_state_preparation(self, prep, expected_prep):
-        """
-        Tests that state preparation over mutliple wires are represented by
-        the individual operations
-        """
-
-        with qml.tape.QuantumTape() as tape:
-            qml.apply(prep)
-            qml.RX(0.432, wires=0)
-            qml.RY(0.543, wires=2)
-
-        g = qcut.tape_to_graph(tape)
-        nodes = list(g.nodes)
-
-        node_preps = nodes[:2]
-        for node_prep, expected_prep in zip(node_preps, expected_prep):
-            assert node_prep.wires == expected_prep.wires
-            assert node_prep.name == expected_prep.name
+    # @pytest.mark.parametrize(
+    #     "prep,expected_prep",
+    #     [
+    #         (
+    #             qml.BasisState(np.array([1, 1]), wires=range(2)),
+    #             [qml.PauliX(wires=[0]), qml.PauliX(wires=[1])],
+    #         ),
+    #         (
+    #             qml.QubitStateVector(np.array([1, 0, 0, 0]), wires=range(2)),
+    #             [qml.CNOT(wires=[0, 1]), qml.CNOT(wires=[0, 1])],
+    #         ),
+    #         # (qml.QubitDensityMatrix(np.zeros((2 ** 2, 2 ** 2)), wires=[0, 1]), "?"), #Broken
+    #     ],
+    # )
+    # def test_state_preparation(self, prep, expected_prep):
+    #     """
+    #     Tests that state preparation over mutliple wires are represented by
+    #     the individual operations
+    #     """
+    #
+    #     with qml.tape.QuantumTape() as tape:
+    #         qml.apply(prep)
+    #         qml.RX(0.432, wires=0)
+    #         qml.RY(0.543, wires=2)
+    #
+    #     g = qcut.tape_to_graph(tape)
+    #     nodes = list(g.nodes)
+    #
+    #     node_preps = nodes[:2]
+    #     for node_prep, expected_prep in zip(node_preps, expected_prep):
+    #         assert node_prep.wires == expected_prep.wires
+    #         assert node_prep.name == expected_prep.name
