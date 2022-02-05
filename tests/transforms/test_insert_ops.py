2ea62a1e782a17a4bb1f190aa0f68c0df6900c68
Jay Soni <jbsoni@uwaterloo.ca>
2022-02-04 19:14:44 -0500

    fixed bug when trying to insert around an observable (or any operation that also inherits from other classes), added tests

--- tests/transforms/test_insert_ops.py
+++ tests/transforms/test_insert_ops.py
@@ -140,9 +140,12 @@ class TestInsert:
         assert tape.observables[0].wires.tolist() == [0, 1]
         assert tape.measurements[0].return_type is Expectation
 
-    def test_operation_as_position(self):
+    op_lst = [qml.RX, qml.PauliZ, qml.Identity]
+
+    @pytest.mark.parametrize("op", op_lst)
+    def test_operation_as_position(self, op):
         """Test if expected tape is returned when an operation is passed in position"""
-        tape = insert(qml.PhaseDamping, 0.4, position=qml.RX, before=True)(self.tape)
+        tape = insert(qml.PhaseDamping, 0.4, position=op, before=True)(self.tape)
 
         with QuantumTape() as tape_exp:
             qml.PhaseDamping(0.4, wires=0)
@@ -151,7 +154,9 @@ class TestInsert:
             qml.CNOT(wires=[0, 1])
             qml.RY(0.5, wires=0)
             qml.PhaseDamping(0.4, wires=1)
-            qml.RX(0.6, wires=1)
+            qml.PauliZ(wires=1)
+            qml.PhaseDamping(0.4, wires=2)
+            qml.Identity(wires=2)
             qml.expval(qml.PauliZ(0) @ qml.PauliZ(1))
 
         assert all(o1.name == o2.name for o1, o2 in zip(tape.operations, tape_exp.operations))
