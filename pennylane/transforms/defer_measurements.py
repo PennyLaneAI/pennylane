08853a6de6e4b2973c66f7301c960f329343e05d
Antal Szava <antalszava@gmail.com>
2022-02-08 17:02:05 -0500

    refactor

--- pennylane/transforms/defer_measurements.py
+++ pennylane/transforms/defer_measurements.py
@@ -19,26 +19,21 @@ from pennylane.tape import QuantumTape
 
 @qfunc_transform
 def defer_measurements(tape):
-    new_tape_ops = []
-    
-    for op in tape.queue:
-        if isinstance(op, qml.ops.mid_circuit_measure._MidCircuitMeasure):
-            pass
 
-        elif op.__class__.__name__ == "_IfOp":
-            control = op.measured_qubit._dependent_on
-            op_class = op.then_op.__class__
-            if op.data:
-                controlled_op = ctrl(op_class, control=control)(*op.data, wires=op.wires)
-                
-            else:
-                controlled_op = ctrl(op_class, control=control)(wires=op.wires)
-            new_tape_ops.append(controlled_op)
-        else:
-            new_tape_ops.append(op)
-        
     with QuantumTape() as new_tape:
-        for op in new_tape_ops:
-            apply(op)
-            
+        for op in tape.queue:
+            if isinstance(op, qml.ops.mid_circuit_measure._MidCircuitMeasure):
+                pass
+
+            elif op.__class__.__name__ == "_IfOp":
+                control = op.measured_qubit._dependent_on
+                op_class = op.then_op.__class__
+                if op.data:
+                    controlled_op = ctrl(op_class, control=control)(*op.data, wires=op.wires)
+
+                else:
+                    controlled_op = ctrl(op_class, control=control)(wires=op.wires)
+            else:
+                apply(op)
+
     return new_tape
