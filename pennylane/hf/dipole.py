73a36a7489b965560d88c04be2bb795de4c83119
soranjh <soranjh@yahoo.com>
2022-02-08 16:52:14 -0500

    add fermionic and qubit functions

--- pennylane/hf/dipole.py
+++ pennylane/hf/dipole.py
@@ -18,9 +18,8 @@ This module contains the functions needed for computing the dipole moment.
 import autograd.numpy as anp
 import pennylane as qml
 from pennylane import numpy as np
-from pennylane.hf.hartree_fock import generate_scf, nuclear_energy
+from pennylane.hf.basis_data import atomic_numbers
 from pennylane.hf.matrices import generate_moment_matrix
-
 
 def generate_dipole_integrals(mol, core=None, active=None):
     r"""Return a function that computes the dipole moment integrals in the molecular orbital basis.
@@ -83,9 +82,9 @@ def generate_dipole_integrals(mol, core=None, active=None):
             args (array[array[float]]): initial values of the differentiable parameters
 
         Returns:
-            tuple[array[float]]: 1D tuple containing dipole moment integrals
+            tuple[array[float]]: tuple containing dipole moment integrals and core/nuclear constants
         """
-        _, coeffs, _, _, _ = generate_scf(mol)(*args)
+        _, coeffs, _, _, _ = qml.hf.generate_scf(mol)(*args)
 
         dx = anp.einsum(
             "qr,rs,st->qt", coeffs.T, generate_moment_matrix(mol.basis_set, 1, 0)(*args), coeffs
@@ -97,19 +96,26 @@ def generate_dipole_integrals(mol, core=None, active=None):
             "qr,rs,st->qt", coeffs.T, generate_moment_matrix(mol.basis_set, 1, 2)(*args), coeffs
         )
 
-        core_constant = anp.array([0])
+        core_x, core_y, core_z = anp.array([0]), anp.array([0]), anp.array([0])
+
+        for i in range(len(mol.symbols)): # nuclear contributions
+            core_x = core_x + atomic_numbers[mol.symbols[i]] * mol.coordinates[i][0]
+            core_y = core_y + atomic_numbers[mol.symbols[i]] * mol.coordinates[i][1]
+            core_z = core_z + atomic_numbers[mol.symbols[i]] * mol.coordinates[i][2]
 
         if core is None and active is None:
-            return core_constant, dx, dy, dz
+            return (core_x, core_y, core_z), (dx, dy, dz)
 
         for i in core:
-            core_constant = core_constant + 2 * (dx[i][i] + dy[i][i] + dz[i][i])
+            core_x = core_x + 2 * dx[i][i]
+            core_y = core_y + 2 * dy[i][i]
+            core_z = core_z + 2 * dz[i][i]
 
         dx = dx[anp.ix_(active, active)]
         dy = dy[anp.ix_(active, active)]
         dz = dz[anp.ix_(active, active)]
 
-        return core_constant, dx, dy, dz
+        return (core_x, core_y, core_z), (dx, dy, dz)
 
     return dipole_integrals
 
@@ -144,22 +150,82 @@ def generate_fermionic_dipole(mol, cutoff=1.0e-12, core=None, active=None):
         Returns:
             tuple(array[float], list[list[int]]): the dipole coefficients and operators
         """
-        core_constant, dx, dy, dz = generate_dipole_integrals(mol, core, active)(*args)
+        f = []
+        constants, integrals = generate_dipole_integrals(mol, core, active)(*args)
+        constants = anp.negative(constants)
 
-        coeffs = anp.array([])
-        operators = [[]]
+        for i in range(3):
+            f.append(
+                one_particle(constants[i], integrals[i], cutoff=cutoff, core=core, active=active))
 
-        coeffs = anp.concatenate((coeffs, core_constant))
-
-        for d in [dx, dy, dz]:
-            indices = anp.argwhere(abs(d) >= cutoff)
-            operators = (
-                operators + (indices * 2).tolist() + (indices * 2 + 1).tolist()
-            )  # up-up + down-down terms
-            coeffs = anp.concatenate((coeffs, anp.tile(d[abs(d) >= cutoff], 2)))
-
-        indices_sort = [operators.index(i) for i in sorted(operators)]
-
-        return coeffs[indices_sort], sorted(operators)
+        return f
 
     return fermionic_dipole
+
+
+def generate_dipole(mol, cutoff=1.0e-12, core=None, active=None):
+
+    def dipole(*args):
+        d = []
+        d_ferm = generate_fermionic_dipole(mol, cutoff, core, active)(*args)
+        for i in d_ferm:
+            d.append(qubit_operator(i, cutoff=cutoff))
+
+        return d
+
+    return dipole
+
+
+def one_particle(core_constant, integral, cutoff=1.0e-12):
+
+    coeffs = anp.array([])
+
+    if core_constant != 0:
+        coeffs = anp.concatenate((coeffs, core_constant))
+        operators = [[]]
+    else:
+        operators = []
+
+    for d in [integral]:
+        i = anp.argwhere(abs(d) >= cutoff)
+        operators = operators + (i * 2).tolist() + (i * 2 + 1).tolist()  # up-up + down-down terms
+        coeffs = anp.concatenate((coeffs, anp.tile(d[abs(d) >= cutoff], 2)))
+
+    indices_sort = [operators.index(i) for i in sorted(operators)]
+
+    return coeffs[indices_sort], sorted(operators)
+
+
+def qubit_operator(d_ferm, cutoff=1.0e-12):
+    if len(d_ferm[0]) == 0 and len(d_ferm[1]) == 0:
+        return qml.Hamiltonian([anp.array([0.0])], [qml.Identity(0)])
+
+    ops = []
+    coeffs = anp.array([])
+
+    for n, t in enumerate(d_ferm[1]):
+
+        if len(t) == 0:
+            coeffs = anp.array([0.0])
+            coeffs = coeffs + np.array([d_ferm[0][n]])
+            ops = ops + [qml.Identity(0)]
+
+        else:
+            op = qml.hf.hamiltonian._generate_qubit_operator(t)
+            if op != 0:
+                for i, o in enumerate(op[1]):
+                    if len(o) == 0:
+                        op[1][i] = qml.Identity(0)
+                    if len(o) == 1:
+                        op[1][i] = qml.hf.hamiltonian._return_pauli(o[0][1])(o[0][0])
+                    if len(o) > 1:
+                        k = qml.Identity(0)
+                        for o_ in o:
+                            k = k @ qml.hf.hamiltonian._return_pauli(o_[1])(o_[0])
+                        op[1][i] = k
+                coeffs = np.concatenate([coeffs, np.array(op[0]) * d_ferm[0][n]])
+                ops = ops + op[1]
+
+    d = qml.hf.hamiltonian.simplify(qml.Hamiltonian(coeffs, ops), cutoff=cutoff) * (-1)
+
+    return d
