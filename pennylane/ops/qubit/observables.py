12d80f6d76f5ddca3d3def76f3a2dc60e4b0286a
Josh Izaac <josh146@gmail.com>
2022-02-10 21:54:45 +0800

    more

--- pennylane/ops/qubit/observables.py
+++ pennylane/ops/qubit/observables.py
@@ -110,7 +110,7 @@ class Hermitian(Observable):
         Returns:
             dict[str, array]: dictionary containing the eigenvalues and the eigenvectors of the Hermitian observable
         """
-        Hmat = self.matrix()
+        Hmat = self.get_matrix()
         Hmat = qml.math.to_numpy(Hmat)
         Hkey = tuple(Hmat.flatten().tolist())
         if Hkey not in Hermitian._eigs:
