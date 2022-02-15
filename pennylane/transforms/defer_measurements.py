adac22d2b2da5322dd2c77dfc39f3c57b42472cf
Samuel Banning <samcbanning@gmail.com>
2022-02-15 15:03:08 -0500

    progress

--- pennylane/transforms/defer_measurements.py
+++ pennylane/transforms/defer_measurements.py
@@ -38,7 +38,7 @@ def defer_measurements(tape):
                                 flip_wires.append(control[i])
                         if flip_wires:
                             qml.RZ(math.pi, wires=flip_wires)
-                        controlled_op = ctrl(op.construct_op, control=control)()
+                        ctrl(op.construct_op, control=control)()
                 flip_wires = []
                 for i, flip in enumerate(flipped):
                     if flip:
