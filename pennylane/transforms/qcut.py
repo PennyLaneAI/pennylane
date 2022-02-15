4c4f2a3a7ed64d18f5dcaffeb08c821f48efd17b
trbromley <brotho02@gmail.com>
2022-02-15 17:53:18 -0500

    Add docstrings

--- pennylane/transforms/qcut.py
+++ pennylane/transforms/qcut.py
@@ -438,14 +438,14 @@ def _process_tensor(results, n_prep: int, n_meas: int):
 
     This function performs the following steps:
 
-    - Reshapes ``results`` into the shape ``(4,) * n_prep + (dim_meas,)``
-    - Shuffles the final axis to follow the standard product over measurement settings. E.g., for
+    1. Reshapes ``results`` into the shape ``(4,) * n_prep + (4**n_meas,)``
+    2. Shuffles the final axis to follow the standard product over measurement settings. E.g., for
       ``n_meas = 2`` the standard product is: II, IX, IY, IZ, XI, ..., ZY, ZZ while the input order
       will be the result of ``qml.grouping.partition_pauli_group(2)``, i.e., II, IZ, ZI, ZZ, ...,
       YY.
-    - Reshapes into the target final shape ``(4,) * (n_prep + n_meas)``
-    - Performs a change of basis for the preparation indices (the first ``n_prep`` indices) from the
-      |0>, |1>, |+>, |+i> basis to the I, X, Y, Z basis.
+    3. Reshapes into the final target shape ``(4,) * (n_prep + n_meas)``
+    4. Performs a change of basis for the preparation indices (the first ``n_prep`` indices) from
+       the |0>, |1>, |+>, |+i> basis to the I, X, Y, Z basis using ``CHANGE_OF_BASIS``.
 
     Args:
         results (array-like): the input execution results
@@ -458,22 +458,27 @@ def _process_tensor(results, n_prep: int, n_meas: int):
     n = n_prep + n_meas
     dim_meas = 4**n_meas
 
+    # Step 1
     intermediate_shape = (4,) * n_prep + (dim_meas,)
     intermediate_tensor = qml.math.reshape(results, intermediate_shape)
 
+    # Step 2
     grouped = qml.grouping.partition_pauli_group(n_meas)
     grouped_flat = [term for group in grouped for term in group]
     order = qml.math.argsort(grouped_flat)
 
     if qml.math.get_interface(intermediate_tensor) == "tensorflow":
+        # TensorFlow does not support slicing
         intermediate_tensor = qml.math.gather(intermediate_tensor, order, axis=-1)
     else:
         sl = [slice(None)] * n_prep + [order]
         intermediate_tensor = intermediate_tensor[tuple(sl)]
 
+    # Step 3
     final_shape = (4,) * n
     final_tensor = qml.math.reshape(intermediate_tensor, final_shape)
 
+    # Step 4
     change_of_basis = qml.math.convert_like(CHANGE_OF_BASIS, intermediate_tensor)
 
     for i in range(n_prep):
@@ -481,6 +486,8 @@ def _process_tensor(results, n_prep: int, n_meas: int):
         final_tensor = qml.math.tensordot(change_of_basis, final_tensor, axes=axes)
 
     axes = list(reversed(range(n_prep))) + list(range(n_prep, n))
+
+    # Use transpose to reorder indices
     final_tensor = qml.math.transpose(final_tensor, axes=axes)
 
     final_tensor *= qml.math.power(2, -(n_meas + n_prep) / 2)
@@ -501,7 +508,8 @@ def _to_tensors(
     Args:
         results (array-like): A collection of execution results corresponding to the
             expansion of circuit fragments in the communication graph over measurement and
-            preparation nodes. These results are processed into tensors by this function.
+            preparation node configurations. These results are processed into tensors by this
+            function.
         prepare_nodes (Sequence[Sequence[PrepareNode]]): a sequence of size
             ``len(communication_graph.nodes)`` that determines the order of preparation indices in
             each tensor
@@ -550,7 +558,8 @@ def qcut_processing_fn(
     Args:
         results (Sequence[Sequence]): A collection of execution results corresponding to the
             expansion of circuit fragments in the ``communication_graph`` over measurement and
-            preparation nodes. These results are processed into tensors and then contracted.
+            preparation node configurations. These results are processed into tensors and then
+            contracted.
         communication_graph (MultiDiGraph): the communication graph determining connectivity between
             circuit fragments
         prepare_nodes (Sequence[Sequence[PrepareNode]]): a sequence of size
