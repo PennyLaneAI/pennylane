62ed4431b39c4f510fa06b676e3849f8a19d4457
trbromley <brotho02@gmail.com>
2022-02-15 17:18:16 -0500

    Add docstring

--- pennylane/transforms/qcut.py
+++ pennylane/transforms/qcut.py
@@ -510,19 +510,40 @@ def _to_tensors(
 
 
 def qcut_processing_fn(
-    results: Sequence,
+    results: Sequence[Sequence],
     communication_graph: MultiDiGraph,
     prepare_nodes: Sequence[Sequence[PrepareNode]],
     measure_nodes: Sequence[Sequence[MeasureNode]],
     use_opt_einsum: bool = False,
 ):
-    """TODO
+    """Processing function for the :func:`cut_circuit` transform.
+
+    .. note::
+
+        This function is designed for use as part of the circuit cutting workflow. Check out the
+        :doc:`transforms </code/qml_transforms>` page for more details.
 
     Args:
-        results:
-        communication_graph:
-        prepare_nodes:
-        measure_nodes:
+        results (Sequence[Sequence]): A collection of execution results corresponding to the
+            expansion of circuit fragments in the ``communication_graph`` over measurement and
+            preparation nodes. These results are processed into tensors and then contracted.
+        communication_graph (MultiDiGraph): the communication graph determining connectivity between
+            circuit fragments
+        prepare_nodes (Sequence[Sequence[PrepareNode]]): a sequence of size
+            ``len(communication_graph.nodes)`` that determines the order of preparation indices in
+            each tensor
+        measure_nodes (Sequence[Sequence[MeasureNode]]): a sequence of size
+            ``len(communication_graph.nodes)`` that determines the order of measurement indices in
+            each tensor
+        use_opt_einsum (bool): Determines whether to use the
+            `opt_einsum <https://dgasmith.github.io/opt_einsum/>`__ package. This package is useful
+            for tensor contractions of large networks but must be installed separately using, e.g.,
+            ``pip install opt_einsum``. Both settings for ``use_opt_einsum`` result in a
+            differentiable contraction.
+
+    Returns:
+        float or array-like: the output of the original uncut circuit arising from contracting
+        the tensor network of circuit fragments
     """
     flat_results = qml.math.concatenate(results)
 
