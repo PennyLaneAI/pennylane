6c568aef70be1d6e637a9ed267aee29a5c5442b9
Josh Izaac <josh146@gmail.com>
2022-02-19 01:15:37 +0800

    Raise a deprecation warning if `Operator.decomposition` is a staticmethod

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -16,6 +16,7 @@ Unit tests for :mod:`pennylane.operation`.
 """
 import itertools
 from functools import reduce
+import warnings
 
 import pytest
 import numpy as np
@@ -1845,3 +1846,46 @@ class TestDeprecationWarnings:
             m1 = op.eigvals
 
         assert np.allclose(m1, op.get_eigvals())
+
+    def test_decomposition_deprecation(self):
+        """Test that old-style staticmethod decompositions raise a warning"""
+        dev = qml.device("default.qubit", wires=1)
+
+        class MyOp(Operation):
+            num_wires = 1
+            num_params = 2
+
+            @staticmethod
+            def decomposition(*params, wires):
+                qml.RY(params[0], wires=wires[0])
+                qml.PauliZ(wires=wires[0])
+                qml.RX(params[1], wires=wires[0])
+
+        @qml.qnode(dev)
+        def qnode(*params):
+            MyOp(*params, wires=0)
+            return qml.state()
+
+        with pytest.warns(UserWarning, match="is now an instance method"):
+            print(qnode(0.1, 0.2))
+
+        # using an instance method will not raise a deprecation warning
+
+        class MyOp(Operation):
+            num_wires = 1
+            num_params = 2
+
+            def decomposition(self):
+                qml.RY(self.parameters[0], wires=self.wires[0])
+                qml.PauliZ(wires=self.wires[0])
+                qml.RX(self.parameters[1], wires=self.wires[0])
+
+        @qml.qnode(dev)
+        def qnode(*params):
+            MyOp(*params, wires=0)
+            return qml.state()
+
+        with warnings.catch_warnings():
+            # any warnings emitted will be raised as errors
+            warnings.simplefilter("error")
+            print(qnode(0.1, 0.2))
