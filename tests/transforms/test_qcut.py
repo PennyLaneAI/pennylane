4eb99414fdd2097bb65359795f6959ccfe6cd15f
trbromley <brotho02@gmail.com>
2022-02-27 13:42:54 -0500

    Add Torch JIT test

--- tests/transforms/test_qcut.py
+++ tests/transforms/test_qcut.py
@@ -1909,6 +1909,47 @@ class TestCutCircuitTransform:
 
         assert np.isclose(grad, grad_expected)
 
+    def test_simple_cut_circuit_torch_jit(self, mocker, use_opt_einsum):
+        """
+        Tests the full circuit cutting pipeline returns the correct value and
+        gradient for a simple circuit using the `cut_circuit` transform with the torch interface and
+        using JIT.
+        """
+        torch = pytest.importorskip("torch")
+
+        dev = qml.device("default.qubit", wires=2)
+
+        @qml.qnode(dev, interface="torch")
+        def circuit(x):
+            qml.RX(x, wires=0)
+            qml.RY(0.543, wires=1)
+            qml.WireCut(wires=0)
+            qml.CNOT(wires=[0, 1])
+            qml.RZ(0.240, wires=0)
+            qml.RZ(0.133, wires=1)
+            return qml.expval(qml.PauliZ(wires=[0]))
+
+        x = torch.tensor(0.531, requires_grad=True)
+
+        cut_circuit_jit = torch.jit.trace(qcut.cut_circuit(circuit, use_opt_einsum=use_opt_einsum), x)
+
+        spy = mocker.spy(qcut, "qcut_processing_fn")
+        res = cut_circuit_jit(x)
+
+        assert spy.call_count == 0
+
+        res_expected = circuit(x)
+        assert np.isclose(res.detach().numpy(), res_expected.detach().numpy())
+
+        res.backward()
+        grad = x.grad.detach().numpy()
+
+        x.grad = None
+        res_expected.backward()
+        grad_expected = x.grad.detach().numpy()
+
+        assert np.isclose(grad, grad_expected)
+
     def test_simple_cut_circuit_tf(self, use_opt_einsum):
         """
         Tests the full circuit cutting pipeline returns the correct value and
