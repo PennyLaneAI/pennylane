24777d242270468a79c5acb81a3cac6bfdc48678
trbromley <brotho02@gmail.com>
2022-02-28 15:40:40 -0500

    Remove repetition

--- tests/transforms/test_qcut.py
+++ tests/transforms/test_qcut.py
@@ -2001,38 +2001,6 @@ class TestCutCircuitTransform:
         assert np.isclose(res, res_expected)
         assert np.isclose(grad, grad_expected)
 
-    def test_simple_cut_circuit_jax(self, use_opt_einsum):
-        """
-        Tests the full circuit cutting pipeline returns the correct value and
-        gradient for a simple circuit using the `cut_circuit` transform with the Jax interface.
-        """
-        jax = pytest.importorskip("jax")
-        import jax.numpy as jnp
-
-        dev = qml.device("default.qubit", wires=2)
-
-        @qml.qnode(dev, interface="jax")
-        def circuit(x):
-            qml.RX(x, wires=0)
-            qml.RY(0.543, wires=1)
-            qml.WireCut(wires=0)
-            qml.CNOT(wires=[0, 1])
-            qml.RZ(0.240, wires=0)
-            qml.RZ(0.133, wires=1)
-            return qml.expval(qml.PauliZ(wires=[0]))
-
-        x = jnp.array(0.531)
-        cut_circuit = qcut.cut_circuit(circuit, use_opt_einsum=use_opt_einsum)
-
-        res = cut_circuit(x)
-        res_expected = circuit(x)
-
-        grad = jax.grad(cut_circuit)(x)
-        grad_expected = jax.grad(circuit)(x)
-
-        assert np.isclose(res, res_expected)
-        assert np.isclose(grad, grad_expected)
-
     def test_simple_cut_circuit_jax_jit(self, mocker, use_opt_einsum):
         """
         Tests the full circuit cutting pipeline returns the correct value and
