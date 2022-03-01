8656d6d9d0796b9c2c6a5267c3babb491980f8c4
Antal Szava <antalszava@gmail.com>
2022-03-01 10:50:48 -0500

    formatting

--- pennylane/transforms/condition.py
+++ pennylane/transforms/condition.py
@@ -77,6 +77,7 @@ def cond(measurement, then_op):
             qml.cond(m_0, qml.RZ)(sec_par, wires=1)
             return qml.expval(qml.PauliZ(1))
     """
+
     @wraps(then_op)
     def wrapper(*args, **kwargs):
         return Conditional(measurement, then_op(*args, do_queue=False, **kwargs))
