05a9ba9b0b471d85db54f49989110a6d7a03b56c
trbromley <brotho02@gmail.com>
2022-03-02 07:42:31 -0500

    Fix test

--- tests/transforms/test_qcut.py
+++ tests/transforms/test_qcut.py
@@ -2077,13 +2077,16 @@ class TestCutCircuitTransform:
     def test_circuit_with_disconnected_components(self, use_opt_einsum, mocker):
         """Tests if a circuit that is fragmented into subcircuits such that some of the subcircuits
         are disconnected from the final terminal measurements is executed correctly"""
-        dev = qml.device("default.qubit", wires=1)
+        dev = qml.device("default.qubit", wires=3)
 
         @qml.transforms.cut_circuit(use_opt_einsum=use_opt_einsum)
         @qml.qnode(dev)
         def circuit(x):
             qml.RX(x, wires=0)
-            qml.RY(x**2, wires=1)
+            qml.CNOT(wires=[0, 1])
+            qml.WireCut(wires=0)
+            qml.CNOT(wires=[0, 1])
+            qml.RY(x**2, wires=2)
             return qml.expval(qml.PauliZ(wires=[0]))
 
         spy = mocker.spy(qcut, "contract_tensors")
@@ -2091,7 +2094,8 @@ class TestCutCircuitTransform:
         x = 0.4
         res = circuit(x)
         assert np.allclose(res, np.cos(x))
-        assert len(spy.call_args[0][0]) == 1  # check number of tensors
+        assert len(spy.call_args[0][0]) == 1  # there should be 2 tensors for wire 0
+        assert spy.call_args[0][0][0].shape == (4, 4)
 
 
 class TestCutCircuitTransformValidation:
