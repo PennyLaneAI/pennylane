f239416f9e51795eeffb7d29f740ef53d9c0be45
jackaraz <jackaraz@gmail.com>
2022-03-03 12:01:09 +0000

    minor fix in documentation

--- pennylane/transforms/batch_input.py
+++ pennylane/transforms/batch_input.py
@@ -14,20 +14,34 @@ def batch_input(
     argnum: Union[Sequence[int], int] = 0,
 ) -> Tuple[Sequence[qml.tape.JacobianTape], Callable]:
     """
+    Transform a QNode to support an initial batch dimension for gate inputs.
     In a classical ML application one needs to batch the non-trainable inputs of the network.
-    This function executes the same analogue for a quantum circuit where separate circuit
-    executions are created for each input examples  which will be executed with the same
-    trainable inputs. The batch dimension assummed to be the first rank of the
-    non trainable tensor object where for a rank 1 feature space the shape needs to be (Nt, x)
-    where `x` indicates the dimension of the features and `Nt` being the number of examples
-    within a batch.
-    Based on https://arxiv.org/abs/2202.10471
+    This function executes the same analogue for a quantum circuit:
+    separate circuit executions are created for each input, which are then executed
+    with the *same* trainable parameters.
 
-    * Example *
+    The batch dimension is assumed to be the first rank of the
+    non trainable tensor object. For a rank 1 feature space, the shape needs to be ``(Nt, x)``
+    where ``x`` indicates the dimension of the features and ``Nt`` being the number of examples
+    within a batch.
+    Based on `arXiv:2202.10471 <https://arxiv.org/abs/2202.10471>__.
+
+    Args:
+        tape (.tape.JacobianTape or .QNode): Input quantum circuit to batch
+        argnum (Sequence[int] or int): One or more location indices indicating the
+            location of the non-trainable batched inputs within the input argument sequence
+            of the circuit. By default first argument is assumed to be the only batched input.
+
+    Returns:
+        Sequence[Sequence[.tape.JacobianTape], Callable]: list of tapes arranged
+        according to unbatched inputs and a callable function to batch the results.
+
+    **Example**
 
     .. code-block:: python
 
             dev = qml.device("default.qubit", wires = 2, shots=None)
+
             @batch_input(argnum=0)
             @qml.qnode(dev, diff_method="parameter-shift", interface="tf")
             def circuit(inputs, weights):
@@ -36,27 +50,15 @@ def batch_input(
                 qml.RY(weights[1], wires=1)
                 return qml.expval(qml.PauliZ(1))
 
-            >>> x = np.random.uniform(0,1,(10,2))
-            >>> x.requires_grad = False
-            >>> w = np.random.uniform(0,1,2)
-            >>> circuit(x, w)
-            <tf.Tensor: shape=(10,), dtype=float64, numpy=
-            array([0.17926078, 0.7480163 , 0.47816999, 0.50381628, 0.349178  ,
-                   0.17511444, 0.03769436, 0.19180259, 0.75867188, 0.55335748])>
+        >>> x = np.random.uniform(0,1,(10,2))
+        >>> x.requires_grad = False
+        >>> w = np.random.uniform(0,1,2)
+        >>> circuit(x, w)
+        <tf.Tensor: shape=(10,), dtype=float64, numpy=
+        array([0.17926078, 0.7480163 , 0.47816999, 0.50381628, 0.349178  ,
+               0.17511444, 0.03769436, 0.19180259, 0.75867188, 0.55335748])>
 
-
-    Parameters
-    ----------
-    tape (qml.tape.JacobianTape or qml.QNode): Record of the inputs to be executed
-    argnum (Sequence[int] or int) : One or more argument numbers indicating the
-        location of the batched inputs. As default first argument is assumed to be the only
-        batched input.
-
-    Returns
-    -------
-    Sequence[Sequence[qml.tape.JacobianTape], Callable]
-        list of tapes arranged according to unbatched inputs and a callable function
-        to batch the results.
+    .. seealso:: :func:`~.batch_params`
     """
     parameters = tape.get_parameters(trainable_only=False)
 
