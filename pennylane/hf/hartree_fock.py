1647e5973eae198c86d3a212d72ad511ab6ed369
soranjh <soranjh@yahoo.com>
2022-03-03 11:31:54 -0500

    update tests

--- pennylane/hf/hartree_fock.py
+++ pennylane/hf/hartree_fock.py
@@ -248,7 +248,7 @@ def hf_energy(mol):
         Returns:
             float: the Hartree-Fock energy
         """
-        _, coeffs, fock_matrix, h_core, _ = generate_scf(mol)(*args)
+        _, coeffs, fock_matrix, h_core, _ = scf(mol)(*args)
         e_rep = nuclear_energy(mol.nuclear_charges, mol.coordinates)(*args)
         e_elec = anp.einsum(
             "pq,qp", fock_matrix + h_core, mol_density_matrix(mol.n_electrons, coeffs)
