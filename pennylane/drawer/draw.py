40d45aba868b1a9f2134158a51a8a2f516c4c6be
albi3ro <chrissie.c.l@gmail.com>
2022-03-04 12:09:56 -0500

    minor

--- pennylane/drawer/draw.py
+++ pennylane/drawer/draw.py
@@ -331,7 +331,7 @@ def draw(
     cache["tape_offset"] += len(tape_cache)
     for i, nested_tape in enumerate(tape_cache):
         label = f"\nTape:{i+current_tape_offset}"
-        tape_str = tape_text(
+        tape_str = draw(
             nested_tape,
             wire_order,
             show_all_wires,
@@ -360,6 +360,155 @@ def draw(
     show_matrices=False,
     expansion_strategy=None,
 ):
+    """Create a function that draws the given qnode.
+
+     Args:
+         qnode (.QNode): the input QNode that is to be drawn.
+         wire_order (Sequence[Any]): the order (from top to bottom) to print the wires of the circuit
+         show_all_wires (bool): If True, all wires, including empty wires, are printed.
+         decimals (int): How many decimal points to include when formatting operation parameters.
+             ``None`` will omit parameters from operation labels.
+         max_length (int): Maximum string width (columns) when printing the circuit
+         show_matrices=False (bool): show matrix valued parameters below all circuit diagrams
+         expansion_strategy (str): The strategy to use when circuit expansions or decompositions
+             are required.
+
+             - ``gradient``: The QNode will attempt to decompose
+               the internal circuit such that all circuit operations are supported by the gradient
+               method.
+
+             - ``device``: The QNode will attempt to decompose the internal circuit
+               such that all circuit operations are natively supported by the device.
+
+
+     Returns:
+         A function that has the same argument signature as ``qnode``. When called,
+         the function will draw the QNode.
+
+     **Example**
+
+     .. code-block:: python3
+
+         @qml.qnode(qml.device('lightning.qubit', wires=2))
+         def circuit(a, w):
+             qml.Hadamard(0)
+             qml.CRX(a, wires=[0, 1])
+             qml.Rot(*w, wires=[1])
+             qml.CRX(-a, wires=[0, 1])
+             return qml.expval(qml.PauliZ(0) @ qml.PauliZ(1))
+
+     >>> print(qml.draw(circuit)(a=2.3, w=[1.2, 3.2, 0.7]))
+     1: ────╭RX(2.30)──Rot(1.20,3.20,0.70)─╭RX(-2.30)─┤ ╭<Z@Z>
+     0: ──H─╰C─────────────────────────────╰C─────────┤ ╰<Z@Z>
+
+     .. UsageDetails::
+
+
+     By specifying the ``decimals`` keyword, parameters are displayed to the specified precision.
+
+     >>> print(qml.draw(circuit, decimals=4)(a=2.3, w=[1.2, 3.2, 0.7]))
+     0: ──H─╭C─────────────────────────────────────╭C───────────┤ ╭<Z@Z>
+     1: ────╰RX(2.3000)──Rot(1.2000,3.2000,0.7000)─╰RX(-2.3000)─┤ ╰<Z@Z>
+
+     Parameters can be omitted by requesting ``decimals=None``:
+
+     >>> print(qml.draw(circuit, decimals=None)(a=2.3, w=[1.2, 3.2, 0.7]))
+     0: ──H─╭C───────╭C──┤ ╭<Z@Z>
+     1: ────╰RX──Rot─╰RX─┤ ╰<Z@Z>
+
+     If the parameters are not acted upon by classical processing like ``-a``, then
+     ``qml.draw`` can handle string-valued parameters as well:
+
+     >>> @qml.qnode(qml.device('lightning.qubit', wires=1))
+     ... def circuit2(x):
+     ...     qml.RX(x, wires=0)
+     ...     return qml.expval(qml.PauliZ(0))
+     >>> print(qml.draw(circuit2)("x"))
+     0: ──RX(x)─┤  <Z>
+
+     When requested with ``show_matrices=True``, matrix valued parameters are printed below the
+     circuit:
+
+     .. code-block:: python
+
+     >>> @qml.qnode(qml.device('default.qubit', wires=2))
+     ... def circuit():
+     ...     qml.QubitUnitary(np.eye(2), wires=0)
+     ...     qml.QubitUnitary(-np.eye(4), wires=(0,1))
+     ...     return qml.expval(qml.Hermitian(np.eye(2), wires=1))
+     >>> print(qml.draw(circuit, show_matrices=True)())
+     0: ──U(M0)─╭U(M1)─┤
+     1: ────────╰U(M1)─┤  <𝓗(M0)>
+     M0 =
+     [[1. 0.]
+     [0. 1.]]
+     M1 =
+     [[-1. -0. -0. -0.]
+     [-0. -1. -0. -0.]
+     [-0. -0. -1. -0.]
+     [-0. -0. -0. -1.]]
+
+     The ``max_length`` keyword warps long circuits:
+
+     .. code-block:: python
+
+         rng = np.random.default_rng(seed=42)
+         shape = qml.StronglyEntanglingLayers.shape(n_wires=3, n_layers=3)
+         params = rng.random(shape)
+
+         @qml.qnode(qml.device('lightning.qubit', wires=3))
+         def longer_circuit(params):
+             qml.StronglyEntanglingLayers(params, wires=range(3))
+             return [qml.expval(qml.PauliZ(i)) for i in range(3)]
+
+         print(qml.draw(longer_circuit, max_length=60)(params))
+
+     .. code-block:: none
+
+         0: ──Rot(0.77,0.44,0.86)─╭C────╭X──Rot(0.45,0.37,0.93)─╭C─╭X
+         1: ──Rot(0.70,0.09,0.98)─╰X─╭C─│───Rot(0.64,0.82,0.44)─│──╰C
+         2: ──Rot(0.76,0.79,0.13)────╰X─╰C──Rot(0.23,0.55,0.06)─╰X───
+
+         ───Rot(0.83,0.63,0.76)──────────────────────╭C────╭X─┤  <Z>
+         ──╭X────────────────────Rot(0.35,0.97,0.89)─╰X─╭C─│──┤  <Z>
+         ──╰C────────────────────Rot(0.78,0.19,0.47)────╰X─╰C─┤  <Z>
+
+     The ``wire_order`` keyword specifies the order of the wires from
+     top to bottom:
+
+     >>> print(qml.draw(circuit, wire_order=[1,0])(a=2.3, w=[1.2, 3.2, 0.7]))
+     1: ────╭RX(2.30)──Rot(1.20,3.20,0.70)─╭RX(-2.30)─┤ ╭<Z@Z>
+     0: ──H─╰C─────────────────────────────╰C─────────┤ ╰<Z@Z>
+
+     If the device or ``wire_order`` has wires not used by operations, those wires are omitted
+     unless requested with ``show_all_wires=True``
+
+     >>> empty_qfunc = lambda : qml.expval(qml.PauliZ(0))
+     >>> empty_circuit = qml.QNode(empty_qfunc, qml.device('lightning.qubit', wires=3))
+     >>> print(qml.draw(empty_circuit, show_all_wires=True)())
+     0: ───┤  <Z>
+     1: ───┤
+     2: ───┤
+
+     Drawing also works on batch transformed circuits:
+
+     .. code-block:: python
+
+         @qml.gradients.param_shift(shift=0.1)
+         @qml.qnode(qml.device('lightning.qubit', wires=1))
+         def transformed_circuit(x):
+             qml.RX(x, wires=0)
+             return qml.expval(qml.PauliZ(0))
+
+         print(qml.draw(transformed_circuit)(np.array(1.0)))
+
+     .. code-block:: none
+
+         0: ──RX(1.10)─┤  <Z>
+
+         0: ──RX(0.90)─┤  <Z>
+
+     """
     @wraps(qnode)
     def wrapper(*args, **kwargs):
 
