0d970c6aaf3d3305ed2da21b6ee07ae7dfecedb5
dwierichs <davidwierichs@gmail.com>
2022-03-06 19:37:05 +0100

    some inconsistencies

--- pennylane/ops/snapshot.py
+++ pennylane/ops/snapshot.py
@@ -17,7 +17,7 @@ cv and qubit computing paradigms in PennyLane.
 """
 import numpy as np
 
-from pennylane.operation import AnyWires, Operation
+from pennylane.operation import AllWires, AnyWires, Operation
 
 
 # pylint: disable=unused-argument
@@ -42,7 +42,7 @@ class Snapshot(Operation):
 
     def __init__(self, tag=None):
         self.tag = tag
-        super().__init__(wires=[], do_queue=True)
+        super().__init__(wires=AllWires, do_queue=True)
 
     def label(self, decimals=None, base_label=None, cache=None):
         return "|S|"
