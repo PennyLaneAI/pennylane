0368195b095b2a2cf15dba3866b8b1353b0eae44
Jay Soni <jbsoni@uwaterloo.ca>
2022-03-15 17:37:58 -0400

    typo in teests

--- tests/test_device.py
+++ tests/test_device.py
@@ -604,7 +604,7 @@ class TestInternalFunctions:
     ):
         dev = mock_device_arbitrary_wires(wires=wires)
         with pytest.raises(
-            KeyError,
+            ValueError,
             match=f"Could not find some or all subset wires {subset} in device wires {wires}",
         ):
             _ = dev.get_ordered_subset(subset_wires=subset)
