16fb3a42a7d59a48f7ea985f6bf82586be4a2a21
Samuel Banning <samcbanning@gmail.com>
2022-03-21 12:22:53 -0400

    progress

--- pennylane/script/tape.py
+++ pennylane/script/tape.py
@@ -6,17 +6,19 @@ class FunctionTape(QuantumTape):
     def __init__(self, param_names, *args, **kwargs):
         self.param_names = param_names
         self.values = None
-        self.vars = {param_names[i]:(lambda: self.values[i]) for i in range(len(param_names))}
+        self.vars = {param_names[i]:(lambda: self.values[i]()) for i in range(len(param_names))}
         super().__init__(*args, **kwargs)
 
     def __call__(self, *args):
         self.values = args
+
 
 class IfTape(QuantumTape):
 
     def __init__(self, expr, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.expr = expr
+
 
 class WhileTape(QuantumTape):
     def __init__(self, expr, *args, **kwargs):
