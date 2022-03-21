79015a47e6682e4de3257eaccd43ef5f1bb3a1e8
Samuel Banning <samcbanning@gmail.com>
2022-03-21 12:32:19 -0400

    progres

--- pennylane/script/transform.py
+++ pennylane/script/transform.py
@@ -3,23 +3,27 @@ import pennylane as qml
 from pennylane.tape.tape import QuantumTape
 from pennylane.script.tape import IfTape, WhileTape, FunctionTape
 
-def remove_control_flow_transform():
-    pass
 
-def remove_control_flow(tape: QuantumTape):
+@qml.qfunc_transform
+def remove_control_flow_transform(tape: QuantumTape):
+    with QuantumTape() as new_tape:
+        _remove_control_flow(tape)
+    return new_tape
+
+def _remove_control_flow(tape: QuantumTape):
         for op in tape.queue:
             if isinstance(op, IfTape):
                 if op.expr():
                     for if_op in op.queue:
                         if isinstance(if_op, QuantumTape):
-                            remove_control_flow(if_op)
+                            _remove_control_flow(if_op)
                         else:
                             qml.apply(if_op)
             elif isinstance(op, WhileTape):
                 while op.expr():
                     for while_op in op.queue:
                         if isinstance(while_op, QuantumTape):
-                            remove_control_flow(while_op)
+                            _remove_control_flow(while_op)
                         else:
                             qml.apply(while_op)
             elif isinstance(op, FunctionTape):
