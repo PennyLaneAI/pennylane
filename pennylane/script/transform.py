1c5dbf88411e9c54eb4153621b35aa9376d3390e
Samuel Banning <samcbanning@gmail.com>
2022-03-21 12:36:21 -0400

    progress

--- pennylane/script/transform.py
+++ pennylane/script/transform.py
@@ -11,21 +11,26 @@ def remove_control_flow_transform(tape: QuantumTape):
     return new_tape
 
 def _remove_control_flow(tape: QuantumTape):
-        for op in tape.queue:
-            if isinstance(op, IfTape):
-                if op.expr():
-                    for if_op in op.queue:
-                        if isinstance(if_op, QuantumTape):
-                            _remove_control_flow(if_op)
-                        else:
-                            qml.apply(if_op)
-            elif isinstance(op, WhileTape):
-                while op.expr():
-                    for while_op in op.queue:
-                        if isinstance(while_op, QuantumTape):
-                            _remove_control_flow(while_op)
-                        else:
-                            qml.apply(while_op)
-            elif isinstance(op, FunctionTape):
-
-            qml.apply(op)
+        if isinstance(tape, IfTape):
+            if tape.expr():
+                for if_op in tape.queue:
+                    if isinstance(if_op, QuantumTape):
+                        _remove_control_flow(if_op)
+                    else:
+                        qml.apply(if_op)
+        elif isinstance(tape, WhileTape):
+            while tape.expr():
+                for while_op in tape.queue:
+                    if isinstance(while_op, QuantumTape):
+                        _remove_control_flow(while_op)
+                    else:
+                        qml.apply(while_op)
+        elif isinstance(tape, FunctionTape):
+            for fun_op in tape.queue:
+                if isinstance(fun_op, QuantumTape):
+                    _remove_control_flow(fun_op)
+                else:
+                    qml.apply(fun_op)
+        else:
+            for op in tape.queue:
+                qml.apply(op)
