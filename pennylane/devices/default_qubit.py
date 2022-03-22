30d2ba4649eea57c5b98de766ac7224aa709a274
albi3ro <chrissie.c.l@gmail.com>
2022-03-22 16:19:19 -0400

    new stopping condition

--- pennylane/devices/default_qubit.py
+++ pennylane/devices/default_qubit.py
@@ -198,6 +198,20 @@ class DefaultQubit(QubitDevice):
         wire_map = zip(wires, consecutive_wires)
         return dict(wire_map)
 
+    @property
+    def stopping_condition(self):
+        def f(obj):
+            if isinstance(obj, qml.tape.QuantumTape):
+                return False
+            if isinstance(obj, qml.operation.Operation):
+                return obj.has_matrix
+            if isinstance(obj, qml.measurements.MeasurementProcess):
+                if obj.obs is not None:
+                    return self.supports_observable(obj.obs)
+                return True
+            return False
+        return qml.BooleanFn(f)
+
     # pylint: disable=arguments-differ
     def apply(self, operations, rotations=None, **kwargs):
         rotations = rotations or []
