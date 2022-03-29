62afe04177fd70544ffd6d866300c8b7e6a2d214
anthayes92 <34694788+anthayes92@users.noreply.github.com>
2022-03-29 09:12:16 -0400

    Qcut sampling postprocessing function (#2358)
    * extend graph_to_tape for sampling, add unit test
    
    * add PR to changelog
    
    * add fragment sample circuit unit test
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * add sampling tensor obs error, add test
    
    * add test for sample obs
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * extend graph to tape logic to sample measurements, add unit test
    
    * remove unused logic, add mid circuit meas test
    
    * update changelog
    
    * fix variables
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * add suggestions from code review
    
    * add upsupported meas exception, add unit test
    
    * fix codefactor issue
    
    * add expand_fragment_tapes_mc function and unit test
    
    * update docs
    
    * update changelog
    
    * update unit test
    
    * add unit tests for MC measurements and state preps
    
    * update expansion logic, add unit test
    
    * fix wire bug
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * add suggestions from code review
    
    * begin mc postprocessing function, add unit test
    
    * attempt MC postprocessing
    
    * update sample postprocessing, add mc, add unit test
    
    * add mc unit test, update docs and changlog
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * add suggestions from code review
    
    * add remaining suggestions
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * add code review suggestions
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * add code review suggestions
    
    * Update pennylane/transforms/qcut.py
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * format
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>

--- pennylane/transforms/__init__.py
+++ pennylane/transforms/__init__.py
@@ -124,6 +124,8 @@ There are also low-level functions that can be used to build up the circuit cutt
     ~transforms.qcut.qcut_processing_fn
     ~transforms.qcut.CutStrategy
     ~transforms.qcut.expand_fragment_tapes_mc
+    ~transforms.qcut.qcut_processing_fn_sample
+    ~transforms.qcut.qcut_processing_fn_mc
     ~transforms.qcut.kahypar_cut
     ~transforms.qcut.place_wire_cuts
     ~transforms.qcut.find_and_place_cuts
