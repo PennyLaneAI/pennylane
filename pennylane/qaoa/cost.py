e9d39c3d45a85831ad669e3fc841d225e5522046
David Wierichs <davidwierichs@gmail.com>
2022-04-01 21:41:44 +0200

    Apply suggestions from code review
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- pennylane/qaoa/cost.py
+++ pennylane/qaoa/cost.py
@@ -341,7 +341,7 @@ def max_independent_set(graph: Union[nx.Graph, rx.PyGraph], constrained: bool = 
 
         .. note::
 
-            This method of constrained QAOA was introduced by 
+            This method of constrained QAOA was introduced by
             `Hadfield, Wang, Gorman, Rieffel, Venturelli, and Biswas (2019) <https://doi.org/10.3390/a12020034>`__.
 
         The Maximum Independent Set cost Hamiltonian for constrained QAOA is defined as:
