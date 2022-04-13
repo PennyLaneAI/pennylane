5ab590e28d1273aea9540d4a87b9fd5b9b8ac706
Diego <diego_guala@hotmail.com>
2022-04-13 17:30:33 -0400

    black -l 100

--- pennylane/templates/tensornetworks/mera.py
+++ pennylane/templates/tensornetworks/mera.py
@@ -53,18 +53,18 @@ def compute_indices(wires, n_block_wires):
             f"The number of wires should be n_block_wires times 2^n; got n_wires/n_block_wires = {n_wires/n_block_wires}"
         )
 
-    #number of layers in MERA
+    # number of layers in MERA
     n_layers = np.floor(np.log2(n_wires / n_block_wires)).astype(int) * 2 + 1
     wires_list = []
     wires_list.append(list(wires[0:n_block_wires]))
     highest_index = n_block_wires
-    
-    #compute block indices for all layers
+
+    # compute block indices for all layers
     for i in range(n_layers - 1):
-        #number of blocks in previous layer
+        # number of blocks in previous layer
         n_elements_pre = 2 ** ((i + 1) // 2)
         if i % 2 == 0:
-            #layer with new wires
+            # layer with new wires
             new_list = []
             list_len = len(wires_list)
             for j in range(list_len - n_elements_pre, list_len):
@@ -80,7 +80,7 @@ def compute_indices(wires, n_block_wires):
                 new_list.append(new_wires + wires_list[j][n_block_wires // 2 : :])
             wires_list = wires_list + new_list
         else:
-            #layer only using previous wires
+            # layer only using previous wires
             list_len = len(wires_list)
             new_list = []
             for j in range(list_len - n_elements_pre, list_len - 1):
