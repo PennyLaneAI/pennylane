273bb192ecdc0002b13ad699257ef77196af95ab
Romain Moyard <rmoyard@gmail.com>
2022-04-14 20:33:22 +0200

    Update pennylane/templates/subroutines/hilbert_schmidt.py
    Co-authored-by: antalszava <antalszava@gmail.com>

--- pennylane/templates/subroutines/hilbert_schmidt.py
+++ pennylane/templates/subroutines/hilbert_schmidt.py
@@ -63,7 +63,7 @@ class HilbertSchmidt(Operation):
 
     .. UsageDetails::
 
-        Consider that we want to evaluate the Hilbert Schmidt Test cost between the unitary ``U`` and an approximate
+        Consider that we want to evaluate the Hilbert-Schmidt Test cost between the unitary ``U`` and an approximate
         unitary ``V``. We need to define some functions where it is possible to use the :class:`~.HilbertSchmidt`
         template. Here the considered unitary is ``Hadamard`` and we try to compute the cost for the approximate
         unitary ``RZ``. For an angle that is equal to ``0`` (``Identity``), we have the maximal cost which is ``1``.
