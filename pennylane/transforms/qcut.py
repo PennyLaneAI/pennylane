fad14e7c6f67c70ea33ad0c67503ed89ec2b981e
Romain Moyard <rmoyard@gmail.com>
2022-04-21 10:42:58 -0400

    Update

--- pennylane/transforms/qcut.py
+++ pennylane/transforms/qcut.py
@@ -734,7 +734,7 @@ def expand_fragment_tapes_mc(
 
     Returns:
         Tuple[List[QuantumTape], np.ndarray]: the tapes corresponding to each configuration and the
-            settings that track each configuration pair
+        settings that track each configuration pair
 
     **Example**
 
@@ -763,7 +763,7 @@ def expand_fragment_tapes_mc(
 
         >>> configs, settings = qml.transforms.qcut.expand_fragment_tapes_mc(tapes, communication_graph, 3)
         >>> print(settings)
-        [[1 6 2]]
+        [[3 6 2]]
         >>> for i, (c1, c2) in enumerate(zip(configs[0], configs[1])):
         ...     print(f"config {i}:")
         ...     print(c1.draw())
