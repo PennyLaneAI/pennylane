88776d4fc2bb5e3ac0e95fc946a4b33bd0b13468
Samuel Banning <samcbanning@gmail.com>
2022-04-22 17:17:28 -0400

    added testcase for custom device

---
+++ tests/gradients/test_custom_jacobian.py
@@ -0,0 +1,35 @@
+import pennylane.numpy as np
+import pennylane as qml
+
+from pennylane.devices.default_qubit import DefaultQubit
+
+class CustomJacobianDevice(DefaultQubit):
+
+    @classmethod
+    def capabilities(cls):
+        capabilities = super().capabilities()
+        capabilities["provides_jacobian"] = True
+        return capabilities
+
+    def jacobian(self, tape):
+        return np.array([1., 2., 3., 4.])
+
+
+class TestCustomJacobian:
+
+    def test_custom_jacobians(self):
+        dev = CustomJacobianDevice(wires=2)
+
+        @qml.qnode(dev, diff_method="device")
+        def circuit(v):
+            qml.RX(v, wires=0)
+            return qml.probs(wires=[0, 1])
+
+        d_circuit = qml.jacobian(circuit, argnum=0)
+
+        params = np.array(1., requires_grad=True)
+
+        d_out = d_circuit(params)
+        assert np.allclose(d_out, np.array([1., 2., 3., 4.]))
+
+
