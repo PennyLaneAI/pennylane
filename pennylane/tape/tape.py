b4d0cac2c4b4d690aafd6f2e9c57eecce12a214b
albi3ro <chrissie.c.l@gmail.com>
2022-04-27 09:57:18 -0400

    oops

--- pennylane/tape/tape.py
+++ pennylane/tape/tape.py
@@ -459,7 +459,7 @@ class QuantumTape(AnnotatedQueue):
                 elif list_order[obj._queue_category] < list_order[current_list]:
                     raise ValueError(
                         f"{obj._queue_category[1:]} operation {obj} must occur prior "
-                        "to {current_list[1:]}. Please place earlier in the queue."
+                        f"to {current_list[1:]}. Please place earlier in the queue."
                     )
                 getattr(self, obj._queue_category).append(obj)
 
