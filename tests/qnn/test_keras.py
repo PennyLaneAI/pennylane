3c3c5753e2af1d2cc7c3f048bd4365a6774dcaad
Romain Moyard <rmoyard@gmail.com>
2022-05-02 14:10:07 -0400

    update

--- tests/qnn/test_keras.py
+++ tests/qnn/test_keras.py
@@ -499,11 +499,11 @@ class TestKerasLayer:
         assert output_shape.as_list() == [None, 1]
 
 
+@pytest.mark.math
 @pytest.mark.parametrize("interface", ["autograd", "torch", "tf"])
 @pytest.mark.parametrize("n_qubits, output_dim", indices_up_to(1))
 @pytest.mark.usefixtures("get_circuit")
-@pytest.mark.usefixtures("skip_if_no_torch_support")
-def test_interface_conversion(get_circuit, output_dim, skip_if_no_torch_support):
+def test_interface_conversion(get_circuit, output_dim):
     """Test if input QNodes with all types of interface are converted internally to the TensorFlow
     interface"""
     c, w = get_circuit
@@ -511,6 +511,7 @@ def test_interface_conversion(get_circuit, output_dim, skip_if_no_torch_support)
     assert layer.qnode.interface == "tf"
 
 
+@pytest.mark.tf
 @pytest.mark.parametrize("interface", ["tf"])
 @pytest.mark.usefixtures("get_circuit", "model")
 class TestKerasLayerIntegration:
@@ -561,6 +562,7 @@ class TestKerasLayerIntegration:
             assert np.allclose(w, weights_loaded[i])
 
 
+@pytest.mark.tf
 @pytest.mark.parametrize("interface", ["tf"])
 @pytest.mark.usefixtures("get_circuit_dm", "model_dm")
 class TestKerasLayerIntegrationDM:
@@ -612,12 +614,14 @@ class TestKerasLayerIntegrationDM:
             assert np.allclose(w, weights_loaded[i])
 
 
+@pytest.mark.tf
 def test_no_attribute():
     """Test that the qnn module raises an AttributeError if accessing an unavailable attribute"""
     with pytest.raises(AttributeError, match="module 'pennylane.qnn' has no attribute 'random'"):
         qml.qnn.random
 
 
+@pytest.mark.tf
 def test_batch_input():
     """Test input batching in keras"""
     dev = qml.device("default.qubit.tf", wires=4)
