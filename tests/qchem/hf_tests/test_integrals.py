6c3610d8f6a1a4bce6961493b57c012e49d6c88b
soranjh <soranjh@yahoo.com>
2022-05-06 10:56:47 -0400

    create classes in integrals

--- tests/qchem/hf_tests/test_integrals.py
+++ tests/qchem/hf_tests/test_integrals.py
@@ -20,58 +20,64 @@ from pennylane import numpy as np
 from pennylane import qchem
 
 
-@pytest.mark.parametrize(
-    ("l", "alpha", "n"),
-    [
-        # normalization constant for an s orbital is :math:`(\frac {2 \alpha}{\pi})^{{3/4}}`.
-        ((0, 0, 0), np.array([3.425250914]), np.array([1.79444183])),
-    ],
-)
-def test_gaussian_norm(l, alpha, n):
-    r"""Test that the computed normalization constant of a Gaussian function is correct."""
-    assert np.allclose(qchem.primitive_norm(l, alpha), n)
+class TestNorm:
+    """Tests for normalization functions"""
+
+    @pytest.mark.parametrize(
+        ("l", "alpha", "n"),
+        [
+            # normalization constant for an s orbital is :math:`(\frac {2 \alpha}{\pi})^{{3/4}}`.
+            ((0, 0, 0), np.array([3.425250914]), np.array([1.79444183])),
+        ],
+    )
+    def test_gaussian_norm(self, l, alpha, n):
+        r"""Test that the computed normalization constant of a Gaussian function is correct."""
+        assert np.allclose(qchem.primitive_norm(l, alpha), n)
+
+    @pytest.mark.parametrize(
+        ("l", "alpha", "a", "n"),
+        [
+            # normalization constant for a contracted Gaussian function composed of three normalized
+            # s orbital is :math:`1/3`.
+            (
+                (0, 0, 0),
+                np.array([3.425250914, 3.425250914, 3.425250914]),
+                np.array([1.79444183, 1.79444183, 1.79444183]),
+                np.array([0.33333333]),
+            )
+        ],
+    )
+    def test_contraction_norm(self, l, alpha, a, n):
+        r"""Test that the computed normalization constant of a contracted Gaussian function is
+        correct."""
+        assert np.allclose(qchem.contracted_norm(l, alpha, a), n)
 
 
-@pytest.mark.parametrize(
-    ("l", "alpha", "a", "n"),
-    [
-        # normalization constant for a contracted Gaussian function composed of three normalized
-        # s orbital is :math:`1/3`.
-        (
-            (0, 0, 0),
-            np.array([3.425250914, 3.425250914, 3.425250914]),
-            np.array([1.79444183, 1.79444183, 1.79444183]),
-            np.array([0.33333333]),
-        )
-    ],
-)
-def test_contraction_norm(l, alpha, a, n):
-    r"""Test that the computed normalization constant of a contracted Gaussian function is correct."""
-    assert np.allclose(qchem.contracted_norm(l, alpha, a), n)
+class TestParams:
+    """Tests for parameter generation functions"""
 
+    @pytest.mark.parametrize(
+        ("alpha", "coeff", "r"),
+        [
+            (
+                np.array([3.42525091, 0.62391373, 0.1688554], requires_grad=True),
+                np.array([0.15432897, 0.53532814, 0.44463454], requires_grad=True),
+                np.array([0.0, 0.0, 0.0], requires_grad=False),
+            ),
+            (
+                np.array([3.42525091, 0.62391373, 0.1688554], requires_grad=False),
+                np.array([0.15432897, 0.53532814, 0.44463454], requires_grad=False),
+                np.array([0.0, 0.0, 0.0], requires_grad=True),
+            ),
+        ],
+    )
+    def test_generate_params(self, alpha, coeff, r):
+        r"""Test that test_generate_params returns correct basis set parameters."""
+        params = [alpha, coeff, r]
+        args = [p for p in [alpha, coeff, r] if p.requires_grad]
+        basis_params = qchem.integrals._generate_params(params, args)
 
-@pytest.mark.parametrize(
-    ("alpha", "coeff", "r"),
-    [
-        (
-            np.array([3.42525091, 0.62391373, 0.1688554], requires_grad=True),
-            np.array([0.15432897, 0.53532814, 0.44463454], requires_grad=True),
-            np.array([0.0, 0.0, 0.0], requires_grad=False),
-        ),
-        (
-            np.array([3.42525091, 0.62391373, 0.1688554], requires_grad=False),
-            np.array([0.15432897, 0.53532814, 0.44463454], requires_grad=False),
-            np.array([0.0, 0.0, 0.0], requires_grad=True),
-        ),
-    ],
-)
-def test_generate_params(alpha, coeff, r):
-    r"""Test that test_generate_params returns correct basis set parameters."""
-    params = [alpha, coeff, r]
-    args = [p for p in [alpha, coeff, r] if p.requires_grad]
-    basis_params = qchem.integrals._generate_params(params, args)
-
-    assert np.allclose(basis_params, (alpha, coeff, r))
+        assert np.allclose(basis_params, (alpha, coeff, r))
 
 
 @pytest.mark.parametrize(
@@ -116,155 +122,157 @@ def test_expansion(la, lb, ra, rb, alpha, beta, t, c):
     assert np.allclose(qchem.expansion(0, 1, ra, rb, alpha, beta, 2), np.array([0.0]))
 
 
-@pytest.mark.parametrize(
-    ("la", "lb", "ra", "rb", "alpha", "beta", "o"),
-    [
-        # two normalized s orbitals
-        (
-            (0, 0, 0),
-            (0, 0, 0),
-            np.array([0.0, 0.0, 0.0]),
-            np.array([0.0, 0.0, 0.0]),
-            np.array([np.pi / 2]),
-            np.array([np.pi / 2]),
-            np.array([1.0]),
-        ),
-        (
-            (0, 0, 0),
-            (0, 0, 0),
-            np.array([0.0, 0.0, 0.0]),
-            np.array([20.0, 0.0, 0.0]),
-            np.array([3.42525091]),
-            np.array([3.42525091]),
-            np.array([0.0]),
-        ),
-        (
-            (1, 0, 0),
-            (0, 0, 1),
-            np.array([0.0, 0.0, 0.0]),
-            np.array([0.0, 0.0, 0.0]),
-            np.array([6.46480325]),
-            np.array([6.46480325]),
-            np.array([0.0]),
-        ),
-    ],
-)
-def test_gaussian_overlap(la, lb, ra, rb, alpha, beta, o):
-    r"""Test that gaussian overlap function returns a correct value."""
-    assert np.allclose(qchem.gaussian_overlap(la, lb, ra, rb, alpha, beta), o)
+class TestOverlap:
+    """Tests for overlap integrals"""
 
-
-@pytest.mark.parametrize(
-    ("symbols", "geometry", "alpha", "coef", "r", "o_ref"),
-    [
-        (
-            ["H", "H"],
-            np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 20.0]], requires_grad=False),
-            np.array(
-                [[3.42525091, 0.62391373, 0.1688554], [3.42525091, 0.62391373, 0.1688554]],
-                requires_grad=False,
+    @pytest.mark.parametrize(
+        ("la", "lb", "ra", "rb", "alpha", "beta", "o"),
+        [
+            # two normalized s orbitals
+            (
+                (0, 0, 0),
+                (0, 0, 0),
+                np.array([0.0, 0.0, 0.0]),
+                np.array([0.0, 0.0, 0.0]),
+                np.array([np.pi / 2]),
+                np.array([np.pi / 2]),
+                np.array([1.0]),
             ),
-            np.array(
-                [[0.15432897, 0.53532814, 0.44463454], [0.15432897, 0.53532814, 0.44463454]],
-                requires_grad=True,
+            (
+                (0, 0, 0),
+                (0, 0, 0),
+                np.array([0.0, 0.0, 0.0]),
+                np.array([20.0, 0.0, 0.0]),
+                np.array([3.42525091]),
+                np.array([3.42525091]),
+                np.array([0.0]),
             ),
-            np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 20.0]], requires_grad=True),
-            np.array([0.0]),
-        ),
-        (
-            ["H", "H"],
-            np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], requires_grad=False),
-            np.array(
-                [[3.42525091, 0.62391373, 0.1688554], [3.42525091, 0.62391373, 0.1688554]],
-                requires_grad=False,
+            (
+                (1, 0, 0),
+                (0, 0, 1),
+                np.array([0.0, 0.0, 0.0]),
+                np.array([0.0, 0.0, 0.0]),
+                np.array([6.46480325]),
+                np.array([6.46480325]),
+                np.array([0.0]),
             ),
-            np.array(
-                [[0.15432897, 0.53532814, 0.44463454], [0.15432897, 0.53532814, 0.44463454]],
-                requires_grad=False,
+        ],
+    )
+    def test_gaussian_overlap(self, la, lb, ra, rb, alpha, beta, o):
+        r"""Test that gaussian overlap function returns a correct value."""
+        assert np.allclose(qchem.gaussian_overlap(la, lb, ra, rb, alpha, beta), o)
+
+    @pytest.mark.parametrize(
+        ("symbols", "geometry", "alpha", "coef", "r", "o_ref"),
+        [
+            (
+                ["H", "H"],
+                np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 20.0]], requires_grad=False),
+                np.array(
+                    [[3.42525091, 0.62391373, 0.1688554], [3.42525091, 0.62391373, 0.1688554]],
+                    requires_grad=False,
+                ),
+                np.array(
+                    [[0.15432897, 0.53532814, 0.44463454], [0.15432897, 0.53532814, 0.44463454]],
+                    requires_grad=True,
+                ),
+                np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 20.0]], requires_grad=True),
+                np.array([0.0]),
             ),
-            np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], requires_grad=True),
-            np.array([1.0]),
-        ),
-        (
-            ["H", "H"],
-            np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], requires_grad=True),
-            np.array(
-                [[3.42525091, 0.62391373, 0.1688554], [3.42525091, 0.62391373, 0.1688554]],
-                requires_grad=True,
+            (
+                ["H", "H"],
+                np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], requires_grad=False),
+                np.array(
+                    [[3.42525091, 0.62391373, 0.1688554], [3.42525091, 0.62391373, 0.1688554]],
+                    requires_grad=False,
+                ),
+                np.array(
+                    [[0.15432897, 0.53532814, 0.44463454], [0.15432897, 0.53532814, 0.44463454]],
+                    requires_grad=False,
+                ),
+                np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], requires_grad=True),
+                np.array([1.0]),
             ),
-            np.array(
-                [[0.15432897, 0.53532814, 0.44463454], [0.15432897, 0.53532814, 0.44463454]],
-                requires_grad=True,
+            (
+                ["H", "H"],
+                np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], requires_grad=True),
+                np.array(
+                    [[3.42525091, 0.62391373, 0.1688554], [3.42525091, 0.62391373, 0.1688554]],
+                    requires_grad=True,
+                ),
+                np.array(
+                    [[0.15432897, 0.53532814, 0.44463454], [0.15432897, 0.53532814, 0.44463454]],
+                    requires_grad=True,
+                ),
+                np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], requires_grad=True),
+                np.array([1.0]),
             ),
-            np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], requires_grad=True),
-            np.array([1.0]),
-        ),
-    ],
-)
-def test_overlap_integral(symbols, geometry, alpha, coef, r, o_ref):
-    r"""Test that overlap_integral function returns a correct value for the overlap integral."""
-    mol = qchem.Molecule(symbols, geometry)
-    basis_a = mol.basis_set[0]
-    basis_b = mol.basis_set[1]
-    args = [p for p in [alpha, coef, r] if p.requires_grad]
+        ],
+    )
+    def test_overlap_integral(self, symbols, geometry, alpha, coef, r, o_ref):
+        r"""Test that overlap_integral function returns a correct value for the overlap integral."""
+        mol = qchem.Molecule(symbols, geometry)
+        basis_a = mol.basis_set[0]
+        basis_b = mol.basis_set[1]
+        args = [p for p in [alpha, coef, r] if p.requires_grad]
 
-    o = qchem.overlap_integral(basis_a, basis_b)(*args)
-    assert np.allclose(o, o_ref)
+        o = qchem.overlap_integral(basis_a, basis_b)(*args)
+        assert np.allclose(o, o_ref)
 
-
-@pytest.mark.parametrize(
-    ("symbols", "geometry", "alpha", "coeff"),
-    [
-        (
-            ["H", "H"],
-            np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]], requires_grad=False),
-            np.array(
-                [[3.42525091, 0.62391373, 0.1688554], [3.42525091, 0.62391373, 0.1688554]],
-                requires_grad=True,
+    @pytest.mark.parametrize(
+        ("symbols", "geometry", "alpha", "coeff"),
+        [
+            (
+                ["H", "H"],
+                np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]], requires_grad=False),
+                np.array(
+                    [[3.42525091, 0.62391373, 0.1688554], [3.42525091, 0.62391373, 0.1688554]],
+                    requires_grad=True,
+                ),
+                np.array(
+                    [[0.15432897, 0.53532814, 0.44463454], [0.15432897, 0.53532814, 0.44463454]],
+                    requires_grad=True,
+                ),
             ),
-            np.array(
-                [[0.15432897, 0.53532814, 0.44463454], [0.15432897, 0.53532814, 0.44463454]],
-                requires_grad=True,
-            ),
-        ),
-    ],
-)
-def test_gradient_overlap(symbols, geometry, alpha, coeff):
-    r"""Test that the overlap gradient computed with respect to the basis parameters is correct."""
-    mol = qchem.Molecule(symbols, geometry, alpha=alpha, coeff=coeff)
-    basis_a = mol.basis_set[0]
-    basis_b = mol.basis_set[1]
-    args = [mol.alpha, mol.coeff]
+        ],
+    )
+    def test_gradient_overlap(self, symbols, geometry, alpha, coeff):
+        r"""Test that the overlap gradient computed with respect to the basis parameters is
+        correct."""
+        mol = qchem.Molecule(symbols, geometry, alpha=alpha, coeff=coeff)
+        basis_a = mol.basis_set[0]
+        basis_b = mol.basis_set[1]
+        args = [mol.alpha, mol.coeff]
 
-    g_alpha = autograd.grad(qchem.overlap_integral(basis_a, basis_b), argnum=0)(*args)
-    g_coeff = autograd.grad(qchem.overlap_integral(basis_a, basis_b), argnum=1)(*args)
+        g_alpha = autograd.grad(qchem.overlap_integral(basis_a, basis_b), argnum=0)(*args)
+        g_coeff = autograd.grad(qchem.overlap_integral(basis_a, basis_b), argnum=1)(*args)
 
-    # compute overlap gradients with respect to alpha and coeff using finite diff
-    delta = 0.0001
-    g_ref_alpha = np.zeros(6).reshape(alpha.shape)
-    g_ref_coeff = np.zeros(6).reshape(coeff.shape)
+        # compute overlap gradients with respect to alpha and coeff using finite diff
+        delta = 0.0001
+        g_ref_alpha = np.zeros(6).reshape(alpha.shape)
+        g_ref_coeff = np.zeros(6).reshape(coeff.shape)
 
-    for i in range(len(alpha)):
-        for j in range(len(alpha[0])):
+        for i in range(len(alpha)):
+            for j in range(len(alpha[0])):
 
-            alpha_minus = alpha.copy()
-            alpha_plus = alpha.copy()
-            alpha_minus[i][j] = alpha_minus[i][j] - delta
-            alpha_plus[i][j] = alpha_plus[i][j] + delta
-            o_minus = qchem.overlap_integral(basis_a, basis_b)(*[alpha_minus, coeff])
-            o_plus = qchem.overlap_integral(basis_a, basis_b)(*[alpha_plus, coeff])
-            g_ref_alpha[i][j] = (o_plus - o_minus) / (2 * delta)
+                alpha_minus = alpha.copy()
+                alpha_plus = alpha.copy()
+                alpha_minus[i][j] = alpha_minus[i][j] - delta
+                alpha_plus[i][j] = alpha_plus[i][j] + delta
+                o_minus = qchem.overlap_integral(basis_a, basis_b)(*[alpha_minus, coeff])
+                o_plus = qchem.overlap_integral(basis_a, basis_b)(*[alpha_plus, coeff])
+                g_ref_alpha[i][j] = (o_plus - o_minus) / (2 * delta)
 
-            coeff_minus = coeff.copy()
-            coeff_plus = coeff.copy()
-            coeff_minus[i][j] = coeff_minus[i][j] - delta
-            coeff_plus[i][j] = coeff_plus[i][j] + delta
-            o_minus = qchem.overlap_integral(basis_a, basis_b)(*[alpha, coeff_minus])
-            o_plus = qchem.overlap_integral(basis_a, basis_b)(*[alpha, coeff_plus])
-            g_ref_coeff[i][j] = (o_plus - o_minus) / (2 * delta)
+                coeff_minus = coeff.copy()
+                coeff_plus = coeff.copy()
+                coeff_minus[i][j] = coeff_minus[i][j] - delta
+                coeff_plus[i][j] = coeff_plus[i][j] + delta
+                o_minus = qchem.overlap_integral(basis_a, basis_b)(*[alpha, coeff_minus])
+                o_plus = qchem.overlap_integral(basis_a, basis_b)(*[alpha, coeff_plus])
+                g_ref_coeff[i][j] = (o_plus - o_minus) / (2 * delta)
 
-    assert np.allclose(g_alpha, g_ref_alpha)
-    assert np.allclose(g_coeff, g_ref_coeff)
+        assert np.allclose(g_alpha, g_ref_alpha)
+        assert np.allclose(g_coeff, g_ref_coeff)
 
 
 @pytest.mark.parametrize(
