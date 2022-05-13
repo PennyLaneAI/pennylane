986f5529abc8e44e383f43b2c841565718c33261
Korbinian Kottmann <43949391+Qottmann@users.noreply.github.com>
2022-05-13 11:25:07 -0400

    Update tests/optimize/test_optimize_shot_adapative.py
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- tests/optimize/test_optimize_shot_adapative.py
+++ tests/optimize/test_optimize_shot_adapative.py
@@ -22,7 +22,8 @@ from pennylane import numpy as np
 def catch_warn_ExpvalCost(ansatz, hamiltonian, device, **kwargs):
     """Computes the ExpvalCost and catches the initial deprecation warning."""
 
-    with pytest.warns(UserWarning, match="will be deprecated,"):
+    with pytest.warns(UserWarning, match="is deprecated,"):
+
         res = qml.ExpvalCost(ansatz, hamiltonian, device, **kwargs)
     return res
 
