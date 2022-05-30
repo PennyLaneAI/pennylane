9d0e5265fa637707fe5e5f0fa46771b74d4ec305
Qottmann <korbinian.kottmann@gmail.com>
2022-05-30 15:11:00 -0400

    [ci skip] initial commit

---
+++ pennylane/quantum_info/classical_fisher.py
@@ -0,0 +1,29 @@
+# Copyright 2022 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Differentiable classical fisher information"""
+# pylint: disable=import-outside-toplevel
+
+import pennylane as qml
+
+from .batch_transform import batch_transform
+
+@batch_transform
+def classical_fisher(tape, wires):
+    """Transform computing a new qfunc that computes the classical fisher information matrix"""
+    with qml.tape.QuantumTape() as new_tape:
+        for op in tape.operations:
+            qml.apply(op)
+        qml.probs(wires=wires)
+    
+    d2p_didj = qml.jacobian(qml.jacobian(new_tape))
