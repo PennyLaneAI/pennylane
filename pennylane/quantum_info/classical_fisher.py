ee13a192df4e765c72ec9b91a89c32941b1d143e
Qottmann <korbinian.kottmann@gmail.com>
2022-06-01 14:01:01 -0400

    CFIM with Davids trick avoiding involving probabilities in the calculation of the matrix

--- pennylane/quantum_info/classical_fisher.py
+++ pennylane/quantum_info/classical_fisher.py
@@ -16,44 +16,76 @@
 import pennylane as qml
 import pennylane.numpy as pnp
 
+import jax
+import torch
+
+
+def torch_jac(circ):
+    def wrapper(params):
+        return torch.autograd.functional.jacobian(circ, (params))
+
+    return wrapper
+
 
 def CFIM(qnode):
     """Computing the classical fisher information matrix (CFIM) using the jacobian of the output probabilities
     as described in eq. (15) in https://arxiv.org/abs/2103.15191
     """
-    new_qnode = qml.transforms._make_probs(qnode)
-    jac = qml.jacobian(new_qnode)
+    new_qnode = qml.transforms._make_probs(
+        qnode, post_processing_fn=lambda x: qml.math.squeeze(qml.math.sqrt(qml.math.stack(x)))
+    )
+
+    interface = qnode.interface
+
+    if interface == "jax":
+        jac = jax.jacobian(new_qnode)
+    if interface == "torch":
+        jac = torch_jac(new_qnode)
+    if interface == "autograd":
+        jac = qml.jacobian(new_qnode)
 
     def wrapper(*args, **kwargs):
         j = jac(*args, **kwargs)
         p = qnode(*args, **kwargs)
-
-        if hasattr(j, "interface"):
-            interface = getattr(j, "interface")
-        else:
-            interface = "autograd"
 
         cfim = _compute_cfim(p, j, interface)
         return cfim
 
     return wrapper
 
+
 def CFIM_alt(qnode):
     """Computing the classical fisher information matrix (CFIM) by computing the hessian of log(p)
     as described in eq. (14) in https://arxiv.org/abs/2103.15191
     """
-    new_qnode = qml.transforms._make_probs(qnode, post_processing_fn=lambda x: qml.math.squeeze(qml.math.log(qml.math.stack(x))))
-    hessian = qml.jacobian(qml.jacobian(new_qnode)) # this is very slow, can be sped up with other interfaces
+    new_qnode = qml.transforms._make_probs(
+        qnode, post_processing_fn=lambda x: qml.math.squeeze(qml.math.log(qml.math.stack(x)))
+    )
+    hessian = qml.jacobian(
+        qml.jacobian(new_qnode)
+    )  # this is very slow, can be sped up with other interfaces
 
     def wrapper(*args, **kwargs):
-        h = hessian(*args, **kwargs)                                # (2**n_wires, num_params, num_params)
-        p = qnode(*args, **kwargs)[:, pnp.newaxis, pnp.newaxis]     # (2**n_wires, 1, 1)
-        return -qml.math.sum(h*p, axis=0)                           # TODO: dont understand why I need the minus sign, most likely some autograd peculiarity in the hessian
+        h = hessian(*args, **kwargs)  # (2**n_wires, num_params, num_params)
+        p = qnode(*args, **kwargs)[:, pnp.newaxis, pnp.newaxis]  # (2**n_wires, 1, 1)
+        return -qml.math.sum(
+            h * p, axis=0
+        )  # TODO: dont understand why I need the minus sign, most likely some autograd peculiarity in the hessian
 
     return wrapper
 
 
-def _compute_cfim(p, dp, interface):
+def _compute_cfim(p, d_sqrt_p, interface=None):
+    """Computes :math:`CFIM_{ij} = \sum_\ell (\partial_i \sqrt{p_\ell}) (\partial_i \sqrt{p_\ell})`"""
+    if any(qml.math.isclose(p, 0)):
+        mask = qml.math.where(qml.math.isclose(p, 0))
+        n_zeros = len(mask[0])
+        n_params = d_sqrt_p.shape[-1]
+        d_sqrt_p[mask] = qml.math.zeros((n_zeros, n_params))
+    return qml.math.tensordot(d_sqrt_p, d_sqrt_p, axes=[[0], [0]])
+
+
+def _compute_cfim_alt(p, dp, interface):
     """Computes the (num_params, num_params) classical fisher information matrix from the probabilities and its derivatives"""
     # Assumes
     # dp shape: (n_probs, n_params)
@@ -63,6 +95,15 @@ def _compute_cfim(p, dp, interface):
     # and then (dp/p) @ dp (n_params, n_params)
     if interface == "jax":
         dp_over_p = dp.T / p
+    if interface == "torch":
+        # TODO: Casting is currently necessary, otherwise dp_over_p @ dp yields mismatching scalar types
+        p = p.type(torch.float64)
+        dp = dp.type(torch.float64)
+
+        one_over_p = torch.full_like(p, fill_value=0.0)
+        mask = p != 0
+        one_over_p[mask] = 1 / p[mask]
+        dp_over_p = one_over_p * dp.T
     else:
         dp_over_p = qml.math.divide(
             dp.T, p, where=(p != 0), out=qml.math.zeros_like(dp.T)
--- tests/quantum_info/test_classical_fisher.py
+++ tests/quantum_info/test_classical_fisher.py
@@ -21,38 +21,65 @@ import pennylane as qml
 import numpy as np
 import pennylane.numpy as pnp
 
-from pennylane.quantum_info import _compute_cfim
+from pennylane.quantum_info import _compute_cfim, CFIM
 
 
-@pytest.mark.parametrize("n_params", np.arange(1, 10))
-@pytest.mark.parametrize("n_wires", np.arange(1, 5))
-def test_construction_of_compute_cfim(n_params, n_wires):
-    """Ensuring the construction in _compute_cfim is correct"""
-    dp = np.arange(2**n_wires * n_params, dtype=float).reshape(2**n_wires, n_params)
-    p = np.ones(2**n_wires)
+class TestComputeCFIMfn:
+    """Testing that given p and dp, _compute_cfim() computes the correct outputs"""
 
-    res = _compute_cfim(p, dp, None)
+    @pytest.mark.parametrize("n_params", np.arange(1, 10))
+    @pytest.mark.parametrize("n_wires", np.arange(1, 5))
+    def test_construction_of_compute_cfim(self, n_params, n_wires):
+        """Ensuring the construction in _compute_cfim is correct"""
+        dp = np.arange(2**n_wires * n_params, dtype=float).reshape(2**n_wires, n_params)
+        p = np.ones(2**n_wires)
 
-    assert np.allclose(res, res.T)
-    assert all(
-        [
-            res[i, j] == np.sum(dp[:, i] * dp[:, j] / p)
-            for i in range(n_params)
-            for j in range(n_params)
-        ]
-    )
+        res = _compute_cfim(p, dp, None)
+
+        assert np.allclose(res, res.T)
+        assert all(
+            [
+                res[i, j] == np.sum(dp[:, i] * dp[:, j] / p)
+                for i in range(n_params)
+                for j in range(n_params)
+            ]
+        )
+
+    @pytest.mark.parametrize("n_params", np.arange(1, 10))
+    @pytest.mark.parametrize("n_wires", np.arange(1, 5))
+    def test_compute_cfim_trivial_distribution(self, n_params, n_wires):
+        """Test that the classical fisher information matrix (CFIM) is
+        constructed correctly for the trivial distirbution p=(1,0,0,...)
+        and some dummy gradient dp"""
+        # implicitly also does the unit test for the correct output shape
+
+        dp = np.ones(2**n_wires * n_params, dtype=float).reshape(2**n_wires, n_params)
+        p = np.zeros(2**n_wires)
+        p[0] = 1
+        res = _compute_cfim(p, dp, None)
+        assert np.allclose(res, np.ones((n_params, n_params)))
 
 
-@pytest.mark.parametrize("n_params", np.arange(1, 10))
-@pytest.mark.parametrize("n_wires", np.arange(1, 5))
-def test_compute_cfim_trivial_distribution(n_params, n_wires):
-    """Test that the classical fisher information matrix (CFIM) is
-    constructed correctly for the trivial distirbution p=(1,0,0,...)
-    and some dummy gradient dp"""
-    # implicitly also does the unit test for the correct output shape
+class TestIntegration:
+    """Integration tests for CFIM"""
 
-    dp = np.ones(2**n_wires * n_params, dtype=float).reshape(2**n_wires, n_params)
-    p = np.zeros(2**n_wires)
-    p[0] = 1
-    res = _compute_cfim(p, dp, None)
-    assert np.allclose(res, np.ones((n_params, n_params)))
+    @pytest.mark.autograd
+    @pytest.mark.parametrize("n_wires", np.arange(1, 5))
+    def test_cfim_autograd(self, n_wires):
+
+        n_params = 2
+        dev = qml.device("default.qubit", wires=n_wires)
+
+        @qml.qnode(dev, interface="autograd")
+        def circ(params):
+
+            for i in range(n_wires):
+                qml.RX(params[0], wires=i)
+            for i in range(n_wires):
+                qml.RY(params[1], wires=i)
+            return qml.probs(wires=range(n_wires))
+
+        params = np.pi / 4 * pnp.ones(2, requires_grad=True)
+
+        cfim = CFIM(circ)(params)
+        assert np.allclose(cfim, (n_wires / 12.0) * np.ones((n_params, n_params)))
