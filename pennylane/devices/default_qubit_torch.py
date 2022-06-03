01e0f476f3bba37d534520c183ff0ad2f9b1a1a6
Romain Moyard <rmoyard@gmail.com>
2022-06-03 13:44:45 -0400

    Test a

--- pennylane/devices/default_qubit_torch.py
+++ pennylane/devices/default_qubit_torch.py
@@ -241,7 +241,7 @@ class DefaultQubitTorch(DefaultQubit):
                 res = torch.from_numpy(res)
                 res = torch.cat([torch.reshape(i, (-1,)) for i in res], dim=0)
             elif len(a) == 1 and len(a[0].shape) > 1:
-                res = torch.cat(a, dim=0)
+                res = a[0]
             else:
                 res = torch.cat([torch.reshape(i, (-1,)) for i in a], dim=0)
                 res = torch.cat([torch.reshape(i, (-1,)) for i in res], dim=0)
