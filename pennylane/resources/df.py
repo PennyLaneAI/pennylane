d4ecc68405adf095371c902c0541fda1d72a586a
soranjh <soranjh@yahoo.com>
2022-06-03 12:24:53 -0400

    add test for rank

--- pennylane/resources/df.py
+++ pennylane/resources/df.py
@@ -18,43 +18,43 @@ This module contains the functions needed for resource estimation with double fa
 from pennylane import numpy as np
 
 
-def rank(factors, eigvals, tol = 1e-5):
+def rank(factors, eigvals, tol=1e-5):
     r"""Return the ranks of double-factorization for a two-electron integral tensor.
 
-    The double factorization of a two-electron integral tensor consists of two factorization steps.
-    First, the tensor is factorized such that
+        The double factorization of a two-electron integral tensor consists of two factorization steps.
+        First, the tensor is factorized such that
 
-    .. math::
-￼
-￼           h_{ijkl} = \sum_r^R L_{ij}^{(r)} L_{kl}^{(r) T},
+        .. math::
+    ￼
+    ￼           h_{ijkl} = \sum_r^R L_{ij}^{(r)} L_{kl}^{(r) T},
 
-    where :math:`R` is the rank of the first factorization step. Then, the matrices :math:`L^{(r)}`
-    are diagonalized to obtain a set of eigenvalues and eigenvectors for each matrix which can be
-    truncated with a rank :math:`M^{(r)}` for each matrix.
+        where :math:`R` is the rank of the first factorization step. Then, the matrices :math:`L^{(r)}`
+        are diagonalized to obtain a set of eigenvalues and eigenvectors for each matrix which can be
+        truncated with a rank :math:`M^{(r)}` for each matrix.
 
-    This function computes the rank :math:`R` and the average rank :math:`M` which is averaged over
-    :math:`M^{(r)}` ranks.
+        This function computes the rank :math:`R` and the average rank :math:`M` which is averaged over
+        :math:`M^{(r)}` ranks.
 
-    Args:
-        factors (array[array[float]]): matrices (factors) obtained from factorizing the
-            two-electron integral tensor
-        eigvals (array[float]): eigenvalues of the matrices obtained from factorizing the
-            two-electron integral tensor
-        tol (float): cutoff value for discarding the negligible eigenvalues
+        Args:
+            factors (array[array[float]]): matrices (factors) obtained from factorizing the
+                two-electron integral tensor
+            eigvals (array[float]): eigenvalues of the matrices obtained from factorizing the
+                two-electron integral tensor
+            tol (float): cutoff value for discarding the negligible eigenvalues
 
-    Returns:
-        tuple(array[float]): the ranks of double-factorized two-electron integral tensor
+        Returns:
+            tuple(array[float]): the ranks of double-factorized two-electron integral tensor
 
-    **Example**
+        **Example**
 
-    >>> symbols  = ['H', 'H']
-    >>> geometry = np.array([[0.0, 0.0, 0.0], [0.74, 0.0, 0.0]], requires_grad = False) / 0.5291772
-    >>> mol = qml.qchem.Molecule(symbols, geometry)
-    >>> core, one, two = qml.qchem.electron_integrals(mol)()
-    >>> two = np.swapaxes(two, 1, 3) # convert to chemist's notation
-    >>> l, w, v = factorize(two, 1e-5)
-    >>> print(rank(l, w))
-    (3, 2)
+        >>> symbols  = ['H', 'H']
+        >>> geometry = np.array([[0.0, 0.0, 0.0], [0.74, 0.0, 0.0]], requires_grad = False) / 0.5291772
+        >>> mol = qml.qchem.Molecule(symbols, geometry)
+        >>> core, one, two = qml.qchem.electron_integrals(mol)()
+        >>> two = np.swapaxes(two, 1, 3) # convert to chemist's notation
+        >>> l, w, v = factorize(two, 1e-5)
+        >>> print(rank(l, w))
+        (3, 2)
     """
     rank_r = len(factors)
 
---
+++ tests/resources/test_df.py
@@ -0,0 +1,47 @@
+# Copyright 2018-2021 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Unit tests for functions needed for computing the Hamiltonian.
+"""
+import pytest
+
+import pennylane as qml
+from pennylane import numpy as np
+
+
+@pytest.mark.parametrize(
+    ("factors", "eigvals", "rank_r_ref", "rank_m_ref"),
+    [
+        (
+            np.array(
+                [
+                    [[1.06723440e-01, 6.31328996e-15], [4.82191426e-15, -1.04898533e-01]],
+                    [[-1.37560572e-13, -4.25688222e-01], [-4.25688222e-01, -1.85111072e-13]],
+                    [[-8.14472856e-01, 3.11914551e-13], [3.11687682e-13, -8.28642140e-01]],
+                ]
+            ),
+            np.array(
+                [[-0.10489853, 0.10672344], [-0.42568822, 0.42568822], [-0.82864214, -0.81447286]]
+            ),
+            3,
+            2,
+        ),
+    ],
+)
+def test_rank(factors, eigvals, rank_r_ref, rank_m_ref):
+    r"""Test that rank function returns the correct ranks."""
+    rank_r, rank_m = qml.resources.rank
+
+    assert rank_r == rank_r_ref
+    assert rank_m == rank_m_ref
