f0e806aa6aed193447bf7efe915e7ce30d1b7ab9
Moritz Willmann <moritz.willmann@web.de>
2022-06-04 18:00:50 +0200

    add parameters to functions

--- pennylane/optimize/spsa.py
+++ pennylane/optimize/spsa.py
@@ -13,8 +13,7 @@
 # limitations under the License.
 """Simultaneous perturbation stochastic approximation (SPSA) optimizer"""
 
-import numpy as np
-from pennylane._grad import grad as get_gradient
+from pennylane import numpy as np
 
 
 class SPSAOptimizer:
@@ -37,8 +36,9 @@ class SPSAOptimizer:
 
         a_k = \frac{a}{(A+k)^\alpha}
         c_k = \frac{c}{k^\gamma}
-    
-    for each step :math:`k`
+
+    for each step :math:`k`, where :math:`\Delta_k` is a random pertubation vector drawn from a
+    +-1 Bernoulli distribution with probability of :math:`1/2` for both `-1` and `+1`.
 
     Args:
         a (float): scaling parameter for step size
@@ -48,20 +48,32 @@ class SPSAOptimizer:
         gamma (float): scaling exponent for evaluation step size
     """
 
-    def __init__(self, p, a, c, A, alpha, gamma):
+    # pylint: disable-msg=too-many-arguments
+    def __init__(self, maxit=100, a=None, c=0.1, A=None, alpha=0.602, gamma=0.101):
         self.a = a
         self.c = c
-        self.A = A
+        if A is None:
+            self.A = maxit / 10
+        else:
+            self.A = A
         self.alpha = alpha
         self.gamma = gamma
-        a.k = 1
+        self.k = 1
 
-    def step_and_cost(self, objective_fn, *args, grad_fn=None, **kwargs):
+    # pylint: disable-msg=too-many-arguments
+    def step_and_cost(
+        self, objective_fn, *args, a=None, c=None, A=None, alpha=None, gamma=None, **kwargs
+    ):
         """Update trainable arguments with one step of the optimizer and return the corresponding
         objective function value prior to the step.
 
         Args:
             objective_fn (function): the objective function for optimization
+            a (float): scaling parameter for step size
+            c (float): scaling parameter for evaluation step size
+            A (float): stability constant
+            alpha (float): scaling exponent for step size
+            gamma (float): scaling exponent for evaluation step size
             *args : variable length argument list for objective function
             **kwargs : variable length of keyword arguments for the objective function
 
@@ -71,23 +83,23 @@ class SPSAOptimizer:
             If single arg is provided, list [array] is replaced by array.
         """
 
-        g = self.compute_grad(objective_fn, args, kwargs)
-        new_args = self.apply_grad(g, args)
+        new_args = self.step(objective_fn, a, c, A, alpha, gamma, *args, **kwargs)
 
         forward = objective_fn(*args, **kwargs)
 
-        self.k += 1
-
-        # unwrap from list if one argument, cleaner return
-        if len(new_args) == 1:
-            return new_args[0], forward
         return new_args, forward
 
-    def step(self, objective_fn, *args, **kwargs):
+    # pylint: disable-msg=too-many-arguments
+    def step(self, objective_fn, *args, a=None, c=None, A=None, alpha=None, gamma=None, **kwargs):
         """Update trainable arguments with one step of the optimizer.
 
         Args:
             objective_fn (function): the objective function for optimization
+            a (float): scaling parameter for step size
+            c (float): scaling parameter for evaluation step size
+            A (float): stability constant
+            alpha (float): scaling exponent for step size
+            gamma (float): scaling exponent for evaluation step size
             *args : Variable length argument list for objective function
             **kwargs : variable length of keyword arguments for the objective function
 
@@ -96,8 +108,19 @@ class SPSAOptimizer:
             If single arg is provided, list [array] is replaced by array.
         """
 
-        g, _ = self.compute_grad(objective_fn, args, kwargs)
-        new_args = self.apply_grad(g, args)
+        if a is None:
+            a = self.a
+        if c is None:
+            c = self.c
+        if A is None:
+            A = self.A
+        if alpha is None:
+            alpha = self.alpha
+        if gamma is None:
+            gamma = self.gamma
+
+        g = self.compute_grad(objective_fn, args, kwargs, c, self.k, gamma)
+        new_args = self.apply_grad(g, args, a, A, self.k, alpha)
 
         self.k += 1
 
@@ -107,7 +130,9 @@ class SPSAOptimizer:
 
         return new_args
 
-    def compute_grad(self, objective_fn, args, kwargs):
+    # pylint: disable-msg=too-many-arguments
+    @staticmethod
+    def compute_grad(objective_fn, args, kwargs, c, k, gamma):
         r"""Compute gradient of the objective function at the given point and return it along with
         the objective function forward pass (if available).
 
@@ -116,15 +141,15 @@ class SPSAOptimizer:
             args (tuple): tuple of NumPy arrays containing the current parameters for the
                 objection function
             kwargs (dict): keyword arguments for the objective function
-            grad_fn (function): optional gradient function of the objective function with respect to
-                the variables ``args``. If ``None``, the gradient function is computed automatically.
-                Must return the same shape of tuple [array] as the autograd derivative.
+            c (float): scaling parameter for evaluation step size
+            k (int): iteration step
+            gamma (float): scaling exponent for evaluation step size
 
         Returns:
-            list[array]: NumPy array containing the gradient :math:`\nabla f(x^{(t)})`.
+            list[array]: NumPy array containing the gradient :math:`\hat{g}(x^{(t)})`.
         """
 
-        ck = self.c / self.k^self.gamma
+        ck = c / (k + 1) ** gamma
 
         delta = []
         args_minus_pert = list(args)
@@ -132,22 +157,23 @@ class SPSAOptimizer:
 
         for index, arg in enumerate(args):
             if getattr(arg, "requires_grad", False):
-                delta.append(np.random.binomial(1, 1/2, arg.size) * 2 - 1)
+                delta.append(np.random.binomial(1, 1 / 2, arg.size) * 2 - 1)
                 args_minus_pert[index] = arg - ck * delta[index]
                 args_plus_pert[index] = arg + ck * delta[index]
 
-        diff_sc = (objective_fn(*args_plus_pert, **kwargs) - objective_fn(*args_minus_pert, **kwargs)) / (2 * ck)
+        diff_sc = (
+            objective_fn(*args_plus_pert, **kwargs) - objective_fn(*args_minus_pert, **kwargs)
+        ) / (2 * ck)
 
-        grad = []
-        for delta_i in delta:
-            grad.append(diff_sc / delta_i)
+        grad = (diff_sc / di for di in delta)
 
         num_trainable_args = sum(getattr(arg, "requires_grad", False) for arg in args)
         grad = (grad,) if num_trainable_args == 1 else grad
 
         return grad
 
-    def apply_grad(self, grad, args):
+    # pylint: disable-msg=too-many-arguments
+    def apply_grad(self, grad, args, a, A, k, alpha):
         r"""Update the variables to take a single optimization step. Flattens and unflattens
         the inputs to maintain nested iterables as the parameters of the optimization.
 
@@ -155,12 +181,20 @@ class SPSAOptimizer:
             grad (tuple [array]): the gradient of the objective
                 function at point :math:`x^{(t)}`: :math:`\nabla f(x^{(t)})`
             args (tuple): the current value of the variables :math:`x^{(t)}`
+            a (float): scaling parameter for step size
+            A (float): stability constant
+            k (int): iteration step
+            alpha (float): scaling exponent for step size
 
         Returns:
             list [array]: the new values :math:`x^{(t+1)}`
         """
 
-        ak = self.a / (self.k + self.A)^self.alpha
+        if a is None:
+            a = (A + 1) ** alpha * 0.1 / max(g.max() for g in grad)
+            self.a = a
+
+        ak = a / (k + A + 1) ** alpha
 
         args_new = list(args)
 
