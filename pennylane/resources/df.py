a613e5b9b1736babc74c80b5fefc27093e355d91
soranjh <soranjh@yahoo.com>
2022-06-06 10:14:48 -0400

    add estimation cost

---
+++ pennylane/resources/df.py
@@ -0,0 +1,24 @@
+# Copyright 2018-2022 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This module contains the functions needed for resource estimation with double factorization method.
+"""
+
+from pennylane import numpy as np
+
+
+def estimation_cost(norm, error):
+    r"""
+    """
+    return np.ceil(np.pi * norm / (2 * error))
