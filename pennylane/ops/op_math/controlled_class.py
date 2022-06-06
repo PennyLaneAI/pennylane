ab68a6e6dc9ac3341608d6c17cc39283677bff69
albi3ro <chrissie.c.l@gmail.com>
2022-06-06 15:28:44 -0400

    improve matrix method

--- pennylane/ops/op_math/controlled_class.py
+++ pennylane/ops/op_math/controlled_class.py
@@ -21,27 +21,20 @@ from pennylane import math as qmlmath
 from pennylane import numpy as np
 
 import pennylane as qml
-from pennylane.operation import Operation, MatrixUndefinedError
-from pennylane.queuing import QueuingContext, QueuingError
+from pennylane.operation import (
+    Operation,
+    expand_matrix,
+)
+from pennylane.queuing import QueuingContext
 from pennylane.wires import Wires
 
 
-def _name_map(base, num_control_wires):
-    if isinstance(base, qml.PauliX):
-        if num_control_wires == 1:
-            return "CNOT"
-        if num_control_wires == 2:
-            return "Toffoli"
-        return "MultiControlledX"
-    if num_control_wires == 1:
-        if isinstance(base, qml.PauliY):
-            return "CY"
-        if isinstance(base, qml.PauliZ):
-            return "CZ"
-    return f"C{base.name}"
-
-
+# pylint: disable=too-many-arguments
 class Controlled(Operation):
+    """
+    Class docstring
+
+    """
 
     # pylint: disable=attribute-defined-outside-init
     def __copy__(self):
@@ -58,18 +51,25 @@ class Controlled(Operation):
 
         return copied_op
 
+    # pylint: disable=super-init-not-called
     def __init__(
         self, base, control_wires, control_values=None, work_wires=None, do_queue=True, id=None
     ):
-
+        control_wires = Wires(control_wires)
         if isinstance(control_values, str):
             warnings.warn(
                 "Specifying control values as a string is deprecated. Please use Sequence[Bool]",
                 UserWarning,
             )
             control_values = [(x == "1") for x in control_values]
+        if control_values is None:
+            control_values = [True] * len(control_wires)
+        if not set(control_values).issubset({False, True}):
+            raise ValueError(
+                "control_values can only take on True or False.  Provided control"
+                f" values are {control_values}."
+            )
 
-        control_wires = Wires(control_wires)
         if Wires.shared_wires([base.wires, control_wires]):
             raise ValueError("The control wires must be different from the base operation wires.")
 
@@ -78,7 +78,7 @@ class Controlled(Operation):
         self.hyperparameters["control_values"] = control_values
         self.hyperparameters["work_wires"] = Wires([]) if work_wires is None else Wires(work_wires)
 
-        self._name = _name_map(base, len(control_wires))
+        self._name = f"C{base.name}"
 
         self._id = id
         self.queue_idx = None
@@ -89,26 +89,32 @@ class Controlled(Operation):
 
     @property
     def base(self):
+        """The Operator being controlled."""
         return self.hyperparameters["base"]
 
     @property
     def control_wires(self):
+        """The control wires."""
         return self.hyperparameters["control_wires"]
 
     @property
     def target_wires(self):
+        """The wires of the base operation."""
         return self.base.wires
 
     @property
     def control_values(self):
+        """Iterable[Bool]. For each control wire, denotes whether to control on ``True`` or ``False``."""
         return self.hyperparameters["control_values"]
 
     @property
     def work_wires(self):
+        """Additional wires that can be used in the decomposition. Not modified by the operation."""
         return self.hyperparameters["work_wires"]
 
     @property
     def data(self):
+        """Trainable parameters that the operator depends on."""
         return self.base.data
 
     @data.setter
@@ -127,10 +133,12 @@ class Controlled(Operation):
     def wires(self):
         return self.control_wires + self.base.wires + self.work_wires
 
+    # pylint: disable=protected-access
     @property
     def _wires(self):
         return self.wires
 
+    # pylint: disable=protected-access
     @_wires.setter
     def _wires(self, new_wires):
         num_control = len(self.control_wires)
@@ -157,44 +165,48 @@ class Controlled(Operation):
 
     @property
     def basis(self):
+        """Used in compilation."""
         return self.base.basis
 
     def queue(self, context=QueuingContext):
-        try:
-            context.update_info(self.base, owner=self)
-        except QueuingError:
-            self.base.queue(context=context)
-            context.update_info(self.base, owner=self)
+        context.safe_update_info(self.base, owner=self)
         context.append(self, owns=self.base)
-
         return self
 
     def label(self, decimals=None, base_label=None, cache=None):
         return self.base.label(decimals=decimals, base_label=base_label, cache=cache)
 
+    # pylint: disable=invalid-overridden-method
     @property
     def has_matrix(self):
         return self.base.has_matrix
 
-    @staticmethod
-    def compute_matrix(*args, base=None, control_wires=None, control_values=None, work_wires=None):
-        if control_values is not None:
-            raise MatrixUndefinedError
+    def matrix(self, wire_order=None):
 
-        base_matrix = base.matrix()
+        base_matrix = self.base.matrix()
         interface = qmlmath.get_interface(base_matrix)
 
         base_matrix_size = qmlmath.shape(base_matrix)[0]
-        num_control_states = 2 ** len(control_wires)
+        num_control_states = 2 ** len(self.control_wires)
         total_matrix_size = num_control_states * base_matrix_size
-        padding = total_matrix_size - base_matrix_size
 
-        left_pad = qmlmath.cast_like(qmlmath.eye(padding, like=interface), 1j)
+        control_int = sum(2**i for i, val in enumerate(reversed(self.control_values)) if val)
+        padding_left = control_int * base_matrix_size
+        padding_right = total_matrix_size - padding_left - base_matrix_size
 
-        return qmlmath.block_diag([left_pad, base_matrix])
+        left_pad = qmlmath.cast_like(qmlmath.eye(padding_left, like=interface), 1j)
+        right_pad = qmlmath.cast_like(qmlmath.eye(padding_right, like=interface), 1j)
+
+        canonical_matrix = qmlmath.block_diag([left_pad, base_matrix, right_pad])
+
+        if wire_order is None or self.wires == Wires(wire_order):
+            return canonical_matrix
+
+        active_wires = self.control_wires + self.target_wires
+        return expand_matrix(canonical_matrix, wires=active_wires, wire_order=wire_order)
 
     def eigvals(self):
-        if self.control_values is None:
+        if all(self.control_values):
             base_eigvals = self.base.eigvals()
             ones = np.ones(2 ** len(self.control_wires))
             return qml.math.concatenate([ones, base_eigvals])
