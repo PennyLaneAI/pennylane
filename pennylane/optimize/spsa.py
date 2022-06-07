588d152289dd05f38318ec07fad2222e75713c89
Ixchel Meza <25181125+ixime@users.noreply.github.com>
2022-06-06 20:02:50 -0500

    fix docstring
    Co-authored-by: antalszava <antalszava@gmail.com>

--- pennylane/optimize/spsa.py
+++ pennylane/optimize/spsa.py
@@ -139,8 +139,8 @@ class SPSAOptimizer:
             for each iteration. Its asymptotically optimal value is 1/6.
         c (float): A hyperparameter related to the expected noise. It should be
             approximately the standard deviation of the expected noise on the cost function
-        A (float): The stability constant expected to be 10% of maximum number
-            of expected iterations
+        A (float): The stability constant expected to be 10% of the maximum number
+            of expected iterations.
         a (float): An hyperparameter expected to be small in noisy situations,
             whose value could be :math:`\frac{mag(\Delta\theta)}{mag(g(\theta))}(A+1)^\alpha`
     """
