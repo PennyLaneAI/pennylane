7ffc0e9a5fae8fd10b2babfcf30ce4e43b700ca9
ankit_linux <ankit27.kh@gmail.com>
2022-06-07 08:50:20 +0530

    remove assert statements in favor of returns

--- pennylane/ops/functions/equal.py
+++ pennylane/ops/functions/equal.py
@@ -67,26 +67,31 @@ def equal(op1, op2, check_interface=True, check_trainability=True, rtol=1e-5, at
         If the parameters do not have a `requires_grad` attribute, the function will return `False` if `check_trainability`
         is `True`.
     """
-    try:
-        assert op1.__class__ is op2.__class__
-        assert all(
-            qml.math.allclose(d1, d2, rtol=rtol, atol=atol) for d1, d2 in zip(op1.data, op2.data)
-        )
-        assert op1.wires == op2.wires
-        for kwarg in op1.hyperparameters:
-            assert op1.hyperparameters[kwarg] == op2.hyperparameters[kwarg]
-
-        if check_trainability:
-            for params_1, params_2 in zip(op1.data, op2.data):
-                try:
-                    assert params_1.requires_grad == params_2.requires_grad
-                except AttributeError:
-                    return False
-
-        if check_interface:
-            for params_1, params_2 in zip(op1.data, op2.data):
-                assert isinstance(params_1, type(params_2))
-
-        return True
-    except AssertionError:
+    if op1.__class__ is not op2.__class__:
         return False
+    if not all(
+        qml.math.allclose(d1, d2, rtol=rtol, atol=atol) for d1, d2 in zip(op1.data, op2.data)
+    ):
+        return False
+    if not op1.wires == op2.wires:
+        return False
+    for kwarg in op1.hyperparameters:
+        if not op1.hyperparameters[kwarg] == op2.hyperparameters[kwarg]:
+            return False
+
+    if check_trainability:
+        for params_1, params_2 in zip(op1.data, op2.data):
+            # Vanilla NumPy and JAX do not have the `requires_grad` attribute.
+            # This will raise an `AttributeError` for such cases.
+            try:
+                if not params_1.requires_grad == params_2.requires_grad:
+                    return False
+            except AttributeError:
+                return False
+
+    if check_interface:
+        for params_1, params_2 in zip(op1.data, op2.data):
+            if not isinstance(params_1, type(params_2)):
+                return False
+
+    return True
