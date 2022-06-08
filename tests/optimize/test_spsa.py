af157beb2bd10c77ef0026754c1efbb617c9ca4f
ixime <ixchel.meza.ch@gmail.com>
2022-06-08 08:14:39 -0500

    fix names and docstrings in tests

--- tests/optimize/test_spsa.py
+++ tests/optimize/test_spsa.py
@@ -57,10 +57,8 @@ class TestSPSAOptimizer:
 
     @pytest.mark.parametrize("f", multivariate)
     def test_apply_grad_multivar(self, f):
-        """
-        Test that a gradient step can be applied correctly to a multivariate
-        function.
-        """
+        """Test that a gradient step can be applied correctly to a multivariate
+        function."""
         alpha = 0.602
         gamma = 0.101
         c = 0.2
@@ -84,10 +82,8 @@ class TestSPSAOptimizer:
 
     @pytest.mark.parametrize("args", [0, -3, 42])
     @pytest.mark.parametrize("f", univariate)
-    def test_step_and_cost_supplied_cost(self, args, f):
-        """
-        Test that returned cost is correct.
-        """
+    def test_step_and_cost_supplied_univar_cost(self, args, f):
+        """Test that returned cost is correct."""
         spsa_opt = qml.SPSAOptimizer(maxiter=10)
         args = np.array(args, requires_grad=True)
 
@@ -95,11 +91,9 @@ class TestSPSAOptimizer:
         expected = f(args)
         assert np.all(res == expected)
 
-    def test_step_and_cost_supplied_cost2(self):
-        """
-        Test that the correct cost is returned via the step_and_cost method
-        for the SPSA optimizer.
-        """
+    def test_step_and_cost_supplied_cost(self):
+        """Test that the correct cost is returned via the step_and_cost method
+        for the SPSA optimizer."""
         spsa_opt = qml.SPSAOptimizer(maxiter=10)
 
         @qml.qnode(qml.device("default.qubit", wires=1))
@@ -117,10 +111,8 @@ class TestSPSAOptimizer:
         assert np.all(res == expected)
 
     def test_step_and_cost_hamiltonian(self):
-        """
-        Test that the correct cost is returned via the step_and_cost method
-        for the SPSA optimizer.
-        """
+        """Test that the correct cost is returned via the step_and_cost method
+        for the SPSA optimizer."""
         spsa_opt = qml.SPSAOptimizer(maxiter=10)
 
         @qml.qnode(qml.device("default.qubit", wires=1))
@@ -135,10 +127,8 @@ class TestSPSAOptimizer:
 
         assert np.all(res == expected)
 
-    def test_step_spsa2(self):
-        """
-        Test that the correct param is returned via the step method.
-        """
+    def test_step_spsa(self):
+        """Test that the correct param is returned via the step method."""
         spsa_opt = qml.SPSAOptimizer(maxiter=10)
 
         @qml.qnode(qml.device("default.qubit", wires=1))
@@ -170,10 +160,8 @@ class TestSPSAOptimizer:
         assert np.allclose(res, expected, atol=tol)
 
     def test_step_and_cost_spsa_single_multid_input(self):
-        """
-        Test that the correct cost is returned via the step_and_cost method
-        with a multidimensional input.
-        """
+        """Test that the correct cost is returned via the step_and_cost method
+        with a multidimensional input."""
         spsa_opt = qml.SPSAOptimizer(maxiter=10)
         multid_array = np.array([[0.1, 0.2], [-0.1, -0.4]])
 
@@ -190,10 +178,8 @@ class TestSPSAOptimizer:
         assert np.all(res == expected)
 
     def test_step_spsa_single_multid_input(self):
-        """
-        Test that the correct param is returned via the step method
-        with a multidimensional input.
-        """
+        """Test that the correct param is returned via the step method
+        with a multidimensional input."""
         spsa_opt = qml.SPSAOptimizer(maxiter=10)
         multid_array = np.array([[0.1, 0.2], [-0.1, -0.4]])
 
@@ -227,11 +213,9 @@ class TestSPSAOptimizer:
 
     @pytest.mark.parametrize("args", [0, -3, 42])
     @pytest.mark.parametrize("f", univariate)
-    def test_step(self, args, f):
-        """
-        Test that a gradient step can be applied correctly with a univariate
-        function.
-        """
+    def test_step_for_univar_cost(self, args, f):
+        """Test that a gradient step can be applied correctly with a univariate
+        function."""
         spsa_opt = qml.SPSAOptimizer(maxiter=10)
         args = np.array([args], requires_grad=True)
 
@@ -255,10 +239,8 @@ class TestSPSAOptimizer:
     @pytest.mark.parametrize("args", [0, -3, 42])
     @pytest.mark.parametrize("f", univariate)
     def test_step_and_cost(self, args, f):
-        """
-        Test that a gradient step can be applied correctly with a univariate
-        function.
-        """
+        """Test that a gradient step can be applied correctly with a univariate
+        function."""
         spsa_opt = qml.SPSAOptimizer(maxiter=10)
         args = np.array([args], requires_grad=True)
 
@@ -281,10 +263,8 @@ class TestSPSAOptimizer:
         assert np.allclose(y, rescost, atol=tol)
 
     def test_parameters_not_a_tensor_and_not_all_require_grad(self):
-        """
-        Test execution of list of parameters of different sizes
-        and not all require grad.
-        """
+        """Test execution of list of parameters of different sizes
+        and not all require grad."""
         spsa_opt = qml.SPSAOptimizer(maxiter=10)
 
         @qml.qnode(qml.device("default.qubit", wires=1))
@@ -306,10 +286,8 @@ class TestSPSAOptimizer:
         assert np.all(res[0] != inputs[0])
 
     def test_parameters_in_step(self):
-        """
-        Test execution of list of parameters of different sizes
-        and not all require grad.
-        """
+        """Test execution of list of parameters of different sizes
+        and not all require grad."""
         spsa_opt = qml.SPSAOptimizer(maxiter=10)
 
         @qml.qnode(qml.device("default.qubit", wires=1))
@@ -331,10 +309,8 @@ class TestSPSAOptimizer:
         assert np.all(res[0] != inputs[0])
 
     def test_parameter_not_an_array(self):
-        """
-        Test function when there is only one float parameter that doesn't
-        require grad.
-        """
+        """Test function when there is only one float parameter that doesn't
+        require grad."""
         dev = qml.device("default.qubit", wires=1)
 
         @qml.qnode(dev)
@@ -354,10 +330,8 @@ class TestSPSAOptimizer:
         assert res == params
 
     def test_obj_func_not_a_scalar_function(self):
-        """
-        Test that if the objective function is not a
-        scalar function, an error is raised.
-        """
+        """Test that if the objective function is not a
+        scalar function, an error is raised."""
 
         n_wires = 4
         n_layers = 3
