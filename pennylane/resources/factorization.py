add error for wrong shape

pennylane/resources/factorization.py
@@ -117,7 +117,12 @@ def factorize(two, tol):
      [[-8.14472856e-01  5.01669019e-13]
       [ 5.01689072e-13 -8.28642140e-01]]]
     """
-    n = two.shape[0]
+    shape = two.shape
+
+    if len(shape) != 4 or len(set(shape)) != 1:
+        raise ValueError("The two-electron repulsion tensor must have a (N x N x N x N) shape.")
+
+    n = shape[0]
     two = two.reshape(n * n, n * n)
 
     eigvals_r, eigvecs_r = np.linalg.eigh(two)

tests/resources/test_factorization.py
@@ -66,7 +66,7 @@ def test_factorize(two_tensor, factors_ref):
 
 
 @pytest.mark.parametrize(
-    ("two_tensor"),
+    "two_tensor",
     [
         # two-electron tensor computed as
         # symbols  = ['H', 'H']
@@ -96,3 +96,23 @@ def test_factorize_reproduce(two_tensor):
         two_computed += np.einsum("ij, lk", mat, mat)
 
     assert np.allclose(two_computed, two_tensor)
+
+
+@pytest.mark.parametrize(
+    "two_tensor",
+    [
+        np.array(
+            [
+                [6.74755872e-01, -2.85826918e-13, -2.85799162e-13, 6.63711349e-01],
+                [-2.85965696e-13, 1.81210478e-01, 1.81210478e-01, -2.63900013e-13],
+                [-2.85854673e-13, 1.81210478e-01, 1.81210478e-01, -2.63900013e-13],
+                [6.63711349e-01, -2.63677968e-13, -2.63788991e-13, 6.97651447e-01],
+            ]
+        ),
+    ],
+)
+def test_shape_error(two_tensor):
+    r"""Test that the factorize function raises an error when the two-electron repulsion tensor does
+    not have the correct shape."""
+    with pytest.raises(ValueError, match="The two-electron repulsion tensor must have"):
+        qml.resources.factorize(two_tensor, 1e-5)