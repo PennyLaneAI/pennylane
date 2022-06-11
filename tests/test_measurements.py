bd07da0dc99c69b48f2eef1c53248b6072ddf86d
breznychenko <bogdan.reznychenko@probayes.com>
2022-06-11 09:37:16 +0200

    unit test for Counts measurement type

--- tests/test_measurements.py
+++ tests/test_measurements.py
@@ -31,6 +31,7 @@ from pennylane.measurements import (
     density_matrix,
     Expectation,
     Sample,
+    Counts,
     State,
     Variance,
     Probability,
@@ -47,6 +48,7 @@ from pennylane.measurements import (
     [
         (Expectation, "expval"),
         (Sample, "sample"),
+        (Counts, "counts"),
         (Variance, "var"),
         (Probability, "probs"),
         (State, "state"),
@@ -504,6 +506,126 @@ class TestSample:
             match="Getting the output shape of a measurement returning samples along with a device with a shot vector is not supported.",
         ):
             res.shape(dev)
+
+
+class TestCounts:
+    """Tests for the counts function"""
+
+    def test_counts_dimension(self, tol):
+        """Test that the sample function outputs counts of the right size"""
+        n_sample = 10
+
+        dev = qml.device("default.qubit", wires=2, shots=n_sample)
+
+        @qml.qnode(dev)
+        def circuit():
+            qml.RX(0.54, wires=0)
+            return qml.sample(qml.PauliZ(0), counts = True), qml.sample(qml.PauliX(1), counts = True)
+
+        sample = circuit()
+
+        assert np.array_equal(sample.shape, (2,)) & np.all([sum(s.unwrap().values())==n_sample for s in sample])
+
+    def test_counts_combination(self, tol):
+        """Test the output of combining expval, var and counts"""
+        n_sample = 10
+
+        dev = qml.device("default.qubit", wires=3, shots=n_sample)
+
+        @qml.qnode(dev, diff_method="parameter-shift")
+        def circuit():
+            qml.RX(0.54, wires=0)
+
+            return qml.sample(qml.PauliZ(0), counts=True), qml.expval(qml.PauliX(1)), qml.var(qml.PauliY(2))
+
+        result = circuit()
+
+        assert len(result) == 3
+        assert sum(result[0].unwrap().values())==n_sample
+        assert isinstance(result[1], np.ndarray)
+        assert isinstance(result[2], np.ndarray)
+
+    def test_single_wire_counts(self, tol):
+        """Test the return type and shape of sampling counts from a single wire"""
+        n_sample = 10
+
+        dev = qml.device("default.qubit", wires=1, shots=n_sample)
+
+        @qml.qnode(dev)
+        def circuit():
+            qml.RX(0.54, wires=0)
+
+            return qml.sample(qml.PauliZ(0), counts = True)
+
+        result = circuit()
+
+        assert isinstance(result, np.ndarray)
+        assert sum(result.unwrap().values())==n_sample
+
+    def test_multi_wire_counts_regular_shape(self, tol):
+        """Test the return type and shape of sampling multiple wires
+        where a rectangular array is expected"""
+        n_sample = 10
+
+        dev = qml.device("default.qubit", wires=3, shots=n_sample)
+
+        @qml.qnode(dev)
+        def circuit():
+            return qml.sample(qml.PauliZ(0), counts = True), qml.sample(qml.PauliZ(1), counts = True), qml.sample(qml.PauliZ(2), counts = True)
+
+        result = circuit()
+
+        # If all the dimensions are equal the result will end up to be a proper rectangular array
+        assert isinstance(result, np.ndarray)
+        assert result.shape[0] == 3
+        assert all(sum(r.unwrap().values())==n_sample for r in result)
+        assert all(all(v.dtype == np.dtype("int") for v in r.unwrap().values()) for r in result)
+
+
+    def test_observable_return_type_is_counts(self):
+        """Test that the return type of the observable is :attr:`ObservableReturnTypes.Counts`"""
+        n_shots = 10
+        dev = qml.device("default.qubit", wires=1, shots=n_shots)
+
+        @qml.qnode(dev)
+        def circuit():
+            res = qml.sample(qml.PauliZ(0), counts = True)
+            assert res.return_type is Counts
+            return res
+
+        circuit()
+
+
+    def test_providing_no_observable_and_no_wires_counts(self):
+        """Test that we can provide no observable and no wires to sample function"""
+        dev = qml.device("default.qubit", wires=2, shots=1000)
+
+        @qml.qnode(dev)
+        def circuit():
+            qml.Hadamard(wires=0)
+            res = qml.sample(counts = True)
+            assert res.obs is None
+            assert res.wires == qml.wires.Wires([])
+            return res
+
+        circuit()
+
+    def test_providing_no_observable_and_wires_counts(self):
+        """Test that we can provide no observable but specify wires to the sample function"""
+        wires = [0, 2]
+        wires_obj = qml.wires.Wires(wires)
+        dev = qml.device("default.qubit", wires=3, shots=1000)
+
+        @qml.qnode(dev)
+        def circuit():
+            qml.Hadamard(wires=0)
+            res = qml.sample(wires=wires, counts=True)
+
+            assert res.obs is None
+            assert res.wires == wires_obj
+            return res
+
+        circuit()
 
 
 class TestMeasure:
