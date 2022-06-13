b4516700b753e826854e910cfd5a012bdc18fa95
mudit2812 <muditpandey2812@gmail.com>
2022-06-13 15:17:03 -0700

    Added tests for wire and prob integration

--- tests/devices/test_default_qutrit.py
+++ tests/devices/test_default_qutrit.py
@@ -246,32 +246,34 @@ class TestApply:
     # TODO: Add tests for state preperation ops
 
 
-class TestExpval:
-    pass
+# TODO: Add tests after addition of observables
+# class TestExpval:
+#     pass
 
 
-class TestVar:
-    pass
+# class TestVar:
+#     pass
 
 
-class TestSample:
-    pass
+# class TestSample:
+#     pass
 
 
 class TestDefaultQutritIntegration:
     pass
 
 
-class TestTensorExpval:
-    pass
+# TODO: Add tests after addition of observables
+# class TestTensorExpval:
+#     pass
 
 
-class TestTensorVar:
-    pass
+# class TestTensorVar:
+#     pass
 
 
-class TestTensorSample:
-    pass
+# class TestTensorSample:
+#     pass
 
 
 class TestDtypePreserved:
@@ -279,11 +281,124 @@ class TestDtypePreserved:
 
 
 class TestProbabilityIntegration:
-    pass
+    """Test probability method for when analytic is True/False"""
+
+    def mock_analytic_counter(self, wires=None):
+        self.analytic_counter += 1
+        return np.array([1, 0, 0, 0, 0, 0, 0, 0, 0], dtype=float)
+
+    @pytest.mark.parametrize("x", [[U_thadamard_01, U_clock], [U_shift, U_thadamard_01], [U_z_12, U_thadamard_01]])
+    def test_probability(self, x, tol):
+        """Test that the probability function works for finite and infinite shots"""
+        dev = qml.device("default.qutrit", wires=2, shots=1000)
+        dev_analytic = qml.device("default.qutrit", wires=2, shots=None)
+
+        def circuit(x):
+            qml.QutritUnitary(x[0], wires=0)
+            qml.QutritUnitary(x[1], wires=0)
+            qml.QutritUnitary(U_tadd, wires=[0, 1])
+            return qml.probs(wires=[0, 1])
+
+        prob = qml.QNode(circuit, dev)
+        prob_analytic = qml.QNode(circuit, dev_analytic)
+
+        assert np.isclose(prob(x).sum(), 1, atol=tol, rtol=0)
+        assert np.allclose(prob_analytic(x), prob(x), atol=0.1, rtol=0)
+        assert not np.array_equal(prob_analytic(x), prob(x))
+
+    def test_call_generate_samples(self, monkeypatch):
+        """Test analytic_probability call when generating samples"""
+        self.analytic_counter = False
+
+        dev = qml.device("default.qutrit", wires=2, shots=1000)
+        monkeypatch.setattr(dev, "analytic_probability", self.mock_analytic_counter)
+
+        # generate samples through `generate_samples` (using 'analytic_probability')
+        dev.generate_samples()
+
+        # should call `analytic_probability` once through `generate_samples`
+        assert self.analytic_counter == 1
+
+    def test_stateless_analytic_return(self):
+        """Test that analytic_probability returns None if device is stateless"""
+        dev = qml.device("default.qutrit", wires=2)
+        dev._state = None
+
+        assert dev.analytic_probability() is None
 
 
 class TestWiresIntegration:
-    pass
+    """Test that the device integrates with PennyLane's wire management."""
+
+    def make_circuit_probs(self, wires):
+        """Factory for a qnode returning probabilities using arbitrary wire labels."""
+        dev = qml.device("default.qutrit", wires=wires)
+        n_wires = len(wires)
+
+        @qml.qnode(dev)
+        def circuit():
+            qml.QutritUnitary(U_shift, wires=wires[0 % n_wires])
+            qml.QutritUnitary(U_clock, wires=wires[1 % n_wires])
+            if n_wires > 1:
+                qml.QutritUnitary(U_tswap, wires=[wires[0], wires[1]])
+            return qml.probs(wires=wires)
+
+        return circuit
+
+    @pytest.mark.parametrize(
+        "wires1, wires2",
+        [
+            (["a", "c", "d"], [2, 3, 0]),
+            ([-1, -2, -3], ["q1", "ancilla", 2]),
+            (["a", "c"], [3, 0]),
+            ([-1, -2], ["ancilla", 2]),
+            (["a"], ["nothing"]),
+        ],
+    )
+    def test_wires_probs(self, wires1, wires2, tol):
+        """Test that the probability vector of a circuit is independent from the wire labels used."""
+
+        circuit1 = self.make_circuit_probs(wires1)
+        circuit2 = self.make_circuit_probs(wires2)
+
+        assert np.allclose(circuit1(), circuit2(), tol)
+
+    def test_wires_not_found_exception(self):
+        """Tests that an exception is raised when wires not present on the device are adressed."""
+        dev = qml.device("default.qutrit", wires=["a", "b"])
+
+        with qml.tape.QuantumTape() as tape:
+            qml.QutritUnitary(np.eye(3), wires="c")
+
+        with pytest.raises(WireError, match="Did not find some of the wires"):
+            dev.execute(tape)
+
+    wires_to_try = [
+        (1, Wires([0]), Wires([0])),
+        (4, Wires([1, 3]), Wires([1, 3])),
+        (["a", 2], Wires([2]), Wires([1])),
+        (["a", 2], Wires([2, "a"]), Wires([1, 0])),
+    ]
+
+    @pytest.mark.parametrize("dev_wires, wires_to_map, res", wires_to_try)
+    def test_map_wires_caches(self, dev_wires, wires_to_map, res, mock_device):
+        """Test that multiple calls to map_wires will use caching."""
+        dev = qml.device("default.qutrit", wires=dev_wires)
+
+        original_hits = dev.map_wires.cache_info().hits
+        original_misses = dev.map_wires.cache_info().misses
+
+        # The first call is computed: it's a miss as it didn't come from the cache
+        dev.map_wires(wires_to_map)
+
+        # The number of misses increased
+        assert dev.map_wires.cache_info().misses > original_misses
+
+        # The second call comes from the cache: it's a hit
+        dev.map_wires(wires_to_map)
+
+        # The number of hits increased
+        assert dev.map_wires.cache_info().hits > original_hits
 
 
 class TestApplyOps:
