a016cd195233ad295f5d45f2986806d25cfff18b
ixime <ixchel.meza.ch@gmail.com>
2022-06-13 10:09:50 -0500

    fix docstring

--- pennylane/optimize/spsa.py
+++ pennylane/optimize/spsa.py
@@ -69,7 +69,7 @@ class SPSAOptimizer:
     .. note::
 
         * One SPSA iteration step of a cost function that involves computing the expectation value of
-        a Hamiltonian with ``M`` terms requires :math:`2*M` quantum device executions.
+          a Hamiltonian with ``M`` terms requires :math:`2*M` quantum device executions.
         * The forward-pass value of the cost function is not computed when stepping the optimizer.
           Therefore, in case of using ``step_and_cost`` method instead of ``step``, the number
           of executions will include the cost function evaluations.
