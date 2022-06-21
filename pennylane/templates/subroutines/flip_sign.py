ba09df2c236ada96083bc82ae531d796bb319b6f
Sergio Martinez <sergiomtz.losa@gmail.com>
2022-06-21 11:19:32 +0200

    FlipSign documentation class review

--- pennylane/templates/subroutines/flip_sign.py
+++ pennylane/templates/subroutines/flip_sign.py
@@ -25,8 +25,8 @@ class FlipSign(Operation):
 
     In a nutshell, this class perform the following operation:
 
-    FlipSign(n)|m> = -|m> if len(m) = len(n)
-    FlipSign(n)|m> = |m> if len(m) != len(n)
+    FlipSign(n):math:`|m\rangle = -|m\rangle` if len(m) = len(n)
+    FlipSign(n):math:`|m\rangle = |m\rangle` if len(m) != len(n)
 
     Where m is the state to flip and n is the array of qubits (wires).
     It flips the sign of the state.
