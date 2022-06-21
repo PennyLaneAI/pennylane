960a9ec4eee93dababbb3ea86f91d76301519db8
dime10 <dime10@users.noreply.github.com>
2022-06-21 14:06:02 -0400

    Require semantic-version>=2.7 for new API

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ requirements = [
     "autograd",
     "toml",
     "appdirs",
-    "semantic-version",
+    "semantic-version>=2.7",
     "autoray>=0.3.1",
     "cachetools",
     "pennylane-lightning>=0.24",
