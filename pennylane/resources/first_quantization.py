180d5432070ffbc467d4781dd2fa137a9370cff8
soranjh <soranjh@yahoo.com>
2022-06-21 22:02:04 -0400

    modify docstring

--- pennylane/resources/first_quantization.py
+++ pennylane/resources/first_quantization.py
@@ -59,38 +59,42 @@ def norm_fq(eta, n, omega, error, br=7, charge=0):
     r"""Return the 1-norm of a first-quantized Hamiltonian in the plane-wave basis.
 
     The expressions needed for computing the norm are taken from
-    [`PRX Quantum 2, 040332 (2021) <https://link.aps.org/doi/10.1103/PRXQuantum.2.040332>`_].
-    For numerical convenience, we have used the following modified expressions for computing
+    [`PRX Quantum 2, 040332 (2021) <https://link.aps.org/doi/10.1103/PRXQuantum.2.040332>`_]. The
+    norm is computed assuming that amplitude ampliﬁcation is performed.
+
+    For numerical convenience, we use the following modified expressions for computing
     parameters that contain a sum over the elements, :math:`\nu`, of the set of reciprocal lattice
-    vectors, :math:`G_0`. For :math:`\lambda_{\nu}` defined in Eq. (25) as
+    vectors, :math:`G_0`.
+
+    For :math:`\lambda_{\nu}` defined in Eq. (25) as
 
     .. math::
 
         \lambda_{\nu} = \sum_{\nu \in G_0} \frac{1}{\left \| \nu \right \|^2},
 
-    we have used
+    we use
 
     .. math::
 
         \lambda_{\nu} = 4\pi \left ( \frac{\sqrt{3}}{2} N^{1/3} - 1 \right) + 3 - \frac{3}{N^{1/3}}
         + 3 \int_{x=1}^{N^{1/3}} \int_{y=1}^{N^{1/3}} \frac{1}{x^2 + y^2} dydx.
 
-    We also need to compute :math:`\lambda^{\alpha}_{\nu}` which is defined in Eq. (123) as
+    We also need to compute :math:`\lambda^{\alpha}_{\nu}` defined in Eq. (123)
 
     .. math::
 
         \lambda^{\alpha}_{\nu} = \alpha \sum_{\nu \in G_0} \frac{\left \lceil M(2^{\mu - 2}) / \left
         \| \nu \right \|^2 \right \rceil}{M2^{2\mu - 4}},
 
-    which we compute, following Eq. (113) for :math:`\alpha = 1`, as
+    which we compute here, following Eq. (113) for :math:`\alpha = 1`, as
 
     .. math::
 
         \lambda^{1}_{\nu} = \lambda_{\nu} + \frac{4}{2^{n_m}} (7 \times 2^{n_p + 1} + 9 n_p -
         11 - 3 \times 2^{-n_p}),
 
-    where :math:`M = 2^{n_m}` and :math:`n_m` is defined in Eq. (132). Finally, for :math:`p_\{nu}`
-    defined in Eq. (128) as
+    where :math:`M = 2^{n_m}` and :math:`n_m` is defined in Eq. (132). Finally, for :math:`p_{\nu}`
+    defined in Eq. (128)
 
     .. math::
 
@@ -143,7 +147,7 @@ def norm_fq(eta, n, omega, error, br=7, charge=0):
     # target error in the qubitization of U+V which we set to be 0.01 of the algorithm error
     error_uv = 0.01 * error
 
-    # n_p is taken from Eq. (22)
+    # taken from Eq. (22) of PRX Quantum 2, 040332 (2021)
     n_p = int(np.ceil(np.log2(n ** (1 / 3) + 1)))
 
     n0 = n ** (1 / 3)
@@ -153,35 +157,35 @@ def norm_fq(eta, n, omega, error, br=7, charge=0):
         - 3 / n ** (1 / 3)
         + 3 * integrate.nquad(lambda x, y: 1 / (x**2 + y**2), [[1, n0], [1, n0]])[0]
     )
-    n_m = np.log2(  # taken from Eq. (132)
+    n_m = np.log2(  # taken from Eq. (132) of PRX Quantum 2, 040332 (2021)
         (2 * eta)
         / (error_uv * np.pi * omega ** (1 / 3))
         * (eta - 1 + 2 * l_z)
         * (7 * 2 ** (n_p + 1) - 9 * n_p - 11 - 3 * 2 ** (-1 * n_p))
     )
-    # computed using Eq. (113)
+    # computed using Eq. (113) of PRX Quantum 2, 040332 (2021)
     lambda_nu_1 = lambda_nu + 4 / 2**n_m * (
         7 * 2 ** (n_p + 1) + 9 * n_p - 11 - 3 * 2 ** (-1 * n_p)
     )
 
     p_nu = 0.2398  # upper bound from Eq. (29) in arxiv:1807.09802
-    p_nu_amp = np.sin(3 * np.arcsin(np.sqrt(p_nu))) ** 2  # p_nu_amp is taken from Eq. (129)
+    p_nu_amp = np.sin(3 * np.arcsin(np.sqrt(p_nu))) ** 2  # Eq. (129), PRX Quantum 2, 040332 (2021)
 
-    # lambda_u and lambda_v are taken from Eq. (25)
+    # lambda_u and lambda_v are taken from Eq. (25) of PRX Quantum 2, 040332 (2021)
     lambda_u = eta * l_z * lambda_nu / (np.pi * omega ** (1 / 3))
     lambda_v = eta * (eta - 1) * lambda_nu / (2 * np.pi * omega ** (1 / 3))
 
-    # lambda_t_p is taken from Eq. (71)
+    # taken from Eq. (71) of PRX Quantum 2, 040332 (2021)
     lambda_t_p = 6 * eta * np.pi**2 / omega ** (2 / 3) * 2 ** (2 * n_p - 2)
 
-    # lambda_u_1 and lambda_v_1 are taken from Eq. (124)
+    # lambda_u_1 and lambda_v_1 are taken from Eq. (124) of PRX Quantum 2, 040332 (2021)
     lambda_u_1 = lambda_u * lambda_nu_1 / lambda_nu
     lambda_v_1 = lambda_v * lambda_nu_1 / lambda_nu
 
-    # p_eq is taken from Eq. (63)
+    # taken from Eq. (63) of PRX Quantum 2, 040332 (2021)
     p_eq = success_prob(3, 8) * success_prob(3 * eta + 2 * charge, br) * success_prob(eta, br) ** 2
 
-    # the equations for computing the final lambda value are taken from Eq. (126)
+    # final lambda value is computed from Eq. (126) of PRX Quantum 2, 040332 (2021)
     lambda_a = lambda_t_p + lambda_u_1 + lambda_v_1
     lambda_b = (lambda_u_1 + lambda_v_1 / (1 - 1 / eta)) / p_nu_amp
 
