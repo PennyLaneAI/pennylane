80534458a26a43154bbcc8c76a38cab4960f25b7
soranjh <40344468+soranjh@users.noreply.github.com>
2022-06-23 09:44:39 -0400

    Add norm for a first-quantized Hamiltonian (#2720)

--- pennylane/resources/__init__.py
+++ pennylane/resources/__init__.py
@@ -16,6 +16,7 @@ The resources module provides the functionality to estimate the cost of implemen
 quantum algorithms.
 """
 from .factorization import factorize
+from .first_quantization import success_prob, norm_fq
 from .second_quantization import (
     _qrom_cost,
     estimation_cost,
