fbb342d5c789c4c0fe6b316efaa0a980b3e324c7
Sergio Martinez <sergiomtz.losa@gmail.com>
2022-07-06 08:40:01 +0200

    added new fixes 07062022 issue #2780

--- tests/templates/test_subroutines/test_flip_sign.py
+++ tests/templates/test_subroutines/test_flip_sign.py
@@ -103,17 +103,3 @@ class TestFlipSign:
         """Assert error raised when given basis state length is less than number of wires"""
         with pytest.raises(ValueError, match=f"cannot encode {n_status} with {n_wires} wires "):
             op = qml.FlipSign(n_status, wires=n_wires)
-
-    @pytest.mark.parametrize(
-        ("n_status, n_wires"),
-        [
-            ([1, 0, 0], [0, 1]),
-        ],
-    )
-    def test_length_not_match_error(self, n_status, n_wires):
-        """Assert error raised when length of basis state and wires length does not match"""
-        with pytest.raises(
-            ValueError,
-            match="Wires length and flipping state length does not match, they must be equal length ",
-        ):
-            op = qml.FlipSign(n_status, wires=n_wires)
