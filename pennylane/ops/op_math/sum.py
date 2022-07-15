a500772bc34a19939f1767f2c3634fa454e0d6d7
AlbertMitjans <a.mitjanscoma@gmail.com>
2022-07-15 17:46:15 +0100

    style (Sum): :art: Removed unused import.

--- pennylane/ops/op_math/sum.py
+++ pennylane/ops/op_math/sum.py
@@ -16,7 +16,6 @@ This file contains the implementation of the Sum class which contains logic for
 computing the sum of operations.
 """
 from functools import reduce
-from typing import List
 
 import numpy as np
 
