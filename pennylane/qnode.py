8521d5075d878401be541aa1f0f6ed29b20f33fc
Romain Moyard <rmoyard@gmail.com>
2022-07-20 14:48:58 -0400

    Revert autograd

--- pennylane/qnode.py
+++ pennylane/qnode.py
@@ -646,13 +646,13 @@ class QNode:
 
         self._update_original_device()
 
-        if (
-            isinstance(self._qfunc_output, Sequence)
-            or self.device._has_partitioned_shots()
-            or self._qfunc_output.return_type is qml.measurements.Counts
+        if isinstance(self._qfunc_output, Sequence) or (
+            self.tape.is_sampled and self.device._has_partitioned_shots()
         ):
-            # Shot vectors outputs are also sequences
             return res
+        if self._qfunc_output.return_type is qml.measurements.Counts:
+            # return a dictionary with counts not as a single-element array
+            return res[0]
 
         # Squeeze arraylike outputs
         return qml.math.squeeze(res)
