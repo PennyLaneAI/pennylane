2676d5ab01bd39e0d3a1445163b66697853e2d4c
Gian-Luca Anselmetti <gian-lucaanselmetti@Gian-Lucas-MacBook-Pro.local>
2022-07-20 17:42:39 -0700

    Making sign_expand an actual usable function

--- pennylane/transforms/sign_expand.py
+++ pennylane/transforms/sign_expand.py
@@ -18,9 +18,8 @@ Contains the sign (and xi) decomposition tape transform
 import pennylane as qml
 import numpy as np
 import json
+import os
 
-# with open('./verisign_opt_data.json', 'r') as f:
-#     data = json.load(f)
 
 def PauliRot(theta, wires, pauli_word, ancillas):
     r"""Representation of the operator as a product of other operators (static method). :
@@ -56,7 +55,7 @@ def PauliRot(theta, wires, pauli_word, ancillas):
         return []
 
     active_wires, active_gates = zip(
-        ,*[(wire, gate) for wire, gate in zip(wires, pauli_word) if gate != "I"]
+        *[(wire, gate) for wire, gate in zip(wires, pauli_word) if gate != "I"]
     )
 
     ops = []
@@ -66,9 +65,9 @@ def PauliRot(theta, wires, pauli_word, ancillas):
         elif gate == "Y":
             ops.append(qml.RX(np.pi / 2, wires=[wire]))
 
-    qml.CNOT(wires=[ancillas[1],wires[0]])
-    ops.append(MultiCRZ(theta, wires=list(active_wires),control=ancillas[0]))
-    qml.CNOT(wires=[ancillas[1],wires[0]])
+    qml.CNOT(wires=[ancillas[1], wires[0]])
+    ops.append(MultiCRZ(theta, wires=list(active_wires), control=ancillas[0]))
+    qml.CNOT(wires=[ancillas[1], wires[0]])
 
     for wire, gate in zip(active_wires, active_gates):
         if gate == "X":
@@ -77,20 +76,26 @@ def PauliRot(theta, wires, pauli_word, ancillas):
             ops.append(qml.RX(-np.pi / 2, wires=[wire]))
     return ops
 
+
 def MultiCRZ(theta, wires, control, **kwargs):
     ops = [qml.CNOT(wires=(w0, w1)) for w0, w1 in zip(wires[~0:0:-1], wires[~1::-1])]
-    ops.append(qml.CRZ(theta, wires=[control,wires[0]]))
+    ops.append(qml.CRZ(theta, wires=[control, wires[0]]))
     ops += [qml.CNOT(wires=(w0, w1)) for w0, w1 in zip(wires[1:], wires[:~0])]
 
     return ops
 
-def evolve_under(ops,coeffs,time):
-    for op,coeff in zip(ops,coeffs):
-            PauliRot(coeff*time,  wires=op.wires , pauli_word=qml.grouping.pauli_word_to_string(op), ancillas = ['Hadamard','Target'])
+
+def evolve_under(ops, coeffs, time):
+    for op, coeff in zip(ops, coeffs):
+        PauliRot(
+            coeff * time,
+            wires=op.wires,
+            pauli_word=qml.grouping.pauli_word_to_string(op),
+            ancillas=["Hadamard", "Target"],
+        )
 
 
-
-def sign_expand(tape, group=True, circuit = False, J=10, delta=0.0):
+def sign_expand(tape, group=True, circuit=False, J=10, delta=0.0):
     r"""
     Splits a tape measuring a (fast-forwardable) Hamiltonian expectation into mutliple tapes of the Xi or sgn decomposition,
     and provides a function to recombine the results.
@@ -113,18 +118,18 @@ def sign_expand(tape, group=True, circuit = False, J=10, delta=0.0):
 
 
     """
-
-    with open('./verisign_opt_data.json', 'r') as f:
+    path = os.path.dirname(__file__)
+    with open(path + "/sign_expand_data.json", "r") as f:
         data = json.load(f)
-    phis = list(filter(lambda data: data['delta'] == delta and data['order']== J, data))[0]['opt_params']
-
+    phis = list(filter(lambda data: data["delta"] == delta and data["order"] == J, data))[0][
+        "opt_params"
+    ]
 
     hamiltonian = tape.measurements[0].obs
 
     hamiltonian.compute_grouping()
-    if len(hamiltonian_grouping_indices) != 1:
-        raise ValueError(
-            "Passed hamiltonian must be jointly measurable"
+    if len(hamiltonian.grouping_indices) != 1:
+        raise ValueError("Passed hamiltonian must be jointly measurable")
 
     wires = hamiltonian.wires
     mat = qml.utils.sparse_hamiltonian(hamiltonian).toarray()
@@ -133,35 +138,35 @@ def sign_expand(tape, group=True, circuit = False, J=10, delta=0.0):
     norm = eigs[-1]
 
     offset = 0.0
-    dEs, mus, projs, times = [],[],[],[]
+    dEs, mus, projs, times = [], [], [], []
 
-    proj = np.identity(size,dtype='complex64')
-    proj += -2*np.outer(np.conjugate(eigvecs[:,0]),eigvecs[:,0])
+    proj = np.identity(size, dtype="complex64")
+    proj += -2 * np.outer(np.conjugate(eigvecs[:, 0]), eigvecs[:, 0])
     last_i = 1
     offset_add = 0
-    offset_add += (eigs[-1]+eigs[0])/2
+    offset_add += (eigs[-1] + eigs[0]) / 2
 
-    for index in range(len(eigs)-1):
-        dE = (eigs[index+1]-eigs[index])/2
-        if np.isclose(dE,0):
-        continue
+    for index in range(len(eigs) - 1):
+        dE = (eigs[index + 1] - eigs[index]) / 2
+        if np.isclose(dE, 0):
+            continue
         dEs.append(dE)
-        mu = (eigs[index+1]+eigs[index])/2
-        time = np.pi/(2*(norm+abs(mu)))
+        mu = (eigs[index + 1] + eigs[index]) / 2
+        time = np.pi / (2 * (norm + abs(mu)))
         times.append(time)
         mus.append(mu)
 
-        for j in range(last_i,index+1):
-        proj += -2*np.outer(np.conjugate(eigvecs[:,j]),eigvecs[:,j])
-        last_i = index+1
+        for j in range(last_i, index + 1):
+            proj += -2 * np.outer(np.conjugate(eigvecs[:, j]), eigvecs[:, j])
+            last_i = index + 1
 
-        projs.append(proj.copy()*dE)
-
+        projs.append(proj.copy() * dE)
 
     if (
         not isinstance(hamiltonian, qml.Hamiltonian)
         or len(tape.measurements) > 1
-        or tape.measurements[0].return_type not in  [qml.measurements.Expectation, qml.measurements.Variance]
+        or tape.measurements[0].return_type
+        not in [qml.measurements.Expectation, qml.measurements.Variance]
     ):
         raise ValueError(
             "Passed tape must end in `qml.expval(H)` or 'qml.var(H)', where H is of type `qml.Hamiltonian`"
@@ -170,45 +175,46 @@ def sign_expand(tape, group=True, circuit = False, J=10, delta=0.0):
     if circuit:
         coeffs = hamiltonian.data
         tapes = []
-        for mu,time in zip(mus,times):
+        for mu, time in zip(mus, times):
             with tape.__class__() as new_tape:
-                #Put state prep and ansatz on tape in the 'old' register
+                # Put state prep and ansatz on tape in the 'old' register
                 for op in tape.operations:
                     op.queue()
 
-                #Put QSP and Hadamard test on the two ancillas Target and Control
-                qml.Hadamard('Hadamard')
-                for i,phi in enumerate(phis):
-                    qml.CRX(phi,wires=['Hadamard','Target'])
-                    if i == len(phis)-1:
-                        qml.CRY(np.pi,wires=['Hadamard','Target'])
+                # Put QSP and Hadamard test on the two ancillas Target and Control
+                qml.Hadamard("Hadamard")
+                for i, phi in enumerate(phis):
+                    qml.CRX(phi, wires=["Hadamard", "Target"])
+                    if i == len(phis) - 1:
+                        qml.CRY(np.pi, wires=["Hadamard", "Target"])
                     else:
-                        evolve_under(hamiltonian.ops,coeffs,2*time)
-                        qml.CRZ(-2*mu*time,wires=['Hadamard','Target'])
-                qml.Hadamard('Hadamard')
+                        evolve_under(hamiltonian.ops, coeffs, 2 * time)
+                        qml.CRZ(-2 * mu * time, wires=["Hadamard", "Target"])
+                qml.Hadamard("Hadamard")
 
                 if tape.measurements[0].return_type == qml.measurements.Expectation:
-                qml.expval(-1*qml.PauliZ('Hadamard'))
+                    qml.expval(-1 * qml.PauliZ("Hadamard"))
                 else:
-                qml.var(qml.PauliZ('Hadamard'))
+                    qml.var(qml.PauliZ("Hadamard"))
 
             tapes.append(new_tape)
 
         # pylint: disable=function-redefined
         def processing_fn(res):
-            # dot_products = [qml.math.dot(qml.math.squeeze(r), c) for c, r in zip(dEs, res)]
-            products = [a * b for a, b in zip(res, dEs)]
-            # return qml.math.sum(qml.math.stack(dot_products), axis=0)
-            return sum(products)
-        if tape.measurements[0].return_type == qml.measurements.Expectation:
-            def processing_fn(res):
             products = [a * b for a, b in zip(res, dEs)]
             return sum(products)
-        else:
-            def processing_fn(res):
-            products = [a * b for a, b in zip(res, dEs)]
-            return sum(products)*len(products)
 
+        if tape.measurements[0].return_type == qml.measurements.Expectation:
+            # pylint: disable=function-redefined
+            def processing_fn(res):
+                products = [a * b for a, b in zip(res, dEs)]
+                return sum(products)
+
+        else:
+            # pylint: disable=function-redefined
+            def processing_fn(res):
+                products = [a * b for a, b in zip(res, dEs)]
+                return sum(products) * len(products)
 
         return tapes, processing_fn
 
@@ -228,12 +234,13 @@ def sign_expand(tape, group=True, circuit = False, J=10, delta=0.0):
         tapes.append(new_tape)
 
     if tape.measurements[0].return_type == qml.measurements.Expectation:
+        # pylint: disable=function-redefined
         def processing_fn(res):
-        return sum(res)
-    else:
-        def processing_fn(res):
-        return sum(res)*len(res)
+            return sum(res)
 
+    else:
+        # pylint: disable=function-redefined
+        def processing_fn(res):
+            return sum(res) * len(res)
 
     return tapes, processing_fn
-
