52d656d939ccd561d6e5cca0d936a95a7fae66be
Christina Lee <christina@xanadu.ai>
2022-07-26 07:32:06 -0700

    Add Controlled Operation Wrapper (#2634)
    * add controlled operation wrapper
    
    * dont inherit for existing ops for this pr
    
    * something
    
    * improve matrix method
    
    * [skip ci] tests
    
    * [skip ci] some tests and fixing
    
    * [skip ci] add inverse information
    
    * tests, sparse_matrix, decomposition
    
    * tests, parameter-frequencies
    
    * [skip ci] imports
    
    * testing and polishing
    
    * tests and polishing
    
    * testing
    
    * changelog
    
    * add pylint suppression
    
    * final testing
    
    * change __new__ behavior
    
    * respond to review comments
    
    * use symbolicop
    
    * improved docs
    
    * fix typo
    
    * add additional test
    
    * Update pennylane/ops/op_math/controlled_class.py
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Albert Mitjans <a.mitjanscoma@gmail.com>
    
    * typo
    
    * switch to value errors, drawer support
    
    * oops
    
    * work wires in matrix
    
    * Update tests/drawer/test_tape_mpl.py
    
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>
    
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    Co-authored-by: Albert Mitjans <a.mitjanscoma@gmail.com>

--- pennylane/drawer/tape_mpl.py
+++ pennylane/drawer/tape_mpl.py
@@ -285,8 +285,9 @@ def tape_mpl(tape, wire_order=None, show_all_wires=False, decimals=None, **kwarg
                 control_values = op.hyperparameters.get("control_values", None)
 
                 if control_values is None:
-                    control_values = "1" * len(control_wires)
-                control_values = [(i == "1") for i in control_values]
+                    control_values = [True for _ in control_wires]
+                elif isinstance(control_values[0], str):
+                    control_values = [(i == "1") for i in control_values]
 
                 if len(control_wires) != 0:
                     drawer.ctrl(
