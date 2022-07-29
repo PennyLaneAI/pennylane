c4cd3a1d661a37fbbd7d2d4c0fd7f39956a9adf8
AlbertMitjans <a.mitjanscoma@gmail.com>
2022-07-29 10:33:40 +0200

    feat (counts): :sparkles: Add qml.counts.

--- pennylane/measurements.py
+++ pennylane/measurements.py
@@ -19,8 +19,8 @@ and measurement samples using AnnotatedQueues.
 """
 # pylint: disable=too-many-instance-attributes
 import copy
-import uuid
 import functools
+import uuid
 from enum import Enum
 from typing import Generic, TypeVar
 
@@ -583,20 +583,19 @@ def var(op):
     return MeasurementProcess(Variance, obs=op)
 
 
-def sample(op=None, wires=None, counts=False):
+def sample(op=None, wires=None):
     r"""Sample from the supplied observable, with the number of shots
     determined from the ``dev.shots`` attribute of the corresponding device,
-    returning raw samples (counts=False) or the number of counts for each sample (counts=True).
-    If no observable is provided then basis state samples are returned directly
-    from the device.
+    returning raw samples. If no observable is provided then basis state samples are returned
+    directly from the device.
 
     Note that the output shape of this measurement process depends on the shots
     specified on the device.
 
     Args:
         op (Observable or None): a quantum observable object
-        wires (Sequence[int] or int or None): the wires we wish to sample from, ONLY set wires if op is None
-        counts (bool): return the result as number of counts for each sample
+        wires (Sequence[int] or int or None): the wires we wish to sample from, ONLY set wires if
+        op is None
 
     Raises:
         QuantumFunctionError: `op` is not an instance of :class:`~.Observable`
@@ -649,25 +648,94 @@ def sample(op=None, wires=None, counts=False):
            [1, 1],
            [0, 0]])
 
-    If specified counts=True, the function returns number of counts for each sample,
-    both for observables eigenvalues or the system eigenstates.
+    .. note::
+
+        QNodes that return samples cannot, in general, be differentiated, since the derivative
+        with respect to a sample --- a stochastic process --- is ill-defined. The one exception
+        is if the QNode uses the parameter-shift method (``diff_method="parameter-shift"``), in
+        which case ``qml.sample(obs)`` is interpreted as a single-shot expectation value of the
+        observable ``obs``.
+    """
+    if op is not None and not op.is_hermitian:  # None type is also allowed for op
+        raise qml.QuantumFunctionError(
+            f"{op.name} is not an observable: cannot be used with sample"
+        )
+
+    if isinstance(op, (qml.ops.Sum, qml.ops.SProd)):  # pylint: disable=no-member
+        raise qml.QuantumFunctionError("Symbolic Operations are not supported for sampling yet.")
+
+    if wires is not None:
+        if op is not None:
+            raise ValueError(
+                "Cannot specify the wires to sample if an observable is "
+                "provided. The wires to sample will be determined directly from the observable."
+            )
+        wires = qml.wires.Wires(wires)
+
+    return MeasurementProcess(Sample, obs=op, wires=wires)
+
+
+def counts(op=None, wires=None):
+    r"""Sample from the supplied observable, with the number of shots
+    determined from the ``dev.shots`` attribute of the corresponding device,
+    returning the number of counts for each sample. If no observable is provided then basis state
+    samples are returned directly from the device.
+
+    Note that the output shape of this measurement process depends on the shots
+    specified on the device.
+
+    Args:
+        op (Observable or None): a quantum observable object
+        wires (Sequence[int] or int or None): the wires we wish to sample from, ONLY set wires if
+        op is None
+
+    Raises:
+        QuantumFunctionError: `op` is not an instance of :class:`~.Observable`
+        ValueError: Cannot set wires if an observable is provided
+
+    The samples are drawn from the eigenvalues :math:`\{\lambda_i\}` of the observable.
+    The probability of drawing eigenvalue :math:`\lambda_i` is given by
+    :math:`p(\lambda_i) = |\langle \xi_i | \psi \rangle|^2`, where :math:`| \xi_i \rangle`
+    is the corresponding basis state from the observable's eigenbasis.
+
+    **Example**
 
     .. code-block:: python3
 
-        dev = qml.device("default.qubit", wires=3, shots=10)
-
+        dev = qml.device("default.qubit", wires=2, shots=4)
 
         @qml.qnode(dev)
-        def my_circ():
-            qml.Hadamard(wires=0)
+        def circuit(x):
+            qml.RX(x, wires=0)
+            qml.Hadamard(wires=1)
             qml.CNOT(wires=[0, 1])
-            qml.PauliX(wires=2)
-            return qml.sample(qml.PauliZ(0), counts=True), qml.sample(counts=True)
+            return qml.counts(qml.PauliY(0))
 
     Executing this QNode:
 
-    >>> my_circ()
-    ({-1: 3, 1: 7}, {'001': 7, '111': 3})
+    >>> circuit(0.5)
+    {-1: 2, 1: 2}
+
+    If no observable is provided, then the raw basis state samples obtained
+    from device are returned (e.g., for a qubit device, samples from the
+    computational device are returned). In this case, ``wires`` can be specified
+    so that sample results only include measurement results of the qubits of interest.
+
+    .. code-block:: python3
+
+        dev = qml.device("default.qubit", wires=2, shots=4)
+
+        @qml.qnode(dev)
+        def circuit(x):
+            qml.RX(x, wires=0)
+            qml.Hadamard(wires=1)
+            qml.CNOT(wires=[0, 1])
+            return qml.counts()
+
+    Executing this QNode:
+
+    >>> circuit(0.5)
+    {'00': 3, '01': 1}
 
     .. note::
 
@@ -685,17 +753,15 @@ def sample(op=None, wires=None, counts=False):
     if isinstance(op, (qml.ops.Sum, qml.ops.SProd)):  # pylint: disable=no-member
         raise qml.QuantumFunctionError("Symbolic Operations are not supported for sampling yet.")
 
-    sample_or_counts = Counts if counts else Sample
-
     if wires is not None:
         if op is not None:
             raise ValueError(
                 "Cannot specify the wires to sample if an observable is "
                 "provided. The wires to sample will be determined directly from the observable."
             )
-        return MeasurementProcess(sample_or_counts, obs=op, wires=qml.wires.Wires(wires))
+        wires = qml.wires.Wires(wires)
 
-    return MeasurementProcess(sample_or_counts, obs=op)
+    return MeasurementProcess(Counts, obs=op, wires=wires)
 
 
 def probs(wires=None, op=None):
