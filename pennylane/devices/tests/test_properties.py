c1ddbda6749616f7c5f85e64ba3d95d26bdac894
dwierichs <davidwierichs@gmail.com>
2022-08-03 09:40:34 +0200

    compatibility with qulacs and braket.loca

--- pennylane/devices/tests/test_properties.py
+++ pennylane/devices/tests/test_properties.py
@@ -269,7 +269,7 @@ class TestCapabilities:
     def test_supports_broadcasting(self, device_kwargs, mocker):
         """Tests that the device reports correctly whether it supports parameter broadcasting
         and that it can execute broadcasted tapes in any case."""
-        # pylint: disable=protected-access
+
         device_kwargs["wires"] = 1
         dev = qml.device(**device_kwargs)
         cap = dev.capabilities()
@@ -286,18 +286,21 @@ class TestCapabilities:
 
         spy = mocker.spy(qml.transforms, "broadcast_expand")
         circuit(0.5)
-        orig_shape = dev._state.shape
+        if cap.get("returns_state"):
+            orig_shape = pnp.array(dev.access_state()).shape
         spy.assert_not_called()
         x = pnp.array([0.5, 2.1, -0.6], requires_grad=True)
 
         if cap["supports_broadcasting"]:
             res = circuit(x)
             spy.assert_not_called()
-            assert dev._state.shape != orig_shape
+            if cap.get("returns_state"):
+                assert pnp.array(dev.access_state()).shape != orig_shape
         else:
             res = circuit(x)
             spy.assert_called()
-            assert dev._state.shape == orig_shape
+            if cap.get("returns_state"):
+                assert pnp.array(dev.access_state()).shape == orig_shape
 
         assert pnp.ndim(res) == 2
         assert res.shape[0] == 3
