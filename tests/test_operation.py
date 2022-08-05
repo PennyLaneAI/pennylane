2bcb86104fc16548e794142b4009a8babff1ce01
Albert Mitjans <a.mitjanscoma@gmail.com>
2022-08-05 15:17:23 +0100

    Add __mul__ dunder method (#2891)
    * feat (Operator): :sparkles: __mul__ dunder method.
    
    * test (Operator): :test_tube: Compare operators.
    
    * test (Operator): :test_tube: Add __mul__ test.
    
    * chore (changelog): :pencil2: Add feature to changelog.
    
    * test (Operator): :test_tube: Add test.
    
    * test (Operator): :test_tube: Add test.
    
    * chore (changelog): :pencil2: Add feature to changelog.
    
    * refactor (Operation): :recycle: Remove dead line.
    
    * docs: :memo: Fix docs.
    
    * refactor (Operation): :recycle: Remove __rmul__.
    
    * refactor (Hamiltonian): :recycle: Remove super call.
    
    * feat (Operator): :sparkles: __matmul__ dunder method.
    
    * chore (changelog): :pencil2: Modify changelog.
    
    * test (Observable): :test_tube: Remove test.
    
    * Update doc/releases/changelog-dev.md
    
    Co-authored-by: Christina Lee <christina@xanadu.ai>
    
    * fix (Operator): :bug: Use only one wire with Identity.
    
    * test (Operator): :test_tube: Increase coverage.
    
    * test (Operator): :test_tube: Increase coverage.
    
    * Update pennylane/operation.py
    
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>
    
    * Update pennylane/operation.py
    
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>
    
    * Update tests/test_operation.py
    
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>
    
    * test (Operator): :test_tube: Fix test.
    
    * test (Operator): :test_tube: Support add for multiple wires.
    
    * test (Operator): :test_tube: Increase coverage.
    
    * feat (s_prod): :sparkles: Add to init.
    
    * Update pennylane/operation.py
    
    Co-authored-by: Christina Lee <christina@xanadu.ai>
    
    * Update pennylane/operation.py
    
    Co-authored-by: Christina Lee <christina@xanadu.ai>
    
    * fix: :bug: Fix small bug.
    
    * test (Operator): :test_tube: Add test.
    
    * test (Operator): :test_tube: Add test.
    
    * test (Operator): :test_tube: Add test.
    
    * test (Operator): :test_tube: Add test.
    
    * refactor (Operator): :recycle: Use wrappers.
    
    * refactor (Operator): :recycle: Use wrappers.
    
    * Update pennylane/operation.py
    
    Co-authored-by: Christina Lee <christina@xanadu.ai>
    
    Co-authored-by: Christina Lee <christina@xanadu.ai>
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -777,14 +777,46 @@ class TestOperatorIntegration:
         with pytest.raises(ValueError, match="Cannot raise an Operator"):
             _ = DummyOp(wires=[0]) ** DummyOp(wires=[0])
 
+    def test_sum_with_operator(self):
+        """Test the __sum__ dunder method with two operators."""
+        sum_op = qml.PauliX(0) + qml.RX(1, 0)
+        final_op = qml.op_sum(qml.PauliX(0), qml.RX(1, 0))
+        #  TODO: Use qml.equal when fixed.
+        assert isinstance(sum_op, qml.ops.Sum)
+        for s1, s2 in zip(sum_op.summands, final_op.summands):
+            assert s1.name == s2.name
+            assert s1.wires == s2.wires
+            assert s1.data == s2.data
+        assert np.allclose(a=sum_op.matrix(), b=final_op.matrix(), rtol=0)
+
     def test_sum_with_scalar(self):
         """Test the __sum__ dunder method with a scalar value."""
-        sum_op = 5 + qml.PauliX(0)
-        final_op = qml.ops.Sum(qml.ops.s_prod(5, qml.Identity(0)), qml.PauliX(0))
+        sum_op = 5 + qml.PauliX(0) + 0
+        final_op = qml.op_sum(qml.PauliX(0), qml.s_prod(5, qml.Identity(0)))
         # TODO: Use qml.equal when fixed.
-        assert np.allclose(sum_op.matrix(), final_op.matrix(), rtol=0)
+        assert isinstance(sum_op, qml.ops.Sum)
+        for s1, s2 in zip(sum_op.summands, final_op.summands):
+            assert s1.name == s2.name
+            assert s1.wires == s2.wires
+            assert s1.data == s2.data
+        assert np.allclose(a=sum_op.matrix(), b=final_op.matrix(), rtol=0)
 
-    def test_dunder_methods(self):
+    def test_sum_multi_wire_operator_with_scalar(self):
+        """Test the __sum__ dunder method with a multi-wire operator and a scalar value."""
+        sum_op = 5 + qml.CNOT(wires=[0, 1])
+        final_op = qml.op_sum(
+            qml.CNOT(wires=[0, 1]),
+            qml.s_prod(5, qml.prod(qml.Identity(0), qml.Identity(1))),
+        )
+        # TODO: Use qml.equal when fixed.
+        assert isinstance(sum_op, qml.ops.Sum)
+        for s1, s2 in zip(sum_op.summands, final_op.summands):
+            assert s1.name == s2.name
+            assert s1.wires == s2.wires
+            assert s1.data == s2.data
+        assert np.allclose(a=sum_op.matrix(), b=final_op.matrix(), rtol=0)
+
+    def test_sub_rsub_and_neg_dunder_methods(self):
         """Test the __sub__, __rsub__ and __neg__ dunder methods."""
         sum_op = qml.PauliX(0) - 5
         sum_op_2 = -(5 - qml.PauliX(0))
@@ -792,6 +824,41 @@ class TestOperatorIntegration:
         assert np.allclose(a=sum_op.matrix(), b=sum_op_2.matrix(), rtol=0)
         neg_op = -qml.PauliX(0)
         assert np.allclose(a=neg_op.matrix(), b=np.array([[0, -1], [-1, 0]]), rtol=0)
+
+    def test_mul_with_scalar(self):
+        """Test the __mul__ dunder method with a scalar value."""
+        sprod_op = 4 * qml.RX(1, 0)
+        sprod_op2 = qml.RX(1, 0) * 4
+        final_op = qml.s_prod(scalar=4, operator=qml.RX(1, 0))
+        assert isinstance(sprod_op, qml.ops.SProd)
+        assert sprod_op.name == sprod_op2.name
+        assert sprod_op.wires == sprod_op2.wires
+        assert sprod_op.data == sprod_op2.data
+        assert sprod_op.name == final_op.name
+        assert sprod_op.wires == final_op.wires
+        assert sprod_op.data == final_op.data
+        assert np.allclose(sprod_op.matrix(), sprod_op2.matrix(), rtol=0)
+        assert np.allclose(sprod_op.matrix(), final_op.matrix(), rtol=0)
+
+    def test_mul_with_operator(self):
+        """Test the __matmul__ dunder method with an operator."""
+        prod_op = qml.RX(1, 0) @ qml.PauliX(0)
+        final_op = qml.prod(qml.RX(1, 0), qml.PauliX(0))
+        assert isinstance(prod_op, qml.ops.Prod)
+        assert prod_op.name == final_op.name
+        assert prod_op.wires == final_op.wires
+        assert prod_op.data == final_op.data
+        assert np.allclose(prod_op.matrix(), final_op.matrix(), rtol=0)
+
+    def test_mul_with_not_supported_object_raises_error(self):
+        """Test that the __mul__ dunder method raises an error when using a non-supported object."""
+        with pytest.raises(ValueError, match="Cannot multiply Observable by"):
+            _ = "dummy" * qml.PauliX(0)
+
+    def test_matmul_with_not_supported_object_raises_error(self):
+        """Test that the __matmul__ dunder method raises an error when using a non-supported object."""
+        with pytest.raises(ValueError, match="Can only perform tensor products between operators."):
+            _ = qml.PauliX(0) @ "dummy"
 
 
 class TestInverse:
@@ -1137,11 +1204,6 @@ class TestTensor:
         with pytest.raises(
             ValueError, match="Can only perform tensor products between observables"
         ):
-            X @ Y
-
-        with pytest.raises(
-            ValueError, match="Can only perform tensor products between observables"
-        ):
             T = X @ Z
             T @ Y
 
@@ -1149,7 +1211,7 @@ class TestTensor:
             ValueError, match="Can only perform tensor products between observables"
         ):
             T = X @ Z
-            Y @ T
+            4 @ T
 
     def test_eigvals(self):
         """Test that the correct eigenvalues are returned for the Tensor"""
