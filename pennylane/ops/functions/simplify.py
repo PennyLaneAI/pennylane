1f13de249292215a08d4f24183dff87a99a26fa3
albi3ro <chrissie.c.l@gmail.com>
2022-08-08 15:53:00 -0400

    move rotation simplify into op method

--- pennylane/ops/functions/simplify.py
+++ pennylane/ops/functions/simplify.py
@@ -22,7 +22,8 @@ from pennylane.operation import Operator
 
 @qml.op_transform
 def simplify(op: Operator):
-    """Reduces the depth of nested operators.
+    """Simplifies the operator by reducing arithmetic depth or number of rotation
+    parameters.
 
 
     Args:
@@ -47,6 +48,12 @@ def simplify(op: Operator):
     (Adjoint(RX)(0.54, wires=[0]),
     Adjoint(PauliX)(wires=[0]),
     Adjoint(PauliZ)(wires=[1]))
+
+    This function also can simplify the number of rotation gate parameters:
+
+    >>> qml.simplify(qml.Rot(np.pi / 2, 0.1, -np.pi / 2, wires=0))
+    RX(0.1, wires=[0])
+
     """
     if qml.queuing.QueuingContext.recording():
         with qml.tape.stop_recording():
