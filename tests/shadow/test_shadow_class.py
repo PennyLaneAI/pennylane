6f72518bb9a4b7f7a2e818c40cef39a457cd7fed
Qottmann <korbinian.kottmann@gmail.com>
2022-08-11 10:42:32 -0400

    black

--- tests/shadow/test_shadow_class.py
+++ tests/shadow/test_shadow_class.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 """Unit tests for the classical shadows class"""
-#pylint:disable=no-self-use, import-outside-toplevel, redefined-outer-name, unpacking-non-sequence, too-few-public-methods, not-an-iterable, inconsistent-return-statements
+# pylint:disable=no-self-use, import-outside-toplevel, redefined-outer-name, unpacking-non-sequence, too-few-public-methods, not-an-iterable, inconsistent-return-statements
 
 import pytest
 
