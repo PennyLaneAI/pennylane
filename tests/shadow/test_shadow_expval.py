1fd4ce25044455eafadab61d06c421cc9ba95fc3
eddddddy <eddy3.14159@gmail.com>
2022-08-11 12:05:06 -0400

    Add gradient tests

--- tests/shadow/test_shadow_expval.py
+++ tests/shadow/test_shadow_expval.py
@@ -60,6 +60,28 @@ def qft_circuit(wires, shots=10000, interface="autograd"):
     return circuit
 
 
+def strongly_entangling_circuit(wires, shots=10000, interface="autograd"):
+    dev = qml.device("default.qubit", wires=wires, shots=shots)
+
+    @qml.qnode(dev, interface=interface)
+    def circuit(x, obs, k):
+        qml.StronglyEntanglingLayers(weights=x, wires=range(wires))
+        return qml.classical_shadow_expval(obs, k=k)
+
+    return circuit
+
+
+def strongly_entangling_circuit_exact(wires, interface="autograd"):
+    dev = qml.device("default.qubit", wires=wires)
+
+    @qml.qnode(dev, interface=interface)
+    def circuit(x, obs):
+        qml.StronglyEntanglingLayers(weights=x, wires=range(wires))
+        return qml.expval(obs)
+
+    return circuit
+
+
 @pytest.mark.autograd
 class TestExpvalMeasurement:
     @pytest.mark.parametrize("wires", [1, 2, 3])
@@ -220,3 +242,149 @@ class TestExpvalForward:
         msg = "Observable must be a linear combination of Pauli observables"
         with pytest.raises(ValueError, match=msg):
             circuit(qml.Hadamard(0) @ qml.Hadamard(2))
+
+
+class TestExpvalBackward:
+    """Test the classical_shadow_expval measurement process backward pass"""
+
+    @pytest.mark.autograd
+    @pytest.mark.parametrize(
+        "obs",
+        [
+            (qml.PauliX(1)),
+            (qml.PauliX(0) @ qml.PauliX(2)),
+            (qml.PauliX(0) @ qml.Identity(1) @ qml.PauliX(2)),
+            (qml.PauliY(2)),
+            (qml.PauliY(1) @ qml.PauliZ(2)),
+            (qml.PauliX(0) @ qml.PauliY(1)),
+            (qml.PauliX(0) @ qml.PauliY(1) @ qml.Identity(2)),
+        ],
+    )
+    def test_backward_autograd(self, obs):
+        """Test that the gradient of the expval estimation is correct for
+        the autograd interface"""
+        shadow_circuit = strongly_entangling_circuit(3, shots=20000, interface="autograd")
+        exact_circuit = strongly_entangling_circuit_exact(3, "autograd")
+
+        # make rotations close to pi / 2 to ensure gradients are not too small
+        x = np.random.uniform(
+            0.8, 2, size=qml.StronglyEntanglingLayers.shape(n_layers=2, n_wires=3)
+        )
+
+        actual = qml.grad(shadow_circuit)(x, obs, k=10)
+        expected = qml.grad(exact_circuit)(x, obs)
+
+        assert qml.math.allclose(actual, expected, atol=1e-1)
+
+    @pytest.mark.jax
+    @pytest.mark.parametrize(
+        "obs",
+        [
+            (qml.PauliX(1)),
+            (qml.PauliX(0) @ qml.PauliX(2)),
+            (qml.PauliX(0) @ qml.Identity(1) @ qml.PauliX(2)),
+            (qml.PauliY(2)),
+            (qml.PauliY(1) @ qml.PauliZ(2)),
+            (qml.PauliX(0) @ qml.PauliY(1)),
+            (qml.PauliX(0) @ qml.PauliY(1) @ qml.Identity(2)),
+        ],
+    )
+    def test_backward_jax(self, obs):
+        """Test that the gradient of the expval estimation is correct for
+        the jax interface"""
+        import jax
+        from jax import numpy as jnp
+
+        shadow_circuit = strongly_entangling_circuit(3, shots=20000, interface="jax")
+        exact_circuit = strongly_entangling_circuit_exact(3, "jax")
+
+        # make rotations close to pi / 2 to ensure gradients are not too small
+        x = jnp.array(
+            np.random.uniform(
+                0.8, 2, size=qml.StronglyEntanglingLayers.shape(n_layers=2, n_wires=3)
+            )
+        )
+
+        actual = jax.grad(shadow_circuit)(x, obs, k=10)
+        expected = jax.grad(exact_circuit)(x, obs)
+
+        assert qml.math.allclose(actual, expected, atol=1e-1)
+
+    @pytest.mark.tf
+    @pytest.mark.parametrize(
+        "obs",
+        [
+            (qml.PauliX(1)),
+            (qml.PauliX(0) @ qml.PauliX(2)),
+            (qml.PauliX(0) @ qml.Identity(1) @ qml.PauliX(2)),
+            (qml.PauliY(2)),
+            (qml.PauliY(1) @ qml.PauliZ(2)),
+            (qml.PauliX(0) @ qml.PauliY(1)),
+            (qml.PauliX(0) @ qml.PauliY(1) @ qml.Identity(2)),
+        ],
+    )
+    def test_backward_tf(self, obs):
+        """Test that the gradient of the expval estimation is correct for
+        the tensorflow interface"""
+        import tensorflow as tf
+
+        shadow_circuit = strongly_entangling_circuit(3, shots=20000, interface="tf")
+        exact_circuit = strongly_entangling_circuit_exact(3, "tf")
+
+        # make rotations close to pi / 2 to ensure gradients are not too small
+        x = tf.Variable(
+            np.random.uniform(
+                0.8, 2, size=qml.StronglyEntanglingLayers.shape(n_layers=2, n_wires=3)
+            )
+        )
+
+        with tf.GradientTape() as tape:
+            out = shadow_circuit(x, obs, k=10)
+
+        actual = tape.gradient(out, x)
+
+        with tf.GradientTape() as tape2:
+            out2 = exact_circuit(x, obs)
+
+        expected = tape2.gradient(out2, x)
+
+        assert qml.math.allclose(actual, expected, atol=1e-1)
+
+    @pytest.mark.torch
+    @pytest.mark.parametrize(
+        "obs",
+        [
+            (qml.PauliX(1)),
+            (qml.PauliX(0) @ qml.PauliX(2)),
+            (qml.PauliX(0) @ qml.Identity(1) @ qml.PauliX(2)),
+            (qml.PauliY(2)),
+            (qml.PauliY(1) @ qml.PauliZ(2)),
+            (qml.PauliX(0) @ qml.PauliY(1)),
+            (qml.PauliX(0) @ qml.PauliY(1) @ qml.Identity(2)),
+        ],
+    )
+    def test_backward_torch(self, obs):
+        """Test that the gradient of the expval estimation is correct for
+        the pytorch interface"""
+        import torch
+
+        shadow_circuit = strongly_entangling_circuit(3, shots=20000, interface="torch")
+        exact_circuit = strongly_entangling_circuit_exact(3, "torch")
+
+        # make rotations close to pi / 2 to ensure gradients are not too small
+        x = torch.tensor(
+            np.random.uniform(
+                0.8, 2, size=qml.StronglyEntanglingLayers.shape(n_layers=2, n_wires=3)
+            ),
+            requires_grad=True,
+        )
+
+        out = shadow_circuit(x, obs, k=10)
+        out.backward()
+        actual = x.grad
+
+        out2 = exact_circuit(x, obs)
+        out2.backward()
+        expected = x.grad
+
+        assert qml.math.allclose(actual, expected, atol=1e-1)
