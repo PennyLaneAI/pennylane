1569f41f812fcb4854a46d81b3889186ea9843ce
Qottmann <korbinian.kottmann@gmail.com>
2022-08-12 09:44:43 -0400

    add break in doc string

--- pennylane/shadows/classical_shadow.py
+++ pennylane/shadows/classical_shadow.py
@@ -102,6 +102,7 @@ class ClassicalShadow:
         Args:
             wires (Iterable[int]): The wires over which to compute the snapshots. For ``wires=None``(default) all ``n`` qubits are used.
             snapshots (Iterable[int] or int): Only compute a subset of local snapshots. For ``snapshots=None``(default), all local snapshots are taken. In case of an integer, a random subset of that size is taken. The subset can also be explicitly fixed by passing an Iterable with the corresponding indices.
+
         """
         bits = self.bits
         recipes = self.recipes
