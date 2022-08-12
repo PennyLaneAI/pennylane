8f2245618407d43b36800931870b293b410bd89b
eddddddy <eddy3.14159@gmail.com>
2022-08-12 16:32:19 -0400

    flaky

--- tests/optimize/test_optimize_shot_adapative.py
+++ tests/optimize/test_optimize_shot_adapative.py
@@ -542,6 +542,7 @@ class TestOptimization:
         assert np.allclose(circuit(params), -1, atol=0.1, rtol=0.2)
         assert opt.shots_used > min_shots
 
+    @flaky(max_runs=5)
     @pytest.mark.slow
     def test_vqe_optimization(self):
         """Test that a simple VQE circuit can be optimized"""
