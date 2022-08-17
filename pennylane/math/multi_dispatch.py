ae061181de5a487ba9d1fc4d2a8e80223810e609
antalszava <antalszava@gmail.com>
2022-08-17 13:52:23 -0400

    lint

--- pennylane/math/multi_dispatch.py
+++ pennylane/math/multi_dispatch.py
@@ -454,7 +454,8 @@ def get_trainable_indices(values, like=None):
 
         if not any(isinstance(v, jax.core.Tracer) for v in values):
             # No JAX tracing is occuring; treat all `DeviceArray` objects as trainable.
-            trainable = lambda p, **kwargs: isinstance(p, jax.numpy.DeviceArray)
+            def trainable(p, **kwargs):
+                return isinstance(p, jax.numpy.DeviceArray)
         else:
             # JAX tracing is occuring; use the default behaviour (only traced arrays
             # are treated as trainable). This is required to ensure that `jax.grad(func, argnums=...)
