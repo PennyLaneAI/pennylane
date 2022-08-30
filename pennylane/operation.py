bc15724e00965ddcf910308ed6434db6cb4f02dd
Jay Soni <jbsoni@uwaterloo.ca>
2022-08-30 13:42:52 -0400

    pylint

--- pennylane/operation.py
+++ pennylane/operation.py
@@ -104,7 +104,7 @@ from typing import List
 
 import numpy as np
 from numpy.linalg import multi_dot
-from scipy.sparse import coo_matrix, eye, kron, csr_matrix, issparse
+from scipy.sparse import coo_matrix, eye, kron
 
 import pennylane as qml
 from pennylane.wires import Wires
