4154180ec4072c21128750e8aca2636470c14f31
albi3ro <chrissie.c.l@gmail.com>
2022-09-07 17:32:47 -0400

    op.hash considers global phases

--- pennylane/operation.py
+++ pennylane/operation.py
@@ -252,10 +252,8 @@ def _process_data(op):
     # Use qml.math.real to take the real part. We may get complex inputs for
     # example when differentiating holomorphic functions with JAX: a complex
     # valued QNode (one that returns qml.state) requires complex typed inputs.
-    if op.name in ("RX", "RY", "RZ", "PhaseShift", "Rot"):
-        return str([qml.math.round(qml.math.real(d) % (2 * np.pi), 10) for d in op.data])
 
-    if op.name in ("CRX", "CRY", "CRZ", "CRot"):
+    if op.name in ("CRX", "CRY", "CRZ", "CRot", "RX", "RY", "RZ", "PhaseShift", "Rot"):
         return str([qml.math.round(qml.math.real(d) % (4 * np.pi), 10) for d in op.data])
 
     return str(op.data)
