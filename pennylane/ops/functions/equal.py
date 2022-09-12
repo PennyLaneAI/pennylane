5a7adaa297993114a5c7efdfb60dbefafc04106c
AlbertMitjans <a.mitjanscoma@gmail.com>
2022-09-12 10:04:06 +0200

    use arithmetic depth check

--- pennylane/ops/functions/equal.py
+++ pennylane/ops/functions/equal.py
@@ -17,7 +17,6 @@ This module contains the qml.equal function.
 # pylint: disable=too-many-arguments,too-many-return-statements
 import pennylane as qml
 from pennylane.operation import Operator
-from pennylane.ops.op_math import Adjoint, Controlled, Exp, Pow, Prod, SProd, Sum
 
 
 def equal(
@@ -73,7 +72,7 @@ def equal(
         >>> qml.equal(op3, op4, check_trainability=False)
         True
     """
-    if isinstance(op1, (Adjoint, Controlled, Exp, Pow, Prod, SProd, Sum)):
+    if op1.arithmetic_depth > 0 or op2.arithmetic_depth > 0:
         return op1.hash == op2.hash
     if op1.__class__ is not op2.__class__ or op1.arithmetic_depth != op2.arithmetic_depth:
         return False
