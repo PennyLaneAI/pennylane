1eca61fca403a74b9679be9db057f239423d5f8f
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2022-09-15 15:35:56 -0400

    1 line

--- tests/drawer/test_tape_mpl.py
+++ tests/drawer/test_tape_mpl.py
@@ -351,7 +351,7 @@ class TestSpecialGates:
         _, ax = tape_mpl(tape)
         layer = 0
 
-        assert len(ax.lines) == 2
+        assert len(ax.lines) == 1
         assert len(ax.collections) == 2
 
         plt.close()
