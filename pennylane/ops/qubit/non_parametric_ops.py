d5e414b94dcf40a51991d71a45edbe7c7946ce91
alvaro-at-xanadu <alvaro@xanadu.ai>
2022-09-26 10:25:04 -0400

    Added patata

--- pennylane/ops/qubit/non_parametric_ops.py
+++ pennylane/ops/qubit/non_parametric_ops.py
@@ -171,6 +171,143 @@ class Hadamard(Observable, Operation):
     def pow(self, z):
         return super().pow(z % 2)
 
+class Patata(Observable, Operation):
+    r"""Hadamard(wires)
+    The Hadamard operator
+
+    .. math:: H = \frac{1}{\sqrt{2}}\begin{bmatrix} 1 & 1\\ 1 & -1\end{bmatrix}.
+
+    **Details:**
+
+    * Number of wires: 1
+    * Number of parameters: 0
+
+    Args:
+        wires (Sequence[int] or int): the wire the operation acts on
+    """
+    num_wires = 1
+    """int: Number of wires that the operator acts on."""
+
+    num_params = 0
+    """int: Number of trainable parameters that the operator depends on."""
+
+    _queue_category = "_ops"
+
+    def label(self, decimals=None, base_label=None, cache=None):
+        return base_label or "H"
+
+    @staticmethod
+    def compute_matrix():  # pylint: disable=arguments-differ
+        r"""Representation of the operator as a canonical matrix in the computational basis (static method).
+
+        The canonical matrix is the textbook matrix representation that does not consider wires.
+        Implicitly, this assumes that the wires of the operator correspond to the global wire order.
+
+        .. seealso:: :meth:`~.Hadamard.matrix`
+
+        Returns:
+            ndarray: matrix
+
+        **Example**
+
+        >>> print(qml.Hadamard.compute_matrix())
+        [[ 0.70710678  0.70710678]
+         [ 0.70710678 -0.70710678]]
+        """
+        return np.array([[INV_SQRT2, INV_SQRT2], [INV_SQRT2, -INV_SQRT2]])
+
+    @staticmethod
+    def compute_sparse_matrix(*params, **hyperparams):
+        return sparse.csr_matrix([[INV_SQRT2, INV_SQRT2], [INV_SQRT2, -INV_SQRT2]])
+
+    @staticmethod
+    def compute_eigvals():  # pylint: disable=arguments-differ
+        r"""Eigenvalues of the operator in the computational basis (static method).
+
+        If :attr:`diagonalizing_gates` are specified and implement a unitary :math:`U`,
+        the operator can be reconstructed as
+
+        .. math:: O = U \Sigma U^{\dagger},
+
+        where :math:`\Sigma` is the diagonal matrix containing the eigenvalues.
+
+        Otherwise, no particular order for the eigenvalues is guaranteed.
+
+        .. seealso:: :meth:`~.Hadamard.eigvals`
+
+        Returns:
+            array: eigenvalues
+
+        **Example**
+
+        >>> print(qml.Hadamard.compute_eigvals())
+        [ 1 -1]
+        """
+        return pauli_eigs(1)
+
+    @staticmethod
+    def compute_diagonalizing_gates(wires):
+        r"""Sequence of gates that diagonalize the operator in the computational basis (static method).
+
+        Given the eigendecomposition :math:`O = U \Sigma U^{\dagger}` where
+        :math:`\Sigma` is a diagonal matrix containing the eigenvalues,
+        the sequence of diagonalizing gates implements the unitary :math:`U^{\dagger}`.
+
+        The diagonalizing gates rotate the state into the eigenbasis
+        of the operator.
+
+        .. seealso:: :meth:`~.Hadamard.diagonalizing_gates`.
+
+        Args:
+            wires (Iterable[Any], Wires): wires that the operator acts on
+        Returns:
+            list[.Operator]: list of diagonalizing gates
+
+        **Example**
+
+        >>> print(qml.Hadamard.compute_diagonalizing_gates(wires=[0]))
+        [RY(-0.7853981633974483, wires=[0])]
+        """
+        return [qml.RY(-np.pi / 4, wires=wires)]
+
+    @staticmethod
+    def compute_decomposition(wires):
+        r"""Representation of the operator as a product of other operators (static method).
+
+        .. math:: O = O_1 O_2 \dots O_n.
+
+        .. seealso:: :meth:`~.Hadamard.decomposition`.
+
+        Args:
+            wires (Any, Wires): Wire that the operator acts on.
+
+        Returns:
+            list[Operator]: decomposition of the operator
+
+        **Example:**
+
+        >>> print(qml.Hadamard.compute_decomposition(0))
+        [PhaseShift(1.5707963267948966, wires=[0]),
+        RX(1.5707963267948966, wires=[0]),
+        PhaseShift(1.5707963267948966, wires=[0])]
+
+        """
+        decomp_ops = [
+            qml.PhaseShift(np.pi / 2, wires=wires),
+            qml.RX(np.pi / 2, wires=wires),
+            qml.PhaseShift(np.pi / 2, wires=wires),
+        ]
+        return decomp_ops
+
+    def adjoint(self):
+        return Hadamard(wires=self.wires)
+
+    def single_qubit_rot_angles(self):
+        # H = RZ(\pi) RY(\pi/2) RZ(0)
+        return [np.pi, np.pi / 2, 0.0]
+
+    def pow(self, z):
+        return super().pow(z % 2)
 
 class PauliX(Observable, Operation):
     r"""PauliX(wires)
