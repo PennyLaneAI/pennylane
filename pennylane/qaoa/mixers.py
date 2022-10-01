4b420de27105f29f71b314bf7854575bbd04c46a
Rishabh S <54768821+anonymousr007@users.noreply.github.com>
2022-10-01 16:47:19 +0530

    Update mixers.py

--- pennylane/qaoa/mixers.py
+++ pennylane/qaoa/mixers.py
@@ -19,7 +19,7 @@ import functools
 from typing import Iterable, Union
 
 import networkx as nx
-import retworkx as rx
+import rustworkx as rx
 
 import pennylane as qml
 from pennylane.wires import Wires
@@ -102,7 +102,7 @@ def xy_mixer(graph: Union[nx.Graph, rx.PyGraph]):
     + (0.5) [X1 X2]
     + (0.5) [Y1 Y2]
 
-    >>> import retworkx as rx
+    >>> import rustworkx as rx
     >>> graph = rx.PyGraph()
     >>> graph.add_nodes_from([0, 1, 2])
     >>> graph.add_edges_from([(0, 1, ""), (1, 2, "")])
@@ -181,7 +181,7 @@ def bit_flip_mixer(graph: Union[nx.Graph, rx.PyGraph], b: int):
     + (0.5) [X2 Z1]
     + (0.25) [X1 Z0 Z2]
 
-    >>> import retworkx as rx
+    >>> import rustworkx as rx
     >>> graph = rx.PyGraph()
     >>> graph.add_nodes_from([0, 1, 2])
     >>> graph.add_edges_from([(0, 1, ""), (1, 2, "")])
