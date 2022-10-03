0d2f9db4cd554baa50494c22f33341e1168bd23a
Edward Jiang <eddy3.14159@gmail.com>
2022-10-03 12:35:05 -0400

    Add comment about diff_method

--- pennylane/optimize/lie_algebra.py
+++ pennylane/optimize/lie_algebra.py
@@ -288,6 +288,8 @@ class LieAlgebraOptimizer:
             lie_gradient, self.stepsize, self.trottersteps, self.exact
         )(self.circuit.func)
 
+        # we can set diff_method=None because the gradient of the QNode is computed
+        # directly in this optimizer
         self.circuit = qml.QNode(new_circuit, self.circuit.device, diff_method=None)
         return self.circuit, cost
 
