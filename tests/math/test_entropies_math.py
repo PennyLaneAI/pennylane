404aab94bf2bfad76327c50fb5db15db0f7077f7
Ashmit JaiSarita Gupta <ashmitgupta.social@gmail.com>
2022-10-09 23:28:30 +0530

    wrote correct tests for min_entropy

--- tests/math/test_entropies_math.py
+++ tests/math/test_entropies_math.py
@@ -235,8 +235,13 @@ class TestMinEntropy:
     @pytest.mark.parametrize("interface", ["autograd", "jax", "tensorflow", "torch"])
     @pytest.mark.parametrize(
         "state, expected",
-        [([1, 0], np.inf), ([1, 1] / np.sqrt(2), np.inf), ([0, 1], 0)],
-    )  # fill correct value of min_entropies for given test states here
+        [
+            ([1, 0], 1),
+            ([0, 1], 1),
+            ([1, 1] / np.sqrt(2), 1),
+            ([1, -1] / np.sqrt(2), 1),
+        ],
+    )
     @pytest.mark.parametrize("base", base)
     @pytest.mark.parametrize("check_state", check_states)
     def test_state(self, interface, state, expected, base, check_state):
@@ -250,10 +255,10 @@ class TestMinEntropy:
     @pytest.mark.parametrize(
         "state, expected",
         [
-            ([[1, 0], [0, 0]], np.inf),
-            ([[0, 0], [0, 1]], 0),
+            ([[1, 0], [0, 0]], 1),
+            ([[0, 0], [0, 1]], 1),
         ],
-    )  # fill correct value of min_entropies for given test states here
+    )
     @pytest.mark.parametrize("base", base)
     @pytest.mark.parametrize("check_state", check_states)
     def test_density_matrix(self, interface, state, expected, base, check_state):
@@ -263,40 +268,3 @@ class TestMinEntropy:
 
         div = 1 if base is None else np.log(base)
         assert np.allclose(actual, expected / div, rtol=1e-06, atol=1e-07)
-
-
-# class TestMinEntropyQNode:
-
-#     parameters = np.linspace(0, 2 * np.pi, 10)
-
-#     devices = ["default.qubit", "default.mixed", "lightning.qubit"]
-
-#     single_wires_list = [
-#         [0],
-#     ]
-
-#     base = [2, np.exp(1), 10]
-
-#     check_state = [True, False]
-
-#     devices = ["default.qubit", "default.mixed"]
-#     diff_methods = ["backprop", "finite-diff"]
-
-#     @pytest.mark.parametrize("wires", single_wires_list)
-#     @pytest.mark.parametrize("param", parameters)
-#     @pytest.mark.parametrize("device", devices)
-#     @pytest.mark.parametrize("base", base)
-#     def test_IsingXX_qnode_entropy(self, param, wires, device, base):
-#         """Test entropy for a QNode numpy."""
-
-#         dev = qml.device(device, wires=1)
-
-#         @qml.qnode(dev)
-#         def circuit_entropy(x):
-#             qml.IsingXX(x, wires=[0])
-#             return qml.vn_entropy(wires=wires, log_base=base)
-
-#         entropy = circuit_entropy(param)
-
-#         expected_entropy = expected_entropy_ising_xx(param) / np.log(base)
-#         assert qml.math.allclose(entropy, expected_entropy)
