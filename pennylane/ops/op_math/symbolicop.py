d98b5b89d42bc6000606ecfb8c600992076773ec
AlbertMitjans <a.mitjanscoma@gmail.com>
2022-10-18 16:56:28 +0200

    Revert

--- pennylane/ops/op_math/symbolicop.py
+++ pennylane/ops/op_math/symbolicop.py
@@ -136,8 +136,3 @@ class SymbolicOp(Operator):
                 self.base.hash,
             )
         )
-
-    def map_wires(self, wire_map: dict):
-        new_op = super().map_wires(wire_map=wire_map)
-        new_op.hyperparameters["base"] = self.base.map_wires(wire_map=wire_map)
-        return new_op
