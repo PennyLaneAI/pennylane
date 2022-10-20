b97e49031ccb452cd45884a2714c76aca5f5cee2
Jay Soni <jbsoni@uwaterloo.ca>
2022-10-20 17:47:08 -0400

    Cleaned up prototype

---
+++ pennylane/pauli/pauli_arithmetic.py
@@ -0,0 +1,234 @@
+# Copyright 2018-2022 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The Pauli arithmetic abstract reduced representation classes"""
+import numpy as np
+from scipy import sparse
+from pennylane import math
+
+I = "I"
+X = "X"
+Y = "Y"
+Z = "Z"
+
+matI = np.eye(2)
+matX = np.array([[0, 1], [1, 0]])
+matY = np.array([[0, -1j], [1j, 0]])
+matZ = np.array([[1, 0], [0, -1]])
+
+mat_map = {
+    I: matI,
+    X: matX,
+    Y: matY,
+    Z: matZ,
+}
+
+sparse_matI = sparse.eye(2, format="csr")
+sparse_matX = sparse.csr_matrix([[0, 1], [1, 0]])
+sparse_matY = sparse.csr_matrix([[0, -1j], [1j, 0]])
+sparse_matZ = sparse.csr_matrix([[1, 0], [0, -1]])
+
+sparse_mat_map = {
+    I: sparse_matI,
+    X: sparse_matX,
+    Y: sparse_matY,
+    Z: sparse_matZ,
+}
+
+_map_I = {
+    I: (1, I),
+    X: (1, X),
+    Y: (1, Y),
+    Z: (1, Z),
+}
+_map_X = {
+    I: (1, X),
+    X: (1, I),
+    Y: (1.0j, Z),
+    Z: (-1.0j, Y),
+}
+_map_Y = {
+    I: (1, Y),
+    X: (-1.0j, Z),
+    Y: (1, I),
+    Z: (1j, X),
+}
+_map_Z = {
+    I: (1, Z),
+    X: (1j, Y),
+    Y: (-1.0j, X),
+    Z: (1, I),
+}
+
+mul_map = {I: _map_I, X: _map_X, Y: _map_Y, Z: _map_Z}
+
+
+class PauliWord(dict):
+    """Immutable dictionary used to represent a Pauli Word.
+    Can be constructed from a standard dictionary.
+
+    >>> w = PauliWord({"a": X, 2: Y, 3: Z})
+    """
+    def __missing__(self, key):
+        """If the wire is not in the Pauli word,
+        then no operator acts on it, so return the Identity."""
+        return I
+
+    def __setitem__(self, key, item):
+        raise NotImplementedError
+
+    def __hash__(self):
+        return hash(frozenset(self.items()))
+
+    def __mul__(self, other):
+        """Multiply two Pauli words together using the matrix product if wires overlap
+        and the tensor product otherwise.
+
+        Args:
+            other (PauliWord): The Pauli word to multiply with
+
+        Returns:
+            result(PauliWord): The resulting operator of the multiplication
+            coeff(complex): The complex phase factor
+        """
+        result, iterator = (dict(self), other) if len(self) > len(other) else (dict(other), self)
+        coeff = 1
+
+        for wire, term in iterator.items():
+            if wire in result:
+                factor, new_op = mul_map[result[wire]][term]
+                if new_op == I:
+                    del result[wire]
+                else:
+                    coeff *= factor
+                    result[wire] = new_op
+            elif term != I:
+                result[wire] = term
+
+        return PauliWord(result), coeff
+
+    @property
+    def wires(self):
+        """Track wires in a PauliWord."""
+        return set(k for k in self.keys())
+
+    def to_mat(self, wire_order=None, format="dense"):
+        """Given a Pauli word, get the matrix representation.
+
+        KeywordArgs:
+            wire_order (iterable or None): The order of qubits in the tensor product.
+            format (str): The format of the matrix ("dense" by default), if not a dense
+                matrix, then the format for the sparse representation of the matrix.
+
+        Returns:
+            (Union[NumpyArray, ScipySparseArray]): Matrix representation of the Pauliword
+        """
+        matrix_map = sparse_mat_map if format != "dense" else mat_map
+        if wire_order is None:
+            wire_order = self.wires
+
+        initial_wire = wire_order[0]
+        result_matrix = matrix_map[self[initial_wire]]
+
+        for wire in wire_order[1:]:
+            result_matrix = math.kron(result_matrix, matrix_map[self[wire]])
+
+        return result_matrix
+
+
+class PauliSentence(dict):
+    """Dict representing a Pauli Sentence. The keys are
+    PauliWord instances and the values correspond to coefficients.
+
+    >>> ps = PauliSentence({
+            PauliWord({0:X, 1:Y}): 1.23
+            PauliWord({2:Z, 0:Y}): -0.45j
+        })
+    """
+
+    def __missing__(self, key):
+        """If the PauliWord is not in the sentence then the coefficient
+        associated with it should be 0."""
+        return 0.0
+
+    def __add__(self, other):
+        """Add two Pauli sentence together by iterating over the smaller
+        one and adding its terms to the larger one."""
+        c_self, c_other = (self, other)
+        smaller_ps, larger_ps = (c_self, c_other) if len(self) < len(other) else (c_other, c_self)
+        for key in smaller_ps:
+            larger_ps[key] += smaller_ps[key]
+
+        return larger_ps
+
+    def __mul__(self, other):
+        """Multiply two Pauli sentences by iterating over each sentence and multiplying
+        the Pauli words pair-wise"""
+        final_ps = PauliSentence()
+
+        for pw1 in self:
+            for pw2 in other:
+                prod_pw, coeff = pw1 * pw2
+                final_ps[prod_pw] += coeff * self[pw1] * other[pw2]
+
+        return final_ps
+
+    def __str__(self):
+        """String representation of the PauliSentence."""
+        rep_str = ""
+        for index, (pw, coeff) in enumerate(self.items()):
+            if index == 0:
+                rep_str += "= "
+            else:
+                rep_str += "+ "
+            rep_str += f"({round(coeff, 2)}) * "
+            for w, op in pw.items():
+                rep_str += f"[{op}({w.labels[0]})]"
+            rep_str += "\n"
+
+        return rep_str
+
+    @property
+    def wires(self):
+        """Track wires of the PauliSentence."""
+        return set().union(*(pw.wires for pw in self.keys()))
+
+    def to_mat(self, wire_order=None, format="dense"):
+        """Given a Pauli word, get the matrix representation.
+
+        KeywordArgs:
+            wire_order (iterable or None): The order of qubits in the tensor product.
+            format (str): The format of the matrix ("dense" by default), if not a dense
+                matrix, then the format for the sparse representation of the matrix.
+
+        Returns:
+            (Union[NumpyArray, ScipySparseArray]): Matrix representation of the PauliSentence.
+        """
+        if wire_order is None:
+            wire_order = self.wires
+
+        mats_and_wires_gen = (
+            (coeff * pw.to_mat(format=format), pw.wires)
+            for pw, coeff in self.items()
+        )
+
+        reduced_mat, result_wire_order = math.reduce_matrices(
+            mats_and_wires_gen=mats_and_wires_gen, reduce_func=math.add
+        )
+
+        return math.expand_matrix(reduced_mat, result_wire_order, wire_order=wire_order)
+
+    def simplify(self, tol=1e-8):
+        for pw in self:
+            if abs(self[pw]) <= tol:
+                del self[pw]
---
+++ tests/pauli/test_pauli_arithmetic.py
@@ -0,0 +1,112 @@
+# Copyright 2018-2022 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit Tests for the PauliWord and PauliSentence classes"""
+import pytest
+# import numpy as np
+# from scipy import sparse
+from pennylane.pauli.pauli_arithmetic import PauliWord, PauliSentence, I, X, Y, Z
+
+pw1 = PauliWord({0: I, 1: X, 2: Y})
+pw2 = PauliWord({'a': X, 'b': X, 'c': Z})
+pw3 = PauliWord({0: Z, 'b': Z})
+pw4 = PauliWord({})
+
+
+class TestPauliWord:
+    def test_missing(self):
+        """Test the result when a missing key is indexed"""
+        pw = PauliWord({0:I, 1:X, 2:Y})
+        assert 3 not in pw.keys()
+        assert pw[3] == I
+
+    def test_set_items(self):
+        """Test that setting items raises an error"""
+        pw = PauliWord({0:I, 1:X, 2:Y})
+        with pytest.raises(NotImplementedError):
+            pw[3] = Z  # trying to add to a pw after instantiation is prohibited
+
+    def test_hash(self):
+        """Test that a unique hash exists for different PauliWords."""
+        pw1 = PauliWord({0:I, 1:X, 2:Y})
+        pw2 = PauliWord({0:I, 1:X, 2:Y})  # same as 1
+        pw3 = PauliWord({1:X, 2:Y, 0:I})  # same as 1 but reordered
+        pw4 = PauliWord({1:Z, 2:Z})  # distinct from above
+
+        assert pw1.__hash__() == pw2.__hash__()
+        assert pw1.__hash__() == pw3.__hash__()
+        assert pw1.__hash__() != pw4.__hash__()
+
+    tup_pws_wires = (
+        (pw1, {0, 1, 2}),
+        (pw2, {'a', 'b', 'c'}),
+        (pw3, {0, 'b'}),
+        (pw4, {})
+    )
+
+    @pytest.mark.parametrize("pw, wires", tup_pws_wires)
+    def test_wires(self, pw, wires):
+        """Test that the wires are tracked correctly."""
+        assert pw.wires == wires
+
+    def test_mul(self):
+        assert True
+
+    def test_to_mat(self):
+        assert True
+
+    def test_to_mat_wire_order(self):
+        assert True
+
+    def test_to_mat_sparse(self):
+        assert True
+
+
+class TestPauliSentence:
+    def test_missing(self):
+        """Test the result when a missing key is indexed"""
+        pw = {0:X}
+        new_pw = {'a':Z}
+        ps = PauliSentence({pw:1.0})
+
+        assert new_pw not in ps.keys()
+        assert ps[new_pw] == 0.0
+
+    def test_set_items(self):
+        """Test that we can added to a PauliSentence"""
+        pw = {0: X}
+        ps = PauliSentence({pw: 1.0})
+
+        new_pw = {'a': Z}
+        assert new_pw not in ps.keys()
+
+        ps[new_pw] = 3.45
+        assert new_pw in ps.keys() and ps[new_pw] == 3.45
+
+    def test_str(self):
+        assert True
+
+    def test_wires(self):
+        assert True
+
+    def test_mul(self):
+        assert True
+
+    def test_add(self):
+        assert True
+
+    def test_to_mat(self):
+        assert True
+
+    def test_simplify(self):
+        assert True
