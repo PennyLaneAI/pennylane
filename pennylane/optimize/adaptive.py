bb593ac4419fc7a637cca4c41764731f2b5434b0
soranjh <soranjh@yahoo.com>
2022-10-20 11:53:29 -0400

    add append gate function

---
+++ pennylane/optimize/adaptive.py
@@ -0,0 +1,41 @@
+# Copyright 2018-2022 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Adaptive optimizer"""
+
+import pennylane as qml
+
+from pennylane import numpy as np
+
+
+@qml.qfunc_transform
+def append_gate(tape, params, gates):
+    """Append parameterized gates to an existing circuit.
+
+    Args:
+        tape (QuantumTape): quantum tape to transform by adding gates
+        params (array[float]): parameters of the gates to be added
+        gates (list[Operator]): list of the gates to be added
+    """
+    if len(params) == 1:
+        gates = [gates]
+
+    for o in tape.operations:
+        qml.apply(o)
+
+    for i, g in enumerate(gates):
+        g.data[0] = params[i]
+        qml.apply(g)
+
+    for m in tape.measurements:
+        qml.apply(m)
