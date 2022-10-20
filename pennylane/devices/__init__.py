f9ba2da9e4375231ec41dcc0922cb50f6c9185bb
Lee J. O'Riordan <loriordan@gmail.com>
2022-10-20 12:08:33 -0400

    Ensure experimental module is imported

--- pennylane/devices/__init__.py
+++ pennylane/devices/__init__.py
@@ -38,3 +38,4 @@ to verify and test quantum gradient computations.
 from .default_qubit import DefaultQubit
 from .default_gaussian import DefaultGaussian
 from .default_mixed import DefaultMixed
+from .experimental import *
