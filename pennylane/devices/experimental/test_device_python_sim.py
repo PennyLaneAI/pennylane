746cfedd4610a1a9c6d65cdb75fa2e4eb0b277b5
Lee J. O'Riordan <loriordan@gmail.com>
2022-10-20 15:51:07 -0400

    Add device wrappin Python sim

---
+++ pennylane/devices/experimental/test_device_python_sim.py
@@ -0,0 +1,12 @@
+from .device_interface import *
+from .python_simulator import *
+
+class TestDevicePythonSim(AbstractDevice):
+    "Device containing a Python simulator favouring composition-like interface"
+
+    def __init__(self, dev_config: Union[DeviceConfig, None] = None, *args, **kwargs):
+        super().__init__(dev_config, *args, **kwargs)
+        self._private_sim = PlainNumpySimulator()
+    
+    def execute(self, qscript: Union[QuantumScript, List[QuantumScript]]):
+        return self._private_sim.execute(qscript)
