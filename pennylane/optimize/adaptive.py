0c1bf07cc232f5ae97cea39f87172f430d4d75a5
soranjh <soranjh@yahoo.com>
2022-10-20 13:56:43 -0400

    add AdaptiveOptimizer class

--- pennylane/optimize/adaptive.py
+++ pennylane/optimize/adaptive.py
@@ -39,3 +39,56 @@ def append_gate(tape, params, gates):
 
     for m in tape.measurements:
         qml.apply(m)
+
+
+class AdaptiveOptimizer:
+    def __init__(self, tol=1e-5):
+        self.tol = tol
+
+    def _equal(self, a, b):
+        if a.name == b.name and a.wires == b.wires:
+            return True
+        return False
+
+    def _circuit(self, params, gates, initial_circuit):
+
+        final_circuit = append_gate(params, gates)(initial_circuit)
+
+        return final_circuit()
+
+    def step_and_cost(self, circuit, pool, drain=False):
+
+        device = circuit.device
+
+        energy = circuit()
+
+        if drain:
+            for gate in pool:
+                for operation in circuit.tape.operations:
+                    if self._equal(gate, operation):
+                        pool.remove(gate)
+
+        params = np.array([x.parameters[0] for x in pool], requires_grad=True)
+
+        _qnode = qml.QNode(self._circuit, device)
+
+        grads = qml.grad(_qnode)(params, gates=pool, initial_circuit=circuit.func)
+
+        if np.max(abs(grads)) > self.tol:
+
+            selected_gate = pool[np.argmax(abs(grads))]
+
+            optimizer = qml.GradientDescentOptimizer(stepsize=0.5)
+
+            params = np.zeros(1)
+
+            for n in range(10):
+                params, energy = optimizer.step_and_cost(
+                    _qnode, params, gates=selected_gate, initial_circuit=circuit.func
+                )
+
+            circuit = append_gate(params, selected_gate)(circuit.func)
+
+        qnode = qml.QNode(circuit, device)
+
+        return qnode, energy
