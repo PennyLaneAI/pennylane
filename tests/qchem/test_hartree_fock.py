aa38f56e683d2652cc76b5376c3cc84482905e87
soranjh <soranjh@yahoo.com>
2022-10-25 14:12:09 -0400

    correct arg indices

--- tests/qchem/test_hartree_fock.py
+++ tests/qchem/test_hartree_fock.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Unit tests for for Hartree-Fock functions.
+Unit tests for Hartree-Fock functions.
 """
 import autograd
 import pytest
