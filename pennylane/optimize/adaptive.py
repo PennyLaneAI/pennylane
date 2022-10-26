8b897d6ee55908ed59c78e004ed0d08f59df120e
soranjh <soranjh@yahoo.com>
2022-10-26 17:28:25 -0400

    fix pylint issue

--- pennylane/optimize/adaptive.py
+++ pennylane/optimize/adaptive.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Adaptive optimizer"""
-# pylint: disable= no-value-for-parameter
+# pylint: disable= no-value-for-parameter, protected-access
 import pennylane as qml
 
 from pennylane import numpy as np
