2575b35683c1fe3f4ee300cfdecf133ead3e2f4a
Jay Soni <jbsoni@uwaterloo.ca>
2022-10-28 15:15:45 -0400

    codefactor

--- pennylane/pauli/pauli_arithmetic.py
+++ pennylane/pauli/pauli_arithmetic.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 """The Pauli arithmetic abstract reduced representation classes"""
 from copy import copy
+from functools import reduce
 
 import numpy as np
 from scipy import sparse
