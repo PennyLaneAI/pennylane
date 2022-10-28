e2fe959e626cafd04db1046a93d7574984ceb436
mudit2812 <muditpandey2812@gmail.com>
2022-10-28 13:18:28 -0700

    Update pennylane/ops/qutrit/matrix_ops.py
    Co-authored-by: Christina Lee <chrissie.c.l@gmail.com>

--- pennylane/ops/qutrit/matrix_ops.py
+++ pennylane/ops/qutrit/matrix_ops.py
@@ -147,7 +147,7 @@ class ControlledQutritUnitary(QutritUnitary):
     available for ``ControlledQutritUnitary``:
 
     * ``control_wires``: wires that act as control for the operation
-    * ``U``: unitary applied to the target wires
+    * ``U``: unitary applied to the target wires. Accessible via ``op.parameters[0]``
     * ``control_values``: a string of trits representing the state of the control
       qutrits to control on (default is the all 2s state)
 
