48afafb19f285cf26ea87e55540436b622190cba
albi3ro <chrissie.c.l@gmail.com>
2022-11-07 11:28:30 -0500

    remove python 3.7, add python3.11

--- setup.py
+++ setup.py
@@ -78,10 +78,10 @@ classifiers = [
     "Operating System :: Microsoft :: Windows",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: 3 :: Only",
     "Topic :: Scientific/Engineering :: Physics",
 ]
