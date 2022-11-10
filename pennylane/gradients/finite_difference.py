48956bcb0e1cfa6e6c7e772db3572ca2c6c91814
antalszava <antalszava@gmail.com>
2022-11-10 16:33:24 -0500

    Return types docs (`v0.27.0` release) (#3308)
    * Restructure
    
    * fixed formating for link in GellMann observable
    
    * another pass
    
    * another pass
    
    * minor
    
    * minor
    
    * added missing entries
    
    * Removed extraneous missing entries, pass through pauli module entry
    
    * return types item: add section and move it down to bottommost item; whitespace linting; qchem typo fix
    
    * Shortened new return type entry
    
    * minor
    
    * added missing entries
    
    * replace 'our demo' with another name to bypass duplicacy issue with Sphinx
    
    * docs
    
    * fix timeit circuit: it was defined with multi-measurement QNode that's not supported
    
    * finite diff docstring with examples
    
    * Hessian docs
    
    * param-shift and finite diff shot vector examples
    
    * update JAX and JAX lib versions in doc/requirements
    
    * updates
    
    * gotcha for Autograd and TF; JAX updates
    
    * section titles
    
    * fix some return type docstrings; fix QNSPSA docstring
    
    * improvement
    
    * update wording
    
    * rewording
    
    * revert
    
    * Update example
    
    Co-authored-by: Isaac De Vlugt <isaacdevlugt@gmail.com>
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>
    Co-authored-by: Romain Moyard <rmoyard@gmail.com>

--- pennylane/gradients/finite_difference.py
+++ pennylane/gradients/finite_difference.py
@@ -250,11 +250,11 @@ def _finite_diff_new(
             device shots for the new return types output system.
 
     Returns:
-        tensor_like or tuple[list[QuantumTape], function]:
+        tensor_like or tuple[tensor_like] or tuple[tuple[tensor_like]] or tuple[list[QuantumTape], function]:
 
-        - If the input is a QNode, a tensor
-          representing the output Jacobian matrix of size ``(number_outputs, number_gate_parameters)``
-          is returned.
+        - If the input is a QNode, an object representing the output Jacobian matrix.
+          The type of the object returned is either a tensor, a tuple or a nested tuple depending on the nesting
+          structure of the output.
 
         - If the input is a tape, a tuple containing a list of generated tapes,
           in addition to a post-processing function to be applied to the
@@ -262,7 +262,106 @@ def _finite_diff_new(
 
     **Example**
 
-    #TODO: Add example for new return type.
+    This transform can be registered directly as the quantum gradient transform
+    to use during autodifferentiation:
+
+    >>> dev = qml.device("default.qubit", wires=2)
+    >>> @qml.qnode(dev, interface="autograd", diff_method="finite-diff")
+    ... def circuit(params):
+    ...     qml.RX(params[0], wires=0)
+    ...     qml.RY(params[1], wires=0)
+    ...     qml.RX(params[2], wires=0)
+    ...     return qml.expval(qml.PauliZ(0))
+    >>> params = np.array([0.1, 0.2, 0.3], requires_grad=True)
+    >>> qml.jacobian(circuit)(params)
+    tensor([-0.38751725, -0.18884792, -0.38355708], requires_grad=True)
+
+    When differentiating QNodes with multiple measurements using Autograd or TensorFlow, the outputs of the QNode first
+    need to be stacked. The reason is that those two frameworks only allow differentiating functions with array or
+    tensor outputs, instead of functions that output sequences. In contrast, Jax and Torch require no additional
+    post-processing.
+
+    >>> import jax
+    >>> dev = qml.device("default.qubit", wires=2)
+    >>> @qml.qnode(dev, interface="jax", diff_method="finite-diff")
+    ... def circuit(params):
+    ...     qml.RX(params[0], wires=0)
+    ...     qml.RY(params[1], wires=0)
+    ...     qml.RX(params[2], wires=0)
+    ...     return qml.expval(qml.PauliZ(0)), qml.var(qml.PauliZ(0))
+    >>> params = jax.numpy.array([0.1, 0.2, 0.3])
+    >>> jax.jacobian(circuit)(params)
+    (DeviceArray([-0.38751727, -0.18884793, -0.3835571 ], dtype=float32), DeviceArray([0.6991687 , 0.34072432, 0.6920237 ], dtype=float32))
+
+
+    .. details::
+        :title: Usage Details
+
+        This gradient transform can also be applied directly to :class:`QNode <pennylane.QNode>` objects:
+
+        >>> @qml.qnode(dev)
+        ... def circuit(params):
+        ...     qml.RX(params[0], wires=0)
+        ...     qml.RY(params[1], wires=0)
+        ...     qml.RX(params[2], wires=0)
+        ...     return qml.expval(qml.PauliZ(0)), qml.var(qml.PauliZ(0))
+        >>> qml.gradients.finite_diff(circuit)(params)
+        ((tensor(-0.38751724, requires_grad=True),
+          tensor(-0.18884792, requires_grad=True),
+          tensor(-0.38355709, requires_grad=True)),
+         (tensor(0.69916868, requires_grad=True),
+          tensor(0.34072432, requires_grad=True),
+          tensor(0.69202366, requires_grad=True)))
+
+        This quantum gradient transform can also be applied to low-level
+        :class:`~.QuantumTape` objects. This will result in no implicit quantum
+        device evaluation. Instead, the processed tapes, and post-processing
+        function, which together define the gradient are directly returned:
+
+        >>> with qml.tape.QuantumTape() as tape:
+        ...     qml.RX(params[0], wires=0)
+        ...     qml.RY(params[1], wires=0)
+        ...     qml.RX(params[2], wires=0)
+        ...     qml.expval(qml.PauliZ(0))
+        ...     qml.var(qml.PauliZ(0))
+        >>> gradient_tapes, fn = qml.gradients.finite_diff(tape)
+        >>> gradient_tapes
+        [<QuantumTape: wires=[0], params=3>,
+         <QuantumTape: wires=[0], params=3>,
+         <QuantumTape: wires=[0], params=3>,
+         <QuantumTape: wires=[0], params=3>]
+
+        This can be useful if the underlying circuits representing the gradient
+        computation need to be analyzed.
+
+        The output tapes can then be evaluated and post-processed to retrieve
+        the gradient:
+
+        >>> dev = qml.device("default.qubit", wires=2)
+        >>> fn(qml.execute(gradient_tapes, dev, None))
+        ((array(-0.38751724), array(-0.18884792), array(-0.38355709)),
+         (array(0.69916868), array(0.34072432), array(0.69202366)))
+
+        Devices that have a shot vector defined can also be used for execution, provided
+        the ``shots`` argument was passed to the transform:
+
+        >>> shots = (10, 100, 1000)
+        >>> dev = qml.device("default.qubit", wires=2, shots=shots)
+        >>> @qml.qnode(dev)
+        ... def circuit(params):
+        ...     qml.RX(params[0], wires=0)
+        ...     qml.RY(params[1], wires=0)
+        ...     qml.RX(params[2], wires=0)
+        ...     return qml.expval(qml.PauliZ(0)), qml.var(qml.PauliZ(0))
+        >>> params = np.array([0.1, 0.2, 0.3], requires_grad=True)
+        >>> qml.gradients.finite_diff(circuit, shots=shots, h=10e-2)(params)
+        (((array(-2.), array(-2.), array(0.)), (array(3.6), array(3.6), array(0.))),
+         ((array(1.), array(0.4), array(1.)),
+          (array(-1.62), array(-0.624), array(-1.62))),
+         ((array(-0.48), array(-0.34), array(-0.46)),
+          (array(0.84288), array(0.6018), array(0.80868))))
+
+        The outermost tuple contains results corresponding to each element of the shot vector.
     """
     if argnum is None and not tape.trainable_params:
         return _no_trainable_grad_new(tape, shots)
