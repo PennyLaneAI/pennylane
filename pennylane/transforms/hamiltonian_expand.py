ff4889cc905601828d9c0930b4b238fd94b44bec
Antal Szava <antalszava@gmail.com>
2022-11-11 14:43:39 -0500

    covert_like

--- pennylane/transforms/hamiltonian_expand.py
+++ pennylane/transforms/hamiltonian_expand.py
@@ -188,6 +188,7 @@ def hamiltonian_expand(tape, group=True):
     # pylint: disable=function-redefined
     def processing_fn(res):
         dot_products = [qml.math.dot(qml.math.squeeze(r), c) for c, r in zip(coeffs, res)]
-        return qml.math.sum(qml.math.stack(dot_products), axis=0)
+        summed = qml.math.sum(qml.math.stack(dot_products), axis=0)
+        return qml.math.convert_like(summed, res[0]) if len(res) > 0 else summed
 
     return tapes, processing_fn
