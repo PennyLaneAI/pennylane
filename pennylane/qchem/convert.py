beee54dd7a2e2bc3ce5158848397a05caa7a672a
soranjh <soranjh@yahoo.com>
2022-11-15 18:03:07 -0500

    modify warning to return complex coeffs

--- pennylane/qchem/convert.py
+++ pennylane/qchem/convert.py
@@ -326,7 +326,7 @@ def import_operator(qubit_observable, format="openfermion", wires=None, tol=1e01
         warnings.warn(
             f"The coefficients entering the QubitOperator must be real;"
             f" got complex coefficients in the operator"
-            f" {[coeff for coeff in coeffs if np.iscomplex(coeff)]}"
+            f" {[coef for coef in coeffs if np.iscomplex(coef)]}"
         )
 
     return qml.Hamiltonian(*_openfermion_to_pennylane(qubit_observable, wires=wires))
