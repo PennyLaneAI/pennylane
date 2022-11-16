b03c8e387b49fd7c640601542da22ed2a7997104
AlbertMitjans <a.mitjanscoma@gmail.com>
2022-11-16 15:22:16 +0100

    Revert

--- pennylane/measurements/probs.py
+++ pennylane/measurements/probs.py
@@ -119,6 +119,30 @@ def probs(wires=None, op=None):
 class _Probability(SampleMeasurement, StateMeasurement):
     """Measurement process that computes the probability of each computational basis state."""
 
+    @property
+    def numeric_type(self):
+        return float
+
+    def shape(self, device):
+        if qml.active_return():
+            return self._shape_new(device)
+        num_shot_elements = (
+            1 if device.shot_vector is None else sum(s.copies for s in device.shot_vector)
+        )
+        len_wires = len(self.wires)
+        dim = self._get_num_basis_states(len_wires, device)
+
+        return (num_shot_elements, dim)
+
+    def _shape_new(self, device):
+        num_shot_elements = (
+            1 if device.shot_vector is None else sum(s.copies for s in device.shot_vector)
+        )
+        len_wires = len(self.wires)
+        dim = self._get_num_basis_states(len_wires, device)
+
+        return (dim,) if num_shot_elements == 1 else tuple((dim,) for _ in range(num_shot_elements))
+
     def process_samples(
         self, samples: Sequence[complex], shot_range: Tuple[int] = None, bin_size: int = None
     ):
