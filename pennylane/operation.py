9d9028c9c78d9c1df5908cc8f802d48fc863ed76
mudit2812 <muditpandey2812@gmail.com>
2022-11-17 18:34:46 -0800

    Added docs for Operator.validate_subspace

--- pennylane/operation.py
+++ pennylane/operation.py
@@ -820,6 +820,14 @@ class Operator(abc.ABC):
 
     @staticmethod
     def validate_subspace(subspace):
+        """Validate the subspace for qutrit operations.
+
+        This method determines whether a given subspace for qutrit operations
+        is defined correctly or not. If not, a `ValueError` is thrown.
+
+        Args:
+            subspace (tuple[int]): Subspace to check for correctness
+        """
         if not hasattr(subspace, "__iter__") or len(subspace) != 2:
             raise ValueError(
                 "The subspace must be a sequence with two unique elements from the set {0, 1, 2}."
