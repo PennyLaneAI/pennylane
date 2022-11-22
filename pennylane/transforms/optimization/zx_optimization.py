f7c76e1bbd3444366a8e3f0dfa5f0a74ceb5dce2
Romain Moyard <rmoyard@gmail.com>
2022-11-22 12:20:40 -0500

    Converter draft

--- pennylane/transforms/optimization/zx_optimization.py
+++ pennylane/transforms/optimization/zx_optimization.py
@@ -19,8 +19,8 @@ from pennylane.tape import QuantumTape
 from pennylane.transforms import qfunc_transform
 
 
-def _import_pyzx_dill():
-    """Import zstd and dill."""
+def _import_pyzx():
+    """Import PyZx."""
     try:
         # pylint: disable=import-outside-toplevel, unused-import, multiple-imports
         import pyzx
@@ -63,7 +63,7 @@ def zx_optimization(tape: QuantumTape):
     0: ──RZ(3.14)──H──RZ(3.14)─┤  <Z>
     """
     # Try import pyzx
-    zx = _import_pyzx_dill()
+    zx = _import_pyzx()
 
     # Save the measurements
     measurements = tape.measurements
@@ -96,3 +96,91 @@ def zx_optimization(tape: QuantumTape):
 
     for m in measurements:
         qml.apply(m)
+
+
+from typing import Dict, List, Optional
+
+import pyzx
+from pyzx import Circuit
+from pyzx import TargetMapper
+from pyzx.utils import EdgeType, VertexType, FloatInt, FractionLike
+from pyzx.graph import Graph
+from pyzx.graph.base import BaseGraph, VT, ET
+
+
+gate_types: Dict[str, Type[Gate]] = {
+    "XPhase": XPhase,
+    "NOT": NOT,
+    "ZPhase": ZPhase,
+    "YPhase": YPhase,
+    "PauliZ": Z,
+    "S": S,
+    "T": T,
+    "CNOT": CNOT,
+    "CZ": CZ,
+    "CX": CX,
+    "SWAP": SWAP,
+    "CRZ": CRZ,
+    "HAD": HAD,
+    "H": HAD,
+    "CHAD": CHAD,
+    "Toffoli": Tofolli,
+    "CCZ": CCZ,
+    "Measurement": Measurement,
+}
+#    "ParityPhase": ParityPhase,
+
+
+def circuit_to_graph(
+    q: QuantumTape, compress_rows: bool = True, backend: Optional[str] = None
+) -> BaseGraph[VT, ET]:
+    """Turns a PennyLane quantum tape into a ZX-Graph.
+    If ``compress_rows`` is set, it tries to put single qubit gates on different qubits,
+    on the same row."""
+    g = Graph(backend)
+    q_mapper: TargetMapper[VT] = TargetMapper()
+    c_mapper: TargetMapper[VT] = TargetMapper()
+    inputs = []
+    outputs = []
+
+    # Tape wires
+    for i in range(q.qubits):
+        v = g.add_vertex(VertexType.BOUNDARY, i, 0)
+        inputs.append(v)
+        q_mapper.set_prev_vertex(i, v)
+        q_mapper.set_next_row(i, 1)
+        q_mapper.set_qubit(i, i)
+
+    # Expand the tape: add rotation first: only Z measurement I think
+
+    # Create graph from circuit in the tape (operations, measurements)
+    for op in q.circuit:
+        # Map the gate
+
+        # Control, target, phase
+        gate = gate_types[op.name]
+        gate = gate(*op.wires)
+
+        if not compress_rows:  # or not isinstance(gate, (ZPhase, XPhase, HAD)):
+            r = max(q_mapper.max_row(), c_mapper.max_row())
+            q_mapper.set_all_rows(r)
+            c_mapper.set_all_rows(r)
+        gate.to_graph(g, q_mapper, c_mapper)
+        if not compress_rows:  # or not isinstance(gate, (ZPhase, XPhase, HAD)):
+            r = max(q_mapper.max_row(), c_mapper.max_row())
+            q_mapper.set_all_rows(r)
+            c_mapper.set_all_rows(r)
+
+    r = max(q_mapper.max_row(), c_mapper.max_row())
+    for mapper in (q_mapper, c_mapper):
+        for l in mapper.labels():
+            o = mapper.to_qubit(l)
+            v = g.add_vertex(VertexType.BOUNDARY, o, r)
+            outputs.append(v)
+            u = mapper.prev_vertex(l)
+            g.add_edge(g.edge(u, v))
+
+    g.set_inputs(tuple(inputs))
+    g.set_outputs(tuple(outputs))
+
+    return g
