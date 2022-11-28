4ca6c141450a0ddbd76d5a048ba1a0bc3268bd0e
Lee J. O'Riordan <loriordan@gmail.com>
2022-11-28 16:48:09 -0500

    Extend sample code

--- pennylane/devices/experimental/__init__.py
+++ pennylane/devices/experimental/__init__.py
@@ -4,3 +4,4 @@ from .custom_device_0 import *
 from .custom_device_1 import *
 from .custom_device_2 import *
 from .custom_device_3_numpydev import *
+from .custom_device_4_cpp import *
--- pennylane/devices/experimental/sample_use.py
+++ pennylane/devices/experimental/sample_use.py
@@ -2,18 +2,24 @@ import pennylane as qml
 import pennylane.numpy as np
 
 # Create a device which holds a simulator
-dev1 = qml.devices.experimental.custom_device_3.TestDevicePythonSim()
+dev = qml.devices.experimental.custom_device_3_numpydev.TestDevicePythonSim()
+#dev = qml.devices.experimental.custom_device_4_cpp.TestDeviceLightningSim()
 
 # define trainable params
 params = np.array([0.432, 0.543, 0.133], requires_grad=True)
 
 # Prepare a an example circuit
-ops = [qml.RX(0.432, 0), qml.RY(0.543, 0), qml.CNOT((0, 1)), qml.RX(0.133, 1)]
+ops = []
+for i in range(4):
+    ops.extend([qml.RX(0.432, i), qml.RY(0.543, i), qml.CNOT((i, i+1)), qml.RX(0.133, i)])
+
 obs = [qml.expval(qml.PauliZ(0))]
 qscript = qml.tape.QuantumScript(ops, obs)
 
 # Run forward pass
-dev1.execute(qscript)
+res = dev.gradient(qscript)
+
+print(res)
 
 # Todo
 # 1. Add support for registrartion of a preprocessing step, to be called before execute
