e1ab02bd49113afbcfd387e9ec9abf9c35bce2b7
Edward Jiang <eddy3.14159@gmail.com>
2022-11-30 12:10:32 -0500

    Fix broken test

--- tests/returntypes/transforms/test_shadow_transforms_new.py
+++ tests/returntypes/transforms/test_shadow_transforms_new.py
@@ -18,6 +18,7 @@ import builtins
 import pytest
 
 import pennylane as qml
+from pennylane.measurements.classical_shadow import _ShadowExpval
 from pennylane import numpy as np
 
 
@@ -318,7 +319,7 @@ class TestExpvalTransform:
 
         assert all(qml.equal(qml.Hadamard(i), tape.operations[i]) for i in range(3))
         assert len(tape.observables) == 1
-        assert isinstance(tape.observables[0], qml.measurements.ShadowMeasurementProcess)
+        assert isinstance(tape.observables[0], _ShadowExpval)
         assert tape.observables[0].H == obs
 
     def test_hadamard_forward(self):
