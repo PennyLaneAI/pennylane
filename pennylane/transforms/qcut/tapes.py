e8e0ec89fecdea397dd91cccfc5563c2bf0d4982
Lillian Frederiksen <lillian542@gmail.com>
2022-12-01 16:16:55 -0500

    pylint

--- pennylane/transforms/qcut/tapes.py
+++ pennylane/transforms/qcut/tapes.py
@@ -90,7 +90,7 @@ def tape_to_graph(tape: QuantumTape) -> MultiDiGraph:
                     "is not supported in circuit cutting"
                 )
             for o in obs.obs:
-                m_ = MeasurementProcess(m.return_type, obs=o)
+                m_ = MeasurementProcess(m.return_type, o)
 
                 _add_operator_node(graph, m_, order, wire_latest_node)
         elif m.return_type is Sample and obs is None:
