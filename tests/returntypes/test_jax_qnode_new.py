9685da5bc52d2afec78c74042b987441b7f717c7
antalszava <antalszava@gmail.com>
2022-12-05 21:01:39 -0500

    port over more tests

--- tests/returntypes/test_jax_qnode_new.py
+++ tests/returntypes/test_jax_qnode_new.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Integration tests for using the jax interface and its jittable variant with
-a QNode"""
+"""Integration tests for using the JAX-Python interface with a QNode"""
 
 import pytest
 
