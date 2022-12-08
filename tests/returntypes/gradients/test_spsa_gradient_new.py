9a13609ee442577f81182e5608afbd2ebd8fa8a0
dwierichs <davidwierichs@gmail.com>
2022-12-08 03:30:22 +0100

    merge moved test files

--- tests/returntypes/gradients/test_spsa_gradient_new.py
+++ tests/returntypes/gradients/test_spsa_gradient_new.py
@@ -64,6 +64,7 @@ class TestSpsaGradient:
 
         res = fn(dev.batch_execute(tapes))
         assert isinstance(res, tuple)
+        assert len(res) == 2
 
         assert isinstance(res[0], numpy.ndarray)
         assert res[0].shape == (4,)
@@ -403,7 +404,6 @@ class TestSpsaGradient:
 
         assert all(t == q for t, q in zip(transform, expected))
 
-    @pytest.mark.skip("The SPSA gradient is not integrated with QNode diff_method yet.")
     def test_special_observable_qnode_differentiation(self):
         """Test differentiation of a QNode on a device supporting a
         special observable that returns an object rather than a number."""
@@ -421,6 +421,9 @@ class TestSpsaGradient:
 
             def __mul__(self, other):
                 return SpecialObject(self.val * other)
+
+            def __rmul__(self, other):
+                return self * other
 
             def __add__(self, other):
                 new = self.val + (other.val if isinstance(other, self.__class__) else other)
