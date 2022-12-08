91f46d404baf2916817754628df49f93ea8781e1
Romain Moyard <rmoyard@gmail.com>
2022-12-08 09:32:41 -0500

    typo

--- tests/ops/qubit/test_hamiltonian.py
+++ tests/ops/qubit/test_hamiltonian.py
@@ -48,9 +48,9 @@ except ImportError:
     pass
 
 try:
-    import torch as t
+    from torch import tensor
 
-    COEFFS_PARAM_INTERFACE.append((t.tensor([-0.05, 0.17]), t.tensor(1.7), "torch"))
+    COEFFS_PARAM_INTERFACE.append((tensor([-0.05, 0.17]), tensor(1.7), "torch"))
 except ImportError:
     pass
 
