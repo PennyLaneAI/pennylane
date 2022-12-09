cab667dfc2287aacd20628e676f1fe7adf7f079b
antalszava <antalszava@gmail.com>
2022-12-09 10:34:01 -0500

    Increase testing coverage (#2529)
    * Device coverage
    
    * QubitDevice coverage
    
    * density matrix
    
    * Circuit graph
    
    * str and repr
    
    * default_gaussian fix (TODO: test)
    
    * Make generate_basis_states an instance method, such that even the child implementation of states_to_binary may be used (JAX backprop device does that)
    
    * var PolyXP test added
    
    * add self to states_to_binary lambda in test
    
    * use no cover for TF
    
    * default.qubit.torch; [skip ci]
    
    * comment; [skip ci]
    
    * skip ci
    
    * [skip ci]
    
    * single dispatch coverage
    
    * measurements cov
    
    * numpy cov
    
    * qaoa cov
    
    * add measurements test
    
    * revert updating tests/devices/test_default_qubit_torch.py because the tests suffice in that file
    
    * docstring
    
    * revert the tests/measurements/test_measurements.py file
    
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>

--- tests/circuit_graph/test_circuit_graph.py
+++ tests/circuit_graph/test_circuit_graph.py
@@ -17,6 +17,8 @@ Unit tests for the :mod:`pennylane.circuit_graph` module.
 # pylint: disable=no-self-use,too-many-arguments,protected-access
 
 import numpy as np
+import io
+import contextlib
 import pytest
 
 import pennylane as qml
@@ -187,6 +189,14 @@ class TestCircuitGraph:
         circuit.update_node(ops[0], new)
         assert circuit.operations[0] is new
 
+    def test_update_node_error(self, ops, obs):
+        """Test that changing nodes in the graph may raise an error."""
+        circuit = CircuitGraph(ops, obs, Wires([0, 1, 2]))
+        new = qml.RX(0.1, wires=0)
+        new = qml.CNOT(wires=[0, 1])
+        with pytest.raises(ValueError):
+            circuit.update_node(ops[0], new)
+
     def test_observables(self, circuit, obs):
         """Test that the `observables` property returns the list of observables in the circuit."""
         assert str(circuit.observables) == str(obs)
@@ -297,3 +307,18 @@ class TestCircuitGraph:
 
             for el1, el2 in zip(lst_no_wires, lst_expected):
                 assert qml.equal(el1, el2)
+
+    def test_print_contents(self):
+        """Tests if the circuit prints correct."""
+        ops = [qml.Hadamard(wires=0), qml.CNOT(wires=[0, 1])]
+        obs_w_wires = [qml.measurements.sample(op=None, wires=[0, 1, 2])]
+
+        circuit_w_wires = CircuitGraph(ops, obs_w_wires, wires=Wires([0, 1, 2]))
+
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            circuit_w_wires.print_contents()
+        out = f.getvalue().strip()
+
+        expected = """Operations\n==========\nHadamard(wires=[0])\nCNOT(wires=[0, 1])\n\nObservables\n===========\nsample(wires=[0, 1, 2])"""
+        assert out == expected
