25143c27ec202ed6801105f497dd278ff188ad03
soranjh <soranjh@yahoo.com>
2022-12-09 13:29:53 -0500

    replace np with math

--- pennylane/pauli/utils.py
+++ pennylane/pauli/utils.py
@@ -1301,6 +1301,7 @@ def simplify(h, cutoff=1.0e-12):
             c[o.index(op)] += h.coeffs[i]
 
     coeffs, ops = [], []
+    c = qml.math.convert_like(c, c[0])
     nonzero_ind = np.argwhere(abs(np.array(c)) > cutoff).flatten()
     for i in nonzero_ind:
         coeffs.append(c[i])
@@ -1311,7 +1312,7 @@ def simplify(h, cutoff=1.0e-12):
     except ValueError:
         pass
 
-    return qml.Hamiltonian(np.array(coeffs), ops)
+    return qml.Hamiltonian(qml.math.array(coeffs), ops)
 
 
 def _pauli_mult(p1, p2):
