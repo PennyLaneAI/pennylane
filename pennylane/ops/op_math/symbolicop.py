6b8e970d4cc6eaf70dc1b59b47086689f3b2c474
Astral Cai <astralcai@gmail.com>
2022-12-12 10:10:49 -0500

    Implement QPE for operators (#3373)
    * Implement QPE for QNode
    
    * Change call signature of QPE to taking operators
    
    * Fix bug caused by switching order or target and estimation wires
    
    * Remove list of operators as input, better implementation of qpe
    
    * Only apply operators when there is an active tape
    
    * Moving changlog entry to improvements
    
    * Override queue for QPE
    
    * Wrap matrix unitary in QubitUnitary operator
    
    * Fix matrix size in testcases
    
    * Update pennylane/templates/subroutines/qpe.py
    
    Co-authored-by: Korbinian Kottmann <43949391+Qottmann@users.noreply.github.com>
    
    * Move unitary to hyperparameters
    
    * Fix bug with QPE.queue
    
    * remove data from qpe
    
    Co-authored-by: Korbinian Kottmann <43949391+Qottmann@users.noreply.github.com>
    Co-authored-by: Christina Lee <christina@xanadu.ai>

--- pennylane/ops/op_math/symbolicop.py
+++ pennylane/ops/op_math/symbolicop.py
@@ -33,8 +33,8 @@ class SymbolicOp(Operator):
     This *developer-facing* class can serve as a parent to single base symbolic operators, such as
     :class:`~.ops.op_math.Adjoint` and :class:`~.ops.op_math.Pow`.
 
-    New symbolic operators can inherit from this class to recieve some common default behavior, such
-    as deferring properties to the the base class, copying the base class during a shallow copy, and
+    New symbolic operators can inherit from this class to receive some common default behavior, such
+    as deferring properties to the base class, copying the base class during a shallow copy, and
     updating the metadata of the base operator during queueing.
 
     The child symbolic operator should define the `_name` property during initialization and define
