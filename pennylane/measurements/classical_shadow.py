b3080197f9ea2c40d7ddff820f494fd02948d2bc
AlbertMitjans <a.mitjanscoma@gmail.com>
2022-12-16 13:37:58 +0100

    revert

--- pennylane/measurements/classical_shadow.py
+++ pennylane/measurements/classical_shadow.py
@@ -322,7 +322,7 @@ class ClassicalShadowMP(MeasurementTransform):
 
         # the first entry of the tensor represents the measured bits,
         # and the second indicate the indices of the unitaries used
-        return (2, device.shots, len(self.wires))
+        return (1, 2, device.shots, len(self.wires))
 
     def __copy__(self):
         return self.__class__(
@@ -368,7 +368,7 @@ class ShadowExpvalMP(MeasurementTransform):
         return ShadowExpval
 
     def shape(self, device=None):
-        return ()
+        return (1,)
 
     @property
     def wires(self):
