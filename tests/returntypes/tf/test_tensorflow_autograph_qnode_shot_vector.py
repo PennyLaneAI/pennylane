962c60bae432ce9f2f83e07db7295f461b217333
dwierichs <davidwierichs@gmail.com>
2022-12-27 15:21:31 +0100

    seed

--- tests/returntypes/tf/test_tensorflow_autograph_qnode_shot_vector.py
+++ tests/returntypes/tf/test_tensorflow_autograph_qnode_shot_vector.py
@@ -397,6 +397,7 @@ class TestReturnShotVectorIntegration:
     ):
         """Tests correct output shape and evaluation for a tape
         with a single expval output"""
+        tf.random.set_seed(21)
         dev = qml.device(dev_name, wires=2, shots=shots)
         x = tf.Variable(0.543, dtype=tf.float64)
         y = tf.Variable(-0.654, dtype=tf.float64)
