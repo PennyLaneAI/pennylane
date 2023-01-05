79359f56474ef0d41bdfc0650977936d9e1be71f
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-01-05 16:51:40 +0100

    :rewind: revert (adjoint): Move batching methods back to Adjoint.

--- pennylane/ops/op_math/adjoint_class.py
+++ pennylane/ops/op_math/adjoint_class.py
@@ -201,6 +201,14 @@ class Adjoint(SymbolicOp):
     def __repr__(self):
         return f"Adjoint({self.base})"
 
+    # pylint: disable=protected-access
+    def _check_batching(self, params):
+        self.base._check_batching(params)
+
+    @property
+    def batch_size(self):
+        return self.base.batch_size
+
     @property
     def ndim_params(self):
         return self.base.ndim_params
