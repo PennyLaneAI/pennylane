ffd3d1953822408023c07220f553907bab76afd4
lillian542 <Lillian.frederiksen@xanadu.ai>
2023-01-09 17:01:30 -0500

    Import TDHamiltonian as qml.ops.TDHamiltonian

--- pennylane/ops/qubit/__init__.py
+++ pennylane/ops/qubit/__init__.py
@@ -36,6 +36,7 @@ from .parametric_ops import *
 from .qchem_ops import *
 from .state_preparation import *
 from .hamiltonian import Hamiltonian
+from .time_dependent_hamiltonian import TDHamiltonian
 from ..identity import Identity
 from ..snapshot import Snapshot
 
@@ -117,6 +118,7 @@ obs = {
     "Projector",
     "SparseHamiltonian",
     "Hamiltonian",
+    "TDHamiltonian",
 }
 
 
