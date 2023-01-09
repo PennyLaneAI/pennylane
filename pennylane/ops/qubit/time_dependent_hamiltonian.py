107b1322c72a84938fcafedf557a8a79af096363
lillian542 <Lillian.frederiksen@xanadu.ai>
2023-01-09 16:24:47 -0500

    Initial draft of time dependent hamiltonian

---
+++ pennylane/ops/qubit/time_dependent_hamiltonian.py
@@ -0,0 +1,210 @@
+"""
+This submodule contains the time-dependent hamiltonian class
+"""
+
+import numbers
+from jax import numpy as jnp
+
+import pennylane as qml
+from pennylane.operation import Observable, Tensor
+from pennylane.ops.qubit.hamiltonian import Hamiltonian
+
+
+# pylint: disable= too-many-instance-attributes
+class TDHamiltonian(Observable):
+    r"""Operator representing a time-dependent Hamiltonian.
+
+    The Hamiltonian is represented as a linear combination of other operators, e.g.,
+    :math:`\sum_{k=0}^{N-1} c_k O_k`, where the :math:`c_k` are trainable parameters,
+    some of which are time-dependent.
+
+    Args:
+        coeffs (Union[tensor_like, callable]): coefficients of the Hamiltonian expression.
+        observables (Iterable[Observable]): observables in the Hamiltonian expression, of same length as coeffs
+        id (str): Custom string to label a specific operator instance.
+        do_queue (bool): indicates whether the operator should be
+            recorded when created in a tape context
+
+    **Example:**
+
+    A time-dependent Hamiltonian can be created by passing a list of coefficients and callables,
+    as well as the list of observables.
+
+    >>> def f1(params, t): return jnp.polyval(params, t)
+    >>> coeffs = [0.2, f1]
+    >>> obs = [qml.PauliX(0) @ qml.PauliZ(1), qml.PauliY(0) @ qml.Identity(1)]
+    >>> H = TDHamiltonian(coeffs, obs)
+    >>> print(H)
+      (-0.543) [Z0 H2]   # ToDo: does not print nicely, and this output is incorrect
+    + (0.2) [X0 Z1]
+
+    The coefficients can be a trainable tensor, for example:  # ToDo: Can parameters be a trainable tensor? Update example regardless.
+
+    >>> coeffs = tf.Variable([0.2, -0.543], dtype=tf.double)
+    >>> obs = [qml.PauliX(0) @ qml.PauliZ(1), qml.PauliZ(0) @ qml.Hadamard(2)]
+    >>> H = qml.Hamiltonian(coeffs, obs)
+    >>> print(H)
+      (-0.543) [Z0 H2]
+    + (0.2) [X0 Z1]
+
+    In many cases, Hamiltonians can be constructed using Pythonic arithmetic operations.
+    For example:
+
+    >>> qml.Hamiltonian([1.], [qml.PauliX(0)]) + 2 * qml.PauliZ(0) @ qml.PauliZ(1)
+
+    is equivalent to the following Hamiltonian:
+
+    >>> qml.Hamiltonian([1, 2], [qml.PauliX(0), qml.PauliZ(0) @ qml.PauliZ(1)])
+
+    Addition of TDHamiltonians with TDHamiltonians, Hamiltonians or other observables is possible with
+    tensor-valued coefficients, i.e.,
+
+    >>> H1 = qml.Hamiltonian(torch.tensor([1.]), [qml.PauliX(0)])
+    >>> H2 = qml.Hamiltonian(torch.tensor([2., 3.]), [qml.PauliY(0), qml.PauliX(1)])
+    >>> obs3 = [qml.PauliX(0), qml.PauliY(0), qml.PauliX(1)]
+    >>> H3 = qml.Hamiltonian(torch.tensor([1., 2., 3.]), obs3)
+    >>> H3.compare(H1 + H2)  # ToDo: compare function not implemented, modify documentation
+
+    # ToDo: do we want any grouping behaviour akin to options on Hamiltonian class?
+
+    """
+
+    num_wires = qml.operation.AnyWires
+    grad_method = "A"  # supports analytic gradients
+
+    def __init__(
+        self,
+        coeffs,
+        observables,
+        id=None,
+        do_queue=True,
+    ):
+
+        if qml.math.shape(coeffs)[0] != len(observables):
+            raise ValueError(
+                "Could not create valid Hamiltonian; "
+                "number of coefficients and operators does not match."
+            )
+
+        for obs in observables:
+            if not isinstance(obs, Observable):
+                raise ValueError(
+                    "Could not create circuits. Some or all observables are not valid."
+                )
+
+        self._ops = list(observables)
+        self._coeffs = coeffs
+
+        self.H_drift_coeffs = []
+        self.H_drift_ops = []
+        self.H_ts_fs = []
+        self.H_ts_ops = []
+
+        # ToDo: so far this only deals with scalar coefficients for H_drift - should it accept tensor_like?
+
+        for coeff, obs in zip(coeffs, observables):
+            if callable(coeff):
+                self.H_ts_fs.append(coeff)
+                self.H_ts_ops.append(obs)
+            else:
+                self.H_drift_coeffs.append(coeff)
+                self.H_drift_ops.append(obs)
+
+        if len(self.H_ts_ops) == 0:
+            raise RuntimeError(
+                "Found no time-dependent terms while initializing time dependent Hamiltonian "
+                f"with coefficients {self.coeffs}"
+            )
+
+        self.H_drift = self._H_drift
+        self.H_ts = {"functions": self.H_ts_fs, "observables": self.H_ts_ops}
+
+        # do we want this?
+        self._hyperparameters = {"ops": self._ops}
+
+        self._wires = qml.wires.Wires.all_wires([op.wires for op in self.ops], sort=True)
+
+        self.return_type = None
+
+        super().__init__(*self._coeffs, wires=self._wires, id=id, do_queue=do_queue)
+
+    def __call__(self, params, t, wire_order=None):
+        # TODO: jax-optimize this?
+        if wire_order is None:
+            wire_order = self.wires
+
+        if len(self.H_ts_ops) == 1:
+            f = self.H_ts_fs[0]
+            op = self.H_ts_ops[0]
+            H = jnp.array(
+                [qml.matrix(self.H_drift, wire_order=wire_order)]
+                + [f(params, t) * op.matrix(wire_order=wire_order)]
+            )
+            print(H)
+            return jnp.sum(H, axis=0)
+
+        H = jnp.array(
+            [qml.matrix(self.H_drift, wire_order=wire_order)]
+            + [
+                f(params, t) * op.matrix(wire_order=wire_order)
+                for f, op in zip(self.H_ts_fs, self.H_ts_ops)
+            ]
+        )
+        print(H)
+        return jnp.sum(H, axis=0)
+
+    @property
+    def _H_drift(self):
+        terms_list = [
+            qml.s_prod(coeff, ob) for coeff, ob in zip(self.H_drift_coeffs, self.H_drift_ops)
+        ]
+        if len(terms_list) == 0:
+            return None
+        if len(terms_list) == 1:
+            return terms_list[0]
+        return qml.op_sum(*terms_list)
+
+    @property
+    def coeffs(self):
+        """Return the coefficients defining the Hamiltonian.
+
+        Returns:
+            Iterable[float]): coefficients in the Hamiltonian expression
+        """
+        return self._coeffs
+
+    @property
+    def ops(self):
+        """Return the operators defining the Hamiltonian.
+
+        Returns:
+            Iterable[Observable]): observables in the Hamiltonian expression
+        """
+        return self._ops
+
+    @property
+    def name(self):
+        return "Time-Dependent Hamiltonian"
+
+    def __add__(self, H):
+        r"""The addition operation between a time dependent Hamiltonian and a
+        Hamiltonian or time-dependent Hamiltonian."""
+        ops = self.ops.copy()
+        coeffs = self.coeffs.copy()
+
+        if isinstance(H, numbers.Number) and H == 0:
+            return self
+
+        if isinstance(H, (Hamiltonian, TDHamiltonian)):
+            coeffs.extend(H.coeffs.copy())
+            ops.extend(H.ops.copy())
+            return TDHamiltonian(coeffs, ops)
+
+        if isinstance(H, (Tensor, Observable)):
+            coeffs.append(qml.math.cast_like([1.0], coeffs))
+            ops.append(H)
+            return TDHamiltonian(coeffs, ops)
+
+        raise ValueError(f"Cannot add Hamiltonian and {type(H)}")
+
+    __radd__ = __add__
