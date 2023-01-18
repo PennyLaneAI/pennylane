e0b0e307adbd74318cfa7052a7c3afa730bb2b93
Albert Mitjans <a.mitjanscoma@gmail.com>
2023-01-18 19:03:40 +0100

    Update pennylane/ops/op_math/parametrized_hamiltonian.py
    Co-authored-by: Korbinian Kottmann <43949391+Qottmann@users.noreply.github.com>

--- pennylane/ops/op_math/parametrized_hamiltonian.py
+++ pennylane/ops/op_math/parametrized_hamiltonian.py
@@ -108,7 +108,6 @@ class ParametrizedHamiltonian:
 
     >>> H.H_fixed()
     2*(PauliX(wires=[0]) @ PauliX(wires=[1]))
-
     >>> H.H_parametrized([2.5, 3.6], 0.5)
     (0.9489846193555862*(PauliY(wires=[0]) @ PauliY(wires=[1]))) + (3.159297222805342*(PauliZ(wires=[0]) @ PauliZ(wires=[1])))
     """
