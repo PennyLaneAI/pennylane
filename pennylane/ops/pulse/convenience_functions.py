13c45ffbb3d56d275a8e3776a00536683d20c2d5
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-01-19 16:51:16 +0100

    :bug: fix (window): Make window jittable.

--- pennylane/ops/pulse/convenience_functions.py
+++ pennylane/ops/pulse/convenience_functions.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """This file contains the ``piecewise`` pulse convenience function."""
+from typing import Callable, Tuple, Union
+
 has_jax = True
 try:
     import jax.numpy as jnp
@@ -19,18 +21,17 @@ except ImportError:
     has_jax = False
 
 
-def window(x, ti, tf):
-    """Evaluates the given function/scalar ``x``inside the time window ``[ti, tf]``.
+def window(x: Union[float, Callable], *cond: Tuple[float, Tuple[float]]):
+    """Evaluates the given function/scalar ``x``inside the time windows defined in ``cond``.
 
     .. note::
 
         If ``x`` is a callable, it must accepts two arguments: the trainable parameters and time.
 
     Args:
-        x (float | callable): a scalar or a function that accepts two arguments: the trainable
+        x (Union[float, Callable]): a scalar or a function that accepts two arguments: the trainable
             parameters and time
-        ti (float): initial time
-        tf (float): final time
+        cond (Tuple[float, Tuple[float]]): tuple of tuples containing time windows where x is evaluated
     """
     if not has_jax:
         raise ImportError(
@@ -47,6 +48,6 @@ def window(x, ti, tf):
 
     def _window(p, t):
         p = jnp.array(p, dtype=float)  # if p is an integer, f(p, t) will be cast to an integer
-        return jnp.piecewise(p, [ti <= t <= tf], [f], t)
+        return jnp.where(jnp.any(jnp.array([(t >= ti) & (t <= tf) for ti, tf in cond])), f(p, t), 0)
 
     return _window
