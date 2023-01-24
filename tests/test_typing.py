e60fc480fa0e371af22bb09f45bc6b10e40d33f3
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-01-24 17:27:13 +0100

    :test_tube: tests (pulse): Use __args__.

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -12,26 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Unit tests for the ``typing.py`` file."""
-import sys
-
 import numpy as np
 import pytest
 
 import pennylane.numpy as pnp
 from pennylane.typing import TensorLike
 
-if sys.version_info.minor < 10:  # python version <= 3.10
-    pytestmark = pytest.mark.skip
-
 
 class TestTensor:
     def test_numpy_array(self):
         """Tests that a numpy array is a Tensor"""
-        assert isinstance(np.array(1), TensorLike)
+        # Using `isinstance` with `Union` types is only supported from python 3.10
+        assert isinstance(np.array(1), TensorLike.__args__)
 
     def test_pennylane_tensor(self):
         """Tests that a PennyLane numpy tensor is a Tensor"""
-        assert isinstance(pnp.array(1), TensorLike)
+        assert isinstance(pnp.array(1), TensorLike.__args__)
 
     @pytest.mark.jax
     def test_jax_array_is_tensor_like(self):
@@ -40,7 +36,7 @@ class TestTensor:
 
         tensor = jax.numpy.array(1)
         assert isinstance(tensor, jax.Array)
-        assert isinstance(tensor, TensorLike)
+        assert isinstance(tensor, TensorLike.__args__)
 
     @pytest.mark.torch
     def test_torch_tensor_is_tensor_like(self):
@@ -48,7 +44,7 @@ class TestTensor:
         import torch
 
         tensor = torch.Tensor(1)
-        assert isinstance(tensor, TensorLike)
+        assert isinstance(tensor, TensorLike.__args__)
 
     @pytest.mark.tensorflow
     def test_tf_tensor_is_tensor_like(self):
@@ -57,6 +53,6 @@ class TestTensor:
 
         tensor = tf.constant([1, 2, 3])
         assert isinstance(tensor, tf.Tensor)
-        assert isinstance(tensor, TensorLike)
+        assert isinstance(tensor, TensorLike.__args__)
         var = tf.Variable(9)
-        assert isinstance(var, TensorLike)
+        assert isinstance(var, TensorLike.__args__)
