40c72cdfdeab6f8e65e070af33e7320a25cf4002
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-01-25 17:16:57 +0100

    :bug: fix (typing): Change module name to _typing.

--- pennylane/__init__.py
+++ pennylane/__init__.py
@@ -112,7 +112,7 @@ from .collections import QNodeCollection, dot, map, sum
 import pennylane.gradients  # pylint:disable=wrong-import-order
 import pennylane.qinfo  # pylint:disable=wrong-import-order
 from pennylane.interfaces import execute  # pylint:disable=wrong-import-order
-from .typing import typing
+from ._typing import typing
 
 # Look for an existing configuration file
 default_config = Configuration("config.toml")
