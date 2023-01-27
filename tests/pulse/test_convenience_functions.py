d739a03bdbe1b20ab333ef53c4cb6c94dc2776a3
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-01-27 12:40:33 +0100

    :test_tube: tests (constant): Fix test.

--- tests/pulse/test_convenience_functions.py
+++ tests/pulse/test_convenience_functions.py
@@ -153,10 +153,8 @@ class TestIntegration:
         H = qml.ops.dot(coeffs, ops)
 
         assert isinstance(H, ParametrizedHamiltonian)
-        # assert that at t=2 both functions are 0
-        assert qml.math.allequal(qml.matrix(H(params=[1, 2], t=2)), 0)
-        # assert that at t=0.3 only the first coefficient is non-zero
-        true_mat = qml.matrix(f1(1, 0.3) * qml.PauliX(0), wire_order=[0, 1])
+        # assert that at t=0.3 both coefficients are non-zero
+        true_mat = qml.matrix(f1(1, 0.3) * qml.PauliX(0) + 2 * qml.PauliY(1), wire_order=[0, 1])
         assert qml.math.allclose(qml.matrix(H(params=[1, 2], t=0.3)), true_mat)
         # assert that at t=0.7 only the second coefficient is non-zero
         true_mat = qml.matrix(2 * qml.PauliY(1), wire_order=[0, 1])
