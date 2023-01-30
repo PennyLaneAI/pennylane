Fix parameter shift hessian

tests/gradients/test_parameter_shift_hessian.py
@@ -1331,7 +1331,7 @@ class TestInterfaces:
 
         expected = qml.jacobian(qml.jacobian(circuit))(x_np)
         circuit.interface = "jax"
-        hess = qml.gradients.param_shift_hessian(circuit)(x_jax)
+        hess = qml.gradients.param_shift_hessian(circuit, argnum=[0, 1])(x_jax)
 
         assert np.allclose(expected, hess)
 