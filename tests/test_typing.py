8aab6c27e50ad3c7f8bdb7270586749458adb1c2
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-02-01 16:23:41 +0100

    :test_tube: tests (typing): Add test.

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -16,17 +16,26 @@ import numpy as np
 import pytest
 
 import pennylane.numpy as pnp
-from pennylane import typing
+from pennylane.typing import TensorLike
 
 
 class TestTensorLike:
+    def test_isinstance_unknown_type(self):
+        """Test that an unknown type returns False."""
+
+        # pylint: disable=too-few-public-methods
+        class UnknownType:
+            """Unknown type."""
+
+        assert not isinstance(UnknownType(), TensorLike)
+
     def test_isinstance_numpy_array(self):
         """Tests that a numpy array is a Tensor"""
-        assert isinstance(np.array(1), typing.TensorLike)
+        assert isinstance(np.array(1), TensorLike)
 
     def test_isinstance_pennylane_tensor(self):
         """Tests that a PennyLane numpy tensor is a Tensor"""
-        assert isinstance(pnp.array(1), typing.TensorLike)
+        assert isinstance(pnp.array(1), TensorLike)
 
     @pytest.mark.jax
     def test_isinstance_jax_array_is_tensor_like(self):
@@ -34,7 +43,7 @@ class TestTensorLike:
         import jax
 
         tensor = jax.numpy.array(1)
-        assert isinstance(tensor, typing.TensorLike)
+        assert isinstance(tensor, TensorLike)
 
     @pytest.mark.torch
     def test_isinstance_torch_tensor_is_tensor_like(self):
@@ -42,7 +51,7 @@ class TestTensorLike:
         import torch
 
         tensor = torch.Tensor(1)
-        assert isinstance(tensor, typing.TensorLike)
+        assert isinstance(tensor, TensorLike)
 
     @pytest.mark.tf
     def test_isinstance_tf_tensor_is_tensor_like(self):
@@ -51,36 +60,45 @@ class TestTensorLike:
 
         tensor = tf.constant([1, 2, 3])
         assert isinstance(tensor, tf.Tensor)
-        assert isinstance(tensor, typing.TensorLike)
+        assert isinstance(tensor, TensorLike)
         var = tf.Variable(9)
-        assert isinstance(var, typing.TensorLike)
+        assert isinstance(var, TensorLike)
+
+    def test_subclass_unknown_type(self):
+        """Test that an unknown type returns False."""
+
+        # pylint: disable=too-few-public-methods
+        class UnknownType:
+            """Unknown type."""
+
+        assert not issubclass(UnknownType, TensorLike)
 
     def test_subclass_numpy_array(self):
         """Tests that a numpy array is a Tensor"""
-        assert issubclass(np.ndarray, typing.TensorLike)
+        assert issubclass(np.ndarray, TensorLike)
 
     def test_subclass_pennylane_tensor(self):
         """Tests that a PennyLane numpy tensor is a Tensor"""
-        assert issubclass(pnp.ndarray, typing.TensorLike)
+        assert issubclass(pnp.ndarray, TensorLike)
 
     @pytest.mark.jax
     def test_subclass_jax_array_is_tensor_like(self):
         """Tests that a jax DeviceArray is a Tensor"""
         import jax
 
-        assert issubclass(jax.numpy.ndarray, typing.TensorLike)
+        assert issubclass(jax.numpy.ndarray, TensorLike)
 
     @pytest.mark.torch
     def test_subclass_torch_tensor_is_tensor_like(self):
         """Tests that a torch Tensor is a Tensor"""
         import torch
 
-        assert issubclass(torch.Tensor, typing.TensorLike)
+        assert issubclass(torch.Tensor, TensorLike)
 
     @pytest.mark.tf
     def test_subclass_tf_tensor_is_tensor_like(self):
         """Tests that a tensorflow Tensor is a Tensor"""
         import tensorflow as tf
 
-        assert issubclass(tf.Tensor, typing.TensorLike)
-        assert issubclass(tf.Variable, typing.TensorLike)
+        assert issubclass(tf.Tensor, TensorLike)
+        assert issubclass(tf.Variable, TensorLike)
