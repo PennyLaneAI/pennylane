ef9bf9fdf5b5d5839b5685353dcda1fb30cb1f74
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-02-03 15:06:20 +0100

    :memo: docs (evolve): Remove docstrings of dispatched functions.

--- pennylane/ops/functions/evolve.py
+++ pennylane/ops/functions/evolve.py
@@ -86,59 +86,13 @@ def evolve(*args, **kwargs):  # pylint: disable=unused-argument
     """
 
 
+# pylint: disable=missing-docstring
 @evolve.register
-def parametrized_evolution(op: ParametrizedHamiltonian):
-    """Returns a new operator that computes the evolution of ``op``.
-
-    Args:
-        op (.ParametrizedHamiltonian): operator to evolve
-
-    Returns:
-        ~pennylane.ops.op_math.evolve.ParametrizedEvolution: evolution operator
-
-    .. seealso:: :class:`.ParametrizedEvolution`
-
-    **Examples**
-
-    When evolving a :class:`.ParametrizedHamiltonian` class, then a :class:`.ParametrizedEvolution`
-    instance is returned:
-
-    >>> coeffs = [lambda p, t: p * t for _ in range(4)]
-    >>> ops = [qml.PauliX(i) for i in range(4)]
-    >>> H = qml.dot(coeffs, ops)
-    >>> qml.evolve(H)
-    ParametrizedEvolution(wires=[0, 1, 2, 3])
-
-    The :class:`.ParametrizedEvolution` instance can then be called to update the needed attributes
-    to compute the evolution of the :class:`.ParametrizedHamiltonian`:
-
-    >>> qml.evolve(H)(params=[1., 2., 3.], t=[4, 10], atol=1e-6, mxstep=1)
-    ParametrizedEvolution(wires=[0, 1, 2, 3])
-
-    Please check the :class:`.ParametrizedEvolution` class for more information."""
-
+def parametrized_hamiltonian(op: ParametrizedHamiltonian):
     return ParametrizedEvolution(H=op)
 
 
+# pylint: disable=missing-docstring
 @evolve.register
 def evolution(op: Operator, coeff: float = 1):
-    r"""Returns a new operator that computes the evolution of ``op``.
-
-    Args:
-        op (.Operator): operator to evolve
-        coeff (float): coefficient multiplying the exponentiated operator: :math:`\exp\{i\bm{O}x)\}`.
-
-    Returns:
-        .Evolution: evolution operator
-
-    .. seealso:: :class:`~.Evolution`
-
-    **Examples**
-
-    We can use ``qml.evolve`` to compute the evolution of any PennyLane operator:
-
-    >>> op = qml.evolve(qml.PauliX(0), coeff=2)
-    >>> op
-    Exp(2j PauliX)"""
-
     return Evolution(op, coeff)
