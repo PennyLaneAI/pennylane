00b3e486b7d30f417c8fc7997c812a1d799b6f20
Albert Mitjans <a.mitjanscoma@gmail.com>
2023-02-07 10:25:49 +0100

    Update pennylane/ops/op_math/exp.py
    Co-authored-by: lillian542 <38584660+lillian542@users.noreply.github.com>

--- pennylane/ops/op_math/exp.py
+++ pennylane/ops/op_math/exp.py
@@ -50,7 +50,7 @@ def exp(op, coeff=1, num_steps=None, id=None):
         num_steps (int): The number of steps used in the decomposition of the exponential operator,
             also known as the Trotter number. If this value is `None` and the Suzuki-Trotter
             decomposition is needed, an error will be raised.
-        id (str): id for the Sum operator. Default is None.
+        id (str): id for the Exp operator. Default is None.
 
     Returns:
        :class:`Exp`: A :class`~.operation.Operator` representing an operator exponential.
