dd1347d4486e1580b492b5b7f1cb2f6a0bfa9500
Mudit Pandey <mudit.pandey@xanadu.ai>
2023-02-09 16:50:46 -0500

    Made ctrl_decomp_zyz more readable

--- pennylane/ops/op_math/controlled_decompositions.py
+++ pennylane/ops/op_math/controlled_decompositions.py
@@ -91,16 +91,24 @@ def ctrl_decomp_zyz(target_operation: Operator, control_wires: Wires):
             )[0]
         phi, theta, omega = zyz_decomp.single_qubit_rot_angles()
 
-    angles = [phi, theta / 2, -theta / 2, -(phi + omega) / 2, (omega - phi) / 2]
-    ops = [qml.RZ, qml.RY, qml.RY, qml.RZ, qml.RZ]
-
     decomp = []
-    for i, (op, angle) in enumerate(zip(ops, angles)):
-        if not qml.math.isclose(angle, 0.0, atol=1e-8, rtol=0):
-            # Add rotation to decomposition if non-trivial
-            decomp.append(op(angle, wires=target_wire))
-        if i % 2 == 1:
-            # Add MultiControlledX to decomposition
-            decomp.append(qml.MultiControlledX(wires=control_wires + target_wire))
+
+    if not qml.math.isclose(phi, 0.0, atol=1e-8, rtol=0):
+        decomp.append(qml.RZ(phi, wires=target_wire))
+    if not qml.math.isclose(theta / 2, 0.0, atol=1e-8, rtol=0):
+        decomp.extend(
+            [
+                qml.RY(theta / 2, wires=target_wire),
+                qml.MultiControlledX(wires=control_wires + target_wire),
+                qml.RY(-theta / 2, wires=target_wire),
+            ]
+        )
+    else:
+        decomp.append(qml.MultiControlledX(wires=control_wires + target_wire))
+    if not qml.math.isclose(-(phi + omega) / 2, 0.0, atol=1e-6, rtol=0):
+        decomp.append(qml.RZ(-(phi + omega) / 2, wires=target_wire))
+    decomp.append(qml.MultiControlledX(wires=control_wires + target_wire))
+    if not qml.math.isclose((omega - phi) / 2, 0.0, atol=1e-8, rtol=0):
+        decomp.append(qml.RZ((omega - phi) / 2, wires=target_wire))
 
     return decomp
