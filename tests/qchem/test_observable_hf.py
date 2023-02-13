1a1e449d142f277234a2ff9124a02a006000c9d2
soranjh <soranjh@yahoo.com>
2023-02-13 14:29:39 -0500

    add none for natation in existing test

--- tests/qchem/test_observable_hf.py
+++ tests/qchem/test_observable_hf.py
@@ -243,19 +243,21 @@ def test_qubit_observable(f_observable, q_observable):
 
 
 @pytest.mark.parametrize(
-    ("f_obs", "q_obs"),
+    ("f_obs", "q_obs", "notation"),
     [
         (
             [0],
             # trivial case of a creation operator, 0^ -> (X_0 - iY_0) / 2
             # reformatted the original openfermion output: (0.5+0j) [] + (-0.5+0j) [Z0]
             ([(0.5 + 0j), (0.0 - 0.5j)], [qml.PauliX(0), qml.PauliY(0)]),
+            None,
         ),
         (
             [0, 0],
             # obtained with openfermion using: jordan_wigner(FermionOperator('0^ 0', 1))
             # reformatted the original openfermion output: (0.5+0j) [] + (-0.5+0j) [Z0]
             ([(0.5 + 0j), (-0.5 + 0j)], [qml.Identity(0), qml.PauliZ(0)]),
+            None,
         ),
         (
             [3, 0],
@@ -270,6 +272,7 @@ def test_qubit_observable(f_observable, q_observable):
                     qml.PauliY(0) @ qml.PauliZ(1) @ qml.PauliZ(2) @ qml.PauliY(3),
                 ],
             ),
+            None,
         ),
         (
             [1, 4],
@@ -284,6 +287,7 @@ def test_qubit_observable(f_observable, q_observable):
                     qml.PauliY(1) @ qml.PauliZ(2) @ qml.PauliZ(3) @ qml.PauliY(4),
                 ],
             ),
+            None,
         ),
         (
             [3, 1, 3, 1],
@@ -293,12 +297,13 @@ def test_qubit_observable(f_observable, q_observable):
                 [(-0.25 + 0j), (0.25 + 0j), (-0.25 + 0j), (0.25 + 0j)],
                 [qml.Identity(0), qml.PauliZ(1), qml.PauliZ(1) @ qml.PauliZ(3), qml.PauliZ(3)],
             ),
+            None,
         ),
     ],
 )
-def test_jordan_wigner(f_obs, q_obs):
+def test_jordan_wigner(f_obs, q_obs, notation):
     r"""Test that jordan_wigner returns the correct operator."""
-    res = qchem.jordan_wigner(f_obs)
+    res = qchem.jordan_wigner(f_obs, notation=notation)
     assert qml.Hamiltonian(res[0], res[1]).compare(qml.Hamiltonian(q_obs[0], q_obs[1]))
 
 
