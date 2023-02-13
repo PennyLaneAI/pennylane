db1710c49a94713dcc1ac98681855987e7172717
lillian542 <Lillian.frederiksen@xanadu.ai>
2023-02-13 15:56:52 -0500

    start

---
+++ pennylane/devices/qubit/adjoint_jacobian.py
@@ -0,0 +1,183 @@
+# Copyright 2018-2023 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Functions to apply adjoint jacobian differentiation"""
+
+import warnings
+import numpy as np
+
+import pennylane as qml
+from pennylane.math import sum as qmlsum
+from pennylane.measurements import (
+    ExpectationMP,
+)
+from pennylane.operation import operation_derivative
+from pennylane.tape import QuantumTape
+
+# from .apply_operation import apply_operation
+# from .initialize_state import create_initial_state
+
+# pylint: disable=protected-access, too-many-branches
+
+
+def adjoint_jacobian(
+    self, tape: QuantumTape, starting_state=None, use_device_state=False
+):  # pylint: disable=too-many-statements
+    """Implements the adjoint method outlined in
+    `Jones and Gacon <https://arxiv.org/abs/2009.02823>`__ to differentiate an input tape.
+
+    After a forward pass, the circuit is reversed by iteratively applying adjoint
+    gates to scan backwards through the circuit.
+
+    .. note::
+        The adjoint differentiation method has the following restrictions:
+
+        * As it requires knowledge of the statevector, only statevector simulator devices can be
+          used.
+
+        * Only expectation values are supported as measurements.
+
+        * Does not work for parametrized observables like
+          :class:`~.Hamiltonian` or :class:`~.Hermitian`.
+
+    Args:
+        tape (.QuantumTape): circuit that the function takes the gradient of
+
+    Keyword Args:
+        starting_state (tensor_like): post-forward pass state to start execution with. It should be
+            complex-valued. Takes precedence over ``use_device_state``.
+        use_device_state (bool): use current device state to initialize. A forward pass of the same
+            circuit should be the last thing the device has executed. If a ``starting_state`` is
+            provided, that takes precedence.
+
+    Returns:
+        array or tuple[array]: the derivative of the tape with respect to trainable parameters.
+        Dimensions are ``(len(observables), len(trainable_params))``.
+
+    Raises:
+        QuantumFunctionError: if the input tape has measurements that are not expectation values
+            or contains a multi-parameter operation aside from :class:`~.Rot`
+    """
+    # broadcasted inner product not summing over first dimension of b
+    sum_axes = tuple(range(1, self.num_wires + 1))
+    # pylint: disable=unnecessary-lambda-assignment
+    dot_product_real = lambda b, k: self._real(qmlsum(self._conj(b) * k, axis=sum_axes))
+
+    for m in tape.measurements:
+        if not isinstance(m, ExpectationMP):
+            raise qml.QuantumFunctionError(
+                "Adjoint differentiation method does not support"
+                f" measurement {m.__class__.__name__}"
+            )
+
+        if m.obs.name == "Hamiltonian":
+            raise qml.QuantumFunctionError(
+                "Adjoint differentiation method does not support Hamiltonian observables."
+            )
+
+        if not hasattr(m.obs, "base_name"):
+            m.obs.base_name = None  # This is needed for when the observable is a tensor product
+
+    if self.shot_vector is not None:
+        raise qml.QuantumFunctionError("Adjoint does not support shot vectors.")
+
+    if self.shots is not None:
+        warnings.warn(
+            "Requested adjoint differentiation to be computed with finite shots."
+            " The derivative is always exact when using the adjoint differentiation method.",
+            UserWarning,
+        )
+
+    # Initialization of state
+    if starting_state is not None:
+        ket = self._reshape(starting_state, [2] * self.num_wires)
+    else:
+        if not use_device_state:
+            self.reset()
+            self.execute(tape)
+        ket = self._pre_rotated_state
+
+    n_obs = len(tape.observables)
+    bras = np.empty([n_obs] + [2] * self.num_wires, dtype=np.complex128)
+    for kk in range(n_obs):
+        bras[kk, ...] = self._apply_operation(ket, tape.observables[kk])
+
+    expanded_ops = []
+    for op in reversed(tape.operations):
+        if op.num_params > 1:
+            if not isinstance(op, qml.Rot):
+                raise qml.QuantumFunctionError(
+                    f"The {op.name} operation is not supported using "
+                    'the "adjoint" differentiation method'
+                )
+            ops = op.decomposition()
+            expanded_ops.extend(reversed(ops))
+        elif op.name not in ("QubitStateVector", "BasisState", "Snapshot"):
+            expanded_ops.append(op)
+
+    trainable_params = []
+    for k in tape.trainable_params:
+        # pylint: disable=protected-access
+        if hasattr(tape._par_info[k]["op"], "return_type"):
+            warnings.warn(
+                "Differentiating with respect to the input parameters of "
+                f"{tape._par_info[k]['op'].name} is not supported with the "
+                "adjoint differentiation method. Gradients are computed "
+                "only with regards to the trainable parameters of the circuit.\n\n Mark "
+                "the parameters of the measured observables as non-trainable "
+                "to silence this warning.",
+                UserWarning,
+            )
+        else:
+            trainable_params.append(k)
+
+    jac = np.zeros((len(tape.observables), len(trainable_params)))
+
+    param_number = len(tape.get_parameters(trainable_only=False, operations_only=True)) - 1
+    trainable_param_number = len(trainable_params) - 1
+    for op in expanded_ops:
+        adj_op = qml.adjoint(op)
+        ket = self._apply_operation(ket, adj_op)
+
+        if op.grad_method is not None:
+            if param_number in trainable_params:
+                d_op_matrix = operation_derivative(op)
+                ket_temp = self._apply_unitary(ket, d_op_matrix, op.wires)
+
+                jac[:, trainable_param_number] = 2 * dot_product_real(bras, ket_temp)
+
+                trainable_param_number -= 1
+            param_number -= 1
+
+        for kk in range(n_obs):
+            bras[kk, ...] = self._apply_operation(bras[kk, ...], adj_op)
+
+    return self._adjoint_jacobian_processing(jac)
+
+
+@staticmethod
+def _adjoint_jacobian_processing(jac):
+    """
+    Post-process the Jacobian matrix returned by ``adjoint_jacobian`` for
+    the new return type system.
+    """
+    jac = np.squeeze(jac)
+
+    if jac.ndim == 0:
+        return np.array(jac)
+
+    if jac.ndim == 1:
+        return tuple(np.array(j) for j in jac)
+
+    # must be 2-dimensional
+    return tuple(tuple(np.array(j_) for j_ in j) for j in jac)
