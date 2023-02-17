0e309ab74157e6999e6fe1abe46cb86c1fbb8dcd
Jay Soni <jbsoni@uwaterloo.ca>
2023-02-17 01:32:38 -0500

    added missing wire_order test

--- tests/pauli/test_pauli_arithmetic.py
+++ tests/pauli/test_pauli_arithmetic.py
@@ -492,7 +492,8 @@ class TestPauliSentence:
     def test_operation_empty(self):
         """Test that an empty PauliSentence with wire_order returns Identity."""
         op = ps5.operation(wire_order=[0, 1])
-        id = qml.Identity(wires=[0, 1])
+        id = qml.s_prod(0.0, qml.Identity(wires=[0, 1]))
+
         assert op.name == id.name
         assert op.wires == id.wires
 
@@ -503,6 +504,13 @@ class TestPauliSentence:
             ps4.operation()
         with pytest.raises(ValueError, match="Can't get the operation for an empty PauliSentence."):
             ps5.operation()
+
+    def test_operation_wire_order(self):
+        """Test that the wire_order parameter is used when the pauli representation is empty"""
+        op = ps5.operation(wire_order=["a", "b"])
+        id = qml.s_prod(0.0, qml.Identity(wires=["a", "b"]))
+
+        assert qml.equal(op, id)
 
     tup_ps_hamiltonian = (
         (PauliSentence({PauliWord({0: X}): 1}), 1 * qml.PauliX(wires=0)),
@@ -534,7 +542,7 @@ class TestPauliSentence:
     def test_hamiltonian_empty(self):
         """Test that an empty PauliSentence with wire_order returns Identity."""
         op = ps5.hamiltonian(wire_order=[0, 1])
-        id = 1 * qml.Identity(wires=[0, 1])
+        id = qml.Hamiltonian([0], [qml.Identity(wires=[0, 1])])
         assert op.compare(id)
 
     def test_hamiltonian_empty_error(self):
@@ -544,6 +552,13 @@ class TestPauliSentence:
             ValueError, match="Can't get the Hamiltonian for an empty PauliSentence."
         ):
             ps5.hamiltonian()
+
+    def test_hamiltonian_wire_order(self):
+        """Test that the wire_order parameter is used when the pauli representation is empty"""
+        op = ps5.hamiltonian(wire_order=["a", "b"])
+        id = qml.Hamiltonian([0], [qml.Identity(wires=["a", "b"])])
+
+        assert qml.equal(op, id)
 
     def test_pickling(self):
         """Check that paulisentences can be pickled and unpickled."""
