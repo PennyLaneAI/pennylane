e09c9c3b5cbd7b5df21a73d7e92b0f620cf32c2c
lillian542 <Lillian.frederiksen@xanadu.ai>
2023-02-22 10:35:47 -0500

    Update state initialization

--- pennylane/devices/qubit/adjoint_jacobian.py
+++ pennylane/devices/qubit/adjoint_jacobian.py
@@ -16,20 +16,22 @@
 import numpy as np
 
 import pennylane as qml
-from pennylane.math import sum as qmlsum
 
 from pennylane.operation import operation_derivative
 from pennylane.tape import QuantumTape
 
 from .apply_operation import apply_operation
 
-# from .initialize_state import create_initial_state
+from .initialize_state import create_initial_state
 
 # pylint: disable=protected-access, too-many-branches
 
 
 def adjoint_jacobian(
-    dev, tape: QuantumTape, starting_state=None, use_device_state=False
+    tape: QuantumTape,
+    prep_operation: qml.operation.StatePrep = None,
+    # starting_state=None,
+    # ToDo: is it okay that this is removed?
 ):  # pylint: disable=too-many-statements
     """Implements the adjoint method outlined in
     `Jones and Gacon <https://arxiv.org/abs/2009.02823>`__ to differentiate an input tape.
@@ -66,23 +68,17 @@ def adjoint_jacobian(
         QuantumFunctionError: if the input tape has measurements that are not expectation values
             or contains a multi-parameter operation aside from :class:`~.Rot`
     """
-    # broadcasted inner product not summing over first dimension of b
-    sum_axes = tuple(range(1, dev.num_wires + 1))
     # pylint: disable=unnecessary-lambda-assignment
-    dot_product_real = lambda b, k: dev._real(
-        qmlsum(dev._conj(b) * k, axis=sum_axes)
-    )  # ToDo: simplify this
 
     # Initialization of state
-    if starting_state is not None:  # ToDo: use new state_preparation fns
-        ket = dev._reshape(starting_state, [2] * dev.num_wires)
-    else:
-        if not use_device_state:
-            dev.reset()
-            dev.execute(tape)
-        ket = dev._pre_rotated_state
+    ket = create_initial_state(
+        wires=tape.wires, prep_operation=prep_operation
+    )  #  ket(0) if prep_operation is None, else
+    for op in tape.operations:
+        ket = apply_operation(op, ket)
 
     # ToDo: compare to demo, make sure it all makes sense
+
     # currently assumes only one observable, no batching
     bra = apply_operation(tape.observables[0], ket)
 
@@ -90,7 +86,7 @@ def adjoint_jacobian(
     for op in reversed(tape.operations):
         if op.num_params > 1:
             ops = op.decomposition()
-            expanded_ops.extend(reversed(ops))
+            expanded_ops.extend(reversed(ops))  # ToDo: why do these need to be expanded?
         elif op.name not in ("QubitStateVector", "BasisState", "Snapshot"):
             expanded_ops.append(op)
 
@@ -101,7 +97,7 @@ def adjoint_jacobian(
     jac = np.zeros((len(tape.observables), len(trainable_params)))
 
     param_number = len(tape.get_parameters(trainable_only=False, operations_only=True)) - 1
-    trainable_param_number = len(trainable_params) - 1
+    trainable_param_number = len(trainable_params) - 1  # ToDo: what's going on here?
     for op in expanded_ops:
         adj_op = qml.adjoint(op)
         ket = apply_operation(adj_op, ket)
@@ -110,21 +106,25 @@ def adjoint_jacobian(
             if param_number in trainable_params:
                 d_op_matrix = operation_derivative(op)
                 ket_temp = apply_operation(qml.QubitUnitary(d_op_matrix, wires=op.wires), ket)
-                jac[:, trainable_param_number] = 2 * dot_product_real([bra], ket_temp)
+                jac[:, trainable_param_number] = 2 * (
+                    qml.math.real(qml.math.vdot(bra, ket_temp))
+                )  # ToDo: is there a reason this was a lambda function instead on the device?
 
                 trainable_param_number -= 1
             param_number -= 1
 
-        jac = np.squeeze(jac)
+        bra = apply_operation(adj_op, bra)
 
-        if jac.ndim == 0:
-            return np.array(jac)
+    jac = np.squeeze(jac)
 
-        if jac.ndim == 1:
-            return tuple(np.array(j) for j in jac)
+    if jac.ndim == 0:
+        return np.array(jac)
 
-        # must be 2-dimensional - I think this is only for batching
-        # ToDo: is this just for batching, and if so should we remove it for now?
-        return tuple(tuple(np.array(j_) for j_ in j) for j in jac)
+    if jac.ndim == 1:
+        return tuple(np.array(j) for j in jac)
+
+    # must be 2-dimensional - I think this is only for batching
+    # ToDo: is this just for batching, and if so should we remove it for now?
+    return tuple(tuple(np.array(j_) for j_ in j) for j in jac)
 
     # ToDo: add tests
