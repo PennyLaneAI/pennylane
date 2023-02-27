9a2bfb64fa8ce1c70a9fd86c7daa525ab82a52fe
lillian542 <38584660+lillian542@users.noreply.github.com>
2023-02-27 11:36:59 -0500

    Raise error if JAX or jaxlib are 0.4.4 (#3813)
    * Raise error for 0.4.4 jaxlib and jax
    
    * add tests
    
    * black for tests
    
    * add jax mark for test
    
    * Remove jaxlib check
    
    Co-authored-by: Filippo Vicentini <filippovicentini@gmail.com>
    
    * More detailed error msg
    
    Co-authored-by: Filippo Vicentini <filippovicentini@gmail.com>
    
    * Update tests
    
    * Tests and black
    
    * Typo
    
    * Change
    
    ---------
    
    Co-authored-by: Filippo Vicentini <filippovicentini@gmail.com>
    Co-authored-by: Romain Moyard <rmoyard@gmail.com>

--- pennylane/devices/default_qubit_jax.py
+++ pennylane/devices/default_qubit_jax.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""This module contains an jax implementation of the :class:`~.DefaultQubit`
+"""This module contains a jax implementation of the :class:`~.DefaultQubit`
 reference plugin.
 """
 # pylint: disable=ungrouped-imports
@@ -30,9 +30,23 @@ try:
 
     from pennylane.pulse.parametrized_hamiltonian_pytree import ParametrizedHamiltonianPytree
 
-
 except ImportError as e:  # pragma: no cover
     raise ImportError("default.qubit.jax device requires installing jax>0.3.20") from e
+
+
+def _validate_jax_version():
+    if jax.__version__ == "0.4.4":
+        raise RuntimeError(
+            "\nYour installed version of JAX is 0.4.4 but Pennylane is incompatible with it.\n\n"
+            "You can either downgrade JAX to version 0.4.3 or update to a more recent version if available."
+            "If you downgrade, you will also need to downgrade JAXLIB to version 0.4.3 or earlier.\n"
+            "If you are using pip to manage your packages, you can run the following command:\n\n"
+            "\tpip install 'jax==0.4.3' 'jaxlib==0.4.3'\n\n"
+            "If you are using conda to manage your packages, you can run the following command:\n\n"
+            "\tconda install 'jax==0.4.3' 'jaxlib==0.4.3'\n\n"
+            "If you still have problems, please open an issue at the following link:\n\n"
+            "\thttps://github.com/PennyLaneAI/pennylane/issues\n"
+        )
 
 
 class DefaultQubitJax(DefaultQubit):
@@ -119,7 +133,7 @@ class DefaultQubitJax(DefaultQubit):
             a = keyed_circuit(key1)
             b = keyed_circuit(key2) # b will be different samples now.
 
-        Check out out the `JAX random documentation <https://jax.readthedocs.io/en/latest/jax.random.html>`__
+        Check out the `JAX random documentation <https://jax.readthedocs.io/en/latest/jax.random.html>`__
         for more information.
 
     Args:
@@ -135,6 +149,8 @@ class DefaultQubitJax(DefaultQubit):
             pseudo random number generator. If None, a random key will be generated.
 
     """
+
+    _validate_jax_version()
 
     name = "Default qubit (jax) PennyLane plugin"
     short_name = "default.qubit.jax"
@@ -163,6 +179,8 @@ class DefaultQubitJax(DefaultQubit):
     _ndim = staticmethod(jnp.ndim)
 
     def __init__(self, wires, *, shots=None, prng_key=None, analytic=None):
+        _validate_jax_version()
+
         if jax_config.read("jax_enable_x64"):
             c_dtype = jnp.complex128
             r_dtype = jnp.float64
