906a6a91307248abfeb3917509e1f5d19d2dc43e
Albert Mitjans <a.mitjanscoma@gmail.com>
2023-02-28 14:35:10 +0100

    Update pennylane/pulse/rydberg_hamiltonian.py
    Co-authored-by: Mudit Pandey <mudit.pandey@xanadu.ai>

--- pennylane/pulse/rydberg_hamiltonian.py
+++ pennylane/pulse/rydberg_hamiltonian.py
@@ -100,7 +100,7 @@ def rydberg_transition(rabi, detuning, phase, wires):
             field
         detuning (Union[float, Callable]): float or callable returning the detuning (in MHz) of a
             laser field
-        phase (float): float containing the phase (in radiants) of the laser field
+        phase (float): float containing the phase (in radians) of the laser field
         wires (Union[int, List[int]]): integer or list containing wire values that the laser field
             acts on
 
