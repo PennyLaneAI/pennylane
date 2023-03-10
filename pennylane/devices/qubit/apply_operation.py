2b6fcec1c48d3510df6dd9b29ea79f575d575bf4
albi3ro <chrissie.c.l@gmail.com>
2023-03-10 11:04:08 -0500

    change back order in multiply function

--- pennylane/devices/qubit/apply_operation.py
+++ pennylane/devices/qubit/apply_operation.py
@@ -211,7 +211,8 @@ def apply_pauliz(op: qml.PauliZ, state, is_state_batched: bool = False):
     sl_1 = _get_slice(1, axis, n_dim)
 
     try:
-        state1 = math.multiply(-1, state[sl_1])
+        # must be first state and then -1 because it breaks otherwise
+        state1 = math.multiply(state[sl_1], -1)
     except Exception as e:
         # slicing fails with tensorflow and states with more than 8 wires.
         # Instead of checking checking for this case all the time, we just fallback
