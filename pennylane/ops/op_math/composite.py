d1c96363fb6c1640e6b7db47a3e29b71c8cebfad
Matthew Silverman <matthews@xanadu.ai>
2023-04-03 16:53:54 -0400

    cache composite operator data

--- pennylane/ops/op_math/composite.py
+++ pennylane/ops/op_math/composite.py
@@ -60,6 +60,7 @@ class CompositeOp(Operator):
         self._hash = None
         self._has_overlapping_wires = None
         self._overlapping_ops = None
+        self._data = None
         self._pauli_rep = self._build_pauli_rep()
 
         if do_queue:
@@ -112,10 +113,12 @@ class CompositeOp(Operator):
     @property
     def data(self):
         """Create data property"""
-        data = []
-        for op in self:
-            data.extend(op.data)
-        return data
+        if self._data is None:
+            data = []
+            for op in self:
+                data.extend(op.data)
+            self._data = data
+        return self._data
 
     @data.setter
     def data(self, new_data):
@@ -124,6 +127,7 @@ class CompositeOp(Operator):
             if op.num_params > 0:
                 op.data = new_data[: op.num_params]
                 new_data = new_data[op.num_params :]
+        self._data = new_data
 
     @property
     def num_wires(self):
