5201ecd20d99404b2eddafd8edba795109f75b5c
Matthew Silverman <matthews@xanadu.ai>
2023-04-03 15:00:54 -0400

    update docs

--- pennylane/measurements/shots.py
+++ pennylane/measurements/shots.py
@@ -37,14 +37,14 @@ class Shots:
     A data class that stores shot information.
 
     Args:
-        shots (Union[None, int, Tuple[int, int], Sequence[int, Tuple[int, int]]]): Raw shot information
+        shots (Union[None, int, Sequence[int, Tuple[int, int]]]): Raw shot information
 
     Defining shots enables users to specify circuit executions, and the Shots class standardizes
     the internal representation of shots. There are three ways to specify shot values:
 
+    * The value ``None``
     * A positive integer
-    * A tuple consisting of a pair of positive integers of the form ``(shots, copies)``
-    * A list of multiple shot values matching one or both of the above value types
+    * A sequence consisting of either positive integers or a tuple-pair of positive integers of the form ``(shots, copies)``
 
     The tuple-pair of the form ``(shots, copies)`` is represented internally by a namedtuple called
     ``ShotCopies``. The first value is the number of shots to execute, and the second value is the
@@ -58,6 +58,54 @@ class Shots:
     Instances of this class are static. If an instance is passed to the constructor, that same
     instance is returned. If an instance is constructed with a ``None`` value, ``total_shots``
     will be ``None``, and this will suggest analytic execution.
+
+    **Examples**
+
+    Example constructing a Shots instance with ``None``:
+
+    >>> shots = Shots(None)
+    >>> shots.total_shots, shots.shot_vector
+    (None, ())
+
+    Example constructing a Shots instance with an int:
+
+    >>> shots = Shots(100)
+    >>> shots.total_shots, shots.shot_vector
+    (100, (ShotCopies(shots=100, copies=1),))
+
+    Example constructing a Shots instance with another instance:
+
+    >>> shots = Shots(100)
+    >>> Shots(shots) is shots
+    True
+
+    Example constructing a Shots instance with a sequence of ints:
+
+    >>> shots = Shots([100, 200])
+    >>> shots.total_shots, shots.shot_vector
+    (300, (ShotCopies(shots=100, copies=1), ShotCopies(shots=200, copies=1)))
+
+    Example constructing a Shots instance with a sequence of tuple-pairs:
+
+    >>> shots = Shots(((100, 3), (200, 4),))
+    >>> shots.total_shots, shots.shot_vector
+    (1100, (ShotCopies(shots=100, copies=3), ShotCopies(shots=200, copies=4)))
+
+    Example constructing a Shots instance with a sequence of both ints and tuple-pairs.
+    Note that the first stand-alone ``100`` gets absorbed into the subsequent tuple because the
+    shot value matches:
+
+    >>> shots = Shots((10, 100, (100, 3), (200, 4),))
+    >>> shots.total_shots, shots.shot_vector
+    (1210, (ShotCopies(shots=10, copies=1), ShotCopies(shots=100, copies=4), ShotCopies(shots=200, copies=4)))
+
+    One should also note that specifying a single tuple of length 2 is considered two different
+    shot values, and _not_ a tuple-pair representing shots and copies to avoid special behaviour
+    depending on the iterable type:
+
+    >>> shots = Shots((100, 2))
+    >>> shots.total_shots, shots.shot_vector
+    (102, (ShotCopies(shots=100, copies=1), ShotCopies(shots=2, copies=1)))
     """
 
     total_shots: int = None
@@ -118,7 +166,7 @@ class Shots:
 
     @property
     def has_partitioned_shots(self):
-        """Checks if the device was instructed to perform executions with partitioned shots.
+        """Checks if this instance represents multiple shot quantities, or the same shot quantity repeated multiple times.
 
         Returns:
             bool: whether or not shots are partitioned
