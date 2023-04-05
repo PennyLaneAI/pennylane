1b173ee707d91efc9979d2e7561a99eb750ba2b8
Diego <67476785+DSGuala@users.noreply.github.com>
2023-04-05 08:54:34 -0400

    Update pennylane/templates/subroutines/qsvt.py
    Co-authored-by: soranjh <40344468+soranjh@users.noreply.github.com>

--- pennylane/templates/subroutines/qsvt.py
+++ pennylane/templates/subroutines/qsvt.py
@@ -33,7 +33,7 @@ def qsvt(A, angles, wires, convention=None):
         A (tensor_like): the general :math:`(n \times m)` matrix to be encoded.
         angles (tensor_like): a list of angles by which to shift to obtain the desired polynomial.
         wires (Iterable): the wires the template acts on.
-        convenction (string): can be set to "Wx" to convert quantum signal processing angles to QSVT
+        convention (string): can be set to "Wx" to convert quantum signal processing angles to QSVT
         angles
 
     .. details::
