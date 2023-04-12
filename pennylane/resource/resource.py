1825ed9e9dc71a8e099fb9e4b275ccbd0c678410
Jay Soni <jbsoni@uwaterloo.ca>
2023-04-12 14:57:35 -0400

    update implementation to prevent updating attributes

--- pennylane/resource/resource.py
+++ pennylane/resource/resource.py
@@ -15,22 +15,26 @@
 The data class which will aggregate all the resource information from a quantum workflow.
 """
 from copy import copy
-from typing import Iterable
 from functools import reduce
 from collections import defaultdict
+from typing import Iterable, DefaultDict
 
 
 class Resources:
     r"""Contains attributes which store key resources (such as number of gates, number of wires, etc.)
     tracked over a quantum workflow.
 
-    This container object tracks the following resources as attributes:
-    - num_wires (int): number of qubits
-    - num_gates (int): number of gates
-    - depth (int): the depth of the circuit (max number of non-parallel operations)
-    - shots (int): number of sampels to measure
-    - gate_types (defaultdict(int)): dictionary with keys are operation names (str) and
-    values are the number of times the operation is repeated in the circuit (int)
+    Args:
+        num_wires (int): number of qubits
+        num_gates (int): number of gates
+        gate_types (defaultdict(int)): dictionary with keys are operation names (str)
+            and values are the number of times the operation is repeated in the circuit (int)
+        depth (int): the depth of the circuit (max number of non-parallel operations)
+        shots (int): number of sampels to measure
+
+    Raises:
+        TypeError: If the attributes provided are not of the correct type.
+        AttributeError: If an attribute is set after initialization.
 
     .. details::
 
@@ -39,47 +43,94 @@ class Resources:
 
         **Example**
 
-        >>> r = Resources()
-        >>> r.num_wires = 2
-        >>> r.num_gates = 2
-        >>> r.depth = 2
-        >>> r.gate_types = {'Hadamard': 1, 'CNOT':1}
+        >>> r = Resources(num_wires=2, num_gates=2, gate_types={'Hadamard': 1, 'CNOT':1}, depth=2)
         >>> print(r)
         wires: 2
         gates: 2
         depth: 2
         shots: 0
         gate_types: {'Hadamard': 1, 'CNOT': 1}
-
-        There is also functionality to combine instances of :code:`Resources` intuatively. There
-        are two basic ways to combine the resources of quantum workflows together. The series method
-        assumes the two workflows occur sequentially on the same qubit register while the parallel
-        method assumes they occur simultaneously on two seperate qubit registers.
-
-        **Example**
-
-        >>> r_in_series = r + r
-        >>> print(r_in_series)
-        wires: 2
-        gates: 4
-        depth: 4
-        shots: 0
-        gate_types: {'Hadamard': 2, 'CNOT': 2}
-        >>>
-        >>> r_in_parallel = r @ r
-        wires: 4
-        gates: 4
-        depth: 2
-        shots: 0
-        gate_types: {'Hadamard': 2, 'CNOT': 2}
     """
 
-    def __init__(self):
-        self.num_wires = 0
-        self.num_gates = 0
-        self.gate_types = defaultdict(int)
-        self.depth = 0
-        self.shots = 0
+    def __init__(self, num_wires=0, num_gates=0, gate_types=defaultdict(int), depth=0, shots=0):
+        """Initialize a Resources instance and perform input type validation."""
+
+        if not (
+            all(isinstance(param, int) for param in [num_wires, num_gates, depth, shots])
+            and all([val > -1 for val in [num_wires, num_gates, depth, shots]])
+            and isinstance(gate_types, DefaultDict)
+        ):
+            raise TypeError(
+                "Incorrect type of input, expected type int for num_wires, num_gates, depth and shots. "
+                f"Got {type(num_wires)}, {type(num_gates)}, {type(depth)}, {type(shots)} respectively. "
+                f"Expected type dict or DefaultDict for gate_types, got {type(gate_types)}."
+            )
+
+        self._num_wires = num_wires
+        self._num_gates = num_gates
+        self._gate_types = gate_types
+        self._depth = depth
+        self._shots = shots
+
+    @property
+    def num_wires(self):
+        """Number of wires in the quantum workflow."""
+        return self._num_wires
+
+    @num_wires.setter
+    def num_wires(self, new_val):
+        """Setting is NOT allowed"""
+        raise AttributeError(
+            f"{type(self)} object does not support assignment for 'num_wires' attribute"
+        )
+
+    @property
+    def num_gates(self):
+        """Number of gates in the quantum workflow."""
+        return self._num_gates
+
+    @num_gates.setter
+    def num_gates(self, new_val):
+        """Setting is NOT allowed"""
+        raise AttributeError(
+            f"{type(self)} object does not support assignment for 'num_gates' attribute"
+        )
+
+    @property
+    def gate_types(self):
+        """Dictionary of unique quantum gates and counts in the quantum workflow."""
+        return self._gate_types
+
+    @gate_types.setter
+    def gate_types(self, new_val):
+        """Setting is NOT allowed"""
+        raise AttributeError(
+            f"{type(self)} object does not support assignment for 'gate_types' attribute"
+        )
+
+    @property
+    def depth(self):
+        """Circuit depth of the quantum workflow."""
+        return self._depth
+
+    @depth.setter
+    def depth(self, new_val):
+        """Setting is NOT allowed"""
+        raise AttributeError(
+            f"{type(self)} object does not support assignment for 'depth' attribute"
+        )
+
+    @property
+    def shots(self):
+        """Number of shots/samples in the quantum workflow."""
+        return self._shots
+
+    @shots.setter
+    def shots(self, new_val):
+        """Setting is NOT allowed"""
+        raise AttributeError(
+            f"{type(self)} object does not support assignment for 'shots' attribute"
+        )
 
     def __str__(self):
         keys = ["wires", "gates", "depth", "shots", "gate_types"]
@@ -101,54 +152,6 @@ class Resources:
         """Displays __str__ in ipython instead of __repr__"""
         print(str(self))
 
-    def __add__(self, other: "Resources") -> "Resources":
-        """Simple add method to combine resources linearly in series."""
-        if not isinstance(other, Resources):
-            raise ValueError(
-                f"Can only combine with another instance of `Resources`, got {type(other)}"
-            )
-
-        total_resources = Resources()
-
-        total_resources.num_wires = max(
-            self.num_wires, other.num_wires
-        )  # qubits in series is just the max of each
-        total_resources.num_gates = self.num_gates + other.num_gates
-        total_resources.depth = self.depth + other.depth
-        total_resources.shots = max(
-            self.shots, other.shots
-        )  # shots in series is just the max of each algorithm
-
-        total_resources.gate_types = copy(self.gate_types)
-        for gate, count in other.gate_types.items():
-            total_resources.gate_types[gate] += count
-
-        return total_resources
-
-    def __matmul__(self, other: "Resources") -> "Resources":
-        """Simple add method to combine resources in parallel."""
-        if not isinstance(other, Resources):
-            raise ValueError(
-                f"Can only combine with another instance of `Resources`, got {type(other)}"
-            )
-
-        total_resources = Resources()
-
-        total_resources.num_wires = (
-            self.num_wires + other.num_wires
-        )  # qubits in parallel is the sum of each algorithm
-        total_resources.num_gates = self.num_gates + other.num_gates
-        total_resources.depth = max(self.depth, other.depth)
-        total_resources.shots = max(
-            self.shots, other.shots
-        )  # shots in parallel is just the max of each algorithm
-
-        total_resources.gate_types = copy(self.gate_types)
-        for gate, count in other.gate_types.items():
-            total_resources.gate_types[gate] += count
-
-        return total_resources
-
     def __eq__(self, other: "Resources") -> bool:
         return all(
             (
@@ -159,10 +162,3 @@ class Resources:
                 self.gate_types == other.gate_types,
             ),
         )
-
-    @staticmethod
-    def combine(lst_resources: Iterable["Resources"], in_parallel=False):
-        """A wrapper function to combine a sequence of Resource objects."""
-        if in_parallel:
-            return reduce(lambda a, b: a @ b, lst_resources)
-        return reduce(lambda a, b: a + b, lst_resources)
--- tests/resource/test_resource.py
+++ tests/resource/test_resource.py
@@ -24,39 +24,54 @@ class TestResources:
     """Test the methods and attributes of the Resource class"""
 
     resource_quantities = (
-        (),
-        (5, 0, {}, 0, 0),
-        (1, 3, {"Hadamard": 1, "PauliZ": 2}, 3, 10),
-        (4, 2, {"Hadamard": 1, "CNOT": 1}, 2, 100),
+        Resources(),
+        Resources(5, 0, defaultdict(int), 0, 0),
+        Resources(1, 3, defaultdict(int, {"Hadamard": 1, "PauliZ": 2}), 3, 10),
+        Resources(4, 2, defaultdict(int, {"Hadamard": 1, "CNOT": 1}), 2, 100),
     )
 
-    @staticmethod
-    def _construct_resource(
-        num_wires=None, num_gates=None, gate_types=None, depth=None, shots=None
-    ):
-        """Construct an instances of the Resources class according to the data provided."""
-        r = Resources()
+    resource_parameters = (
+        (0, 0, defaultdict(int), 0, 0),
+        (5, 0, defaultdict(int), 0, 0),
+        (1, 3, defaultdict(int, {"Hadamard": 1, "PauliZ": 2}), 3, 10),
+        (4, 2, defaultdict(int, {"Hadamard": 1, "CNOT": 1}), 2, 100),
+    )
 
-        if num_wires:
-            r.num_wires = num_wires
-        if num_gates:
-            r.num_gates = num_gates
-        if gate_types:
-            r.gate_types = defaultdict(int, gate_types)
-        if depth:
-            r.depth = depth
-        if shots:
-            r.shots = shots
-        return r
-
-    def test_init(self):
+    @pytest.mark.parametrize("r, attribute_tup", zip(resource_quantities, resource_parameters))
+    def test_init(self, r, attribute_tup):
         """Test that the Resource class is instantiated as expected."""
+        num_wires, num_gates, gate_types, depth, shots = attribute_tup
+
+        assert r.num_wires == num_wires
+        assert r.num_gates == num_gates
+        assert r.depth == depth
+        assert r.shots == shots
+        assert r.gate_types == gate_types
+
+    params_error = (
+        (0, 1, {}, 2, 3),
+        (0.0, 1.1, defaultdict(int, {"Hadamard": 1}), 2.2, 3.3),
+        (0, -1, defaultdict(int, {"Hadamard": 1}), 2, -3),
+        ("0", [1], {"dict": 1}, True, False),
+    )
+
+    @pytest.mark.parametrize("params", params_error)
+    def test_init_error(self, params):
+        """Test that an error is raised if an attribute is initialized with the wrong type."""
+        with pytest.raises(TypeError, match="Incorrect type of input,"):
+            Resources(*params)
+
+    def test_set_attributes_error(self):
+        """Test that an error is raised if we try to set any attribute."""
         r = Resources()
-        assert r.num_wires == 0
-        assert r.num_gates == 0
-        assert r.depth == 0
-        assert r.shots == 0
-        assert r.gate_types == defaultdict(int)
+        attr_lst = ["num_wires", "num_gates", "depth", "shots", "gate_types"]
+        message = (
+            "<class 'pennylane.resource.resource.Resources'> object does not support assignment"
+        )
+
+        for attr_name in attr_lst:
+            with pytest.raises(AttributeError, match=message):
+                setattr(r, attr_name, 1)
 
     test_str_data = (
         ("wires: 0\n" + "gates: 0\n" + "depth: 0\n" + "shots: 0\n" + "gate_types: \n" + "{}"),
@@ -79,10 +94,9 @@ class TestResources:
         ),
     )
 
-    @pytest.mark.parametrize("r_params, rep", zip(resource_quantities, test_str_data))
-    def test_to_str(self, r_params, rep):
+    @pytest.mark.parametrize("r, rep", zip(resource_quantities, test_str_data))
+    def test_to_str(self, r, rep):
         """Test the string representation of a Resources instance."""
-        r = self._construct_resource(*r_params)
         assert str(r) == rep
 
     test_rep_data = (
@@ -94,125 +108,14 @@ class TestResources:
         "gate_types=defaultdict(<class 'int'>, {'Hadamard': 1, 'CNOT': 1})>",
     )
 
-    @pytest.mark.parametrize("r_params, rep", zip(resource_quantities, test_rep_data))
-    def test_repr(self, r_params, rep):
+    @pytest.mark.parametrize("r, rep", zip(resource_quantities, test_rep_data))
+    def test_repr(self, r, rep):
         """Test the repr method of a Resources instance looks as expected."""
-        r = self._construct_resource(*r_params)
         assert repr(r) == rep
 
-    @pytest.mark.parametrize("r_params, rep", zip(resource_quantities, test_str_data))
-    def test_ipython_display(self, r_params, rep, capsys):
+    @pytest.mark.parametrize("r, rep", zip(resource_quantities, test_str_data))
+    def test_ipython_display(self, r, rep, capsys):
         """Test that the ipython display prints the string representation of a Resources instance."""
-        r = self._construct_resource(*r_params)
         r._ipython_display_()  # pylint: disable=protected-access
         captured = capsys.readouterr()
         assert rep in captured.out
-
-    test_add_data = (
-        (
-            (),
-            (1, 3, {"Hadamard": 1, "PauliZ": 2}, 3, 10),
-            (1, 3, {"Hadamard": 1, "PauliZ": 2}, 3, 10),
-        ),
-        (
-            (1, 3, {"Hadamard": 1, "PauliZ": 2}, 3, 10),
-            (1, 3, {"Hadamard": 1, "PauliZ": 2}, 3, 10),
-            (1, 6, {"Hadamard": 2, "PauliZ": 4}, 6, 10),
-        ),
-        (
-            (1, 3, {"Hadamard": 1, "PauliZ": 2}, 3, 10),
-            (4, 2, {"Hadamard": 1, "CNOT": 1}, 2, 100),
-            (4, 5, {"Hadamard": 2, "PauliZ": 2, "CNOT": 1}, 5, 100),
-        ),
-        (
-            (5, 0, {}, 0, 0),
-            (4, 2, {"Hadamard": 1, "CNOT": 1}, 2, 100),
-            (5, 2, {"Hadamard": 1, "CNOT": 1}, 2, 100),
-        ),
-    )
-
-    @pytest.mark.parametrize("r1_params, r2_params, result_params", test_add_data)
-    def test_add(self, r1_params, r2_params, result_params):
-        """Test adding two Resource instances (sum in series)."""
-        r1 = self._construct_resource(*r1_params)
-        r2 = self._construct_resource(*r2_params)
-
-        result = r1 + r2
-        expected = self._construct_resource(*result_params)
-        assert expected == result
-
-    test_matmul_data = (
-        (
-            (),
-            (1, 3, {"Hadamard": 1, "PauliZ": 2}, 3, 10),
-            (1, 3, {"Hadamard": 1, "PauliZ": 2}, 3, 10),
-        ),
-        (
-            (1, 3, {"Hadamard": 1, "PauliZ": 2}, 3, 10),
-            (1, 3, {"Hadamard": 1, "PauliZ": 2}, 3, 10),
-            (2, 6, {"Hadamard": 2, "PauliZ": 4}, 3, 10),
-        ),
-        (
-            (1, 3, {"Hadamard": 1, "PauliZ": 2}, 3, 10),
-            (4, 2, {"Hadamard": 1, "CNOT": 1}, 2, 100),
-            (5, 5, {"Hadamard": 2, "PauliZ": 2, "CNOT": 1}, 3, 100),
-        ),
-        (
-            (5, 0, {}, 0, 0),
-            (4, 2, {"Hadamard": 1, "CNOT": 1}, 2, 100),
-            (9, 2, {"Hadamard": 1, "CNOT": 1}, 2, 100),
-        ),
-    )
-
-    @pytest.mark.parametrize("r1_params, r2_params, result_params", test_matmul_data)
-    def test_matmul(self, r1_params, r2_params, result_params):
-        """Test the matmul of two Resource instances (sum in parallel)."""
-        r1 = self._construct_resource(*r1_params)
-        r2 = self._construct_resource(*r2_params)
-
-        result = r1 @ r2
-        expected = self._construct_resource(*result_params)
-        assert expected == result
-
-    test_combine_data = (
-        (True, (5, 5, {"Hadamard": 2, "PauliZ": 2, "CNOT": 1}, 3, 100)),
-        (False, (4, 5, {"Hadamard": 2, "PauliZ": 2, "CNOT": 1}, 5, 100)),
-    )
-
-    @pytest.mark.parametrize("is_parallel, result_params", test_combine_data)
-    def test_combine(self, is_parallel, result_params):
-        """Test that we can combine Resource instances between series and parallel combine methods."""
-        r0 = self._construct_resource()
-        r1 = self._construct_resource(1, 3, {"Hadamard": 1, "PauliZ": 2}, 3, 10)
-        r2 = self._construct_resource(4, 2, {"Hadamard": 1, "CNOT": 1}, 2, 100)
-
-        list_of_resources = [r0, r1, r2]
-        result = self._construct_resource(*result_params)
-
-        assert Resources.combine(list_of_resources, in_parallel=is_parallel) == result
-
-    def test_invalid_type_error(self):
-        """Test that if you try to combine Resources instances with other types an error is raised."""
-        r0 = self._construct_resource()
-        lst_objs = [0, True, "str", tuple(), []]
-
-        for other in lst_objs:
-            with pytest.raises(
-                ValueError, match="Can only combine with another instance of `Resources`"
-            ):
-                _ = r0 + other
-
-            with pytest.raises(
-                ValueError, match="Can only combine with another instance of `Resources`"
-            ):
-                _ = r0 @ other
-
-            with pytest.raises(
-                ValueError, match="Can only combine with another instance of `Resources`"
-            ):
-                _ = Resources.combine([r0, other], in_parallel=True)
-
-            with pytest.raises(
-                ValueError, match="Can only combine with another instance of `Resources`"
-            ):
-                _ = Resources.combine([r0, other], in_parallel=False)
