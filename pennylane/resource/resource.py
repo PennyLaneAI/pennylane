d02a6a2bd83fc28e233f04be2cba7486fe27f501
Jay Soni <jbsoni@uwaterloo.ca>
2023-04-19 14:55:06 -0400

    Add `Resources` base class  (#3981)
    * init commit of feature branch
    
    * initial commit to add base resource class
    
    * tests + docs
    
    * lint
    
    * lint + fix docs
    
    * more lint + docs
    
    * update implementation to prevent updating attributes
    
    * lint
    
    * Apply suggestions from code review
    
    Co-authored-by: soranjh <40344468+soranjh@users.noreply.github.com>
    
    * Address review comments, add test for __eq__ op
    
    * lint
    
    * code review comment
    
    * fix if
    
    * fix typo
    
    * Update pennylane/resource/resource.py
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>
    
    * swapped over to data class implementation
    
    * fix type hint
    
    * changelog
    
    * lint
    
    * ci
    
    * fixed docstring
    
    ---------
    
    Co-authored-by: soranjh <40344468+soranjh@users.noreply.github.com>
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>

---
+++ pennylane/resource/resource.py
@@ -0,0 +1,66 @@
+# Copyright 2018-2023 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+The data class which will aggregate all the resource information from a quantum workflow.
+"""
+from dataclasses import dataclass, field
+
+
+@dataclass(frozen=True)
+class Resources:
+    r"""Contains attributes which store key resources such as number of gates, number of wires, shots,
+    depth and gate types.
+
+    Args:
+        num_wires (int): number of qubits
+        num_gates (int): number of gates
+        gate_types (dict): dictionary storing operation names (str) as keys
+            and the number of times they are used in the circuit (int) as values
+        depth (int): the depth of the circuit defined as the maximum number of non-parallel operations
+        shots (int): number of samples to generate
+
+    .. details::
+
+        The resources being tracked can be accessed as class attributes.
+        Additionally, the :code:`Resources` instance can be nicely displayed in the console.
+
+        **Example**
+
+        >>> r = Resources(num_wires=2, num_gates=2, gate_types={'Hadamard': 1, 'CNOT':1}, depth=2)
+        >>> print(r)
+        wires: 2
+        gates: 2
+        depth: 2
+        shots: 0
+        gate_types: {'Hadamard': 1, 'CNOT': 1}
+    """
+    num_wires: int = 0
+    num_gates: int = 0
+    gate_types: dict = field(default_factory=dict)
+    depth: int = 0
+    shots: int = 0
+
+    def __str__(self):
+        keys = ["wires", "gates", "depth", "shots", "gate_types"]
+        vals = [self.num_wires, self.num_gates, self.depth, self.shots, self.gate_types]
+        items = "\n".join([str(i) for i in zip(keys, vals)])
+        items = items.replace("('", "")
+        items = items.replace("',", ":")
+        items = items.replace(")", "")
+        items = items.replace("{", "\n{")
+        return items
+
+    def _ipython_display_(self):
+        """Displays __str__ in ipython instead of __repr__"""
+        print(str(self))
