7d166dccf39743930067b3c0b16ff987ae062b75
lillian542 <Lillian.frederiksen@xanadu.ai>
2023-04-29 13:24:42 -0400

    pylint

--- pennylane/pulse/hardware_hamiltonian.py
+++ pennylane/pulse/hardware_hamiltonian.py
@@ -433,7 +433,7 @@ class HardwarePulse:
 
 
 def amplitude_and_phase(trig_fn, amp, phase, hz_to_rads=2 * np.pi):
-    """Wrapper function for combining amplitude and phase into a single callable
+    r"""Wrapper function for combining amplitude and phase into a single callable
     (or constant if neither amplitude nor phase are callable). The factor of :math:`2 \pi` converts
     amplitude in Hz to amplitude in radians/second."""
     if not callable(amp) and not callable(phase):
