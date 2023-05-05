78191a3b541cb2e4a748546661f92bb656724cb7
soranjh <soranjh@yahoo.com>
2023-05-05 13:49:29 -0400

    correct differentiable hamiltonian reconstruction

--- pennylane/qchem/openfermion_obs.py
+++ pennylane/qchem/openfermion_obs.py
@@ -944,13 +944,18 @@ def molecular_hamiltonian(
         )
         if args is None:
             h = qml.qchem.diff_hamiltonian(mol, core=core, active=active)()
-        else:
-            h = qml.qchem.diff_hamiltonian(mol, core=core, active=active)(*args)
+            h = qml.Hamiltonian(
+                qml.numpy.real(h.coeffs, requires_grad=False),
+                h.ops,
+                grouping_type=grouping_type,
+                method=grouping_method,
+            )
+            if wires:
+                h = qml.map_wires(h, wires_map)
+            return h, 2 * len(active)
+        h = qml.qchem.diff_hamiltonian(mol, core=core, active=active)(*args)
         h = qml.Hamiltonian(
-            qml.numpy.real(h.coeffs, requires_grad=False),
-            h.ops,
-            grouping_type=grouping_type,
-            method=grouping_method,
+            qml.numpy.real(h.coeffs), h.ops, grouping_type=grouping_type, method=grouping_method
         )
         if wires:
             h = qml.map_wires(h, wires_map)
