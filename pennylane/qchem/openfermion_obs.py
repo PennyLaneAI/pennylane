306222597975d502f3eb8bf06af6224061fd0ba6
soranjh <soranjh@yahoo.com>
2023-05-05 14:03:45 -0400

    make code compact

--- pennylane/qchem/openfermion_obs.py
+++ pennylane/qchem/openfermion_obs.py
@@ -944,19 +944,12 @@ def molecular_hamiltonian(
         )
         if args is None:
             h = qml.qchem.diff_hamiltonian(mol, core=core, active=active)()
-            h = qml.Hamiltonian(
-                qml.numpy.real(h.coeffs, requires_grad=False),
-                h.ops,
-                grouping_type=grouping_type,
-                method=grouping_method,
-            )
-            if wires:
-                h = qml.map_wires(h, wires_map)
-            return h, 2 * len(active)
-        h = qml.qchem.diff_hamiltonian(mol, core=core, active=active)(*args)
-        h = qml.Hamiltonian(
-            qml.numpy.real(h.coeffs), h.ops, grouping_type=grouping_type, method=grouping_method
-        )
+            coeffs = qml.numpy.real(h.coeffs, requires_grad=False)
+            h = qml.Hamiltonian(coeffs, h.ops, grouping_type=grouping_type, method=grouping_method)
+        else:
+            h = qml.qchem.diff_hamiltonian(mol, core=core, active=active)(*args)
+            coeffs = qml.numpy.real(h.coeffs)
+            h = qml.Hamiltonian(coeffs, h.ops, grouping_type=grouping_type, method=grouping_method)
         if wires:
             h = qml.map_wires(h, wires_map)
         return h, 2 * len(active)
