20f64895df71556c20b9908892494caefd3d7d3e
Jack Brown <jack@xanadu.ai>
2023-05-09 15:40:12 -0400

    dict tests

--- tests/data/attributes/test_dict.py
+++ tests/data/attributes/test_dict.py
@@ -3,9 +3,28 @@ import pytest
 from pennylane.data.attributes import DatasetDict
 
 
-class TestDatasetString:
-    @pytest.mark.parametrize("value", [{"a": 1, "b": 2}, {}, {"a": 1, "b": {"x": "y"}}])
-    def test_from_value(self, value):
+class TestDatasetDict:
+    @pytest.mark.parametrize("value", [{"a": 1, "b": 2}, {}, {"a": 1, "b": {"x": "y", "z": [1, 2]}}])
+    def test_value_init(self, value):
+        """Test that a DatasetDict is correctly value-initialized."""
         dset_dict = DatasetDict(value)
 
         assert dset_dict == value
+        assert dset_dict.info.type_id == "dict"
+        assert dset_dict.info.py_type == "dict"
+        assert dset_dict.bind.keys() == value.keys()
+        assert len(dset_dict) == len(value)
+
+    @pytest.mark.parametrize("value", [{"a": 1, "b": 2}, {}, {"a": 1, "b": {"x": "y", "z": [1, 2]}}])
+    def test_bind_init(self, value):
+        """Test that a DatasetDict is correctly bind-initialized."""
+        bind = DatasetDict(value).bind
+
+        dset_dict = DatasetDict(bind=bind)
+
+        assert dset_dict == value
+        assert dset_dict.info.type_id == "dict"
+        assert dset_dict.info.py_type == "dict"
+        assert dset_dict.bind.keys() == value.keys()
+        assert len(dset_dict) == len(value)
+        
