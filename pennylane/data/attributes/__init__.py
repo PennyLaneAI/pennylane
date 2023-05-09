36d1d51e2d44388931baaad265265e9af0c28dad
Jack Brown <jack@xanadu.ai>
2023-05-09 14:28:19 -0400

    module docstring more generic

--- pennylane/data/attributes/__init__.py
+++ pennylane/data/attributes/__init__.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Contains AttributeType definitions for builtin types, including dicts,
-heterogenous lists, strings and numpy arrays, as well as Pennylane classes."""
+"""Contains AttributeType definitions."""
 
 from .array import DatasetArray
 from .scalar import DatasetScalar
