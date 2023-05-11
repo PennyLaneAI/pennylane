080d2aee9b61c64cba2e08c3e73beff30ab10984
rmoyard <rmoyard@gmail.com>
2023-05-11 14:37:38 -0400

    Changes for fwd pass

--- pennylane/interfaces/jax_jit_tuple.py
+++ pennylane/interfaces/jax_jit_tuple.py
@@ -376,22 +376,57 @@ def _execute_fwd(
     @partial(execute_wrapper.defjvp, symbolic_zeros=True)
     def execute_wrapper_jvp(primals, tangents):
         """Primals[0] are parameters as Jax tracers and tangents[0] is a list of tangent vectors as Jax tracers."""
-        list_trainable_parameters = [
+        original_shape = _jac_shape_dtype_tuple(tapes, device)
+
+        list_original_trainable_parameters = [tape.trainable_params for tape in tapes]
+        list_new_trainable_parameters = [
             [idx for idx, t in enumerate(tangent) if not isinstance(t, Zero)]
             for tangent in tangents[0]
         ]
+        switch_trainable = [False for _ in range(len(tapes))]
 
-        for trainable_params, tape in zip(list_trainable_parameters, tapes):
-            tape.trainable_params = trainable_params
+        # Update the trainable params for the forward execution
+        for i, (new_trainable_params, tape) in enumerate(zip(list_new_trainable_parameters, tapes)):
+            if tape.trainable_params != new_trainable_params:
+                tape.trainable_params = new_trainable_params
+                switch_trainable[i] = True
 
         res, jacs = execute_wrapper(primals[0])
-        multi_measurements = [len(tape.measurements) > 1 for tape in tapes]
-
         jacs_ = [jacs] if len(tapes) == 1 else jacs
+
+        multi_measurements = [len(tape.measurements) > 1 for tape in tapes]
+        new_jacs = []
+
+        # Add zeros in the jacobians if the trainable params were switched
+        for i, (switch, multi_measurement) in enumerate(zip(switch_trainable, multi_measurements)):
+            if switch:
+                intermediate_jacs = []
+                # Multi measurement
+                shape_dtype = original_shape[0] if len(tapes) == 1 else original_shape[i][0]
+
+                if multi_measurement:
+                    jac_empty = tuple(
+                        jax.numpy.zeros(shape=tensor.shape, dtype=tensor.dtype)
+                        for tensor in shape_dtype
+                    )
+                # Single measurement
+                else:
+                    jac_empty = jnp.zeros(shape_dtype.shape, shape_dtype.dtype)
+
+                for j, t_param in enumerate(list_original_trainable_parameters[i]):
+                    if t_param in list_new_trainable_parameters[i]:
+                        intermediate_jacs.append(jacs_[i])
+                    else:
+                        intermediate_jacs.append(jac_empty)
+                new_jacs.append(tuple(intermediate_jacs))
+
+        if new_jacs:
+            new_jacs = new_jacs[0] if len(tapes) == 1 else tuple(new_jacs)
 
         tangents = _filter_zeros_tangents(tangents[0])
         jvps = _compute_jvps(jacs_, tangents, multi_measurements)
 
+        jacs = jacs if not new_jacs else new_jacs
         return (res, jacs), (jvps, jacs)
 
     res = execute_wrapper(params)
