15a4b5ac0d3c97c50e3a4d686142c624f2166b04
Jack Brown <jack@xanadu.ai>
2023-05-12 14:41:01 -0400

    sparse array tests

--- pennylane/data/attributes/sparse_array.py
+++ pennylane/data/attributes/sparse_array.py
@@ -13,44 +13,99 @@
 # limitations under the License.
 """Contains AttributeType definition for ``scipy.sparse.csr_array``."""
 
-from typing import Tuple, Type
+from typing import Generic, Tuple, Type, TypeVar, Union, cast
 
 import numpy as np
-from scipy.sparse import csr_array
+from scipy.sparse import (
+    bsr_array,
+    coo_array,
+    csc_array,
+    csr_array,
+    dia_array,
+    dok_array,
+    lil_array,
+)
 
-from pennylane.data.base.attribute import AttributeType
+from pennylane.data.base.attribute import AttributeInfo, AttributeType
 from pennylane.data.base.typing_util import ZarrGroup
 
+_ALL_SPARSE = (
+    bsr_array,
+    coo_array,
+    csc_array,
+    csr_array,
+    dia_array,
+    dok_array,
+    lil_array,
+)
+_ALL_SPARSE_MAP = {type_.__qualname__: type_ for type_ in _ALL_SPARSE}
 
-class DatasetSparseArray(AttributeType[ZarrGroup, csr_array, csr_array]):
-    """Attribute type for ``scipy.sparse.csr_array``."""
+SparseArray = Union[bsr_array, coo_array, csc_array, csr_array, dia_array, dok_array, lil_array]
+SparseArrayT = TypeVar("SparseArrayT", bound=SparseArray)
+
+
+class DatasetSparseArray(
+    Generic[SparseArrayT], AttributeType[ZarrGroup, SparseArrayT, SparseArrayT]
+):
+    """Attribute type for Scipy sparse arrays. Can accept values of any type in
+    ``scipy.sparse``. Arrays are stored in CSR format."""
 
     type_id = "sparse_array"
 
-    @classmethod
-    def consumes_types(cls) -> Tuple[Type[csr_array], ...]:
-        return (csr_array,)
+    def __post_init__(self, value: SparseArrayT, info) -> None:
+        super().__post_init__(value, info)
+        self.info["sparse_array_class"] = type(value).__qualname__
+
+    @property
+    def sparse_array_class(self) -> Type[SparseArrayT]:
+        return cast(Type[SparseArrayT], _ALL_SPARSE_MAP[self.info["sparse_array_class"]])
 
     @classmethod
-    def py_type(cls, value_type: Type[csr_array]) -> str:
-        """The module path of ``csr_matrix`` is ``scipy.sparse._csr.csr_matrix``, which
-        is private. This method returns the public path ``scipy.sparse.csr_matrix``
-        instead."""
+    def consumes_types(
+        cls,
+    ) -> Tuple[
+        Type[bsr_array],
+        Type[coo_array],
+        Type[csc_array],
+        Type[csr_array],
+        Type[dia_array],
+        Type[dok_array],
+        Type[lil_array],
+    ]:
+        return _ALL_SPARSE
 
-        return "scipy.sparse.csr_array"
+    @classmethod
+    def py_type(cls, value_type: Type[SparseArray]) -> str:
+        """The module path of sparse array types is private, e.g ``scipy.sparse._csr.csr_array``.
+        This method returns the public path e.g ``scipy.sparse.csr_array`` instead."""
 
-    def zarr_to_value(self, bind: ZarrGroup) -> csr_array:
-        return csr_array(
+        return f"scipy.sparse.{value_type.__qualname__}"
+
+    def zarr_to_value(self, bind: ZarrGroup) -> SparseArrayT:
+        info = AttributeInfo(bind.attrs)
+
+        value = csr_array(
             (np.array(bind["data"]), np.array(bind["indices"]), np.array(bind["indptr"])),
             shape=tuple(bind["shape"]),
         )
 
-    def value_to_zarr(self, bind_parent: ZarrGroup, key: str, value: csr_array) -> ZarrGroup:
+        sparse_array_class = cast(Type[SparseArrayT], _ALL_SPARSE_MAP[info["sparse_array_class"]])
+        if not isinstance(value, sparse_array_class):
+            value = sparse_array_class(value)
+
+        return value
+
+    def value_to_zarr(self, bind_parent: ZarrGroup, key: str, value: SparseArrayT) -> ZarrGroup:
+        if not isinstance(value, csr_array):
+            csr_value = csr_array(value)
+        else:
+            csr_value = value
+
         bind = bind_parent.create_group(key)
 
-        bind["data"] = value.data
-        bind["indices"] = value.indices
-        bind["indptr"] = value.indptr
-        bind["shape"] = value.shape
+        bind["data"] = csr_value.data
+        bind["indices"] = csr_value.indices
+        bind["indptr"] = csr_value.indptr
+        bind["shape"] = csr_value.shape
 
         return bind
--- tests/data/attributes/test_sparse_array.py
+++ tests/data/attributes/test_sparse_array.py
@@ -1,24 +1,23 @@
 import pytest
 import scipy.sparse
-from scipy.sparse import csr_array
-
-from pennylane.data.attributes.sparse_array import DatasetSparseArray
+from pennylane.data.attributes.sparse_array import DatasetSparseArray, _ALL_SPARSE
 
 
-class TestDatasetsSparseMatrix:
-    @pytest.mark.parametrize("sp_in", [scipy.sparse.random(10, 15, format="csr")])
+SP_MATRIX = scipy.sparse.random(10, 15)
+
+
+class TestDatasetsSparseArray:
+    @pytest.mark.parametrize("sp_in", [sp_type(SP_MATRIX) for sp_type in _ALL_SPARSE])
     def test_value_init_(self, sp_in):
-        """Test that a ``DatasetSparseArray`` can be value-initialized."""
+        """Test that a ``DatasetSparseArray`` can be value-initialized from
+        any Scipy sparse array."""
 
         dset_sm = DatasetSparseArray(sp_in)
 
         assert dset_sm.info["type_id"] == "sparse_array"
-        assert dset_sm.info["py_type"] == "scipy.sparse.csr_matrix"
+        assert dset_sm.info["py_type"] == f"scipy.sparse.{type(sp_in).__qualname__}"
 
         sp_out = dset_sm.get_value()
-        assert isinstance(sp_out, csr_array)
+        assert isinstance(sp_out, type(sp_in))
 
-        assert (sp_out.data == sp_in.data).all()
-        assert (sp_out.indices == sp_in.indices).all()
-        assert (sp_out.indptr == sp_in.indptr).all()
-        assert sp_out.shape == sp_in.shape
+        assert (sp_in.todense() == sp_out.todense()).all()
