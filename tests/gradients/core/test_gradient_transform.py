8aff00603e1e10aebccb60c65cfed5f822a9f19a
Matthew Silverman <matthews@xanadu.ai>
2023-05-24 18:33:08 -0400

    bump the atol on flaky test

--- tests/gradients/core/test_gradient_transform.py
+++ tests/gradients/core/test_gradient_transform.py
@@ -221,7 +221,7 @@ class TestGradientTransformIntegration:
         else:
             assert np.allclose(res, expected, atol=atol, rtol=0)
 
-    @pytest.mark.parametrize("shots, atol", [(None, 1e-6), (1000, 1e-1), ([1000, 100], 2e-1)])
+    @pytest.mark.parametrize("shots, atol", [(None, 1e-6), (1000, 1e-1), ([1000, 100], 3e-1)])
     def test_acting_on_qnodes_multi_param(self, shots, atol):
         """Test that a gradient transform acts on QNodes with multiple parameters correctly"""
         dev = qml.device("default.qubit", wires=2, shots=shots)
