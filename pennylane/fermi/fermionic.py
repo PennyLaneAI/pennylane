ebc9f57aef1e8c7a780036c93bccdfbc1d31f58a
soranjh <soranjh@yahoo.com>
2023-05-30 20:04:54 -0400

    [skip ci] rebase and fix conflict

--- pennylane/fermi/fermionic.py
+++ pennylane/fermi/fermionic.py
@@ -130,3 +130,78 @@ class FermiWord(dict):
 
 # TODO: create __add__ method when PauliSentence is merged.
 # TODO: create mapping method when the ne jordan_wigner function is added.
+
+
+class FermiSentence(dict):
+    """Immutable dictionary used to represent a Fermi sentence, a linear combination of Fermi words, with the keys
+    as FermiWord instances and the values correspond to coefficients.
+
+    >>> w1 = FermiWord({(0, 0) : '+', (1, 1) : '-'})
+    >>> w2 = FermiWord({(0, 1) : '+', (1, 2) : '-'})
+    >>> s = FermiSentence({w1 : 1.2, w2: 3.1})
+    >>> s
+    1.2 * '0+ 1-' + 3.1 * '1+ 2-'
+    """
+
+    @property
+    def wires(self):
+        """Return wires of the FermiSentence."""
+        return set().union(*(fw.wires for fw in self.keys()))
+
+    def __str__(self):
+        """String representation of a FermiSentence."""
+
+        return "\n+ ".join(f"{coeff} * '{fw.to_string()}'" for fw, coeff in self.items())
+
+    def __repr__(self):
+        """Terminal representation for FermiSentence."""
+        return str(self)
+
+    def __missing__(self, key):
+        """If the FermiSentence does not contain a FermiWord then the associated value will be 0."""
+        return 0.0
+
+    def __add__(self, other):
+        """Add two Fermi sentence together by iterating over the smaller one and adding its terms
+        to the larger one."""
+        smaller_fs, larger_fs = (
+            (self, copy(other)) if len(self) < len(other) else (other, copy(self))
+        )
+        for key in smaller_fs:
+            larger_fs[key] += smaller_fs[key]
+
+        return larger_fs
+
+    def __mul__(self, other):
+        """Multiply two Fermi sentences by iterating over each sentence and multiplying the Fermi
+        words pair-wise"""
+
+        if len(self) == 0:
+            return copy(other)
+
+        if len(other) == 0:
+            return copy(self)
+
+        keys = [i * j for i in self.keys() for j in other.keys()]
+        vals = [i * j for i in self.values() for j in other.values()]
+
+        return FermiSentence(dict(zip(keys, vals)))
+
+    def __pow__(self, value):
+        if not isinstance(value, int):
+            raise TypeError("The exponent must be integer.")
+
+        operator = FermiSentence({})
+
+        for _ in range(value):
+            operator *= self
+
+        return operator
+
+    def simplify(self, tol=1e-8):
+        """Remove any FermiWords in the FermiSentence with coefficients less than the threshold
+        tolerance."""
+        items = list(self.items())
+        for fw, coeff in items:
+            if abs(coeff) <= tol:
+                del self[fw]
