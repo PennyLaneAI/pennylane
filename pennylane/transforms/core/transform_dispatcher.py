2965cf42a95c5f0f58128214ddb493c2af4fc1c0
Romain Moyard <rmoyard@gmail.com>
2023-06-02 16:41:35 -0400

    Update pennylane/transforms/core/transform_dispatcher.py
    Co-authored-by: BorjaRequena <59647767+BorjaRequena@users.noreply.github.com>

--- pennylane/transforms/core/transform_dispatcher.py
+++ pennylane/transforms/core/transform_dispatcher.py
@@ -169,7 +169,7 @@ def _transform_signature_check(signature):
 
 
 class TransformDispatcher:
-    r"""This object is developer facing and should not be used directly to create transforms.
+    r"""This object is developer facing and should not be used directly to create transforms. Use :func:`~.pennylane.transforms.core.transform`.
 
     Convert a transform that has the signature (tape -> Sequence(tape), fn) to a transform dispatcher that can act
     on tape, qfunc and qnode.
