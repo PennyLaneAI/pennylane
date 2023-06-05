042c7c537d228a5a9330b643b26729ec5fefdae1
dwierichs <david.wierichs@xanadu.ai>
2023-06-05 22:54:07 +0200

    rest of ops

--- tests/ops/test_snapshot.py
+++ tests/ops/test_snapshot.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Unit tests for the Snapshot operation."""
+# pylint: disable=protected-access
 from pennylane import Snapshot
 import pennylane as qml
 
