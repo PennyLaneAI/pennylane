eac03b7326056219a19e7bde779cb2be05644400
soranjh <40344468+soranjh@users.noreply.github.com>
2023-06-06 15:48:35 -0400

    Add the `FermiSentence` class (#4195)
    * create fermiword class
    
    * create fermisentence class
    
    * [skip ci] modify order
    
    * [skip ci] remove unused import
    
    * [skip ci] add deepcopy method
    
    * [skip ci] replace list with generator
    
    * [skip ci] add tests
    
    * [skip ci] add tests
    
    * [skip ci] add tests
    
    * add tests
    
    * [skip ci] run black
    
    * [skip ci] copy fermiword
    
    * [skip ci] rebase and fix conflict
    
    * [skip ci] run black
    
    * [skip ci] copy fermiword
    
    * [skip ci] add tests
    
    * add test to tests_passing_pylint
    
    * add sum mult test
    
    * [skip ci] add tests
    
    * [skip ci] add pow tests
    
    * add tests
    
    * fix pylint
    
    * add error tests
    
    * fix pylint
    
    * add TODO
    
    * modify docstrings
    
    * update changelog
    
    * update changelog
    
    * modify docstrings
    
    * fix codecov
    
    * add code review comments
    
    * add test for indices
    
    * fix codefactor
    
    * Update pennylane/fermi/fermionic.py
    
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>
    
    * black
    
    * update todos
    
    * update tests
    
    * Apply suggestions from code review
    
    * handle grouping in product of FermiSentences
    
    * Update pennylane/fermi/fermionic.py
    
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>
    
    * refactor __mul__
    
    * Apply suggestions from code review
    
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>
    
    ---------
    
    Co-authored-by: lillian542 <38584660+lillian542@users.noreply.github.com>
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>
    Co-authored-by: lillian542 <Lillian.frederiksen@xanadu.ai>

--- tests/fermi/test_fermionic.py
+++ tests/fermi/test_fermionic.py
@@ -17,7 +17,7 @@ from copy import copy, deepcopy
 
 import pytest
 
-from pennylane.fermi.fermionic import FermiWord
+from pennylane.fermi.fermionic import FermiSentence, FermiWord
 
 fw1 = FermiWord({(0, 0): "+", (1, 1): "-"})
 fw2 = FermiWord({(0, 0): "+", (1, 0): "-"})
@@ -171,3 +171,235 @@ class TestFermiWord:
         """Test that an error is raised if the operator orders are not correct."""
         with pytest.raises(ValueError, match="The operator indices must belong to the set"):
             FermiWord(operator)
+
+
+fs1 = FermiSentence({fw1: 1.23, fw2: 4j, fw3: -0.5})
+fs2 = FermiSentence({fw1: -1.23, fw2: -4j, fw3: 0.5})
+fs1_hamiltonian = FermiSentence({fw1: 1.23, fw2: 4, fw3: -0.5})
+fs2_hamiltonian = FermiSentence({fw1: -1.23, fw2: -4, fw3: 0.5})
+fs3 = FermiSentence({fw3: -0.5, fw4: 1})
+fs4 = FermiSentence({fw4: 1})
+fs5 = FermiSentence({})
+
+fs1_x_fs2 = FermiSentence(  # fs1 * fs1, computed by hand
+    {
+        FermiWord({(0, 0): "+", (1, 1): "-", (2, 0): "+", (3, 1): "-"}): 1.5129,
+        FermiWord({(0, 0): "+", (1, 1): "-", (2, 0): "+", (3, 0): "-"}): 4.92j,
+        FermiWord(
+            {
+                (0, 0): "+",
+                (1, 1): "-",
+                (2, 0): "+",
+                (3, 3): "-",
+                (4, 0): "+",
+                (5, 4): "-",
+            }
+        ): -0.615,
+        FermiWord({(0, 0): "+", (1, 0): "-", (2, 0): "+", (3, 1): "-"}): 4.92j,
+        FermiWord({(0, 0): "+", (1, 0): "-", (2, 0): "+", (3, 0): "-"}): -16,
+        FermiWord(
+            {
+                (0, 0): "+",
+                (1, 0): "-",
+                (2, 0): "+",
+                (3, 3): "-",
+                (4, 0): "+",
+                (5, 4): "-",
+            }
+        ): (-0 - 2j),
+        FermiWord(
+            {
+                (0, 0): "+",
+                (1, 3): "-",
+                (2, 0): "+",
+                (3, 4): "-",
+                (4, 0): "+",
+                (5, 1): "-",
+            }
+        ): -0.615,
+        FermiWord(
+            {
+                (0, 0): "+",
+                (1, 3): "-",
+                (2, 0): "+",
+                (3, 4): "-",
+                (4, 0): "+",
+                (5, 0): "-",
+            }
+        ): (-0 - 2j),
+        FermiWord(
+            {
+                (0, 0): "+",
+                (1, 3): "-",
+                (2, 0): "+",
+                (3, 4): "-",
+                (4, 0): "+",
+                (5, 3): "-",
+                (6, 0): "+",
+                (7, 4): "-",
+            }
+        ): 0.25,
+    }
+)
+
+
+class TestFermiSentence:
+    def test_missing(self):
+        """Test the result when a missing key is indexed."""
+        fw = FermiWord({(0, 0): "+", (1, 1): "-"})
+        new_fw = FermiWord({(0, 2): "+", (1, 3): "-"})
+        fs = FermiSentence({fw: 1.0})
+
+        assert new_fw not in fs.keys()
+        assert fs[new_fw] == 0.0
+
+    def test_set_items(self):
+        """Test that we can add to a FermiSentence."""
+        fw = FermiWord({(0, 0): "+", (1, 1): "-"})
+        fs = FermiSentence({fw: 1.0})
+
+        new_fw = FermiWord({(0, 2): "+", (1, 3): "-"})
+        assert new_fw not in fs.keys()
+
+        fs[new_fw] = 3.45
+        assert new_fw in fs.keys() and fs[new_fw] == 3.45
+
+    tup_fs_str = (
+        (fs1, "1.23 * '0+ 1-'\n" + "+ 4j * '0+ 0-'\n" + "+ -0.5 * '0+ 3- 0+ 4-'"),
+        (fs2, "-1.23 * '0+ 1-'\n" + "+ (-0-4j) * '0+ 0-'\n" + "+ 0.5 * '0+ 3- 0+ 4-'"),
+        (fs3, "-0.5 * '0+ 3- 0+ 4-'\n" + "+ 1 * 'I'"),
+        (fs4, "1 * 'I'"),
+        (fs5, "0 * 'I'"),
+    )
+
+    @pytest.mark.parametrize("fs, str_rep", tup_fs_str)
+    def test_str(self, fs, str_rep):
+        """Test the string representation of the FermiSentence."""
+        print(str(fs))
+        assert str(fs) == str_rep
+        assert repr(fs) == str_rep
+
+    tup_fs_wires = (
+        (fs1, {0, 1, 3, 4}),
+        (fs2, {0, 1, 3, 4}),
+        (fs3, {0, 3, 4}),
+        (fs4, set()),
+    )
+
+    @pytest.mark.parametrize("fs, wires", tup_fs_wires)
+    def test_wires(self, fs, wires):
+        """Test the correct wires are given for the FermiSentence."""
+        assert fs.wires == wires
+
+    @pytest.mark.parametrize("fs", (fs1, fs2, fs3, fs4))
+    def test_copy(self, fs):
+        """Test that the copy is identical to the original."""
+        copy_fs = copy(fs)
+        deep_copy_fs = deepcopy(fs)
+
+        assert copy_fs == fs
+        assert deep_copy_fs == fs
+        assert copy_fs is not fs
+        assert deep_copy_fs is not fs
+
+    tup_fs_mult = (  # computed by hand
+        (
+            fs1,
+            fs1,
+            fs1_x_fs2,
+        ),
+        (
+            fs3,
+            fs4,
+            fs3,
+        ),
+        (
+            fs4,
+            fs4,
+            FermiSentence(
+                {
+                    FermiWord({}): 1,
+                }
+            ),
+        ),
+        (fs5, fs3, fs5),
+        (fs3, fs5, fs5),
+        (fs4, fs3, fs3),
+        (fs3, fs4, fs3),
+        (
+            FermiSentence({fw2: 1, fw3: 1, fw4: 1}),
+            FermiSentence({fw4: 1, fw2: 1}),
+            FermiSentence({fw2: 2, fw3: 1, fw4: 1, fw2 * fw2: 1, fw3 * fw2: 1}),
+        ),
+    )
+
+    @pytest.mark.parametrize("f1, f2, result", tup_fs_mult)
+    def test_mul(self, f1, f2, result):
+        """Test that the correct result of multiplication is produced."""
+        simplified_product = f1 * f2
+        simplified_product.simplify()
+
+        assert simplified_product == result
+
+    tup_fs_add = (  # computed by hand
+        (fs1, fs1, FermiSentence({fw1: 2.46, fw2: 8j, fw3: -1})),
+        (fs1, fs2, FermiSentence({})),
+        (fs1, fs3, FermiSentence({fw1: 1.23, fw2: 4j, fw3: -1, fw4: 1})),
+        (fs2, fs5, fs2),
+    )
+
+    @pytest.mark.parametrize("f1, f2, result", tup_fs_add)
+    def test_add(self, f1, f2, result):
+        """Test that the correct result of addition is produced."""
+
+        simplified_product = f1 + f2
+        simplified_product.simplify()
+
+        assert simplified_product == result
+
+    def test_simplify(self):
+        """Test that simplify removes terms in the FermiSentence with coefficient less than the
+        threshold."""
+        un_simplified_fs = FermiSentence({fw1: 0.001, fw2: 0.05, fw3: 1})
+
+        expected_simplified_fs0 = FermiSentence({fw1: 0.001, fw2: 0.05, fw3: 1})
+        expected_simplified_fs1 = FermiSentence({fw2: 0.05, fw3: 1})
+        expected_simplified_fs2 = FermiSentence({fw3: 1})
+
+        un_simplified_fs.simplify()
+        assert un_simplified_fs == expected_simplified_fs0  # default tol = 1e-8
+        un_simplified_fs.simplify(tol=1e-2)
+        assert un_simplified_fs == expected_simplified_fs1
+        un_simplified_fs.simplify(tol=1e-1)
+        assert un_simplified_fs == expected_simplified_fs2
+
+    def test_pickling(self):
+        """Check that FermiSentences can be pickled and unpickled."""
+        f1 = FermiWord({(0, 0): "+", (1, 1): "-"})
+        f2 = FermiWord({(0, 0): "+", (1, 3): "-", (2, 0): "+", (3, 4): "-"})
+        fs = FermiSentence({f1: 1.5, f2: -0.5})
+
+        serialization = pickle.dumps(fs)
+        new_fs = pickle.loads(serialization)
+        assert fs == new_fs
+
+    tup_fs_pow = (
+        (fs1, 0, FermiSentence({FermiWord({}): 1})),
+        (fs1, 1, fs1),
+        (fs1, 2, fs1_x_fs2),
+        (fs3, 0, FermiSentence({FermiWord({}): 1})),
+        (fs3, 1, fs3),
+        (fs4, 0, FermiSentence({FermiWord({}): 1})),
+        (fs4, 3, fs4),
+    )
+
+    @pytest.mark.parametrize("f1, pow, result", tup_fs_pow)
+    def test_pow(self, f1, pow, result):
+        assert f1**pow == result
+
+    tup_fs_pow_error = ((fs1, -1), (fs3, 1.5))
+
+    @pytest.mark.parametrize("f1, pow", tup_fs_pow_error)
+    def test_pow_error(self, f1, pow):
+        with pytest.raises(ValueError, match="The exponent must be a positive integer."):
+            f1**pow  # pylint: disable=pointless-statement
