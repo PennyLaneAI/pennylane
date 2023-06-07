9a3b2b4f14d5f8135cc215bf7e97650838e586db
rmoyard <rmoyard@gmail.com>
2023-06-07 17:22:52 -0400

    Tests and doc

--- pennylane/transforms/core/transform_program.py
+++ pennylane/transforms/core/transform_program.py
@@ -12,50 +12,109 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-This module contains the transform function, the transform dispatcher and the transform container.
+This module contains the transform program class.
 """
 import pennylane
 from .transform_dispatcher import TransformContainer, TransformError
 
 
 class TransformProgram:
-    """Class that contains a transform program and the method to interact with it. The order of execution is the order
-    in the list containing the containers."""
+    """Class that contains a transform program and the methods to interact with it. The order of execution is the order
+    in the list containing the containers.
+
+    .. warning::
+
+        This class is developer-facing and should not be used directly.
+
+    .. seealso:: :func:`~.pennylane.transforms.core.transform`
+
+    """
 
     def __init__(self):
         self._transform_program = []
 
+    def __iter__(self):
+        """list[TransformContainer]: Return an iterator to the underlying transform program."""
+        return self._transform_program.__iter__()
+
+    def __len__(self):
+        """int: Return the number transforms in the program."""
+        return len(self._transform_program)
+
+    def __getitem__(self, idx):
+        """(TransformContainer, List[TransformContainer]): Return the indexed transform container from underlying
+        transform program"""
+        return self._transform_program[idx]
+
+    def __repr__(self):
+        """The string representation of the transform program class."""
+        repr = "TransformProgram("
+        transforms_repr = "".join([f"{transform_c.transform}, " for transform_c in self])
+        transforms_repr = transforms_repr.removesuffix(", ")
+        end = ")"
+        return repr + transforms_repr + end
+
     def push_back(self, transform_container: TransformContainer):
-        """Add a transform (container) to the program."""
+        """Add a transform (container) to the end of the program.
+
+        Args:
+            transform_container(TransformContainer): A transform represented by its container.
+        """
         if not isinstance(transform_container, TransformContainer):
             raise TransformError("Only transform container can be added to the transform program.")
-        if (
-            not self.empty()
-            and self.last_program().is_informative
-            and transform_container.is_informative
-        ):
-            raise TransformError("The transform program already have an informative transform.")
+
+        # Program can only contain one informative transform and at the end of the program
+        if not self.is_empty() and self.get_last().is_informative:
+            raise TransformError("The transform program already has an informative transform.")
         self._transform_program.append(transform_container)
 
-    def insert_front(self, transform_container):
+    def insert_front(self, transform_container: TransformContainer):
+        """Insert the transform container at the beginning of the program.
+
+        Args:
+            transform_container(TransformContainer): A transform represented by its container.
+        """
         self._transform_program.insert(0, transform_container)
 
     def pop_front(self):
+        """Pop the transform container at the beginning of the program.
+
+        Returns:
+            TransformContainer: The transform container at the beginning of the program.
+        """
         first_container = self._transform_program.pop(0)
         return first_container
 
+    def get_last(self):
+        """Get the last transform container.
+
+        Returns:
+            TransformContainer: The last transform in the program.
+
+        Raises:
+            TransformError: It raises an error if the program is empty.
+        """
+        if not self.is_empty():
+            return self._transform_program[-1]
+        raise TransformError(
+            "The transform program is empty and you cannot get the last transform container."
+        )
+
+    def is_empty(self):
+        """Check if the transform program is empty or not.
+
+        Returns:
+            bool: Boolean, True if empty, False otherwise.
+        """
+        return len(self) == 0
+
     def add_device_pre_processing(self, device: pennylane.Device):
+        """Add the transforms from the device pre-processing.
+
+        Args:
+            device(Device): The considered quantum device.
+        """
+        # TODO: Change this part
         device_preprocessing_container = TransformContainer(device.preprocessing)
         # Device pre-processing is first.
-        self.insert_front(0, device_preprocessing_container)
-
-    def last_program(self):
-        if not self.empty():
-            return self._transform_program[-1]
-        else:
-            raise TransformError(
-                "The transform program is empty and you cannot get the last transform container."
-            )
-
-    def empty(self):
-        return len(self._transform_program) == 0
+        self.insert_front(device_preprocessing_container)
