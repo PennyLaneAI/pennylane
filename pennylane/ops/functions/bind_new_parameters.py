c48d73f89aac7b9ef13a652b20ed81fde37c898c
Mudit Pandey <mudit.pandey@xanadu.ai>
2023-06-15 17:26:15 -0400

    Updated for CodeFactor

--- pennylane/ops/functions/bind_new_parameters.py
+++ pennylane/ops/functions/bind_new_parameters.py
@@ -46,7 +46,7 @@ def bind_new_parameters(op: Operator, params: Sequence[TensorLike]) -> Operator:
     """
     try:
         return op.__class__(*params, wires=op.wires, **copy.deepcopy(op.hyperparameters))
-    except:
+    except TypeError:
         new_op = copy.deepcopy(op)
         new_op.data = tuple(params)
         return new_op
