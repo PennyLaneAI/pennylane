e9fb58c9b3b0d562c9d51958d899bebbe3630859
soranjh <soranjh@yahoo.com>
2023-06-15 23:46:32 -0400

    correct description

--- pennylane/fermi/fermionic.py
+++ pennylane/fermi/fermionic.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""The Fermionic representation classes."""
+"""The Fermionic representation classes and functions."""
 import re
 from copy import copy
 from numbers import Number
