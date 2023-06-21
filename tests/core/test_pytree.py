ca14cc62e20a696d1d420a77a5d48219bb70d903
Filippo Vicentini <filippovicentini@gmail.com>
2023-06-21 11:50:12 +0200

    lint tests

--- tests/core/test_pytree.py
+++ tests/core/test_pytree.py
@@ -1,8 +1,30 @@
+# Copyright 2018-2020 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Unit tests for the :mod:`pennylane.core.pytree.HashablePartial` class.
+"""
+# pylint: disable=too-many-arguments,unused-argument,
+
 from functools import partial
 from pennylane.core.pytree import HashablePartial
 
 
 def test_hashable_partial_merges_with_partial():
+    """
+    Test nesting of partials builds the correct HashablePartial
+    (this checks for a bug arising in python 3.10+ only)
+    """
     def f(a, b, c, d, e, f, g):
         pass
 
@@ -21,6 +43,9 @@ def test_hashable_partial_merges_with_partial():
 
 
 def test_hashable_partial_merges_with_hashable_partial():
+    """
+    Test nesting of HashablePartials
+    """
     def f(a, b, c):
         pass
 
@@ -31,6 +56,7 @@ def test_hashable_partial_merges_with_hashable_partial():
 
 
 def test_hashable_partial_repr():
+    """tests the repr method does not crash"""
     def f(a, b, c):
         pass
 
