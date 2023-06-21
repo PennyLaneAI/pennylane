0157ef3ab38001e5f4cd4ad9c7823f449e41b6d9
Filippo Vicentini <filippovicentini@gmail.com>
2023-06-21 11:50:58 +0200

    lint

--- pennylane/core/pytree/__init__.py
+++ pennylane/core/pytree/__init__.py
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Core utilities needed to work with PyTrees.
+"""
+
 from .partial import HashablePartial
