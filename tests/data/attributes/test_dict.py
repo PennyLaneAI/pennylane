f9c7833cebf8a8a12d40b3594200496718581a78
Diego <diego_guala@hotmail.com>
2023-06-22 11:18:49 -0400

    Add codecov-requested tests: DatasetsDict

--- tests/data/attributes/test_dict.py
+++ tests/data/attributes/test_dict.py
@@ -77,3 +77,47 @@ class TestDatasetDict:
         keys."""
         with pytest.raises(TypeError, match="'DatasetDict' keys must be strings"):
             DatasetDict(data)
+
+    @pytest.mark.parametrize(
+        "value", [{"a": 1, "b": 2}, {}, {"a": 1, "b": {"x": "y", "z": [1, 2]}}]
+    )
+    def test_copy(self, value):
+        """Test that `DatasetDict.copy` can copy contents to a built-in dictionary."""
+        dset_dict = DatasetDict(value)
+        builtin_dict = dset_dict.copy()
+
+        assert isinstance(builtin_dict,dict)
+        assert builtin_dict == value
+
+        assert builtin_dict.keys() == value.keys()
+        assert len(builtin_dict) == len(value)
+        assert repr(builtin_dict) == repr(value)
+
+    @pytest.mark.parametrize(
+        "value", [{"a": 1, "b": 2}, {}, {"a": 1, "b": {"x": "y", "z": [1, 2]}}]
+    )
+    def test_equality(self, value):
+        """Test that `DatasetDict` can be compared to other objects."""
+        dset_dict = DatasetDict(value)
+        dset_dict2 = DatasetDict(value)
+
+        assert dset_dict == dset_dict
+        assert dset_dict == dset_dict2
+        assert dset_dict == value
+
+        assert dset_dict != []
+        dset_dict2['additional'] = 'additional'
+        assert dset_dict != dset_dict2
+
+    def test_equality_same_length(self):
+        """Test that `DatasetDict` objects are different when they have same length
+        but different keys."""
+        dset_dict = DatasetDict({"a": 1, "b": 2})
+        assert dset_dict != {"a": 1, "c": 2}
+
+    @pytest.mark.parametrize(
+        "value", [{"a": 1, "b": 2}, {}, {"a": 1, "b": {"x": "y", "z": [1, 2]}}]
+    )
+    def test_string_conversion(self, value):
+        dset_dict = DatasetDict(value)
+        assert str(dset_dict) == str(value)
