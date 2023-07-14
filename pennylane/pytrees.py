6519cf96792439d97fca8c7e45ff07eee91df13a
albi3ro <chrissie.c.l@gmail.com>
2023-07-14 17:51:21 -0400

    register with torch too

--- pennylane/pytrees.py
+++ pennylane/pytrees.py
@@ -1,20 +1,53 @@
+# Copyright 2018-2023 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+
+"""
+
+has_jax = True
+try:
+    import jax.tree_util as jax_tree_util
+except ImportError:
+    has_jax = False
+
+has_torch = True
+try:
+    from torch.utils._pytree import _register_pytree_node as torch_register_pytree
+except ImportError:
+    has_torch = False
 
 
-
-# pylint: disable=import-outside-toplevel
 def _register_pytree_with_jax(pytree_type, flatten_fn, unflatten_fn):
+    def jax_unflatten(aux, parameters):
+        return unflatten_fn(parameters, aux)
 
-    try:
-        import jax
-    except ImportError:
-        return
+    jax_tree_util.register_pytree_node(pytree_type, flatten_fn, jax_unflatten)
+    return
 
-    jax.tree_util.register_pytree_node(pytree_type, flatten_fn, unflatten_fn)
+
+def _register_pytree_with_torch(pytree_type, flatten_fn, unflatten_fn):
+
+    torch_register_pytree(pytree_type, flatten_fn, unflatten_fn)
     return
 
 
 def register_pytree(pytree_type, flatten_fn, unflatten_fn):
 
-    _register_pytree_with_jax(pytree_type, flatten_fn, unflatten_fn)
+    if has_jax:
+        _register_pytree_with_jax(pytree_type, flatten_fn, unflatten_fn)
+
+    if has_torch:
+        _register_pytree_with_torch(pytree_type, flatten_fn, unflatten_fn)
 
     return
