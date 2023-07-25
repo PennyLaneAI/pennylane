bbc8aa432b72ecf524fe715b38f02f1db511ec44
Matthew Silverman <matthews@xanadu.ai>
2023-07-25 15:51:45 -0400

    make draw_mpl use style from use_style (#4357)
    * make draw_mpl use style from use_style
    
    * also reset font weight and linewidth
    
    * changelog
    
    * re-render from other folders too
    
    * only execute replacement in tape_mpl
    
    * refresh images
    
    * style tests use _set_style
    
    * allow style='rcParams' to use current rcParams
    
    * refresh images
    
    * fix rcParams usage
    
    * update docstring to mention use_style

--- tests/drawer/test_draw_mpl.py
+++ tests/drawer/test_draw_mpl.py
@@ -243,14 +243,14 @@ class TestMPLIntegration:
     """Test using matplotlib styling to modify look of graphic."""
 
     def test_rcparams(self):
-        """Test setting rcParams modifies style for draw_mpl(circuit, style=None)."""
+        """Test setting rcParams modifies style for draw_mpl(circuit, style="rcParams")."""
 
         rgba_red = (1, 0, 0, 1)
         rgba_green = (0, 1, 0, 1)
         plt.rcParams["patch.facecolor"] = rgba_red
         plt.rcParams["lines.color"] = rgba_green
 
-        _, ax = qml.draw_mpl(circuit1, style=None)(1.23, 2.34)
+        _, ax = qml.draw_mpl(circuit1, style="rcParams")(1.23, 2.34)
 
         assert ax.patches[0].get_facecolor() == rgba_red
         assert ax.patches[1].get_facecolor() == rgba_red
@@ -258,15 +258,15 @@ class TestMPLIntegration:
         for l in ax.lines[:-1]:  # final is fancy arrow, has different styling
             assert l.get_color() == rgba_green
 
-        plt.style.use("default")
+        qml.drawer.use_style("black_white")
         plt.close()
 
     def test_style_with_matplotlib(self):
-        """Test matplotlib styles impact figure styling for draw_mpl(circuit, style=None)."""
+        """Test matplotlib styles impact figure styling for draw_mpl(circuit, style="rcParams")."""
 
         plt.style.use("fivethirtyeight")
 
-        _, ax = qml.draw_mpl(circuit1, style=None)(1.23, 2.34)
+        _, ax = qml.draw_mpl(circuit1, style="rcParams")(1.23, 2.34)
 
         expected_facecolor = mpl.colors.to_rgba(plt.rcParams["patch.facecolor"])
         assert ax.patches[0].get_facecolor() == expected_facecolor
@@ -276,7 +276,7 @@ class TestMPLIntegration:
         for l in ax.lines[:-1]:  # final is fancy arrow, has different styling
             assert mpl.colors.to_rgba(l.get_color()) == expected_linecolor
 
-        plt.style.use("default")
+        qml.drawer.use_style("black_white")
         plt.close()
 
     def test_style_restores_settings(self):
