567479ae96ee13415ac7c8bea26cbe9b7fd951d4
AmintorDusko <amintor_dusko@hotmail.com>
2023-07-25 08:56:05 -0400

    add __init__ docstring

--- pennylane/logging/__init__.py
+++ pennylane/logging/__init__.py
@@ -1,3 +1,5 @@
+"""This module enables support for log-level messaging throughout PennyLane, following the native Python logging framework interface. Please see the official documentation for details on usage https://docs.python.org/3/library/logging.html"""
+
 from .configure import configure_logging
 from .configure import enable_logging
 from .configure import TRACE
