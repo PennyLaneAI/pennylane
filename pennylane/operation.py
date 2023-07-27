1cf7edeb3ef4d316cc92c72f362b52e0f4760095
Mudit Pandey <mudit.pandey@xanadu.ai>
2023-07-27 16:01:14 -0400

    Update pennylane/operation.py
    Co-authored-by: Matthew Silverman <matthews@xanadu.ai>

--- pennylane/operation.py
+++ pennylane/operation.py
@@ -1560,12 +1560,6 @@ class Operator(abc.ABC):
             return qml.pow(self, z=other)
         return NotImplemented
 
-    # def __eq__(self, other):
-    #     return qml.equal(self, other)
-
-    # def __hash__(self):
-    #     return self.hash
-
     def _flatten(self):
         """Serialize the operation into trainable and non-trainable components.
 
