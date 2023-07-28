e5d3b1ab756b08dcc5e83a51ba06477b4531f56f
Jay Soni <jbsoni@uwaterloo.ca>
2023-07-28 16:16:16 -0400

    lint

--- tests/test_device.py
+++ tests/test_device.py
@@ -445,8 +445,8 @@ class TestInternalFunctions:
         expanded_tape = dev.default_expand_fn(circuit, max_expansion=depth)
 
         for op, expected_op in zip(
-            expanded_tape._ops,
-            expanded_ops,  # pylint: disable=protected-access
+            expanded_tape._ops,  # pylint: disable=protected-access
+            expanded_ops,
         ):
             assert qml.equal(op, expected_op)
 
