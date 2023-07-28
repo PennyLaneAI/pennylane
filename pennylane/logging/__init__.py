3d481a5bf350849177f93e63eaa38021e0490d0e
AmintorDusko <amintor_dusko@hotmail.com>
2023-07-28 07:35:39 -0400

    review suggestions

--- pennylane/logging/__init__.py
+++ pennylane/logging/__init__.py
@@ -1,5 +1,5 @@
 """This module enables support for log-level messaging throughout PennyLane, following the native Python logging framework interface. Please see the official documentation for details on usage https://docs.python.org/3/library/logging.html"""
 
-from .configure import configure_logging
-from .configure import enable_logging
-from .configure import TRACE
+from .configuration import configure_logging
+from .configuration import enable_logging
+from .configuration import TRACE
