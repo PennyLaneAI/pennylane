85a80016694f033bb7feb78685d77d0ae1a548bd
BorjaRequena <borja.requena.pozo@gmail.com>
2023-08-04 15:14:43 -0400

    precision 1e-16

--- pennylane/pauli/pauli_arithmetic.py
+++ pennylane/pauli/pauli_arithmetic.py
@@ -496,7 +496,7 @@ class PauliSentence(dict):
         matrix.indices = np.take_along_axis(indices, idx, axis=1).ravel()
         matrix.data = np.take_along_axis(data, idx, axis=1).ravel()
         matrix.indptr = _cached_arange(size + 1) * indices.shape[1]
-        matrix.data[np.abs(matrix.data) < 1e-8] = 0  # Faster than np.isclose(matrix.data, 0)
+        matrix.data[np.abs(matrix.data) < 1e-16] = 0  # Faster than np.isclose(matrix.data, 0)
         matrix.eliminate_zeros()
         return matrix
 
