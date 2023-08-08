2b19a585a7d9072046f5a45327e08d82f28f8173
Mudit Pandey <mudit.pandey@xanadu.ai>
2023-08-08 11:52:30 -0400

    Update docs

--- pennylane/measurements/mid_measure.py
+++ pennylane/measurements/mid_measure.py
@@ -57,9 +57,8 @@ def measure(
     >>> func(*pars)
     tensor([0.90165331, 0.09834669], requires_grad=True)
 
-    Wires can be reused after measurement as usual. Moreover, measured wires can
-    be reset to the :math:`|0\rangle` state by setting the ``reset`` keyword argument
-    to ``True``:
+    Wires can be reused after measurement as usual. Moreover, measured wires
+    can be reset to the :math:`| 0 \rangle` state by setting ``reset=True``:
 
     .. code-block:: python3
 
