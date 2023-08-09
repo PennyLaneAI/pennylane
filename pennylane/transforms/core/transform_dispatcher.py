320d718762f0b67c14fdf256182c8d7eb8ae689c
Edward Jiang <34989448+eddddddy@users.noreply.github.com>
2023-08-09 17:10:26 -0400

    Update `batch_input` and `batch_params` transforms (#4449)
    * update batch input
    
    * update batch_params
    
    * pylint and remove tape mutation
    
    * more pylint

--- pennylane/transforms/core/transform_dispatcher.py
+++ pennylane/transforms/core/transform_dispatcher.py
@@ -57,8 +57,7 @@ class TransformDispatcher:
             obj, *targs = targs
 
         if isinstance(obj, (qml.tape.QuantumTape, qml.tape.QuantumScript)):
-            new_tape = copy.deepcopy(obj)
-            return self._transform(new_tape, *targs, **tkwargs)
+            return self._transform(obj, *targs, **tkwargs)
         if isinstance(obj, qml.QNode):
             return self._qnode_transform(
                 obj,
