175dc84bb3b29cded30ecadbca2acf53187803d0
rmoyard <rmoyard@gmail.com>
2023-08-11 17:05:46 -0400

    Undo swap

--- pennylane/transforms/optimization/pattern_matching.py
+++ pennylane/transforms/optimization/pattern_matching.py
@@ -17,18 +17,15 @@ substitution."""
 import copy
 import itertools
 from collections import OrderedDict
+from typing import Sequence, Callable
 
 import numpy as np
 
-from typing import Sequence, Callable
-
 import pennylane as qml
 from pennylane.transforms.core import transform
-from pennylane.tape import QuantumTape
 from pennylane import adjoint
 from pennylane.ops.qubit.attributes import symmetric_over_all_wires
 from pennylane.tape import QuantumTape, QuantumScript
-from pennylane.transforms import qfunc_transform
 from pennylane.transforms.commutation_dag import commutation_dag
 from pennylane.wires import Wires
 
