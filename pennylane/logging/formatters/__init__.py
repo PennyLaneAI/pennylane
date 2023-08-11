c1b109c29d5451620407563cfb308293a7c942fd
Lee J. O'Riordan <lee@xanadu.ai>
2023-08-11 13:02:59 -0400

    Ensure formatter links display in docs

--- pennylane/logging/formatters/__init__.py
+++ pennylane/logging/formatters/__init__.py
@@ -12,4 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """This module defines formatting rules for the PennyLane loggers."""
-from .formatter import Formatter
+from .formatter import DefaultFormatter
+from .formatter import SimpleFormatter
