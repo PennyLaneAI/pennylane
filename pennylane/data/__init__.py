9475956b0078004a60cbe76a9269118680a75507
Diego <diego_guala@hotmail.com>
2023-08-17 14:18:48 -0400

    change links

--- pennylane/data/__init__.py
+++ pennylane/data/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""The data subpackage provides functionality to access, store and manipulate `quantum datasets <https://pennylane.ai/qml/datasets.html>`_.
+"""The data subpackage provides functionality to access, store and manipulate `quantum datasets <https://pennylane.ai/datasets>`_.
 
 .. note::
 
