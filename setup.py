a9bc37a5f10490694179d17a6becfbf00e3976ab
Mudit Pandey <mudit.pandey@xanadu.ai>
2023-08-28 13:10:06 -0400

    Update lightning requirement (#4530)
    * Update lightning requirement
    
    * Trigger CI
    
    * Trigger CI

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
     "semantic-version>=2.7",
     "autoray>=0.3.1",
     "cachetools",
-    "pennylane-lightning>=0.31",
+    "pennylane-lightning>=0.32",
     "requests",
     "typing_extensions",
 ]
