5d2742ca229d2644d64827d66c4d8db84a99cc2d
Mudit Pandey <mudit.pandey@xanadu.ai>
2023-09-14 18:36:59 -0400

    Update `op_transforms` (#4573)
    **Context:**
    Updating `op_transform`s to use new `transform` decorator.
    
    **Description of the Change:**
    * Added `last_transform` kwarg to `TransformDispatcher` and
    `TransformContainer` to differentiate between informative transforms
    (which do not require execution), and any other transforms that should
    be at the end of the transform program but require execution.
    * `TransformDispatcher` now returns the processed results for
    informative transforms rather than the transformed tapes and processing
    function.
    
    TODO:
    
    Update op transforms:
    * [x] eigvals
    * [x] matrix
    * [x] to_zx
    * [x] generator
    
    **Benefits:**
    
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**
    
    ---------
    
    Co-authored-by: rmoyard <rmoyard@gmail.com>
    Co-authored-by: Edward Jiang <34989448+eddddddy@users.noreply.github.com>
    Co-authored-by: Edward Jiang <eddy3.14159@gmail.com>
    Co-authored-by: soranjh <40344468+soranjh@users.noreply.github.com>
    Co-authored-by: Matthew Silverman <matthews@xanadu.ai>
    Co-authored-by: Utkarsh <utkarshazad98@gmail.com>
    Co-authored-by: lillian542 <Lillian.frederiksen@xanadu.ai>
    Co-authored-by: lillian542 <38584660+lillian542@users.noreply.github.com>

--- doc/releases/changelog-dev.md
+++ doc/releases/changelog-dev.md
@@ -4,6 +4,10 @@
 
 <h3>New features since last release</h3>
 
+* Operator transforms `qml.matrix`, `qml.eigvals`, `qml.generator`, and `qml.transforms.to_zx` are updated
+  to the new transform program system.
+  [(#4573)](https://github.com/PennyLaneAI/pennylane/pull/4573)
+
 * All quantum functions transforms are update to the new transform program system.
  [(#4439)](https://github.com/PennyLaneAI/pennylane/pull/4439)
 
@@ -218,4 +222,3 @@ Romain Moyard,
 Mudit Pandey,
 Matthew Silverman,
 Jay Soni,
-
--- pennylane/ops/functions/eigvals.py
+++ pennylane/ops/functions/eigvals.py
@@ -14,18 +14,20 @@
 """
 This module contains the qml.eigvals function.
 """
+from typing import Sequence, Callable
 import warnings
 
 # pylint: disable=protected-access
-from functools import reduce
-
+from functools import reduce, partial
 import scipy
 
 import pennylane as qml
+from pennylane.transforms.op_transforms import OperationTransformError
+from pennylane.transforms.core import transform
+from pennylane.typing import TensorLike
 
 
-@qml.op_transform
-def eigvals(op, k=1, which="SA"):
+def eigvals(op: qml.operation.Operator, k=1, which="SA") -> TensorLike:
     r"""The eigenvalues of one or more operations.
 
     .. note::
@@ -39,8 +41,7 @@ def eigvals(op, k=1, which="SA"):
         `documentation <https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.eigsh.html#scipy.sparse.linalg.eigsh>`_.
 
     Args:
-        op (.Operator, pennylane.QNode, .QuantumTape, or Callable): An operator, quantum node, tape,
-            or function that applies quantum operations.
+        op (.Operator or .QuantumTape): A quantum operator or tape.
         k (int): The number of eigenvalues to be returned for a :class:`~.SparseHamiltonian`.
         which (str): Method for computing the eigenvalues of a :class:`~.SparseHamiltonian`. The
             possible methods are ``'LM'`` (largest in magnitude), ``'SM'`` (smallest in magnitude),
@@ -48,11 +49,10 @@ def eigvals(op, k=1, which="SA"):
             from each end of the spectrum).
 
     Returns:
-        tensor_like or function: If an operator is provided as input, the eigenvalues are returned
-        directly. If a QNode or quantum function is provided as input, a function which accepts the
-        same arguments as the QNode or quantum function is returned. When called, this function will
-        return the unitary matrix in the appropriate autodiff framework (Autograd, TensorFlow,
-        PyTorch, JAX) given its parameters.
+        TensorLike or (Sequence[.QuantumTape], Callable): If an operator is provided as input, the eigenvalues
+        are returned directly. If a quantum tape is provided as input, a list of transformed tapes and a post-processing
+        function are returned. When called, this function will return the unitary matrix in the appropriate autodiff
+        framework (Autograd, TensorFlow, PyTorch, JAX) given its parameters.
 
     **Example**
 
@@ -104,6 +104,13 @@ def eigvals(op, k=1, which="SA"):
         array([ 0.92387953+0.38268343j,  0.92387953-0.38268343j,
                -0.92387953+0.38268343j, -0.92387953-0.38268343j])
     """
+    if not isinstance(op, qml.operation.Operator):
+        if not isinstance(op, (qml.tape.QuantumScript, qml.QNode)) and not callable(op):
+            raise OperationTransformError(
+                "Input is not an Operator, tape, QNode, or quantum function"
+            )
+        return _eigvals_tranform(op, k=k, which=which)
+
     if isinstance(op, qml.Hamiltonian):
         warnings.warn(
             "For Hamiltonians, the eigenvalues will be computed numerically. "
@@ -120,30 +127,39 @@ def eigvals(op, k=1, which="SA"):
         return qml.math.linalg.eigvalsh(sparse_matrix.toarray())
 
     # TODO: make `eigvals` take a `wire_order` argument to mimic `matrix`
-    return op.eigvals()
+    try:
+        return op.eigvals()
+    except qml.operation.EigvalsUndefinedError:
+        return eigvals(op.expand(), k=k, which=which)
 
 
-@eigvals.tape_transform
-def _eigvals(tape):
-    op_wires = [op.wires for op in tape.operations]
-    all_wires = qml.wires.Wires.all_wires(op_wires).tolist()
-    unique_wires = qml.wires.Wires.unique_wires(op_wires).tolist()
+@partial(transform, is_informative=True)
+def _eigvals_tranform(
+    tape: qml.tape.QuantumTape, k=1, which="SA"
+) -> (Sequence[qml.tape.QuantumTape], Callable):
+    def processing_fn(res):
+        op_wires = [op.wires for op in res[0].operations]
+        all_wires = qml.wires.Wires.all_wires(op_wires).tolist()
+        unique_wires = qml.wires.Wires.unique_wires(op_wires).tolist()
 
-    if len(all_wires) != len(unique_wires):
-        warnings.warn(
-            "For multiple operations, the eigenvalues will be computed numerically. "
-            "This may be computationally intensive for a large number of wires.",
-            UserWarning,
-        )
-        return qml.math.linalg.eigvals(qml.matrix(tape))
+        if len(all_wires) != len(unique_wires):
+            warnings.warn(
+                "For multiple operations, the eigenvalues will be computed numerically. "
+                "This may be computationally intensive for a large number of wires.",
+                UserWarning,
+            )
+            matrix = qml.matrix(res[0])
+            return qml.math.linalg.eigvals(matrix)
 
-    # TODO: take into account wire ordering, by reordering eigenvalues
-    # as per operator wires/wire ordering, and by inserting implicit identity
-    # matrices (eigenvalues [1, 1]) at missing locations.
+        # TODO: take into account wire ordering, by reordering eigenvalues
+        # as per operator wires/wire ordering, and by inserting implicit identity
+        # matrices (eigenvalues [1, 1]) at missing locations.
 
-    ev = [eigvals(op) for op in tape.operations]
+        ev = [eigvals(op, k=k, which=which) for op in res[0].operations]
 
-    if len(ev) == 1:
-        return ev[0]
+        if len(ev) == 1:
+            return ev[0]
 
-    return reduce(qml.math.kron, ev)
+        return reduce(qml.math.kron, ev)
+
+    return [tape], processing_fn
