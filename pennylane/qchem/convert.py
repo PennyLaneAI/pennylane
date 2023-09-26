a8d54f8a6a18121ea4f336c72c0aa858747fa716
Stepan Fomichev <stepan.fomichev95@gmail.com>
2023-09-26 14:53:02 -0400

    fix docstring of import state

--- pennylane/qchem/convert.py
+++ pennylane/qchem/convert.py
@@ -573,9 +573,11 @@ def import_state(solver, tol=1e-15):
         * the library Block2 implementing the DMRG method.
 
     Args:
-        solver: external wavefunction object. a) For PySCF, this is the R/UCISD or R/UCCSD Solver object;
-        b) for SHCI via Dice, this is the tuple(list[str], array[float]) of Slater determinants and their coefficients;
-        c) for DMRG via Block2, this is the tuple(list[int], array[float]) of Slater determinants and their coefficients
+        solver: external wavefunction object. 
+        
+            * For PySCF, this is the R/UCISD or R/UCCSD Solver object,
+            * For SHCI via Dice, this is the tuple(list[str], array[float]) of Slater determinants and their coefficients;
+            * For DMRG via Block2, this is the tuple(list[int], array[float]) of Slater determinants and their coefficients
         tol (float): the tolerance for discarding Slater determinants based on their coefficients
 
     Raises:
