get non-infinite reduction; still wrong result

pennylane/transforms/decompositions/clifford_plus_t/gridpoints.py
@@ -24,12 +24,12 @@ from .conversion import (
     from_z_root_two,
     operator_to_bl2z,
 )
-from .rings import Matrix, RootTwo, Dyadic, SQRT2
+from .rings import Matrix, RootTwo, ZRootTwo, SQRT2
 from .shapes import ConvexSet, Ellipse, Point
 
-LAMBDA = RootTwo(1, 1)
-LAMBDA_INV = RootTwo(-1, 1)
-log_lambda = lambda x: np.emath.logn(LAMBDA, x)
+LAMBDA = ZRootTwo(1, 1)
+LAMBDA_INV = ZRootTwo(-1, 1)
+log_lambda = lambda x: np.emath.logn(1 + SQRT2, float(x))
 
 
 def gridpoints2_increasing(region: ConvexSet):
@@ -109,7 +109,7 @@ def gridpoints_scaled(x, y, k):
     y = (-scale_inv * y[1], -scale_inv * y[0]) if k % 2 else (scale_inv * y[0], scale_inv * y[1])
     a = (x[0] + y[0]) // 2
     b = (SQRT2 * (x[0] - y[0])) // 4
-    alpha = RootTwo(a, b)
+    alpha = ZRootTwo(a, b)
     xoff = a + SQRT2 * b
     yoff = a - SQRT2 * b
     x_ = (x[0] - xoff, x[1] - xoff)
@@ -166,7 +166,7 @@ def gridpoints_internal(x, y):
     amax = (x[1] + y[1]) // 2
     bmin = lambda a: np.ceil((a - y[1]) / SQRT2)
     bmax = lambda a: (a - y[0]) // SQRT2
-    return [RootTwo(a, b) for a in range(amin, amax + 1) for b in range(bmin(a), bmax(a) + 1)]
+    return [ZRootTwo(a, b) for a in range(amin, amax + 1) for b in range(bmin(a), bmax(a) + 1)]
 
 
 def floorlog(b, x):
@@ -184,51 +184,48 @@ def floorlog(b, x):
 
 
 def step_lemma(matA: np.ndarray, matB: np.ndarray) -> Matrix:
-    """The Step Lemma from the paper."""
+    """The Step Lemma from the paper.
+
+    Returns:
+        Matrix[DRootTwo]: the result of the Step Lemma
+    """
     # pylint:disable=too-many-return-statements
     b, l2z = operator_to_bl2z(matA)
     beta, l2zeta = operator_to_bl2z(matB)
 
     if beta < -1e-16:
-        return wlog_using(matA, matB, Matrix([[1, 0], [0, -1]]))
+        return wlog_using(matA, matB, Matrix.array([[1, 0], [0, -1]]))
 
     if l2z * l2zeta < 1:
-        return wlog_using(matA, matB, Matrix([[0, 1], [1, 0]]))
+        return wlog_using(matA, matB, Matrix.array([[0, 1], [1, 0]]))
 
     l2z_minus_zeta = l2z / l2zeta
     if l2z_minus_zeta > 33.971 or l2z_minus_zeta < 0.029437:
-        s_power = np.round(log_lambda(l2z_minus_zeta / 4))
-        s_mat = Matrix([[LAMBDA**s_power, 0], [0, LAMBDA_INV**s_power]])
-        print("s_mat:", s_mat)
+        s_power = int(np.round(log_lambda(l2z_minus_zeta) / 8))
+        s_mat = Matrix.array([[LAMBDA**s_power, 0], [0, LAMBDA_INV**s_power]])
         return wlog_using(matA, matB, s_mat)
 
     if matA[0][1] * matA[1][0] + matB[0][1] * matB[1][0] <= 15:
         return None
 
     if l2z_minus_zeta > 5.8285 or l2z_minus_zeta < 0.17157:
-        return with_shift(matA, matB, int(np.round(log_lambda(l2z_minus_zeta / 4))))
+        return with_shift(matA, matB, int(np.round(log_lambda(l2z_minus_zeta) / 4)))
 
     if 0.24410 <= l2z <= 4.0968 and 0.24410 <= l2zeta <= 4.0968:
-        one_over_root2 = RootTwo(0, Dyadic(1, 1))
-        return Matrix(
-            [
-                [one_over_root2, RootTwo(0, Dyadic(-1, 1))],
-                [one_over_root2, one_over_root2],
-            ],
-        )
+        return Matrix.array([[1, -1], [1, 1]]) / RootTwo(0, 1)
 
     if b >= 0:
         if l2z <= 1.6969:
-            return Matrix([[RootTwo(1, -1), -1], [LAMBDA, 1]]) / np.sqrt(2)
+            return Matrix.array([[-LAMBDA_INV, -1], [LAMBDA, 1]]) / RootTwo(0, 1)
         if l2zeta <= 1.6969:
-            return adj2(Matrix([[RootTwo(1, -1), -1], [LAMBDA, 1]]) / np.sqrt(2))
+            return adj2(Matrix.array([[-LAMBDA_INV, -1], [LAMBDA, 1]]) / RootTwo(0, 1))
         l2c = min(l2z, l2zeta)
         power = max(1, int(np.sqrt(l2c // 4)))
-        return Matrix([[1, 2 * power], [0, 1]])
+        return Matrix.array([[1, -2 * power], [0, 1]])
 
     l2c = min(l2z, l2zeta)
     power = max(1, int(np.sqrt(l2c // 2)))
-    return Matrix([[1, RootTwo(0, power)], [0, 1]])
+    return Matrix.array([[1, ZRootTwo(0, power)], [0, 1]])
 
 
 def wlog_using(matA, matB, op):
@@ -249,20 +246,18 @@ def with_shift(matA, matB, k):
 def shift_sigma(k, op):
     """Shift helper for Step Lemma."""
     (a, b), (c, d) = op
-    a *= LAMBDA**k
-    d *= LAMBDA**-k
-    return Matrix([[a, b], [c, d]])
+    lambda_k = LAMBDA**k
+    return Matrix.array([[a * lambda_k, b], [c, d / lambda_k]])
 
 
 def shift_tau(k, op):
     """Shift helper for Step Lemma."""
     (a, b), (c, d) = op
-    a *= LAMBDA**-k
-    d *= LAMBDA**k
+    lambda_k = LAMBDA**k
     if k % 2:
-        b *= -1
-        c *= -1
-    return Matrix([[a, b], [c, d]])
+        b = -b
+        c = -c
+    return Matrix.array([[a / lambda_k, b], [c, d * lambda_k]])
 
 
 def reduction(matA, matB) -> Matrix:

pennylane/transforms/decompositions/clifford_plus_t/rings.py
@@ -14,64 +14,20 @@
 """Various ring definitions for the gridsynth implementation."""
 # pylint:disable=too-few-public-methods
 
+from fractions import Fraction
 import numpy as np
 
 SQRT2 = float(np.sqrt(2))
 
 
-class RootTwo:
-    """Any object that can be represented as a + b√2"""
-
-    def __init__(self, a, b):
-        """Default constructor."""
-        self.a = a
-        self.b = b
-
-    def __float__(self):
-        """Convert from ring to float."""
-        return float(self.a) + float(self.b) * SQRT2
-
-    def adj2(self):
-        """Return the root-2 adjoint."""
-        return type(self)(self.a, -self.b)
-
-    def __repr__(self):
-        if self.b == 0:
-            return repr(self.a)
-        if self.b > 0:
-            return f"{self.a}+{self.b}√2"
-        return f"{self.a}{self.b}√2"
-
-    def __add__(self, other):
-        if isinstance(other, RootTwo):
-            a = self.a + other.a
-            b = self.b + other.b
-            return a if b == 0 else RootTwo(a, b)
-        return RootTwo(self.a + other, self.b)
-
-    def __mul__(self, other):
-        if isinstance(other, RootTwo):
-            return RootTwo(
-                self.a * other.a + 2 * self.b * other.b,
-                self.a * other.b + self.b * other.a,
-            )
-        return RootTwo(self.a * other, self.b * other)
-
-    __rmul__ = __mul__
-
-    def __eq__(self, other):
-        if isinstance(other, RootTwo):
-            return self.a == other.a and self.b == other.b
-        return np.isclose(float(self), float(other))
-
-    def __neg__(self):
-        return RootTwo(-self.a, -self.b)
-
-
 class Dyadic:
     """Dyadic numbers, of the form x/(2**k) | x,k E Z"""
 
-    def __init__(self, x, k):
+    def __init__(self, x, k=0):
+        if not isinstance(x, int) or not isinstance(k, int):
+            raise TypeError(f"x and k must be ints for Dyadic; got: {x}, {k}")
+        if k < 0:
+            raise ValueError(f"exponent k must be greater than or equal to zero; got {k}")
         self.x = x
         self.k = k
 
@@ -80,12 +36,12 @@ class Dyadic:
 
     def __mul__(self, other):
         if isinstance(other, Dyadic):
-            return Dyadic(self.x * other.x, self.k * other.k)
+            return Dyadic(self.x * other.x, self.k + other.k)
         if isinstance(other, int):
             return Dyadic(self.x * other, self.k)
+        if isinstance(other, RootTwo):
+            return other * self
         return float(self) * other
-
-    __rmul__ = __mul__
 
     def __add__(self, other):
         if isinstance(other, Dyadic):
@@ -96,7 +52,18 @@ class Dyadic:
             return Dyadic(a.x * int(2**k_delta) + b.x, b.k)
         if isinstance(other, int):
             return Dyadic(self.x + other * 2**self.k, self.k)
-        return other + float(self)
+        if isinstance(other, RootTwo):
+            return other + self
+        return float(self) + other
+
+    def __sub__(self, other):
+        if isinstance(other, Dyadic):
+            return self + (-other)
+        if isinstance(other, int):
+            return Dyadic(self.x - other * 2**self.k, self.k)
+        if isinstance(other, RootTwo):
+            return -other + self
+        return float(self) - other
 
     def __eq__(self, other):
         if isinstance(other, Dyadic):
@@ -104,38 +71,170 @@ class Dyadic:
         return np.isclose(float(self), float(other))
 
     def __gt__(self, other):
-        return float(self) >= float(other)
+        return float(self) > float(other)
 
     def __repr__(self):
-        denom = 2 if self.k == 1 else f"2^{self.k}"
-        return f"({self.x}/{denom})"
+        return str(self.x) if self.k == 0 else f"{self.x}/{2**self.k}"
+
+    def __neg__(self):
+        return Dyadic(-self.x, self.k)
+
+    __radd__ = __add__
+    __rmul__ = __mul__
+
+    def conjugate(self):
+        """The complex conjugate. Defaults to itself, needed for Matrix.adjoint()."""
+        return self
 
 
-class Matrix(list):
+class Matrix(np.ndarray):
     """Assumes input is a 2x2 matrix."""
 
-    def __matmul__(self, other):
-        (a1, b1), (c1, d1) = self
-        (a2, b2), (c2, d2) = other
-        return Matrix(
-            [
-                [a1 * a2 + b1 * c2, a1 * b2 + b1 * d2],
-                [c1 * a2 + d1 * c2, c1 * b2 + d1 * d2],
-            ],
-        )
+    @classmethod
+    def array(cls, value) -> "Matrix":
+        """The ndarray constructor is messy, it's easier to provide this (like np.array)."""
+        return np.array(value).view(cls)
 
     def inverse(self):
         """Return the inverse of the matrix, assuming |det M| = 1."""
         (a, b), (c, d) = self
         det = a * d - b * c
         if det == 1:
-            return Matrix([[d, -b], [-c, a]])
+            return self.array([[d, -b], [-c, a]])
         if det == -1:
-            return Matrix([[-d, b], [c, -a]])
+            return self.array([[-d, b], [c, -a]])
         raise ValueError("can only get special inverse.")
 
     def adjoint(self):
         """Returns the adjoint of the matrix.
         TODO: not implemented."""
-        (a, b), (c, d) = self
-        return Matrix([[a, c], [b, d]])
+        return np.conj(self).T
+
+
+class RootTwo:
+    """Any object that can be represented as a + b√2"""
+
+    def __new__(cls, a, b):
+        types = {type(a), type(b)} - {int}
+        if not types:
+            return object.__new__(ZRootTwo)
+        if types == {Dyadic}:
+            return object.__new__(DRootTwo)
+        if types == {Fraction}:
+            return object.__new__(QRootTwo)
+        return float.__new__(float, a + b * SQRT2)
+
+    def __init__(self, a, b):
+        """Default constructor."""
+        dtype = self.dtype  # pylint:disable=no-member
+        self.a = a if isinstance(a, dtype) else dtype(a)
+        self.b = b if isinstance(b, dtype) else dtype(b)
+
+    def conjugate(self):
+        """The complex conjugate. Defaults to itself, needed for Matrix.adjoint()."""
+        return self
+
+    def adj2(self):
+        """Return the root-2 adjoint."""
+        return type(self)(self.a, -self.b)
+
+    def __repr__(self):
+        if self.a == 0:
+            return f"{self.b}√2"
+        if self.b == 0:
+            return repr(self.a)
+        if self.b > 0:
+            return f"{self.a}+{self.b}√2"
+        return f"{self.a}{self.b}√2"
+
+    def __float__(self):
+        """Convert from ring to float."""
+        return float(self.a) + float(self.b) * SQRT2
+
+    def __add__(self, other):
+        if isinstance(other, RootTwo):
+            return RootTwo(self.a + other.a, self.b + other.b)
+        return RootTwo(self.a + other, self.b)
+
+    def __sub__(self, other):
+        if isinstance(other, RootTwo):
+            return RootTwo(self.a - other.a, self.b - other.b)
+        return RootTwo(self.a - other, self.b)
+
+    def __mul__(self, other):
+        if isinstance(other, RootTwo):
+            return RootTwo(
+                self.a * other.a + 2 * self.b * other.b,
+                self.a * other.b + self.b * other.a,
+            )
+        return RootTwo(self.a * other, self.b * other)
+
+    def __eq__(self, other):
+        if isinstance(other, RootTwo):
+            return self.a == other.a and self.b == other.b
+        return np.isclose(float(self), float(other))
+
+    def __gt__(self, other):
+        return float(self) > float(other)
+
+    def __lt__(self, other):
+        return float(self) < float(other)
+
+    def __neg__(self):
+        return type(self)(-self.a, -self.b)
+
+    def __pow__(self, power):
+        if not isinstance(power, int):
+            raise ValueError(f"Cannot raise RootTwo to non-int power {power}")
+        if power == 0:
+            return 1
+        if power < 0:
+            return 1 / (self**-power)
+        result = self
+        power -= 1
+        while power > 0:
+            result *= self
+            power -= 1
+        return result
+
+    def __truediv__(self, other):
+        if isinstance(other, RootTwo):
+            return self * (1 / other)
+        return float(self) / other
+
+    def __rtruediv__(self, other):
+        if isinstance(other, RootTwo):
+            return other * (1 / self)
+        return other / float(self)
+
+    __radd__ = __add__
+    __rmul__ = __mul__
+
+
+class ZRootTwo(RootTwo):
+    """Z√2"""
+
+    dtype = int
+
+    def __truediv__(self, other):
+        if isinstance(other, int):
+            return QRootTwo(Fraction(self.a, other), Fraction(self.b, other))
+        return super().__truediv__(other)
+
+    def __rtruediv__(self, other):
+        if isinstance(other, int):
+            k = self.a**2 - 2 * self.b**2
+            return QRootTwo(Fraction(other * self.a, k), Fraction(-other * self.b, k))
+        return super().__rtruediv__(other)
+
+
+class DRootTwo(RootTwo):
+    """D√2"""
+
+    dtype = Dyadic
+
+
+class QRootTwo(RootTwo):
+    """Q√2"""
+
+    dtype = Fraction