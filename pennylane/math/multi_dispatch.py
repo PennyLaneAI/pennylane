5980358de18add5e4fa2c59707ab7ff63e2e712d
albi3ro <chrissie.c.l@gmail.com>
2023-10-05 12:21:20 -0400

    fix nested unwrap

--- pennylane/math/multi_dispatch.py
+++ pennylane/math/multi_dispatch.py
@@ -754,7 +754,7 @@ def unwrap(values, max_depth=None):
     """
 
     def convert(val):
-        if isinstance(val, list):
+        if isinstance(val, (tuple, list)):
             return unwrap(val)
         new_val = (
             np.to_numpy(val, max_depth=max_depth) if isinstance(val, ArrayBox) else np.to_numpy(val)
