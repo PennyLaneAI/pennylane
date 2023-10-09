46491613a976509bf1388c0945377fa304d24c96
ankit_linux <ankit27.kh@gmail.com>
2023-10-09 08:40:24 +0530

    add order logic

--- pennylane/templates/subroutines/aqft.py
+++ pennylane/templates/subroutines/aqft.py
@@ -16,6 +16,7 @@ This submodule contains the template for AQFT.
 """
 # pylint:disable=abstract-method,arguments-differ,protected-access
 
+import warnings
 import numpy as np
 
 import pennylane as qml
@@ -29,8 +30,26 @@ class AQFT(Operation):
 
     def __init__(self, order, wires=None, id=None):
         wires = qml.wires.Wires(wires)
-        self.hyperparameters["order"] = order
+
         self.hyperparameters["n_wires"] = len(wires)
+
+        if not isinstance(order, int):
+            warnings.warn(f"The order must be an integer. Using order = {round(order)}")
+            order = round(order)
+
+        if order >= self.hyperparameters["n_wires"] - 1:
+            warnings.warn(
+                f'The order ({order}) is >= to the number of wires - 1 ({self.hyperparameters["n_wires"]-1}). Using the QFT class is recommended in this case.'
+            )
+            order = self.hyperparameters["n_wires"] - 1
+
+        if order < 0:
+            raise ValueError("Order can not be less than 0")
+
+        if order == 0:
+            warnings.warn("order=0, applying Hadamard transform")
+
+        self.hyperparameters["order"] = order
         super().__init__(wires=wires, id=id)
 
     @property
@@ -41,7 +60,27 @@ class AQFT(Operation):
     def compute_decomposition(
         wires, n_wires, order
     ):  # pylint: disable=arguments-differ,unused-argument
-        r"""stuff"""
+        r"""Representation of the operator as a product of other operators (static method).
+
+        .. math:: O = O_1 O_2 \dots O_n.
+
+
+        .. seealso:: :meth:`~.AQFT.decomposition`.
+
+        Args:
+            wires (Iterable, Wires): wires that the operator acts on
+            n_wires (int): number of wires or ``len(wires)``
+            order (int): order of approximation
+
+        Returns:
+            list[Operator]: decomposition of the operator
+
+        **Example:**
+
+        >>> qml.AQFT.compute_decomposition((0,1,2,3),3,order=1)
+        [Hadamard(wires=[0]), ControlledPhaseShift(1.5707963267948966, wires=[1, 0]), Hadamard(wires=[1]), ControlledPhaseShift(1.5707963267948966, wires=[2, 1]), Hadamard(wires=[2]), Hadamard(wires=[3]), SWAP(wires=[0, 3])]
+
+        """
         shifts = [2 * np.pi * 2**-i for i in range(2, n_wires + 1)]
 
         decomp_ops = []
@@ -50,11 +89,12 @@ class AQFT(Operation):
             counter = 0
 
             for shift, control_wire in zip(shifts[: len(shifts) - i], wires[i + 1 :]):
+                if counter >= order:
+                    break
+
                 op = qml.ControlledPhaseShift(shift, wires=[control_wire, wire])
                 decomp_ops.append(op)
                 counter = counter + 1
-                if counter >= order:
-                    break
 
         first_half_wires = wires[: n_wires // 2]
         last_half_wires = wires[-(n_wires // 2) :]
