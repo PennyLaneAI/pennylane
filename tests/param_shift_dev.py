4c013bf32e79a1377938a6b59dc87feb51794d54
albi3ro <chrissie.c.l@gmail.com>
2023-10-19 14:16:06 -0400

    fix merge issue

--- tests/param_shift_dev.py
+++ tests/param_shift_dev.py
@@ -73,3 +73,83 @@ class ParamShiftDerivativesDevice(qml.devices.DefaultQubit):
         results = all_results[: len(circuits)]
         jacs = fn(all_results[len(circuits) :])
         return (results[0], jacs[0]) if is_single_circuit else (results, jacs)
+
+    def compute_jvp(self, circuits, tangents, execution_config=qml.devices.DefaultExecutionConfig):
+        is_single_circuit = False
+        if isinstance(circuits, qml.tape.QuantumScript):
+            is_single_circuit = True
+            circuits = (circuits,)
+            tangents = (tangents,)
+
+        if self.tracker.active:
+            self.tracker.update(jvp_batches=1, jvps=len(circuits))
+            self.tracker.record()
+
+        batch, fn = qml.gradient.batch_jvp(circuits, tangents, qml.gradients.param_shift)
+        results = self.execute(batch)
+        return fn(results)[0] if is_single_circuit else fn(results)
+
+    def execute_and_compute_jvp(
+        self, circuits, tangents, execution_config=qml.devices.DefaultExecutionConfig
+    ):
+        is_single_circuit = False
+        if isinstance(circuits, qml.tape.QuantumScript):
+            is_single_circuit = True
+            circuits = [circuits]
+            tangents = [tangents]
+
+        if self.tracker.active:
+            for c in circuits:
+                self.tracker.update(resources=c.specs["resources"])
+            self.tracker.update(
+                execute_and_jvp_batches=1, executions=len(circuits), jvps=len(circuits)
+            )
+            self.tracker.record()
+
+        batch, fn = qml.gradients.batch_jvp(circuits, tangents, qml.gradients.param_shift)
+        full_batch = tuple(circuits) + tuple(batch)
+        all_results = self.execute(full_batch)
+        results = all_results[: len(circuits)]
+        jvps = fn(all_results[len(circuits) :])
+        return (results[0], jvps[0]) if is_single_circuit else results, jvps
+
+    def compute_vjp(
+        self, circuits, cotangents, execution_config=qml.devices.DefaultExecutionConfig
+    ):
+        is_single_circuit = False
+        if isinstance(circuits, qml.tape.QuantumScript):
+            is_single_circuit = True
+            circuits = [circuits]
+            cotangents = [cotangents]
+
+        if self.tracker.active:
+            self.tracker.update(vjp_batches=1, vjps=len(circuits))
+            self.tracker.record()
+
+        batch, fn = qml.gradients.batch_vjp(circuits, cotangents, qml.gradients.param_shift)
+        results = self.execute(batch)
+        return fn(results)[0] if is_single_circuit else fn(results)
+
+    def execute_and_compute_vjp(
+        self, circuits, cotangents, execution_config=qml.devices.DefaultExecutionConfig
+    ):
+        is_single_circuit = False
+        if isinstance(circuits, qml.tape.QuantumScript):
+            is_single_circuit = True
+            circuits = [circuits]
+            cotangents = [cotangents]
+
+        if self.tracker.active:
+            for c in circuits:
+                self.tracker.update(resources=c.specs["resources"])
+            self.tracker.update(
+                execute_and_vjp_batches=1, executions=len(circuits), vjps=len(circuits)
+            )
+            self.tracker.record()
+
+        batch, fn = qml.gradients.batch_vjp(circuits, cotangents, qml.gradients.param_shift)
+        full_batch = tuple(circuits) + tuple(batch)
+        all_results = self.execute(full_batch)
+        results = all_results[: len(circuits)]
+        vjps = fn(all_results[len(circuits) :])
+        return (results[0], vjps[0]) if is_single_circuit else results, vjps
