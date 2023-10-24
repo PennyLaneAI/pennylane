00e8dc73dc64a7042c3b8a8bb5be9bf6e36eac37
rmoyard <rmoyard@gmail.com>
2023-10-24 18:49:41 -0400

    Draft

---
+++ pennylane/gradients/vjp_jvp.py
@@ -0,0 +1,131 @@
+# Copyright 2018-2021 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This module contains functions for computing the vector-Jacobian product and Jacobian-vector product from Catalyst.
+"""
+from pennylane.compiler import compiler
+
+# pylint: disable=too-many-arguments
+def vjp(f, params, cotangents, *, method=None, h=None, argnum=None):
+    """A :func:`~.qjit` compatible Vector-Jacobian product for PennyLane/Catalyst.
+
+    This function allows the Vector-Jacobian Product of a hybrid quantum-classical function to be
+    computed within the compiled program.
+
+    Args:
+        f(Callable): Function-like object to calculate JVP for
+        params(List[Array]): List (or a tuple) of f's arguments specifying the point to calculate
+                             VJP at. A subset of these parameters are declared as
+                             differentiable by listing their indices in the ``argnum`` parameter.
+        cotangents(List[Array]): List (or a tuple) of tangent values to use in JVP. The list size
+                                 and shapes must match the size and shape of ``f`` outputs.
+        method(str): Differentiation method to use, same as in ``grad``.
+        h (float): the step-size value for the finite-difference (``"fd"``) method
+        argnum (Union[int, List[int]]): the params' indices to differentiate.
+
+    Returns (Tuple[Array]):
+        Return values of ``f`` paired with the JVP values.
+
+    Raises:
+        TypeError: invalid parameter types
+        ValueError: invalid parameter values
+
+    **Example**
+
+    .. code-block:: python
+
+        @qjit
+        def vjp(params, cotangent):
+          def f(x):
+              y = [jnp.sin(x[0]), x[1] ** 2, x[0] * x[1]]
+              return jnp.stack(y)
+
+          return catalyst.vjp(f, [params], [cotangent])
+
+    >>> x = jnp.array([0.1, 0.2])
+    >>> dy = jnp.array([-0.5, 0.1, 0.3])
+    >>> vjp(x, dy)
+    [array([0.09983342, 0.04      , 0.02      ]),
+    array([-0.43750208,  0.07000001])]
+    """
+
+
+
+# pylint: disable=too-many-arguments
+def jvp(f, params, tangents, *, method=None, h=None, argnum=None):
+    """A :func:`~.qjit` compatible Jacobian-vector product for PennyLane/Catalyst.
+
+    This function allows the Jacobian-vector Product of a hybrid quantum-classical function to be
+    computed within the compiled program.
+
+    Args:
+        f (Callable): Function-like object to calculate JVP for
+        params (List[Array]): List (or a tuple) of the function arguments specifying the point
+                              to calculate JVP at. A subset of these parameters are declared as
+                              differentiable by listing their indices in the ``argnum`` parameter.
+        tangents(List[Array]): List (or a tuple) of tangent values to use in JVP. The list size and
+                               shapes must match the ones of differentiable params.
+        method(str): Differentiation method to use, same as in :func:`~.grad`.
+        h (float): the step-size value for the finite-difference (``"fd"``) method
+        argnum (Union[int, List[int]]): the params' indices to differentiate.
+
+    Returns (Tuple[Array]):
+        Return values of ``f`` paired with the JVP values.
+
+    Raises:
+        TypeError: invalid parameter types
+        ValueError: invalid parameter values
+
+    **Example 1 (basic usage)**
+
+    .. code-block:: python
+
+        @qjit
+        def jvp(params, tangent):
+          def f(x):
+              y = [jnp.sin(x[0]), x[1] ** 2, x[0] * x[1]]
+              return jnp.stack(y)
+
+          return catalyst.jvp(f, [params], [tangent])
+
+    >>> x = jnp.array([0.1, 0.2])
+    >>> tangent = jnp.array([0.3, 0.6])
+    >>> jvp(x, tangent)
+    [array([0.09983342, 0.04      , 0.02      ]),
+    array([0.29850125, 0.24000006, 0.12      ])]
+
+    **Example 2 (argnum usage)**
+
+    Here we show how to use ``argnum`` to ignore the non-differentiable parameter ``n`` of the
+    target function. Note that the length and shapes of tangents must match the length and shape of
+    primal parameters which we mark as differentiable by passing their indices to ``argnum``.
+
+    .. code-block:: python
+
+        @qjit
+        @qml.qnode(qml.device("lightning.qubit", wires=2))
+        def circuit(n, params):
+            qml.RX(params[n, 0], wires=n)
+            qml.RY(params[n, 1], wires=n)
+            return qml.expval(qml.PauliZ(1))
+
+        @qjit
+        def workflow(primals, tangents):
+            return catalyst.jvp(circuit, [1, primals], [tangents], argnum=[1])
+
+    >>> params = jnp.array([[0.54, 0.3154], [0.654, 0.123]])
+    >>> dy = jnp.array([[1.0, 1.0], [1.0, 1.0]])
+    >>> workflow(params, dy)
+    [array(0.78766064), array(-0.7011436)]
+    """
