bdd5f91d5e86a8e228173f78ed0d44a1c5911681
Matthew Silverman <matthews@xanadu.ai>
2023-10-26 14:06:09 -0400

    add comment

--- pennylane/gradients/spsa_gradient.py
+++ pennylane/gradients/spsa_gradient.py
@@ -330,6 +330,8 @@ def spsa_grad(
     all_coeffs = []
     for idx_rep in range(num_directions):
         direction = sampler(indices, num_trainable_params, idx_rep, rng=sampler_rng)
+        # the `where` arg is being cast to list to avoid unexpected side effects from types that
+        # override __array_ufunc__. See https://github.com/numpy/numpy/pull/23240 for more details
         inv_direction = qml.math.divide(
             1, direction, where=list(direction != 0), out=qml.math.zeros_like(direction)
         )
