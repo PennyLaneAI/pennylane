0a116f236f471d5a15ca4a4327cba0823afa916d
Anurav Modak <anurav.modak@oracle.com>
2023-11-05 22:38:17 +0530

    Group utility operations into same file. V2
    Signed-off-by: Anurav Modak <anurav.modak@oracle.com>

--- pennylane/ops/cv.py
+++ pennylane/ops/cv.py
@@ -45,7 +45,7 @@ from pennylane import math as qml_math
 from pennylane.operation import AnyWires, CVObservable, CVOperation
 
 from .identity import Identity  # pylint: disable=unused-import
-from .meta import Snapshot
+# from .meta import Snapshot
 
 _two_term_shift_rule = [[0.5, 1, np.pi / 2], [-0.5, 1, -np.pi / 2]]
 
