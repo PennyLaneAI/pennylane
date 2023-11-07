7493a37fe7bf7b6cdbbe034db9baf62658078930
obliviateandsurrender <utkarshazad98@gmail.com>
2023-11-07 15:43:13 -0500

    `transform` init

--- pennylane/transforms/__init__.py
+++ pennylane/transforms/__init__.py
@@ -74,6 +74,17 @@ There are also utility functions that take a circuit and return a DAG.
     ~transforms.CommutationDAG
     ~transforms.CommutationDAGNode
 
+Transform for Clifford+T compilation
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The following functions assist in decomposing operations to the Clifford+T basis.
+
+.. autosummary::
+    :toctree: api
+
+    ~transforms.sk_decomposition
+    ~transforms.decompositions.sk_approximate_set
+
 Transform for circuit cutting
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -243,6 +254,7 @@ from .compile import compile
 from .decompositions import (
     one_qubit_decomposition,
     two_qubit_decomposition,
+    sk_decomposition,
 )
 from .defer_measurements import defer_measurements
 from .sign_expand import sign_expand
--- pennylane/transforms/optimization/optimization_utils.py
+++ pennylane/transforms/optimization/optimization_utils.py
@@ -18,8 +18,19 @@ from functools import partial
 import numpy as np
 
 from pennylane.math import abs as math_abs
-from pennylane.math import allclose, arccos, arctan2, cos, get_interface, is_abstract, sin, stack
+from pennylane.math import (
+    allclose,
+    arccos,
+    arctan2,
+    cos,
+    get_interface,
+    is_abstract,
+    sin,
+    stack,
+    sum,
+)
 from pennylane.wires import Wires
+from pennylane.ops.identity import GlobalPhase
 
 
 def find_next_gate(wires, op_list):
@@ -185,3 +196,24 @@ def fuse_rot_angles(angles_1, angles_2):
         return _no_fuse(angles_1, angles_2)
 
     return _fuse(angles_1, angles_2)
+
+
+def _fuse_global_phases(operations):
+    """Fuse all the global phase operations into single one
+
+    Args:
+        operations (list[Operation]): list of operations to be iterated over
+
+    Returns:
+        transformed list with a single :func:`~.pennylane.GlobalPhase` operation.
+    """
+
+    fused_ops, global_ops = [], []
+    for op in operations:
+        if isinstance(op, GlobalPhase):
+            global_ops.append(op)
+        else:
+            fused_ops.append(op)
+
+    fused_ops.append(GlobalPhase(sum(op.data[0] for op in global_ops)))
+    return fused_ops
