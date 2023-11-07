3cc2c09306515bec39fcf007c404fca4c345ebb4
Ali Asadi <ali@xanadu.ai>
2023-11-07 10:57:35 -0500

    [Compiler] Add Catalyst jacobian and grad methods (#4709)
    Context:
    This PR adds support for QJIT compatible `qml.jacobian` and `qml.grad`.
    Please see ADR 74 for implementation details.
    
    [sc-46406]
    
    ---------
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -21,6 +21,8 @@ TODO: Uncomment 'pytest.mark.external' to check these tests in GitHub actions wi
 import pytest
 import pennylane as qml
 
+from pennylane import numpy as np
+
 catalyst = pytest.importorskip("catalyst")
 jax = pytest.importorskip("jax")
 
@@ -206,3 +208,139 @@ class TestCatalyst:
         mlir_str = str(circuit.mlir)
         result_header = "func.func private @circuit(%arg0: tensor<f64>) -> tensor<f64>"
         assert result_header in mlir_str
+
+    def test_grad_classical_preprocessing(self):
+        """Test the grad transformation with classical preprocessing."""
+
+        dev = qml.device("lightning.qubit", wires=1)
+
+        @qml.qjit
+        def workflow(x):
+            @qml.qnode(dev)
+            def circuit(x):
+                qml.RX(jnp.pi * x, wires=0)
+                return qml.expval(qml.PauliY(0))
+
+            g = qml.grad(circuit)
+            return g(x)
+
+        assert jnp.allclose(workflow(2.0), -jnp.pi)
+
+    def test_grad_with_postprocessing(self):
+        """Test the grad transformation with classical preprocessing and postprocessing."""
+        dev = qml.device("lightning.qubit", wires=1)
+
+        @qml.qjit
+        def workflow(theta):
+            @qml.qnode(dev, diff_method="adjoint")
+            def circuit(theta):
+                qml.RX(jnp.exp(theta**2) / jnp.cos(theta / 4), wires=0)
+                return qml.expval(qml.PauliZ(wires=0))
+
+            def loss(theta):
+                return jnp.pi / jnp.tanh(circuit(theta))
+
+            return qml.grad(loss, method="auto")(theta)
+
+        assert jnp.allclose(workflow(1.0), 5.04324559)
+
+    def test_grad_with_multiple_qnodes(self):
+        """Test the grad transformation with multiple QNodes with their own differentiation methods."""
+        dev = qml.device("lightning.qubit", wires=1)
+
+        @qml.qjit
+        def workflow(theta):
+            @qml.qnode(dev, diff_method="parameter-shift")
+            def circuit_A(params):
+                qml.RX(jnp.exp(params[0] ** 2) / jnp.cos(params[1] / 4), wires=0)
+                return qml.probs()
+
+            @qml.qnode(dev, diff_method="adjoint")
+            def circuit_B(params):
+                qml.RX(jnp.exp(params[1] ** 2) / jnp.cos(params[0] / 4), wires=0)
+                return qml.expval(qml.PauliZ(wires=0))
+
+            def loss(params):
+                return jnp.prod(circuit_A(params)) + circuit_B(params)
+
+            return qml.grad(loss)(theta)
+
+        result = workflow(jnp.array([1.0, 2.0]))
+        reference = jnp.array([0.57367285, 44.4911605])
+
+        assert jnp.allclose(result, reference)
+
+    def test_grad_with_pure_classical(self):
+        """Test the grad transformation with purely classical functions."""
+
+        def square(x: float):
+            return x**2
+
+        @qml.qjit
+        def dsquare(x: float):
+            return catalyst.grad(square)(x)
+
+        assert jnp.allclose(dsquare(2.3), 4.6)
+
+    def test_jacobian_diff_method(self):
+        """Test the Jacobian transformation with the device diff_method."""
+        dev = qml.device("lightning.qubit", wires=1)
+
+        @qml.qnode(dev, diff_method="parameter-shift")
+        def func(p):
+            qml.RY(p, wires=0)
+            return qml.probs(wires=0)
+
+        @qml.qjit
+        def workflow(p: float):
+            return qml.jacobian(func, method="auto")(p)
+
+        result = workflow(0.5)
+        reference = qml.jacobian(func, argnum=0)(0.5)
+
+        assert jnp.allclose(result, reference)
+
+    def test_jacobian_auto(self):
+        """Test the Jacobian transformation with 'auto'."""
+        dev = qml.device("lightning.qubit", wires=1)
+
+        def workflow(x):
+            @qml.qnode(dev)
+            def circuit(x):
+                qml.RX(jnp.pi * x[0], wires=0)
+                qml.RY(x[1], wires=0)
+                return qml.probs()
+
+            g = qml.jacobian(circuit)
+            return g(x)
+
+        reference = workflow(np.array([2.0, 1.0]))
+        result = qml.qjit(workflow)(jnp.array([2.0, 1.0]))
+
+        assert jnp.allclose(result, reference)
+
+    def test_jacobian_fd(self):
+        """Test the Jacobian transformation with 'fd'."""
+        dev = qml.device("lightning.qubit", wires=1)
+
+        def workflow(x):
+            @qml.qnode(dev)
+            def circuit(x):
+                qml.RX(np.pi * x[0], wires=0)
+                qml.RY(x[1], wires=0)
+                return qml.probs()
+
+            g = qml.jacobian(circuit, method="fd", step_size=0.3)
+            return g(x)
+
+        result = qml.qjit(workflow)(np.array([2.0, 1.0]))
+        print(result)
+
+        reference = np.array([[-0.37120096, -0.45467246], [0.37120096, 0.45467246]])
+        print(jnp.allclose(result, reference))
+
+        with pytest.raises(
+            ValueError,
+            match="Invalid values for 'method=fd' and 'step_size=0.3' in interpreted mode",
+        ):
+            workflow(np.array([2.0, 1.0]))
