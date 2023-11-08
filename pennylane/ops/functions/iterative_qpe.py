fb62e5e744552a0134ff9f29a97d3162a9705d08
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2023-11-08 12:51:29 -0500

    Update pennylane/ops/functions/iterative_qpe.py
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>

--- pennylane/ops/functions/iterative_qpe.py
+++ pennylane/ops/functions/iterative_qpe.py
@@ -23,7 +23,7 @@ def iterative_qpe(base, estimation_wire, iters):
     r"""Performs the `iterative quantum phase estimation <https://arxiv.org/pdf/quant-ph/0610214.pdf>`_ circuit.
 
     Given a unitary matrix :math:`U`, this function applies the circuit for iterative quantum phase
-    estimation and returns a list of the mid-circuit measurements.
+    estimation and returns a list of the mid-circuit measurements with qubit reset.
 
     Args:
       base (Operator): the phase estimation unitary, specified as an :class:`~.Operator`
