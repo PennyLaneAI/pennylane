469bd62d0a192a20246a4656dff06dda31eb881c
AmintorDusko <amintor_dusko@hotmail.com>
2023-11-09 07:56:51 -0500

    add conversion to JSON-XUBM before upload

---
+++ .github/workflows/benchmark_scripts/convert_pytest_JSON_to_XUBM.py
@@ -0,0 +1,76 @@
+import argparse, json
+########################################################################
+# Parsing arguments
+########################################################################
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--filename",
+        type=str,
+        default="benchmarks.json",
+        nargs="?",
+        help="Name of the Pytest-benchmark JSON file to be imported.",
+    )
+
+    parser.add_argument(
+        "--filename_XUBM",
+        type=str,
+        default="benchmarks_xubm.json",
+        nargs="?",
+        help="Name of the XUBM JSON file to be exported.",
+    )
+
+    parser.add_argument(
+        "--author",
+        type=str,
+        nargs="?",
+        help="User associated with the commit.",
+    )
+
+    return parser.parse_args()
+
+def create_benchmark_XUBM(data, args):
+    XUBM_data = {"xubm": []}
+
+    hash_commit = hash(data["commit_info"]["time"]) + hash(data["commit_info"]["branch"])
+    for benchmark in data["benchmarks"]:
+        benchmark_xubm = {}
+        #I propose an alternative way of calcuting the unique identifier with information provided by pytest:
+        benchmark_xubm["uid"] = hash_commit + hash(benchmark["fullname"]) + hash(json.dumps(benchmark["params"]))
+        benchmark_xubm["date"] = data["commit_info"]["time"]
+        benchmark_xubm["name"] = benchmark["name"]
+        # pytest provide more complete information than what we defined for "suite" ["Directory & filename"].
+        # It will provide the full address of the test, relative to the tests folder.
+        # Ex: "devices/test_default_qubit_jax.py::TestQNodeIntegration::test_qubit_circuit_with_jit"
+        # Because of how Pennylane structure its test suite, the reference position will vary (we have more than one tests directory.)
+        # I will follow pytest and call its fullname for now:
+        benchmark_xubm["fullname"] = benchmark["fullname"]
+        benchmark_xubm["gitID"] = data["commit_info"]["id"]
+        benchmark_xubm["runs"] = benchmark["stats"]["iterations"]
+        benchmark_xubm["params"] = benchmark["params"]
+        # benchmark_xubm["runtime"] = benchmark["stats"]["mean"] * (1 + random.uniform(-0.25, 0.25))
+        benchmark_xubm["runtime"] = benchmark["stats"]["mean"]
+        #Results are always in second:
+        benchmark_xubm["timeUnit"] = "seconds"
+        benchmark_xubm["project"] = data["commit_info"]["project"]
+        #Pytest provides a lot of machine info, we may consider filter it:
+        benchmark_xubm["machine"] = data["machine_info"]
+
+        benchmark_xubm["users"] = args.author
+        # Pytest-benchmark provides a reach variety of statistics, I'm storing it as metadata.
+        benchmark_xubm["metadata"] = {"stats": benchmark["stats"]}
+
+        XUBM_data["xubm"] += [benchmark_xubm, ]
+    return XUBM_data
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    file = open(args.filename)
+    data = json.load(file)
+
+    XUBM_data = create_benchmark_XUBM(data, args)
+
+    with open(args.filename_XUBM, 'w') as file:
+        json.dump(XUBM_data, fp=file)
