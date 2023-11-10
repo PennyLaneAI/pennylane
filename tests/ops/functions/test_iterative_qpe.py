12f0217f7f491435514d475b96db315f2fde5260
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2023-11-10 14:09:36 -0500

    Update test_iterative_qpe.py

--- tests/ops/functions/test_iterative_qpe.py
+++ tests/ops/functions/test_iterative_qpe.py
@@ -120,7 +120,7 @@ class TestIQPE:
         phi = torch.tensor(1.0, requires_grad=True)
         assert torch.isclose(torch.func.grad(circuit)(phi), torch.func.grad(manual_circuit)(phi))
 
-    @pytest.mark.tensorflow
+    @pytest.mark.tf
     def test_check_gradients_tf(self):
         """Test to check that the gradients are correct comparing with the expanded circuit using TensorFlow"""
 
