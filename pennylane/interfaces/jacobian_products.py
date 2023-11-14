fix jax jacobians when one circuit not trainable

pennylane/interfaces/jacobian_products.py
@@ -21,6 +21,7 @@ import logging
 from typing import Tuple, Callable, Optional, Union
 
 from cachetools import LRUCache
+import numpy as np
 
 import pennylane as qml
 from pennylane.tape import QuantumScript
@@ -54,7 +55,14 @@ def _compute_jvps(jacs, tangents, tapes):
     jvps = []
     for jac, dx, t in zip(jacs, tangents, tapes):
         multi = len(t.measurements) > 1
-        if t.shots.has_partitioned_shots:
+        if len(t.trainable_params) == 0:
+            zeros_jvp = tuple(np.zeros(mp.shape(None, t.shots)) for mp in t.measurements)
+            zeros_jvp = zeros_jvp[0] if len(t.measurements) == 1 else zeros_jvp
+            if t.shots.has_partitioned_shots:
+                jvps.extend(zeros_jvp for _ in t.shots.num_copies)
+            else:
+                jvps.append(zeros_jvp)
+        elif t.shots.has_partitioned_shots:
             jvps.append(tuple(f[multi](dx, j) for j in jac))
         else:
             jvps.append(f[multi](dx, jac))

tests/interfaces/default_qubit_2_integration/test_jax_default_qubit_2.py
@@ -250,19 +250,12 @@ class TestJaxExecuteIntegration:
         expected = 2 + jnp.cos(0.5) + jnp.cos(x) * jnp.cos(y)
         assert np.allclose(res, expected, atol=atol_for_shots(shots), rtol=0)
 
-        # TODO: jax does not allow computing tapes with different gradient
-        # shapes like [None, None, *, None] here
-
-        # grad = jax.grad(cost)(params)
-        # expected = [-jnp.cos(y) * jnp.sin(x), -jnp.cos(x) * jnp.sin(y)]
-        # assert np.allclose(grad, expected, atol=atol_for_shots(shots), rtol=0)
+        grad = jax.grad(cost)(params)
+        expected = [-jnp.cos(y) * jnp.sin(x), -jnp.cos(x) * jnp.sin(y)]
+        assert np.allclose(grad, expected, atol=atol_for_shots(shots), rtol=0)
 
     def test_tapes_with_different_return_size(self, execute_kwargs, shots, device):
         """Test that tapes wit different can be executed and differentiated."""
-
-        # TODO: Will probably fail if we update jax to fix this test
-        # if execute_kwargs["gradient_fn"] == "backprop":
-        #     pytest.xfail("backprop is not compatible with something about this situation.")
 
         def cost(params):
             tape1 = qml.tape.QuantumScript(
@@ -294,21 +287,19 @@ class TestJaxExecuteIntegration:
         assert np.allclose(res[2], jnp.cos(0.5), atol=atol_for_shots(shots))
         assert np.allclose(res[3], jnp.cos(x) * jnp.cos(y), atol=atol_for_shots(shots))
 
-        # TODO: jax does not allow computing tapes with different gradient shapes
+        jac = jax.jacobian(cost)(params)
+        assert isinstance(jac, jnp.ndarray)
+        assert jac.shape == (4, 2)  # pylint: disable=no-member
 
-        # jac = jax.jacobian(cost)(params)
-        # assert isinstance(jac, jnp.ndarray)
-        # assert jac.shape == (4, 2)  # pylint: disable=no-member
+        assert np.allclose(jac[1:3], 0, atol=atol_for_shots(shots))
 
-        # assert np.allclose(jac[1:3], 0, atol=atol_for_shots(shots))
+        d1 = -jnp.sin(x) * jnp.cos(y)
+        assert np.allclose(jac[0, 0], d1, atol=atol_for_shots(shots))
+        assert np.allclose(jac[3, 0], d1, atol=atol_for_shots(shots))
 
-        # d1 = -jnp.sin(x) * jnp.cos(y)
-        # assert np.allclose(jac[0, 0], d1, atol=atol_for_shots(shots))
-        # assert np.allclose(jac[3, 0], d1, atol=atol_for_shots(shots))
-
-        # d2 = -jnp.cos(x) * jnp.sin(y)
-        # assert np.allclose(jac[0, 1], d2, atol=atol_for_shots(shots))
-        # assert np.allclose(jac[3, 1], d2, atol=atol_for_shots(shots))
+        d2 = -jnp.cos(x) * jnp.sin(y)
+        assert np.allclose(jac[0, 1], d2, atol=atol_for_shots(shots))
+        assert np.allclose(jac[3, 1], d2, atol=atol_for_shots(shots))
 
     def test_reusing_quantum_tape(self, execute_kwargs, shots, device):
         """Test re-using a quantum tape by passing new parameters"""

tests/interfaces/test_jax.py
@@ -148,6 +148,13 @@ class TestJaxExecuteUnitTests:
         assert dev.num_executions == 3
         spy.assert_called()
 
+        g = jax.jacobian(cost)(a)
+        expected_g = (-np.sin(x) * np.cos(y), -np.cos(x) * np.sin(y))
+        assert qml.math.allclose(g[0][0], expected_g)
+        assert qml.math.allclose(g[0][1], np.zeros(2))
+        assert qml.math.allclose(g[1], np.zeros(2))
+        assert qml.math.allclose(g[2], expected_g)
+
     def test_no_grad_on_execution(self, mocker):
         """Test that no grad on execution uses the `device.batch_execute` and `device.gradients` pathway"""
         dev = qml.device("default.qubit.legacy", wires=1)
@@ -592,7 +599,7 @@ class TestJaxExecuteIntegration:
         res = cost(a, U, device=dev)
         assert np.allclose(res, -np.cos(a), atol=tol, rtol=0)
 
-        jac_fn = jax.grad(cost, argnums=(0))
+        jac_fn = jax.grad(cost, argnums=0)
         res = jac_fn(a, U, device=dev)
         assert np.allclose(res, np.sin(a), atol=tol, rtol=0)
 
@@ -629,7 +636,7 @@ class TestJaxExecuteIntegration:
         )
         assert np.allclose(res, expected, atol=tol, rtol=0)
 
-        jac_fn = jax.grad(cost_fn, argnums=(1))
+        jac_fn = jax.grad(cost_fn, argnums=1)
         res = jac_fn(a, p, device=dev)
         expected = jax.numpy.array(
             [