a72180f0c0e249b889fd338ffda4065fa0c6900f
Gabriel Bottrill <78718539+Gabriel-Bottrill@users.noreply.github.com>
2023-11-17 12:35:21 -0800

    Update pennylane/devices/default_qutrit.py
    Co-authored-by: Olivia Di Matteo <2068515+glassnotes@users.noreply.github.com>

--- pennylane/devices/default_qutrit.py
+++ pennylane/devices/default_qutrit.py
@@ -22,7 +22,6 @@ import functools
 import numpy as np
 
 import pennylane as qml  # pylint: disable=unused-import
-
 from pennylane import QutritDevice, QutritBasisState, DeviceError
 from pennylane.wires import WireError
 from pennylane.devices.default_qubit_legacy import _get_slice
