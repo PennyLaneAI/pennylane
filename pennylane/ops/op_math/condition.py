0581d3c43b4281fe19002385231c8fc3de7e5f7d
Mudit Pandey <mudit.pandey@xanadu.ai>
2023-11-20 17:30:45 -0500

    Removed type hint

--- pennylane/ops/op_math/condition.py
+++ pennylane/ops/op_math/condition.py
@@ -17,7 +17,6 @@ Contains the condition transform.
 from functools import wraps
 from typing import Type
 
-import pennylane as qml
 from pennylane.operation import AnyWires, Operation
 from pennylane.tape import make_qscript
 
@@ -48,9 +47,7 @@ class Conditional(Operation):
 
     num_wires = AnyWires
 
-    def __init__(
-        self, expr: "qml.measurements.MeasurementValue[bool]", then_op: Type[Operation], id=None
-    ):
+    def __init__(self, expr, then_op: Type[Operation], id=None):
         self.meas_val = expr
         self.then_op = then_op
         super().__init__(wires=then_op.wires, id=id)
