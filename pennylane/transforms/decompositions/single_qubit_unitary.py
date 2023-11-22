e18b4b66adcb3bb649f9b3e3a736639668e4a8b7
Astral Cai <astralcai@gmail.com>
2023-11-22 14:35:05 -0500

    XZX Single Qubit Decomposition (#4862)
    **Context:**
    
    Implementing the XZX single qubit decomposition as requested in
    https://github.com/PennyLaneAI/pennylane/issues/4826
    
    **Description of the Change:**
    
    A new single qubit decomposition is added which decomposes a 1-qubit
    unitary as
    
    $$U=e^{i\gamma}\cdot R_x(\phi)\cdot R_z(\theta)\cdot R_x(\lambda)$$
    
    Additionally, cleaned up ```tests/transforms/test_decompositions.py```:
    
    1. Generalized the _run_assertions method, removed duplicate code.
    2. For tests with jax where ```remember =
    config.read("jax_enable_x64")``` was created to remember the current
    config before updating it to ```True```, but never actually used to
    restore the config, add ```config.update("jax_enable_x64", remember)```
    at the end of the test case.
    3. Renaming ```TestOneQubitUnitaryDecomposition``` to
    ```TestOneQubitRotDecomposition```, and removing test cases for other
    decomposition types from this test class. Since now each different type
    of decomposition has its own test class, the
    ```TestOneQubitRotDecomposition``` should now be dedicated to only the
    rot decomposition.
    4. Added tests for tf, torch, jax for the rot transform.
    
    **Benefits:**
    
    Nice to have.
    
    **Related GitHub Issues:**
    
    https://github.com/PennyLaneAI/pennylane/issues/4826
    
    ---------
    
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>

--- pennylane/transforms/decompositions/single_qubit_unitary.py
+++ pennylane/transforms/decompositions/single_qubit_unitary.py
@@ -82,7 +82,7 @@ def _rot_decomposition(U, wire):
     can instead recover a ``Rot`` gate that implements the same operation, up
     to a global phase:
 
-    >>> decomp = zyz_decomposition(U, 0)
+    >>> decomp = _rot_decomposition(U, 0)
     >>> decomp
     [Rot(-0.24209529417800013, 1.14938178234275, 1.7330581433950871, wires=[0])]
     """
@@ -261,6 +261,76 @@ def _xyx_decomposition(U, wire, return_global_phase=False):
     return operations
 
 
+def _xzx_decomposition(U, wire, return_global_phase=False):
+    r"""Computes the decomposition of a single-qubit matrix :math:`U` in terms
+    of elementary operations, as a product of Z and X rotations in the form
+    :math:`e^{i\gamma} RX(\phi) RZ(\theta) RX(\lambda)`. (batched operation)
+
+    Args:
+        U (tensor): A :math:`2 \times 2` unitary matrix.
+        wire (Union[Wires, Sequence[int] or int]): The wire on which to apply the operation.
+        return_global_phase (bool): Whether to return the global phase ``qml.GlobalPhase(-gamma)``
+            as the last element of the returned list of operations.
+
+    Returns:
+        list[Operation]: Returns a list of gates, an ``RX``, an ``RZ`` and
+        another ``RX`` gate, which when applied in the order of appearance in the list is
+        equivalent to the unitary :math:`U` up to a global phase. If `return_global_phase=True`,
+        the global phase is returned as the last element of the list.
+
+    **Example**
+
+    >>> U = np.array(
+    ...     [[-0.28829348-0.78829734j,  0.30364367+0.45085995j],
+    ...      [ 0.53396245-0.10177564j,  0.76279558-0.35024096j]]
+    ... )
+    >>> decompositions = _xzx_decomposition(U, 0, return_global_phase=True)
+    >>> decompositions
+    [RX(12.416147693665032, wires=[0]),
+     RZ(1.3974974090935608, wires=[0]),
+     RX(11.448040119199066, wires=[0]),
+     GlobalPhase((1.1759220332464762-0j), wires=[0])]
+
+    """
+
+    # Small number to add to denominators to avoid division by zero
+    EPS = 1e-64
+
+    # Choose gamma such that exp(-i*gamma)*U is special unitary (detU==1).
+    U = math.expand_dims(U, axis=0) if len(U.shape) == 2 else U
+    U_det1, gammas = _convert_to_su2(U, return_global_phase=return_global_phase)
+
+    # Compute \phi, \theta and \lambda after analytically solving for them from
+    # U_det1 = RX(\phi) RZ(\theta) RX(\lambda)
+    sum_diagonal_real = math.real(U_det1[:, 0, 0] + U_det1[:, 1, 1])
+    sum_off_diagonal_imag = math.imag(U_det1[:, 0, 1] + U_det1[:, 1, 0])
+    phi_plus_lambdas_d2 = math.arctan2(-sum_off_diagonal_imag, sum_diagonal_real + EPS)
+    diff_diagonal_imag = math.imag(U_det1[:, 0, 0] - U_det1[:, 1, 1])
+    diff_off_diagonal_real = math.real(U_det1[:, 0, 1] - U_det1[:, 1, 0])
+    phi_minus_lambdas_d2 = math.arctan2(diff_off_diagonal_real, -diff_diagonal_imag + EPS)
+    lams = phi_plus_lambdas_d2 - phi_minus_lambdas_d2
+    phis = phi_plus_lambdas_d2 + phi_minus_lambdas_d2
+
+    # Compute \theta
+    thetas = math.where(
+        math.isclose(math.sin(phi_plus_lambdas_d2), math.zeros_like(phi_plus_lambdas_d2)),
+        2 * math.arccos(sum_diagonal_real / (2 * math.cos(phi_plus_lambdas_d2) + EPS)),
+        2 * math.arccos(-sum_off_diagonal_imag / (2 * math.sin(phi_plus_lambdas_d2) + EPS)),
+    )
+
+    phis, thetas, lams, gammas = map(math.squeeze, [phis, thetas, lams, gammas])
+
+    phis = phis % (4 * np.pi)
+    thetas = thetas % (4 * np.pi)
+    lams = lams % (4 * np.pi)
+
+    operations = [qml.RX(lams, wire), qml.RZ(thetas, wire), qml.RX(phis, wire)]
+    if return_global_phase:
+        operations.append(qml.GlobalPhase(-gammas))
+
+    return operations
+
+
 def _zxz_decomposition(U, wire, return_global_phase=False):
     r"""Compute the decomposition of a single-qubit matrix :math:`U` in terms
     of elementary operations, as a product of X and Z rotations in the form
@@ -310,6 +380,7 @@ def _zxz_decomposition(U, wire, return_global_phase=False):
         math.real(U_det1[:, 0, 0]) / (math.cos(phis_plus_psis) + EPS),
         -math.imag(U_det1[:, 0, 0]) / (math.sin(phis_plus_psis) + EPS),
     )
+
     # Arcos is only defined between -1 and 1
     thetas = qml.math.clip(thetas, -1.0, 1.0)
     thetas = 2 * math.arccos(thetas)
@@ -334,7 +405,7 @@ def one_qubit_decomposition(U, wire, rotations="ZYZ", return_global_phase=False)
     Any one qubit unitary operation can be implemented up to a global phase by composing RX, RY,
     and RZ gates.
 
-    Currently supported values for ``rotations`` are "ZYZ", "XYX", and "ZXZ".
+    Currently supported values for ``rotations`` are "ZYZ", "XYX", "XZX", and "ZXZ".
 
     Args:
         U (tensor): A :math:`2 \times 2` unitary matrix.
@@ -363,6 +434,7 @@ def one_qubit_decomposition(U, wire, rotations="ZYZ", return_global_phase=False)
         "rot": _rot_decomposition,
         "ZYZ": _zyz_decomposition,
         "XYX": _xyx_decomposition,
+        "XZX": _xzx_decomposition,
         "ZXZ": _zxz_decomposition,
     }
 
