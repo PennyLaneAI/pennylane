505ff1bd77a7c64b57b264b7e8979fea56bfda7e
Gabe PC <bottrill@student.ubc.ca>
2023-11-23 12:25:08 -0800

    Fixed wires to convert to array

--- pennylane/devices/qutrit_mixed/initialize_state.py
+++ pennylane/devices/qutrit_mixed/initialize_state.py
@@ -32,30 +32,34 @@ def create_initial_state(
     Returns:
         array: The initial state of a circuit
     """
+    if isinstance(wires, Wires):
+        wire_array = wires.toarray()
+    else:
+        wire_array = np.array(wires)
+    num_wires = len(wire_array)
+
     if not prep_operation:
-        num_wires = len(wires)
         rho = _create_basis_state(num_wires, 0)
 
     elif isinstance(prep_operation, QutritBasisState):
-        rho = _apply_basis_state(prep_operation.parameters[0], wires)
+        rho = _apply_basis_state(prep_operation.parameters[0], wire_array)
 
     elif isinstance(prep_operation, StatePrepBase):
-        rho = _apply_state_vector(prep_operation.state_vector(wire_order=list(wires)), wires)
+        rho = _apply_state_vector(prep_operation.state_vector(wire_order=list(wire_array)), num_wires)
 
     # TODO: add instance for prep_operations as added
 
     return qml.math.asarray(rho, like=like)
 
 
-def _apply_state_vector(state, wires):  # function is easy to abstract for qudit
+def _apply_state_vector(state, num_wires):  # function is easy to abstract for qudit
     """Initialize the internal state in a specified pure state.
 
     Args:
         state (array[complex]): normalized input state of length
             ``3**len(wires)``
-        device_wires (Wires): wires that get initialized in the state
+        num_wires (array[int]): wires that get initialized in the state
     """
-    num_wires = len(wires)
 
     # Initialize the entire wires with the state
     rho = qml.math.outer(state, qml.math.conj(state))
@@ -68,17 +72,13 @@ def _apply_basis_state(state, wires):  # function is easy to abstract for qudit
     Args:
         state (array[int]): computational basis state of shape ``(wires,)``
             consisting of 0s and 1s.
-        wires (Wires): wires that the provided computational state should be initialized on
+        wires (array[int]): wires that the provided computational state should be initialized on
 
     """
     num_wires = len(wires)
 
-    if isinstance(wires, Wires):
-        wire_array = wires.toarray()
-    else:
-        wire_array = np.array(wires)
     # get computational basis state number
-    basis_states = qudit_dim ** (num_wires - 1 - wire_array)
+    basis_states = qudit_dim ** (num_wires - 1 - wires)
     print(qml.math.dot(state, basis_states))
     num = int(qml.math.dot(state, basis_states))
 
