a5abcbfe8b320097bf7f63e1d83369c18c8092ec
albi3ro <chrissie.c.l@gmail.com>
2023-11-27 15:04:01 -0500

    merge fixes

--- pennylane/pauli/pauli_arithmetic.py
+++ pennylane/pauli/pauli_arithmetic.py
@@ -316,49 +316,6 @@ class PauliWord(dict):
             current_size *= 2
         return indices
 
-    def _get_csr_data(self, wire_order, coeff):
-        """Computes the sparse matrix data of the Pauli word times a coefficient, given a wire order."""
-        full_word = [self[wire] for wire in wire_order]
-        matrix_size = 2 ** len(wire_order)
-        data = np.empty(matrix_size, dtype=np.complex128)  # Non-zero values
-
-        current_size = 2
-        data[:current_size], _ = _cached_sparse_data(full_word[-1])
-        data[:current_size] *= coeff  # Multiply initial term better than the full matrix
-        for s in full_word[-2::-1]:
-            if s == "I":
-                data[current_size : 2 * current_size] = data[:current_size]
-            elif s == "X":
-                data[current_size : 2 * current_size] = data[:current_size]
-            elif s == "Y":
-                data[current_size : 2 * current_size] = 1j * data[:current_size]
-                data[:current_size] *= -1j
-            elif s == "Z":
-                data[current_size : 2 * current_size] = -data[:current_size]
-            current_size *= 2
-        return data
-
-    def _get_csr_indices(self, wire_order):
-        """Computes the sparse matrix indices of the Pauli word times a coefficient, given a wire order."""
-        full_word = [self[wire] for wire in wire_order]
-        matrix_size = 2 ** len(wire_order)
-        indices = np.empty(matrix_size, dtype=np.int64)  # Column index of non-zero values
-        current_size = 2
-        _, indices[:current_size] = _cached_sparse_data(full_word[-1])
-        for s in full_word[-2::-1]:
-            if s == "I":
-                indices[current_size : 2 * current_size] = indices[:current_size] + current_size
-            elif s == "X":
-                indices[current_size : 2 * current_size] = indices[:current_size]
-                indices[:current_size] += current_size
-            elif s == "Y":
-                indices[current_size : 2 * current_size] = indices[:current_size]
-                indices[:current_size] += current_size
-            elif s == "Z":
-                indices[current_size : 2 * current_size] = indices[:current_size] + current_size
-            current_size *= 2
-        return indices
-
     def operation(self, wire_order=None, get_as_tensor=False):
         """Returns a native PennyLane :class:`~pennylane.operation.Operation` representing the PauliWord."""
         if len(self) == 0:
