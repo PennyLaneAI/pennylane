ca1eab3c1f7f9b7917bb21b8b7c6ffa67ed5fa93
minhtriet <minhtriet09@gmail.com>
2023-11-30 17:47:58 +0000

    fix tests

--- tests/ops/functions/test_equal.py
+++ tests/ops/functions/test_equal.py
@@ -1800,18 +1800,18 @@ class TestParametrizedEvolutionComparisons:
         assert not qml.equal(ev1(params1, t), ev3(params3, t))
 
 
-class TestQuantumScriptEqual:
-    def test_equal_quscript(self):
-        tape1 = qml.tape.QuantumScript(
-            [qml.PauliX(0), qml.RX(1.2, wires=0)], [qml.expval(qml.PauliZ(0))], shots=10
-        )
-        tape2 = qml.tape.QuantumScript([qml.PauliX(0)], [qml.expval(qml.PauliZ(0))], shots=10)
-        assert qml.equal(tape1, tape2) is False
-        assert qml.equal(tape1, tape1)
-        tape3 = qml.tape.QuantumScript([qml.PauliX(0)], [qml.expval(qml.PauliZ(0))], shots=None)
-        assert qml.equal(tape2, tape3) is False
-        tape4 = qml.tape.QuantumScript(
-            [qml.PauliX(0), qml.RX(1.2 + 1e-6, wires=0)], [qml.expval(qml.PauliZ(0))], shots=10
-        )
-        assert qml.equal(tape1, tape4, atol=1e-5)
-        assert qml.equal(tape1, tape4, rtol=0, atol=1e-7) is False
+def test_equal_quscript():
+    """Test that QuantumScript can be compared"""
+    tape1 = qml.tape.QuantumScript(
+        [qml.PauliX(0), qml.RX(1.2, wires=0)], [qml.expval(qml.PauliZ(0))], shots=10
+    )
+    tape2 = qml.tape.QuantumScript([qml.PauliX(0)], [qml.expval(qml.PauliZ(0))], shots=10)
+    assert qml.equal(tape1, tape2) is False
+    assert qml.equal(tape1, tape1)
+    tape3 = qml.tape.QuantumScript([qml.PauliX(0)], [qml.expval(qml.PauliZ(0))], shots=None)
+    assert qml.equal(tape2, tape3) is False
+    tape4 = qml.tape.QuantumScript(
+        [qml.PauliX(0), qml.RX(1.2 + 1e-6, wires=0)], [qml.expval(qml.PauliZ(0))], shots=10
+    )
+    assert qml.equal(tape1, tape4, atol=1e-5)
+    assert qml.equal(tape1, tape4, rtol=0, atol=1e-7) is False
