2ee8d040bd4345c56b37776b94c6c9102dc2318d
lillian542 <38584660+lillian542@users.noreply.github.com>
2023-12-06 14:25:59 -0500

    black formatting

--- pennylane/ops/op_math/exp.py
+++ pennylane/ops/op_math/exp.py
@@ -177,9 +177,7 @@ class Exp(ScalarSymbolicOp, Operation):
     # pylint: disable=too-many-arguments
     def __init__(self, base, coeff=1, num_steps=None, id=None):
         if not isinstance(base, Operator):
-            raise TypeError(
-                f"base is expected to be of type Operator, but received {type(base)}"
-            )
+            raise TypeError(f"base is expected to be of type Operator, but received {type(base)}")
         super().__init__(base, scalar=coeff, id=id)
         self.grad_recipe = [None]
         self.num_steps = num_steps
