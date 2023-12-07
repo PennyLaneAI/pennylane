8c78dba70a4189c40fd807c28eee2ca7b1a78e01
mchau <minhtriet09@gmail.com>
2023-12-07 21:30:14 +0000

    reformat

--- pennylane/ops/functions/equal.py
+++ pennylane/ops/functions/equal.py
@@ -19,8 +19,6 @@ from collections.abc import Iterable
 from functools import singledispatch
 from typing import Union
 
-import numpy as np
-
 import pennylane as qml
 from pennylane.measurements import MeasurementProcess
 from pennylane.measurements.classical_shadow import ShadowExpvalMP
