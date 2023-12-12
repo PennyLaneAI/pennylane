926ddd97250ebf2c69c2e5dd7cf6cbb3a3f4ccfd
albi3ro <chrissie.c.l@gmail.com>
2023-12-12 10:55:58 -0500

    add process counts method to SampleMeasurement

--- doc/releases/changelog-dev.md
+++ doc/releases/changelog-dev.md
@@ -325,6 +325,9 @@
 
 <h4>Other improvements</h4>
 
+* `SampleMeasurement` now as an optional method `process_counts` for computing the measurement results from a counts
+  dictionary.
+
 * A new function called `ops.functions.assert_valid` has been added for checking if an `Operator` class is defined correctly.
   [(#4764)](https://github.com/PennyLaneAI/pennylane/pull/4764)
 
--- tests/measurements/test_measurements.py
+++ tests/measurements/test_measurements.py
@@ -581,6 +581,22 @@ class TestSampleMeasurement:
         ):
             circuit()
 
+    def test_process_samples_not_implemented(self):
+        """Test taht process_samples is not implemented by default."""
+
+        class MyMeasurement(SampleMeasurement):
+            # pylint: disable=signature-differs
+            def process_samples(self, samples, wire_order, shot_range, bin_size):
+                return qml.math.sum(samples[..., self.wires])
+
+            @property
+            def return_type(self):
+                return Sample
+
+        m = MyMeasurement(wires=[0])
+        with pytest.raises(NotImplementedError):
+            m.process_counts({"0": 10}, wire_order=qml.wires.Wires(0))
+
 
 class TestStateMeasurement:
     """Tests for the SampleMeasurement class."""
