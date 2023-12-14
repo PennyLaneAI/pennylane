57719187ed0839caf8968108156e5f6b3446d806
Mudit Pandey <mudit.pandey@xanadu.ai>
2023-12-14 13:37:09 -0500

    Update docstring

--- pennylane/drawer/drawable_layers.py
+++ pennylane/drawer/drawable_layers.py
@@ -61,6 +61,8 @@ def _recursive_find_mcm_stats_layer(
         op_occupied_cwires (set(int)): classical wires occupied by measurement
         occupied_cwires_per_layer (list[set[int]]): which classical wires are already
             in use for collecting statistics. Each set is a different layer.
+        bit_map (dict): A map containing mid-circuit measurements used for classical conditions
+            or collecting statistics as keys
 
     Returns:
         int: layer to place measurement process in
