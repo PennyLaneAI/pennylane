bc33a95c74d54ee9d8cb8e9d4093cfa85489f408
lillian542 <Lillian.frederiksen@xanadu.ai>
2023-12-15 11:13:15 -0500

    update docstring

--- pennylane/templates/subroutines/controlled_sequence.py
+++ pennylane/templates/subroutines/controlled_sequence.py
@@ -152,6 +152,24 @@ class ControlledSequence(SymbolicOp, Operation):
                 op.decomposition()
                 return qml.state()
 
+        >>> print(qml.draw(circuit, wire_order=[0,1,2,3])())
+        0: ─╭●─────────────────────────────────────┤  State
+        1: ─│────────────╭●────────────────────────┤  State
+        2: ─│────────────│────────────╭●───────────┤  State
+        3: ─╰(RX(1.00))──╰(RX(0.50))──╰(RX(0.25))──┤  State
+
+        To display the operators as powers of the base operator without further simplifcation,
+        the `compute_decompostion` method can be used with `lazy=True`.
+
+        .. code-block:: python
+
+            dev = qml.device("default.qubit")
+            op = qml.ControlledSequence(qml.RX(0.25, wires = 3), control = [0, 1, 2])
+
+            @qml.qnode(dev)
+            def circuit():
+                op.compute_decomposition(base=op.base, control_wires=op.control, lazy=True)
+                return qml.state()
 
         >>> print(qml.draw(circuit, wire_order=[0,1,2,3])())
         0: ─╭●─────────────────────────────────────┤  State
