836369598399576c591519361fab5b335071fac0
qottmann <korbinian.kottmann@gmail.com>
2023-12-19 12:16:39 +0100

    update test

--- tests/shadow/test_shadow_entropies.py
+++ tests/shadow/test_shadow_entropies.py
@@ -59,7 +59,7 @@ def test_deprecation_warning_atol():
     bits, recipes = max_entangled_circuit(wires=2)()
     shadow = ClassicalShadow(bits, recipes)
     with pytest.warns(DeprecationWarning):
-        shadow.entropy(atol=1e-3)
+        shadow.entropy([0, 1], atol=1e-3)
 
 
 class TestShadowEntropies:
