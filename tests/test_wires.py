3ffb99d7eaf596a0162d3fda8723760f843c1cf1
Sergei Mironov <sergei.mironov@xanadu.ai>
2024-01-05 09:51:16 +0000

    Test Pytree interface of Wires

--- tests/test_wires.py
+++ tests/test_wires.py
@@ -18,6 +18,7 @@ import pytest
 import numpy as np
 import pennylane as qml
 from pennylane.wires import Wires, WireError
+from jax.tree_util import tree_flatten, tree_unflatten
 
 
 # pylint: disable=too-many-public-methods
@@ -362,3 +363,16 @@ class TestWires:
         assert wires._hash is None
         h = hash(wires)
         assert wires._hash == h
+
+    @pytest.mark.parametrize(
+        "source",
+        [1, -2, "a", "q1", -1.4, np.array([0, 1, 2]), [0, 1, 2], (0, 1, 2), range(3)]
+    )
+    def test_wires_pytree(self, source):
+        """ Test that Wires class supports the PyTree flattening interface """
+        wires = Wires(source)
+        wires_flat, tree = tree_flatten(wires)
+        wires2 = tree_unflatten(tree, wires_flat)
+        assert isinstance(wires2, Wires), f"{wires2} is not Wires"
+        assert wires == wires2, f"{wires} != {wires2}"
+
