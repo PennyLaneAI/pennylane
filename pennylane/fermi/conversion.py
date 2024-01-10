58145d6e0e94339a05819290de334281bc7f1232
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2024-01-10 11:47:02 -0500

    Update pennylane/fermi/conversion.py
    Co-authored-by: Thomas R. Bromley <49409390+trbromley@users.noreply.github.com>

--- pennylane/fermi/conversion.py
+++ pennylane/fermi/conversion.py
@@ -184,7 +184,7 @@ def parity_transform(
         fermi_operator(FermiWord, FermiSentence): the fermionic operator
         n (int): Number of qubits, i.e., spin orbitals in the system
         ps (bool): whether to return the result as a :class:`~.PauliSentence` instead of an
-            Operator. Defaults to False.
+            :class:`~.Operator`. Defaults to ``False``.
         wire_map (dict): a dictionary defining how to map the orbitals of
             the Fermi operator to qubit wires. If ``None``, the integers used to
             order the orbitals will be used as wire labels. Defaults to ``None``.
