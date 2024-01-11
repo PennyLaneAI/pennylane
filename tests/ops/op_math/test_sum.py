d9d32e61c34f12bda7c3c062dd2bc96fd46c0c41
Jay Soni <jbsoni@uwaterloo.ca>
2024-01-11 15:53:33 -0500

    put back import

--- tests/ops/op_math/test_sum.py
+++ tests/ops/op_math/test_sum.py
@@ -23,6 +23,7 @@ import pytest
 import pennylane as qml
 import pennylane.numpy as qnp
 from pennylane import math
+from pennylane.wires import Wires
 from pennylane.operation import AnyWires, MatrixUndefinedError, Operator
 from pennylane.ops.op_math import Prod, Sum
 
