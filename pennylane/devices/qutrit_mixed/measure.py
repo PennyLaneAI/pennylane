9651d0a134698a5dbe528ae6ecaad0c0cd1c4271
gabrielLydian <gabrielbottrill@gmail.com>
2024-01-11 18:17:58 -0800

    Removed repeated code added comments for TODOs

--- pennylane/devices/qutrit_mixed/measure.py
+++ pennylane/devices/qutrit_mixed/measure.py
@@ -53,53 +53,8 @@ def resquare_state(state, num_wires):
     return math.reshape(state, shape)
 
 
-def get_einsum_indices(obs: Observable, state, is_state_batched: bool = False):
-    r"""Finds the indices for einsum TODO when it's merged over
-    TODO remove this and abstract the apply_ops to do this
-
-    Args:
-        obs (Operator): Operator to apply to the quantum state
-        state (array[complex]): Input quantum state
-        is_state_batched (bool): Boolean representing whether the state is batched or not
-
-    Returns:
-        dict: indices used for einsum
-    """
-    num_ch_wires = len(obs.wires)
-    num_wires = int((len(math.shape(state)) - is_state_batched) / 2)
-    rho_dim = 2 * num_wires
-
-    # Tensor indices of the state. For each qutrit, need an index for rows *and* columns
-    state_indices = alphabet[:rho_dim]
-
-    # row indices of the quantum state affected by this operation
-    row_wires_list = obs.wires.tolist()
-    row_indices = "".join(alphabet_array[row_wires_list].tolist())
-
-    # column indices are shifted by the number of wires
-    col_wires_list = [w + num_wires for w in row_wires_list]
-    col_indices = "".join(alphabet_array[col_wires_list].tolist())
-
-    # indices in einsum must be replaced with new ones
-    new_row_indices = alphabet[rho_dim : rho_dim + num_ch_wires]
-    new_col_indices = alphabet[rho_dim + num_ch_wires : rho_dim + 2 * num_ch_wires]
-
-    # index for summation over Kraus operators
-    kraus_index = alphabet[rho_dim + 2 * num_ch_wires : rho_dim + 2 * num_ch_wires + 1]
-
-    # new state indices replace row and column indices with new ones
-    new_state_indices = functools.reduce(
-        lambda old_string, idx_pair: old_string.replace(idx_pair[0], idx_pair[1]),
-        zip(col_indices + row_indices, new_col_indices + new_row_indices),
-        state_indices,
-    )
-    operation_1_indices = f"{kraus_index}{new_row_indices}{row_indices}"
-    return {"op1": operation_1_indices, "state": state_indices, "new_state": new_state_indices}
-
-
 def apply_observable_einsum(obs: Observable, state, is_state_batched: bool = False):
     r"""Applies an observable to a density matrix rho, giving obs@state
-    TODO remove this and abstract the apply_ops to do this
 
     Args:
         obs (Operator): Operator to apply to the quantum state
@@ -130,36 +85,8 @@ def trace_method(
     """
     obs = measurementprocess.obs
     rho_mult_obs = apply_observable_einsum(obs, state, is_state_batched)
-
+    #TODO, probably need to reshape?
     return math.real(math.trace(rho_mult_obs))
-
-
-def sum_of_terms_method(  # TODO this is copied code, should this borrow from qubit?
-    measurementprocess: ExpectationMP, state: TensorLike, is_state_batched: bool = False
-) -> TensorLike:
-    """Measure the expecation value of the state when the measured observable is a ``Hamiltonian`` or ``Sum``
-    and it must be backpropagation compatible.
-
-    Args:
-        measurementprocess (ExpectationMP): measurement process to apply to the state
-        state (TensorLike): the state to measure
-        is_state_batched (bool): whether the state is batched or not
-
-    Returns:
-        TensorLike: the result of the measurement
-    """
-    if isinstance(measurementprocess.obs, Sum):
-        # Recursively call measure on each term, so that the best measurement method can
-        # be used for each term
-        return sum(
-            measure(ExpectationMP(term), state, is_state_batched=is_state_batched)
-            for term in measurementprocess.obs
-        )
-    # else hamiltonian
-    return sum(
-        c * measure(ExpectationMP(t), state, is_state_batched=is_state_batched)
-        for c, t in zip(*measurementprocess.obs.terms())
-    )
 
 
 def state_diagonalizing_gates(
@@ -175,11 +102,12 @@ def state_diagonalizing_gates(
     Returns:
         TensorLike: the result of the measurement
     """
-    # for op in measurementprocess.diagonalizing_gates(): TODO re-add
-    #     state = apply_operation(op, state, is_state_batched=is_state_batched)
+    for op in measurementprocess.diagonalizing_gates():
+        state = apply_operation(op, state, is_state_batched=is_state_batched)
 
     total_indices = len(state.shape) - is_state_batched
     wires = Wires(range(total_indices))
+    #resquared state or probs?
     resquared_state = resquare_state(state, total_indices)
     return measurementprocess.process_state(resquared_state, wires)
 
@@ -227,4 +155,32 @@ def measure(
     """
     return get_measurement_function(measurementprocess, state)(
         measurementprocess, state, is_state_batched
+    )
+
+def sum_of_terms_method(  # TODO this is copied code, should this borrow from qubit?
+    measurementprocess: ExpectationMP, state: TensorLike, is_state_batched: bool = False, measure_func=measure
+) -> TensorLike:
+    """Measure the expecation value of the state when the measured observable is a ``Hamiltonian`` or ``Sum``
+    and it must be backpropagation compatible.
+
+    Args:
+        measurementprocess (ExpectationMP): measurement process to apply to the state
+        state (TensorLike): the state to measure
+        is_state_batched (bool): whether the state is batched or not
+        measure_func (function): measure function to use
+
+    Returns:
+        TensorLike: the result of the measurement
+    """
+    if isinstance(measurementprocess.obs, Sum):
+        # Recursively call measure on each term, so that the best measurement method can
+        # be used for each term
+        return sum(
+            measure_func(ExpectationMP(term), state, is_state_batched=is_state_batched)
+            for term in measurementprocess.obs
+        )
+    # else hamiltonian
+    return sum(
+        c * measure_func(ExpectationMP(t), state, is_state_batched=is_state_batched)
+        for c, t in zip(*measurementprocess.obs.terms())
     )
