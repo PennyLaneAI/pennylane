ab188407ddd2081cf676d50cbc3690dec260e381
Astral Cai <astral.cai@xanadu.ai>
2024-01-19 11:02:29 -0500

    Minor doc update

--- tests/ops/op_math/test_controlled_ops.py
+++ tests/ops/op_math/test_controlled_ops.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Unit tests for parametric Operators inheriting from ControlledOp.
+Unit tests for Operators inheriting from ControlledOp.
 """
 
 import functools
