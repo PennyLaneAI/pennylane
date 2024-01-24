6062d273817c437320286c361836d237ea9fe681
obliviateandsurrender <utkarshazad98@gmail.com>
2024-01-24 17:27:32 -0500

    modify from suggestion

--- pennylane/ops/functions/eigvals.py
+++ pennylane/ops/functions/eigvals.py
@@ -39,11 +39,10 @@ def eigvals(op: qml.operation.Operator, k=1, which="SA") -> TensorLike:
           :math:`k` is equal or larger than :math:`N - 1`, the regular ``qml.math.linalg.eigvalsh``
           is applied on the dense matrix. For more details see the ``scipy.sparse.linalg.eigsh``
           `documentation <https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.eigsh.html#scipy.sparse.linalg.eigsh>`_.
-        - For a second-quantized :mod:`molecular Hamiltonian <pennylane.qchem.molecular_hamiltonian>`,
-          the eigenspectrum with :math:`k \geq 1` will contain energies of the charged species of the
-          molecule in addition to the neutral one, as this Hamiltonian is independent of the number
-          of electrons. Therefore, the `smallest` eigenvalue returned by ``qml.eigvals`` might not
-          always have the correct particle number and correspond to the correct ground state energy.
+        - A second-quantized :mod:`molecular Hamiltonian <pennylane.qchem.molecular_hamiltonian>` is
+          independent of the number of electrons and its eigenspectrum contains the energies of the
+          neutral and charged molecules. Therefore, the `smallest` eigenvalue returned by ``qml.eigvals``
+          for a molecular Hamiltonian might not always correspond to the neutral molecule.
 
     Args:
         op (Operator or QNode or QuantumTape or Callable): A quantum operator or quantum circuit.
