0f311db8778a5b119b6ca32fe93b2ecbb427e6eb
qottmann <korbinian.kottmann@gmail.com>
2024-01-25 16:32:58 +0100

    revert merge error

--- tests/ops/functions/test_commutator.py
+++ tests/ops/functions/test_commutator.py
@@ -76,10 +76,10 @@ class TestLegacySupport:
 
 def test_alias():
     """Test that the alias qml.comm() works as expected"""
-    res1 = qml.commutator(X0, Y0)
+    res1 = qml.comm(X0, Y0)
     res1_true = qml.commutator(X0, Y0)
     assert res1 == res1_true
-    res2 = qml.commutator(X0, Y0, pauli=True)
+    res2 = qml.comm(X0, Y0, pauli=True)
     res2_true = qml.commutator(X0, Y0, pauli=True)
     assert res2 == res2_true
 
