8ac66bfb65072b5453c107746057cb214e722301
Romain Moyard <rmoyard@gmail.com>
2024-01-29 13:01:11 -0500

    Add version to name

--- pennylane/compiler/compiler.py
+++ pennylane/compiler/compiler.py
@@ -61,18 +61,17 @@ def _check_compiler_version(name):
     """Check if the installed version of the given compiler is greater than
     or equal to the required minimum version.
     """
-    # if AvailableCompilers.compiler_checked[name]:
-    #     return  # Used the cached value!
+    if AvailableCompilers.compiler_checked[name]:
+        return  # Used the cached value!
 
     if name == "catalyst":
         installed_catalyst_version = metadata.version("pennylane-catalyst")
-        print(installed_catalyst_version)
+        name = name + installed_catalyst_version
         if Version(re.sub(r"\.dev\d+", "", installed_catalyst_version)) < PL_CATALYST_MIN_VERSION:
             raise CompileError(
                 f"PennyLane-Catalyst {PL_CATALYST_MIN_VERSION} or greater is required, but installed {installed_catalyst_version}"
             )
 
-    # else
     AvailableCompilers.compiler_checked[name] = True
 
 
