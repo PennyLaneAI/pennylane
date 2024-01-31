a30f3dfc1ec2b78fde2f84909699c0e13a41fb46
qottmann <korbinian.kottmann@gmail.com>
2024-01-31 11:31:23 +0100

    revert change

--- pennylane/measurements/expval.py
+++ pennylane/measurements/expval.py
@@ -62,7 +62,7 @@ def expval(op: Union[Operator, MeasurementValue]):
             "qml.expval does not support measuring sequences of measurements or observables"
         )
 
-    if op == qml.Identity() and len(op.wires) == 0:
+    if isinstance(op, qml.Identity) and len(op.wires) == 0:
         # temporary solution to merge https://github.com/PennyLaneAI/pennylane/pull/5106
         raise NotImplementedError(
             "Expectation values of qml.Identity() without wires are currently not allowed."
