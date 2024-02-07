7b0777aba56bcb8193c08f18aa02f501863fad16
Mudit Pandey <mudit.pandey@xanadu.ai>
2024-02-07 14:55:11 +0530

    Added test for coverage

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -2017,6 +2017,19 @@ class TestTensor:
         with pytest.raises(TypeError, match="unsupported operand type"):
             _ = op @ 1.0
 
+    def test_matmul_not_implemented(self, mocker):
+        """Test that matrix multiplication raises TypeError if unsupported
+        object is used."""
+
+        op = Tensor(qml.PauliX(0), qml.PauliZ(1))
+
+        spy = mocker.spy(op, "__matmul__")
+
+        with pytest.raises(TypeError, match="unsupported operand type"):
+            _ = op @ 1.0
+
+        spy.assert_called_once()
+
 
 equal_obs = [
     (qml.PauliZ(0), qml.PauliZ(0), True),
