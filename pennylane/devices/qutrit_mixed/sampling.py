70bb67b20f5c10e2c8cc360a937ccbcc10b504ed
gabrielLydian <gabrielbottrill@gmail.com>
2024-02-08 16:15:24 -0800

    Fixed pylint error

--- pennylane/devices/qutrit_mixed/sampling.py
+++ pennylane/devices/qutrit_mixed/sampling.py
@@ -118,9 +118,11 @@ def _measure_with_samples_diagonalizing_gates(
                 res = mp.process_samples(samples, wires)
             elif isinstance(mp, CountsMP):
                 with qml.queuing.QueuingManager.stop_recording():
+                    # pylint:disable = protected-access
                     processed_samples = qml.sample(op=mp.obs, wires=mp._wires).process_samples(
                         samples, wires
-                    )  # pylint:disable = protected-access
+                    )
+                    # pylint:enable = protected-access
 
                 mp_has_obs = bool(mp.obs)
                 observables, counts = np.unique(
