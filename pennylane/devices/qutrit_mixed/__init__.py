748d281dcd1c87909bc7d039e984162dc9bbe6c1
gabrielLydian <gabrielbottrill@gmail.com>
2024-02-08 16:48:42 -0800

    Added to init and changelog

--- pennylane/devices/qutrit_mixed/__init__.py
+++ pennylane/devices/qutrit_mixed/__init__.py
@@ -25,8 +25,11 @@ at your own discretion.
     create_initial_state
     apply_operation
     measure
+    measure_with_samples
+    sample_state
 """
 
 from .apply_operation import apply_operation
 from .initialize_state import create_initial_state
 from .measure import measure
+from .sampling import sample_state, measure_with_samples
