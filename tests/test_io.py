c07950e8fdae1a103085205adceed8b518c8b3c6
lillian542 <Lillian.frederiksen@xanadu.ai>
2024-02-15 15:28:38 -0500

    improve test coverage

--- tests/test_io.py
+++ tests/test_io.py
@@ -73,7 +73,7 @@ class TestLoad:
 
     def test_qiskit_not_installed(self, monkeypatch):
         """Test that a specific error is raised if qml.from_qiskit is called and the qiskit
-        plugin converter isn't found"""
+        plugin converter isn't found, instead of the generic 'ValueError: Converter does not exist.'"""
 
         # temporarily make a mock_converter_dict with no "qiskit"
         mock_plugin_converter_dict = {
@@ -82,10 +82,23 @@ class TestLoad:
         del mock_plugin_converter_dict["qiskit"]
         monkeypatch.setattr(qml.io, "plugin_converters", mock_plugin_converter_dict)
 
+        # calling from_qiskit raises the specific RuntimeError rather than the generic ValueError
         with pytest.raises(
             RuntimeError,
             match="Conversion from Qiskit requires the PennyLane-Qiskit plugin. "
             "You can install the plugin by",
+        ):
+            qml.from_qiskit("Test")
+
+        # if load raises some other ValueError instead of the "converter does not exist" error, it is unaffected
+        def mock_load_with_error(*args, **kwargs):
+            raise ValueError("Some other error raised than instead of converter does not exist")
+
+        monkeypatch.setattr(qml.io, "load", mock_load_with_error)
+
+        with pytest.raises(
+            ValueError,
+            match="Some other error raised than instead of converter does not exist",
         ):
             qml.from_qiskit("Test")
 
