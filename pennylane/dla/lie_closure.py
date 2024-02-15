46d1c3bfdd4f09acc93f0db6646a03ddd629fedd
qottmann <korbinian.kottmann@gmail.com>
2024-02-15 13:44:30 +0100

    module description

--- pennylane/dla/lie_closure.py
+++ pennylane/dla/lie_closure.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Functions to convert a fermionic operator to the qubit basis."""
+"""A function to compute the Lie closure of a set of operators"""
 
 from functools import reduce
 
