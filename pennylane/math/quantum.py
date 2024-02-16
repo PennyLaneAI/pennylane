6ac76df04852c757e824472fe276c73f4d9730b1
babcockt18 <39506616+babcockt18@users.noreply.github.com>
2024-02-16 18:03:03 -0500

    docs(quantum): update code block language in docstrings from python3 to python (#32)

--- pennylane/math/quantum.py
+++ pennylane/math/quantum.py
@@ -263,12 +263,12 @@ def partial_trace(matrix, indices, c_dtype="complex128"):
     **Example**
 
     We can compute the partial trace of the matrix ``x`` with respect to its 0th index.
-    .. code-block:: python3
+    .. code-block:: python
         >>> x = np.array([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
         >>> partial_trace(x, indices=[0])
         array([[1, 0], [0, 0]])
     We can also pass a batch of matrices ``x`` to the function and return the partial trace of each matrix with respect to each matrix's 0th index.
-    .. code-block:: python3
+    .. code-block:: python
         >>> x = np.array([[[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
                         [[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]])
         >>> partial_trace(x, indices=[0])
@@ -278,10 +278,10 @@ def partial_trace(matrix, indices, c_dtype="complex128"):
             [[0, 0],
                 [0, 1]]])
     The partial trace can also be computed with respect to multiple indices within different frameworks such as TensorFlow and PyTorch.
-    .. code-block:: python3
+    .. code-block:: python
         >>> x = tf.Variable([[[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
                             [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]], dtype=tf.complex128)
-    .. code-block:: python3
+    .. code-block:: python
         >>> partial_trace(x, indices=[1])
         <tf.Tensor: shape=(2, 2, 2), dtype=complex128, numpy=
         array([[[1.+0.j, 0.+0.j],
