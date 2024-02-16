53f46159fb9ce722eb722d13bba8551e393978f3
babcockt18 <39506616+babcockt18@users.noreply.github.com>
2024-02-15 21:39:47 -0500

    feat(math): enhance partial_trace docstrings with examples and framework compatibility (#31)

--- pennylane/math/quantum.py
+++ pennylane/math/quantum.py
@@ -262,29 +262,33 @@ def partial_trace(matrix, indices, c_dtype="complex128"):
 
     **Example**
 
-    We can compute the partial trace of the matrix ``x`` with respect to its first index.
-    >>> x = np.array([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
-    >>> partial_trace(x, indices=[0])
-    array([[1, 0], [0, 0]])
+    We can compute the partial trace of the matrix ``x`` with respect to its 0th index.
+    .. code-block:: python3
+        >>> x = np.array([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+        >>> partial_trace(x, indices=[0])
+        array([[1, 0], [0, 0]])
+    We can also pass a batch of matrices ``x`` to the function and return the partial trace of each matrix with respect to each matrix's 0th index.
+    .. code-block:: python3
+        >>> x = np.array([[[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+                        [[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]])
+        >>> partial_trace(x, indices=[0])
+        array([[[1, 0],
+                [0, 0]],
 
-    >>> x = np.array([[[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
-                     [[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]])
-    >>> partial_trace(x, indices=[0])
-    array([[[1, 0],
-            [0, 0]],
+            [[0, 0],
+                [0, 1]]])
+    The partial trace can also be computed with respect to multiple indices within different frameworks such as TensorFlow and PyTorch.
+    .. code-block:: python3
+        >>> x = tf.Variable([[[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+                            [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]], dtype=tf.complex128)
+    .. code-block:: python3
+        >>> partial_trace(x, indices=[1])
+        <tf.Tensor: shape=(2, 2, 2), dtype=complex128, numpy=
+        array([[[1.+0.j, 0.+0.j],
+                [0.+0.j, 0.+0.j]],
 
-           [[0, 0],
-            [0, 1]]])
-
-    >>> x = tf.Variable([[[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
-                        [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]], dtype=tf.complex128)
-    >>> partial_trace(x, indices=[1])
-    <tf.Tensor: shape=(2, 2, 2), dtype=complex128, numpy=
-    array([[[1.+0.j, 0.+0.j],
-            [0.+0.j, 0.+0.j]],
-
-           [[1.+0.j, 0.+0.j],
-            [0.+0.j, 0.+0.j]]])>
+            [[1.+0.j, 0.+0.j],
+                [0.+0.j, 0.+0.j]]])>
     """
     # Autograd does not support same indices sum in backprop, and tensorflow
     # has a limit of 8 dimensions if same indices are used
