363a9c6dbf3c574eb6ab8db06feb05f1964506d0
Matthew Silverman <matthews@xanadu.ai>
2024-02-16 18:44:52 -0500

    add the touch change

--- .github/workflows/scripts/docs_freeze_push.sh
+++ .github/workflows/scripts/docs_freeze_push.sh
@@ -1,12 +1,9 @@
 DEPS_BRANCH="timmy-deps-test"
-git fetch origin master
 git checkout master
 git restore .
-git pull
 touch .github/stable/doc.txt
+git add .github/stable/doc.txt
 pip freeze | grep -v "file:///" > .github/stable/doc.txt
-ls
-git status
 git diff --quiet
 if [ $? -eq 0 ]; then
     echo no changes, exiting
@@ -22,4 +19,3 @@ fi
 git add .github/stable/
 git commit -m "Update changed dependencies"
 git push -f --set-upstream origin ${DEPS_BRANCH}
-open \"$(git ls-remote --get-url $(git config --get branch.$(git branch --show-current).remote) | sed 's|git@github.com:\\(.*\\)$|https://github.com/\\1|' | sed 's|\\.git$||')/compare/$(git config --get branch.$(git branch --show-current).merge | cut -d / -f 3-)?expand=1\"
