f22bca9fcf1bb60ffdf3feeee21685cf4e8e1384
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2024-02-20 16:40:38 -0500

    black

--- tests/templates/test_subroutines/test_amplitude_amplification.py
+++ tests/templates/test_subroutines/test_amplitude_amplification.py
@@ -182,15 +182,25 @@ class TestIntegration:
         qml.Hadamard(wires=1)
         qml.Hadamard(wires=2)
 
-        qml.AmplitudeAmplification(generator(range(3)), oracle([0], range(3)), fixed_point=True, iters=3, work_wire=3)
+        qml.AmplitudeAmplification(
+            generator(range(3)), oracle([0], range(3)), fixed_point=True, iters=3, work_wire=3
+        )
         return qml.probs(wires=range(3))
 
     # not calculated analytically, we are only ensuring that the results are consistent accross interfaces
 
     exp_result = np.array(
-        [0.67103818, 0.04699455, 0.04699455, 0.04699455, 0.04699455, 0.04699455, 0.04699455, 0.04699455]
+        [
+            0.67103818,
+            0.04699455,
+            0.04699455,
+            0.04699455,
+            0.04699455,
+            0.04699455,
+            0.04699455,
+            0.04699455,
+        ]
     )
-
 
     def test_qnode_numpy(self):
         """Test that the QNode executes with Numpy."""
@@ -210,6 +220,7 @@ class TestIntegration:
         assert res.shape == (8,)
         assert np.allclose(res, self.exp_result, atol=0.002)
 
+
 def test_correct_queueing():
     """Test that the AmplitudeAmplification operator is correctly queued in the circuit"""
     dev = qml.device("default.qubit", wires=2)
@@ -225,21 +236,20 @@ def test_correct_queueing():
 
     @qml.qnode(dev)
     def circuit2():
-        generator(wires=[0,1,2])
+        generator(wires=[0, 1, 2])
 
         qml.AmplitudeAmplification(generator(range(3)), oracle([0], range(3)))
         return qml.state()
 
-    U = generator(wires=[0,1,2])
-    O = oracle([0], wires=[0,1,2])
+    U = generator(wires=[0, 1, 2])
+    O = oracle([0], wires=[0, 1, 2])
 
     @qml.qnode(dev)
     def circuit3():
-        generator(wires=[0,1,2])
+        generator(wires=[0, 1, 2])
 
         qml.AmplitudeAmplification(U=U, O=O)
         return qml.state()
 
     assert np.allclose(circuit1(), circuit2())
     assert np.allclose(circuit1(), circuit3())
-
