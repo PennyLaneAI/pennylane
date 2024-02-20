7fe4b3ce02c06fbb38117d89e85c0868753f7958
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2024-02-20 16:32:33 -0500

    Update test_amplitude_amplification.py

--- tests/templates/test_subroutines/test_amplitude_amplification.py
+++ tests/templates/test_subroutines/test_amplitude_amplification.py
@@ -97,7 +97,7 @@ class TestInitialization:
             ([0, 1], False, 0, False),
         ),
     )
-    def test_error_wrong_wire(self, wires, fixed_point, work_wire, raise_error):
+    def test_error_wrong_work_wire(self, wires, fixed_point, work_wire, raise_error):
         """Test an error is raised if work_wire is part of the U wires."""
 
         U = generator(wires=wires)
@@ -147,7 +147,7 @@ class TestInitialization:
         (5, [0, 1, 2, 3, 4], 4),
     ),
 )
-def test_compare_grover(self, n_wires, items, iters):
+def test_compare_grover(n_wires, items, iters):
     U = generator(wires=range(n_wires))
     O = oracle(items, wires=range(n_wires))
 
@@ -170,3 +170,76 @@ def test_compare_grover(self, n_wires, items, iters):
         return qml.probs(wires=range(n_wires))
 
     assert np.allclose(circuit_amplitude_amplification(), circuit_grover(), atol=1e-5)
+
+
+class TestIntegration:
+    """Tests that the AmplitudeAmplification is executable in a QNode context"""
+
+    @staticmethod
+    def circuit():
+        """Test circuit"""
+        qml.Hadamard(wires=0)
+        qml.Hadamard(wires=1)
+        qml.Hadamard(wires=2)
+
+        qml.AmplitudeAmplification(generator(range(3)), oracle([0], range(3)), fixed_point=True, iters=3, work_wire=3)
+        return qml.probs(wires=range(3))
+
+    # not calculated analytically, we are only ensuring that the results are consistent accross interfaces
+
+    exp_result = np.array(
+        [0.67103818, 0.04699455, 0.04699455, 0.04699455, 0.04699455, 0.04699455, 0.04699455, 0.04699455]
+    )
+
+
+    def test_qnode_numpy(self):
+        """Test that the QNode executes with Numpy."""
+        dev = qml.device("default.qubit")
+        qnode = qml.QNode(self.circuit, dev, interface=None)
+
+        res = qnode()
+        assert res.shape == (8,)
+        assert np.allclose(res, self.exp_result, atol=0.002)
+
+    def test_lightning_qubit(self):
+        """Test that the QNode executes with the Lightning Qubit simulator."""
+        dev = qml.device("lightning.qubit", wires=3)
+        qnode = qml.QNode(self.circuit, dev)
+
+        res = qnode()
+        assert res.shape == (8,)
+        assert np.allclose(res, self.exp_result, atol=0.002)
+
+def test_correct_queueing():
+    """Test that the AmplitudeAmplification operator is correctly queued in the circuit"""
+    dev = qml.device("default.qubit", wires=2)
+
+    @qml.qnode(dev)
+    def circuit1():
+        qml.Hadamard(wires=0)
+        qml.Hadamard(wires=1)
+        qml.Hadamard(wires=2)
+
+        qml.AmplitudeAmplification(generator(range(3)), oracle([0], range(3)))
+        return qml.state()
+
+    @qml.qnode(dev)
+    def circuit2():
+        generator(wires=[0,1,2])
+
+        qml.AmplitudeAmplification(generator(range(3)), oracle([0], range(3)))
+        return qml.state()
+
+    U = generator(wires=[0,1,2])
+    O = oracle([0], wires=[0,1,2])
+
+    @qml.qnode(dev)
+    def circuit3():
+        generator(wires=[0,1,2])
+
+        qml.AmplitudeAmplification(U=U, O=O)
+        return qml.state()
+
+    assert np.allclose(circuit1(), circuit2())
+    assert np.allclose(circuit1(), circuit3())
+
