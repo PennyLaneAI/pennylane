e3f9ac47342630e1f175d0c2a0db1e8072e9a36d
qottmann <korbinian.kottmann@gmail.com>
2024-02-21 11:41:56 +0100

    black formatting

--- pennylane/pauli/pauli_arithmetic.py
+++ pennylane/pauli/pauli_arithmetic.py
@@ -195,7 +195,7 @@ class PauliWord(dict):
     @property
     def pauli_rep(self):
         """Trivial pauli_rep"""
-        return PauliSentence({self: 1.})
+        return PauliSentence({self: 1.0})
 
     def __reduce__(self):
         """Defines how to pickle and unpickle a PauliWord. Otherwise, un-pickling
