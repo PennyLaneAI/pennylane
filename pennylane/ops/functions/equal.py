35d489f890eac3c9ed9b049c4236c6995e1c71a5
Mudit Pandey <mudit.pandey@xanadu.ai>
2024-03-05 14:23:26 -0500

    Small dev comment to equal

--- pennylane/ops/functions/equal.py
+++ pennylane/ops/functions/equal.py
@@ -235,6 +235,8 @@ def _equal_operators(
         return False
 
     if isinstance(op1, qml.Identity):
+        # We already know op1 and op2 are of the same type, so no need to check
+        # that op2 is also an Identity
         return True
 
     if op1.arithmetic_depth != op2.arithmetic_depth:
