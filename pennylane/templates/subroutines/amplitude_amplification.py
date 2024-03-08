404f870688f5f11cfb6571a309aea862c35ca64a
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2024-03-08 14:36:02 -0500

    Update pennylane/templates/subroutines/amplitude_amplification.py
    Co-authored-by: Thomas R. Bromley <49409390+trbromley@users.noreply.github.com>

--- pennylane/templates/subroutines/amplitude_amplification.py
+++ pennylane/templates/subroutines/amplitude_amplification.py
@@ -57,7 +57,7 @@ class AmplitudeAmplification(Operation):
         iters (int): the number of iterations of the amplitude amplification subroutine, default is ``1``
         fixed_point (bool): whether to use the fixed-point amplitude amplification algorithm, default is ``False``
         work_wire (int): the auxiliary wire to use for the fixed-point amplitude amplification algorithm, default is ``None``
-        reflection_wires (Wires): the wires to reflect on, default is the wires of U
+        reflection_wires (Wires): the wires to reflect on, default is the wires of ``U``
         p_min (int): the lower bound for the probability of success in fixed-point amplitude amplification, default is ``0.9``
 
     Raises:
