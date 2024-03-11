5311bcbbbd13ec430fcb6936b376dde5fbc6ded0
qottmann <korbinian.kottmann@gmail.com>
2024-03-11 12:29:39 +0100

    fix error disabling new opmath when there is no Hamiltonian imported yet

--- pennylane/operation.py
+++ pennylane/operation.py
@@ -3008,9 +3008,12 @@ def disable_new_opmath():
         return
 
     with contextlib.ExitStack() as stack:
-        _mock = mock.patch("__main__.Hamiltonian", qml.Hamiltonian)
-        stack.enter_context(_mock)
-        _mock_lc_stack.append(stack.pop_all())
+        try:
+            _mock = mock.patch("__main__.Hamiltonian", qml.Hamiltonian)
+            stack.enter_context(_mock)
+            _mock_lc_stack.append(stack.pop_all())
+        except:
+            return
 
 
 def active_new_opmath():
