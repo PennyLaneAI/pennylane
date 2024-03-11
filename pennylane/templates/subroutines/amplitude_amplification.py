f4446710fa0d02fa235a41f036314f12931caf56
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2024-03-11 17:24:14 -0400

    Update pennylane/templates/subroutines/amplitude_amplification.py
    Co-authored-by: soranjh <40344468+soranjh@users.noreply.github.com>

--- pennylane/templates/subroutines/amplitude_amplification.py
+++ pennylane/templates/subroutines/amplitude_amplification.py
@@ -46,7 +46,7 @@ class AmplitudeAmplification(Operation):
 
     .. math::
 
-            \text{A}(U, O)|\Psi\rangle \sim |\phi\rangle
+            \text{A}(U, O)|\Psi\rangle \sim |\phi\rangle.
 
     The implementation of the algorithm is based on `[arXiv:quant-ph/0005055] <https://arxiv.org/abs/quant-ph/0005055>`__.
     The template also unlocks advanced techniques such as fixed-point quantum search [`arXiv:1409.3305 <https://arxiv.org/abs/1409.3305>`__] and oblivious amplitude amplification [`arXiv:1312.1414 <https://arxiv.org/abs/1312.1414>`__] by reflecting on a subset of the wires.
