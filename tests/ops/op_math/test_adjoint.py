febc83ce8a3d9cc62d5ddca8bdda0f4f02062c6c
PietropaoloFrisoni <pietropaolo.frisoni@xanadu.ai>
2024-03-21 09:55:47 -0400

    Removing additional test

--- tests/ops/op_math/test_adjoint.py
+++ tests/ops/op_math/test_adjoint.py
@@ -489,20 +489,13 @@ class TestAdjointOperation:
 
         assert op.has_generator is False
 
-    @pytest.mark.usefixtures("use_legacy_opmath")
-    def test_generator_legacy(self):
-        """Assert that the generator of an Adjoint is -1.0 times the base generator with opmath disabled."""
-        base = qml.RX(1.23, wires=0)
-        op = Adjoint(base)
-
-        assert qml.equal(base.generator(), -1.0 * op.generator())
-
+    @pytest.mark.usefixtures("use_legacy_and_new_opmath")
     def test_generator(self):
         """Assert that the generator of an Adjoint is -1.0 times the base generator."""
         base = qml.RX(1.23, wires=0)
         op = Adjoint(base)
 
-        assert qml.equal(base.generator(), qml.Hamiltonian([-1.0], [op.generator()]))
+        assert qml.equal(base.generator(), -1.0 * op.generator())
 
     def test_no_generator(self):
         """Test that an adjointed non-Operation raises a GeneratorUndefinedError."""
