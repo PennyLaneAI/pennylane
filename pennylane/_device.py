49da7f296c9172c97b8568783ad2e3a6c0a1896f
Astral Cai <astral.cai@xanadu.ai>
2024-03-21 12:56:49 -0400

    Fix error in _device

--- pennylane/_device.py
+++ pennylane/_device.py
@@ -677,7 +677,9 @@ class Device(abc.ABC):
             len(circuit.measurements) > 1 and circuit.samples_computational_basis
         )
         obs_on_same_wire = len(circuit._obs_sharing_wires) > 0 or comp_basis_sampled_multi_measure
-        obs_on_same_wire &= not any(isinstance(o, Hamiltonian) for o in circuit._obs_sharing_wires)
+        obs_on_same_wire &= not any(
+            isinstance(o, (Hamiltonian, LinearCombination)) for o in circuit._obs_sharing_wires
+        )
         ops_not_supported = not all(self.stopping_condition(op) for op in circuit.operations)
 
         if obs_on_same_wire:
