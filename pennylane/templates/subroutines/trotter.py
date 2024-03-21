fef4fcd8d62349eb18577cd3356ff1852b181d1e
Astral Cai <astral.cai@xanadu.ai>
2024-03-21 09:08:44 -0400

    Fix isinstance check in trotter

--- pennylane/templates/subroutines/trotter.py
+++ pennylane/templates/subroutines/trotter.py
@@ -171,7 +171,7 @@ class TrotterProduct(Operation):
                 f"The order of a TrotterProduct must be 1 or a positive even integer, got {order}."
             )
 
-        if isinstance(hamiltonian, qml.ops.Hamiltonian):
+        if isinstance(hamiltonian, (qml.ops.Hamiltonian, qml.ops.LinearCombination)):
             coeffs, ops = hamiltonian.terms()
             if len(coeffs) < 2:
                 raise ValueError(
