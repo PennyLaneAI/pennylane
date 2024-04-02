fcec3dbc16ee9d986d85a4de833e1688dd3b9fcf
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2024-04-02 02:21:09 -0400

    Update pennylane/fermi/conversion.py
    Co-authored-by: soranjh <40344468+soranjh@users.noreply.github.com>

--- pennylane/fermi/conversion.py
+++ pennylane/fermi/conversion.py
@@ -317,7 +317,7 @@ def bravyi_kitaev(
         compatible with the Bravyi-Kitaev basis.
 
     In Bravyi-Kitaev mapping, both occupation number and parity of the orbitals are stored non-locally.
-    In comparison, :func:`~.jordan_wigner` stores the occupation number locally while saving the parity non-locally and vice-versa for :func:`~.parity_transform`.
+    In comparison, :func:`~.jordan_wigner` stores the occupation number locally while storing the parity non-locally and vice-versa for :func:`~.parity_transform`.
 
     In Bravyi-Kitaev mapping, the fermionic creation and annihilation operators for even labelled orbitals are mapped to the Pauli operators as
 
