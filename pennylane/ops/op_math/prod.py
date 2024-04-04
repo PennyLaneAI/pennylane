88f746e676edeb824b21c6e416eb02f8b214619a
qottmann <korbinian.kottmann@gmail.com>
2024-04-04 16:57:49 +0200

    remove changes to matrix

--- pennylane/ops/op_math/prod.py
+++ pennylane/ops/op_math/prod.py
@@ -295,8 +295,8 @@ class Prod(CompositeOp):
     def matrix(self, wire_order=None):
         """Representation of the operator as a matrix in the computational basis."""
 
-        if self.pauli_rep:  # Get the matrix from the PauliSentence representation
-            return self.pauli_rep.to_mat(wire_order=wire_order or self.wires)
+        # if self.pauli_rep:  # Get the matrix from the PauliSentence representation
+        #     return self.pauli_rep.to_mat(wire_order=wire_order or self.wires, format="csr").toarray()
 
         mats: List[TensorLike] = []
         batched: List[bool] = []  # batched[i] tells if mats[i] is batched or not
