111bf91d28402d132f7de8ec74a2de5eb9f57736
qottmann <korbinian.kottmann@gmail.com>
2024-04-05 15:29:14 +0200

    tests rearranged

--- tests/dla/test_center.py
+++ tests/dla/test_center.py
@@ -33,19 +33,18 @@ DLA_CENTERS = (
     ([qml.X(0), qml.X(1)], [qml.X(0), qml.X(1)]),  # two non-overlapping wires
     ([qml.X(0), qml.Y(1)], [qml.X(0), qml.Y(1)]),  # two non-overlapping wires with different ops
     ([qml.X(0), qml.Y(0), qml.Z(0), qml.I()], [qml.I()]),  # non-trivial DLA, but trivial center
-    ([qml.X(0), qml.X(0) @ qml.X(1), qml.Y(1)], [qml.X(0)]) # non-trivial DLA
 )
 
 
 @pytest.mark.parametrize("ops, true_res", DLA_CENTERS)
-def test_trivial_center(ops, true_res):
+def test_center(ops, true_res):
     """Test a trivial centers with Identity operators or non-overlapping wires"""
     res = center(ops)
     assert res == true_res
 
 
 @pytest.mark.parametrize("ops, true_res", DLA_CENTERS)
-def test_trivial_center_pauli(ops, true_res):
+def test_center_pauli(ops, true_res):
     """Test a trivial centers with Identity operators or non-overlapping wires using their pauli_rep"""
     ops = [op.pauli_rep for op in ops]
     res = center(ops, pauli=True)
@@ -54,3 +53,14 @@ def test_trivial_center_pauli(ops, true_res):
     true_res = [op.pauli_rep for op in true_res]
     assert res == true_res
 
+GENERATOR_CENTERS = (
+    ([qml.X(0), qml.X(0) @ qml.X(1), qml.Y(1)], [qml.X(0)]),
+    ([p(0) @ p(1) for p in [qml.X, qml.Y, qml.Z]], [p(0) @ p(1) for p in [qml.X, qml.Y, qml.Z]])
+)
+
+@pytest.mark.parametrize("generators, true_res", GENERATOR_CENTERS)
+def test_center_dla(generators, true_res):
+    """Test computing the center for a non-trivial DLA"""
+    g = qml.dla.lie_closure(generators)
+    res = center(g)
+    assert res == true_res
