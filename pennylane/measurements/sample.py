38c50b1d238f07ed2e6338cd1f24820895397f8e
Astral Cai <astral.cai@xanadu.ai>
2024-04-08 09:11:15 -0400

    Update sample.py

--- pennylane/measurements/sample.py
+++ pennylane/measurements/sample.py
@@ -23,7 +23,6 @@ import numpy as np
 import pennylane as qml
 from pennylane.operation import Operator
 from pennylane.wires import Wires
-from pennylane.typing import TensorLike
 
 from .measurements import MeasurementShapeError, Sample, SampleMeasurement
 from .mid_measure import MeasurementValue
