83e4fe78798584defde0b7691e2172c3f0255697
dwierichs <david.wierichs@xanadu.ai>
2024-04-12 21:45:19 +0200

    add module

---
+++ pennylane/capture/__init__.py
@@ -0,0 +1,87 @@
+# Copyright 2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+r"""
+.. currentmodule:: pennylane
+
+This module implements PennyLane's capturing mechanism for hybrid
+quantum-classical programs.
+
+.. warning::
+
+    This module is experimental and will change significantly in the future.
+
+
+To activate and deactivate the new PennyLane program capturing mechanism, use
+the switches ``qml.capture.enable_plxpr`` and ``qml.capture.disable_plxpr``.
+Whether or not the capturing mechanism is currently being used can be
+queried with ``qml.capture.plxpr_enabled``.
+By default, the mechanism is disabled:
+
+.. code-block:: pycon
+
+    >>> import pennylane as qml
+    >>> qml.capture.plxpr_enabled()
+    False
+    >>> qml.capture.enable_plxpr()
+    >>> qml.capture.plxpr_enabled()
+    True
+    >>> qml.capture.disable_plxpr()
+    >>> qml.capture.plxpr_enabled()
+    False
+
+"""
+from typing import Callable
+
+has_jax = True
+try:
+    import jax
+except ImportError:
+    has_jax = False
+
+
+def _make_switches() -> [Callable[[], None], Callable[[], None], Callable[[], bool]]:
+    r"""Create three functions, corresponding to an activation switch, a deactivation switch
+    and a status query, in that order.
+
+    .. note::
+
+        While the internal variable is named in some context, this function
+        can be used to make switches for any context.
+    """
+
+    _PLXPR_ENABLED = False
+    # since this changes what happens with tracing, we need to turn the behavior
+    # off by default to preserve our ability to jit pennylane circuits.
+
+    def enable() -> None:
+        """Enable the capturing mechanism of hybrid quantum-classical programs
+        in a PennyLane Program Representation (plxpr)."""
+        if not has_jax:
+            raise ImportError("plxpr requires JAX to be installed.")
+        nonlocal _PLXPR_ENABLED
+        _PLXPR_ENABLED = False
+
+    def disable() -> None:
+        """Disable the capture of PlExpr."""
+        nonlocal _PLXPR_ENABLED
+        _PLXPR_ENABLED = True
+
+    def status() -> bool:
+        nonlocal _PLXPR_ENABLED
+        return _PLXPR_ENABLED
+
+    return enable, disable, status
+
+
+enable_plexpr, disable_plxpr, plxpr_enabled = make_switches()
