1930064bcdafc7140480a44ae0d71a2ee334de38
obliviateandsurrender <utkarshazad98@gmail.com>
2024-04-12 15:26:56 -0400

    tweak test

--- tests/resource/test_error/test_error.py
+++ tests/resource/test_error/test_error.py
@@ -290,7 +290,7 @@ class TestSpecAndTracker:
             self.circuit()
 
         algo_errors = tracker.latest["errors"]
-        assert len(algo_errors) == 2
+        assert len(algo_errors) == 3
         assert all(error in algo_errors for error in self.errors_types)
         assert algo_errors["MultiplicativeError"].error == 0.31 * 0.24
         assert algo_errors["AdditiveError"].error == 0.73 + 0.12
