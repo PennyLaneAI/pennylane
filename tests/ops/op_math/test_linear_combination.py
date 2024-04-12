704745cdf76f18b9c1418abe08f922a116605287
qottmann <korbinian.kottmann@gmail.com>
2024-04-12 11:55:25 +0200

    add test for warning about mixing systems

--- tests/ops/op_math/test_linear_combination.py
+++ tests/ops/op_math/test_linear_combination.py
@@ -53,10 +53,20 @@ class TestParityWithHamiltonian:
         assert isinstance(H, qml.Hamiltonian)
 
 @pytest.mark.filterwarnings("ignore:Using 'qml.ops.Hamiltonian' with new operator arithmetic is deprecated")
-def test_mixed_legacy_warning():
+def test_mixed_legacy_warning_Hamiltonian():
     """Test that mixing legacy ops and LinearCombination.compare raises a warning"""
     op1 = qml.ops.LinearCombination([0.5, 0.5], [X(0) @ X(1), qml.Hadamard(0)])
     op2 = qml.ops.Hamiltonian([0.5, 0.5], [qml.operation.Tensor(X(0), X(1)), qml.Hadamard(0)])
+
+    with pytest.warns(UserWarning, match="Attempting to compare a legacy operator class instance"):
+        res = op1.compare(op2)
+    
+    assert res
+
+def test_mixed_legacy_warning_Tensor():
+    """Test that mixing legacy ops and LinearCombination.compare raises a warning"""
+    op1 = qml.ops.LinearCombination([1.], [X(0) @ qml.Hadamard(1)])
+    op2 = qml.operation.Tensor(X(0), qml.Hadamard(1))
 
     with pytest.warns(UserWarning, match="Attempting to compare a legacy operator class instance"):
         res = op1.compare(op2)
