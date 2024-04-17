1484efdd6089bb22068327f875c9ba817676cfa3
qottmann <korbinian.kottmann@gmail.com>
2024-04-17 13:55:46 +0200

    cross-referencing

--- pennylane/pauli/dla/center.py
+++ pennylane/pauli/dla/center.py
@@ -41,6 +41,8 @@ def center(
 
     Returns:
         List[Union[Operator, PauliSentence]]: Center of ``g``
+    
+    .. see-also:: :func:`~.lie_closure`, :func:`~.structure_constants`, `Demo: Introduction to Dynamical Lie Algebras for quantum practitioners <https://pennylane.ai/qml/demos/tutorial_liealgebra/>`__
 
     **Example**
 
