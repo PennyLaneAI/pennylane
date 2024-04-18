64b9f3caab13481bd6ad0a00dc6f616c110b5573
qottmann <korbinian.kottmann@gmail.com>
2024-04-18 15:12:32 +0200

    docstring

--- pennylane/ops/op_math/prod.py
+++ pennylane/ops/op_math/prod.py
@@ -283,6 +283,9 @@ class Prod(CompositeOp):
 
     @property
     def obs(self):
+        """Access the operands of a ``Prod`` instance"""
+        # This is temporary property to smoothen the transition to the new operator arithmetic system.
+        # In particular, now that the __matmul__ (@ python operator) method between operators generates Prod instead of Tensor instances.
         warnings.warn(
             "Accessing the terms of a tensor product operator via op.obs is deprecated, please use op.operands instead.",
             qml.PennyLaneDeprecationWarning,
