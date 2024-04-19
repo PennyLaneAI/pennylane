89fdb90f2ce16bc937af9746284773adb3362ed7
gabrielLydian <gabrielbottrill@gmail.com>
2024-04-19 16:00:23 -0700

    Added jax update to fix failing tests

--- tests/devices/test_default_qutrit_mixed.py
+++ tests/devices/test_default_qutrit_mixed.py
@@ -86,6 +86,9 @@ class TestPRNGKeySeed:
 
     def test_prng_key_as_seed(self):
         """Test that a jax PRNG can be passed as a seed."""
+        from jax.config import update
+        update("jax_enable_x64", True)
+
         from jax import random
 
         key1 = random.key(123)
