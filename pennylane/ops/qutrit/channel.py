0aec1516a41218e9964cbdcd07579d4943eeed33
gabrielLydian <gabrielbottrill@gmail.com>
2024-04-20 00:22:29 -0700

    Fixed compute_kraus_matrices

--- pennylane/ops/qutrit/channel.py
+++ pennylane/ops/qutrit/channel.py
@@ -16,8 +16,11 @@
 This module contains the available built-in noisy
 quantum channels supported by PennyLane, as well as their conventions.
 """
+import numpy as np
 from pennylane import math
 from pennylane.operation import Channel
+
+QUDIT_DIM = 3
 
 
 class QutritDepolarizingChannel(Channel):
@@ -25,27 +28,26 @@ class QutritDepolarizingChannel(Channel):
     Single-qutrit symmetrically depolarizing error channel.
     This channel is modelled by the following Kraus matrices:
     where :math:`p \in [0, 1]` is the depolarization probability and is equally
-    divided in the application of all Pauli operations.
+    divided in the application of all TODO operations.
     .. note::
-        Multiple equivalent definitions of the Kraus operators :math:`\{K_0 \ldots K_3\}` exist in
+        Multiple equivalent definitions of the Kraus operators :math:`\{K_0 \ldots K_8\}` exist in #TODO fix liturature
         the literature [`1 <https://michaelnielsen.org/qcqi/>`_] (Eqs. 8.102-103). Here, we adopt the
         one from Eq. 8.103, which is also presented in [`2 <http://theory.caltech.edu/~preskill/ph219/chap3_15.pdf>`_] (Eq. 3.85).  # TODO change sources
         For this definition, please make a note of the following:
         * For :math:`p = 0`, the channel will be an Identity channel, i.e., a noise-free channel.
-        * For :math:`p = \frac{3}{4}`, the channel will be a fully depolarizing channel.
-        * For :math:`p = 1`, the channel will be a uniform Pauli error channel.
+        * For :math:`p = TODO \frac{3}{4}`, the channel will be a fully depolarizing channel.
+        * For :math:`p = 1`, the channel will be a uniform TODO error channel.
     **Details:**
     * Number of wires: 1
     * Number of parameters: 1
     Args:
-        p (float): Each Pauli gate is applied with probability :math:`\frac{p}{3}`
+        p (float): Each TODO gate is applied with probability :math:`\frac{p}{3}`
         wires (Sequence[int] or int): the wire the channel acts on
         id (str or None): String representing the operation (optional)
     """
     num_params = 1
     num_wires = 1
-    grad_method = "A"
-    grad_recipe = ([[1, 0, 1], [-1, 0, 0]],)  # TODO
+    grad_method = "F"
 
     def __init__(self, p, wires, id=None):
         super().__init__(p, wires=wires, id=id)
@@ -54,41 +56,76 @@ class QutritDepolarizingChannel(Channel):
     def compute_kraus_matrices(p):  # pylint:disable=arguments-differ
         r"""Kraus matrices representing the depolarizing channel.
         Args:
-            p (float): each Pauli gate is applied with probability :math:`\frac{p}{3}`
+            p (float): each TODO gate is applied with probability :math:`\frac{p}{9}`
         Returns:
             list (array): list of Kraus matrices
         **Example**
-        >>> qml.DepolarizingChannel.compute_kraus_matrices(0.5)
-        [array([[0.70710678, 0.        ], [0.        , 0.70710678]]),
-         array([[0.        , 0.40824829], [0.40824829, 0.        ]]),
-         array([[0.+0.j        , 0.-0.40824829j], [0.+0.40824829j, 0.+0.j        ]]),
-         array([[ 0.40824829,  0.        ], [ 0.        , -0.40824829]])]
+        >>> qml.QutritDepolarizingChannel.compute_kraus_matrices(0.5)
+        [array([[0.74535599+0.j, 0.        +0.j, 0.        +0.j],
+       [0.        +0.j, 0.74535599+0.j, 0.        +0.j],
+       [0.        +0.j, 0.        +0.j, 0.74535599+0.j]]), array([[0.        , 0.23570226, 0.        ],
+       [0.        , 0.        , 0.23570226],
+       [0.23570226, 0.        , 0.        ]]), array([[0.        , 0.        , 0.23570226],
+       [0.23570226, 0.        , 0.        ],
+       [0.        , 0.23570226, 0.        ]]), array([[ 0.23570226+0.j        ,  0.        +0.j        ,
+         0.        +0.j        ],
+       [ 0.        +0.j        , -0.11785113+0.20412415j,
+         0.        +0.j        ],
+       [ 0.        +0.j        ,  0.        +0.j        ,
+        -0.11785113-0.20412415j]]), array([[ 0.        +0.j        , -0.11785113+0.20412415j,
+         0.        +0.j        ],
+       [ 0.        +0.j        ,  0.        +0.j        ,
+        -0.11785113-0.20412415j],
+       [ 0.23570226+0.j        ,  0.        +0.j        ,
+         0.        +0.j        ]]), array([[ 0.        +0.j        ,  0.        +0.j        ,
+        -0.11785113-0.20412415j],
+       [ 0.23570226+0.j        ,  0.        +0.j        ,
+         0.        +0.j        ],
+       [ 0.        +0.j        , -0.11785113+0.20412415j,
+         0.        +0.j        ]]), array([[ 0.23570226+0.j        ,  0.        +0.j        ,
+         0.        +0.j        ],
+       [ 0.        +0.j        , -0.11785113-0.20412415j,
+         0.        +0.j        ],
+       [ 0.        +0.j        ,  0.        +0.j        ,
+        -0.11785113+0.20412415j]]), array([[ 0.        +0.j        , -0.11785113-0.20412415j,
+         0.        +0.j        ],
+       [ 0.        +0.j        ,  0.        +0.j        ,
+        -0.11785113+0.20412415j],
+       [ 0.23570226+0.j        ,  0.        +0.j        ,
+         0.        +0.j        ]]), array([[ 0.        +0.j        ,  0.        +0.j        ,
+        -0.11785113+0.20412415j],
+       [ 0.23570226+0.j        ,  0.        +0.j        ,
+         0.        +0.j        ],
+       [ 0.        +0.j        , -0.11785113-0.20412415j,
+         0.        +0.j        ]])]
         """
         if not math.is_abstract(p) and not 0.0 <= p <= 1.0:
             raise ValueError("p must be in the interval [0,1]")
 
-        if math.get_interface(p) == "tensorflow":
+        interface = math.get_interface(p)
+        w = math.exp(2j * np.pi / 3)
+        one = 1
+        z = 0
+        if interface == "tensorflow":
             p = math.cast_like(p, 1j)
+            w = math.cast_like(w, p)
+            one = math.cast_like(one, p)
+            z = math.cast_like(z, p)
 
-        Z0 = math.eye(3)
-        Z1 = math.diag([1, math.exp(2j * math.pi / 3), math.exp(4j * math.pi / 3)])
-        Z2 = math.diag([1, math.exp(4j * math.pi / 3), math.exp(8j * math.pi / 3)])
+        depolarizing_mats = [
+            [[z, one, z], [z, z, one], [one, z, z]],
+            [[z, z, one], [one, z, z], [z, one, z]],
 
-        X0 = math.eye(3)
-        X1 = math.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]])
-        X2 = math.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
+            [[one, z, z], [z, w, z], [z, z, w ** 2]],
+            [[z, w, 0j], [z, z, w**2], [one, z, z]],
+            [[z, z, w**2], [one, z, z], [z, w, z]],
 
-        Ks = [
-            math.sqrt(1 - (8 * p / 9) + math.eps) * math.convert_like(math.eye(2, dtype=complex), p)
+            [[one, z, z], [z, w ** 2, z], [z, z, w ** 4]],
+            [[z, w ** 2, z], [z, z, w ** 4], [one, z, z]],
+            [[z, z, w**4], [one, z, z], [z, w**2, z]]
         ]
+        normalization = math.sqrt(p / 9 + math.eps)
+        Ks = [normalization * math.array(m, like=interface) for m in depolarizing_mats]
+        identity = math.sqrt(1 - (8 * p / 9) + math.eps) * math.array(math.eye(QUDIT_DIM, dtype=complex), like=interface)
 
-        for i, Z in enumerate((Z0, Z1, Z2)):
-            for j, X in enumerate((X0, X1, X2)):
-                if i == 0 and j == 0:
-                    continue
-                Ks.append(
-                    math.sqrt(p / 9 + math.eps)
-                    * math.convert_like(math.array(X @ Z, dtype=complex), p)
-                )
-
-        return Ks
+        return [identity] + Ks
--- tests/ops/qutrit/test_qutrit_channel_ops.py
+++ tests/ops/qutrit/test_qutrit_channel_ops.py
@@ -20,78 +20,125 @@ import pennylane as qml
 from pennylane import numpy as pnp
 from pennylane.ops.qutrit import channel
 
+QUDIT_DIM = 3
+
 
 class TestQutritDepolarizingChannel:
     """Tests for the qutrit quantum channel QutritDepolarizingChannel"""
 
+    @staticmethod
+    def get_expected_kraus_matrices(p):
+        """Gets the expected Kraus matrices given probability p."""
+        w = np.exp(2j * np.pi / 3)
+
+        Z0 = np.eye(QUDIT_DIM)
+        Z1 = np.diag([1, w, w ** 2])
+        Z2 = np.diag([1, w ** 2, w ** 4])
+
+        X0 = np.eye(QUDIT_DIM)
+        X1 = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]])
+        X2 = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
+
+        Ks = [np.sqrt(1 - (8 * p / 9)) * np.eye(QUDIT_DIM)]
+        for i, Z in enumerate((Z0, Z1, Z2)):
+            for j, X in enumerate((X0, X1, X2)):
+                if i == 0 and j == 0:
+                    continue
+                Ks.append(np.sqrt(p / 9) * X @ Z)
+
+        return Ks
+
     def test_p_zero(self, tol):
         """Test p=0 gives correct Kraus matrices"""
         op = qml.QutritDepolarizingChannel
-        assert np.allclose(op(0, wires=0).kraus_matrices()[0], np.eye(2), atol=tol, rtol=0)
-        assert np.allclose(op(0, wires=0).kraus_matrices()[1], np.zeros((2, 2)), atol=tol, rtol=0)
+        kraus_matrices = op(0, wires=0).kraus_matrices()
+
+        assert len(kraus_matrices) == 9
+        assert np.allclose(kraus_matrices[0], np.eye(QUDIT_DIM), atol=tol, rtol=0)
+        assert np.allclose(np.array(kraus_matrices[1:]), 0, atol=tol, rtol=0)
 
     def test_p_arbitrary(self, tol):
         """Test p=0.1 gives correct Kraus matrices"""
         p = 0.1
-        op = qml.QutritDepolarizingChannel
-        expected = np.sqrt(p / 3) * X
-        assert np.allclose(op(0.1, wires=0).kraus_matrices()[1], expected, atol=tol, rtol=0)
+        kraus_matrices = qml.QutritDepolarizingChannel(p, wires=0).kraus_matrices()
+        expected_matrices = self.get_expected_kraus_matrices(p)
+        for krause_matrix, expected_matrix in zip(kraus_matrices, expected_matrices):
+            assert np.allclose(krause_matrix, expected_matrix, atol=tol, rtol=0)
 
     @pytest.mark.parametrize("angle", np.linspace(0, 2 * np.pi, 7))
     def test_grad_depolarizing(self, angle):
         """Test that analytical gradient is computed correctly for different states. Channel
         grad recipes are independent of channel parameter"""
 
-        dev = qml.device("default.mixed", wires=1)
+        dev = qml.device("default.qutrit.mixed")
         prob = pnp.array(0.5, requires_grad=True)
 
         @qml.qnode(dev)
         def circuit(p):
-            qml.RX(angle, wires=0)
+            qml.TRX(angle, wires=0)
             qml.QutritDepolarizingChannel(p, wires=0)
-            return qml.expval(qml.PauliZ(0))
+            return qml.expval(qml.GellMann(0, 3) + qml.GellMann(0, 8))
 
         gradient = np.squeeze(qml.grad(circuit)(prob))
         assert np.allclose(gradient, circuit(1) - circuit(0))
         assert np.allclose(gradient, -(4 / 3) * np.cos(angle))
 
     def test_p_invalid_parameter(self):
+        """Test that error is raised given an inappropriate p value."""
         with pytest.raises(ValueError, match="p must be in the interval"):
             qml.QutritDepolarizingChannel(1.5, wires=0).kraus_matrices()
 
     @staticmethod
     def expected_jac_fn(p):
-        return [
-            -1 / (2 * qml.math.sqrt(1 - p)) * qml.math.eye(2),
-            1 / (6 * qml.math.sqrt(p / 3)) * qml.math.array([[0, 1], [1, 0]]),
-            1 / (6 * qml.math.sqrt(p / 3)) * qml.math.array([[0, -1j], [1j, 0]]),
-            1 / (6 * qml.math.sqrt(p / 3)) * qml.math.diag([1, -1]),
-        ]
+        """Gets the expected Jacobian of Kraus matrices given probability p."""
+        w = np.exp(2j * np.pi / 3)
+
+        Z0 = np.eye(QUDIT_DIM)
+        Z1 = np.diag([1, w, w ** 2])
+        Z2 = np.diag([1, w ** 2, w ** 4])
+
+        X0 = np.eye(QUDIT_DIM)
+        X1 = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]])
+        X2 = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
+
+        jacs = [-(4 / 9) / np.sqrt(1 - (8 * p / 9)) * np.eye(QUDIT_DIM)]
+        for i, Z in enumerate((Z0, Z1, Z2)):
+            for j, X in enumerate((X0, X1, X2)):
+                if i == 0 and j == 0:
+                    continue
+                jacs.append(1 / (18 * np.sqrt(p / 9)) * X @ Z)
+
+        return jacs
 
     @staticmethod
     def kraus_fn(x):
+        """Gets a matrix of the Kraus matrices to be tested."""
         return qml.math.stack(channel.QutritDepolarizingChannel(x, wires=0).kraus_matrices())
 
     @staticmethod
     def kraus_fn_real(x):
+        """Gets a matrix of the real part of the Kraus matrices to be tested."""
         return qml.math.real(
             qml.math.stack(channel.QutritDepolarizingChannel(x, wires=0).kraus_matrices())
         )
 
     @staticmethod
     def kraus_fn_imag(x):
+        """Gets a matrix of the imaginary part of the Kraus matrices to be tested."""
         return qml.math.imag(
             qml.math.stack(channel.QutritDepolarizingChannel(x, wires=0).kraus_matrices())
         )
 
     @pytest.mark.autograd
     def test_kraus_jac_autograd(self):
+        """Tests Jacobian of Kraus matrices using autograd."""
         p = pnp.array(0.43, requires_grad=True)
         jac = qml.jacobian(self.kraus_fn_real)(p) + 1j * qml.jacobian(self.kraus_fn_imag)(p)
         assert qml.math.allclose(jac, self.expected_jac_fn(p))
 
     @pytest.mark.torch
     def test_kraus_jac_torch(self):
+        """Tests Jacobian of Kraus matrices using torch."""
         import torch
 
         p = torch.tensor(0.43, requires_grad=True)
@@ -101,16 +148,22 @@ class TestQutritDepolarizingChannel:
 
     @pytest.mark.tf
     def test_kraus_jac_tf(self):
+        """Tests Jacobian of Kraus matrices using tensorflow."""
         import tensorflow as tf
-
         p = tf.Variable(0.43)
-        with tf.GradientTape() as tape:
-            out = self.kraus_fn(p)
-        jac = tape.jacobian(out, p)
-        assert qml.math.allclose(jac, self.expected_jac_fn(p))
+        with tf.GradientTape() as real_tape:
+            real_out = self.kraus_fn_real(p)
+        with tf.GradientTape() as imag_tape:
+            imag_out = self.kraus_fn_imag(p)
+
+        real_jac = qml.math.cast(real_tape.jacobian(real_out, p), complex)
+        imaj_jac = qml.math.cast(imag_tape.jacobian(imag_out, p), complex)
+        jac = real_jac + 1j * imaj_jac
+        assert qml.math.allclose(jac, self.expected_jac_fn(0.43))
 
     @pytest.mark.jax
     def test_kraus_jac_jax(self):
+        """Tests Jacobian of Kraus matrices using jax."""
         import jax
 
         jax.config.update("jax_enable_x64", True)
