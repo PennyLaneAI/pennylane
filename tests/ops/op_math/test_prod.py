0ff160ddc37407b01f6064d588c9e978c628324f
Korbinian Kottmann <43949391+Qottmann@users.noreply.github.com>
2024-04-25 15:45:23 +0200

    [new opmath] Add a `Prod.obs` property for the transition period of switching operator arithmetic (#5539)
    Adding a property `Prod.obs` to not break code that uses `Tensor.obs`,
    but immediately deprecating it.
    
    Add a Prod obs property for smooth switchover [sc-61680]
    
    ---------
    
    Co-authored-by: Thomas R. Bromley <49409390+trbromley@users.noreply.github.com>

--- tests/ops/op_math/test_prod.py
+++ tests/ops/op_math/test_prod.py
@@ -103,6 +103,18 @@ def test_legacy_coeffs():
         _ = H.coeffs
 
 
+def test_obs_attribute():
+    """Test that operands can be accessed via Prod.obs and a deprecation warning is raised"""
+    op = qml.prod(X(0), X(1), X(2))
+    with pytest.warns(
+        qml.PennyLaneDeprecationWarning,
+        match="Accessing the terms of a tensor product operator via op.obs is deprecated",
+    ):
+        obs = op.obs
+
+    assert obs == (X(0), X(1), X(2))
+
+
 # currently failing due to has_diagonalizing_gates logic
 @pytest.mark.xfail  # TODO: fix with story 49608
 def test_basic_validity():
