cdffb474b375d10df4d1421e5e52e0ef0310495b
qottmann <korbinian.kottmann@gmail.com>
2024-04-25 18:14:57 +0200

    update explicit enable/disable in codebase, some docs updates

--- pennylane/ops/op_math/prod.py
+++ pennylane/ops/op_math/prod.py
@@ -458,7 +458,6 @@ class Prod(CompositeOp):
 
         **Example**
 
-        >>> qml.operation.enable_new_opmath()
         >>> op = X(0) @ (0.5 * X(1) + X(2))
         >>> op.terms()
         ([0.5, 1.0],
