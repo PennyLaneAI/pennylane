d3750009407abc6dbd5bb49e0c4fc424ea31933d
Astral Cai <astral.cai@xanadu.ai>
2024-04-29 16:46:10 -0400

    update tests

--- tests/ops/op_math/test_linear_combination.py
+++ tests/ops/op_math/test_linear_combination.py
@@ -1628,11 +1628,12 @@ class TestLinearCombinationEvaluation:
         # simplify worked and added 1. and 2.
         assert pars == [0.1, 3.0]
 
+    @pytest.mark.usefixtures("use_legacy_and_new_opmath")
     def test_queuing_behaviour(self):
         """Tests that the base observables are correctly dequeued with simplify=True"""
 
         with qml.queuing.AnnotatedQueue() as q:
-            obs = qml.Hamiltonian([1, 1], [qml.X(0), qml.Z(0)])
+            obs = qml.Hamiltonian([1, 1], [qml.X(0), qml.Z(0)], simplify=True)
 
         assert len(q) == 1
         assert q.queue[0] == obs
