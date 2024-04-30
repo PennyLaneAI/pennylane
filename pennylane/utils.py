e920c926ce514a7ac0df835869c0ff2f2bd0b6e1
lillian542 <Lillian.frederiksen@xanadu.ai>
2024-04-30 16:26:34 -0400

    change eigvals to floats

--- pennylane/utils.py
+++ pennylane/utils.py
@@ -151,7 +151,7 @@ def pauli_eigs(n):
         list: the eigenvalues of the specified observable
     """
     if n == 1:
-        return np.array([1, -1])
+        return np.array([1.0, -1.0])
     return np.concatenate([pauli_eigs(n - 1), -pauli_eigs(n - 1)])
 
 
