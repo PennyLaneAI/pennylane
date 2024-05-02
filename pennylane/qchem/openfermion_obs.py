a52a5cb316bf2b3fffe1d69859e62d14e1ca5dca
soranjh <soran.jahangiri@gmail.com>
2024-05-02 13:25:24 -0400

    fix trailing whitespace

--- pennylane/qchem/openfermion_obs.py
+++ pennylane/qchem/openfermion_obs.py
@@ -816,7 +816,6 @@ def decompose(hf_file, mapping="jordan_wigner", core=None, active=None):
 def molecular_hamiltonian(*args, **kwargs):
     """molecular_hamiltonian(molecule, method="dhf", active_electrons=None, active_orbitals=None,\
     mapping="jordan_wigner", outpath=".", wires=None, args=None, load_data=False, convert_tol=1e012)
-    
     Generate the qubit Hamiltonian of a molecule.
 
     This function drives the construction of the second-quantized electronic Hamiltonian
