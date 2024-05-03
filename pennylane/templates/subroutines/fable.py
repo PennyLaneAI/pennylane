c44449a2f854f1e6241ed0b8ad54ff7ae6403fc0
Mudit Pandey <mudit.pandey@xanadu.ai>
2024-05-03 14:21:25 -0400

    Fix FABLE desc

--- pennylane/templates/subroutines/fable.py
+++ pennylane/templates/subroutines/fable.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-This module contains the template for the Fast Approximate Block Encoding (FABLE) technique.
+This module contains the template for the Fast Approximate BLock Encoding (FABLE) technique.
 """
 import warnings
 import numpy as np
