4c03e36e19fdab463e4a4a1c7b080328041322b8
obliviateandsurrender <utkarshazad98@gmail.com>
2024-05-04 11:00:30 -0400

    init

--- pennylane/noise/conditionals.py
+++ pennylane/noise/conditionals.py
@@ -12,10 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class Conditional():
-    pass
-
-
+import pennylane as qml
+from pennylane.ops import Conditional
 
 
 class OperationConditional(Conditional):
