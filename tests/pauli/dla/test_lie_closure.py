94a770ecc08b64cd4616c44d980cc384e67256ac
Astral Cai <astral.cai@xanadu.ai>
2024-05-06 09:13:04 -0400

    Fix import order

--- tests/pauli/dla/test_lie_closure.py
+++ tests/pauli/dla/test_lie_closure.py
@@ -19,10 +19,8 @@ import numpy as np
 import pytest
 
 import pennylane as qml
-
-from pennylane import X, Y, Z, I
-
-from pennylane.pauli import PauliWord, PauliSentence, PauliVSpace, lie_closure
+from pennylane import I, X, Y, Z
+from pennylane.pauli import PauliSentence, PauliVSpace, PauliWord, lie_closure
 
 ops1 = [
     PauliSentence({PauliWord({0: "X", 1: "X"}): 1.0, PauliWord({0: "Y", 1: "Y"}): 1.0}),
