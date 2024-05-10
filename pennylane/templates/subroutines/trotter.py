63b25471ce7323d8847706f0a9ab825f54773bd6
Jay Soni <jbsoni@uwaterloo.ca>
2024-05-10 17:15:52 -0400

    Add resources to TrotterProduct class

--- pennylane/templates/subroutines/trotter.py
+++ pennylane/templates/subroutines/trotter.py
@@ -15,9 +15,13 @@
 Contains templates for Suzuki-Trotter approximation based subroutines.
 """
 
+import copy
+from collections import defaultdict
+
 import pennylane as qml
 from pennylane.ops import Sum
 from pennylane.ops.op_math import SProd
+from pennylane.resource import Resources, ResourcesOperation
 from pennylane.resource.error import ErrorOperation, SpectralNormError
 
 
@@ -62,7 +66,7 @@ def _recursive_expression(x, order, ops):
     return (2 * ops_lst_1) + ops_lst_2 + (2 * ops_lst_1)
 
 
-class TrotterProduct(ErrorOperation):
+class TrotterProduct(ErrorOperation, ResourcesOperation):
     r"""An operation representing the Suzuki-Trotter product approximation for the complex matrix
     exponential of a given Hamiltonian.
 
@@ -235,6 +239,30 @@ class TrotterProduct(ErrorOperation):
         context.remove(self.hyperparameters["base"])
         context.append(self)
         return self
+
+    def resources(self) -> Resources:
+        """The resource requirements for a given instance of the Suzuki-Trotter product.
+
+        Returns:
+            Resources: The resources for an instance of TrotterProduct.
+        """
+        with qml.QueuingManager.stop_recording():
+            decomp = self.compute_decomposition(*self.parameters, **self.hyperparameters)
+
+        num_wires = len(self.wires)
+        num_gates = len(decomp)
+
+        unique_operation_decomp = (copy.deepcopy(op) for op in decomp)
+        depth = qml.tape.QuantumTape(ops=unique_operation_decomp).graph.get_depth()
+
+        gate_types = defaultdict(int)
+        gate_sizes = defaultdict(int)
+
+        for op in decomp:
+            gate_types[op.name] += 1
+            gate_sizes[len(op.wires)] += 1
+
+        return Resources(num_wires, num_gates, gate_types, gate_sizes, depth)
 
     def error(
         self, method: str = "commutator-bound", fast: bool = True
