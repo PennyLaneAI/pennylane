8cd6f2405d44dcbc085d6337c31faa34b98ef0c3
obliviateandsurrender <utkarshazad98@gmail.com>
2024-05-20 11:43:11 -0400

    improve op check

--- pennylane/transforms/add_noise.py
+++ pennylane/transforms/add_noise.py
@@ -26,7 +26,7 @@ def add_noise(tape, noise_model, level=None):
 
     Args:
         tape (QNode or QuantumTape or Callable or pennylane.devices.Device): the input circuit to be transformed.
-        noise_model (NoiseModel): noise model for
+        noise_model (NoiseModel): noise model according to which noise has to be inserted.
         level (None, str, int, slice): An indication of a stage in the transform program.
             * ``None``: expands the tape to have no ``Adjoint`` and ``Templates``.
             * ``str``: Acceptable keys are ``"top"``, ``"user"``, ``"device"``, and ``"gradient"``
@@ -35,7 +35,6 @@ def add_noise(tape, noise_model, level=None):
 
     Returns:
         qnode (QNode) or quantum function (Callable) or tuple[List[.QuantumTape], function] or device (pennylane.devices.Device):
-
         The transformed circuit as described in :func:`qml.transform <pennylane.transform>`.
 
     Raises:
@@ -64,17 +63,22 @@ def add_noise(tape, noise_model, level=None):
     metadata = noise_model.metadata
     for condition, noise in noise_model.model_map.items():
         conditions.append(lru_cache(maxsize=512)(condition))
-        noises.append(noise)
+        noises.append(qml.tape.make_qscript(noise))
 
-    with qml.queuing.AnnotatedQueue() as new_operations:
-        for operation in tape.operations:
-            qml.apply(operation)
-            new_operations.append(operation)
-            for condition, noise in zip(conditions, noises):
-                if condition(operation):
-                    noise(operation, **metadata)
+    new_operations = []
+    for operation in tape.operations:
+        curr_ops = [operation]
+        for condition, noise in zip(conditions, noises):
+            if condition(operation):
+                noise_ops = noise(operation, **metadata).operations
+                if operation in noise_ops and _check_queue_op(operation, noise, metadata):
+                    ops_indx = noise_ops.index(operation)
+                    curr_ops = noise_ops[:ops_indx] + curr_ops + noise_ops[ops_indx + 1 :]
+                else:
+                    curr_ops.extend(noise_ops)
+        new_operations.extend(curr_ops)
 
-    new_tape = type(tape)(new_operations.queue, tape.measurements, shots=tape.shots)
+    new_tape = type(tape)(new_operations, tape.measurements, shots=tape.shots)
 
     def null_postprocessing(results):
         """A postprocesing function returned by a transform that only converts the batch of results
@@ -83,3 +87,14 @@ def add_noise(tape, noise_model, level=None):
         return results[0]
 
     return [new_tape], null_postprocessing
+
+
+def _check_queue_op(operation, noise_func, metadata):
+    """Performs a secondary check for existence of an operation in the queue using a randomized ID"""
+
+    test_id = "f49968bfc4W0H86df3A733bf6e92904d21a_!$-T-@!_c131S549b169b061I25b85398bfd8ec1S3c"
+    test_queue = noise_func(
+        qml.noise.partial_wires(operation, id=test_id)(operation.wires), **metadata
+    ).operations
+
+    return test_id in [getattr(o, "id", "") for o in test_queue]
