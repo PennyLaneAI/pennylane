76a35f4d072f6b5206c18493158f8be452295909
dwierichs <david.wierichs@xanadu.ai>
2024-05-24 14:16:07 +0200

    lint

--- pennylane/gradients/parameter_shift.py
+++ pennylane/gradients/parameter_shift.py
@@ -36,9 +36,6 @@ from .general_shift_rules import (
 from .gradient_transform import (
     _all_zero_grad,
     _contract_qjac_with_cjac,
-    _swap_first_two_axes,
-    _move_first_axis_to_third_pos,
-    _no_trainable_grad,
     _move_first_axis_to_third_pos,
     _no_trainable_grad,
     _swap_first_two_axes,
