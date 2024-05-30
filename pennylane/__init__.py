471cdd9c734ae653485e9ffb1e288811b84ee655
PietropaoloFrisoni <pietropaolo.frisoni@xanadu.ai>
2024-05-30 19:06:51 -0400

    Reverting last change

--- pennylane/__init__.py
+++ pennylane/__init__.py
@@ -21,8 +21,8 @@ from sys import version_info
 
 import numpy as _np
 
-from packaging.specifiers import SpecifierSet
 from packaging.version import Version
+from semantic_version import SimpleSpec
 
 from pennylane.boolean_fn import BooleanFn
 import pennylane.numpy
@@ -388,7 +388,7 @@ def device(name, *args, **kwargs):
 
         if hasattr(plugin_device_class, "pennylane_requires") and Version(
             version()
-        ) not in SpecifierSet(f"=={plugin_device_class.pennylane_requires}"):
+        ) not in SimpleSpec(plugin_device_class.pennylane_requires):
             raise DeviceError(
                 f"The {name} plugin requires PennyLane versions {plugin_device_class.pennylane_requires}, "
                 f"however PennyLane version {__version__} is installed."
