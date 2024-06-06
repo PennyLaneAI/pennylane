ed18e4151b1b58e765d762003f4ab3b73da25c79
Astral Cai <astral.cai@xanadu.ai>
2024-06-06 11:49:50 -0400

    add missing line

--- pennylane/workflow/qnode.py
+++ pennylane/workflow/qnode.py
@@ -1071,6 +1071,7 @@ class QNode:
                 qml.devices.preprocess.mid_circuit_measurements,
                 device=self.device,
                 mcm_config=self.execute_kwargs["mcm_config"],
+                interface=getattr(self, "interface", None),
             )
             override_shots = 1
         elif hasattr(self.device, "capabilities"):
