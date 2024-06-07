7b69d2818153033735fd4b1407af9854c2c5b051
Tarun Kumar Allamsetty <70093909+Tarun-Kumar07@users.noreply.github.com>
2024-06-07 06:40:04 +0530

    Improve _flatten unit test
    Co-authored-by: Christina Lee <chrissie.c.l@gmail.com>

--- tests/ops/qutrit/test_qutrit_channel_ops.py
+++ tests/ops/qutrit/test_qutrit_channel_ops.py
@@ -380,6 +380,8 @@ class TestQutritChannel:
 
         qutrit_channel = qml.QutritChannel(kraus, 1, id="test")
         data, metadata = qutrit_channel._flatten()  # pylint: disable=protected-access
+        new_op = qml.QutritChannel._unflatten(data, metadata)
+        assert qml.equal(qutrit_channel, new_op)
 
         assert np.allclose(kraus, data)
         assert metadata == (qml.wires.Wires(1), ())
