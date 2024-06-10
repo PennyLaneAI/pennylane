d9740c066949146c425d6842ef23f57b04b0519c
Pietropaolo Frisoni <pietropaolo.frisoni@xanadu.ai>
2024-06-10 10:30:37 -0400

    Revert "Removing `semantic_version` from required packages" (#5825)
    Reverts PennyLaneAI/pennylane#5782

--- pennylane/__init__.py
+++ pennylane/__init__.py
@@ -21,6 +21,8 @@ from sys import version_info
 
 import numpy as _np
 
+from semantic_version import SimpleSpec, Version
+
 from pennylane.boolean_fn import BooleanFn
 import pennylane.numpy
 from pennylane.queuing import QueuingManager, apply
@@ -138,9 +140,6 @@ from pennylane.compiler import qjit, while_loop, for_loop
 import pennylane.compiler
 
 import pennylane.data
-
-from packaging.specifiers import SpecifierSet
-from packaging.version import Version
 
 # Look for an existing configuration file
 default_config = Configuration("config.toml")
@@ -391,7 +390,7 @@ def device(name, *args, **kwargs):
 
         if hasattr(plugin_device_class, "pennylane_requires") and Version(
             version()
-        ) not in SpecifierSet(f"=={plugin_device_class.pennylane_requires}"):
+        ) not in SimpleSpec(plugin_device_class.pennylane_requires):
             raise DeviceError(
                 f"The {name} plugin requires PennyLane versions {plugin_device_class.pennylane_requires}, "
                 f"however PennyLane version {__version__} is installed."
