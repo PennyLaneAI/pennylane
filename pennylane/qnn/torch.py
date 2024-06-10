cdb323bbc98f9c21af125eff5f7beb4d3ed71e49
PietropaoloFrisoni <pietropaolo.frisoni@xanadu.ai>
2024-06-10 16:59:30 -0400

    Turning tuple of tensors into tuple of list of tensor

--- pennylane/qnn/torch.py
+++ pennylane/qnn/torch.py
@@ -438,6 +438,8 @@ class TorchLayer(Module):
             return torch.hstack(_res).type(x.dtype)
 
         if isinstance(res, tuple) and len(res) > 1:
+            if isinstance(res[0], torch.Tensor):
+                res = tuple([t] for t in res)
             return tuple(_combine_dimensions(r) for r in res)
 
         return _combine_dimensions(res)
