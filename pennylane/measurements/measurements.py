9c580d3e6784bb7dfacb0f01a3dec6e5ffca5a03
JerryChen97 <chenys13@outlook.com>
2024-06-11 16:48:56 -0400

    double back ticks for density_matrix

--- pennylane/measurements/measurements.py
+++ pennylane/measurements/measurements.py
@@ -690,7 +690,7 @@ class StateMeasurement(MeasurementProcess):
                 which may be single or batched. For a single matrix, the shape should be ``(2^n, 2^n)``
                 where `n` is the number of wires the matrix acts upon. For batched matrices, the shape
                 should be ``(batch_size, 2^n, 2^n)``.
-            wire_order (Wires): The wires determining the subspace that the `density_matrix` acts on.
+            wire_order (Wires): The wires determining the subspace that the ``density_matrix`` acts on.
                 A matrix of dimension `2^n` acts on a subspace of `n` wires. This parameter specifies
                 the mapping of matrix dimensions to physical qubits, allowing the function to correctly
                 trace out the subsystems not involved in the measurement or operation.
