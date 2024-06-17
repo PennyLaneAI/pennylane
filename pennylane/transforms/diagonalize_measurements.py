3f62981e598fd2d150494e5c5aa66fa7c87389fe
lillian542 <Lillian.frederiksen@xanadu.ai>
2024-06-17 15:47:21 -0400

    switch unsupported_obs for supported_obs

--- pennylane/transforms/diagonalize_measurements.py
+++ pennylane/transforms/diagonalize_measurements.py
@@ -11,19 +11,21 @@ from pennylane.transforms.core import transform
 
 # pylint: disable=protected-access
 
-_default_unsupported_obs = frozenset(["PauliX", "PauliY", "Hadamard"])
+_default_supported_obs = frozenset(["PauliZ"])
 
 
 @transform
-def diagonalize_tape_measurements(tape, unsupported_obs=_default_unsupported_obs):
+def diagonalize_tape_measurements(tape, supported_base_obs=_default_supported_obs):
     """Diagonalize the measurements on a tape if they are not supported. Assumes all observables commute."""
 
-    bad_obs_input = set(unsupported_obs) - {"PauliX", "PauliY", "Hadamard"}
+    bad_obs_input = [
+        o for o in supported_base_obs if not o in {"PauliX", "PauliY", "PauliZ", "Hadamard"}
+    ]
 
     if bad_obs_input:
         raise ValueError(
-            "Unsupported observables to diagonalize must be a subset of "
-            f"[PauliX, PauliY, Hadamard] but received {list(bad_obs_input)}"
+            "Supported base observables must be a subset of [PauliX, PauliY, PauliZ, Hadamard] "
+            f"but received {list(bad_obs_input)}"
         )
 
     diagonalization_wires = list(tape.wires)
@@ -33,7 +35,7 @@ def diagonalize_tape_measurements(tape, unsupported_obs=_default_unsupported_obs
     for m in tape.measurements:
         if m.obs:
             wires = [w for w in m.obs.wires if w in diagonalization_wires]
-            gates, new_obs = diagonalize_observable(m.obs, wires, unsupported_obs)
+            gates, new_obs = diagonalize_observable(m.obs, wires, supported_base_obs)
             if gates:
                 diagonalizing_gates.extend(gates)
                 for w in wires:
@@ -57,11 +59,11 @@ def diagonalize_tape_measurements(tape, unsupported_obs=_default_unsupported_obs
         trainable_params=tape.trainable_params,
     )
 
-    return new_tape, null_postprocessing
+    return (new_tape,), null_postprocessing
 
 
 def diagonalize_observable(
-    observable, diagonalization_wires, unsupported_obs=_default_unsupported_obs
+    observable, diagonalization_wires, supported_base_obs=_default_supported_obs
 ):
     """takes an observable and changes all unsupported obs to the measurement
     basis. Applies diagonalizing gates if the observable being diagonalized is
@@ -71,7 +73,7 @@ def diagonalize_observable(
         return [], observable
 
     if isinstance(observable, (qml.X, qml.Y, qml.Hadamard)):
-        switch_basis = observable.name in unsupported_obs
+        switch_basis = observable.name not in supported_base_obs
         diagonalize = switch_basis and observable.wires[0] in diagonalization_wires
 
         new_obs = qml.Z(wires=observable.wires) if switch_basis else observable
@@ -80,11 +82,11 @@ def diagonalize_observable(
         return diagonalizing_gates, new_obs
 
     return _diagonalize_complex_observable(
-        observable, diagonalization_wires, unsupported_obs=unsupported_obs
+        observable, diagonalization_wires, supported_base_obs=supported_base_obs
     )
 
 
-def get_obs_and_gates(obs_list, diagonalization_wires, unsupported_obs=_default_unsupported_obs):
+def get_obs_and_gates(obs_list, diagonalization_wires, supported_base_obs=_default_supported_obs):
     """Calls diagonalize_observables on each observable in a list. After each observable is diagonalized,
     if diagonalizing gates were added, its wire is removed from the list of wires still needing
     diagonalization. This prevents diagonalizing twice on the same wire for overlapping measurements, like
@@ -94,7 +96,7 @@ def get_obs_and_gates(obs_list, diagonalization_wires, unsupported_obs=_default_
     diagonalizing_gates = []
 
     for o in obs_list:
-        gates, obs = diagonalize_observable(o, diagonalization_wires, unsupported_obs)
+        gates, obs = diagonalize_observable(o, diagonalization_wires, supported_base_obs)
         if gates:
             diagonalizing_gates.extend(gates)
             for w in obs.wires:
@@ -106,7 +108,7 @@ def get_obs_and_gates(obs_list, diagonalization_wires, unsupported_obs=_default_
 
 @singledispatch
 def _diagonalize_complex_observable(
-    observable, diagonalization_wires, unsupported_obs=_default_unsupported_obs
+    observable, diagonalization_wires, supported_base_obs=_default_supported_obs
 ):
     """takes an observable consisting of multiple other observables, and changes all
     unsupported obs to the measurement basis. Applies diagonalizing gates unless the
@@ -119,10 +121,10 @@ def _diagonalize_complex_observable(
 
 @_diagonalize_complex_observable.register
 def _diagonalize_symbolic_op(
-    observable: SymbolicOp, diagonalization_wires, unsupported_obs=_default_unsupported_obs
+    observable: SymbolicOp, diagonalization_wires, supported_base_obs=_default_supported_obs
 ):
     diagonalizing_gates, new_base = diagonalize_observable(
-        observable.base, diagonalization_wires, unsupported_obs
+        observable.base, diagonalization_wires, supported_base_obs
     )
 
     new_observable = copy(observable)
@@ -133,10 +135,10 @@ def _diagonalize_symbolic_op(
 
 @_diagonalize_complex_observable.register
 def _diagonalize_tensor(
-    observable: Tensor, diagonalization_wires, unsupported_obs=_default_unsupported_obs
+    observable: Tensor, diagonalization_wires, supported_base_obs=_default_supported_obs
 ):
     diagonalizing_gates, new_obs = get_obs_and_gates(
-        observable.obs, diagonalization_wires, unsupported_obs
+        observable.obs, diagonalization_wires, supported_base_obs
     )
 
     new_observable = copy(observable)
@@ -149,10 +151,10 @@ def _diagonalize_tensor(
 def _diagonalize_hamiltonian(
     observable: qml.ops.Hamiltonian,
     diagonalization_wires,
-    unsupported_obs=_default_unsupported_obs,
+    supported_base_obs=_default_supported_obs,
 ):
     diagonalizing_gates, new_ops = get_obs_and_gates(
-        observable.ops, diagonalization_wires, unsupported_obs
+        observable.ops, diagonalization_wires, supported_base_obs
     )
 
     new_observable = copy(observable)
@@ -163,10 +165,10 @@ def _diagonalize_hamiltonian(
 
 @_diagonalize_complex_observable.register
 def _diagonalize_composite_op(
-    observable: CompositeOp, diagonalization_wires, unsupported_obs=_default_unsupported_obs
+    observable: CompositeOp, diagonalization_wires, supported_base_obs=_default_supported_obs
 ):
     diagonalizing_gates, new_operands = get_obs_and_gates(
-        observable.operands, diagonalization_wires, unsupported_obs
+        observable.operands, diagonalization_wires, supported_base_obs
     )
 
     new_observable = copy(observable)
