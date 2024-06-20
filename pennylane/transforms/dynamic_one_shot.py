d751ab98cda7c466804e4325082459b133716800
Mudit Pandey <mudit.pandey@xanadu.ai>
2024-06-20 15:24:23 -0400

    Minor changes to work with new changes coming to Catalyst

--- pennylane/transforms/dynamic_one_shot.py
+++ pennylane/transforms/dynamic_one_shot.py
@@ -209,10 +209,13 @@ def init_auxiliary_tape(circuit: qml.tape.QuantumScript):
             else:
                 new_measurements.append(m)
     for op in circuit.operations:
-        if "MidCircuitMeasure" in str(type(op)):  # pragma: no cover
-            new_measurements.append(qml.sample(op.out_classical_tracers[0]))
-        elif isinstance(op, MidMeasureMP):
-            new_measurements.append(qml.sample(MeasurementValue([op], lambda res: res)))
+        if isinstance(op, MidMeasureMP):
+            mv = (
+                op.mcm_tracer
+                if "MidCircuitMeasure" in str(type(op))
+                else MeasurementValue([op], lambda res: res)
+            )
+            new_measurements.append(qml.sample(op=mv))
     return qml.tape.QuantumScript(
         circuit.operations,
         new_measurements,
@@ -323,7 +326,7 @@ def gather_mcm_qjit(measurement, samples, is_valid):  # pragma: no cover
     """
     found, meas = False, None
     for k, meas in samples.items():
-        if measurement.mv is k.out_classical_tracers[0]:
+        if measurement.mv is k.mcm_tracer:
             found = True
             break
     if not found:
