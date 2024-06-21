33b946cee271499e0de19e94f9936419b94ad9fd
albi3ro <chrissie.c.l@gmail.com>
2024-06-21 13:43:59 -0400

    lightning interpreter

--- pennylane/capture/interpreters.py
+++ pennylane/capture/interpreters.py
@@ -100,6 +100,23 @@ class DefaultQubitInterpreter(PlxprInterpreter):
     def interpret_measurement(self, m):
         return measure(m, self._state)
 
+from pennylane_lightning.lightning_qubit._state_vector import LightningStateVector, LightningMeasurements
+
+class LightningInterpreter(PlxprInterpreter):
+
+    def __init__(self, num_wires):
+        self._num_wires = num_wires
+
+    def setup(self):
+        self._state = LightningStateVector(self._num_wires)
+
+    def interpret_operation(self, op):
+        self._state._apply_lightning([op])
+
+    def interpret_measurement(self, m):
+        return LightningMeasurements(self._state).measurement(m)
+
+
 
 class DecompositionInterpreter(PlxprInterpreter):
     """
