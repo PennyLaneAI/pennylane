48ec33f3c93586a116f5712a1aa84b3d862a289c
lillian542 <Lillian.frederiksen@xanadu.ai>
2024-07-04 17:02:54 -0400

    alternate solution

--- pennylane/ops/op_math/controlled_ops.py
+++ pennylane/ops/op_math/controlled_ops.py
@@ -801,6 +801,33 @@ class CNOT(ControlledOp):
     def _controlled(self, wire):
         return qml.Toffoli(wires=wire + self.wires)
 
+    @property
+    def has_decomposition(self):
+        return False
+
+    @staticmethod
+    def compute_decomposition(*params, wires=None, **hyperparameters):  # -> List["Operator"]:
+        r"""Representation of the operator as a product of other operators (static method).
+
+        .. math:: O = O_1 O_2 \dots O_n.
+
+        .. note::
+
+            Operations making up the decomposition should be queued within the
+            ``compute_decomposition`` method.
+
+        .. seealso:: :meth:`~.Operator.decomposition`.
+
+        Args:
+            *params (list): trainable parameters of the operator, as stored in the ``parameters`` attribute
+            wires (Iterable[Any], Wires): wires that the operator acts on
+            **hyperparams (dict): non-trainable hyperparameters of the operator, as stored in the ``hyperparameters`` attribute
+
+        Returns:
+            list[Operator]: decomposition of the operator
+        """
+        raise qml.operation.DecompositionUndefinedError
+
 
 class Toffoli(ControlledOp):
     r"""Toffoli(wires)
