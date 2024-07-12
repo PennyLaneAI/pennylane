36b9578d3175bf01753c8f443c10fa2a1fa2e22b
Austin Huang <gengminghuang@gmail.com>
2024-07-12 13:47:36 -0400

    black

--- pennylane/registers.py
+++ pennylane/registers.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 """
 This module contains the :func:`registers` function, which builds registers for a given dictionary
-of registers. Registers are a dictionary of :class:`~.Wires` objects, where the key is the name of 
+of registers. Registers are a dictionary of :class:`~.Wires` objects, where the key is the name of
 the register and the value is the ``Wires`` object.
 """
 
