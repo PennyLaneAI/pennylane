74c638a9b176ae3303062b0729ae23882a5eef68
Josh Izaac <josh146@gmail.com>
2024-07-17 10:28:14 -0400

    Update doc/conf.py
    Co-authored-by: Rashid N H M <95639609+rashidnhm@users.noreply.github.com>

--- doc/conf.py
+++ doc/conf.py
@@ -64,7 +64,7 @@ html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
 
 # Tell Jinja2 templates the build is running on Read the Docs
 if os.environ.get("READTHEDOCS", "") == "True":
-    html_context["READTHEDOCS"] = True
+    html_context = {"READTHEDOCS": True}
 
 os.environ["SPHINX_BUILD"] = "1"
 
