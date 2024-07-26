8d48d9ed10fbe84c9feab8e39a5c5e501d515402
albi3ro <chrissie.c.l@gmail.com>
2024-07-26 10:33:12 -0400

    black

--- pennylane/workflow/qnode.py
+++ pennylane/workflow/qnode.py
@@ -883,7 +883,6 @@ class QNode:
                         "The switching of devices for backpropagation is now deprecated in v0.38 and "
                         "will be removed in v0.39, as this behavior was developed purely for the "
                         "deprecated default.qubit.legacy.",
-
                         qml.PennyLaneDeprecationWarning,
                     )
 
