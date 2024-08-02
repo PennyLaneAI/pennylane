d9712b710397895190b6403989f35d8d99f14a73
Ahmed Darwish <exclass9.24@gmail.com>
2024-08-02 11:37:51 -0400

    Update pennylane/workflow/jacobian_products.py
    Co-authored-by: Christina Lee <christina@xanadu.ai>

--- pennylane/workflow/jacobian_products.py
+++ pennylane/workflow/jacobian_products.py
@@ -398,7 +398,7 @@ class DeviceDerivatives(JacobianProductCalculator):
     def __init__(
         self,
         device: Union["qml.devices.Device", "qml.Device"],
-        execution_config: Optional["qml.devices.ExecutionConfig"] = None,
+        execution_config: qml.devices.ExecutionConfig = qml.devices.DefaultExecutionConfig,
         gradient_kwargs: dict = None,
     ):
         if execution_config is None:
