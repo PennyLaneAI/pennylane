4ade58df8ac480479ca8da5694331a7ae53e993b
Jay Soni <jbsoni@uwaterloo.ca>
2024-08-05 12:13:24 -0400

    Moved the ResourceOperation to the Operation folder to resolve recurr… (#6070)
    **Context:** 
    This resolves some of the recursive import errors encountered during
    prototyping

--- pennylane/operation.py
+++ pennylane/operation.py
@@ -2593,6 +2593,47 @@ class Tensor(Observable):
 
 
 # =============================================================================
+# Base Resource class
+# =============================================================================
+
+
+class ResourcesOperation(Operation):
+    r"""Base class that represents quantum gates or channels applied to quantum
+    states and stores the resource requirements of the quantum gate.
+
+    .. note::
+        Child classes must implement the :func:`~.ResourcesOperation.resources` method which computes
+        the resource requirements of the operation.
+    """
+
+    @abc.abstractmethod
+    def resources(self):
+        r"""Compute the resources required for this operation.
+
+        Returns:
+            ~.Resources: The resources required by this operation.
+
+        **Examples**
+
+        >>> class CustomOp(ResourcesOperation):
+        ...     num_wires = 2
+        ...     def resources(self):
+        ...         return Resources(num_wires=self.num_wires, num_gates=3, depth=2)
+        ...
+        >>> op = CustomOp(wires=[0, 1])
+        >>> print(op.resources())
+        wires: 2
+        gates: 3
+        depth: 2
+        shots: Shots(total=None)
+        gate_types:
+        {}
+        gate_sizes:
+        {}
+        """
+
+
+# =============================================================================
 # CV Operations and observables
 # =============================================================================
 
