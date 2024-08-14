22cf67280b398f7664846f9872acecb1d302bbdf
Ahmed Darwish <exclass9.24@gmail.com>
2024-08-14 11:27:14 -0400

    Apply suggestions from code review
    Co-authored-by: Christina Lee <christina@xanadu.ai>

--- pennylane/pauli/grouping/group_observables.py
+++ pennylane/pauli/grouping/group_observables.py
@@ -184,7 +184,7 @@ class PauliGroupingStrategy:  # pylint: disable=too-many-instance-attributes
 
 
 def group_observables(
-    observables: list["qml.operation.Observable"],
+    observables: list["qml.operation.Operator"],
     coefficients: Optional[TensorLike] = None,
     grouping_type: Literal["qwc", "commuting", "anticommuting"] = "qwc",
     method: Literal["lf", "rlf"] = "rlf",
