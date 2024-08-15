41924c3701e830766ab35c8cd18b9730cc5541ee
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2024-08-15 12:38:49 +0200

    Update quantum.py

--- pennylane/math/quantum.py
+++ pennylane/math/quantum.py
@@ -18,8 +18,8 @@ import functools
 import itertools
 from string import ascii_letters as ABC
 
-import autoray.numpy as np
-from numpy import float64
+from autoray import numpy as np
+from numpy import float64 # pylint:disable=wrong-import-order
 
 import pennylane as qml
 
