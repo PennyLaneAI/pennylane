88d97b43566ddfd5262edbb6c9f5b6001a3378a0
Shiro-Raven <exclass9.24@gmail.com>
2024-08-16 16:37:19 -0400

    typo fix

--- pennylane/devices/__init__.py
+++ pennylane/devices/__init__.py
@@ -37,7 +37,7 @@ to verify and test quantum gradient computations.
     default_qutrit_mixed
     default_clifford
     default_tensor
-    _device
+    _legacy_device
     _qubit_device
     _qutrit_device
     null_qubit
