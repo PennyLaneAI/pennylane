8d7c5e0cac3929b62ed8a8a4b80effd54429e682
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2024-08-19 14:03:11 -0400

    Update test_templates.py

--- pennylane/devices/tests/test_templates.py
+++ pennylane/devices/tests/test_templates.py
@@ -225,15 +225,6 @@ class TestTemplates:  # pylint:disable=too-many-public-methods
             [math.fidelity_statevector(circuit(), exp_state)], [1.0], atol=tol(dev.shots)
         )
 
-    @pytest.fixture(scope="function", autouse=False)
-    def capture_warnings(self, recwarn):
-        """Capture warnings."""
-        yield
-        if len(recwarn) > 0:
-            for w in recwarn:
-                assert isinstance(w.message, qml.PennyLaneDeprecationWarning)
-                assert "BasisStatePreparation is deprecated" in str(w.message)
-
     def test_BasisStatePreparation(self, device, tol):
         """Test the BasisStatePreparation template."""
         dev = device(4)
