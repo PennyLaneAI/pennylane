66e4fed83ff9c74e7739a77fc9161cd12847fe8c
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2024-08-21 18:49:36 -0400

    Update test_out_multiplier.py

--- tests/templates/test_subroutines/test_out_multiplier.py
+++ tests/templates/test_subroutines/test_out_multiplier.py
@@ -19,6 +19,7 @@ import pytest
 
 import pennylane as qml
 from pennylane import numpy as np
+from pennylane.templates.subroutines.out_multiplier import OutMultiplier
 
 
 def test_standard_validity_OutMultiplier():
@@ -28,11 +29,11 @@ def test_standard_validity_OutMultiplier():
     y_wires = [2, 3, 4]
     output_wires = [6, 7, 8, 9]
     work_wires = [5, 10]
-    op = qml.OutMultiplier(x_wires, y_wires, output_wires, mod, work_wires)
+    op = OutMultiplier(x_wires, y_wires, output_wires, mod, work_wires)
     qml.ops.functions.assert_valid(op)
 
 
-class TestMultiplier:
+class TestOutMultiplier:
     """Test the qml.OutMultiplier template."""
 
     @pytest.mark.parametrize(
@@ -66,6 +67,20 @@ class TestMultiplier:
                 22,
                 [11, 12],
             ),
+            (
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                None,
+                [9, 10],
+            ),
+            (
+                [0, 1],
+                [3, 4, 5],
+                [6, 7, 8],
+                None,
+                None,
+            ),
         ],
     )
     def test_operation_result(
@@ -78,10 +93,10 @@ class TestMultiplier:
         def circuit(x, y):
             qml.BasisEmbedding(x, wires=x_wires)
             qml.BasisEmbedding(y, wires=y_wires)
-            qml.OutMultiplier(x_wires, y_wires, output_wires, mod, work_wires)
+            OutMultiplier(x_wires, y_wires, output_wires, mod, work_wires)
             return qml.sample(wires=output_wires)
 
-        if mod == None:
+        if mod is None:
             max = 2 ** len(output_wires)
         else:
             max = mod
@@ -89,72 +104,6 @@ class TestMultiplier:
             assert np.allclose(
                 sum(bit * (2**i) for i, bit in enumerate(reversed(circuit(x, y)))), (x * y) % max
             )
-
-    @pytest.mark.parametrize(
-        ("x_wires", "y_wires", "output_wires", "mod", "work_wires"),
-        [
-            (
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                None,
-                [9, 10],
-            ),
-            (
-                [0, 1],
-                [3, 4, 5],
-                [6, 7, 8],
-                6,
-                None,
-            ),
-            (
-                [0, 1],
-                [3, 4, 5],
-                [6, 7, 8],
-                None,
-                None,
-            ),
-        ],
-    )
-    def test_operation_result_args_None(
-        self, x_wires, y_wires, output_wires, mod, work_wires
-    ):  # pylint: disable=too-many-arguments
-        """Test the correctness of the OutMultiplier template output."""
-        dev = qml.device("default.qubit", shots=1)
-
-        @qml.qnode(dev)
-        def circuit(x, y):
-            qml.BasisEmbedding(x, wires=x_wires)
-            qml.BasisEmbedding(y, wires=y_wires)
-            qml.OutMultiplier(x_wires, y_wires, output_wires, mod, work_wires)
-            return qml.sample(wires=output_wires)
-
-        if mod == None:
-            max = 2 ** len(output_wires)
-        else:
-            max = mod
-        for x, y in zip(range(len(x_wires)), range(len(y_wires))):
-            assert np.allclose(
-                sum(bit * (2**i) for i, bit in enumerate(reversed(circuit(x, y)))), (x * y) % max
-            )
-
-    @pytest.mark.parametrize(
-        ("x_wires", "y_wires", "output_wires", "mod", "work_wires", "msg_match"),
-        [
-            (
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                9,
-                [9, 10],
-                "OutMultiplier must have at least enough wires to represent mod.",
-            ),
-        ],
-    )
-    def test_operation_error(self, x_wires, y_wires, output_wires, mod, work_wires, msg_match):
-        """Test an error is raised when k or mod don't meet the requirements"""
-        with pytest.raises(ValueError, match=msg_match):
-            qml.OutMultiplier(x_wires, y_wires, output_wires, mod, work_wires)
 
     @pytest.mark.parametrize(
         ("x_wires", "y_wires", "output_wires", "mod", "work_wires", "msg_match"),
@@ -199,53 +148,43 @@ class TestMultiplier:
                 [9, 10],
                 "None of the wires in x_wires should be included in output_wires.",
             ),
+            (
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                9,
+                [9, 10],
+                "OutMultiplier must have at least enough wires to represent mod.",
+            ),
+            (
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                9,
+                None,
+                "If mod is not",
+            ),
         ],
     )
     def test_wires_error(self, x_wires, y_wires, output_wires, mod, work_wires, msg_match):
         """Test an error is raised when some work_wires don't meet the requirements"""
         with pytest.raises(ValueError, match=msg_match):
-            qml.OutMultiplier(x_wires, y_wires, output_wires, mod, work_wires)
+            OutMultiplier(x_wires, y_wires, output_wires, mod, work_wires)
 
-    @pytest.mark.parametrize(
-        ("x_wires", "y_wires", "output_wires", "mod", "work_wires"),
-        [
-            (
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                None,
-                [9, 10],
-            ),
-            (
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                7,
-                [9, 10],
-            ),
-            (
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                7,
-                [9, 10, 11],
-            ),
-            (
-                [0, 1, 2],
-                [3, 5],
-                [6, 8],
-                3,
-                [9, 10],
-            ),
-        ],
-    )
-    def test_decomposition(self, x_wires, y_wires, output_wires, mod, work_wires):
+    def test_decomposition(self):
         """Test that compute_decomposition and decomposition work as expected."""
+        x_wires, y_wires, output_wires, mod, work_wires = (
+            [0, 1, 2],
+            [3, 5],
+            [6, 8],
+            3,
+            [9, 10],
+        )
         multiplier_decomposition = OutMultiplier(
             x_wires, y_wires, output_wires, mod, work_wires
         ).compute_decomposition(x_wires, y_wires, output_wires, mod, work_wires)
         op_list = []
-        if mod != 2 ** (len(output_wires)):
+        if mod != 2 ** len(output_wires):
             qft_output_wires = work_wires[:1] + output_wires
         else:
             qft_output_wires = output_wires
@@ -253,7 +192,7 @@ class TestMultiplier:
         op_list.append(
             qml.ControlledSequence(
                 qml.ControlledSequence(
-                    PhaseAdder(1, output_wires, mod, work_wires), control=x_wires
+                    qml.PhaseAdder(1, qft_output_wires, mod, work_wires[1:]), control=x_wires
                 ),
                 control=y_wires,
             )
@@ -263,7 +202,7 @@ class TestMultiplier:
         for op1, op2 in zip(multiplier_decomposition, op_list):
             qml.assert_equal(op1, op2)
 
-    # @pytest.mark.jax
+    @pytest.mark.jax
     def test_jit_compatible(self):
         """Test that the template is compatible with the JIT compiler."""
 
@@ -272,10 +211,8 @@ class TestMultiplier:
         jax.config.update("jax_enable_x64", True)
 
         x, y = 2, 3
-
-        # x, y in binary
-        x_list = [0, 1, 0]
-        y_list = [0, 1, 1]
+        x_list = [1, 0]
+        y_list = [1, 1]
         mod = 12
         x_wires = [0, 1]
         y_wires = [2, 3]
@@ -286,9 +223,9 @@ class TestMultiplier:
         @jax.jit
         @qml.qnode(dev)
         def circuit():
-            qml.BasisEmbedding(x, wires=x_wires)
-            qml.BasisEmbedding(y, wires=y_wires)
-            qml.OutMultiplier(x_wires, y_wires, output_wires, mod, work_wires)
+            qml.BasisEmbedding(x_list, wires=x_wires)
+            qml.BasisEmbedding(y_list, wires=y_wires)
+            OutMultiplier(x_wires, y_wires, output_wires, mod, work_wires)
             return qml.sample(wires=output_wires)
 
         assert jax.numpy.allclose(
