c6a3af6a8864ef24ef0a0bf1c81ee399eb4bbd84
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2024-08-21 18:34:27 -0400

    adder

--- pennylane/templates/subroutines/adder.py
+++ pennylane/templates/subroutines/adder.py
@@ -26,7 +26,7 @@ class Adder(Operation):
 
     .. math::
 
-        \text{Adder}(k,mod) |x \rangle = | x+k \, \text{mod} \, mod \rangle,
+        \text{Adder}(k,mod) |x \rangle = | x+k \quad (\text{mod}) \rangle,
 
     The decomposition of this operator is based on the QFT-based method presented in `arXiv:2311.08555 <https://arxiv.org/abs/2311.08555>`_.
 
@@ -45,8 +45,10 @@ class Adder(Operation):
         x = 8
         k = 5
         mod = 15
+
         x_wires =[0,1,2,3]
         work_wires=[4,5]
+
         dev = qml.device("default.qubit", shots=1)
         @qml.qnode(dev)
         def adder_modulo(x, k, mod, x_wires, work_wires):
@@ -57,7 +59,7 @@ class Adder(Operation):
     .. code-block:: pycon
 
         >>> adder_modulo(x, k, mod,x_wires, work_wire)
-        [1 1 0 1]
+            [1 1 0 1]
 
     The result [1 1 0 1] is the ket representation of :math:`8 + 5  \, \text{mod} \, 15 = 13`.
     """
@@ -68,20 +70,66 @@ class Adder(Operation):
         self, k, x_wires, mod=None, work_wires=None, id=None
     ):  # pylint: disable=too-many-arguments
 
+        x_wires = qml.wires.Wires(x_wires)
+        if mod is None:
+            mod = 2 ** len(x_wires)
+        elif work_wires is None and mod != 2 ** len(x_wires):
+            raise ValueError(f"If mod is not 2^{len(x_wires)} you should provide two work_wire")
         if work_wires is not None:
             if any(wire in work_wires for wire in x_wires):
                 raise ValueError("None wire in work_wires should be included in x_wires.")
+        if mod > 2 ** len(x_wires):
+            raise ValueError("Adder must have at least enough x_wires to represent mod.")
 
         self.hyperparameters["k"] = k
         self.hyperparameters["mod"] = mod
         self.hyperparameters["work_wires"] = qml.wires.Wires(work_wires)
-        self.hyperparameters["x_wires"] = qml.wires.Wires(x_wires)
+        self.hyperparameters["x_wires"] = x_wires
         all_wires = qml.wires.Wires(x_wires) + qml.wires.Wires(work_wires)
         super().__init__(wires=all_wires, id=id)
 
     @property
     def num_params(self):
         return 0
+
+    def _flatten(self):
+        metadata = tuple((key, value) for key, value in self.hyperparameters.items())
+        return tuple(), metadata
+
+    @classmethod
+    def _unflatten(cls, data, metadata):
+        hyperparams_dict = dict(metadata)
+        return cls(**hyperparams_dict)
+
+    def map_wires(self, wire_map: dict):
+        new_dict = {
+            key: [wire_map.get(w, w) for w in self.hyperparameters[key]]
+            for key in ["x_wires", "work_wires"]
+        }
+
+        return Adder(
+            self.hyperparameters["k"],
+            new_dict["x_wires"],
+            self.hyperparameters["mod"],
+            new_dict["work_wires"],
+        )
+
+    @property
+    def x_wires(self):
+        """The wires where the operator is applied."""
+        return self.hyperparameters["x_wires"]
+
+    @property
+    def work_wires(self):
+        """The work_wires."""
+        return self.hyperparameters["work_wires"]
+
+    def decomposition(self):  # pylint: disable=arguments-differ
+        return self.compute_decomposition(**self.hyperparameters)
+
+    @classmethod
+    def _primitive_bind_call(cls, *args, **kwargs):
+        return cls._primitive.bind(*args, **kwargs)
 
     @staticmethod
     def compute_decomposition(k, x_wires, mod, work_wires):  # pylint: disable=arguments-differ
@@ -96,18 +144,18 @@ class Adder(Operation):
 
         **Example**
 
-        >>> qml.Adder.compute_decomposition(k=2,x_wires=[0,1,2], mod = 8, work_wire=None)
+        >>> qml.Adder.compute_decomposition(k=2,x_wires=[0,1,2], mod = 8, work_wires=None)
         [QFT(wires=[0, 1, 2]),
         PhaseAdder(wires=[0, 1, 2]),
         Adjoint(QFT(wires=[0, 1, 2]))]
         """
         op_list = []
-        if mod == 2 ** (len(x_wires)):
+        if mod == 2 ** len(x_wires):
             qft_wires = x_wires
         else:
             qft_wires = work_wires[:1] + x_wires
         op_list.append(qml.QFT(qft_wires))
-        op_list.append(qml.PhaseAdder(k, x_wires, mod, work_wires))
+        op_list.append(qml.PhaseAdder(k, qft_wires, mod, work_wires[1:]))
         op_list.append(qml.adjoint(qml.QFT)(qft_wires))
 
         return op_list
