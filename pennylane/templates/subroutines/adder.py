09597b4e168ed77803eb5e4eb9a5352dae963ca2
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2024-08-21 18:43:55 -0400

    adder.py

--- pennylane/templates/subroutines/adder.py
+++ pennylane/templates/subroutines/adder.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2024 Xanadu Quantum Technologies Inc.
+# Copyright 2024 Xanadu Quantum Technologies Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
 Contains the Adder template.
 """
 
-import numpy as np
 import pennylane as qml
 from pennylane.operation import Operation
 
@@ -26,88 +25,137 @@ class Adder(Operation):
     This operator adds the integer :math:`k` modulo :math:`mod` in the computational basis:
 
     .. math::
-        Adder(k,mod) |m \rangle = | m+k mod mod \rangle,
 
-    The quantum circuit that represents the Adder operator is:
-    TO DO
+        \text{Adder}(k,mod) |x \rangle = | x+k \quad (\text{mod}) \rangle,
+
+    The decomposition of this operator is based on the QFT-based method presented in `arXiv:2311.08555 <https://arxiv.org/abs/2311.08555>`_.
 
     Args:
-        k (int): number that wants to be added
-        wires (Sequence[int]): the wires the operation acts on. There are needed at least enough wires to represent k and mod.
-        mod (int): modulo with respect to which the sum is performed, default value will be ``2^len(wires)``
-        work_wires (Sequence[int]): the auxiliary wires to use for the sum modulo :math:`mod` when :math:`mod \neq 2^{\textrm{len(wires)}}`
+        k (int): the number that needs to be added
+        x_wires (Sequence[int]): the wires the operation acts on
+        mod (int): modulo with respect to which the sum is performed, default value will be ``2^len(wires)``.
+        work_wires (Sequence[int]): the auxiliary wires to use for the sum modulo :math:`mod` when :math:`mod \neq 2^{\text{len(x_wires)}}`
 
     **Example**
 
-    Sum of two integers :math:`m=8` and :math:`k=5` modulo :math:`mod=15`. Note that to perform this sum using qml.Adder we need that :math:`m,k < mod`
+    This example computes the sum of two integers :math:`x=8` and :math:`k=5` modulo :math:`mod=15`. Note that to perform this sum using qml.Adder, when :math:`mod \neq \text{len(x_wires)}` we need that :math:`x < \text{len(x_wires)}/2`.
 
     .. code-block::
-        m = 8
+
+        x = 8
         k = 5
         mod = 15
-        wires_m =[0,1,2,3]
+
+        x_wires =[0,1,2,3]
         work_wires=[4,5]
+
         dev = qml.device("default.qubit", shots=1)
         @qml.qnode(dev)
-        def adder_modulo(m, k, mod, wires_m, work_wires):
-            # Function that performs m + k modulo mod in the computational basis
-            qml.BasisEmbedding(m, wires=wires_m)
-            qml.Adder(k, wires_m, mod, work_wires)
-            return qml.sample(wires=wires_m)
+        def adder_modulo(x, k, mod, x_wires, work_wires):
+            qml.BasisEmbedding(x, wires=x_wires)
+            qml.Adder(k, x_wires, mod, work_wire)
+            return qml.sample(wires=x_wires)
 
     .. code-block:: pycon
 
-        >>> print(f"The ket representation of {m} + {k} mod {mod} is {adder_modulo(m, k, mod,wires_m, work_wires)}")
-        The ket representation of 8 + 5 mod 15 is [1 1 0 1]
+        >>> adder_modulo(x, k, mod,x_wires, work_wire)
+            [1 1 0 1]
 
-    We can see that the result [1 1 0 1] corresponds to 13, which comes from :math:`8+5=13 \longrightarrow 13 mod 15 = 13`.
+    The result [1 1 0 1] is the ket representation of :math:`8 + 5  \, \text{mod} \, 15 = 13`.
     """
 
     grad_method = None
 
-    def __init__(self, k, wires, mod=None, work_wires=None, id=None):
-        if mod == None:
-            mod = 2 ** (len(wires))
-        if work_wires != None:
-            if any(wire in work_wires for wire in wires):
-                raise ValueError("Any wire in work_wires should not be included in wires.")
-        else:
-            work_wires = [wires[-1] + 1, wires[-1] + 2]
+    def __init__(
+        self, k, x_wires, mod=None, work_wires=None, id=None
+    ):  # pylint: disable=too-many-arguments
+
+        x_wires = qml.wires.Wires(x_wires)
+        if mod is None:
+            mod = 2 ** len(x_wires)
+        elif work_wires is None and mod != 2 ** len(x_wires):
+            raise ValueError(f"If mod is not 2^{len(x_wires)} you should provide two work_wire")
+        if work_wires is not None:
+            if any(wire in work_wires for wire in x_wires):
+                raise ValueError("None wire in work_wires should be included in x_wires.")
+        if mod > 2 ** len(x_wires):
+            raise ValueError("Adder must have at least enough x_wires to represent mod.")
+
         self.hyperparameters["k"] = k
         self.hyperparameters["mod"] = mod
         self.hyperparameters["work_wires"] = qml.wires.Wires(work_wires)
-        super().__init__(wires=wires, id=id)
+        self.hyperparameters["x_wires"] = x_wires
+        all_wires = qml.wires.Wires(x_wires) + qml.wires.Wires(work_wires)
+        super().__init__(wires=all_wires, id=id)
 
     @property
     def num_params(self):
         return 0
 
+    def _flatten(self):
+        metadata = tuple((key, value) for key, value in self.hyperparameters.items())
+        return tuple(), metadata
+
+    @classmethod
+    def _unflatten(cls, data, metadata):
+        hyperparams_dict = dict(metadata)
+        return cls(**hyperparams_dict)
+
+    def map_wires(self, wire_map: dict):
+        new_dict = {
+            key: [wire_map.get(w, w) for w in self.hyperparameters[key]]
+            for key in ["x_wires", "work_wires"]
+        }
+
+        return Adder(
+            self.hyperparameters["k"],
+            new_dict["x_wires"],
+            self.hyperparameters["mod"],
+            new_dict["work_wires"],
+        )
+
+    @property
+    def x_wires(self):
+        """The wires where the operator is applied."""
+        return self.hyperparameters["x_wires"]
+
+    @property
+    def work_wires(self):
+        """The work_wires."""
+        return self.hyperparameters["work_wires"]
+
+    def decomposition(self):  # pylint: disable=arguments-differ
+        return self.compute_decomposition(**self.hyperparameters)
+
+    @classmethod
+    def _primitive_bind_call(cls, *args, **kwargs):
+        return cls._primitive.bind(*args, **kwargs)
+
     @staticmethod
-    def compute_decomposition(k, mod, work_wires, wires):
+    def compute_decomposition(k, x_wires, mod, work_wires):  # pylint: disable=arguments-differ
         r"""Representation of the operator as a product of other operators.
         Args:
             k (int): number that wants to be added
+            x_wires (Sequence[int]): the wires the operation acts on. There are needed at least enough wires to represent mod.
             mod (int): modulo of the sum
-            work_wires (Sequence[int]): the auxiliary wires to use for the sum modulo :math:`mod` when :math:`mod \neq 2^{\textrm{len(wires)}}`
-            wires (Sequence[int]): the wires the operation acts on
+            work_wires (Sequence[int]): the auxiliary wires to use for the sum modulo :math:`mod` when :math:`mod \neq 2^{\textrm{len(x_wires)}}`
         Returns:
             list[.Operator]: Decomposition of the operator
 
         **Example**
 
-        >>> qml.Adder.compute_decomposition(k=2,mod=8,wires=[0,1,2],work_wires=[3,4])
+        >>> qml.Adder.compute_decomposition(k=2,x_wires=[0,1,2], mod = 8, work_wires=None)
         [QFT(wires=[0, 1, 2]),
         PhaseAdder(wires=[0, 1, 2]),
         Adjoint(QFT(wires=[0, 1, 2]))]
         """
         op_list = []
-        if mod == 2 ** (len(wires)):
-            qft_wires = wires
+        if mod == 2 ** len(x_wires):
+            qft_wires = x_wires
         else:
-            qft_wires = work_wires[:1] + wires
-        # we perform m+k modulo mod
+            qft_wires = work_wires[:1] + x_wires
         op_list.append(qml.QFT(qft_wires))
-        op_list.append(qml.PhaseAdder(k, wires, mod, work_wires))
+        op_list.append(qml.PhaseAdder(k, qft_wires, mod, work_wires[1:]))
         op_list.append(qml.adjoint(qml.QFT)(qft_wires))
 
         return op_list
