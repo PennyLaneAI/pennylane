a502b57f4dc3c1cb7242a663fa7141b9a58400d5
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2024-08-23 07:27:09 -0400

    Update pennylane/spin/spin_hamiltonian.py
    Co-authored-by: Utkarsh <utkarshazad98@gmail.com>

--- pennylane/spin/spin_hamiltonian.py
+++ pennylane/spin/spin_hamiltonian.py
@@ -241,7 +241,7 @@ def fermi_hubbard(
     if hopping.shape == (neighbour_order,):
         for edge in lattice.edges:
             for s in range(spin):
-                i, j, order = edge[0], edge[1], edge[2]
+                i, j, order = edge
                 s1 = i * spin + s
                 s2 = j * spin + s
                 hopping_term = -hopping[order] * (
