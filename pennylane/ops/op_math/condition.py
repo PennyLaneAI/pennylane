7d58f3bc7f5abf95c666fed8867fa186c942e51e
Mudit Pandey <mudit.pandey@xanadu.ai>
2024-08-28 18:05:45 -0400

    Minor fixes for 0.38 (#6163)
    Minor changes for QA for 0.38:
    
    * Trailing whitespace removed from changelog
    * Change `qml.cond`'s docs to stop using `np.array(<value>,
    requires_grad=True)` as `np` should be vanilla numpy, not autograd.
    Additionally, the use of `pennylane.numpy` does not add anything
    meaningful to the examples in the docs.
    * I also also added a note to the `qml.cond` docs about falling back to
    python control flow when `qml.qjit` is not active.
    * Fix whitespace in `LinearCombination` docs.
    * Fix formatting and type hints in docs for functions related to
    `group_observables`.
    * Add license note to `diagonalize_measurements` and wrap some stuff in
    inline code blocks in the doc string.
    * Minor doc fix to `single_qubit_fusion`.
    * Minor formatting and code correction to `split_to_single_terms`'s
    docstring
    * Minor doc fix for `qml.UCCSD`.
    * Squeeze batch dimension of inputs to a function given as input to
    `qml.fourier.coefficients`. The squeezing was being done by
    `broadcast_expand` before, which is no longer the case.

--- pennylane/ops/op_math/condition.py
+++ pennylane/ops/op_math/condition.py
@@ -347,8 +347,8 @@ def cond(
 
     .. code-block :: pycon
 
-        >>> first_par = np.array(0.3, requires_grad=True)
-        >>> sec_par = np.array(1.23, requires_grad=True)
+        >>> first_par = np.array(0.3)
+        >>> sec_par = np.array(1.23)
         >>> qnode(first_par, sec_par)
         tensor(0.32677361, requires_grad=True)
 
@@ -387,9 +387,9 @@ def cond(
             return qml.expval(qml.Z(0))
 
     >>> circuit(1.4)
-    array(0.16996714)
+    Array(0.16996714, dtype=float64)
     >>> circuit(1.6)
-    array(0.)
+    Array(0., dtype=float64)
 
     Additional 'else-if' clauses can also be included via the ``elif`` argument:
 
@@ -412,7 +412,13 @@ def cond(
             return qml.expval(qml.Z(0))
 
     >>> circuit(1.2)
-    array(0.13042371)
+    Array(0.13042371, dtype=float64)
+
+    .. note::
+
+        If the above syntax is used with a ``QNode`` that is not decorated with
+        :func:`~pennylane.qjit` and none of the predicates contain mid-circuit measurements,
+        ``qml.cond`` will fall back to using native Python ``if``-``elif``-``else`` blocks.
 
     .. details::
         :title: Usage Details
@@ -438,7 +444,7 @@ def cond(
 
         .. code-block :: pycon
 
-            >>> par = np.array(0.3, requires_grad=True)
+            >>> par = np.array(0.3)
             >>> qnode(par)
             tensor(0.3522399, requires_grad=True)
 
@@ -493,7 +499,7 @@ def cond(
 
         .. code-block :: pycon
 
-            >>> par = np.array(0.3, requires_grad=True)
+            >>> par = np.array(0.3)
             >>> qnode1(par)
             tensor(-0.1477601, requires_grad=True)
 
@@ -543,10 +549,10 @@ def cond(
 
         .. code-block :: pycon
 
-            >>> par = np.array(0.3, requires_grad=True)
-            >>> x = np.array(1.2, requires_grad=True)
-            >>> y = np.array(1.1, requires_grad=True)
-            >>> z = np.array(0.3, requires_grad=True)
+            >>> par = np.array(0.3)
+            >>> x = np.array(1.2)
+            >>> y = np.array(1.1)
+            >>> z = np.array(0.3)
             >>> qnode(par, x, y, z)
             tensor(-0.30922805, requires_grad=True)
     """
