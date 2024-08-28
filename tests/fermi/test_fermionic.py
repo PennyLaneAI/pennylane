ec0974a699fb2c8db124320f9990331e09fd9037
Will <wmaxwell90@gmail.com>
2024-08-28 14:06:05 -0400

    linting

--- tests/fermi/test_fermionic.py
+++ tests/fermi/test_fermionic.py
@@ -20,7 +20,14 @@ import pytest
 
 import pennylane as qml
 from pennylane import numpy as pnp
-from pennylane.fermi.fermionic import FermiA, FermiC, FermiSentence, FermiWord, _to_string, from_string
+from pennylane.fermi.fermionic import (
+    FermiA,
+    FermiC,
+    FermiSentence,
+    FermiWord,
+    _to_string,
+    from_string,
+)
 
 # pylint: disable=too-many-public-methods
 
