7d02f2a401e974fa7839de5c411659b6199ae735
Austin Huang <65315367+austingmhuang@users.noreply.github.com>
2024-08-29 11:41:44 -0400

    Update pennylane/qchem/molecule.py
    Co-authored-by: Utkarsh <utkarshazad98@gmail.com>

--- pennylane/qchem/molecule.py
+++ pennylane/qchem/molecule.py
@@ -55,7 +55,7 @@ class Molecule:
         r (array[float]): positions of the Gaussian functions
         normalize (bool): if True, the basis functions get normalized
         unit (str): unit of atomic coordinates. Available options are ``unit="bohr"`` and ``unit="angstrom"``.
-        argnum (int, list(int), None): differentiability of coords, coeffs, and alpha (in that order)
+        argnum (int | list(int) | None): index (indices) of the positional argument(s) - [``coordinates``, ``alpha``, ``coeff``] that should support differentiation. For example, ``argnums=[0, 2]`` would mean derivatives can be computed with respect to both ``coordinates`` and ``coeff``.
 
     **Example**
 
