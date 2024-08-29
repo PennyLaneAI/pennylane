be4adf944d7e334eceb7955fc98b891b1c5c9d5f
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2024-08-29 11:11:18 -0400

    Update pennylane/spin/spin_hamiltonian.py
    Co-authored-by: Utkarsh <utkarshazad98@gmail.com>

--- pennylane/spin/spin_hamiltonian.py
+++ pennylane/spin/spin_hamiltonian.py
@@ -100,7 +100,7 @@ def transverse_ising(
 
 
 def heisenberg(lattice, n_cells, coupling=None, boundary_condition=False, neighbour_order=1):
-    r"""Generates the Heisenberg Hamiltonian on a lattice.
+    r"""Generates the Hamiltonian for a Heisenberg model on a lattice.
 
     The Hamiltonian is represented as:
 
