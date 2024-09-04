f1ebc804715fb67a4d44a5ebd1853d8705f06201
Lillian <lillian542@gmail.com>
2024-09-04 10:53:11 -0400

    make process_samples on SampleMP jit-compatible

--- pennylane/math/single_dispatch.py
+++ pennylane/math/single_dispatch.py
@@ -793,7 +793,7 @@ ar.register_function(
     "jax",
     "take",
     lambda x, indices, axis=None, **kwargs: _i("jax").numpy.take(
-        x, np.array(indices), axis=axis, **kwargs
+        x, indices, axis=axis, **kwargs
     ),
 )
 ar.register_function("jax", "coerce", lambda x: x)
