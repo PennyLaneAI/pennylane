dd139cadd81230b397373bb1b650834a6fd81eb9
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2024-09-05 04:19:22 -0400

    Update pennylane/spin/spin_hamiltonian.py
    Co-authored-by: Austin Huang <65315367+austingmhuang@users.noreply.github.com>

--- pennylane/spin/spin_hamiltonian.py
+++ pennylane/spin/spin_hamiltonian.py
@@ -346,7 +346,7 @@ def emery(
             neighbouring sites, it can be a number, a list of length equal to ``neighbour_order`` or
             a square matrix of size ``(num_spins, num_spins)``, where ``num_spins`` is the total
             number of spins. Default value is 1.0.
-        coulomb (float or List[float]): Coulomb interaction between spins. It can be a constant or a
+        coulomb (float | list[float]): Coulomb interaction between spins. It can be a constant or a
             list of length equal to number of spins.
         intersite_coupling (float | list[float] | list[math.array(float)]): Interaction strength between spins on
             neighbouring sites. It can be a number, a list with length equal to ``neighbour_order`` or
