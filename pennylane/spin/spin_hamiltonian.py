aac85caa2cc12f566ce2183dbff9ddd67a33f7e6
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2024-09-06 05:07:58 -0400

    Update pennylane/spin/spin_hamiltonian.py
    Co-authored-by: Utkarsh <utkarshazad98@gmail.com>

--- pennylane/spin/spin_hamiltonian.py
+++ pennylane/spin/spin_hamiltonian.py
@@ -495,7 +495,7 @@ def haldane(
     where :math:`t_1` is the hopping term representing the hopping amplitude between neighbouring
     sites, :math:`t_2` is the hopping amplitude between next nearest neighbours, :math:`\phi` is the phase
     factor, :math:`\langle i,j \rangle`, :math:`\langle \langle i,j \rangle \rangle` represent the
-    indices for nearest neighbour and next nearest neighbour spins respectively,
+    indices for nearest neighbour and next-nearest neighbour spins respectively,
     and :math:`\sigma` is the spin degree of freedom.
     This function assumes there are two fermions with opposite spins on each lattice site.
 
