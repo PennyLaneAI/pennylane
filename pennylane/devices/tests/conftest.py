08a37544b521b516fdbe38875b444ef540571827
Astral Cai <astral.cai@xanadu.ai>
2024-09-18 09:30:41 -0400

    make pylint happy

--- pennylane/devices/tests/conftest.py
+++ pennylane/devices/tests/conftest.py
@@ -14,7 +14,6 @@
 """Contains shared fixtures for the device tests."""
 import argparse
 import os
-import warnings
 
 import numpy as np
 import pytest
--- tests/conftest.py
+++ tests/conftest.py
@@ -19,7 +19,6 @@ import contextlib
 import os
 import pathlib
 import sys
-import warnings
 
 import numpy as np
 import pytest
