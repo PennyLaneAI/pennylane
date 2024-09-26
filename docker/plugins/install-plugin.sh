37ff50c9d77fe98ab47ae708daa3f8a0ebc98335
PietropaoloFrisoni <pietropaolo.frisoni@xanadu.ai>
2024-09-26 14:26:31 -0400

    Replacing Copyright

--- docker/plugins/install-plugin.sh
+++ docker/plugins/install-plugin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2018-2021 Xanadu Quantum Technologies Inc.
+# Copyright 2018-2024 Xanadu Quantum Technologies Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- pennylane/drawer/draw.py
+++ pennylane/drawer/draw.py
@@ -1,6 +1,6 @@
 # pylint: disable=too-many-arguments
 
-# Copyright 2018-2021 Xanadu Quantum Technologies Inc.
+# Copyright 2018-2024 Xanadu Quantum Technologies Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
