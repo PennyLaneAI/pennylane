4d14bbb1d3419f6063dde4eecaa9a9a0db9ce1ce
Will <wmaxwell90@gmail.com>
2024-10-03 14:56:00 -0400

    removing comment

--- pennylane/templates/subroutines/qubitization.py
+++ pennylane/templates/subroutines/qubitization.py
@@ -150,8 +150,6 @@ class Qubitization(Operation):
 
         decomp_ops = []
 
-        # QJIT is not compatible with qml.Identity(control), so we build the
-        # identity as a tensor product instead
         identity = qml.prod(*[qml.Identity(wire) for wire in control])
 
         decomp_ops.append(qml.Reflection(identity))
