54c695c301e1beb127fbac0fd8d9a6e84b9a3624
lillian542 <Lillian.frederiksen@xanadu.ai>
2024-10-03 09:30:21 -0400

    update documentation

--- pennylane/capture/make_plxpr.py
+++ pennylane/capture/make_plxpr.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Xanadu Quantum Technologies Inc.
+# Copyright 2018-2024 Xanadu Quantum Technologies Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,23 +22,58 @@ except ImportError:
 
 
 # ToDo: type hints
-def make_plxpr(circuit, static_argnums, **kwargs):
-    """Creates a function that produces its PLxPR given example args.
+def make_plxpr(circuit, static_argnums=None, **kwargs):
+    """Takes a QNode and returns a function that, when called, produces a PLxPR representing
+    the QNode with the given args.
 
     This function relies on `jax.make_jaxpr` as part of creating the representation. Any
     keyword arguments passed to `make_plxpr` that are not directly used in the function will
     be passed to make_jaxpr.
 
-        Args:
-            circuit (~.tape.QuantumTape):  # is this a qnode or a tape or both?
+    Args:
+        circuit (QNode):  the QNode to be captured
 
-        Kwargs:
-            static_argnums (Union(int, Sequence[int], None)): optional, an int or
-                collection of ints that specify which positional arguments to treat
-                as static (trace- and compile-time constant).
+    Kwargs:
+        static_argnums (Union(int, Sequence[int], None)): optional, an int or collection of ints
+        that specify which positional arguments to treat as static (trace- and compile-time constant).
 
-        Returns:
-            Callable: function that, when called, returns the PLxPR representation of `circuit` for the specified inputs.
+    Returns:
+        Callable: function that, when called, returns the PLxPR representation of `circuit` for the specified inputs.
+
+
+    ** Example **
+
+    .. code-block:: python
+
+        qml.capture.enable()
+
+        dev = qml.device("default.qubit", wires=1)
+
+        @qml.qnode(dev)
+        def circ(x):
+            qml.RX(x, 0)
+            qml.Hadamard(0)
+            return qml.expval(qml.X(0))
+
+        plxpr = qml.capture.make_plxpr(circ)(1.2)
+
+
+    >>> print(plxpr)
+    { lambda ; a:f32[]. let
+        b:f32[] = qnode[
+            device=<default.qubit device (wires=1) at 0x152a6f010>
+            n_consts=0
+            qfunc_jaxpr={ lambda ; c:f32[]. let
+                _:AbstractOperator() = RX[n_wires=1] c 0
+                _:AbstractOperator() = Hadamard[n_wires=1] 0
+                d:AbstractOperator() = PauliX[n_wires=1] 0
+                e:AbstractMeasurement(n_wires=None) = expval_obs d
+              in (e,) }
+            qnode=<QNode: device='<default.qubit device (wires=1) at 0x152a6f010>', interface='auto', diff_method='best'>
+            qnode_kwargs={'diff_method': 'best', 'grad_on_execution': 'best', 'cache': False, 'cachesize': 10000, 'max_diff': 1, 'device_vjp': False, 'mcm_method': None, 'postselect_mode': None}
+            shots=Shots(total=None)
+        ] a
+      in (b,) }
 
     """
     return jax.make_jaxpr(circuit, static_argnums, **kwargs)
