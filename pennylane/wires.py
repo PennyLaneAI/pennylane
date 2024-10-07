4c61871cc9525edc6fc8a4c890f9688edc9ea75e
PietropaoloFrisoni <pietropaolo.frisoni@xanadu.ai>
2024-10-07 12:02:28 -0400

    Creating one unique PR

--- pennylane/wires.py
+++ pennylane/wires.py
@@ -17,12 +17,24 @@ This module contains the :class:`Wires` class, which takes care of wire bookkeep
 import functools
 import itertools
 from collections.abc import Hashable, Iterable, Sequence
+from importlib import import_module, util
 from typing import Union
 
 import numpy as np
 
 import pennylane as qml
 from pennylane.pytrees import register_pytree
+
+if util.find_spec("jax") is not None:
+    jax = import_module("jax")
+    jax_available = True
+else:
+    jax_available = False
+    jax = None
+
+if jax_available:
+    # pylint: disable=unnecessary-lambda
+    setattr(jax.interpreters.partial_eval.DynamicJaxprTracer, "__hash__", lambda x: id(x))
 
 
 class WireError(Exception):
