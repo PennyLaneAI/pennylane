138358cb01270d6a0c612a8ec77f8ba30d19303e
Will <wmaxwell90@gmail.com>
2024-10-08 11:22:53 -0400

    removing unused import

--- pennylane/ops/qutrit/non_parametric_ops.py
+++ pennylane/ops/qutrit/non_parametric_ops.py
@@ -18,7 +18,7 @@ that do not depend on any parameters.
 # pylint:disable=arguments-differ
 import numpy as np
 
-from pennylane.operation import AdjointUndefinedError, Operation, PowUndefinedError
+from pennylane.operation import AdjointUndefinedError, Operation
 from pennylane.wires import Wires
 
 from .parametric_ops import validate_subspace
