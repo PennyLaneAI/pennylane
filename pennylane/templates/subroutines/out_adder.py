83c3d53b7719c552b87c35918f89b434711d25cd
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2024-10-09 13:02:48 -0400

    silence codefactor
    suggestion is wrong

--- pennylane/templates/subroutines/out_adder.py
+++ pennylane/templates/subroutines/out_adder.py
@@ -171,6 +171,8 @@ class OutAdder(Operation):
             raise ValueError("None of the wires in y_wires should be included in output_wires.")
         for key in ["x_wires", "y_wires", "output_wires", "work_wires"]:
             self.hyperparameters[key] = qml.wires.Wires(locals()[key])
+
+        # pylint: disable=consider-using-generator
         all_wires = sum(
             [self.hyperparameters[key] for key in ["x_wires", "y_wires", "output_wires"]], start=[]
         )
--- pennylane/templates/subroutines/out_multiplier.py
+++ pennylane/templates/subroutines/out_multiplier.py
@@ -184,6 +184,8 @@ class OutMultiplier(Operation):
         for name, wires in zip(wires_name, wires_list):
             self.hyperparameters[name] = qml.wires.Wires(wires)
         self.hyperparameters["mod"] = mod
+
+        # pylint: disable=consider-using-generator
         all_wires = sum([self.hyperparameters[name] for name in wires_name], start=[])
         super().__init__(wires=all_wires, id=id)
 
