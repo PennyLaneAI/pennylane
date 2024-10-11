a11d2db74d7b59d178b834fdc7ad4aeeb5d6339b
Utkarsh <utkarshazad98@gmail.com>
2024-10-11 12:57:50 -0400

    Update pennylane/templates/subroutines/basis_rotation.py

--- pennylane/templates/subroutines/basis_rotation.py
+++ pennylane/templates/subroutines/basis_rotation.py
@@ -112,7 +112,7 @@ class BasisRotation(Operation):
 
         if M != N:
             raise ValueError(
-                f"The unitary matrix should be of shape NxN, got {qml.math.shape(unitary_matrix)}"
+                f"The unitary matrix should be of shape NxN, got {(M, N)}"
             )
 
         if check:
