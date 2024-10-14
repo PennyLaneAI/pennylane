6e889ab3acb7e9520e27384bb2ef8eb960f220ee
qottmann <korbinian.kottmann@gmail.com>
2024-10-14 13:16:04 +0200

    init

--- pennylane/labs/dla/__init__.py
+++ pennylane/labs/dla/__init__.py
@@ -15,3 +15,4 @@
 
 from .lie_closure_dense import lie_closure_dense
 from .structure_constants_dense import structure_constants_dense
+from .cartan import cartan_decomposition, even_odd_involution, concurrence_involution
