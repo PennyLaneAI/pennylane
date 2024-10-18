1e82ede660876034eea82d395613f7ab99505bd5
Diksha Dhawan <ddhawan@umich.edu>
2024-10-18 18:58:16 +0000

    Added normal ordering

--- pennylane/labs/vibrational_ham/vib_runscript.py
+++ pennylane/labs/vibrational_ham/vib_runscript.py
@@ -4,20 +4,31 @@ import numpy as np
 from christiansenForm import christiansen_ham, christiansen_dipole
 from vib_observables import *
 from bosonic_mapping import *
+from mpi4py import MPI 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
-sym = ["H", "F"]
-geometry = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
+
+
+#sym = ["H", "F"]
+#geometry = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
 
 #sym = ["C", "O"]
 #geometry = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
 
-#sym = ["H", "H", "S"]
-#geometry = np.array([[0.0,-1.0,-1.0], [0.0,1.0,-1.0], [0.0,0.0,0.0]])
+sym = ["H", "H", "S"]
+geometry = np.array([[0.0,-1.0,-1.0], [0.0,1.0,-1.0], [0.0,0.0,0.0]])
 load_data = True
 
 ## Generate PES object
 molecule = qml.qchem.Molecule(sym, geometry, basis_name="6-31g", unit="Angstrom", load_data=load_data)
-pes = pes_generator.vibrational(molecule, quad_order=17, do_cubic=True, get_anh_dipole=2)
+pes = pes_generator.vibrational(molecule, quad_order=9, do_cubic=True, get_anh_dipole=2)
+
+if rank == 0:
+    print("onebody: ", pes.pes_onebody, pes.dipole_onebody)
+    print("twobody: ", pes.pes_twobody, pes.dipole_twobody)
+    print("threebody: ", pes.pes_threebody, pes.dipole_threebody)
 
 ## Generate Christiansen Hamiltonian and dipole
 c_ham = christiansen_ham(pes, nbos=4, do_cubic=False)
