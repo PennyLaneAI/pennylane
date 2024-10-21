f6030d68b56cf7f73308ae3d0b6b16cbfb0e8397
andrijapau <andrija.paurevic@xanadu.ai>
2024-10-21 16:09:38 -0400

    improve tests

--- tests/workflow/test_construct_tape.py
+++ tests/workflow/test_construct_tape.py
@@ -23,8 +23,63 @@ from pennylane.workflow import construct_tape
 
 
 class TestConstructTape:
+    """Tests for the construct_tape function."""
 
-    def test_error_is_raised(self):
+    @pytest.mark.parametrize(
+        ("level", "weights", "order", "expected_ops"),
+        [
+            (
+                0,
+                qml.math.array([[1.0, 2.0]]),
+                [2, 1, 0],
+                [
+                    qml.RandomLayers(qml.math.array([[1.0, 2.0]]), wires=(0, 1)),
+                    qml.Permute([2, 1, 0], wires=(0, 1, 2)),
+                    qml.PauliX(0),
+                    qml.PauliX(0),
+                    qml.RX(0.1, wires=0),
+                    qml.RX(-0.1, wires=0),
+                ],
+            ),
+            (
+                1,
+                qml.math.array([[1.0, 2.0]]),
+                [2, 1, 0],
+                [
+                    qml.RandomLayers(qml.math.array([[1.0, 2.0]]), wires=(0, 1)),
+                    qml.Permute([2, 1, 0], wires=(0, 1, 2)),
+                    # cancel inverses
+                    qml.RX(0.1, wires=0),
+                    qml.RX(-0.1, wires=0),
+                ],
+            ),
+        ],
+    )
+    def test_level_argument(self, level, weights, order, expected_ops):
+        """Tests that the level argument is correctly passed through."""
+
+        @qml.transforms.merge_rotations
+        @qml.transforms.cancel_inverses
+        @qml.qnode(qml.device("default.qubit"), diff_method="parameter-shift")
+        def circuit(weights, order):
+            qml.RandomLayers(weights, wires=(0, 1))
+            qml.Permute(order, wires=(0, 1, 2))
+            qml.PauliX(0)
+            qml.PauliX(0)
+            qml.RX(0.1, wires=0)
+            qml.RX(-0.1, wires=0)
+            return qml.expval(qml.PauliX(0))
+
+        tape = construct_tape(circuit, level=level)(weights, order, shots=10)
+
+        trainable_params = [] if level == 0 else None
+        expected_tape = qml.tape.QuantumScript(
+            expected_ops, [qml.expval(qml.PauliX(0))], shots=10, trainable_params=trainable_params
+        )
+        qml.assert_equal(tape, expected_tape)
+
+    def test_level_error_is_raised(self):
+        """Tests that a ValueError is raised if the user requests a level that corresponds to more than one tape."""
 
         @qml.gradients.param_shift
         @qml.transforms.merge_rotations
@@ -37,21 +92,27 @@ class TestConstructTape:
         with pytest.raises(ValueError, match="Level requested corresponds to more than one tape."):
             construct_tape(circuit, level=None)(0.5)
 
-    def test_handle_dynamic_shots(self):
+    def test_handle_qfunc_with_dynamic_shots(self):
+        """Tests that dynamic shots can be handled properly."""
 
-        @qml.qnode(qml.device("default.qubit"))
-        def circuit():
-            qml.X(1)
-            return qml.expval(qml.X(0) + qml.Y(0))
+        with pytest.warns(UserWarning, match="Detected 'shots' as an argument"):
 
-        tape = construct_tape(circuit)(shots=10)
+            @qml.qnode(qml.device("default.qubit"))
+            def circuit(shots):
+                for _ in range(shots):
+                    qml.X(0)
+                return qml.expval(qml.PauliZ(0))
+
+        num_shots = 10
+        tape = construct_tape(circuit)(shots=num_shots)
         expected_tape = qml.tape.QuantumScript(
-            [qml.X(1)], [qml.expval(qml.X(0) + qml.Y(0))], shots=10
+            [qml.X(0)] * num_shots, [qml.expval(qml.PauliZ(0))], trainable_params=[]
         )
 
         qml.assert_equal(tape, expected_tape)
 
-    def test_correct_output_tape(self):
+    def test_correct_tape_is_constructed(self):
+        """Tests that the constructed tape is as expected."""
 
         @qml.qnode(qml.device("default.qubit"))
         def circuit(x):
@@ -61,10 +122,7 @@ class TestConstructTape:
 
         tape = construct_tape(circuit)(0.5)
         expected_tape = qml.tape.QuantumScript(
-            [qml.RY(0.5, 0), qml.X(1)],
-            [qml.expval(qml.X(0) + qml.Y(0))],
+            [qml.RY(0.5, 0), qml.X(1)], [qml.expval(qml.X(0) + qml.Y(0))], trainable_params=[]
         )
-        # FIXME: Need this b/c of L617 in qscript.py
-        expected_tape.trainable_params = []
 
         qml.assert_equal(tape, expected_tape)
