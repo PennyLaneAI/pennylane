f7bf31f24f881846ee974d86b9ee661748b286cd
Will <wmaxwell90@gmail.com>
2024-10-24 11:29:37 -0400

    populate resource constructor

--- pennylane/labs/resource_estimation/resource_constructor.py
+++ pennylane/labs/resource_estimation/resource_constructor.py
@@ -1,1 +1,24 @@
-temp
+from abc import ABC, abstractmethod
+from typing import Callable
+
+from .resource_container import CompressedResourceOp, Resources
+
+
+class ResourceConstructor(ABC):
+    @staticmethod
+    @abstractmethod
+    def compute_resources(*args, **kwargs) -> Resources:
+        """Returns the Resource object associated with the Operator."""
+
+    @classmethod
+    def set_compute_resources(cls, new_func: Callable) -> None:
+        """Override the compute_resources method."""
+        cls.compute_resources = new_func
+
+    @abstractmethod
+    def resource_rep(self) -> CompressedResourceOp:
+        """Returns a compressed representation containing only the parameters of
+        the Operator that are needed to compute a resource estimation."""
+
+class ResourcesNotDefined(Exception):
+    """Exception to be raised when a ResourceConstructor does not implement compute_resources"""
