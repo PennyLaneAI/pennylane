cc908da7c07a2deb0a8b884b3023350ff13e3cc9
qottmann <korbinian.kottmann@gmail.com>
2024-10-28 11:40:29 +0100

    AI singledispatch

---
+++ pennylane/labs/tests/dla/test_involutions.py
@@ -0,0 +1,31 @@
+# Copyright 2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for pennylane/labs/dla/lie_closure_dense.py functionality"""
+# pylint: disable=too-few-public-methods, protected-access, no-self-use
+import pytest
+
+import pennylane as qml
+from pennylane import X, Y, Z
+from pennylane.labs.dla import AI
+
+
+@pytest.mark.parametrize(
+    "op",
+    [
+        X(0) @ Y(1) @ Z(2),
+    ],
+)
+def test_AI_dispatch(op):
+    """Test singledispatch for AI involution"""
+    assert all([AI(op), AI(op.pauli_rep), AI(qml.matrix(op))])
