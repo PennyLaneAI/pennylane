3ca7d8cb4fb51051e0cae6b7773cfdcdbc7f003d
Lee James O'Riordan <mlxd@users.noreply.github.com>
2024-10-28 09:01:44 -0400

    Update pennylane/labs/tests/conftest.py
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>

--- pennylane/labs/tests/conftest.py
+++ pennylane/labs/tests/conftest.py
@@ -198,4 +198,5 @@ def import_labs():
     Fixture for a scoped import of qml.labs
     To be used instead of direct imports allowing `import pennylane as qml` to give access to `qml.labs` in the subsequent tests.
     """
-    import pennylane.labs
+    import pennylane.labs  # pylint: disable=import-outside-toplevel, unused-import
+
