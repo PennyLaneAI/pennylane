46e95a2aa5c2701d15d8f1f9540a2298fd9a65a4
Astral Cai <astral.cai@xanadu.ai>
2024-10-30 12:27:08 -0400

    more more tiny fix

--- pennylane/templates/subroutines/qubitization.py
+++ pennylane/templates/subroutines/qubitization.py
@@ -55,8 +55,8 @@ class Qubitization(Operation):
 
             # apply QPE
             measurements = qml.iterative_qpe(
-                         qml.Qubitization(H, control = [3,4]), aux_wire = 5, iters = 3
-                         )
+                qml.Qubitization(H, control = [3,4]), aux_wire = 5, iters = 3
+            )
             return qml.probs(op = measurements)
 
         output = circuit()
