a7b39bd97ed01a65b35d78c22bf6197deb6a7a3e
Will <wmaxwell90@gmail.com>
2024-10-30 11:51:10 -0400

    adding global phase

--- pennylane/labs/resource_estimation/ops/identity.py
+++ pennylane/labs/resource_estimation/ops/identity.py
@@ -6,6 +6,7 @@ import pennylane.labs.resource_estimation as re
 #pylint: disable=too-many-ancestors
 
 class ResourceIdentity(qml.Identity, re.ResourceConstructor):
+    """Resource class for Identity"""
 
     @staticmethod
     def _resource_decomp() -> Dict[re.CompressedResourceOp, int]:
@@ -17,3 +18,17 @@ class ResourceIdentity(qml.Identity, re.ResourceConstructor):
     @staticmethod
     def resource_rep() -> re.CompressedResourceOp:
         return re.CompressedResourceOp(qml.Identity, {})
+
+class ResourceGlobalPhase(qml.GlobalPhase, re.ResourceConstructor):
+    """Resource class for GlobalPhase"""
+
+    @staticmethod
+    def _resource_decomp() -> Dict[re.CompressedResourceOp, int]:
+        return {}
+
+    def resource_params(self) -> dict:
+        return {}
+
+    @staticmethod
+    def resource_rep() -> re.CompressedResourceOp:
+        return re.CompressedResourceOp(qml.GlobalPhase, {})
