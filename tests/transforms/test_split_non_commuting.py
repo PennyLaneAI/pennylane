832d0ec925ef51ecbfaa02fa5ee27e37c43ad358
Astral Cai <astral.cai@xanadu.ai>
2024-10-30 14:04:15 -0400

    one more

--- tests/transforms/test_split_non_commuting.py
+++ tests/transforms/test_split_non_commuting.py
@@ -740,10 +740,10 @@ class TestIntegration:
             ),
         ],
     )
-    def test_multiple_expval(self, grouping_strategy, shots, params, expected_results):
+    def test_multiple_expval(self, grouping_strategy, shots, params, expected_results, seed):
         """Tests that a QNode with multiple expval measurements is executed correctly"""
 
-        dev = qml.device("default.qubit", wires=2, shots=shots)
+        dev = qml.device("default.qubit", wires=2, shots=shots, seed=seed)
 
         obs_list = complex_obs_list
         if not qml.operation.active_new_opmath():
