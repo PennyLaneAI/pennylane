5ce14f005b6b84b6d4cfb7400eab30f1b5a527fe
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2024-11-01 14:50:03 -0400

    Fmt

--- pennylane/labs/tests/test_placeholder.py
+++ pennylane/labs/tests/test_placeholder.py
@@ -4,6 +4,7 @@ This is a placeholder test to allow pytest to pass.
 Without this pytest exits with non-zero exit code which fails the build.
 """
 
+
 def test_always_pass():
     """A placeholder test that always passes."""
     assert True
