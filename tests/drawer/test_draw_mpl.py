432b8189a9145a070e94f02d3df436d359e35c7e
zazabap <sweynan@icloud.com>
2024-11-01 14:37:46 +0900

    Update the test and format of mpldrawer.py

--- tests/drawer/test_draw_mpl.py
+++ tests/drawer/test_draw_mpl.py
@@ -328,6 +328,43 @@ class TestWireBehaviour:
             assert w.get_color() == "black"
             assert w.get_linewidth() == 4
 
+        # Define the quantum function with @qml.qnode
+        @qml.qnode(dev)
+        def node(x):
+            qml.RX(x, wires=0)
+            qml.Hadamard(wires=1)
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.PauliZ(0) @ qml.PauliY(1))
+
+        # Draw the circuit and save the plot
+        _, ax = qml.draw_mpl(node)(0.52)
+
+        # All wires are orange
+        wire_options = {"color": "orange"}
+        _, ax = qml.draw_mpl(node, wire_options=wire_options)(0.52)
+
+        # Wires are orange and cyan
+        wire_options = {
+            0: {"color": "orange"},
+            1: {"color": "cyan"}
+        }
+        _,ax = qml.draw_mpl(node, wire_options=wire_options)(0.52) 
+
+        @qml.qnode(qml.device("default.qubit"))
+        def node(x):
+            for w in range(10):
+                qml.Hadamard(w) 
+            return qml.expval(qml.PauliZ(0) @ qml.PauliY(1))
+
+        # Make all wires cyan and bold, 
+        # except for wires 2 and 6, which are dashed and another color
+        wire_options = {"color": "cyan", 
+                        "linewidth": 5, 
+                        2: {"linestyle": "--", "color": "red"}, 
+                        6: {"linestyle": "--", "color": "orange"}
+                    }
+        _,ax  = qml.draw_mpl(node, wire_options=wire_options)(0.52)
+
         plt.close()
 
 
