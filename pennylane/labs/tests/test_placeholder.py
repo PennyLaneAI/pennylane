592adc555e0ac7335d7f400e4e286d87afc33cfc
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2024-11-04 13:47:00 -0500

    Adding new workflow for qml labs testing (#6441)
    **Context:**
    QML Labs Requires a separate CI workflow that runs separate from the
    current test suite of unit-tests on core pennylane.
    
    This PR introduces the new workflow with the assumption of:
    
    QML Labs tests will reside under:
    `pennylane/labs/tests/*`
    
    QML Labs source code will reside under:
    `pennylane/labs/*`
    
    **Description of the Change:**
    - Coverage config is no longer a hard-coded env var in unit-tests. This
    allows us to have multiple coveragerc files.
    - `disable_new_opmath` input has been removed and the jobs using it now
    pass it under `pytest_additional_args` instead.
    - The job that stored tf/jax/torch tests are now in a separate workflow.
    This allows us to keep one source of truth for them and use them across
    core and labs tests.
    
    **Benefits:**
    Support for QML labs.
    
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**
    
    [sc-76031](https://app.shortcut.com/xanaduai/story/76031/create-ci-in-pennylane-to-run-qml-labs-unit-tests)
    
    ---------
    
    Co-authored-by: Lee J. O'Riordan <lee@xanadu.ai>
    Co-authored-by: Lee James O'Riordan <mlxd@users.noreply.github.com>
    Co-authored-by: Mudit Pandey <mudit.pandey@xanadu.ai>
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>

---
+++ pennylane/labs/tests/test_placeholder.py
@@ -0,0 +1,10 @@
+"""
+This is a placeholder test to allow pytest to pass.
+
+Without this pytest exits with non-zero exit code which fails the build.
+"""
+
+
+def test_always_pass():
+    """A placeholder test that always passes."""
+    assert True
