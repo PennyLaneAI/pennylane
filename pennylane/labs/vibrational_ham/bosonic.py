57e55d04c592da35978994aab4c8f2526c4b615a
Austin Huang <gengminghuang@gmail.com>
2024-11-05 14:27:38 -0500

    small

--- pennylane/labs/vibrational_ham/bosonic.py
+++ pennylane/labs/vibrational_ham/bosonic.py
@@ -12,10 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """The bosonic representation classes and functions."""
-import re
 from copy import copy
 
-import numpy as np
 from pennylane.typing import TensorLike
 import pennylane as qml
 
