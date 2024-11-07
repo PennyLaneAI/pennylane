8eb92ddbfcfe5cfdad460bb46063f301f6c84f3b
Will <wmaxwell90@gmail.com>
2024-11-07 08:52:18 -0500

    adding exception

--- pennylane/labs/resource_estimation/resource_operator.py
+++ pennylane/labs/resource_estimation/resource_operator.py
@@ -95,3 +95,5 @@ class ResourceOperator(ABC):
         return self.__class__.resource_rep(**params)
 
 
+class ResourcesNotDefined(Exception):
+    """Exception to be raised when a ``ResourceOperator`` does not implement _resource_decomp"""
