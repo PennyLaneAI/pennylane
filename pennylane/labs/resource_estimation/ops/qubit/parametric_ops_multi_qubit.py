c948338e25859250c707a0b58d5e03f78a0040be
Will <wmaxwell90@gmail.com>
2024-11-07 13:53:12 -0500

    resource for MultiRZ

--- pennylane/labs/resource_estimation/ops/qubit/parametric_ops_multi_qubit.py
+++ pennylane/labs/resource_estimation/ops/qubit/parametric_ops_multi_qubit.py
@@ -1,7 +1,32 @@
 import pennylane as qml
-import pennylane.labs.resource_estimation.resource_constructor as rc
+import pennylane.labs.resource_estimation as re
 
-class ResourceMultiRZ(qml.MultiRZ, rc.ResourceConstructor):
+#pylint: disable=arguments-differ
+
+class ResourceMultiRZ(qml.MultiRZ, re.ResourceOperator):
+    """Resource class for MultiRZ"""
+
+    @staticmethod
+    def _resource_decomp(num_wires, **kwargs):
+        cnot = re.CompressedResourceOp(re.ResourceCNOT, {})
+        rz = re.CompressedResourceOp(re.ResourceRZ, {})
+
+        gate_types = {}
+        gate_types[cnot] = 2*(num_wires-1)
+        gate_types[rz] = 1
+
+        return gate_types
+
+    def resource_params(self):
+        return {"num_wires": len(self.wires)}
+
+    @classmethod
+    def resource_rep(cls, num_wires, **kwargs):
+        return re.CompressedResourceOp(cls, {"num_wires": num_wires})
+
+class ResourcePauliRot(qml.PauliRot, re.ResourceOperator):
+    """Resource class for PauliRot"""
+
     @staticmethod
     def _resource_decomp(*args, **kwargs):
         return
@@ -10,10 +35,12 @@ class ResourceMultiRZ(qml.MultiRZ, rc.ResourceConstructor):
         return
 
     @classmethod
-    def resource_rep(cls, **kwargs):
+    def resource_rep(cls, *args, **kwargs):
         return
 
-class ResourcePauliRot(qml.PauliRot, rc.ResourceConstructor):
+class ResourceIsingXX(qml.IsingXX, re.ResourceOperator):
+    """Resource class for IsingXX"""
+
     @staticmethod
     def _resource_decomp(*args, **kwargs):
         return
@@ -22,10 +49,12 @@ class ResourcePauliRot(qml.PauliRot, rc.ResourceConstructor):
         return
 
     @classmethod
-    def resource_rep(cls, **kwargs):
+    def resource_rep(cls, *args, **kwargs):
         return
 
-class ResourceIsingXX(qml.IsingXX, rc.ResourceConstructor):
+class ResourceIsingYY(qml.IsingYY, re.ResourceOperator):
+    """Resource class for IsingYY"""
+
     @staticmethod
     def _resource_decomp(*args, **kwargs):
         return
@@ -34,10 +63,12 @@ class ResourceIsingXX(qml.IsingXX, rc.ResourceConstructor):
         return
 
     @classmethod
-    def resource_rep(cls, **kwargs):
+    def resource_rep(cls, *args, **kwargs):
         return
 
-class ResourceIsingYY(qml.IsingYY, rc.ResourceConstructor):
+class ResourceIsingZZ(qml.IsingZZ, re.ResourceOperator):
+    """Resource class for IsingZZ"""
+
     @staticmethod
     def _resource_decomp(*args, **kwargs):
         return
@@ -46,10 +77,12 @@ class ResourceIsingYY(qml.IsingYY, rc.ResourceConstructor):
         return
 
     @classmethod
-    def resource_rep(cls, **kwargs):
+    def resource_rep(cls, *args, **kwargs):
         return
 
-class ResourceIsingZZ(qml.IsingZZ, rc.ResourceConstructor):
+class ResourceIsingXY(qml.IsingXY, re.ResourceOperator):
+    """Resource class for IsingXY"""
+
     @staticmethod
     def _resource_decomp(*args, **kwargs):
         return
@@ -58,10 +91,12 @@ class ResourceIsingZZ(qml.IsingZZ, rc.ResourceConstructor):
         return
 
     @classmethod
-    def resource_rep(cls, **kwargs):
+    def resource_rep(cls, *args, **kwargs):
         return
 
-class ResourceIsingXY(qml.IsingXY, rc.ResourceConstructor):
+class PSWAP(qml.PSWAP, re.ResourceOperator):
+    """Resource class for PSWAP"""
+
     @staticmethod
     def _resource_decomp(*args, **kwargs):
         return
@@ -70,17 +105,5 @@ class ResourceIsingXY(qml.IsingXY, rc.ResourceConstructor):
         return
 
     @classmethod
-    def resource_rep(cls, **kwargs):
-        return
-
-class PSWAP(qml.PSWAP, rc.ResourceConstructor):
-    @staticmethod
-    def _resource_decomp(*args, **kwargs):
-        return
-
-    def resource_params(self):
-        return
-
-    @classmethod
-    def resource_rep(cls, **kwargs):
+    def resource_rep(cls, *args, **kwargs):
         return
