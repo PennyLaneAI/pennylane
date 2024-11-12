4bb50af0236cf7be70b32c7e81a42743c0d24089
Astral Cai <astral.cai@xanadu.ai>
2024-11-12 10:38:13 -0500

    pylint

--- tests/transforms/test_qcut.py
+++ tests/transforms/test_qcut.py
@@ -15,7 +15,7 @@
 Unit tests for the `pennylane.qcut` package.
 """
 # pylint: disable=protected-access,too-few-public-methods,too-many-arguments
-# pylint: disable=too-many-public-methods,comparison-with-callable
+# pylint: disable=too-many-public-methods,comparison-with-callable,unused-argument
 # pylint: disable=no-value-for-parameter,no-member,not-callable, use-implicit-booleaness-not-comparison
 import copy
 import itertools
