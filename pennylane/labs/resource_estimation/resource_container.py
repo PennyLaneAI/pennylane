89f12b53fe2c0c93abe118637abede22f73c53a3
Will <wmaxwell90@gmail.com>
2024-11-13 09:34:17 -0500

    remove unused import

--- pennylane/labs/resource_estimation/resource_container.py
+++ pennylane/labs/resource_estimation/resource_container.py
@@ -15,7 +15,6 @@ r"""Base classes for resource estimation."""
 import copy
 from collections import defaultdict
 from dataclasses import dataclass, field
-from typing import Tuple
 
 from pennylane.labs.resource_estimation import ResourceOperator
 
