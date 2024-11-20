03644f7db08da78e8b72505da5f071ced5791e86
Christina Lee <christina@xanadu.ai>
2024-11-20 13:00:15 -0500

    Update tests/devices/default_qubit/test_default_qubit_plxpr.py
    Co-authored-by: Pietropaolo Frisoni <pietropaolo.frisoni@xanadu.ai>

--- tests/devices/default_qubit/test_default_qubit_plxpr.py
+++ tests/devices/default_qubit/test_default_qubit_plxpr.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for default qubit executing jaxpr.."""
+"""Tests for default qubit executing jaxpr."""
 
 import pytest
 
