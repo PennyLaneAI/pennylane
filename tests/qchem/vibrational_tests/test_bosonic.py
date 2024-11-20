1eaeab1eb036e81b5918634a5b147b13f8440a36
Austin Huang <65315367+austingmhuang@users.noreply.github.com>
2024-11-20 09:37:13 -0500

    Update tests/qchem/vibrational_tests/test_bosonic.py
    Co-authored-by: soranjh <40344468+soranjh@users.noreply.github.com>

--- tests/qchem/vibrational_tests/test_bosonic.py
+++ tests/qchem/vibrational_tests/test_bosonic.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Unit Tests for the Boseonic representation classes."""
+"""Unit Tests for the Bosonic representation classes."""
 import pickle
 from copy import copy, deepcopy
 
