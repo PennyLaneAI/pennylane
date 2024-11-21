9e42ab8d543b496673991f5c075da301939d6536
Will <wmaxwell90@gmail.com>
2024-11-21 08:13:40 -0500

    fixing merge error

--- pennylane/labs/resource_estimation/__init__.py
+++ pennylane/labs/resource_estimation/__init__.py
@@ -91,14 +91,11 @@ from .ops import (
     ResourceCNOT,
     ResourceControlledPhaseShift,
     ResourceCRY,
-<<<<<<< HEAD
+    ResourceCZ,
     ResourceDoubleExcitation,
     ResourceDoubleExcitationMinus,
     ResourceDoubleExcitationPlus,
     ResourceFermionicSWAP,
-=======
-    ResourceCZ,
->>>>>>> dcea65b0a0ce037ee50c403250762ff8668bc8d2
     ResourceGlobalPhase,
     ResourceHadamard,
     ResourceIdentity,
