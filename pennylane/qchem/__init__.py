4b7bd11b6bc9954fdaa12465e02709b0a41715bf
ddhawan11 <ddhawan@umich.edu>
2024-11-21 13:27:58 -0500

    [skip ci] Removed bose operators code

--- pennylane/qchem/__init__.py
+++ pennylane/qchem/__init__.py
@@ -15,7 +15,7 @@
 This submodule provides the functionality to perform quantum chemistry calculations.
 """
 
-from .vibrational import BoseSentence, BoseWord
+from .vibrational import unary_mapping
 from .basis_data import load_basisset
 from .basis_set import BasisFunction, atom_basis_data, mol_basis_data
 from .convert import import_operator, import_state
@@ -85,11 +85,3 @@ from .tapering import (
     taper_operation,
 )
 
-from .vibrational.bosonic import (
-    BoseSentence,
-    BoseWord
-)
-
-from .vibrational.bosonic_mapping import (
-    unary_mapping
-)
