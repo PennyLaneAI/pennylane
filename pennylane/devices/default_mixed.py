1f9dae4062fdfddcd08abc31c824cffe0d745a18
JerryChen97 <chenys13@outlook.com>
2024-11-22 18:43:03 -0500

    ...

--- pennylane/devices/default_mixed.py
+++ pennylane/devices/default_mixed.py
@@ -984,7 +984,7 @@ class DefaultMixedNewAPI(Device):
         )
 
         return tuple(
-            qml.devices.qubit_mixed.simulate.simulate(
+            simulate(
                 c,
                 rng=self._rng,
                 prng_key=self._prng_key,
@@ -1068,3 +1068,39 @@ class DefaultMixedNewAPI(Device):
             transform_program.add_transform(warn_readout_error_state)
 
         return transform_program, config
+
+
+# pylint: disable=too-many-positional-arguments, too-many-arguments
+def simulate(
+    circuit: qml.tape.QuantumScript,
+    rng=None,
+    prng_key=None,
+    debugger=None,
+    interface=None,
+    readout_errors=None,
+) -> Result:
+    """Simulate a single quantum script.
+
+    This is an internal function that will be called by ``default.qubit.mixed``.
+
+    Args:
+        circuit (QuantumTape): The single circuit to simulate
+        rng (Union[None, int, array_like[int], SeedSequence, BitGenerator, Generator]): A
+            seed-like parameter matching that of ``seed`` for ``numpy.random.default_rng``.
+            If no value is provided, a default RNG will be used.
+        prng_key (Optional[jax.random.PRNGKey]): An optional ``jax.random.PRNGKey``. This is
+            the key to the JAX pseudo random number generator. If None, a random key will be
+            generated. Only for simulation using JAX.
+        debugger (_Debugger): The debugger to use
+        interface (str): The machine learning interface to create the initial state with
+        readout_errors (List[Callable]): List of channels to apply to each wire being measured
+            to simulate readout errors.
+
+    Returns:
+        tuple(TensorLike): The results of the simulation
+
+    Note that this function can return measurements for non-commuting observables simultaneously.
+
+    This function assumes that all operations provide matrices.
+    """
+    raise NotImplementedError
