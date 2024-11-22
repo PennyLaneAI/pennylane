6f3b6fa86c5b5a394bca84ee799f5d9fdf143b6b
JerryChen97 <chenys13@outlook.com>
2024-11-22 17:00:21 -0500

    try

--- pennylane/devices/qubit_mixed/simulate.py
+++ pennylane/devices/qubit_mixed/simulate.py
@@ -26,4 +26,28 @@ def simulate(  # pylint: disable=too-many-arguments
     interface=None,
     readout_errors=None,
 ) -> Result:
+    """Simulate a single quantum script.
+
+    This is an internal function that will be called by ``default.qubit.mixed``.
+
+    Args:
+        circuit (QuantumTape): The single circuit to simulate
+        rng (Union[None, int, array_like[int], SeedSequence, BitGenerator, Generator]): A
+            seed-like parameter matching that of ``seed`` for ``numpy.random.default_rng``.
+            If no value is provided, a default RNG will be used.
+        prng_key (Optional[jax.random.PRNGKey]): An optional ``jax.random.PRNGKey``. This is
+            the key to the JAX pseudo random number generator. If None, a random key will be
+            generated. Only for simulation using JAX.
+        debugger (_Debugger): The debugger to use
+        interface (str): The machine learning interface to create the initial state with
+        readout_errors (List[Callable]): List of channels to apply to each wire being measured
+            to simulate readout errors.
+
+    Returns:
+        tuple(TensorLike): The results of the simulation
+
+    Note that this function can return measurements for non-commuting observables simultaneously.
+
+    This function assumes that all operations provide matrices.
+    """
     raise NotImplementedError
