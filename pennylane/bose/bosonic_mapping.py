f4320ae2b3bd385bfb6b6d484f6e747a449cb4b9
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2024-11-25 10:52:15 -0500

    Update pennylane/bose/bosonic_mapping.py
    Co-authored-by: Austin Huang <65315367+austingmhuang@users.noreply.github.com>

--- pennylane/bose/bosonic_mapping.py
+++ pennylane/bose/bosonic_mapping.py
@@ -184,7 +184,7 @@ def unary_mapping(
         tol (float): tolerance for discarding the imaginary part of the coefficients
 
     Returns:
-        Union[PauliSentence, Operator]: A linear combination of qubit operators
+        Union[PauliSentence, Operator]: A linear combination of qubit operators.
 
     **Example**
 
