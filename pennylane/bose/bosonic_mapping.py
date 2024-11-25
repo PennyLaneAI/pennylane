6c4ece203d633b603517ed1dc4f1ebdf0bee226c
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2024-11-25 11:57:43 -0500

    Update pennylane/bose/bosonic_mapping.py
    Co-authored-by: Austin Huang <65315367+austingmhuang@users.noreply.github.com>

--- pennylane/bose/bosonic_mapping.py
+++ pennylane/bose/bosonic_mapping.py
@@ -64,15 +64,15 @@ def christiansen_mapping(
 
     Args:
         bose_operator(BoseWord, BoseSentence): the bosonic operator
-        ps (bool): whether to return the result as a PauliSentence instead of an
+        ps (bool): Whether to return the result as a PauliSentence instead of an
             operator. Defaults to False.
-        wire_map (dict): a dictionary defining how to map the states of
+        wire_map (dict): A dictionary defining how to map the states of
             the Bose operator to qubit wires. If None, integers used to
             label the bosonic states will be used as wire labels. Defaults to None.
         tol (float): tolerance for discarding the imaginary part of the coefficients
 
     Returns:
-        Union[PauliSentence, Operator]: A linear combination of qubit operators
+        Union[PauliSentence, Operator]: A linear combination of qubit operators.
     """
 
     qubit_operator = _christiansen_mapping_dispatch(bose_operator, tol)
