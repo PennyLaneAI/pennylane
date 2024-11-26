54dd010e09f475ef6028eedd272855e10153af50
Will <wmaxwell90@gmail.com>
2024-11-26 10:27:54 -0500

    type annotations

--- pennylane/labs/resource_estimation/ops/op_math/symbolic.py
+++ pennylane/labs/resource_estimation/ops/op_math/symbolic.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 r"""Resource operators for symbolic operations."""
 from collections import defaultdict
+from typing import Dict
 
 import pennylane.labs.resource_estimation as re
 from pennylane.labs.resource_estimation.resource_container import _combine_dict, _scale_dict
@@ -27,7 +28,7 @@ class ResourceAdjoint(AdjointOperation, re.ResourceOperator):
     """Resource class for Adjoint"""
 
     @staticmethod
-    def _resource_decomp(base_class, base_params, **kwargs):
+    def _resource_decomp(base_class, base_params, **kwargs) -> Dict[re.CompressedResourceOp, int]:
         try:
             return base_class.adjoint_resource_decomp(**base_params)
         except re.ResourcesNotDefined:
@@ -40,18 +41,20 @@ class ResourceAdjoint(AdjointOperation, re.ResourceOperator):
 
             return gate_types
 
-    def resource_params(self):
+    def resource_params(self) -> dict:
         return {"base_class": type(self.base), "base_params": self.base.resource_params()}
 
     @classmethod
-    def resource_rep(cls, base_class, base_params, **kwargs):
+    def resource_rep(cls, base_class, base_params, **kwargs) -> re.CompressedResourceOp:
         name = f"Adjoint({base_class.__name__})".replace("Resource", "")
         return re.CompressedResourceOp(
             cls, {"base_class": base_class, "base_params": base_params}, name=name
         )
 
     @staticmethod
-    def adjoint_resource_decomp(base_class, base_params, **kwargs):
+    def adjoint_resource_decomp(
+        base_class, base_params, **kwargs
+    ) -> Dict[re.CompressedResourceOp, int]:
         return base_class._resource_decomp(**base_params)
 
 
@@ -61,7 +64,7 @@ class ResourceControlled(ControlledOp, re.ResourceOperator):
     @staticmethod
     def _resource_decomp(
         base_class, base_params, num_ctrl_wires, num_ctrl_values, num_work_wires, **kwargs
-    ):
+    ) -> Dict[re.CompressedResourceOp, int]:
         try:
             return base_class.controlled_resource_decomp(
                 num_ctrl_wires, num_ctrl_values, num_work_wires, **base_params
@@ -80,7 +83,7 @@ class ResourceControlled(ControlledOp, re.ResourceOperator):
 
             return gate_types
 
-    def resource_params(self):
+    def resource_params(self) -> dict:
         return {
             "base_class": type(self.base),
             "base_params": self.base.resource_params(),
@@ -92,7 +95,7 @@ class ResourceControlled(ControlledOp, re.ResourceOperator):
     @classmethod
     def resource_rep(
         cls, base_class, base_params, num_ctrl_wires, num_ctrl_values, num_work_wires, **kwargs
-    ):
+    ) -> re.CompressedResourceOp:
         name = f"Controlled({base_class.__name__}, wires={num_ctrl_wires})".replace("Resource", "")
         return re.CompressedResourceOp(
             cls,
@@ -117,7 +120,7 @@ class ResourceControlled(ControlledOp, re.ResourceOperator):
         num_ctrl_wires,
         num_ctrl_values,
         num_work_wires,
-    ):
+    ) -> Dict[re.CompressedResourceOp, int]:
         return cls._resource_decomp(
             base_class,
             base_params,
@@ -131,7 +134,9 @@ class ResourcePow(PowOperation, re.ResourceOperator):
     """Resource class for Pow"""
 
     @staticmethod
-    def _resource_decomp(base_class, z, base_params, **kwargs):
+    def _resource_decomp(
+        base_class, z, base_params, **kwargs
+    ) -> Dict[re.CompressedResourceOp, int]:
         try:
             return base_class.pow_resource_decomp(z, **base_params)
         except re.ResourcesNotDefined:
@@ -144,7 +149,7 @@ class ResourcePow(PowOperation, re.ResourceOperator):
 
         return {base_class.resource_rep(): z}
 
-    def resource_params(self):
+    def resource_params(self) -> dict:
         return {
             "base_class": type(self.base),
             "z": self.z,
@@ -152,12 +157,14 @@ class ResourcePow(PowOperation, re.ResourceOperator):
         }
 
     @classmethod
-    def resource_rep(cls, base_class, z, base_params, **kwargs):
+    def resource_rep(cls, base_class, z, base_params, **kwargs) -> re.CompressedResourceOp:
         name = f"{base_class.__name__}**{z}".replace("Resource", "")
         return re.CompressedResourceOp(
             cls, {"base_class": base_class, "z": z, "base_params": base_params}, name=name
         )
 
     @classmethod
-    def pow_resource_decomp(cls, z0, base_class, z, base_params, **kwargs):
+    def pow_resource_decomp(
+        cls, z0, base_class, z, base_params, **kwargs
+    ) -> Dict[re.CompressedResourceOp, int]:
         return cls._resource_decomp(base_class, z0 * z, base_params)
