987d09de1e25b392ec9597be9ff32d55c7983e74
David Wierichs <david.wierichs@xanadu.ai>
2024-11-26 23:18:27 +0100

    Apply suggestions from code review

--- tests/ops/qubit/test_parametric_ops.py
+++ tests/ops/qubit/test_parametric_ops.py
@@ -20,7 +20,6 @@ from functools import reduce
 
 import numpy as np
 import pytest
-import torch as torch
 from gate_data import CPhaseShift00, CPhaseShift01, CPhaseShift10, Z
 from scipy import sparse
 
@@ -75,11 +74,7 @@ SINGLE_QUBIT_PARAMETRIZED_OPERATIONS = [
     qml.RZ(2.774, wires=-1),
     qml.Rot(0.123, 0.456, 0.789, wires="a"),
     qml.U1(0.123, wires="qubit1"),
-    qml.U2(
-        torch.tensor(3.556),
-        torch.tensor(2.134),
-        wires="qubit2",
-    ),
+    qml.U2(3.556, 2.134, wires="qubit2"),
     qml.U3(
         np.array(3.556),
         np.array(2.134),
