c32087519645ab147ab84465f6043ce8d245d8fe
andrijapau <andrija.paurevic@xanadu.ai>
2024-11-27 16:21:45 -0500

    fix mocking in test_resolve_execution_config.py

--- tests/workflow/test_resolve_execution_config.py
+++ tests/workflow/test_resolve_execution_config.py
@@ -23,85 +23,71 @@ from pennylane.workflow.resolution import _resolve_execution_config
 
 
 @pytest.fixture
-def mock_lightning_device():
-    """Fixture for creating a mock device with 'lightning' in its name."""
-    return qml.device("lightning.qubit", wires=2)
-
-
-@pytest.fixture
-def mock_device():
-    """Fixture for creating a generic mock device."""
-    return qml.device("default.qubit")
-
-
-@pytest.fixture
-def mock_tape_with_finite_shots():
+def tape_with_finite_shots():
     """Fixture for creating a batch of quantum tapes with finite shots."""
     return [qml.tape.QuantumScript([], [qml.expval(qml.Z(0))], shots=100)]
 
 
 @pytest.fixture
-def mock_empty_tape():
+def empty_tape():
     """Fixture for creating a batch of empty quantum tapes."""
     return [qml.tape.QuantumScript([], [qml.expval(qml.Z(0))])]
 
 
 @pytest.fixture
-def mock_metric_tensor_tp():
+def metric_tensor_tp():
     """Fixture for creating a transform program with the metric tensor transform."""
     return TransformProgram([qml.metric_tensor])
 
 
 @pytest.fixture
-def mock_empty_tp():
+def empty_tp():
     """Fixture for creating an empty transform program."""
     return TransformProgram()
 
 
-def test_resolve_execution_config_with_gradient_method(mock_device, mock_empty_tape, mock_empty_tp):
+def test_resolve_execution_config_with_gradient_method(empty_tape, empty_tp):
     """Test resolving an ExecutionConfig with a specified gradient method."""
     execution_config = ExecutionConfig(gradient_method="best", interface=None)
+    device = qml.device("default.qubit")
 
-    resolved_config = _resolve_execution_config(
-        execution_config, mock_device, mock_empty_tape, mock_empty_tp
-    )
+    resolved_config = _resolve_execution_config(execution_config, device, empty_tape, empty_tp)
 
     assert resolved_config.gradient_method == "backprop"
 
 
-def test_metric_tensor_lightning_edge_case(
-    mock_lightning_device, mock_empty_tape, mock_metric_tensor_tp
-):
+def test_metric_tensor_lightning_edge_case(empty_tape, metric_tensor_tp):
     """Test resolving an ExecutionConfig with the metric tensor transform on a lightning device."""
     execution_config = ExecutionConfig(
         gradient_method="best",
     )
+    device = qml.device("lightning.qubit", wires=2)
 
     resolved_config = _resolve_execution_config(
-        execution_config, mock_lightning_device, mock_empty_tape, mock_metric_tensor_tp
+        execution_config, device, empty_tape, metric_tensor_tp
     )
 
     assert resolved_config.gradient_method is qml.gradients.param_shift
 
 
-def test_param_shift_cv_kwargs(mock_empty_tp):
+def test_param_shift_cv_kwargs(empty_tp):
     """Test resolving an ExecutionConfig with parameter-shift and validating gradient keyword arguments."""
     dev = qml.device("default.gaussian", wires=1)
     tape = qml.tape.QuantumScript([qml.Displacement(0.5, 0.0, wires=0)])
     execution_config = ExecutionConfig(gradient_method="parameter-shift")
 
-    resolved_config = _resolve_execution_config(execution_config, dev, [tape], mock_empty_tp)
+    resolved_config = _resolve_execution_config(execution_config, dev, [tape], empty_tp)
 
     assert resolved_config.gradient_keyword_arguments["dev"] == dev
 
 
-def test_mcm_config_validation(mock_device, mock_empty_tape, mock_empty_tp):
+def test_mcm_config_validation(empty_tape, empty_tp):
     """Test validation of MCMConfig within an ExecutionConfig."""
     mcm_config = MCMConfig(postselect_mode="hw-like")
     execution_config = ExecutionConfig(mcm_config=mcm_config, interface=None)
-    resolved_config = _resolve_execution_config(
-        execution_config, mock_device, mock_empty_tape, mock_empty_tp
-    )
+    device = qml.device("default.qubit")
+
+    resolved_config = _resolve_execution_config(execution_config, device, empty_tape, empty_tp)
 
     expected_mcm_config = MCMConfig(postselect_mode=None)
 
@@ -111,14 +97,14 @@ def test_mcm_config_validation(mock_device, mock_empty_tape, mock_empty_tp):
 @pytest.mark.parametrize("mcm_method", [None, "one-shot"])
 @pytest.mark.parametrize("postselect_mode", [None, "hw-like"])
 @pytest.mark.jax
-def test_jax_interface(
-    mcm_method, postselect_mode, mock_device, mock_tape_with_finite_shots, mock_empty_tp
-):
+def test_jax_interface(mcm_method, postselect_mode, tape_with_finite_shots, empty_tp):
     """Test resolving ExecutionConfig with JAX interface and different MCMConfig settings."""
     mcm_config = MCMConfig(mcm_method=mcm_method, postselect_mode=postselect_mode)
     execution_config = ExecutionConfig(mcm_config=mcm_config, interface="jax")
+    device = qml.device("default.qubit")
+
     resolved_config = _resolve_execution_config(
-        execution_config, mock_device, mock_tape_with_finite_shots, mock_empty_tp
+        execution_config, device, tape_with_finite_shots, empty_tp
     )
 
     expected_mcm_config = MCMConfig(mcm_method, postselect_mode="pad-invalid-samples")
@@ -127,13 +113,13 @@ def test_jax_interface(
 
 
 @pytest.mark.jax
-def test_jax_jit_interface(mock_device, mock_empty_tape, mock_empty_tp):
+def test_jax_jit_interface(empty_tape, empty_tp):
     """Test resolving ExecutionConfig with JAX-JIT interface and deferred MCMConfig method."""
     mcm_config = MCMConfig(mcm_method="deferred")
     execution_config = ExecutionConfig(mcm_config=mcm_config, interface="jax-jit")
-    resolved_config = _resolve_execution_config(
-        execution_config, mock_device, mock_empty_tape, mock_empty_tp
-    )
+    device = qml.device("default.qubit")
+
+    resolved_config = _resolve_execution_config(execution_config, device, empty_tape, empty_tp)
 
     expected_mcm_config = MCMConfig(mcm_method="deferred", postselect_mode="fill-shots")
 
