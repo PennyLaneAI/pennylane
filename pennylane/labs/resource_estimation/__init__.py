8e2da5606476ce36db1ce131b6cfa183e09529b2
Jay Soni <jbsoni@uwaterloo.ca>
2024-11-27 11:19:58 -0500

    complete merge

--- pennylane/labs/resource_estimation/__init__.py
+++ pennylane/labs/resource_estimation/__init__.py
@@ -120,10 +120,7 @@ Exceptions
 """
 
 from .resource_operator import ResourceOperator, ResourcesNotDefined
-<<<<<<< HEAD
-from .resource_container import CompressedResourceOp, Resources
 from .resource_tracking import DefaultGateSet, get_resources, resource_config
-=======
 
 from .resource_container import (
     CompressedResourceOp,
@@ -134,7 +131,6 @@ from .resource_container import (
     mul_in_parallel,
     substitute,
 )
->>>>>>> labs-resource-sub
 
 from .ops import (
     ResourceAdjoint,
