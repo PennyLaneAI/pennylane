1b221a63998efed35aa0dfc8dc00521e265dd98c
JerryChen97 <chenys13@outlook.com>
2024-11-27 14:01:50 -0500

    add sim to init

--- pennylane/devices/qubit_mixed/__init__.py
+++ pennylane/devices/qubit_mixed/__init__.py
@@ -26,3 +26,4 @@ at your own discretion.
 from .apply_operation import apply_operation
 from .initialize_state import create_initial_state
 from .measure import measure
+from .simulate import get_final_state, measure_final_state, simulate
