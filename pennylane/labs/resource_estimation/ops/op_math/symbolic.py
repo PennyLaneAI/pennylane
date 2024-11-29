e305474286960266d02ba4a716b67d5e63d46b49
Will <wmaxwell90@gmail.com>
2024-11-29 14:54:38 -0500

    redo control decomposition logic

--- pennylane/labs/resource_estimation/ops/op_math/symbolic.py
+++ pennylane/labs/resource_estimation/ops/op_math/symbolic.py
@@ -61,9 +61,9 @@ class ResourceAdjoint(AdjointOperation, re.ResourceOperator):
 class ResourceControlled(ControlledOp, re.ResourceOperator):
     """Resource class for Controlled"""
 
-    @staticmethod
+    @classmethod
     def _resource_decomp(
-        base_class, base_params, num_ctrl_wires, num_ctrl_values, num_work_wires, **kwargs
+        cls, base_class, base_params, num_ctrl_wires, num_ctrl_values, num_work_wires, **kwargs
     ) -> Dict[re.CompressedResourceOp, int]:
         try:
             return base_class.controlled_resource_decomp(
@@ -72,16 +72,22 @@ class ResourceControlled(ControlledOp, re.ResourceOperator):
         except re.ResourcesNotDefined:
             pass
 
-        gate_types = defaultdict(int)
-        decomp = base_class.resources(**base_params, **kwargs)
-        for gate, count in decomp.items():
-            resources = gate.op_type.controlled_resource_decomp(
-                num_ctrl_wires, num_ctrl_values, num_work_wires, **gate.params
-            )
-            _scale_dict(resources, count, in_place=True)
-            _combine_dict(gate_types, resources, in_place=True)
+        gate_types = {}
+
+        if num_ctrl_values == 0:
+            decomp = base_class.resources(**base_params, **kwargs)
+            for gate, count in decomp.items():
+                rep = cls.resource_rep(gate.op_type, gate.params, num_ctrl_wires, 0, num_work_wires)
+                gate_types[rep] = count
 
             return gate_types
+
+        no_control = cls.resource_rep(base_class, base_params, num_ctrl_wires, 0, num_work_wires)
+        x = re.ResourceX.resource_rep()
+        gate_types[no_control] = 1
+        gate_types[x] = 2*num_ctrl_values
+
+        return gate_types
 
     def resource_params(self) -> dict:
         return {
@@ -177,3 +183,34 @@ class ResourcePow(PowOperation, re.ResourceOperator):
     def tracking_name(base_class, z, base_params) -> str:
         base_name = base_class.tracking_name(**base_params)
         return f"({base_name})**{z}"
+
+def zyz_resources(num_ctrl_wires, num_work_wires):
+    """The zyz decomposition of a controlled unitary from SU(2)
+    defined in https://arxiv.org/pdf/quant-ph/9503016"""
+
+    gate_types = {}
+
+    # Lemma 5.1
+    if num_ctrl_wires == 1:
+        cnot = re.ResourceCNOT.resource_rep()
+        phase = re.ResourceGlobalPhase.resource_rep()
+        ry = re.ResourceRZ.resource_rep()
+        rz = re.ResourceRZ.resource_rep()
+
+        gate_types[cnot] = 2
+        gate_types[phase] = 1
+        gate_types[ry] = 2
+        gate_types[rz] = 3
+
+        return gate_types
+
+    # Lemma 7.9
+    cry = re.ResourceCRY.resource_rep()
+    crz = re.ResourceCRZ.resource_rep()
+    multix = re.ResourceMultiControlledX.resource_rep(num_ctrl_wires-1, 0, num_work_wires)
+
+    gate_types[cry] = 2
+    gate_types[crz] = 3
+    gate_types[multix] = 2
+
+    return gate_types
