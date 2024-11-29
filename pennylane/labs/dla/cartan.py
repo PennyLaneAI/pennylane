68aff6c50c5308541508aca4b778711cfed5fbef
qottmann <korbinian.kottmann@gmail.com>
2024-11-29 15:32:36 +0100

    doc

--- pennylane/labs/dla/cartan.py
+++ pennylane/labs/dla/cartan.py
@@ -44,8 +44,8 @@ def cartan_decomp(g, involution):
             E.g., :func:`~even_odd_involution` or :func:`~concurrence_involution`.
 
     Returns:
-        k (List[Union[PauliSentence, Operator]]): the even parity subspace :math:`\Theta(\mathfrak{k}) = \mathfrak{k}`
-        m (List[Union[PauliSentence, Operator]]): the odd parity subspace :math:`\Theta(\mathfrak{m}) = -\mathfrak{m}`
+        k (List[Union[PauliSentence, Operator]]): the even parity subspace :math:`\Theta(\mathfrak{k}) = \mathfrak{k}`.
+        m (List[Union[PauliSentence, Operator]]): the odd parity subspace :math:`\Theta(\mathfrak{m}) = -\mathfrak{m}`.
 
     .. seealso:: :func:`~even_odd_involution`, :func:`~concurrence_involution`, :func:`~check_cartan_decomp`
 
