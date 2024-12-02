12fbdd2f8130ddbab38590e20a1096ba5dca78ba
Yushao Chen (Jerry) <chenys13@outlook.com>
2024-12-02 10:03:56 -0500

    Update pennylane/devices/qubit_mixed/measure.py
    Co-authored-by: Mudit Pandey <mudit.pandey@xanadu.ai>

--- pennylane/devices/qubit_mixed/measure.py
+++ pennylane/devices/qubit_mixed/measure.py
@@ -286,4 +286,4 @@ def measure(
         Tensorlike: the result of the measurement process being applied to the state.
     """
     measurement_function = get_measurement_function(measurementprocess)
-    return measurement_function(measurementprocess, state, is_state_batched, readout_errors)
+    return measurement_function(measurementprocess, state, is_state_batched=is_state_batched, readout_errors=readout_errors)
