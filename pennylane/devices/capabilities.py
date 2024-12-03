a8de60aa2e6992bbdeefcdea19707678ae4aabbb
Astral Cai <astral.cai@xanadu.ai>
2024-12-03 10:14:07 -0500

    sphinx

--- pennylane/devices/capabilities.py
+++ pennylane/devices/capabilities.py
@@ -19,7 +19,7 @@ import sys
 from dataclasses import dataclass, field, replace
 from enum import Enum
 from itertools import repeat
-from typing import Callable, Optional
+from typing import Callable, Optional, Union
 
 import pennylane as qml
 from pennylane.operation import Operator
@@ -176,12 +176,12 @@ class DeviceCapabilities:  # pylint: disable=too-many-instance-attributes
         update_device_capabilities(capabilities, document, runtime_interface)
         return capabilities
 
-    def supports_operation(self, operation: str | Operator) -> bool:
+    def supports_operation(self, operation: Union[str, Operator]) -> bool:
         """Checks if the given operation is supported by name."""
         operation_name = operation if isinstance(operation, str) else operation.name
         return bool(_get_supported_base_op(operation_name, self.operations))
 
-    def supports_observable(self, observable: str | Operator) -> bool:
+    def supports_observable(self, observable: Union[str, Operator]) -> bool:
         """Checks if the given observable is supported by name."""
         observable_name = observable if isinstance(observable, str) else observable.name
         return bool(_get_supported_base_op(observable_name, self.observables))
