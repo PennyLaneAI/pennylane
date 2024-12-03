bbc0a848969d0c190f04fb4df96cde7f4ca09a50
Will <wmaxwell90@gmail.com>
2024-12-03 08:52:38 -0500

    adding circuit diagrams to docstrings

--- pennylane/labs/resource_estimation/ops/qubit/parametric_ops_multi_qubit.py
+++ pennylane/labs/resource_estimation/ops/qubit/parametric_ops_multi_qubit.py
@@ -108,6 +108,13 @@ class ResourceIsingXX(qml.IsingXX, re.ResourceOperator):
                 0 & -i \sin(\phi / 2) & \cos(\phi / 2) & 0 \\
                 -i \sin(\phi / 2) & 0 & 0 & \cos(\phi / 2)
             \end{bmatrix}.
+
+        The circuit implementing this transformation is given by
+
+        .. code-block:: bash
+
+            0: ─╭●──RX(0.10)─╭●─┤  
+            1: ─╰X───────────╰X─┤ 
     """
 
     @staticmethod
@@ -142,6 +149,13 @@ class ResourceIsingYY(qml.IsingYY, re.ResourceOperator):
                 0 & -i \sin(\phi / 2) & \cos(\phi / 2) & 0 \\
                 i \sin(\phi / 2) & 0 & 0 & \cos(\phi / 2)
             \end{bmatrix}.
+
+        The circuit implementing this transoformation is given by
+
+        .. code-block: bash
+
+            0: ─╭●──RY(0.10)─╭●─┤  
+            1: ─╰Y───────────╰Y─┤  
     """
 
     @staticmethod
@@ -176,6 +190,13 @@ class ResourceIsingXY(qml.IsingXY, re.ResourceOperator):
                 0 & i \sin(\phi / 2) & \cos(\phi / 2) & 0 \\
                 0 & 0 & 0 & 1
             \end{bmatrix}.
+
+        The circuit implementing this gate is given by
+
+        .. code-block:: bash
+
+            0: ──H─╭●──RY(0.05)──╭●──H─┤  
+            1: ────╰Y──RX(-0.05)─╰Y────┤ 
     """
 
     @staticmethod
@@ -214,6 +235,13 @@ class ResourceIsingZZ(qml.IsingZZ, re.ResourceOperator):
                 0 & 0 & e^{i \phi / 2} & 0 \\
                 0 & 0 & 0 & e^{-i \phi / 2}
             \end{bmatrix}.
+
+        The circuit implmenting this transformation is given by
+
+        .. code-block:: bash
+
+            0: ─╭●───────────╭●─┤  
+            1: ─╰X──RZ(0.10)─╰X─┤ 
     """
 
     @staticmethod
@@ -245,6 +273,13 @@ class ResourcePSWAP(qml.PSWAP, re.ResourceOperator):
                 0 & e^{i \phi} & 0 & 0 \\
                 0 & 0 & 0 & 1
             \end{bmatrix}.
+
+        The circuit implementing this transformation is given by
+
+        .. code-block:: bash
+
+            0: ─╭SWAP─╭●───────────╭●─┤  
+            1: ─╰SWAP─╰X──Rϕ(0.10)─╰X─┤ 
     """
 
     @staticmethod
