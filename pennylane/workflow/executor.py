a835d34e2c75f2ffddf540f3505caceccb50a181
Lee J. O'Riordan <lee@xanadu.ai>
2024-12-04 18:06:04 -0500

    Add dask executor

--- pennylane/workflow/executor.py
+++ pennylane/workflow/executor.py
@@ -17,6 +17,8 @@ Contains concurrent executor abstractions for task-based workloads.
 
 import abc
 from collections.abc import Callable, Sequence
+from functools import singledispatchmethod
+from types import NoneType
 
 class RemoteExecABC(abc.ABC):
     """
@@ -80,13 +82,45 @@ class MPICommExec(ExtExecABC):
                 raise RuntimeError(f"Failed to start executor {self._exec_backend}")
         return output_f
 
-
 class DaskExec(ExtExecABC):
-    pass
+    """
+    Dask distributed abstraction class functor.
+    """
+    from dask.distributed.deploy import Cluster
+
+    @singledispatch
+    def __init__(self, client_provider = None, max_workers = 4):
+        from dask.distributed import Client, LocalCluster
+        cluster = LocalCluster(n_workers=max_workers, processes=True)
+        self._exec_backend = Client(cluster)
+
+    @__init__.register
+    def _url_scheduler(self, client_provider: str):
+        from dask.distributed import Client
+        self._exec_backend = Client(client_provider)
+
+    @__init__.register
+    def _cluster_provider(self, client_provider: Cluster):
+        self._exec_backend = client_provider.get_client()
+
+    def __call__(self, fn: Callable, data: Sequence):
+        output_f = self._exec_backend.map(fn, data)
+        return [o.result() for o in output_f]
+
 
 class RayExec(ExtExecABC):
     pass
 
 class ProcPoolExec(IntExecABC):
-    pass
+    """
+    concurrent.futures.ProcessPoolExecutor abstraction class functor.
+    """
+    def __init__(self, max_workers=None):
+        from concurrent.futures import ProcessPoolExecutor
+        self._exec_backend = ProcessPoolExecutor
+        self._max_workers = max_workers
 
+    def __call__(self, fn: Callable, data: Sequence):
+        with self._exec_backend(max_workers=self._max_workers) as executor:
+            output_f = executor.map(fn, data)
+        return output_f
