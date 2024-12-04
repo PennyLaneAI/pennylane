4ed272b768b83a0601d3a70b4c2cacec27aeecec
Lee J. O'Riordan <lee@xanadu.ai>
2024-12-04 17:00:24 -0500

    Add prelim executor abstractions

---
+++ pennylane/workflow/executor.py
@@ -0,0 +1,92 @@
+# Copyright 2018-2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Contains concurrent executor abstractions for task-based workloads.
+"""
+
+import abc
+from collections.abc import Callable, Sequence
+
+class RemoteExecABC(abc.ABC):
+    """
+    Abstract base class for defining a task-based parallel executor backend for running python functions
+    """
+    def __init__(self, *args, **kwargs):
+        ...
+    @abc.abstractmethod
+    def __call__(self, fn: Callable, data: Sequence):
+        """
+            fn:     the callable function to run on the executor backend
+            data:   is a sequence where each work-item is a packaged chunk for execution
+        """
+        ...
+
+class IntExecABC(RemoteExecABC, abc.ABC):
+    """
+    Executor class for native Python library concurrency support
+    """
+    pass
+
+class ExtExecABC(RemoteExecABC, abc.ABC):
+    """
+    Executor class for external package provided concurrency support
+    """
+    pass
+
+
+class MPIPoolExec(ExtExecABC):
+    """
+    MPIPoolExecutor abstraction class functor.
+    """
+
+    def __init__(self):
+        from mpi4py import MPI  # Required to call MPI_Init
+        from mpi4py.futures import MPIPoolExecutor as executor
+        self._exec_backend = executor
+
+    def __call__(self, fn: Callable, data: Sequence):
+        kwargs = {"use_pkl5": True}
+        with self._exec_backend(**kwargs) as executor:
+            output_f = executor.map(fn, data)
+        return output_f
+
+class MPICommExec(ExtExecABC):
+    """
+    MPICommExecutor abstraction class functor.
+    """
+    def __init__(self):
+        from mpi4py import MPI  # Required to call MPI_Init
+        from mpi4py.futures import MPICommExecutor as executor
+        self._exec_backend = executor
+        self._comm = MPI.COMM_WORLD
+
+    def __call__(self, fn: Callable, data: Sequence):
+        kwargs = {"use_pkl5": True}
+        with self._exec_backend(self._comm, root=0) as executor:
+            if executor is not None:
+                output_f = executor.map(fn, data)
+            else:
+                raise RuntimeError(f"Failed to start executor {self._exec_backend}")
+        return output_f
+
+
+class DaskExec(ExtExecABC):
+    pass
+
+class RayExec(ExtExecABC):
+    pass
+
+class ProcPoolExec(IntExecABC):
+    pass
+
