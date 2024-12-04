ed55ada0f282d105815f22d59dd2a7383514c84d
Austin Huang <gengminghuang@gmail.com>
2024-12-04 11:38:16 -0500

    fix again

--- pennylane/qchem/vibrational/vibrational_class.py
+++ pennylane/qchem/vibrational/vibrational_class.py
@@ -25,6 +25,8 @@ from ..openfermion_pyscf import _import_pyscf
 
 # pylint: disable=import-outside-toplevel, unused-variable, too-many-instance-attributes, too-many-arguments
 
+BOHR_TO_ANG = 0.5291772106  # factor to convert bohr to angstrom
+
 
 @dataclass
 class VibrationalPES:
