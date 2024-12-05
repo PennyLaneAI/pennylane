9b52a87b5c15193d9e847f02c2fb3ac0a30400b0
Mudit Pandey <mudit.pandey@xanadu.ai>
2024-12-05 10:07:47 -0500

    [skip ci] Remove commented code

--- pennylane/math/utils.py
+++ pennylane/math/utils.py
@@ -406,9 +406,6 @@ def is_abstract(tensor, like=None):
     if interface == "jax":
         import jax
 
-        # from jax.interpreters.partial_eval import DynamicJaxprTracer
-        # from pennylane.capture import TransformTracer
-
         if isinstance(
             tensor,
             (
@@ -422,7 +419,6 @@ def is_abstract(tensor, like=None):
             # Otherwise, it will be abstract.
             return not isinstance(tensor.aval, jax.core.ConcreteArray)
 
-        # return isinstance(tensor, (DynamicJaxprTracer, TransformTracer))
         return isinstance(tensor, jax.core.Tracer)
 
     if interface == "tensorflow":
