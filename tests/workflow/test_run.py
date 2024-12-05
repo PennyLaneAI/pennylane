dd003e05c5fa433f797aab39891d29c2a8fc9b1b
andrijapau <andrija.paurevic@xanadu.ai>
2024-12-05 10:01:25 -0500

    feat: Add tests to test_run.py

--- tests/workflow/test_run.py
+++ tests/workflow/test_run.py
@@ -13,3 +13,49 @@
 # limitations under the License.
 
 """Unit tests for the `run` helper function in the `qml.workflow` module."""
+
+import pytest
+
+import pennylane as qml
+from pennylane.devices import ExecutionConfig
+from pennylane.numpy import pi as PI
+from pennylane.tape import QuantumScript
+from pennylane.transforms.core import TransformContainer, TransformProgram
+from pennylane.transforms.optimization import merge_rotations
+from pennylane.workflow import run
+
+
+@pytest.mark.parametrize(
+    "interface, gradient_method",
+    (["numpy", qml.gradients.param_shift], ["torch", None], ["torch", "backprop"]),
+)
+def test_no_interface_boundary_required(interface, gradient_method):
+    """Test that tapes are executed without an ML boundary if no interface boundary required."""
+    container = TransformContainer(merge_rotations)
+    inner_tp = TransformProgram((container,))
+    device = qml.device("default.qubit")
+    tapes = [QuantumScript([qml.RX(PI, wires=0), qml.RX(PI, wires=0)], [qml.expval(qml.PauliZ(0))])]
+    config = ExecutionConfig(interface=interface, gradient_method=gradient_method)
+    results = run(tapes, device, config, inner_tp)
+
+    assert qml.math.get_interface(results) == "numpy"
+    assert qml.math.allclose(results[0], 1.0)
+
+
+def test_grad_on_execution_error():
+    """Tests that a ValueError is raised if the config uses grad_on_execution."""
+    inner_tp = TransformProgram()
+    device = qml.device("default.qubit")
+    tapes = [QuantumScript([qml.RX(PI, wires=0), qml.RX(PI, wires=0)], [qml.expval(qml.PauliZ(0))])]
+    config = ExecutionConfig(
+        interface="torch",
+        gradient_method=qml.gradients.param_shift,
+        grad_on_execution=True,
+        use_device_jacobian_product=False,
+        use_device_gradient=False,
+    )
+
+    with pytest.raises(
+        ValueError, match="Gradient transforms cannot be used with grad_on_execution=True"
+    ):
+        run(tapes, device, config, inner_tp)
