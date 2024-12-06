c3a40d5e6e4c16564e4ff9e2d9c54543d2e8c252
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2024-12-06 09:57:57 -0500

    Update pennylane/templates/subroutines/gqsp.py
    Co-authored-by: Soran Jahangiri <40344468+soranjh@users.noreply.github.com>

--- pennylane/templates/subroutines/gqsp.py
+++ pennylane/templates/subroutines/gqsp.py
@@ -55,7 +55,7 @@ class GQSP(Operation):
 
     .. code-block:: python
 
-        # P(x) = -0.1 + 0.2j x + 0.3 x^2
+        # P(x) = 0.1 + 0.2j x + 0.3 x^2
         poly = [0.1, 0.2j, 0.3]
 
         angles = qml.poly_to_angles(poly, "GQSP")
