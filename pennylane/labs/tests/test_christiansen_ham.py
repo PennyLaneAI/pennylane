c3607509f576e1fb4f72166c682b38de63119de3
Austin Huang <gengminghuang@gmail.com>
2024-12-09 14:26:07 -0500

    what

---
+++ pennylane/labs/tests/test_christiansen_ham.py
@@ -0,0 +1,23 @@
+# Copyright 2018-2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit Tests for the Christiansen Hamiltonian construction functions."""
+
+from pathlib import Path
+
+import pytest
+
+
+from pennylane.labs.vibrational.christiansen_ham import christiansen_bosonic, christiansen_hamiltonian, christiansen_dipole, christiansen_integrals, christiansen_integrals_dipole
+
+cform_file = Path(__file__).resolve().parent / "test_ref_files" / "H2S.hdf5"
