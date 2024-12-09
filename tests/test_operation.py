89197f48429e89c013a138c7c9eff3bae3200393
Astral Cai <astral.cai@xanadu.ai>
2024-12-09 17:02:46 -0500

    more

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1798,7 +1798,6 @@ class TestHamiltonianLinearCombinationAlias:
 @pytest.mark.parametrize(
     "op",
     [
-        # pytest.param(qml.CZ(wires=[1, 0]), marks=pytest.mark.xfail),
         qml.CZ(wires=[1, 0]),
         qml.CCZ(wires=[2, 0, 1]),
         qml.SWAP(wires=[1, 0]),
