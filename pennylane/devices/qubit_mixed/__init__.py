34c9365d038ca989c63ae15801246d82c2ea1e29
Yushao Chen (Jerry) <chenys13@outlook.com>
2024-12-09 15:18:00 -0500

    Update pennylane/devices/qubit_mixed/__init__.py

--- pennylane/devices/qubit_mixed/__init__.py
+++ pennylane/devices/qubit_mixed/__init__.py
@@ -24,8 +24,8 @@ at your own discretion.
     apply_operation
     create_initial_state
     measure
-    simulate
     sampling
+    simulate
 """
 from .apply_operation import apply_operation
 from .initialize_state import create_initial_state
