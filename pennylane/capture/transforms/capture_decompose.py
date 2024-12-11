6ba44de7c472f7dcf81bab4914d3584375e212c5
Mudit Pandey <mudit.pandey@xanadu.ai>
2024-12-11 14:09:24 -0500

    Update pennylane/capture/transforms/capture_decompose.py
    Co-authored-by: Pietropaolo Frisoni <pietropaolo.frisoni@xanadu.ai>

--- pennylane/capture/transforms/capture_decompose.py
+++ pennylane/capture/transforms/capture_decompose.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-A transform for decomposing quantum circuits into user defined gate sets. Offers an alternative to the more device-focused decompose transform.
+A transform for decomposing quantum circuits into user-defined gate sets. Offers an alternative to the more device-focused decompose transform.
 """
 # pylint: disable=protected-access
 # pylint: disable=unnecessary-lambda-assignment
