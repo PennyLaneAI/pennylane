818098f8225142f547ae86b76c8abcc333867731
andrijapau <andrija.paurevic@xanadu.ai>
2024-12-11 09:27:17 -0500

    feat: Add more testing

--- tests/workflow/test_run.py
+++ tests/workflow/test_run.py
@@ -17,6 +17,7 @@
 # pylint: disable=too-few-public-methods
 from dataclasses import replace
 
+import autograd
 import numpy as np
 import pytest
 from param_shift_dev import ParamShiftDerivativesDevice
@@ -44,6 +45,7 @@ def get_device(device_name, seed):
 
 # Create the device and execution configurations
 test_matrix = [
+    # 0
     [
         "default.qubit",
         replace(
@@ -52,6 +54,7 @@ test_matrix = [
         ),
         Shots((100000, 100000)),
     ],
+    # 1
     [
         "default.qubit",
         replace(
@@ -60,6 +63,7 @@ test_matrix = [
         ),
         Shots(100000),
     ],
+    # 2
     [
         "default.qubit",
         replace(
@@ -68,6 +72,7 @@ test_matrix = [
         ),
         Shots(None),
     ],
+    # 3
     [
         "default.qubit",
         replace(
@@ -76,14 +81,17 @@ test_matrix = [
         ),
         Shots(None),
     ],
+    # 4
     [
         "default.qubit",
         replace(
             DefaultExecutionConfig,
             gradient_method="adjoint",
+            use_device_jacobian_product=True,
         ),
         Shots(None),
     ],
+    # 5
     [
         "default.qubit",
         replace(
@@ -91,6 +99,7 @@ test_matrix = [
         ),
         Shots(None),
     ],
+    # 6
     [
         "reference.qubit",
         replace(
@@ -99,6 +108,7 @@ test_matrix = [
         ),
         Shots((100000, 100000)),
     ],
+    # 7
     [
         "reference.qubit",
         replace(
@@ -107,6 +117,7 @@ test_matrix = [
         ),
         Shots(100000),
     ],
+    # 8
     [
         "reference.qubit",
         replace(
@@ -115,13 +126,35 @@ test_matrix = [
         ),
         Shots(None),
     ],
+    # 9
     [
         "param_shift.qubit",
         replace(
             DefaultExecutionConfig,
             gradient_method="device",
+            use_device_jacobian_product=False,
         ),
         Shots((100000, 100000)),
+    ],
+    # 10
+    [
+        "param_shift.qubit",
+        replace(
+            DefaultExecutionConfig,
+            gradient_method="device",
+            use_device_jacobian_product=False,
+        ),
+        Shots(100000),
+    ],
+    # 11
+    [
+        "param_shift.qubit",
+        replace(
+            DefaultExecutionConfig,
+            gradient_method="device",
+            use_device_jacobian_product=False,
+        ),
+        Shots(None),
     ],
 ]
 
@@ -172,6 +205,13 @@ torch = pytest.importorskip("torch")
 @pytest.mark.torch
 @pytest.mark.parametrize("device, config, shots", test_matrix)
 class TestTorchRun:
+
+    @pytest.fixture(autouse=True)
+    def run_before_and_after_tests(self):
+        torch.set_default_dtype(torch.float64)
+        yield
+        torch.set_default_dtype(torch.float32)
+
     def test_run(self, device, config, shots, seed):
         """Test execution of tapes on 'torch' interface."""
         device = get_device(device, seed=seed)
@@ -217,6 +257,90 @@ class TestTorchRun:
             for wire in range(2):
                 assert qml.math.allclose(res[wire], exp, atol=atol_for_shots(shots))
 
+    def test_scalar_jacobian(self, device, config, shots, seed):
+        """Test scalar jacobian calculation"""
+        device = get_device(device, seed)
+        config = replace(config, interface="torch")
+
+        def cost(a):
+            tape = qml.tape.QuantumScript([qml.RY(a, 0)], [qml.expval(qml.PauliZ(0))], shots=shots)
+            resolved_config = _resolve_execution_config(config, device, [tape], TransformProgram())
+            _, inner_tp = _setup_transform_program(TransformProgram(), device, resolved_config)
+            return run([tape], device, resolved_config, inner_tp)[0]
+
+        a = torch.tensor(0.1, requires_grad=True)
+        print(cost(a))
+        res = torch.autograd.functional.jacobian(cost, a)
+        if not shots.has_partitioned_shots:
+            assert res.shape == ()  # pylint: disable=no-member
+
+        # compare to standard tape jacobian
+        tape = qml.tape.QuantumScript([qml.RY(a, wires=0)], [qml.expval(qml.PauliZ(0))])
+        tape.trainable_params = [0]
+        tapes, fn = qml.gradients.param_shift(tape)
+        expected = fn(device.execute(tapes))
+
+        assert expected.shape == ()
+        if shots.has_partitioned_shots:
+            for i in range(shots.num_copies):
+                assert torch.allclose(res[i], expected, atol=atol_for_shots(shots), rtol=0)
+                assert torch.allclose(res[i], -torch.sin(a), atol=atol_for_shots(shots))
+        else:
+            assert torch.allclose(res, expected, atol=atol_for_shots(shots), rtol=0)
+            assert torch.allclose(res, -torch.sin(a), atol=atol_for_shots(shots))
+
+    def test_jacobian(self, device, config, shots, seed):
+        """Test jacobian calculation"""
+        device = get_device(device, seed)
+        config = replace(config, interface="torch")
+
+        def cost(a, b):
+            ops = [qml.RY(a, wires=0), qml.RX(b, wires=1), qml.CNOT(wires=[0, 1])]
+            m = [qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliY(1))]
+            tape = qml.tape.QuantumScript(ops, m, shots=shots)
+
+            resolved_config = _resolve_execution_config(config, device, [tape], TransformProgram())
+            _, inner_tp = _setup_transform_program(TransformProgram(), device, resolved_config)
+            res = run([tape], device, resolved_config, inner_tp)[0]
+
+            if shots.has_partitioned_shots:
+                return torch.hstack(res[0] + res[1])
+
+            return torch.hstack(res)
+
+        a = torch.tensor(0.1, requires_grad=True)
+        b = torch.tensor(0.2, requires_grad=True)
+
+        res = cost(a, b)
+        expected = torch.tensor([torch.cos(a), -torch.cos(a) * torch.sin(b)])
+        if shots.has_partitioned_shots:
+            assert torch.allclose(res[:2], expected, atol=atol_for_shots(shots), rtol=0)
+            assert torch.allclose(res[2:], expected, atol=atol_for_shots(shots), rtol=0)
+        else:
+            assert torch.allclose(res, expected, atol=atol_for_shots(shots), rtol=0)
+
+        res = torch.autograd.functional.jacobian(cost, (a, b))
+        assert isinstance(res, tuple) and len(res) == 2
+
+        expected = (
+            torch.tensor([-torch.sin(a), torch.sin(a) * torch.sin(b)]),
+            torch.tensor([0, -torch.cos(a) * torch.cos(b)]),
+        )
+        if shots.has_partitioned_shots:
+            assert res[0].shape == (4,)
+            assert res[1].shape == (4,)
+
+            for _r, _e in zip(res, expected):
+                assert torch.allclose(_r[:2], _e, atol=atol_for_shots(shots))
+                assert torch.allclose(_r[2:], _e, atol=atol_for_shots(shots))
+
+        else:
+            assert res[0].shape == (2,)
+            assert res[1].shape == (2,)
+
+            for _r, _e in zip(res, expected):
+                assert torch.allclose(_r, _e, atol=atol_for_shots(shots))
+
 
 @pytest.mark.parametrize("device, config, shots", test_matrix)
 class TestAutogradRun:
@@ -259,6 +383,75 @@ class TestAutogradRun:
 
         assert qml.math.allclose(res[0], np.cos(a) * np.cos(b), atol=atol_for_shots(shots))
         assert qml.math.allclose(res[1], np.cos(a) * np.cos(b), atol=atol_for_shots(shots))
+
+    def test_scalar_jacobian(self, device, config, shots, seed):
+        """Test scalar jacobian calculation"""
+        device = get_device(device, seed=seed)
+        config = replace(config, interface="autograd")
+
+        def cost(a):
+            tape = qml.tape.QuantumScript([qml.RY(a, 0)], [qml.expval(qml.PauliZ(0))], shots=shots)
+            resolved_config = _resolve_execution_config(config, device, [tape], TransformProgram())
+            inner_tp = _setup_transform_program(TransformProgram(), device, resolved_config)[1]
+            return run([tape], device, resolved_config, inner_tp)[0]
+
+        a = pnp.array(0.1, requires_grad=True)
+        if shots.has_partitioned_shots:
+            res = qml.jacobian(lambda x: qml.math.hstack(cost(x)))(a)
+        else:
+            res = qml.jacobian(cost)(a)
+            assert res.shape == ()  # pylint: disable=no-member
+
+        # compare to standard tape jacobian
+        tape = qml.tape.QuantumScript([qml.RY(a, wires=0)], [qml.expval(qml.PauliZ(0))])
+        tape.trainable_params = [0]
+        tapes, fn = qml.gradients.param_shift(tape)
+        expected = fn(device.execute(tapes))
+
+        assert expected.shape == ()
+        assert np.allclose(res, expected, atol=atol_for_shots(shots), rtol=0)
+        assert np.allclose(res, -np.sin(a), atol=atol_for_shots(shots))
+
+    def test_jacobian(self, device, config, shots, seed):
+        """Test jacobian calculation"""
+        device = get_device(device, seed=seed)
+        config = replace(config, interface="autograd")
+
+        def cost(a, b):
+            ops = [qml.RY(a, wires=0), qml.RX(b, wires=1), qml.CNOT(wires=[0, 1])]
+            m = [qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliY(1))]
+            tape = qml.tape.QuantumScript(ops, m, shots=shots)
+            resolved_config = _resolve_execution_config(config, device, [tape], TransformProgram())
+            inner_tp = _setup_transform_program(TransformProgram(), device, resolved_config)[1]
+            return autograd.numpy.hstack(run([tape], device, resolved_config, inner_tp)[0])
+
+        a = pnp.array(0.1, requires_grad=True)
+        b = pnp.array(0.2, requires_grad=True)
+        res = cost(a, b)
+        expected = [np.cos(a), -np.cos(a) * np.sin(b)]
+        if shots.has_partitioned_shots:
+            assert np.allclose(res[:2], expected, atol=atol_for_shots(shots), rtol=0)
+            assert np.allclose(res[2:], expected, atol=atol_for_shots(shots), rtol=0)
+        else:
+            assert np.allclose(res, expected, atol=atol_for_shots(shots), rtol=0)
+
+        res = qml.jacobian(cost)(a, b)
+        assert isinstance(res, tuple) and len(res) == 2
+        if shots.has_partitioned_shots:
+            assert res[0].shape == (4,)
+            assert res[1].shape == (4,)
+
+            expected = ([-np.sin(a), np.sin(a) * np.sin(b)], [0, -np.cos(a) * np.cos(b)])
+            for _r, _e in zip(res, expected):
+                assert np.allclose(_r[:2], _e, atol=atol_for_shots(shots))
+                assert np.allclose(_r[2:], _e, atol=atol_for_shots(shots))
+        else:
+            assert res[0].shape == (2,)
+            assert res[1].shape == (2,)
+
+            expected = ([-np.sin(a), np.sin(a) * np.sin(b)], [0, -np.cos(a) * np.cos(b)])
+            for _r, _e in zip(res, expected):
+                assert np.allclose(_r, _e, atol=atol_for_shots(shots))
 
 
 jax = pytest.importorskip("jax")
@@ -308,6 +501,74 @@ class TestJaxRun:
         assert qml.math.allclose(res[0], jnp.cos(a) * jnp.cos(b), atol=atol_for_shots(shots))
         assert qml.math.allclose(res[1], jnp.cos(a) * jnp.cos(b), atol=atol_for_shots(shots))
 
+    def test_scalar_jacobian(self, device, config, shots, seed):
+        """Test scalar jacobian calculation"""
+        device = get_device(device, seed)
+        config = replace(config, interface="jax")
+
+        def cost(a):
+            tape = qml.tape.QuantumScript([qml.RY(a, 0)], [qml.expval(qml.PauliZ(0))], shots=shots)
+            resolved_config = _resolve_execution_config(config, device, [tape], TransformProgram())
+            inner_tp = _setup_transform_program(TransformProgram(), device, resolved_config)[1]
+            return run([tape], device, resolved_config, inner_tp)[0]
+
+        a = jnp.array(0.1)
+        res = jax.jacobian(cost)(a)
+        if not shots.has_partitioned_shots:
+            assert res.shape == ()  # pylint: disable=no-member
+
+        # compare to standard tape jacobian
+        tape = qml.tape.QuantumScript([qml.RY(a, wires=0)], [qml.expval(qml.PauliZ(0))])
+        tape.trainable_params = [0]
+        tapes, fn = qml.gradients.param_shift(tape)
+        expected = fn(device.execute(tapes))
+
+        assert expected.shape == ()
+        assert np.allclose(res, expected, atol=atol_for_shots(shots), rtol=0)
+        assert np.allclose(res, -jnp.sin(a), atol=atol_for_shots(shots))
+
+    def test_jacobian(self, device, config, shots, seed):
+        """Test jacobian calculation"""
+        device = get_device(device, seed)
+        config = replace(config, interface="jax")
+
+        def cost(a, b):
+            ops = [qml.RY(a, wires=0), qml.RX(b, wires=1), qml.CNOT(wires=[0, 1])]
+            m = [qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliY(1))]
+            tape = qml.tape.QuantumScript(ops, m, shots=shots)
+
+            resolved_config = _resolve_execution_config(config, device, [tape], TransformProgram())
+            inner_tp = _setup_transform_program(TransformProgram(), device, resolved_config)[1]
+            return run([tape], device, resolved_config, inner_tp)[0]
+
+        a = jnp.array(0.1)
+        b = jnp.array(0.2)
+        res = cost(a, b)
+
+        expected = [jnp.cos(a), -jnp.cos(a) * jnp.sin(b)]
+        if shots.has_partitioned_shots:
+            assert np.allclose(res[0], expected, atol=atol_for_shots(shots), rtol=0)
+            assert np.allclose(res[1], expected, atol=2 * atol_for_shots(shots), rtol=0)
+        else:
+            assert np.allclose(res, expected, atol=atol_for_shots(shots), rtol=0)
+
+        g = jax.jacobian(cost, argnums=[0, 1])(a, b)
+        assert isinstance(g, tuple) and len(g) == 2
+
+        expected = ([-jnp.sin(a), jnp.sin(a) * jnp.sin(b)], [0, -jnp.cos(a) * jnp.cos(b)])
+
+        if shots.has_partitioned_shots:
+            for i in (0, 1):
+                assert np.allclose(g[i][0][0], expected[0][0], atol=atol_for_shots(shots), rtol=0)
+                assert np.allclose(g[i][1][0], expected[0][1], atol=atol_for_shots(shots), rtol=0)
+                assert np.allclose(g[i][0][1], expected[1][0], atol=atol_for_shots(shots), rtol=0)
+                assert np.allclose(g[i][1][1], expected[1][1], atol=atol_for_shots(shots), rtol=0)
+        else:
+            assert np.allclose(g[0][0], expected[0][0], atol=atol_for_shots(shots), rtol=0)
+            assert np.allclose(g[1][0], expected[0][1], atol=atol_for_shots(shots), rtol=0)
+            assert np.allclose(g[0][1], expected[1][0], atol=atol_for_shots(shots), rtol=0)
+            assert np.allclose(g[1][1], expected[1][1], atol=atol_for_shots(shots), rtol=0)
+
 
 tf = pytest.importorskip("tensorflow")
 
@@ -355,6 +616,71 @@ class TestTensorFlowRun:
 
         assert qml.math.allclose(res[0], tf.cos(a) * tf.cos(b), atol=atol_for_shots(shots))
         assert qml.math.allclose(res[1], tf.cos(a) * tf.cos(b), atol=atol_for_shots(shots))
+
+    def test_scalar_jacobian(self, device, config, shots, seed):
+        """Test scalar jacobian calculation"""
+        if shots.has_partitioned_shots:
+            pytest.xfail(reason="Partitioned shots are not supported yet.")
+        device_vjp = config.use_device_jacobian_product
+        device = get_device(device, seed=seed)
+        config = replace(config, interface="tensorflow")
+
+        def cost(a):
+            tape = qml.tape.QuantumScript([qml.RY(a, 0)], [qml.expval(qml.PauliZ(0))], shots=shots)
+            resolved_config = _resolve_execution_config(config, device, [tape], TransformProgram())
+            inner_tp = _setup_transform_program(TransformProgram(), device, resolved_config)[1]
+            return run([tape], device, resolved_config, inner_tp)[0]
+
+        a = tf.Variable(0.1, dtype=tf.float64)
+        with tf.GradientTape(persistent=device_vjp) as tape:
+            cost_res = cost(a)
+        res = tape.jacobian(cost_res, a, experimental_use_pfor=not device_vjp)
+        assert res.shape == ()  # pylint: disable=no-member
+
+        # compare to standard tape jacobian
+        tape = qml.tape.QuantumScript([qml.RY(a, wires=0)], [qml.expval(qml.PauliZ(0))])
+        tape.trainable_params = [0]
+        tapes, fn = qml.gradients.param_shift(tape)
+        expected = fn(device.execute(tapes))
+
+        assert expected.shape == ()
+        assert np.allclose(res, expected, atol=atol_for_shots(shots), rtol=0)
+        assert np.allclose(res, -tf.sin(a), atol=atol_for_shots(shots))
+
+    def test_jacobian(self, device, config, shots, seed):
+        """Test jacobian calculation"""
+        if shots.has_partitioned_shots:
+            pytest.xfail(reason="Partitioned shots are not supported yet.")
+
+        config = replace(config, interface="tensorflow")
+        device = get_device(device, seed=seed)
+        device_vjp = config.use_device_jacobian_product
+
+        def cost(a, b):
+            ops = [qml.RY(a, wires=0), qml.RX(b, wires=1), qml.CNOT(wires=[0, 1])]
+            m = [qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliY(1))]
+            tape = qml.tape.QuantumScript(ops, m, shots=shots)
+            resolved_config = _resolve_execution_config(config, device, [tape], TransformProgram())
+            inner_tp = _setup_transform_program(TransformProgram(), device, resolved_config)[1]
+            return qml.math.hstack(
+                run([tape], device, resolved_config, inner_tp)[0], like="tensorflow"
+            )
+
+        a = tf.Variable(0.1)
+        b = tf.Variable(0.2)
+        with tf.GradientTape(persistent=device_vjp) as tape:
+            res = cost(a, b)
+        expected = [tf.cos(a), -tf.cos(a) * tf.sin(b)]
+        assert np.allclose(res, expected, atol=atol_for_shots(shots), rtol=0)
+
+        jac = tape.jacobian(res, [a, b], experimental_use_pfor=not device_vjp)
+        assert isinstance(jac, list) and len(jac) == 2
+        assert jac[0].shape == (2,)
+        assert jac[1].shape == (2,)
+
+        expected = ([-tf.sin(a), tf.sin(a) * tf.sin(b)], [0, -tf.cos(a) * tf.cos(b)])
+        for _r, _e in zip(jac, expected):
+            assert np.allclose(_r, _e, atol=atol_for_shots(shots))
 
 
 @pytest.mark.tf
