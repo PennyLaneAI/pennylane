356c0eca2f9d299e9d1ff1f43ddd21db41d2cf40
Austin Huang <65315367+austingmhuang@users.noreply.github.com>
2024-12-13 12:08:28 -0500

    Update pennylane/labs/vibrational/christiansen_utils.py
    Co-authored-by: Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>

--- pennylane/labs/vibrational/christiansen_utils.py
+++ pennylane/labs/vibrational/christiansen_utils.py
@@ -448,7 +448,6 @@ def _cform_threemode_dipole(pes, n_states):
     local_dipole_cform_threebody = np.zeros((len(all_mode_combos) * chunksize, 3))
 
     for nn, (ii1, ii2, ii3) in enumerate(all_mode_combos):
-        ii1, ii2, ii3 = int(ii1), int(ii2), int(ii3)
         # TODO: Skip unnecessary combinations
         if ii2 >= ii1 or ii3 >= ii2:
             continue
