a90542a393246d92717e2fbcdd1c2d946e68ea68
Astral Cai <astral.cai@xanadu.ai>
2024-12-13 14:58:24 -0500

    wip

--- tests/workflow/interfaces/qnode/conftest.py
+++ tests/workflow/interfaces/qnode/conftest.py
@@ -16,7 +16,6 @@
 
 import warnings
 import itertools
-from typing import Callable
 
 import pytest
 import pennylane as qml
@@ -38,20 +37,43 @@ def get_device(device_name, wires, seed=None):
     return qml.device(device_name, seed=seed)
 
 
-_device_names = ("default.qubit", "param_shift.qubit", "lightning.qubit", "reference.qubit")
+_dev_names = ("default.qubit", "param_shift.qubit", "lightning.qubit", "reference.qubit")
 _diff_methods = ("backprop", "adjoint", "finite-diff", "parameter-shift", "hadamard", "spsa")
-_grad_on_execution = (True, False)
+_grad_on_exec = (True, False)
 _device_vjp = (True, False)
 
 
-def generate_test_matrix(xfail_condition: Callable, skip_condition: Callable):
+def generate_test_matrix(
+    parametrize_devices=True,
+    parametrize_diff_methods=True,
+    parametrize_grad_on_execution=True,
+    parametrize_device_vjp=True,
+    xfail_conditions=None,
+    skip_conditions=None,
+):
     """Generates the test matrix for different combinations."""
+
+    xfail_conditions = xfail_conditions or []
+    skip_conditions = skip_conditions or []
+
+    def _first_reason(conditions, params):
+        """Returns the first condition that is met."""
+        for condition in conditions:
+            reason = condition(*params)
+            if reason:
+                return reason
+        return None
 
     def _test_matrix_iter():
         """Yields tuples of (device_name, diff_method, grad_on_execution, device_vjp)."""
 
-        all_combinations = itertools.product(_device_names, _diff_methods, _grad_on_execution)
-        for device_name, diff_method, grad_on_execution, device_vjp in all_combinations:
+        dev_names = _dev_names if parametrize_devices else (None,)
+        diff_methods = _diff_methods if parametrize_diff_methods else (None,)
+        grad_on_execs = _grad_on_exec if parametrize_grad_on_execution else (None,)
+        device_vjp = _device_vjp if parametrize_device_vjp else (None,)
+
+        combinations = itertools.product(dev_names, diff_methods, grad_on_execs, device_vjp)
+        for device_name, diff_method, grad_on_execution, device_vjp in combinations:
 
             if diff_method == "adjoint" and device_name not in ("default.qubit", "lightning.qubit"):
                 continue  # adjoint diff is only supported on DQ and LQ
@@ -59,28 +81,16 @@ def generate_test_matrix(xfail_condition: Callable, skip_condition: Callable):
             if device_name == "param_shift.qubit" and diff_method != "parameter-shift":
                 continue  # param_shift.qubit is not intended to be used with anything else
 
-            xfail_reason = xfail_condition(device_name, diff_method, grad_on_execution, device_vjp)
-            skip_reason = skip_condition(device_name, diff_method, grad_on_execution, device_vjp)
+            if diff_method not in ("adjoint", "backprop") and grad_on_execution:
+                continue  # Gradient transforms cannot be used with grad_on_execution=True
 
-            if xfail_reason:
-                yield pytest.param(
-                    device_name,
-                    diff_method,
-                    grad_on_execution,
-                    device_vjp,
-                    marks=pytest.mark.xfail(reason=xfail_reason),
-                )
-
-            elif skip_reason:
-                yield pytest.param(
-                    device_name,
-                    diff_method,
-                    grad_on_execution,
-                    device_vjp,
-                    marks=pytest.mark.skip(reason=skip_reason),
-                )
-
+            params = device_name, diff_method, grad_on_execution, device_vjp
+            filtered_params = tuple(p for p in params if p is not None)
+            if xfail_reason := _first_reason(xfail_conditions, params):
+                yield pytest.param(*filtered_params, marks=pytest.mark.xfail(reason=xfail_reason))
+            elif skip_reason := _first_reason(skip_conditions, params):
+                yield pytest.param(*filtered_params, marks=pytest.mark.skip(reason=skip_reason))
             else:
-                yield device_name, diff_method, grad_on_execution, device_vjp
+                yield filtered_params if len(filtered_params) > 1 else filtered_params[0]
 
     return [_params for _params in _test_matrix_iter()]
