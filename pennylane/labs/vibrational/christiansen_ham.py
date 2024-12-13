f0202475e432acc9a6b508c81da35117e66b2d41
Austin Huang <65315367+austingmhuang@users.noreply.github.com>
2024-12-13 15:48:47 -0500

    Update pennylane/labs/vibrational/christiansen_ham.py
    Co-authored-by: Soran Jahangiri <40344468+soranjh@users.noreply.github.com>

--- pennylane/labs/vibrational/christiansen_ham.py
+++ pennylane/labs/vibrational/christiansen_ham.py
@@ -23,7 +23,7 @@ from .christiansen_utils import christiansen_integrals, christiansen_integrals_d
 
 
 def christiansen_bosonic(one, modes=None, modals=None, two=None, three=None, ordered=True):
-    r"""Build the bosonic operators in the Christiansen form.
+    r"""Return Christiansen bosonic vibrational Hamiltonian.
 
     The construction of the second-quantized Hamiltonian is based on Eqs. 19-21 of
     `J. Chem. Theory Comput. 2023, 19, 24, 9329–9343 <https://pubs.acs.org/doi/10.1021/acs.jctc.3c00902?ref=PDF>`_.
