3c69fe4cd1a5aacf685c3635ffb89378d396d79b
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2024-12-17 14:46:44 +0530

    Add function to rotate vibrational Hamiltonian to VSCF basis (#6688)
    **Context:**
    Added functionality to rotate vibrational Hamiltonian integrals to VSCF
    basis
    
    **Description of the Change:**
    
    **Benefits:**
    
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**
    
    [sc-80080]
    
    ---------
    
    Co-authored-by: Soran Jahangiri <40344468+soranjh@users.noreply.github.com>
    Co-authored-by: Austin Huang <65315367+austingmhuang@users.noreply.github.com>

--- pennylane/qchem/vibrational/__init__.py
+++ pennylane/qchem/vibrational/__init__.py
@@ -1,3 +1,16 @@
+# Copyright 2018-2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 This submodule provides the functionality to calculate vibrational Hamiltonians.
 """
@@ -10,3 +23,4 @@ from .taylor_ham import (
 )
 from .localize_modes import localize_normal_modes
 from .vibrational_class import VibrationalPES, optimize_geometry
+from .vscf import vscf_integrals
