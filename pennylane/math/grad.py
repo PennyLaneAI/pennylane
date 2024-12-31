78601e290e13804b5112eb67b13d3230a8e404ca
Christina Lee <christina@xanadu.ai>
2024-12-31 12:00:18 -0500

    Update pennylane/math/grad.py
    Co-authored-by: Pietropaolo Frisoni <pietropaolo.frisoni@xanadu.ai>

--- pennylane/math/grad.py
+++ pennylane/math/grad.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-This submodule defines grad and jacobian for differentiating circuits in an interface
-independent way.
+This submodule defines grad and jacobian for differentiating circuits in an interface-independent way.
 """
 
 from typing import Callable, Sequence, Union
