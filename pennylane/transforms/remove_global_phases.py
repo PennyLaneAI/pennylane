bb6fea417428ed7e05c8cd18500a4fe264e94446
andrijapau <andrija.paurevic@xanadu.ai>
2025-01-02 18:05:36 -0500

    fix: Add transform and basic tests

---
+++ pennylane/transforms/remove_global_phases.py
@@ -0,0 +1,86 @@
+# Copyright 2018-2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Provides a transform to remove all ``qml.GlobalPhase`` gates in a circuit. 
+"""
+
+import pennylane as qml
+from pennylane.measurements import StateMP
+from pennylane.tape import QuantumScript, QuantumScriptBatch
+from pennylane.transforms import transform
+from pennylane.typing import PostprocessingFn
+
+
+@transform
+def remove_global_phases(tape: QuantumScript) -> tuple[QuantumScriptBatch, PostprocessingFn]:
+    """Remove all ``qml.GlobalPhase`` gates present in a quantum circuit.
+
+    This transform returns a new circuit where all ``qml.GlobalPhase`` gates in the original circuit (if exists)
+    are removed.
+
+    Args:
+        tape (QNode or QuantumScript or Callable): the input circuit to be transformed.
+
+    Returns:
+        qnode (QNode) or quantum function (Callable) or tuple[List[QuantumScript], function]:
+        the transformed circuit as described in :func:`qml.transform <pennylane.transform>`.
+
+    **Example**
+
+    Suppose we want to remove all of the global phase gates in a given quantum circuit.
+    The ``remove_global_phases`` transform can be used to do this as follows:
+
+    .. code-block:: python3
+
+        dev = qml.device("default.qubit", wires=3)
+
+        @qml.transforms.remove_global_phases
+        @qml.qnode(dev)
+        def circuit():
+            qml.GlobalPhase(0.3, wires=0)
+            qml.PauliY(wires=0)
+            qml.Hadamard(wires=1)
+            qml.GlobalPhase(0.4, wires=0)
+            qml.CNOT(wires=(1,2))
+            qml.GlobalPhase(0.5, wires=2)
+            return qml.expval(qml.X(0) @ qml.Z(1))
+
+    To check the result, let's print out the circuit:
+
+    >>> print(qml.draw(circuit)())
+    0: ──Y────┤ ╭<X@Z>
+    1: ──H─╭●─┤ ╰<X@Z>
+    2: ────╰X─┤
+    """
+
+    if any(isinstance(mp, StateMP) for mp in tape.measurements):
+        raise qml.QuantumFunctionError(
+            "The quantum circuit cannot contain a state measurement. Removing GlobalPhase operators in this case can cause errors."
+        )
+
+    operations = []
+    for op in tape.operations:
+        if not isinstance(op, qml.GlobalPhase):
+            operations.append(op)
+
+    new_tape = tape.copy(operations=operations)
+
+    def null_postprocessing(results):
+        """A postprocesing function returned by a transform that only converts the batch of results
+        into a result for a single ``QuantumScript``.
+        """
+        return results[0]
+
+    return (new_tape,), null_postprocessing
---
+++ tests/transforms/test_remove_global_phases.py
@@ -0,0 +1,143 @@
+"""
+Tests for the remove_global_phases transform.
+"""
+
+import pytest
+
+import pennylane as qml
+from pennylane.transforms import remove_global_phases
+
+
+def original_qfunc(phi1, phi2):
+    qml.Hadamard(wires=1)
+    qml.GlobalPhase(phi1, wires=[0, 1])
+    qml.PauliY(wires=0)
+    qml.PauliX(wires=2)
+    qml.CNOT(wires=[1, 2])
+    qml.GlobalPhase(phi2, wires=1)
+    qml.CNOT(wires=[2, 0])
+    return qml.expval(qml.Z(0) @ qml.X(1))
+
+
+def expected_qfunc():
+    qml.Hadamard(wires=1)
+    qml.PauliY(wires=0)
+    qml.PauliX(wires=2)
+    qml.CNOT(wires=[1, 2])
+    qml.CNOT(wires=[2, 0])
+    return qml.expval(qml.Z(0) @ qml.X(1))
+
+
+def test_global_phase_removal_error():
+    """Test that an error is raised if the transform is applied to a circuit with no measurements."""
+    qscript = qml.tape.QuantumScript([qml.Hadamard(0), qml.RX(0, 0)], [qml.state()])
+
+    expected_qscript = qml.tape.QuantumScript([qml.Hadamard(0), qml.RX(0, 0)], [qml.state()])
+    with pytest.raises(
+        qml.QuantumFunctionError, match="The quantum circuit cannot contain a state measurement"
+    ):
+        (transformed_qscript,), _ = remove_global_phases(qscript)
+        qml.assert_equal(transformed_qscript, expected_qscript)
+
+
+def test_no_global_phase_gate():
+    """Test that when the input ``QuantumScript`` has no ``qml.GlobalPhase`` gate, the returned output is exactly the same"""
+    qscript = qml.tape.QuantumScript([qml.Hadamard(0), qml.RX(0, 0)], [qml.expval(qml.Z(0))])
+
+    expected_qscript = qml.tape.QuantumScript(
+        [qml.Hadamard(0), qml.RX(0, 0)], [qml.expval(qml.Z(0))]
+    )
+    (transformed_qscript,), _ = remove_global_phases(qscript)
+
+    qml.assert_equal(expected_qscript, transformed_qscript)
+
+
+def test_remove_global_phase_gates():
+    """Test that when the input ``QuantumScript`` has ``qml.GlobalPhase`` gates, they are removed with the transform."""
+    qscript = qml.tape.QuantumScript(
+        [qml.GlobalPhase(1.23, 0), qml.Hadamard(0), qml.GlobalPhase(4.56, 0), qml.RX(0, 0)],
+        [qml.expval(qml.Z(0))],
+    )
+
+    expected_qscript = qml.tape.QuantumScript(
+        [qml.Hadamard(0), qml.RX(0, 0)], [qml.expval(qml.Z(0))]
+    )
+    (transformed_qscript,), _ = remove_global_phases(qscript)
+
+    qml.assert_equal(expected_qscript, transformed_qscript)
+
+
+@pytest.mark.autograd
+def test_differentiability_autograd():
+    """Test that the output of the ``remove_global_phases`` transform is differentiable with autograd"""
+    import pennylane.numpy as pnp
+
+    dev = qml.device("default.qubit", wires=3)
+    original_qnode = qml.QNode(original_qfunc, device=dev)
+    transformed_qnode = remove_global_phases(original_qnode)
+
+    phi1 = pnp.array(0.25)
+    phi2 = pnp.array(-0.6)
+    grad1, grad2 = qml.jacobian(transformed_qnode)(phi1, phi2)
+
+    assert qml.math.isclose(grad1, 0.0)
+    assert qml.math.isclose(grad2, 0.0)
+
+
+@pytest.mark.jax
+@pytest.mark.parametrize("use_jit", [False, True])
+def test_differentiability_jax(use_jit):
+    """Test that the output of the ``remove_global_phases`` transform is differentiable with JAX"""
+    import jax
+    import jax.numpy as jnp
+
+    dev = qml.device("default.qubit", wires=3)
+    original_qnode = qml.QNode(original_qfunc, device=dev)
+    transformed_qnode = remove_global_phases(original_qnode)
+
+    if use_jit:
+        transformed_qnode = jax.jit(transformed_qnode)
+
+    phi1 = jnp.array(0.25)
+    phi2 = jnp.array(-0.6)
+    grad1, grad2 = jax.jacobian(transformed_qnode, argnums=[0, 1])(phi1, phi2)
+
+    assert qml.math.isclose(grad1, 0.0)
+    assert qml.math.isclose(grad2, 0.0)
+
+
+@pytest.mark.torch
+def test_differentiability_torch():
+    """Test that the output of the ``remove_global_phases`` transform is differentiable with Torch"""
+    import torch
+    from torch.autograd.functional import jacobian
+
+    dev = qml.device("default.qubit", wires=3)
+    original_qnode = qml.QNode(original_qfunc, device=dev)
+    transformed_qnode = remove_global_phases(original_qnode)
+
+    phi1 = torch.tensor(0.25)
+    phi2 = torch.tensor(-0.6)
+    grad1, grad2 = jacobian(transformed_qnode, (phi1, phi2))
+
+    zero = torch.tensor(0.0)
+    assert qml.math.isclose(grad1, zero)
+    assert qml.math.isclose(grad2, zero)
+
+
+@pytest.mark.tf
+def test_differentiability_tensorflow():
+    """Test that the output of the ``remove_global_phases`` transform is differentiable with TensorFlow"""
+    import tensorflow as tf
+
+    dev = qml.device("default.qubit", wires=3)
+    original_qnode = qml.QNode(original_qfunc, device=dev)
+
+    phi1 = tf.Variable(0.25)
+    phi2 = tf.Variable(-0.6)
+    with tf.GradientTape() as tape:
+        transformed_qnode = remove_global_phases(original_qnode)(phi1, phi2)
+    grad1, grad2 = tape.jacobian(transformed_qnode, (phi1, phi2))
+
+    assert qml.math.isclose(grad1, 0.0)
+    assert qml.math.isclose(grad2, 0.0)
