940929cbbe769e79e07fe36e0e1bba07075052fe
qottmann <korbinian.kottmann@gmail.com>
2025-01-02 16:18:02 +0100

    module description

--- pennylane/labs/dla/__init__.py
+++ pennylane/labs/dla/__init__.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 r"""
-Experimental Lie theory features
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Experimental functionality to handle (dynamical) Lie algebras (DLAs)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 .. currentmodule:: pennylane.labs.dla
 
