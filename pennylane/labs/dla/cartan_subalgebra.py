fedcfb19a171ed8f39358ec721dc9326b3a42698
Korbinian Kottmann <43949391+Qottmann@users.noreply.github.com>
2025-01-03 17:15:52 +0100

    Update pennylane/labs/dla/cartan_subalgebra.py
    Co-authored-by: Pietropaolo Frisoni <pietropaolo.frisoni@xanadu.ai>

--- pennylane/labs/dla/cartan_subalgebra.py
+++ pennylane/labs/dla/cartan_subalgebra.py
@@ -113,7 +113,7 @@ def cartan_subalgebra(
     >>> newg == k + mtilde + a
     True
     >>> a
-    [-1.0 * Z(0) @ Z(1), -1.0 * Y(0) @ Y(1), 1.0 * X(0) @ X(1)]
+    [-1.0 * Z(0) @ Z(1), 1.0 * Y(0) @ Y(1), -1.0 * X(0) @ X(1)]
 
     We can confirm that these all commute with each other, as the CSA is Abelian (= all operators commute).
 
