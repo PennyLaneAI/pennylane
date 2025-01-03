cc344b70da430c46c417cd26be4a1c8f75b20de0
JerryChen97 <chenys13@outlook.com>
2025-01-03 11:28:40 -0500

    add another for ShadowExpvalMP

--- pennylane/measurements/classical_shadow.py
+++ pennylane/measurements/classical_shadow.py
@@ -664,6 +664,29 @@ class ShadowExpvalMP(MeasurementTransform):
         shadow = qml.shadows.ClassicalShadow(bits, recipes, wire_map=self.wires.tolist())
         return shadow.expval(self.H, self.k)
 
+    def process_density_matrix_with_shots(
+        self, state: Sequence[complex], wire_order: Wires, shots: int, rng=None
+    ):
+        """Process the given quantum state with the given number of shots
+
+        Args:
+            state (Sequence[complex]): quantum state
+            wire_order (Wires): wires determining the subspace that ``state`` acts on; a matrix of
+                dimension :math:`2^n` acts on a subspace of :math:`n` wires
+            shots (int): The number of shots
+            rng (Union[None, int, array_like[int], SeedSequence, BitGenerator, Generator]): A
+                seed-like parameter matching that of ``seed`` for ``numpy.random.default_rng``.
+                If no value is provided, a default RNG will be used.
+
+        Returns:
+            float: The estimate of the expectation value.
+        """
+        bits, recipes = qml.classical_shadow(
+            wires=self.wires, seed=self.seed
+        ).process_density_matrix_with_shots(state, wire_order, shots, rng=rng)
+        shadow = qml.shadows.ClassicalShadow(bits, recipes, wire_map=self.wires.tolist())
+        return shadow.expval(self.H, self.k)
+
     @property
     def samples_computational_basis(self):
         return False
