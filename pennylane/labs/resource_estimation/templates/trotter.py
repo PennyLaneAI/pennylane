b5324687e13518fa338d3c79789cffc8f17b8074
Jay Soni <jbsoni@uwaterloo.ca>
2025-01-03 14:42:52 -0500

    clean ResourceTrotterProduct implementation

--- pennylane/labs/resource_estimation/templates/trotter.py
+++ pennylane/labs/resource_estimation/templates/trotter.py
@@ -14,10 +14,9 @@
 """
 Contains templates for Suzuki-Trotter approximation based subroutines.
 """
-import copy
-from collections import defaultdict
-from functools import wraps
 from typing import Dict
+from functools import wraps
+from collections import defaultdict
 
 import pennylane as qml
 from pennylane.labs.resource_estimation import (
@@ -26,15 +25,11 @@ from pennylane.labs.resource_estimation import (
     ResourceOperator,
     ResourcesNotDefined,
 )
-from pennylane.operation import Operation, Operator
-from pennylane.ops import Sum
-from pennylane.ops.op_math import SProd
-from pennylane.resource.error import ErrorOperation, SpectralNormError
-from pennylane.templates.subroutines.trotter import TrotterizedQfunc, _recursive_expression, _scalar
-from pennylane.wires import Wires
+from pennylane.templates import TrotterProduct
+from pennylane.templates.subroutines.trotter import TrotterizedQfunc, _scalar
 
 
-class ResourceTrotterProduct(ErrorOperation, ResourceOperator):
+class ResourceTrotterProduct(TrotterProduct, ResourceOperator):
     r"""An operation representing the Suzuki-Trotter product approximation for the complex matrix
     exponential of a given Hamiltonian.
 
@@ -154,61 +149,6 @@ class ResourceTrotterProduct(ErrorOperation, ResourceOperator):
         (tensor(0.00961064, requires_grad=True), tensor(-0.12338274, requires_grad=True), tensor(-5.43401259, requires_grad=True))
     """
 
-    @classmethod
-    def _primitive_bind_call(cls, *args, **kwargs):
-        # accepts no wires, so bypasses the wire processing.
-        return cls._primitive.bind(*args, **kwargs)
-
-    def __init__(  # pylint: disable=too-many-arguments
-        self, hamiltonian, time, n=1, order=1, check_hermitian=True, id=None
-    ):
-        r"""Initialize the TrotterProduct class"""
-
-        if order <= 0 or order != 1 and order % 2 != 0:
-            raise ValueError(
-                f"The order of a TrotterProduct must be 1 or a positive even integer, got {order}."
-            )
-
-        if isinstance(hamiltonian, qml.ops.LinearCombination):
-            coeffs, ops = hamiltonian.terms()
-            if len(coeffs) < 2:
-                raise ValueError(
-                    "There should be at least 2 terms in the Hamiltonian. Otherwise use `qml.exp`"
-                )
-            if qml.QueuingManager.recording():
-                qml.QueuingManager.remove(hamiltonian)
-            hamiltonian = qml.dot(coeffs, ops)
-
-        if isinstance(hamiltonian, SProd):
-            if qml.QueuingManager.recording():
-                qml.QueuingManager.remove(hamiltonian)
-            hamiltonian = hamiltonian.simplify()
-            if len(hamiltonian.terms()[0]) < 2:
-                raise ValueError(
-                    "There should be at least 2 terms in the Hamiltonian. Otherwise use `qml.exp`"
-                )
-
-        if not isinstance(hamiltonian, Sum):
-            raise TypeError(
-                f"The given operator must be a PennyLane ~.Sum or ~.SProd, got {hamiltonian}"
-            )
-
-        if check_hermitian:
-            for op in hamiltonian.operands:
-                if not op.is_hermitian:
-                    raise ValueError(
-                        "One or more of the terms in the Hamiltonian may not be Hermitian"
-                    )
-
-        self._hyperparameters = {
-            "n": n,
-            "order": order,
-            "base": hamiltonian,
-            "check_hermitian": check_hermitian,
-        }
-
-        super().__init__(*hamiltonian.data, time, wires=hamiltonian.wires, id=id)
-
     @staticmethod
     def _resource_decomp(base, time, n, order, **kwargs) -> Dict[CompressedResourceOp, int]:
         k = order // 2
@@ -247,186 +187,11 @@ class ResourceTrotterProduct(ErrorOperation, ResourceOperator):
         }
         return CompressedResourceOp(cls, params)
 
-    def map_wires(self, wire_map: dict):
-        # pylint: disable=protected-access
-        new_op = copy.deepcopy(self)
-        new_op._wires = Wires([wire_map.get(wire, wire) for wire in self.wires])
-        new_op._hyperparameters["base"] = qml.map_wires(new_op._hyperparameters["base"], wire_map)
-        return new_op
-
-    def queue(self, context=qml.QueuingManager):
-        context.remove(self.hyperparameters["base"])
-        context.append(self)
-        return self
-
-    def error(
-        self, method: str = "commutator-bound", fast: bool = True
-    ):  # pylint: disable=arguments-differ
-        # pylint: disable=protected-access
-        r"""Compute an *upper-bound* on the spectral norm error for approximating the
-        time-evolution of the base Hamiltonian using the Suzuki-Trotter product formula.
-
-        The error in the Suzuki-Trotter product formula is defined as
-
-        .. math:: || \ e^{iHt} - \left [S_{m}(t / n)  \right ]^{n} \ ||,
-
-        Where the norm :math:`||\cdot||` is the spectral norm. This function supports two methods
-        from literature for upper-bounding the error, the "one-norm" error bound and the "commutator"
-        error bound.
-
-        **Example:**
-
-        The "one-norm" error bound can be computed by passing the kwarg :code:`method="one-norm-bound"`, and
-        is defined according to Section 2.3 (lemma 6, equation 22 and 23) of
-        `Childs et al. (2021) <https://arxiv.org/abs/1912.08854>`_.
-
-        >>> hamiltonian = qml.dot([1.0, 0.5, -0.25], [qml.X(0), qml.Y(0), qml.Z(0)])
-        >>> op = qml.TrotterProduct(hamiltonian, time=0.01, order=2)
-        >>> op.error(method="one-norm-bound")
-        SpectralNormError(8.039062500000003e-06)
-
-        The "commutator" error bound can be computed by passing the kwarg :code:`method="commutator-bound"`, and
-        is defined according to Appendix C (equation 189) `Childs et al. (2021) <https://arxiv.org/abs/1912.08854>`_.
-
-        >>> hamiltonian = qml.dot([1.0, 0.5, -0.25], [qml.X(0), qml.Y(0), qml.Z(0)])
-        >>> op = qml.TrotterProduct(hamiltonian, time=0.01, order=2)
-        >>> op.error(method="commutator-bound")
-        SpectralNormError(6.166666666666668e-06)
-
-        Args:
-            method (str, optional): Options include "one-norm-bound" and "commutator-bound" and specify the
-                method with which the error is computed. Defaults to "commutator-bound".
-            fast (bool, optional): Uses more approximations to speed up computation. Defaults to True.
-
-        Raises:
-            ValueError: The method is not supported.
-
-        Returns:
-            SpectralNormError: The spectral norm error.
-        """
-        base_unitary = self.hyperparameters["base"]
-        t, p, n = (self.parameters[-1], self.hyperparameters["order"], self.hyperparameters["n"])
-
-        parameters = [t] + base_unitary.parameters
-        if any(
-            qml.math.get_interface(param) == "tensorflow" for param in parameters
-        ):  # TODO: Add TF support
-            raise TypeError(
-                "Calculating error bound for Tensorflow objects is currently not supported"
-            )
-
-        terms = base_unitary.operands
-        if method == "one-norm-bound":
-            return SpectralNormError(qml.resource.error._one_norm_error(terms, t, p, n, fast=fast))
-
-        if method == "commutator-bound":
-            return SpectralNormError(
-                qml.resource.error._commutator_error(terms, t, p, n, fast=fast)
-            )
-
-        raise ValueError(
-            f"The '{method}' method is not supported for computing the error. Please select a valid method for computing the error."
-        )
-
-    def _flatten(self):
-        """Serialize the operation into trainable and non-trainable components.
-
-        Returns:
-            data, metadata: The trainable and non-trainable components.
-
-        See ``Operator._unflatten``.
-
-        The data component can be recursive and include other operations. For example, the trainable component of ``Adjoint(RX(1, wires=0))``
-        will be the operator ``RX(1, wires=0)``.
-
-        The metadata **must** be hashable.  If the hyperparameters contain a non-hashable component, then this
-        method and ``Operator._unflatten`` should be overridden to provide a hashable version of the hyperparameters.
-
-        **Example:**
-
-        >>> op = qml.Rot(1.2, 2.3, 3.4, wires=0)
-        >>> qml.Rot._unflatten(*op._flatten())
-        Rot(1.2, 2.3, 3.4, wires=[0])
-        >>> op = qml.PauliRot(1.2, "XY", wires=(0,1))
-        >>> qml.PauliRot._unflatten(*op._flatten())
-        PauliRot(1.2, XY, wires=[0, 1])
-
-        Operators that have trainable components that differ from their ``Operator.data`` must implement their own
-        ``_flatten`` methods.
-
-        >>> op = qml.ctrl(qml.U2(3.4, 4.5, wires="a"), ("b", "c") )
-        >>> op._flatten()
-        ((U2(3.4, 4.5, wires=['a']),),
-        (Wires(['b', 'c']), (True, True), Wires([])))
-        """
-        hamiltonian = self.hyperparameters["base"]
-        time = self.data[-1]
-
-        hashable_hyperparameters = tuple(
-            item for item in self.hyperparameters.items() if item[0] != "base"
-        )
-        return (hamiltonian, time), hashable_hyperparameters
-
     @classmethod
-    def _unflatten(cls, data, metadata):
-        """Recreate an operation from its serialized format.
-
-        Args:
-            data: the trainable component of the operation
-            metadata: the non-trainable component of the operation.
-
-        The output of ``Operator._flatten`` and the class type must be sufficient to reconstruct the original
-        operation with ``Operator._unflatten``.
-
-        **Example:**
-
-        >>> op = qml.Rot(1.2, 2.3, 3.4, wires=0)
-        >>> op._flatten()
-        ((1.2, 2.3, 3.4), (Wires([0]), ()))
-        >>> qml.Rot._unflatten(*op._flatten())
-        >>> op = qml.PauliRot(1.2, "XY", wires=(0,1))
-        >>> op._flatten()
-        ((1.2,), (Wires([0, 1]), (('pauli_word', 'XY'),)))
-        >>> op = qml.ctrl(qml.U2(3.4, 4.5, wires="a"), ("b", "c") )
-        >>> type(op)._unflatten(*op._flatten())
-        Controlled(U2(3.4, 4.5, wires=['a']), control_wires=['b', 'c'])
-
-        """
-        return cls(*data, **dict(metadata))
-
-    @staticmethod
-    def compute_decomposition(*args, **kwargs):
-        r"""Representation of the operator as a product of other operators (static method).
-
-        .. math:: O = O_1 O_2 \dots O_n.
-
-        .. note::
-
-            Operations making up the decomposition should be queued within the
-            ``compute_decomposition`` method.
-
-        .. seealso:: :meth:`~.Operator.decomposition`.
-
-        Args:
-            *params (list): trainable parameters of the operator, as stored in the ``parameters`` attribute
-            wires (Iterable[Any], Wires): wires that the operator acts on
-            **hyperparams (dict): non-trainable hyperparameters of the operator, as stored in the ``hyperparameters`` attribute
-
-        Returns:
-            list[Operator]: decomposition of the operator
-        """
-        time = args[-1]
-        n = kwargs["n"]
-        order = kwargs["order"]
-        ops = kwargs["base"].operands
-
-        decomp = _recursive_expression(time / n, order, ops)[::-1] * n
-
-        if qml.QueuingManager.recording():
-            for op in decomp:  # apply operators in reverse order of expression
-                qml.apply(op)
-
-        return decomp
+    def resources(cls, *args, **kwargs) -> Dict[CompressedResourceOp, int]:
+        """Returns a dictionary containing the counts of each operator type used to
+        compute the resources of the operator."""
+        return cls._resource_decomp(*args, **kwargs)
 
 
 class ResourceTrotterizedQfunc(TrotterizedQfunc, ResourceOperator):
@@ -545,31 +310,29 @@ def resource_trotterize(qfunc, n=1, order=2, reverse=False):
 
     .. code-block:: python3
 
-def first_order_expansion(time, theta, phi, wires, flip=False):
-    "This is the first order expansion (U_1)."
-    ResourceRX(time*theta, wires[0])
-    ResourceRY(time*phi, wires[1])
-    if flip:
-        ResourceCNOT(wires=wires[:2])
+        def first_order_expansion(time, theta, phi, wires, flip=False):
+            "This is the first order expansion (U_1)."
+            ResourceRX(time*theta, wires[0])
+            ResourceRY(time*phi, wires[1])
+            if flip:
+                ResourceCNOT(wires=wires[:2])
 
-@qml.qnode(qml.device("default.qubit"))
-def my_circuit(time, theta, phi, num_trotter_steps):
-    resource_trotterize(
-        first_order_expansion,
-        n=num_trotter_steps,
-        order=2,
-    )(time, theta, phi, wires=['a', 'b', 'c'], flip=True)
-    return qml.state()
+        @qml.qnode(qml.device("default.qubit"))
+        def my_circuit(time, theta, phi, num_trotter_steps):
+            resource_trotterize(
+                first_order_expansion,
+                n=num_trotter_steps,
+                order=2,
+            )(time, theta, phi, wires=['a', 'b', 'c'], flip=True)
+            return qml.state()
         
     We can visualize the circuit to see the Suzuki-Trotter product formula being applied:
             
         >>> time = 0.1
         >>> theta, phi = (0.12, -3.45)
-        >>> 
         >>> print(qml.draw(my_circuit, level=3)(time, theta, phi, num_trotter_steps=1))
         a: ──RX(0.01)──╭●─╭●──RX(0.01)──┤  State
         b: ──RY(-0.17)─╰X─╰X──RY(-0.17)─┤  State
-        >>>
         >>>
         >>> print(qml.draw(my_circuit, level=3)(time, angles, num_trotter_steps=3))
         a: ──RX(0.00)──╭●─╭●──RX(0.00)───RX(0.00)──╭●─╭●──RX(0.00)───RX(0.00)──╭●─╭●──RX(0.00)──┤  State
