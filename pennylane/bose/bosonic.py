18552c2ffb8cfbf63ede93257bf91b26ff92b537
andrijapau <andrija.paurevic@xanadu.ai>
2025-01-09 10:05:31 -0500

    fix: add qml. prefix to bose sentence

--- pennylane/bose/bosonic.py
+++ pennylane/bose/bosonic.py
@@ -422,7 +422,7 @@ class BoseSentence(dict):
 
     >>> w1 = qml.BoseWord({(0, 0) : '+', (1, 1) : '-'})
     >>> w2 = qml.BoseWord({(0, 1) : '+', (1, 2) : '-'})
-    >>> s = BoseSentence({w1 : 1.2, w2: 3.1})
+    >>> s = qml.BoseSentence({w1 : 1.2, w2: 3.1})
     >>> print(s)
     1.2 * b⁺(0) b(1)
     + 3.1 * b⁺(1) b(2)
