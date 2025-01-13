7973fa664f6ea13d4a9d155a94eb08738b21ead8
andrijapau <andrija.paurevic@xanadu.ai>
2025-01-13 13:45:34 -0500

    docfix: Make docs build better

--- pennylane/workflow/qnode.py
+++ pennylane/workflow/qnode.py
@@ -652,8 +652,9 @@ class QNode:
                 qml.RY(x, wires=1)
                 return qml.expval(qml.PauliZ(1))
 
-        If we wish to try out a new object configuration, we can use the ``QNode.update``
-        method to reconfigure the differentiation method and execution arguments,
+        If we wish to try out a new configuration without having to repeat the
+        boiler plate above, we can use the ``QNode.update`` method. For example,
+        we can update the differentiation method and execution arguments,
 
         >>> new_circuit = circuit.update(diff_method="adjoint", device_vjp=True)
         >>> print(new_circuit.diff_method)
