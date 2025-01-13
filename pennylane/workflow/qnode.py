988d05a90997c3b036585637d4fe269013ac5e04
andrijapau <andrija.paurevic@xanadu.ai>
2025-01-13 14:20:44 -0500

    docfix: Improve docstring

--- pennylane/workflow/qnode.py
+++ pennylane/workflow/qnode.py
@@ -635,7 +635,7 @@ class QNode:
 
         Keyword Args:
             **kwargs: The provided keyword arguments must match that of :meth:`QNode.__init__`.
-                See the list of supported gradient keyword arguments :attr:`here <pennylane.gradients.SUPPORTED_GRADIENT_KWARGS>`.
+                The list of supported gradient keyword arguments can be found at `qml.gradients.SUPPORTED_GRADIENT_KWARGS`.
 
         **Example**
 
