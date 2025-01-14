16f64ecc0b9dfac4c70d7c50caf7df09805ba07a
andrijapau <andrija.paurevic@xanadu.ai>
2025-01-14 10:45:19 -0500

    doc: Fix docstring in update

--- pennylane/workflow/qnode.py
+++ pennylane/workflow/qnode.py
@@ -633,6 +633,9 @@ class QNode:
     def update(self, **kwargs) -> "QNode":
         """Returns a new QNode instance but with updated settings (e.g., a different `diff_method`). Any settings not specified will retain their original value.
 
+        .. note::
+            The QNode`s transform program cannot be updated using this method.
+
         Keyword Args:
             **kwargs: The provided keyword arguments must match that of :meth:`QNode.__init__`.
                 The list of supported gradient keyword arguments can be found at ``qml.gradients.SUPPORTED_GRADIENT_KWARGS``.
