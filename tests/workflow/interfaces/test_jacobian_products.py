ebdf86769f345556aaee67e6662eef405c92baae
Christina Lee <christina@xanadu.ai>
2025-01-15 16:25:47 -0500

    Update tests/workflow/interfaces/test_jacobian_products.py

--- tests/workflow/interfaces/test_jacobian_products.py
+++ tests/workflow/interfaces/test_jacobian_products.py
@@ -86,22 +86,22 @@ def test_no_gradients():
     jpc = NoGradients()
 
     with pytest.raises(
-        qml.QuantumFunctionError, match="cannot be calcualted with diff_method=None"
+        qml.QuantumFunctionError, match="cannot be calculated with diff_method=None"
     ):
         jpc.compute_jacobian(())
 
     with pytest.raises(
-        qml.QuantumFunctionError, match="cannot be calcualted with diff_method=None"
+        qml.QuantumFunctionError, match="cannot be calculated with diff_method=None"
     ):
         jpc.compute_vjp((), ())
 
     with pytest.raises(
-        qml.QuantumFunctionError, match="cannot be calcualted with diff_method=None"
+        qml.QuantumFunctionError, match="cannot be calculated with diff_method=None"
     ):
         jpc.execute_and_compute_jvp((), ())
 
     with pytest.raises(
-        qml.QuantumFunctionError, match="cannot be calcualted with diff_method=None"
+        qml.QuantumFunctionError, match="cannot be calculated with diff_method=None"
     ):
         jpc.execute_and_compute_jacobian(())
 
