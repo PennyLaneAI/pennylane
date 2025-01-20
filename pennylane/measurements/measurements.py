216b72e7ced2278c99eb6b926b2f1870c5ddd7c3
JerryChen97 <chenys13@outlook.com>
2025-01-20 11:27:50 -0500

    pylint

--- pennylane/measurements/measurements.py
+++ pennylane/measurements/measurements.py
@@ -275,7 +275,7 @@ class MeasurementProcess(ABC, metaclass=qml.capture.ABCCaptureMeta):
             "Instead, please use isinstance for type checking directly.",
             qml.PennyLaneDeprecationWarning,
         )
-        return None
+        return None # pylint: disable=useless-return
 
     @property
     def numeric_type(self) -> type:
