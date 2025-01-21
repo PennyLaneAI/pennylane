441b37ab80b885f86c815412e5f80751eebb681f
Mudit Pandey <mudit.pandey@xanadu.ai>
2025-01-21 12:23:08 -0500

    Refactor where validation happens

--- pennylane/workflow/resolution.py
+++ pennylane/workflow/resolution.py
@@ -14,12 +14,7 @@
 """This module contains the necessary helper functions for setting up the workflow for execution."""
 from collections.abc import Callable
 from dataclasses import replace
-from importlib.metadata import version
-from importlib.util import find_spec
 from typing import Literal, Optional, Union, get_args
-from warnings import warn
-
-from packaging.version import Version
 
 import pennylane as qml
 from pennylane.logging import debug_logger
@@ -46,22 +41,6 @@ def _get_jax_interface_name() -> Interface:
     """
     x = qml.math.asarray([0], like="jax")
     return Interface.JAX_JIT if qml.math.is_abstract(x) else Interface.JAX
-
-
-def _validate_jax_version() -> None:
-    """Checks if the installed version of JAX is supported. If an unsupported version of
-    JAX is installed, a ``RuntimeWarning`` is raised."""
-    if not find_spec("jax"):
-        return
-
-    jax_version = version("jax")
-
-    if Version(jax_version) > Version("0.4.28"):  # pragma: no cover
-        warn(
-            "PennyLane is currently not compatible with versions of JAX > 0.4.28. "
-            f"You have version {jax_version} installed.",
-            RuntimeWarning,
-        )
 
 
 # pylint: disable=import-outside-toplevel
