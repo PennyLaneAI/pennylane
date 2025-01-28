4c09a075fbf4a94ea6e7048d719c83fd6153e343
andrijapau <andrija.paurevic@xanadu.ai>
2025-01-28 10:11:47 -0500

    fix: Update test_capture_for_loop and test_capture_while_loop to use xfail

--- tests/capture/test_capture_for_loop.py
+++ tests/capture/test_capture_for_loop.py
@@ -422,12 +422,14 @@ class TestCaptureCircuitsForLoop:
     @pytest.mark.parametrize(
         "upper_bound, arg, expected", [(3, 0.5, 0.00223126), (2, 12, 0.2653001)]
     )
-    def test_nested_for_and_while_loop(self, upper_bound, arg, expected):
+    @pytest.mark.parametrize("autograph", [True, False])
+    def test_nested_for_and_while_loop(self, upper_bound, arg, expected, autograph):
         """Test that a nested for loop and while loop is correctly captured into a jaxpr."""
-
+        if autograph:
+            pytest.xfail(reason="Autograph bug with lambda functions as condition, see sc-82837")
         dev = qml.device("default.qubit", wires=3)
 
-        @qml.qnode(dev, autograph=False)
+        @qml.qnode(dev, autograph=autograph)
         def circuit(upper_bound, arg):
 
             # for loop with dynamic bounds
