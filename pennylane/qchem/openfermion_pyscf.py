7e41928802ad24a99072e0a07bc4e81e693cdc20
albi3ro <chrissie.c.l@gmail.com>
2025-01-29 09:40:53 -0500

    running most recent black

--- pennylane/qchem/openfermion_pyscf.py
+++ pennylane/qchem/openfermion_pyscf.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""This module contains functions to construct many-body observables with ``OpenFermion-PySCF``.
-"""
+"""This module contains functions to construct many-body observables with ``OpenFermion-PySCF``."""
 # pylint: disable=too-many-arguments, too-few-public-methods, too-many-branches, unused-variable
 # pylint: disable=consider-using-generator, protected-access
 import os
