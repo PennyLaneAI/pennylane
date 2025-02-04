c045fe45c598623d4ff83ff32580d24c6fffc308
andrijapau <andrija.paurevic@xanadu.ai>
2025-02-04 15:52:47 -0500

    add pytest disable for protected access

--- tests/capture/transforms/test_capture_merge_amplitude_embedding.py
+++ tests/capture/transforms/test_capture_merge_amplitude_embedding.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Unit tests for the ``MergeAmplitudeEmbeddingInterpreter`` class"""
+# pylint : disable = protected-access
 import pytest
 
 import pennylane as qml
