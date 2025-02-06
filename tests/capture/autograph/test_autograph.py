581eaa2030a13342a62b3234c7f1c6ba691bb2da
andrijapau <andrija.paurevic@xanadu.ai>
2025-02-06 12:01:03 -0500

    Fix test_autograph.py

--- tests/capture/autograph/test_autograph.py
+++ tests/capture/autograph/test_autograph.py
@@ -403,6 +403,9 @@ class TestIntegration:
         with pytest.raises(NotImplementedError):
             ag_fn(0.5)
 
+    @pytest.mark.xfail(
+        strict=False, reason="MCM one shot not currently supported with program capture."
+    )
     def test_mcm_one_shot(self, seed):
         """Test if mcm one-shot miss transforms."""
         dev = qml.device("default.qubit", wires=5, shots=20, seed=seed)
