364bb7cb37c634a67071c293aa23b2b6d76944df
Will <wmaxwell90@gmail.com>
2025-02-07 15:34:39 -0500

    small changes

--- pennylane/labs/pf/wavefunctions/ho_state.py
+++ pennylane/labs/pf/wavefunctions/ho_state.py
@@ -1,23 +1,38 @@
 """Contains the HOState class which represents a wavefunction in the Harmonic Oscillator basis"""
+from __future__ import annotations
 
 from typing import Dict, Tuple
+
+from scipy.sparse import coo_matrix, coo_array
 
 class HOState:
     """Representation of a wavefunction in the Harmonic Oscillator basis"""
 
-    def __init__(self, modes: int, coeffs: Dict[Tuple[int], float]):
-        _validate_indices(modes, coeffs.keys())
+    def __init__(self, modes: int, gridpoints: int, coeffs: Dict[Tuple[int], float]):
+        self.dim = gridpoints ** modes
 
-        self.modes = modes
+        self.vector = coo_array()
 
-    def to_dict(self) -> Dict[Tuple[int], float]:
-        """Returns a dictionary representation of the wavefunction"""
+    def apply_momentum(self, mode: int) -> HOState:
+        """Apply momentum operator on specified mode"""
 
-    def annihilation(self, mode) -> None:
+    def apply_position(self, mode: int) -> HOState:
+        """Apply position operator on specified mode"""
+
+    def apply_creation(self, mode: int) -> HOState:
+        """Apply creation operator on specified mode"""
+
+    def apply_annihilation(self, mode: int) -> HOState:
         """Apply annihilation operator on specified mode"""
 
-    def creation(self, mode) -> None:
-        """Apply creation operator on specified mode"""
+    def __add__(self, other: HOState) -> HOState:
+        pass
+
+    def __mul__(self, scalar: float) -> HOState:
+        pass
+
+    def to_dict(self) -> Dict[Tuple[int], float]:
+        """Return the dictionary representation"""
 
 def _validate_indices(modes, indices) -> None:
     for index in indices:
