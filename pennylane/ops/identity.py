a31c66b426f94b7435d49ddb22b48e74a33c851e
Yushao Chen (Jerry) <chenys13@outlook.com>
2025-02-11 13:34:45 -0500

    Update pennylane/ops/identity.py
    Co-authored-by: Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>

--- pennylane/ops/identity.py
+++ pennylane/ops/identity.py
@@ -457,7 +457,6 @@ class GlobalPhase(Operation):
         return self.compute_matrix(self.data[0], n_wires=n_wires)
 
     def sparse_matrix(self, wire_order: Sequence = None):
-        # maybe some logic here to check / handle non sequence types
         n_wires = len(self.wires) if wire_order is None else len(wire_order)
         return self.compute_sparse_matrix(self.data[0], n_wires=n_wires)
 
