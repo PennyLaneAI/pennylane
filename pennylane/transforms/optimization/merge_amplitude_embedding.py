050076148c1285c9cd33a3059f65b7e170ee43e0
andrijapau <andrija.paurevic@xanadu.ai>
2025-02-13 12:34:04 -0500

    remove pause context

--- pennylane/transforms/optimization/merge_amplitude_embedding.py
+++ pennylane/transforms/optimization/merge_amplitude_embedding.py
@@ -75,48 +75,44 @@ def _get_plxpr_merge_amplitude_embedding():  # pylint: disable=missing-docstring
             See also: :meth:`~.interpret_operation_eqn`.
 
             """
-            with qml.capture.pause():
-                if not isinstance(op, AmplitudeEmbedding):
-                    self.new_operations.append(op)
-                    self.visited_wires = self.visited_wires.union(set(op.wires))
-                    return
-
-                if len(self.visited_wires.intersection(set(op.wires))) > 0:
-                    raise qml.DeviceError(
-                        f"Operation {op.name} cannot be used after other Operation applied in the same qubit "
-                    )
-
-                self.input_wires.append(op.wires)
-                self.input_vectors.append(op.parameters[0])
-                self.input_batch_size.append(op.batch_size)
+            if not isinstance(op, AmplitudeEmbedding):
+                self.new_operations.append(op)
                 self.visited_wires = self.visited_wires.union(set(op.wires))
+                return
+
+            if len(self.visited_wires.intersection(set(op.wires))) > 0:
+                raise qml.DeviceError(
+                    f"Operation {op.name} cannot be used after other Operation applied in the same qubit "
+                )
+
+            self.input_wires.append(op.wires)
+            self.input_vectors.append(op.parameters[0])
+            self.input_batch_size.append(op.batch_size)
+            self.visited_wires = self.visited_wires.union(set(op.wires))
 
         def purge_seen_operations(self):
             """Merge the gates and insert it at the beginning of the "seen" gates; then interpret said gates."""
-            with qml.capture.pause():
-                if len(self.input_wires) > 0:
-                    final_wires = self.input_wires[0]
-                    final_vector = self.input_vectors[0]
-                    final_batch_size = self.input_batch_size[0]
+            if len(self.input_wires) > 0:
+                final_wires = self.input_wires[0]
+                final_vector = self.input_vectors[0]
+                final_batch_size = self.input_batch_size[0]
 
-                    for w, v, b in zip(
-                        self.input_wires[1:],
-                        self.input_vectors[1:],
-                        self.input_batch_size[1:],
-                        strict=True,
-                    ):
-                        final_vector = final_vector[..., :, None] * v[..., None, :]
-                        final_batch_size = final_batch_size or b
-                        final_wires = final_wires + w
+                for w, v, b in zip(
+                    self.input_wires[1:],
+                    self.input_vectors[1:],
+                    self.input_batch_size[1:],
+                    strict=True,
+                ):
+                    final_vector = final_vector[..., :, None] * v[..., None, :]
+                    final_batch_size = final_batch_size or b
+                    final_wires = final_wires + w
 
-                        if final_batch_size:
-                            final_vector = reshape(final_vector, (final_batch_size, -1))
-                        else:
-                            final_vector = flatten(final_vector)
+                    if final_batch_size:
+                        final_vector = reshape(final_vector, (final_batch_size, -1))
+                    else:
+                        final_vector = flatten(final_vector)
 
-                    self.new_operations.insert(
-                        0, AmplitudeEmbedding(final_vector, wires=final_wires)
-                    )
+                self.new_operations.insert(0, AmplitudeEmbedding(final_vector, wires=final_wires))
 
             for op in self.new_operations:
                 super().interpret_operation(op)
