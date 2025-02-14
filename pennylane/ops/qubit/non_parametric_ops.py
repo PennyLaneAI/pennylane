4dc1a210556e4970653dd90fede07ab2354b1c16
Yushao Chen (Jerry) <chenys13@outlook.com>
2025-02-14 09:42:28 -0500

    Update pennylane/ops/qubit/non_parametric_ops.py
    Co-authored-by: Lee James O'Riordan <mlxd@users.noreply.github.com>

--- pennylane/ops/qubit/non_parametric_ops.py
+++ pennylane/ops/qubit/non_parametric_ops.py
@@ -1289,7 +1289,7 @@ class SWAP(Operation):
           (2, 1)        1
           (3, 3)        1
         """
-        return sparse.csr_matrix([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
+        return sparse.csr_matrix(([1,1,1,1], [0,2,1,3], [0,1,2,3,4]))
 
     @staticmethod
     def compute_decomposition(wires: WiresLike) -> list[qml.operation.Operator]:
