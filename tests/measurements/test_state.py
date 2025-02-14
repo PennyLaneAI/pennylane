87fef473694b17763bfb4f36f6705972aeb3b909
JerryChen97 <chenys13@outlook.com>
2025-02-14 16:16:43 -0500

    format

--- tests/measurements/test_state.py
+++ tests/measurements/test_state.py
@@ -15,10 +15,10 @@
 
 import numpy as np
 import pytest
+from default_qubit_legacy import DefaultQubitLegacy
 
 import pennylane as qml
 from pennylane import numpy as pnp
-from default_qubit_legacy import DefaultQubitLegacy
 from pennylane.math.matrix_manipulation import _permute_dense_matrix
 from pennylane.math.quantum import reduce_dm, reduce_statevector
 from pennylane.measurements import DensityMatrixMP, StateMP, density_matrix, expval, state
