25ac1cc85639286c7c837a94dab8b36b1b3f33f0
Shuli Shu <08cnbj@gmail.com>
2025-02-18 16:04:39 -0500

    add docstr to unit tests

--- tests/ftqc/test_lattice.py
+++ tests/ftqc/test_lattice.py
@@ -20,7 +20,10 @@ from pennylane.ftqc import Lattice, generate_lattice
 
 
 class TestLattice:
+    """Test for the qml.ftqc.Lattice class."""
+
     def test_lattice_creation_with_graph(self):
+        """Test for Lattice object created by a nx.Graph object."""
         graph = nx.Graph([(0, 1), (1, 2)])
         lattice = Lattice("test", graph=graph)
         assert lattice.get_lattice_shape() == "test"
@@ -28,6 +31,7 @@ class TestLattice:
         assert len(lattice.get_edges()) == 2
 
     def test_lattice_creation_with_nodes_and_edges(self):
+        """Test for Lattice object created by a list of nodes and edges."""
         nodes = [0, 1, 2]
         edges = [(0, 1), (1, 2)]
         lattice = Lattice("test", nodes=nodes, edges=edges)
@@ -35,14 +39,17 @@ class TestLattice:
         assert len(lattice.get_edges()) == 2
 
     def test_lattice_creation_invalid(self):
+        """Test for Lattice object created with lattice name only. ValueError will be raised as neither graph nor nodes/edges provided"""
         with pytest.raises(ValueError):
-            Lattice("test")  # Neither graph nor nodes/edges provided
+            Lattice("test")
 
     def test_get_lattice_shape(self):
+        """Test for get_lattice_shape()."""
         lattice = Lattice("test_shape", nx.Graph())
         assert lattice.get_lattice_shape() == "test_shape"
 
     def test_relabel_nodes(self):
+        """Test for nodes relabelling."""
         graph = nx.Graph()
         graph.add_nodes_from([0, 1, 2])
         graph.add_edges_from([(0, 1), (1, 2)])
@@ -52,6 +59,7 @@ class TestLattice:
         assert list(lattice.get_nodes()) == ["a", "b", "c"]
 
     def test_set_node_attributes(self):
+        """Test for adding attributes to nodes in a graph."""
         graph = nx.Graph()
         graph.add_nodes_from([0, 1, 2])
         graph.add_edges_from([(0, 1), (1, 2)])
@@ -61,6 +69,7 @@ class TestLattice:
         assert lattice.get_node_attributes("color") == attributes
 
     def test_set_edge_attributes_dict(self):
+        """Test for adding attributes to edges in a graph with provided dict."""
         graph = nx.Graph([(0, 1), (1, 2)])
         lattice = Lattice("test", graph=graph)
         attributes = {(0, 1): {"weight": 10}, (1, 2): {"weight": 20}}
@@ -68,6 +77,7 @@ class TestLattice:
         assert lattice.get_edge_attributes("weight") == attributes
 
     def test_set_edge_attributes_scalar(self):
+        """Test for adding attributes to edges in a graph with provided scalar."""
         graph = nx.Graph([(0, 1), (1, 2)])
         lattice = Lattice("test", graph=graph)
         lattice.set_edge_attributes("weight", 10)
@@ -75,59 +85,70 @@ class TestLattice:
         assert nx.get_edge_attributes(lattice._graph, "weight") == expected_attributes
 
     def test_get_neighbors(self):
+        """Test for getting the neighbors of a node."""
         graph = nx.grid_graph([3, 3])
         lattice = Lattice("rectangle", graph)
         neighbors = list(lattice.get_neighbors((1, 1)))
         assert len(neighbors) == 4
 
     def test_get_nodes(self):
+        """Test for getting nodes."""
         nodes = [0, 1, 2]
         edges = [(0, 1), (1, 2)]
         lattice = Lattice("test", nodes=nodes, edges=edges)
-        assert set(lattice.get_nodes()) == set(nodes)  # Use set for unordered comparison
+        assert set(lattice.get_nodes()) == set(nodes)
 
     def test_get_edges(self):
+        """Test for getting edges."""
         edges = [(0, 1), (1, 2)]
         lattice = Lattice("test", nodes=[0, 1, 2], edges=edges)
-        assert set(lattice.get_edges()) == set(edges)  # Use set for unordered comparison
-
+        assert set(lattice.get_edges()) == set(edges)
     def test_get_graph(self):
+        """Test for getting graph."""
         graph = nx.Graph([(0, 1), (1, 2)])
         lattice = Lattice("test", graph=graph)
-        assert lattice.get_graph() is graph  # Check if it's the same object
+        assert lattice.get_graph() is graph
 
 
 class TestGenerateLattice:
+    """Test for generate_lattice method."""
     def test_generate_chain_lattice(self):
+        """Test for generate a 1D chain lattice."""
         lattice = generate_lattice("chain", [5])
         assert isinstance(lattice, Lattice)
         assert len(lattice.get_nodes()) == 5
 
     def test_generate_rectangle_lattice(self):
+        """Test for generate a 2D rectangle lattice."""
         lattice = generate_lattice("rectangle", [3, 4])
         assert isinstance(lattice, Lattice)
         assert len(lattice.get_nodes()) == 12
 
     def test_generate_cubic_lattice(self):
+        """Test for generate a 3D cubic lattice."""
         lattice = generate_lattice("cubic", [2, 2, 2])
         assert isinstance(lattice, Lattice)
         assert len(lattice.get_nodes()) == 8
 
     def test_generate_triangle_lattice(self):
+        """Test for generate a 2D triangle lattice."""
         lattice = generate_lattice("triangle", [3, 4])
         assert isinstance(lattice, Lattice)
-        assert len(lattice.get_nodes()) > 0  # Basic check
+        assert len(lattice.get_nodes()) > 0
 
     def test_generate_honeycomb_lattice(self):
+        """Test for generate a 3D honeycomb lattice."""
         lattice = generate_lattice("honeycomb", [3, 4])
         assert isinstance(lattice, Lattice)
-        assert len(lattice.get_nodes()) > 0  # Basic check
+        assert len(lattice.get_nodes()) > 0
 
     def test_generate_invalid_lattice_shape(self):
+        """Test for an unsupported lattice shape."""
         with pytest.raises(ValueError):
             generate_lattice("invalid_shape", [2, 2])
 
     def test_generate_invalid_dimensions(self):
+        """Test for an incorrect dims input."""
         with pytest.raises(ValueError):
             generate_lattice("chain", [2, 2])
         with pytest.raises(ValueError):
