bab14aa13ccf316614768a10327807c55fbc5180
Joey Carter <joseph.carter@xanadu.ai>
2025-02-20 12:57:59 -0500

    Fix import order

--- pennylane/ftqc/qubit_graph.py
+++ pennylane/ftqc/qubit_graph.py
@@ -17,7 +17,7 @@ and defines the API to access and manipulate these data structures.
 """
 
 import warnings
-from typing import Union, Optional
+from typing import Optional, Union
 
 import networkx as nx
 
