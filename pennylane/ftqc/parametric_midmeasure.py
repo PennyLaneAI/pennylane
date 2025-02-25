8b59d23b4dc8a1b158d83daa7b69a4bd577bc87a
lillian542 <Lillian.frederiksen@xanadu.ai>
2025-02-25 16:53:55 -0500

    add details about axis, plane and angle

--- pennylane/ftqc/parametric_midmeasure.py
+++ pennylane/ftqc/parametric_midmeasure.py
@@ -95,6 +95,22 @@ def measure_arbitrary_basis(
     tensor([0.90165331, 0.09834669], requires_grad=True)
 
     .. details::
+        :title: Plane and angle
+
+        The plane and angle are related to the axis of measurement by the following formuals:
+
+        :math:`XY=\frac{1}{\sqrt{2}} (|0\rangle + e^{i\phi} |1\rangle)`,
+
+        :math:`YZ=(\cos{\frac{\theta}{2}}|0\rangle + i \sin{\frac{\theta}{2}} |1\rangle)`, and
+
+        :math:`ZX= (\cos{\frac{\theta}{2}}|0\rangle + \sin{\frac{\theta}{2}} |1\rangle)`
+
+        where, in terms of spherical coordinates in the physics convention, the angle :math:`\phi`
+        is the azimuthal angle denoting rotation of the axis of measurement around the Z (polar) axis of
+        the Bloch sphere, and angle :math:`theta` is the polar angle between the axis of measurement and
+        the Z (polar) axis.
+
+    .. details::
         :title: Using mid-circuit measurements
 
         Measurement outcomes can be used to conditionally apply operations, and measurement
