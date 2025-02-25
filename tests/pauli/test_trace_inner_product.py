2b740f502c0617ecc675ef71d530707eba313f74
Korbinian Kottmann <43949391+Qottmann@users.noreply.github.com>
2025-02-26 00:01:56 +0100

    Apply suggestions from code review
    Co-authored-by: Diego <67476785+DSGuala@users.noreply.github.com>

--- tests/pauli/test_trace_inner_product.py
+++ tests/pauli/test_trace_inner_product.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for pennylane/labs/dla/dense_util.py functionality"""
+"""Tests for pennylane/pauli/dla/dense_util.py functionality"""
 import numpy as np
 import pytest
 
