d54e819968b726c972a1ee500a3420491ecf04a9
andrijapau <andrija.paurevic@xanadu.ai>
2025-02-26 16:40:23 -0500

    account for scipy in single_dispatch.py

--- pennylane/math/single_dispatch.py
+++ pennylane/math/single_dispatch.py
@@ -55,8 +55,13 @@ def _builtins_shape(x):
     return ar.shape(x)
 
 
+def _builtins_coerce(x, like=None):
+    return ar.numpy.asarray(x, like=like)
+
+
 ar.register_function("builtins", "ndim", _builtins_ndim)
 ar.register_function("builtins", "shape", _builtins_shape)
+ar.register_function("builtins", "coerce", _builtins_coerce)
 ar.register_function("builtins", "logical_mod", lambda x, y: x % y)
 ar.register_function("builtins", "logical_xor", lambda x, y: x ^ y)
 
@@ -64,8 +69,9 @@ ar.register_function("builtins", "logical_xor", lambda x, y: x ^ y)
 # the following is required to ensure that SciPy sparse Hamiltonians passed to
 # qml.SparseHamiltonian are not automatically 'unwrapped' to dense NumPy arrays.
 ar.register_function("scipy", "to_numpy", lambda x: x)
-
+ar.register_function("scipy", "coerce", ar.numpy.coerce)
 ar.register_function("scipy", "shape", np.shape)
+ar.register_function("scipy", "dot", np.dot)
 ar.register_function("scipy", "conj", np.conj)
 ar.register_function("scipy", "transpose", np.transpose)
 ar.register_function("scipy", "ndim", np.ndim)
