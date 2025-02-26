5a054306b0d877dfed51df41cb5a54abebc3fa91
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2025-02-26 08:58:05 -0500

    Update pennylane/templates/subroutines/qsvt.py
    Co-authored-by: Utkarsh <utkarshazad98@gmail.com>

--- pennylane/templates/subroutines/qsvt.py
+++ pennylane/templates/subroutines/qsvt.py
@@ -953,7 +953,7 @@ def poly_to_angles(poly, routine, angle_solver: Literal["root-finding"] = "root-
 
     """
 
-    poly = qml.math.array(poly, like="numpy")
+    poly = qml.math.trim_zeros(qml.math.array(poly, like="numpy"))
     poly = np.trim_zeros(poly, "b")
 
     if len(poly) == 1:
