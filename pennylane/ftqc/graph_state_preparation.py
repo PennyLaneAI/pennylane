639ec6d0d0289eacabf6f849d5bdf1e31f7e6bed
Shuli Shu <08cnbj@gmail.com>
2025-02-28 22:22:05 +0000

    update

--- pennylane/ftqc/graph_state_preparation.py
+++ pennylane/ftqc/graph_state_preparation.py
@@ -85,11 +85,11 @@ class GraphStatePreparation(Operation):
         op_list = []
         # Add qubit_ops to the queue
         # traverse the nodes in the qubit graph
-        for v in lattice.get_graph:
+        for v in lattice.graph:
             op_list.append(qubit_ops(wires[v]))
 
         # Add entanglement_ops to the queue
         # traverse the edges in the qubit graph
-        for v0, v1 in lattice.get_edges:
+        for v0, v1 in lattice.edges:
             op_list.append(entanglement_ops(wires[v0], wires[v1]))
         return op_list
