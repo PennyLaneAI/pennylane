c81d9be683635d8f512b00ed4efb5b0e475fa060
Austin Huang <gengminghuang@gmail.com>
2025-03-03 13:30:04 -0500

    import fix

--- pennylane/io/__init__.py
+++ pennylane/io/__init__.py
@@ -3,5 +3,13 @@ This module contains functions to load circuits from other frameworks as
 PennyLane templates.
 """
 
-from .io import *
+from .io import (
+    from_pyquil,
+    from_qasm,
+    from_qiskit,
+    from_qiskit_noise,
+    from_qiskit_op,
+    from_quil,
+    from_quil_file,
+)
 from .qualtran_io import FromBloq, get_bloq_registers_info
