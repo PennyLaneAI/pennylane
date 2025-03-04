dcec6848382c54a337d26a1d5dda115d4fc88bdc
Will <wmaxwell90@gmail.com>
2025-03-04 11:09:01 -0500

    remove commented code

--- pennylane/labs/tests/trotter/fragments/test_trotter.py
+++ pennylane/labs/tests/trotter/fragments/test_trotter.py
@@ -25,17 +25,6 @@ def test_epsilon(modes):
         2 * nested_commutator([fragments[2], fragments[1], fragments[2]])
     ]
 
-    #terms = [
-    #    vham._commute_fragments(0, 0, 1),
-    #    2 * vham._commute_fragments(1, 0, 1),
-    #    2 * vham._commute_fragments(2, 0, 1),
-    #    vham._commute_fragments(0, 0, 2),
-    #    2 * vham._commute_fragments(1, 0, 2),
-    #    2 * vham._commute_fragments(2, 0, 2),
-    #    vham._commute_fragments(1, 1, 2),
-    #    2 * vham._commute_fragments(2, 1, 2),
-    #]
-
     actual = trotter(fragments, delta, order=2)
     expected = scalar * sum(terms, VibronicMatrix(states, modes))
 
