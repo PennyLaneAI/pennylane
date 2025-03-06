ab0ab427ee2486b419355b4730b387dc332e56b7
qottmann <korbinian.kottmann@gmail.com>
2025-03-06 10:08:20 +0100

    fix tests

--- tests/liealg/test_involutions.py
+++ tests/liealg/test_involutions.py
@@ -21,7 +21,7 @@ import pytest
 
 import pennylane as qml
 from pennylane import X, Y, Z
-from pennylane.liealg import AI, AII, AIII, BDI, CI, CII, DIII, ClassB
+from pennylane.liealg import AI, AII, AIII, BD, BDI, CI, CII, DIII, A, C
 from pennylane.liealg.involutions import Ipq, J, Kpq
 
 
@@ -160,7 +160,9 @@ DIII_cases = [  # I/Y on wire 0?
     (Z(0) + 0.2 * X(2) @ X(0), False),
 ]
 
-ClassB_cases = DIII_cases  # ClassB = DIII
+A_cases = DIII_cases  # A = DIII
+BD_cases = DIII_cases  # BD = DIII
+C_cases = DIII_cases  # C = DIII
 
 
 class TestInvolutions:
@@ -210,7 +212,17 @@ class TestInvolutions:
         """Test singledispatch for DIII involution"""
         self.run_test_case(op, expected, DIII)
 
-    @pytest.mark.parametrize("op, expected", ClassB_cases)
-    def test_ClassB(self, op, expected):
-        """Test singledispatch for ClassB involution"""
-        self.run_test_case(op, expected, ClassB)
+    @pytest.mark.parametrize("op, expected", A_cases)
+    def test_A(self, op, expected):
+        """Test singledispatch for A involution"""
+        self.run_test_case(op, expected, A)
+
+    @pytest.mark.parametrize("op, expected", BD_cases)
+    def test_BD(self, op, expected):
+        """Test singledispatch for BD involution"""
+        self.run_test_case(op, expected, BD)
+
+    @pytest.mark.parametrize("op, expected", C_cases)
+    def test_C(self, op, expected):
+        """Test singledispatch for C involution"""
+        self.run_test_case(op, expected, C)
