1dceee5d376c4172fbd39b19c61421d1c721d618
Will <wmaxwell90@gmail.com>
2025-03-06 09:38:37 -0500

    remove utils folder

--- pennylane/labs/trotter/realspace/realspace_operator.py
+++ pennylane/labs/trotter/realspace/realspace_operator.py
@@ -11,7 +11,7 @@ import numpy as np
 import scipy as sp
 
 from pennylane.labs.trotter import Fragment
-from pennylane.labs.trotter.utils import _zeros, op_norm, string_to_matrix, tensor_with_identity
+from pennylane.labs.trotter.realspace.matrix import _zeros, op_norm, string_to_matrix, tensor_with_identity
 
 from .realspace_coefficients import RealspaceCoeffs
 
