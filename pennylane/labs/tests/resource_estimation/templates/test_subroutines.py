b8c35ba220fe22823dc5c17b7a0280a7688585d0
Austin Huang <gengminghuang@gmail.com>
2025-03-06 13:52:45 -0500

    qrom resource params

--- pennylane/labs/tests/resource_estimation/templates/test_subroutines.py
+++ pennylane/labs/tests/resource_estimation/templates/test_subroutines.py
@@ -15,6 +15,7 @@
 Test the ResourceQFT class
 """
 import pytest
+import math
 
 import pennylane.labs.resource_estimation as re
 import pennylane as qml
@@ -225,16 +226,16 @@ class TestPhaseAdder:
             {"num_ctrl_wires": 1, "num_ctrl_values": 0, "num_work_wires": 1},
         )
 
-        gate_types = {}
-        gate_types[qft] = 2
-        gate_types[qft_dag] = 2
-        gate_types[phase_shift] = 2 * num_x_wires
-        gate_types[phase_shift_dag] = 2 * num_x_wires
-        gate_types[ctrl_phase_shift] = num_x_wires
-        gate_types[cnot] = 1
-        gate_types[multix] = 1
+        expected = {}
+        expected[qft] = 2
+        expected[qft_dag] = 2
+        expected[phase_shift] = 2 * num_x_wires
+        expected[phase_shift_dag] = 2 * num_x_wires
+        expected[ctrl_phase_shift] = num_x_wires
+        expected[cnot] = 1
+        expected[multix] = 1
 
-        assert re.ResourcePhaseAdder.resources(mod, num_x_wires) == gate_types
+        assert re.ResourcePhaseAdder.resources(mod, num_x_wires) == expected
 
     @pytest.mark.parametrize(
         "mod, x_wires",
@@ -306,16 +307,16 @@ class TestPhaseAdder:
             {"num_ctrl_wires": 1, "num_ctrl_values": 0, "num_work_wires": 1},
         )
 
-        gate_types = {}
-        gate_types[qft] = 2
-        gate_types[qft_dag] = 2
-        gate_types[phase_shift] = 2 * num_x_wires
-        gate_types[phase_shift_dag] = 2 * num_x_wires
-        gate_types[ctrl_phase_shift] = num_x_wires
-        gate_types[cnot] = 1
-        gate_types[multix] = 1
+        expected = {}
+        expected[qft] = 2
+        expected[qft_dag] = 2
+        expected[phase_shift] = 2 * num_x_wires
+        expected[phase_shift_dag] = 2 * num_x_wires
+        expected[ctrl_phase_shift] = num_x_wires
+        expected[cnot] = 1
+        expected[multix] = 1
 
-        assert actual == gate_types
+        assert actual == expected
 
     def test_tracking_name(self):
         """Test that the tracking name is correct."""
@@ -367,14 +368,14 @@ class TestResourceMultiplier:
 
         cnot = re.CompressedResourceOp(re.ResourceCNOT, {})
 
-        gate_types = {}
-        gate_types[qft] = 2
-        gate_types[qft_dag] = 2
-        gate_types[sequence] = 1
-        gate_types[sequence_dag] = 1
-        gate_types[cnot] = min(num_x_wires, num_aux_swap)
+        expected = {}
+        expected[qft] = 2
+        expected[qft_dag] = 2
+        expected[sequence] = 1
+        expected[sequence_dag] = 1
+        expected[cnot] = min(num_x_wires, num_aux_swap)
 
-        assert re.ResourceMultiplier.resources(mod, num_work_wires, num_x_wires) == gate_types
+        assert re.ResourceMultiplier.resources(mod, num_work_wires, num_x_wires) == expected
 
     @pytest.mark.parametrize(
         "k, mod, work_wires, x_wires",
@@ -483,7 +484,7 @@ class TestResourceModExp:
         mult_resources = re.ResourceMultiplier._resource_decomp(
             mod, num_work_wires, num_output_wires
         )
-        gate_types = {}
+        expected = {}
 
         for comp_rep, _ in mult_resources.items():
             new_rep = re.CompressedResourceOp(
@@ -498,13 +499,13 @@ class TestResourceModExp:
             )
 
             if comp_rep._name in ("QFT", "Adjoint(QFT)"):
-                gate_types[new_rep] = 1
+                expected[new_rep] = 1
             else:
-                gate_types[new_rep] = mult_resources[comp_rep] * ((2**num_x_wires) - 1)
+                expected[new_rep] = mult_resources[comp_rep] * ((2**num_x_wires) - 1)
 
         assert (
             re.ResourceModExp.resources(mod, num_output_wires, num_work_wires, num_x_wires)
-            == gate_types
+            == expected
         )
 
     @pytest.mark.parametrize(
@@ -524,6 +525,188 @@ class TestResourceModExp:
             "mod": mod,
             "num_work_wires": len(work_wires),
             "num_x_wires": len(x_wires),
+        }
+
+    @pytest.mark.parametrize(
+        "mod, num_output_wires, num_work_wires, num_x_wires",
+        [(7, 5, 5, 3), (8, 5, 5, 3)],
+    )
+    def test_resource_rep(self, mod, num_output_wires, num_work_wires, num_x_wires):
+        """Test the resource_rep returns the correct CompressedResourceOp"""
+
+        expected = re.CompressedResourceOp(
+            re.ResourceModExp,
+            {
+                "mod": mod,
+                "num_output_wires": num_output_wires,
+                "num_work_wires": num_work_wires,
+                "num_x_wires": num_x_wires,
+            },
+        )
+        assert expected == re.ResourceModExp.resource_rep(
+            mod, num_output_wires, num_work_wires, num_x_wires
+        )
+
+    @pytest.mark.parametrize(
+        "mod, num_output_wires, num_work_wires, num_x_wires",
+        [(7, 5, 5, 3), (8, 5, 5, 3)],
+    )
+    def test_resources_from_rep(self, num_output_wires, mod, num_work_wires, num_x_wires):
+        """Test that computing the resources from a compressed representation works"""
+        mult_resources = re.ResourceMultiplier._resource_decomp(
+            mod, num_work_wires, num_output_wires
+        )
+        expected = {}
+
+        for comp_rep, _ in mult_resources.items():
+            new_rep = re.CompressedResourceOp(
+                re.ResourceControlled,
+                {
+                    "base_class": comp_rep.op_type,
+                    "base_params": comp_rep.params,
+                    "num_ctrl_wires": 1,
+                    "num_ctrl_values": 0,
+                    "num_work_wires": 0,
+                },
+            )
+
+            if comp_rep._name in ("QFT", "Adjoint(QFT)"):
+                expected[new_rep] = 1
+            else:
+                expected[new_rep] = mult_resources[comp_rep] * ((2**num_x_wires) - 1)
+
+        rep = re.ResourceModExp.resource_rep(mod, num_output_wires, num_work_wires, num_x_wires)
+        actual = rep.op_type.resources(**rep.params)
+
+        assert actual == expected
+
+    def test_tracking_name(self):
+        """Test that the tracking name is correct."""
+        assert re.ResourceModExp.tracking_name() == f"ModExp"
+
+
+class TestResourceQROM:
+    """Test the ResourceQROM class"""
+
+    @pytest.mark.parametrize(
+        "num_bitstrings, num_bit_flips, num_control_wires, num_work_wires, size_bitstring, clean",
+        [(4, 2, 2, 3, 3, True), (4, 5, 2, 3, 3, False)],
+    )
+    def test_resources(
+        self,
+        num_bitstrings,
+        num_bit_flips,
+        num_control_wires,
+        num_work_wires,
+        size_bitstring,
+        clean,
+    ):
+        expected = {}
+        x = re.CompressedResourceOp(re.ResourceX, {})
+
+        if num_control_wires == 0:
+            expected[x] = num_bit_flips
+            assert (
+                re.ResourceQROM.resources(
+                    num_bitstrings,
+                    num_bit_flips,
+                    num_control_wires,
+                    num_work_wires,
+                    size_bitstring,
+                    clean,
+                )
+                == expected
+            )
+            return
+
+        cnot = re.CompressedResourceOp(re.ResourceCNOT, {})
+        hadamard = re.CompressedResourceOp(re.ResourceHadamard, {})
+
+        num_parallel_computations = (num_work_wires + size_bitstring) // size_bitstring
+        num_parallel_computations = min(num_parallel_computations, num_bitstrings)
+
+        num_swap_wires = math.floor(math.log2(num_parallel_computations))
+        num_select_wires = math.ceil(math.log2(math.ceil(num_bitstrings / (2**num_swap_wires))))
+        assert num_swap_wires + num_select_wires <= num_control_wires
+
+        swap_clean_prefactor = 1
+        select_clean_prefactor = 1
+
+        if clean:
+            expected[hadamard] = 2 * size_bitstring
+            swap_clean_prefactor = 4
+            select_clean_prefactor = 2
+
+        # SELECT cost:
+        expected[cnot] = num_bit_flips  # each unitary in the select is just a CNOT
+
+        multi_x = re.CompressedResourceOp(
+            re.ResourceMultiControlledX,
+            {
+                "num_ctrl_wires": num_select_wires,
+                "num_ctrl_values": 0,
+                "num_work_wires": 0,
+            },
+        )
+
+        num_total_ctrl_possibilities = 2**num_select_wires
+        expected[multi_x] = select_clean_prefactor * (
+            2 * num_total_ctrl_possibilities  # two applications targetting the aux qubit
+        )
+        num_zero_controls = (2 * num_total_ctrl_possibilities * num_select_wires) // 2
+        expected[x] = select_clean_prefactor * (
+            num_zero_controls * 2  # conjugate 0 controls on the multi-qubit x gates from above
+        )
+        # SWAP cost:
+        ctrl_swap = re.CompressedResourceOp(re.ResourceCSWAP, {})
+        expected[ctrl_swap] = swap_clean_prefactor * ((2**num_swap_wires) - 1) * size_bitstring
+
+        assert (
+            re.ResourceQROM.resources(
+                num_bitstrings,
+                num_bit_flips,
+                num_control_wires,
+                num_work_wires,
+                size_bitstring,
+                clean,
+            )
+            == expected
+        )
+
+    @pytest.mark.parametrize(
+        "bitstrings, control_wires, work_wires, target_wires, clean",
+        [
+            (["000", "001", "010", "100"], [0, 1], [2, 3, 4], [5, 6, 7], True),
+            (["010", "111", "110", "000"], [0, 1], [2, 3, 4], [5, 6, 7], False),
+        ],
+    )
+    def test_resource_params(self, bitstrings, control_wires, work_wires, target_wires, clean):
+        """Test that the resource params are correct"""
+        op = re.ResourceQROM(
+            bitstrings=bitstrings,
+            control_wires=control_wires,
+            target_wires=work_wires,
+            work_wires=target_wires,
+            clean=clean,
+        )
+
+        num_bitstrings = len(bitstrings)
+
+        num_bit_flips = 0
+        for bit_string in bitstrings:
+            num_bit_flips += bit_string.count("1")
+
+        num_work_wires = len(work_wires)
+        size_bitstring = len(target_wires)
+        num_control_wires = len(control_wires)
+
+        assert op.resource_params == {
+            "num_bitstrings": num_bitstrings,
+            "num_bit_flips": num_bit_flips,
+            "num_control_wires": num_control_wires,
+            "num_work_wires": num_work_wires,
+            "size_bitstring": size_bitstring,
+            "clean": clean,
         }
 
     @pytest.mark.parametrize(
