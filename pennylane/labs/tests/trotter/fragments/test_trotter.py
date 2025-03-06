fd65db2c6877d2797e9748bd93b4f4d08f982b24
Will <wmaxwell90@gmail.com>
2025-03-06 09:32:11 -0500

    move private coeff helper function

--- pennylane/labs/tests/trotter/fragments/test_trotter.py
+++ pennylane/labs/tests/trotter/fragments/test_trotter.py
@@ -1,10 +1,33 @@
 import pytest
+import numpy as np
 
 from pennylane.labs.trotter import nested_commutator
 from pennylane.labs.trotter.fragments import vibronic_fragments
 from pennylane.labs.trotter.product_formulas import trotter
-from pennylane.labs.trotter.utils import coeffs
 from pennylane.labs.trotter.realspace import VibronicMatrix
+
+def _coeffs(states: int, modes: int, order: int):
+    """Produce random coefficients for input"""
+
+    phis = []
+    symmetric_phis = []
+    for i in range(order + 1):
+        shape = (states, states) + (modes,) * i
+        phi = np.random.random(size=shape)
+        phis.append(phi)
+        symmetric_phis.append(np.zeros(shape))
+
+    for phi in phis:
+        for i in range(states):
+            for j in range(states):
+                phi[i, j] = (phi[i, j] + phi[i, j].T) / 2
+
+    for phi, symmetric_phi in zip(phis, symmetric_phis):
+        for i in range(states):
+            for j in range(states):
+                symmetric_phi[i, j] = (phi[i, j] + phi[j, i]) / 2
+
+    return np.random.random(size=(modes,)), symmetric_phis
 
 @pytest.mark.parametrize("modes", range(5))
 def test_epsilon(modes):
@@ -12,7 +35,7 @@ def test_epsilon(modes):
     states = 2
     delta = 0.72
     scalar = -(delta**2) / 24
-    fragments = vibronic_fragments(states, modes, *coeffs(states, modes, order=2))
+    fragments = vibronic_fragments(states, modes, *_coeffs(states, modes, order=2))
 
     terms = [
         nested_commutator([fragments[0], fragments[0], fragments[1]]),
