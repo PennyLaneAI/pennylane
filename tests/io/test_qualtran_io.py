07e5e0e98479c6d3467c0441fd72396d14320dca
Austin Huang <gengminghuang@gmail.com>
2025-03-07 11:17:21 -0500

    additional testing

--- tests/io/test_qualtran_io.py
+++ tests/io/test_qualtran_io.py
@@ -163,6 +163,7 @@ class TestFromBloq:
             qml.FromBloq(textbook_qpe_small, wires=list(range(4)))
             return qml.state()
 
+        # Expected value computed via tensor_contract()
         assert np.allclose(
             circuit(),
             np.array(
