8c898e6f78415cf66bb0415513a6a8111f588a50
Astral Cai <astral.cai@xanadu.ai>
2025-03-12 11:10:09 -0400

    simplify tests

--- tests/decomposition/conftest.py
+++ tests/decomposition/conftest.py
@@ -12,135 +12,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Utility methods and dummy classes for testing the decomposition graph."""
+"""A fixed set of decomposition rules for testing purposes."""
 
 # pylint: disable=too-few-public-methods
 
 from collections import defaultdict
 
 import pennylane as qml
-from pennylane.operation import Operation
 
 decompositions = defaultdict(list)
 
 
-class CustomHadamard(Operation):
-    r"""Hadamard(wires)
-    The Hadamard operator"""
-
-    resource_param_keys = ()
-
-    @property
-    def resource_params(self) -> dict:
-        return {}
-
-
-class CustomRX(Operation):
-    r"""
-    The single qubit X rotation
-    """
-
-    resource_param_keys = ()
-
-    @property
-    def resource_params(self) -> dict:
-        return {}
-
-
-class CustomRY(Operation):
-    r"""
-    The single qubit Y rotation
-    """
-
-    resource_param_keys = ()
-
-    @property
-    def resource_params(self) -> dict:
-        return {}
-
-
-class CustomRZ(Operation):
-    r"""
-    The single qubit Z rotation
-    """
-
-    resource_param_keys = ()
-
-    @property
-    def resource_params(self) -> dict:
-        return {}
-
-
-class CustomRot(Operation):
-    r"""
-    The single qubit arbitrary rotation
-    """
-
-    resource_param_keys = ()
-
-    @property
-    def resource_params(self) -> dict:
-        return {}
-
-
-class CustomPhaseShift(Operation):
-    """Phase shift gate."""
-
-    resource_param_keys = ()
-
-    @property
-    def resource_params(self) -> dict:
-        return {}
-
-
-class CustomCNOT(Operation):
-    """The CNOT gate."""
-
-    resource_param_keys = ()
-
-    @property
-    def resource_params(self) -> dict:
-        return {}
-
-
-class CustomCZ(Operation):
-    """The CZ gate."""
-
-    resource_param_keys = ()
-
-    @property
-    def resource_params(self) -> dict:
-        return {}
-
-
-class CustomMultiRZ(Operation):
-    """The MultiRZ gate."""
-
-    resource_param_keys = ("num_wires",)
-
-    @property
-    def resource_params(self) -> dict:
-        return {"num_wires": len(self.wires)}
-
-
-@qml.register_resources({CustomHadamard: 2, CustomCNOT: 1})
+@qml.register_resources({qml.Hadamard: 2, qml.CNOT: 1})
 def _cz_to_cnot(*_, **__):
     raise NotImplementedError
 
 
-decompositions[CustomCZ] = [_cz_to_cnot]
+decompositions[qml.CZ] = [_cz_to_cnot]
 
 
-@qml.register_resources({CustomHadamard: 2, CustomCZ: 1})
+@qml.register_resources({qml.Hadamard: 2, qml.CZ: 1})
 def _cnot_to_cz_h(*_, **__):
     raise NotImplementedError
 
 
-decompositions[CustomCNOT] = [_cnot_to_cz_h]
+decompositions[qml.CNOT] = [_cnot_to_cz_h]
 
 
 def _multi_rz_decomposition_resources(num_wires):
-    return {CustomRZ: 1, CustomCNOT: 2 * (num_wires - 1)}
+    return {qml.RZ: 1, qml.CNOT: 2 * (num_wires - 1)}
 
 
 @qml.register_resources(_multi_rz_decomposition_resources)
@@ -148,25 +48,25 @@ def _multi_rz_decomposition(*_, **__):
     raise NotImplementedError
 
 
-decompositions[CustomMultiRZ] = [_multi_rz_decomposition]
+decompositions[qml.MultiRZ] = [_multi_rz_decomposition]
 
 
-@qml.register_resources({CustomRZ: 2, CustomRX: 1, qml.GlobalPhase: 1})
+@qml.register_resources({qml.RZ: 2, qml.RX: 1, qml.GlobalPhase: 1})
 def _hadamard_to_rz_rx(*_, **__):
     raise NotImplementedError
 
 
-@qml.register_resources({CustomRZ: 1, CustomRY: 1, qml.GlobalPhase: 1})
+@qml.register_resources({qml.RZ: 1, qml.RY: 1, qml.GlobalPhase: 1})
 def _hadamard_to_rz_ry(*_, **__):
     raise NotImplementedError
 
 
-decompositions[CustomHadamard] = [_hadamard_to_rz_rx, _hadamard_to_rz_ry]
+decompositions[qml.Hadamard] = [_hadamard_to_rz_rx, _hadamard_to_rz_ry]
 
 
-@qml.register_resources({CustomRX: 1, CustomRZ: 2})
+@qml.register_resources({qml.RX: 1, qml.RZ: 2})
 def _ry_to_rx_rz(*_, **__):
     raise NotImplementedError
 
 
-decompositions[CustomRY] = [_ry_to_rx_rz]
+decompositions[qml.RY] = [_ry_to_rx_rz]
