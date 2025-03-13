7561973c4ab6f93a9c3cfa40b37d4ea07dd2aec6
Will <wmaxwell90@gmail.com>
2025-03-13 09:00:25 -0400

    run black

--- pennylane/labs/trotter/fragments/generic_fragments.py
+++ pennylane/labs/trotter/fragments/generic_fragments.py
@@ -1,8 +1,11 @@
 """Wrapper class for generic fragment objects"""
 
 from __future__ import annotations
+
 from typing import Any, Callable, Dict, List, Sequence
+
 from pennylane.labs.trotter import Fragment
+
 
 def generic_fragments(fragments: Sequence[Any], norm_fn: Callable = None) -> List[GenericFragment]:
     """Wrapper function for GenericFragment"""
