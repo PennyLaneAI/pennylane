81e592b88cc29cc1ac5db2aa0a1fb93edc62db7d
andrijapau <andrija.paurevic@xanadu.ai>
2025-03-18 12:01:00 -0400

    remove check to see if device program is empty or not

--- pennylane/workflow/_capture_qnode.py
+++ pennylane/workflow/_capture_qnode.py
@@ -201,24 +201,23 @@ def _(*args, qnode, shots, device, execution_config, qfunc_jaxpr, n_consts, batc
     non_const_args = args[n_consts:]
 
     device_program, config = device.preprocess(execution_config)
-    if device_program:
-        if batch_dims is not None:
-            temp_all_args = []
-            for a, d in zip(args, batch_dims, strict=True):
-                if d is not None:
-                    slices = [slice(None)] * qml.math.ndim(a)
-                    slices[d] = 0
-                    temp_all_args.append(a[tuple(slices)])
-                else:
-                    temp_all_args.append(a)
-            temp_consts = temp_all_args[:n_consts]
-            temp_args = temp_all_args[n_consts:]
-        else:
-            temp_consts = consts
-            temp_args = non_const_args
-        qfunc_jaxpr = device_program(qfunc_jaxpr, temp_consts, *temp_args)
-        consts = qfunc_jaxpr.consts
-        qfunc_jaxpr = qfunc_jaxpr.jaxpr
+    if batch_dims is not None:
+        temp_all_args = []
+        for a, d in zip(args, batch_dims, strict=True):
+            if d is not None:
+                slices = [slice(None)] * qml.math.ndim(a)
+                slices[d] = 0
+                temp_all_args.append(a[tuple(slices)])
+            else:
+                temp_all_args.append(a)
+        temp_consts = temp_all_args[:n_consts]
+        temp_args = temp_all_args[n_consts:]
+    else:
+        temp_consts = consts
+        temp_args = non_const_args
+    qfunc_jaxpr = device_program(qfunc_jaxpr, temp_consts, *temp_args)
+    consts = qfunc_jaxpr.consts
+    qfunc_jaxpr = qfunc_jaxpr.jaxpr
 
     partial_eval = partial(device.eval_jaxpr, qfunc_jaxpr, consts, execution_config=config)
     if batch_dims is None:
