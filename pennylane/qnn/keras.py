d2a248114f26867548ac7488ea9b6e309e5d530f
Yushao Chen (Jerry) <chenys13@outlook.com>
2025-03-19 11:45:56 -0400

    Update pennylane/qnn/keras.py
    Co-authored-by: Christina Lee <christina@xanadu.ai>

--- pennylane/qnn/keras.py
+++ pennylane/qnn/keras.py
@@ -55,7 +55,7 @@ class KerasLayer(Layer):
     creating quantum and hybrid models.
 
     .. warning::
-        This class is deprecated because Keras 2 is no longer actively maintained.  Please consider using a different machine learning framework instead of TensorFlow/Keras 2.
+        This class is deprecated because Keras 2 is no longer actively maintained.  Please consider using torch instead of TensorFlow/Keras 2.
 
     .. note::
 
