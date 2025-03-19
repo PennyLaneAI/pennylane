b916e734b3985c6c820d83442988a5f243e400cf
Will <wmaxwell90@gmail.com>
2025-03-19 18:42:58 -0400

    docs

--- pennylane/labs/trotter/__init__.py
+++ pennylane/labs/trotter/__init__.py
@@ -11,6 +11,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Module containing functions for computing Trotter error"""
+"""Module containing functions for computing Trotter error
+
+.. warning::
+
+    This module is experimental. Frequent changes will occur,
+    with no guarantees of stability or backwards compatibility.
+
+.. currentmodule:: pennylane.labs.resource_estimation
+
+Trotter Abstract Classes and Functions:
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. autosummary::
+    :toctree: api
+
+    ~Fragment
+    ~commutator
+    ~nested_commutator
+
+Realspace Hamiltonian Classes:
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. autosummary::
+    :toctree: api
+
+    ~RealspaceCoeffs
+    ~RealspaceOperator
+    ~RealspaceSum
+    ~VibronicMatrix
+"""
 
 from .abstract_fragment import Fragment, commutator, nested_commutator
+from .realspace import RealspaceCoeffs, RealspaceOperator, RealspaceSum, VibronicMatrix
