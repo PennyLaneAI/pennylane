1eb8b12c3923d6cf44f5e609e5d579a488ed931c
albi3ro <chrissie.c.l@gmail.com>
2025-03-21 10:45:44 -0400

    responding to feedback, moving helpers to support file

--- pennylane/control_flow/_loop_abstract_axes.py
+++ pennylane/control_flow/_loop_abstract_axes.py
@@ -21,11 +21,139 @@ as they are specific to just ``for_loop`` and ``while_loop``.
 """
 
 from collections import namedtuple
-from typing import Any
+from typing import Any, Callable, Optional
+
+import numpy as np
 
 from pennylane.typing import TensorLike
 
 AbstractShapeLocation = namedtuple("AbstractShapeLocation", ("arg_idx", "shape_idx"))
+
+
+def add_abstract_shapes(f, shape_locations: list[list[AbstractShapeLocation]]):
+    """Add the abstract shapes that the specified locations to the output of f.
+
+    .. code-block:: python
+
+        def f(x, y): return [x, y]
+
+        loc1 = AbstractShapeLocation(arg_idx=0, shape_idx=0)
+        loc2 = AbstractShapeLocation(arg_idx=1, shape_idx=1)
+        repeat_loc = AbstractShapeLocation(arg_idx=0, shape_idx=1)
+        locations = [[loc1, repeat_loc], [loc2]]
+
+        add_abstract_shapes(f, locations)(jnp.zeros((1,1)), jnp.zeros((3,4)))
+
+    .. code-block::
+
+        [1,
+        4,
+        Array([[0.]], dtype=float32),
+        Array([[0., 0., 0., 0.],
+                [0., 0., 0., 0.],
+                [0., 0., 0., 0.]], dtype=float32)]
+
+    """
+
+    def new_f(*args, **kwargs):
+        results = f(*args, **kwargs)
+        new_shapes = [results[loc[0].arg_idx].shape[loc[0].shape_idx] for loc in shape_locations]
+        return new_shapes + results
+
+    return new_f
+
+
+def get_dummy_arg(arg):
+    """If any axes are abstract, replace with an empty numpy array.
+
+    Even if abstracted_axes specifies two dimensions as having different dynamic shapes,
+    if they have the dimension is the same tracer, jaxpr will still treat them as the same shape.
+
+    .. code-block:: python
+
+        def f(a, b): return 0
+
+        def w(i0):
+            a = jnp.arange(i0)
+            b = jnp.arange(i0)
+            jaxpr = jax.make_jaxpr(f, abstracted_axes=({0:0}, {0:1}))(a, b)
+            print(jaxpr)
+
+        _ = jax.make_jaxpr(w)(2)
+
+    .. code-block::
+
+        { lambda ; a:i32[] b:i32[a] c:i32[a]. let  in (0,) }
+
+    So we need to override this behavior by not giving them any abstract shapes to focus on.
+    Instead, we just pass in an empty numpy array with all abstract dimensions replaced with ``2``.
+    We use numpy instead of jax so the creation of the array will not show up in the jaxpr.
+
+    """
+    if all(isinstance(s, int) for s in arg.shape):
+        return arg
+    shape = tuple(s if isinstance(s, int) else 2 for s in arg.shape)
+    return np.empty(shape=shape, dtype=arg.dtype)
+
+
+def validate_no_resizing_returns(
+    jaxpr: "jax.core.Jaxpr",
+    locations: list[list[AbstractShapeLocation]],
+    name: str = "while_loop",
+) -> Optional[str]:
+    """Validate that all jaxpr outputs that should have the same shape as specified in ``locations``
+    continue to have the same shape.  Returns a string with an error message so we can
+    either decide to raise the error, or try again with different settings.
+    """
+    offset = len(locations)  # number of abstract shapes. We start from the first normal arg.
+
+    for locations_list in locations:
+        loc0 = locations_list[0]
+        first_var = jaxpr.outvars[loc0.arg_idx + offset].aval.shape[loc0.shape_idx]
+        for compare_loc in locations_list[1:]:
+            compare_var = jaxpr.outvars[compare_loc.arg_idx + offset].aval.shape[
+                compare_loc.shape_idx
+            ]
+            if compare_var is not first_var:
+                return (
+                    "Detected dynamically shaped arrays being resized independently. "
+                    f"\nReturned variables at {loc0.arg_idx} and {compare_loc.arg_idx} must keep the same size "
+                    "with allow_array_resizing=False."
+                    f"\nPlease specify allow_array_resizing=True to `qml.{name}` to allow "
+                    "dynamically shaped arrays to be reshaped independently. "
+                )
+
+    return None
+
+
+def _has_dynamic_shape(val):
+    return any(not isinstance(s, int) for s in getattr(val, "shape", ()))
+
+
+def handle_jaxpr_error(
+    e: ValueError, fns: tuple[Callable, ...], allow_array_resizing, name: str = "while_loop"
+):
+    """Handle any ValueError's raised by the creation of the jaxpr, adding information to any error
+    about 'Incompatible shapes for broadcasting'."""
+    import jax  # pylint: disable=import-outside-toplevel
+
+    if "Incompatible shapes for broadcasting" in str(e) and jax.config.jax_dynamic_shapes:
+        closures = sum(((fn.__closure__ or ()) for fn in fns), ())
+        if any(_has_dynamic_shape(i.cell_contents) for i in closures):
+            msg = (
+                "Detected an attempt to combine arrays with different dynamic shapes. "
+                "\nThis also may be due to a closure variable with a dynamic shape."
+                " Try promoting the closure variable with the dynamic shape to being an explicit argument. "
+            )
+            if allow_array_resizing is True:
+                msg += "\nThis may also be due to allow_array_resizing=True. Try with allow_array_resizing=False instead."
+        else:
+            msg = (
+                "Detected an attempt to combine arrays with two different dynamic shapes. "
+                f"To keep dynamic shapes matching, please specify `allow_array_resizing=False` to `qml.{name}`."
+            )
+        raise ValueError(msg) from e
+    raise e
 
 
 # pylint: disable=too-few-public-methods
@@ -93,7 +221,7 @@ def loop_determine_abstracted_axes(
 
     .. code-block::
 
-        ({}, {0: 'a'}, {1: 'a'})
+        ({}, {0: 0}, {1: 0})
         [Traced<ShapedArray(int32[], weak_type=True)>with<DynamicJaxprTrace(level=1/0)>]
         [[AbstractShapeLocation(arg_idx=1, shape_idx=0), AbstractShapeLocation(arg_idx=2, shape_idx=1)]]
 
@@ -109,7 +237,7 @@ def loop_determine_abstracted_axes(
 
     .. code-block::
 
-        ({}, {0: 'a'}, {1: 'b'})
+        ({}, {0: 0}, {1: 1})
         [Traced<ShapedArray(int32[], weak_type=True)>with<DynamicJaxprTrace(level=1/0)>, Traced<ShapedArray(int32[], weak_type=True)>with<DynamicJaxprTrace(level=1/0)>]
         [[AbstractShapeLocation(arg_idx=1, shape_idx=0)], [AbstractShapeLocation(arg_idx=2, shape_idx=1)]]
 
