452dbb4c1d12b90e6c23b82b99e6b95a5de53f58
Will <wmaxwell90@gmail.com>
2025-03-24 15:48:49 -0400

    minor doc tweak

--- pennylane/labs/trotter_error/realspace/vibronic_matrix.py
+++ pennylane/labs/trotter_error/realspace/vibronic_matrix.py
@@ -36,7 +36,7 @@ class VibronicMatrix(Fragment):
 
     .. math:: V_{i,j} = \lambda_{i,j} + \sum_{r} \phi^{(1)}_{i,j,r} Q_r + \sum_{r,s} \phi^{(2)}_{i,j,r,s} Q_r Q_s + \sum_{r,s,t} \phi^{(3)}_{i,j,r,s,t} Q_r Q_s Q_t + \dots,
 
-    where the dictionary is indexed by tuples ``(i, j)`` and the values are :class:`~.RealspaceSum` objects representing the operator :math:`V_{i,j}`.
+    where the dictionary is indexed by tuples :math:`(i, j)` and the values are :class:`~.RealspaceSum` objects representing the operator :math:`V_{i,j}`.
 
     Args:
         states (int): the number of electronic states
