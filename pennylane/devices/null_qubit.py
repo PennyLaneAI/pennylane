886168af28f247742d97b5094f3d116afc4f7d35
Shuli Shu <08cnbj@gmail.com>
2025-04-01 16:07:36 -0400

    make format

--- pennylane/devices/null_qubit.py
+++ pennylane/devices/null_qubit.py
@@ -65,8 +65,8 @@ def _zero_measurement(
     if batch_size is not None:
         shape = (batch_size,) + shape
     if "jax" not in interface:
-        return _cached_zero_return(shape, interface, dtype = "int8" if shots else mp.numeric_type)
-    return math.zeros(shape, like=interface, dtype= "int8" if shots else mp.numeric_type)
+        return _cached_zero_return(shape, interface, dtype="int8" if shots else mp.numeric_type)
+    return math.zeros(shape, like=interface, dtype="int8" if shots else mp.numeric_type)
 
 
 @lru_cache(maxsize=128)
