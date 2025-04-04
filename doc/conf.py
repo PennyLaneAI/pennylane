013698a8de6d334164f302dc5328ff24a96213af
Runor Agbaire <runor.agbaire@xanadu.ai>
2025-04-04 15:37:36 -0400

    Changing Sphinx minimum version

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(".")), "doc"))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = "3.3"
+needs_sphinx = "4.0"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
