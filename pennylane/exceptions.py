a3a8eb11257acd396495e3bc80a82242f39bdded
andrijapau <andrija.paurevic@xanadu.ai>
2025-04-08 14:47:10 -0400

    modifying exceptions.py file description

--- pennylane/exceptions.py
+++ pennylane/exceptions.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-This module contains all the custom exceptions and warnings used in PennyLane.
+This file contains all the custom exceptions and warnings used in PennyLane.
 """
 
 
