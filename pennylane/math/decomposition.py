fix order of rotation angles

pennylane/math/decomposition.py
@@ -75,15 +75,15 @@ def xyx_rotation_angles(U: TensorLike, return_global_phase=False):
 
     U, global_phase = math.convert_to_su2(U, return_global_phase=True)
 
-    half_lam_plus_phi = math.arctan2(-math.imag(U[:, 0, 1]), math.real(U[:, 0, 0]) + EPS)
-    half_lam_minus_phi = math.arctan2(math.imag(U[:, 0, 0]), -math.real(U[:, 0, 1]) + EPS)
+    half_lam_plus_phi = math.arctan2(-math.imag(U[..., 0, 1]), math.real(U[..., 0, 0]) + EPS)
+    half_lam_minus_phi = math.arctan2(math.imag(U[..., 0, 0]), -math.real(U[..., 0, 1]) + EPS)
     lam = half_lam_plus_phi + half_lam_minus_phi
     phi = half_lam_plus_phi - half_lam_minus_phi
 
     theta = math.where(
         math.isclose(math.sin(half_lam_plus_phi), math.zeros_like(half_lam_plus_phi)),
-        2 * math.arccos(math.real(U[:, 1, 1]) / (math.cos(half_lam_plus_phi) + EPS)),
-        2 * math.arccos(-math.imag(U[:, 0, 1]) / (math.sin(half_lam_plus_phi) + EPS)),
+        2 * math.arccos(math.real(U[..., 1, 1]) / (math.cos(half_lam_plus_phi) + EPS)),
+        2 * math.arccos(-math.imag(U[..., 0, 1]) / (math.sin(half_lam_plus_phi) + EPS)),
     )
 
     phi = math.squeeze(phi % (4 * np.pi))
@@ -91,9 +91,9 @@ def xyx_rotation_angles(U: TensorLike, return_global_phase=False):
     lam = math.squeeze(lam % (4 * np.pi))
 
     if return_global_phase:
-        return phi, theta, lam, global_phase
+        return lam, theta, phi, global_phase
 
-    return phi, theta, lam
+    return lam, theta, phi
 
 
 def xzx_rotation_angles(U: TensorLike, return_global_phase=False):
@@ -109,11 +109,11 @@ def xzx_rotation_angles(U: TensorLike, return_global_phase=False):
 
     # Compute \phi, \theta and \lambda after analytically solving for them from
     # U = RX(\phi) RZ(\theta) RX(\lambda)
-    sum_diagonal_real = math.real(U[:, 0, 0] + U[:, 1, 1])
-    sum_off_diagonal_imag = math.imag(U[:, 0, 1] + U[:, 1, 0])
+    sum_diagonal_real = math.real(U[..., 0, 0] + U[..., 1, 1])
+    sum_off_diagonal_imag = math.imag(U[..., 0, 1] + U[..., 1, 0])
     half_phi_plus_lambdas = math.arctan2(-sum_off_diagonal_imag, sum_diagonal_real + EPS)
-    diff_diagonal_imag = math.imag(U[:, 0, 0] - U[:, 1, 1])
-    diff_off_diagonal_real = math.real(U[:, 0, 1] - U[:, 1, 0])
+    diff_diagonal_imag = math.imag(U[..., 0, 0] - U[..., 1, 1])
+    diff_off_diagonal_real = math.real(U[..., 0, 1] - U[..., 1, 0])
     half_phi_minus_lambdas = math.arctan2(diff_off_diagonal_real, -diff_diagonal_imag + EPS)
     lam = half_phi_plus_lambdas - half_phi_minus_lambdas
     phi = half_phi_plus_lambdas + half_phi_minus_lambdas
@@ -130,9 +130,9 @@ def xzx_rotation_angles(U: TensorLike, return_global_phase=False):
     lam = math.squeeze(lam % (4 * np.pi))
 
     if return_global_phase:
-        return phi, theta, lam, global_phase
+        return lam, theta, phi, global_phase
 
-    return phi, theta, lam
+    return lam, theta, phi
 
 
 def zxz_rotation_angles(U: TensorLike, return_global_phase=False):
@@ -145,10 +145,10 @@ def zxz_rotation_angles(U: TensorLike, return_global_phase=False):
     theta = math.where(abs_a > abs_b, 2 * math.arccos(abs_a), 2 * math.arcsin(abs_b))
 
     half_phi_plus_omega = math.angle(U[..., 1, 1])
-    half_omega_minus_phi = math.angle(-1j * U[..., 1, 0])
+    half_phi_minus_omega = math.angle(1j * U[..., 1, 0])
 
-    phi = half_phi_plus_omega - half_omega_minus_phi
-    omega = half_phi_plus_omega + half_omega_minus_phi
+    phi = half_phi_plus_omega + half_phi_minus_omega
+    omega = half_phi_plus_omega - half_phi_minus_omega
 
     # Normalize the angles
     phi = math.squeeze(phi % (4 * np.pi))
@@ -156,6 +156,6 @@ def zxz_rotation_angles(U: TensorLike, return_global_phase=False):
     omega = math.squeeze(omega % (4 * np.pi))
 
     if return_global_phase:
-        return phi, theta, omega, global_phase
+        return omega, theta, phi, global_phase
 
-    return phi, theta, omega
+    return omega, theta, phi

pennylane/math/matrix_manipulation.py
@@ -430,8 +430,10 @@ def convert_to_su2(U, return_global_phase=False):
     with np.errstate(divide="ignore", invalid="ignore"):
         determinant = math.linalg.det(U)
     phase = math.angle(determinant) / 2
-    if batch_size:
-        phase = math.reshape(phase, (batch_size, 1, 1))
     U, phase = math.cast_like(U, determinant), math.cast_like(phase, 1j)
-    U = U * math.exp(-1j * phase)
+    if batch_size:
+        batched_phase = math.reshape(phase, (batch_size, 1, 1))
+        U = U * math.exp(-1j * batched_phase)
+    else:
+        U = U * math.exp(-1j * phase)
     return (U, phase) if return_global_phase else U

tests/ops/op_math/test_decompositions.py
@@ -339,7 +339,7 @@ test_cases_zxz = [
     (S, [np.pi / 4, 0.0, np.pi / 4, -np.pi / 4]),
     (T, [np.pi / 8, 0.0, np.pi / 8, -np.pi / 8]),
     (H, [np.pi / 2, np.pi / 2, np.pi / 2, -np.log(1j) / 1j]),
-    (X, [0, np.pi, 4 * np.pi, -np.log(1j) / 1j]),
+    (X, [4 * np.pi, np.pi, 0, -np.log(1j) / 1j]),
     (qml.RZ(0.3, wires=0).matrix(), [0.15, 0.0, 0.15, 0]),
     (qml.RZ(-0.5, wires=0).matrix(), [4 * np.pi - 0.25, 0.0, 4 * np.pi - 0.25, 0]),
     (qml.Rot(0.2, 0.5, -0.3, wires=0).matrix(), [11.195574287564275, 0.5, 1.2707963267948965, 0]),