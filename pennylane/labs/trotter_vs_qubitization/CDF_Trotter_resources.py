2d4dc7003ecd18bef334d05fea020a639780afd8
PabloAMC <paulmorecasa@gmail.com>
2025-04-08 15:02:09 +0200

    Resources required for QPE and time evolution. To be debugged.

---
+++ pennylane/labs/trotter_vs_qubitization/CDF_Trotter_resources.py
@@ -0,0 +1,140 @@
+import numpy as np
+
+
+def CDF_terms(N): return N
+
+def basis_rotation(N, spin_breaking = False):
+    r"""
+    N is the number of spin orbitals.
+    If spin_breaking is False:
+        Each spin is rotated independently.
+        We need (N/2)(N/2-1)/2 Givens rotations for each spin
+        Each Givens rotation requires two single qubit rotation, one of type RX and another of type RY.
+    else:
+        We need N(N-1)/2 Givens rotations.
+        Each Givens rotation requires two single qubit rotation, one of type RX and another of type RY.
+    Additionally we always need N RZ rotations.
+    """
+    if not spin_breaking:
+        rotations = {
+            'RZ': N,
+            'RX': 2*(N/2)*((N/2)-1)/2,
+            'RY': 2*(N/2)*((N/2)-1)/2,
+        }
+    else:
+        rotations = {
+            'RZ': N,
+            'RX': N*(N-1)/2,
+            'RY': N*(N-1)/2,
+        }
+    rotations['Hadamard'] = 4*(rotations['RX'] + rotations['RY'])
+    rotations['S'] = 2*(rotations['RY'])
+    rotations['Adjoint(S)'] = 2*(rotations['RY'])
+    rotations['CNOT'] = 2*(rotations['RX'] + rotations['RY'] + rotations['RZ'])
+    return rotations
+
+def CZZ(N, one_body = False):
+    r"""
+    N is the number of spin orbitals.
+    Each spin is rotated independently.
+    We need N*(N-1)/2 CZZ gates.
+    """
+    rotations = {
+        'RZ': N if one_body else N*(N+1)/2,
+        'CNOT': 0 if one_body else 2*N*(N+1)/2,
+        'RX': 0, 'RY': 0, 'Hadamard': 0, 'S': 0, 'Adjoint(S)': 0
+    }
+    return rotations
+
+def Trotter_cost(N, order):
+    r"""
+    This function computes the cost of implementing a Trotter step for a given number of spatial orbitals.
+    N is the number of spin orbitals.
+    Each CDF term contains a basis rotation and a set of CZZ gates.
+    """
+    if order == 1:
+        first_order_uses = 1
+    elif order == 2:
+        first_order_uses = 2
+    elif order == 4:
+        first_order_uses = 10
+    elif order == 6:
+        first_order_uses = 26
+    rotations = {'RZ': 0, 'RX': 0, 'RY': 0, 'Hadamard': 0, 'S': 0, 'Adjoint(S)': 0, 'CNOT': 0, 'Toffoli': 0, 'SWAP': 0, 'T': 0, 'Adjoint(T)': 0}
+    for rot in ['RX', 'RY', 'RZ', 'Hadamard', 'S', 'Adjoint(S)', 'CNOT']:
+        rotations[rot] += basis_rotation(N)[rot] * CDF_terms(N) * first_order_uses
+        rotations[rot] += CZZ(N)[rot] * (CDF_terms(N)-1) * first_order_uses
+        rotations[rot] += CZZ(N, one_body = True)[rot] * first_order_uses
+    return rotations
+
+def active_volume(rotations: dict) -> float:
+    """
+    Computes the active volume of the circuit
+    """
+    b = 10 # 10 bits of precision per rotation
+
+    av = 0
+    T = 25
+    CCZ = 35
+
+    av += rotations['RZ']*(4+b/40*(305+6*CCZ+24*T))
+    av += rotations['RX']*(4+b/40*(305+6*CCZ+24*T))
+    av += rotations['RY']*(4+3/2+b/40*(305+6*CCZ+24*T))
+
+    av += rotations['Hadamard']*3
+    av += rotations['CNOT']*4
+    av += rotations['Toffoli']*(12+CCZ)
+
+    av += rotations['SWAP']*12
+    av += rotations['S']*3
+    av += rotations['Adjoint(S)']*3
+
+    av += rotations['T']*(T + (3+1)+1.5)
+    av += rotations['Adjoint(T)']*(T + (3+1)+1.5)
+
+    return av
+
+def QPE_cost(N, order, Y2kp1dN2 = 1, epsilon = 1e-3):
+    r"""
+    This function computes the cost of implementing a QPE step for a given number of spatial orbitals.
+    N is the number of spin orbitals.
+    Each CDF term contains a basis rotation and a set of CZZ gates.
+    """
+    Y2kp1 = Y2kp1dN2 * N**2
+    rotations = Trotter_cost(N, order)
+    for key, value in rotations.items():
+        rotations[key] = value * (Y2kp1/epsilon)**(1/(order+1)) * 1/epsilon
+    return rotations
+
+def scientific_notation(num):
+    """Convert number to LaTeX scientific notation"""
+    if num == 0:
+        return "$0$"
+    exp = int(np.floor(np.log10(abs(num))))
+    coef = num / 10**exp
+    return f"${coef:.2f} \\times 10^{{{exp}}}$"
+
+N = 58
+order = 2
+Tgates_per_rot = np.ceil(9.2 + 1.15*np.log2(1/1e-4))
+Y2kp1dN2 = 1e-2
+
+alg_t = Tgates_per_rot * np.sum([QPE_cost(N, order, Y2kp1dN2)[k] for k in ['RZ', 'RX', 'RY']])
+print(f'Number of T gates in algorithm = {scientific_notation(alg_t)}')
+rotations = {k: v for k, v in QPE_cost(N, order, Y2kp1dN2).items()}
+alg_av = active_volume(rotations)
+print(f'Active volume of the algorithm = {scientific_notation(alg_av)}')
+
+def simulation_time(N, order, time, Y2kp1dN2 = 1, epsilon = 1e-3):
+    r"""
+    This function computes the cost of simulating the Trotter formula for a fixed amount of time.
+    N is the number of spin orbitals.
+    order is the order of the Trotter formula.
+    time is the time to simulate.
+    """
+    Y2kp1 = Y2kp1dN2 * N**2
+    rotations = Trotter_cost(N, order)
+
+    for key, value in rotations.items():
+        rotations[key] = value * (Y2kp1/epsilon)**(1/order) * time**(1+1/order)
+    return rotations
---
+++ pennylane/labs/trotter_vs_qubitization/THC_resources.py
@@ -0,0 +1,102 @@
+import numpy as np
+from scipy.special import jv
+
+
+def Toffoli_cost(N, M, aleph, beth, br = 7):
+    r"""
+    Cost of implementing THC according to eq 44 in https://journals.aps.org/prxquantum/pdf/10.1103/PRXQuantum.2.030305
+
+
+    """
+
+    nM = np.ceil(np.log2(M+1))
+    d = N/2 + M*(M+1)/2
+    m = 2*nM + 2 + aleph
+
+    ks1 = np.min([np.ceil(d/2**n) + m*(2**n - 1) for n in range(1, 25)])
+    ks2 = np.min([np.ceil(d/ks2) + ks2 for ks2 in range(1, 25)])
+    kr1 = np.min([np.ceil(M/kr1) + np.ceil(N/(2*kr1)) + kr1 for kr1 in range(1, 25)])
+    kr2 = np.min([np.ceil(M/kr2) + kr2 for kr2 in range(1, 25)])
+
+    cost = 30*nM + 4*br - 16 + 2*nM**2 + 3*aleph + np.ceil(d/ks1) \
+        + m*(ks1 - 1) + np.ceil(d/ks2) + ks2 \
+        + 2*M + 4*N*beth - 11*N/2 + np.ceil(M/kr1) + np.ceil(N/(2*kr1)) \
+        + kr1 + np.ceil(M/kr2) + kr2
+
+    return cost
+
+def qubit_cost(N, M, aleph, beth, br = 7):
+    r"""
+    Qubit cost of implementing THC according to eq 43 in https://journals.aps.org/prxquantum/pdf/10.1103/PRXQuantum.2.030305
+
+    N is the number of qubits.
+    M is the number of CDF terms.
+    aleph is the number of RX gates.
+    beth is the number of RY gates.
+    br is the number of CNOT gates.
+    """
+    nM = float(np.ceil(np.log2(M+1)))
+    d = N/2 + M*(M+1)/2
+    m = 2*nM + 2 + aleph
+
+    ks1 = float(np.min([np.ceil(d/2**n) + m*(2**n - 1) for n in range(1, 25)]))
+
+    cost = N + 2*nM + beth + np.log2(d) + aleph + 5 + \
+        np.max([m*ks1 + np.ceil(np.log2(d/ks1)), m + beth*N/2 + beth - 2])
+
+    return cost
+
+
+def qubitization_order(t, epsilon, one_norm):
+    r"""
+    t is the scaled simulation parameter
+    epsilon is the error tolerance
+    one_norm is the norm of the Hamiltonian
+    """
+
+    n = 0
+    theta = np.linspace(0, np.pi, 1000)
+    target = np.exp(1j*t * np.cos(theta))
+    ft = jv(0, t)
+    error = np.abs(target - ft)
+
+    while np.any(error > epsilon/one_norm):
+        n += 1
+        target += (-1j)**n * jv(n, t) * np.exp(1j*n*theta)
+        target += (-1j)**(-n) * jv(-n, t) * np.exp(-1j*n*theta)
+
+        error = np.abs(target - ft)
+
+    return n
+
+
+
+# Parameters from https://arxiv.org/abs/2501.06165 for QPE
+# System: P450 
+# M = 160
+# N = 2*58
+# aleph = 13
+# beth = 13
+# one_norm = 130.9
+
+# System: FeMoco
+M = 290
+N = 2*76
+aleph = 15
+beth = 16
+one_norm = 198.9
+
+
+epsilon = 1e-3
+time = 1e3
+br = 7
+
+
+
+qpe_cost = Toffoli_cost(N, M, aleph, beth, br) * np.pi*one_norm/(2*epsilon)
+print(f'QPE cost: {qpe_cost:.2e}')
+qubit_cost = qubit_cost(N, M, aleph, beth, br)
+print(f'Qubit cost: {qubit_cost:.2e}')
+simulation_cost = Toffoli_cost(N, M, aleph, beth, br) * qubitization_order(time, epsilon, one_norm)
+print(f'Simulation cost: {simulation_cost:.2e}')
+
