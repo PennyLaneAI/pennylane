28445f08fd0a8fac2af653b8a443426186422fba
andrijapau <andrija.paurevic@xanadu.ai>
2025-04-10 13:54:17 -0400

    gradient_transform.py

--- pennylane/gradients/gradient_transform.py
+++ pennylane/gradients/gradient_transform.py
@@ -18,7 +18,7 @@ import warnings
 # pylint: disable=too-few-public-methods
 from functools import partial
 
-import pennylane as qml
+from pennylane import math
 from pennylane.measurements import MutualInfoMP, ProbabilityMP, StateMP, VarianceMP, VnEntropyMP
 
 SUPPORTED_GRADIENT_KWARGS = {
@@ -257,14 +257,14 @@ def _all_zero_grad(tape):
     """Auxiliary function to return zeros for the all-zero gradient case."""
     list_zeros = []
 
-    par_shapes = [qml.math.shape(p) for p in tape.get_parameters()]
+    par_shapes = [math.shape(p) for p in tape.get_parameters()]
     for m in tape.measurements:
         # TODO: Update shape for CV variables
         shape = (2 ** len(m.wires),) if isinstance(m, ProbabilityMP) else ()
         if len(tape.trainable_params) == 1:
-            sub_list_zeros = qml.math.zeros(par_shapes[0] + shape)
+            sub_list_zeros = math.zeros(par_shapes[0] + shape)
         else:
-            sub_list_zeros = tuple(qml.math.zeros(sh + shape) for sh in par_shapes)
+            sub_list_zeros = tuple(math.zeros(sh + shape) for sh in par_shapes)
 
         list_zeros.append(sub_list_zeros)
 
@@ -292,15 +292,15 @@ def _no_trainable_grad(tape):
     warnings.warn(_no_trainable_grad_warning)
     if tape.shots.has_partitioned_shots:
         if len(tape.measurements) == 1:
-            return [], lambda _: tuple(qml.math.zeros([0]) for _ in range(tape.shots.num_copies))
+            return [], lambda _: tuple(math.zeros([0]) for _ in range(tape.shots.num_copies))
         return [], lambda _: tuple(
-            tuple(qml.math.zeros([0]) for _ in range(len(tape.measurements)))
+            tuple(math.zeros([0]) for _ in range(len(tape.measurements)))
             for _ in range(tape.shots.num_copies)
         )
 
     if len(tape.measurements) == 1:
-        return [], lambda _: qml.math.zeros([0])
-    return [], lambda _: tuple(qml.math.zeros([0]) for _ in range(len(tape.measurements)))
+        return [], lambda _: math.zeros([0])
+    return [], lambda _: tuple(math.zeros([0]) for _ in range(len(tape.measurements)))
 
 
 def _swap_first_two_axes(grads, first_axis_size, second_axis_size, squeeze=True):
@@ -394,8 +394,8 @@ def reorder_grads(grads, tape_specs):
     return _move_first_axis_to_third_pos(grads, num_params, shots.num_copies, num_measurements)
 
 
-tdot = partial(qml.math.tensordot, axes=[[0], [0]])
-stack = qml.math.stack
+tdot = partial(math.tensordot, axes=[[0], [0]])
+stack = math.stack
 
 
 # pylint: disable=too-many-return-statements,too-many-branches
@@ -434,7 +434,7 @@ def _contract_qjac_with_cjac(qjac, cjac, tape):
     if single_tape_param:
         # Without dimension (e.g. expval) or with dimension (e.g. probs)
         def _reshape(x):
-            return qml.math.reshape(x, (1,) if x.shape == () else (1, -1))
+            return math.reshape(x, (1,) if x.shape == () else (1, -1))
 
         if not (multi_meas or has_partitioned_shots):
             # Single parameter, single measurements, no shot vector
