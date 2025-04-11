f85c9ff130b798e22768a7a9df5d89b46725728b
Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>
2025-04-11 09:31:15 -0400

    [v0.41.0 - QA] Documentation Fixes (#7237)
    **Context:**
    
    Fixing some things in documentation that were encountered during the
    v0.41.0 QA process.
    
    **Description of the Change:**
    
    - `QROMStatePreparation` doc string example has been updated
    - Sphinx rendering in `liealg` module
    
    **Benefits:** Quality has been assured.
    
    **Possible Drawbacks:** N/A
    
    ---------
    
    Co-authored-by: Isaac De Vlugt <34751083+isaacdevlugt@users.noreply.github.com>

--- pennylane/liealg/horizontal_cartan_subalgebra.py
+++ pennylane/liealg/horizontal_cartan_subalgebra.py
@@ -95,7 +95,7 @@ def horizontal_cartan_subalgebra(
         tol (float): Numerical tolerance for linear independence check.
         verbose (bool): Whether or not to output progress during computation.
         return_adjvec (bool): Determine the output format. If ``False``, returns operators in their original
-            input format (matrices or :class:`~.PauliSentence`). If ``True``, returns the spaces as adjoint representation vectors (see :func`~op_to_adjvec` and `~adjvec_to_op`).
+            input format (matrices or :class:`~.PauliSentence`). If ``True``, returns the spaces as adjoint representation vectors (see :func:`~op_to_adjvec` and :func:`~adjvec_to_op`).
         is_orthogonal (bool): Whether the basis elements are all orthogonal, both within
             and between ``g``, ``k`` and ``m``.
 
@@ -495,6 +495,7 @@ def change_basis_ad_rep(adj: TensorLike, basis_change: TensorLike):
 
     We choose a basis of a Lie algebra, compute its adjoint representation.
 
+    >>> import numpy as np
     >>> from pennylane.liealg import change_basis_ad_rep
     >>> basis = [qml.X(0), qml.Y(0), qml.Z(0)]
     >>> adj = qml.structure_constants(basis)
@@ -509,7 +510,8 @@ def change_basis_ad_rep(adj: TensorLike, basis_change: TensorLike):
     transform the old adjoint representation with the change of basis matrix.
 
     >>> new_adj_re = change_basis_ad_rep(adj, basis_change)
-    np.allclose(new_adj, new_adj_re)
+    >>> np.allclose(new_adj, new_adj_re)
+    True
     """
     # Perform the einsum contraction "mnp, hm, in, jp -> hij" via three einsum steps
     new_adj = qml.math.einsum("mnp,im->inp", adj, qml.math.linalg.pinv(basis_change.T))
