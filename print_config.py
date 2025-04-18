5306fb5210c109d081a38b4788a0a5deca80d749
Po-Ying Chiu <charlie910417@gmail.com>
2025-04-19 01:51:07 +0800

    Print ExecutionConfig to check the output
    Add a simple script that create QNode and prints its corresponding
    ExecutionConfig using construct_execution_config with resolve=False.

---
+++ print_config.py
@@ -0,0 +1,13 @@
+import pennylane as qml
+from pennylane.workflow import construct_execution_config
+
+
+@qml.qnode(qml.device("default.qubit", wires=1))
+def circuit(x):
+    qml.RX(x, 0)
+    return qml.expval(qml.Z(0))
+
+
+config = construct_execution_config(circuit, resolve=False)(1)
+
+print(config)
