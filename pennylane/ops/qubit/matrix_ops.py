833bdc464893fc184144fc4cceb52782017aa95f
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2025-04-22 13:24:17 -0400

    Update matrix_ops.py

--- pennylane/ops/qubit/matrix_ops.py
+++ pennylane/ops/qubit/matrix_ops.py
@@ -87,7 +87,7 @@ def _walsh_hadamard_transform(D: TensorLike, n: Optional[int] = None):
 
 
 def _compute_udv(a, b):
-    """Given the matrices `a` and `b`, calculates the matrices `u`, `d` and `v`
+    r"""Given the matrices `a` and `b`, calculates the matrices `u`, `d` and `v`
     of equation (36) [https://arxiv.org/pdf/quant-ph/0504100]:
 
     .. math::
