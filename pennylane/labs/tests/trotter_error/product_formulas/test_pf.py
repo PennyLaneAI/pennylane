2ab6280e2c1438cea9f5e7a4c17b26f5d5925824
Will <wmaxwell90@gmail.com>
2025-04-24 11:45:05 -0400

    formatting

--- pennylane/labs/tests/trotter_error/product_formulas/test_pf.py
+++ pennylane/labs/tests/trotter_error/product_formulas/test_pf.py
@@ -12,31 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Tests for the product formula representations"""
-import pytest
-
 import numpy as np
+import pytest
 
 from pennylane.labs.trotter_error import ProductFormula, effective_hamiltonian
 
 fragment_dicts = [
-        #{ 0: np.zeros(shape=(3, 3)), 1: np.zeros(shape=(3, 3))},
-        { 0: np.random.random(size=(3, 3)), 1: np.random.random(size=(3, 3))},
-    ]
+    # { 0: np.zeros(shape=(3, 3)), 1: np.zeros(shape=(3, 3))},
+    {0: np.random.random(size=(3, 3)), 1: np.random.random(size=(3, 3))},
+]
+
 
 @pytest.mark.parametrize("fragment_dict", fragment_dicts)
 def test_fourth_order_recursive(fragment_dict):
     """Test that the recursively defined product formula yields the same effective Hamiltonian as the unraveled product formula"""
 
-    u = 1 / (4 - 4**(1/3))
+    u = 1 / (4 - 4 ** (1 / 3))
     frag_labels1 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
-    frag_coeffs1 = [u/2, u, u, u, (1-3*u)/2, 1 - 4*u, (1-3*u)/2, u, u, u, u/2]
+    frag_coeffs1 = [u / 2, u, u, u, (1 - 3 * u) / 2, 1 - 4 * u, (1 - 3 * u) / 2, u, u, u, u / 2]
 
     fourth_order_1 = ProductFormula(frag_coeffs1, frag_labels1)
 
-    second_order =  ProductFormula([1/2, 1/2, 1/2, 1/2], [0, 1, 1, 0])
+    second_order = ProductFormula([1 / 2, 1 / 2, 1 / 2, 1 / 2], [0, 1, 1, 0])
 
     frag_labels2 = [second_order**2, second_order, second_order**2]
-    frag_coeffs2 = [u, 1 - 4*u, u]
+    frag_coeffs2 = [u, 1 - 4 * u, u]
 
     fourth_order_2 = ProductFormula(frag_coeffs2, frag_labels2)
 
@@ -45,21 +45,23 @@ def test_fourth_order_recursive(fragment_dict):
 
     assert np.isclose(eff_1, eff_2)
 
+
 fragment_dicts = [
-        #{ 0: np.zeros(shape=(3, 3)), 1: np.zeros(shape=(3, 3))},
-        { 0: np.random.random(size=(3, 3)), 1: np.random.random(size=(3, 3))},
-    ]
+    # { 0: np.zeros(shape=(3, 3)), 1: np.zeros(shape=(3, 3))},
+    {0: np.random.random(size=(3, 3)), 1: np.random.random(size=(3, 3))},
+]
+
 
 @pytest.mark.parametrize("fragment_dict", fragment_dicts)
 def test_fourth_order_mult(fragment_dict):
-    u = 1 / (4 - 4**(1/3))
+    u = 1 / (4 - 4 ** (1 / 3))
     frag_labels1 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
-    frag_coeffs1 = [u/2, u, u, u, (1-3*u)/2, 1 - 4*u, (1-3*u)/2, u, u, u, u/2]
+    frag_coeffs1 = [u / 2, u, u, u, (1 - 3 * u) / 2, 1 - 4 * u, (1 - 3 * u) / 2, u, u, u, u / 2]
 
-    second_order =  ProductFormula([1/2, 1/2, 1/2, 1/2], [0, 1, 1, 0])
+    second_order = ProductFormula([1 / 2, 1 / 2, 1 / 2, 1 / 2], [0, 1, 1, 0])
 
     frag_labels = [second_order**2, second_order, second_order**2]
-    frag_coeffs = [u, 1 - 4*u, u]
+    frag_coeffs = [u, 1 - 4 * u, u]
 
     fourth_order_1 = ProductFormula(frag_coeffs, frag_labels)
 
