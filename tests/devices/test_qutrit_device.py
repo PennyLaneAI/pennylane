1a3f8a12390388e741e232a37a7aba24b99e263a
Yushao Chen (Jerry) <chenys13@outlook.com>
2025-04-23 21:24:45 -0400

    Update test_qutrit_device.py

--- tests/devices/test_qutrit_device.py
+++ tests/devices/test_qutrit_device.py
@@ -25,18 +25,12 @@ import pennylane as qml
 from pennylane import numpy as pnp
 from pennylane.devices import QubitDevice, QutritDevice
 from pennylane.measurements import (
-    Counts,
     CountsMP,
-    Expectation,
     ExpectationMP,
     MeasurementProcess,
-    Probability,
     ProbabilityMP,
-    Sample,
     SampleMP,
-    State,
     StateMP,
-    Variance,
     VarianceMP,
 )
 from pennylane.tape import QuantumScript
@@ -359,7 +353,7 @@ class TestExtractStatistics:
     ):
         """Tests that the statistics method raises an error if the return type is not well-defined and is not None"""
 
-        assert returntype not in [Expectation, Variance, Sample, Probability, State, Counts, None]
+        assert returntype not in ["Expectation", "Variance", "Sample", "Probability", "State", "Counts", None]
 
         class UnsupportedMeasurement(MeasurementProcess):
             _shortname = returntype
