9e8ffb28430280aafd567598f1fcbce5e347e0ee
Lee James O'Riordan <mlxd@users.noreply.github.com>
2025-04-28 10:58:11 -0400

    Add remote executor support for concurrent execution (#7303)
    ### Before submitting
    
    Please complete the following checklist when submitting a PR:
    
    - [x] All new features must include a unit test.
    If you've fixed a bug or added code that should be tested, add a test to
    the
          test directory!
    
    - [x] All new functions and code must be clearly commented and
    documented.
    If you do make documentation changes, make sure that the docs build and
          render correctly by running `make docs`.
    
    - [x] Ensure that the test suite passes, by running `make test`.
    
    - [x] Add a new entry to the `doc/releases/changelog-dev.md` file,
    summarizing the
          change, and including a link back to the PR.
    
    - [x] The PennyLane source code conforms to
          [PEP8 standards](https://www.python.org/dev/peps/pep-0008/).
    We check all of our code against [Pylint](https://www.pylint.org/).
          To lint modified files, simply `pip install pylint`, and then
          run `pylint pennylane/path/to/file.py`.
    
    When all the above are checked, delete everything above the dashed
    line and fill in the pull request template.
    
    
    ------------------------------------------------------------------------------------------------------------
    
    **Context:** This PR adds abstractions for concurrent task-based
    execution of functions to PennyLane. The design follows the Python
    `concurrent.futures` interface, with modifications to allow unification
    and execution with:
    
    ### Native & local executors
    *
    [`multiprocessing.pool.Pool`](https://docs.python.org/3/library/multiprocessing.html#multiprocessing.pool.Pool)
    *
    [`concurrent.futures.ProcessPoolExecutor`](https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ProcessPoolExecutor)
    *
    [`concurrent.futures.ThreadPoolExecutor`](https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor)
    * Native Python
    [`map`](https://docs.python.org/3/library/functions.html#map) and
    [`itertools.starmap`](https://docs.python.org/3/library/itertools.html#itertools.starmap)
    
    ### 3rd-party & remote executors
    *
    [`mpi4py.futures.PoolExecutor`](https://mpi4py.readthedocs.io/en/stable/mpi4py.futures.html#mpipoolexecutor)
    *
    [`mpi4py.futures.CommExecutor`](https://mpi4py.readthedocs.io/en/stable/mpi4py.futures.html#mpicommexecutor)
    *
    [`dask.distributed`](https://distributed.dask.org/en/stable/#dask-distributed)
    
    For brevity, the 3rd party executors remain untested, and are currently
    for internal R&D purposes only. They are excluded from the
    documentation, but will be kept for utility and validation on larger
    workloads.
    
    **Description of the Change:**
    * Added new `qml.concurrency` module, with task-based executor
    abstractions
    * Modified the previous `default.qubit`
    `concurrent.futures.ProcessPoolExecutor` pipeline to rely on the new
    abstractions
    * Added support for local and remote executors, allowing for better
    abstractions with scaling out data-parallel workloads.
    
    **Benefits:** Allows safer abstractions and platform agnostic
    implementations for running task-based data-parallel workloads.
    
    **Possible Drawbacks:** Additional complexity of the stack due to the
    abstraction internals. Potential caveats to be unearthed with use in
    remote-execution environments. Unknown how functionality will integrate
    with compilation with further abstractions.
    
    **Related GitHub Issues:**
    
    ---------
    
    Co-authored-by: Isaac De Vlugt <34751083+isaacdevlugt@users.noreply.github.com>
    Co-authored-by: Joey Carter <joseph.carter@xanadu.ai>

--- tests/workflow/interfaces/test_jacobian_products.py
+++ tests/workflow/interfaces/test_jacobian_products.py
@@ -163,7 +163,8 @@ class TestBasics:
             r" use_device_jacobian_product=None,"
             r" gradient_method='adjoint', gradient_keyword_arguments={},"
             r" device_options={}, interface=<Interface.NUMPY: 'numpy'>, derivative_order=1,"
-            r" mcm_config=MCMConfig(mcm_method=None, postselect_mode=None), convert_to_numpy=True)>"
+            r" mcm_config=MCMConfig(mcm_method=None, postselect_mode=None), convert_to_numpy=True,"
+            r" executor_backend=<class 'pennylane.concurrency.executors.native.multiproc.MPPoolExec'>)>"
         )
 
         assert repr(jpc) == expected
@@ -182,7 +183,8 @@ class TestBasics:
             r" use_device_jacobian_product=None,"
             r" gradient_method='adjoint', gradient_keyword_arguments={}, device_options={},"
             r" interface=<Interface.NUMPY: 'numpy'>, derivative_order=1,"
-            r" mcm_config=MCMConfig(mcm_method=None, postselect_mode=None), convert_to_numpy=True)>"
+            r" mcm_config=MCMConfig(mcm_method=None, postselect_mode=None), convert_to_numpy=True,"
+            r" executor_backend=<class 'pennylane.concurrency.executors.native.multiproc.MPPoolExec'>)>"
         )
 
         assert repr(jpc) == expected
