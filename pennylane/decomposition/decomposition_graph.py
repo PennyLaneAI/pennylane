3ffed84ecf3a72fd7e24d4f775bf6f9c27972008
Marcus Edwards <94946848+comp-phys-marc@users.noreply.github.com>
2025-05-07 12:14:35 -0400

    Stylistic change in set instantiation syntax
    Co-authored-by: Astral Cai <astral.cai@xanadu.ai>

--- pennylane/decomposition/decomposition_graph.py
+++ pennylane/decomposition/decomposition_graph.py
@@ -336,7 +336,7 @@ class DecompositionGraph:  # pylint: disable=too-many-instance-attributes
             unsolved_ops = [self._graph[op_idx] for op_idx in self._visitor.unsolved_op_indices]
             op_names = set(op.name for op in unsolved_ops)
             raise DecompositionError(
-                f"Decomposition not found for {op_names} to the gate set {self._weights}"
+                f"Decomposition not found for {op_names} to the gate set {set(self._weights)}"
             )
 
     def is_solved_for(self, op):
