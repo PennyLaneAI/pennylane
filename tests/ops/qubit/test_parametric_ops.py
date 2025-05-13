88f83f73e625965f78c1daef1ef8af1bf2c43e89
dwierichs <david.wierichs@xanadu.ai>
2025-05-13 11:17:35 +0200

    move test

--- tests/ops/qubit/test_parametric_ops.py
+++ tests/ops/qubit/test_parametric_ops.py
@@ -27,7 +27,6 @@ import pennylane as qml
 from pennylane import numpy as npp
 from pennylane.ops.qubit import RX as old_loc_RX
 from pennylane.ops.qubit import MultiRZ as old_loc_MultiRZ
-from pennylane.ops.qubit.parametric_ops_multi_qubit import _decomp_int_to_powers_of_two
 from pennylane.wires import Wires
 
 PARAMETRIZED_OPERATIONS = [
@@ -4112,37 +4111,3 @@ def test_op_aliases_are_valid():
     """Tests that ops in new files can still be accessed from the old parametric_ops module."""
     assert qml.ops.qubit.parametric_ops_multi_qubit.MultiRZ is old_loc_MultiRZ
     assert qml.ops.qubit.parametric_ops_single_qubit.RX is old_loc_RX
-
-
-@pytest.mark.parametrize(
-    "k, n, exp_R",
-    [
-        (0, 1, [0]),
-        (0, 2, [0, 0]),
-        (1, 2, [0, 1]),
-        (0, 3, [0, 0, 0]),
-        (1, 3, [0, 0, 1]),
-        (2, 3, [0, 1, 0]),
-        (3, 3, [1, 0, -1]),
-        (4, 3, [1, 0, 0]),
-        (0, 4, [0, 0, 0, 0]),
-        (1, 4, [0, 0, 0, 1]),
-        (2, 4, [0, 0, 1, 0]),
-        (3, 4, [0, 1, 0, -1]),
-        (4, 4, [0, 1, 0, 0]),
-        (5, 4, [0, 1, 0, 1]),
-        (6, 4, [1, 0, -1, 0]),
-        (7, 4, [1, 0, 0, -1]),
-        (8, 4, [1, 0, 0, 0]),
-        (121, 8, [1, 0, 0, 0, -1, 0, 0, 1]),  # 121=01111001_2
-        (245, 10, [0, 1, 0, 0, 0, -1, 0, 1, 0, 1]),  # 245=0011110101_2
-        (8716, 15, [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -1, 0, 0]),  # 8716=010001000001100_2
-    ],
-)
-def test_decomp_int_to_powers_of_two(k, n, exp_R):
-    """Tests for ``_decomp_int_to_powers_of_two``, which is used to decompose
-    ``PCPhase`` operations."""
-
-    R = _decomp_int_to_powers_of_two(k, n)
-    assert R == exp_R, f"\n{R}\n{exp_R}"
-    assert sum(val != 0 for val in R) == (k ^ (3 * k)).bit_count()
