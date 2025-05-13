6e031a52f5214335d63db7bc7a7b9c255102851c
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2025-05-13 10:43:29 -0400

    Update unitary_decompositions.py

--- pennylane/ops/op_math/decompositions/unitary_decompositions.py
+++ pennylane/ops/op_math/decompositions/unitary_decompositions.py
@@ -790,8 +790,9 @@ def _compute_udv(a, b):
 def multi_qubit_decomposition(U, wires):
     r"""Decompose a multi-qubit unitary :math:`U` in terms of elementary operations.
     The n-qubit unitary :math:`U`, with :math:`n > 1`, is decomposed into four (:math:`n-1`)-qubit
-    unitaries (:class:`~.QubitUnitary`) and three multiplexers (:class:`~.SelectPauliRot`)  using the cosine-sine
-    decomposition. This implementation is based on `arXiv:quant-ph/0504100 <https://arxiv.org/pdf/quant-ph/0504100>`__.
+    unitaries (:class:`~pennylane.ops.QubitUnitary`) and three multiplexers (:class:`~pennylane.templates.SelectPauliRot`)
+    using the cosine-sine decomposition.
+    This implementation is based on `arXiv:quant-ph/0504100 <https://arxiv.org/pdf/quant-ph/0504100>`__.
 
     Args:
         U (tensor): A :math:`2^n \times 2^n` unitary matrix with :math:`n > 1`.
@@ -803,17 +804,13 @@ def multi_qubit_decomposition(U, wires):
 
     **Example**
 
-    .. code-block::
-
-        matrix_target = qml.matrix(qml.QFT([0,1,2]))
-        ops = multi_qubit_decomposition(matrix_target, [0,1,2])
-        matrix_decomposition = qml.matrix(qml.prod(*ops[::-1]), wire_order = [0,1,2])
-
     .. code-block:: pycon
 
+        >>> matrix_target = qml.matrix(qml.QFT([0,1,2]))
+        >>> ops = multi_qubit_decomposition(matrix_target, [0,1,2])
+        >>> matrix_decomposition = qml.matrix(qml.prod(*ops[::-1]), wire_order = [0,1,2])
         >>> print([op.name for op in ops])
         ['QubitUnitary', 'SelectPauliRot', 'QubitUnitary', 'SelectPauliRot', 'QubitUnitary', 'SelectPauliRot', 'QubitUnitary']
-
         >>> print(np.allclose(matrix_decomposition, matrix_target))
         True
     """
