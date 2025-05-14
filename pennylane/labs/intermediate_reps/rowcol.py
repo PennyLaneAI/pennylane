f2c9d4e8c6cd2b0b9502ffdb4b5b468d32f746ab
dwierichs <david.wierichs@xanadu.ai>
2025-05-14 09:46:28 +0200

    review: examples, walrus instead of in-place mutation

--- pennylane/labs/intermediate_reps/rowcol.py
+++ pennylane/labs/intermediate_reps/rowcol.py
@@ -37,6 +37,58 @@ def postorder_traverse(tree: nx.Graph, source: int, source_parent: int = None):
 
     A useful illustration of depth-first tree traversals can be found on
     `Wikipedia <https://en.wikipedia.org/wiki/Tree_traversal#Depth-first%20search>`__.
+
+    **Example**
+
+    Consider the tree
+
+    ```
+                      (4)
+                       |
+    (6) - (2) - (0) - (1) - (3) - (8)
+           |           |
+          (7)         (5)
+    ```
+
+    and consider ``(0)`` to be the source, or root, of the tree.
+    We may construct this tree as a ``nx.Graph`` by providing the edge data:
+
+    >>> import networkx as nx
+    >>> G = nx.Graph([(0, 1), (0, 2), (1, 3), (1, 4), (1, 5), (2, 6), (2, 7), (3, 8)])
+
+    As for every tree traversal, post-order traversal results in a reordering of the nodes of
+    the tree, with each node appearing exactly once. Post-order traversing the graph means that
+    for every node we reach, we first visit its child nodes (in standard sorting of the node
+    labels/indices) and then append the node itself to the ordering.
+    Starting at the root ``(0)``, we thus perform the following steps:
+
+    1. Visit ``(1)``, ``(3)`` and then ``(8)``, without appending any of them.
+    2. Append ``(8)`` because there are no child nodes to visit (it is a leaf node).
+    3. Append ``(3)`` because all children of it have been visited.
+    4. Visit the next child of ``(1)``, which is ``(4)``, and append it because it is a leaf node.
+    5. Visit the last child of ``(1)``, which is ``(5)``, and append it because it is a leaf node.
+    6. Append ``(1)`` because all its children have been visited.
+    7. Visit ``(2)`` and then ``(6)``, the latter of which is appended (leaf node).
+    8. Visit the second and last child of ``(2))``, which is ``(7)``, and append it (leaf node).
+    9. Append ``(2)`` becaues all its children have been visited.
+    10. Append ``(0)`` becaues all its children have been visited.
+
+    Overall, the post-order traversal reads ``[8, 3, 4, 5, 1, 6, 7, 2, 0]``.
+    For the output convention of this function, each node is accompanied by its
+    parent node, because this is useful information needed down the line, and it is not easy to
+    retrieve from the ``nx.Graph`` itself. In addition, the last entry, which always is the root
+    of the tree provided via the ``source`` argument, is *not* included in the output.
+
+    >>> from pennylane.labs.intermediate_reps import postorder_traverse
+    >>> traversal = postorder_traverse(G, 0)
+    >>> print(traversal)
+    [(8, 3), (3, 1), (4, 1), (5, 1), (1, 0), (6, 2), (7, 2), (2, 0)]
+    >>> expected = [8, 3, 4, 5, 1, 6, 7, 2] # Skipping trailing root
+    >>> all(child == exp for (child, parent), exp in zip(traversal, expected, strict=True))
+    True
+
+    Note that the ``source_parent`` argument should not be provided when calling the function
+    but is used for the internal recursive structure.
     """
     out = []
     # First, traverse the subtrees attached to the current node ``source``
@@ -69,6 +121,57 @@ def preorder_traverse(tree: nx.Graph, source: int, source_parent: int = None):
 
     A useful illustration of depth-first tree traversals can be found on
     `Wikipedia <https://en.wikipedia.org/wiki/Tree_traversal#Depth-first%20search>`__.
+
+    **Example**
+
+    Consider the tree
+
+    ```
+                      (4)
+                       |
+    (6) - (2) - (0) - (1) - (3) - (8)
+           |           |
+          (7)         (5)
+    ```
+
+    and consider ``(0)`` to be the source, or root, of the tree.
+    We may construct this tree as a ``nx.Graph`` by providing the edge data:
+
+    >>> import networkx as nx
+    >>> G = nx.Graph([(0, 1), (0, 2), (1, 3), (1, 4), (1, 5), (2, 6), (2, 7), (3, 8)])
+
+    As for every tree traversal, pre-order traversal results in a reordering of the nodes of
+    the tree, with each node appearing exactly once. Pre-order traversing the graph means that
+    for every node we reach, we first append the node itself to the ordering and then visit its
+    child nodes (in standard sorting of the node labels/indices).
+    Starting at the root ``(0)``, we thus perform the following steps:
+
+    1. Append ``(0)``, the root.
+    2. Visit the first child of ``(0)``, which is ``(1)``, and append it.
+    3. Visit the first child of ``(1)``, which is ``(3)``, and append it.
+    4. Visit the first and only child of ``(3)``, which is ``(8)``, and append it.
+    5. Visit the next child of ``(1)``, which is ``(4)``, and append it.
+    6. Visit the last child of ``(1)``, which is ``(5)``, and append it.
+    7. Visit the second and last child of ``(0)``, which is ``(2)``, and append it.
+    8. Visit the first child of ``(2)``, which is ``(6)``, and append it.
+    9. Visit the second and last child of ``(2)``, which is ``(7)``, and append it.
+
+    Overall, the pre-order traversal then reads ``[0, 1, 3, 8, 4, 5, 2, 6, 7]``.
+    For the output convention of this function, each node is accompanied by its
+    parent node, because this is useful information needed down the line, and it is not easy to
+    retrieve from the ``nx.Graph`` itself. In addition, the first entry, which always is the root
+    of the tree provided via the ``source`` argument, is *not* included in the output.
+
+    >>> from pennylane.labs.intermediate_reps import preorder_traverse
+    >>> traversal = preorder_traverse(G, 0)
+    >>> print(traversal)
+    [(1, 0), (3, 1), (8, 3), (4, 1), (5, 1), (2, 0), (6, 2), (7, 2)]
+    >>> expected = [1, 3, 8, 4, 5, 2, 6, 7] # Skipping leading root
+    >>> all(child == exp for (child, parent), exp in zip(traversal, expected, strict=True))
+    True
+
+    Note that the ``source_parent`` argument should not be provided when calling the function
+    but is used for the internal recursive structure.
     """
     out = []
 
@@ -91,6 +194,7 @@ def _update(P: np.ndarray, cnots: list[tuple[int]], control: int, target: int):
     to parity matrix P and list of CNOT gates ``cnots``."""
     P[target] += P[control]
     cnots.append((control, target))
+    return P, cnots
 
 
 F_2 = galois.GF(2)
@@ -144,22 +248,28 @@ def _eliminate(P: np.ndarray, connectivity: nx.Graph, idx: int, mode: str, verbo
     # Need post-order nodes in any case
     visit_nodes = postorder_traverse(T, source=idx)
 
+    # For some reason, Pylint does not understand this usage of the Walrus operator.
+    state = (P, cnots)  # pylint: disable=unused-variable
     if mode == "column":
         # For column mode use post-order and parities constraint in first pass (i.1.2)...
         _ = [
-            _update(P, cnots, child, parent)
+            state := _update(*state, child, parent)
             for child, parent in visit_nodes
             if P[child, idx] == 1 and P[parent, idx] == 0
         ]
         # ... and no constraints for second pass (i.1.3)
-        _ = [_update(P, cnots, parent, child) for child, parent in visit_nodes]
+        _ = [state := _update(*state, parent, child) for child, parent in visit_nodes]
 
     else:
         # For row mode use pre-order and constraints from S' in first pass (i.2.2)...
         previsit_nodes = preorder_traverse(T, source=idx)
-        _ = [_update(P, cnots, child, parent) for child, parent in previsit_nodes if child not in S]
+        _ = [
+            state := _update(*state, child, parent)
+            for child, parent in previsit_nodes
+            if child not in S
+        ]
         # ... and no constraints for second pass (i.2.3)
-        _ = [_update(P, cnots, child, parent) for child, parent in visit_nodes]
+        _ = [state := _update(*state, child, parent) for child, parent in visit_nodes]
 
     if verbose:
         print(f"CNOTs from {mode} elimination ({idx}): {cnots}")
