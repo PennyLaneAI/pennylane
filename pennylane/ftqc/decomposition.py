17c637aba8754931d1675e8500e61f695b02f62b
lillian542 <Lillian.frederiksen@xanadu.ai>
2025-05-21 17:25:04 -0400

    disable pylint complaint

--- pennylane/ftqc/decomposition.py
+++ pennylane/ftqc/decomposition.py
@@ -92,6 +92,7 @@ def convert_to_mbqc_formalism(tape):
                 )
                 cnot_corrections(measurements)(wire_map[ctrl], wire_map[tgt])
             else:  # one wire
+                # pylint: disable=isinstance-second-argument-not-valid-type
                 if isinstance(op, (X, Y, Z, Identity)):
                     wire = wire_map[op.wires[0]] if op.wires else ()
                     op.__class__(wire)
