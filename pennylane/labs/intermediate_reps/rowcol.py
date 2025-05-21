b3a06c8b007e05d06a3bdf65bedc782bfe4823fa
dwierichs <david.wierichs@xanadu.ai>
2025-05-21 11:41:04 +0200

    more docs

--- pennylane/labs/intermediate_reps/rowcol.py
+++ pennylane/labs/intermediate_reps/rowcol.py
@@ -28,7 +28,8 @@ def postorder_traverse(tree: nx.Graph, source: int, source_parent: int = None):
         tree (nx.Graph): Tree graph to traverse. Must contain ``source``. Must contain
             ``source_parent`` if it is not None. Typing assumes integer-labeled nodes.
         source (int): Node to start the traversal from
-        source_parent (Optional[int]): Parent node of ``source`` in ``tree``.
+        source_parent (Optional[int]): Parent node of ``source`` in ``tree``. Should not provided
+            manually but is used in recursion.
 
     Returns:
         list[tuple[int]]: Pairs of nodes that constitute post-order traversal of ``tree``
@@ -61,20 +62,7 @@ def postorder_traverse(tree: nx.Graph, source: int, source_parent: int = None):
     for every node we reach, we first visit its child nodes (in standard sorting of the node
     labels/indices) and then append the node itself to the ordering.
 
-    Overall, the post-order traversal reads ``[8, 3, 4, 5, 1, 6, 7, 2, 0]``.
-    To see how this comes about, start at the root ``(0)`` and perform the following steps:
-
-    1. Visit ``(1)``, ``(3)`` and then ``(8)``, without appending any of them.
-    2. Append ``(8)`` because there are no child nodes to visit (it is a leaf node).
-    3. Append ``(3)`` because all children of it have been visited.
-    4. Visit the next child of ``(1)``, which is ``(4)``, and append it because it is a leaf node.
-    5. Visit the last child of ``(1)``, which is ``(5)``, and append it because it is a leaf node.
-    6. Append ``(1)`` because all its children have been visited.
-    7. Visit ``(2)`` and then ``(6)``, the latter of which is appended (leaf node).
-    8. Visit the second and last child of ``(2))``, which is ``(7)``, and append it (leaf node).
-    9. Append ``(2)`` becaues all its children have been visited.
-    10. Append ``(0)`` becaues all its children have been visited.
-
+    The post-order traversal reads ``[8, 3, 4, 5, 1, 6, 7, 2, 0]``.
     For the output convention of this function, each node is accompanied by its
     parent node, because this is useful information needed down the line, and it is not easy to
     retrieve from the ``nx.Graph`` itself. In addition, the last entry, which always is the root
@@ -87,6 +75,19 @@ def postorder_traverse(tree: nx.Graph, source: int, source_parent: int = None):
     >>> expected = [8, 3, 4, 5, 1, 6, 7, 2] # Skipping trailing root
     >>> all(child == exp for (child, parent), exp in zip(traversal, expected, strict=True))
     True
+
+      To see how this comes about, start at the root ``(0)`` and perform the following steps:
+
+      #. Visit ``(1)``, ``(3)`` and then ``(8)``, without appending any of them.
+      #. Append ``(8)`` because there are no child nodes to visit (it is a leaf node).
+      #. Append ``(3)`` because all children of it have been visited.
+      #. Visit the next child of ``(1)``, which is ``(4)``, and append it because it is a leaf node.
+      #. Visit the last child of ``(1)``, which is ``(5)``, and append it because it is a leaf node.
+      #. Append ``(1)`` because all its children have been visited.
+      #. Visit ``(2)`` and then ``(6)``, the latter of which is appended (leaf node).
+      #. Visit the second and last child of ``(2))``, which is ``(7)``, and append it (leaf node).
+      #. Append ``(2)`` becaues all its children have been visited.
+      #. Append ``(0)`` becaues all its children have been visited.
 
     Note that the ``source_parent`` argument should not be provided when calling the function
     but is used for the internal recursive structure.
@@ -113,7 +114,8 @@ def preorder_traverse(tree: nx.Graph, source: int, source_parent: int = None):
         tree (nx.Graph): Tree graph to traverse. Must contain ``source``. Must contain
             ``source_parent`` if it is not None. Typing assumes integer-labeled nodes.
         source (int): Node to start the traversal from
-        source_parent (Optional[int]): Parent node of ``source`` in ``tree``.
+        source_parent (Optional[int]): Parent node of ``source`` in ``tree``. Should not provided
+            manually but is used in recursion.
 
     Returns:
         list[tuple[int]]: Pairs of nodes that constitute pre-order traversal of ``tree``
@@ -127,13 +129,13 @@ def preorder_traverse(tree: nx.Graph, source: int, source_parent: int = None):
 
     Consider the tree
 
-    ```python
-    #                   (4)
-    #                    |
-    # (6) - (2) - (0) - (1) - (3) - (8)
-    #        |           |
-    #       (7)         (5)
-    ```
+    .. code-block:: python
+
+                          (4)
+                           |
+        (6) - (2) - (0) - (1) - (3) - (8)
+               |           |
+              (7)         (5)
 
     and consider ``(0)`` to be the source, or root, of the tree.
     We may construct this tree as a ``nx.Graph`` by providing the edge data:
@@ -146,19 +148,7 @@ def preorder_traverse(tree: nx.Graph, source: int, source_parent: int = None):
     for every node we reach, we first append the node itself to the ordering and then visit its
     child nodes (in standard sorting of the node labels/indices).
 
-    Overall, the pre-order traversal reads ``[0, 1, 3, 8, 4, 5, 2, 6, 7]``.
-    To see how this comes about, start at the root ``(0)`` and perform the following steps:
-
-    1. Append ``(0)``, the root.
-    2. Visit the first child of ``(0)``, which is ``(1)``, and append it.
-    3. Visit the first child of ``(1)``, which is ``(3)``, and append it.
-    4. Visit the first and only child of ``(3)``, which is ``(8)``, and append it.
-    5. Visit the next child of ``(1)``, which is ``(4)``, and append it.
-    6. Visit the last child of ``(1)``, which is ``(5)``, and append it.
-    7. Visit the second and last child of ``(0)``, which is ``(2)``, and append it.
-    8. Visit the first child of ``(2)``, which is ``(6)``, and append it.
-    9. Visit the second and last child of ``(2)``, which is ``(7)``, and append it.
-
+    The pre-order traversal reads ``[0, 1, 3, 8, 4, 5, 2, 6, 7]``.
     For the output convention of this function, each node is accompanied by its
     parent node, because this is useful information needed down the line, and it is not easy to
     retrieve from the ``nx.Graph`` itself. In addition, the first entry, which always is the root
@@ -171,6 +161,18 @@ def preorder_traverse(tree: nx.Graph, source: int, source_parent: int = None):
     >>> expected = [1, 3, 8, 4, 5, 2, 6, 7] # Skipping leading root
     >>> all(child == exp for (child, parent), exp in zip(traversal, expected, strict=True))
     True
+
+      To see how this comes about, start at the root ``(0)`` and perform the following steps:
+
+      #. Append ``(0)``, the root.
+      #. Visit the first child of ``(0)``, which is ``(1)``, and append it.
+      #. Visit the first child of ``(1)``, which is ``(3)``, and append it.
+      #. Visit the first and only child of ``(3)``, which is ``(8)``, and append it.
+      #. Visit the next child of ``(1)``, which is ``(4)``, and append it.
+      #. Visit the last child of ``(1)``, which is ``(5)``, and append it.
+      #. Visit the second and last child of ``(0)``, which is ``(2)``, and append it.
+      #. Visit the first child of ``(2)``, which is ``(6)``, and append it.
+      #. Visit the second and last child of ``(2)``, which is ``(7)``, and append it.
 
     Note that the ``source_parent`` argument should not be provided when calling the function
     but is used for the internal recursive structure.
@@ -342,13 +344,13 @@ def rowcol(P: np.ndarray, connectivity: nx.Graph = None, verbose: bool = False) 
 
     To start, we have the connectivity graph
 
-    ```python
-    # (0) - (3) - (4)
-    #        |
-    #       (2)
-    #        |
-    #       (1)
-    ```
+    .. code-block:: python
+
+        (0) - (3) - (4)
+               |
+              (2)
+               |
+              (1)
 
     and define it in code as a ``networkx.Graph``
     (`networkx documentation <https://networkx.org/documentation/stable/index.html>`__).
@@ -401,13 +403,13 @@ def rowcol(P: np.ndarray, connectivity: nx.Graph = None, verbose: bool = False) 
         We walk through example 1 in arXiv:1910.14478 by Wu et al., which was demonstrated
         in code above, in detail. We restrict ourselves to the following connectivity graph.
 
-        ```python
-        # (0) - (3) - (4)
-        #        |
-        #       (2)
-        #        |
-        #       (1)
-        ```
+        .. code-block:: python
+
+            (0) - (3) - (4)
+                   |
+                  (2)
+                   |
+                  (1)
 
         We look at a CNOT circuit with the following parity matrix. The first column is
         highlighted as we are going to start by eliminating it (``(0)`` is not a cut vertex).
@@ -431,11 +433,11 @@ def rowcol(P: np.ndarray, connectivity: nx.Graph = None, verbose: bool = False) 
         All non-zero vertices are ``S = [0, 2, 3, 4]``. A Steiner tree ``T`` connecting those
         vertices is given by
 
-        ```python
-        # (0) - (3) - (4)
-        #        |
-        #       (2)
-        ```
+        .. code-block:: python
+
+            (0) - (3) - (4)
+                   |
+                  (2)
 
         *Step 2.b: First post-order traversal*
 
@@ -481,11 +483,11 @@ def rowcol(P: np.ndarray, connectivity: nx.Graph = None, verbose: bool = False) 
         first unit vector :math:`e_0`.
         The Steiner tree :math:`T'` encapsulating :math:`S'` is then
 
-        ```python
-        # (0) - (3) - (4)
-        #        |
-        #       (2)
-        ```
+        .. code-block:: python
+
+            (0) - (3) - (4)
+                   |
+                  (2)
 
         where :math:`4 \notin S'`.
 
@@ -529,13 +531,13 @@ def rowcol(P: np.ndarray, connectivity: nx.Graph = None, verbose: bool = False) 
         The set :math:`S` of non-zero entries in column ``P[:,1]`` together with :math:`i` itself
         is :math:`S = [1, 2, 3]` and the encapsulating Steiner tree :math:`T` is simply
 
-        ```python
-        # (3)
-        #  |
-        # (2)
-        #  |
-        # (1)
-        ```
+        .. code-block:: python
+
+            (3)
+             |
+            (2)
+             |
+            (1)
 
         *Step 2.b: First post-order traversal*
 
@@ -567,13 +569,13 @@ def rowcol(P: np.ndarray, connectivity: nx.Graph = None, verbose: bool = False) 
         We first construct :math:`S' = [1, 3, 4]`, which labels rows whose sum yields :math:`e_1`.
         The encapsulating Steiner tree :math:`T'` is
 
-        ```python
-        # (3) - (4)
-        #  |
-        # (2)
-        #  |
-        # (1)
-        ```
+        .. code-block:: python
+
+            (3) - (4)
+             |
+            (2)
+             |
+            (1)
 
         where :math:`2 \notin S'`.
 
