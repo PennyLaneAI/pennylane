0e7ed157ceeb660267dcef7cf98956ccedfcdb1b
JerryChen97 <chenys13@outlook.com>
2025-05-21 10:44:34 -0400

    local salt for `test_nonsample_measure`

--- tests/devices/qubit/test_sampling.py
+++ tests/devices/qubit/test_sampling.py
@@ -978,6 +978,7 @@ class TestBroadcastingPRNG:
         # third batch of samples can be any of |00>, |01>, |10>, or |11>
         assert np.all(np.logical_or(res[2] == 0, res[2] == 1))
 
+    @pytest.mark.local_salt(2)
     @pytest.mark.parametrize(
         "measurement, expected",
         [
