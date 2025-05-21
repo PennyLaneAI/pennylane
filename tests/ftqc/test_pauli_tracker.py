ffdf62e590abdac539401034b72fc8d8362207aa
Shuli Shu <08cnbj@gmail.com>
2025-05-21 13:56:36 -0400

    update unit test

--- tests/ftqc/test_pauli_tracker.py
+++ tests/ftqc/test_pauli_tracker.py
@@ -16,23 +16,18 @@
 import pytest
 
 import pennylane as qml
-from pennylane.ftqc.pauli_tracker import pauli_to_xz, pauli_prod, xz_to_pauli
+from pennylane.ftqc.pauli_tracker import pauli_prod, pauli_to_xz, xz_to_pauli
 
 
 class TestPauliTracker:
     """Test for the pauli tracker related functions."""
 
-    @pytest.mark.parametrize("op", [qml.I, qml.X, qml.Y, qml.Z])
-    def test_pauli_to_xz(self, op):
-        x, z = pauli_to_xz(op)
-        assert x in [0, 1]
-        assert z in [0, 1]
-
-        x_res = 1 if op in [qml.X, qml.Y] else 0
-        z_res = 1 if op in [qml.Y, qml.Z] else 0
-
-        assert x == x_res
-        assert z == z_res
+    @pytest.mark.parametrize(
+        "op, expected", [(qml.I, (0, 0)), (qml.X, (1, 0)), (qml.Y, (1, 1)), (qml.Z, (0, 1))]
+    )
+    def test_pauli_to_xz(self, op, expected):
+        xz = pauli_to_xz(op)
+        assert xz == expected
 
     @pytest.mark.parametrize("op", [qml.S, qml.CNOT, qml.H])
     def test_unsuppored_ops_pauli_to_xz(self, op):
@@ -40,11 +35,11 @@ class TestPauliTracker:
             _ = pauli_to_xz(op)
 
     @pytest.mark.parametrize(
-        "x, z, res", [(0, 0, qml.I), (1, 0, qml.X), (1, 1, qml.Y), (0, 1, qml.Z)]
+        "x, z, expected", [(0, 0, qml.I), (1, 0, qml.X), (1, 1, qml.Y), (0, 1, qml.Z)]
     )
-    def test_xz_to_pauli(self, x, z, res):
+    def test_xz_to_pauli(self, x, z, expected):
         op = xz_to_pauli(x, z)
-        assert op == res
+        assert op == expected
 
     @pytest.mark.parametrize("x, z", [(0, -1), (-1, 0), (-1, -1)])
     def test_xz_decode_pauli_unsupported_error(self, x, z):
@@ -52,7 +47,7 @@ class TestPauliTracker:
             _ = xz_to_pauli(x, z)
 
     @pytest.mark.parametrize(
-        "ops, res",
+        "ops, expected",
         [
             ([qml.I], qml.I),
             ([qml.X], qml.X),
@@ -77,9 +72,9 @@ class TestPauliTracker:
             ([qml.X, qml.Y, qml.Z, qml.I, qml.Z], qml.Z),
         ],
     )
-    def test_pauli_prod(self, ops, res):
+    def test_pauli_prod(self, ops, expected):
         op = pauli_prod(ops)
-        assert res == op
+        assert op == expected
 
     @pytest.mark.parametrize("ops", [[]])
     def test_pauli_prod_to_xz_unsupported_error(self, ops):
