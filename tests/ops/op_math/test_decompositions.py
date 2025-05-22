10815b35bf02a5ea2f9584b07cc5cb642eab2007
Astral Cai <astral.cai@xanadu.ai>
2025-05-22 11:18:19 -0400

    update

--- tests/ops/op_math/test_decompositions.py
+++ tests/ops/op_math/test_decompositions.py
@@ -1387,12 +1387,35 @@ class TestTwoQubitDecompositionWarnings:
             qnode(1.0 + 0.5j)
 
 
+@pytest.mark.unit
 @pytest.mark.parametrize(
     "U, n_wires",
     [
         (qml.matrix(qml.CRX(0.123, [0, 2]) @ qml.CRY(0.456, [1, 3])), 4),
         (qml.QFT.compute_matrix(5), 5),
         (qml.GroverOperator.compute_matrix(6, []), 6),
+        (
+            qml.matrix(
+                qml.Toffoli([0, 4, 5])
+                @ qml.QFT([4, 2])
+                @ qml.CRX(0.123, [0, 2])
+                @ qml.CRY(0.456, [1, 3])
+            ),
+            6,
+        ),
+        (
+            qml.matrix(
+                qml.CRX(0.123, [4, 5])
+                @ qml.CRY(0.123, [4, 2])
+                @ qml.CRX(0.123, [0, 2])
+                @ qml.CRY(0.456, [1, 3])
+            ),
+            6,
+        ),
+        (
+            qml.matrix(qml.CRX(0.123, [4, 2]) @ qml.CRX(0.123, [0, 2]) @ qml.CRY(0.456, [1, 3])),
+            5,
+        ),
     ],
 )
 def test_multi_qubit_decomposition(U, n_wires):
