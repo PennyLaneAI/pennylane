6399e5f80237b741c240cd13ae0ec8e3fec1832b
lillian542 <Lillian.frederiksen@xanadu.ai>
2025-05-23 10:31:36 -0400

    add figure number to paper reference

--- pennylane/ftqc/decomposition.py
+++ pennylane/ftqc/decomposition.py
@@ -116,7 +116,7 @@ def queue_single_qubit_gate(q_mgr, op, in_wire):
     """Queue the resource state preparation, measurements and byproducts
     to execute the operation in the MBQC formalism. This implementation
     follows the procedures defined in Raussendorf et al. 2003,
-    https://doi.org/10.1103/PhysRevA.68.022312"""
+    https://doi.org/10.1103/PhysRevA.68.022312, see Fig. 2"""
 
     graph_wires = q_mgr.acquire_qubits(4)
     wires = [in_wire] + graph_wires
@@ -257,7 +257,7 @@ def _s_corrections(op, measurements):
 def queue_cnot(q_mgr, ctrl_idx, target_idx):
     """Queue the resource state preparation, measurements and byproducts to execute
     the operation in the MBQC formalism. This is the 15-qubit procedure from
-    Raussendorf et al. 2003, https://doi.org/10.1103/PhysRevA.68.022312"""
+    Raussendorf et al. 2003, https://doi.org/10.1103/PhysRevA.68.022312, Fig. 2"""
 
     graph_wires = q_mgr.acquire_qubits(13)
 
@@ -285,7 +285,7 @@ def queue_cnot(q_mgr, ctrl_idx, target_idx):
 def cnot_measurements(wires):
     """Queue the measurements needed to execute CNOT in the MBQC formalism.
     Numbering convention follows the procedure in Raussendorf et al. 2003,
-    https://doi.org/10.1103/PhysRevA.68.022312"""
+    https://doi.org/10.1103/PhysRevA.68.022312, see Fig. 2"""
     ctrl_idx, target_idx, graph_wires = wires
 
     m1 = measure_x(ctrl_idx, reset=True)
@@ -312,7 +312,7 @@ def cnot_corrections(measurements):
     the MBQC formalism, based on measurement results"""
 
     # Numbering convention follows the procedure in Raussendorf et al. 2003,
-    # https://doi.org/10.1103/PhysRevA.68.022312
+    # https://doi.org/10.1103/PhysRevA.68.022312, Fig 2
     m1, m2, m3, m4, m5, m6, m8, m9, m10, m11, m12, m13, m14 = measurements
 
     # corrections on control
@@ -334,7 +334,8 @@ def cnot_corrections(measurements):
 
 def _generate_cnot_graph():
     """Generate a graph for creating the resource state for a
-    CNOT gate"""
+    CNOT gate. Raussendorf et al. 2003, Fig. 2a.
+    https://doi.org/10.1103/PhysRevA.68.022312"""
     wires = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     g = nx.Graph()
     g.add_nodes_from(wires)
