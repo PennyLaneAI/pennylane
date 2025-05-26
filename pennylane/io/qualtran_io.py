5ca5edabe09fad75319459893c81d7297f15f257
Austin Huang <gengminghuang@gmail.com>
2025-05-26 12:41:38 -0400

    dones

--- pennylane/io/qualtran_io.py
+++ pennylane/io/qualtran_io.py
@@ -183,7 +183,7 @@ def _get_op_call_graph():
             gate_types[_map_to_bloq()(qrom_op)] += 1
             gate_types[_map_to_bloq()(qml.adjoint(qrom_op))] += 1
             gate_types[
-                _map_to_bloq()(qml.CRY(np.pi * rotation_angles[0], wires=[0, op.wires[i]]))
+                _map_to_bloq()(qml.CRY(np.pi * rotation_angles[0], wires=[0, 1]))
             ] += len(precision_wires)
 
         if not qml.math.allclose(phases, 0.0):
@@ -229,25 +229,26 @@ def _get_op_call_graph():
 
         num_ctrl_wires = len(op.control_wires)
         control_wires = op.control_wires
+        target_wires = op.hyperparameters["target_wires"]
 
         gate_types = defaultdict(int, {})
         if num_ctrl_wires == 0:
             for bits in op.bitstrings:
                 gate_types[
-                    _map_to_bloq()(qml.BasisEmbedding(int(bits, 2), wires=op.target_wires))
+                    _map_to_bloq()(qml.BasisEmbedding(int(bits, 2), wires=target_wires))
                 ] += 1
 
             return gate_types
 
-        swap_wires = op.target_wires + op.work_wires
+        swap_wires = target_wires + op.work_wires
 
         # number of operators we store per column (power of 2)
-        depth = len(swap_wires) // len(op.target_wires)
+        depth = len(swap_wires) // len(target_wires)
         depth = int(2 ** np.floor(np.log2(depth)))
         depth = min(depth, len(op.bitstrings))
 
-        ops = [qml.BasisEmbedding(int(bits, 2), wires=op.target_wires) for bits in op.bitstrings]
-        ops_identity = ops + [qml.I(op.target_wires)] * int(2 ** len(op.control_wires) - len(ops))
+        ops = [qml.BasisEmbedding(int(bits, 2), wires=target_wires) for bits in op.bitstrings]
+        ops_identity = ops + [qml.I(target_wires)] * int(2 ** len(op.control_wires) - len(ops))
 
         n_columns = len(ops) // depth if len(ops) % depth == 0 else len(ops) // depth + 1
         new_ops = []
@@ -255,8 +256,8 @@ def _get_op_call_graph():
             column_ops = []
             for j in range(depth):
                 dic_map = {
-                    ops_identity[i * depth + j].wires[l]: swap_wires[j * len(op.target_wires) + l]
-                    for l in range(len(op.target_wires))
+                    ops_identity[i * depth + j].wires[l]: swap_wires[j * len(target_wires) + l]
+                    for l in range(len(target_wires))
                 }
                 column_ops.append(qml.map_wires(ops_identity[i * depth + j], dic_map))
             new_ops.append(qml.prod(*column_ops))
@@ -273,21 +274,21 @@ def _get_op_call_graph():
             gate_types[select_op] = 1
             select_ops.append(select_op)
         else:
-            for op in new_ops:
-                bloq = _map_to_bloq()(op)
+            for new_op in new_ops:
+                bloq = _map_to_bloq()(new_op)
                 gate_types[bloq] += 1
                 select_ops.append(bloq)
 
         # Swap block
         control_swap_wires = control_wires[n_control_select_wires:]
-        gate_types[CSwap(len(op.target_wires))] = 2 ** len(control_swap_wires) - 1
+        gate_types[CSwap(len(target_wires))] = 2 ** len(control_swap_wires) - 1
 
         if not op.clean or depth == 1:
             return gate_types
         else:
             # Based on this paper (Fig 4): https://arxiv.org/abs/1902.02134
-            gate_types[Hadamard()] = 2 * len(op.target_wires)
-            gate_types[CSwap(len(op.target_wires))] *= 4
+            gate_types[Hadamard()] = 2 * len(target_wires)
+            gate_types[CSwap(len(target_wires))] *= 4
             gate_types[_map_to_bloq()(qml.Select(new_ops, control=control_select_wires))] = 1
             for op in select_ops:
                 gate_types[op] *= 2
@@ -348,6 +349,10 @@ def _map_to_bloq():
     @singledispatch
     def _to_qt_bloq(op):
         return ToBloq(op)
+    
+    @_to_qt_bloq.register
+    def _(op: qops.Adjoint):
+        return _map_to_bloq()(op.base).adjoint()
 
     # @_to_qt_bloq.register
     # def _(op: qtemps.subroutines.qpe.QuantumPhaseEstimation, *args, **kwargs):
