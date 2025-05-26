13bb8a190be4d605ce83a8d5a74403017a62fb4c
JerryChen97 <chenys13@outlook.com>
2025-05-26 14:54:02 -0400

    My bad: `qdrift` should only xfail the param0

--- tests/templates/test_subroutines/test_qdrift.py
+++ tests/templates/test_subroutines/test_qdrift.py
@@ -269,11 +269,19 @@ class TestIntegration:
 
         assert allclose(expected_state, state)
 
-    @pytest.mark.xfail(
-        reason="Suspicious test that seems to indicate bug. Xfailed temporarily. sc-91298"
-    )
     @pytest.mark.tf
-    @pytest.mark.parametrize("coeffs, ops", test_hamiltonians)
+    @pytest.mark.parametrize(
+        "coeffs, ops",
+        [
+            pytest.param(
+                *test_hamiltonians[0],
+                marks=pytest.mark.xfail(
+                    reason="Suspicious test that seems to indicate bug. Xfailed temporarily. sc-91298"
+                ),
+            ),
+            *test_hamiltonians[1:],
+        ],
+    )
     def test_execution_tf(self, coeffs, ops, seed):
         """Test that the circuit executes as expected using tensorflow"""
         import tensorflow as tf
@@ -302,11 +310,19 @@ class TestIntegration:
 
         assert allclose(expected_state, state)
 
-    @pytest.mark.xfail(
-        reason="Suspicious test that seems to indicate bug. Xfailed temporarily. sc-91298"
-    )
     @pytest.mark.jax
-    @pytest.mark.parametrize("coeffs, ops", test_hamiltonians)
+    @pytest.mark.parametrize(
+        "coeffs, ops",
+        [
+            pytest.param(
+                *test_hamiltonians[0],
+                marks=pytest.mark.xfail(
+                    reason="Suspicious test that seems to indicate bug. Xfailed temporarily. sc-91298"
+                ),
+            ),
+            *test_hamiltonians[1:],
+        ],
+    )
     def test_execution_jax(self, coeffs, ops, seed):
         """Test that the circuit executes as expected using jax"""
         from jax import numpy as jnp
