4fb90eb76679841c41ce3d600d56c821532c1406
dwierichs <david.wierichs@xanadu.ai>
2025-05-30 12:48:27 +0200

    bug fix

--- pennylane/templates/subroutines/select.py
+++ pennylane/templates/subroutines/select.py
@@ -19,10 +19,15 @@ Contains the Select template.
 import copy
 import itertools
 
+import numpy as np
+
 import pennylane as qml
 from pennylane import math
-from pennylane.decomposition import add_decomps, register_resources
+from pennylane.decomposition import add_decomps, adjoint_resource_rep, register_resources
 from pennylane.operation import Operation
+from pennylane.ops import CNOT, Hadamard, S, T, X, adjoint, ctrl
+from pennylane.templates import TemporaryAnd
+from pennylane.wires import Wires
 
 
 class Select(Operation):
@@ -79,8 +84,9 @@ class Select(Operation):
     def __repr__(self):
         return f"Select(ops={self.ops}, control={self.control})"
 
-    def __init__(self, ops, control, work_wires, id=None):
-        control = qml.wires.Wires(control)
+    def __init__(self, ops, control, work_wires=None, id=None):
+        control = Wires(control)
+        work_wires = Wires(() if work_wires is None else work_wires)
         self.hyperparameters["ops"] = tuple(ops)
         self.hyperparameters["control"] = control
         self.hyperparameters["work_wires"] = work_wires
@@ -88,20 +94,19 @@ class Select(Operation):
         if 2 ** len(control) < len(ops):
             raise ValueError(
                 f"Not enough control wires ({len(control)}) for the desired number of "
-                + f"operations ({len(ops)}). At least {int(math.ceil(math.log2(len(ops))))} control "
+                + f"operations ({len(ops)}). At least {_ceil_log(len(ops))} control "
                 + "wires required."
             )
 
         if any(
-            control_wire in qml.wires.Wires.all_wires([op.wires for op in ops])
-            for control_wire in control
+            control_wire in Wires.all_wires([op.wires for op in ops]) for control_wire in control
         ):
             raise ValueError("Control wires should be different from operation wires.")
 
         for op in ops:
             qml.QueuingManager.remove(op)
 
-        target_wires = qml.wires.Wires.all_wires([op.wires for op in ops])
+        target_wires = Wires.all_wires([op.wires for op in ops])
         self.hyperparameters["target_wires"] = target_wires
 
         all_wires = target_wires + control
@@ -199,7 +204,7 @@ class Select(Operation):
         """
         states = list(itertools.product([0, 1], repeat=len(control)))
         decomp_ops = [
-            qml.ctrl(op, control, control_values=states[index]) for index, op in enumerate(ops)
+            ctrl(op, control, control_values=states[index]) for index, op in enumerate(ops)
         ]
         return decomp_ops
 
@@ -224,102 +229,137 @@ class Select(Operation):
         return self.hyperparameters["control"] + self.hyperparameters["target_wires"]
 
 
-def ceil(a):
-    return int(qml.math.ceil(a))
+def _ceil(a):
+    return int(math.ceil(a))
 
 
-def ceil_log(a):
-    return ceil(qml.math.log2(a))
+def _ceil_log(a):
+    return _ceil(math.log2(a))
 
-def add_k_units(ops, controls, work_wires, k, first_iter=False):
-    assert k == len(ops)
-    if k == 0:
-        return []
-    needed_controls = 2 * ceil_log(k) + 1
-    if first_iter:
-        needed_controls -= 2
+
+def _add_first_k_units(ops, controls, work_wires, k):
+    assert k == len(ops) > 2
+
+    needed_controls = 2 * _ceil_log(k) - 1
     assert len(controls) >= needed_controls, f"{len(controls)=}, {needed_controls=}"
     controls = controls[:needed_controls]
-
-    if k == 1:
-        assert (
-            not first_iter
-        )  # If there only is one operation, we should not have control structure at all!
-        return [
-            qml.ctrl(ops[0], control=controls[-1], control_values=[1], work_wires=work_wires)
-        ]
 
     and_wires = controls[:3]
     new_work_wires = work_wires + controls[:2]
     new_controls = controls[2:]
-    _k = 2 ** (ceil_log(k) - 1)
-    if first_iter:
-        k0 = 2 ** (ceil_log(k) - 2)
-        k1 = _k - k0
-        k2 = ceil(2 ** (ceil_log(k - _k) - 1))
-        k3 = k - _k - k2
-        return (
-                [qml.X(and_wires[0]), qml.X(and_wires[1])]
-                + [qml.TemporaryAnd(and_wires)]
-                + [qml.X(and_wires[0]), qml.X(and_wires[1])]
-                + add_k_units(ops[:k0], new_controls, new_work_wires, k0)
-                + [qml.ctrl(qml.X(controls[2]), control=controls[0], control_values=[0])]
-                + add_k_units(ops[k0: k0 + k1], new_controls, new_work_wires, k1)
-                + [qml.CNOT(and_wires[::2]), qml.CNOT(and_wires[1:])]
-                + add_k_units(ops[k0 + k1: k0 + k1 + k2], new_controls, new_work_wires, k2)
-                + [qml.CNOT(and_wires[::2])]
-                + add_k_units(ops[k0 + k1 + k2:], new_controls, new_work_wires, k3)
-                + [qml.adjoint(qml.TemporaryAnd)(and_wires)]
-        )
-    return (
-            [qml.X(and_wires[1])]
-            + [qml.TemporaryAnd(and_wires)]
-            + [qml.X(and_wires[1])]
-            + add_k_units(ops[:_k], new_controls, new_work_wires, _k)
-            + [qml.CNOT(and_wires[::2])]
-            + add_k_units(ops[_k:], new_controls, new_work_wires, k - _k)
-            + [qml.adjoint(qml.TemporaryAnd)(and_wires)]
+
+    _k = 2 ** (_ceil_log(k) - 1)
+    k0 = _k // 2
+    k1 = _k - k0
+    k2 = _ceil(2 ** (_ceil_log(k - _k) - 1))
+    k3 = k - _k - k2
+
+    first_half = (
+        [X(and_wires[0]), X(and_wires[1])]
+        + [TemporaryAnd(and_wires)]
+        + [X(and_wires[0]), X(and_wires[1])]
+        + _add_k_units(ops[:k0], new_controls, new_work_wires, k0)
+        + [ctrl(X(controls[2]), control=controls[0], control_values=[0])]
+        + _add_k_units(ops[k0 : k0 + k1], new_controls, new_work_wires, k1)
     )
 
+    gap_bitstring = np.binary_repr(k - _k, width=(len(controls) + 1) // 2)
+    assert gap_bitstring[0] == "0"
+    i = list(gap_bitstring).index("1")
+    and_wires_sec_half = [controls[0], controls[2 * i - 1], controls[2 * i]]
+    new_controls_sec_half = controls[2 * i :]
+    new_work_wires_sec_half = work_wires + controls[: 2 * i]
+    if and_wires == and_wires_sec_half:
+        middle_part = [CNOT(and_wires[::2]), CNOT(and_wires[1:])]
+    else:
+        middle_part = [
+            X(and_wires[0]),
+            adjoint(TemporaryAnd)(and_wires),
+            X(and_wires[0]),
+            X(and_wires_sec_half[1]),
+            TemporaryAnd(and_wires_sec_half),
+            X(and_wires_sec_half[1]),
+        ]
+
+    second_half = (
+        _add_k_units(
+            ops[k0 + k1 : k0 + k1 + k2], new_controls_sec_half, new_work_wires_sec_half, k2
+        )
+        + [CNOT(and_wires_sec_half[::2])]
+        + _add_k_units(ops[k0 + k1 + k2 :], new_controls_sec_half, new_work_wires_sec_half, k3)
+        + [adjoint(TemporaryAnd)(and_wires_sec_half)]
+    )
+
+    return first_half + middle_part + second_half
+
+
+def _add_k_units(ops, controls, work_wires, k):
+    assert k == len(ops)
+    if k == 0:
+        return []
+    needed_controls = 2 * _ceil_log(k) + 1
+    assert len(controls) >= needed_controls, f"{len(controls)=}, {needed_controls=}"
+    controls = controls[:needed_controls]
+
+    if k == 1:
+        return [ctrl(ops[0], control=controls[-1], control_values=[1], work_wires=work_wires)]
+
+    and_wires = controls[:3]
+    new_work_wires = work_wires + controls[:2]
+    new_controls = controls[2:]
+    _k = 2 ** (_ceil_log(k) - 1)
+    return (
+        [X(and_wires[1])]
+        + [TemporaryAnd(and_wires)]
+        + [X(and_wires[1])]
+        + _add_k_units(ops[:_k], new_controls, new_work_wires, _k)
+        + [CNOT(and_wires[::2])]
+        + _add_k_units(ops[_k:], new_controls, new_work_wires, k - _k)
+        + [adjoint(TemporaryAnd)(and_wires)]
+    )
+
+
+# todo:
 def _unary_select_resources():
     return {
-        qml.Hadamard: 2,
-        qml.CNOT: 3,
-        qml.T: 2,
-        qml.decomposition.adjoint_resource_rep(qml.T, {}): 2,
-        qml.decomposition.adjoint_resource_rep(qml.S, {}): 1,
+        Hadamard: 2,
+        CNOT: 3,
+        T: 2,
+        adjoint_resource_rep(T, {}): 2,
+        adjoint_resource_rep(S, {}): 1,
     }
 
 
 @register_resources(_unary_select_resources)
 def _unary_select(ops, control, work_wires):
     r"""This function reproduces the unary iterator behaviour in https://arxiv.org/abs/1805.03662.
-        For :math:`L` operators this decomposition requires at least :math:`c=\lceil\log_2 L\rceil`
-        control wires (as usual for Select), and :math:`c-1` additional work wires.
+    For :math:`L` operators this decomposition requires at least :math:`c=\lceil\log_2 L\rceil`
+    control wires (as usual for Select), and :math:`c-1` additional work wires.
 
-        Note that there might be a slightly improved implementation if a modified embedding is allowed.
-        """
+    Note that there might be a slightly improved implementation if a modified embedding is allowed.
+    """
+    if len(ops) == 0:
+        return []
 
-
-    min_num_controls = int(qml.math.ceil(qml.math.log2(len(ops))))
+    min_num_controls = _ceil_log(len(ops))
     assert len(control) >= min_num_controls
     control = control[:min_num_controls]
     if len(work_wires) < len(control) - 1:
         raise ValueError(
             f"Can't use this decomposition with less than {len(control) - 1} work wires for {len(control)} controls."
         )
-    if len(ops) == 2:
+    if 1 <= len(ops) <= 2:
         # Don't need unary iterator!
         return [
-            qml.ctrl(ops[0], control=control[0], control_values=[0], work_wires=work_wires),
-            qml.ctrl(ops[1], control=control[0], control_values=[1], work_wires=work_wires),
+            ctrl(op, control=control[0], control_values=[i], work_wires=work_wires)
+            for i, op in enumerate(ops)
         ]
     aux_control = [control[0]]
     for i in range(min_num_controls - 1):
         aux_control.append(control[i + 1])
         aux_control.append(work_wires[i])
-    work_wires = work_wires[min_num_controls - 1:]
-    return add_k_units(ops, aux_control, work_wires, len(ops), first_iter=True)
+    work_wires = work_wires[min_num_controls - 1 :]
+    return _add_first_k_units(ops, aux_control, work_wires, len(ops))
 
 
 add_decomps(Select, _unary_select)
