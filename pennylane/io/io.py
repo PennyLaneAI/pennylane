c8ad00ca642f59b4ca6f40dcbec0825f6e343da1
Marcus Edwards <94946848+comp-phys-marc@users.noreply.github.com>
2025-05-30 11:49:20 -0400

    Rename QASM -> QASM 3.0
    Co-authored-by: Isaac De Vlugt <34751083+isaacdevlugt@users.noreply.github.com>

--- pennylane/io/io.py
+++ pennylane/io/io.py
@@ -850,7 +850,7 @@ def from_qasm3(quantum_circuit: str, wire_map: dict = None):
         for directions.
 
     Args:
-        quantum_circuit (str): a QASM string containing a simple quantum circuit.
+        quantum_circuit (str): a QASM 3.0 string containing a simple quantum circuit.
         qubit_mapping Optional[dict]:  the mapping from OpenQASM 3.0 qubit names to PennyLane wires.
 
     Returns:
