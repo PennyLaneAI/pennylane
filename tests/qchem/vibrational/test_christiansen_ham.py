8367b99cc8bd94e390018facd120291344c19962
soranjh <soran.jahangiri@gmail.com>
2025-06-02 13:27:10 -0400

    fix import

--- tests/qchem/vibrational/test_christiansen_ham.py
+++ tests/qchem/vibrational/test_christiansen_ham.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Unit Tests for the Christiansen Hamiltonian construction functions."""
-import os
-import sys
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
