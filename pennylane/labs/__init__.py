5d397b9135a3610a2f7a7d4dd740781d183ebda1
soranjh <soran.jahangiri@gmail.com>
2025-06-02 14:38:30 -0400

    fix import

--- pennylane/labs/__init__.py
+++ pennylane/labs/__init__.py
@@ -34,7 +34,6 @@ Modules
 
 from pennylane.labs import dla
 from pennylane.labs import resource_estimation
-from pennylane.labs import vibrational
 from pennylane.labs import trotter_error
 from pennylane.labs import intermediate_reps
 
