548cf6af2954f35f527e2ad3aa7024af2f43e8af
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-06-05 15:44:05 -0400

    Debug (brackets)

--- pennylane/ops/functions/equal.py
+++ pennylane/ops/functions/equal.py
@@ -322,12 +322,12 @@ def _equal_pauli_errors(
     op1_non_nums = set(op1.data) - op1_nums
     op2_non_nums = set(op2.data) - op2_nums
 
-    if not qml.math.allclose(
+    if not (qml.math.allclose(
         list(op1_nums),
         list(op2_nums),
         rtol=rtol,
         atol=atol
-    ) and all([(nn1 == nn2) for nn1, nn2 in zip(op1_non_nums, op2_non_nums)]):
+    ) and all([(nn1 == nn2) for nn1, nn2 in zip(op1_non_nums, op2_non_nums)])):
         return f"op1 and op2 have different data.\nGot {op1.data} and {op2.data}"
 
     if check_trainability:
