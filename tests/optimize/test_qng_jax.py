112a34979aa3c4a4ff8d2524dac4d63f7328bddb
SimoneGasperini <simone.gasperini4@unibo.it>
2025-06-09 11:04:35 -0400

    Add more tests for gradient computation with jax

--- tests/optimize/test_qng_jax.py
+++ tests/optimize/test_qng_jax.py
@@ -13,7 +13,22 @@
 # limitations under the License.
 """Test Jax-based Catalyst-compatible QNG optimizer"""
 
+import pytest
+
 import pennylane as qml
+import pennylane.numpy as pnp
+
+dev_names = (
+    "default.qubit",
+    "lightning.qubit",
+)
+
+
+def circuit(params):
+    """Simple circuit to use for testing."""
+    qml.RX(params[0], wires=0)
+    qml.RY(params[1], wires=1)
+    return qml.expval(qml.Z(wires=0))
 
 
 class TestBasics:
@@ -32,3 +47,57 @@ class TestBasics:
         assert opt.stepsize == 0.05
         assert opt.approx == "diag"
         assert opt.lam == 1e-9
+
+    def test_init_none_state(self):
+        """Test that the QNGOptimizerJax state is initialized to `None`."""
+        opt = qml.QNGOptimizerJax()
+        state = opt.init([0.1, 0.2])
+        assert state is None
+
+
+class TestGradients:
+    """Test gradient computation in the QNGOptimizerJax."""
+
+    @pytest.mark.jax
+    @pytest.mark.parametrize("dev_name", dev_names)
+    def test_get_grad_jax(self, dev_name):
+        """Test `_get_grad` method with Jax interface."""
+
+        import jax.numpy as jnp
+
+        device = qml.device(dev_name, wires=2)
+        qml_qnode = qml.QNode(circuit, device=device)
+
+        opt = qml.QNGOptimizerJax()
+        params = [0.1, 0.2]
+
+        params_qml = pnp.array(params)
+        grad_qml = qml.grad(qml_qnode)(params_qml)
+
+        params_jax = jnp.array(params)
+        grad_jax = opt._get_grad(qml_qnode, params_jax)
+
+        assert qml.math.allclose(grad_qml, grad_jax)
+
+    @pytest.mark.jax
+    @pytest.mark.parametrize("dev_name", dev_names)
+    def test_get_value_and_grad_jax(self, dev_name):
+        """Test `_get_value_and_grad` method with Jax interface."""
+
+        import jax.numpy as jnp
+
+        device = qml.device(dev_name, wires=2)
+        qml_qnode = qml.QNode(circuit, device=device)
+
+        opt = qml.QNGOptimizerJax()
+        params = [0.1, 0.2]
+
+        params_qml = pnp.array(params)
+        cost_qml = qml_qnode(params_qml)
+        grad_qml = qml.grad(qml_qnode)(params_qml)
+
+        params_jax = jnp.array(params)
+        cost_jax, grad_jax = opt._get_value_and_grad(qml_qnode, params_jax)
+
+        assert qml.math.allclose(cost_qml, cost_jax)
+        assert qml.math.allclose(grad_qml, grad_jax)
