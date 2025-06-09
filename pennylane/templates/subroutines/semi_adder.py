b2f7cd472915d9946a98799b3ee17aa3d6262791
AntonNI8 <AntonNI8@users.noreply.github.com>
2025-06-09 16:49:58 -0400

    resetting

--- pennylane/templates/subroutines/semi_adder.py
+++ pennylane/templates/subroutines/semi_adder.py
@@ -11,71 +11,78 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-Contains the SemiAdder template.
-"""
+"""Contains the SemiAdder template for performing the semi-out-place addition."""
 
 import pennylane as qml
+from pennylane.decomposition import add_decomps, register_resources
 from pennylane.operation import Operation
 from pennylane.wires import WiresLike
 
 
-class SemiAdder(Operation):
-    r"""Performs the semi-out-place modular addition operation.
-    More specifically, the operation is an in-place quantum-quantum modular addition.
+def _left_operator(ck, ik, tk, aux):
+    """Implement the left block in figure 2, https://arxiv.org/pdf/1709.06648"""
+    op_list = []
+    op_list.append(qml.CNOT([ck, ik]))
+    op_list.append(qml.CNOT([ck, tk]))
+    op_list.append(qml.TemporaryAND([ik, tk, aux]))
+    op_list.append(qml.CNOT([ck, aux]))
+    return op_list
 
-    This operator performs the modular addition of two integers :math:`x` and :math:`y` modulo
-    :math:`mod` in the computational basis:
+
+def _right_operator(ck, ik, tk, aux):
+    """Implement the right block in figure 2, https://arxiv.org/pdf/1709.06648"""
+    op_list = []
+    op_list.append(qml.CNOT([ck, aux]))
+    op_list.append(qml.adjoint(qml.TemporaryAND([ik, tk, aux])))
+    op_list.append(qml.CNOT([ck, ik]))
+    op_list.append(qml.CNOT([ik, tk]))
+    return op_list
+
+
+class SemiAdder(Operation):
+    r"""Performs the semi-out-place addition operation.
+    More specifically, the operation is an in-place quantum-quantum addition.
+
+    This operator performs the addition of two integers :math:`x` and :math:`y` in the computational basis:
 
     .. math::
 
-        \text{SemiAdder}(mod) |x \rangle | y \rangle = |x \rangle | x+y \; \text{mod} \; mod \rangle,
+        \text{SemiAdder} |x \rangle | y \rangle = |x \rangle | x + y  \rangle,
 
-    The implementation is based on the quantum Fourier transform method presented in
-    `arXiv:2311.08555 <https://arxiv.org/abs/2311.08555>`_.
-
-    .. note::
-
-        To obtain the correct result, :math:`x`, :math:`y` must be smaller than :math:`mod`.
-
-    .. seealso:: :class:`~.PhaseAdder` and :class:`~.Adder`.
+    The implementation is based on `arXiv:1709.06648 <https://arxiv.org/abs/1709.06648>`_.
 
     Args:
         x_wires (Sequence[int]): the wires that store the integer :math:`x`
         y_wires (Sequence[int]): the wires that store the integer :math:`y`
-        mod (int): the modulo for performing the addition. If not provided, it will be set to its maximum value, :math:`2^{\text{len(y_wires)}}`.
-        work_wires (Sequence[int]): the auxiliary wires to use for the addition. The work wires are not needed if :math:`mod=2^{\text{len(y_wires)}}`,
-            otherwise two work wires should be provided. Defaults to empty tuple.
+        work_wires (Sequence[int]): the auxiliary wires to use for the addition. ``len(y_wires) - 1`` work
+            wires should be provided.
 
     **Example**
 
-    This example computes the sum of two integers :math:`x=5` and :math:`y=6` modulo :math:`mod=7`.
+    This example computes the sum of two integers :math:`x=5` and :math:`y=6`.
 
     .. code-block::
 
-        x=5
-        y=6
-        mod=7
+        x = 3
+        y = 4
 
-        x_wires=[0,1,2]
-        y_wires=[3,4,5]
-        work_wires=[6,7]
+        wires = qml.registers({"x":3, "y":6, "work":5})
 
         dev = qml.device("default.qubit", shots=1)
+
         @qml.qnode(dev)
         def circuit():
-            qml.BasisEmbedding(x, wires=x_wires)
-            qml.BasisEmbedding(y, wires=y_wires)
-            qml.SemiAdder(x_wires, y_wires, mod, work_wires)
-            return qml.sample(wires=y_wires)
+            qml.BasisEmbedding(x, wires=wires["x"])
+            qml.BasisEmbedding(y, wires=wires["y"])
+            qml.SemiAdder(wires["x"], wires["y"], wires["work"])
+            return qml.sample(wires=wires["y"])
 
     .. code-block:: pycon
 
         >>> print(circuit())
-        [1 0 0]
+        [0 0 0 1 1 1]
 
-    The result :math:`[1 0 0]`, is the binary representation of
-    :math:`5 + 6 \; \text{modulo} \; 7 = 4`.
+    The result :math:`[0 0 0 1 1 1]`, is the binary representation of :math:`3 + 4 = 7`.
 
     .. details::
         :title: Usage Details
@@ -83,74 +90,58 @@ class SemiAdder(Operation):
         This template takes as input three different sets of wires.
 
         The first one is ``x_wires`` which is used
-        to encode the integer :math:`x < mod` in the computational basis. Therefore, ``x_wires`` must contain
+        to encode the integer :math:`x` in the computational basis. Therefore, ``x_wires`` must contain
         at least :math:`\lceil \log_2(x)\rceil` to represent :math:`x`.
 
         The second one is ``y_wires`` which is used
-        to encode the integer :math:`y < mod` in the computational basis. Therefore, ``y_wires`` must contain
-        at least :math:`\lceil \log_2(y)\rceil` wires to represent :math:`y`.
-        ``y_wires`` is also used
-        to encode the integer :math:`x+y \; \text{mod} \; mod` in the computational basis. Therefore, it will require at least
-        :math:`\lceil \log_2(mod)\rceil` wires to represent :math:`x+y \; \text{mod} \; mod`.
+        to encode the integer :math:`y` in the computational basis. Therefore, ``y_wires`` must contain
+        at least :math:`\lceil \log_2(y)\rceil` wires to represent :math:`y`. ``y_wires`` is also used
+        to encode the integer :math:`x+y` in the computational basis.
 
-        The fourth set of wires is ``work_wires`` which consist of the auxiliary qubits used to perform the modular addition operation.
-
-        - If :math:`mod = 2^{\text{len(y_wires)}}`, there will be no need for ``work_wires``, hence ``work_wires=None``. This is the case by default.
-
-        - If :math:`mod \neq 2^{\text{len(y_wires)}}`, two ``work_wires`` have to be provided.
-
-        Note that the ``SemiAdder`` template allows us to perform modular addition in the computational basis.
-        However if one just wants to perform standard addition (with no modulo),
-        that would be equivalent to setting the modulo :math:`mod` to a large enough value to ensure that :math:`x+k < mod`.
+        The fourth set of wires is ``work_wires`` which consist of :math:`m-1` auxiliary qubits used to perform the addition operation,
+        where :math:`m` is the number of ``y_wires``.
     """
 
     grad_method = None
+
+    resource_keys = {"num_y_wires"}
 
     def __init__(
         self,
         x_wires: WiresLike,
         y_wires: WiresLike,
-        mod=None,
-        work_wires: WiresLike = (),
+        work_wires,
         id=None,
     ):  # pylint: disable=too-many-arguments
 
         x_wires = qml.wires.Wires(x_wires)
         y_wires = qml.wires.Wires(y_wires)
-        work_wires = qml.wires.Wires(() if work_wires is None else work_wires)
+        work_wires = qml.wires.Wires(work_wires)
 
-        num_work_wires = len(work_wires)
-
-        if mod is None:
-            mod = 2 ** (len(y_wires))
-        if mod > 2 ** len(y_wires):
-            raise ValueError(
-                "SemiAdder must have enough wires to represent mod. The maximum mod "
-                f"with len(y_wires)={len(y_wires)} is {2 ** len(y_wires)}, but received {mod}."
-            )
-        if mod != 2 ** len(y_wires) and num_work_wires != 2:
-            raise ValueError(
-                f"If mod is not 2^{len(y_wires)}, two work wires should be provided."
-            )
-        if len(work_wires) != 0:
-            if any(wire in work_wires for wire in x_wires):
-                raise ValueError("None of the wires in work_wires should be included in x_wires.")
-            if any(wire in work_wires for wire in y_wires):
-                raise ValueError("None of the wires in work_wires should be included in y_wires.")
-        if any(wire in y_wires for wire in x_wires):
+        if len(work_wires) != len(y_wires) - 1:
+            raise ValueError(f"At least {len(y_wires)-1} work_wires should be provided.")
+        if not set(work_wires).isdisjoint(x_wires):
+            raise ValueError("None of the wires in work_wires should be included in x_wires.")
+        if not set(work_wires).isdisjoint(y_wires):
+            raise ValueError("None of the wires in work_wires should be included in y_wires.")
+        if not set(x_wires).isdisjoint(y_wires):
             raise ValueError("None of the wires in y_wires should be included in x_wires.")
+
         for key in ["x_wires", "y_wires", "work_wires"]:
             self.hyperparameters[key] = qml.wires.Wires(locals()[key])
 
         # pylint: disable=consider-using-generator
         all_wires = sum(
-            [self.hyperparameters[key] for key in ["x_wires", "y_wires"]], start=[]
+            [self.hyperparameters[key] for key in ["x_wires", "y_wires", "work_wires"]], start=[]
         )
-        if num_work_wires != 0:
-            all_wires += self.hyperparameters["work_wires"]
 
-        self.hyperparameters["mod"] = mod
         super().__init__(wires=all_wires, id=id)
+
+    @property
+    def resource_params(self) -> dict:
+        return {
+            "num_y_wires": len(self.hyperparameters["y_wires"]),
+        }
 
     @property
     def num_params(self):
@@ -174,7 +165,6 @@ class SemiAdder(Operation):
         return SemiAdder(
             new_dict["x_wires"],
             new_dict["y_wires"],
-            self.hyperparameters["mod"],
             new_dict["work_wires"],
         )
 
@@ -187,40 +177,109 @@ class SemiAdder(Operation):
         return cls._primitive.bind(*args, **kwargs)
 
     @staticmethod
-    def compute_decomposition(
-        x_wires, y_wires, mod, work_wires
-    ):  # pylint: disable=arguments-differ
+    def compute_decomposition(x_wires, y_wires, work_wires):  # pylint: disable=arguments-differ
         r"""Representation of the operator as a product of other operators.
 
         Args:
             x_wires (Sequence[int]): the wires that store the integer :math:`x`
             y_wires (Sequence[int]): the wires that store the integer :math:`y`
-            mod (int): the modulo for performing the addition. If not provided, it will be set to its maximum value, :math:`2^{\text{len(y_wires)}}`.
-            work_wires (Sequence[int]): the auxiliary wires to use for the addition. The work wires are not needed if :math:`mod=2^{\text{len(y_wires)}}`,
-                otherwise two work wires should be provided. Defaults to ``None``.
+            work_wires (Sequence[int]): the auxiliary wires to use for the addition. ``len(y_wires) . 1`` work
+                wires should be provided.
+
         Returns:
             list[.Operator]: Decomposition of the operator
-
-        **Example**
-
-        >>> qml.SemiAdder.compute_decomposition(x_wires=[0,1], y_wires=[2,3], mod=4, work_wires=[4,5])
-        [QFT(wires=[2, 3]),
-        ControlledSequence(PhaseAdder(wires=[2, 3, None]), control=[0, 1]),
-        Adjoint(QFT(wires=[2, 3]))]
         """
         op_list = []
-        if mod != 2 ** len(y_wires) and mod is not None:
-            qft_wires = work_wires[:1] + y_wires
-            work_wire = work_wires[1:]
-        else:
-            qft_wires = y_wires
-            work_wire = ()
-        op_list.append(qml.QFT(wires=qft_wires))
+
+        # revert wires to follow PennyLane convention
+
+        x_wires_pl = x_wires[::-1][: len(y_wires)]
+        y_wires_pl = y_wires[::-1]
+        work_wires_pl = work_wires[::-1]
+        op_list.append(qml.TemporaryAND([x_wires_pl[0], y_wires_pl[0], work_wires_pl[0]]))
+
+        for i in range(1, len(y_wires_pl) - 1):
+            if i < len(x_wires_pl):
+                op_list += _left_operator(
+                    work_wires_pl[i - 1], x_wires_pl[i], y_wires_pl[i], work_wires_pl[i]
+                )
+            else:
+                # If the number of qubits in |x> is smaller than |y>, we can conceptually complete the |x> state
+                # with |0> on the left making sure they are of the same size. Assuming this, we can simplify the left operator.
+                op_list.append(qml.CNOT([work_wires_pl[i - 1], y_wires_pl[i]]))
+                op_list.append(
+                    qml.TemporaryAND([work_wires_pl[i - 1], y_wires_pl[i], work_wires_pl[i]])
+                )
+                op_list.append(qml.CNOT([work_wires_pl[i - 1], work_wires_pl[i]]))
+
+        op_list.append(qml.CNOT([work_wires_pl[-1], y_wires_pl[-1]]))
+
+        if len(x_wires_pl) >= len(y_wires_pl):
+            op_list.append(qml.CNOT([x_wires_pl[-1], y_wires_pl[-1]]))
+
+        for i in range(len(y_wires_pl) - 2, 0, -1):
+            if i < len(x_wires_pl):
+                op_list += _right_operator(
+                    work_wires_pl[i - 1], x_wires_pl[i], y_wires_pl[i], work_wires_pl[i]
+                )
+            else:
+                # If the number of qubits in |x> is smaller than |y>, we can conceptually complete the |x> state
+                # with |0> on the left making sure they are of the same size. Assuming this, we can simplify the right operator.
+                op_list.append(qml.CNOT([work_wires_pl[i - 1], work_wires_pl[i]]))
+                op_list.append(
+                    qml.adjoint(
+                        qml.TemporaryAND([work_wires_pl[i - 1], y_wires_pl[i], work_wires_pl[i]])
+                    )
+                )
+
         op_list.append(
-            qml.ControlledSequence(
-                qml.PhaseAdder(1, qft_wires, mod, work_wire), control=x_wires
-            )
+            qml.adjoint(qml.TemporaryAND([x_wires_pl[0], y_wires_pl[0], work_wires_pl[0]]))
         )
-        op_list.append(qml.adjoint(qml.QFT)(wires=qft_wires))
+
+        op_list.append(qml.CNOT([x_wires_pl[0], y_wires_pl[0]]))
 
         return op_list
+
+
+def _semiadder_resources(num_y_wires):
+    # In the case where len(x_wires) < len(y_wires), this is an upper bound.
+    return {
+        qml.TemporaryAND: num_y_wires - 1,
+        qml.decomposition.adjoint_resource_rep(qml.TemporaryAND, {}): num_y_wires - 1,
+        qml.CNOT: 6 * (num_y_wires - 2) + 3,
+    }
+
+
+@register_resources(_semiadder_resources)
+def _semiadder(x_wires, y_wires, work_wires, **_):
+    x_wires_pl = x_wires[::-1][: len(y_wires)]
+    y_wires_pl = y_wires[::-1]
+    work_wires_pl = work_wires[::-1]
+    qml.TemporaryAND([x_wires_pl[0], y_wires_pl[0], work_wires_pl[0]])
+
+    for i in range(1, len(y_wires_pl) - 1):
+        if i < len(x_wires_pl):
+            _left_operator(work_wires_pl[i - 1], x_wires_pl[i], y_wires_pl[i], work_wires_pl[i])
+        else:
+            qml.CNOT([work_wires_pl[i - 1], y_wires_pl[i]])
+            qml.TemporaryAND([work_wires_pl[i - 1], y_wires_pl[i], work_wires_pl[i]])
+            qml.CNOT([work_wires_pl[i - 1], work_wires_pl[i]])
+
+    qml.CNOT([work_wires_pl[-1], y_wires_pl[-1]])
+
+    if len(x_wires_pl) >= len(y_wires_pl):
+        qml.CNOT([x_wires_pl[-1], y_wires_pl[-1]])
+
+    for i in range(len(y_wires_pl) - 2, 0, -1):
+        if i < len(x_wires_pl):
+            _right_operator(work_wires_pl[i - 1], x_wires_pl[i], y_wires_pl[i], work_wires_pl[i])
+        else:
+            qml.CNOT([work_wires_pl[i - 1], work_wires_pl[i]])
+            qml.adjoint(qml.TemporaryAND([work_wires_pl[i - 1], y_wires_pl[i], work_wires_pl[i]]))
+
+    qml.adjoint(qml.TemporaryAND([x_wires_pl[0], y_wires_pl[0], work_wires_pl[0]]))
+
+    qml.CNOT([x_wires_pl[0], y_wires_pl[0]])
+
+
+add_decomps(SemiAdder, _semiadder)
--- tests/templates/test_subroutines/test_semi_adder.py
+++ tests/templates/test_subroutines/test_semi_adder.py
@@ -19,15 +19,15 @@ import pytest
 
 import pennylane as qml
 from pennylane import numpy as np
+from pennylane.ops.functions.assert_valid import _test_decomposition_rule
 
 
 def test_standard_validity_SemiAdder():
     """Check the operation using the assert_valid function."""
-    x_wires = [0, 1]
-    y_wires = [2, 3, 6]
-    mod = 7
-    work_wires = [4,5]
-    op = qml.SemiAdder(x_wires, y_wires, mod, work_wires)
+    x_wires = [0, 1, 2]
+    y_wires = [4, 5, 6]
+    work_wires = [7, 8]
+    op = qml.SemiAdder(x_wires, y_wires, work_wires)
     qml.ops.functions.assert_valid(op)
 
 
@@ -35,18 +35,21 @@ class TestSemiAdder:
     """Test the qml.SemiAdder template."""
 
     @pytest.mark.parametrize(
-        ("x_wires", "y_wires", "mod", "work_wires", "x", "y"),
+        ("x_wires", "y_wires", "work_wires", "x", "y"),
         [
-            ([0, 1, 2], [3, 4, 5], 3, [6, 7], 1, 2),
-            ([0, 1, 2], [3, 4, 5], 7, [6, 7], 5, 6),
-            ([0, 1], [2, 3, 4], 5, [5, 6], 3, 2),
-            ([0, 1], [2, 3, 4, 5], None, [6, 7], 3, 10),
-            ([0, 1, 2], [3, 4, 5], None, None, 7, 7),
-            ([0, 1, 2], [3, 4, 5, 6], None, [7, 8], 6, 5),
+            ([0, 1, 2], [3, 4, 5], [6, 7], 1, 2),
+            ([0, 1, 2], [3, 4, 5], [6, 7], 5, 6),
+            ([0, 1], [2, 3, 4], [5, 6], 3, 2),
+            ([0, 1], [2, 3, 4, 5], [6, 7, 8], 3, 10),
+            ([0, 1, 2], [3, 4, 5], [6, 7], 7, 7),
+            ([0, 1, 2], [3, 4, 5, 6], [7, 8, 9], 6, 5),
+            ([0], [3, 4, 5, 6], [7, 8, 9], 1, 5),
+            ([0, 1, 2, 3, 4], [5, 6], [7], 11, 2),
+            (["a", "b", "d"], ["e", "h", "p"], ["f", "z"], 4, 2),
         ],
     )
     def test_operation_result(
-        self, x_wires, y_wires, mod, work_wires, x, y
+        self, x_wires, y_wires, work_wires, x, y
     ):  # pylint: disable=too-many-arguments
         """Test the correctness of the SemiAdder template output."""
         dev = qml.device("default.qubit", shots=1)
@@ -55,112 +58,85 @@ class TestSemiAdder:
         def circuit(x, y):
             qml.BasisEmbedding(x, wires=x_wires)
             qml.BasisEmbedding(y, wires=y_wires)
-            qml.SemiAdder(x_wires, y_wires, mod, work_wires)
+            qml.SemiAdder(x_wires, y_wires, work_wires)
             return qml.sample(wires=y_wires)
-
-        if mod is None:
-            mod = 2 ** len(y_wires)
 
         # pylint: disable=bad-reversed-sequence
         assert np.allclose(
             sum(bit * (2**i) for i, bit in enumerate(reversed(circuit(x, y)))),
-            (x + y) % mod,
+            (x + y) % 2 ** len(y_wires),
         )
 
     @pytest.mark.parametrize(
-        ("x_wires", "y_wires", "mod", "work_wires", "msg_match"),
+        ("x_wires", "y_wires", "work_wires", "msg_match"),
         [
             (
                 [0, 1, 2],
                 [3, 4, 5],
-                9,
-                [6, 7],
-                "SemiAdder must have enough wires to represent mod.",
+                [1, 6, 7],
+                "At least 2 work_wires should be provided.",
             ),
             (
                 [0, 1, 2],
                 [3, 4, 5],
-                7,
                 [1, 6],
                 "None of the wires in work_wires should be included in x_wires.",
             ),
             (
                 [0, 1, 2],
                 [3, 4, 5],
-                7,
                 [3, 6],
                 "None of the wires in work_wires should be included in y_wires.",
             ),
             (
                 [0, 1, 2],
                 [2, 3, 4, 5],
-                7,
-                [6, 7],
+                [6, 7, 8],
                 "None of the wires in y_wires should be included in x_wires.",
             ),
         ],
     )
     def test_wires_error(
-        self, x_wires, y_wires, mod, work_wires, msg_match
+        self, x_wires, y_wires, work_wires, msg_match
     ):  # pylint: disable=too-many-arguments
         """Test an error is raised when some work_wires don't meet the requirements"""
         with pytest.raises(ValueError, match=msg_match):
-            qml.SemiAdder(x_wires, y_wires, mod, work_wires)
-
-    @pytest.mark.parametrize("work_wires", [None, [6], [6, 7, 8]])
-    def test_validation_of_num_work_wires(self, work_wires):
-        """Test that when mod is not 2**len(y_wires), validation confirms two
-        work wires are present, while any work wires are accepted for mod=2**len(y_wires)"""
-
-        # if mod=2**len(output_wires), anything goes
-        qml.SemiAdder(
-            x_wires=[0, 1, 2],
-            y_wires=[3, 4, 5],
-            mod=8,
-            work_wires=work_wires,
-        )
-
-        with pytest.raises(ValueError, match="two work wires should be provided"):
-            qml.SemiAdder(
-                x_wires=[0, 1, 2],
-                y_wires=[3, 4, 5],
-                mod=7,
-                work_wires=work_wires,
-            )
+            qml.SemiAdder(x_wires, y_wires, work_wires)
 
     def test_decomposition(self):
         """Test that compute_decomposition and decomposition work as expected."""
-        x_wires, y_wires, output_wires, mod, work_wires = (
+        x_wires, y_wires, work_wires = (
+            [0, 1, 2, 3, 4],
+            [5, 6, 7, 8, 9],
+            [10, 11, 12, 13],
+        )
+
+        adder_decomposition = qml.SemiAdder(x_wires, y_wires, work_wires).compute_decomposition(
+            x_wires, y_wires, work_wires
+        )
+
+        names = [op.name for op in adder_decomposition]
+
+        # Example in Fig 1.  https://arxiv.org/pdf/1709.06648
+        assert names.count("TemporaryAND") == 4
+        assert names.count("Adjoint(TemporaryAND)") == 4
+        assert names.count(("CNOT")) == 21
+
+    @pytest.mark.parametrize(
+        ("x_wires"),
+        [
             [0, 1, 2],
-            [3, 4, 5],
-            7,
-            [6, 7],
-        )
-        adder_decomposition = qml.SemiAdder(
-            x_wires, y_wires, mod, work_wires
-        ).compute_decomposition(x_wires, y_wires, mod, work_wires)
-        op_list = []
-        if mod != 2 ** len(y_wires) and mod is not None:
-            qft_wires = work_wires[:1] + output_wires
-            work_wire = work_wires[1:]
-        else:
-            qft_wires = output_wires
-            work_wire = None
-        op_list.append(qml.QFT(wires=qft_wires))
-        op_list.append(
-            qml.ControlledSequence(
-                qml.PhaseAdder(1, qft_wires, mod, work_wire), control=x_wires
+            [0, 1],
+            [0, 1, 2, 3],
+        ],
+    )
+    def test_decomposition_rule(self, x_wires):
+        """Tests that SemiAdder is decomposed properly."""
+
+        for rule in qml.list_decomps(qml.SemiAdder):
+            _test_decomposition_rule(
+                qml.SemiAdder(x_wires, [5, 6, 7, 8], [9, 10, 11]), rule, heuristic_resources=True
             )
-        )
-        op_list.append(qml.adjoint(qml.QFT)(wires=qft_wires))
-
-        for op1, op2 in zip(adder_decomposition, op_list):
-            qml.assert_equal(op1, op2)
-
-    def test_work_wires_added_correctly(self):
-        """Test that no work wires are added if work_wire = None"""
-        wires = qml.SemiAdder(x_wires=[1, 2], y_wires=[3, 4]).wires
-        assert wires == qml.wires.Wires([1, 2, 3, 4])
 
     @pytest.mark.jax
     def test_jit_compatible(self):
@@ -172,7 +148,6 @@ class TestSemiAdder:
 
         x, y = 2, 3
 
-        mod = 7
         x_wires = [0, 1, 4]
         y_wires = [2, 3, 5]
         work_wires = [7, 8]
@@ -183,10 +158,11 @@ class TestSemiAdder:
         def circuit():
             qml.BasisEmbedding(x, wires=x_wires)
             qml.BasisEmbedding(y, wires=y_wires)
-            qml.SemiAdder(x_wires, y_wires, mod, work_wires)
+            qml.SemiAdder(x_wires, y_wires, work_wires)
             return qml.sample(wires=y_wires)
 
         # pylint: disable=bad-reversed-sequence
         assert jax.numpy.allclose(
-            sum(bit * (2**i) for i, bit in enumerate(reversed(circuit()))), (x + y) % mod
+            sum(bit * (2**i) for i, bit in enumerate(reversed(circuit()))),
+            (x + y) % 2 ** len(y_wires),
         )
