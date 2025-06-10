f722d6d47ef61bfb799efe70555ec24a2f8080f0
Astral Cai <astral.cai@xanadu.ai>
2025-06-10 16:02:04 -0400

    move function

--- pennylane/templates/state_preparations/mottonen.py
+++ pennylane/templates/state_preparations/mottonen.py
@@ -120,30 +120,6 @@ def compute_theta(alpha: TensorLike, num_qubits: Optional[int] = None):
     return qml.math.reshape(qml.math.transpose(alpha), orig_shape)
 
 
-def _uniform_rotation_dagger_ops(gate, alpha, control_wires, target_wire):
-    r"""Returns a list of operators that applies a uniformly-controlled rotation to the target qubit.
-
-    Args:
-        gate (.Operation): gate to be applied, needs to have exactly one parameter
-        alpha (tensor_like): angles to decompose the uniformly-controlled rotation into multi-controlled rotations
-        control_wires (array[int]): wires that act as control
-        target_wire (int): wire that acts as target
-
-    Returns:
-          list[.Operator]: sequence of operators defined by this function
-
-    """
-
-    with qml.queuing.AnnotatedQueue() as q:
-        _apply_uniform_rotation_dagger(gate, alpha, control_wires, target_wire)
-
-    if qml.queuing.QueuingManager.recording():
-        for op in q.queue:
-            qml.apply(op)
-
-    return q.queue
-
-
 def _apply_uniform_rotation_dagger(gate, alpha, control_wires, target_wire):
     r"""Applies a uniformly-controlled rotation to the target qubit.
 
@@ -195,6 +171,30 @@ def _apply_uniform_rotation_dagger(gate, alpha, control_wires, target_wire):
         if skip_none or qml.math.all(theta[..., i] != 0.0):
             gate(theta[..., i], wires=[target_wire])
         qml.CNOT(wires=[control_wires[control_index], target_wire])
+
+
+def _uniform_rotation_dagger_ops(gate, alpha, control_wires, target_wire):
+    r"""Returns a list of operators that applies a uniformly-controlled rotation to the target qubit.
+
+    Args:
+        gate (.Operation): gate to be applied, needs to have exactly one parameter
+        alpha (tensor_like): angles to decompose the uniformly-controlled rotation into multi-controlled rotations
+        control_wires (array[int]): wires that act as control
+        target_wire (int): wire that acts as target
+
+    Returns:
+          list[.Operator]: sequence of operators defined by this function
+
+    """
+
+    with qml.queuing.AnnotatedQueue() as q:
+        _apply_uniform_rotation_dagger(gate, alpha, control_wires, target_wire)
+
+    if qml.queuing.QueuingManager.recording():
+        for op in q.queue:
+            qml.apply(op)
+
+    return q.queue
 
 
 def _get_alpha_z(omega, n, k):
