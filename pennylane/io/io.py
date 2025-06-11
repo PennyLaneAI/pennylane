119f8d329af2d7f9e573b67ae07a4c4b436ad57d
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-06-11 13:14:12 -0400

    Return outputs from from_qasm3

--- pennylane/io/io.py
+++ pennylane/io/io.py
@@ -890,4 +890,6 @@ def from_qasm3(quantum_circuit: str, wire_map: dict = None, **kwargs):
         ast, context={"name": "global", "wire_map": wire_map}, **kwargs
     )
 
+    if len(context["return"].keys()) > 0:
+        return tuple(map(lambda v: v.val, context["return"].values()))
     return context
