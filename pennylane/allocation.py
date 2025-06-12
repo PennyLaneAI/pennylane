57bcb47c9725e5b2e0ca5b81b8a85384943228e3
albi3ro <chrissie.c.l@gmail.com>
2025-06-12 14:31:34 -0400

    add allocate, deallocate, and allocate_ctx

---
+++ pennylane/allocation.py
@@ -0,0 +1,275 @@
+# Copyright 2025 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This module contains the commands for allocating and freeing wires dynamically.
+"""
+import uuid
+from typing import Optional, Sequence
+
+from pennylane.operation import Operator
+from pennylane.wires import Wires
+
+
+class DynamicWire:
+    """A wire whose concrete value will be determined later during a compilation step or execution.
+
+    Multiple dynamic wires can correspond to the same device wire as long as they are properly allocated and
+    freed.
+
+    Args:
+        key (Optional[str]): a ``uuid4`` string to uniquely identify the dynamic wire.
+    """
+
+    def __init__(self, key: Optional[uuid.UUID] = None):
+        if key:
+            self.key = key
+        else:
+            self.key = uuid.uuid4()
+
+    def __eq__(self, other):
+        if not isinstance(other, DynamicWire):
+            return False
+        return self.key == other.key
+
+    def __hash__(self):
+        return hash(("DynamicWire", self.key))
+
+    def __repr__(self):
+        return "<DynamicWire>"
+
+
+class Allocate(Operator):
+    """An instruction to values for dynamic wires.
+
+    Args:
+        wires (list[DynamicWire]): a list of dynamic wire values.
+
+    Keyword Args:
+        require_zeros (bool): Whether or not the wire must start in a ``0`` state.
+
+    ..see-also:: :func:`~.allocate`, :func:`~.allocate_ctx`.
+
+    """
+
+    def __init__(self, wires, require_zeros=True):
+        super().__init__(wires=wires)
+        self._hyperparameters = {
+            "require_zeros": require_zeros,
+        }
+
+    @property
+    def require_zeros(self):
+        """Whether or not the allocated wires are required to be in the zero state."""
+        return self.hyperparameters["require_zeros"]
+
+    @classmethod
+    def from_num_wires(cls, num_wires: int, require_zeros=True) -> "Allocate":
+        """Initialize an ``Allocate`` op from a number of wires instead of already constructed dynamic wires."""
+        wires = tuple(DynamicWire() for _ in range(num_wires))
+        return cls(wires=wires, require_zeros=require_zeros)
+
+
+class Deallocate(Operator):
+    """An instruction to deallocate the provided ``DynamicWire``'s.
+
+    Args:
+        wires (DynamicWire, Sequence[DynamicWire]): one or more dynamic wires to deallocate.
+        reset_to_original (bool): Whether or not the qubit will be in the same state upon being freed.
+
+    """
+
+    def __init__(self, wires: DynamicWire | Sequence[DynamicWire], reset_to_original=False):
+        super().__init__(wires=wires)
+        self._hyperparameters = {"reset_to_original": reset_to_original}
+
+    @property
+    def reset_to_original(self):
+        """Whether or not the dynamic wire will be returned to its original state."""
+        return self.hyperparameters["reset_to_original"]
+
+
+def allocate(num_wires: int, require_zeros: bool = True) -> Wires:
+    """Allocate dynamic wires for temporary use within a circuit.
+
+    .. warning::
+
+        This feature is experimental and may not be possible on all devices.
+
+
+    Args:
+        num_wires (int): the number of wires to allocate
+
+    Keyword Args:
+        require_zeros (bool): Whether or not the wires must start in the ``0`` state.
+
+    Returns:
+        Wires: A wires object containing ``DynamicWire`` objects.
+
+    .. see-also:: :class:`~.allocate_ctx`
+
+    :class:`~.allocate_ctx` is recommended as the preferred way to allocate wires, as it enforces automatic deallocation.
+    Manual use of ``allocate`` and ``deallocate`` should be used with more deliberate care.
+
+    ..code-block:: python
+
+        @qml.qnode(qml.device('default.qubit'))
+        def c():
+            qml.H(0)
+
+            wires = qml.allocate(1, require_zeros=True)
+            qml.CNOT((0, wires[0]))
+            qml.CNOT((0, wires[0]))
+            qml.deallocate(wires, reset_to_original=True)
+
+            new_wires = qml.allocate(1)
+            qml.SWAP((0, new_wires[0]))
+            qml.deallocate(new_wires)
+
+            return qml.probs(wires=0)
+
+        print(qml.draw(c, level="user")())
+
+
+    >>> print(qml.draw(c, level="user")())
+                0: ──H────────╭●─╭●─────────────╭SWAP─────────────┤  Probs
+    <DynamicWire>: ──Allocate─╰X─╰X──Deallocate─│─────────────────┤
+    <DynamicWire>: ──Allocate───────────────────╰SWAP──Deallocate─┤
+    >>> print(qml.draw(c, level="device")())
+    0: ──H─╭●─╭●─╭SWAP─┤  Probs
+    1: ────╰X─╰X─╰SWAP─┤
+
+
+    Here two dynamic wires are allocated in the circuit originally. When we are determining
+    what concrete values to use for dynamic wires, we can see that the first dynamic wire is already
+    deallocated back into the zero state. This allows us to use it for the second allocation used in the ``SWAP``
+    gate as well.
+
+
+    """
+    op = Allocate.from_num_wires(num_wires, require_zeros=require_zeros)
+    return op.wires
+
+
+def deallocate(
+    wires: DynamicWire | Wires | Sequence[DynamicWire], reset_to_original: bool = False
+) -> Deallocate:
+    """Deallocate dynamic wires back to the pool of wires available for allocation.
+
+    .. warning::
+
+        This feature is experimental and may not be possible on all devices.
+
+
+    Args:
+        wires (DynamicWire, Wires, Sequence[DynamicWire]): One or more dynamic wires.
+
+    Keyword Args:
+        reset_to_original (bool): Whether or not the qubits will be reset to their original state upon deallocation.
+
+
+    .. see-also:: :class:`~.allocate_ctx`
+
+    :class:`~.allocate_ctx` is recommended as the preferred way to allocate wires, as it enforces automatic deallocation.
+    Manual use of ``allocate`` and ``deallocate`` should be used with more deliberate care.
+
+    ..code-block:: python
+
+        @qml.qnode(qml.device('default.qubit'))
+        def c():
+            qml.H(0)
+
+            wires = qml.allocate(1, require_zeros=True)
+            qml.CNOT((0, wires[0]))
+            qml.CNOT((0, wires[0]))
+            qml.deallocate(wires, reset_to_original=True)
+
+            new_wires = qml.allocate(1)
+            qml.SWAP((0, new_wires[0]))
+            qml.deallocate(new_wires)
+
+            return qml.probs(wires=0)
+
+        print(qml.draw(c, level="user")())
+
+
+    >>> print(qml.draw(c, level="user")())
+                0: ──H────────╭●─╭●─────────────╭SWAP─────────────┤  Probs
+    <DynamicWire>: ──Allocate─╰X─╰X──Deallocate─│─────────────────┤
+    <DynamicWire>: ──Allocate───────────────────╰SWAP──Deallocate─┤
+    >>> print(qml.draw(c, level="device")())
+    0: ──H─╭●─╭●─╭SWAP─┤  Probs
+    1: ────╰X─╰X─╰SWAP─┤
+
+
+    Here two dynamic wires are allocated in the circuit originally. When we are determining
+    what concrete values to use for dynamic wires, we can see that the first dynamic wire is already
+    deallocated back into the zero state. This allows us to use it for the second allocation used in the ``SWAP``
+    gate as well.
+
+    """
+    wires = Wires(wires)
+    if not_dynamic_wires := [w for w in wires if not isinstance(w, DynamicWire)]:
+        raise ValueError(f"deallocate can only accept DynamicWire wires. Got {not_dynamic_wires}")
+    return Deallocate(wires, reset_to_original=reset_to_original)
+
+
+class allocate_ctx:
+    """Temporarily allocate dynamic wires while making sure to automatically deallocate them at the end.
+
+    .. warning::
+
+        This feature is experimental and may not be possible on all devices.
+
+    Args:
+        num_wires (int): the number of dynamic wires to allocate.
+
+    Keyword Args:
+        require_zeros (bool): whether or not the wires must start in the ``0`` state
+        reset_to_original (bool): whether or not the wires return to the same state as they started.
+
+    .. code-block:: python
+
+        @qml.qnode(qml.device('default.qubit', wires=("a", "b")))
+        def c():
+            with qml.allocate_ctx(2, require_zeros=True, reset_to_original=False) as wires:
+                qml.CNOT(wires)
+            with qml.allocate_ctx(2, require_zeros=True, reset_to_original=False) as wires:
+                qml.IsingXX(0.5, wires)
+            return qml.probs()
+
+
+    >>> print(qml.draw(c, level="user")())
+    <DynamicWire>: ─╭Allocate─╭●─────────────╭Deallocate─┤  Probs
+    <DynamicWire>: ─╰Allocate─╰X─────────────╰Deallocate─┤  Probs
+    <DynamicWire>: ─╭Allocate─╭IsingXX(0.50)─╭Deallocate─┤  Probs
+    <DynamicWire>: ─╰Allocate─╰IsingXX(0.50)─╰Deallocate─┤  Probs
+    >>> print(qml.draw(c, level="device")())
+    a: ─╭●──┤↗│  │0⟩─╭IsingXX(0.50)─┤ ╭Probs
+    b: ─╰X──┤↗│  │0⟩─╰IsingXX(0.50)─┤ ╰Probs
+
+    The initial circuit has the ``DynamicWire``'s present, but when executing on the device, those are converted into
+    the device wires ``("a", "b")``. As the wires are not reset to their original state when deallocated, they are reset
+    before being re-used again in the second block.
+
+    """
+
+    def __init__(self, num_wires: int, require_zeros: bool = True, reset_to_original: bool = False):
+        self.wires = allocate(num_wires, require_zeros=require_zeros)
+        self._reset_to_original = reset_to_original
+
+    def __enter__(self):
+        return self.wires
+
+    def __exit__(self, *_, **__):
+        deallocate(self.wires, reset_to_original=self._reset_to_original)
---
+++ tests/test_allocation.py
@@ -0,0 +1,186 @@
+# Copyright 2018-2025 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Tests for the allocation module.
+"""
+import uuid
+
+import pytest
+
+import pennylane as qml
+from pennylane.allocation import (
+    Allocate,
+    Deallocate,
+    DynamicWire,
+    allocate,
+    allocate_ctx,
+    deallocate,
+)
+
+
+class TestDynamicWire:
+
+    def test_uuid_intialization(self):
+        """Test that a uuid is created upon initialization."""
+        w = DynamicWire()
+        assert isinstance(w.key, uuid.UUID)
+
+        w2 = DynamicWire(w.key)
+        assert w2.key == w.key
+
+    def test_dynamic_wire_repr(self):
+        """Test the repr of a Dynamic Wire"""
+        assert repr(DynamicWire()) == "<DynamicWire>"
+
+    def test_equality(self):
+        """Test that equality can be determined based on key."""
+
+        a = DynamicWire()
+        assert a != 2
+
+        b = DynamicWire()
+        assert a != b
+
+        c = DynamicWire(key=a.key)
+        assert a == c
+
+    def test_hash(self):
+        """Test that the hash of the dynamic wire depends on the uuid."""
+
+        a = DynamicWire()
+        b = DynamicWire()
+        c = DynamicWire(key=a.key)
+        assert len({a, b, c}) == 2
+        assert c in {a, b}
+        assert b not in {a}
+
+
+class TestAllocateOp:
+
+    def test_valid_operation(self):
+        """Test that Allocate is a valid Operator."""
+        op = Allocate.from_num_wires(3)
+        qml.ops.functions.assert_valid(op)
+
+    def test_allocate_from_num_wires(self):
+        """Test that the op can be instantiated with from_num_wires"""
+
+        op = Allocate.from_num_wires(3, require_zeros=False)
+        assert len(op.wires) == 3
+        assert op.hyperparameters == {"require_zeros": False}
+        assert not op.require_zeros
+
+    def test_normal_initialization(self):
+        """Test that the op can also be initialized with already created dynamic wires."""
+        wires = [DynamicWire() for _ in range(5)]
+        op = Allocate(wires, require_zeros=False)
+        assert op.wires == qml.wires.Wires(wires)
+        assert op.hyperparameters == {"require_zeros": False}
+        assert not op.require_zeros
+
+    def test_default_hyperparameters(self):
+        """Test the values of the default hyperparameters."""
+
+        op = Allocate.from_num_wires(2)
+        assert op.require_zeros
+
+        op2 = Allocate(DynamicWire())
+        assert op2.require_zeros
+
+
+def test_Deallocate_validity():
+    """Test that Deallocate is a valid operation."""
+    wires = [DynamicWire(), DynamicWire()]
+    op = Deallocate(wires, reset_to_original=True)
+    assert op.reset_to_original
+    assert op.hyperparameters == {"reset_to_original": True}
+    assert op.wires == qml.wires.Wires(wires)
+    qml.ops.functions.assert_valid(op)
+
+
+def test_allocate_function():
+    """Test that allocate returns dynamic wires and queues an Allocate op."""
+    with qml.queuing.AnnotatedQueue() as q:
+        wires = allocate(4)
+    assert isinstance(wires, qml.wires.Wires)
+    assert len(wires) == 4
+    assert all(isinstance(w, DynamicWire) for w in wires)
+
+    assert len(q) == 1
+    op = q.queue[0]
+    assert op.wires == wires
+    assert op.require_zeros
+
+
+def test_allocate_kwargs():
+    """Test that the kwargs to allocate get passed to the op."""
+
+    with qml.queuing.AnnotatedQueue() as q:
+        allocate(3, require_zeros=False)
+
+    op = q.queue[0]
+    assert not op.require_zeros
+
+
+class TestDeallocate:
+
+    def test_single_dynamic_wire(self):
+        """Test that deallocate can accept a single dynamic wire."""
+        wire = DynamicWire()
+        with qml.queuing.AnnotatedQueue() as q:
+            op = deallocate(wire)
+        assert op.wires == qml.wires.Wires((wire,))
+
+        assert len(q.queue) == 1
+        assert op is q.queue[0]
+
+    def test_error_non_dynamic_wire(self):
+        """Test that an error is raised if a non-dynamic wire is attempted to be deallocated."""
+        with pytest.raises(ValueError, match="only accept DynamicWire wires."):
+            deallocate((DynamicWire, 1))
+
+    def test_multiple_dynamic_wires(self):
+        """Test multiple dynamic wires can be deallocated."""
+
+        wires = [DynamicWire(), DynamicWire()]
+        with qml.queuing.AnnotatedQueue() as q:
+            op = deallocate(wires)
+        assert op.wires == qml.wires.Wires(wires)
+
+        assert len(q.queue) == 1
+        assert op is q.queue[0]
+
+    def test_reset_to_original(self):
+        """Test that reset_to_original is passed to the operator."""
+
+        op = deallocate(DynamicWire(), reset_to_original=True)
+        assert op.reset_to_original
+        assert op.hyperparameters == {"reset_to_original": True}
+
+
+def test_allocate_ctx():
+    """Test that allocate_ctx allocates and deallocates qubits."""
+
+    with qml.queuing.AnnotatedQueue() as q:
+        with allocate_ctx(3, require_zeros=False, reset_to_original=True) as wires:
+            assert len(wires) == 3
+            assert all(isinstance(w, DynamicWire) for w in wires)
+            assert len(set(wires)) == 3
+
+            qml.I(wires)
+
+    assert len(q.queue) == 3
+    qml.assert_equal(q.queue[0], Allocate(wires, require_zeros=False))
+    qml.assert_equal(q.queue[1], qml.I(wires))
+    qml.assert_equal(q.queue[2], Deallocate(wires, reset_to_original=True))
