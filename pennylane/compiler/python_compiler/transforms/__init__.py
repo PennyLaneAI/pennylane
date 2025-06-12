13490eab2ff20b231e6a0614680180fbf83682df
Mudit Pandey <mudit.pandey@xanadu.ai>
2025-06-12 14:14:25 -0400

    Update top level message in transforms init files

--- pennylane/compiler/python_compiler/transforms/__init__.py
+++ pennylane/compiler/python_compiler/transforms/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""PennyLane-xDSL transformations API."""
+"""PennyLane-xDSL transformations."""
 
 from .cancel_inverses import IterativeCancelInversesPass, iterative_cancel_inverses_pass
 from .merge_rotations import MergeRotationsPass, merge_rotations_pass
