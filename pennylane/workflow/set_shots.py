5fd3c18766bc3d2caedd162645c92bc935c1f3ff
Yushao Chen (Jerry) <chenys13@outlook.com>
2025-06-12 15:43:58 -0400

    Update pennylane/workflow/set_shots.py
    Co-authored-by: Christina Lee <christina@xanadu.ai>

--- pennylane/workflow/set_shots.py
+++ pennylane/workflow/set_shots.py
@@ -23,7 +23,6 @@ from .qnode import QNode
 
 def set_shots(
     qnode: QNode,
-    *,
     shots: Union[Shots, None, int, Sequence[Union[int, Tuple[int, int]]]] = None,
 ):
     """Transform used to set or update a circuit's shots.
