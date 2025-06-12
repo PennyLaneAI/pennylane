1168317cb27849da4479e9902cb76a040d9af033
Runor Agbaire <runor.agbaire@xanadu.ai>
2025-06-12 16:14:23 -0400

    moved future

--- doc/conf.py
+++ doc/conf.py
@@ -12,11 +12,12 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
+from __future__ import annotations
+
 import os
 import re
 import sys
 from datetime import datetime
-from __future__ import annotations
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
