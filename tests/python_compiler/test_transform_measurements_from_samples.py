53adc91588460cc72c32270c273ab735e539de4c
Joey Carter <joseph.carter@xanadu.ai>
2025-06-12 11:03:21 -0400

    Add tests

--- tests/python_compiler/test_transform_measurements_from_samples.py
+++ tests/python_compiler/test_transform_measurements_from_samples.py
@@ -18,9 +18,12 @@
 
 import io
 
+import numpy as np
 import pytest
 
 # pytestmark = pytest.mark.external
+import pennylane as qml
+from pennylane.compiler.python_compiler.transforms import MeasurementsFromSamplesPass
 
 xdsl = pytest.importorskip("xdsl")
 
@@ -28,8 +31,8 @@ xdsl = pytest.importorskip("xdsl")
 # a Python re-implementation of FileCheck
 filecheck = pytest.importorskip("filecheck")
 
-import pennylane as qml
-from pennylane.compiler.python_compiler.transforms import MeasurementsFromSamplesPass
+catalyst = pytest.importorskip("catalyst")
+from catalyst.passes import xdsl_plugin
 
 
 def test_transform():
@@ -60,31 +63,226 @@ def test_transform():
 
 
 class TestMeasurementsFromSamplesExecution:
-    """TODO"""
+    """Tests of the execution of simple workloads with the xDSL-based MeasurementsFromSamplesPass
+    transform.
+    """
 
-    def test_measurements_from_samples_basic(self):
-        """TODO"""
-        from catalyst.passes import xdsl_plugin
+    @pytest.mark.usefixtures("enable_disable_plxpr")
+    @pytest.mark.parametrize("shots", [1, 2])
+    @pytest.mark.parametrize(
+        "measurement_process, initial_op, expected_res",
+        [
+            (qml.expval, qml.I, 1.0),
+            (qml.expval, qml.X, -1.0),
+            (qml.var, qml.I, 0.0),
+            (qml.var, qml.X, 0.0),
+        ],
+    )
+    def test_1_wire_pauliZ_basis(self, shots, measurement_process, initial_op, expected_res):
+        """Test the measurements_from_samples transform on a device with a single wire and terminal
+        measurements in the PauliZ (computational) basis."""
 
-        qml.capture.enable()
-
-        dev = qml.device("lightning.qubit", wires=2, shots=10)
+        dev = qml.device("lightning.qubit", wires=1, shots=shots)
 
         @qml.qjit(pass_plugins=[xdsl_plugin.getXDSLPluginAbsolutePath()])
         @MeasurementsFromSamplesPass
         @qml.qnode(dev)
         def circuit():
-            qml.H(0)
-            return qml.expval(qml.Z(0)), qml.expval(qml.Z(1))
+            initial_op(wires=0)
+            return measurement_process(qml.Z(wires=0))
 
-        print("\n")
-        print(circuit())
-        print("\n")
-        print(circuit.mlir)
+        result = circuit()
+        assert result == expected_res
 
-        # TODO: Add asserts!
+    # -------------------------------------------------------------------------------------------- #
 
-        qml.capture.disable()
+    @pytest.mark.xfail(
+        reason="Have not yet implemented transform for probs",
+        strict=True,
+    )
+    @pytest.mark.usefixtures("enable_disable_plxpr")
+    @pytest.mark.parametrize("shots", [1, 2])
+    @pytest.mark.parametrize(
+        "measurement_process, initial_op, expected_res",
+        [
+            (qml.probs, qml.I, [1.0, 0.0]),
+            (qml.probs, qml.X, [0.0, 1.0]),
+        ],
+    )
+    def test_1_wire_pauliZ_basis_probs(self, shots, measurement_process, initial_op, expected_res):
+        """Test the measurements_from_samples transform on a device with a single wire and terminal
+        measurements in the PauliZ (computational) basis.
+
+        TODO: for probs
+        """
+
+        dev = qml.device("lightning.qubit", wires=1, shots=shots)
+
+        @qml.qjit(pass_plugins=[xdsl_plugin.getXDSLPluginAbsolutePath()])
+        @MeasurementsFromSamplesPass
+        @qml.qnode(dev)
+        def circuit():
+            initial_op(wires=0)
+            return measurement_process(wires=0)
+
+        result = circuit()
+        assert np.array_equal(result, expected_res)
+
+    # -------------------------------------------------------------------------------------------- #
+
+    @pytest.mark.xfail(
+        reason="Have currently only implemented support for computation-basis measurements",
+        strict=False,
+    )
+    @pytest.mark.usefixtures("enable_disable_plxpr")
+    @pytest.mark.parametrize("shots", [1, 2])
+    @pytest.mark.parametrize(
+        "measurement_process, initial_op, expected_res",
+        [
+            (qml.expval, qml.I, 1.0),
+            (qml.expval, qml.Z, -1.0),
+            (qml.var, qml.I, 0.0),
+            (qml.var, qml.Z, 0.0),
+        ],
+    )
+    def test_1_wire_pauliX_basis(self, shots, measurement_process, initial_op, expected_res):
+        """Test the measurements_from_samples transform on a device with a single wire and terminal
+        measurements in the PauliX basis.
+
+        A measurement process in the PauliX basis should trigger diagonalize_measurements.
+        """
+
+        dev = qml.device("lightning.qubit", wires=1, shots=shots)
+
+        @qml.qjit(pass_plugins=[xdsl_plugin.getXDSLPluginAbsolutePath()])
+        @MeasurementsFromSamplesPass
+        @qml.qnode(dev)
+        def circuit():
+            qml.H(wires=0)
+            initial_op(wires=0)
+            return measurement_process(qml.Z(wires=0))
+
+        result = circuit()
+        assert result == expected_res
+
+    # -------------------------------------------------------------------------------------------- #
+
+    @pytest.mark.xfail(
+        reason="Have currently only implemented support for computation-basis measurements",
+        strict=False,  # Allow xpass; there is some probability that we return the expected result
+    )
+    @pytest.mark.usefixtures("enable_disable_plxpr")
+    @pytest.mark.parametrize("shots", [1, 2])
+    @pytest.mark.parametrize(
+        "measurement_process, initial_op, expected_res",
+        [
+            (qml.expval, qml.I, 1.0),
+            (qml.expval, qml.Z, -1.0),
+            (qml.var, qml.I, 0.0),
+            (qml.var, qml.Z, 0.0),
+        ],
+    )
+    def test_1_wire_pauliY_basis(self, shots, measurement_process, initial_op, expected_res):
+        """Test the measurements_from_samples transform on a device with a single wire and terminal
+        measurements in the PauliY basis.
+
+        A measurement process in the PauliY basis should trigger diagonalize_measurements.
+        """
+
+        dev = qml.device("lightning.qubit", wires=1, shots=shots)
+
+        @qml.qjit(pass_plugins=[xdsl_plugin.getXDSLPluginAbsolutePath()])
+        @MeasurementsFromSamplesPass
+        @qml.qnode(dev)
+        def circuit():
+            qml.H(wires=0)
+            qml.S(wires=0)
+            initial_op(wires=0)
+            return measurement_process(qml.Z(wires=0))
+
+        result = circuit()
+        assert result == expected_res
+
+    # -------------------------------------------------------------------------------------------- #
+
+    @pytest.mark.usefixtures("enable_disable_plxpr")
+    @pytest.mark.parametrize("shots", [1, 2])
+    @pytest.mark.parametrize(
+        "measurement_process, initial_ops, expected_res",
+        [
+            (qml.expval, (qml.I, qml.I), (1.0, 1.0)),
+            (qml.expval, (qml.I, qml.X), (1.0, -1.0)),
+            (qml.expval, (qml.X, qml.I), (-1.0, 1.0)),
+            (qml.expval, (qml.X, qml.X), (-1.0, -1.0)),
+            (qml.var, (qml.I, qml.I), (0.0, 0.0)),
+            (qml.var, (qml.I, qml.X), (0.0, 0.0)),
+            (qml.var, (qml.X, qml.I), (0.0, 0.0)),
+            (qml.var, (qml.X, qml.X), (0.0, 0.0)),
+        ],
+    )
+    def test_2_wires_pauliZ_basis_separate(
+        self, shots, measurement_process, initial_ops, expected_res
+    ):
+        """Test the measurements_from_samples transform on a device with two wires and terminal
+        measurements in the PauliZ (computational) basis.
+
+        In this test, the terminal measurements are performed separately per wire.
+        """
+
+        dev = qml.device("lightning.qubit", wires=2, shots=shots)
+
+        @qml.qjit(pass_plugins=[xdsl_plugin.getXDSLPluginAbsolutePath()])
+        @MeasurementsFromSamplesPass
+        @qml.qnode(dev)
+        def circuit():
+            initial_ops[0](wires=0)
+            initial_ops[1](wires=1)
+            return measurement_process(qml.Z(wires=0)), measurement_process(qml.Z(wires=1))
+
+        result = circuit()
+        assert result == expected_res
+
+    # -------------------------------------------------------------------------------------------- #
+
+    @pytest.mark.xfail(
+        reason="Operator arithmetic not yet supported with capture enabled", strict=True
+    )
+    @pytest.mark.usefixtures("enable_disable_plxpr")
+    @pytest.mark.parametrize("shots", [1, 2])
+    @pytest.mark.parametrize(
+        "measurement_process, initial_ops, expected_res",
+        [
+            (qml.expval, (qml.I, qml.I), 1.0),
+            (qml.expval, (qml.I, qml.X), -1.0),
+            (qml.expval, (qml.X, qml.I), -1.0),
+            (qml.expval, (qml.X, qml.X), 1.0),
+            (qml.var, (qml.I, qml.I), 0.0),
+            (qml.var, (qml.I, qml.X), 0.0),
+            (qml.var, (qml.X, qml.I), 0.0),
+            (qml.var, (qml.X, qml.X), 0.0),
+        ],
+    )
+    def test_2_wires_pauliZ_basis_combined(
+        self, shots, measurement_process, initial_ops, expected_res
+    ):
+        """Test the measurements_from_samples transform on a device with two wires and terminal
+        measurements in the PauliZ (computational) basis.
+
+        In this test, the terminal measurements are performed on the combination of both wires.
+        """
+
+        dev = qml.device("lightning.qubit", wires=2, shots=shots)
+
+        @qml.qjit(pass_plugins=[xdsl_plugin.getXDSLPluginAbsolutePath()])
+        @MeasurementsFromSamplesPass
+        @qml.qnode(dev)
+        def circuit():
+            initial_ops[0](wires=0)
+            initial_ops[1](wires=1)
+            return measurement_process(qml.Z(wires=0) @ qml.Z(wires=1))
+
+        result = circuit()
+        assert result == expected_res
 
 
 if __name__ == "__main__":
