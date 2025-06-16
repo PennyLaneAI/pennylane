5367de9295c544aff596fbe513b91b0d0df4d3a5
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-06-16 10:13:23 -0400

    Format

--- tests/io/qasm_interpreter/test_interpreter.py
+++ tests/io/qasm_interpreter/test_interpreter.py
@@ -157,7 +157,9 @@ class TestSubroutine:
 
     def test_subroutines(self):
         # parse the QASM
-        ast = parse(open("tests/io/qasm_interpreter/subroutines.qasm", mode="r").read(), permissive=True)
+        ast = parse(
+            open("tests/io/qasm_interpreter/subroutines.qasm", mode="r").read(), permissive=True
+        )
 
         # run the program
         with queuing.AnnotatedQueue() as q:
@@ -423,7 +425,9 @@ class TestVariables:
 
     def test_variables(self):
         # parse the QASM
-        ast = parse(open("tests/io/qasm_interpreter/variables.qasm", mode="r").read(), permissive=True)
+        ast = parse(
+            open("tests/io/qasm_interpreter/variables.qasm", mode="r").read(), permissive=True
+        )
 
         # run the program
         context = QasmInterpreter().interpret(
@@ -492,7 +496,9 @@ class TestVariables:
 
     def test_classical_variables(self):
         # parse the QASM
-        ast = parse(open("tests/io/qasm_interpreter/classical.qasm", mode="r").read(), permissive=True)
+        ast = parse(
+            open("tests/io/qasm_interpreter/classical.qasm", mode="r").read(), permissive=True
+        )
 
         # run the program
         context = QasmInterpreter().interpret(ast, context={"wire_map": None, "name": "basic-vars"})
