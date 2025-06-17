c7e90b72dc30ce86ddb3c751e07abf66f858c0ad
Diksha Dhawan <ddhawan@umich.edu>
2025-06-17 17:36:50 -0400

    Added the functionality

---
+++ pennylane/labs/tests/resource_estimation/templates/test_trotter.py
@@ -0,0 +1,177 @@
+# Copyright 2025 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Test the Resource classes for Trotterization
+"""
+from collections import defaultdict
+
+import pytest
+
+import pennylane.labs.resource_estimation as plre
+from pennylane.labs.resource_estimation import QubitManager
+
+# Expected resources were obtained from the XAS notebook
+hamiltonian_data = [
+    (
+        8,
+        4,
+        100,
+        2,
+        {
+            "qubit_manager": QubitManager(
+                work_wires={"clean": 0, "dirty": 0}, algo_wires=16, tight_budget=False
+            ),
+            "gate_types": defaultdict(
+                int,
+                {
+                    "T": 3683512.0,
+                    "Adjoint(T)": 67312.0,
+                    "Hadamard": 134624.0,
+                    "S": 67312.0,
+                    "Adjoint(S)": 67312.0,
+                    "CNOT": 187312.0,
+                },
+            ),
+        },
+    ),
+    (
+        10,
+        5,
+        1000,
+        1,
+        {
+            "qubit_manager": QubitManager(
+                work_wires={"clean": 0, "dirty": 0}, algo_wires=20, tight_budget=False
+            ),
+            "gate_types": defaultdict(
+                int,
+                {
+                    "T": 47730000.0,
+                    "Adjoint(T)": 900000.0,
+                    "Hadamard": 1800000.0,
+                    "S": 900000.0,
+                    "Adjoint(S)": 900000.0,
+                    "CNOT": 2420000.0,
+                },
+            ),
+        },
+    ),
+    (
+        12,
+        8,
+        750,
+        4,
+        {
+            "qubit_manager": QubitManager(
+                work_wires={"clean": 0, "dirty": 0}, algo_wires=24, tight_budget=False
+            ),
+            "gate_types": defaultdict(
+                int,
+                {
+                    "T": 761364588.0,
+                    "Adjoint(T)": 13860264.0,
+                    "Hadamard": 27720528.0,
+                    "S": 13860264.0,
+                    "Adjoint(S)": 13860264.0,
+                    "CNOT": 40770264.0,
+                },
+            ),
+        },
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "num_orbitals, num_fragments, num_steps, order, expected_res", hamiltonian_data
+)
+def test_resource_trotter_cdf(num_orbitals, num_fragments, num_steps, order, expected_res):
+    """Test the ResourceTrotterCDF class for compressed double factorization"""
+    compact_ham = plre.CompactHamiltonian.from_cdf(
+        num_orbitals=num_orbitals, num_fragments=num_fragments
+    )
+
+    def circ():
+        plre.ResourceTrotterCDF(compact_ham, num_steps=num_steps, order=order)
+
+    res = plre.estimate_resources(circ)()
+
+    assert res.qubit_manager == expected_res["qubit_manager"]
+    assert res.clean_gate_counts == expected_res["gate_types"]
+
+
+# Expected resources were obtained manually
+hamiltonian_data = [
+    (
+        8,
+        20,
+        100,
+        2,
+        {
+            "qubit_manager": QubitManager(
+                work_wires={"clean": 0, "dirty": 0}, algo_wires=20, tight_budget=False
+            ),
+            "gate_types": defaultdict(
+                int,
+                {
+                    "T": 4627512.0,
+                    "Adjoint(T)": 87312.0,
+                    "Hadamard": 174624.0,
+                    "S": 87312.0,
+                    "Adjoint(S)": 87312.0,
+                    "CNOT": 243312.0,
+                },
+            ),
+        },
+    ),
+    (
+        10,
+        40,
+        1000,
+        1,
+        {
+            "qubit_manager": QubitManager(
+                work_wires={"clean": 0, "dirty": 0}, algo_wires=40, tight_budget=False
+            ),
+            "gate_types": defaultdict(
+                int,
+                {
+                    "T": 176130000.0,
+                    "Adjoint(T)": 3300000.0,
+                    "Hadamard": 6600000.0,
+                    "S": 3300000.0,
+                    "Adjoint(S)": 3300000.0,
+                    "CNOT": 9620000.0,
+                },
+            ),
+        },
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "num_orbitals, tensor_rank, num_steps, order, expected_res", hamiltonian_data
+)
+def test_resource_trotter_thc(num_orbitals, tensor_rank, num_steps, order, expected_res):
+    """Test the ResourceTrotterTHC class for tensor hypercontraction"""
+    compact_ham = plre.CompactHamiltonian.from_thc(
+        num_orbitals=num_orbitals, tensor_rank=tensor_rank
+    )
+
+    def circ():
+        plre.ResourceTrotterTHC(compact_ham, num_steps=num_steps, order=order)
+
+    res = plre.estimate_resources(circ)()
+
+    assert res.qubit_manager == expected_res["qubit_manager"]
+    assert res.clean_gate_counts == expected_res["gate_types"]
