0fa80a55f8e90ef9f3a807ce48f2a37ea0367a14
Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>
2025-06-17 14:39:21 -0400

    Update pennylane/devices/default_qubit.py

--- pennylane/devices/default_qubit.py
+++ pennylane/devices/default_qubit.py
@@ -881,8 +881,6 @@ class DefaultQubit(Device):
         Returns:
             bool: Whether or not a derivative can be calculated provided the given information
         """
-        if execution_config is None:
-            execution_config = ExecutionConfig()
         return self.supports_derivatives(execution_config, circuit)
 
     @debug_logger
