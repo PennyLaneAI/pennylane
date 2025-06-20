90adbc7a106ddb853880dbe0704c77be857b4b16
Yushao Chen (Jerry) <chenys13@outlook.com>
2025-06-20 13:47:52 -0400

    Update pennylane/workflow/construct_batch.py

--- pennylane/workflow/construct_batch.py
+++ pennylane/workflow/construct_batch.py
@@ -426,7 +426,7 @@ def construct_batch(
 
     """
     is_torch_layer = type(qnode).__name__ == "TorchLayer"
-    has_shots_param = "shots" in inspect.signature(qnode.func).parameters
+    has_shots_param = qnode._shots_override_device  # pylint:disable=protected-access
     default_shots = qnode._shots  # pylint:disable=protected-access
 
     user_program = qnode.transform_program
