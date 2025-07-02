b67a66bbcb87fc2242732f460c0355b36c53e3ed
Marcus Edwards <94946848+comp-phys-marc@users.noreply.github.com>
2025-07-02 10:25:03 -0400

    QASM 3.0 registers not yet supported (#7766)
    ------------------------------------------------------------------------------------------------------------
    
    **Context:** Qubit register declarations were treated as single qubit
    declarations in previous commits.
    
    **Description of the Change:** Raises an error when a qubit register
    declaration is encountered, saying registers are not yet supported.
    Until we are able to add support for registers in a coming PR.
    
    **Benefits:** Incorrect behaviour avoided.
    
    **Possible Drawbacks:** Just a temporary band-aid.
    
    **Related ShortCut Stories:** [sc-94195]
    
    ---------
    
    Co-authored-by: Christina Lee <christina@xanadu.ai>

--- tests/io/qasm_interpreter/test_interpreter.py
+++ tests/io/qasm_interpreter/test_interpreter.py
@@ -595,11 +595,27 @@ class TestVariables:
         ):
             QasmInterpreter().interpret(ast, context={"wire_map": None, "name": "mutate-error"})
 
+    def test_declare_register(self):
+        # parse the QASM
+        ast = parse(
+            """
+            qubit[2] q;
+            """,
+            permissive=True,
+        )
+
+        with pytest.raises(
+            TypeError,
+            match="Qubit registers are not yet supported, "
+            "please declare each qubit individually.",
+        ):
+            QasmInterpreter().interpret(ast, context={"wire_map": None, "name": "qubit-register"})
+
     def test_retrieve_wire(self):
         # parse the QASM
         ast = parse(
             """
-            qubit[0] q;
+            qubit q;
             let s = q;
             """,
             permissive=True,
@@ -1071,7 +1087,7 @@ class TestGates:
             """
             qubit q0;
             qubit q1;
-            qubit[1] q2;
+            qubit q2;
             ccx q0, q2, q1;
             cswap q1, q2, q0;
             """,
