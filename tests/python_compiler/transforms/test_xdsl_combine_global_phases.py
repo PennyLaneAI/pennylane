6fec3a90d39328b40377d56cfe7612b212f9f6f6
Shuli Shu <31480676+multiphaseCFD@users.noreply.github.com>
2025-07-04 12:54:01 -0400

    Update tests/python_compiler/transforms/test_xdsl_combine_global_phases.py
    Co-authored-by: Joey Carter <joseph.carter@xanadu.ai>

--- tests/python_compiler/transforms/test_xdsl_combine_global_phases.py
+++ tests/python_compiler/transforms/test_xdsl_combine_global_phases.py
@@ -30,7 +30,7 @@ class TestCombineGlobalPhasesPass:
     """Unit tests for CombineGlobalPhasesPass."""
 
     def test_combinable_ops_without_control_flow(self, run_filecheck):
-        """Test that combine global phases in a func without control flows."""
+        """Test that combines global phases in a func without control flow."""
         program = """
             func.func @test_func(%arg0: f64, %arg1: f64) {
                 // CHECK: [[q0:%.*]] = "test.op"() : () -> !quantum.bit
