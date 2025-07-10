7618bd1bb60058510161370c5171ce4052a6add1
andrijapau <andrija.paurevic@xanadu.ai>
2025-07-10 16:35:37 -0400

    [circular dependencies] enforce pulse module as auxiliary

--- pennylane/pulse/parametrized_hamiltonian_pytree.py
+++ pennylane/pulse/parametrized_hamiltonian_pytree.py
@@ -21,7 +21,7 @@ import jax.numpy as jnp
 from jax.experimental import sparse
 from jax.tree_util import register_pytree_node_class
 
-import pennylane as qml
+from pennylane.ops.functions.matrix import matrix
 
 from .hardware_hamiltonian import HardwareHamiltonian
 from .parametrized_hamiltonian import ParametrizedHamiltonian
@@ -53,12 +53,12 @@ class ParametrizedHamiltonianPytree:
         make_array = jnp.array if dense else sparse.BCSR.fromdense
 
         if len(H.ops_fixed) > 0:
-            mat_fixed = make_array(qml.matrix(H.H_fixed(), wire_order=wire_order))
+            mat_fixed = make_array(matrix(H.H_fixed(), wire_order=wire_order))
         else:
             mat_fixed = None
 
         mats_parametrized = tuple(
-            make_array(qml.matrix(op, wire_order=wire_order)) for op in H.ops_parametrized
+            make_array(matrix(op, wire_order=wire_order)) for op in H.ops_parametrized
         )
 
         if isinstance(H, HardwareHamiltonian):
