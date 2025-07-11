28e6eb77e134121bb422b1f117680b0dcddf2d31
andrijapau <andrija.paurevic@xanadu.ai>
2025-07-11 15:14:20 -0400

    fix qscript.py manually

--- pennylane/tape/qscript.py
+++ pennylane/tape/qscript.py
@@ -22,7 +22,7 @@ import copy
 from collections import Counter
 from collections.abc import Callable, Hashable, Iterable, Iterator, Sequence
 from functools import cached_property
-from typing import Any, Optional, ParamSpec, TypeVar, Union
+from typing import Any, Optional, ParamSpec, TypeVar
 
 import pennylane as qml
 from pennylane.measurements import MeasurementProcess
@@ -788,7 +788,7 @@ class QuantumScript:
     # ========================================================
 
     def shape(
-        self, device: Union["qml.devices.Device", "qml.devices.LegacyDevice"]
+        self, device: "qml.devices.Device" | "qml.devices.LegacyDevice"
     ) -> tuple[int, ...] | tuple[tuple[int, ...], ...]:
         """Produces the output shape of the quantum script by inspecting its measurements
         and the device used for execution.
@@ -1427,7 +1427,7 @@ def make_qscript(fn: Callable[P, T], shots: Optional[ShotsLike] = None) -> Calla
 
 
 QuantumScriptBatch = Sequence[QuantumScript]
-QuantumScriptOrBatch = Union[QuantumScript, QuantumScriptBatch]
+QuantumScriptOrBatch = QuantumScript | QuantumScriptBatch
 
 register_pytree(QuantumScript, QuantumScript._flatten, QuantumScript._unflatten)
 
