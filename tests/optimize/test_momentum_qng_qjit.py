013928c91b81fdebf655050a04cbaab89ea27466
SimoneGasperini <simone.gasperini4@unibo.it>
2025-07-11 16:08:53 -0400

    Add more tests

--- tests/optimize/test_momentum_qng_qjit.py
+++ tests/optimize/test_momentum_qng_qjit.py
@@ -14,6 +14,7 @@
 """Test Jax-based Catalyst-compatible Momentum-QNG optimizer"""
 
 import numpy as np
+import pytest
 
 import pennylane as qml
 
@@ -48,3 +49,44 @@ class TestBasics:
         params = np.array([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]])
         state = opt.init(params)
         assert np.all(state == np.zeros_like(params))
+
+
+class TestOptimize:
+    """Test basic optimization integration."""
+
+    @pytest.mark.jax
+    @pytest.mark.parametrize("dev_name", dev_names)
+    @pytest.mark.parametrize("rho", [0.9, 0.0])
+    def test_step_and_cost(self, dev_name, rho):
+        """Test that the step and step_and_cost methods are returning
+        the correct result for a few optimization steps."""
+        import jax.numpy as jnp
+
+        @qml.qnode(qml.device(dev_name))
+        def circ(params):
+            qml.RX(params[0], wires=0)
+            qml.RY(params[1], wires=0)
+            return qml.expval(qml.PauliZ(0))
+
+        params = [0.11, 0.412]
+        params_qml = qml.numpy.array(params)
+        params_jax = jnp.array(params)
+        stepsize = 0.01
+
+        opt = qml.MomentumQNGOptimizerQJIT(stepsize=stepsize, momentum=rho)
+        state = opt.init(params_jax)
+
+        for _ in range(4):
+            new_params1, state1 = opt.step(circ, params_jax, state)
+            new_params2, state2, cost = opt.step_and_cost(circ, params_jax, state)
+
+            expected_mt = np.array([0.25, (np.cos(params[0]) ** 2) / 4])
+            state = rho * state + stepsize * qml.grad(circ)(params_qml) / expected_mt
+            expected_params = params_qml - state
+            expected_cost = circ(params)
+
+            assert np.allclose(new_params1, expected_params)
+            assert np.allclose(new_params2, expected_params)
+            assert np.allclose(state1, state)
+            assert np.allclose(state2, state)
+            assert np.allclose(cost, expected_cost)
