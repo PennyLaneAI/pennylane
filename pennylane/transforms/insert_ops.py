c5a564f63d9b681634c4f499dd4453e3d659e456
andrijapau <andrija.paurevic@xanadu.ai>
2025-07-11 18:57:45 -0400

    fix insert_ops.py

--- pennylane/transforms/insert_ops.py
+++ pennylane/transforms/insert_ops.py
@@ -53,9 +53,9 @@ def _check_position(position):
 @transform
 def insert(
     tape: QuantumScript,
-    op: callable | type[Operation],
+    op: callable | Operation,
     op_args: tuple | float,
-    position: str | list | type[Operation] = "all",
+    position: str | list | Operation = "all",
     before: bool = False,
 ) -> tuple[QuantumScriptBatch, PostprocessingFn]:
     """Insert an operation into specified points in an input circuit.
