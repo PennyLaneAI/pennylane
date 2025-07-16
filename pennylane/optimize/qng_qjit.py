d786d2d1e7ae010b876bfd727b4484831bdd5502
SimoneGasperini <simone.gasperini4@unibo.it>
2025-07-16 10:39:08 -0400

    Revert docs fix for QNGOptimizerQJIT

--- pennylane/optimize/qng_qjit.py
+++ pennylane/optimize/qng_qjit.py
@@ -44,21 +44,6 @@ class QNGOptimizerQJIT:
             - ``QNGOptimizerQJIT`` does not work correctly if there is any classical processing in the QNode circuit
               (e.g., ``2 * theta`` as a gate parameter).
 
-    Keyword Args:
-        stepsize=0.01 (float): the user-defined stepsize hyperparameter
-        approx="block-diag" (str): approximation method for the metric tensor.
-
-            - If ``None``, the full metric tensor is computed
-
-            - If ``"block-diag"``, the block-diagonal approximation is computed, reducing
-              the number of evaluated circuits significantly
-
-            - If ``"diag"``, the diagonal approximation is computed, slightly
-              reducing the classical overhead but not the quantum resources
-              (compared to ``"block-diag"``)
-
-        lam=0 (float): metric tensor regularization to be applied at each optimization step
-
     **Example:**
 
     Consider a hybrid workflow to optimize an objective function defined by a quantum circuit.
@@ -125,6 +110,21 @@ class QNGOptimizerQJIT:
 
     >>> params
     Array([ 3.14159265, -1.57079633], dtype=float64)
+
+    Keyword Args:
+        stepsize=0.01 (float): the user-defined stepsize hyperparameter
+        approx="block-diag" (str): approximation method for the metric tensor.
+
+            - If ``None``, the full metric tensor is computed
+
+            - If ``"block-diag"``, the block-diagonal approximation is computed, reducing
+              the number of evaluated circuits significantly
+
+            - If ``"diag"``, the diagonal approximation is computed, slightly
+              reducing the classical overhead but not the quantum resources
+              (compared to ``"block-diag"``)
+
+        lam=0 (float): metric tensor regularization to be applied at each optimization step
     """
 
     def __init__(self, stepsize=0.01, approx="block-diag", lam=0):
