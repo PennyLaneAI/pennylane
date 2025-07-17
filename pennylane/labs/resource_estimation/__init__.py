f754b5fb68445149fbdf400e22b422cf25c19a49
Diksha Dhawan <ddhawan@umich.edu>
2025-07-17 12:05:47 -0400

    doc fix

--- pennylane/labs/resource_estimation/__init__.py
+++ pennylane/labs/resource_estimation/__init__.py
@@ -136,6 +136,8 @@ Templates:
     ~ResourceTrotterVibrational
     ~ResourceTrotterVibronic
     ~ResourceQubitizeTHC
+    ~ResourceUniformStatePrep
+    ~ResourceAliasSampling
 
 Compact Hamiltonian Class:
 ~~~~~~~~~~~~~~~~~~~~~~~~~~
