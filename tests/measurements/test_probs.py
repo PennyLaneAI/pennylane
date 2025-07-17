dc486c642fc45358e393b3a6a357c1def145239f
JerryChen97 <chenys13@outlook.com>
2025-07-17 17:21:39 -0400

    salt it

--- tests/measurements/test_probs.py
+++ tests/measurements/test_probs.py
@@ -336,7 +336,7 @@ class TestProbs:
         expected = np.array([0.5, 0.5, 0, 0])
         assert np.allclose(res, expected, atol=tol, rtol=0)
 
-    @pytest.mark.skip
+    @pytest.mark.local_salt(2)
     @pytest.mark.jax
     @pytest.mark.parametrize("shots", (None, 500))
     @pytest.mark.parametrize("obs", ([0, 1], qml.PauliZ(0) @ qml.PauliZ(1)))
