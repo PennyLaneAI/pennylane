4bbd7016b0ff88679f0a0a75ffce32fd1071aa9a
Mehrdad Malekmohammadi <mehrdad.malek@xanadu.ai>
2025-07-18 08:50:32 -0400

    correct directory names

--- tests/python_compiler/dialects/test_quantum_dialect.py
+++ tests/python_compiler/dialects/test_quantum_dialect.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Unit test module for pennylane/compiler/python_compiler/quantum_dialect.py."""
+"""Unit test module for pennylane/compiler/python_compiler/dialects/quantum.py."""
 
 import io
 
