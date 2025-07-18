163b116a78ddd8ea7f55d6a7057147646aa03ad9
Will <wmaxwell90@gmail.com>
2025-07-18 15:28:00 -0400

    small edits

--- pennylane/labs/trotter_error/parallel/compute_norms.py
+++ pennylane/labs/trotter_error/parallel/compute_norms.py
@@ -21,8 +21,7 @@ from vibronic_norm import _next_pow_2, _block_norm, chunkify, _get_eigenvalue_ba
 
 # FILE, GRIDPOINTS, MODES
 jobs = [
-    # ("VCHLIB/maleimide_5s_24m.pkl", 4, 6),
-    ("no4a_sf.pkl", 4, 16),
+    ("no4a_monomer.pkl", 4, 19),
 ]
 
 if __name__ == "__main__":
@@ -33,69 +32,66 @@ if __name__ == "__main__":
 
     start = MPI.Wtime()
 
-    if rank == 0:
+    #if rank == 0:
 
-        for file, gridpoints, modes in jobs:
-            path = f"hamiltonians/{file}"
-            with open(path, "rb") as f:
-                freqs, taylor_coeffs = pickle.load(f)
-    
-            err = build_error_term(freqs, taylor_coeffs, modes)
-            numbers = list(i for i in product(range(err.states), repeat=2))
+    for file, gridpoints, modes in jobs:
+        path = f"hamiltonians/{file}"
+        with open(path, "rb") as f:
+            freqs, taylor_coeffs = pickle.load(f)
 
-    else:
-        err, numbers, gridpoints, modes = None, None, None, None
+        err = build_error_term(freqs, taylor_coeffs, modes)
+        numbers = list(i for i in product(range(err.states), repeat=2))
 
-    rs_mat = comm.bcast(err, root=0)
-    numbers = comm.bcast(numbers, root=0)
-    gridpoints = comm.bcast(gridpoints, root=0)
-        
+    #else:
+    #    err, numbers, gridpoints, modes = None, None, None, None
+
+
 # --------------------------------------------------------------------------
 
-    padded = RealspaceMatrix(_next_pow_2(rs_mat.states), rs_mat.modes, rs_mat._blocks)
-    norms = np.zeros(shape=(padded.states, padded.states))
-   
-    if rank == 0:
+        padded = RealspaceMatrix(_next_pow_2(err.states), err.modes, err._blocks)
+        norms = np.zeros(shape=(padded.states, padded.states))
 
-        all_matrices = [_block_norm(padded.block(i, j), gridpoints) for i, j in numbers]
-        list_lens = [len(m) for m in all_matrices]
-        flat_matrices = [vals for m in all_matrices for vals in m.values()]
-        batches = chunkify(flat_matrices, size)
-        
-    else:
-        batches = None
+        if rank == 0:
 
-    batch = comm.scatter(batches, root=0)
+            all_matrices = [_block_norm(padded.block(i, j), gridpoints) for i, j in numbers]
+            list_lens = [len(m) for m in all_matrices]
+            flat_matrices = [vals for m in all_matrices for vals in m.values()]
+            batches = chunkify(flat_matrices, size)
 
-    print(rank, size, len(batch))
- 
-    local_result = _get_eigenvalue_batch(batch, gridpoints)
+        else:
+            batches = None
 
-    all_results = comm.gather(local_result, root=0)
+        batch = comm.scatter(batches, root=0)
 
-    comm.Barrier()
+        print(rank, size, len(batch))
 
-    if rank == 0:
+        local_result = _get_eigenvalue_batch(batch, gridpoints)
 
-        all_eigvals = np.concatenate(all_results)
+        all_results = comm.gather(local_result, root=0)
 
-        grouped_eigvals = zip([0] + list(itertools.accumulate(list_lens))[: -1], list_lens)
-        
-        grouped_sums = [sum(all_eigvals[i: i + n]) for i, n in grouped_eigvals]
-    
-        norms[tuple(zip(*numbers))] = grouped_sums
+        comm.Barrier()
 
-        norm = _compute_norm(norms)
+        if rank == 0:
 
-        end = MPI.Wtime()
+            all_eigvals = np.concatenate(all_results)
 
-        print(f"Norm: {norm} | Time: {end - start:.4f}")
+            grouped_eigvals = zip([0] + list(itertools.accumulate(list_lens))[: -1], list_lens)
 
-        with open("output.csv", "a+") as output:
-            csv_writer = csv.writer(output)
-            csv_writer.writerow((file, gridpoints, modes, norm))
+            grouped_sums = [sum(all_eigvals[i: i + n]) for i, n in grouped_eigvals]
 
-        path = f"norms/{file}_g{gridpoints}_m{modes}.pkl"
-        os.makedirs("norms", exist_ok=True)
-        with open(path, "wb+") as f:
-            pickle.dump(norms[: err.states, : err.states], f)
+            norms[tuple(zip(*numbers))] = grouped_sums
+
+            norm = _compute_norm(norms)
+
+            end = MPI.Wtime()
+
+            print(f"Norm: {norm} | Time: {end - start:.4f}")
+
+            with open("output.csv", "a+") as output:
+                csv_writer = csv.writer(output)
+                csv_writer.writerow((file, gridpoints, modes, norm))
+
+            path = f"norms/{file.replace('.pkl', '')}_g{gridpoints}_m{modes}.pkl"
+            os.makedirs("norms", exist_ok=True)
+            with open(path, "wb+") as f:
+                pickle.dump(norms[: err.states, : err.states], f)
