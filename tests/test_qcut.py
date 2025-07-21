b352122ffc2396a2578ed4f05cfc7d3901a4dfa1
JerryChen97 <chenys13@outlook.com>
2025-07-21 18:22:11 -0400

    qcut leftover

--- tests/test_qcut.py
+++ tests/test_qcut.py
@@ -2620,7 +2620,9 @@ class TestCutCircuitMCTransform:
         v = 0.319
 
         temp_shots = 333
-        cut_res = qml.set_shots(shots=temp_shots)(cut_circuit)(v)  # pylint: disable=unexpected-keyword-arg
+        cut_res = qml.set_shots(shots=temp_shots)(cut_circuit)(
+            v
+        )  # pylint: disable=unexpected-keyword-arg
 
         assert cut_res.shape == (temp_shots, 2)
 
