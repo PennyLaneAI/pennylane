30270098986d71bb04a82e97641f9190a94e009f
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-07-21 15:31:35 -0400

    Format

--- pennylane/io/qasm_interpreter.py
+++ pennylane/io/qasm_interpreter.py
@@ -539,7 +539,9 @@ class QasmInterpreter:
                     # not good if we're building a qscript for a controlled branch
                     raise e
                 # we are in the construction of the qscript for a controlled branch
-                raise NotImplementedError("End statements in measurement conditioned branches are not supported.") from e
+                raise NotImplementedError(
+                    "End statements in measurement conditioned branches are not supported."
+                ) from e
 
     def interpret(self, node: QASMNode, context: dict, **inputs):
         """
