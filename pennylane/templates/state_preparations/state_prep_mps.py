83c0a670cde90fda60d8d4ff7728c0644e52ef49
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-07-23 13:57:43 -0400

    Visual simplificaiton of logical expression

--- pennylane/templates/state_preparations/state_prep_mps.py
+++ pennylane/templates/state_preparations/state_prep_mps.py
@@ -521,9 +521,7 @@ def _work_wires_bond_dimension_condition(
 ):  # pylint: disable=unused-argument
     max_bond_dimension = max(bond_dimensions[:-1])
 
-    return (max_bond_dimension <= 2**num_work_wires) and not (
-        num_work_wires is None and num_work_wires == 0
-    )
+    return num_work_wires is not None and num_work_wires > 0 and 2**num_work_wires >= max_bond_dimension
 
 
 @qml.register_condition(_work_wires_bond_dimension_condition)
