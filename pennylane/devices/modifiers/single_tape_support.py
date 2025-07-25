38daf795bff1b435d3338e0a52f728cbd9cb4baa
Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>
2025-07-25 16:27:15 -0400

    Update pennylane/devices/modifiers/single_tape_support.py

--- pennylane/devices/modifiers/single_tape_support.py
+++ pennylane/devices/modifiers/single_tape_support.py
@@ -91,8 +91,6 @@ def _make_execute_and_compute_jvp(batch_execute_and_compute_jvp):
     def execute_and_compute_jvp(
         self, circuits, tangents, execution_config: ExecutionConfig | None = None
     ):
-        if execution_config is None:
-            execution_config = ExecutionConfig()
         is_single_circuit = False
         if isinstance(circuits, QuantumScript):
             is_single_circuit = True
