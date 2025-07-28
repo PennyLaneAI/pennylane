b84970b6f30242207cd29e252b4fccd664ab4963
PabloAMC <paulmorecasa@gmail.com>
2025-07-28 19:22:26 +0200

    Sparse fragments

--- pennylane/labs/trotter_error/__init__.py
+++ pennylane/labs/trotter_error/__init__.py
@@ -94,7 +94,7 @@ Classes for representing product formulas.
 """
 
 from .abstract import AbstractState, Fragment
-from .fragments import GenericFragment, generic_fragments, vibrational_fragments, vibronic_fragments
+from .fragments import GenericFragment, generic_fragments, vibrational_fragments, vibronic_fragments, sparse_fragments
 from .product_formulas import (
     ProductFormula,
     bch_expansion,
