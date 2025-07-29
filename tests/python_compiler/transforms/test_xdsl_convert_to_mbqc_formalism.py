22d2eddab395e22585912c02774d33be8afcf52a
Shuli Shu <08cnbj@gmail.com>
2025-07-29 20:25:11 +0000

    add unit test for S gate

--- tests/python_compiler/transforms/test_xdsl_convert_to_mbqc_formalism.py
+++ tests/python_compiler/transforms/test_xdsl_convert_to_mbqc_formalism.py
@@ -96,3 +96,69 @@ class TestConvertToMBQCFormalismPass:
 
         pipeline = (ConvertToMBQCFormalismPass(),)
         run_filecheck(program, pipeline)
+
+    def test_s_gate(self, run_filecheck):
+        """Test for lowering a S gate to a MBQC formalism."""
+        program = """
+            func.func @test_func() {
+                // CHECK: [[q0:%.+]] = "test.op"() : () -> !quantum.bit
+                %0 = "test.op"() : () -> !quantum.bit
+                // CHECK: [[qb1:%.+]] = quantum.alloc_qb : !quantum.bit
+                // CHECK: [[qb2:%.+]] = quantum.alloc_qb : !quantum.bit
+                // CHECK: [[qb3:%.+]] = quantum.alloc_qb : !quantum.bit
+                // CHECK: [[qb4:%.+]] = quantum.alloc_qb : !quantum.bit
+                // CHECK: [[qb1:%.+]] = quantum.custom "Hadamard"() [[qb1:%.+]] : !quantum.bit
+                // CHECK: [[qb2:%.+]] = quantum.custom "Hadamard"() [[qb2:%.+]] : !quantum.bit
+                // CHECK: [[qb3:%.+]] = quantum.custom "Hadamard"() [[qb3:%.+]] : !quantum.bit
+                // CHECK: [[qb4:%.+]] = quantum.custom "Hadamard"() [[qb4:%.+]] : !quantum.bit
+                // CHECK: [[qb1:%.+]], [[qb2:%.+]] = quantum.custom "CZ"() [[qb1:%.+]], [[qb2:%.+]] : !quantum.bit, !quantum.bit
+                // CHECK: [[qb2:%.+]], [[qb3:%.+]] = quantum.custom "CZ"() [[qb2:%.+]], [[qb3:%.+]] : !quantum.bit, !quantum.bit
+                // CHECK: [[qb3:%.+]], [[qb4:%.+]] = quantum.custom "CZ"() [[qb3:%.+]], [[qb4:%.+]] : !quantum.bit, !quantum.bit
+                // CHECK: [[q0:%.+]], [[qb1:%.+]] = quantum.custom "CZ"() [[q0:%.+]], [[qb1:%.+]] : !quantum.bit, !quantum.bit
+                // CHECK: [[cst_zero:%.+]] = arith.constant {{0.+}} : f64
+                // CHECK: [[m1:%.+]], [[q0:%.+]] = mbqc.measure_in_basis[XY, [[q0:%.+]]] [[cst_zero:%.+]] : i1, !quantum.bit
+                // CHECK: [[half_pi:%.+]] = arith.constant {{0.+}} : f64
+                // CHECK: [[m2:%.+]], [[qb1:%.+]] = mbqc.measure_in_basis[XY, [[qb1:%.+]]] [[half_pi:%.+]] : i1, !quantum.bit
+                // CHECK: [[half_pi:%.+]] = arith.constant {{1.+}} : f64
+                // CHECK: [[m3:%.+]], [[qb2:%.+]] = mbqc.measure_in_basis[XY, [[qb2:%.+]]] [[half_pi:%.+]] : i1, !quantum.bit
+                // CHECK: [[half_pi:%.+]] = arith.constant {{0.+}} : f64
+                // CHECK: [[m4:%.+]], [[qb3:%.+]] = mbqc.measure_in_basis[XY, [[qb3:%.+]]] [[half_pi:%.+]] : i1, !quantum.bit
+                // CHECK: [[m24:%.+]] = arith.addi [[m2:%.+]], [[m4:%.+]] : i1
+                // CHECK: [[cst_ture:%.+]] = arith.constant true
+                // CHECK: [[x_idx:%.+]] = arith.xori [[m24:%.+]], [[cst_ture:%.+]] : i1
+                // CHECK: [[cst_ture:%.+]] = arith.constant true
+                // CHECK: [[cmp_res:%.+]] = arith.cmpi eq, [[x_idx:%.+]], [[cst_ture:%.+]] : i1
+                // CHECK: [[qb4_x_res:%.+]] = scf.if [[cmp_res:%.+]] -> (!quantum.bit) {
+                // CHECK: [[qb4:%.+]] = quantum.custom "PauliX"() [[qb4:%.+]] : !quantum.bit
+                // CHECK: scf.yield [[qb4:%.+]] : !quantum.bit
+                // CHECK: } else {
+                // CHECK: [[qb4:%.+]] = quantum.custom "Identity"() [[qb4:%.+]] : !quantum.bit
+                // CHECK: scf.yield [[qb4:%.+]] : !quantum.bit
+                // CHECK: }
+                // CHECK: [[m12:%.+]] = arith.addi [[m1:%.+]], [[m2:%.+]] : i1
+                // CHECK: [[m123:%.+]] = arith.addi [[m12:%.+]], [[m3:%.+]] : i1
+                // CHECK: [[cst_ture:%.+]] = arith.constant true
+                // CHECK: [[m1231:%.+]] = arith.addi [[m123:%.+]], [[cst_ture:%.+]] : i1
+                // CHECK: [[z_idx:%.+]] = arith.xori [[m1231:%.+]], [[cst_ture:%.+]] : i1
+                // CHECK: [[cst_ture:%.+]] = arith.constant true
+                // CHECK: [[cmp_res:%.+]] = arith.cmpi eq, [[z_idx:%.+]], [[cst_ture:%.+]] : i1
+                // CHECK: [[qb4_res:%.+]] = scf.if [[cmp_res:%.+]] -> (!quantum.bit) {
+                // CHECK: [[qb4_x_res:%.+]] = quantum.custom "PauliZ"() [[qb4_x_res:%.+]] : !quantum.bit
+                // CHECK: scf.yield [[qb4_x_res:%.+]] : !quantum.bit
+                // CHECK: } else {
+                // CHECK: [[qb4_x_res:%.+]] = quantum.custom "Identity"() [[qb4_x_res:%.+]] : !quantum.bit
+                // CHECK: scf.yield [[qb4_x_res:%.+]] : !quantum.bit
+                // CHECK: }
+                // CHECK: [[qb4_res:%.+]] = quantum.custom "Identity"() [[qb4_res:%.+]] : !quantum.bit
+                // CHECK: [[q0_res:%.+]], [[qb4:%.+]] = quantum.custom "SWAP"() [[q0:%.+]], [[qb4_res:%.+]] : !quantum.bit, !quantum.bit
+                // CHECK: quantum.dealloc_qb [[qb1:%.+]] : !quantum.bit
+                // CHECK: quantum.dealloc_qb [[qb2:%.+]] : !quantum.bit
+                // CHECK: quantum.dealloc_qb [[qb3:%.+]] : !quantum.bit
+                // CHECK: quantum.dealloc_qb [[qb4:%.+]] : !quantum.bit
+                %1 = quantum.custom "S"() %0 : !quantum.bit
+                return
+            }
+        """
+
+        pipeline = (ConvertToMBQCFormalismPass(),)
+        run_filecheck(program, pipeline)
