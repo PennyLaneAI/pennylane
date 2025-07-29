7c6d253bd98e1a9fcdc0030de5e2aada5b87c1fc
Mudit Pandey <mudit.pandey@xanadu.ai>
2025-07-29 16:40:53 -0400

    Add qubit state management class; add method to insert constant op

--- pennylane/compiler/python_compiler/transforms/api/pattern_rewriter.py
+++ pennylane/compiler/python_compiler/transforms/api/pattern_rewriter.py
@@ -13,11 +13,66 @@
 # limitations under the License.
 """Pattern rewriter API for quantum compilation passes."""
 
-from xdsl.ir import Operation
+from collections.abc import Sequence
+from numbers import Number
+
+from xdsl.dialects import arith, builtin, tensor
+from xdsl.ir import Operation, SSAValue
 from xdsl.pattern_rewriter import PatternRewriter, PatternRewriterListener, PatternRewriteWalker
 from xdsl.rewriter import InsertPoint
 
 from ...dialects import quantum
+
+
+class StateManagement:
+    """A container class for managing state."""
+
+    wire_to_qubit_map: dict[int, SSAValue]
+    qubit_to_wire_map: dict[SSAValue, int]
+
+    def __init__(self, wires: Sequence[int]):
+        self._wires = tuple(wires)
+        self.wire_to_qubit_map = {}
+        self.qubit_to_wire_map = {}
+
+    @property
+    def wires(self) -> tuple[int]:
+        """Wire labels."""
+        return self._wires
+
+    def update_qubit(self, old_qubit: SSAValue, new_qubit: SSAValue):
+        """Update a qubit."""
+        wire = self.qubit_to_wire_map[old_qubit]
+        self.wire_to_qubit_map[wire] = new_qubit
+        self.qubit_to_wire_map[new_qubit] = wire
+        self.qubit_to_wire_map.pop(old_qubit, None)
+
+    def __getitem__(self, val: int | SSAValue) -> int | SSAValue:
+        if isinstance(val, SSAValue):
+            return self.qubit_to_wire_map[val]
+        return self.wire_to_qubit_map[val]
+
+    def __setitem__(self, key: int | SSAValue, item: SSAValue | int):
+        if isinstance(key, SSAValue):
+            old_wire = self.qubit_to_wire_map.pop(key, None)
+            self.wire_to_qubit_map.pop(old_wire, None)
+            self.qubit_to_wire_map[key] = item
+            self.wire_to_qubit_map[item] = key
+        else:
+            old_qubit = self.wire_to_qubit_map.pop(key, None)
+            self.qubit_to_wire_map.pop(old_qubit, None)
+            self.wire_to_qubit_map[key] = item
+            self.qubit_to_wire_map[item] = key
+
+    def get_static_wire(self, op: quantum.ExtractOp, update=True) -> int | None:
+        """Get the wire label to which a qubit extraction corresponds."""
+        if (idx_attr := getattr(op, "idx_attr", None)) is not None:
+            wire = idx_attr.value.data
+            if update:
+                self[wire] = op.qubit
+            return wire
+
+        return None
 
 
 class PLPatternRewriter(PatternRewriter):
@@ -51,6 +106,36 @@ class PLPatternRewriter(PatternRewriter):
             self.replace_all_uses_with(ocq, icq)
 
         self.erase_op(op)
+
+    def create_constant_op(self, cst: Number, insert_point: InsertPoint = None):
+        """Create a constant and insert it into the IR. The corresponding SSA value is returned."""
+        data = [cst]
+        if isinstance(cst, float):
+            elem_type = builtin.Float64Type()
+        elif isinstance(cst, complex):
+            elem_type = builtin.ComplexType()
+        else:
+            elem_type = builtin.IntegerType(64)
+
+        type_ = builtin.TensorType(elem_type, [1])
+        constAttr = builtin.DenseIntOrFPElementsAttr.from_list(type_, data)
+        constantOp = arith.ConstantOp(constAttr)
+        indexOp = arith.ConstantOp.from_int_and_width(0, 64)
+        extractOp = tensor.ExtractOp(
+            tensor=constantOp.result, indices=indexOp.result, result_type=elem_type
+        )
+
+        if insert_point is None:
+            self.insert_op_before_matched_op(constantOp)
+            self.insert_op_before_matched_op(indexOp)
+            self.insert_op_before_matched_op(extractOp)
+
+        else:
+            self.insert_op(constantOp, insert_point)
+            self.insert_op(indexOp, insert_point)
+            self.insert_op(extractOp, insert_point)
+
+        return extractOp.result
 
 
 class PLPatternRewriteWalker(PatternRewriteWalker):
