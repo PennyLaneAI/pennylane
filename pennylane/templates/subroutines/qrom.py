a16f3b0a6099191b4357e65328903ef6ac6491f3
dwierichs <david.wierichs@xanadu.ai>
2025-08-03 21:34:56 +0200

    use partial=True

--- pennylane/templates/subroutines/qrom.py
+++ pennylane/templates/subroutines/qrom.py
@@ -237,7 +237,7 @@ class QROM(Operation):
 
             select_ops = []
             if control_select_wires:
-                select_ops += [Select(new_ops, control=control_select_wires)]
+                select_ops += [Select(new_ops, control=control_select_wires, partial=True)]
             else:
                 select_ops = new_ops
 
