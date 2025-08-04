8d03558a1e1f8502d39846ba48f1af492f4e408a
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-08-04 11:39:12 -0400

    Format

--- pennylane/templates/state_preparations/superposition.py
+++ pennylane/templates/state_preparations/superposition.py
@@ -410,7 +410,8 @@ def _suerposition_resources(num_wires, num_coeffs, bases):
             ] += 1
 
             resources[qml.CNOT] += reduce(
-                lambda acc, ib: acc + int(ib[1] != basis2[ib[0]]),  # pylint: disable=cell-var-from-loop
+                lambda acc, ib: acc
+                + int(ib[1] != basis2[ib[0]]),  # pylint: disable=cell-var-from-loop
                 enumerate(basis1),
                 0,
             )
