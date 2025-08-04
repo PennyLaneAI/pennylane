10303d9cca745909b9fc03734a737dbd21c4cd09
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-08-04 16:45:39 -0400

    Format

--- pennylane/templates/layers/strongly_entangling.py
+++ pennylane/templates/layers/strongly_entangling.py
@@ -14,7 +14,7 @@
 r"""
 Contains the StronglyEntanglingLayers template.
 """
-from pennylane import math, capture
+from pennylane import capture, math
 from pennylane.control_flow import for_loop
 from pennylane.decomposition import add_decomps, register_resources
 
