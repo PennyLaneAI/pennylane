105cf7d0087459fc9d5a3592bf659e73be7676da
Luis Alfredo Nuñez Meneses <alfredo.nunez@xanadu.ai>
2025-08-06 13:28:56 -0400

    mpi profile

--- pennylane/concurrency/executors/external/mpi.py
+++ pennylane/concurrency/executors/external/mpi.py
@@ -19,6 +19,8 @@ Contains concurrent executor abstractions for task-based workloads backed by mpi
 
 from collections.abc import Callable, Sequence
 from typing import Any
+import time
+
 
 from ..base import ExecBackendConfig, ExtExec
 
@@ -41,8 +43,11 @@ class MPIPoolExec(ExtExec):  # pragma: no cover
         All calls to the executor are synchronous, and do not currently support the use of futures as a return object.
 
     Args:
-        *args: non keyword arguments to pass through to the executor backend.
-        **kwargs: keyword arguments to pass through to the executor backend.
+        max_workers (int, optional): Maximum number of worker processes to use. If None, defaults to the number of available MPI processes.
+        persist (bool): Whether to persist the executor. Currently not supported and will raise an error if True.
+        use_pkl5 (bool): Whether to use the pkl5 protocol for serialization. Defaults to True
+        profile (bool): Whether to enable profiling of function execution. Defaults to False.
+        **kwargs: Additional keyword arguments to pass through to the executor backend.
 
 
     """
@@ -68,6 +73,9 @@ class MPIPoolExec(ExtExec):  # pragma: no cover
             map_unpack=True,
             blocking=True,
         )
+        
+        self._profile_fn = kwargs.pop("profile", False)
+        self._use_pkl5 = kwargs.pop("use_pkl5", True) 
 
     def __call__(self, dispatch: str, fn: Callable, *args, **kwargs):
         r"""
@@ -76,7 +84,7 @@ class MPIPoolExec(ExtExec):  # pragma: no cover
         args:       the arguments to pass to ``fn``
         kwargs:     the keyword arguments to pass to ``fn``
         """
-        kwargs.update({"use_pkl5": True})
+        kwargs.update({"use_pkl5": self._use_pkl5})
         return super().__call__(
             dispatch,
             fn,
@@ -89,21 +97,39 @@ class MPIPoolExec(ExtExec):  # pragma: no cover
         return self._size
 
     def submit(self, fn: Callable, *args, **kwargs):
-        with self._exec_backend()(max_workers=self.size, use_pkl5=True) as executor:
+        with self._exec_backend()(max_workers=self.size, use_pkl5=self._use_pkl5) as executor:
             output_f = executor.submit(fn, *args, **kwargs)
         return output_f.result()
 
     def map(self, fn: Callable, *args: Sequence[Any], **kwargs):
         chunksize = max(len(args) // self._size, 1)
 
-        with self._exec_backend()(max_workers=self.size, use_pkl5=True) as executor:
+        if self._profile_fn:
+            
+            self.call_fn = fn
+            
+            with self._exec_backend()(max_workers=self.size, use_pkl5=self._use_pkl5) as executor:
+                output_f = executor.map(self.profile_fn, *args, chunksize=chunksize, **kwargs)
+
+            return list(output_f)
+            
+        with self._exec_backend()(max_workers=self.size, use_pkl5=self._use_pkl5) as executor:
             output_f = executor.map(fn, *args, chunksize=chunksize, **kwargs)
         return list(output_f)
 
     def starmap(self, fn: Callable, args: Sequence[tuple], **kwargs):
         chunksize = max(len(args) // self._size, 1)
 
-        with self._exec_backend()(max_workers=self.size, use_pkl5=True) as executor:
+        if self._profile_fn:
+            
+            self.call_fn = fn
+            
+            with self._exec_backend()(max_workers=self.size, use_pkl5=self._use_pkl5) as executor:
+                output_f = executor.starmap(self.profile_fn, args, chunksize=chunksize, **kwargs)
+
+            return list(output_f)
+            
+        with self._exec_backend()(max_workers=self.size, use_pkl5=self._use_pkl5) as executor:
             output_f = executor.starmap(fn, args, chunksize=chunksize, **kwargs)
         return list(output_f)
 
@@ -112,6 +138,25 @@ class MPIPoolExec(ExtExec):  # pragma: no cover
 
     def __del__(self):
         self.shutdown()
+        
+    def profile_fn(self, *args, **kwargs):
+        """
+        A function to profile the execution of the executor.
+        This is a placeholder for any profiling logic that may be added in the future.
+        """
+        # print("Profiling function execution...")  # Placeholder for profiling logic
+        from mpi4py import MPI
+
+        rank = MPI.COMM_WORLD.Get_rank()
+        size = MPI.COMM_WORLD.Get_size()
+        
+        start = time.perf_counter()
+        results = self.call_fn(*args, **kwargs)  # Call the function to be profiled
+        end = time.perf_counter()
+
+
+        print(f"Rank {rank:<3}/ {size:<3} | finished: {self.call_fn.__name__} | time: {end - start:.4f} seconds",flush=True)
+        return results
 
     @classmethod
     def _exec_backend(cls):
@@ -137,6 +182,13 @@ class MPICommExec(ExtExec):  # pragma: no cover
 
     .. note::
         All calls to the executor are synchronous, and do not currently support the use of futures as a return object.
+
+    Args:
+        max_workers (int, optional): Maximum number of worker processes to use. If None, defaults to the number of available MPI processes.
+        persist (bool): Whether to persist the executor. Currently not supported and will raise an error if True.
+        use_pkl5 (bool): Whether to use the pkl5 protocol for serialization. Defaults to True
+        profile (bool): Whether to enable profiling of function execution. Defaults to False.
+        **kwargs: Additional keyword arguments to pass through to the executor backend.
     """
 
     def __init__(self, max_workers=None, persist: bool = False, **kwargs):
@@ -159,6 +211,10 @@ class MPICommExec(ExtExec):  # pragma: no cover
             map_unpack=True,
             blocking=False,
         )
+        
+        self._profile_fn = kwargs.pop("profile", False)
+        self._use_pkl5 = kwargs.pop("use_pkl5", True) 
+        
 
     def __call__(self, dispatch: str, fn: Callable, *args, **kwargs):
         r"""
@@ -167,7 +223,7 @@ class MPICommExec(ExtExec):  # pragma: no cover
         args:       the arguments to pass to ``fn``
         kwargs:     the keyword arguments to pass to ``fn``
         """
-        kwargs.update({"use_pkl5": True})
+        kwargs.update({"use_pkl5": self._use_pkl5})
         return super().__call__(
             dispatch,
             fn,
@@ -176,21 +232,39 @@ class MPICommExec(ExtExec):  # pragma: no cover
         )
 
     def submit(self, fn: Callable, *args, **kwargs):
-        with self._exec_backend()(max_workers=self.size, use_pkl5=True) as executor:
+        with self._exec_backend()(max_workers=self.size, use_pkl5=self._use_pkl5) as executor:
             output_f = executor.submit(fn, *args, **kwargs)
         return output_f.result()
 
     def map(self, fn: Callable, *args: Sequence[Any], **kwargs):
         chunksize = max(len(args) // self._size, 1)
+        
+        if self._profile_fn:
+            print("Using profile function for execution...")  # Debugging statement
+            self.call_fn = fn
+            
+            with self._exec_backend()(max_workers=self.size, use_pkl5=self._use_pkl5) as executor:
+                output_f = executor.map(self.profile_fn, *args, chunksize=chunksize, **kwargs)
 
-        with self._exec_backend()(max_workers=self.size, use_pkl5=True) as executor:
+            return list(output_f)
+
+        with self._exec_backend()(max_workers=self.size, use_pkl5=self._use_pkl5) as executor:
             output_f = executor.map(fn, *args, chunksize=chunksize, **kwargs)
         return list(output_f)
 
     def starmap(self, fn: Callable, args: Sequence[tuple], **kwargs):
         chunksize = max(len(args) // self._size, 1)
+        
+        if self._profile_fn:
+            print("Using profile function for execution...")  # Debugging statement
+            self.call_fn = fn
+            
+            with self._exec_backend()(max_workers=self.size, use_pkl5=self._use_pkl5) as executor:
+                output_f = executor.starmap(self.profile_fn, args, chunksize=chunksize, **kwargs)
 
-        with self._exec_backend()(max_workers=self.size, use_pkl5=True) as executor:
+            return list(output_f)
+
+        with self._exec_backend()(max_workers=self.size, use_pkl5=self._use_pkl5) as executor:
             output_f = executor.starmap(fn, args, chunksize=chunksize, **kwargs)
         return list(output_f)
 
@@ -203,6 +277,24 @@ class MPICommExec(ExtExec):  # pragma: no cover
 
     def __del__(self):
         self.shutdown()
+
+    def profile_fn(self, *args, **kwargs):
+        """
+        A function to profile the execution of the executor.
+        This is a placeholder for any profiling logic that may be added in the future.
+        """
+        # print("Profiling function execution...")  # Placeholder for profiling logic
+        from mpi4py import MPI
+
+        rank = MPI.COMM_WORLD.Get_rank()
+        size = MPI.COMM_WORLD.Get_size()
+        
+        start = time.perf_counter()
+        results = self.call_fn(*args, **kwargs)  # Call the function to be profiled
+        end = time.perf_counter()
+
+        print(f"Rank {rank:<3}/ {size:<3} | finished: {self.call_fn.__name__} | time: {end - start:.4f} seconds",flush=True)
+        return results
 
     @classmethod
     def _exec_backend(cls):
--- pennylane/labs/trotter_error/product_formulas/error.py
+++ pennylane/labs/trotter_error/product_formulas/error.py
@@ -189,6 +189,7 @@ def perturbation_error(
     commutators = _group_sums(bch_expansion(product_formula(1j * timestep), order))
 
     if backend == "serial":
+        print(f"DEBUG: perturbation_error SERIAL")
         assert num_workers == 1, "num_workers must be set to 1 for serial execution."
         expectations = []
         for state in states:
@@ -201,9 +202,10 @@ def perturbation_error(
         return expectations
 
     if parallel_mode == "state":
+        print(f"DEBUG: perturbation_error PARALLEL STATE")
         states_execution = [(commutators, fragments, state) for state in states]
         executor = concurrency.backends.get_executor(backend)
-        with executor(max_workers=num_workers) as ex:
+        with executor(max_workers=num_workers, profile=True) as ex:
             ex.starmap(
                 _get_expval_state,
                 states_execution
@@ -212,10 +214,11 @@ def perturbation_error(
         return expectations
 
     if parallel_mode == "commutator":
+        print(f"DEBUG: perturbation_error PARALLEL COMMUTATOR")
         executor = concurrency.backends.get_executor(backend)
         expectations = []
         for state in states:
-            with executor(max_workers=num_workers) as ex:
+            with executor(max_workers=num_workers, profile=True) as ex:
                 applied_commutators = ex.starmap(
                     _apply_commutator,
                     [(commutator, fragments, state) for commutator in commutators],
@@ -235,10 +238,11 @@ def _get_expval_state(*args, **kwargs):
     """This function is used to get the expectation value of a state with respect to the operator obtained by substituting fragments into commutators."""
     return __get_expval_state(*args, **kwargs)
 
-@timeit
+# @timeit
 def __get_expval_state(commutators, fragments, state: AbstractState) -> float:
     """Returns the expectation value of ``state`` with respect to the operator obtained by substituting ``fragments`` into ``commutators``."""
 
+    print(f"DEBUG: __get_expval_state {state}")
     new_state = _AdditiveIdentity()
     for commutator in commutators:
         new_state += _apply_commutator(commutator, fragments, state)
@@ -249,7 +253,7 @@ def _apply_commutator(*args, **kwargs):
     """This function is used to apply a commutator to a state. It is used in the parallelization of the perturbation error computation."""
     return __apply_commutator(*args, **kwargs)
 
-@timeit
+# @timeit
 def __apply_commutator(
     commutator: tuple[Hashable], fragments: dict[Hashable, Fragment], state: AbstractState
 ) -> AbstractState:
