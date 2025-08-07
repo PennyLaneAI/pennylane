5b7a9334b63bb0934178da6f0af5732fbc3eed78
SimoneGasperini <simone.gasperini4@unibo.it>
2025-08-07 15:46:53 -0400

    Add push_hadamards to the transform for circuit compilation page

--- pennylane/transforms/__init__.py
+++ pennylane/transforms/__init__.py
@@ -64,6 +64,7 @@ A set of transforms to perform basic circuit compilation tasks.
     ~transforms.transpile
     ~transforms.undo_swaps
     ~transforms.unitary_to_rot
+    ~transforms.zx.push_hadamards
 
 There are also utility functions and decompositions available that assist with
 both transforms, and decompositions within the larger PennyLane codebase.
@@ -335,6 +336,7 @@ from .tape_expand import (
 )
 from .transpile import transpile
 from .zx import to_zx, from_zx
+from .zx.push_hadamards import push_hadamards
 from .broadcast_expand import broadcast_expand
 from .decompose import decompose
 
