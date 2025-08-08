f38c63c2aff29f8b4e08e205c7ea1d20abb3d1b0
Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>
2025-08-08 13:49:39 -0400

    Update pennylane/exceptions.py
    Co-authored-by: Isaac De Vlugt <34751083+isaacdevlugt@users.noreply.github.com>

--- pennylane/exceptions.py
+++ pennylane/exceptions.py
@@ -213,5 +213,5 @@ class NonDifferentiableError(Exception):
 _TF_DEPRECATION_MSG = (
     "Support for the TensorFlow interface is deprecated and will be removed in v0.44. "
     "Future versions of PennyLane are not guaranteed to work with TensorFlow. "
-    "Please migrate your workflows to JAX or torch to benefit from enhanced support and features."
+    "Please migrate your workflows to JAX or Pytorch to benefit from enhanced support and features."
 )
