792f63e06d06e77befae41e61b93b36515fdefa6
PietropaoloFrisoni <pietropaolo.frisoni@xanadu.ai>
2025-08-08 13:45:10 -0400

    Simplifying class

--- pennylane/compiler/python_compiler/transforms/visualization.py
+++ pennylane/compiler/python_compiler/transforms/visualization.py
@@ -32,11 +32,10 @@ from xdsl.dialects.tensor import ExtractOp as TensorExtractOp
 from xdsl.ir import SSAValue
 
 import pennylane as qml
-from pennylane import ops
+from pennylane import measurements, ops
+from pennylane.measurements import MeasurementProcess
 from pennylane.operation import Operator
 from pennylane.ops import __all__ as ops_all
-from pennylane import measurements
-from pennylane.measurements import MeasurementProcess
 
 from ..dialects.quantum import AllocOp as AllocOpPL
 from ..dialects.quantum import CustomOp
@@ -57,6 +56,10 @@ from_str_to_PL_measurement = {
     "quantum.state": qml.state,
 }
 
+######################################################
+### Gate/Measurement resolution
+######################################################
+
 
 def resolve_gate(name: str) -> Operator:
     """Resolve the gate from the name."""
@@ -74,14 +77,9 @@ def resolve_measurement(name: str) -> MeasurementProcess:
         raise ValueError(f"Unsupported measurement: {name}") from exc
 
 
-def qml_to_xdsl_param(param: Union[int, float]) -> Union[IntegerAttr, FloatAttr]:
-    """Convert a PennyLane parameter (int or float) into an xDSL constant-attr."""
-    val = param.item() if hasattr(param, "item") else param
-    if isinstance(val, int):
-        return IntegerAttr(val, IntegerType(64))
-    if isinstance(val, float):
-        return FloatAttr(val, Float64Type())
-    raise TypeError(f"Unsupported parameter type: {type(val)}")
+######################################################
+### Parameters/Wires traceback
+######################################################
 
 
 def _extract_dense_constant_value(op) -> float | int:
@@ -93,7 +91,81 @@ def _extract_dense_constant_value(op) -> float | int:
     raise NotImplementedError(f"Unexpected attr type in constant: {type(attr)}")
 
 
-# pylint: disable=too-many-instance-attributes
+def resolve_constant_params(ssa: SSAValue) -> float | int:
+    """Resolve a constant parameter SSA value to a Python float or int."""
+    op = ssa.owner
+    if isinstance(op, TensorExtractOp):
+        return resolve_constant_params(op.tensor)
+    if op.name == "builtin.unregistered":
+        if hasattr(op, "attributes"):
+            if op.attributes["op_name__"].data == "stablehlo.convert":
+                return resolve_constant_params(op.operands[0])
+    if op.name == "stablehlo.constant":
+        return _extract_dense_constant_value(op)
+    raise NotImplementedError(f"Cannot resolve parameters for op: {op}")
+
+
+def resolve_constant_wire(ssa: SSAValue) -> int:
+    """Resolve the wire for the given SSA qubit."""
+    op = ssa.owner
+    if isinstance(op, TensorExtractOp):
+        return resolve_constant_wire(op.tensor)
+    if op.name == "builtin.unregistered":
+        if hasattr(op, "attributes"):
+            if op.attributes["op_name__"].data == "stablehlo.convert":
+                return resolve_constant_wire(op.operands[0])
+    if op.name == "stablehlo.constant":
+        return _extract_dense_constant_value(op)
+    if isinstance(op, CustomOp):
+        return resolve_constant_wire(op.in_qubits[ssa.index])
+    if isinstance(op, ExtractOpPL):
+        return resolve_constant_wire(op.idx)
+    raise NotImplementedError(f"Cannot resolve wire for op: {op}")
+
+
+######################################################
+### Parameters/Wires Conversion
+######################################################
+
+
+def ssa_to_qml_params(op: CustomOp) -> list[float | int]:
+    """Get the parameters from the operation."""
+    if not hasattr(op, "params"):
+        return []
+    return [resolve_constant_params(p) for p in op.params if p is not None]
+
+
+def ssa_to_qml_wires(op: CustomOp) -> list[int]:
+    """Get the wires from the operation."""
+    if not hasattr(op, "in_qubits"):
+        return []
+    return [resolve_constant_wire(q) for q in op.in_qubits if q is not None]
+
+
+############################################################
+### xDSL ---> PennyLane Operators/Measurements conversion
+############################################################
+
+
+def xdsl_to_qml_op(op: CustomOp) -> Operator:
+    """Given a ``quantum.custom`` xDSL op, convert it to a PennyLane operator."""
+    gate_name = op.properties["gate_name"].data
+    parameters = ssa_to_qml_params(op)
+    wires = ssa_to_qml_wires(op)
+    gate = resolve_gate(gate_name)(*parameters, wires=wires)
+    if op.properties.get("adjoint") is not None:
+        gate = qml.adjoint(gate)
+    return gate
+
+
+def xdsl_to_qml_meas(meas) -> Operator:
+    """Given a measurement in xDSL, convert it to a PennyLane measurement."""
+    meas_name = meas.name
+    # So far, only `qml.state` is supported
+    measurement = resolve_measurement(meas_name)
+    return measurement()
+
+
 class VisualizationTransform(pattern_rewriter.RewritePattern):
     """A pattern that matches a function and rewrites it to collect
     the quantum operations into a list of PennyLane operators."""
@@ -115,71 +187,10 @@ class VisualizationTransform(pattern_rewriter.RewritePattern):
     ):
         super().__init__()
         self.module = module
-
         self.wire_to_ssa_qubits: Dict[int, SSAValue] = {}
         self.ssa_qubits_to_wires: Dict[SSAValue, int] = {}
         self.params_to_ssa_params: Dict[Union[FloatAttr, IntegerAttr], SSAValue] = {}
-
         self.quantum_register: Union[SSAValue, None] = None
-
-    def resolve_constant_params(self, ssa: SSAValue) -> float | int:
-        """Resolve a constant parameter SSA value to a Python float or int."""
-        op = ssa.owner
-        if isinstance(op, TensorExtractOp):
-            return self.resolve_constant_params(op.tensor)
-        if op.name == "builtin.unregistered":
-            if hasattr(op, "attributes"):
-                if op.attributes["op_name__"].data == "stablehlo.convert":
-                    return self.resolve_constant_params(op.operands[0])
-        if op.name == "stablehlo.constant":
-            return _extract_dense_constant_value(op)
-        raise NotImplementedError(f"Cannot resolve parameters for op: {op}")
-
-    def resolve_constant_wire(self, ssa: SSAValue) -> int:
-        """Resolve the wire for the given SSA qubit."""
-        op = ssa.owner
-        if isinstance(op, TensorExtractOp):
-            return self.resolve_constant_wire(op.tensor)
-        if op.name == "builtin.unregistered":
-            if hasattr(op, "attributes"):
-                if op.attributes["op_name__"].data == "stablehlo.convert":
-                    return self.resolve_constant_wire(op.operands[0])
-        if op.name == "stablehlo.constant":
-            return _extract_dense_constant_value(op)
-        if isinstance(op, CustomOp):
-            return self.resolve_constant_wire(op.in_qubits[ssa.index])
-        if isinstance(op, ExtractOpPL):
-            return self.resolve_constant_wire(op.idx)
-        raise NotImplementedError(f"Cannot resolve wire for op: {op}")
-
-    def ssa_to_qml_params(self, op: CustomOp) -> list[float | int]:
-        """Get the parameters from the operation."""
-        if not hasattr(op, "params"):
-            return []
-        return [self.resolve_constant_params(p) for p in op.params if p is not None]
-
-    def ssa_to_qml_wires(self, op: CustomOp) -> list[int]:
-        """Get the wires from the operation."""
-        if not hasattr(op, "in_qubits"):
-            return []
-        return [self.resolve_constant_wire(q) for q in op.in_qubits if q is not None]
-
-    def xdsl_to_qml_op(self, op: CustomOp) -> Operator:
-        """Given a ``quantum.custom`` xDSL op, convert it to a PennyLane operator."""
-        gate_name = op.properties["gate_name"].data
-        parameters = self.ssa_to_qml_params(op)
-        wires = self.ssa_to_qml_wires(op)
-        gate = resolve_gate(gate_name)(*parameters, wires=wires)
-        if op.properties.get("adjoint") is not None:
-            gate = qml.adjoint(gate)
-        return gate
-
-    def xdsl_to_qml_meas(self, meas) -> Operator:
-        """Given a measurement in xDSL, convert it to a PennyLane measurement."""
-        meas_name = meas.name
-        # So far, only `qml.state` is supported
-        measurement = resolve_measurement(meas_name)
-        return measurement()
 
     # TODO: this will probably no longer be needed once PR #7937 is merged
     def initialize_qubit_mapping(self, funcOp: func.FuncOp):
@@ -192,7 +203,7 @@ class VisualizationTransform(pattern_rewriter.RewritePattern):
                 saw_alloc = True
                 self.quantum_register = op.qreg
             elif isinstance(op, ExtractOpPL):
-                wire = self.resolve_constant_wire(op.idx)
+                wire = resolve_constant_wire(op.idx)
                 if wire not in self.wire_to_ssa_qubits:
                     self.wire_to_ssa_qubits[wire] = op.qubit
                     # We update the reverse mapping as well for completeness,
@@ -208,11 +219,9 @@ class VisualizationTransform(pattern_rewriter.RewritePattern):
         collected_meas = []
 
         for xdsl_op in funcOp.body.walk():
-
             if isinstance(xdsl_op, StateOp):
-                qml_meas = self.xdsl_to_qml_meas(xdsl_op)
+                qml_meas = xdsl_to_qml_meas(xdsl_op)
                 collected_meas.append(qml_meas)
-                print(f"collected meas: {collected_meas}")
             if not isinstance(xdsl_op, CustomOp):
                 continue
             if self.quantum_register is None:
@@ -220,9 +229,11 @@ class VisualizationTransform(pattern_rewriter.RewritePattern):
             if len(self.wire_to_ssa_qubits) == 0:
                 raise NotImplementedError("No wires extracted from the register have been found.")
 
-            qml_op = self.xdsl_to_qml_op(xdsl_op)
+            qml_op = xdsl_to_qml_op(xdsl_op)
             collected_ops.append(qml_op)
-            print(f"collected ops: {collected_ops}")
+
+        print(f"collected ops: {collected_ops}")
+        print(f"collected meas: {collected_meas}")
 
 
 @dataclass(frozen=True)
@@ -230,12 +241,6 @@ class VisualizationTransformPass(passes.ModulePass):
     """A pass that applies the Transform pattern to a module."""
 
     name = "visualization-transform"
-
-    # We use type annotations since these are (configurable) dataclass fields
-    gate_set: Optional[
-        Union[Iterable[Union[str, type]], Dict[Union[str, type], float], Callable]
-    ] = None
-    max_expansion: Optional[int] = None
 
     # pylint: disable=arguments-renamed,no-self-use, arguments-differ
     def apply(self, _ctx: context.MLContext, module: builtin.ModuleOp) -> None:
