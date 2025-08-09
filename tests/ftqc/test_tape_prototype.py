ab365bedcbcdc1850ee37c1509edcfa0e981c3f1
lillian542 <38584660+lillian542@users.noreply.github.com>
2025-08-09 01:59:26 -0400

    Ftqc device frontend (#7656)
    **Context:**
    We want to prototype executing on a backend with a feedback loop
    (execution of part of the tape, updates based on measurement results,
    continued execution), while leaning where possible on the existing
    architecture in PennyLane.
    
    We already have tape transforms in place for conversion to the MBQC
    formalism, as well as the framework for a transform for using a Pauli
    tracker to consolidate corrections.
    
    **Description of the Change:**
    - We create an `ftqc.device` whose main functions are to:
    1. define a `preprocess` that is essentially the compilation pipeline
    for going from an arbitrary circuit defined in PennyLane to the MBQC
    formalism
    2. define a setup for the execution config with the relevant entries for
    integration with PennyLane's execution and device API
    3. define its own execution function that will handle the overhead for
    executing the tapes stepwise with corrections
    
    In this PR, only the first two points are relevant. The 3rd will be its
    own PR.
    
    - We create backends that are currently just wrappers for
    "lightning.qubit" and "null.qubit", but with their own `capabilities`
    that reflect the limitations of the fake HW device.
    - In a follow-up PR, the `lightning.qubit` backend will be updated with
    modified `simulate` and `execute` methods that allow us to maintain the
    `_statevector` from one execution to the next, unless we are explicitly
    resetting the system. The `null.qubit` backend may be updated if needed
    to mimic a mocked version of the same behaviour, but likely won't need
    much modification.
    
    **Benefits:**
    The main goal here was to create two separate objects with different
    responsibilities for the execution pipeline - one that applies
    preprocessing and orchestrates the feedback loop, and another that
    executes. Which thing goes in which box is subject to change as this
    prototype develops over this iteration.
    
    The important thing here is that the boxes exist, the preprocessing
    works as expected, and we can use this setup to execute arbitrary
    PennyLane circuits in the simplest (online corrections after every
    operation) implementation of textbook MBQC.
    
    [sc-90413] [sc-93075]
    
    ---------
    
    Co-authored-by: Isaac De Vlugt <34751083+isaacdevlugt@users.noreply.github.com>
    Co-authored-by: Christina Lee <christina@xanadu.ai>
    Co-authored-by: Yushao Chen (Jerry) <chenys13@outlook.com>
    Co-authored-by: Lee James O'Riordan <mlxd@users.noreply.github.com>

---
+++ tests/ftqc/test_tape_prototype.py
@@ -0,0 +1,146 @@
+# Copyright 2025 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Basic tests and sanity checks for the tape-based execution pipeline prototype. Not intended to be comprehensive."""
+
+import numpy as np
+import pytest
+
+import pennylane as qml
+from pennylane.devices.execution_config import ExecutionConfig, MCMConfig
+from pennylane.ftqc.ftqc_device import FTQCQubit, LightningQubitBackend, NullQubitBackend
+from pennylane.measurements import MidMeasureMP
+from pennylane.ops import CZ, Adjoint, Conditional, H
+
+
+@pytest.mark.parametrize(
+    "backend_cls, n_wires, name",
+    [(LightningQubitBackend, 25, "lightning"), (NullQubitBackend, 1000, "null")],
+)
+def test_backend_initializes(backend_cls, n_wires, name):
+    """Test that the backends initialize successfully"""
+
+    backend = backend_cls()
+
+    assert isinstance(backend.device, qml.devices.Device)
+    assert backend.device.name == f"{name}.qubit"
+    assert backend.wires == qml.wires.Wires(range(n_wires))
+    assert isinstance(backend.capabilities, qml.devices.DeviceCapabilities)
+
+
+@pytest.mark.parametrize("backend_cls", [LightningQubitBackend, NullQubitBackend])
+def test_ftqc_device_initializes(backend_cls):
+    """Test that the ftqc.qubit device initializes as expected"""
+
+    backend = backend_cls()
+    dev = FTQCQubit(wires=2, backend=backend)
+
+    assert isinstance(dev, qml.devices.Device)
+    assert dev.name == "ftqc.qubit"
+    assert dev.wires == qml.wires.Wires([0, 1])
+    assert isinstance(dev.capabilities, qml.devices.DeviceCapabilities)
+
+
+@pytest.mark.parametrize("wires", ([0, 1], ["a", "b"]))
+@pytest.mark.parametrize("backend_cls", [LightningQubitBackend, NullQubitBackend])
+def test_executing_arbitrary_circuit(wires, backend_cls):
+    """Test that an arbitrary circuit is preprocessed to be expressed in the
+    MBQC formalism before executing, and executes as expected"""
+
+    qml.decomposition.enable_graph()
+
+    backend = backend_cls()
+    dev = FTQCQubit(wires=wires, backend=backend)
+
+    def circ():
+        qml.RY(1.2, wires[1])  # tape and device wire order do not match
+        qml.RX(0.45, wires[0])
+        # observables don't commute
+        return qml.expval(qml.X(wires[1])), qml.expval(qml.Y(wires[0])), qml.expval(qml.Y(wires[1]))
+
+    ftqc_circ = qml.qnode(device=dev)(circ)
+    ftqc_circ = qml.set_shots(ftqc_circ, shots=3000)
+
+    ref_circ = qml.qnode(device=qml.device("lightning.qubit", wires=wires))(circ)
+
+    # the processed circuit is two tapes (split_non_commuting), returning
+    # only samples, and expressed in the MBQC formalism
+    tapes, _ = qml.workflow.construct_batch(ftqc_circ, level="device")()
+    assert len(tapes) == 2
+    for tape in tapes:
+        assert all(isinstance(mp, qml.measurements.SampleMP) for mp in tape.measurements)
+        assert all(isinstance(op, (Conditional, CZ, H, MidMeasureMP)) for op in tape.operations)
+
+    # circuit executes
+    res = ftqc_circ()
+
+    expected_res = (1.0, 1.0, 1.0) if backend_cls is NullQubitBackend else ref_circ()
+
+    assert np.allclose(res, expected_res, atol=0.05)
+
+
+@pytest.mark.parametrize("wires", ([0, 1], ["a", "b"]))
+@pytest.mark.parametrize("backend_cls", [LightningQubitBackend, NullQubitBackend])
+def test_executing_arbitrary_circuit_two_qubit_gate(wires, backend_cls):
+    """Test that an arbitrary circuit is preprocessed to be expressed in the
+    MBQC formalism before executing, and executes as expected"""
+
+    qml.decomposition.enable_graph()
+
+    backend = backend_cls()
+    dev = FTQCQubit(wires=wires, backend=backend)
+
+    def circ():
+        qml.RX(np.pi / 4, wires[1])
+        qml.CNOT([wires[1], wires[0]])
+        return qml.expval(qml.Z(wires[0]))
+
+    ftqc_circ = qml.qnode(device=dev)(circ)
+    ftqc_circ = qml.set_shots(ftqc_circ, shots=3000)
+
+    ref_circ = qml.qnode(device=qml.device("lightning.qubit", wires=wires))(circ)
+
+    # the processed circuit is two tapes (split_non_commuting), returning
+    # only samples, and expressed in the MBQC formalism
+    tapes, _ = qml.workflow.construct_batch(ftqc_circ, level="device")()
+    assert len(tapes) == 1
+    assert all(isinstance(mp, qml.measurements.SampleMP) for mp in tapes[0].measurements)
+    # CZ, H, MCMs, Conditional MCMs/Pauli corrections, + Adjoint for diagonalizing gates
+    assert all(
+        isinstance(op, (Conditional, CZ, H, MidMeasureMP, Adjoint)) for op in tapes[0].operations
+    )
+
+    # circuit executes
+    res = ftqc_circ()
+
+    expected_res = (1.0, 1.0, 1.0) if backend_cls is NullQubitBackend else ref_circ()
+
+    assert np.allclose(res, expected_res, atol=0.05)
+
+
+@pytest.mark.parametrize(
+    "backend, expected_method",
+    [(LightningQubitBackend(), "one-shot"), (NullQubitBackend(), "device")],
+)
+@pytest.mark.parametrize(
+    "initial_config", [ExecutionConfig(mcm_config=MCMConfig(mcm_method="placeholder")), None]
+)
+def test_setup_execution_config(backend, expected_method, initial_config):
+    """Test that setup_execution_config works as expected"""
+
+    dev = FTQCQubit(wires=2, backend=backend)
+    config = dev.setup_execution_config(initial_config)
+
+    assert isinstance(config, ExecutionConfig)
+    assert config.mcm_config.mcm_method == expected_method
