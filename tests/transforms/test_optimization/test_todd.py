aa3399e6502e90de47f55d28981d36f64183a555
Simone Gasperini <simone.gasperini4@unibo.it>
2025-08-13 14:40:44 -0400

    Fix state fidelity check
    Co-authored-by: Yushao Chen (Jerry) <chenys13@outlook.com>

--- tests/transforms/test_optimization/test_todd.py
+++ tests/transforms/test_optimization/test_todd.py
@@ -211,5 +211,5 @@ class TestTODD:
         state2 = reduced_circ()
 
         # test that the states are equivalent up to a global phase
-        check = np.abs(np.conj(state1) @ state2)
+        check = qml.math.fidelity_statevector(state1, state2)
         assert np.isclose(check, 1)
