954991c3f1f4fbcdaa45b4bd163a27794d4c70bf
Joey Carter <joseph.carter@xanadu.ai>
2025-08-14 17:34:15 -0400

    Initial commit

---
+++ pennylane/compiler/python_compiler/transforms/decompose_graph_state.py
@@ -0,0 +1,151 @@
+# Copyright 2025 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This module contains the implementation of the decompose_graph_state transform,
+written using xDSL.
+"""
+
+from dataclasses import dataclass
+from typing import Generator
+
+import numpy as np
+from xdsl import context, ir, passes, pattern_rewriter
+from xdsl.dialects import arith, builtin, func, tensor
+from xdsl.pattern_rewriter import PatternRewriter, RewritePattern
+from xdsl.rewriter import InsertPoint
+
+from pennylane.exceptions import CompileError
+
+from ..dialects import mbqc, quantum
+from .api import compiler_transform
+
+
+@dataclass(frozen=True)
+class DecomposeGraphStatePass(passes.ModulePass):
+    """Pass that ... [TODO]"""
+
+    name = "decompose-graph-state"
+
+    # pylint: disable=arguments-renamed,no-self-use
+    def apply(self, _ctx: context.Context, module: builtin.ModuleOp) -> None:
+        """Apply the decompose-graph-state pass."""
+
+        greedy_applier = pattern_rewriter.GreedyRewritePatternApplier(
+            [DecomposeGraphStatePattern()]
+        )
+        walker = pattern_rewriter.PatternRewriteWalker(greedy_applier)
+        walker.rewrite_module(module)
+        breakpoint()
+
+
+decompose_graph_state_pass = compiler_transform(DecomposeGraphStatePass)
+
+
+# pylint: disable=too-few-public-methods
+class DecomposeGraphStatePattern(RewritePattern):
+    """Rewrite pattern ... [TODO]"""
+
+    def __init__(self):
+        super().__init__()
+
+    # pylint: disable=arguments-renamed
+    @pattern_rewriter.op_type_rewrite_pattern
+    def match_and_rewrite(self, graph_prep_op: mbqc.GraphStatePrepOp, rewriter: PatternRewriter, /):
+        # These are the names of the gates that realize the desired initial individual qubit state
+        # and entangled state, respectively
+        init_op_gate_name = graph_prep_op.init_op.data
+        entangle_op_gate_name = graph_prep_op.entangle_op.data
+
+        # Parse the adjacency matrix from the result of the ConstantOp given as input to the
+        # graph_state_prep op. We assume that the adjacency matrix is stored as a
+        # DenseIntOrFPElementsAttr, whose data is accessible as a 'bytes' array.
+        adj_matrix_const_op = graph_prep_op.adj_matrix.owner
+        adj_matrix_value = adj_matrix_const_op.properties.get("value")
+        assert adj_matrix_value is not None and hasattr(
+            adj_matrix_value, "data"
+        ), f"Unable to read graph adjacency matrix from op `{adj_matrix_const_op}`"
+
+        adj_matrix_bytes = adj_matrix_value.data
+        assert isinstance(adj_matrix_bytes, builtin.BytesAttr), (
+            f"Expected graph adjacency matrix data to be of type 'builtin.BytesAttr', but got "
+            f"{type(adj_matrix_bytes).__name__}"
+        )
+
+        adj_matrix = list(adj_matrix_bytes.data)
+        n_vertices = _n_vertices_from_packed_adj_matrix(adj_matrix)
+
+        alloc_op = quantum.AllocOp(n_vertices)
+        rewriter.insert_op(alloc_op)
+
+        extract_ops: list[quantum.ExtractOp] = []
+        for i in range(n_vertices):
+            extract_op = quantum.ExtractOp(alloc_op.qreg, i)
+            # rewriter.insert_op(extract_op)
+            extract_ops.append(extract_op)
+
+        init_ops: list[quantum.CustomOp] = []
+        for i in range(n_vertices):
+            init_op = quantum.CustomOp(in_qubits=extract_ops[i].qubit, gate_name=init_op_gate_name)
+            # rewriter.insert_op(extract_op)
+            init_ops.append(init_op)
+
+        # FIXME!! This block is wrong, we need to cascade the output qubits from the previous op to
+        # the next
+        entangle_ops: list[quantum.CustomOp] = []
+        for edge in _edge_iter(adj_matrix):
+            q0 = init_ops[edge[0]].out_qubits[0]
+            q1 = init_ops[edge[1]].out_qubits[0]
+            entangle_op = quantum.CustomOp(in_qubits=(q0, q1), gate_name=entangle_op_gate_name)
+            # rewriter.insert_op(entangle_op)
+            entangle_ops.append(entangle_op)
+
+        for extract_op in extract_ops:
+            rewriter.insert_op(extract_op)
+
+        for init_op in init_ops:
+            rewriter.insert_op(init_op)
+
+        for entangle_op in entangle_ops:
+            rewriter.insert_op(entangle_op)
+
+        rewriter.erase_matched_op()
+        rewriter.erase_op(graph_prep_op.adj_matrix.owner)
+
+
+def _n_vertices_from_packed_adj_matrix(adj_matrix: list) -> int:
+    """TODO"""
+    m = len(adj_matrix)
+
+    N = (1 + np.sqrt(1 + 8 * m)) / 2
+
+    if N != int(N):
+        raise CompileError(
+            f"The number of elements in the densely packed adjacency matrix is {m}, which does not "
+            f"correspond to an integer number of graph vertices"
+        )
+
+    return int(N)
+
+
+def _edge_iter(adj_matrix: list) -> Generator[tuple[int, int], None, None]:
+    """TODO"""
+    j = 1
+    k = 0
+    for i in range(len(adj_matrix)):
+        if adj_matrix[i]:
+            yield (k, j)
+        k += 1
+        if k == j:
+            k = 0
+            j += 1
---
+++ tests/python_compiler/transforms/test_xdsl_decompose_graph_state.py
@@ -0,0 +1,137 @@
+# Copyright 2025 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit and integration tests for the Python compiler `decompose-graph-state` transform."""
+
+# pylint: disable=wrong-import-position
+
+import pytest
+
+pytestmark = pytest.mark.external
+
+xdsl = pytest.importorskip("xdsl")
+catalyst = pytest.importorskip("catalyst")
+from catalyst.passes import xdsl_plugin
+
+import pennylane as qml
+from pennylane.compiler.python_compiler.transforms import (
+    DecomposeGraphStatePass,
+    decompose_graph_state_pass,
+)
+
+
+class TestDecomposeGraphStatePass:
+    """Unit tests for the decompose-graph-state pass."""
+
+    def test_2_qubit_chain(self, run_filecheck):
+        """
+
+        0 -- 1
+
+        0  1
+        1  0
+
+        [1]
+        """
+        program = """
+        // CHECK-LABEL: circuit
+        func.func @circuit() {
+            // CHECK-NOT: arith.constant dense<[1]> : tensor<1xi1>
+            // CHECK-NOT: mbqc.graph_state_prep
+
+            // CHECK: [[graph_reg:%.+]] = quantum.alloc( 2) : !quantum.reg
+            // CHECK: [[q00:%.+]] = quantum.extract [[graph_reg]][ 0] : !quantum.reg -> !quantum.bit
+            // CHECK: [[q10:%.+]] = quantum.extract [[graph_reg]][ 1] : !quantum.reg -> !quantum.bit
+
+            // CHECK: [[q01:%.+]] = quantum.custom "Hadamard"() %q00 : !quantum.bit
+            // CHECK: [[q11:%.+]] = quantum.custom "Hadamard"() %q10 : !quantum.bit
+            // CHECK: [[q20:%.+]]:2 = quantum.custom "CZ"() [[q01]], [[q11]] : !quantum.bit, !quantum.bit
+
+            // CHECK: [[out_reg0:%.+]] = quantum.insert [[graph_reg]][ 0], %q20#0 : !quantum.reg, !quantum.bit
+            // CHECK: [[out_reg1:%.+]] = quantum.insert [[graph_reg]][ 1], %q20#1 : !quantum.reg, !quantum.bit
+
+            %adj_matrix = arith.constant dense<[1]> : tensor<1xi1>
+            %qreg = mbqc.graph_state_prep (%adj_matrix : tensor<1xi1>) [init "Hadamard", entangle "CZ"] : !quantum.reg
+            func.return
+        }
+        """
+
+        pipeline = (DecomposeGraphStatePass(),)
+        run_filecheck(program, pipeline)
+
+    def test_3_qubit_chain(self, run_filecheck):
+        """
+
+        0 -- 1 -- 2
+
+        0  1  0
+        1  0  1
+        0  1  0
+
+        [1, 0, 1]
+        """
+        program = """
+        // CHECK-LABEL: circuit
+        func.func @circuit() {
+            %adj_matrix = arith.constant dense<[1, 0, 1]> : tensor<3xi1>
+            %qreg = mbqc.graph_state_prep (%adj_matrix : tensor<3xi1>) [init "Hadamard", entangle "CZ"] : !quantum.reg
+            func.return
+        }
+        """
+
+        pipeline = (DecomposeGraphStatePass(),)
+        run_filecheck(program, pipeline)
+
+    def test_4_qubit_square_lattice(self, run_filecheck):
+        """
+
+        0 -- 1
+        |    |
+        2 -- 3
+
+        0  1  1  0
+        1  0  0  1
+        1  0  0  1
+        0  1  1  0
+
+        [1, 1, 0, 0, 1, 1]
+        """
+        program = """
+        // CHECK-LABEL: circuit
+        func.func @circuit() {
+            %adj_matrix = arith.constant dense<[1, 1, 0, 0, 1, 1]> : tensor<6xi1>
+            %qreg = mbqc.graph_state_prep (%adj_matrix : tensor<6xi1>) [init "Hadamard", entangle "CZ"] : !quantum.reg
+            func.return
+        }
+        """
+
+        pipeline = (DecomposeGraphStatePass(),)
+        run_filecheck(program, pipeline)
+
+    def test_2_qubit_chain_non_standard_init(self, run_filecheck):
+        """ """
+        program = """
+        // CHECK-LABEL: circuit
+        func.func @circuit() {
+            %adj_matrix = arith.constant dense<[1]> : tensor<1xi1>
+            %qreg = mbqc.graph_state_prep (%adj_matrix : tensor<1xi1>) [init "S", entangle "CNOT"] : !quantum.reg
+            func.return
+        }
+        """
+
+        pipeline = (DecomposeGraphStatePass(),)
+        run_filecheck(program, pipeline)
+
+    class TestDecomposeGraphStateIntegration:
+        pass
