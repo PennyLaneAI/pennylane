93b25b8e68d87e83d2eceb4eaba6fc4834319cfa
SimoneGasperini <simone.gasperini4@unibo.it>
2025-08-18 14:40:28 -0400

    Check unitaries equivalence up to global phase

--- tests/transforms/test_optimization/test_optimize_t_count.py
+++ tests/transforms/test_optimization/test_optimize_t_count.py
@@ -212,3 +212,11 @@ class TestOptimizeTCount:
         # test that the states are equivalent up to a global phase
         check = qml.math.fidelity_statevector(state1, state2)
         assert np.isclose(check, 1)
+
+        u1 = qml.matrix(original_circ, wire_order=range(num_wires))()
+        u2 = qml.matrix(reduced_circ, wire_order=range(num_wires))()
+
+        # test that the unitaries are equivalent up to a global phase
+        prod = u1 @ np.conj(u2.T)
+        glob_phase = prod[0, 0]
+        assert np.allclose(prod, glob_phase * np.eye(2**num_wires))
