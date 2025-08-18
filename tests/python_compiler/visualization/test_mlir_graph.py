7a333007b97b16c5f83a48abe41f8cba170e2821
PietropaoloFrisoni <pietropaolo.frisoni@xanadu.ai>
2025-08-18 15:23:49 -0400

    Trying to fix `graphiz` failure

--- tests/python_compiler/visualization/test_mlir_graph.py
+++ tests/python_compiler/visualization/test_mlir_graph.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Unit test module for the merge rotations transform"""
+"""Unit test module for the MLIR graph generation in the Python Compiler visualization module."""
 
 from pathlib import Path
 
