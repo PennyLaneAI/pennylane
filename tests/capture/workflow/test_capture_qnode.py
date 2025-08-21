83a12a2bcdc997dbc8c06b05215aba55544f58f2
andrijapau <andrija.paurevic@xanadu.ai>
2025-08-21 13:36:18 -0400

    fix test

--- tests/capture/workflow/test_capture_qnode.py
+++ tests/capture/workflow/test_capture_qnode.py
@@ -556,9 +556,10 @@ class TestDevicePreprocessing:
         """Test that using a qnode with postselect_mode="fill-shots" gives the expected results."""
 
         shots = 1000
-        dev = qml.device(dev_name, wires=3, shots=shots, seed=seed)
+        dev = qml.device(dev_name, wires=3, seed=seed)
         postselect = 1 if dev_name == "default.qubit" else None
 
+        @qml.set_shots(shots=shots)
         @qml.qnode(dev, mcm_method=mcm_method, postselect_mode="fill-shots")
         def circuit():
             qml.Hadamard(0)
@@ -597,9 +598,13 @@ class TestDevicePreprocessing:
 
         @qml.qnode(dev, mcm_method=mcm_method, postselect_mode="hw-like")
         def circuit():
-            for _ in range(n_postselects):
+
+            @qml.for_loop(n_postselects)
+            def loop(i):  # pylint: disable=unused-argument
                 qml.Hadamard(0)
                 qml.measure(0, postselect=postselect)
+
+            loop()
             return qml.sample(wires=[0])
 
         res = circuit()
