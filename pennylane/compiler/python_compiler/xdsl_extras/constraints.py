f246b2b0c7df61abe5fe9f0dfd63d4d7baa5f787
Mudit Pandey <mudit.pandey@xanadu.ai>
2025-08-22 12:08:13 -0400

    Update constructor

--- pennylane/compiler/python_compiler/xdsl_extras/constraints.py
+++ pennylane/compiler/python_compiler/xdsl_extras/constraints.py
@@ -16,9 +16,9 @@
 upstream in xDSL."""
 
 from abc import ABC, abstractmethod
-from collections.abc import Sequence
+from collections.abc import Iterable
 from dataclasses import dataclass
-from typing import TypeVar, get_args, get_origin
+from typing import TypeVar
 
 from xdsl.dialects.builtin import ArrayAttr, IntAttr, IntAttrConstraint, MemRefType, TensorType
 from xdsl.ir import Attribute, AttributeInvT
@@ -35,15 +35,8 @@ from xdsl.irdl import (
 )
 from xdsl.utils.exceptions import VerifyException
 
-# Get all bases of type alias
-_constr_bases = get_args(IRDLAttrConstraint)
-# If base is generic, get the origin
-_valid_constraint_bases = tuple(
-    c if (origin := get_origin(c)) is None else origin for c in _constr_bases
-)
 
-
-@dataclass(frozen=True)
+@dataclass(frozen=True, init=False)
 class ContainerConstraint(AttrConstraint, ABC):
     r"""Internal base class for constraining the element type and shape of container types.
 
@@ -82,39 +75,46 @@ class ContainerConstraint(AttrConstraint, ABC):
 
     def __init__(
         self,
+        *,
         element_type: IRDLAttrConstraint[AttributeInvT] | None = None,
-        shape: IRDLAttrConstraint[AttributeInvT] | Sequence[int] | None = None,
-        rank: IRDLAttrConstraint[AttributeInvT] | Sequence[int] | int | None = None,
+        shape: IRDLAttrConstraint[AttributeInvT] | Iterable[int] | None = None,
+        rank: IRDLAttrConstraint[AttributeInvT] | Iterable[int] | int | None = None,
     ):
-        self.element_type = element_type or AnyAttr()
-        if shape is None and rank is None:
-            self.shape = AnyAttr()
-            return
+        element_type = element_type or AnyAttr()
+        element_type_constr = element_type
+        shape_constr = None
 
         if shape is not None and rank is not None:
             raise ValueError("Only one of 'shape' or 'rank' must be provided.")
 
-        if shape is not None:
-            if isinstance(shape, Sequence):
-                shape = ArrayAttr([IntAttr(s) for s in shape])
-            assert isinstance(shape, _valid_constraint_bases)
-            self.shape = shape
-            return
+        if shape is None and rank is None:
+            shape_constr = AnyAttr()
+
+        elif shape is not None:
+            shape_constr = shape
+            if isinstance(shape_constr, Iterable):
+                shape_constr = ArrayAttr([IntAttr(s) for s in shape])
 
         # rank is not None
-        if isinstance(rank, (int, Sequence)):
-            # Constrain shape to have length `rank` if `rank` is an int
-            if isinstance(rank, int):
-                length_constr = EqIntConstraint(rank)
-            else:
-                length_constr = IntSetConstraint(frozenset(rank))
-            rank = ArrayAttr.constr(
-                RangeLengthConstraint(
-                    constraint=RangeOf(IntAttrConstraint(AnyInt())), length=length_constr
+        else:
+            shape_constr = rank
+            if isinstance(shape_constr, (int, Iterable)):
+                # Constrain shape to have length `rank` if `rank` is an int
+                if isinstance(shape_constr, int):
+                    length_constr = EqIntConstraint(shape_constr)
+                else:
+                    length_constr = IntSetConstraint(frozenset(shape_constr))
+                shape_constr = ArrayAttr.constr(
+                    RangeLengthConstraint(
+                        constraint=RangeOf(IntAttrConstraint(AnyInt())), length=length_constr
+                    )
                 )
-            )
-        assert isinstance(rank, _valid_constraint_bases)
-        self.shape = rank
+
+        assert isinstance(element_type_constr, (Attribute, AttrConstraint))
+        assert isinstance(shape_constr, (Attribute, AttrConstraint))
+
+        object.__setattr__(self, "element_type", element_type_constr)
+        object.__setattr__(self, "shape", shape_constr)
 
     @property
     @abstractmethod
@@ -126,12 +126,16 @@ class ContainerConstraint(AttrConstraint, ABC):
         """The name of the type for use in error messages (e.g., 'tensor')."""
         return self.expected_type.name
 
+    def get_bases(self) -> set[type[Attribute]]:
+        """Get a set of base types that can satisfy this constraint (e.g., {builtin.TensorType})."""
+        return {self.expected_type}
+
     def verify(self, attr: Attribute, constraint_context: ConstraintContext) -> None:
         # pylint: disable=missing-function-docstring
         if not isinstance(attr, self.expected_type):
             raise VerifyException(f"{attr} should be of type {self.expected_type.__name__}.")
-        self.element_type.verify(attr.element_type, constraint_context=constraint_context)
-        self.shape.verify(attr.shape, constraint_context=constraint_context)
+        constr = self.expected_type.constr(element_type=self.element_type, shape=self.shape)
+        constr.verify(attr, constraint_context)
 
     def mapping_type_vars(
         self, type_var_mapping: dict[TypeVar, AttrConstraint]
@@ -140,7 +144,7 @@ class ContainerConstraint(AttrConstraint, ABC):
         return self
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, init=False)
 class TensorConstraint(ContainerConstraint):
     """TensorType constraint for element type and shape."""
 
@@ -149,7 +153,7 @@ class TensorConstraint(ContainerConstraint):
         return TensorType
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, init=False)
 class MemRefConstraint(ContainerConstraint):
     """MemRefType constraint for element type and shape."""
 
