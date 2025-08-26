f80c028759c130f6cfefc99ebc48a77483aaa9f7
Austin Huang <gengminghuang@gmail.com>
2025-08-26 15:20:55 -0400

    tests

--- pennylane/labs/tests/resource_estimation/test_resource_mapping.py
+++ pennylane/labs/tests/resource_estimation/test_resource_mapping.py
@@ -20,6 +20,9 @@ import pennylane as qml
 from pennylane.labs.resource_estimation import map_to_resource_op
 from pennylane.operation import Operation
 
+import pennylane.labs.resource_estimation.ops as re_ops
+import pennylane.templates as qtemps
+
 # pylint: disable= no-self-use
 
 
@@ -38,3 +41,58 @@ class Test_map_to_resource_op:
             NotImplementedError, match="Operation doesn't have a resource equivalent"
         ):
             map_to_resource_op(operation)
+
+
+    @pytest.mark.parametrize(
+        "operator, expected_res_op",
+        [
+            (qml.Identity(0), re_ops.ResourceIdentity()),
+            # Single-Qubit Gates
+            (qml.Hadamard(0), re_ops.ResourceHadamard()),
+            (qml.S(0), re_ops.ResourceS()),
+            (qml.T(0), re_ops.ResourceT()),
+            (qml.PauliX(0), re_ops.ResourceX()),
+            (qml.PauliY(0), re_ops.ResourceY()),
+            (qml.PauliZ(0), re_ops.ResourceZ()),
+            (qml.PhaseShift(0.1, wires=0), re_ops.ResourcePhaseShift()),
+            (qml.Rot(0.1, 0.2, 0.3, wires=0), re_ops.ResourceRot()),
+            (qml.RX(0.1, wires=0), re_ops.ResourceRX()),
+            (qml.RY(0.1, wires=0), re_ops.ResourceRY()),
+            (qml.RZ(0.1, wires=0), re_ops.ResourceRZ()),
+
+            # Two-Qubit Gates
+            (qml.SWAP(wires=(0, 1)), re_ops.ResourceSWAP()),
+            (qml.IsingXX(0.1, wires=(0, 1)), re_ops.ResourceIsingXX()),
+            (qml.IsingYY(0.1, wires=(0, 1)), re_ops.ResourceIsingYY()),
+            (qml.IsingXY(0.1, wires=(0, 1)), re_ops.ResourceIsingXY()),
+            (qml.IsingZZ(0.1, wires=(0, 1)), re_ops.ResourceIsingZZ()),
+            (qml.PSWAP(0.1, wires=(0, 1)), re_ops.ResourcePSWAP()),
+            (qml.SingleExcitation(0.1, wires=(0, 1)), re_ops.ResourceSingleExcitation()),
+            (qml.CH(wires=(0, 1)), re_ops.ResourceCH()),
+            (qml.CNOT(wires=(0, 1)), re_ops.ResourceCNOT()),
+            (qml.ControlledPhaseShift(0.1, wires=(0, 1)), re_ops.ResourceControlledPhaseShift()),
+            (qml.CRot(0.1, 0.2, 0.3, wires=(0, 1)), re_ops.ResourceCRot()),
+            (qml.CRX(0.1, wires=(0, 1)), re_ops.ResourceCRX()),
+            (qml.CRY(0.1, wires=(0, 1)), re_ops.ResourceCRY()),
+            (qml.CRZ(0.1, wires=(0, 1)), re_ops.ResourceCRZ()),
+            (qml.CY(wires=(0, 1)), re_ops.ResourceCY()),
+            (qml.CZ(wires=(0, 1)), re_ops.ResourceCZ()),
+            
+            # Three-Qubit Gates
+            (qml.CCZ(wires=(0, 1, 2)), re_ops.ResourceCCZ()),
+            (qml.CSWAP(wires=(0, 1, 2)), re_ops.ResourceCSWAP()),
+            (qml.Toffoli(wires=(0, 1, 2)), re_ops.ResourceToffoli()),
+            
+            # Multi-Qubit Gates
+            (qml.MultiRZ(0.1, wires=[0, 1, 2]), re_ops.ResourceMultiRZ()),
+            (qml.PauliRot(0.1, "XYZ", wires=[0, 1, 2]), re_ops.ResourcePauliRot()),
+            (qml.MultiControlledX(wires=[0, 1, 2]), re_ops.ResourceMultiControlledX()),
+
+            # Custom/Template Gates
+            (qtemps.TemporaryAND(wires=[0, 1, 2]), re_ops.ResourceTempAND()),
+        ],
+    )
+    def test_map_to_resource_op(self, operator, expected_res_op):
+        """Test that map_to_resource_op maps to the appropriate resource operator"""
+
+        assert map_to_resource_op(operator) == expected_res_op 
