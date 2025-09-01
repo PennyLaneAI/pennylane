7fed85585f1a224d25d6b0b9b1cdceed18e15419
qottmann <korbinian.kottmann@gmail.com>
2025-09-01 11:22:52 +0200

    move intermediate reps to mainline pennylane

--- pennylane/transforms/__init__.py
+++ pennylane/transforms/__init__.py
@@ -79,6 +79,8 @@ both transforms, and decompositions within the larger PennyLane codebase.
     ~transforms.pattern_matching
     ~transforms.to_zx
     ~transforms.from_zx
+    ~transforms.parity_matrix
+    ~transforms.phase_polynomials
 
 There are also utility functions that take a circuit and return a DAG.
 
@@ -348,6 +350,7 @@ from .zx import (
 )
 from .broadcast_expand import broadcast_expand
 from .decompose import decompose
+from .intermediate_reps import parity_matrix, phase_polynomial
 
 
 def __getattr__(name):
