8877a86191928409f48b650a3a810124a298f725
qottmann <korbinian.kottmann@gmail.com>
2025-09-01 17:48:09 +0200

    fully remove from labs

--- pennylane/labs/__init__.py
+++ pennylane/labs/__init__.py
@@ -28,7 +28,6 @@ Modules
     dla
     resource_estimation
     trotter_error
-    intermediate_reps
     zxopt
 
 """
@@ -36,7 +35,6 @@ Modules
 from pennylane.labs import dla
 from pennylane.labs import resource_estimation
 from pennylane.labs import trotter_error
-from pennylane.labs import intermediate_reps
 from pennylane.labs import zxopt
 
 __all__ = []
