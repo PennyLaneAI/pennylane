4dd4e8144779adc21da33672dbd1060e8d4ea04f
lillian542 <Lillian.frederiksen@xanadu.ai>
2025-09-05 15:54:24 -0400

    update tests

--- tests/ftqc/test_tape_prototype.py
+++ tests/ftqc/test_tape_prototype.py
@@ -60,20 +60,38 @@ def test_ftqc_device_initializes(backend_cls):
 
 
 @pytest.mark.parametrize(
-    "backend, expected_method",
+    "backend, preferred_method",
     [(LightningQubitBackend(), "one-shot"), (NullQubitBackend(), "device")],
 )
-@pytest.mark.parametrize(
-    "initial_config", [ExecutionConfig(mcm_config=MCMConfig(mcm_method="placeholder")), None]
-)
-def test_setup_execution_config(backend, expected_method, initial_config):
-    """Test that setup_execution_config works as expected"""
+@pytest.mark.parametrize("initial_config", (True, False))
+def test_setup_execution_config(backend, preferred_method, initial_config):
+    """Test that setup_execution_config works as expected when the input ExecutionConfig
+    is None or is consistent with the device MCM capabilities"""
 
     dev = FTQCQubit(wires=2, backend=backend)
+    initial_config = (
+        ExecutionConfig(mcm_config=MCMConfig(mcm_method=preferred_method))
+        if initial_config
+        else None
+    )
+
     config = dev.setup_execution_config(initial_config)
 
     assert isinstance(config, ExecutionConfig)
-    assert config.mcm_config.mcm_method == expected_method
+    assert config.mcm_config.mcm_method == preferred_method
+
+
+@pytest.mark.parametrize("backend", [LightningQubitBackend(), NullQubitBackend()])
+def test_invalid_mcm_method_raises_error(backend):
+    """Test that setup_execution_config raises en error for incompatible MCM methods"""
+
+    dev = FTQCQubit(wires=2, backend=backend)
+    with pytest.raises(
+        qml.exceptions.QuantumFunctionError, match='"placeholder" unsupported by the device'
+    ):
+        _ = dev.setup_execution_config(
+            ExecutionConfig(mcm_config=MCMConfig(mcm_method="placeholder"))
+        )
 
 
 @pytest.mark.parametrize("wires", ([0, 1], ["a", "b"]))
@@ -141,13 +159,16 @@ def test_executing_arbitrary_circuit_two_qubit_gate(wires, backend_cls):
 
     # the processed circuit is two tapes (split_non_commuting), returning
     # only samples, and expressed in the MBQC formalism
-    tapes, _ = qml.workflow.construct_batch(ftqc_circ, level="device")()
-    assert len(tapes) == 1
-    assert all(isinstance(mp, qml.measurements.SampleMP) for mp in tapes[0].measurements)
-    # CZ, H, MCMs, Conditional MCMs/Pauli corrections, + Adjoint for diagonalizing gates
-    assert all(
-        isinstance(op, (Conditional, CZ, H, MidMeasureMP, Adjoint)) for op in tapes[0].operations
-    )
+    sequences, _ = qml.workflow.construct_batch(ftqc_circ, level="device")()
+    assert len(sequences) == 1
+    assert isinstance(sequences[0], QuantumScriptSequence)
+
+    for tape in sequences[0]:
+        assert all(isinstance(mp, qml.measurements.SampleMP) for mp in tape.measurements)
+        # CZ, H, MCMs, Conditional MCMs/Pauli corrections, + Adjoint for diagonalizing gates
+        assert all(
+            isinstance(op, (Conditional, CZ, H, MidMeasureMP, Adjoint)) for op in tape.operations
+        )
 
     # circuit executes
     res = ftqc_circ()
