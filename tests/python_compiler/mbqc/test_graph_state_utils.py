128d693459492e8a967f0d29e0aed89950aecbaf
Shuli Shu <08cnbj@gmail.com>
2025-09-10 15:22:52 +0000

    suppress pylint complains

--- tests/python_compiler/mbqc/test_graph_state_utils.py
+++ tests/python_compiler/mbqc/test_graph_state_utils.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Unit test module for the graph state utils"""
+
+# pylint: disable=wrong-import-position
+
 import pytest
 
 pytestmark = pytest.mark.external
