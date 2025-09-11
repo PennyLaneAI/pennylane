a98df25df4d8e6b33bde694442c0fb76d08ec2e0
andrijapau <andrija.paurevic@xanadu.ai>
2025-09-11 15:40:25 -0400

    improve

--- pennylane/devices/qubit_mixed/apply_operation.py
+++ pennylane/devices/qubit_mixed/apply_operation.py
@@ -655,8 +655,7 @@ def apply_diagonal_unitary(op, state, is_state_batched: bool = False, debugger=N
 
     # Basically, we want to do, lambda_a rho_ab lambda_b
     # Use ellipsis to represent the batch dimensions as eigvals.shape = (batch, 2 * len(channel_wires))
-    is_batched_eigvals = eigvals.ndim == 2
-    ellipsis = "..." if is_batched_eigvals else ""
+    ellipsis = "..." if eigvals.ndim == 2 else ""
     einsum_indices = f"{ellipsis}{row_indices},{ellipsis}{state_indices},{ellipsis}{col_indices}->{ellipsis}{state_indices}"
 
     return math.einsum(einsum_indices, eigvals, state, math.conj(eigvals))
