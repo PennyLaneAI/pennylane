ca7780d08c59465dbe7ac7bb4ae48aa693b2e706
Austin Huang <gengminghuang@gmail.com>
2025-09-12 12:00:38 -0400

    tests

--- tests/estimator/test_resource_config.py
+++ tests/estimator/test_resource_config.py
@@ -16,28 +16,8 @@ Test the resource configuration class.
 """
 import pytest
 
-from pennylane.labs.resource_estimation.ops.op_math.controlled_ops import (
-    ResourceCRX,
-    ResourceCRY,
-    ResourceCRZ,
-)
-from pennylane.labs.resource_estimation.ops.qubit.parametric_ops_single_qubit import (
-    ResourceRX,
-    ResourceRY,
-    ResourceRZ,
-)
-from pennylane.labs.resource_estimation.resource_config import ResourceConfig
-from pennylane.labs.resource_estimation.resource_operator import ResourceOperator
-from pennylane.labs.resource_estimation.templates import ResourceSelectPauliRot
-from pennylane.labs.resource_estimation.templates.qubitize import ResourceQubitizeTHC
-from pennylane.labs.resource_estimation.templates.select import ResourceSelectTHC
-from pennylane.labs.resource_estimation.templates.stateprep import (
-    ResourceAliasSampling,
-    ResourceMPSPrep,
-    ResourcePrepTHC,
-    ResourceQROMStatePreparation,
-)
-from pennylane.labs.resource_estimation.templates.subroutines import ResourceQubitUnitary
+from pennylane.estimator.resource_config import ResourceConfig
+from pennylane.estimator.resource_operator import ResourceOperator
 
 # pylint: disable=protected-access, unused-argument
 
@@ -65,73 +45,6 @@ class TestResourceConfig:
         assert config._adj_custom_decomps == {}
         assert config._ctrl_custom_decomps == {}
 
-    @pytest.mark.parametrize(
-        "op_with_precision",
-        [
-            ResourceRX,
-            ResourceRY,
-            ResourceRZ,
-            ResourceCRX,
-            ResourceCRY,
-            ResourceCRZ,
-            ResourceSelectPauliRot,
-            ResourceQubitUnitary,
-            ResourceQROMStatePreparation,
-            ResourceMPSPrep,
-            ResourceAliasSampling,
-        ],
-    )
-    def test_initialization_sets_default_precision(self, op_with_precision):
-        """Test that default precision is set for standard operations on initialization."""
-        config = ResourceConfig()
-        assert op_with_precision in config.resource_op_precisions
-        assert config.resource_op_precisions[op_with_precision].get("precision") > 0
-
-    @pytest.mark.parametrize(
-        "op_with_rotation_and_coeff_precision",
-        [
-            ResourceQubitizeTHC,
-            ResourceSelectTHC,
-            ResourcePrepTHC,
-        ],
-    )
-    def test_initialization_sets_default_rotation_precision(
-        self, op_with_rotation_and_coeff_precision
-    ):
-        """Test that default precision is set for standard operations on initialization."""
-        config = ResourceConfig()
-        assert op_with_rotation_and_coeff_precision in config.resource_op_precisions
-        assert (
-            config.resource_op_precisions[op_with_rotation_and_coeff_precision].get(
-                "rotation_precision"
-            )
-            > 0
-        )
-        assert (
-            config.resource_op_precisions[op_with_rotation_and_coeff_precision].get(
-                "coeff_precision"
-            )
-            > 0
-        )
-
-    @pytest.mark.parametrize(
-        "rotation_op",
-        [
-            ResourceRX,
-            ResourceRY,
-            ResourceRZ,
-            ResourceCRX,
-            ResourceCRY,
-            ResourceCRZ,
-        ],
-    )
-    def test_set_single_qubit_rotation_precision_for_rotation_ops(self, rotation_op):
-        """Test that the single qubit rotation error is set correctly for a given rotation gate."""
-        config = ResourceConfig()
-        new_error = 1e-5
-        config.set_single_qubit_rot_precision(new_error)
-
-        assert config.resource_op_precisions[rotation_op]["precision"] == new_error
 
     # pylint: disable=use-implicit-booleaness-not-comparison
     @pytest.mark.parametrize(
@@ -199,37 +112,6 @@ class TestResourceConfig:
 
         assert repr_str == expected_repr_str
 
-    def test_set_decomp_invalid_type_raises_error(self):
-        """Test that set_decomp raises a ValueError for an invalid decomposition type."""
-        config = ResourceConfig()
-        invalid_type_string = "this_is_not_a_valid_type"
-
-        def dummy_decomp(**kwargs):
-            """A placeholder decomposition function."""
-            return []
-
-        with pytest.raises(
-            ValueError, match=f"'{invalid_type_string}' is not a valid DecompositionType"
-        ):
-            config.set_decomp(ResourceRX, dummy_decomp, decomp_type=invalid_type_string)
-
-    @pytest.mark.parametrize(
-        "op_type_to_set",
-        [
-            ResourceSelectPauliRot,
-            ResourceRX,
-            ResourceQubitUnitary,
-        ],
-    )
-    def test_set_precision_updates_correctly(self, op_type_to_set):
-        """Test that set_precision correctly updates the precision for a valid operator."""
-        config = ResourceConfig()
-        new_precision = 1e-5
-
-        assert config.resource_op_precisions[op_type_to_set]["precision"] != new_precision
-
-        config.set_precision(op_type_to_set, new_precision)
-        assert config.resource_op_precisions[op_type_to_set]["precision"] == new_precision
 
     def test_set_precision_raises_error_for_unknown_op(self):
         """Test that set_precision raises ValueError for an unknown operator."""
@@ -238,20 +120,6 @@ class TestResourceConfig:
         with pytest.raises(ValueError, match=match_str):
             config.set_precision(DummyOp, 0.123)
 
-    @pytest.mark.parametrize(
-        "unsupported_op",
-        [
-            ResourceQubitizeTHC,
-            ResourceSelectTHC,
-            ResourcePrepTHC,
-        ],
-    )
-    def test_set_precision_raises_error_for_unsupported_op(self, unsupported_op):
-        """Test ValueError for known operators that do not support single-precision setting."""
-        config = ResourceConfig()
-        match_str = f"Setting precision for {unsupported_op.__name__} is not supported."
-        with pytest.raises(ValueError, match=match_str):
-            config.set_precision(unsupported_op, 0.456)
 
     def test_set_single_qubit_rot_precision_raises_for_negative_value(self):
         """Test that set_single_qubit_rot_precision raises a ValueError for a negative precision."""
@@ -260,9 +128,3 @@ class TestResourceConfig:
 
         with pytest.raises(ValueError, match="Precision must be a non-negative value"):
             config.set_single_qubit_rot_precision(negative_precision)
-
-    def test_set_precision_raises_for_negative_value(self):
-        """Test that set_precision raises a ValueError for a negative precision."""
-        config = ResourceConfig()
-        with pytest.raises(ValueError, match="Precision must be a non-negative value"):
-            config.set_precision(ResourceRX, precision=-1e-5)
