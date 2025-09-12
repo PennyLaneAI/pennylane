9ff0b7562df95b110fbc44ea6aaab16367c66dde
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2025-09-12 12:18:51 -0400

    Update pennylane/estimator/resource_operator.py
    Co-authored-by: Christina Lee <christina@xanadu.ai>

--- pennylane/estimator/resource_operator.py
+++ pennylane/estimator/resource_operator.py
@@ -18,7 +18,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from collections import defaultdict
 from collections.abc import Hashable, Iterable
-from typing import Any, Union
+from typing import Any
 
 import numpy as np
 
