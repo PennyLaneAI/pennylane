683c9a04a21da9752444dd1d4642eb7f00a5f46a
Mudit Pandey <mudit.pandey@xanadu.ai>
2025-09-16 14:28:33 -0400

    Update map to use a single dict

--- pennylane/compiler/python_compiler/rewriter/wire_qubit_map.py
+++ pennylane/compiler/python_compiler/rewriter/wire_qubit_map.py
@@ -19,6 +19,7 @@ from uuid import UUID, uuid4
 
 from xdsl.dialects import builtin
 from xdsl.ir import SSAValue
+from xdsl.utils.hints import isa
 
 from ..dialects import quantum
 
@@ -28,26 +29,25 @@ class AbstractWire:
     """An abstract wire."""
 
     qreg: quantum.QuregSSAValue | None = None
-    """The SSA quantum register from which the qubit corresponding to this
-    abstract wire has been extracted. This is used to differentiate between
-    qubits allocated in static and dynamic registers. For qubits that are
-    allocated dynamically without the use of a register, this is ``None``."""
+    """The SSA quantum register from which the qubit corresponding to this abstract
+    wire has been extracted. This is used to differentiate between qubits allocated
+    in static and dynamic registers. For qubits that are allocated dynamically
+    without the use of a register, this is ``None``."""
 
     idx: SSAValue[builtin.I64] | None = None
-    """The SSA integer value that corresponds to the index of the qubit
-    in the quantum register. This is used for qubits that are allocated
-    in the quantum register. For qubits that are dynamically allocated
-    individually, this is ``None``."""
+    """The SSA integer value that corresponds to the index of the qubit in the quantum
+    register. This is used for qubits that are allocated in the quantum register. For
+    qubits that are dynamically allocated individually, this is ``None``."""
 
-    id: UUID = field(default_factory=uuid4, init=False)
-    """A universally unique identifier for the abstract wire. Used when
-    qubits are dynamically allocated, since such qubits are not in the
-    quantum register."""
+    uuid: UUID = field(default_factory=uuid4, init=False)
+    """A universally unique identifier for the abstract wire. Used when qubits are
+    dynamically allocated individually, since such qubits are not in a quantum
+    register."""
 
     def __hash__(self) -> int:
         if self.qreg and self.idx:
             return hash(self.qreg) + hash(self.idx)
-        return hash(self.id)
+        return hash(self.uuid)
 
     def __eq__(self, other) -> bool:
         if not isinstance(other, AbstractWire):
@@ -55,7 +55,7 @@ class AbstractWire:
 
         if self.qreg and other.qreg and self.idx and other.idx:
             return self.qreg == other.qreg and self.idx == other.idx
-        return self.id == other.id
+        return self.uuid == other.uuid
 
 
 @dataclass
@@ -65,120 +65,145 @@ class WireQubitMap:
     wires: tuple[int, ...] | None = None
     """Tuple containing all available static wire labels. None if not provided."""
 
-    from_wires: dict[int | AbstractWire, list[quantum.QubitSSAValue]] = field(
-        default_factory=dict, init=False
-    )
-    """Map from wire labels to a list of all known qubit SSAValues to which they correspond."""
+    _map: dict[
+        int | AbstractWire | quantum.QubitSSAValue, int | AbstractWire | list[quantum.QubitSSAValue]
+    ] = field(default_factory=dict, init=False)
+    """Internal map that stores the bidirectional mapping between wire labels and
+    SSA qubits. If the key is an integer or ``AbstractWire``, then the value will be
+    a list of all SSA qubits that correspond to that wire label. If the key is an
+    SSA qubit, then the value will be the wire (integer or ``AbstractWire``) to which
+    it corresponds."""
 
-    from_qubits: dict[quantum.QubitSSAValue, int | AbstractWire] = field(
-        default_factory=dict, init=False
+    _valid_types: tuple[type, ...] = field(
+        default=(int, AbstractWire, quantum.QubitSSAValue), init=False
     )
-    """Map from qubit SSAValues to their corresponding wire labels."""
+    """The allowed types for the keys and values in the bidirectional map."""
+
+    def _assert_valid_type(self, obj: Any) -> None:
+        """Check if the given value is valid. Valid types are ``int``, ``AbstractWire``,
+        or ``xdsl.ir.SSAValue`` of type ``quantum.QubitType``. If not, raise an error."""
+
+        # We use ``xdsl.utils.hints.isa`` here instead of ``isinstance`` because that is
+        # designed to allow checking the input object againt generic types, which
+        # ``isinstance`` cannot handle.
+        if not any(isa(obj, hint) for hint in self._valid_types):
+            raise AssertionError(
+                f"{obj} is not a valid key or value for a WireQubitMap. Valid keys and values "
+                f"must be of one of the following types: {self._valid_types}."
+            )
+
+    def _assert_valid_wire_label(self, wire: int | AbstractWire) -> None:
+        """Check if the given wire label is valid. Valid wire labels are either of type
+        ``AbstractWire``, or integers that belong to ``self.wires``, if provided."""
+        if isinstance(wire, int) and self.wires is not None and wire not in self.wires:
+            raise AssertionError(
+                f"{wire} is not a valid wire label. Static wire labels must belong "
+                f"to {self.wires}."
+            )
 
     def __contains__(self, key: int | AbstractWire | quantum.QubitSSAValue) -> bool:
         """Check if the map contains a wire label or qubit."""
-        return key in self.from_wires or key in self.from_qubits
+        self._assert_valid_type(key)
+        return key in self._map
 
     def __getitem__(
         self, key: int | AbstractWire | quantum.QubitSSAValue
     ) -> int | AbstractWire | list[quantum.QubitSSAValue]:
         """Get a value from the wire/qubit maps."""
-        if isinstance(key, SSAValue):
-            return self.from_qubits[key]
-
-        return self.from_wires[key]
+        self._assert_valid_type(key)
+        return self._map[key]
 
     def __setitem__(
         self,
         key: int | AbstractWire | quantum.QubitSSAValue,
-        val: quantum.QubitSSAValue | int | AbstractWire,
+        value: quantum.QubitSSAValue | int | AbstractWire,
     ) -> None:
         """Update the wire/qubit maps."""
-        if isinstance(key, SSAValue):
-            if not isinstance(key.type, quantum.QubitType):
-                raise KeyError(
-                    "Expected key to be a QubitType SSAValue, instead got SSAValue "
-                    f"with type {key.type}"
-                )
+        self._assert_valid_type(key)
+        self._assert_valid_type(value)
 
-            if key in self.from_qubits:
-                raise ValueError("Cannot update qubits that are already in the map.")
+        if isa(key, quantum.QubitSSAValue):
+            if key in self._map:
+                raise KeyError("Cannot update qubits that are already in the map.")
 
-            assert isinstance(val, (int, AbstractWire))
-            if isinstance(val, int) and self.wires is not None and val not in self.wires:
-                raise ValueError(f"{val} is not an available wire.")
-
-            self.from_qubits[key] = val
-            qubits = self.from_wires.setdefault(val, [])
+            self._assert_valid_wire_label(value)
+            self._map[key] = value
+            qubits = self._map.setdefault(value, [])
             qubits.append(key)
             return
 
-        if isinstance(key, (int, AbstractWire)):
-            if isinstance(key, int) and self.wires is not None and key not in self.wires:
-                raise KeyError(f"{key} is not an available wire.")
-            if not isinstance(val, SSAValue) or not isinstance(val.type, quantum.QubitType):
-                raise ValueError(f"Expected value to be a QubitType SSAValue, instead got {val}.")
-            if val in self.from_qubits:
-                raise ValueError("Cannot update qubits that are already in the map.")
+        self._assert_valid_wire_label(key)
+        if value in self._map:
+            raise ValueError("Cannot update qubits that are already in the map.")
 
-            qubits = self.from_wires.setdefault(key, [])
-            qubits.append(val)
-            self.from_qubits[val] = key
-            return
-
-        raise KeyError(f"{key} is not a valid wire label or QubitType SSAValue.")
+        qubits = self._map.setdefault(key, [])
+        qubits.append(value)
+        self._map[value] = key
 
     def get(
         self, key: int | AbstractWire | quantum.QubitSSAValue, default: Any | None = None
     ) -> int | AbstractWire | list[quantum.QubitSSAValue] | None:
         """Return an item from the map without removing it, if it exists. Else, return
         the provided default value."""
-        if isinstance(key, SSAValue):
-            wire = self.from_qubits.get(key, default)
-            return wire
-
-        qubit = self.from_wires.get(key, default)
-        return qubit
+        self._assert_valid_type(key)
+        return self._map.get(key, default)
 
     def pop(
         self, key: int | AbstractWire | quantum.QubitSSAValue, default: Any | None = None
     ) -> int | AbstractWire | list[quantum.QubitSSAValue] | None:
         """Remove and return an item from the map, if it exists. Else, return the
         provided default value."""
-        if isinstance(key, SSAValue):
-            wire = self.from_qubits.pop(key, default)
-            qubits = self.from_wires[wire]
+        self._assert_valid_type(key)
+
+        if key not in self._map:
+            return default
+
+        # At this point, we know that the given key has to be in the map
+        if isa(key, quantum.QubitSSAValue):
+            wire = self._map.pop(key)
+            qubits = self._map[wire]
             _ = qubits.pop(qubits.index(key))
-            if len(qubits) == 0:
-                _ = self.from_wires.pop(wire, None)
             return wire
 
-        qubits = self.from_wires.pop(key, default)
+        qubits = self._map.pop(key)
         for q in qubits:
-            _ = self.from_qubits.pop(q, None)
+            _ = self._map.pop(q)
         return qubits
 
     def verify(self):
-        """Check if the map is valid."""
-        all_qubits_from_wires = []
+        """Check if the map is valid. To be valid, the map must satisfy the following conditions:
 
-        for qubits in self.from_wires.values():
-            all_qubits_from_wires.extend(qubits)
+            * The items inside the map can only be of type ``int``, ``AbstractWire``,
+              or ``SSAValue[QubitType]``
+            * All wire keys and wire values must be valid wire labels, i.e. belong to
+              ``self.wires`` if they are static.
+            * The set of wire keys is equal to the set of wire labels.
+            * The set of qubit keys is equal to the set of qubit values.
+            * There is no intersection between the lists of qubit values.
 
-        all_qubits_from_wires_set = set(all_qubits_from_wires)
-        assert len(all_qubits_from_wires_set) == len(
-            all_qubits_from_wires
-        ), "There is more than one wire that maps to the same qubit."
+        If any of the above conditions are not met, an error will be raised.
+        """
+        qubit_keys = set()
+        wire_keys = set()
+        all_qubit_values = []
+        wire_values = set()
 
-        all_qubits_from_qubits_set = set(self.from_qubits.keys())
-        assert (
-            all_qubits_from_wires_set == all_qubits_from_qubits_set
-        ), """The wire-qubit map is invalid. There is a mismatch between the
-        number of qubits in both internal maps."""
+        for k, v in self._map.items():
+            self._assert_valid_type(k)
+            self._assert_valid_type(v)
+            if isa(k, quantum.QubitSSAValue):
+                qubit_keys.add(k)
+                self._assert_valid_wire_label(v)
+                wire_values.add(v)
+            else:
+                self._assert_valid_wire_label(k)
+                wire_keys.add(k)
+                all_qubit_values.extend(v)
 
-        all_wires_from_wires_set = set(self.from_wires.keys())
-        all_wires_from_qubits_set = set(self.from_qubits.values())
-        assert (
-            all_wires_from_wires_set == all_wires_from_qubits_set
-        ), """The wire-qubit map is invalid. There is a mismatch between the
-        number of wires in both internal maps."""
+        assert wire_keys == wire_values, "The wire label keys do not match the wire label values."
+
+        all_qubit_values_set = set(all_qubit_values)
+        assert qubit_keys == all_qubit_values_set, "The qubit keys do not match the qubit values."
+        assert len(all_qubit_values) == len(
+            all_qubit_values_set
+        ), "Multiple wires are being mapped to the same qubit."
