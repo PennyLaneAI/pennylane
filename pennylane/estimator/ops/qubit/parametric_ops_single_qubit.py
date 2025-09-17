2040e981f407655b6cc576e3df68515968996bee
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2025-09-17 10:15:49 -0400

    Update pennylane/estimator/ops/qubit/parametric_ops_single_qubit.py
    Co-authored-by: Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>

--- pennylane/estimator/ops/qubit/parametric_ops_single_qubit.py
+++ pennylane/estimator/ops/qubit/parametric_ops_single_qubit.py
@@ -451,8 +451,8 @@ class RY(ResourceOperator):
     @classmethod
     def controlled_resource_decomp(
         cls,
-        num_ctrl_wires,
-        num_zero_ctrl,
+        num_ctrl_wires: int,
+        num_zero_ctrl: int,
         target_resource_params: dict,
     ) -> list[GateCount]:
         r"""Returns a list representing the resources for a controlled version of the operator.
