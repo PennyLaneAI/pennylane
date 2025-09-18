1508695bf4108865cb2f8a8d9c93b4264113ea94
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2025-09-18 14:08:34 -0400

    Update pennylane/estimator/ops/op_math/controlled_ops.py
    Co-authored-by: ANT0N <39093564+AntonNI8@users.noreply.github.com>

--- pennylane/estimator/ops/op_math/controlled_ops.py
+++ pennylane/estimator/ops/op_math/controlled_ops.py
@@ -830,7 +830,7 @@ class TemporaryAND(ResourceOperator):
     Args:
         wires (Sequence[int] | None): the wires the operation acts on
 
-    This gate was introduced in Fig 4 of `Babbush 2018 <https://arxiv.org/pdf/1805.03662>`_ along
+    This gate was introduced in Fig 4 of `Babbush et al. (2018) <https://arxiv.org/pdf/1805.03662>`_ along
     with its adjoint.
 
     .. seealso:: The corresponding PennyLane operation :class:`~.pennylane.TemporaryAND`.
