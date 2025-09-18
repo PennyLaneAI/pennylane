0b8ad2c36bd2bae43f9141965242a100839547c6
soranjh <soran.jahangiri@gmail.com>
2025-09-18 17:41:16 -0400

    update import

--- pennylane/estimator/templates/stateprep.py
+++ pennylane/estimator/templates/stateprep.py
@@ -18,7 +18,7 @@ from typing import Dict
 import pennylane.numpy as np
 import pennylane.estimator as qre
 from pennylane.estimator.wires_manager import Allocate, Deallocate
-from pennylane.estimator import (
+from pennylane.estimator.resource_operator import (
     CompressedResourceOp,
     GateCount,
     ResourceOperator,
