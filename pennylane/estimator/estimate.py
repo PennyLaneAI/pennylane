366be7c619553b0d9d1b4a4325884054d81e976c
Austin Huang <gengminghuang@gmail.com>
2025-09-18 11:08:53 -0400

    address

--- pennylane/estimator/estimate.py
+++ pennylane/estimator/estimate.py
@@ -353,6 +353,6 @@ def _get_decomposition(
     # TODO: Restore logic to handle symbolic operators when symboli operators are merged.
 
     kwargs = config.resource_op_precisions.get(op_type, {})
-    decomp_func = config._custom_decomps.get(op_type, op_type.resource_decomp)
+    decomp_func = config.custom_decomps.get(op_type, op_type.resource_decomp)
 
     return decomp_func, kwargs
