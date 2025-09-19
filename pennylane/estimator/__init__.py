134ce27fba21255449160dd19bb77bbd7ac9858c
AntonNI8 <AntonNI8@users.noreply.github.com>
2025-09-19 17:45:35 -0400

    fix rebase

--- pennylane/estimator/__init__.py
+++ pennylane/estimator/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-r"""This module contains tools for logical resource estimation."""
+r"""This module contains tools for dedicated logical resource estimation."""
 
 from .wires_manager import Allocate, Deallocate, WireResourceManager
 
