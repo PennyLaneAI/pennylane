57054dd1dc5131e28ac32d611611cc48df113979
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2025-09-29 16:53:32 -0400

    Apply suggestions from code review

--- tests/estimator/ops/test_estimator_parametric_single_qubit.py
+++ tests/estimator/ops/test_estimator_parametric_single_qubit.py
@@ -186,6 +186,7 @@ class TestPauliRotation:
         with pytest.raises(ValueError, match="Expected 1 wires, got 2"):
             resource_op_class(wires=[0, 1])
 
+
 class TestRot:
     """Test ResourceRot"""
 
@@ -288,6 +289,7 @@ class TestRot:
         """Test that an error is raised when wrong number of wires is provided."""
         with pytest.raises(ValueError, match="Expected 1 wires, got 2"):
             Rot(wires=[0, 1])
+
 
 class TestPhaseShift:
     """Test PhaseShift"""
