a7c33dec5def39a69b1777b554ce8ebad0de2612
Jay Soni <jbsoni@uwaterloo.ca>
2025-10-02 17:47:55 -0400

    bugfix pr

--- tests/estimator/test_estimator_mapping.py
+++ tests/estimator/test_estimator_mapping.py
@@ -195,3 +195,27 @@ class TestMapToResourceOp:
     def test_map_to_resource_op_templates(self, operator, expected_res_op):
         """Test that _map_to_resource_op maps templates to the appropriate resource operator"""
         assert _map_to_resource_op(operator) == expected_res_op
+
+    def test_map_from_decomposition(self):
+        """Test that the decomposition is used when a resource equivalent is not defined"""
+
+        class DummyOp(Operation):
+            num_wires = 2
+
+            def __init__(self, theta, wires=None, id=None):
+                super().__init__(theta, wires=wires, id=id)
+
+            @staticmethod
+            def compute_decomposition(theta, wires):
+                return [qml.RX(theta, wires[0]), qml.CNOT(wires)]
+
+        def actual_circ():
+            DummyOp(theta=1.23, wires=[0, 1])
+            return
+
+        def expected_circ():
+            re_ops.RX(wires=0)
+            re_ops.CNOT(wires=[0, 1])
+            return
+
+        assert re_ops.estimate(actual_circ)() == re_ops.estimate(expected_circ)()
