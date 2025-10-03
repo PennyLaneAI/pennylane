5630389e1d367d365f503b2fac5674ec66ed655f
AntonNI8 <AntonNI8@users.noreply.github.com>
2025-10-03 11:16:11 +0200

    minor

--- pennylane/estimator/resource_config.py
+++ pennylane/estimator/resource_config.py
@@ -18,9 +18,9 @@ from collections.abc import Callable
 from enum import StrEnum
 from typing import TYPE_CHECKING
 
-from pennylane.estimator.ops.qubit.parametric_ops_single_qubit import RX, RY, RZ
 from pennylane.estimator.ops.op_math.controlled_ops import CRX, CRY, CRZ
 from pennylane.estimator.ops.qubit.matrix_ops import QubitUnitary
+from pennylane.estimator.ops.qubit.parametric_ops_single_qubit import RX, RY, RZ
 from pennylane.estimator.templates import (
     AliasSampling,
     MPSPrep,
