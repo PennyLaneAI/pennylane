5716fea1509c1017420a3b83ef0ec81ba87aee6d
andrijapau <andrija.paurevic@xanadu.ai>
2025-10-09 16:43:51 -0400

    doctest skip

--- pennylane/transforms/zx/converter.py
+++ pennylane/transforms/zx/converter.py
@@ -298,8 +298,8 @@ def to_zx(tape, expand_measurements=False):
                 return qml.expval(qml.Z(0))
 
         >>> result = mod_5_4()
-        >>> print(np.isclose(result, 1.0))
-        True
+        >>> print(result) # doctest: +SKIP
+        1.0
 
     .. note::
 
