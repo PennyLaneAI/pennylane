e4789727b9e2c3899d9b544ef4bfdbfaaf7b1db8
Shuli Shu <08cnbj@gmail.com>
2025-10-10 19:58:19 +0000

    revise the unit tests

--- tests/python_compiler/transforms/mbqc/test_xdsl_outline_state_evolution.py
+++ tests/python_compiler/transforms/mbqc/test_xdsl_outline_state_evolution.py
@@ -34,16 +34,26 @@ from pennylane.compiler.python_compiler.transforms import (
 from pennylane.ftqc import RotXZX
 
 
+@qml.while_loop(lambda i: i < 1000)
+def _while_for(i):
+    qml.H(i)
+    qml.S(i)
+    RotXZX(0.1, 0.2, 0.3, wires=[i])
+    qml.RZ(phi=0.1, wires=[i])
+    i = i + 1
+    return i
+
+
 class TestOutlineStateEvolutionPass:
     """Unit tests for OutlineStateEvolutionPass."""
 
     @pytest.mark.usefixtures("enable_disable_plxpr")
-    def test_outline_state_evolution_pass_lower(self, run_filecheck_qjit):
-        """Test if outline_state_evolution_pass works would create a state_evolution funcOp."""
-        dev = qml.device("null.qubit", wires=1000)
+    def test_outline_state_evolution_pass_only(self, run_filecheck_qjit):
+        """Test the outline_state_evolution_pass only."""
+        dev = qml.device("lightning.qubit", wires=1000)
 
-        @qml.while_loop(lambda i: i > 1000)
-        def while_for(i):
+        @qml.while_loop(lambda i: i < 1000)
+        def _while_for(i):
             qml.H(i)
             qml.S(i)
             RotXZX(0.1, 0.2, 0.3, wires=[i])
@@ -54,24 +64,136 @@ class TestOutlineStateEvolutionPass:
         @qml.qjit(
             target="mlir",
             pass_plugins=[getXDSLPluginAbsolutePath()],
-            pipelines=mbqc_pipeline(),
-            autograph=True,
+            autograph=False,
         )
         @outline_state_evolution_pass
         @qml.set_shots(1000)
         @qml.qnode(dev)
         def circuit():
-            # CHECK: func.call @circuit.state_evolution
-            # CHECK: func.func private @circuit.state_evolution
-            while_for(0)
+            # CHECK-LABEL: func.func public @circuit()
+            # CHECK-NOT: scf.while
+            # CHECK-NOT: quantum.custom "Hadamard"()
+            # CHECK-NOT: quantum.custom "S"()
+            # CHECK-NOT: quantum.custom "RotXZX"
+            # CHECK-NOT: quantum.custom "RZ"
+            # CHECK-NOT: quantum.custom "CNOT"
+            # CHECK-NOT: func.func private @circuit.state_evolution
+            # CHECK: quantum.alloc
+            # CHECK-NEXT: func.call @circuit.state_evolution
+            # CHECK-LABEL: func.func private @circuit.state_evolution
+            # CHECK-NOT: quantum.alloc
+            # CHECK-NOT: quantum.namedobs
+            # CHECK: scf.while
+            # CHECK: quantum.custom "Hadamard"()
+            # CHECK: quantum.custom "S"()
+            # CHECK: quantum.custom "RotXZX"
+            # CHECK: quantum.custom "RZ"
+            # CHECK: quantum.custom "CNOT"
+            _while_for(0)
             qml.CNOT(wires=[0, 1])
             return qml.expval(qml.Z(wires=0))
 
         run_filecheck_qjit(circuit)
 
     @pytest.mark.usefixtures("enable_disable_plxpr")
-    def test_gates_in_mbqc_gate_set_e2e_wo_for_loop(self):
-        """Test that the outline_state_evolution_pass end to end on null.qubit."""
+    def test_outline_state_evolution_pass_with_convert_to_mbqc_formalism(self, run_filecheck_qjit):
+        """Test if the outline_state_evolution_pass works with the convert-to-mbqc-fromalism pass on lightning.qubit."""
+        dev = qml.device("lightning.qubit", wires=1000)
+
+        @qml.qjit(
+            target="mlir",
+            pass_plugins=[getXDSLPluginAbsolutePath()],
+            pipelines=mbqc_pipeline(),
+            autograph=True,
+        )
+        @decompose_graph_state_pass
+        @convert_to_mbqc_formalism_pass
+        @outline_state_evolution_pass
+        @qml.set_shots(1000)
+        @qml.qnode(dev)
+        def circuit():
+            # CHECK-LABEL: func.func public @circuit()
+            # CHECK-NOT: quantum.custom "Hadamard"()
+            # CHECK-NOT: quantum.custom "S"()
+            # CHECK-NOT: quantum.custom "RotXZX"
+            # CHECK-NOT: quantum.custom "RZ"
+            # CHECK-NOT: quantum.custom "CNOT"()
+            # CHECK-NOT: mbqc.measure_in_basis
+            # CHECK-NOT: scf.if
+            # CHECK-NOT: quantum.dealloc_qb
+            # CHECK-LABEL: func.func private @circuit.state_evolution
+            # CHECK-NOT: quantum.custom "S"()
+            # CHECK-NOT: quantum.custom "RotXZX"
+            # CHECK-NOT: quantum.custom "RZ"
+            # CHECK-NOT: quantum.custom "CNOT"()
+            # CHECK-NOT: quantum.namedobs
+            # CHECK: quantum.custom "Hadamard"()
+            # CHECK: quantum.custom "CZ"()
+            # CHECK: mbqc.measure_in_basis
+            # CHECK: scf.if
+            # CHECK: quantum.custom "PauliX"()
+            # CHECK: quantum.custom "PauliZ"()
+            # CHECK: quantum.dealloc_qb
+            qml.H(0)
+            qml.S(1)
+            RotXZX(0.1, 0.2, 0.3, wires=[2])
+            qml.RZ(phi=0.1, wires=[3])
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.X(wires=0))
+
+        run_filecheck_qjit(circuit)
+
+    @pytest.mark.usefixtures("enable_disable_plxpr")
+    def test_outline_state_evolution_pass_with_mbqc_pipeline(self, run_filecheck_qjit):
+        """Test if the outline_state_evolution_pass works with all mbqc transform pipeline on null.qubit."""
+        dev = qml.device("null.qubit", wires=1000)
+
+        @qml.qjit(
+            target="mlir",
+            pass_plugins=[getXDSLPluginAbsolutePath()],
+            pipelines=mbqc_pipeline(),
+            autograph=True,
+        )
+        @decompose_graph_state_pass
+        @convert_to_mbqc_formalism_pass
+        @measurements_from_samples_pass
+        @diagonalize_final_measurements_pass
+        @outline_state_evolution_pass
+        @qml.set_shots(1000)
+        @qml.qnode(dev)
+        def circuit():
+            # CHECK-LABEL: func.func public @circuit()
+            # NOTE: There is scf.if, mbqc.measure_in_basis in the circuit()
+            # scope as X obs is decomposed into HZ and H is converted to MBQC formalism.
+            # CHECK-NOT: quantum.custom "S"()
+            # CHECK-NOT: quantum.custom "RotXZX"
+            # CHECK-NOT: quantum.custom "RZ"
+            # CHECK-NOT: quantum.custom "CNOT"()
+            # CHECK-LABEL: func.func private @circuit.state_evolution
+            # CHECK-NOT: quantum.custom "S"()
+            # CHECK-NOT: quantum.custom "RotXZX"
+            # CHECK-NOT: quantum.custom "RZ"
+            # CHECK-NOT: quantum.custom "CNOT"()
+            # CHECK-NOT: quantum.namedobs
+            # CHECK: quantum.custom "Hadamard"()
+            # CHECK: quantum.custom "CZ"()
+            # CHECK: mbqc.measure_in_basis
+            # CHECK: scf.if
+            # CHECK: quantum.custom "PauliX"()
+            # CHECK: quantum.custom "PauliZ"()
+            # CHECK: quantum.dealloc_qb
+            qml.H(0)
+            qml.S(1)
+            RotXZX(0.1, 0.2, 0.3, wires=[2])
+            qml.RZ(phi=0.1, wires=[3])
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.X(wires=0))
+
+        run_filecheck_qjit(circuit)
+
+    @pytest.mark.usefixtures("enable_disable_plxpr")
+    def test_outline_state_evolution_pass_with_mbqc_pipeline_run_on_nullqubit(self):
+        """Test if a circuit can be transfored with the outline_state_evolution_pass and all mbqc transform pipeline can be executed on null.qubit."""
         dev = qml.device("null.qubit", wires=1000)
 
         @qml.qjit(
@@ -96,100 +218,11 @@ class TestOutlineStateEvolutionPass:
             return qml.expval(qml.Z(wires=0))
 
         res = circuit()
-        assert res == 1.0
+        assert res == 0.0
 
     @pytest.mark.usefixtures("enable_disable_plxpr")
-    def test_circ_with_loop(self):
-        """Test that the outline_state_evolution_pass end to end on null.qubit."""
-        dev = qml.device("null.qubit", wires=1000)
-
-        @qml.for_loop(0, 1000, 1)
-        def for_fn(i):
-            qml.H(i)
-            qml.S(i)
-            RotXZX(0.1, 0.2, 0.3, wires=[i])
-            qml.RZ(phi=0.1, wires=[i])
-
-        @qml.while_loop(lambda i: i > 1000)
-        def while_fn(i):
-            qml.H(i)
-            qml.S(i)
-            RotXZX(0.1, 0.2, 0.3, wires=[i])
-            qml.RZ(phi=0.1, wires=[i])
-            i = i + 1
-            return i
-
-        @qml.qjit(
-            target="mlir",
-            pass_plugins=[getXDSLPluginAbsolutePath()],
-            pipelines=mbqc_pipeline(),
-            autograph=True,
-        )
-        @decompose_graph_state_pass
-        @convert_to_mbqc_formalism_pass
-        @outline_state_evolution_pass
-        @qml.set_shots(1000)
-        @qml.qnode(dev)
-        def circuit():
-            for_fn()
-            while_fn(0)
-            qml.CNOT(wires=[0, 1])
-            return qml.expval(qml.prod(qml.X(0), qml.Z(1))), qml.expval(
-                qml.sum(qml.s_prod(0.1, qml.Z(0)), qml.prod(qml.X(0), qml.Z(1)))
-            )
-
-        res = circuit()
-        assert res == (0.0, 0.0)
-
-    @pytest.mark.xfail(reason="measurements_from_samples_pass only supports NamedObs")
-    @pytest.mark.usefixtures("enable_disable_plxpr")
-    def test_gates_in_mbqc_gate_set_e2e_loop(self):
-        """Test that the outline_state_evolution_pass end to end on null.qubit."""
-        dev = qml.device("null.qubit", wires=1000)
-
-        @qml.for_loop(0, 1000, 1)
-        def for_fn(i):
-            qml.H(i)
-            qml.S(i)
-            RotXZX(0.1, 0.2, 0.3, wires=[i])
-            qml.RZ(phi=0.1, wires=[i])
-
-        @qml.while_loop(lambda i: i > 1000)
-        def while_fn(i):
-            qml.H(i)
-            qml.S(i)
-            RotXZX(0.1, 0.2, 0.3, wires=[i])
-            qml.RZ(phi=0.1, wires=[i])
-            i = i + 1
-            return i
-
-        @qml.qjit(
-            target="mlir",
-            pass_plugins=[getXDSLPluginAbsolutePath()],
-            pipelines=mbqc_pipeline(),
-            autograph=True,
-        )
-        @decompose_graph_state_pass
-        @convert_to_mbqc_formalism_pass
-        @measurements_from_samples_pass
-        @diagonalize_final_measurements_pass
-        @outline_state_evolution_pass
-        @qml.set_shots(1000)
-        @qml.qnode(dev)
-        def circuit():
-            for_fn()
-            while_fn(0)
-            qml.CNOT(wires=[0, 1])
-            return qml.expval(qml.prod(qml.X(0), qml.Z(1))), qml.expval(
-                qml.sum(qml.s_prod(0.1, qml.Z(0)), qml.prod(qml.X(0), qml.Z(1)))
-            )
-
-        res = circuit()
-        assert res == 1.0
-
-    @pytest.mark.usefixtures("enable_disable_plxpr")
-    def test_gates_in_mbqc_gate_set_e2e_loop(self):
-        """Test that the outline_state_evolution_pass end to end on null.qubit."""
+    def test_lightning_execution_with_structure(self):
+        """Test that the outline_state_evolution_pass on lightning.qubit for a circuit with program structure is executable and returns results as expected."""
         dev = qml.device("lightning.qubit", wires=10)
 
         @qml.for_loop(0, 10, 1)
@@ -198,7 +231,7 @@ class TestOutlineStateEvolutionPass:
             qml.S(i)
             qml.RZ(phi=0.1, wires=[i])
 
-        @qml.while_loop(lambda i: i > 10)
+        @qml.while_loop(lambda i: i < 10)
         def while_fn(i):
             qml.H(i)
             qml.S(i)
@@ -209,8 +242,6 @@ class TestOutlineStateEvolutionPass:
         @qml.qjit(
             target="mlir",
             pass_plugins=[getXDSLPluginAbsolutePath()],
-            pipelines=mbqc_pipeline(),
-            autograph=True,
         )
         @outline_state_evolution_pass
         @qml.qnode(dev)
@@ -224,7 +255,6 @@ class TestOutlineStateEvolutionPass:
 
         @qml.qjit(
             target="mlir",
-            autograph=True,
         )
         @qml.qnode(dev)
         def circuit_ref():
