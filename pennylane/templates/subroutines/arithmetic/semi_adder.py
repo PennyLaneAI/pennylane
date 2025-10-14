76f1ef029b166bb6d1ed3ee3e985ed1c11fe80d4
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2025-10-14 14:07:50 -0400

    isort

--- pennylane/templates/subroutines/arithmetic/semi_adder.py
+++ pennylane/templates/subroutines/arithmetic/semi_adder.py
@@ -16,8 +16,8 @@
 from pennylane.decomposition import (
     add_decomps,
     adjoint_resource_rep,
-    register_resources,
     controlled_resource_rep,
+    register_resources,
 )
 from pennylane.operation import Operation
 from pennylane.ops import CNOT, adjoint, ctrl
