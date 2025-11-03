0747c60e3beef310836b4461d5d0adbee5c5ea62
albi3ro <chrissie.c.l@gmail.com>
2025-11-03 14:24:37 -0500

    oops

--- pennylane/transforms/catalyst_passes.py
+++ pennylane/transforms/catalyst_passes.py
@@ -13,10 +13,14 @@ def _make_no_tape_implementation(name):
     return no_tape_implementation
 
 
-disentangle_cnot = transform(_make_no_tape_implementation("disentangle_cnot"), "disentangle-CNOT")
-disentangle_swap = transform(_make_no_tape_implementation("disentangle_swap"), "disentangle-SWAP")
-ions_decomposition = transform(
-    _make_no_tape_implementation("ions_decomposition"), "ions-decomposition"
+disentangle_cnot = transform(
+    _make_no_tape_implementation("disentangle_cnot"), pass_name="disentangle-CNOT"
 )
-to_ppr = transform(_make_no_tape_implementation("to_ppr"), "to-ppr")
-commute_ppr = transform(_make_no_tape_implementation("commute_ppr"), "commute_ppr")
+disentangle_swap = transform(
+    _make_no_tape_implementation("disentangle_swap"), pass_name="disentangle-SWAP"
+)
+ions_decomposition = transform(
+    _make_no_tape_implementation("ions_decomposition"), pass_name="ions-decomposition"
+)
+to_ppr = transform(_make_no_tape_implementation("to_ppr"), pass_name="to-ppr")
+commute_ppr = transform(_make_no_tape_implementation("commute_ppr"), pass_name="commute_ppr")
