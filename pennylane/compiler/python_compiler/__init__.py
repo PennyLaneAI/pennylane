51d89e55c58e4ae7fd16a1b67a0271ee175e8b67
Mudit Pandey <mudit.pandey@xanadu.ai>
2025-11-04 17:24:24 -0500

    Expand doc in python_compiler.__init__; move cookbook to docs folder

--- pennylane/compiler/python_compiler/__init__.py
+++ pennylane/compiler/python_compiler/__init__.py
@@ -20,7 +20,8 @@ This submodule contains the API for the integration of PennyLane and Catalyst wi
 
     This module is currently experimental and will not maintain API stability between releases.
 
-Te get started with the unified compiler and xDSL, check out the :doc:`quickstart guide <./unified_compiler_cookbook>`
+To get started with the unified compiler and xDSL, check out the
+:doc:`quickstart guide </development/unified_compiler/unified_compiler_cookbook>`
 
 .. automodapi:: pennylane.compiler.python_compiler
     :no-heading:
@@ -29,30 +30,84 @@ Te get started with the unified compiler and xDSL, check out the :doc:`quickstar
 Dialects
 --------
 
-.. automodapi:: pennylane.compiler.python_compiler.dialects
-    :no-heading:
-    :include-all-objects:
+.. currentmodule:: pennylane.compiler.python_compiler.dialects
+.. autosummary::
+    :toctree: api
+
+    Catalyst
+    MBQC
+    Quantum
+    QEC
+    StableHLO
+    Transform
+
 
 Available Transforms
 --------------------
 
-.. automodapi:: pennylane.compiler.python_compiler.transforms
-    :no-heading:
-    :include-all-objects:
+Quantum
+~~~~~~~
+
+.. currentmodule:: pennylane.compiler.python_compiler.transforms.quantum
+.. autosummary::
+    :toctree: api
+
+    combine_global_phases_pass
+    CombineGlobalPhasesPass
+    diagonalize_final_measurements_pass
+    DiagonalizeFinalMeasurementsPass
+    iterative_cancel_inverses_pass
+    IterativeCancelInversesPass
+    measurements_from_samples_pass
+    MeasurementsFromSamplesPass
+    merge_rotations_pass
+    MergeRotationsPass
+    split_non_commuting_pass
+    SplitNonCommutingPass
+
+
+MBQC
+~~~~
+
+.. currentmodule:: pennylane.compiler.python_compiler.transforms.mbqc
+.. autosummary::
+    :toctree: api
+
+    convert_to_mbqc_formalism_pass
+    ConvertToMBQCFormalismPass
+    decompose_graph_state_pass
+    DecomposeGraphStatePass
+    OutlineStateEvolutionPass
+    outline_state_evolution_pass
+    null_decompose_graph_state_pass
+    NullDecomposeGraphStatePass
+
 
 Transforms Core API
 -------------------
 
-.. automodapi:: pennylane.compiler.python_compiler.pass_api
-    :no-heading:
-    :include-all-objects:
+.. currentmodule:: pennylane.compiler.python_compiler.pass_api
+.. autosummary::
+    :toctree: api
+
+    ApplyTransformSequence
+    available_passes
+    PassDispatcher
+    register_pass
+    TransformFunctionsExt
+    TransformInterpreterPass
+    compiler_transform
+
 
 Inspection and Visualization
 ----------------------------
 
-.. automodapi:: pennylane.compiler.python_compiler.visualization
-    :no-heading:
-    :include-all-objects:
+.. currentmodule:: pennylane.compiler.python_compiler.pass_api
+.. autosummary::
+    :toctree: api
+
+    draw
+    generate_mlir_graph
 
 """
 
