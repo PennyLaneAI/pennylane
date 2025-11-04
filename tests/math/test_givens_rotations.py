32bbaf3d77ce36e482ae0412a3a1063d585119ae
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-11-04 15:29:47 -0500

    pylint

--- tests/math/test_givens_rotations.py
+++ tests/math/test_givens_rotations.py
@@ -19,7 +19,6 @@ import pytest
 from scipy.stats import ortho_group, unitary_group
 
 import pennylane as qml
-from pennylane import math
 from pennylane import numpy as np
 from pennylane.math import is_real_obj_or_close
 from pennylane.math.decomposition import _givens_matrix, _set_unitary_matrix, givens_decomposition
