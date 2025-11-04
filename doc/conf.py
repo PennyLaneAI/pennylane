2ffe484c42e458cb3881d0d3f38293729ee4de35
Mudit Pandey <mudit.pandey@xanadu.ai>
2025-11-04 16:16:31 -0500

    Test change to conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -118,6 +118,7 @@ add_module_names = False
 # built documents.
 
 import pennylane
+from pennylane.compiler import python_compiler
 
 pennylane.Hamiltonian = pennylane.ops.op_math.linear_combination.LinearCombination
 
@@ -378,14 +379,15 @@ def add_links_to_estimator_table(app, doctree, fromdocname):
             name = literal.astext()
             url = f"code/api/pennylane.estimator.{module_name}.{name}"
             refuri = app.builder.get_relative_uri(fromdocname, url)
-            refnode = nodes.reference('', refuri=refuri)
+            refnode = nodes.reference("", refuri=refuri)
             refnode += nodes.literal(text=name)
             literal.parent.replace(literal, refnode)
             logger.info(
-                f"[add_noindex_links] Linked pennylane.estimator.{module_name}.{name} to {refuri}")
+                f"[add_noindex_links] Linked pennylane.estimator.{module_name}.{name} to {refuri}"
+            )
 
 
 def setup(app):
     """Sphinx entry point for this extension."""
-    app.connect('source-read', add_noindex_to_estimator_stubs)
+    app.connect("source-read", add_noindex_to_estimator_stubs)
     app.connect("doctree-resolved", add_links_to_estimator_table)
