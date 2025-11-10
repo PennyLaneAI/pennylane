5a059ad2d4aed8416b8155929b5074e8d3e5e242
Mudit Pandey <mudit.pandey@xanadu.ai>
2025-11-10 15:25:13 -0500

    Add cancel inverses pass that uses the new pass API

---
+++ pennylane/compiler/python_compiler/pass_api/cancel_inverses_prototype.py
@@ -0,0 +1,82 @@
+# Copyright 2025 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Cancel inverses prototype using the new pass API."""
+
+from xdsl.ir import Operation
+
+from ..dialects import quantum
+from .compiler_transform import compiler_transform
+from .passes import PLModulePass
+
+self_inverses = [
+    "Identity",
+    "Hadamard",
+    "PauliX",
+    "PauliY",
+    "PauliZ",
+    "CNOT",
+    "CZ",
+    "CY",
+    "CH",
+    "SWAP",
+    "Toffoli",
+    "CCZ",
+]
+
+
+def _can_cancel(op: quantum.CustomOp, next_op: Operation) -> bool:
+    """Check if ops can be cancelled."""
+    if isinstance(next_op, quantum.CustomOp):
+        if op.gate_name.data == next_op.gate_name.data:
+            if (
+                op.out_qubits == next_op.in_qubits
+                and op.out_ctrl_qubits == next_op.in_ctrl_qubits
+                and op.in_ctrl_values == next_op.in_ctrl_values
+            ):
+                return True
+
+    return False
+
+
+class CancelInverses(PLModulePass):
+    """Reimplementation of cancel_inverses using new API."""
+
+    name = "cancel-inverses-2"
+
+
+# pylint: disable=unused-argument
+@CancelInverses.rewrite_rule(quantum.CustomOp)
+def rewrite_custom_op(self, op, rewriter):
+    """Rewrite rule for CustomOp."""
+    while isinstance(op, quantum.CustomOp) and op.gate_name.data in self_inverses:
+        next_user = None
+        for use in op.results[0].uses:
+            user = use.operation
+            if _can_cancel(op, user):
+                next_user: quantum.CustomOp = user
+                break
+
+        if next_user is None:
+            break
+
+        for q1, q2 in zip(op.in_qubits, next_user.out_qubits, strict=True):
+            rewriter.replace_all_uses_with(q2, q1)
+        for cq1, cq2 in zip(op.in_ctrl_qubits, next_user.out_ctrl_qubits, strict=True):
+            rewriter.replace_all_uses_with(cq2, cq1)
+        rewriter.erase_op(next_user)
+        rewriter.erase_op(op)
+        op = op.in_qubits[0].owner
+
+
+cancel_inverses_2 = compiler_transform(CancelInverses)
