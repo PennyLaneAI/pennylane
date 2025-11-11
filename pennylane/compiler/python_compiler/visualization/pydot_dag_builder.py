1cc58ff1d7d74965760097e2d442da47bf7fb4b3
andrijapau <andrija.paurevic@xanadu.ai>
2025-11-11 16:26:07 -0500

    fix docstring

--- pennylane/compiler/python_compiler/visualization/pydot_dag_builder.py
+++ pennylane/compiler/python_compiler/visualization/pydot_dag_builder.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""File that defines the DAGBuilder abstract base class."""
+"""File that defines the PyDotDAGBuilder subclass of DAGBuilder."""
 
 from .dag_builder import DAGBuilder
 
