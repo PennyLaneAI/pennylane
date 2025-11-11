36c44c5486bbb7af35c017d6225cc2b78c554ceb
JerryChen97 <chenys13@outlook.com>
2025-11-11 15:57:06 -0500

    mf

--- pennylane/capture/custom_primitives.py
+++ pennylane/capture/custom_primitives.py
@@ -45,7 +45,7 @@ def _make_hashable(obj: Any) -> Any:
     """
 
     # slice is unhashable in 3.11, but hashable in 3.12+
-    # so we have to treat it first incase incompatibility among 
+    # so we have to treat it first incase incompatibility among
     # different Python versions
     if isinstance(obj, slice):
         return (obj.start, obj.stop, obj.step)
