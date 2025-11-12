026f7b1212d0ab9426d578c05cf8f394b0b0482b
andrijapau <andrija.paurevic@xanadu.ai>
2025-11-12 09:30:38 -0500

    add more tests

--- tests/python_compiler/visualization/test_pydot_dag_builder.py
+++ tests/python_compiler/visualization/test_pydot_dag_builder.py
@@ -13,6 +13,46 @@
 # limitations under the License.
 """Unit tests for the PyDotDAGBuilder subclass."""
 
+import pydot
 import pytest
 
 from pennylane.compiler.python_compiler.visualization.pydot_dag_builder import PyDotDAGBuilder
+
+
+@pytest.mark.unit
+def test_initialization_defaults():
+    """Tests the initiliazation of the PyDotDAGBuilder"""
+
+    dag_builder = PyDotDAGBuilder()
+
+    assert isinstance(dag_builder.graph, pydot.Graph)
+    assert dag_builder.graph.get_graph_type() == "digraph"
+    assert dag_builder.graph.get_rankdir() == "TB"
+    assert dag_builder.graph.get_compound() == "true"
+
+
+@pytest.mark.unit
+def test_add_node():
+    """Unit test the `add_node` method."""
+
+    dag_builder = PyDotDAGBuilder()
+
+    dag_builder.add_node("0", "node0")
+    node_list = dag_builder.graph.get_node_list()
+    assert len(node_list) == 1
+    assert node_list[0].get_label() == "node0"
+
+
+@pytest.mark.unit
+def test_add_edge():
+    """Unit test the `add_edge` method."""
+
+    dag_builder = PyDotDAGBuilder()
+    dag_builder.add_node("0", "node0")
+    dag_builder.add_node("1", "node1")
+    dag_builder.add_edge("0", "1")
+
+    assert len(dag_builder.graph.get_edges()) == 1
+    edge = dag_builder.graph.get_edges()[0]
+    assert edge.get_source() == "0"
+    assert edge.get_destination() == "1"
