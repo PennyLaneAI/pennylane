7fc2f0dcea82252bf6c8f18999176ab2e613e39d
Jake Zaia <jake.zaia@xanadu.ai>
2025-11-13 21:00:23 +0000

    Move get_mlir_module to shared utility file

--- pennylane/compiler/python_compiler/inspection/draw.py
+++ pennylane/compiler/python_compiler/inspection/draw.py
@@ -19,34 +19,19 @@ import warnings
 from functools import wraps
 from typing import TYPE_CHECKING
 
-from catalyst import qjit
-from catalyst.passes.xdsl_plugin import getXDSLPluginAbsolutePath
-
 from pennylane.tape import QuantumScript
 
 from ..compiler import Compiler
 from .collector import QMLCollector
+from .xdsl_conversion import get_mlir_module
 
 if TYPE_CHECKING:
-    from xdsl.dialects.builtin import ModuleOp
-
     from pennylane.typing import Callable
     from pennylane.workflow.qnode import QNode
 
 # TODO: This caching mechanism should be improved,
 # because now it relies on a mutable global state
 _cache_store: dict[Callable, dict[int, tuple[str, str]]] = {}
-
-
-def _get_mlir_module(qnode: QNode, args, kwargs) -> ModuleOp:
-    """Ensure the QNode is compiled and return its MLIR module."""
-    if hasattr(qnode, "mlir_module") and qnode.mlir_module is not None:
-        return qnode.mlir_module
-
-    func = getattr(qnode, "user_function", qnode)
-    jitted_qnode = qjit(pass_plugins=[getXDSLPluginAbsolutePath()])(func)
-    jitted_qnode.jit_compile(args, **kwargs)
-    return jitted_qnode.mlir_module
 
 
 def draw(qnode: QNode, *, level: None | int = None) -> Callable:
@@ -93,7 +78,7 @@ def draw(qnode: QNode, *, level: None | int = None) -> Callable:
                 "`compiler.python_compiler.inspection.generate_mlir_graph` function instead.",
                 UserWarning,
             )
-        mlir_module = _get_mlir_module(qnode, args, kwargs)
+        mlir_module = get_mlir_module(qnode, args, kwargs)
         Compiler.run(mlir_module, callback=_draw_callback)
 
         if not cache:
