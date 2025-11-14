a98a1a69e745fb349e0f9296459f7ccde4eea5fb
Marcus Edwards <94946848+comp-phys-marc@users.noreply.github.com>
2025-11-14 15:46:12 -0500

    re-use variable
    Co-authored-by: Yushao Chen (Jerry) <chenys13@outlook.com>

--- pennylane/templates/subroutines/hilbert_schmidt.py
+++ pennylane/templates/subroutines/hilbert_schmidt.py
@@ -479,7 +479,7 @@ def _local_hilbert_schmidt_resources(
     num_wires: int, u_reps: list[CompressedResourceOp], v_wires: list[int]
 ):
     num_first_range = num_wires // 2
-    num_second_range = num_wires - num_wires // 2
+    num_second_range = num_wires - num_first_range
 
     resources = defaultdict(int)
 
