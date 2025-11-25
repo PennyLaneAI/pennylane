2cc4b81953b7dcaf6224b4d51264d995dd6cbbe1
JerryChen97 <chenys13@outlook.com>
2025-11-25 12:14:30 -0500

    not include and xfail

--- pennylane/capture/__init__.py
+++ pennylane/capture/__init__.py
@@ -169,8 +169,6 @@ from .make_plxpr import make_plxpr
 from .autograph import run_autograph, disable_autograph
 from .dynamic_shapes import determine_abstracted_axes, register_custom_staging_rule
 
-from . import jax_patches
-
 # by defining this here, we avoid
 # E0611: No name 'AbstractOperator' in module 'pennylane.capture' (no-name-in-module)
 # on use of from capture import AbstractOperator
