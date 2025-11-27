71fc6279082823140df4ed43104f3837c925e657
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-11-27 16:03:28 -0500

    Revert "simplify one-liner"
    This reverts commit b66423d3f7a9baea9e3c56e144f7d0c1f5d75bf5.

--- pennylane/templates/subroutines/qram.py
+++ pennylane/templates/subroutines/qram.py
@@ -475,7 +475,7 @@ def _select_only_qram_resources(
                     0,
                 ),
             )
-        ] += bits[j].count("1")
+        ] += sum([1 if bits[j] == "1" else 0 for j in range(len(bitstrings[0]))])
 
     return resources
 
