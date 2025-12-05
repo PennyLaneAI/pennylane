d151ac6dacac607f16874897ec3c489f7855c388
dwierichs <david.wierichs@xanadu.ai>
2025-12-05 13:41:52 +0100

    openqasm fix

--- tests/io/test_io.py
+++ tests/io/test_io.py
@@ -358,8 +358,7 @@ class TestOpenQasm:
             h q[0];
             cx q[0],q[1];
             h q[0];
-            z q[1];
-            s q[1];
+            sdg q[1];
             h q[1];
             measure q[0] -> c[0];
             measure q[1] -> c[1];
