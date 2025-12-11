e0fc340542804d2d8181826f2e612e933071033f
josephbowles <54283511+josephbowles@users.noreply.github.com>
2025-12-11 10:57:57 +0100

    correct typo

--- pennylane/templates/subroutines/iqp.py
+++ pennylane/templates/subroutines/iqp.py
@@ -32,7 +32,7 @@ class IQP(Operation):
     A template that builds an Instantaneous Quantum Polynomial (IQP) circuit.
 
     In this `IQPOpt <https://arxiv.org/pdf/2501.04776>`__ paper and this
-    `train on classical, deploy on quantum <https://arxiv.org/pdf/2503.02934>`__ paper, tht authors
+    `train on classical, deploy on quantum <https://arxiv.org/pdf/2503.02934>`__ paper, the authors
     present methods for analytically approximating expectation values coming from measurements following IQP circuits.
     This allows for the classical training of the parameters of these circuits prior to deploying them to a
     quantum computer for actual computation.
