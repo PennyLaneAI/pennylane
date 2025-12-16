3dc193b03bfc08ad124f492e22c85f7d14e0c067
Jeffrey Kam <jeffrey.kam@xanadu.ai>
2025-12-15 23:03:15 -0500

    Fix format

--- tests/capture/test_capture_cond.py
+++ tests/capture/test_capture_cond.py
@@ -286,10 +286,14 @@ class TestCond:
 
 def test_static_string_keyword_argument():
     """Test that string keyword arguments are treated as static."""
+
     def f(pred):
-        return qml.cond(pred, qml.PauliRot)(theta=np.pi/2, wires=0, pauli_word="X")
+        return qml.cond(pred, qml.PauliRot)(theta=np.pi / 2, wires=0, pauli_word="X")
+
     jaxpr = jax.make_jaxpr(f)(True)
-    assert jaxpr.eqns[0].params["jaxpr_branches"][0].eqns[0].primitive == qml.PauliRot._primitive  # pylint: disable=protected-access
+    assert (
+        jaxpr.eqns[0].params["jaxpr_branches"][0].eqns[0].primitive == qml.PauliRot._primitive
+    )  # pylint: disable=protected-access
     assert jaxpr.eqns[0].params["jaxpr_branches"][0].eqns[0].params["pauli_word"] == "X"
 
 
