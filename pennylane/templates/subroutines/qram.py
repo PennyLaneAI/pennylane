f6173e7436c1e6a8e7065af54cc4334d2750c0b2
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-12-18 09:47:25 -0500

    Typo

--- pennylane/templates/subroutines/qram.py
+++ pennylane/templates/subroutines/qram.py
@@ -385,7 +385,7 @@ class HybridQRAM(Operation):
 
     For each prefix :math:`s \in {0, …, 2^k - 1}`:
 
-    - Perofrm a multi-controlled-X on a "signal" auxiliary, controlled by the :math:`k` select bits being equal to :math:`s`.
+    - Perform a multi-controlled-X on a "signal" auxiliary, controlled by the :math:`k` select bits being equal to :math:`s`.
     - Conditioned on ``signal==1``, perform a bucket-brigade QRAM (:class:`~.BBQRAM`) query using only the lower :math:`n-k` address bits and the sub-table of bitstrings whose prefix is :math:`s`.
     - Uncompute the signal with the same multi-controlled-X.
 
