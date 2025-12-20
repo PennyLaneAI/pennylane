9e10ea065b17eafabb5a1b55953c1a76727731a8
cvqmm <oumarou.oumarou@covestro.com>
2025-12-20 09:36:40 +0100

    make format

--- pennylane/templates/subroutines/qsvt.py
+++ pennylane/templates/subroutines/qsvt.py
@@ -963,8 +963,10 @@ def optax_opt(initial_guess, x, y, maxiter, tol):
 
     opt = optax.lbfgs()
     init_carry = (initial_guess, opt.init(initial_guess))
+
     def lambda_obj_function(phi):
         return obj_function(phi, x=x, y=y)
+
     val_and_grad = optax.value_and_grad_from_state(lambda_obj_function)
 
     def optimizer_iter_update(carry):
