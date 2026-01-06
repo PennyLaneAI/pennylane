9bdc8b2d6e93c3b4c86e49d5a626732e13c45adf
albi3ro <chrissie.c.l@gmail.com>
2026-01-06 10:23:08 -0500

    and one more attempt

--- pennylane/_grad/grad.py
+++ pennylane/_grad/grad.py
@@ -52,12 +52,14 @@ def _get_jacobian_prim():
     jacobian_prim.prim_type = "higher_order"
 
     @jacobian_prim.def_impl
-    def _grad_impl(*args, argnums, jaxpr, method, h, scalar_out, fn):
+    def _grad_impl(*args, argnums, jaxpr, n_consts, method, h, scalar_out, fn):
         if method != "auto":  # pragma: no cover
             raise ValueError(f"Invalid value '{method=}' without QJIT.")
+        consts = args[:n_consts]
+        args = args[n_consts:]
 
         def func(*inner_args):
-            res = jax.core.eval_jaxpr(jaxpr, [], *inner_args)
+            res = jax.core.eval_jaxpr(jaxpr, consts, *inner_args)
             return res[0] if scalar_out else res
 
         if scalar_out:
@@ -68,10 +70,10 @@ def _get_jacobian_prim():
 
     # pylint: disable=unused-argument
     @jacobian_prim.def_abstract_eval
-    def _grad_abstract(*args, argnums, jaxpr, method, h, scalar_out, fn):
+    def _grad_abstract(*args, argnums, jaxpr, n_consts, method, h, scalar_out, fn):
         if scalar_out and not (len(jaxpr.outvars) == 1 and jaxpr.outvars[0].aval.shape == ()):
             raise TypeError("Grad only applies to scalar-output functions. Try jacobian.")
-        in_avals = tuple(args[i] for i in argnums)
+        in_avals = tuple(args[i + n_consts] for i in argnums)
         out_shapes = tuple(outvar.aval.shape for outvar in jaxpr.outvars)
         return [
             _shape(out_shape + in_aval.shape, in_aval.dtype, weak_type=in_aval.weak_type)
