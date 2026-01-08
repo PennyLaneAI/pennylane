1b00d6b41c99d617a265fadca808898b7e541ca5
Yushao Chen (Jerry) <chenys13@outlook.com>
2026-01-08 18:32:27 -0500

    Remove merge mistake

--- tests/templates/layers/test_gate_fabric.py
+++ tests/templates/layers/test_gate_fabric.py
@@ -659,7 +659,6 @@ class TestDecomposition:
         ),
     ]
 
-    @pytest.mark.xfail(reason="arrays should never be in metadata")
     @pytest.mark.capture
     @pytest.mark.parametrize(("weights", "wires", "init_state", "include_pi"), DECOMP_PARAMS)
     def test_decomposition_new(self, weights, wires, init_state, include_pi):
