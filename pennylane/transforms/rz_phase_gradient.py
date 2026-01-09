21e87550e5888113cac21133507bdd9f8fa84b1e
Yushao Chen (Jerry) <chenys13@outlook.com>
2026-01-09 09:35:20 -0500

    Yushao docs fixing (#8874)
    1. typos: `procesing` -> `processing`
    2. sample code syntax fix: in each line of python function definition,
    we don't use comma
    3. sample code mistake fix: definition of `stopping_condition` in rst
    won't work due to var name conflicts
    3. sample code mistake fix: definition of `decompose` in rst won't work
    due to wrong `device_wires` init assignment

--- pennylane/transforms/rz_phase_gradient.py
+++ pennylane/transforms/rz_phase_gradient.py
@@ -212,7 +212,7 @@ def rz_phase_gradient(
     new_tape = tape.copy(operations=operations)
 
     def null_postprocessing(results):
-        """A postprocesing function returned by a transform that only converts the batch of results
+        """A postprocessing function returned by a transform that only converts the batch of results
         into a result for a single ``QuantumTape``.
         """
         return results[0]
