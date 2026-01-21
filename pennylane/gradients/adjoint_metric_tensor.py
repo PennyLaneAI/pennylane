2e081ad6bd371971d530b9150131d6b6d532f5ca
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-01-21 13:10:46 -0500

    Cleanup

--- pennylane/gradients/adjoint_metric_tensor.py
+++ pennylane/gradients/adjoint_metric_tensor.py
@@ -65,8 +65,8 @@ def _group_operations(tape):
 def _multipar_stopping_fn(obj):
     try:
         return len(obj.data) == 0 or (obj.has_generator and len(obj.generator().terms()[0]) == 1)
-    except TermsUndefinedError:
-        return True
+    except TermsUndefinedError:  # pragma: no cover
+        return True  # pragma: no cover
 
 
 def _trainable_multipar_stopping_fn(obj):
