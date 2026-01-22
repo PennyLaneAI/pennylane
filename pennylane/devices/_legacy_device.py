bfc899c060e4503d8e2d92806bbc61c19af84eb7
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-01-22 10:20:28 -0500

    Imports

--- pennylane/devices/_legacy_device.py
+++ pennylane/devices/_legacy_device.py
@@ -38,7 +38,7 @@ from pennylane.measurements import (
 from pennylane.operation import Operation, Operator, StatePrepBase
 from pennylane.ops import LinearCombination, MidMeasure, Prod, Projector, SProd, Sum
 from pennylane.queuing import QueuingManager
-from pennylane.tape import QuantumScript, expand_tape_state_prep
+from pennylane.tape import QuantumScript
 from pennylane.transforms import broadcast_expand, decompose, split_non_commuting
 from pennylane.wires import Wires
 
