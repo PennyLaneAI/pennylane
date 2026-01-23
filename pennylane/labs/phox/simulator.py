3cd7fe4f9c2886c3228613c5cbdf0baf682f1ea6
Will <wmaxwell90@gmail.com>
2026-01-23 15:02:08 -0500

    updated simulator and test against pl

--- pennylane/labs/phox/simulator.py
+++ pennylane/labs/phox/simulator.py
@@ -1,48 +1,51 @@
+import math
+
 import jax
 import jax.numpy as jnp
 from numpy.typing import ArrayLike
 
-
 class IqpSimulator:
-    def __init__(
-        self, generators: ArrayLike, trainable: list[list[int]], fixed: list[list[int]] = None
-    ):
+    def __init__(self, generators: ArrayLike, trainable: list[list[int]], fixed: list[list[int]] = None):
 
         self.generators = generators
         self.trainable = trainable
         self.fixed = fixed
         self.n_generators, self.n_qubits = generators.shape
 
-    def expval_gen_obs(self, params: ArrayLike, ops: ArrayLike, n_samples: int):
+    def expval_gen(self, params: ArrayLike, ops: ArrayLike, n_samples: int, init_state: ArrayLike = None):
+        if init_state is not None:
+            return self._expval_gen_init_state(params, ops, n_samples, init_state=init_state)
+
         key = jax.random.PRNGKey(42)
         key, subkey = jax.random.split(key, 2)
         key = subkey
 
         samples = jax.random.randint(key, (n_samples, self.n_qubits), 0, 2)
 
-        bitflips = []
-        phases = []
+        bitflips = jnp.array([[1 if gate in {"Z", "Y"} else 0 for gate in op] for op in ops])
+        phases = jnp.array([[math.prod([_phase(gate, qubit) for gate, qubit in zip(op, sample)]) for sample in samples] for op in ops])
 
-        for op in ops:
-            generator = []
-            for pauli in op:
-                match pauli:
-                    case "I":
-                        generator.append(0)
-                    case "X":
-                        generator.append(1)
-                    case "Y":
-                        generator.append(1)
-                    case "Z":
-                        generator.append(0)
-                    case _:
-                        raise ValueError
+        expvals = _expval_gen_matrix(params, self.generators, bitflips, phases, samples)
 
-                bitflips.append(generator)
+        std_err = jnp.std(expvals, axis=-1, ddof=1) / jnp.sqrt(n_samples)
+        return jnp.mean(expvals, axis=1), std_err
 
-    def expval(
-        self, params: ArrayLike, ops: list[list[str]], n_samples: int, init_state: ArrayLike = None
-    ):
+    def _expval_gen_init_state(self, params: ArrayLike, ops: list[list[int]], n_samples: int, init_state: ArrayLike = None):
+        key = jax.random.PRNGKey(42)
+        key, subkey = jax.random.split(key, 2)
+        key = subkey
+
+        samples = jax.random.randint(key, (n_samples, self.n_qubits), 0, 2)
+
+        bitflips = jnp.array([[1 if gate in {"Z", "Y"} else 0 for gate in op] for op in ops])
+        phases = jnp.array([[math.prod([_phase(gate, qubit) for gate, qubit in zip(op, sample)]) for sample in samples] for op in ops])
+
+        expvals = _expval_gen_init_matrix(params, self.generators, bitflips, phases, samples, init_state)
+
+        std_err = jnp.std(expvals, axis=-1, ddof=1) / jnp.sqrt(n_samples)
+        return jnp.mean(expvals, axis=1), std_err
+
+    def expval(self, params: ArrayLike, ops: list[list[int]], n_samples: int, init_state: ArrayLike = None):
         """Compute expval of a batch of Pauli Z operators
 
         ops - A
@@ -54,7 +57,7 @@ class IqpSimulator:
         """
 
         if init_state is not None:
-            return self._expval_init_state(params, ops, n_samples, init_state)
+            return self._expval_init_state(params, ops, n_samples, init_state=init_state)
 
         key = jax.random.PRNGKey(42)
         key, subkey = jax.random.split(key, 2)
@@ -71,9 +74,7 @@ class IqpSimulator:
         std_err = jnp.std(expvals, axis=-1, ddof=1) / jnp.sqrt(n_samples)
         return jnp.mean(expvals, axis=1), std_err
 
-    def _expval_init_state(
-        self, params: ArrayLike, ops: ArrayLike, n_samples: int, init_state: ArrayLike
-    ):
+    def _expval_init_state(self, params: ArrayLike, ops: ArrayLike, n_samples: int, init_state: ArrayLike):
         """Compute expval of a batch of Pauli Z operators
 
         ops - A
@@ -95,7 +96,7 @@ class IqpSimulator:
         X, P = init_state
         P = P[:, jnp.newaxis]
 
-        F = jnp.broadcast_to(P, (P.shape[0], n_samples)) * ((-1) ** (X @ samples.T))
+        F = jnp.broadcast_to(P, (P.shape[0], n_samples)) * ((-1)**(X @ samples.T))
 
         H1 = ((-1) ** (ops @ X.T)) @ F
         col_sums = jnp.sum(F.conj(), axis=0, keepdims=True)
@@ -107,11 +108,8 @@ class IqpSimulator:
         std_err = jnp.std(expvals, axis=-1, ddof=1) / jnp.sqrt(n_samples)
         return jnp.mean(expvals, axis=1), std_err
 
-
 class BitflipSimulator:
-    def __init__(
-        self, generators: ArrayLike, trainable: list[list[int]], fixed: list[list[int]] = None
-    ):
+    def __init__(self, generators: ArrayLike, trainable: list[list[int]], fixed: list[list[int]] = None):
 
         self.generators = generators
         self.trainable = trainable
@@ -134,22 +132,53 @@ class BitflipSimulator:
 
         return jnp.prod(jnp.where(X == 0, 1.0, X), axis=1), jnp.zeros(ops.shape[0])
 
-
 def _phase(pauli: str, qubit: int) -> complex:
+    """ For a Pauli P return the phase applied by HPH
 
-    if pauli in ("I", "X"):
+    HXH = Z
+    HYH = -Y
+    HZH = X
+
+    """
+
+    if pauli in ("I", "Z"):
         return 1
 
     if pauli == "Y":
         if qubit == 0:
-            return 1j
+            return -1j
 
-        return -1j
+        return 1j
 
-    if pauli == "Z":
+    if pauli == "X":
         if qubit == 0:
             return 1
 
         return -1
 
     raise ValueError(f"Expected Pauli I, X, Y, or Z, got {pauli}.")
+
+def _expval_gen_matrix(params, generators, bitflips, phases, samples):
+    B = (-1)**(samples @ generators.T)
+    C = 1 - ((-1) ** (bitflips @ generators.T))
+    E = C @ jnp.diag(params) @ B.T
+    M = phases * jnp.exp(-1j * E)
+
+    return jnp.real(M)
+
+def _expval_gen_init_matrix(params, generators, bitflips, phases, samples, init_state):
+    B = (-1) ** (samples @ generators.T)
+    C = 1 - ((-1) ** (bitflips @ generators.T))
+    E = C @ jnp.diag(params) @ B.T
+    M = phases * jnp.exp(1j * E)
+    X, P = init_state
+    P = P[:, jnp.newaxis]
+
+    F = jnp.broadcast_to(P, (P.shape[0], samples.shape[0])) * ((-1)**(X @ samples.T))
+
+    H1 = ((-1) ** (bitflips @ X.T)) @ F
+    col_sums = jnp.sum(F.conj(), axis=0, keepdims=True)
+    H2 = jnp.broadcast_to(col_sums, (bitflips.shape[0], samples.shape[0]))
+    H = H1 * H2
+
+    return jnp.real(M * H)
