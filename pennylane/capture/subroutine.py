90a4395594e4f226336d0723e6d5df478577078f
Christina Lee <christina@xanadu.ai>
2026-01-27 14:05:52 -0500

    Apply suggestion from @comp-phys-marc
    Co-authored-by: Marcus Edwards <94946848+comp-phys-marc@users.noreply.github.com>

--- pennylane/capture/subroutine.py
+++ pennylane/capture/subroutine.py
@@ -159,6 +159,8 @@ def subroutine(func, static_argnums=None, static_argnames=None):
 
     @wraps(func)
     def inside(*args, **kwargs):
+        # Inside our "quantum subroutine", we want to be able to do normal jit on classical subroutines 
+        # with the normal jit pipeline. Hence why it's patched back to the original function in inside
         with Patcher(
             (
                 jax._src.pjit,  # pylint: disable=protected-access
