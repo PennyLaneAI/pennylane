2fc1ba013836740a02fcb894edc6aa541fae5961
Astral Cai <astral.cai@xanadu.ai>
2026-02-02 14:54:49 -0500

    fix

--- pennylane/ops/op_math/evolution.py
+++ pennylane/ops/op_math/evolution.py
@@ -180,11 +180,10 @@ def pauli_rot_decomp(*params, wires, base, **_):  # pylint: disable=unused-argum
         base = base.simplify()
     coeff = -1j * params[0]
     if isinstance(base, qml.ops.SProd):
-        coeff, base = params[0] * base.scalar, base.base
+        coeff, base = coeff * base.scalar, base.base
     coeff = 2j * coeff  # The 2j cancels the coefficient added by PauliRot
     pauli_word = qml.pauli.pauli_word_to_string(base)
-    if not all(p == "I" for p in pauli_word):
-        qml.PauliRot(coeff, pauli_word, wires)
+    qml.PauliRot(coeff, pauli_word, wires)
 
 
 qml.add_decomps(Evolution, pauli_rot_decomp)
