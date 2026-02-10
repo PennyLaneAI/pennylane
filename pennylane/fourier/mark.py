d1dc82094681bfee6e47d912dacebaa56ac14552
andrijapau <andrija.paurevic@xanadu.ai>
2026-02-10 14:55:39 -0500

    add mark alternative

--- pennylane/fourier/mark.py
+++ pennylane/fourier/mark.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2021 Xanadu Quantum Technologies Inc.
+# Copyright 2018-2026 Xanadu Quantum Technologies Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,13 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""This module contains functions to analyze the Fourier representation
-of quantum circuits."""
-import warnings
+"""
+Contains the 'label' function for customizing operator labels.
+"""
 
-from .mark import mark
-from .circuit_spectrum import circuit_spectrum
-from .coefficients import coefficients
-from .qnode_spectrum import qnode_spectrum
-from .reconstruct import reconstruct
-from .utils import get_spectrum, join_spectra
+from pennylane.operation import Operator
+from pennylane.ops.op_math import SymbolicOp
+
+
+class MarkedOp(SymbolicOp):
+    """Creates a marked operator."""
+
+
+def mark(op: Operator, tag: str) -> MarkedOp:
+    """Marks an operator with a custom tag."""
+    return MarkedOp(op, tag)
