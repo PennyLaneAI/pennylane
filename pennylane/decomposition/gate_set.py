1c1c3f6287cb2a634f57e674dec7eebdf6872214
Astral Cai <astral.cai@xanadu.ai>
2026-02-10 11:26:45 -0500

    Yet another batch of graph-decomp related fixes (#9001)
    **Context:**
    This PR resolves the issues surfaced by treating the warning raised from
    the decomposition graph as errors. See
    https://github.com/PennyLaneAI/pennylane/pull/8999
    
    **Description of the Change:**
    
    **Benefits:**
    
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**
    [sc-109010]
    
    ---------
    
    Co-authored-by: Marcus Edwards <marcusstephenedwards@gmail.com>
    Co-authored-by: Marcus Edwards <94946848+comp-phys-marc@users.noreply.github.com>
    Co-authored-by: Mudit Pandey <18223836+mudit2812@users.noreply.github.com>

--- pennylane/decomposition/gate_set.py
+++ pennylane/decomposition/gate_set.py
@@ -18,9 +18,7 @@ from __future__ import annotations
 
 from collections.abc import Iterable, Mapping
 
-from pennylane.operation import Operator
-
-from .utils import translate_op_alias
+from .utils import to_name
 
 
 class GateSet(Mapping):
@@ -32,7 +30,7 @@ class GateSet(Mapping):
         if any(v < 0 for v in gate_set.values()):
             raise ValueError("Negative weights are not supported in the gate_set.")
         self.name = name
-        self._gate_set = {_to_name(op): weight for op, weight in gate_set.items()}
+        self._gate_set = {to_name(op): weight for op, weight in gate_set.items()}
 
     def __eq__(self, value: object, /) -> bool:
         if not isinstance(value, GateSet):
@@ -46,13 +44,13 @@ class GateSet(Mapping):
         return len(self._gate_set)
 
     def __getitem__(self, key, /):
-        return self._gate_set[_to_name(key)]
+        return self._gate_set[to_name(key)]
 
     def __setitem__(self, key, value, /) -> None:
         raise TypeError("The GateSet is immutable.")
 
     def __contains__(self, op) -> bool:
-        return _to_name(op) in self._gate_set
+        return to_name(op) in self._gate_set
 
     def __iter__(self):
         return iter(self._gate_set)
@@ -70,19 +68,10 @@ class GateSet(Mapping):
         return self._gate_set.items()
 
     def get(self, key, default=None):
-        return self._gate_set.get(_to_name(key), default)
+        return self._gate_set.get(to_name(key), default)
 
     def __repr__(self) -> str:
         if self.name:
             return self.name
         inner_str = ", ".join(list(self))
         return f"{{{inner_str}}}"
-
-
-def _to_name(op):
-    if isinstance(op, type):
-        op = op.__name__
-    if isinstance(op, Operator):
-        op = op.name
-    assert isinstance(op, str)
-    return translate_op_alias(op)
