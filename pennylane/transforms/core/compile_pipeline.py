6542a381d5abbf5f26bb43af7aace4b5231476ec
andrijapau <andrija.paurevic@xanadu.ai>
2026-02-12 17:56:33 -0500

    artifact

--- pennylane/transforms/core/compile_pipeline.py
+++ pennylane/transforms/core/compile_pipeline.py
@@ -1,4 +1,4 @@
-# BEFORE  Copyright 2023 Xanadu Quantum Technologies Inc.
+# Copyright 2023 Xanadu Quantum Technologies Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
