168149b913ed8197c4f25fb45e13c0a1c6624cd6
dwierichs <david.wierichs@xanadu.ai>
2026-02-17 11:32:04 +0100

    simplify parameters

--- tests/math/test_basic_math.py
+++ tests/math/test_basic_math.py
@@ -25,12 +25,12 @@ jax = pytest.importorskip("jax")
 jnp = pytest.importorskip("jax.numpy")
 
 
+@pytest.mark.parametrize(
+    "n, exp", [(1, 0), (2, 1), (4, 2), (1024, 10), (3, 2), (17, 5), (1023, 10)]
+)
 class TestCeilLog2:
     """Tests for ``qml.math.ceil_log2``."""
 
-    @pytest.mark.parametrize(
-        "n, exp", [(1, 0), (2, 1), (4, 2), (1024, 10), (3, 2), (17, 5), (1023, 10)]
-    )
     def test_ceil_log2_basic(self, n, exp):
         """Test ``ceil_log2``, which computes the ceiling of log2, cast to a builtin integer."""
         out = fn.ceil_log2(n)
@@ -38,11 +38,9 @@ class TestCeilLog2:
         assert out == exp
         assert fn.ceil_log2(2**out) == out
 
-    @pytest.mark.parametrize(
-        "n, exp", [(1, 0), (2, 1), (4, 2), (1024, 10), (3, 2), (17, 5), (1023, 10)]
-    )
     def test_ceil_log2_jit(self, n, exp):
-        """Test ``ceil_log2``, which computes the ceiling of log2, cast to a builtin integer."""
+        """Test ``ceil_log2`` with JIT, which computes the ceiling of log2,
+        cast to integer dtype."""
         out = jax.jit(fn.ceil_log2)(jnp.array(n))
         assert isinstance(out, jnp.ndarray)
         assert out.dtype == jnp.int64
