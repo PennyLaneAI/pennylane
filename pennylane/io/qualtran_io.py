96c613ea60ba704f9a6bdee9fc21b0a3cb5f09ee
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-02-19 15:37:35 -0500

    Squash bug

--- pennylane/io/qualtran_io.py
+++ pennylane/io/qualtran_io.py
@@ -437,7 +437,7 @@ def _(op: qtemps.subroutines.Select):
     return gate_types
 
 
-@_register_subroutine_call_graph(qops.StatePrep.operator())
+@_register_subroutine_call_graph(qops.StatePrep)
 def _(op):
     """Call graph for StatePrep"""
 
