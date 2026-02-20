f14979d5bdfa8ac849f3658cd7d37d4c516001c7
Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>
2026-02-20 18:19:14 -0500

    deprecate: `get_transform_program` in favour of `get_compile_pipeline` (#9077)
    Now that we have the new and improved `get_compile_pipeline`, we can
    deprecated `get_transform_program`.
    
    Note that this is used by `add_noise` and cannot be replaced with
    `get_compile_pipeline` so we decided to move it to a private helper
    instead.
    
    📖 :
    https://xanaduai-pennylane--9077.com.readthedocs.build/en/9077/code/api/pennylane.workflow.get_transform_program.html
    
    [sc-105432]
    [sc-108742]

--- tests/workflow/test_construct_batch.py
+++ tests/workflow/test_construct_batch.py
@@ -12,17 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Contains tests for the `qml.workflow.get_transform_program` getter and `construct_batch`.
+Contains tests for `construct_batch`.
 
 """
+
 
 import numpy as np
 import pytest
 from default_qubit_legacy import DefaultQubitLegacy
 
 import pennylane as qml
-from pennylane.transforms.core import BoundTransform, CompilePipeline
-from pennylane.workflow import construct_batch, get_transform_program
+from pennylane.exceptions import PennyLaneDeprecationWarning
+from pennylane.workflow import construct_batch
 
 
 class TestMarker:
@@ -94,180 +95,17 @@ class TestMarker:
         assert qml.math.allclose(res, np.cos(0.5))
 
 
-class TestCompilePipelineGetter:
-    def test_bad_string_key(self):
-        """Test a value error is raised if a bad string key is provided."""
+def test_get_transform_program_is_deprecated():
+    """Tests that the 'get_transform_program' function is deprecated."""
 
-        @qml.qnode(qml.device("default.qubit"))
-        def circuit():
-            return qml.state()
+    @qml.qnode(qml.device("default.qubit"))
+    def circuit():
+        return qml.state()
 
-        with pytest.raises(ValueError, match=r"Level bla not found in transform program."):
-            get_transform_program(circuit, level="bla")
-
-    def test_bad_other_key(self):
-        """Test a value error is raised if a bad, unrecognized key is provided."""
-
-        @qml.qnode(qml.device("default.qubit"))
-        def circuit():
-            return qml.state()
-
-        with pytest.raises(ValueError, match=r"not recognized."):
-            get_transform_program(circuit, level=["bah"])
-
-    def test_get_transform_program_diff_method_transform(self):
-        """Tests for the transform program when the diff_method is a transform."""
-
-        dev = qml.device("default.qubit", wires=4)
-
-        @qml.transforms.compile(num_passes=2)
-        @qml.transforms.merge_rotations(atol=1e-5)
-        @qml.transforms.cancel_inverses
-        @qml.qnode(dev, diff_method="parameter-shift", gradient_kwargs={"shifts": 2})
-        def circuit():
-            return qml.expval(qml.PauliZ(0))
-
-        expected_p0 = BoundTransform(qml.transforms.cancel_inverses)
-        expected_p1 = BoundTransform(qml.transforms.merge_rotations, kwargs={"atol": 1e-5})
-        expected_p2 = BoundTransform(qml.transforms.compile, kwargs={"num_passes": 2})
-
-        ps_expand_fn = BoundTransform(
-            qml.transform(qml.gradients.param_shift.expand_transform), kwargs={"shifts": 2}
-        )
-
-        p0 = get_transform_program(circuit, level=0)
-        assert isinstance(p0, CompilePipeline)
-        assert len(p0) == 0
-
-        p0 = get_transform_program(circuit, level="top")
-        assert isinstance(p0, CompilePipeline)
-        assert len(p0) == 0
-
-        p_grad = get_transform_program(circuit, level="gradient")
-        assert isinstance(p_grad, CompilePipeline)
-        assert len(p_grad) == 4
-        assert p_grad == CompilePipeline(expected_p0, expected_p1, expected_p2, ps_expand_fn)
-
-        p_dev = get_transform_program(circuit, level="device")
-        assert isinstance(p_grad, CompilePipeline)
-        p_default = get_transform_program(circuit)
-        assert p_dev == p_default
-
-        assert len(p_dev) == 10
-        config = qml.devices.ExecutionConfig(
-            interface=getattr(circuit, "interface", None),
-            mcm_config=qml.devices.MCMConfig(mcm_method="deferred"),
-        )
-        assert p_dev == p_grad + dev.preprocess_transforms(config)
-
-        # slicing
-        p_sliced = get_transform_program(circuit, slice(2, 7, 2))
-        assert len(p_sliced) == 3
-        assert p_sliced[0].tape_transform == qml.compile.tape_transform
-        assert (
-            p_sliced[2].tape_transform
-            == qml.devices.preprocess.device_resolve_dynamic_wires.tape_transform
-        )
-        assert p_sliced[1].tape_transform == qml.defer_measurements.tape_transform
-
-    def test_diff_method_device_gradient(self):
-        """Test that if level="gradient" but the gradient does not have preprocessing, the program is strictly user transforms."""
-
-        @qml.transforms.cancel_inverses
-        @qml.qnode(qml.device("default.qubit"), diff_method="backprop")
-        def circuit():
-            return qml.state()
-
-        prog = get_transform_program(circuit, level="gradient")
-        assert len(prog) == 1
-        assert qml.transforms.cancel_inverses in prog
-
-    def test_get_transform_program_device_gradient(self):
-        """Test the trnsform program contents when using a device derivative."""
-
-        dev = qml.device("default.qubit")
-
-        @qml.transforms.split_non_commuting
-        @qml.qnode(dev, diff_method="adjoint", device_vjp=False)
-        def circuit(x):
-            qml.RX(x, 0)
-            return qml.expval(qml.PauliZ(0))
-
-        full_prog = get_transform_program(circuit)
-        assert len(full_prog) == 14
-
-        config = qml.devices.ExecutionConfig(
-            interface=getattr(circuit, "interface", None),
-            gradient_method="adjoint",
-            use_device_jacobian_product=False,
-        )
-        config = dev.setup_execution_config(config)
-        dev_program = dev.preprocess_transforms(config)
-
-        expected = CompilePipeline()
-        expected.add_transform(qml.transforms.split_non_commuting)
-        expected += dev_program
-        assert full_prog == expected
-
-    def test_get_transform_program_legacy_device_interface(self):
-        """Test the contents of the transform program with the legacy device interface."""
-
-        dev = DefaultQubitLegacy(wires=5)
-
-        @qml.transforms.merge_rotations
-        @qml.qnode(dev, diff_method="backprop")
-        def circuit(x):
-            qml.RX(x, wires=0)
-            return qml.expval(qml.PauliZ(0))
-
-        program = get_transform_program(circuit)
-
-        m1 = BoundTransform(qml.transforms.merge_rotations)
-        assert program[:1] == CompilePipeline([m1])
-
-        m2 = BoundTransform(qml.devices.legacy_facade.legacy_device_batch_transform)
-        assert program[1].tape_transform == m2.tape_transform
-        assert program[1].kwargs["device"] == dev
-
-        # a little hard to check the contents of a expand_fn transform
-        # this is the best proxy I can find
-        assert (
-            program[2].tape_transform
-            == qml.devices.legacy_facade.legacy_device_expand_fn.tape_transform
-        )
-
-    def test_get_transform_program_final_transform(self):
-        """Test that gradient preprocessing and device transform occur before a final transform."""
-
-        @qml.metric_tensor
-        @qml.compile
-        @qml.qnode(qml.device("default.qubit"), diff_method="parameter-shift")
-        def circuit():
-            qml.IsingXX(1.234, wires=(0, 1))
-            return qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliX(0))
-
-        user_program = get_transform_program(circuit, level="user")
-        assert len(user_program) == 3
-        assert user_program[0].tape_transform == qml.compile.tape_transform
-        assert user_program[1].tape_transform == qml.metric_tensor.expand_transform
-        assert user_program[2].tape_transform == qml.metric_tensor.tape_transform
-
-        grad_program = get_transform_program(circuit, level="gradient")
-        assert len(grad_program) == 4
-        assert grad_program[0].tape_transform == qml.compile.tape_transform
-        assert grad_program[1].tape_transform == qml.metric_tensor.expand_transform
-        assert grad_program[2].tape_transform == qml.metric_tensor.tape_transform
-        assert grad_program[3].tape_transform == qml.gradients.param_shift.expand_transform
-
-        dev_program = get_transform_program(circuit, level="device")
-        config = qml.devices.ExecutionConfig(interface=getattr(circuit, "interface", None))
-        config = qml.device("default.qubit").setup_execution_config(config)
-        assert len(dev_program) == 4 + len(
-            circuit.device.preprocess_transforms(config)
-        )  # currently 8
-
-        full_program = get_transform_program(circuit)
-        assert dev_program == full_program
+    with pytest.warns(
+        PennyLaneDeprecationWarning, match="The 'get_transform_program' function is deprecated"
+    ):
+        _ = qml.workflow.get_transform_program(circuit)
 
 
 @qml.transforms.merge_rotations
@@ -285,6 +123,17 @@ def circuit1(weights, order):
 
 class TestConstructBatch:
     """Tests for the construct_batch function."""
+
+    @pytest.mark.parametrize("level", [[], 0.5, True])
+    def test_level_not_recognized(self, level):
+        """Tests when the level is not recognized."""
+
+        @qml.qnode(qml.device("null.qubit"))
+        def c():
+            return qml.state()
+
+        with pytest.raises(ValueError, match=r"level .* not recognized"):
+            construct_batch(c, level=level)()
 
     def test_level_zero(self):
         """Test that level zero is purely the queued circuit."""
