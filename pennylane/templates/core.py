426f36d266143cf5e2bd73a558f7c9e0f24915b5
Christina Lee <christina@xanadu.ai>
2026-02-25 14:06:04 -0500

    Update pennylane/templates/core.py
    Co-authored-by: Gabriela Sánchez Díaz <gsdiaz21@gmail.com>

--- pennylane/templates/core.py
+++ pennylane/templates/core.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Contains the abstractions for defining subroutines.
+This module contains the abstractions for defining subroutines.
 
 .. currentmodule:: pennylane.templates.core
 
