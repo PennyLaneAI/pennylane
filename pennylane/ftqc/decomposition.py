0c529911cae9b2209d8f0e74966b90fd3027e67e
andrijapau <andrija.paurevic@xanadu.ai>
2026-02-27 11:24:16 -0500

    fix?

--- pennylane/ftqc/decomposition.py
+++ pennylane/ftqc/decomposition.py
@@ -130,7 +130,7 @@ ppr_to_mbqc = transform(pass_name="ppr_to_mbqc", setup_inputs=ppr_to_mbqc_setup_
 def decompose_clifford_ppr_setup_inputs():
     """
     Decompose the PPR (pi/4) into PPR and PPMs operations via flattening method
-    as described in Figure 11(b) in the paper: `arXiv:1808.02892. <https://arxiv.org/abs/1808.02892>`_
+    as described in Figure 11(b) in the paper: `arXiv:1808.02892 <https://arxiv.org/abs/1808.02892>`_.
     """
     return (), {}
 
@@ -143,7 +143,7 @@ decompose_clifford_ppr = transform(
 def decompose_non_clifford_ppr_setup_inputs():
     """
     Decompose the Non-Clifford (pi/8) PPR into PPR and PPMs operations via pauli corrected method
-    as described in Figure 13(a) in the paper: `arXiv:2211.15465. <https://arxiv.org/pdf/2211.15465>`_
+    as described in Figure 13(a) in the paper: `arXiv:2211.15465 <https://arxiv.org/pdf/2211.15465>`_.
     """
     return (), {}
 
