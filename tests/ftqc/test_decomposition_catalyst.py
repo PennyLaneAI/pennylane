b347d34c03e2a9e6de6ff67609c5e3efb5d88e39
andrijapau <andrija.paurevic@xanadu.ai>
2026-03-02 09:41:53 -0500

    add more tests

--- tests/ftqc/test_decomposition_catalyst.py
+++ tests/ftqc/test_decomposition_catalyst.py
@@ -17,6 +17,7 @@
 import pytest
 
 import pennylane as qml
+from pennylane.capture import make_plxpr
 from pennylane.ftqc.decomposition import (
     decompose_clifford_ppr,
     decompose_non_clifford_ppr,
@@ -51,3 +52,53 @@ def test_converstion_to_mlir(pass_fn):
         return qml.sample()
 
     assert pass_fn.pass_name in circ.mlir
+
+
+@pytest.mark.catalyst
+@pytest.mark.parametrize(
+    "pass_fn",
+    [
+        ppr_to_mbqc,
+        decompose_clifford_ppr,
+        decompose_non_clifford_ppr,
+    ],
+)
+def test_pass_is_captured(pass_fn):
+    @pass_fn
+    @qml.qnode(qml.device("lightning.qubit", wires=3), shots=1000)
+    def circ():
+        qml.H(0)
+        qml.S(0)
+        qml.T(1)
+        qml.CNOT([0, 1])
+        return qml.sample()
+
+    plxpr = make_plxpr(circ)()
+    prim = plxpr.eqns[0].primitive
+    assert prim.name == "transform"
+    assert plxpr.eqns[0].params["transform"] == pass_fn
+
+
+@pytest.mark.catalyst
+@pytest.mark.parametrize(
+    "pass_fn",
+    [
+        ppr_to_mbqc,
+        decompose_clifford_ppr,
+        decompose_non_clifford_ppr,
+    ],
+)
+def test_pass_without_qjit_raises_error(pass_fn):
+    """Test that trying to apply the transform without QJIT raises an error"""
+
+    @pass_fn
+    @qml.qnode(qml.device("lightning.qubit", wires=3), shots=1000)
+    def circ():
+        qml.H(0)
+        qml.S(0)
+        qml.T(1)
+        qml.CNOT([0, 1])
+        return qml.sample()
+
+    with pytest.raises(NotImplementedError, match="only implemented when using capture and QJIT"):
+        circ()
