cab18136a1ac8d569002387e566255a827993d40
Jay Soni <jbsoni@uwaterloo.ca>
2026-03-05 17:44:28 -0500

    fix tests

--- pennylane/labs/tests/estimator_beta/wire_manager_tests/test_wire_counting.py
+++ pennylane/labs/tests/estimator_beta/wire_manager_tests/test_wire_counting.py
@@ -329,7 +329,11 @@ class TestProcessCircuitLst:
         actual_processed_circ, actual_circ_wires = _process_circuit_lst(circ)
 
         assert actual_circ_wires == expected_circ_wires
-        assert actual_processed_circ == expected_processed_circ
+        for elem1, elem2 in zip(actual_processed_circ, expected_processed_circ):
+            assert (
+                elem1[0].equal(elem2[0]) if isinstance(elem1[0], MarkClean) else elem1[0] == elem2[0]
+            )
+            assert elem1[1] == elem2[1]
 
 
 class TestEstimateAuxiliaryWires:
