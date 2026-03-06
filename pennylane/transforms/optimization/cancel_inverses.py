199ab43e97d564237890dc676cdc5db8bcd776fa
isaacdevlugt <isaacdevlugt@gmail.com>
2026-03-06 10:33:59 -0500

    format

--- pennylane/transforms/optimization/cancel_inverses.py
+++ pennylane/transforms/optimization/cancel_inverses.py
@@ -452,9 +452,9 @@ def cancel_inverses(
 
         Additionally, the ``cancel_inverses`` transform supports
         `loop-boundary optimization <https://pennylane.ai/compilation/loop-boundary-optimization>`_.
-        
+
         For more technical information on how this transform behaves, consult the Catalyst documentation: :func:`catalyst.passes.cancel_inverses`_.
-        
+
     """
     # Make a working copy of the list to traverse
     list_copy = tape.operations.copy()
