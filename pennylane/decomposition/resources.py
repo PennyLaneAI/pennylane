33d266e92c8c385f1ed4d83cd6dfc99b19cd7ea2
Astral Cai <astral.cai@xanadu.ai>
2026-03-17 10:28:10 -0400

    minor updates

--- pennylane/decomposition/resources.py
+++ pennylane/decomposition/resources.py
@@ -593,7 +593,12 @@ _reconstructors = {}
 
 
 def register_reconstructor(op_type: type[Operator]):
-    """Registers a function as the reconstructor of op_type."""
+    """A decorator that registers a function as the reconstructor of op_type.
+
+    A reconstructor is expected to take ``(*op.data, wires=op.wires, **op.resource_params)``
+    as input and return an instance of the original op.
+
+    """
 
     def _decorator(func: Callable):
         _reconstructors[op_type] = func
@@ -601,20 +606,21 @@ def register_reconstructor(op_type: type[Operator]):
     return _decorator
 
 
-def has_reconstructor(op_class, op_params):
+def has_reconstructor(op_class: type[Operator], op_params: dict):
     """Checks whether a reconstructor exists for the resource rep."""
 
     if op_class not in _reconstructors and not op_class.resource_keys - {"num_wires"}:
         return True
 
-    if op_class in (qml.ops.Adjoint, qml.ops.Controlled, qml.ops.Pow):
+    # TODO: Controlled to be implemented in a follow-up PR [sc-110068]
+    if op_class in (qml.ops.Adjoint, qml.ops.Pow):
         base_class, base_params = op_params["base_class"], op_params["base_params"]
         return has_reconstructor(base_class, base_params)
 
     return op_class in _reconstructors
 
 
-def reconstruct(data, wires: Wires, op_type: type[Operator], op_params: dict) -> Operator:
+def reconstruct(data: tuple, wires: Wires, op_type: type[Operator], op_params: dict) -> Operator:
     """Reconstruct an instance of op_type with resource params."""
 
     if op_type not in _reconstructors and not op_type.resource_keys - {"num_wires"}:
@@ -625,20 +631,20 @@ def reconstruct(data, wires: Wires, op_type: type[Operator], op_params: dict) ->
         # wires. Such an operator also doesn't take anything beyond data and wires
         # in its constructor. The number of wires is typically redundant since this
         # information is apparant from the shape of the wires array already.
-        return op_type(data, wires=wires)
+        return op_type(*data, wires=wires)
 
     if op_type is qml.ops.Adjoint:
         base_class, base_params = op_params["base_class"], op_params["base_params"]
         return qml.adjoint(reconstruct)(data, wires, base_class, base_params)
 
     if op_type is qml.ops.Controlled:
-        raise NotImplementedError  # TODO: to be implemented in a follow-up PR
+        raise NotImplementedError  # TODO: to be implemented in a follow-up PR [sc-110068]
 
     if op_type is qml.ops.Pow:
         base_class, base_params = op_params["base_class"], op_params["base_params"]
         return qml.pow(reconstruct(data, wires, base_class, base_params), z=op_params["z"])
 
     if op_type in _reconstructors:
-        return _reconstructors[op_type](*data, wires, **op_params)
+        return _reconstructors[op_type](*data, wires=wires, **op_params)
 
     raise NotImplementedError
