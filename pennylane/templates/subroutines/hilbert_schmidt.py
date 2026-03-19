9cd1ffd6f648f41fcee9c6db6c76c1e6aca4c7ea
Marcus Edwards <94946848+comp-phys-marc@users.noreply.github.com>
2026-03-19 10:54:51 -0400

    Signature
    Co-authored-by: Astral Cai <astral.cai@xanadu.ai>

--- pennylane/templates/subroutines/hilbert_schmidt.py
+++ pennylane/templates/subroutines/hilbert_schmidt.py
@@ -567,7 +567,6 @@ def _local_hilbert_schmidt_decomposition(
     wires: int | Iterable[int | str] | Wires,
     U: Operator | Iterable[Operator],
     V: Operator | Iterable[Operator],
-    **_,
 ):  # pylint: disable=unused-argument
     n_wires, _, _, wires = _up_to_last_layer(wires, U, V)
 
