3e5727b397700e22182201576328c13676410b72
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-03-23 09:27:03 -0400

    Update docs

--- pennylane/ops/op_math/change_op_basis.py
+++ pennylane/ops/op_math/change_op_basis.py
@@ -95,7 +95,7 @@ def change_op_basis(
     **Example**
 
     Consider the following example involving a ``ChangeOpBasis``. The compute, uncompute pattern is composed of
-    a Quantum Fourier Transform (``QFT``), followed by a ``PhaseAdder``, and finally an inverse ``QFT``.
+    a ``SWAP``, followed by a ``PhaseAdder``, and finally an inverse ``SWAP``.
 
     .. code-block:: python
 
