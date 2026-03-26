da5081e7e2fb94d2f2d99b6b4da4182e33ff2942
David Wierichs <david.wierichs@xanadu.ai>
2026-03-26 18:50:13 -0400

    Small qjit-compatibility patches (#9157)
    **Context:**
    Patch some incompatibilities with `qjit`
    
    **Description of the Change:**
    - Make `DiagonalQubitUnitary` decomposition qjit-compatible (old and new
    system) by using `qml.math.exp` instead of `np.exp`.
    - Make `MultiRZ` work with traced wires.
    - Make `TemporaryAND` work with traced control values.
    
    **Benefits:**
    Compatibility with `qjit`.
    
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**

--- pennylane/ops/qubit/parametric_ops_multi_qubit.py
+++ pennylane/ops/qubit/parametric_ops_multi_qubit.py
@@ -24,7 +24,7 @@ from operator import matmul
 import numpy as np
 
 import pennylane as qml
-from pennylane import math, queuing
+from pennylane import compiler, math, queuing
 from pennylane.capture.autograph import disable_autograph
 from pennylane.decomposition import (
     add_decomps,
@@ -236,6 +236,9 @@ def _multi_rz_decomposition_resources(num_wires):
 
 @register_resources(_multi_rz_decomposition_resources)
 def _multi_rz_decomposition(theta: TensorLike, wires: WiresLike, **__):
+
+    if compiler.active() or qml.capture.enabled():
+        wires = math.array(wires, like="jax")
 
     @qml.for_loop(len(wires) - 1, 0, -1)
     def _pre_cnot(i):
