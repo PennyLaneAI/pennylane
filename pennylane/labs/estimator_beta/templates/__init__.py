739e9cb0e493a75ebf9d86442ce55a76d3966060
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2026-03-27 15:31:04 -0400

    Update pennylane/labs/estimator_beta/templates/__init__.py
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>

--- pennylane/labs/estimator_beta/templates/__init__.py
+++ pennylane/labs/estimator_beta/templates/__init__.py
@@ -11,6 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-r"""This module contains resource operators for basic PennyLane Operators"""
+r"""This module contains resource operators for PennyLane subroutines and templates"""
 
 from .subroutines import selectPauliRot_controlled_resource_decomp
