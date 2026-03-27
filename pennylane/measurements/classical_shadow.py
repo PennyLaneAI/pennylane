e5aabff02f76b7bca05cba90ffffe923194b684c
andrijapau <andrija.paurevic@xanadu.ai>
2026-03-27 10:45:47 -0400

    fix?

--- pennylane/measurements/classical_shadow.py
+++ pennylane/measurements/classical_shadow.py
@@ -603,9 +603,9 @@ def shadow_expval(
          H (Sequence[Operator] | Operator): Obserable(s) whose expectation values are to be estimated.
              Provide a single observable or a sequence to estimate the expectation values of multiple
              observables from the same classical shadows data.
-         k (int): Number of equal parts for which to split the shadow's measurements in order to compute the median of means. The default is ``k=1``, which simply computes
-             the mean of all measurements. ``k>1`` provides no expected advantage for Pauli measurements and Pauli
-             observables.
+         k (int): Number of equal parts for which to split the shadow's measurements in order to compute the median of means.
+             The default is ``k=1``, which simply computes the mean of all measurements.
+             ``k>1`` provides no expected advantage for Pauli measurements and Pauli observables.
          seed (int | None): Optional seed for the random Pauli measurement basis in the
              classical shadows protocol. This controls which bases (X, Y or Z) each qubit is measured
              in per shot. If ``None``, a random seed will be generated.
