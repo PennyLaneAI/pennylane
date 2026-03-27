229e4bf28507cb33c10771ae558565d8a059555d
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2026-03-27 15:23:04 -0400

    Update pennylane/labs/estimator_beta/ops/qubit/__init__.py
    Co-authored-by: Jay Soni <jbsoni@uwaterloo.ca>

--- pennylane/labs/estimator_beta/ops/qubit/__init__.py
+++ pennylane/labs/estimator_beta/ops/qubit/__init__.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-r"""This module contains single qubit operations."""
+r"""This module contains resource operators for single qubit operations."""
 
 from .parametric_ops_multi_qubit import pauliRot_controlled_resource_decomp
