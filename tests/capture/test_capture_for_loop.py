e333358c9eea9fe3482eed56b5f71accbe5eb47c
albi3ro <chrissie.c.l@gmail.com>
2026-04-15 10:48:42 -0400

    stupid formatting in github comment suggestions

--- tests/capture/test_capture_for_loop.py
+++ tests/capture/test_capture_for_loop.py
@@ -350,9 +350,8 @@ class TestDynamicShapes:
 
     @pytest.mark.parametrize("allow_array_resizing", (False, "auto"))
     def test_combine_with_dynamic_closure_var(self, allow_array_resizing):
-"""Test that if the closure variable has a dynamic shape that matches an input dynamic shape, they
-can be combines."""
-        """
+        """Test that if the closure variable has a dynamic shape that matches an input dynamic shape, they
+        can be combines."""
 
         def w(i0):
             c = jnp.arange(i0)
