abef438a45f9b0ff53c12f110e361999c2b81fa3
Christina Lee <christina@xanadu.ai>
2026-04-15 12:40:48 -0400

    Update tests/capture/test_capture_for_loop.py
    Co-authored-by: Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>

--- tests/capture/test_capture_for_loop.py
+++ tests/capture/test_capture_for_loop.py
@@ -351,7 +351,7 @@ class TestDynamicShapes:
     @pytest.mark.parametrize("allow_array_resizing", (False, "auto"))
     def test_combine_with_dynamic_closure_var(self, allow_array_resizing):
         """Test that if the closure variable has a dynamic shape that matches an input dynamic shape, they
-        can be combines."""
+        can be combined."""
 
         def w(i0):
             c = jnp.arange(i0)
