69106c2b86c3c4df3a84722f536cd7dc93f711f2
andrijapau <andrija.paurevic@xanadu.ai>
2026-04-16 10:06:56 -0400

    update tests using sed

--- tests/workflow/test_resolve_mcm_config.py
+++ tests/workflow/test_resolve_mcm_config.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Unit tests for the `qml.workflow.resolution._resolve_mcm_config` helper function"""
+"""Unit tests for the `qp.workflow.resolution._resolve_mcm_config` helper function"""
 
 import pytest
 
