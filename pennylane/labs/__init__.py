dd15bb28c8bd67ddc41f5225504349e498ee6f12
Jay Soni <jbsoni@uwaterloo.ca>
2026-04-17 09:58:25 -0400

    add files

--- pennylane/labs/__init__.py
+++ pennylane/labs/__init__.py
@@ -28,6 +28,7 @@ Modules
     dla
     estimator_beta
     phox
+    profiler
     transforms
     trotter_error
 
@@ -36,6 +37,7 @@ Modules
 from pennylane.labs import dla
 from pennylane.labs import estimator_beta
 from pennylane.labs import phox
+from pennylane.labs import profiler
 from pennylane.labs import transforms
 from pennylane.labs import trotter_error
 
