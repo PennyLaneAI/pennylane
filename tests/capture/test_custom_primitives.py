0af9cd5282c54232ee4ff3b460e337b5f9fc3091
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-04-21 14:54:59 -0400

    Test names

--- tests/capture/test_custom_primitives.py
+++ tests/capture/test_custom_primitives.py
@@ -25,7 +25,7 @@ from pennylane.capture.custom_primitives import PrimitiveType, QmlPrimitive
 pytestmark = pytest.mark.jax
 
 
-def test_qml_primitive_prim_type_default():
+def test_qp_primitive_prim_type_default():
     """Test that the default prim_type of a QmlPrimitive is set correctly."""
     prim = QmlPrimitive("primitive")
     assert prim._prim_type == PrimitiveType("default")  # pylint: disable=protected-access
@@ -34,7 +34,7 @@ def test_qml_primitive_prim_type_default():
 
 @pytest.mark.parametrize("cast_in_enum", [True, False])
 @pytest.mark.parametrize("prim_type", ["operator", "measurement", "transform", "higher_order"])
-def test_qml_primitive_prim_type_setter(prim_type, cast_in_enum):
+def test_qp_primitive_prim_type_setter(prim_type, cast_in_enum):
     """Test that the QmlPrimitive.prim_type setter works correctly"""
     prim = QmlPrimitive("primitive")
     prim.prim_type = PrimitiveType(prim_type) if cast_in_enum else prim_type
@@ -42,7 +42,7 @@ def test_qml_primitive_prim_type_setter(prim_type, cast_in_enum):
     assert prim.prim_type == prim_type
 
 
-def test_qml_primitive_prim_type_setter_invalid():
+def test_qp_primitive_prim_type_setter_invalid():
     """Test that setting an invalid prim_type raises an error"""
     prim = QmlPrimitive("primitive")
     with pytest.raises(ValueError, match="not a valid PrimitiveType"):
