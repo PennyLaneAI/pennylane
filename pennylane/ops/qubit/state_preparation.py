8169bba135cf7b3a00dd288a6e565b5575186732
andrijapau <andrija.paurevic@xanadu.ai>
2026-04-21 14:21:38 -0400

    fix

--- pennylane/ops/qubit/state_preparation.py
+++ pennylane/ops/qubit/state_preparation.py
@@ -88,7 +88,7 @@ class BasisState(StatePrepBase):
     @classmethod
     def _primitive_bind_call(cls, state, wires, **kwargs):
         if isinstance(state, (tuple, list)):
-            state = qp.math.array(state, like="jax")
+            state = qp.math.asarray(state)
         return super()._primitive_bind_call(state, wires, **kwargs)
 
     @property
@@ -398,7 +398,7 @@ class StatePrep(StatePrepBase):
     @classmethod
     def _primitive_bind_call(cls, state, wires, **kwargs):
         if isinstance(state, (tuple, list)):
-            state = qp.math.array(state, like="jax")
+            state = qp.math.asarray(state)
         return super()._primitive_bind_call(state, wires, **kwargs)
 
     # pylint: disable=too-many-arguments,too-many-positional-arguments
