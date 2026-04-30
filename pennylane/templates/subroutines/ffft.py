4ef9b49cf1d6fa6a35d669b1193a27093590f9eb
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-04-30 09:48:46 -0400

    Update circuit diagram

--- pennylane/templates/subroutines/ffft.py
+++ pennylane/templates/subroutines/ffft.py
@@ -122,10 +122,15 @@ class FFFT(Operator):
 
 
     >>> print(qp.draw(circuit, level="device")())
-    0: ─╭TwoQubitFFT───────╭TwoQubitFFT──────────────┤  State
-    1: ─╰TwoQubitFFT───────│────────────╭TwoQubitFFT─┤  State
-    2: ─╭TwoQubitFFT──Z⁰⋅⁰─╰TwoQubitFFT─│────────────┤  State
-    3: ─╰TwoQubitFFT──Z⁰⋅⁵──────────────╰TwoQubitFFT─┤  State
+    0: ─╭TwoQubitFFT────────────────────╭TwoQubitFFT─────────────────────────────────────── ···
+    1: ─╰TwoQubitFFT───────╭fSWAP(3.14)─╰TwoQubitFFT─╭fSWAP(3.14)──────────────╭TwoQubitFFT ···
+    2: ─╭TwoQubitFFT──Z⁰⋅⁰─╰fSWAP(3.14)──────────────╰fSWAP(3.14)─╭fSWAP(3.14)─╰TwoQubitFFT ···
+    3: ─╰TwoQubitFFT──Z⁰⋅⁵────────────────────────────────────────╰fSWAP(3.14)───────────── ···
+    <BLANKLINE>
+    0: ··· ──────────────┤  State
+    1: ··· ──────────────┤  State
+    2: ··· ─╭fSWAP(3.14)─┤  State
+    3: ··· ─╰fSWAP(3.14)─┤  State
 
     The FFFT operation is decomposed recursively into :class:`~TwoQubitFFT` operations (2-site Fermionic Fourier transforms) according to the equation above.
     """
