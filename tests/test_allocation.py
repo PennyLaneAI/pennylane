52bce90a90bed297903972bf520778e503999899
paul0403 <paulhaochen.wang@gmail.com>
2026-05-04 10:11:19 -0400

    test update expected type from int to abstractqubit

--- tests/test_allocation.py
+++ tests/test_allocation.py
@@ -248,9 +248,7 @@ class TestCaptureIntegration:
             "restored": True,
         }
         assert len(jaxpr.eqns[0].outvars) == 2
-        assert all(v.aval.shape == () for v in jaxpr.eqns[0].outvars)
-        for v in jaxpr.eqns[0].outvars:
-            assert v.aval.dtype == jax.numpy.int64
+        assert all(isinstance(v.aval, qp.allocation.AbstractQubit) for v in jaxpr.eqns[0].outvars)
 
         assert jaxpr.eqns[1].invars[0] is jaxpr.eqns[0].outvars[0]
         assert jaxpr.eqns[2].invars[0] is jaxpr.eqns[0].outvars[1]
@@ -283,9 +281,7 @@ class TestCaptureIntegration:
             "restored": False,
         }
         assert len(jaxpr.eqns[0].outvars) == 1
-        assert all(v.aval.shape == () for v in jaxpr.eqns[0].outvars)
-        for v in jaxpr.eqns[0].outvars:
-            assert v.aval.dtype == jax.numpy.int64
+        assert all(isinstance(v.aval, qp.allocation.AbstractQubit) for v in jaxpr.eqns[0].outvars)
 
         assert jaxpr.eqns[1].invars[0] is jaxpr.eqns[0].outvars[0]
 
