fe32aaa8f1569cbd2684f04003cb42831071ef6e
Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>
2026-05-07 16:42:58 -0400

    Update pennylane/workflow/get_compile_pipeline.py
    Co-authored-by: Marcus Edwards <94946848+comp-phys-marc@users.noreply.github.com>

--- pennylane/workflow/get_compile_pipeline.py
+++ pennylane/workflow/get_compile_pipeline.py
@@ -245,7 +245,7 @@ def get_compile_pipeline(
     if _is_qjit(qnode):
         qnode = qnode.user_function
         if not hasattr(qnode, "compile_pipeline"):
-            raise ValueError("Can only retrieve the compilation pipeline of a QJIT'd QNode object.")
+            raise ValueError("Can only retrieve the compilation pipeline if the QJIT'd object is a QNode.")
         is_qjit_qnode = True
 
     @wraps(qnode)
