7fb582a475b9cb5c6d9416db59219ea0b200b2ba
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-05-11 14:39:27 -0400

    [WIP] Resources

--- pennylane/templates/subroutines/arithmetic/signed_out_multiplier.py
+++ pennylane/templates/subroutines/arithmetic/signed_out_multiplier.py
@@ -14,6 +14,10 @@
 """
 Contains the SignedOutMultiplier template.
 """
+from collections import defaultdict
+
+from pennylane.decomposition import register_resources, add_decomps, resource_rep, controlled_resource_rep
+
 from pennylane import measure
 from pennylane.ops import PauliX, Toffoli, Controlled, CNOT
 
@@ -33,8 +37,7 @@ class SignedOutMultiplier(Operator):
     the magnitudes of the encoded inputs, and a quantum comparator on their sign bits to determine
     the final sign of the result.
 
-    If the inputs are given in 2s complement, the sign bits are recorded before their 2s complements
-    are taken, and they can be multiplied by the approach specified above.
+    The inputs are given in 2s complement.
 
     Args:
         x_wires (Sequence[int]): wires that store the signed integer :math:`x`
@@ -49,7 +52,6 @@ class SignedOutMultiplier(Operator):
         output_wires_zeroed (bool): Whether the ``output_wires`` are guaranteed to be in state
             :math:`|0\rangle` initially. Setting this argument to ``True`` reduces the cost of
             the operation.
-        twos_complement (bool): If ``True``, the inputs are taken to be encoded in two's complement
     """
 
     def __init__(
@@ -60,7 +62,6 @@ class SignedOutMultiplier(Operator):
         mod=None,
         work_wires: WiresLike = (),
         output_wires_zeroed: bool = False,
-        twos_complement: bool = False,
         id=None,
     ):
 
@@ -105,7 +106,6 @@ class SignedOutMultiplier(Operator):
             self.hyperparameters[name] = Wires(wires)
         self.hyperparameters["mod"] = mod
         self.hyperparameters["output_wires_zeroed"] = output_wires_zeroed
-        self.hyperparameters["twos_complement"] = twos_complement
 
         # pylint: disable=consider-using-generator
         all_wires = sum([self.hyperparameters[name] for name in wires_name], start=[])
@@ -113,69 +113,110 @@ class SignedOutMultiplier(Operator):
 
     @property
     def resource_params(self) -> dict:
-        return {
-            "num_output_wires": len(self.hyperparameters["output_wires"]),
-            "num_x_wires": len(self.hyperparameters["x_wires"]),
-            "num_y_wires": len(self.hyperparameters["y_wires"]),
-            "num_work_wires": len(self.hyperparameters["work_wires"]),
-            "mod": self.hyperparameters["mod"],
-            "output_wires_zeroed": self.hyperparameters["output_wires_zeroed"],
-            "twos_complement": self.hyperparameters["twos_complement"],
-        }
 
-    @staticmethod
-    def compute_decomposition(
-        x_wires: WiresLike,
-        y_wires: WiresLike,
-        output_wires: WiresLike,
-        mod,
-        work_wires: WiresLike,
-        output_wires_zeroed: bool = False,
-        twos_complement: bool = False,
-    ):  # pylint: disable=arguments-differ, too-many-arguments, unused-argument
-        """Computes the decomposition of the operator as a product of other operators."""
 
-        def twos_complement_helper(input_reg):
-            # Invert all bits
-            for w in input_reg[1:]:
-                # sign bit of 1 indicates a negative value
-                Controlled(PauliX(w), control_wires=(input_reg[0],), control_values=(1,))
-
-            # Add one
-            Controlled(
-                # TODO: make Incrementer a Template
-                _increment(
-                    wires=input_reg[1:],
-                    work_wires=work_wires,
-                ),
-                control_wires=(input_reg[0],),
-                control_values=(1,),
-            )
-
-        # Compute the sign
-        CNOT([x_wires[0], output_wires[0]])
-        CNOT([y_wires[0], output_wires[0]])
-
-        # Take 2s complements if necessary
-        for input_reg in [x_wires, y_wires]:
-            twos_complement_helper(input_reg)
-
-            # we would reset the sign bit here to complete the two's complement,
-            # but we do not, so that we can remember the sign
-
-        # Multiply the magnitudes
-        OutMultiplier(
-            x_wires[1:],
-            y_wires[1:],
-            output_wires[1:],
+def _signed_out_multiplier_resources(num_x_wires, num_y_wires, num_output_wires, num_work_wires, mod, output_wires_zeroed):
+    """
+    Computes the resources for the SignedOutMultiplier.
+    Assumes the worst case that both numbers are negative.
+    """
+    resources = defaultdict(int)
+    resources[
+        controlled_resource_rep(
+            PauliX,
+            {},
+            1,
+            0
+        )
+    ] = (num_x_wires - 1 + num_y_wires - 1) * 2 + num_output_wires - 1
+    resources[
+        controlled_resource_rep(
+            Incrementer,
+            num_wires=num_x_wires - 1
+        )
+    ] += 2
+    resources[
+        resource_rep(
+            OutMultiplier,
+            num_output_wires=num_output_wires - 1,
+            num_x_wires=num_x_wires - 1,
+            num_y_wires=num_y_wires - 1,
+            num_work_wires=num_work_wires,
             mod=mod,
-            work_wires=work_wires,
             output_wires_zeroed=output_wires_zeroed,
         )
+    ] = 1
+    resources[
+        controlled_resource_rep(
+            Incrementer,
+            num_wires=num_output_wires - 1
+        )
+    ] += 1
+    resources[
+        controlled_resource_rep(
+            Incrementer,
+            num_wires=num_y_wires - 1
+        )
+    ] += 2
+    resources[resource_rep(CNOT)] = 2
 
-        # Encode the output
-        twos_complement_helper(output_wires)
 
-        # Return inputs to original state
-        for input_reg in [x_wires, y_wires]:
-            twos_complement_helper(input_reg)
+def _twos_complement_helper(input_reg, work_wires):
+    # Invert all bits
+    for w in input_reg[1:]:
+        # sign bit of 1 indicates a negative value
+        Controlled(PauliX(w), control_wires=(input_reg[0],), control_values=(1,))
+
+    # Add one
+    Controlled(
+        # TODO: make Incrementer a Template
+        _increment(
+            wires=input_reg[1:],
+            work_wires=work_wires,  # TODO: think about whether we can use these work wires
+        ),
+        control_wires=(input_reg[0],),
+        control_values=(1,),
+    )
+
+
+@register_resources(_signed_out_multiplier_resources, exact=False)
+def _signed_out_multiplier_decomposition(
+    x_wires: WiresLike,
+    y_wires: WiresLike,
+    output_wires: WiresLike,
+    mod,
+    work_wires: WiresLike,
+    output_wires_zeroed: bool = False,
+):
+    """Computes the decomposition of the operator as a product of other operators."""
+
+    # Compute the sign
+    CNOT([x_wires[0], output_wires[0]])
+    CNOT([y_wires[0], output_wires[0]])
+
+    # Take 2s complements if necessary
+    for input_reg in [x_wires, y_wires]:
+        _twos_complement_helper(input_reg, work_wires)
+
+        # we would reset the sign bit here to complete the two's complement,
+        # but we do not, so that we can remember the signs
+
+    # Multiply the magnitudes
+    OutMultiplier(
+        x_wires[1:],
+        y_wires[1:],
+        output_wires[1:],
+        mod=mod,
+        work_wires=work_wires,
+        output_wires_zeroed=output_wires_zeroed,
+    )
+
+    # Encode the output
+    _twos_complement_helper(output_wires, work_wires)
+
+    # Return inputs to original state
+    for input_reg in [x_wires, y_wires]:
+        _twos_complement_helper(input_reg, work_wires)
+
+
+add_decomps(SignedOutMultiplier, _signed_out_multiplier_decomposition)
