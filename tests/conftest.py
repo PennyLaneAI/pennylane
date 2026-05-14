c8e60ecc9e504d10f16c5cb24392dceaadaa9067
Lee J. O'Riordan <loriordan@gmail.com>
2026-05-14 11:55:00 -0400

    Capture global enable in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -35,6 +35,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "helpers"))
 TOL = 1e-3
 TF_TOL = 2e-2
 TOL_STOCHASTIC = 0.05
+qp.capture.enable()
 
 
 # pylint: disable=too-few-public-methods
