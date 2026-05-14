09f3f18b7a4b7218e49721b7010d1c5a2e91d6e9
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-05-14 14:46:47 -0400

    Document test

--- tests/templates/subroutines/arithmetic/test_signed_out_multiplier.py
+++ tests/templates/subroutines/arithmetic/test_signed_out_multiplier.py
@@ -64,38 +64,55 @@ def test_signed_out_multiplier_correct(x_wires, y_wires, work_wires, output_wire
     """Tests with a few examples that the Template yields correct results."""
 
     def bin_to_int(bits):
+        """Converts a binary array to an integer."""
         return int("".join(map(str, bits)), 2)
 
-    def twos_complement(bits):
+    def twos_complement_value(bits):
+        """Calculates the value of a number encoded as a twos complement."""
         sum = 0
         for i, bit in enumerate(bits[1:][::-1]):
             sum += (2 ** i) * bit
         sum -= (2 ** (len(bits) - 1)) * bits[0]
         return sum
 
+    # get the initial state of our inputs
     x_state = [init_state[x] for x in x_wires]
     y_state = [init_state[y] for y in y_wires]
 
+    # get the integer value of the x input
     if init_state[0] == 1:
-        x = twos_complement(x_state)
+        # get the value encoded using twos complement if it is negative
+        x = twos_complement_value(x_state)
     else:
+        # otherwise just convert from binary to int
         x = bin_to_int(x_state)
+
+    # get the integer value of the y input
     if init_state[3] == 1:
-        y = twos_complement(y_state)
+        # get the value encoded using twos complement if it is negative
+        y = twos_complement_value(y_state)
     else:
+        # otherwise just convert from binary to int
         y = bin_to_int(y_state)
 
+    # calculate the expected result
     expected = x * y
 
+    # execute the quantum signed out multiplier circuit
     result = signed_multiply(x_wires, y_wires, output_wires, work_wires, init_state)
 
+    # extract the output from the output histogram
     result = math.ceil_log2(list(math.round(result)).index(1)) % (2 ** len(output_wires))
+
+    # convert the result to binary
     binary_result = reduce(lambda acc, bit: acc + [int(bit)], bin(result)[2:], [])
 
+    # pad the result
     while len(binary_result) < len(output_wires):
         binary_result = [0] + binary_result
 
+    # get the value encoded as a twos complement if the result is negative
     if binary_result[0] == 1:
-        result = twos_complement(binary_result)
+        result = twos_complement_value(binary_result)
 
     assert result == expected
