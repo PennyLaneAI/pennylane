8a451e881e52c516ae4afa6125d83e6d6a852158
paul0403 <paulhaochen.wang@gmail.com>
2026-05-15 16:27:48 -0400

    test

--- tests/capture/test_capture_cond.py
+++ tests/capture/test_capture_cond.py
@@ -649,6 +649,34 @@ class TestCondCircuits:
         assert np.allclose(res_ev_jxpr, expected), f"Expected {expected}, but got {res_ev_jxpr}"
 
     @pytest.mark.parametrize(
+        "pred, expected",
+        [
+            (1, -1),
+            (0, 1),
+        ],
+    )
+    def test_circuit_cond_on_operator(self, pred, expected):
+        """Test circuit with qp.cond() on operators directly."""
+
+        @qp.qnode(dev)
+        def circuit_with_cond_on_operator(p):
+            qp.cond(p, qp.X, qp.Z)(0)
+            return qp.expval(qp.Z(0))
+
+        result = circuit_with_cond_on_operator(pred)
+        assert np.allclose(result, expected), f"Expected {expected}, but got {result}"
+
+        args = [pred]
+        jaxpr = jax.make_jaxpr(circuit_with_cond_on_operator)(*args)
+
+        # cond primitive must not return `AbstractQperator`s when the branch fns are operators
+        cond_eqn = jaxpr.eqns[0].params["qfunc_jaxpr"].eqns[1]
+        assert not cond_eqn.outvars
+
+        res_ev_jxpr = jax.core.eval_jaxpr(jaxpr.jaxpr, jaxpr.consts, *args)
+        assert np.allclose(res_ev_jxpr, expected), f"Expected {expected}, but got {res_ev_jxpr}"
+
+    @pytest.mark.parametrize(
         "pred, arg1, arg2, expected",
         [
             (1, 0.5, 0.6, 0.43910855),  # RX(0.10) -> RY(0.5) -> RY(0.6) -> RX(0.10)
