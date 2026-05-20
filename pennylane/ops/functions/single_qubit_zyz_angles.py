b6d4e35259f0f199cd55c5590ef916601fd2243d
Astral Cai <astral.cai@xanadu.ai>
2026-05-20 10:15:37 -0400

    tests

--- pennylane/ops/functions/single_qubit_zyz_angles.py
+++ pennylane/ops/functions/single_qubit_zyz_angles.py
@@ -18,8 +18,8 @@ from functools import singledispatch
 
 import numpy as np
 
-from pennylane.estimator.ops.op_math.symbolic import Adjoint
 from pennylane.operation import Operator
+from pennylane.ops.op_math.adjoint import AdjointOperation
 from pennylane.ops.qubit import (
     RX,
     RY,
@@ -46,75 +46,75 @@ def single_qubit_zyz_angles(op: Operator) -> tuple[TensorLike, TensorLike, Tenso
 @single_qubit_zyz_angles.register
 def _h_rot_angles(op: Hadamard):  # pylint: disable=unused-argument
     # H = RZ(\pi) RY(\pi/2) RZ(0)
-    return (np.pi, np.pi / 2, 0.0, 0.0)
+    return (np.pi, np.pi / 2, 0.0, -np.pi / 2)
 
 
 @single_qubit_zyz_angles.register
 def _x_rot_angles(op: PauliX):  # pylint: disable=unused-argument
     # X = RZ(-\pi/2) RY(\pi) RZ(\pi/2)
-    return (np.pi / 2, np.pi, -np.pi / 2, 0.0)
+    return (np.pi / 2, np.pi, -np.pi / 2, -np.pi / 2)
 
 
 @single_qubit_zyz_angles.register
 def _y_rot_angles(op: PauliY):  # pylint: disable=unused-argument
     # Y = RZ(0) RY(\pi) RZ(0)
-    return (0.0, np.pi, 0.0, 0.0)
+    return (0.0, np.pi, 0.0, -np.pi / 2)
 
 
 @single_qubit_zyz_angles.register
 def _z_rot_angles(op: PauliZ):  # pylint: disable=unused-argument
     # Z = RZ(\pi) RY(0) RZ(0)
-    return (np.pi, 0.0, 0.0, 0.0)
+    return (np.pi, 0.0, 0.0, -np.pi / 2)
 
 
 @single_qubit_zyz_angles.register
 def _s_rot_angles(op: S):  # pylint: disable=unused-argument
     # S = RZ(\pi/2) RY(0) RZ(0)
-    return (np.pi / 2, 0.0, 0.0, 0.0)
+    return (np.pi / 2, 0.0, 0.0, -np.pi / 4)
 
 
 @single_qubit_zyz_angles.register
 def _t_rot_angles(op: T):  # pylint: disable=unused-argument
     # T = RZ(\pi/4) RY(0) RZ(0)
-    return (np.pi / 4, 0.0, 0.0, 0.0)
+    return (np.pi / 4, 0.0, 0.0, -np.pi / 8)
 
 
 @single_qubit_zyz_angles.register
 def _sx_rot_angles(op: SX):  # pylint: disable=unused-argument
     # SX = RZ(-\pi/2) RY(\pi/2) RZ(\pi/2)
-    return (np.pi / 2, np.pi / 2, -np.pi / 2, 0.0)
+    return (np.pi / 2, np.pi / 2, -np.pi / 2, -np.pi / 4)
 
 
 @single_qubit_zyz_angles.register
-def _(op: RX):
+def _rx_rot_angles(op: RX):
     # RX(\theta) = RZ(-\pi/2) RY(\theta) RZ(\pi/2)
     return (np.pi / 2, op.data[0], -np.pi / 2, 0.0)
 
 
 @single_qubit_zyz_angles.register
-def _(op: RY):
+def _ry_rot_angles(op: RY):
     # RY(\theta) = RZ(0) RY(\theta) RZ(0)
     return (0.0, op.data[0], 0.0, 0.0)
 
 
 @single_qubit_zyz_angles.register
-def _(op: RZ):
+def _rz_rot_angles(op: RZ):
     # RZ(\theta) = RZ(\theta) RY(0) RZ(0)
     return (op.data[0], 0.0, 0.0, 0.0)
 
 
 @single_qubit_zyz_angles.register
-def _(op: PhaseShift):
+def _ps_rot_angles(op: PhaseShift):
     # PhaseShift(\theta) = RZ(\theta) RY(0) RZ(0)
-    return (op.data[0], 0.0, 0.0, 0.0)
+    return (op.data[0], 0.0, 0.0, -op.data[0] / 2)
 
 
 @single_qubit_zyz_angles.register
-def _(op: Rot):
+def _rot_rot_angles(op: Rot):
     return tuple(op.data) + (0.0,)
 
 
 @single_qubit_zyz_angles.register
-def _(op: Adjoint):
+def _adjoint_rot_angles(op: AdjointOperation):
     omega, theta, phi, alpha = single_qubit_zyz_angles(op.base)
     return (-phi, -theta, -omega, -alpha)
---
+++ tests/ops/functions/test_single_qubit_zyz_angles.py
@@ -0,0 +1,84 @@
+# Copyright 2026 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for qp.single_qubit_zyz_angles."""
+
+import pytest
+
+import pennylane as qp
+from pennylane.ops import (
+    CRX,
+    RX,
+    RY,
+    RZ,
+    SX,
+    GlobalPhase,
+    Hadamard,
+    PauliX,
+    PauliY,
+    PauliZ,
+    PhaseShift,
+    Rot,
+    S,
+    T,
+)
+from pennylane.ops.functions.single_qubit_zyz_angles import single_qubit_zyz_angles
+
+TEST_OPS = [
+    RX(0.123, wires=0),
+    RY(0.123, wires=0),
+    RZ(0.123, wires=0),
+    PhaseShift(0.123, wires=0),
+    Rot(0.1, 0.2, 0.3, wires=0),
+    SX(0),
+    Hadamard(0),
+    PauliX(0),
+    PauliY(0),
+    PauliZ(0),
+    S(0),
+    T(0),
+]
+
+
+def test_not_implemented_error():
+    """Tests that a NotImplementedError is raised for unregistered ops."""
+
+    with pytest.raises(NotImplementedError):
+        single_qubit_zyz_angles(CRX(0.5, [0, 1]))
+
+
+@pytest.mark.parametrize("op", TEST_OPS)
+def test_angles_correct(op):
+    """Tests that single_qubit_zyz_angles are correct."""
+
+    expected_mat = qp.matrix(op)
+
+    phi, theta, omega, alpha = single_qubit_zyz_angles(op)
+    decomp = [RZ(phi, wires=0), RY(theta, wires=0), RZ(omega, wires=0), GlobalPhase(alpha)]
+    decomp_mat = qp.matrix(decomp, wire_order=[0])
+
+    assert qp.math.allclose(decomp_mat, expected_mat)
+
+
+@pytest.mark.parametrize("op", TEST_OPS)
+def test_angles_correct_adjoint(op):
+    """Tests that single_qubit_zyz_angles are correct for adjoint ops"""
+
+    expected_mat = qp.matrix(qp.adjoint(op))
+
+    phi, theta, omega, alpha = single_qubit_zyz_angles(qp.adjoint(op))
+    decomp = [RZ(phi, wires=0), RY(theta, wires=0), RZ(omega, wires=0), GlobalPhase(alpha)]
+    decomp_mat = qp.matrix(decomp, wire_order=[0])
+
+    assert qp.math.allclose(decomp_mat, expected_mat)
