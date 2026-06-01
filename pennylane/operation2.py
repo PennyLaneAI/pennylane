346fd2bd78f52eed6184941fb5cd739abb625938
Marcus Edwards <94946848+comp-phys-marc@users.noreply.github.com>
2026-06-01 15:10:49 -0400

    Not porting operation_derivative
    Co-authored-by: Mudit Pandey <18223836+mudit2812@users.noreply.github.com>

--- pennylane/operation2.py
+++ pennylane/operation2.py
@@ -1402,25 +1402,3 @@ class Operation2(Operator2, is_base=True):
         super().__init_subclass__()
 
 
-def operation_derivative(operation: Operation2) -> TensorLike:
-    r"""Calculate the derivative of an operation.
-
-    For an operation :math:`e^{i \hat{H} \phi t}`, this function returns the matrix representation
-    in the standard basis of its derivative with respect to :math:`t`, i.e.,
-
-    .. math:: \frac{d \, e^{i \hat{H} \phi t}}{dt} = i \phi \hat{H} e^{i \hat{H} \phi t},
-
-    where :math:`\phi` is a real constant.
-
-    Args:
-        operation (.Operation): The operation to be differentiated.
-
-    Returns:
-        array: the derivative of the operation as a matrix in the standard basis
-
-    Raises:
-        ValueError: if the operation does not have a generator or is not composed of a single
-            trainable parameter
-    """
-    generator = qp.matrix(qp.generator(operation, format="observable"), wire_order=operation.wires)
-    return 1j * generator @ operation.matrix()
