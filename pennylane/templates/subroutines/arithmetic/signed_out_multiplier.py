dba852844337c894e760456d5ac9e790a624cd4d
Marcus Edwards <94946848+comp-phys-marc@users.noreply.github.com>
2026-06-03 08:49:23 -0400

    notation tweaks
    Co-authored-by: David Wierichs <david.wierichs@xanadu.ai>

--- pennylane/templates/subroutines/arithmetic/signed_out_multiplier.py
+++ pennylane/templates/subroutines/arithmetic/signed_out_multiplier.py
@@ -44,7 +44,7 @@ except (ModuleNotFoundError, ImportError) as import_error:  # pragma: no cover
 
 class SignedOutMultiplier(Operator):
     r"""
-    Implements signed out-place multiplication :math:`|x,y,z\rangle \mapsto |x,y,(z + x*y) \text{mod} 2^{|z|}\rangle`.
+    Implements signed out-place multiplication :math:`|x,y,z\rangle \mapsto |x,y,(z + xy) \mod 2^{|z|}\rangle`.
 
     The inputs and output are given in `2s complement <https://en.wikipedia.org/wiki/Two%27s_complement>`__. 
     The value :math:`x` encoded by a bitstring :math:`x_{n-1} x_{n-2}\dots x_0` using 2s complement
