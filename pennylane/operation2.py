10fea2ce99f664a4dd15a81f78a6d80a8a5bc44a
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-06-03 14:06:45 -0400

    Make subclass of Operator2

--- pennylane/operation2.py
+++ pennylane/operation2.py
@@ -1258,7 +1258,7 @@ def _is_hash_leaf(l) -> bool:
     return _is_op(l) or _is_wires(l)
 
 
-class StatePrepBase2:
+class StatePrepBase2(Operator2):
     """An interface for state-prep operations."""
 
     @abc.abstractmethod
