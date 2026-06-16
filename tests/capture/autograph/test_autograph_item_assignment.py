4bddc5622c532188be54f47199473f48f923b986
Astral Cai <astral.cai@xanadu.ai>
2026-06-16 10:46:18 -0400

    Apply suggestions from code review
    Co-authored-by: Astral Cai <astralcai@gmail.com>

--- tests/capture/autograph/test_autograph_item_assignment.py
+++ tests/capture/autograph/test_autograph_item_assignment.py
@@ -203,10 +203,9 @@ def test_qnode_with_python_array_assignment():
     assert jnp.allclose(res, -1.0)
 
     # Test gradient
-    if not qp.capture.enabled():
-        grad = jax.grad(ag_circuit, argnums=0)(new_angle)
-        # d/dx cos(x) = -sin(x), at x=pi, -sin(pi) = 0
-        assert jnp.allclose(grad, 0.0)
+    jaxpr = jax.make_jaxpr(ag_circuit)(jnp.pi)
+    qfunc_jaxpr = jaxpr.eqns[0].params['qfunc_jaxpr']
+    assert qfunc_jaxpr.eqns[0].invars[0] is qfunc_jaxpr.invars[0]
 
 
 @pytest.mark.usefixtures("enable_disable_plxpr")
@@ -230,10 +229,10 @@ def test_qnode_with_jax_array_assignment():
     assert jnp.allclose(res, -1.0)
 
     # Test gradient
-    if not qp.capture.enabled():
-        grad = jax.grad(ag_circuit, argnums=1)(angles_in, new_angle)
-        # d/dx cos(x) = -sin(x), at x=pi, -sin(pi) = 0
-        assert jnp.allclose(grad, 0.0)
+    jaxpr = jax.make_jaxpr(ag_circuit)(angles_in, jnp.pi)
+    qfunc_jaxpr = jaxpr.eqns[0].params['qfunc_jaxpr']
+    assert qfunc_jaxpr.eqns[3].primitive.name == "scatter"
+    assert qfunc_jaxpr.eqns[4].primitive.name == "slice"
 
 
 @pytest.mark.usefixtures("enable_disable_plxpr")
