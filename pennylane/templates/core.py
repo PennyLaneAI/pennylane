6c07728bdf4803525e2041a1d79e946ccf260b11
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-06-18 17:06:16 -0400

    Update AbstractWires refs

--- pennylane/templates/core.py
+++ pennylane/templates/core.py
@@ -51,7 +51,7 @@ from pennylane.decomposition.resources import auto_wrap
 from pennylane.core import Operation, Operator
 from pennylane.ops import ChangeOpBasis
 from pennylane.pytrees import flatten, unflatten
-from pennylane.typing import AbstractArray, AbstractWires
+from pennylane.typing import AbstractArray, AbstractWires, Wire
 from pennylane.wires import Wires, is_abstract_qubit
 
 has_jax = find_spec("jax") is not None
@@ -187,7 +187,7 @@ def subroutine_resource_rep(subroutine: "Subroutine", *args, **kwargs) -> Compre
             pass
 
         abstract_params = Float[4]
-        abstract_wires = qp.wires.Wires[1] # a single wire
+        abstract_wires = qp.typing.Wire[1] # a single wire
         S_rep = subroutine_resource_rep(S, abstract_params, abstract_wires, qp.RX)
 
         @qp.decomposition.register_resources({S_rep: 1})
@@ -228,7 +228,7 @@ def _create_signature_key(
         if arg in static_argnames:
             key.append(val)
         elif arg in wire_argnames:
-            key.append(Wires[len(val)])
+            key.append(Wire[len(val)])
         else:
             leaves, struct = flatten(val)
 
@@ -610,7 +610,7 @@ class Subroutine:
 
     >>> from pennylane.typing import Float
     >>> abstract_params = Float[10]
-    >>> abstract_wires = qp.wires.Wires[1]
+    >>> abstract_wires = qp.typing.Wire[1]
     >>> RXLayer.compute_resources(abstract_params, abstract_wires)
     {<class 'pennylane.ops.qubit.parametric_ops_single_qubit.RX'>: 10}
 
@@ -626,7 +626,7 @@ class Subroutine:
             pass
 
         abstract_params = Float[3]
-        abstract_wires = qp.wires.Wires[3]
+        abstract_wires = qp.typing.Wire[3]
         rxlayer_rep = subroutine_resource_rep(RXLayer, abstract_params, abstract_wires)
 
         @qp.decomposition.register_resources({rxlayer_rep: 1})
