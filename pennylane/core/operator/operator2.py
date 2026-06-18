7051c95f95474916ba0a031a2f0a976e72e1732b
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-06-18 10:14:03 -0400

    pylint

--- pennylane/core/operator/operator2.py
+++ pennylane/core/operator/operator2.py
@@ -22,7 +22,7 @@ from collections.abc import Hashable, Iterable, Sequence
 from copy import copy, deepcopy
 from functools import partial
 from inspect import BoundArguments, Signature, signature
-from typing import Any, Callable, ClassVar, Union
+from typing import Any, Callable, ClassVar
 
 import numpy as np
 from scipy.sparse import spmatrix
