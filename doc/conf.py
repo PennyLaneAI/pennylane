8289d9a9b161be2e0a8e6334ddca0f2e3dba808a
runora95 <runor.agbaire@xanadu.ai>
2026-06-19 13:13:53 -0400

    update filter

--- doc/conf.py
+++ doc/conf.py
@@ -102,7 +102,7 @@ mathjax_path = (
 )
 ignore_warnings = [
     ("code/api/qp_transforms*", "no module named pennylane.transforms"),
-    (None, "Explicit markup ends without a blank line"),
+    (None, "Explicit markup ends without a blank line; unexpected unindent. [docutils]"),
 ]
 autodoc_mock_imports = ["torch"]
 
