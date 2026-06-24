6a56686b9349ab2033dac3ab2a10d70c719456ef
dwierichs <david.wierichs@xanadu.ai>
2026-06-24 09:09:37 +0200

    add semi signed multiplier

--- pennylane/labs/templates/__init__.py
+++ pennylane/labs/templates/__init__.py
@@ -21,11 +21,14 @@ This module contains templates, which are pre-coded routines that can be used in
 
     ~LeftClassicalComparator
     ~LeftQuantumComparator
+    ~semi_signed_out_multiplier
     ~SumOfSlatersPrep2
     ~trotter_fragmented
+    ~trotter_vibronic
 """
 
 from .left_classical_comparator import LeftClassicalComparator
 from .left_quantum_comparator import LeftQuantumComparator
+from .semi_signed_out_multiplier import semi_signed_out_multiplier
 from .sum_of_slaters2 import SumOfSlatersPrep2
 from .trotter_fragmented import trotter_fragmented
