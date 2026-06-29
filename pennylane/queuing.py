1c9570b572e38554c000d76f74c626b38fab8942
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-06-29 13:38:18 -0400

    Format

--- pennylane/queuing.py
+++ pennylane/queuing.py
@@ -18,6 +18,7 @@ The previous location for pennylane.core.queuing.
 # pylint: disable=wildcard-import, unused-wildcard-import
 from pennylane.core.queuing import *  # tach-ignore
 
+
 def __getattr__(key):
     if key == "process_queue":
         raise AttributeError(
