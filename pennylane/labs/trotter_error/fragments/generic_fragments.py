8469b8bd3ec19818e2e1b7b58398229c3202ef05
Will <wmaxwell90@gmail.com>
2026-06-29 08:28:05 -0400

    Update pennylane/labs/trotter_error/fragments/generic_fragments.py
    Co-authored-by: Soran Jahangiri <40344468+soranjh@users.noreply.github.com>

--- pennylane/labs/trotter_error/fragments/generic_fragments.py
+++ pennylane/labs/trotter_error/fragments/generic_fragments.py
@@ -169,7 +169,9 @@ class SparseFragment(Fragment):
 
 class SparseState(TrotterState):
     """A wrapper class to allow scipy sparse vectors to be used in the Trotter error estimation
-    functions. This class is intended to instantiate states to be used along with
+    functions. 
+    
+    This class is intended to instantiate states to be used along with
     the `SparseFragment` class.
     """
 
