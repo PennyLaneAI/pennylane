b92fde2ad389ce53a71bf2ad1cc150c8817f4933
Astral Cai <astral.cai@xanadu.ai>
2026-07-06 10:00:56 -0400

    nvm

--- pennylane/ops/op_math/controlled2.py
+++ pennylane/ops/op_math/controlled2.py
@@ -445,7 +445,7 @@ class ControlledOp2(Controlled2):  # pylint: disable=too-few-public-methods
 
     hybrid_argnames = ("base",)
 
-    compilable_argnames = ("work_wire_type", "_one_controlled")
+    static_argnames = ("work_wire_type", "_one_controlled")
 
     arg_specs = {"control_values": Bool[-1], "control_wires": Wire[-1], "work_wires": Wire[-1]}
 
