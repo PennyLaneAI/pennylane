23777ee23546b03dc73d8a3cb3ecbb08510bccc9
andrijapau <andrija.paurevic@xanadu.ai>
2026-07-09 12:49:15 -0400

    fix

--- pennylane/ops/qubit/parametric_ops_single_qubit.py
+++ pennylane/ops/qubit/parametric_ops_single_qubit.py
@@ -29,7 +29,7 @@ import numpy as np
 import scipy as sp
 
 import pennylane as qp
-from pennylane.core.operator import Operation, Operator2
+from pennylane.core.operator import Operator2
 from pennylane.decomposition import (
     add_decomps,
     adjoint_resource_rep,
@@ -42,7 +42,7 @@ from pennylane.decomposition.symbolic_decomposition import (
     flip_zero_control,
     pow_rotation,
 )
-from pennylane.exceptions import DecompositionUndefinedError, PennyLaneDeprecationWarning
+from pennylane.exceptions import PennyLaneDeprecationWarning
 from pennylane.typing import Complex, TensorLike, Wire
 from pennylane.wires import WiresLike
 
