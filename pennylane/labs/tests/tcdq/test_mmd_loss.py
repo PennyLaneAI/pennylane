106f32730d8c179e96f8f437bd0f4a3a96f12ad6
Austin Huang <austin.huang@xanadu.ai>
2026-07-10 09:57:28 -0400

    Port tcdq docs pr (#9774)
    **Context:**
    This PR refactors the docs for the tcdq module as a whole.
    
    **Description of the Change:**
    
    **Benefits:**
    
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**
    
    ---------
    
    Co-authored-by: Soran Jahangiri <40344468+soranjh@users.noreply.github.com>
    Co-authored-by: Will <wmaxwell90@gmail.com>

--- pennylane/labs/tests/tcdq/test_mmd_loss.py
+++ pennylane/labs/tests/tcdq/test_mmd_loss.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for the tcdq MMD loss function."""
+"""Regression tests for the qubit TCDQ MMD loss."""
 
 import itertools
 
