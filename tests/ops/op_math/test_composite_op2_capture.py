12413fba82397aa4010dfac5bec5a92f5281740e
andrijapau <andrija.paurevic@xanadu.ai>
2026-07-14 15:14:56 -0400

    fix test

--- tests/ops/op_math/test_composite_op2_capture.py
+++ tests/ops/op_math/test_composite_op2_capture.py
@@ -96,21 +96,20 @@ def test_public_sum_binding():
 
 
 @pytest.mark.parametrize("defined_outside", (True, False))
-@pytest.mark.parametrize("creation_method", (qp.change_op_basis, ChangeOpBasis))
-def test_change_op_basis(defined_outside, creation_method):
+def test_change_op_basis_public_api(defined_outside):
     """Tests that change_op_basis captures correctly."""
 
     outside_op = NonParametricOp(0) if defined_outside else None
 
     def f(x):
         op = outside_op if defined_outside else NonParametricOp(0)
-        creation_method(op, DynOp(x, 1))
+        qp.change_op_basis(op, DynOp(x, 1))
 
     cjaxpr = jax.make_jaxpr(f)(1.2)
 
     eqns = cjaxpr.eqns
 
-    assert len(eqns) == 4 if creation_method is ChangeOpBasis else 3  # Op1 + Op2 + Adjoint(Op1)
+    assert len(eqns) == 3  # Op1 + Op2 + Adjoint(Op1)
 
     assert eqns[0].primitive.name == "operator"
     assert eqns[0].params["op_cls"] is NonParametricOp
@@ -123,6 +122,39 @@ def test_change_op_basis(defined_outside, creation_method):
     assert eqns[2].primitive.name == "operator"
     assert eqns[2].params["op_cls"] is NonParametricOp
     assert eqns[2].params["adjoint"] is True
+
+
+@pytest.mark.parametrize("defined_outside", (True, False))
+def test_change_op_basis_operator(defined_outside):
+    """Tests that change_op_basis captures correctly."""
+
+    outside_op = NonParametricOp(0) if defined_outside else None
+
+    def f(x):
+        op = outside_op if defined_outside else NonParametricOp(0)
+        ChangeOpBasis(op, DynOp(x, 1))
+
+    cjaxpr = jax.make_jaxpr(f)(1.2)
+
+    eqns = cjaxpr.eqns
+
+    assert len(eqns) == 4  # Op1 + Op2 + Adjoint(Op1) + ChangeOpBasis
+
+    assert eqns[2].primitive.name == "operator"
+    assert eqns[2].params["op_cls"] is NonParametricOp
+    assert eqns[2].params["adjoint"] is False
+
+    assert eqns[0].primitive.name == "operator"
+    assert eqns[0].params["op_cls"] is DynOp
+    assert eqns[0].params["adjoint"] is False
+
+    assert eqns[1].primitive.name == "operator"
+    assert eqns[1].params["op_cls"] is NonParametricOp
+    assert eqns[1].params["adjoint"] is True
+
+    assert eqns[3].invars[0] == eqns[2].outvars[0]  # compute_op
+    assert eqns[3].invars[1] == eqns[0].outvars[0]  # target
+    assert eqns[3].invars[2] == eqns[1].outvars[0]  # uncompute_op
 
 
 def test_linear_combination():
