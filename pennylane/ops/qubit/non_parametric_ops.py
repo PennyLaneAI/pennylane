354e7a0c3466e1289d7ac1c0428e5260c15ddf89
JerryChen97 <chenys13@outlook.com>
2026-07-16 12:16:14 -0400

    pylint!!!!

--- pennylane/ops/qubit/non_parametric_ops.py
+++ pennylane/ops/qubit/non_parametric_ops.py
@@ -665,12 +665,12 @@ class PauliY(Operator2):
     def __init__(self, wires: WiresLike):
         super().__init__(wires=wires)
 
-    def __repr__(self) -> str:  # pylint:disable=unsubscriptable-object
+    def __repr__(self) -> str:
         """String representation."""
         if not isinstance(self.wires, Wires):
             return self.name
 
-        wire = self.wires[0]
+        wire = self.wires[0]  # pylint:disable=unsubscriptable-object
         if isinstance(wire, str):
             return f"Y('{wire}')"
         return f"Y({wire})"
