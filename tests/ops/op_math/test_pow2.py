5308f179d9a1290d5442f1b4b98994e5320616d5
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-07-17 09:43:41 -0400

    Improve test init

--- tests/ops/op_math/test_pow2.py
+++ tests/ops/op_math/test_pow2.py
@@ -13,8 +13,9 @@
 # limitations under the License.
 
 """Tests for the Pow2 class."""
-from pennylane.core import AnnotatedQueue
-from pennylane.ops.op_math.pow2 import pow2, Pow2
+
+from pennylane.ops import ISWAP, Identity, PhaseShift, T, Z
+from pennylane.ops.op_math.pow2 import Pow2, pow2
 from tests.core.operator.operator2_utils import DynOp
 
 
@@ -23,11 +24,31 @@ def test_initialization():
 
     base = DynOp(0.5, wires=0)
 
-    op = pow2(base, z=0.5)
+    # lazy
+    op = pow2(base, z=0.5, lazy=True)
     assert isinstance(op, Pow2)
     assert op.static_args["z"] == 0.5
     assert op.base == base
 
+    # eager
+    op = pow2(base, z=0.5, lazy=False)
+    assert isinstance(op, Pow2)
+    assert op.static_args["z"] == 0.5
+    assert op.base == base
+
+    # has a custom pow()
+    op = pow2(T(0), z=3, lazy=False)
+    assert isinstance(op, PhaseShift)
+
+    # produces no ops
+    op = pow2(Identity(0), z=2, lazy=False)
+    assert isinstance(op, Identity)
+
+    # produces multiple ops
+    ops = pow2(ISWAP((0, 1)), z=6, lazy=False)
+    assert ops == Z(0) @ Z(1)
+
+    # we call Pow2 directly
     op = Pow2(base, z=1.5)
     assert isinstance(op, Pow2)
     assert op.static_args["z"] == 1.5
