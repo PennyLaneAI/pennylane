cec348f255c992cb551d6d8241407af3e2bda773
Astral Cai <astral.cai@xanadu.ai>
2026-07-21 10:52:16 -0400

    more fixes

--- pennylane/fourier/mark.py
+++ pennylane/fourier/mark.py
@@ -17,9 +17,9 @@ Contains the 'label' function for customizing operator labels.
 
 # pylint: disable=unused-argument
 
-from pennylane.core.operator import Operator
+from pennylane.core.operator import Operator, abstractify
 from pennylane.core.queuing import apply
-from pennylane.decomposition import add_decomps, register_resources, resource_rep
+from pennylane.decomposition import add_decomps, register_resources
 from pennylane.ops.functions.equal import (
     BASE_OPERATION_MISMATCH_ERROR_MESSAGE,
     _equal,
@@ -53,7 +53,7 @@ class MarkedOp(SymbolicOp):
         hyperparams_dict = dict(metadata)
         return cls(data[0], **hyperparams_dict)
 
-    resource_keys = {"base_class", "base_params"}
+    resource_keys = {"base"}
 
     def __init__(self, base: Operator, marker: str):
         super().__init__(base)
@@ -64,7 +64,7 @@ class MarkedOp(SymbolicOp):
 
     @property
     def resource_params(self) -> dict:
-        return {"base_class": type(self.base), "base_params": self.base.resource_params}
+        return {"base": abstractify(self.base)}
 
     # pylint: disable=arguments-renamed, invalid-overridden-method
     @property
@@ -94,8 +94,8 @@ class MarkedOp(SymbolicOp):
         return self.base.matrix(wire_order=wire_order)
 
 
-def _resources(base_class, base_params):
-    return {resource_rep(base_class, **base_params): 1}
+def _resources(base):
+    return {base: 1}
 
 
 @register_resources(_resources)
