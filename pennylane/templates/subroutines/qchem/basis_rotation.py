a7a83b4a3df01450fb77d9445cf467b5ff4d5a2c
Marcus Edwards <94946848+comp-phys-marc@users.noreply.github.com>
2026-07-23 13:20:17 -0400

    match sig
    Co-authored-by: Astral Cai <astral.cai@xanadu.ai>

--- pennylane/templates/subroutines/qchem/basis_rotation.py
+++ pennylane/templates/subroutines/qchem/basis_rotation.py
@@ -389,7 +389,7 @@ class BasisRotation(Operator2):
 
 
 # pylint: disable=unused-argument
-def _basis_rotation_decomp_resources(wires, unitary_matrix, check=False):
+def _basis_rotation_decomp_resources(unitary_matrix, wires):
     dim = math.shape(unitary_matrix)[0]
 
     se_count = dim * (dim - 1) / 2
