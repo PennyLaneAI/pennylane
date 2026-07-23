c4c8d4b8e95812dd66f76ce0bcc3bc064060b8cc
andrijapau <andrija.paurevic@xanadu.ai>
2026-07-23 17:01:06 -0400

    xfail

--- tests/templates/embeddings/test_iqp_emb.py
+++ tests/templates/embeddings/test_iqp_emb.py
@@ -150,6 +150,9 @@ class TestDecomposition:
         ([[1.0, 1.0, 1.0], [2.0, 2.0, 2.0], [3.0, 3.0, 3.0]], [1, 2, 3], 4, [[2, 1], [1, 3]]),
     ]
 
+    @pytest.mark.xfail(
+        reason="Parameter broadcasting is being re-visited in the future for Operator2."
+    )
     @pytest.mark.capture
     @pytest.mark.parametrize(("features", "wires", "num_repeats", "pattern"), DECOMP_PARAMS)
     def test_decomposition_new(self, features, wires, num_repeats, pattern):
