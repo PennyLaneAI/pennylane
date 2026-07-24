72dbc66ff05c9f293e154ed5ec4c3d57f72fd7d0
Mudit Pandey <mudit.pandey@xanadu.ai>
2026-07-24 12:28:29 -0400

    Fix another failure

--- pennylane/decomposition/symbolic_decomposition.py
+++ pennylane/decomposition/symbolic_decomposition.py
@@ -111,7 +111,7 @@ def _adjoint_rotation_resource(base=None, base_class=None, base_params=None, **_
 def adjoint_rotation(*_, base, **__):
     """Decompose the adjoint of a rotation operator by inverting the angle."""
     # A rotation should only have 1 dynamic parameter
-    angle = tuple(base.dynamic_args.values)[0] if isinstance(base, Operator2) else base.data[0]
+    angle = tuple(base.dynamic_args.values())[0] if isinstance(base, Operator2) else base.data[0]
     qp.ops.functions.bind_new_parameters(base, (-angle,))
 
 
@@ -216,7 +216,7 @@ def _pow_rotation_resource(
 def pow_rotation(*_, base, z, **__):
     """Decompose the power of a general rotation operator by multiplying the power by the angle."""
     # A rotation should only have 1 dynamic parameter
-    angle = tuple(base.dynamic_args.values)[0] if isinstance(base, Operator2) else base.data[0]
+    angle = tuple(base.dynamic_args.values())[0] if isinstance(base, Operator2) else base.data[0]
     qp.ops.functions.bind_new_parameters(base, (angle * z,))
 
 
