29888dcdd625745d4b5600627dd0552e68c386c8
andrijapau <andrija.paurevic@xanadu.ai>
2026-07-29 14:20:36 -0400

    rename

--- pennylane/labs/transforms/__init__.py
+++ pennylane/labs/transforms/__init__.py
@@ -37,6 +37,3 @@ Custom decomposition rules
 """
 
 from .select_pauli_rot_phase_gradient import select_pauli_rot_phase_gradient
-from .decomp_rz_phase_gradient import make_rz_to_phase_gradient_decomp
-from .decomp_crz_phase_gradient import make_crz_to_phase_gradient_decomp
-from .decomp_selectpaulirot_phase_gradient import make_selectpaulirot_to_phase_gradient_decomp
--- pennylane/transforms/decompositions/__init__.py
+++ pennylane/transforms/decompositions/__init__.py
@@ -15,3 +15,6 @@ r"""This module contains decompositions for quantum circuits."""
 
 from .clifford_t_transform import clifford_t_decomposition
 from .gridsynth import gridsynth
+from .decomp_rz_phase_gradient import make_rz_to_phase_gradient_decomp
+from .decomp_crz_phase_gradient import make_crz_to_phase_gradient_decomp
+from .decomp_selectpaulirot_phase_gradient import make_selectpaulirot_to_phase_gradient_decomp
