18bc4cadc6c75f74fb75ea870673be650ccd5c89
Will <wmaxwell90@gmail.com>
2026-07-30 11:37:06 -0400

    disable pylint warning

--- pennylane/labs/tcdq/qudit_expval_functions.py
+++ pennylane/labs/tcdq/qudit_expval_functions.py
@@ -478,7 +478,8 @@ def _compute_mc_statistics(
     return expvals, cov, mean_y_sq
 
 
-def build_qudit_expval_func(
+def build_qudit_expval_func( #pylint: disable=too-many-statements
+
     config: QuditCircuitConfig,
 ) -> Callable:
     """Build an estimator for expectation values of a qudit IQP circuit.
