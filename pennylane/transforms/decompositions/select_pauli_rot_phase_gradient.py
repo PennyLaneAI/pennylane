1d65cb8598316c980a06251ee3450e18be79d7f0
andrijapau <andrija.paurevic@xanadu.ai>
2026-08-03 09:52:55 -0400

    update qrom resource rep

--- pennylane/transforms/decompositions/select_pauli_rot_phase_gradient.py
+++ pennylane/transforms/decompositions/select_pauli_rot_phase_gradient.py
@@ -25,7 +25,7 @@ from pennylane.ops import Prod
 from pennylane.ops.op_math import change_op_basis
 from pennylane.ops.op_math.adjoint2 import _adjoint_abstract
 from pennylane.ops.op_math.controlled2 import _ctrl_abstract
-from pennylane.typing import Wire
+from pennylane.typing import Int, Wire
 from pennylane.wires import WireError, Wires
 
 from .rz_phase_gradient import validate_phase_gradient_wires
@@ -188,13 +188,12 @@ def make_selectpaulirot_to_phase_gradient_decomp(angle_wires, phase_grad_wires, 
                     return {qp.RZ: 1}
 
         # 1. QROM compressed rep
-        qrom_rep = resource_rep(
-            qp.QROM,
+        qrom_rep = qp.QROM(
+            data=Int[2**num_control_wires, len(angle_wires)],
+            control_wires=Wire[num_control_wires],
+            target_wires=Wire[len(angle_wires)],
+            work_wires=Wire[num_control_wires - 1],
             clean=True,
-            num_bitstrings=2**num_control_wires,
-            num_control_wires=num_control_wires,
-            num_target_wires=len(angle_wires),
-            num_work_wires=num_control_wires - 1,
         )
 
         # 2. ctrl(X, control=target_wire, control_values=[0])
