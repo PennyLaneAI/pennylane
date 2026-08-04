c03a7591a99e8ddcbb270b9cb3a3f8ad4a81cd4b
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-08-04 14:01:10 -0400

    pylint

--- pennylane/templates/state_preparations/multiplexer_state_prep.py
+++ pennylane/templates/state_preparations/multiplexer_state_prep.py
@@ -17,7 +17,7 @@ import pennylane as qp
 from pennylane import math
 from pennylane.core import queuing
 from pennylane.core.operator import Operation
-from pennylane.decomposition import add_decomps, register_resources, resource_rep
+from pennylane.decomposition import add_decomps, register_resources
 from pennylane.templates.state_preparations.mottonen import _get_alpha_y
 from pennylane.typing import Complex, Float, Wire
 from pennylane.wires import Wires
