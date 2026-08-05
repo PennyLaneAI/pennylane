959148755e5fbfe9fd90bfa95bb9e89e2e3ea206
INMLe <64839985+INMLe@users.noreply.github.com>
2026-08-05 17:41:17 -0400

    Update init

--- pennylane/labs/templates/__init__.py
+++ pennylane/labs/templates/__init__.py
@@ -35,4 +35,4 @@ from .superposition_thc import SuperpositionTHC
 from .half_signed_out_multiplier import half_signed_out_multiplier
 from .sum_of_slaters2 import SumOfSlatersPrep2
 from .trotter_fragmented import trotter_fragmented
-from .mps_synthesis import mps_synthesis
+from .mps_synthesis import mps_preparation, mps_synthesis
