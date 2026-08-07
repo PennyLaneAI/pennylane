2a88c91e38363b7fdd70f1dcc130f5c598dc7449
andrijapau <andrija.paurevic@xanadu.ai>
2026-08-07 13:11:47 -0400

    fix some doctests

--- pennylane/ops/op_math/decompositions/unitary_decompositions.py
+++ pennylane/ops/op_math/decompositions/unitary_decompositions.py
@@ -66,13 +66,13 @@ def one_qubit_decomposition(U, wire, rotations="ZYZ", return_global_phase=False)
     [RZ(3.14159..., wires=[0]),
      RY(np.float64(1.57079...), wires=[0]),
      RZ(0.0, wires=[0]),
-     GlobalPhase(np.float64(-1.57079...), wires=[])]
+     GlobalPhase(-1.57079..., wires=[])]
     >>> decomp = qp.ops.one_qubit_decomposition(U, 0, rotations='XZX', return_global_phase=True)
     >>> pprint(decomp)
     [RX(np.float64(1.57079...), wires=[0]),
      RZ(1.57079..., wires=[0]),
      RX(np.float64(1.57079...), wires=[0]),
-     GlobalPhase(np.float64(-1.57079...), wires=[])]
+     GlobalPhase(-1.57079..., wires=[])]
     """
 
     supported_rotations = {
