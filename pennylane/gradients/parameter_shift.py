694ae02264bc29da0eef4679f19b868dcd9c7825
Astral Cai <astral.cai@xanadu.ai>
2026-08-07 10:40:06 -0400

    oooops

--- pennylane/gradients/parameter_shift.py
+++ pennylane/gradients/parameter_shift.py
@@ -31,7 +31,7 @@ from pennylane.exceptions import (
     ParameterFrequenciesUndefinedError,
 )
 from pennylane.measurements import ExpectationMP, VarianceMP, expval
-from pennylane.ops import CRot, PauliRot, Prod, prod
+from pennylane.ops import CRot, PauliRot, Prod, Rot, prod
 from pennylane.ops.functions import eigvals, generator
 from pennylane.ops.op_math.adjoint2 import Adjoint2
 from pennylane.transforms import decompose, split_to_single_terms
@@ -1351,6 +1351,12 @@ def _handle_pauli_rot(op: PauliRot):
     ``PauliRot``.
     """
     return [(1,)]
+
+
+@parameter_frequencies.register
+def _handle_rot(op: Rot):
+    """Calculates the parameter frequencies for an Rot."""
+    return [(1,), (1,), (1,)]
 
 
 @parameter_frequencies.register
