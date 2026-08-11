f4a11a5713746e9d4f13b2ad596edf0dc1db442d
Astral Cai <astral.cai@xanadu.ai>
2026-08-11 19:31:24 -0400

    Update pennylane/core/operator/utils.py
    Co-authored-by: Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>

--- pennylane/core/operator/utils.py
+++ pennylane/core/operator/utils.py
@@ -76,7 +76,6 @@ def abstractify(val) -> AbstractArray | AbstractWires | Operator | CompressedRes
         ...
     TypeError: 'PauliRot' must set 'arg_specs' and cover all dynamic and wire arguments with fixed abstract types to be abstractified.
 
-    TypeError: 'PauliRot' must set 'arg_specs' and cover all dynamic and wire arguments with fixed abstract types to be abstractified.
 
     """
 
