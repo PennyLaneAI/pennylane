ec62044a0b0a00aef22a1d3b2b94b7596661fddd
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-08-12 16:35:47 -0400

    fix wires

--- pennylane/ops/qubit/matrix_ops.py
+++ pennylane/ops/qubit/matrix_ops.py
@@ -398,7 +398,7 @@ def _matrix_pow(U, z):
 
 @register_resources(_qubit_unitary_resource)
 def _pow_qubit_unitary(base, z, **_):
-    QubitUnitary(_matrix_pow(base.U, z), wires=wires)
+    QubitUnitary(_matrix_pow(base.U, z), wires=base.wires)
 
 
 add_decomps("Pow(QubitUnitary)", _pow_qubit_unitary)
