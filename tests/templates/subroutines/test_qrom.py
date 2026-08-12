be242950fbdca798871693de18a78a0774051a00
Mudit Pandey <mudit.pandey@xanadu.ai>
2026-08-12 17:28:18 -0400

    Fix qrom test

--- tests/templates/subroutines/test_qrom.py
+++ tests/templates/subroutines/test_qrom.py
@@ -700,7 +700,7 @@ class TestMeasurementQROM:
         assert len(ops) == 1
         assert isinstance(ops[0], qp.BasisState)
         assert ops[0].wires == qp.wires.Wires([1, 2, 3])
-        assert np.array_equal(ops.arguments["data"], np.array([1, 0, 1]))
+        assert np.array_equal(ops[0].data[0], np.array([1, 0, 1]))
 
     def test_decomposition_two_bitstrings(self):
         """Test the L == 2 branch of the measurement decomposition."""
