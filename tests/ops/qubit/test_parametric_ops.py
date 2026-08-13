daf75c2ef04f08626c43bd90a1db98d79e9d7a9f
Astral Cai <astral.cai@xanadu.ai>
2026-08-13 11:17:11 -0400

    another batch

--- tests/ops/qubit/test_parametric_ops.py
+++ tests/ops/qubit/test_parametric_ops.py
@@ -2531,6 +2531,9 @@ class TestGrad:
         assert np.allclose(np.real(res), expected, atol=tol, rtol=0)
 
     @pytest.mark.jax
+    @pytest.mark.xfail(
+        reason="jax.jacobian requires complex inputs which RX doesn't support anymore"
+    )
     @pytest.mark.parametrize("par", np.linspace(0, 2 * np.pi, 3))
     def test_qnode_with_rx_and_state_jacobian_jax(self, par, tol):
         """Test the jacobian of a complex valued QNode that contains a rotation
