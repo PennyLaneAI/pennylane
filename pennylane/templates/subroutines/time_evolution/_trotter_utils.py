d863052aef5c6a7f84808b2d3f40796c78703d75
qottmann <qottmann@gmail.com>
2026-08-21 13:12:12 +0200

    change submodule helper description

--- pennylane/templates/subroutines/time_evolution/_trotter_utils.py
+++ pennylane/templates/subroutines/time_evolution/_trotter_utils.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Scheme-agnostic scaffolding shared by the fragmented-Hamiltonian Trotter templates
+"""Shared functionality for fragmented-Hamiltonian Trotter templates
 (:class:`~.TrotterCDF` and :class:`~.TrotterCGF`)."""
 
 from pennylane import capture, compiler, math
