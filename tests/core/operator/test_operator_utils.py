cd177e2c4523e8f672fbf01755dbf230b60bf47c
Astral Cai <astral.cai@xanadu.ai>
2026-08-25 14:38:26 -0400

    revert

--- tests/core/operator/test_operator_utils.py
+++ tests/core/operator/test_operator_utils.py
@@ -93,10 +93,10 @@ class TestAbstractifyBasics:
             _ = abstractify(input)
 
     def test_abstractify_already_abstract_op(self):
-        """Tests that abstractify works on an abstract op."""
+        """Tests that the original op is returned iff it is already abstract."""
         op = DynOp(Float, wires=Wire[2])
         result = abstractify(op)
-        assert result == op
+        assert result is op
 
         op = DynOp(1.2, wires=(0, 1))
         result = abstractify(op)
