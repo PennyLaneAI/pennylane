e0e92b1c024c3011709842fb59b86bcd143ad4d8
JerryChen97 <chenys13@outlook.com>
2026-08-25 22:40:50 -0400

    with compositeop2 instead

--- pennylane/ops/op_math/composite2.py
+++ pennylane/ops/op_math/composite2.py
@@ -69,7 +69,7 @@ class CompositeOp2(Operator2, is_baseclass=True):
 
     def __repr__(self):
         return f" {self._op_symbol} ".join(
-            [f"({op})" if op.arithmetic_depth > 0 else f"{op}" for op in self]
+            [f"({op})" if getattr(op, "arithmetic_depth", 0) > 0 else f"{op}" for op in self]
         )
 
     def __iter__(self):
@@ -326,7 +326,7 @@ class CompositeOp2(Operator2, is_baseclass=True):
     @property
     @handle_recursion_error
     def arithmetic_depth(self) -> int:
-        return 1 + max(op.arithmetic_depth for op in self)
+        return 1 + max(getattr(op, "arithmetic_depth", 0) for op in self)
 
     @property
     @abc.abstractmethod
