8b77e2885874979646d56218c0ea8274fedc3716
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2026-08-31 10:56:56 -0400

    Update pennylane/labs/tests/templates/test_one_body_walk.py
    Co-authored-by: Utkarsh <utkarshazad98@gmail.com>

--- pennylane/labs/tests/templates/test_one_body_walk.py
+++ pennylane/labs/tests/templates/test_one_body_walk.py
@@ -11,9 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-Tests for the one-body qubitization walk operator.
-"""
+"""Tests for the one-body qubitization walk operator."""
 
 import numpy as np
 import pytest
