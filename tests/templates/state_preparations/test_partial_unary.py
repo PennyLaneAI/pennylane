af6323baaa843a67429b1e0f1bb4b4cf7adab6b3
dwierichs <david.wierichs@xanadu.ai>
2026-09-04 21:35:45 +0200

    abstract init

--- tests/templates/state_preparations/test_partial_unary.py
+++ tests/templates/state_preparations/test_partial_unary.py
@@ -26,6 +26,8 @@ from pennylane.templates.state_preparations.partial_unary import (
     PartialUnaryStatePreparation,
     PUIsometryFinder,
 )
+from pennylane.typing import Float, Complex, Wire
+from pennylane.wires import Wires
 
 # pylint: disable=protected-access
 
@@ -371,6 +373,17 @@ class TestPartialUnaryStatePreparation:
             coefficients, wires, indices=indices, work_wires=work_wires
         )
         assert_valid(op, skip_differentiation=True)
+
+    @pytest.mark.parametrize("coeffs", [Complex[15], Float[15], np.arange(15)/np.linalg.norm(np.arange(15))])
+    @pytest.mark.parametrize("wires", [Wire[9], Wires(range(9))])
+    @pytest.mark.parametrize("work_wires", [Wire[0], Wire[4], (), Wires(range(10, 14))])
+    def test_abstract_init(self, coeffs, wires, work_wires):
+        """Test that PartialUnaryStatePreparation can be initialized with abstract inputs."""
+        indices = tuple(range(15))
+        op = PartialUnaryStatePreparation(coeffs, wires, indices=indices, work_wires=work_wires)
+        assert len(op.wires) == 9
+        assert len(op.coefficients) == 15
+        assert len(op.indices) == 15
 
     @pytest.mark.catalyst
     @pytest.mark.parametrize("provide_work_wires", [False, True])
