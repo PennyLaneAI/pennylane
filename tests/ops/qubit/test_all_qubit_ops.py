71816aae70711cd03ef1c54df8434477ff3f5e35
andrijapau <andrija.paurevic@xanadu.ai>
2026-09-08 14:50:02 -0400

    expand comment

--- tests/ops/qubit/test_all_qubit_ops.py
+++ tests/ops/qubit/test_all_qubit_ops.py
@@ -40,7 +40,7 @@ import pennylane as qp
         (qp.PCPhase(1.23, 7, (1, 2, 3)), "Z"),
         (qp.X(0) + qp.Y(0), None),
         (qp.X(0) @ qp.Y(1), None),
-        # NOTE: @ dispatches to Prod2 now, keep Prod for legacy testing
+        # NOTE: @ dispatches to Prod2 now and doesn't have basis method, keep Prod for legacy testing
         (qp.ops.Prod(qp.X(0), qp.Y(1)), None),
     ],
 )
