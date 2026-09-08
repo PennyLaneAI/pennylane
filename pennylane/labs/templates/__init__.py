852d341204ea94eb6d409347ad785ea87c9e6868
ddhawan11 <ddhawan@umich.edu>
2026-09-07 23:47:57 -0400

    Add code and tests

--- pennylane/labs/templates/__init__.py
+++ pennylane/labs/templates/__init__.py
@@ -33,3 +33,4 @@ from .superposition_thc import SuperpositionTHC
 from .half_signed_out_multiplier import half_signed_out_multiplier
 from .sum_of_slaters2 import SumOfSlatersPrep2
 from .trotter_fragmented import trotter_fragmented
+from .select_thc import select_thc, select_thc_wires
