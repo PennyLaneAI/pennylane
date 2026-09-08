23d016633d15abc42d64d581dd449e091afa7957
JerryChen97 <chenys13@outlook.com>
2026-09-08 10:06:50 -0400

    Revert https://github.com/PennyLaneAI/pennylane/pull/10115/commits/7a68ccc01ef14285d72b54f896d858b84a3b5a56

--- tests/resource/test_specs.py
+++ tests/resource/test_specs.py
@@ -333,7 +333,7 @@ class TestSpecsTransform:
         info = qp.specs(circuit)()
 
         assert info.resources == SpecsResources(
-            counts={"MidMeasure": 1},
+            counts={"MidMeasureMP": 1},
             measurement_processes={"sample(mcm)": 1},
             num_wires=1,
             circuit_depth=1,
