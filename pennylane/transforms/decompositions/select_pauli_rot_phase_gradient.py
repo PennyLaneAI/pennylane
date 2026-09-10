f447a6f0cdd14e24dd5e322a3e348f4c98a4968a
andrijapau <andrija.paurevic@xanadu.ai>
2026-09-10 16:15:38 -0400

    rename selectpaulirot decomp

--- pennylane/transforms/decompositions/select_pauli_rot_phase_gradient.py
+++ pennylane/transforms/decompositions/select_pauli_rot_phase_gradient.py
@@ -240,8 +240,8 @@ def make_selectpaulirot_to_phase_gradient_decomp(angle_wires, phase_grad_wires, 
         resources = {change_basis_rep_basis_adapted: 1}
         return resources
 
-    @qp.register_resources(_resource_fn, name="select_pauli_rot_phase_gradient")
-    def _decomp_fn(angles, control_wires, target_wire, rot_axis, **_):
+    @qp.register_resources(_resource_fn)
+    def _select_pauli_rot_phase_gradient_decomp(angles, control_wires, target_wire, rot_axis, **_):
         if len(control_wires) == 0:
             match rot_axis:
                 case "X":
@@ -269,4 +269,4 @@ def make_selectpaulirot_to_phase_gradient_decomp(angle_wires, phase_grad_wires, 
             work_wires=work_wires,
         )
 
-    return _decomp_fn
+    return _select_pauli_rot_phase_gradient_decomp
