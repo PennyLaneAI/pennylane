39d360bffb9621f0853c980bb4a16fe4cefa47df
dwierichs <david.wierichs@xanadu.ai>
2026-09-14 14:50:24 +0200

    device pointer :lightning qubit executed Pauli measure

--- pennylane/ops/mid_measure/pauli_measure.py
+++ pennylane/ops/mid_measure/pauli_measure.py
@@ -153,9 +153,8 @@ def pauli_measure(pauli_word: str, wires: WiresLike, postselect: int | None = No
 
     .. note::
 
-        Circuits comprising ``pauli_measure`` are currently not executable on any backend.
-        This function is only for analysis using the ``null.qubit`` device and potential future
-        execution when a suitable backend is available.
+        Circuits comprising ``pauli_measure`` are currently only executable with
+        :func:`~.qjit` and lightning qubit.
 
     .. seealso::
         For more information on Pauli product measurements, check out the
