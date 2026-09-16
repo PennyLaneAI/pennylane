4eee8762a1f4b6d0b9ad7bc6eef82664cefdeb9f
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-09-16 09:42:55 -0400

    Circular import

--- pennylane/templates/subroutines/time_evolution/trotter_vibronic.py
+++ pennylane/templates/subroutines/time_evolution/trotter_vibronic.py
@@ -26,7 +26,6 @@ from pennylane.core.operator import Operator2
 from pennylane.decomposition import add_decomps, register_resources
 from pennylane.numeric_hamiltonians import VibronicHamiltonian
 from pennylane.ops import CNOT, Hadamard, adjoint, cond, ctrl
-from pennylane.templates.subroutines.multix import MultiX
 from pennylane.typing import AbstractWires, Float, Wire
 from pennylane.wires import Wires, WiresLike
 
@@ -36,6 +35,7 @@ from ..arithmetic.out_multiplier import OutMultiplier
 from ..arithmetic.semi_adder import SemiAdder
 from ..arithmetic.signed_out_multiplier import SignedOutMultiplier, _twos_complement_helper
 from ..arithmetic.signed_out_square import SignedOutSquare
+from ..multix import MultiX
 from ..qrom import QROM
 
 
