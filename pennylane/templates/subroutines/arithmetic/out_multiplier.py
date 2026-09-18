dfd3a4bcd0688f469c47537c5a77383de613489a
Astral Cai <astral.cai@xanadu.ai>
2026-09-18 14:26:10 -0400

    OutMultiplier decomp rules work with wires as traced arrays

--- pennylane/templates/subroutines/arithmetic/out_multiplier.py
+++ pennylane/templates/subroutines/arithmetic/out_multiplier.py
@@ -510,7 +510,7 @@ def _adder_flipped_first_work_wire(x_wires, y_wires, work_wires, flip_control=No
     unchanged. We only expect this function to be used with two values for `flip_control`: None
     or a tuple ``(c_wire, c_val)`` for a single control wire.
     """
-    if not work_wires:
+    if len(work_wires) == 0:
         _semi_adder(x_wires, y_wires, work_wires)
         return
 
@@ -633,7 +633,7 @@ def _out_multiplier_with_caddsub(
     """
     # We extend our output by one wire because we need to store 2x*y intermediately, instead
     # of x*y. This also multiplies the value stored in output_wires with two.
-    output_wires = output_wires + [work_wires[0]]
+    output_wires = list(output_wires) + [work_wires[0]]
     # The other work wires can be used for arithmetic building blocks
     work_wires = work_wires[1:]
     n = len(x_wires)
