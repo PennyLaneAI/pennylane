0fc743043bd7d0feb9b4dcb58f9aa83bc9819205
dwierichs <david.wierichs@xanadu.ai>
2026-09-23 22:35:14 +0200

    gqsp for_loop

--- pennylane/templates/subroutines/gqsp.py
+++ pennylane/templates/subroutines/gqsp.py
@@ -15,17 +15,12 @@
 Contains the GQSP template.
 """
 
-from pennylane import capture, math, ops
+from pennylane import capture, compiler, math, ops
+from pennylane.control_flow import for_loop
 from pennylane.core.operator import Operator2, abstractify
 from pennylane.decomposition import add_decomps, register_resources
-from pennylane.ops.op_math.controlled2 import _ctrl_abstract
+from pennylane.ops.op_math.controlled2 import _ctrl_abstract, _validate_work_wire_type
 from pennylane.typing import Float, Wire
-
-has_jax = True
-try:
-    from jax import numpy as jnp
-except (ModuleNotFoundError, ImportError) as import_error:  # pragma: no cover
-    has_jax = False  # pragma: no cover
 
 
 class GQSP(Operator2):
@@ -88,33 +83,45 @@ class GQSP(Operator2):
     """
 
     dynamic_argnames = ("angles",)
+    static_argnames = ("work_wire_type",)
     hybrid_argnames = ("unitary",)
-    wire_argnames = ("control",)
+    wire_argnames = ("control", "work_wires")
 
-    arg_specs = {"angles": Float[3, -1], "control": Wire[1]}
+    arg_specs = {"angles": Float[3, -1], "control": Wire[1], "work_wires": Wire[-1]}
 
-    def __init__(self, unitary, angles, control):
+    def __init__(self, unitary, angles, control, work_wires, work_wire_type):
+        # pylint: disable=too-many-arguments
+        _validate_work_wire_type(work_wire_type)
         if isinstance(angles, (list, tuple)):
             angles = math.stack(angles)
-        super().__init__(unitary, angles, control)
+        super().__init__(unitary, angles, control, work_wires, work_wire_type)
 
 
-def _GQSP_resources(unitary, angles, control):  # pylint: disable=unused-argument
+def _GQSP_resources(
+    unitary, angles, control, work_wires, work_wire_type
+):  # pylint: disable=unused-argument
     num_iters = angles.shape[1]
     return {
         ops.X: 2 + 2 * (num_iters - 1),
         ops.U3: num_iters,
         ops.Z: num_iters,
-        _ctrl_abstract(abstractify(unitary), Wire[1], num_zero_control_values=1): num_iters - 1,
+        _ctrl_abstract(
+            abstractify(unitary),
+            Wire[1],
+            num_zero_control_values=1,
+            work_wires=Wire[len(work_wires)],
+            work_wire_type=work_wire_type,
+        ): num_iters
+        - 1,
     }
 
 
 @register_resources(_GQSP_resources)
-def _GQSP_decomposition(unitary, angles, control):
-    thetas, phis, lambdas = angles[0], angles[1], angles[2]
+def _GQSP_decomposition(unitary, angles, control, work_wires, work_wire_type):
+    if compiler.active() or capture.enabled():
+        angles = math.array(angles, like="jax")
 
-    if has_jax and capture.enabled():
-        thetas, phis, lambdas = jnp.array(thetas), jnp.array(phis), jnp.array(lambdas)
+    thetas, phis, lambdas = angles[0], angles[1], angles[2]
 
     # These four gates adapt PennyLane's ops.U3 to the chosen U3 format in the GQSP paper.
     ops.X(control)
@@ -122,13 +129,21 @@ def _GQSP_decomposition(unitary, angles, control):
     ops.X(control)
     ops.Z(control)
 
-    for theta, phi, lamb in zip(thetas[1:], phis[1:], lambdas[1:], strict=True):
-        ops.ctrl(unitary, control=control, control_values=[0])
-
+    @for_loop(len(thetas) - 1)
+    def gqsp_loop(i):
+        ops.ctrl(
+            unitary,
+            control=control,
+            control_values=[0],
+            work_wires=work_wires,
+            work_wire_type=work_wire_type,
+        )
         ops.X(control)
-        ops.U3(2 * theta, phi, lamb, wires=control)
+        ops.U3(2 * thetas[i], phis[i], lambdas[i], wires=control)
         ops.X(control)
         ops.Z(control)
+
+    gqsp_loop()  # pylint:disable= no-value-for-parameter
 
 
 add_decomps(GQSP, _GQSP_decomposition)
