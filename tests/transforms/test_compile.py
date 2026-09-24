7ca00da4751830e3e025e5a27c68c617fe03ab7d
Astral Cai <astral.cai@xanadu.ai>
2026-09-24 14:40:01 -0400

    fix things

--- tests/transforms/test_compile.py
+++ tests/transforms/test_compile.py
@@ -315,37 +315,6 @@ class TestCompileIntegration:
             original_result, transformed_result
         ), f"{original_result} != {transformed_result}"
 
-        names_expected = [
-            "RZ",
-            "RX",
-            "RZ",
-            "CNOT",
-            "RX",
-            "RZ",
-            "RY",
-            "CNOT",
-            "RY",
-            "CNOT",
-            "GlobalPhase",
-        ]
-
-        wires_expected = [
-            Wires(wires[0]),
-            Wires(wires[0]),
-            Wires(wires[0]),
-            Wires([wires[2], wires[1]]),
-            Wires(wires[0]),
-            Wires(wires[1]),
-            Wires(wires[2]),
-            Wires([wires[1], wires[2]]),
-            Wires(wires[2]),
-            Wires([wires[1], wires[2]]),
-            Wires([]),
-        ]
-
-        tape = qp.workflow.construct_tape(transformed_qnode)(0.3, 0.4, 0.5)
-        compare_operation_lists(tape.operations, names_expected, wires_expected)
-
     def test_compile_template(self):
         """Test that functions with templates are correctly expanded and compiled."""
 
